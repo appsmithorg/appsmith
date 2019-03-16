94fba22aad07a35c5e7fee74c330ab8918d3f283
Arpit Mohan <me@arpitmohan.com>
2019-03-16 18:28:44 +0530

    Adding functionality to execute a query via mustache template.

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module gitlab.com/mobtools/internal-tools-server
 
 require (
+	github.com/cbroglie/mustache v1.0.1
 	github.com/gorilla/mux v1.6.2
 	github.com/jinzhu/gorm v1.9.2
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
--- services/query.go
+++ services/query.go
@@ -2,7 +2,9 @@ package services
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/cbroglie/mustache"
 	"github.com/jinzhu/gorm"
 	"gitlab.com/mobtools/internal-tools-server/models"
 	"gitlab.com/mobtools/internal-tools-server/storage"
@@ -10,25 +12,42 @@ import (
 
 // ExecuteQuery runs a custom SQL query on the client database
 func ExecuteQuery(queryBody models.ExecQuery) ([]map[string]interface{}, error) {
-	if queryBody.QueryType == "sql" {
-		// Get the actual query from the DB
-		datastore := storage.StorageEngine.GetDatastore()
-		queryDAO := &models.Query{}
+	datastore := storage.StorageEngine.GetDatastore()
+	queryDAO := &models.Query{}
 
-		if err := datastore.Where("name = ?", queryBody.Name).First(queryDAO).Error; gorm.IsRecordNotFoundError(err) {
-			return nil, fmt.Errorf("Invalid queryName: %s provided", queryBody.Name)
+	if err := datastore.Where("name = ?", queryBody.Name).First(queryDAO).Error; gorm.IsRecordNotFoundError(err) {
+		return nil, fmt.Errorf("Invalid queryName: %s provided", queryBody.Name)
+	}
+
+	//TODO: Move to a factory method for better readability
+	if queryDAO.QueryType == "sql" {
+		// Get the actual query from the DB
+		queryStr := queryDAO.Executable
+
+		// Extract the key-value pairs from the request
+		templateKeyValue := make(map[string]string)
+		if len(queryBody.Params.QueryParams) > 0 {
+			for _, elem := range queryBody.Params.QueryParams {
+				templateKeyValue[elem.Key] = elem.Value
+			}
 		}
 
-		queryStr := queryDAO.Executable
-		mapArray, err := storage.StorageEngine.ExecuteQuery(queryStr)
+		// Substitute it in the template string
+		log.Printf("Going to parse string: %s", queryStr)
+		renderedStr, err := mustache.Render(queryStr, templateKeyValue)
+		if err != nil {
+			log.Printf("Error while parsing the mustache template. %s", err.Error())
+			return nil, err
+		}
 
+		mapArray, err := storage.StorageEngine.ExecuteQuery(renderedStr)
 		if err != nil {
 			return nil, err
 		}
 		return mapArray, nil
 	}
 
-	return nil, fmt.Errorf("QueryType: %s not supported", queryBody.QueryType)
+	return nil, fmt.Errorf("QueryType: %s not supported", queryDAO.QueryType)
 }
 
 // CreateQuery creates a new query that can be executed by name at runtime
