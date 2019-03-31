5570020f6dce86e75f78df2be4d593f5a6660d89
Arpit Mohan <me@arpitmohan.com>
2019-03-31 21:49:39 +0530

    Adding Configuration annotation on CORS config class

--- app/server/src/main/java/com/mobtools/server/configurations/CorsGlobalConfiguration.java
+++ app/server/src/main/java/com/mobtools/server/configurations/CorsGlobalConfiguration.java
@@ -1,8 +1,10 @@
 package com.mobtools.server.configurations;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+@Configuration
 public class CorsGlobalConfiguration implements WebFluxConfigurer {
 
     @Override
