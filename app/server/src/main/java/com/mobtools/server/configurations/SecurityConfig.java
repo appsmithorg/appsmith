6d91ab32ce4309897249dc048652ec188250b949
Arpit Mohan <me@arpitmohan.com>
2019-04-02 20:05:48 +0530

    Adding the bean corsWebFilter to enable CORS in our codebase

--- app/server/src/main/java/com/mobtools/server/configurations/SecurityConfig.java
+++ app/server/src/main/java/com/mobtools/server/configurations/SecurityConfig.java
@@ -13,13 +13,36 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.config.EnableWebFlux;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFlux
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
+
+    /**
+     * This configuration enables CORS requests for the most common HTTP Methods
+     * @return
+     */
+    @Bean
+    CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setMaxAge(8000L);
+        corsConfig.setAllowedHeaders(Arrays.asList("GET", "PUT", "POST", "HEAD", "OPTIONS", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
 
     @Bean
     public MapReactiveUserDetailsService userDetailsService() {
