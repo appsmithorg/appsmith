ae0fd72d48daa602acf783db05116eb9a144ffb3
Trisha Anand <trisha@appsmith.com>
2019-07-28 04:46:53 +0000

    The create tenant flow is completely reactive. This was done because the earstwhile implementation was erroneous leading to unpredictable crashes.

--- app/server/src/main/java/com/mobtools/server/controllers/TenantController.java
+++ app/server/src/main/java/com/mobtools/server/controllers/TenantController.java
@@ -2,15 +2,10 @@ package com.mobtools.server.controllers;
 
 import com.mobtools.server.constants.Url;
 import com.mobtools.server.domains.Tenant;
-import com.mobtools.server.dtos.ResponseDto;
 import com.mobtools.server.services.TenantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(Url.TENANT_URL)
@@ -20,12 +15,6 @@ public class TenantController extends BaseController<TenantService, Tenant, Stri
     @Autowired
     public TenantController(TenantService tenantService) {
         super(tenantService);
-    }
-
-    @GetMapping("/{name}")
-    public Mono<ResponseDto<Tenant>> getByName(@PathVariable String name) {
-        return service.getByName(name)
-                .map(widget -> new ResponseDto<>(HttpStatus.OK.value(), widget, null));
     }
 
 }
