7ba9dea9a0003324a061718955988a974ae1bf68
Trisha Anand <trisha@appsmith.com>
2019-08-27 11:31:44 +0000

    Action CRUD implemented

---
+++ src/main/java/com/appsmith/server/repositories/ActionRepository.java
@@ -0,0 +1,8 @@
+package com.appsmith.server.repositories;
+
+import com.appsmith.server.domains.Action;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActionRepository extends BaseRepository<Action, String> {
+}
--- src/main/java/com/appsmith/server/repositories/ResourceRepository.java
+++ src/main/java/com/appsmith/server/repositories/ResourceRepository.java
@@ -2,7 +2,9 @@ package com.appsmith.server.repositories;
 
 import com.appsmith.server.domains.Resource;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface ResourceRepository extends BaseRepository<Resource, String>{
+public interface ResourceRepository extends BaseRepository<Resource, String> {
+    Mono<Resource> findByName(String name);
 }
