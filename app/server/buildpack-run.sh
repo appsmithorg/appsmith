f5ea85f6366454497df85735b204bbe9d6c31a16
Arpit Mohan <arpit@appsmith.com>
2019-09-16 17:19:55 +0530

    Running only part of build.sh in the buildpack-run.sh. This is because all the buildpacks in Heroku are run sequentially

--- app/server/buildpack-run.sh
+++ app/server/buildpack-run.sh
@@ -1,1 +1,8 @@
-./build.sh -DskipTests
+# Create the dist directory
+mkdir -p dist/plugins
+
+# Copy the server jar
+cp ./appsmith-server/target/server-1.0-SNAPSHOT.jar dist/
+
+# Copy all the plugins
+cp ./appsmith-plugins/*/target/*.jar dist/plugins/
