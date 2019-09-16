a8adc4e5f30b81c9f64f4729f762d4d440b2c401
Nikhil Nandagopal <nikhil@appsmith.com>
2019-09-16 16:07:38 +0530

    ran lint & prettier manually

--- app/client/src/api/ActionAPI.tsx
+++ app/client/src/api/ActionAPI.tsx
@@ -1,71 +1,76 @@
-import Api, { HttpMethod } from "./Api"
-import { ApiResponse } from "./ApiResponses"
-import { APIRequest } from './ApiRequests';
+import Api, { HttpMethod } from "./Api";
+import { ApiResponse } from "./ApiResponses";
+import { APIRequest } from "./ApiRequests";
 
 export interface CreateActionRequest<T> extends APIRequest {
-  resourceId: string
-  actionConfiguration: T
+  resourceId: string;
+  actionConfiguration: T;
 }
 
 export interface UpdateActionRequest<T> extends CreateActionRequest<T> {
-  actionId: string
+  actionId: string;
 }
 
 export interface APIConfig {
-  requestHeaders: Record<string, string>
-  method: HttpMethod
-  path: string
-  APIName: string
-  body: JSON
-  queryParams: Record<string, string>
+  requestHeaders: Record<string, string>;
+  method: HttpMethod;
+  path: string;
+  APIName: string;
+  body: JSON;
+  queryParams: Record<string, string>;
 }
 
 export interface QueryConfig {
-  queryString: string
+  queryString: string;
 }
 
-export interface ActionCreatedResponse extends ApiResponse {
-  actionId: string
-  dynamicBindingMap: Record<string, string>
-}
-
-export interface ActionUpdatedResponse extends ActionCreatedResponse {
-  
+export interface ActionCreateUpdateResponse extends ApiResponse {
+  actionId: string;
+  dynamicBindingMap: Record<string, string>;
 }
 
 export interface ExecuteActionRequest extends APIRequest {
-  actionId: string
-  dynamicBindingMap: Record<string, any>
+  actionId: string;
+  dynamicBindingMap: Record<string, any>;
 }
 
 export interface ExecuteActionResponse extends ApiResponse {
-  actionId: string
-  data: any
+  actionId: string;
+  data: any;
 }
 
 class ActionAPI extends Api {
-  static url = "/actions"
-  
-  static createAPI(createAPI: CreateActionRequest<APIConfig>): Promise<ActionCreatedResponse> {
-    return Api.post(ActionAPI.url, createAPI)
+  static url = "/actions";
+
+  static createAPI(
+    createAPI: CreateActionRequest<APIConfig>,
+  ): Promise<ActionCreateUpdateResponse> {
+    return Api.post(ActionAPI.url, createAPI);
   }
 
-  static updateAPI(updateAPI: UpdateActionRequest<APIConfig>): Promise<ActionUpdatedResponse> {
-    return Api.post(ActionAPI.url, updateAPI)
+  static updateAPI(
+    updateAPI: UpdateActionRequest<APIConfig>,
+  ): Promise<ActionCreateUpdateResponse> {
+    return Api.post(ActionAPI.url, updateAPI);
   }
 
-  static createQuery(createQuery: CreateActionRequest<QueryConfig>): Promise<ActionCreatedResponse> {
-    return Api.post(ActionAPI.url, createQuery)
+  static createQuery(
+    createQuery: CreateActionRequest<QueryConfig>,
+  ): Promise<ActionCreateUpdateResponse> {
+    return Api.post(ActionAPI.url, createQuery);
   }
 
-  static updateQuery(updateQuery: UpdateActionRequest<QueryConfig>): Promise<ActionUpdatedResponse> {
-    return Api.post(ActionAPI.url, updateQuery)
+  static updateQuery(
+    updateQuery: UpdateActionRequest<QueryConfig>,
+  ): Promise<ActionCreateUpdateResponse> {
+    return Api.post(ActionAPI.url, updateQuery);
   }
 
-  static executeAction(executeAction: ExecuteActionRequest): Promise<ActionUpdatedResponse> {
-    return Api.post(ActionAPI.url, executeAction)
+  static executeAction(
+    executeAction: ExecuteActionRequest,
+  ): Promise<ActionCreateUpdateResponse> {
+    return Api.post(ActionAPI.url, executeAction);
   }
-
 }
 
-export default ActionAPI
+export default ActionAPI;
--- app/client/src/sagas/ActionSagas.tsx
+++ app/client/src/sagas/ActionSagas.tsx
@@ -1,59 +1,98 @@
-import CanvasWidgetsNormalizer from "../normalizers/CanvasWidgetsNormalizer"
-import { ReduxActionTypes, ReduxAction } from "../constants/ReduxActionConstants"
-import PageApi, { PageResponse, PageRequest } from "../api/PageApi"
-import { call, put, takeEvery, select, all } from "redux-saga/effects"
-import { RenderModes } from "../constants/WidgetConstants"
-import { APIActionPayload, QueryActionPayload, PageAction } from '../constants/ActionConstants';
-import ActionAPI, { ActionCreatedResponse } from '../api/ActionAPI';
-import { AppState } from '../reducers';
-import {JSONPath} from 'jsonpath-plus';
-import _ from "lodash"
+import CanvasWidgetsNormalizer from "../normalizers/CanvasWidgetsNormalizer";
+import {
+  ReduxActionTypes,
+  ReduxAction,
+} from "../constants/ReduxActionConstants";
+import PageApi, { PageResponse, PageRequest } from "../api/PageApi";
+import { call, put, takeEvery, select, all } from "redux-saga/effects";
+import { RenderModes } from "../constants/WidgetConstants";
+import {
+  APIActionPayload,
+  QueryActionPayload,
+  PageAction,
+} from "../constants/ActionConstants";
+import ActionAPI, { ActionCreateUpdateResponse } from "../api/ActionAPI";
+import { AppState } from "../reducers";
+import { JSONPath } from "jsonpath-plus";
+import _ from "lodash";
 
 const getDataTree = (state: AppState) => {
-  return state.entities
-}
+  return state.entities;
+};
 
-const getAction = (state: AppState, actionId: string): ActionCreatedResponse => {
-  return state.entities.actions[actionId]
-}
+const getAction = (
+  state: AppState,
+  actionId: string,
+): ActionCreateUpdateResponse => {
+  return state.entities.actions[actionId];
+};
 
 export function* evaluateJSONPath(jsonPath: string): any {
-  const dataTree = yield select(getDataTree)
-  const result = JSONPath({path: jsonPath, json: dataTree})
-  return result
+  const dataTree = yield select(getDataTree);
+  const result = JSONPath({ path: jsonPath, json: dataTree });
+  return result;
 }
 
 export function* executeAPIAction(apiAction: APIActionPayload) {
-  const api: PageAction = yield select(getAction, apiAction.apiId)
-  const responses: any = yield all(api.dynamicBindings.map((jsonPath: string) => { return call(evaluateJSONPath, jsonPath)}))
-  const dynamicBindingMap: Record<string, any> = _.keyBy(responses, (response: string, index: number) => { return api.dynamicBindings[index] })
-  yield ActionAPI.executeAction({ actionId: apiAction.apiId, dynamicBindingMap: dynamicBindingMap })
+  const api: PageAction = yield select(getAction, apiAction.apiId);
+  const responses: any = yield all(
+    api.dynamicBindings.map((jsonPath: string) => {
+      return call(evaluateJSONPath, jsonPath);
+    }),
+  );
+  const dynamicBindingMap: Record<string, any> = _.keyBy(
+    responses,
+    (response: string, index: number) => {
+      return api.dynamicBindings[index];
+    },
+  );
+  yield ActionAPI.executeAction({
+    actionId: apiAction.apiId,
+    dynamicBindingMap: dynamicBindingMap,
+  });
 }
 
 export function* executeQueryAction(queryAction: QueryActionPayload) {
-  const query: PageAction = yield select(getAction, queryAction.queryId)
-  const responses: any = yield all(query.dynamicBindings.map((jsonPath: string) => { return call(evaluateJSONPath, jsonPath)}))
-  const dynamicBindingMap: Record<string, any> = _.keyBy(responses, (response: string, index: number) => { return query.dynamicBindings[index] })
-  yield ActionAPI.executeAction({ actionId: query.actionId, dynamicBindingMap: dynamicBindingMap })
+  const query: PageAction = yield select(getAction, queryAction.queryId);
+  const responses: any = yield all(
+    query.dynamicBindings.map((jsonPath: string) => {
+      return call(evaluateJSONPath, jsonPath);
+    }),
+  );
+  const dynamicBindingMap: Record<string, any> = _.keyBy(
+    responses,
+    (response: string, index: number) => {
+      return query.dynamicBindings[index];
+    },
+  );
+  yield ActionAPI.executeAction({
+    actionId: query.actionId,
+    dynamicBindingMap: dynamicBindingMap,
+  });
 }
 
 export function* executeAction(pageRequestAction: ReduxAction<PageRequest>) {
-  const pageRequest = pageRequestAction.payload
+  const pageRequest = pageRequestAction.payload;
   try {
-    const pageResponse: PageResponse = yield call(PageApi.fetchPage, pageRequest)
+    const pageResponse: PageResponse = yield call(
+      PageApi.fetchPage,
+      pageRequest,
+    );
     if (pageRequest.renderMode === RenderModes.CANVAS) {
-      const normalizedResponse = CanvasWidgetsNormalizer.normalize(pageResponse)
+      const normalizedResponse = CanvasWidgetsNormalizer.normalize(
+        pageResponse,
+      );
       const payload = {
         pageWidgetId: normalizedResponse.result,
-        widgets: normalizedResponse.entities.canvasWidgets
-      }
-      yield put({ type: ReduxActionTypes.UPDATE_CANVAS, payload })
+        widgets: normalizedResponse.entities.canvasWidgets,
+      };
+      yield put({ type: ReduxActionTypes.UPDATE_CANVAS, payload });
     }
-  } catch(err){
+  } catch (err) {
     //TODO(abhinav): REFACTOR THIS
   }
 }
 
 export function* watchExecuteAction() {
-  yield takeEvery(ReduxActionTypes.EXECUTE_ACTION, executeAction)
+  yield takeEvery(ReduxActionTypes.EXECUTE_ACTION, executeAction);
 }
