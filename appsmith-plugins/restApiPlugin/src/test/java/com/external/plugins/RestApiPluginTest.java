31df5aa5aecba015e42ce7537420dfc92c3c2f21
Arpit Mohan <arpit@appsmith.com>
2019-09-17 12:24:45 +0000

    Adding a very initial version of the RestApiPlugin
    Requires a bunch of testing for corner cases and error scenarios.

---
+++ appsmith-plugins/restApiPlugin/src/test/java/com/external/plugins/RestApiPluginTest.java
@@ -0,0 +1,20 @@
+package com.external.plugins;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RestApiPluginTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
