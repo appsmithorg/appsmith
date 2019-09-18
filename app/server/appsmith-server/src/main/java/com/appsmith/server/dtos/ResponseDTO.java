ac44e2c970e8ba44a0918401c9e4044be923ae4d
Arpit Mohan <arpit@appsmith.com>
2019-09-18 08:52:14 +0000

    Modifying the ResponseDTO to contain the ResponseMeta object. Also moving ErrorDTO inside ResponseMeta object.
    This also fixes the logic for multiple ResponseDTOs that were being sent when the service level response was a Flux. Now we only return a single array inside a Mono ResponseDTO object.

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ResponseDTO.java
@@ -0,0 +1,39 @@
+package com.appsmith.server.dtos;
+
+import com.appsmith.server.exceptions.ErrorDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO<T> implements Serializable {
+
+    private static final long serialVersionUID = 8965011907233699993L;
+
+    private ResponseMetaDTO responseMeta;
+
+    private T data;
+
+    public ResponseDTO(int status, T data, String message) {
+        this.responseMeta = new ResponseMetaDTO(status, message);
+        this.data = data;
+    }
+
+    public ResponseDTO(int status, T data, String message, boolean success) {
+        this.responseMeta = new ResponseMetaDTO(status, message, success);
+        this.data = data;
+    }
+
+    public ResponseDTO(int status, ErrorDTO errorDTO) {
+        this.responseMeta = new ResponseMetaDTO(status, errorDTO);
+    }
+
+}
