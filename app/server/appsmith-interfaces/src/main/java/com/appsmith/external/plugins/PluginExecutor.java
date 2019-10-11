e1d59ca3a6c41bf1ece319ca9a9b74f8a678804f
Trisha Anand <trisha@appsmith.com>
2019-10-11 10:32:18 +0000

    Multi data source support for plugins using Resource Context

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/plugins/PluginExecutor.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/plugins/PluginExecutor.java
@@ -11,5 +11,27 @@ import java.util.List;
 
 public interface PluginExecutor extends ExtensionPoint {
 
-    Mono<ActionExecutionResult> execute(ResourceConfiguration resourceConfiguration, ActionConfiguration action, List<Param> params);
+    /**
+     * This function is used to execute the action. 
+     * @param connection : This is the connection that is established to the data source. This connection is according
+     *                   to the parameters in Resource Configuration
+     * @param resourceConfiguration : These are the configurations which have been used to create a Resource from a Plugin
+     * @param actionConfiguration : These are the configurations which have been used to create an Action from a Resource.
+     * @return ActionExecutionResult : This object is returned to the user which contains the result values from the execution.
+     */
+    Mono<ActionExecutionResult> execute(Object connection, ResourceConfiguration resourceConfiguration, ActionConfiguration actionConfiguration);
+
+    /**
+     * This function is responsible for creating the connection to the data source and returning the connection variable
+     * on success. For executing actions, this connection object would be passed for each function call.
+     * @param resourceConfiguration
+     * @return Connection object
+     */
+    Object resourceCreate(ResourceConfiguration resourceConfiguration);
+
+    /**
+     * This function is used to bring down/destroy the connection to the data source.
+     * @param connection
+     */
+    void resourceDestroy(Object connection);
 }
---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ResourceContext.java
@@ -0,0 +1,14 @@
+package com.appsmith.server.domains;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ResourceContext {
+    Object connection;
+}
