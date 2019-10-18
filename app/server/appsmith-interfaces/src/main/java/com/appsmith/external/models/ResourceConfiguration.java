84837fe57f39198ae5626518ae8726e7a1fb27f7
Trisha Anand <trisha@appsmith.com>
2019-10-18 08:27:19 +0000

    Mongo Plugin works here. Barring the scenarios where the query json contains special characters in keys ($, .) it works properly.

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/ResourceConfiguration.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/ResourceConfiguration.java
@@ -23,4 +23,9 @@ public class ResourceConfiguration {
 
     //For REST API
     List<Property> headers;
+
+    //This field is for plugins which allow the database name to be specified outside of the connection URL. The
+    //expectation from the plugins is that if the database name has been provided via this field, the database name
+    //should be according to this, else if database name is null, pick the database name from the URL.
+    String databaseName;
 }
