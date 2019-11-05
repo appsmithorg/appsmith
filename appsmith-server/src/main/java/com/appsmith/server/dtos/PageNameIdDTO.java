f93be715c69f746a849aceea84f68bca6e24c0df
Trisha Anand <trisha@appsmith.com>
2019-11-05 11:36:10 +0000

    Page Controller now can return list of pages by application id and list of page names & ids by application id

---
+++ appsmith-server/src/main/java/com/appsmith/server/dtos/PageNameIdDTO.java
@@ -0,0 +1,9 @@
+package com.appsmith.server.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public interface PageNameIdDTO {
+    public String getId();
+    public String getName();
+}
--- appsmith-server/src/main/java/com/appsmith/server/services/PageService.java
+++ appsmith-server/src/main/java/com/appsmith/server/services/PageService.java
@@ -1,6 +1,8 @@
 package com.appsmith.server.services;
 
 import com.appsmith.server.domains.Page;
+import com.appsmith.server.dtos.PageNameIdDTO;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface PageService extends CrudService<Page, String> {
@@ -16,4 +18,6 @@ public interface PageService extends CrudService<Page, String> {
     Mono<Page> findByName(String name);
 
     Mono<Void> deleteAll();
+
+    Flux<PageNameIdDTO> findNamesByApplicationId(String applicationId);
 }
