9ba4ad2d7019fcc75b8325c7f9e70cee40340a21
Trisha Anand <trisha@appsmith.com>
2019-11-05 09:39:54 +0000

    Added ActionCollectionService which would act on composite functions. Need to handle all scenarios here.

--- appsmith-server/src/main/java/com/appsmith/server/controllers/ActionController.java
+++ appsmith-server/src/main/java/com/appsmith/server/controllers/ActionController.java
@@ -3,20 +3,53 @@ package com.appsmith.server.controllers;
 import com.appsmith.external.models.ActionExecutionResult;
 import com.appsmith.server.constants.Url;
 import com.appsmith.server.domains.Action;
+import com.appsmith.server.domains.Collection;
 import com.appsmith.server.dtos.ExecuteActionDTO;
+import com.appsmith.server.dtos.ResponseDTO;
+import com.appsmith.server.exceptions.AppsmithException;
+import com.appsmith.server.services.ActionCollectionService;
 import com.appsmith.server.services.ActionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping(Url.ACTION_URL)
+@Slf4j
 public class ActionController extends BaseController<ActionService, Action, String> {
 
-    public ActionController(ActionService service) {
+    private final ActionCollectionService actionCollectionService;
+
+    @Autowired
+    public ActionController(ActionService service,
+                            ActionCollectionService actionCollectionService) {
         super(service);
+        this.actionCollectionService = actionCollectionService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<ResponseDTO<Action>> create(@Valid @RequestBody Action resource) throws AppsmithException {
+        log.debug("Going to create resource {}", resource.getClass().getName());
+        return actionCollectionService.createAction(resource)
+                .map(created -> new ResponseDTO<>(HttpStatus.CREATED.value(), created, null));
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseDTO<Action>> update(@PathVariable String id, @RequestBody Action resource) {
+        log.debug("Going to update resource with id: {}", id);
+        return actionCollectionService.updateAction(id, resource)
+                .map(updatedResource -> new ResponseDTO<>(HttpStatus.OK.value(), updatedResource, null));
     }
 
     @PostMapping("/execute")
---
+++ appsmith-server/src/main/java/com/appsmith/server/domains/Collection.java
@@ -0,0 +1,28 @@
+package com.appsmith.server.domains;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Document
+public class Collection extends BaseDomain{
+
+    String name;
+
+    String applicationId;
+
+    String organizationId;
+
+    Boolean shared;
+
+    //To save space, when creating/updating collection, only add Action's id field instead of the entire action.
+    List<Action> actions;
+}
