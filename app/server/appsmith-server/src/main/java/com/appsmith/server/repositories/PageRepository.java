3f968afac69185c9ac36393cb32419aa46e3941d
Trisha Anand <trisha@appsmith.com>
2019-11-05 11:36:10 +0000

    Page Controller now can return list of pages by application id and list of page names & ids by application id

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/PageRepository.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/PageRepository.java
@@ -1,7 +1,9 @@
 package com.appsmith.server.repositories;
 
 import com.appsmith.server.domains.Page;
+import com.appsmith.server.dtos.PageNameIdDTO;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
@@ -11,4 +13,6 @@ public interface PageRepository extends BaseRepository<Page, String> {
     Mono<Page> findByName(String name);
 
     Mono<Void> deleteAll();
+
+    Flux<PageNameIdDTO> findPageNameIdDtoByApplicationId(String applicationId);
 }
