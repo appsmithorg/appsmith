5ba09f3a878d4f6733beaeb23f1688b9eaa0d5e9
Trisha Anand <trisha@appsmith.com>
2019-11-12 09:59:29 +0000

    Get all actions returns them in sorted order

--- appsmith-server/src/main/java/com/appsmith/server/repositories/ActionRepository.java
+++ appsmith-server/src/main/java/com/appsmith/server/repositories/ActionRepository.java
@@ -2,10 +2,13 @@ package com.appsmith.server.repositories;
 
 import com.appsmith.server.domains.Action;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface ActionRepository extends BaseRepository<Action, String> {
 
     Mono<Action> findById(String id);
+
+    Flux<Action> findAllByOrderByName();
 }
