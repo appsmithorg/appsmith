ca4ff6a28b1ee9eb765cca4442b024daee9ee639
Arpit Mohan <arpit@appsmith.com>
2019-11-26 18:19:12 +0530

    Fixing action execution timeout bug which was defaulting the execution timeout to 0 instead of 10.
    Now if we provide a null / negative value to action execution timeout, it'll default to 10s

---
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/constants/ActionConstants.java
@@ -0,0 +1,6 @@
+package com.appsmith.external.constants;
+
+public class ActionConstants {
+    // Default the action execution to 10s
+    public static final int DEFAULT_ACTION_EXECUTION_TIMEOUT_MS = 10000;
+}
