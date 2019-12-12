af96c11d3eece827b6ae3806e08817cb2e7be15b
Arpit Mohan <arpit@appsmith.com>
2019-12-12 09:46:38 +0000

    On username + pwd auth success, we redirect the browser to the client's index page via the origin header.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/filters/FormAuthenticationSuccessHandler.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/filters/FormAuthenticationSuccessHandler.java
@@ -1,51 +1,40 @@
 package com.appsmith.server.filters;
 
-import com.appsmith.server.dtos.ResponseDTO;
-import com.appsmith.server.exceptions.AppsmithError;
-import com.appsmith.server.exceptions.AppsmithException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.server.DefaultServerRedirectStrategy;
+import org.springframework.security.web.server.ServerRedirectStrategy;
 import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class FormAuthenticationSuccessHandler implements ServerAuthenticationSuccessHandler {
 
-    private final ObjectMapper objectMapper;
+    private ServerRedirectStrategy redirectStrategy = new DefaultServerRedirectStrategy();
 
     @Override
     public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange,
                                               Authentication authentication) {
-        log.debug("In the form auth success function");
+        log.debug("Login succeeded for user: {}", authentication.getPrincipal());
         ServerWebExchange exchange = webFilterExchange.getExchange();
-        ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(HttpStatus.OK);
-        response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
 
-        ResponseDTO<Boolean> responseDTO = new ResponseDTO<>(HttpStatus.OK.value(), true, null);
-
-        String responseStr;
-        try {
-            responseStr = objectMapper.writeValueAsString(responseDTO);
-        } catch (JsonProcessingException e) {
-            log.error("IOException caught while serializing auth success response to string. Cause: ", e);
-            return response.writeWith(Mono.error(new AppsmithException(AppsmithError.LOGIN_INTERNAL_ERROR)));
+        // On authentication success, we send a redirect to the client's home page. This ensures that the session
+        // is set in the cookie on the browser.
+        String originHeader = exchange.getRequest().getHeaders().getOrigin();
+        if(originHeader == null || originHeader.isEmpty()) {
+            originHeader = "/";
         }
 
-        DataBuffer buffer = response.bufferFactory().wrap(responseStr.getBytes());
-        return response.writeWith(Mono.just(buffer));
+        URI defaultRedirectLocation = URI.create(originHeader);
+        return this.redirectStrategy.sendRedirect(exchange, defaultRedirectLocation);
     }
 
 }
