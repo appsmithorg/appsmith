6deb0d786efddc7d22581b529ce29c5ce4abb306
Arpit Mohan <arpit@appsmith.com>
2019-12-19 13:01:16 +0530

    Adding a flag to determine if we should send emails for that particular environment

---
+++ appsmith-server/src/main/java/com/appsmith/server/configurations/EmailConfig.java
@@ -0,0 +1,16 @@
+package com.appsmith.server.configurations;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+public class EmailConfig {
+
+    @Value("${mail.enabled}")
+    private boolean emailEnabled = true;
+
+}
--- appsmith-server/src/main/java/com/appsmith/server/notifications/EmailSender.java
+++ appsmith-server/src/main/java/com/appsmith/server/notifications/EmailSender.java
@@ -1,5 +1,6 @@
 package com.appsmith.server.notifications;
 
+import com.appsmith.server.configurations.EmailConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -13,9 +14,17 @@ public class EmailSender {
     @Autowired
     JavaMailSender emailSender;
 
+    @Autowired
+    EmailConfig emailConfig;
+
     private final String MAIL_FROM = "hello@appsmith.com";
 
     public void sendMail(String to, String subject, String text) {
+        // Don't send an email for local, dev or test environments
+        if(!emailConfig.isEmailEnabled()) {
+            return;
+        }
+
         log.debug("Going to send email to {} with subject {}", to, subject);
 
         SimpleMailMessage message = new SimpleMailMessage();
