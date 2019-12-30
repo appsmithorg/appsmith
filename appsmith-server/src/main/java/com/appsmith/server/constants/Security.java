3eb3b40079f5b1bee5992de37c4e814a7da2a85b
Arpit Mohan <arpit@appsmith.com>
2019-12-30 18:07:30 +0530

    Adding the redirect back to the client's index page on successful oauth2 login by the user
    We use the referer header to find the client's endpoint and redirect to that endpoint once the OAuth2 login is successful. This is implemented via CustomServerOAuth2AuthorizationRequestResolver and AuthenticationSuccessHandler

--- appsmith-server/src/main/java/com/appsmith/server/constants/Security.java
+++ appsmith-server/src/main/java/com/appsmith/server/constants/Security.java
@@ -2,4 +2,7 @@ package com.appsmith.server.constants;
 
 public interface Security {
     String USER_ROLE = "USER_ROLE";
+    String QUERY_PARAMETER_STATE = "state";
+    String REFERER_HEADER = "Referer";
+    String STATE_PARAMETER_ORIGIN = "origin=";
 }
