4640dfe24d7851300b386a1dd9834b22d526f091
Trisha Anand <trisha@appsmith.com>
2020-01-07 11:34:44 +0000

    Removed the creation of dependency graph during update layout. Instead simple mustache finding are done now to get pageload actions.

--- appsmith-server/src/main/java/com/appsmith/server/services/LayoutServiceImpl.java
+++ appsmith-server/src/main/java/com/appsmith/server/services/LayoutServiceImpl.java
@@ -7,11 +7,12 @@ import com.appsmith.server.domains.Page;
 import com.appsmith.server.dtos.DslActionDTO;
 import com.appsmith.server.exceptions.AppsmithError;
 import com.appsmith.server.exceptions.AppsmithException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONObject;
 import org.bson.types.ObjectId;
 import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static com.appsmith.server.helpers.MustacheHelper.extractMustacheKeys;
@@ -36,14 +38,24 @@ public class LayoutServiceImpl implements LayoutService {
     private final ApplicationPageService applicationPageService;
     private final PageService pageService;
     private final ActionService actionService;
+    /*
+     * This pattern finds all the String which have been extracted from the mustache dynamic bindings.
+     * e.g. for the given JS function using action with name "fetchUsers"
+     * {{JSON.stringify(fetchUsers)}}
+     * This pattern should return ["JSON.stringify", "fetchUsers"]
+     */
+    private final Pattern pattern = Pattern.compile("[a-zA-Z0-9._]+");
+    private final ObjectMapper objectMapper;
 
     @Autowired
     public LayoutServiceImpl(ApplicationPageService applicationPageService,
                              PageService pageService,
-                             ActionService actionService) {
+                             ActionService actionService,
+                             ObjectMapper objectMapper) {
         this.applicationPageService = applicationPageService;
         this.pageService = pageService;
         this.actionService = actionService;
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -90,105 +102,76 @@ public class LayoutServiceImpl implements LayoutService {
 
     @Override
     public Mono<Layout> updateLayout(String pageId, String layoutId, Layout layout) {
+        Set<String> dynamicBindingNames = new HashSet<>();
+        String dslString = "";
 
+        // Convert the DSL into a String
         JSONObject dsl = layout.getDsl();
+        try {
+            dslString = objectMapper.writeValueAsString(dsl);
+        } catch (JsonProcessingException e) {
+            log.debug("Exception caught during conversion of DSL Json object to String. ", e);
+        }
 
-        Graph<String, DefaultEdge> graph = new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+        // Extract all the mustache keys in the DSL to get the dynamic bindings used in the DSL.
+        Set<String> dynamicBindings = extractMustacheKeys(dslString);
 
-        //Walk through the DSL and extract the basic relationship of widgets with actions in the DSL.
-        extractWidgetRelationship(dsl, graph);
+        if (!dynamicBindings.isEmpty()) {
+            for (String mustacheKey : dynamicBindings) {
+                String key = mustacheKey.trim();
 
-        Mono<Set<DslActionDTO>> actionsInPage =
-                updatePageIdsForActionsAndReturnDslActions(graph.vertexSet(), pageId)
-                        .map(action -> {
-                            // Since we are only interested in few fields, prepare the DslActionDTO that needs to be stored in
-                            // the layout and return it to be collected in to a set.
-                            DslActionDTO newAction = new DslActionDTO();
-                            newAction.setId(action.getId());
-                            newAction.setPluginType(action.getPluginType());
-                            newAction.setJsonPathKeys(action.getJsonPathKeys());
-                            newAction.setName(action.getName());
-                            return newAction;
-                        })
-                        .collect(toSet())
-                        .map(actions -> {
-                            Set<String> actionVertices = new HashSet<>();
+                // Extract all the words in the dynamic bindings
+                Matcher matcher = pattern.matcher(key);
 
-                            // We are only interested in the action-action relationship via dynamic binding. Action-widget relationship
-                            // is to be ignored and handled by the front-end. So add actions to vertices set if it exists in the
-                            // graph.
-                            actions.forEach(action -> {
-                                if (graph.vertexSet().contains(action.getName())) {
-                                    actionVertices.add(action.getName());
-                                }
-                            });
+                while (matcher.find()) {
+                    String word = matcher.group();
 
-                            // Now only add edges in the graph to include action-action relationship
-                            actions.forEach(action -> {
-                                Set<String> jsonPathKeys = action.getJsonPathKeys();
-                                if (jsonPathKeys != null && !jsonPathKeys.isEmpty()) {
-                                    for (String jsonPathKey : jsonPathKeys) {
+                    String[] subStrings = word.split(Pattern.quote("."));
 
-                                        // Split the json path keys using '.' and extract the first string of the array
-                                        String[] substrings = jsonPathKey.split(Pattern.quote("."));
+                    // We are only interested in the top level. e.g. if its Input1.text, we want just Input1
+                    dynamicBindingNames.add(subStrings[0]);
+                }
+            }
+        }
 
-                                        // We are only interested in dependencies which are actions aka
-                                        if (actionVertices.contains(substrings[0])) {
-                                            if (!graph.vertexSet().contains(substrings[0])) {
-                                                graph.addVertex(substrings[0]);
-                                            }
-                                            // Since the graph was created using widget and action names, we are already guaranteed
-                                            // to find the action name in the graph.
-                                            graph.addEdge(substrings[0], action.getName());
-                                        }
-
-                                    }
-                                }
-                            });
-                            return actions;
-                        });
+        Mono<Set<DslActionDTO>> onLoadActionsMono = findRestApiActionsByPageIdAndHTTPMethodGET(dynamicBindingNames, pageId)
+                .map(action -> {
+                    // Since we are only interested in few fields, prepare the DslActionDTO that needs to be stored in
+                    // the layout and return it to be collected in to a set.
+                    DslActionDTO newAction = new DslActionDTO();
+                    newAction.setId(action.getId());
+                    newAction.setPluginType(action.getPluginType());
+                    newAction.setJsonPathKeys(action.getJsonPathKeys());
+                    newAction.setName(action.getName());
+                    return newAction;
+                })
+                .collect(toSet());
 
         return pageService.findByIdAndLayoutsId(pageId, layoutId)
                 .switchIfEmpty(Mono.error(new AppsmithException(AppsmithError.INVALID_PARAMETER, FieldName.PAGE_ID + " or " + FieldName.LAYOUT_ID)))
                 .flatMap(applicationPageService::doesPageBelongToCurrentUserOrganization)
                 //The pageId given is correct and belongs to the current user's organization.
-                .zipWith(actionsInPage)
+                .zipWith(onLoadActionsMono)
                 .map(tuple -> {
                     Page page = tuple.getT1();
-                    Set<DslActionDTO> actions = tuple.getT2();
+                    Set<DslActionDTO> onLoadActions = tuple.getT2();
+
                     List<Layout> layoutList = page.getLayouts();
-                    Set<DslActionDTO> pageLoadActions = new HashSet<>();
-
-
-                    // Find the top nodes in the graph : These are the nodes which are not dependent on any other node in the graph
-                    ArrayList<String> rootNodesOfGraph = getRootNodesOfGraph(graph);
-
-                    // The vertices of the graph currently contain both the actions and the widgets to show their dependency relationship
-                    // Since we are only interested in top nodes which are actions, compare with actions set and
-                    // add these actions to the array list to create page load actions.
-                    actions.forEach(action -> {
-                        if (rootNodesOfGraph.contains(action.getName())) {
-                            pageLoadActions.add(action);
-                        }
-                    });
 
                     //Because the findByIdAndLayoutsId call returned non-empty result, we are guaranteed to find the layoutId here.
                     for (Layout storedLayout : layoutList) {
                         if (storedLayout.getId().equals(layoutId)) {
                             //Copy the variables to conserve before update
                             JSONObject publishedDsl = storedLayout.getPublishedDsl();
-                            Set<DslActionDTO> publishedLayoutActions = storedLayout.getPublishedLayoutActions();
                             Set<DslActionDTO> publishedLayoutOnLoadActions = storedLayout.getPublishedLayoutOnLoadActions();
 
                             //Update
-                            layout.setLayoutActions(actions);
-                            layout.setLayoutOnLoadActions(pageLoadActions);
+                            layout.setLayoutOnLoadActions(onLoadActions);
                             BeanUtils.copyProperties(layout, storedLayout);
                             storedLayout.setId(layoutId);
 
                             //Copy back the conserved variables.
                             storedLayout.setPublishedDsl(publishedDsl);
-                            storedLayout.setPublishedLayoutActions(publishedLayoutActions);
                             storedLayout.setPublishedLayoutOnLoadActions(publishedLayoutOnLoadActions);
                             break;
                         }
@@ -208,11 +191,33 @@ public class LayoutServiceImpl implements LayoutService {
                 });
     }
 
-    void extractWidgetRelationship(net.minidev.json.JSONObject dsl, Graph graph) {
+    /**
+     * Given a list of names of actions (nodes) and pageId, it hits the database and returns all the actions matching
+     * this criteria of name and pageId with http method 'GET'
+     *
+     * @param nodes
+     * @param pageId
+     * @return
+     */
+    Flux<Action> findRestApiActionsByPageIdAndHTTPMethodGET(Set<String> nodes, String pageId) {
+
+        return actionService
+                .findDistinctRestApiActionsByNameInAndPageIdAndHttpMethod(nodes, pageId, "GET");
+    }
+
+    /**
+     * Walks the DSL and extracts all the widget names from it and adds it to the graph.
+     *
+     * @param dsl
+     * @param graph
+     */
+    @Deprecated
+    private void extractAllWidgetNamesAndAddThemAsVerticesToTheGraph(JSONObject dsl, Graph<String, DefaultEdge> graph) {
         if (dsl.get(FieldName.WIDGET_NAME) == null) {
             //This isnt a valid widget configuration. No need to traverse this.
             return;
         }
+
         String widgetName = dsl.getAsString(FieldName.WIDGET_NAME);
 
         //Since we are parsing this widget in this, add it to the graph as a vertex.
@@ -220,6 +225,37 @@ public class LayoutServiceImpl implements LayoutService {
             graph.addVertex(widgetName);
         }
 
+        ArrayList<Object> children = (ArrayList<Object>) dsl.get(FieldName.CHILDREN);
+        if (children != null) {
+            for (int i = 0; i < children.size(); i++) {
+                Map data = (Map) children.get(i);
+                JSONObject object = new JSONObject();
+                object.putAll(data);
+                extractAllWidgetNamesAndAddThemAsVerticesToTheGraph(object, graph);
+            }
+        }
+
+    }
+
+    /**
+     * Walks the DSL and adds relationship between widgets and actions. Widgets have at this point already been added
+     * to the graph by function extractAllWidgetNamesAndAddThemAsVerticesToTheGraph. Actions are recognized by comparing
+     * them to a master list of action names which could be found in the DSL by the name of masterPageActions. Once
+     * an action name has been found in the dynamic binding, it is then added to boundPageActions list.
+     *
+     * @param dsl
+     * @param graph
+     * @param masterPageActions
+     * @param boundPageActions
+     */
+    @Deprecated
+    private void extractRelationshipsFromDSLAndAddThemAsEdgesToTheGraph(net.minidev.json.JSONObject dsl, Graph graph, List<String> masterPageActions, List<String> boundPageActions) {
+        if (dsl.get(FieldName.WIDGET_NAME) == null) {
+            //This isnt a valid widget configuration. No need to traverse this.
+            return;
+        }
+
+        String widgetName = dsl.getAsString(FieldName.WIDGET_NAME);
 
         Object bindings = dsl.get(FieldName.DYNAMIC_BINDINGS);
 
@@ -241,14 +277,30 @@ public class LayoutServiceImpl implements LayoutService {
                 if (!extractMustacheKeys.isEmpty()) {
                     for (String mustacheKey : extractMustacheKeys) {
                         String key = mustacheKey.trim();
-                        // We are only interested in the top level. e.g. if its Input1.text, we want just Input1
-                        String[] subStrings = key.split(Pattern.quote("."));
 
-                        if (!graph.vertexSet().contains(subStrings[0])) {
-                            graph.addVertex(subStrings[0]);
+                        Matcher matcher = pattern.matcher(key);
+
+                        while (matcher.find()) {
+                            String word = matcher.group();
+
+                            // We are only interested in the top level. e.g. if its Input1.text, we want just Input1
+                            String[] subStrings = word.split(Pattern.quote("."));
+
+                            // This only adds an edge if the string matches any action name or widget name
+                            if (masterPageActions.contains(subStrings[0]) || graph.vertexSet().contains(subStrings[0])) {
+
+                                //If this is an action and not a widget, add it to list of bound actions
+                                if (masterPageActions.contains(subStrings[0])) {
+                                    boundPageActions.add(subStrings[0]);
+                                }
+
+                                if (!graph.vertexSet().contains(subStrings[0])) {
+                                    graph.addVertex(subStrings[0]);
+                                }
+                                //Now add the edge to represent this relationship between the widget and widget/action
+                                graph.addEdge(subStrings[0], widgetName);
+                            }
                         }
-                        //Now add the edge to represent this relationship
-                        graph.addEdge(subStrings[0], widgetName);
                     }
                 }
             });
@@ -261,7 +313,7 @@ public class LayoutServiceImpl implements LayoutService {
                 Map data = (Map) children.get(i);
                 JSONObject object = new JSONObject();
                 object.putAll(data);
-                extractWidgetRelationship(object, graph);
+                extractRelationshipsFromDSLAndAddThemAsEdgesToTheGraph(object, graph, masterPageActions, boundPageActions);
             }
         }
     }
@@ -275,7 +327,8 @@ public class LayoutServiceImpl implements LayoutService {
      * @param graph
      * @return
      */
-    ArrayList<String> getRootNodesOfGraph(Graph graph) {
+    @Deprecated
+    private ArrayList<String> getRootNodesOfGraph(Graph graph) {
         Set vertexSet = graph.vertexSet();
         ArrayList<String> topVertices = new ArrayList<>();
         vertexSet.forEach(vertex -> {
@@ -288,12 +341,6 @@ public class LayoutServiceImpl implements LayoutService {
         return topVertices;
     }
 
-    Flux<Action> updatePageIdsForActionsAndReturnDslActions(Set<String> nodes, String pageId) {
-
-        // Find all the actions from the vertices which belong to the page and are of HTTP type "GET"
-        return actionService
-                .findDistinctRestApiActionsByNameInAndPageIdAndHttpMethod(nodes, pageId, "GET");
-    }
 
 }
 
