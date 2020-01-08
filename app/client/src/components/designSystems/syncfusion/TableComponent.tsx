7379e4ac5d9a8d8ae395227ed641f92a3fbb06a3
Satbir Singh <satbir121@gmail.com>
2020-01-08 12:53:26 +0000

    Changed TableComponent from class to function

--- app/client/src/components/designSystems/syncfusion/TableComponent.tsx
+++ app/client/src/components/designSystems/syncfusion/TableComponent.tsx
@@ -9,7 +9,7 @@ import {
   Page,
   SelectionSettingsModel,
 } from "@syncfusion/ej2-react-grids";
-import * as React from "react";
+import React, { useEffect, useRef, MutableRefObject, memo } from "react";
 import styled from "constants/DefaultTheme";
 
 export interface TableComponentProps {
@@ -31,74 +31,77 @@ const settings: SelectionSettingsModel = {
   type: "Multiple",
 };
 
-export default class TableComponent extends React.Component<
-  TableComponentProps,
-  {}
-> {
-  private grid: Grid | null | undefined;
-  rowSelected = () => {
-    if (this.grid) {
-      /** Get the selected row indexes */
-      const selectedrowindex: number[] = this.grid.getSelectedRowIndexes();
-      /** Get the selected records. */
-      const selectedrecords: object[] = this.grid.getSelectedRecords();
-      if (selectedrecords.length !== 0) {
-        this.props.onRowClick(selectedrecords[0], selectedrowindex[0]);
+type GridRef = MutableRefObject<Grid | undefined>;
+
+function reCalculatePageSize(grid: GridRef) {
+  if (grid.current) {
+    const rowHeight: number = grid.current.getRowHeight();
+    /** Grid height */
+    const gridHeight: number = grid.current.height as number;
+    /** initial page size */
+    const pageSize: number = grid.current.pageSettings.pageSize as number;
+    /** new page size is obtained here */
+    const pageResize: any = (gridHeight - pageSize * rowHeight) / rowHeight;
+    grid.current.pageSettings.pageSize = pageSize + Math.round(pageResize);
+  }
+}
+/* eslint-disable react/display-name */
+const TableComponent = memo(
+  (props: TableComponentProps) => {
+    const grid: GridRef = useRef();
+
+    // componentDidUpdate start
+    useEffect(() => {
+      props.height && reCalculatePageSize(grid);
+    }, [props.height]);
+    // componentDidUpdate end
+
+    function dataBound() {
+      if (grid.current) {
+        grid.current.autoFitColumns();
       }
     }
-  };
 
-  reCalculatePageSize = () => {
-    if (this.grid) {
-      /** height of the each row */
-      const rowHeight: number = this.grid.getRowHeight();
-      /** Grid height */
-      const gridHeight: number = this.grid.height as number;
-      /** initial page size */
-      const pageSize: number = this.grid.pageSettings.pageSize as number;
-      /** new page size is obtained here */
-      const pageResize: any = (gridHeight - pageSize * rowHeight) / rowHeight;
-      this.grid.pageSettings.pageSize = pageSize + Math.round(pageResize);
+    function rowSelected() {
+      if (grid.current) {
+        /** Get the selected row indexes */
+        const selectedrowindex: number[] = grid.current.getSelectedRowIndexes();
+        /** Get the selected records. */
+        const selectedrecords: object[] = grid.current.getSelectedRecords();
+        if (selectedrecords.length !== 0) {
+          props.onRowClick(selectedrecords[0], selectedrowindex[0]);
+        }
+      }
     }
-  };
-  dataBound = () => {
-    if (this.grid) {
-      this.grid.autoFitColumns();
-    }
-  };
 
-  shouldComponentUpdate(nextProps: TableComponentProps) {
-    const propsNotEqual =
-      JSON.stringify(nextProps.data) !== JSON.stringify(this.props.data) ||
-      nextProps.height !== this.props.height ||
-      nextProps.width !== this.props.width;
-
-    return propsNotEqual;
-  }
-  componentDidUpdate(prevProps: TableComponentProps) {
-    if (prevProps.height !== this.props.height) {
-      this.reCalculatePageSize();
-    }
-  }
-  render() {
     return (
       <StyledGridComponent
         selectionSettings={settings}
-        dataSource={this.props.data}
-        rowSelected={this.rowSelected}
-        ref={(g: GridComponent) => (this.grid = g)}
-        width={this.props.width - 16}
-        height={this.props.height - 107}
-        dataBound={this.dataBound}
+        dataSource={props.data}
+        rowSelected={rowSelected}
+        ref={grid}
+        width={props.width - 16}
+        height={props.height - 107}
+        dataBound={dataBound}
         allowPaging={true}
       >
         <Inject services={[Resize, Page]} />
         <ColumnsDirective>
-          {this.props.columns.map(col => {
+          {props.columns.map(col => {
             return <ColumnDirective key={col.field} field={col.field} />;
           })}
         </ColumnsDirective>
       </StyledGridComponent>
     );
-  }
-}
+  },
+  (prevProps, nextProps) => {
+    const propsNotEqual =
+      JSON.stringify(nextProps.data) !== JSON.stringify(prevProps.data) ||
+      nextProps.height !== prevProps.height ||
+      nextProps.width !== prevProps.width;
+
+    return !propsNotEqual;
+  },
+);
+
+export default TableComponent;
