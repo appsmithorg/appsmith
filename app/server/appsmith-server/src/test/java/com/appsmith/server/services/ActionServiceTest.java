2ad6d3f11b014f9afb038478bface830f6701373
Arpit Mohan <arpit@appsmith.com>
2020-02-26 12:46:10 +0530

    Adding test cases in ActionService for some create Action workflows

---
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/ActionServiceTest.java
@@ -0,0 +1,104 @@
+package com.appsmith.server.services;
+
+import com.appsmith.external.models.ActionConfiguration;
+import com.appsmith.server.constants.FieldName;
+import com.appsmith.server.domains.Action;
+import com.appsmith.server.exceptions.AppsmithError;
+import com.appsmith.server.exceptions.AppsmithException;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+@DirtiesContext
+public class ActionServiceTest {
+    @Autowired
+    ActionService actionService;
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void createValidActionWithJustName() {
+        Action action = new Action();
+        action.setName("randomActionName");
+        action.setPageId("randomPageId");
+        ActionConfiguration actionConfiguration = new ActionConfiguration();
+        actionConfiguration.setHttpMethod(HttpMethod.GET);
+        action.setActionConfiguration(actionConfiguration);
+        Mono<Action> actionMono = Mono.just(action)
+                .flatMap(actionService::create);
+        StepVerifier
+                .create(actionMono)
+                .assertNext(createdAction -> {
+                    assertThat(createdAction.getId()).isNotEmpty();
+                    assertThat(createdAction.getName()).isEqualTo(action.getName());
+                    assertThat(createdAction.getIsValid()).isFalse();
+                    assertThat(createdAction.getInvalids().size()).isEqualTo(1);
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void createValidActionNullActionConfiguration() {
+        Action action = new Action();
+        action.setName("randomActionName");
+        action.setPageId("randomPageId");
+        Mono<Action> actionMono = Mono.just(action)
+                .flatMap(actionService::create);
+        StepVerifier
+                .create(actionMono)
+                .assertNext(createdAction -> {
+                    assertThat(createdAction.getId()).isNotEmpty();
+                    assertThat(createdAction.getName()).isEqualTo(action.getName());
+                    assertThat(createdAction.getIsValid()).isFalse();
+                    assertThat(createdAction.getInvalids()).contains(AppsmithError.NO_CONFIGURATION_FOUND_IN_ACTION.getMessage());
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void invalidCreateActionNullName() {
+        Action action = new Action();
+        action.setPageId("randomPageId");
+        ActionConfiguration actionConfiguration = new ActionConfiguration();
+        actionConfiguration.setHttpMethod(HttpMethod.GET);
+        action.setActionConfiguration(actionConfiguration);
+        Mono<Action> actionMono = Mono.just(action)
+                .flatMap(actionService::create);
+        StepVerifier
+                .create(actionMono)
+                .expectErrorMatches(throwable -> throwable instanceof AppsmithException &&
+                        throwable.getMessage().equals(AppsmithError.INVALID_PARAMETER.getMessage(FieldName.NAME)))
+                .verify();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void invalidCreateActionNullPageId() {
+        Action action = new Action();
+        action.setName("randomActionName");
+        ActionConfiguration actionConfiguration = new ActionConfiguration();
+        actionConfiguration.setHttpMethod(HttpMethod.GET);
+        action.setActionConfiguration(actionConfiguration);
+        Mono<Action> actionMono = Mono.just(action)
+                .flatMap(actionService::create);
+        StepVerifier
+                .create(actionMono)
+                .expectErrorMatches(throwable -> throwable instanceof AppsmithException &&
+                        throwable.getMessage().equals(AppsmithError.INVALID_PARAMETER.getMessage(FieldName.PAGE_ID)))
+                .verify();
+    }
+}
