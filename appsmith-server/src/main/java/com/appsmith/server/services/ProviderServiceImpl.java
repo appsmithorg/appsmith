30ab75af9a70a2fa67cd1b200f53e5fa59c3fbbc
Trisha Anand <trisha@appsmith.com>
2020-03-04 15:24:14 +0530

    Filter providers by category added.

--- appsmith-server/src/main/java/com/appsmith/server/services/ProviderServiceImpl.java
+++ appsmith-server/src/main/java/com/appsmith/server/services/ProviderServiceImpl.java
@@ -3,6 +3,7 @@ package com.appsmith.server.services;
 import com.appsmith.external.models.Provider;
 import com.appsmith.server.constants.FieldName;
 import com.appsmith.server.repositories.ProviderRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
@@ -13,8 +14,11 @@ import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Scheduler;
 
 import javax.validation.Validator;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
+@Slf4j
 public class ProviderServiceImpl extends BaseService<ProviderRepository, Provider, String> implements ProviderService {
 
     public ProviderServiceImpl(Scheduler scheduler,
@@ -33,6 +37,12 @@ public class ProviderServiceImpl extends BaseService<ProviderRepository, Provide
 
         if (params.getFirst(FieldName.NAME) != null) {
             providerExample.setName(params.getFirst(FieldName.NAME));
+        }
+
+        if (params.getFirst(FieldName.CATEGORY) != null) {
+            List<String> categories = new ArrayList<>();
+            categories.add(params.getFirst(FieldName.CATEGORY));
+            providerExample.setCategories(categories);
         }
 
         return repository.findAll(Example.of(providerExample), sort);
