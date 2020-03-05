b9dfd0e71e937ee1a3e32ba507ee1b9aceddfb37
Satbir Singh <satbir121@gmail.com>
2020-03-05 09:17:10 +0000

    No sentry source maps uploaded on local build.

--- app/client/craco.build.config.js
+++ app/client/craco.build.config.js
@@ -3,14 +3,20 @@ const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./craco.common.config.js');
 
+const env = process.env.REACT_APP_ENVIRONMENT
+
+const plugins = []
+
+if(env === "PRODUCTION" || env === "STAGING") {
+  plugins.push(new SentryWebpackPlugin({
+    include: 'build',
+    ignore: ['node_modules', 'webpack.config.js'],
+    release: process.env.REACT_APP_SENTRY_RELEASE
+  }))
+}
+
 module.exports = merge(common, {
   webpack: {
-    plugins: [
-      new SentryWebpackPlugin({
-        include: 'build',
-        ignore: ['node_modules', 'webpack.config.js'],
-        release: process.env.REACT_APP_SENTRY_RELEASE
-      })
-    ]
+    plugins: plugins
   },
 });
