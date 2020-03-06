f4ceb432dcbd2693b69d855ebde83550e8dcbd4e
Arpit Mohan <arpit@appsmith.com>
2020-03-06 06:17:00 +0000

    Adding /profile endpoint to return enhanced user profile to the client.

--- appsmith-server/src/main/java/com/appsmith/server/domains/User.java
+++ appsmith-server/src/main/java/com/appsmith/server/domains/User.java
@@ -1,11 +1,13 @@
 package com.appsmith.server.domains;
 
 import com.appsmith.external.models.BaseDomain;
+import com.appsmith.server.dtos.ApplicationNameIdDTO;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.security.core.GrantedAuthority;
@@ -14,6 +16,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
--- appsmith-server/src/main/java/com/appsmith/server/services/UserService.java
+++ appsmith-server/src/main/java/com/appsmith/server/services/UserService.java
@@ -3,6 +3,7 @@ package com.appsmith.server.services;
 import com.appsmith.server.domains.InviteUser;
 import com.appsmith.server.domains.User;
 import com.appsmith.server.dtos.ResetUserPasswordDTO;
+import com.appsmith.server.dtos.UserProfileDTO;
 import reactor.core.publisher.Mono;
 
 public interface UserService extends CrudService<User, String> {
@@ -22,4 +23,6 @@ public interface UserService extends CrudService<User, String> {
     Mono<Boolean> verifyInviteToken(String email, String token);
 
     Mono<Boolean> confirmInviteUser(InviteUser inviteUser);
+
+    Mono<UserProfileDTO> getUserProfile();
 }
