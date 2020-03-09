e5e321bce7017541b7d30c27b7aabc09d23bc70d
Satbir Singh <apple@apples-MacBook-Pro.local>
2020-03-09 10:08:53 +0530

    Separating Sentry stage events from production.

--- app/client/src/constants/ThirdPartyConstants.tsx
+++ app/client/src/constants/ThirdPartyConstants.tsx
@@ -5,7 +5,7 @@ export const SENTRY_PROD_CONFIG = {
   release: process.env.REACT_APP_SENTRY_RELEASE,
 };
 export const SENTRY_STAGE_CONFIG = {
-  dsn: "https://a63362b692d64edeb175741f1f80b091@sentry.io/1546547",
+  dsn: "https://26e99889a7f14b418a66cb2deafeb40c@sentry.io/4113637",
   environment: "Staging",
   release: process.env.REACT_APP_SENTRY_RELEASE,
 };
