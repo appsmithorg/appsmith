1d404cb9bf46b923efdeabd43cc2dd8d3dfd07d2
Trisha Anand <trisha@appsmith.com>
2020-03-12 20:01:43 +0530

    Get all categories now return Mono of List instead of Flux.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ProviderService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ProviderService.java
@@ -1,8 +1,10 @@
 package com.appsmith.server.services;
 
 import com.appsmith.external.models.Provider;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public interface ProviderService extends CrudService<Provider, String> {
-    public Flux<String> getAllCategories();
+    public Mono<List<String>> getAllCategories();
 }
