bc7c3b8765d80d414ea53351181eee0eb17de2a1
Satbir Singh <satbir@appsmith.com>
2020-03-19 08:37:31 +0530

    Fixing radio option blur on edit.

--- app/client/src/components/propertyControls/OptionControl.tsx
+++ app/client/src/components/propertyControls/OptionControl.tsx
@@ -10,6 +10,7 @@ import { ControlType } from "constants/PropertyControlConstants";
 import styled from "constants/DefaultTheme";
 import { FormIcons } from "icons/FormIcons";
 import { AnyStyledComponent } from "styled-components";
+import { generateReactKey } from "utils/generators";
 
 const StyledDeleteIcon = styled(FormIcons.DELETE_ICON as AnyStyledComponent)`
   padding: 5px 5px;
@@ -28,19 +29,64 @@ const StyledOptionControlWrapper = styled(ControlWrapper)`
   padding-right: 16px;
 `;
 
-class OptionControl extends BaseControl<ControlProps> {
+function updateOptionLabel<T>(
+  options: Array<T>,
+  index: number,
+  updatedLabel: string,
+) {
+  return options.map((option: T, optionIndex) => {
+    if (index !== optionIndex) {
+      return option;
+    }
+    return {
+      ...option,
+      label: updatedLabel,
+    };
+  });
+}
+
+function updateOptionValue<T>(
+  options: Array<T>,
+  index: number,
+  updatedValue: string,
+) {
+  return options.map((option, optionIndex) => {
+    if (index !== optionIndex) {
+      return option;
+    }
+    return {
+      ...option,
+      value: updatedValue,
+    };
+  });
+}
+
+type DropDownOptionWithKey = DropdownOption & {
+  key: string;
+};
+
+class OptionControl extends BaseControl<
+  ControlProps,
+  {
+    renderOptions: DropDownOptionWithKey[];
+  }
+> {
+  constructor(props: ControlProps) {
+    super(props);
+    this.state = {
+      renderOptions: [],
+    };
+  }
   render() {
-    const { propertyValue } = this.props;
-    const options: DropdownOption[] = Array.isArray(propertyValue)
-      ? propertyValue
-      : [{}];
+    const { renderOptions } = this.state;
+    debugger;
     return (
       <React.Fragment>
-        {options.map((option, index) => {
+        {renderOptions.map((option, index) => {
           return (
             <StyledOptionControlWrapper
               orientation={"HORIZONTAL"}
-              key={option.value}
+              key={option.key}
             >
               <StyledOptionControlInputGroup
                 type={"text"}
@@ -79,13 +125,43 @@ class OptionControl extends BaseControl<ControlProps> {
     );
   }
 
+  componentDidMount() {
+    const { propertyValue } = this.props;
+
+    const options: DropdownOption[] = Array.isArray(propertyValue)
+      ? propertyValue
+      : [{}];
+
+    options.map(option => {
+      return {
+        ...option,
+        key: generateReactKey(),
+      };
+    });
+    this.setState({
+      renderOptions: options.map(option => {
+        return {
+          ...option,
+          key: generateReactKey(),
+        };
+      }),
+    });
+  }
+
   deleteOption = (index: number) => {
     const { propertyValue } = this.props;
     const options: DropdownOption[] = Array.isArray(propertyValue)
       ? propertyValue
       : [{}];
+    const { renderOptions } = this.state;
+
     const newOptions = options.filter((o, i) => i !== index);
+    const newRenderOptions = renderOptions.filter((o, i) => i !== index);
+
     this.updateProperty("options", newOptions);
+    this.setState({
+      renderOptions: newRenderOptions,
+    });
   };
 
   updateOptionLabel = (index: number, updatedLabel: string) => {
@@ -95,16 +171,16 @@ class OptionControl extends BaseControl<ControlProps> {
       : [{}];
     this.updateProperty(
       "options",
-      options.map((option, optionIndex) => {
-        if (index !== optionIndex) {
-          return option;
-        }
-        return {
-          ...option,
-          label: updatedLabel,
-        };
-      }),
+      updateOptionLabel(options, index, updatedLabel),
     );
+
+    this.setState({
+      renderOptions: updateOptionLabel(
+        this.state.renderOptions,
+        index,
+        updatedLabel,
+      ),
+    });
   };
 
   updateOptionValue = (index: number, updatedValue: string) => {
@@ -114,16 +190,16 @@ class OptionControl extends BaseControl<ControlProps> {
       : [{}];
     this.updateProperty(
       "options",
-      options.map((option, optionIndex) => {
-        if (index !== optionIndex) {
-          return option;
-        }
-        return {
-          ...option,
-          value: updatedValue,
-        };
-      }),
+      updateOptionValue(options, index, updatedValue),
     );
+
+    this.setState({
+      renderOptions: updateOptionValue(
+        this.state.renderOptions,
+        index,
+        updatedValue,
+      ),
+    });
   };
 
   addOption = () => {
@@ -131,7 +207,18 @@ class OptionControl extends BaseControl<ControlProps> {
     const options: DropdownOption[] = Array.isArray(propertyValue)
       ? propertyValue
       : [{}];
+    const { renderOptions } = this.state;
+
     options.push({ label: "", value: "" });
+    renderOptions.push({
+      label: "",
+      value: "",
+      key: generateReactKey(),
+    });
+
+    this.setState({
+      renderOptions: renderOptions,
+    });
     this.updateProperty("options", options);
   };
 
