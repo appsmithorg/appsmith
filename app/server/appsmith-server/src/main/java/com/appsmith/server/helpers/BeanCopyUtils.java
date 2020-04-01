1f524827b9f0d0c63cff3127506cb5a7a7feff75
Shrikant Kandula <shrikant@appsmith.com>
2020-04-01 08:50:36 +0000

    Datasource CRUD APIs

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/BeanCopyUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/BeanCopyUtils.java
@@ -3,13 +3,19 @@ package com.appsmith.server.helpers;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.PropertyAccessorFactory;
 
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public final class BeanCopyUtils {
 
     private static String[] getNullPropertyNames(Object source) {
+        // TODO: The `BeanWrapperImpl` class has been declared to be an internal class. Migrate to using
+        //  `PropertyAccessorFactory.forBeanPropertyAccess` instead.
         final BeanWrapper src = new BeanWrapperImpl(source);
         java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
 
@@ -42,5 +48,45 @@ public final class BeanCopyUtils {
         }
 
         return count++;
+    }
+
+    public static void copyNestedNonNullProperties(Object source, Object target) {
+        if (source == null || target ==  null) {
+            return;
+        }
+
+        final BeanWrapper sourceBeanWrapper = PropertyAccessorFactory.forBeanPropertyAccess(source);
+        BeanWrapper targetBeanWrapper = PropertyAccessorFactory.forBeanPropertyAccess(target);
+        PropertyDescriptor[] propertyDescriptors = sourceBeanWrapper.getPropertyDescriptors();
+
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+            String name = propertyDescriptor.getName();
+
+            // For properties like `class` that don't have a set method, we can't copy so we just ignore them.
+            if (targetBeanWrapper.getPropertyDescriptor(name).getWriteMethod() == null) {
+                continue;
+            }
+
+            Object sourceValue = sourceBeanWrapper.getPropertyValue(name);
+
+            // If sourceValue is null, don't copy it over to target and just move on to the next property.
+            if (sourceValue == null) {
+                continue;
+            }
+
+            Object targetValue = targetBeanWrapper.getPropertyValue(name);
+
+            if (targetValue != null && isDomainModel(propertyDescriptor.getPropertyType())) {
+                // Go deeper *only* if the property belongs to Appsmith's models, and both the source and target values
+                // are not null.
+                copyNestedNonNullProperties(sourceValue, targetValue);
+            } else {
+                targetBeanWrapper.setPropertyValue(name, sourceValue);
+            }
+        }
+    }
+
+    public static boolean isDomainModel(Class<?> type) {
+        return type.getPackageName().startsWith("com.appsmith.");
     }
 }
