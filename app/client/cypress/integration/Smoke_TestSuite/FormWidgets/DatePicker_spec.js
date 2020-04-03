47f8f63f46a096f0c1524db4de0dde1b11c7d9d7
Pushpa B <pushpa.b@moolya.com>
2020-04-03 09:29:52 +0000

    Feature/edit form widget 2

--- app/client/cypress/integration/Smoke_TestSuite/FormWidgets/DatePicker_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/FormWidgets/DatePicker_spec.js
@@ -2,26 +2,26 @@ const commonlocators = require("../../../locators/commonlocators.json");
 const formWidgetsPage = require("../../../locators/FormWidgets.json");
 
 context("Cypress test", function() {
-  it("Checkbox Widget Functionality", function() {
+  it("DatePicker Widget Functionality", function() {
     cy.NavigateToFormWidgets();
-    cy.get(formWidgetsPage.checkboxWidget)
+    cy.get(formWidgetsPage.datepickerWidget)
       .first()
       .trigger("mouseover");
-    cy.get(formWidgetsPage.checkboxWidget)
+    cy.get(formWidgetsPage.datepickerWidget)
       .children(commonlocators.editicon)
       .first()
       .click({ force: true });
-    //Checking the edit props for Checkbox and also the properties of Checkbox widget
+    //Checking the edit props for DatePicker and also the properties of DatePicker widget
     cy.get(".CodeMirror textarea")
       .first()
       .focus()
       .type("{ctrl}{shift}{downarrow}")
       .should("be.empty")
       .clear({ force: true })
-      .type("Test Checkbox");
+      .type("From Date");
     cy.get(".CodeMirror textarea")
       .first()
-      .should("have.value", "Test Checkbox");
+      .should("have.value", "From Date");
     cy.get(commonlocators.editPropCrossButton).click();
   });
 });
--- app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
@@ -2,26 +2,26 @@ const commonlocators = require("../../../locators/commonlocators.json");
 const formWidgetsPage = require("../../../locators/FormWidgets.json");
 
 context("Cypress test", function() {
-  it("Checkbox Widget Functionality", function() {
+  it("FilePicker Widget Functionality", function() {
     cy.NavigateToFormWidgets();
-    cy.get(formWidgetsPage.checkboxWidget)
+    cy.get(formWidgetsPage.filepickerWidget)
       .first()
       .trigger("mouseover");
-    cy.get(formWidgetsPage.checkboxWidget)
+    cy.get(formWidgetsPage.filepickerWidget)
       .children(commonlocators.editicon)
       .first()
       .click({ force: true });
-    //Checking the edit props for Checkbox and also the properties of Checkbox widget
+    //Checking the edit props for FilePicker and also the properties of FilePicker widget
     cy.get(".CodeMirror textarea")
       .first()
       .focus()
       .type("{ctrl}{shift}{downarrow}")
       .should("be.empty")
       .clear({ force: true })
-      .type("Test Checkbox");
+      .type("Upload Files");
     cy.get(".CodeMirror textarea")
       .first()
-      .should("have.value", "Test Checkbox");
+      .should("have.value", "Upload Files");
     cy.get(commonlocators.editPropCrossButton).click();
   });
 });
