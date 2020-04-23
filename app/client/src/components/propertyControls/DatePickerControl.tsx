d36092c0890a0a76cd276e98c3c19c9bd1aa8473
Hetu Nandu <hetu@appsmith.com>
2020-04-23 06:12:13 +0000

    Fix Date widget default date

--- app/client/src/components/propertyControls/DatePickerControl.tsx
+++ app/client/src/components/propertyControls/DatePickerControl.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import BaseControl, { ControlProps } from "./BaseControl";
-import {
-  StyledDatePicker,
-  StyledTimeZonePicker,
-  StyledSwitch,
-} from "./StyledControls";
+import { StyledDatePicker } from "./StyledControls";
 import moment from "moment-timezone";
-import "../../../node_modules/@blueprintjs/timezone/lib/css/blueprint-timezone.css";
 import styled from "styled-components";
-import { TIMEZONE, ENABLE_TIME } from "constants/messages";
 import { TimePrecision } from "@blueprintjs/datetime";
 
 const DatePickerControlWrapper = styled.div`
@@ -24,75 +18,7 @@ const DatePickerControlWrapper = styled.div`
   }
 `;
 
-const LabelWrapper = styled.div`
-  color: ${props => props.theme.colors.paneText};
-  margin-bottom: 4px;
-  font-size: ${props => props.theme.fontSizes[3]}px;
-  margin-top: 10px;
-`;
-
-type DatePickerContorlState = {
-  date: Date;
-  dateValue: number;
-  isTimeEnabled: boolean;
-  timezone: string;
-};
-
-class DatePickerControl extends BaseControl<
-  DatePickerControlProps,
-  DatePickerContorlState
-> {
-  constructor(props: DatePickerControlProps) {
-    super(props);
-    const timezone =
-      this.props.propertyValue && this.props.propertyValue.timezone
-        ? this.props.propertyValue.timezone
-        : moment.tz.guess();
-    const isTimeEnabled =
-      this.props.propertyValue && this.props.propertyValue.isTimeEnabled
-        ? this.props.propertyValue.isTimeEnabled
-        : false;
-    const dateValue =
-      this.props.propertyValue && this.props.propertyValue.dateValue
-        ? this.props.propertyValue.dateValue
-        : new Date().getTime();
-    const offset = this.getOffset(timezone, dateValue);
-    const date = new Date(dateValue + offset);
-    this.state = {
-      date,
-      timezone,
-      dateValue,
-      isTimeEnabled,
-    };
-  }
-
-  componentDidMount() {
-    const timezone =
-      this.props.propertyValue && this.props.propertyValue.timezone
-        ? this.props.propertyValue.timezone
-        : moment.tz.guess();
-    const isTimeEnabled =
-      this.props.propertyValue && this.props.propertyValue.isTimeEnabled
-        ? this.props.propertyValue.isTimeEnabled
-        : false;
-    const dateValue =
-      this.props.propertyValue && this.props.propertyValue.dateValue
-        ? this.props.propertyValue.dateValue
-        : new Date().getTime();
-    const offset = this.getOffset(timezone, dateValue);
-    const date = new Date(dateValue + offset);
-    this.setState({
-      date,
-      timezone,
-      dateValue,
-      isTimeEnabled,
-    });
-  }
-
-  getOffset = (timezone: string, timeStamp: number) => {
-    return moment.tz.zone(timezone)!.utcOffset(timeStamp) * 60 * 1000;
-  };
-
+class DatePickerControl extends BaseControl<DatePickerControlProps> {
   render() {
     const now = moment();
     const year = now.get("year");
@@ -100,85 +26,37 @@ class DatePickerControl extends BaseControl<
     const month = now.get("month");
     const minDate = now.clone().set({ month, date: date - 1, year: year - 20 });
     const maxDate = now.clone().set({ month, date: date + 1, year: year + 20 });
-
     return (
       <DatePickerControlWrapper>
         <StyledDatePicker
           formatDate={this.formatDate}
           parseDate={this.parseDate}
-          placeholder="DD/MM/YYYY"
+          placeholder="DD/MM/YYYY HH:mm"
           showActionsBar
-          timePrecision={
-            this.state.isTimeEnabled ? TimePrecision.MINUTE : undefined
-          }
+          timePrecision={TimePrecision.MINUTE}
           closeOnSelection
           onChange={this.onDateSelected}
           maxDate={maxDate.toDate()}
           minDate={minDate.toDate()}
-          value={this.state.date}
-        />
-        <LabelWrapper>{TIMEZONE}</LabelWrapper>
-        <StyledTimeZonePicker
-          onChange={this.onTimeZoneSelected}
-          valueDisplayFormat="composite"
-          showLocalTimezone
-          value={this.state.timezone || moment.tz.guess()}
-        />
-        <LabelWrapper>{ENABLE_TIME}</LabelWrapper>
-        <StyledSwitch
-          onChange={this.onToggle}
-          checked={this.state.isTimeEnabled}
-          large
+          value={moment(this.props.propertyValue).toDate()}
         />
       </DatePickerControlWrapper>
     );
   }
 
-  onToggle = () => {
-    const { isTimeEnabled } = this.state;
-    this.setState({ isTimeEnabled: !isTimeEnabled }, () => {
-      this.updatePropertyValue();
-    });
-  };
-
-  onTimeZoneSelected = (timezone: string): void => {
-    const { dateValue } = this.state;
-    const offset = this.getOffset(timezone, dateValue);
-    const dateVisible = new Date(dateValue + offset);
-    this.setState({ timezone: timezone, date: dateVisible }, () => {
-      this.updatePropertyValue();
-    });
-  };
-
   onDateSelected = (date: Date): void => {
-    const { timezone } = this.state;
-    const offset = this.getOffset(timezone, date.getTime());
-    const dateValue = new Date(date.getTime() - offset).getTime();
-    this.setState({ date: date, dateValue: dateValue }, () => {
-      this.updatePropertyValue();
-    });
-  };
-
-  updatePropertyValue = () => {
-    const { dateValue, isTimeEnabled, timezone } = this.state;
-    this.updateProperty(this.props.propertyName, {
-      timezone: timezone,
-      dateValue: dateValue,
-      isTimeEnabled: isTimeEnabled,
-    });
+    this.updateProperty(
+      this.props.propertyName,
+      moment(date).toISOString(true),
+    );
   };
 
   formatDate = (date: Date): string => {
-    let dateFormat = "DD/MM/YYYY";
-    if (this.state.isTimeEnabled) {
-      dateFormat = "DD/MM/YYYY HH:mm";
-    }
-    return moment(date).format(dateFormat);
+    return moment(date).format("DD/MM/YYYY HH:mm");
   };
 
   parseDate = (dateStr: string): Date => {
-    const date = moment(dateStr).toDate();
-    return date;
+    return moment(dateStr).toDate();
   };
 
   static getControlType() {
@@ -188,11 +66,7 @@ class DatePickerControl extends BaseControl<
 
 export interface DatePickerControlProps extends ControlProps {
   placeholderText: string;
-  propertyValue: {
-    timezone: string;
-    dateValue: number;
-    isTimeEnabled: boolean;
-  };
+  propertyValue: string;
 }
 
 export default DatePickerControl;
