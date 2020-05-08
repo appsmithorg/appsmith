ff2f32290f98fdaf13e5ea6c75012cdd238fcb76
Hetu Nandu <hetu@appsmith.com>
2020-05-08 12:54:27 +0000

    Fix content type switching to raw when custom

--- app/client/src/transformers/RestActionTransformers.test.ts
+++ app/client/src/transformers/RestActionTransformers.test.ts
@@ -1,28 +1,9 @@
 import { transformRestAction } from "transformers/RestActionTransformer";
 import { RestAction } from "api/ActionAPI";
 
-const input: RestAction = {
-  pageId: "",
-  pluginId: "",
-  id: "testId",
-  datasource: {
-    id: "testDataSource",
-  },
-  name: "testName",
-  pluginType: "API",
-  actionConfiguration: {
-    path: "users?page=1",
-    queryParameters: [
-      {
-        key: "page",
-        value: "1",
-      },
-    ],
-  },
-  jsonPathKeys: [],
-};
+// jest.mock("POST_");
 
-const output = {
+const BASE_ACTION: RestAction = {
   pageId: "",
   pluginId: "",
   id: "testId",
@@ -32,18 +13,135 @@ const output = {
   name: "testName",
   pluginType: "API",
   actionConfiguration: {
+    httpMethod: "GET",
     path: "users",
-    queryParameters: [
-      {
-        key: "page",
-        value: "1",
-      },
-    ],
   },
   jsonPathKeys: [],
 };
 
-it("Transform the input", () => {
-  const result = transformRestAction(input);
-  expect(result).toEqual(output);
+describe("Api action transformer", () => {
+  it("Removes params from path", () => {
+    const input: RestAction = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        path: "users?page=1",
+        queryParameters: [
+          {
+            key: "page",
+            value: "1",
+          },
+        ],
+      },
+    };
+
+    const output = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        path: "users",
+        queryParameters: [
+          {
+            key: "page",
+            value: "1",
+          },
+        ],
+      },
+    };
+    const result = transformRestAction(input);
+    expect(result).toEqual(output);
+  });
+
+  it("removes body for GET calls", () => {
+    const input = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        httpMethod: "GET",
+        body: [null, null],
+      },
+    };
+    const output = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        httpMethod: "GET",
+      },
+    };
+    const result = transformRestAction(input);
+    expect(result).toEqual(output);
+  });
+
+  it("Sets the correct body for JSON display type", () => {
+    const input = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        httpMethod: "POST",
+        headers: [{ key: "content-type", value: "application/json" }],
+        body: ["{ name: 'test' }", null],
+      },
+    };
+    const output = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        httpMethod: "POST",
+        headers: [{ key: "content-type", value: "application/json" }],
+        body: "{ name: 'test' }",
+      },
+    };
+    const result = transformRestAction(input);
+    expect(result).toEqual(output);
+  });
+
+  it("Sets the correct body for xxx-form-encoded-data display type", () => {
+    const input = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        httpMethod: "POST",
+        headers: [
+          { key: "content-type", value: "application/x-www-form-urlencoded" },
+        ],
+        body: [{ name: "test" }, [{ key: "hey", value: "ho" }]],
+      },
+    };
+    const output = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        httpMethod: "POST",
+        headers: [
+          { key: "content-type", value: "application/x-www-form-urlencoded" },
+        ],
+        body: '[{"key":"hey","value":"ho"}]',
+      },
+    };
+    const result = transformRestAction(input);
+    expect(result).toEqual(output);
+  });
+
+  it("Sets the correct body for custom/raw display type", () => {
+    const input = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        headers: [{ key: "content-type", value: "text/html" }],
+        httpMethod: "POST",
+        body: [{ name: "test" }, [{ key: "hey", value: "ho" }], "raw body"],
+      },
+    };
+    const output = {
+      ...BASE_ACTION,
+      actionConfiguration: {
+        ...BASE_ACTION.actionConfiguration,
+        headers: [{ key: "content-type", value: "text/html" }],
+        httpMethod: "POST",
+        body: "raw body",
+      },
+    };
+    const result = transformRestAction(input);
+    expect(result).toEqual(output);
+  });
 });
