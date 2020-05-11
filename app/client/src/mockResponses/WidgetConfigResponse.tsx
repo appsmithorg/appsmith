5a06911b2f14d53e2e45db290ddbb4c1294e9745
Satbir Singh <satbir@appsmith.com>
2020-05-11 19:49:58 +0530

    Adding aligment property to TextWidget

--- app/client/src/mockResponses/WidgetConfigResponse.tsx
+++ app/client/src/mockResponses/WidgetConfigResponse.tsx
@@ -17,6 +17,7 @@ const WidgetConfigResponse: WidgetConfigReducerState = {
     TEXT_WIDGET: {
       text: "Label",
       textStyle: "LABEL",
+      textAlign: "LEFT",
       rows: 1,
       columns: 4,
       widgetName: "Text",
@@ -398,6 +399,7 @@ const WidgetConfigResponse: WidgetConfigReducerState = {
                     props: {
                       text: "Title",
                       textStyle: "HEADING",
+                      textAlign: "LEFT",
                     },
                   },
                   {
