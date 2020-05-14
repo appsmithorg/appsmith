cb6098ff167e12ef542ed09466f4e43b9078d0e0
Arpit Mohan <arpit@appsmith.com>
2020-05-14 08:49:06 +0530

    Using the last value of duplicate headers as that is the behaviour displayed by webclient and other clients as well.

--- appsmith-server/src/test/java/com/appsmith/server/services/ActionServiceTest.java
+++ appsmith-server/src/test/java/com/appsmith/server/services/ActionServiceTest.java
@@ -22,9 +22,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -246,7 +249,7 @@ public class ActionServiceTest {
         actionConfiguration.setHeaders(List.of(
                 new Property("random-header-key", "random-header-value"),
                 new Property("dup-key", "dup-value1"),
-                new Property("dup-key", "dup-value2")
+                new Property("DUP-key", "dup-value2")
         ));
         action.setActionConfiguration(actionConfiguration);
 
@@ -256,9 +259,11 @@ public class ActionServiceTest {
         Mono<ActionExecutionResult> executionResultMono = executeAction(executeActionDTO, actionConfiguration, mockResult);
         StepVerifier.create(executionResultMono)
                 .assertNext(result -> {
-                    // The fxn should have ignored all duplicate headers
+                    // Assert that the fxn should pick up the latest key based on the case-insensitive values
                     assertThat(result.getRequestHeaders().size()).isEqualTo(2);
-                    Map<String, String> resultHeader = Map.of("random-header-key", "random-header-value", "dup-key", "dup-value1");
+                    MultiValueMap<String, String> resultHeader = CollectionUtils.toMultiValueMap(Map.of(
+                            "random-header-key", Arrays.asList("random-header-value"),
+                            "DUP-key", Arrays.asList("dup-value2")));
                     assertThat(result.getRequestHeaders()).isEqualTo(objectMapper.valueToTree(resultHeader));
                 })
                 .verifyComplete();
@@ -288,7 +293,9 @@ public class ActionServiceTest {
                 .assertNext(result -> {
                     // The fxn should have ignored all duplicate headers
                     assertThat(result.getRequestHeaders().size()).isEqualTo(1);
-                    Map<String, String> resultHeader = Map.of("random-header-key", "random-header-value");
+                    MultiValueMap<String, String> resultHeader = CollectionUtils.toMultiValueMap(Map.of(
+                            "random-header-key", Arrays.asList("random-header-value")));
+
                     assertThat(result.getRequestHeaders()).isEqualTo(objectMapper.valueToTree(resultHeader));
                 })
                 .verifyComplete();
