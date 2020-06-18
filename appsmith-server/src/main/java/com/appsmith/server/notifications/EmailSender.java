dc3654cb979422f89d791a80bf55645dc672f244
Shrikant Kandula <shrikant@appsmith.com>
2020-06-18 17:29:36 +0530

    Sending emails is now done in a non-blocking way

--- appsmith-server/src/main/java/com/appsmith/server/notifications/EmailSender.java
+++ appsmith-server/src/main/java/com/appsmith/server/notifications/EmailSender.java
@@ -5,11 +5,12 @@ import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import reactor.core.Exceptions;
+import reactor.core.publisher.Mono;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
@@ -25,20 +26,39 @@ import java.util.regex.Pattern;
 @Slf4j
 public class EmailSender {
 
-    @Autowired
-    JavaMailSender emailSender;
+    final JavaMailSender javaMailSender;
 
-    @Autowired
-    EmailConfig emailConfig;
+    final EmailConfig emailConfig;
 
     private static final InternetAddress MAIL_FROM = makeFromAddress();
 
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
+    public EmailSender(JavaMailSender javaMailSender, EmailConfig emailConfig) {
+        this.javaMailSender = javaMailSender;
+        this.emailConfig = emailConfig;
+    }
+
     private static boolean validateEmail(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
         return matcher.find();
+    }
+
+    public Mono<Void> sendMail(String to, String subject, String text, Map<String, String> params) {
+        return Mono
+                .fromSupplier(() -> {
+                    try {
+                        return replaceEmailTemplate(text, params);
+                    } catch (IOException e) {
+                        throw Exceptions.propagate(e);
+                    }
+                })
+                .flatMap(emailBody -> sendMail(to, subject, emailBody));
+    }
+
+    public Mono<Void> sendMail(String to, String subject, String text) {
+        return Mono.fromRunnable(() -> sendMailSync(to, subject, text));
     }
 
     /**
@@ -48,7 +68,7 @@ public class EmailSender {
      * @param subject Subject string.
      * @param text    HTML Body of the message. This method assumes UTF-8.
      */
-    public void sendMail(String to, String subject, String text) {
+    private void sendMailSync(String to, String subject, String text) {
         log.debug("Got request to send email to: {} with subject: {}", to, subject);
         // Don't send an email for local, dev or test environments
         if (!emailConfig.isEmailEnabled()) {
@@ -67,7 +87,7 @@ public class EmailSender {
         }
 
         log.debug("Going to send email to {} with subject {}", to, subject);
-        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
         try {
@@ -75,7 +95,7 @@ public class EmailSender {
             helper.setFrom(MAIL_FROM);
             helper.setSubject(subject);
             helper.setText(text, true);
-            emailSender.send(mimeMessage);
+            javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
             log.error("Unable to create the mime message while sending an email to {} with subject: {}. Cause: ", to, subject, e);
         } catch (MailException e) {
@@ -89,16 +109,15 @@ public class EmailSender {
      * @param template The name of the template where the HTML text can be found
      * @param params   A Map of key-value pairs with the key being the variable in the template & value being the actual
      *                 value with which it must be replaced.
-     * @return
-     * @throws IOException
+     * @return Template string with Mustache replacements applied.
+     * @throws IOException bubbled from Mustache renderer.
      */
-    public String replaceEmailTemplate(String template, Map<String, String> params) throws IOException {
+    private String replaceEmailTemplate(String template, Map<String, String> params) throws IOException {
         MustacheFactory mf = new DefaultMustacheFactory();
         StringWriter stringWriter = new StringWriter();
         Mustache mustache = mf.compile(template);
         mustache.execute(stringWriter, params).flush();
-        String emailTemplate = stringWriter.toString();
-        return emailTemplate;
+        return stringWriter.toString();
     }
 
     private static InternetAddress makeFromAddress() {
