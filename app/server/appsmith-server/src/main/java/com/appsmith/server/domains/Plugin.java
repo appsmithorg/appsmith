eee2cfcaff944d14ccaa2ffe530424386b2f2a32
Shrikant Kandula <shrikant@appsmith.com>
2020-06-24 11:08:25 +0000

    Add server-side templates support for plugins

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Plugin.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Plugin.java
@@ -6,9 +6,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import net.minidev.json.annotate.JsonIgnore;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
@@ -43,5 +45,8 @@ public class Plugin extends BaseDomain {
     Boolean defaultInstall;
 
     Boolean allowUserDatasources = true;
+
+    @Transient
+    Map<String, String> templates;
 
 }
