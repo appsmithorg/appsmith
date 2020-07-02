2b2b52dcca7c596c114bae4ecf618296eccb06aa
Abhinav Jha <abhinav@appsmith.com>
2020-07-02 05:38:56 +0530

    Use envsubst and nginx templates to generate nginx configs which can substitute environment variables and inject into the index.html file

--- app/client/start-https.sh
+++ app/client/start-https.sh
@@ -11,6 +11,14 @@ if ! docker_loc="$(type -p "docker")" || [[ -z $docker_loc ]]; then
   exit
 fi
 
+if ! envsubst_loc="$(type -p "envsubst")" || [[ -z $envsubst_loc ]]; then
+	echo "Could not find envsubst: If you're on a mac; brew install gettext"
+  exit
+else
+	echo "found envsubst"
+fi
+
+
 KEY_FILE=./docker/_wildcard.appsmith.com-key.pem
 CERT_FILE=./docker/_wildcard.appsmith.com.pem
 if ! test -f "$KEY_FILE" || ! test -f "$CERT_FILE"; then
@@ -26,13 +34,25 @@ if ! test -f "$KEY_FILE" || ! test -f "$CERT_FILE"; then
     exit
 fi
 
+ENV_FILE=../../.env
+if ! test -f "$ENV_FILE"; then
+    echo "
+        Please populate the .env at the root of the project and run again
+        Or add the environment variables defined in .env.example to the environment
+        -- to enable features
+    "
+else
+    export $(cat ${ENV_FILE} | xargs)
+fi
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=Linux
                 echo "
     Starting nginx for Linux...
     "
-                sudo docker run --network host --name wildcard-nginx -d -p 80:80 -p 443:443 -v `pwd`/docker/nginx-linux.conf:/etc/nginx/conf.d/app.conf -v `pwd`/docker/_wildcard.appsmith.com.pem:/etc/certificate/dev.appsmith.com.pem -v `pwd`/docker/_wildcard.appsmith.com-key.pem:/etc/certificate/dev.appsmith.com-key.pem nginx:latest \
+                envsubst \$SENTRY_DSN,\$HOTJAR_HJID,\$GOOGLE_CLIENT_ID,\$GITHUB_CLIENT_ID < ./docker/templates/nginx-linux.conf.template > ./docker/nginx.conf &&
+                sudo docker run --network host --name wildcard-nginx -d -p 80:80 -p 443:443 -v `pwd`/docker/nginx.conf:/etc/nginx/conf.d/app.conf -v `pwd`/docker/_wildcard.appsmith.com.pem:/etc/certificate/dev.appsmith.com.pem -v `pwd`/docker/_wildcard.appsmith.com-key.pem:/etc/certificate/dev.appsmith.com-key.pem nginx:latest \
                 && echo "
     nginx is listening on port 443 and forwarding to port 3000
     visit https://dev.appsmith.com
@@ -42,7 +62,8 @@ case "${unameOut}" in
                 echo "
     Starting nginx for MacOS...
     "
-                docker run --name wildcard-nginx -d -p 80:80 -p 443:443 -v `pwd`/docker/nginx-mac.conf:/etc/nginx/conf.d/app.conf -v `pwd`/docker/_wildcard.appsmith.com.pem:/etc/certificate/dev.appsmith.com.pem -v `pwd`/docker/_wildcard.appsmith.com-key.pem:/etc/certificate/dev.appsmith.com-key.pem nginx:latest \
+                envsubst \$SENTRY_DSN,\$HOTJAR_HJID,\$GOOGLE_CLIENT_ID,\$GITHUB_CLIENT_ID < ./docker/templates/nginx-mac.conf.template > ./docker/nginx.conf &&
+                docker run --name wildcard-nginx -d -p 80:80 -p 443:443 -v `pwd`/docker/nginx.conf:/etc/nginx/conf.d/app.conf -v `pwd`/docker/_wildcard.appsmith.com.pem:/etc/certificate/dev.appsmith.com.pem -v `pwd`/docker/_wildcard.appsmith.com-key.pem:/etc/certificate/dev.appsmith.com-key.pem nginx:latest \
                 && echo "
     nginx is listening on port 443 and forwarding to port 3000
     visit https://dev.appsmith.com
