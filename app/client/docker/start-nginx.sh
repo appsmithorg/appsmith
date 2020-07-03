c0dd3144cdc99294780bc1aa3bbdd5e0487cc3f5
Abhinav Jha <abhinav@appsmith.com>
2020-07-03 17:26:44 +0530

    Remove debug code from start-nginx.sh

--- app/client/docker/start-nginx.sh
+++ app/client/docker/start-nginx.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -ue
-which envsubst;
-export APPSMITH_GOOGLE_MAPS_API_KEY=" "
-export APPSMITH_OPTIMIZELY_KEY=SOMETHING
-export APPSMITH_ALGOLIA_API_KEY="algolia something"
 cat /nginx.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z_]*\)}||g' | tee /etc/nginx/conf.d/app.conf
 exec nginx -g 'daemon off;'
