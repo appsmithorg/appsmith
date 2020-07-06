c6899096d56dbbda0da69de876a773b0a3a85abc
Shrikant Sharat Kandula <shrikant@appsmith.com>
2020-07-06 14:17:01 +0530

    Source .env file in the root of repo in start script

--- app/server/scripts/start-dev-server.sh
+++ app/server/scripts/start-dev-server.sh
@@ -4,7 +4,10 @@
 cd "$(cd "$(dirname "$0")" && pwd)/.."
 
 # Ref: <https://stackoverflow.com/a/30969768/151048>.
-set -o allexport
-source envs/dev.env
+if [[ -f .env ]]; then
+  echo "Found a .env file, loading environment variables from that file."
+  set -o allexport
+  source .env
+fi
 
 exec java -jar dist/server-*.jar
