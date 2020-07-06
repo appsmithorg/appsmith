d753374aa9495a8397e5e3c01d56bca8c8dfe196
satbir121 <39981226+satbir121@users.noreply.github.com>
2020-07-06 19:06:42 +0530

    Using TinyMCE from cdn to remove the warning message. (#39)
    * Removing tinymce api key message.
    
    * Updating yarn lock.
    
    * Fixing editor disabled.
    
    * Default set on refresh.
    
    Co-authored-by: Satbir Singh <apple@apples-MacBook-Pro.local>

--- app/client/src/components/designSystems/appsmith/RichTextEditorComponent.tsx
+++ app/client/src/components/designSystems/appsmith/RichTextEditorComponent.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Editor } from "@tinymce/tinymce-react";
+import React, { useEffect, useState } from "react";
+import { debounce } from "lodash";
 import styled from "styled-components";
 const StyledRTEditor = styled.div`
   && {
@@ -22,26 +22,66 @@ export interface RichtextEditorComponentProps {
 export const RichtextEditorComponent = (
   props: RichtextEditorComponentProps,
 ) => {
+  const [editorInstance, setEditorInstance] = useState(null as any);
+  useEffect(() => {
+    if (editorInstance !== null) {
+      editorInstance.mode.set(
+        props.isDisabled === true ? "readonly" : "design",
+      );
+    }
+  }, [props.isDisabled]);
+
+  useEffect(() => {
+    if (editorInstance !== null) {
+      editorInstance.setContent(props.defaultValue, { format: "html" });
+    }
+  }, [props.defaultValue]);
+  useEffect(() => {
+    const onChange = debounce(props.onValueChange, 200);
+    (window as any).tinyMCE.init({
+      height: "100%",
+      selector: `textarea#${props.widgetId}`,
+      menubar: false,
+      branding: false,
+      resize: false,
+      setup: (editor: any) => {
+        editor.mode.set(props.isDisabled === true ? "readonly" : "design");
+        // Without timeout default value is not set on browser refresh.
+        setTimeout(() => {
+          editor.setContent(props.defaultValue, { format: "html" });
+        }, 300);
+        editor
+          .on("Change", () => {
+            onChange(editor.getContent());
+          })
+          .on("Undo", () => {
+            onChange(editor.getContent());
+          })
+          .on("Redo", () => {
+            onChange(editor.getContent());
+          })
+          .on("KeyUp", () => {
+            // console.log("change: ", editor.getContent())
+            onChange(editor.getContent());
+          });
+        setEditorInstance(editor);
+      },
+      plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table paste code help",
+      ],
+      toolbar:
+        "undo redo | formatselect | bold italic backcolor forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+    });
+
+    return () => {
+      editorInstance !== null && editorInstance.destroy();
+    };
+  }, []);
   return (
     <StyledRTEditor>
-      <Editor
-        value={props.defaultValue}
-        disabled={props.isDisabled}
-        init={{
-          height: "100%",
-          menubar: false,
-          branding: false,
-          resize: false,
-          plugins: [
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table paste code help",
-          ],
-          toolbar:
-            "undo redo | formatselect | bold italic backcolor forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
-        }}
-        onEditorChange={(content: any) => props.onValueChange(content)}
-      />
+      <textarea id={props.widgetId}></textarea>
     </StyledRTEditor>
   );
 };
