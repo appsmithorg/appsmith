da852bb1210aeeac2aa66592502e5daa84aeffa8
Nikhil Nandagopal <nikhil@appsmith.com>
2020-07-09 19:47:45 +0530

    fixed script syntax

--- deploy/install.sh
+++ deploy/install.sh
@@ -47,7 +47,7 @@ echo ""
 read -p 'Would you like to setup a custom domain to access appsmith? [Y/n]: ' setup_domain
 setup_domain=${setup_domain:-Y}
 
-if [$setup_domain == "Y" -o $setup_domain == "y" -o $setup_domain == "yes" -o $setup_domain == "Yes" ];then
+if [ $setup_domain == "Y" -o $setup_domain == "y" -o $setup_domain == "yes" -o $setup_domain == "Yes" ];then
 	read -p 'Enter your domain URL (https://example.com): ' custom_domain
 fi
 
