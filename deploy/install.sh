7d90cc5da79288763b2306ee6fb692f6c62f2daf
Arpit Mohan <me@arpitmohan.com>
2020-07-10 15:59:17 +0530

    Removing python Docker bindings

--- deploy/install.sh
+++ deploy/install.sh
@@ -99,8 +99,6 @@ echo "Installing docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-pip3 install docker
-
 # Role - folders
 ubuntu="/etc/debian_version"
 centos="/etc/centos-release"
