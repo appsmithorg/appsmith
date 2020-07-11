0e15b8537c00560f2c8d9799d37823e332d18ae4
Arpit Mohan <me@arpitmohan.com>
2020-07-11 14:38:58 +0530

    Adding matrix strategy to parallelize the Cypress tests

--- app/client/cypress/test.sh
+++ app/client/cypress/test.sh
@@ -30,7 +30,7 @@ if [ "$target" == "ci" ]; then
     $(npm bin)/cypress run --headless --browser chrome \
     --record --key "$CYPRESS_RECORD_KEY" --ci-build-id $BUILD_ID \
     --parallel --group "Electrons on Gitlab CI" \
-    --spec "cypress/integration/Smoke_TestSuite/ApiPaneTests/API_all_sidebar_actions_spec.js"
+    --spec "cypress/integration/Smoke_TestSuite/ApiPaneTests/*.js"
 else
-    $(npm bin)/cypress run --headless --browser chrome --spec "cypress/integration/Smoke_TestSuite/ApiPaneTests/API_all_sidebar_actions_spec.js"
+    $(npm bin)/cypress run --headless --browser chrome --spec "cypress/integration/Smoke_TestSuite/ApiPaneTests/*.js"
 fi
