d33cb5f8ebddd72f1a1816426e821c2e9be946da
Arpit Mohan <me@arpitmohan.com>
2020-07-11 15:11:06 +0530

    Removing the committed build folder and building the image on the fly now

--- app/client/build/static/js/appsmith.b49c4faa.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpappsmith=this.webpackJsonpappsmith||[]).push([[6],{1090:function(t,n,p){"use strict";p.r(n),p.d(n,"App",(function(){return r}));var e=p(0),a=p.n(e),s=p(362),i=p(33),r=function(){return a.a.createElement(s.a,{to:i.d})};n.default=r}}]);
-//# sourceMappingURL=appsmith.b49c4faa.chunk.js.map
