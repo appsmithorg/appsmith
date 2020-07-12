8a67771c746d598b239eb2f06ec2359230914da2
Arpit Mohan <me@arpitmohan.com>
2020-07-12 22:39:04 +0530

    Adding the cypress env variable to the client.yml step in github action

--- app/client/cypress/support/index.js
+++ app/client/cypress/support/index.js
@@ -29,6 +29,8 @@ before(function() {
   cy.startServerAndRoutes();
   const username = Cypress.env("USERNAME");
   const password = Cypress.env("PASSWORD");
+  cy.task("log", "Got the username from the environment as:");
+  cy.task("log", username);
   cy.LoginFromAPI(username, password);
   cy.visit("/applications");
   cy.wait("@applications").should(
