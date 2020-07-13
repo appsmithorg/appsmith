17bdf2e27b69f9a0c34c2fb0ca795dc800bdd4e1
Manish <manish535@users.noreply.github.com>
2020-07-13 15:57:49 +0530

    Update install.sh
    The following cmd moved inside if block 'echo "Installing docker-compose"'

--- deploy/install.sh
+++ deploy/install.sh
@@ -27,9 +27,9 @@ install_docker() {
     sudo ${package_manager} -y update --quiet
     echo "Installing docker"
     sudo ${package_manager} -y install docker-ce docker-ce-cli containerd.io --quiet --nobest
-
-    echo "Installing docker-compose"
+   
     if [ ! -f /usr/bin/docker-compose ];then
+        echo "Installing docker-compose"
         sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
         sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
