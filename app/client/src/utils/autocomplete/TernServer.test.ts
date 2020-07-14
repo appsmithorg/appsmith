cb95bc21cc40349d65f00f928512858df20872ce
Akash N <akash@codemonk.in>
2020-07-14 12:46:23 +0530

    Keep input and expected values together

--- app/client/src/utils/autocomplete/TernServer.test.ts
+++ app/client/src/utils/autocomplete/TernServer.test.ts
@@ -8,35 +8,43 @@ describe("Tern server", () => {
 
     const testCases = [
       {
-        name: "test",
-        doc: ({
-          getCursor: () => ({ ch: 0, line: 0 }),
-          getLine: () => "{{Api.}}",
-        } as unknown) as CodeMirror.Doc,
-        changed: null,
+        input: {
+          name: "test",
+          doc: ({
+            getCursor: () => ({ ch: 0, line: 0 }),
+            getLine: () => "{{Api.}}",
+          } as unknown) as CodeMirror.Doc,
+          changed: null,
+        },
+        expectedOutput: "{{Api.}}",
       },
       {
-        name: "test",
-        doc: ({
-          getCursor: () => ({ ch: 0, line: 0 }),
-          getLine: () => "a{{Api.}}",
-        } as unknown) as CodeMirror.Doc,
-        changed: null,
+        input: {
+          name: "test",
+          doc: ({
+            getCursor: () => ({ ch: 0, line: 0 }),
+            getLine: () => "a{{Api.}}",
+          } as unknown) as CodeMirror.Doc,
+          changed: null,
+        },
+        expectedOutput: "a{{Api.}}",
       },
       {
-        name: "test",
-        doc: ({
-          getCursor: () => ({ ch: 2, line: 0 }),
-          getLine: () => "a{{Api.}}",
-        } as unknown) as CodeMirror.Doc,
-        changed: null,
+        input: {
+          name: "test",
+          doc: ({
+            getCursor: () => ({ ch: 2, line: 0 }),
+            getLine: () => "a{{Api.}}",
+          } as unknown) as CodeMirror.Doc,
+          changed: null,
+        },
+        expectedOutput: "{{Api.}}",
       },
     ];
-    const expectedValues = ["{{Api.}}", "a{{Api.}}", "{{Api.}}"];
 
-    testCases.forEach((testCase, index) => {
-      const value = ternServer.getFocusedDynamicValue(testCase);
-      expect(value).toBe(expectedValues[index]);
+    testCases.forEach(testCase => {
+      const value = ternServer.getFocusedDynamicValue(testCase.input);
+      expect(value).toBe(testCase.expectedOutput);
     });
   });
 
@@ -44,43 +52,47 @@ describe("Tern server", () => {
     const ternServer = new TernServer({});
     const testCases = [
       {
-        name: "test",
-        doc: ({
-          getCursor: () => ({ ch: 0, line: 0 }),
-          getLine: () => "{{Api.}}",
-          somethingSelected: () => false,
-        } as unknown) as CodeMirror.Doc,
-        changed: null,
+        input: {
+          name: "test",
+          doc: ({
+            getCursor: () => ({ ch: 0, line: 0 }),
+            getLine: () => "{{Api.}}",
+            somethingSelected: () => false,
+          } as unknown) as CodeMirror.Doc,
+          changed: null,
+        },
+        expectedOutput: { ch: 0, line: 0 },
       },
       {
-        name: "test",
-        doc: ({
-          getCursor: () => ({ ch: 0, line: 1 }),
-          getLine: () => "{{Api.}}",
-          somethingSelected: () => false,
-        } as unknown) as CodeMirror.Doc,
-        changed: null,
+        input: {
+          name: "test",
+          doc: ({
+            getCursor: () => ({ ch: 0, line: 1 }),
+            getLine: () => "{{Api.}}",
+            somethingSelected: () => false,
+          } as unknown) as CodeMirror.Doc,
+          changed: null,
+        },
+        expectedOutput: { ch: 0, line: 0 },
       },
       {
-        name: "test",
-        doc: ({
-          getCursor: () => ({ ch: 3, line: 1 }),
-          getLine: () => "g {{Api.}}",
-          somethingSelected: () => false,
-        } as unknown) as CodeMirror.Doc,
-        changed: null,
+        input: {
+          name: "test",
+          doc: ({
+            getCursor: () => ({ ch: 3, line: 1 }),
+            getLine: () => "g {{Api.}}",
+            somethingSelected: () => false,
+          } as unknown) as CodeMirror.Doc,
+          changed: null,
+        },
+        expectedOutput: { ch: 1, line: 0 },
       },
-    ];
-    const expectedValues = [
-      { ch: 0, line: 0 },
-      { ch: 0, line: 0 },
-      { ch: 1, line: 0 },
     ];
 
     testCases.forEach((testCase, index) => {
-      const request = ternServer.buildRequest(testCase, {});
+      const request = ternServer.buildRequest(testCase.input, {});
 
-      expect(request.query.end).toEqual(expectedValues[index]);
+      expect(request.query.end).toEqual(testCase.expectedOutput);
     });
   });
 
@@ -90,60 +102,64 @@ describe("Tern server", () => {
 
     const testCases = [
       {
-        codeEditor: {
-          value: "{{}}",
-          cursor: { ch: 2, line: 0 },
-          doc: ({
-            getCursor: () => ({ ch: 2, line: 0 }),
-            getLine: () => "{{}}",
-            somethingSelected: () => false,
-          } as unknown) as CodeMirror.Doc,
+        input: {
+          codeEditor: {
+            value: "{{}}",
+            cursor: { ch: 2, line: 0 },
+            doc: ({
+              getCursor: () => ({ ch: 2, line: 0 }),
+              getLine: () => "{{}}",
+              somethingSelected: () => false,
+            } as unknown) as CodeMirror.Doc,
+          },
+          requestCallbackData: {
+            completions: [{ name: "Api1" }],
+            start: { ch: 2, line: 0 },
+            end: { ch: 6, line: 0 },
+          },
         },
-        requestCallbackData: {
-          completions: [{ name: "Api1" }],
-          start: { ch: 2, line: 0 },
-          end: { ch: 6, line: 0 },
-        },
+        expectedOutput: { ch: 2, line: 0 },
       },
       {
-        codeEditor: {
-          value: "\n {{}}",
-          cursor: { ch: 3, line: 1 },
-          doc: ({
-            getCursor: () => ({ ch: 3, line: 1 }),
-            getLine: () => " {{}}",
-            somethingSelected: () => false,
-          } as unknown) as CodeMirror.Doc,
+        input: {
+          codeEditor: {
+            value: "\n {{}}",
+            cursor: { ch: 3, line: 1 },
+            doc: ({
+              getCursor: () => ({ ch: 3, line: 1 }),
+              getLine: () => " {{}}",
+              somethingSelected: () => false,
+            } as unknown) as CodeMirror.Doc,
+          },
+          requestCallbackData: {
+            completions: [{ name: "Api1" }],
+            start: { ch: 2, line: 1 },
+            end: { ch: 6, line: 1 },
+          },
         },
-        requestCallbackData: {
-          completions: [{ name: "Api1" }],
-          start: { ch: 2, line: 1 },
-          end: { ch: 6, line: 1 },
-        },
+        expectedOutput: { ch: 3, line: 1 },
       },
-    ];
-    const expectedValues = [
-      { ch: 2, line: 0 },
-      { ch: 3, line: 1 },
     ];
 
     testCases.forEach((testCase, index) => {
       MockCodemirrorEditor.getValue.mockReturnValueOnce(
-        testCase.codeEditor.value,
+        testCase.input.codeEditor.value,
       );
       MockCodemirrorEditor.getCursor.mockReturnValueOnce(
-        testCase.codeEditor.cursor,
+        testCase.input.codeEditor.cursor,
       );
-      MockCodemirrorEditor.getDoc.mockReturnValueOnce(testCase.codeEditor.doc);
+      MockCodemirrorEditor.getDoc.mockReturnValueOnce(
+        testCase.input.codeEditor.doc,
+      );
 
       const value: any = ternServer.requestCallback(
         null,
-        testCase.requestCallbackData,
+        testCase.input.requestCallbackData,
         (MockCodemirrorEditor as unknown) as CodeMirror.Editor,
         () => null,
       );
 
-      expect(value.from).toEqual(expectedValues[index]);
+      expect(value.from).toEqual(testCase.expectedOutput);
     });
   });
 });
