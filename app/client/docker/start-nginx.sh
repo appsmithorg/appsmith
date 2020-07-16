1060423b9989ea074c55faad2a0fae9eb7a36a8c
Arpit Mohan <mohanarpit@users.noreply.github.com>
2020-07-16 11:47:45 +0530

    Modifying deploy script to replace nginx configuration at runtime (#106)
    * Getting the script to work on Mac OS X Bash Version 3
    
    * Correcting the nginx template configuration and Dockerfile for the appsmith-editor. Now any replaced environment variables will be replaced when the Nginx Docker container restarts.

--- app/client/docker/start-nginx.sh
+++ app/client/docker/start-nginx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ue
-cat /nginx.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z0-9_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' | tee /etc/nginx/conf.d/app.conf
+cat /nginx.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z0-9_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
