1eb4cc7f14dbfa8658946329fc46fd86efc6b3f4
Tejaaswini <tejaaswini.narendra@codemonk.in>
2020-07-17 11:18:43 +0530

    fix: Add home button on appviewer screen

--- app/client/src/pages/AppViewer/index.tsx
+++ app/client/src/pages/AppViewer/index.tsx
@@ -77,6 +77,7 @@ class AppViewer extends Component<
 > {
   public state = {
     registered: false,
+    isSideNavOpen: true,
   };
   componentDidMount() {
     editorInitializer().then(() => {
@@ -87,6 +88,10 @@ class AppViewer extends Component<
       this.props.initializeAppViewer(applicationId);
     }
   }
+
+  toggleCollapse = (open: boolean) => {
+    this.setState({ isSideNavOpen: open });
+  };
 
   public render() {
     const { isInitialized, currentApplication } = this.props;
@@ -120,10 +125,16 @@ class AppViewer extends Component<
             url={this.props.editorURL}
             permissions={userPermissions || []}
             permissionRequired={PERMISSION_TYPE.MANAGE_APPLICATION}
+            open={this.state.isSideNavOpen}
           />
           <AppViewerBody>
             <AppViewerSideNavWrapper>
-              <SideNav items={items} active={this.props.currentDSLPageId} />
+              <SideNav
+                items={items}
+                active={this.props.currentDSLPageId}
+                open={this.state.isSideNavOpen}
+                toggleCollapse={this.toggleCollapse}
+              />
             </AppViewerSideNavWrapper>
             <Switch>
               <AppRoute
--- app/client/src/pages/AppViewer/viewer/AppViewerHeader.tsx
+++ app/client/src/pages/AppViewer/viewer/AppViewerHeader.tsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { AppState } from "reducers";
 import styled from "styled-components";
 import StyledHeader from "components/designSystems/appsmith/StyledHeader";
+import { withRouter, RouteComponentProps } from "react-router";
+import { fetchApplication } from "actions/applicationActions";
+import { FormIcons } from "icons/FormIcons";
 import Button from "components/editorComponents/Button";
 import { BACK_TO_EDITOR } from "constants/messages";
 import { isPermitted } from "pages/Applications/permissionHelpers";
+import { ApplicationPayload } from "constants/ReduxActionConstants";
+import { APPLICATIONS_URL } from "constants/routes";
+
 const HeaderWrapper = styled(StyledHeader)`
   position: fixed;
   top: 0;
   left: 0;
   background: white;
+  justify-content: space-between;
+`;
+
+const StyledHomeButton = styled.div<{
+  open: boolean;
+}>`
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
+  padding-left: ${props =>
+    props.open ? props.theme.sideNav.maxWidth : props.theme.sideNav.minWidth}px;
 `;
 
 const StyledButton = styled(Button)`
   max-width: 200px;
+  display: flex;
+  justify-content: flex-end;
 `;
-type AppViewerHeaderProps = {
+
+const StyledApplicationName = styled.span`
+  font-size: 15px;
+  padding-left: 8px;
+  &&:hover {
+    text-decoration: none;
+  }
+`;
+type AppViewerHeaderProps = RouteComponentProps<{ applicationId: string }> & {
   url?: string;
+  open: boolean;
   permissionRequired: string;
   permissions: string[];
+  currentApplicationDetails?: ApplicationPayload;
+  fetchApplication: (applicationId: string) => void;
 };
 
 export const AppViewerHeader = (props: AppViewerHeaderProps) => {
@@ -28,8 +58,38 @@ export const AppViewerHeader = (props: AppViewerHeaderProps) => {
     props.permissionRequired,
   );
 
+  const {
+    match: {
+      params: { applicationId },
+    },
+    fetchApplication,
+    currentApplicationDetails,
+    open,
+  } = props;
+
+  useEffect(() => {
+    fetchApplication(applicationId);
+  }, [fetchApplication, applicationId]);
+
   return (
     <HeaderWrapper>
+      {currentApplicationDetails && (
+        <StyledHomeButton open={open}>
+          <Link to={APPLICATIONS_URL}>
+            <FormIcons.HOME_ICON
+              height={20}
+              width={20}
+              color={"grey"}
+              background={"grey"}
+              onClick={() => {}}
+              style={{ alignSelf: "center", cursor: "pointer" }}
+            />
+            <StyledApplicationName>
+              {currentApplicationDetails.name}
+            </StyledApplicationName>
+          </Link>
+        </StyledHomeButton>
+      )}
       {props.url && hasPermission && (
         <StyledButton
           className="t--back-to-editor"
@@ -45,4 +105,16 @@ export const AppViewerHeader = (props: AppViewerHeaderProps) => {
   );
 };
 
-export default AppViewerHeader;
+const mapStateToProps = (state: AppState) => ({
+  currentApplicationDetails: state.ui.applications.currentApplication,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  fetchApplication: (applicationId: string) => {
+    return dispatch(fetchApplication(applicationId));
+  },
+});
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(AppViewerHeader),
+);
