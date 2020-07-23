379d5e62eedc41264c11a0e05fec951ff4d356a0
Hetu Nandu <hetunandu@gmail.com>
2020-07-23 11:34:19 +0530

    Add auth call

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 import { APPLICATIONS_URL, USER_AUTH_URL } from "constants/routes";
+import { useDispatch } from "react-redux";
+import { getCurrentUser } from "actions/userActions";
 
 export const App = () => {
+  const dispatch = useDispatch();
+  dispatch(getCurrentUser());
   return <Redirect to={USER_AUTH_URL} />;
 };
 
