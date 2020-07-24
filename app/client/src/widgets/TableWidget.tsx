eabd50e197e34aa3df6c8a8d12816abcd7cd82d4
vicky-primathon.in <vicky.bansal@primathon.in>
2020-07-24 15:50:01 +0530

    searchKeyword renamed to searchText. searchText added to entity definitions

--- app/client/src/widgets/TableWidget.tsx
+++ app/client/src/widgets/TableWidget.tsx
@@ -33,7 +33,7 @@ class TableWidget extends BaseWidget<TableWidgetProps, WidgetState> {
       prevPageKey: VALIDATION_TYPES.TEXT,
       label: VALIDATION_TYPES.TEXT,
       selectedRowIndex: VALIDATION_TYPES.NUMBER,
-      searchKeyword: VALIDATION_TYPES.TEXT,
+      searchText: VALIDATION_TYPES.TEXT,
       // columnActions: VALIDATION_TYPES.ARRAY_ACTION_SELECTOR,
       // onRowSelected: VALIDATION_TYPES.ACTION_SELECTOR,
       // onPageChange: VALIDATION_TYPES.ACTION_SELECTOR,
@@ -50,7 +50,7 @@ class TableWidget extends BaseWidget<TableWidgetProps, WidgetState> {
       pageNo: 1,
       pageSize: undefined,
       selectedRowIndex: -1,
-      searchKeyword: "",
+      searchText: "",
     };
   }
 
@@ -199,8 +199,8 @@ class TableWidget extends BaseWidget<TableWidgetProps, WidgetState> {
 
   searchTableData = (tableData: object[]) => {
     const searchKey =
-      this.props.searchKeyword !== undefined
-        ? this.props.searchKeyword.toString().toUpperCase()
+      this.props.searchText !== undefined
+        ? this.props.searchText.toString().toUpperCase()
         : "";
     return tableData.filter((item: object) => {
       return Object.values(item)
@@ -246,7 +246,7 @@ class TableWidget extends BaseWidget<TableWidgetProps, WidgetState> {
           isLoading={this.props.isLoading}
           widgetId={this.props.widgetId}
           widgetName={this.props.widgetName}
-          searchKey={this.props.searchKeyword}
+          searchKey={this.props.searchText}
           renderMode={this.props.renderMode}
           hiddenColumns={hiddenColumns}
           columnActions={this.props.columnActions}
@@ -298,7 +298,7 @@ class TableWidget extends BaseWidget<TableWidgetProps, WidgetState> {
 
   handleSearchTable = (searchKey: any) => {
     const { onSearchTextChanged } = this.props;
-    super.updateWidgetMetaProperty("searchKeyword", searchKey);
+    super.updateWidgetMetaProperty("searchText", searchKey);
     if (onSearchTextChanged) {
       super.executeAction({
         dynamicString: onSearchTextChanged,
@@ -381,7 +381,7 @@ export interface TableWidgetProps extends WidgetProps {
   nextPageKey?: string;
   prevPageKey?: string;
   label: string;
-  searchKeyword: string;
+  searchText: string;
   tableData: object[];
   onPageChange?: string;
   pageSize: number;
