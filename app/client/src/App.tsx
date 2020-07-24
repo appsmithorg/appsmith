c17076867c851af7a0863a68fc74f83b11d3585c
Hetu Nandu <hetunandu@gmail.com>
2020-07-24 18:41:00 +0530

    check for auth

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,8 +1,25 @@
 import React from "react";
 import CenteredWrapper from "components/designSystems/appsmith/CenteredWrapper";
+import { AppState } from "@appsmith/reducers";
+import { getCurrentUser } from "@appsmith/selectors/usersSelectors";
+import { connect } from "react-redux";
+import { User } from "@appsmith/constants/userConstants";
+import { Redirect } from "react-router";
+import { APPLICATIONS_URL } from "@appsmith/constants/routes";
 
-const App = () => {
-  return <CenteredWrapper>Loading</CenteredWrapper>;
+type Props = {
+  user?: User;
 };
 
-export default App;
+const App = (props: Props) => {
+  if (props.user) {
+    return <Redirect to={APPLICATIONS_URL} />;
+  }
+  return <CenteredWrapper>Checking auth</CenteredWrapper>;
+};
+
+const mapStateToProps = (state: AppState) => ({
+  user: getCurrentUser(state),
+});
+
+export default connect(mapStateToProps)(App);
