577838f04a075c23b212a19f1d0eeb2357dd84e3
Satbir Singh <satbir121@gmail.com>
2020-07-29 12:03:42 +0530

    Adding background param to components.

--- app/client/src/components/stories/Button.stories.tsx
+++ app/client/src/components/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "components/ads/Button";
+import Button, { Size } from "components/ads/Button";
 import { withKnobs, select, boolean, text } from "@storybook/addon-knobs";
 import { withDesign } from "storybook-addon-designs";
 
@@ -10,30 +10,21 @@ export default {
 };
 
 export const withDynamicProps = () => (
-  <div
-    style={{
-      width: "100%",
-      height: "600px",
-      backgroundColor: "#090707",
-      padding: "100px",
-    }}
-  >
-    <Button
-      size={select("size", ["small", "medium", "large"], "large")}
-      category={select(
-        "category",
-        ["primary", "secondary", "tertiary"],
-        "primary",
-      )}
-      variant={select(
-        "variant",
-        ["info", "success", "warning", "danger"],
-        "info",
-      )}
-      icon={select("iconName", ["delete", "user"], undefined)}
-      isLoading={boolean("Loading", false)}
-      isDisabled={boolean("Disabled", false)}
-      text={text("text", "Get")}
-    ></Button>
-  </div>
+  <Button
+    size={select("size", ["small", "medium", "large"], "large")}
+    category={select(
+      "category",
+      ["primary", "secondary", "tertiary"],
+      "primary",
+    )}
+    variant={select(
+      "variant",
+      ["info", "success", "warning", "danger"],
+      "info",
+    )}
+    icon={select("iconName", ["delete", "user"], undefined)}
+    isLoading={boolean("Loading", false)}
+    isDisabled={boolean("Disabled", false)}
+    text={text("text", "Get")}
+  ></Button>
 );
