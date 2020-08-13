cc8616f5b78e9a078d78a5b45914475871be6d3b
Akash N <akash@codemonk.in>
2020-08-13 18:18:09 +0530

    App homepage header new design

--- app/client/src/pages/common/PageHeader.tsx
+++ app/client/src/pages/common/PageHeader.tsx
@@ -4,29 +4,40 @@ import { connect } from "react-redux";
 import { getCurrentUser } from "selectors/usersSelectors";
 import styled from "styled-components";
 import StyledHeader from "components/designSystems/appsmith/StyledHeader";
+import AppsmithLogo from "assets/images/appsmith_logo_white.png";
 import CustomizedDropdown from "./CustomizedDropdown";
 import DropdownProps from "./CustomizedDropdown/HeaderDropdownData";
 import { AppState } from "reducers";
 import { User, ANONYMOUS_USERNAME } from "constants/userConstants";
-import Logo from "assets/images/appsmith_logo.png";
 import { AUTH_LOGIN_URL, APPLICATIONS_URL } from "constants/routes";
 import Button from "components/editorComponents/Button";
 import history from "utils/history";
+import { Colors } from "constants/Colors";
 
 const StyledPageHeader = styled(StyledHeader)`
-  width: 100%;
+  background: ${Colors.BALTIC_SEA};
+  height: 48px;
+  color: white;
+  flex-direction: row;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.05);
+`;
+
+const HeaderContentWrapper = styled.header`
   display: flex;
-  justify-content: space-between;
-  padding: ${props => props.theme.spaces[4]}px
-    ${props => props.theme.spaces[4]}px;
+  flex-direction: row;
+  width: ${props => props.theme.pageContentWidth - 25}px;
+`;
+
+const HeaderSection = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
 `;
 
 const StyledDropDownContainer = styled.div``;
 
-const LogoContainer = styled.div`
-  .logoimg {
-    width: 15%;
-  }
+const AppsmithLogoImg = styled.img`
+  max-width: 110px;
 `;
 
 type PageHeaderProps = {
@@ -44,26 +55,28 @@ export const PageHeader = (props: PageHeaderProps) => {
 
   return (
     <StyledPageHeader>
-      <LogoContainer>
-        <Link to={APPLICATIONS_URL}>
-          <img className="logoimg" src={Logo} alt="Appsmith Logo" />
-        </Link>
-      </LogoContainer>
-      {user && (
-        <StyledDropDownContainer>
-          {user.username === ANONYMOUS_USERNAME ? (
-            <Button
-              filled
-              text="Sign In"
-              intent={"primary"}
-              size="small"
-              onClick={() => history.push(loginUrl)}
-            />
-          ) : (
-            <CustomizedDropdown {...DropdownProps(user, user.username)} />
-          )}
-        </StyledDropDownContainer>
-      )}
+      <HeaderContentWrapper>
+        <HeaderSection>
+          <Link to={APPLICATIONS_URL}>
+            <AppsmithLogoImg src={AppsmithLogo} alt="Appsmith logo" />
+          </Link>
+        </HeaderSection>
+        {user && (
+          <StyledDropDownContainer>
+            {user.username === ANONYMOUS_USERNAME ? (
+              <Button
+                filled
+                text="Sign In"
+                intent={"primary"}
+                size="small"
+                onClick={() => history.push(loginUrl)}
+              />
+            ) : (
+              <CustomizedDropdown {...DropdownProps(user, user.username)} />
+            )}
+          </StyledDropDownContainer>
+        )}
+      </HeaderContentWrapper>
     </StyledPageHeader>
   );
 };
