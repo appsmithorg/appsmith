aab93af3e0ee4917d79502fe8dc0bdb4a17d227a
Abhinav Jha <abhinav@appsmith.com>
2020-08-21 00:08:31 +0530

    Fix: When copying a query, the app tries to open the API page with the queryID
    Fix: When moving a query, the query input box is empty

--- app/client/src/pages/Editor/QueryEditor/index.tsx
+++ app/client/src/pages/Editor/QueryEditor/index.tsx
@@ -53,7 +53,7 @@ type ReduxStateProps = {
   responses: any;
   isCreating: boolean;
   pluginImages: Record<string, string>;
-  editorConfig: [];
+  editorConfig: any;
   loadingFormConfigs: boolean;
   isEditorInitialized: boolean;
 };
@@ -157,9 +157,25 @@ class QueryEditor extends React.Component<Props> {
 const mapStateToProps = (state: AppState, props: any): ReduxStateProps => {
   const { runErrorMessage } = state.ui.queryPane;
   const { plugins } = state.entities;
+
   const { editorConfigs, loadingFormConfigs } = plugins;
   const formData = getFormValues(QUERY_EDITOR_FORM_NAME)(state) as QueryAction;
   const queryAction = getAction(state, props.match.params.queryId);
+
+  let editorConfig: any = [];
+  if (
+    queryAction &&
+    queryAction.pluginId &&
+    editorConfigs[queryAction.pluginId]
+  ) {
+    editorConfig = editorConfigs[queryAction.pluginId];
+  } else if (
+    queryAction &&
+    queryAction.datasource.pluginId &&
+    editorConfigs[queryAction.datasource.pluginId]
+  ) {
+    editorConfig = editorConfigs[queryAction.datasource.pluginId];
+  }
 
   return {
     pluginImages: getPluginImages(state),
@@ -170,9 +186,7 @@ const mapStateToProps = (state: AppState, props: any): ReduxStateProps => {
     responses: getActionResponses(state),
     queryPane: state.ui.queryPane,
     formData,
-    editorConfig: queryAction?.pluginId
-      ? editorConfigs[queryAction.pluginId]
-      : [],
+    editorConfig,
     loadingFormConfigs,
     isCreating: state.ui.apiPane.isCreating,
     isEditorInitialized: getIsEditorInitialized(state),
