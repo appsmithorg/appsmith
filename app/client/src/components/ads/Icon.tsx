372fb3b33d91e426c7d67ac5e735c29e2db5caec
devrk96 <rohit.kumawat@primathon.in>
2020-08-26 14:34:58 +0530

    Editable text component (#362)
    * editable text component started
    
    * success and error callback implemented
    
    * feedback implemented and success or error state corrected
    
    * editable input ui fixed
    
    * PR comments resolved
    
    * isSaving state refactored from PR comment
    
    * icon import updated
    
    * Changing path
    
    * icon import fixed
    
    Co-authored-by: Satbir Singh <satbir121@gmail.com>

--- app/client/src/components/ads/Icon.tsx
+++ app/client/src/components/ads/Icon.tsx
@@ -3,6 +3,9 @@ import { ReactComponent as DeleteIcon } from "assets/icons/ads/delete.svg";
 import { ReactComponent as UserIcon } from "assets/icons/ads/user.svg";
 import { ReactComponent as GeneralIcon } from "assets/icons/ads/general.svg";
 import { ReactComponent as BillingIcon } from "assets/icons/ads/billing.svg";
+import { ReactComponent as EditIcon } from "assets/icons/ads/edit.svg";
+import { ReactComponent as ErrorIcon } from "assets/icons/ads/error.svg";
+import { ReactComponent as SuccessIcon } from "assets/icons/ads/success.svg";
 import { ReactComponent as SearchIcon } from "assets/icons/ads/search.svg";
 import { ReactComponent as CloseIcon } from "assets/icons/ads/close.svg";
 import styled from "styled-components";
@@ -15,6 +18,9 @@ export type IconName =
   | "user"
   | "general"
   | "billing"
+  | "edit"
+  | "error"
+  | "success"
   | "search"
   | "close"
   | undefined;
@@ -25,8 +31,10 @@ const IconWrapper = styled.div<IconProps>`
   }
   display: flex;
   svg {
-    width: ${props => sizeHandler(props)}px;
-    height: ${props => sizeHandler(props)}px;
+    width: ${props =>
+      props.size ? sizeHandler(props) : props.theme.spaces[9]}px;
+    height: ${props =>
+      props.size ? sizeHandler(props) : props.theme.spaces[9]}px;
     path {
       fill: ${props => props.theme.colors.blackShades[4]};
     }
@@ -70,6 +78,15 @@ const Icon = (props: IconProps) => {
       break;
     case "billing":
       returnIcon = <BillingIcon />;
+      break;
+    case "edit":
+      returnIcon = <EditIcon />;
+      break;
+    case "error":
+      returnIcon = <ErrorIcon />;
+      break;
+    case "success":
+      returnIcon = <SuccessIcon />;
       break;
     case "search":
       returnIcon = <SearchIcon />;
