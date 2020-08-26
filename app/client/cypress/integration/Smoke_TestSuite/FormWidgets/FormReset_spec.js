ccc49e7eb9ccd5ec4d31e88e0d4e32ba3aeb6741
Hetu Nandu <hetunandu@gmail.com>
2020-08-26 17:30:27 +0530

    Form Reset test

---
+++ app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FormReset_spec.js
@@ -0,0 +1,32 @@
+const dsl = require("../../../fixtures/formResetDsl.json");
+const widgetsPage = require("../../../locators/Widgets.json");
+
+describe("Form reset functionality", function() {
+  before(() => {
+    cy.addDsl(dsl);
+  });
+
+  it("Resets the form ", () => {
+    cy.get(".tr")
+      .eq(2)
+      .click()
+      .should("have.class", "selected-row");
+
+    cy.get(widgetsPage.inputWidget + " " + "input")
+      .invoke("attr", "value")
+      .should("contain", "lindsay.ferguson@reqres.in");
+
+    cy.get(widgetsPage.formButtonWidget)
+      .contains("Reset")
+      .click();
+
+    cy.get(".tr")
+      .eq(2)
+      .click()
+      .should("not.have.class", "selected-row");
+
+    cy.get(widgetsPage.inputWidget + " " + "input")
+      .invoke("attr", "value")
+      .should("not.contain", "lindsay.ferguson@reqres.in");
+  });
+});
