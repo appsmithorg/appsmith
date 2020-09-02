3ba02f0a7372353b110cfcdc993aaf00d5f5898b
Shrikant Sharat Kandula <shrikant@appsmith.com>
2020-09-02 22:19:47 +0530

    Move certificate generation into install script

--- deploy/install.sh
+++ deploy/install.sh
@@ -221,6 +221,84 @@ confirm() {
     [[ yY =~ $answer ]]
 }
 
+init_ssl_cert() {
+    local domain="$1"
+    echo "Creating certificate for '$domain'."
+
+    local rsa_key_size=4096
+    local data_path="./data/certbot"
+
+    if [[ -d "$data_path" ]]; then
+        if ! confirm n "Existing certificate data found at '$data_path'. Continue and replace existing certificate?"; then
+            return
+        fi
+    fi
+
+    mkdir -p "$data_path"/{conf,www}
+
+    if ! [[ -e "$data_path/conf/options-ssl-nginx.conf" && -e "$data_path/conf/ssl-dhparams.pem" ]]; then
+        echo "### Downloading recommended TLS parameters..."
+        curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+        curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+        echo
+    fi
+
+    echo "### Requesting Let's Encrypt certificate for '$domain'..."
+
+    local email
+    read -rp 'Enter email address to create SSL certificate: (Optional, but strongly recommended): ' email
+    if [[ -z $email ]]; then
+        local email_arg="--register-unsafely-without-email"
+    else
+        local email_arg="--email $email --no-eff-email"
+    fi
+
+    if confirm n 'Do you want to create certificate in staging mode (which is used for dev purposes and is not subject to rate limits)?'; then
+        local staging_arg="--staging"
+    else
+        local staging_arg=""
+    fi
+
+    echo "### Generating OpenSSL key for '$domain'..."
+    local live_path="/etc/letsencrypt/live/$domain"
+    certbot_cmd \
+        "sh -c \"mkdir -p '$live_path' && openssl req -x509 -nodes -newkey rsa:1024 -days 1 \
+            -keyout '$live_path/privkey.pem' \
+            -out '$live_path/fullchain.pem' \
+            -subj '/CN=localhost' \
+            \""
+    echo
+
+    echo "### Starting nginx..."
+    sudo docker-compose up --force-recreate --detach nginx
+    echo
+
+    echo "### Removing key now that validation is done for $domain..."
+    certbot_cmd \
+        "rm -Rfv /etc/letsencrypt/live/$domain /etc/letsencrypt/archive/$domain /etc/letsencrypt/renewal/$domain.conf"
+    echo
+
+    # The following command exits with a non-zero status code even if the certificate was generated, but some checks failed.
+    # So we explicitly ignore such failure with a `|| true` in the end, to avoid bash quitting on us because this looks like
+    # a failed command.
+    certbot_cmd "certbot certonly --webroot --webroot-path=/var/www/certbot \
+            $staging_arg \
+            $email_arg \
+            --domains $domain \
+            --rsa-key-size $rsa_key_size \
+            --agree-tos \
+            --force-renewal" \
+        || true
+    echo
+
+    echo "### Reloading nginx..."
+    sudo docker-compose exec nginx nginx -s reload
+}
+
+certbot_cmd() {
+    sudo docker-compose run --rm --entrypoint "$1" certbot
+}
+
 echo_contact_support() {
     echo "Please contact <support@appsmith.com> with your OS details and version${1:-.}"
 }
@@ -393,7 +471,6 @@ mkdir -p "$templates_dir"
     cd "$templates_dir"
     curl --remote-name-all --silent --show-error \
         https://raw.githubusercontent.com/appsmithorg/appsmith/release/deploy/template/docker-compose.yml.sh \
-        https://raw.githubusercontent.com/appsmithorg/appsmith/release/deploy/template/init-letsencrypt.sh \
         https://raw.githubusercontent.com/appsmithorg/appsmith/release/deploy/template/mongo-init.js.sh \
         https://raw.githubusercontent.com/appsmithorg/appsmith/release/deploy/template/docker.env.sh \
         https://raw.githubusercontent.com/appsmithorg/appsmith/release/deploy/template/nginx_app.conf.sh \
@@ -423,8 +500,7 @@ echo ""
 
 cd "$install_dir"
 if [[ -n $custom_domain ]]; then
-    echo "Running init-letsencrypt.sh..."
-    sudo bash "$templates_dir/init-letsencrypt.sh" "$custom_domain"
+    init_ssl_cert "$custom_domain"
 else
     echo "No domain found. Skipping generation of SSL certificate."
 fi
