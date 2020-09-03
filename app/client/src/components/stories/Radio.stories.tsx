c7f6a5c757f843e2f68066e1486cf95fc5c37d00
Rohit Kumawat <rohit.kumawat@primathon.in>
2020-09-03 11:24:01 +0530

    radio component height fixed in story

--- app/client/src/components/stories/Radio.stories.tsx
+++ app/client/src/components/stories/Radio.stories.tsx
@@ -18,42 +18,44 @@ export default {
 };
 
 export const Radio = () => (
-  <StoryWrapper style={{ height: "300px" }}>
-    <RadioComponent
-      defaultValue={select(
-        "defaultValue",
-        ["React", "Angular", "Vue"],
-        "React",
-      )}
-      align={select(
-        "Alignment",
-        ["horizontal", "vertical", "column", "row"],
-        "horizontal",
-      )}
-      disabled={boolean("Radio group disabled", false)}
-      columns={number("Column number", 2)}
-      rows={number("Row number", 2)}
-      onSelect={action("selected-radio-option")}
-      options={[
-        {
-          label: "React",
-          value: "React",
-          onSelect: action("first-radio-option"),
-          disabled: boolean("Option-1-disabled", false),
-        },
-        {
-          label: "Angular",
-          value: "Angular",
-          onSelect: action("second-radio-option"),
-          disabled: boolean("Option-2-disabled", false),
-        },
-        {
-          label: "Vue",
-          value: "Vue",
-          onSelect: action("third-radio-option"),
-          disabled: boolean("Option-3-disabled", false),
-        },
-      ]}
-    />
+  <StoryWrapper>
+    <div style={{ height: "133px" }}>
+      <RadioComponent
+        defaultValue={select(
+          "defaultValue",
+          ["React", "Angular", "Vue"],
+          "React",
+        )}
+        align={select(
+          "Alignment",
+          ["horizontal", "vertical", "column", "row"],
+          "horizontal",
+        )}
+        disabled={boolean("Radio group disabled", false)}
+        columns={number("Column number", 2)}
+        rows={number("Row number", 2)}
+        onSelect={action("selected-radio-option")}
+        options={[
+          {
+            label: "React",
+            value: "React",
+            onSelect: action("first-radio-option"),
+            disabled: boolean("Option-1-disabled", false),
+          },
+          {
+            label: "Angular",
+            value: "Angular",
+            onSelect: action("second-radio-option"),
+            disabled: boolean("Option-2-disabled", false),
+          },
+          {
+            label: "Vue",
+            value: "Vue",
+            onSelect: action("third-radio-option"),
+            disabled: boolean("Option-3-disabled", false),
+          },
+        ]}
+      />
+    </div>
   </StoryWrapper>
 );
