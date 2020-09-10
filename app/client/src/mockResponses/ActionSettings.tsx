09373df4b1ffed3d7ff8e394cce25c097f65059c
Akash N <akash@codemonk.in>
2020-09-10 12:57:54 +0530

    Revert removal of run query on page load

--- app/client/src/mockResponses/ActionSettings.tsx
+++ app/client/src/mockResponses/ActionSettings.tsx
@@ -3,12 +3,12 @@ export const queryActionSettingsConfig = [
     sectionName: "",
     id: 1,
     children: [
-      // {
-      //   label: "Run query on Page load",
-      //   configProperty: "executeOnLoad",
-      //   controlType: "SWITCH",
-      //   info: "Will refresh data everytime page is reloaded",
-      // },
+      {
+        label: "Run query on Page load",
+        configProperty: "executeOnLoad",
+        controlType: "SWITCH",
+        info: "Will refresh data everytime page is reloaded",
+      },
       {
         label: "Request confirmation before running query",
         configProperty: "confirmBeforeExecute",
