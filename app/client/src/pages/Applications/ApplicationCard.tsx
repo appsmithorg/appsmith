51da9dd618b67da7ddc7dff7f31d9e1b4af19df3
Satbir Singh <satbir121@gmail.com>
2020-09-16 00:19:44 +0530

    Fixing app card context dropdown

--- app/client/src/pages/Applications/ApplicationCard.tsx
+++ app/client/src/pages/Applications/ApplicationCard.tsx
@@ -191,8 +191,8 @@ const EditButton = styled(Button)`
 
 const ContextDropdownWrapper = styled.div`
   position: absolute;
-  top: 3px;
-  right: 7px;
+  top: -6px;
+  right: -3px;
 `;
 
 const StyledAppIcon = styled(AppIcon)`
@@ -205,7 +205,7 @@ const StyledAppIcon = styled(AppIcon)`
 `;
 
 export const ApplicationCard = (props: ApplicationCardProps) => {
-  const [showOverlay, setShowOverlay] = useState(false);
+  const [showOverlay, setShowOverlay] = useState(true);
   const themeDetails = useSelector(getThemeDetails);
   const initialsAndColorCode = getInitialsAndColorCode(
     props.application.name,
@@ -287,7 +287,7 @@ export const ApplicationCard = (props: ApplicationCardProps) => {
         }}
         onClosing={() => {
           setIsMenuOpen(false);
-          setShowOverlay(false);
+          setShowOverlay(true);
         }}
       >
         {hasEditPermission && (
@@ -369,7 +369,7 @@ export const ApplicationCard = (props: ApplicationCardProps) => {
       onMouseLeave={() => {
         // If the menu is not open, then setOverlay false
         // Set overlay false on outside click.
-        !isMenuOpen && setShowOverlay(false);
+        !isMenuOpen && setShowOverlay(true);
       }}
       hasReadPermission={hasReadPermission}
       className="t--application-card"
