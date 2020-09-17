86883ff6682adb6ac0b1962c3c374c6411da88dc
Shrikant Sharat Kandula <shrikant@appsmith.com>
2020-09-17 20:58:50 +0530

    Use a unique instance ID for pinging home

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Config.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Config.java
@@ -1,6 +1,7 @@
 package com.appsmith.server.domains;
 
 import com.appsmith.external.models.BaseDomain;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class Config extends BaseDomain {
     JSONObject config;
