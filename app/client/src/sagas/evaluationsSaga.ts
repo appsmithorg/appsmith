33ef6f807e4c5c7cd96096695fbed2e9ebd903c7
Hetu Nandu <hetunandu@gmail.com>
2020-09-18 16:17:43 +0530

    Change listener before eval

--- app/client/src/sagas/evaluationsSaga.ts
+++ app/client/src/sagas/evaluationsSaga.ts
@@ -1,7 +1,9 @@
-import { all, takeLatest, select, put } from "redux-saga/effects";
+import { all, call, select, put, takeLatest, take } from "redux-saga/effects";
 import { ReduxActionTypes } from "../constants/ReduxActionConstants";
 import { getUnevaluatedDataTree } from "../selectors/dataTreeSelectors";
 import { getEvaluatedDataTree } from "../utils/DynamicBindingUtils";
+import { DataTree } from "../entities/DataTree/dataTreeFactory";
+import isEqual from "fast-deep-equal/es6";
 
 function* evaluateTreeSaga() {
   const unEvalTree = yield select(getUnevaluatedDataTree(true));
@@ -12,8 +14,37 @@ function* evaluateTreeSaga() {
   });
 }
 
+let oldUnEvalTree: DataTree = {};
+function* evaluationChangeListenerSaga() {
+  yield call(evaluateTreeSaga);
+  while (true) {
+    yield take("*");
+    const unEvalTree = yield select(getUnevaluatedDataTree(false));
+    if (!isEqual(unEvalTree, oldUnEvalTree)) {
+      oldUnEvalTree = unEvalTree;
+      yield call(evaluateTreeSaga);
+    }
+    // ;
+    // const changes = Object.keys(unEvalTree).map(key => {
+    //   if (key && oldUnEvalTree && key in oldUnEvalTree) {
+    //     return { [key]: oldUnEvalTree[key] !== unEvalTree[key] };
+    //   } else {
+    //     return { [key]: true };
+    //   }
+    // });
+    // if (_.some(_.values(changes))) {
+    //   oldUnEvalTree = unEvalTree;
+    //   console.log({ changes });
+    //   yield call(evaluateTreeSaga);
+    // }
+  }
+}
+
 export default function* evaluationSagaListeners() {
   yield all([
-    // takeLatest(ReduxActionTypes.INITIALIZE_EDITOR_SUCCESS, evaluateTreeSaga),
+    takeLatest(
+      ReduxActionTypes.INITIALIZE_EDITOR_SUCCESS,
+      evaluationChangeListenerSaga,
+    ),
   ]);
 }
