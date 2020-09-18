7e20e9e237ae6e9c104b4874bf68d9259754ed4d
Hetu Nandu <hetunandu@gmail.com>
2020-09-18 09:38:35 +0530

    Creating store

--- app/client/src/sagas/evaluationsSaga.ts
+++ app/client/src/sagas/evaluationsSaga.ts
@@ -1,11 +1,10 @@
-import { all, call, select, put, takeLatest, take } from "redux-saga/effects";
-import { ReduxActionTypes } from "constants/ReduxActionConstants";
-import { getUnevaluatedDataTree } from "selectors/dataTreeSelectors";
-import { getEvaluatedDataTree } from "utils/DynamicBindingUtils";
-import jsonFn from "json-fn";
+import { all, takeLatest, select, put } from "redux-saga/effects";
+import { ReduxActionTypes } from "../constants/ReduxActionConstants";
+import { getUnevaluatedDataTree } from "../selectors/dataTreeSelectors";
+import { getEvaluatedDataTree } from "../utils/DynamicBindingUtils";
 
 function* evaluateTreeSaga() {
-  const unEvalTree = yield select(getUnevaluatedDataTree);
+  const unEvalTree = yield select(getUnevaluatedDataTree(true));
   const evalTree = getEvaluatedDataTree(unEvalTree);
   yield put({
     type: ReduxActionTypes.SET_EVALUATED_TREE,
@@ -13,30 +12,8 @@ function* evaluateTreeSaga() {
   });
 }
 
-function* evaluationChangeListenerSaga() {
-  yield call(evaluateTreeSaga);
-  let oldUnEvalTree = "";
-  while (true) {
-    yield take("*");
-    const unEvalTree = yield select(getUnevaluatedDataTree);
-    const unEvalString = jsonFn.stringify(unEvalTree);
-    if (unEvalString !== oldUnEvalTree) {
-      yield call(evaluateTreeSaga);
-      oldUnEvalTree = unEvalString;
-    }
-  }
-  // TODO(hetu) need an action to stop listening and evaluate (exit editor)
-}
-
 export default function* evaluationSagaListeners() {
   yield all([
-    takeLatest(
-      ReduxActionTypes.INITIALIZE_EDITOR_SUCCESS,
-      evaluationChangeListenerSaga,
-    ),
-    takeLatest(
-      ReduxActionTypes.INITIALIZE_PAGE_VIEWER_SUCCESS,
-      evaluationChangeListenerSaga,
-    ),
+    // takeLatest(ReduxActionTypes.INITIALIZE_EDITOR_SUCCESS, evaluateTreeSaga),
   ]);
 }
