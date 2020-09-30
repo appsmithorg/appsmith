8091b70c9efbe44f29b521df314d0d97bed1383a
Trisha Anand <trisha@appsmith.com>
2020-09-30 13:07:04 +0530

    Solidified the new action structure.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/NewAction.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/NewAction.java
@@ -1,7 +1,10 @@
 package com.appsmith.server.domains;
 
+import com.appsmith.server.dtos.ActionDTO;
+
 public class NewAction {
 
+    // Fields in action that are not allowed to change between published and unpublished versions
     String applicationId;
 
     String organizationId;
@@ -9,5 +12,10 @@ public class NewAction {
     PluginType pluginType;
 
     String pluginId;
+
+    // Action specific fields that are allowed to change between published and unpublished versions
+    ActionDTO unpublishedAction;
+
+    ActionDTO publishedAction;
 
 }
