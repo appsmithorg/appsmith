ce3e39453c04bec80df2e7b7910a9caecfc3bf3d
jack1142 <6032823+jack1142@users.noreply.github.com>
2020-10-03 09:05:49 +0200

    Update deploy install script to use api64.ipify.org which supports IPv4 (#887)
    Co-authored-by: Nikhil Nandagopal <nikhil.nandagopal@gmail.com>

--- deploy/install.sh
+++ deploy/install.sh
@@ -334,7 +334,7 @@ desired_os=0
 os=""
 echo -e "\U1F575  Detecting your OS"
 check_os
-APPSMITH_INSTALLATION_ID=$(curl -s 'https://api.ipify.org')
+APPSMITH_INSTALLATION_ID=$(curl -s 'https://api64.ipify.org')
 
 # Run bye if failure happens
 trap bye EXIT
