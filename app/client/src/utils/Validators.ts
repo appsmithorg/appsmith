6b49d4c4e4c0d85f80648c14a7b6fc13bbde64d4
Sanchit Jain <171220040@nitdelhi.ac.in>
2020-10-05 10:48:33 +0530

    Options Validaor checks for empty values
    Fixes: #662

--- app/client/src/utils/Validators.ts
+++ app/client/src/utils/Validators.ts
@@ -365,7 +365,12 @@ export const VALIDATORS: Record<ValidationType, Validator> = {
     }
     const hasOptions = _.every(parsed, (datum: { label: any; value: any }) => {
       if (_.isObject(datum)) {
-        return _.isString(datum.label) && _.isString(datum.value);
+        return (
+            _.isString(datum.label) 
+            && _.isString(datum.value) 
+            && !_.isEmpty(datum.label) 
+            && !_.isEmpty(datum.value)
+        );
       } else {
         return false;
       }
