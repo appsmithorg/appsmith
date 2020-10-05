d77fc52d0345fa6ed79e62d9d3d632007069ef47
Ajay Kumar <visibleajay@gmail.com>
2020-10-05 22:19:24 +0530

    Sample Commit to run the CI.

--- app/client/src/widgets/CustomWidget.test.ts
+++ app/client/src/widgets/CustomWidget.test.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
 describe("<CustomWidget />", () => {
-  it("Should work as expected", () => {
+  it("Should work as expected ", () => {
     expect(true).toBeTruthy();
   });
 });
