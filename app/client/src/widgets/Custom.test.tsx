3506fc5963ac76fb3c2cacd1a98bbf68d146b6b2
Ajay Kumar <visibleajay@gmail.com>
2020-10-05 22:37:32 +0530

    Remove sample test files.

--- app/client/src/widgets/Custom.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-describe("<Custom />", () => {
-  it("Should work as expected", () => {
-    expect(true).toBeTruthy();
-  });
-});
--- app/client/src/widgets/CustomWidget.test.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-describe("<CustomWidget />", () => {
-  it("Should work as expected ", () => {
-    expect(true).toBeTruthy();
-  });
-});
