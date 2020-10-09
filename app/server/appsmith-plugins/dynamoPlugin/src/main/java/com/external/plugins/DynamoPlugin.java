aac73fa09a65ea1642b398679abd04d0a4677678
Shrikant Sharat Kandula <shrikant@appsmith.com>
2020-10-09 21:52:53 +0530

    Use reflection to build request objects for AWS SDK

--- app/server/appsmith-plugins/dynamoPlugin/src/main/java/com/external/plugins/DynamoPlugin.java
+++ app/server/appsmith-plugins/dynamoPlugin/src/main/java/com/external/plugins/DynamoPlugin.java
@@ -9,6 +9,8 @@ import com.appsmith.external.pluginExceptions.AppsmithPluginError;
 import com.appsmith.external.pluginExceptions.AppsmithPluginException;
 import com.appsmith.external.plugins.BasePlugin;
 import com.appsmith.external.plugins.PluginExecutor;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
@@ -18,20 +20,32 @@ import org.pf4j.PluginWrapper;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.awscore.client.handler.AwsSyncClientHandler;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.core.client.config.SdkClientConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ListTablesRequest;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
 public class DynamoPlugin extends BasePlugin {
+
+    private static final ObjectMapper objectMapper1 = new ObjectMapper();
+
+    static {
+        objectMapper1.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES);
+    }
 
     public DynamoPlugin(PluginWrapper wrapper) {
         super(wrapper);
@@ -55,7 +69,7 @@ public class DynamoPlugin extends BasePlugin {
             final Command command;
 
             try {
-                command = objectMapper.readValue(actionConfiguration.getBody(), Command.class);
+                command = objectMapper1.readValue(actionConfiguration.getBody(), Command.class);
             } catch (MismatchedInputException e) {
                 return Mono.error(new AppsmithPluginException(AppsmithPluginError.PLUGIN_ERROR,
                         "Mismatched input types. Need `action` string and `parameters` object."));
@@ -67,12 +81,20 @@ public class DynamoPlugin extends BasePlugin {
             final String action = command.getAction();
             final Map<String, Object> parameters = command.getParameters();
 
-            final Map<String, Object> correctedCaseParameters = new HashMap<>();
-            for (final Map.Entry<String, Object> entry : parameters.entrySet()) {
-                correctedCaseParameters.put(
-                        entry.getKey().substring(0, 1).toLowerCase() + entry.getKey().substring(1),
-                        entry.getValue()
-                );
+            new AwsSyncClientHandler(SdkClientConfiguration.builder().build());
+
+            try {
+                if ("ListTables".equals(action)) {
+                    ListTablesRequest request = convertValue(parameters, ListTablesRequest.class);
+                    result.setBody(ddb.listTables(request));
+                } else if ("PutItem".equals(action)) {
+                    PutItemRequest request = convertValue(parameters, PutItemRequest.class);
+                    result.setBody(ddb.putItem(request));
+                } else {
+                    result.setBody("Unknown action " + action);
+                }
+            } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | InstantiationException e) {
+                e.printStackTrace();
             }
 
             // Get the class implementing this DynamoDB action.
@@ -94,20 +116,20 @@ public class DynamoPlugin extends BasePlugin {
                 return Mono.error(new AppsmithPluginException(AppsmithPluginError.PLUGIN_ERROR,
                         "Missing execute method on action class for action `" + action + "`."));
             }
-
+/*
             try {
                 result.setBody(executeMethod.get().invoke(
                         null,
                         ddb,
-                        objectMapper.convertValue(
-                                correctedCaseParameters,
+                        objectMapper1.convertValue(
+                                parameters,
                                 Class.forName(actionClass.getName() + "$Parameters")
                         )
                 ));
             } catch (IllegalAccessException | InvocationTargetException | ClassNotFoundException e) {
                 return Mono.error(e);
             }
-
+*/
             log.debug("In the DynamoPlugin, got action execution result: " + result.toString());
             result.setIsExecutionSuccess(true);
             return Mono.just(result);
@@ -144,17 +166,118 @@ public class DynamoPlugin extends BasePlugin {
         @Override
         public Mono<DatasourceTestResult> testDatasource(DatasourceConfiguration datasourceConfiguration) {
             return datasourceCreate(datasourceConfiguration)
-                .map(client -> {
-                    client.close();
-                    return true;
-                })
-                .defaultIfEmpty(false)
-                .map(isValid -> BooleanUtils.isTrue(isValid)
-                        ? new DatasourceTestResult()
-                        : new DatasourceTestResult("Unable to create DynamoDB Client.")
-                );
+                    .map(client -> {
+                        client.close();
+                        return true;
+                    })
+                    .defaultIfEmpty(false)
+                    .map(isValid -> BooleanUtils.isTrue(isValid)
+                            ? new DatasourceTestResult()
+                            : new DatasourceTestResult("Unable to create DynamoDB Client.")
+                    );
         }
 
+    }
+
+    private static <T> T convertValue(Map<String, Object> mapping, Class<T> type)
+            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException, InstantiationException {
+        final Class<?> builderType;
+        try {
+            builderType = Class.forName(type.getName() + "$Builder");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        final Object builder = type.getMethod("builder").invoke(null);
+
+        for (final Map.Entry<String, Object> entry : mapping.entrySet()) {
+            final String key = entry.getKey();
+            Object value = entry.getValue();
+
+            String setterName1 = key;
+
+            if ("NULL".equals(setterName1)) {
+                // Since `null` is a reserved word in Java, AWS_SDK uses `nul` for this field.
+                setterName1 = "nul";
+            } else if (isUpperCase(setterName1)) {
+                setterName1 = setterName1.toLowerCase();
+            } else {
+                setterName1 = setterName1.substring(0, 1).toLowerCase() + setterName1.substring(1);
+            }
+            final String setterName = setterName1;
+
+            if (value instanceof String) {
+                final Method setterMethod = Arrays.stream(builderType.getMethods()).filter(m -> m.getName().equals(setterName))
+                        .findFirst()
+                        .orElse(null);
+                if (SdkBytes.class.isAssignableFrom(setterMethod.getParameterTypes()[0])) {
+                    value = SdkBytes.fromUtf8String((String) value);
+                }
+                setterMethod.invoke(builder, value);
+
+            } else if (value instanceof Boolean) {
+                builderType.getMethod(setterName, Boolean.class).invoke(builder, value);
+
+            } else if (value instanceof Integer) {
+                builderType.getMethod(setterName, Integer.class).invoke(builder, value);
+
+            } else if (value instanceof Map) {
+                Map<String, Object> valueAsMap = (Map) value;
+                final Method setterMethod = Arrays.stream(builderType.getMethods())
+                        .filter(m -> m.getName().equals(setterName))
+                        .findFirst()
+                        .orElse(null);
+                final ParameterizedType valueType = (ParameterizedType) setterMethod.getGenericParameterTypes()[0];
+                for (final String innerKey : valueAsMap.keySet()) {
+                    final Object innerValue = valueAsMap.get(innerKey);
+                    if (innerValue instanceof Map) {
+                        valueAsMap.put(innerKey, convertValue((Map) innerValue, (Class<?>) valueType.getActualTypeArguments()[1]));
+                    }
+                }
+                if (!Map.class.isAssignableFrom((Class<?>) valueType.getRawType())) {
+                    value = convertValue((Map) value, (Class<T>) valueType.getRawType());
+                }
+                setterMethod.invoke(builder, value);
+
+            } else if (value instanceof Collection) {
+                final Collection valueAsCollection = (Collection) value;
+                final Method setterMethod = Arrays.stream(builderType.getMethods())
+                        // Find method by name and exclude the varargs version of the method.
+                        .filter(m -> m.getName().equals(setterName) && !m.getParameterTypes()[0].getName().startsWith("[L"))
+                        .findFirst()
+                        .orElse(null);
+                final ParameterizedType valueType = (ParameterizedType) setterMethod.getGenericParameterTypes()[0];
+                final Collection reTypedList = valueAsCollection.getClass().getConstructor().newInstance();
+                for (final Object innerValue : valueAsCollection) {
+                    if (innerValue instanceof Map) {
+                        reTypedList.add(convertValue((Map) innerValue, (Class<?>) valueType.getActualTypeArguments()[0]));
+                    } else if (innerValue instanceof String && SdkBytes.class.isAssignableFrom((Class<?>) valueType.getActualTypeArguments()[0])) {
+                        reTypedList.add(SdkBytes.fromUtf8String((String) innerValue));
+                    } else {
+                        reTypedList.add(innerValue);
+                    }
+                }
+                setterMethod.invoke(builder, reTypedList);
+
+            } else {
+                System.out.println("Unknown value type while deserializing: " + value.getClass().getName());
+
+            }
+        }
+
+        final T out = (T) builderType.getMethod("build").invoke(builder);
+        System.out.println("out " + out);
+        return out;
+    }
+
+    private static boolean isUpperCase(String s) {
+        for (char c : s.toCharArray()) {
+            if (!Character.isUpperCase(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
