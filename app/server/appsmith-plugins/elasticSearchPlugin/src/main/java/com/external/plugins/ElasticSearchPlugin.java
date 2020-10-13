792789de3b1d4c5afe6eb640763d69023b3b4d7d
Shrikant Sharat Kandula <shrikant@appsmith.com>
2020-10-13 22:42:42 +0530

    Get plugin to a base working state

--- app/server/appsmith-plugins/elasticSearchPlugin/src/main/java/com/external/plugins/ElasticSearchPlugin.java
+++ app/server/appsmith-plugins/elasticSearchPlugin/src/main/java/com/external/plugins/ElasticSearchPlugin.java
@@ -4,13 +4,23 @@ import com.appsmith.external.models.ActionConfiguration;
 import com.appsmith.external.models.ActionExecutionResult;
 import com.appsmith.external.models.DatasourceConfiguration;
 import com.appsmith.external.models.DatasourceTestResult;
+import com.appsmith.external.models.Endpoint;
 import com.appsmith.external.plugins.BasePlugin;
 import com.appsmith.external.plugins.PluginExecutor;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.RestClient;
 import org.pf4j.Extension;
 import org.pf4j.PluginWrapper;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 public class ElasticSearchPlugin extends BasePlugin {
@@ -21,26 +31,77 @@ public class ElasticSearchPlugin extends BasePlugin {
 
     @Slf4j
     @Extension
-    public static class ElasticSearchPluginExecutor implements PluginExecutor {
+    public static class ElasticSearchPluginExecutor implements PluginExecutor<RestClient> {
 
         @Override
-        public Mono<ActionExecutionResult> execute(Object connection, DatasourceConfiguration datasourceConfiguration, ActionConfiguration actionConfiguration) {
-            return null;
+        public Mono<ActionExecutionResult> execute(RestClient client,
+                                                   DatasourceConfiguration datasourceConfiguration,
+                                                   ActionConfiguration actionConfiguration) {
+            ActionExecutionResult result = new ActionExecutionResult();
+
+            final String body = actionConfiguration.getBody();
+            final HashMap<String, Object> configBody;
+            try {
+                configBody = objectMapper.readValue(body, HashMap.class);
+            } catch (IOException e) {
+                return Mono.error(e);
+            }
+
+            Request request = new Request(
+                    configBody.get("method").toString().toUpperCase(),
+                    configBody.get("path").toString()
+            );
+
+            if (configBody.get("body") != null) {
+                try {
+                    request.setJsonEntity(objectMapper.writeValueAsString(configBody.get("body")));
+                } catch (JsonProcessingException e) {
+                    return Mono.error(e);
+                }
+            }
+
+            final Response response;
+            try {
+                response = client.performRequest(request);
+            } catch (IOException e) {
+                return Mono.error(e);
+            }
+
+            final String responseBody;
+            try {
+                responseBody = new String(response.getEntity().getContent().readAllBytes());
+            } catch (IOException e) {
+                return Mono.error(e);
+            }
+
+            try {
+                result.setBody(objectMapper.readValue(responseBody, HashMap.class));
+            } catch (IOException e) {
+                return Mono.error(e);
+            }
+
+            result.setIsExecutionSuccess(true);
+            return Mono.just(result);
         }
 
         @Override
-        public Mono<Object> datasourceCreate(DatasourceConfiguration datasourceConfiguration) {
-            return null;
+        public Mono<RestClient> datasourceCreate(DatasourceConfiguration datasourceConfiguration) {
+            final List<HttpHost> hosts = new ArrayList<>();
+
+            for (Endpoint endpoint : datasourceConfiguration.getEndpoints()) {
+                hosts.add(new HttpHost(endpoint.getHost(), endpoint.getPort().intValue(), "http"));
+            }
+
+            return Mono.just(RestClient.builder(hosts.toArray(new HttpHost[]{})).build());
         }
 
         @Override
-        public void datasourceDestroy(Object connection) {
-
-        }
-
-        @Override
-        public boolean isDatasourceValid(DatasourceConfiguration datasourceConfiguration) {
-            return false;
+        public void datasourceDestroy(RestClient client) {
+            try {
+                client.close();
+            } catch (IOException e) {
+                log.warn("Error closing connection to ElasticSearch.", e);
+            }
         }
 
         @Override
--- app/server/appsmith-plugins/elasticSearchPlugin/src/test/java/com/external/plugins/ElasticSearchPluginTest.java
+++ app/server/appsmith-plugins/elasticSearchPlugin/src/test/java/com/external/plugins/ElasticSearchPluginTest.java
@@ -1,44 +1,82 @@
 package com.external.plugins;
 
+import com.appsmith.external.models.ActionConfiguration;
+import com.appsmith.external.models.ActionExecutionResult;
+import com.appsmith.external.models.DatasourceConfiguration;
+import com.appsmith.external.models.Endpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.Request;
-import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 @Slf4j
 public class ElasticSearchPluginTest {
     ElasticSearchPlugin.ElasticSearchPluginExecutor pluginExecutor = new ElasticSearchPlugin.ElasticSearchPluginExecutor();
 
     @ClassRule
-    public static final ElasticsearchContainer container = new ElasticsearchContainer()
-            .withEnv("discovery.type", "single-node");
+    public static final ElasticsearchContainer container =
+            new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:6.4.1")
+                    .withEnv("discovery.type", "single-node");
+
+    private static final DatasourceConfiguration dsConfig = new DatasourceConfiguration();
 
     @BeforeClass
     public static void setUp() throws IOException {
+        final Integer port = container.getMappedPort(9200);
+
         final RestClient client = RestClient.builder(
-                new HttpHost("localhost", container.getMappedPort(9200), "http")
+                new HttpHost("localhost", port, "http")
         ).build();
 
-        final Request request = new Request("PUT", "/target1/_create/id1");
+        Request request;
+
+        request = new Request("PUT", "/planets/doc/id1");
         request.setJsonEntity("{\"name\": \"Mercury\"}");
-        final Response response = client.performRequest(request);
-        log.info("Response: {}", response.toString());
+        client.performRequest(request);
+
+        request = new Request("PUT", "/planets/doc/id2");
+        request.setJsonEntity("{\"name\": \"Venus\"}");
+        client.performRequest(request);
 
         client.close();
+
+        dsConfig.setEndpoints(List.of(new Endpoint("localhost", port.longValue())));
+    }
+
+    private Mono<ActionExecutionResult> execute(String body) {
+        final ActionConfiguration actionConfiguration = new ActionConfiguration();
+        actionConfiguration.setBody(body);
+
+        return pluginExecutor
+                .datasourceCreate(dsConfig)
+                .flatMap(conn -> pluginExecutor.execute(conn, dsConfig, actionConfiguration));
     }
 
     @Test
     public void testValidJsonApiExecution() {
-        container.start();
-        System.out.println("-=-=--=-=-=-=-=-=-=-=-=-=-=----=-=-=-=-=-=-=-=-=--=-=");
-        System.out.println(container.getHttpHostAddress());
-        System.out.println("-=-=--=-=-=-=-=-=-=-=-=-=-=----=-=-=-=-=-=-=-=-=--=-=");
+        StepVerifier.create(execute("{\"method\": \"GET\", \"path\": \"/planets/doc/id1\"}"))
+                .assertNext(result -> {
+                    assertNotNull(result);
+                    assertTrue(result.getIsExecutionSuccess());
+                    assertNotNull(result.getBody());
+                    final Map<String, Object> resultBody = (Map) result.getBody();
+                    assertEquals("Mercury", ((Map<String, String>) resultBody.get("_source")).get("name"));
+                })
+                .verifyComplete();
     }
+
 }
