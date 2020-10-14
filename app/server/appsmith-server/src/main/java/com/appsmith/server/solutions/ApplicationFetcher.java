eee588e87af6fec901b55d3cab8341738d5748d6
Shinn Lok <shinn.lok@liferay.com>
2020-10-14 15:06:36 -0700

    Adjust test

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ApplicationFetcher.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ApplicationFetcher.java
@@ -13,7 +13,6 @@ import com.appsmith.server.services.SessionUserServiceImpl;
 import com.appsmith.server.services.UserService;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
