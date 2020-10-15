eae7813b4ecaed8518b3059f40181d728caeca84
Arpit Mohan <arpit@appsmith.com>
2020-10-15 22:59:52 +0530

    Fixing the nginx-root.conf in the Docker container

--- app/client/docker/start-nginx.sh
+++ app/client/docker/start-nginx.sh
@@ -3,4 +3,5 @@
 # Refer: /app/client/Dockerfile
 set -ue
 cat /nginx.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z0-9_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' > /etc/nginx/conf.d/default.conf
+cat /nginx-root.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z0-9_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' > /etc/nginx/nginx.conf
 exec nginx -g 'daemon off;'
