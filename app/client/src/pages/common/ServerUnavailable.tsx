463225887ce0d1df8faf20692d6f497d4d9b65c5
Jeff Rescignano <jeff@jeffresc.dev>
2020-10-22 20:23:31 -0400

    Import Button and APPLICATIONS_URL

--- app/client/src/pages/common/ServerUnavailable.tsx
+++ app/client/src/pages/common/ServerUnavailable.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import Button from "components/editorComponents/Button";
 import PageUnavailableImage from "assets/images/404-image.png";
+import { APPLICATIONS_URL } from "constants/routes";
 
 const Wrapper = styled.div`
   text-align: center;
