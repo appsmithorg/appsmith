eb6c8ee8f0519c81367bb7b703c9bbba9c38c2ab
NandanAnantharamu <67676905+NandanAnantharamu@users.noreply.github.com>
2020-11-02 20:06:16 +0530

    Added tests for button click navigation (#1467)

---
+++ app/client/cypress/integration/Smoke_TestSuite/Binding/ButtonWidgets_NavigateTo_validation_spec.js
@@ -0,0 +1,43 @@
+const commonlocators = require("../../../locators/commonlocators.json");
+const formWidgetsPage = require("../../../locators/FormWidgets.json");
+const dsl = require("../../../fixtures/buttondsl.json");
+const pages = require("../../../locators/Pages.json");
+const widgetsPage = require("../../../locators/Widgets.json");
+const publish = require("../../../locators/publishWidgetspage.json");
+const testdata = require("../../../fixtures/testdata.json");
+const dsl2 = require("../../../fixtures/displayWidgetDsl.json");
+const explorer = require("../../../locators/explorerlocators.json");
+
+describe("Binding the button Widgets and validating NavigateTo Page functionality", function() {
+  before(() => {
+    cy.addDsl(dsl);
+  });
+
+  it("Button widget with action navigate to page", function() {
+    cy.openPropertyPane("buttonwidget");
+    cy.get(widgetsPage.actionSelect).click();
+    cy.get(commonlocators.chooseAction)
+      .children()
+      .contains("Navigate To")
+      .click();
+    cy.enterActionValue(testdata.externalPage);
+    cy.get(commonlocators.editPropCrossButton).click();
+    cy.wait(300);
+  });
+
+  it("Button click should take the control to page link validation", function() {
+    cy.PublishtheApp();
+    cy.get(publish.buttonWidget).click();
+    cy.wait(500);
+    cy.get(publish.buttonWidget).should("not.be.visible");
+    cy.go("back");
+    cy.get(publish.backToEditor)
+      .first()
+      .click();
+    cy.wait("@getPage").should(
+      "have.nested.property",
+      "response.body.responseMeta.status",
+      200,
+    );
+  });
+});
