c68f29583d215727f8f612c21b8061998a56bd96
Hetu Nandu <hetunandu@gmail.com>
2020-11-05 15:16:17 +0530

    Add checks for bad alert types

--- app/client/src/components/editorComponents/ToastComponent.tsx
+++ app/client/src/components/editorComponents/ToastComponent.tsx
@@ -87,6 +87,12 @@ const Toaster = {
       console.error("Toast message needs to be a string");
       return;
     }
+    if (!(config.type && config.type in ToastType)) {
+      console.error(
+        "Toast type needs to be a one of " + Object.keys(ToastType).join(", "),
+      );
+      return;
+    }
     toast(
       <ToastComponent
         {...config}
--- app/client/src/sagas/ActionExecutionSagas.ts
+++ app/client/src/sagas/ActionExecutionSagas.ts
@@ -64,7 +64,7 @@ import {
 import { AppState } from "reducers";
 import { mapToPropList } from "utils/AppsmithUtils";
 import { validateResponse } from "sagas/ErrorSagas";
-import { ToastType } from "react-toastify";
+import { ToastType, TypeOptions } from "react-toastify";
 import { PLUGIN_TYPE_API } from "constants/ApiEditorConstants";
 import { DEFAULT_EXECUTE_ACTION_TIMEOUT_MS } from "constants/ApiConstants";
 import { updateAppStore } from "actions/pageActions";
@@ -169,6 +169,29 @@ async function downloadSaga(
     });
     if (event.callback) event.callback({ success: false });
   }
+}
+
+function* showAlertSaga(
+  payload: { message: string; style: TypeOptions },
+  event: ExecuteActionPayloadEvent,
+) {
+  if (typeof payload.message !== "string") {
+    console.error("Toast message needs to be a string");
+    if (event.callback) event.callback({ success: false });
+    return;
+  }
+  if (!(payload.style && payload.style in ToastType)) {
+    console.error(
+      "Toast type needs to be a one of " + Object.keys(ToastType).join(", "),
+    );
+    if (event.callback) event.callback({ success: false });
+    return;
+  }
+  AppToaster.show({
+    message: payload.message,
+    type: payload.style,
+  });
+  if (event.callback) event.callback({ success: true });
 }
 
 export const getActionTimeout = (
@@ -439,11 +462,7 @@ function* executeActionTriggers(
         yield call(navigateActionSaga, trigger.payload, event);
         break;
       case "SHOW_ALERT":
-        AppToaster.show({
-          message: trigger.payload.message,
-          type: trigger.payload.style,
-        });
-        if (event.callback) event.callback({ success: true });
+        yield call(showAlertSaga, trigger.payload, event);
         break;
       case "SHOW_MODAL_BY_NAME":
         yield put(trigger);
