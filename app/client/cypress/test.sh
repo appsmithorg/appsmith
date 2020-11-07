7b985360d96cc75542ac840a82becbff257b2b3b
Hetu Nandu <hetunandu@gmail.com>
2020-11-08 00:26:11 +0530

    Change ci test command

--- app/client/cypress/test.sh
+++ app/client/cypress/test.sh
@@ -26,11 +26,7 @@ echo "Got the target: $target"
 if [ "$target" == "ci" ]; then
     # On the CI server run the tests in parallel
     # This requires the projectId and the record_key to be configured in your environment variables. By default this is defined on the CI server
-    echo "Got the Build ID: $BUILD_ID"
-    $(npm bin)/cypress run --headless --browser chrome \
-    --record --key "$CYPRESS_RECORD_KEY" --ci-build-id $BUILD_ID \
-    --parallel --group "Electrons on Gitlab CI" \
-    --spec "cypress/integration/Smoke_TestSuite/*/*"
+    $(npm bin)/cypress run
 else
     $(npm bin)/cypress run --headless --browser chrome --spec "cypress/integration/Smoke_TestSuite/*/*"
 fi
