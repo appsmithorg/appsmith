a90a62d65733e1b3d296b95eb0cf5a34423ed2c7
nandan.anantharamu <nandan.anantharamu@thoughtspot.com>
2020-11-17 12:56:31 +0530

    Updated test

--- app/client/cypress/integration/Smoke_TestSuite/OrganisationTests/CreateDuplicateAppWithinOrg_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/OrganisationTests/CreateDuplicateAppWithinOrg_spec.js
@@ -14,24 +14,20 @@ describe("Create new org and an app within the same", function() {
       localStorage.setItem("OrgName", orgid);
       cy.createOrg(orgid);
       cy.CreateAppForOrg(orgid, appid);
+      cy.NavigateToHome();
+      cy.get(homePage.searchInput).type(appid);
+      cy.CreateAppForOrg(orgid, appid);
+      cy.wait(2000);
+      cy.get(homePage.appsContainer).contains(orgid);
+      cy.get(homePage.applicationCard)
+        .first()
+        .trigger("mouseover");
+      cy.get(homePage.appEditIcon).should("be.visible");
+      cy.get(homePage.applicationCard)
+        .last()
+        .trigger("mouseover");
+      cy.get(homePage.appEditIcon).should("be.visible");
     });
-    cy.LogOut();
-  });
-
-  it("login again and create app with same name in same org", function() {
-    cy.LogintoApp(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
-    cy.CreateAppForOrg(orgid, appid);
-    cy.get(homePage.searchInput).type(appid);
-    cy.wait(2000);
-    cy.get(homePage.appsContainer).contains(orgid);
-    cy.xpath(homePage.ShareBtn).should("not.exist");
-    cy.get(homePage.applicationCard)
-      .first()
-      .trigger("mouseover");
-    cy.get(homePage.applicationCard)
-      .last()
-      .trigger("mouseover");
-    cy.get(homePage.appEditIcon).should("not.exist");
     cy.LogOut();
   });
 });
