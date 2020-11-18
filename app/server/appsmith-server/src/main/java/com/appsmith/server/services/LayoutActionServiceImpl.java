962ec2844bcaf2a0bed434696b5da43232e3d6ec
Nidhi <nidhi.nair93@gmail.com>
2020-11-18 13:37:20 +0530

    Fixed check for incorect dynamic bindings

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/LayoutActionServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/LayoutActionServiceImpl.java
@@ -455,10 +455,12 @@ public class LayoutActionServiceImpl implements LayoutActionService {
                         }
                     }
                     if (parent == null) {
-                        throw new AppsmithException(AppsmithError.INVALID_DYNAMIC_BINDING_REFERENCE, nextKey);
+                        log.warn("Unable to find dynamically bound key {} for the widget with id {}", nextKey, dsl.get(FieldName.WIDGET_ID));
                     }
                 }
-                dynamicBindings.addAll(MustacheHelper.extractMustacheKeysFromFields(parent));
+                if(parent != null) {
+                    dynamicBindings.addAll(MustacheHelper.extractMustacheKeysFromFields(parent));
+                }
             }
         }
 
