f18e94ffce3220b80dd1d89941951d8a44f73617
Rohit Kumawat <rohit.kumawat@primathon.in>
2020-11-19 20:26:57 +0530

    Hover ui fixed on all sides of app card

--- app/client/src/pages/Applications/ApplicationCard.tsx
+++ app/client/src/pages/Applications/ApplicationCard.tsx
@@ -115,9 +115,7 @@ const Wrapper = styled(
   height: ${props => props.theme.card.minHeight}px;
   position: relative;
   background-color: ${props => props.backgroundColor};
-  margin: ${props => props.theme.spaces[4]}px
-    ${props => props.theme.spaces[4]}px ${props => props.theme.spaces[4]}px
-    ${props => props.theme.spaces[5] + 1}px;
+  margin: ${props => props.theme.spaces[5]}px;
   .overlay {
     display: block;
     position: absolute;
