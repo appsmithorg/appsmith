84457c2471bf955fe46d95e384324bf9cbc69214
Piyush <piyush@codeitout.com>
2020-11-24 09:49:28 +0530

    use dev server to proxy

--- app/client/craco.dev.config.js
+++ app/client/craco.dev.config.js
@@ -4,5 +4,19 @@ const common = require("./craco.common.config.js");
 module.exports = merge(common, {
     babel: {
         plugins: ["babel-plugin-styled-components"]
+    },
+    devServer:{
+        https: true,
+        proxy: [{
+            context: ["/api", "/login", "/oauth2"],
+            target: process.env.REACT_APP_APPSMITH_BACKEND_PROXY || "http://localhost:8080",
+            changeOrigin: true,
+            secure: false,
+        },
+        {
+            context: ["/f"],
+            target: "https://cdn.optimizely.com",
+            secure: false,
+        }]
     }
 });
