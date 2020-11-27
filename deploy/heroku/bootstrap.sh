7513d2846a514d06e6148caf815a58ba2c64d004
Minh Hieu <prj_legodevops@geekup.vn>
2020-11-27 18:28:42 +0700

    Update bootstrap.sh & README.MD to match master branch

--- deploy/heroku/bootstrap.sh
+++ deploy/heroku/bootstrap.sh
@@ -15,13 +15,11 @@ function get_maximum_heap(){
 
 function start_applcation(){
     nginx
-    echo "Maximum_heap : $maximum_heap"
     if [[ ! -z ${maximum_heap} ]]; then
         backend_start_command="java -Xmx${maximum_heap}m -Dserver.port=8080 -Djava.security.egd='file:/dev/./urandom' -jar server.jar"
     else
-        backend_start_command="java -XX:+UseContainerSupport -Dserver.port=8080 -Djava.security.egd='file:/dev/./urandom' -jar server.jar"
+        backend_start_command="java -Dserver.port=8080 -Djava.security.egd='file:/dev/./urandom' -jar server.jar"
     fi
-    echo "Backend command : $backend_start_command"
     eval $backend_start_command
 }
 
