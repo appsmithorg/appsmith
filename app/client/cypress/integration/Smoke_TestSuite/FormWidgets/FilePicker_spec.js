58d6e5cde4c64348b8f65f9acb377dac04042abe
Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>
2020-12-01 16:29:07 +0530

    add tests cases

--- app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
@@ -6,14 +6,14 @@ describe("FilePicker Widget Functionality", function() {
   beforeEach(() => {
     cy.addDsl(dsl);
   });
-  // it("FilePicker Widget Functionality", function() {
-  //   cy.openPropertyPane("filepickerwidget");
+  it("FilePicker Widget Functionality", function() {
+    cy.openPropertyPane("filepickerwidget");
 
-  //   //Checking the edit props for FilePicker and also the properties of FilePicker widget
+    //Checking the edit props for FilePicker and also the properties of FilePicker widget
 
-  //   cy.testCodeMirror("Upload Files");
-  //   cy.get(commonlocators.editPropCrossButton).click();
-  // });
+    cy.testCodeMirror("Upload Files");
+    cy.get(commonlocators.editPropCrossButton).click();
+  });
 
   it("It checks the loading state of filepicker on call the action", function() {
     cy.openPropertyPane("filepickerwidget");
@@ -25,10 +25,6 @@ describe("FilePicker Widget Functionality", function() {
       .attachFile(fixturePath);
     cy.get(commonlocators.filePickerUploadButton).click();
     cy.get(".bp3-spinner").should("have.length", 1);
-    //Checking the edit props for FilePicker and also the properties of FilePicker widget
-
-    // cy.testCodeMirror("Upload Files");
-    // cy.get(commonlocators.editPropCrossButton).click();
   });
 
   afterEach(() => {
