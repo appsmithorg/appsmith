8b0853e6844cb9bbf57cd0e3fb626f3f897972b0
prapullac <71753653+prapullac@users.noreply.github.com>
2020-12-03 12:08:26 +0530

    Added testcase for tablefilter

---
+++ app/client/cypress/mannual Testing/Table.js
@@ -0,0 +1,14 @@
+const dsl = require("../../../fixtures/tableWidgetDsl.json");
+
+
+describe("Filter ", function() {
+    it("Test table Filter", function() 
+    {
+        //Add a table
+    // click  on the column action item 
+    // Click on Select a datatype 
+    // Click on Filter option
+    // ensure to add filter 
+    }
+
+    
