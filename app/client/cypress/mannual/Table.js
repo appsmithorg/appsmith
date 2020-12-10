5120f67c04fb0f064b0f08d22e833fb89d48f11c
prapullac <71753653+prapullac@users.noreply.github.com>
2020-12-10 13:39:27 +0530

    Homepage mannaul test ideas

---
+++ app/client/cypress/mannual/Table.js
@@ -0,0 +1,15 @@
+const dsl = require("../../../fixtures/tableWidgetDsl.json");
+
+
+describe("Filter ", function()  {
+    it("Test table Filter", function() 
+    {
+        //Add a table
+    // click  on the column action item 
+    // Click on Select a datatype 
+    // Click on Filter option
+    // ensure to add filter 
+    }
+    )
+}
+)
