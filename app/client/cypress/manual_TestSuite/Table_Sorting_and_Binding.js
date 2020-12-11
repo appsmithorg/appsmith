b27a32940ebe9bce2aefe4f88be456eecbcd3277
prapullac <71753653+prapullac@users.noreply.github.com>
2020-12-11 17:25:54 +0530

    Manual Test Ideas  (#2129)

---
+++ app/client/cypress/manual_TestSuite/Table_Sorting_and_Binding.js
@@ -0,0 +1,20 @@
+const dsl = require("../../../fixtures/tableWidgetDsl.json");
+
+
+describe("Sorting and binding ", function() {
+    it("Test table Sorting and binding", function() 
+    {
+        //Add a table
+        //Click on the column and observe the sorting
+        // Click on the property pane
+        // Navigate to Default selected Row 
+        // Add an index
+        // Add an input widget
+        // Click to open property pane
+        // Navigate to Default text
+        // Add a binding to reflect any column 
+        // and observe that as we sort the input widget must get updated 
+    }
+    )
+}
+)
