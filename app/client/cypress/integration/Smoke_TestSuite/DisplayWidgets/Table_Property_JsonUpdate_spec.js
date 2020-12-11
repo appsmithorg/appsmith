74faa210193a5ad5a0acdb04e893f7728351dca3
nandan.anantharamu <nandan.anantharamu@thoughtspot.com>
2020-12-11 13:19:03 +0530

    Updated test

--- app/client/cypress/integration/Smoke_TestSuite/DisplayWidgets/Table_Property_JsonUpdate_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/DisplayWidgets/Table_Property_JsonUpdate_spec.js
@@ -1,36 +1,39 @@
-const widgetsPage = require("../../../locators/Widgets.json");
 const commonlocators = require("../../../locators/commonlocators.json");
-const publish = require("../../../locators/publishWidgetspage.json");
-const dsl = require("../../../fixtures/tableNewDsl.json");
+const dsl = require("../../../fixtures/tableTextPaginationDsl.json");
 const pages = require("../../../locators/Pages.json");
+const apiPage = require("../../../locators/ApiEditor.json");
+const publishPage = require("../../../locators/publishWidgetspage.json");
 const testdata = require("../../../fixtures/testdata.json");
 
-describe("Table Widget property pane feature validation", function() {
+
+describe("Test Create Api and Bind to Table widget", function() {
   before(() => {
     cy.addDsl(dsl);
   });
 
-  it("Add column in the property pane and validate in table widget", function() {
-    cy.openPropertyPane("tablewidget");
-    cy.tableColumnDataValidation("id");
-    cy.tableColumnDataValidation("email");
-    cy.tableColumnDataValidation("userName");
-    cy.tableColumnDataValidation("productName");
-    cy.tableColumnDataValidation("orderAmount");
-    cy.tableColumnPopertyUpdate("id", "TestUpdated");
+  it("Create an API and Execute the API and bind with Table", function() {
+    cy.createAndFillApi(this.data.paginationUrl, this.data.paginationParam);
+    cy.RunAPI();
+  });
+
+  it("Validate Table with API data and then add a column", function() {
+    cy.SearchEntityandOpen("Table1");
+    cy.testJsontext("tabledata", "{{Api1.data.users}}");
+    cy.CheckWidgetProperties(commonlocators.serverSidePaginationCheckbox);
+    cy.SearchEntityandOpen("Text1");
+    cy.testJsontext("text", "{{Table1.selectedRow.url}}");
+    cy.SearchEntityandOpen("Table1");
+    cy.readTabledata("0", "0").then(tabData => {
+      const tableData = tabData;
+      localStorage.setItem("tableDataPage1", tableData);
+    });
+    cy.ValidateTableData("1");
     cy.addColumn("CustomColumn");
-    cy.tableColumnDataValidation("DERIVED1"); //To be updated later
-    cy.hideColumn("email");
-    cy.hideColumn("userName");
-    cy.hideColumn("productName");
-    cy.hideColumn("orderAmount");
-    cy.get(".draggable-header:contains('CustomColumn')").should("be.visible");
-    cy.closePropertyPane();
+
   });
 
   it("Update table json data and check the column names updated", function() {
-    cy.openPropertyPane("tablewidget");
-    cy.widgetText("Table1", widgetsPage.tableWidget, commonlocators.tableInner);
+    cy.SearchEntityandOpen("Table1");
     cy.testJsontext("tabledata", JSON.stringify(this.data.TableInputUpdate));
     cy.wait("@updateLayout");
     cy.tableColumnDataValidation("id");
@@ -38,7 +41,12 @@ describe("Table Widget property pane feature validation", function() {
     cy.tableColumnDataValidation("userName");
     cy.tableColumnDataValidation("productName");
     cy.tableColumnDataValidation("orderAmount");
-    cy.tableColumnDataValidation("DERIVED1"); //To be updated later
-    cy.get(".draggable-header:contains('TestUpdated')").should("be.visible");
+    cy.tableColumnDataValidation("DERIVED1");
+    cy.hideColumn("email");
+    cy.hideColumn("userName");
+    cy.hideColumn("productName");
+    cy.hideColumn("orderAmount");
+    cy.get(".draggable-header:contains('CustomColumn')").should("be.visible");
+    cy.closePropertyPane();
   });
 });
