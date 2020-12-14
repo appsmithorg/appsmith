3b4d12b8dce02e6dcba93079744d397519b431c8
Hetu Nandu <hetunandu@gmail.com>
2020-12-14 23:03:48 +0530

    Test by just one case

--- app/client/cypress/integration/Smoke_TestSuite/ActionExecution/ExecutionParams_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ActionExecution/ExecutionParams_spec.js
@@ -5,17 +5,17 @@ describe("API Panel Test Functionality", function() {
   before(() => {
     cy.addDsl(dsl);
   });
-  it("Will pass static values in params", function() {
+  it("Will pass execution params", function() {
     cy.NavigateToAPI_Panel();
-    cy.CreateAPI("StaticParams");
+    cy.CreateAPI("MultiApi");
     cy.enterDatasourceAndPath(
       "https://jsonplaceholder.typicode.com/",
-      "{{this.params.endpoint}}",
+      "{{this.params.endpoint || 'posts'}}",
     );
     cy.WaitAutoSave();
 
     cy.SearchEntityandOpen("Table1");
-    cy.testJsontext("tabledata", "{{StaticParams.data");
+    cy.testJsontext("tabledata", "{{MultiApi.data");
 
     cy.SearchEntityandOpen("RunButton");
     cy.get(".t--property-control-onclick")
@@ -24,7 +24,7 @@ describe("API Panel Test Functionality", function() {
 
     cy.testJsontext(
       "onclick",
-      "{{StaticParams.run(undefined, undefined, { endpoint: 'users",
+      "{{MultiApi.run(undefined, undefined, { endpoint: 'users",
     );
     cy.PublishtheApp();
     cy.get(publishPage.buttonWidget).click();
@@ -32,25 +32,11 @@ describe("API Panel Test Functionality", function() {
       expect(cellData).to.be.equal("Ervin Howell");
     });
     cy.get(publishPage.backToEditor).click({ force: true });
-  });
 
-  it("Will pass dynamic values in params", function() {
-    cy.NavigateToAPI_Panel();
-    cy.CreateAPI("DynamicParams");
-    cy.enterDatasourceAndPath(
-      "https://jsonplaceholder.typicode.com/",
-      "{{this.params.endpoint || 'posts'}}",
-    );
-
-    cy.SearchEntityandOpen("RunButton");
     cy.testJsontext(
       "onclick",
-      "{{DynamicParams.run(undefined, undefined, { endpoint: EndpointInput.text",
+      "{{MultiApi.run(undefined, undefined, { endpoint: 'users",
     );
-
-    cy.PublishtheApp();
-
-    cy.get(publishPage.buttonWidget).click();
 
     cy.readTabledataPublish("0", "2").then(cellData => {
       expect(cellData).to.be.equal("delectus aut autem");
