2699a58e36cdf40f7f1a73707d5a9b7d1eb29a0b
Shrikant Sharat Kandula <shrikant@appsmith.com>
2020-12-18 10:55:43 +0530

    Carry build's exit code over to CI

--- app/client/build.sh
+++ app/client/build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-GIT_SHA=$(eval git rev-parse HEAD)
-echo $GIT_SHA
-echo "Sentry Auth Token: $SENTRY_AUTH_TOKEN"
+GIT_SHA="$(git rev-parse HEAD)"
+echo "GIT_SHA: '$GIT_SHA'"
+echo "Sentry Auth Token: '$SENTRY_AUTH_TOKEN'"
 
-REACT_APP_SENTRY_RELEASE=$GIT_SHA EXTEND_ESLINT=true craco --max-old-space-size=4096 build --config craco.build.config.js
+REACT_APP_SENTRY_RELEASE="$GIT_SHA" EXTEND_ESLINT=true \
+  npx craco --max-old-space-size=4096 build --config craco.build.config.js
+build_result=$?
 
 rm ./build/static/js/*.js.map
+
+echo 'Package size:'
+du -sh ./build
+
 echo "build finished"
+
+exit $build_result
