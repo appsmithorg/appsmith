99d29fa9f94aaeefce5008f8b0b9b054ad2b3063
Sumit Kumar <sumit@appsmith.com>
2020-12-22 16:59:18 +0530

    Revert "Redshift plugin  (#2112)" (#2314)
    This reverts commit 708ca7fedf3fb8dd876f2fb2e90ec50564401a60.

--- app/server/appsmith-plugins/redshiftPlugin/src/main/resources/templates/CREATE.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users
-  (name, gender, email)
-VALUES
-  (
-    '{{ nameInput.text }}',
-    '{{ genderDropdown.selectedOptionValue }}',
-    '{{ nameInput.text }}'
-  ); -- nameInput and genderDropdown are example widgets, replace them with your widget names. Read more at http://bit.ly/postgres-widget-docs 
--- app/server/appsmith-plugins/redshiftPlugin/src/main/resources/templates/SELECT.sql
+++
@@ -1,1 +0,0 @@
-SELECT * FROM users where role = 'Admin' ORDER BY id LIMIT 10; -- Use widget data in a query using {{ widgetName.property }}. Read more at http://bit.ly/postgres-widget-docs
