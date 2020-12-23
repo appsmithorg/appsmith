105955eb271ecb895af6be1d1eb7f3dec6fe9809
prapullac <71753653+prapullac@users.noreply.github.com>
2020-12-23 15:16:26 +0530

    Adding test case for API associated with API

---
+++ app/client/cypress/manual_TestSuite/API_Datasource_Spec.js
@@ -0,0 +1,37 @@
+const dsl = require("../../../fixtures/tableWidgetDsl.json");
+
+describe("API and cURL functionailty test", function() {
+    it("Execution of cRUL Command", function() 
+    {
+        // Create an application
+        // Click on the + icon of the API 
+        // Click on cURL option
+        // Add the cURL command
+        // Click on Import
+        // Ensure the API is correctly imported 
+        // Click on the "RUN"
+    }
+    )
+    it("Execution of API ", function() 
+    {
+        // Create an application
+        // Click on the + icon of the API 
+        // Click on API option
+        // Add an API 
+        // Add parameter
+        // Click on the "RUN"
+    }
+    )
+    it("Making an API to Datasource", function() 
+    {
+        // Create an application
+        // Click on the + icon of the API 
+        // Click on API option
+        // Add an API 
+        // Add parameter
+        //Click on the three dots  
+        //Click on the "RUN"
+    }
+    )
+}
+)
