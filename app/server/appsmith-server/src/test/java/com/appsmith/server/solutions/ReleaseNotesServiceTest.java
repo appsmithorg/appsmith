afcf6b17af728eecfb30a5af46990c6c271fc57f
Shrikant Sharat Kandula <shrikant@appsmith.com>
2020-12-24 15:52:35 +0530

    Fix missing runner for test class

--- app/server/appsmith-server/src/test/java/com/appsmith/server/solutions/ReleaseNotesServiceTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/solutions/ReleaseNotesServiceTest.java
@@ -1,12 +1,17 @@
 package com.appsmith.server.solutions;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ReleaseNotesServiceTest {
 
     @Autowired
