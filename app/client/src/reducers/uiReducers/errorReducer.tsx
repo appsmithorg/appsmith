8e9fc0b20a33b7005b250a943fe7873a067640bb
a <rishabh.robben@gmail.com>
2020-12-28 19:13:09 +0530

    trigger tests

--- app/client/src/reducers/uiReducers/errorReducer.tsx
+++ app/client/src/reducers/uiReducers/errorReducer.tsx
@@ -20,7 +20,7 @@ const errorReducer = createReducer(initialState, {
   ) => ({
     ...state,
     safeCrash: true,
-    safeCrashCode: _.get(action, "payload.code", 502),
+    safeCrashCode: _.get(action, "payload.code", 502), // when the server is not responding
   }),
   [ReduxActionTypes.REPORT_ERROR]: (
     state: ErrorReduxState,
