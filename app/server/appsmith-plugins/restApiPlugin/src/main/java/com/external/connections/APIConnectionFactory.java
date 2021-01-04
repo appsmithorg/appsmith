dbc53e4f57515a3af6805708729068a9bcc05a36
Nidhi <nidhi.nair93@gmail.com>
2021-01-04 21:15:27 +0530

    Changed logic for factory

--- app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/connections/APIConnectionFactory.java
+++ app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/connections/APIConnectionFactory.java
@@ -1,20 +1,16 @@
 package com.external.connections;
 
 import com.appsmith.external.models.AuthenticationDTO;
+import com.appsmith.external.models.OAuth2;
 
 
 public class APIConnectionFactory {
 
     public static APIConnection createConnection(AuthenticationDTO authenticationType) {
-        String connectionType = null;
-
-        if (authenticationType != null) {
-            connectionType = authenticationType.getClass().getSimpleName();
-        }
-
-        switch (connectionType) {
-            default:
-                return null;
+        if (authenticationType instanceof OAuth2) {
+            return OAuth2Connection.create((OAuth2) authenticationType);
+        } else {
+            return null;
         }
     }
 }
