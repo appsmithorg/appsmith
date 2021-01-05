e15c5dc85dc57527716fdcbb7542cc0404f8eae1
Sumit Kumar <sumit@appsmith.com>
2021-01-05 13:39:39 +0530

    add fix

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/MongoConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/MongoConfig.java
@@ -16,6 +16,8 @@ import org.springframework.data.convert.TypeInformationMapper;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
 import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.core.convert.MongoTypeMapper;
@@ -70,8 +72,10 @@ public class MongoConfig {
     }
 
     @Bean
-    public MappingMongoConverter mappingMongoConverter(DefaultTypeMapper typeMapper, MongoMappingContext context) {
-        MappingMongoConverter converter = new MappingMongoConverter(NoOpDbRefResolver.INSTANCE, context);
+    public MappingMongoConverter mappingMongoConverter(DefaultTypeMapper typeMapper, MongoMappingContext context,
+                                                       MongoDbFactory mongoDbFactory) {
+        DbRefResolver defaultDbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        MappingMongoConverter converter = new MappingMongoConverter(defaultDbRefResolver, context);
         converter.setTypeMapper((MongoTypeMapper) typeMapper);
         return converter;
     }
