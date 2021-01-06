563c32d203e2e7e0cd2f9b080984d95f5807f632
Pawan Kumar <pawan.stardust@gmail.com>
2021-01-06 16:54:16 +0530

    [Fix] - Authentication after 404 not redirecting back to the original page (#2423)
    * add redirect issue
    
    * add check for crash code
    
    * redirect anonymous user to login page
    
    * remove unused selector
    
    * move redirection logic to error saga level
    
    * update action type name
    
    * replace occurance of redirectTo to redirectUrl
    
    * remove unused selector in PagenoutFound + change saga name
    
    Co-authored-by: Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>

--- app/client/src/sagas/ErrorSagas.tsx
+++ app/client/src/sagas/ErrorSagas.tsx
@@ -1,19 +1,22 @@
-import _ from "lodash";
+import { get } from "lodash";
 import {
   ReduxActionTypes,
   ReduxActionErrorTypes,
   ReduxAction,
 } from "constants/ReduxActionConstants";
-import { DEFAULT_ERROR_MESSAGE, DEFAULT_ACTION_ERROR } from "constants/errors";
+import log from "loglevel";
+import history from "utils/history";
 import { ApiResponse } from "api/ApiResponses";
-import { put, takeLatest, call, select } from "redux-saga/effects";
-import { ERROR_401, ERROR_500, ERROR_0 } from "constants/messages";
 import { Variant } from "components/ads/common";
 import { Toaster } from "components/ads/Toast";
-import log from "loglevel";
 import { flushErrors } from "actions/errorActions";
-import history from "utils/history";
+import { AUTH_LOGIN_URL } from "constants/routes";
+import { ERROR_CODES } from "constants/ApiConstants";
 import { getSafeCrash } from "selectors/errorSelectors";
+import { getCurrentUser } from "selectors/usersSelectors";
+import { put, takeLatest, call, select } from "redux-saga/effects";
+import { ERROR_401, ERROR_500, ERROR_0 } from "constants/messages";
+import { DEFAULT_ERROR_MESSAGE, DEFAULT_ACTION_ERROR } from "constants/errors";
 
 export function* callAPI(apiCall: any, requestPayload: any) {
   try {
@@ -80,7 +83,7 @@ Object.keys(ReduxActionErrorTypes).forEach((type: string) => {
 ActionErrorDisplayMap = {
   ...ActionErrorDisplayMap,
   [ReduxActionErrorTypes.API_ERROR]: (error) =>
-    _.get(error, "message", DEFAULT_ERROR_MESSAGE),
+    get(error, "message", DEFAULT_ERROR_MESSAGE),
   [ReduxActionErrorTypes.FETCH_PAGE_ERROR]: () =>
     DEFAULT_ACTION_ERROR("fetching the page"),
   [ReduxActionErrorTypes.SAVE_PAGE_ERROR]: () =>
@@ -111,6 +114,7 @@ export function* errorSaga(
   if (error && error.crash) {
     effects.push(ErrorEffectTypes.SAFE_CRASH);
   }
+
   for (const effect of effects) {
     switch (effect) {
       case ErrorEffectTypes.LOG_ERROR: {
@@ -127,6 +131,7 @@ export function* errorSaga(
       }
     }
   }
+
   yield put({
     type: ReduxActionTypes.REPORT_ERROR,
     payload: {
@@ -152,6 +157,33 @@ function* crashAppSaga() {
 }
 
 /**
+ * this saga do some logic before actually setting safeCrash to true
+ */
+function* safeCrashSagaRequest(action: ReduxAction<{ code?: string }>) {
+  const user = yield select(getCurrentUser);
+  const code = get(action, "payload.code");
+
+  // if user is not logged and the error is "PAGE_NOT_FOUND",
+  // redirecting user to login page with redirecTo param
+  if (
+    get(user, "email") === "anonymousUser" &&
+    code === ERROR_CODES.PAGE_NOT_FOUND
+  ) {
+    window.location.href = `${AUTH_LOGIN_URL}?redirectUrl=${window.location.href}`;
+
+    return false;
+  }
+
+  // if there is no action to be done, just calling the safe crash action
+  yield put({
+    type: ReduxActionTypes.SAFE_CRASH_APPSMITH,
+    payload: {
+      code,
+    },
+  });
+}
+
+/**
  * flush errors and redirect users to a url
  *
  * @param action
@@ -173,5 +205,9 @@ export default function* errorSagas() {
   yield takeLatest(
     ReduxActionTypes.FLUSH_AND_REDIRECT,
     flushErrorsAndRedirectSaga,
+  );
+  yield takeLatest(
+    ReduxActionTypes.SAFE_CRASH_APPSMITH_REQUEST,
+    safeCrashSagaRequest,
   );
 }
