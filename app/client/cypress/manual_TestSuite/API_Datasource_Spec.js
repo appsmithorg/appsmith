28f50e9d1d755f89179ebd64c5172684266c003a
prapullac <71753653+prapullac@users.noreply.github.com>
2021-01-07 19:26:54 +0530

    Test Ideas for Invite flow and API Pane

--- app/client/cypress/manual_TestSuite/API_Datasource_Spec.js
+++ app/client/cypress/manual_TestSuite/API_Datasource_Spec.js
@@ -1,25 +1,6 @@
 const commonlocators = require("../../../locators/commonlocators.json");
 
 describe("API associated with Datasource", function() {
-    it("Edit name of the Datasource from Pane and refeclected in the Page ", function() 
-    {
-        // Click on the API datasource 
-        // Click on Action icon (Three Dots)
-        // Click on "Edit Name"
-        // Rename the Datasource
-        // Click on the datasource
-        // Ensure the name is updated on the Page 
-    }
-    )
-    it("Edit name of the Datasource from Page and refeclected in the Pane", function() 
-    {
-        // Click on the API datasource 
-        // Navigate to respective 
-        // Click on "Edit " option next to the Name of the datasource
-        // Rename the Datasource
-        // Ensure the name is updated in the Pane 
-    }
-    )
     it("Edit the API Datasource", function() 
     {
         // Click on the API datasource 
@@ -56,5 +37,14 @@ describe("API associated with Datasource", function() {
         // Ensure loading icon 
     }
     )
+    it("User must be displayed with error message when tried to run an empty API ", function() 
+    {
+        // Navigate to the API
+        // Click on "RUN"
+        // Ensure an Information j /Ún80jq3message is dispalyed in the Response Body
+
+    }
+    )
+    
 }
 )
---
+++ app/client/cypress/manual_TestSuite/Invite_flow_Spec.js
@@ -0,0 +1,32 @@
+const homePage = require("../../../locators/HomePage.json");
+
+
+describe("adding role without Email Id", function()  {
+    it("Empty Email ID Invite flow", function() 
+    {
+     // Navigate to Home Page
+     // Click on "Share" option
+     // Add Role from the dropdown
+     // Ensure the "Invite" option is "Inactive"
+    }
+    )
+    it("Error message must be dispalyed to user on inappropriate Email ID", function() 
+    {
+     // Navigate to Home Page
+     // Click on "Share" option
+     // Add inappropriate Email Id
+     // Select the "Role"
+     // Ensure the "Invite" option is "Inactive" and error message is displayed to user
+    }
+    )
+    it("Clicking on the organisation list the user must be lead to organisation Station ", function() 
+    {
+     // Navigate to Home Page
+     // Navigate to Organisation list
+     // Click on one of the organisation name
+     // Ensure user is directed to the organisation
+    }
+    )
+
+}
+)
