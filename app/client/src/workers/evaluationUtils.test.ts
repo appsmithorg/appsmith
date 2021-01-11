16cb6cce29f0ceeef0395181e645c5c616d4404b
Trisha Anand <trisha@appsmith.com>
2021-01-11 13:05:22 +0530

    Update app/client/src/workers/evaluationUtils.test.ts
    Added another test case scenario for isChildPropertyPath

--- app/client/src/workers/evaluationUtils.test.ts
+++ app/client/src/workers/evaluationUtils.test.ts
@@ -7,6 +7,7 @@ describe("isChildPropertyPath function", () => {
       ["Table1.selectedRow", "Table1.selectedRow.email", true],
       ["Table1.selectedRow", "1Table1.selectedRow", false],
       ["Table1.selectedRow", "Table11selectedRow", false],
+      ["Table1.selectedRow", "Table1.selectedRow", true],
     ];
     cases.forEach((testCase) => {
       const result = isChildPropertyPath(testCase[0], testCase[1]);
