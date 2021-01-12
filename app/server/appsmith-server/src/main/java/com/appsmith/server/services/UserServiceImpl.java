40003dfe2aa084baa1c18bc9ae5a1d2e9b35192f
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-01-12 15:23:04 +0530

    Remove unused API endpoints for invite confirmation

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/UserServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/UserServiceImpl.java
@@ -59,7 +59,6 @@ import static com.appsmith.server.acl.AclPermission.USER_MANAGE_ORGANIZATIONS;
 public class UserServiceImpl extends BaseService<UserRepository, User, String> implements UserService {
 
     private final OrganizationService organizationService;
-    private final AnalyticsService analyticsService;
     private final SessionUserService sessionUserService;
     private final PasswordResetTokenRepository passwordResetTokenRepository;
     private final PasswordEncoder passwordEncoder;
@@ -102,7 +101,6 @@ public class UserServiceImpl extends BaseService<UserRepository, User, String> i
                            CommonConfig commonConfig) {
         super(scheduler, validator, mongoConverter, reactiveMongoTemplate, repository, analyticsService);
         this.organizationService = organizationService;
-        this.analyticsService = analyticsService;
         this.sessionUserService = sessionUserService;
         this.passwordResetTokenRepository = passwordResetTokenRepository;
         this.passwordEncoder = passwordEncoder;
@@ -363,61 +361,6 @@ public class UserServiceImpl extends BaseService<UserRepository, User, String> i
                 .thenReturn(inviteUser);
 
         return userMono;
-    }
-
-    /**
-     * This function checks if the inviteToken is valid for the user. If the token is incorrect or it as expired,
-     * the client should show the appropriate message to the user
-     *
-     * @param email
-     * @param token
-     * @return
-     */
-    @Override
-    public Mono<Boolean> verifyInviteToken(String email, String token) {
-        log.debug("Verifying token: {} for email: {}", token, email);
-        return repository.findByEmail(email)
-                .switchIfEmpty(Mono.error(new AppsmithException(AppsmithError.NO_RESOURCE_FOUND, "email", email)))
-                .flatMap(inviteUser -> passwordEncoder.matches(token, inviteUser.getInviteToken()) ?
-                        Mono.just(true) : Mono.just(false));
-    }
-
-    /**
-     * This function confirms the signup for a new invited user. Primarily it will be used to set the password
-     * for the user and set the user to enabled. The user should have been created during the invite flow.
-     *
-     * @param inviteUser
-     * @return
-     */
-    @Override
-    public Mono<Boolean> confirmInviteUser(User inviteUser, String originHeader) {
-
-        if (inviteUser.getEmail() == null || inviteUser.getEmail().isEmpty()) {
-            return Mono.error(new AppsmithException(AppsmithError.INVALID_PARAMETER, "email"));
-        }
-
-        if (inviteUser.getPassword() == null || inviteUser.getPassword().isEmpty()) {
-            return Mono.error(new AppsmithException(AppsmithError.INVALID_PARAMETER, "password"));
-        }
-
-        log.debug("Confirming the signup for the user: {} and token: {}", inviteUser.getEmail(), inviteUser.getInviteToken());
-
-        inviteUser.setPassword(this.passwordEncoder.encode(inviteUser.getPassword()));
-
-        return repository.findByEmail(inviteUser.getEmail())
-                .switchIfEmpty(Mono.error(new AppsmithException(AppsmithError.NO_RESOURCE_FOUND, "email", inviteUser.getEmail())))
-                .flatMap(newUser -> {
-
-                    // Activate the user now :
-                    newUser.setIsEnabled(true);
-                    newUser.setPassword(inviteUser.getPassword());
-                    // The user has now been invited and has signed up. Delete the invite token because its no longer required
-                    newUser.setInviteToken(null);
-
-                    return repository.save(newUser)
-                            .map(savedUser -> sendWelcomeEmail(savedUser, originHeader))
-                            .thenReturn(true);
-                });
     }
 
     @Override
