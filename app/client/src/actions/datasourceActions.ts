1e9e9a548d0cfecefd046ab9af7673184c61dab4
vicky-primathon.in <vicky.bansal@primathon.in>
2021-01-13 11:34:45 +0530

    Added page not found page in API editor and Datasource editor

--- app/client/src/actions/datasourceActions.ts
+++ app/client/src/actions/datasourceActions.ts
@@ -17,6 +17,7 @@ export const updateDatasource = (payload: Datasource) => {
 };
 
 export const fetchDatasourceStructure = (id: string) => {
+  console.log({ type: ReduxActionTypes.FETCH_DATASOURCE_STRUCTURE_INIT, id });
   return {
     type: ReduxActionTypes.FETCH_DATASOURCE_STRUCTURE_INIT,
     payload: {
---
+++ app/client/src/pages/Editor/PageNotFound.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "components/editorComponents/Button";
+import PageUnavailableImage from "assets/images/404-image.png";
+import { useHistory } from "react-router-dom";
+
+const Wrapper = styled.div`
+  text-align: center;
+  padding-top: 5%;
+  .bold-text {
+    font-weight: ${(props) => props.theme.fontWeights[3]};
+    font-size: 24px;
+  }
+  .page-unavailable-img {
+    width: 35%;
+  }
+  .button-position {
+    margin: auto;
+  }
+`;
+
+const PageNotFound = () => {
+  const history = useHistory();
+  return (
+    <Wrapper>
+      <img
+        src={PageUnavailableImage}
+        alt="Page Unavailable"
+        className="page-unavailable-img"
+      />
+      <div>
+        <p className="bold-text">Page not found</p>
+        <p>
+          Either this page doesn&apos;t exist, or you don&apos;t have access to{" "}
+          <br />
+          this page.
+        </p>
+        <Button
+          filled
+          text="Go Back"
+          intent="primary"
+          icon="arrow-right"
+          iconAlignment="right"
+          size="small"
+          className="button-position"
+          onClick={() => history.goBack()}
+        />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default PageNotFound;
