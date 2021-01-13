91b8922f1c9502ba290489d8f8992ca88d0293ce
Trisha Anand <trisha@appsmith.com>
2021-01-13 14:30:23 +0530

    In case cs service returns an error, stop processing release notes and resume with the caller flow. (#2550)
    * In case cs service returns an error, stop processing release notes and resume with the caller flow.
    
    * In case the server was unable to exchange messages with the CS server, bail out of further processing gracefully since this is not a must have but rather a good to have feature.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ReleaseNotesService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ReleaseNotesService.java
@@ -2,23 +2,29 @@ package com.appsmith.server.solutions;
 
 import com.appsmith.server.configurations.CloudServicesConfig;
 import com.appsmith.server.dtos.ResponseDTO;
+import com.appsmith.server.exceptions.AppsmithError;
+import com.appsmith.server.exceptions.AppsmithException;
 import com.appsmith.server.services.ConfigService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ReleaseNotesService {
 
     private final CloudServicesConfig cloudServicesConfig;
@@ -26,7 +32,10 @@ public class ReleaseNotesService {
     private final ConfigService configService;
 
     public final List<ReleaseNode> releaseNodesCache = new ArrayList<>();
+
     private Instant cacheExpiryTime = null;
+
+    private final ObjectMapper objectMapper;
 
     @Data
     static class Releases {
@@ -61,13 +70,37 @@ public class ReleaseNotesService {
         }
 
         return configService.getInstanceId()
-                .flatMap(instanceId -> WebClient
-                        .create(baseUrl + "/api/v1/releases?instanceId=" + instanceId)
-                        .get()
-                        .exchange()
-                )
-                .flatMap(response -> response.bodyToMono(new ParameterizedTypeReference<ResponseDTO<Releases>>() {}))
-                .map(result -> result.getData().getNodes())
+                .flatMap(instanceId -> {
+                    WebClient.Builder webClientBuilder = WebClient.builder();
+                    return webClientBuilder
+                            .baseUrl(baseUrl + "/api/v1/releases?instanceId=" + instanceId)
+                            .build()
+                            .method(HttpMethod.GET)
+                            .exchange();
+                })
+                .doOnError(error -> log.error("Error fetching release notes from CS Server : {}", String.valueOf(error)))
+                // In case of an error in exchange with CS Server, stop processing further.
+                .onErrorResume(error -> Mono.empty())
+                .flatMap(clientResponse -> clientResponse.toEntity(String.class))
+                .map(response -> {
+                    ResponseDTO<Releases> releasesResponseDTO;
+                    try {
+                        releasesResponseDTO = objectMapper.readValue(response.toString(), ResponseDTO.class);
+                    } catch (IOException e) {
+                        return Mono.error(new AppsmithException(AppsmithError.JSON_PROCESSING_ERROR, e));
+                    }
+                    return releasesResponseDTO;
+                })
+                .flatMap(result -> {
+                    // If valid response, cast and return the nodes
+                    if (result.getClass().isAssignableFrom(ResponseDTO.class)) {
+                        return Mono.just(((ResponseDTO<Releases>)result).getData().getNodes());
+                    } else {
+                        // An error was returned by the cloud service. Stop the processing without
+                        // throwing an error.
+                        return Mono.empty();
+                    }
+                })
                 .map(nodes -> {
                     releaseNodesCache.clear();
                     releaseNodesCache.addAll(nodes);
