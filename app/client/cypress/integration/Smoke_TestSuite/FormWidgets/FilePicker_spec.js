460bb122f99b9875f1fc5d40dbbc45912cfdcf98
nandan.anantharamu <nandan.anantharamu@thoughtspot.com>
2021-01-19 12:50:43 +0530

    updated review comments

--- app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
@@ -17,13 +17,14 @@ describe("FilePicker Widget Functionality", function() {
 
   it("It checks the loading state of filepicker on call the action", function() {
     cy.openPropertyPane("filepickerwidget");
-    const fixturePath = "example.json";
+    const fixturePath = "testFile.mov";
     cy.getAlert(commonlocators.filePickerOnFilesSelected);
     cy.get(commonlocators.filePickerButton).click();
     cy.get(commonlocators.filePickerInput)
       .first()
       .attachFile(fixturePath);
     cy.get(commonlocators.filePickerUploadButton).click();
+    cy.get(".bp3-spinner").should("have.length", 1);
     cy.wait("@updateLayout").should(
       "have.nested.property",
       "response.body.responseMeta.status",
