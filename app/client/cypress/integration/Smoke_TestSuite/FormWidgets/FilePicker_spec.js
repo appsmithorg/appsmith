3a3266f34b165042462c715a6641a821dd5b5428
nandan.anantharamu <nandan.anantharamu@thoughtspot.com>
2021-01-23 11:48:10 +0530

    updated action on file selected

--- app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/FormWidgets/FilePicker_spec.js
@@ -6,6 +6,7 @@ describe("FilePicker Widget Functionality", function() {
   beforeEach(() => {
     cy.addDsl(dsl);
   });
+
   it("FilePicker Widget Functionality", function() {
     cy.openPropertyPane("filepickerwidget");
 
@@ -18,18 +19,23 @@ describe("FilePicker Widget Functionality", function() {
   it("It checks the loading state of filepicker on call the action", function() {
     cy.openPropertyPane("filepickerwidget");
     const fixturePath = "testFile.mov";
-    cy.getAlert(commonlocators.filePickerOnFilesSelected);
+    cy.addAPI();
+    cy.enterDatasourceAndPath(
+      this.data.paginationUrl,
+      this.data.paginationParam,
+    );
+    cy.get(".t--store-as-datasource-menu").click();
+    cy.get(".t--store-as-datasource").click();
+    cy.saveDatasource();
+    cy.contains(".datasource-highlight", this.data.paginationUrl);
+    cy.SaveAndRunAPI();
+    cy.SearchEntityandOpen("FilePicker1");
     cy.get(commonlocators.filePickerButton).click();
     cy.get(commonlocators.filePickerInput)
       .first()
       .attachFile(fixturePath);
     cy.get(commonlocators.filePickerUploadButton).click();
     cy.get(".bp3-spinner").should("have.length", 1);
-    cy.wait("@updateLayout").should(
-      "have.nested.property",
-      "response.body.responseMeta.status",
-      200,
-    );
     cy.wait(500);
     cy.get("button").contains("1 files selected");
   });
