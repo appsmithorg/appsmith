301fcd8347e244b30711e6df872b061ad3001f34
Piyush <piyush@codeitout.com>
2021-01-29 19:48:19 +0530

    add comments

--- app/client/src/workers/evaluation.worker.ts
+++ app/client/src/workers/evaluation.worker.ts
@@ -1242,21 +1242,25 @@ const extractReferencesFromBinding = (
   const subDeps: Array<string> = [];
   const identifiers = path.match(/[a-zA-Z_$][a-zA-Z_$0-9.\[\]]*/g) || [path];
   identifiers.forEach((identifier: string) => {
+    // If the identifier exists directly, add it and return
     if (all.hasOwnProperty(identifier)) {
       subDeps.push(identifier);
       return;
     }
     const subpaths = _.toPath(identifier);
     let current = "";
+    // We want to keep going till we reach top level, but not add top level
+    // Eg: Input1.text should not depend on entire Table1 unless it explicitly asked for that.
+    // This is mainly to avoid a lot of unnecessary evals, if we feel this is wrong
+    // we can remove the length requirement and it will still work
     while (subpaths.length > 1) {
       current = convertPathToString(subpaths);
+      // We've found the dep, add it and return
       if (all.hasOwnProperty(current)) {
-        break;
+        subDeps.push(current);
+        return;
       }
       subpaths.pop();
-    }
-    if (subpaths && subpaths.length > 1) {
-      subDeps.push(current);
     }
   });
   return _.uniq(subDeps);
