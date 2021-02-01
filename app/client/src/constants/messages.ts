6f093f906e43f72271aee3cebf21da75b7ec2be1
Arpit Mohan <arpit@appsmith.com>
2021-02-01 19:13:22 +0530

    Improving the import statement for util in messages.ts

--- app/client/src/constants/messages.ts
+++ app/client/src/constants/messages.ts
@@ -1,4 +1,4 @@
-const util = require("util");
+import util from "util";
 
 export const ERROR_MESSAGE_SELECT_ACTION = "Please select an action";
 export const ERROR_MESSAGE_SELECT_ACTION_TYPE = "Please select an action type";
