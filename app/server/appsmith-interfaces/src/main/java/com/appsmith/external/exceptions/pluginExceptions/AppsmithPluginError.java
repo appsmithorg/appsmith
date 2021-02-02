6a74f3f3507fc68dc4148a99b8a01ab388d7690d
Sumit Kumar <sumit@appsmith.com>
2021-02-02 11:30:58 +0530

    cleanup

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/exceptions/pluginExceptions/AppsmithPluginError.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/exceptions/pluginExceptions/AppsmithPluginError.java
@@ -8,7 +8,7 @@ import java.text.MessageFormat;
 @Getter
 public enum AppsmithPluginError {
 
-    PLUGIN_ERROR(500, 5000, "PluginExecution failed with error: {0}", AppsmithErrorAction.LOG_EXTERNALLY),
+    PLUGIN_ERROR(500, 5000, "{0}", AppsmithErrorAction.LOG_EXTERNALLY),
     PLUGIN_GET_STRUCTURE_ERROR(500, 5001, "Plugin failed to get structure with error: {0}",
             AppsmithErrorAction.LOG_EXTERNALLY),
     PLUGIN_QUERY_TIMEOUT_ERROR(504, 5002, "Plugin execution for query \"{0}\" timed out in {1}ms. Please " +
