31f23e73d8a1892baa52529d06ebd2b142648ffa
a <rishabh.robben@gmail.com>
2021-02-03 17:04:55 +0530

    set default theme to storybook

--- app/client/.storybook/configs/contexts.js
+++ app/client/.storybook/configs/contexts.js
@@ -1,10 +1,24 @@
 import { ThemeProvider, theme } from "../../src/constants/DefaultTheme";
+import { light, dark } from "constants/DefaultTheme";
 
 export const contexts = [
-    {
-        icon: "box",
-        title: "Themes",
-        components: [ThemeProvider],
-        params: [{ name: "default", props: { theme: theme } }],
-    },
+  {
+    icon: "box",
+    title: "Themes",
+    components: [ThemeProvider],
+    params: [
+      {
+        name: "default",
+        props: {
+          theme: {
+            ...theme,
+            colors: {
+              ...theme.colors,
+              ...dark,
+            },
+          },
+        },
+      },
+    ],
+  },
 ];
