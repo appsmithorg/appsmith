4ecd80d2eb0f487c8a177438d23ebb7877dcc42b
Nidhi <nidhi.nair93@gmail.com>
2021-02-09 14:14:33 +0530

    Review changes

--- app/server/appsmith-interfaces/src/test/java/com.appsmith.external.models/DatasourceTestResultTest.java
+++ app/server/appsmith-interfaces/src/test/java/com.appsmith.external.models/DatasourceTestResultTest.java
@@ -1,5 +1,6 @@
 package com.appsmith.external.models;
 
+import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginError;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNotNull;
@@ -11,10 +12,10 @@ public class DatasourceTestResultTest {
     public void testNewDatasourceTestResult_NullInvalidArray() {
         DatasourceTestResult nullInvalidsResult = new DatasourceTestResult((String) null);
         assertNotNull(nullInvalidsResult);
-        assertTrue(nullInvalidsResult.getInvalids().contains("Unable to test datasource with the given configuration. Please reach out to Appsmith customer support to report this"));
+        assertTrue(nullInvalidsResult.getInvalids().contains(AppsmithPluginError.PLUGIN_DATASOURCE_TEST_GENERIC_ERROR.getMessage()));
 
         nullInvalidsResult = new DatasourceTestResult(new String[]{null});
         assertNotNull(nullInvalidsResult);
-        assertTrue(nullInvalidsResult.getInvalids().contains("Unable to test datasource with the given configuration. Please reach out to Appsmith customer support to report this"));
+        assertTrue(nullInvalidsResult.getInvalids().contains(AppsmithPluginError.PLUGIN_DATASOURCE_TEST_GENERIC_ERROR.getMessage());
     }
 }
