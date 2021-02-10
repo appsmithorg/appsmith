2a7f7ba8c449929ff07d91f624d697614ccabcd4
Sumit Kumar <sumit@appsmith.com>
2021-02-10 15:26:01 +0530

    s3 plugin (#2735)
    1. Integrate S3 plugin.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/DatabaseChangelog.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/DatabaseChangelog.java
@@ -1617,4 +1617,24 @@ public class DatabaseChangelog {
             mongoTemplate.save(plugin);
         }
     }
+
+    @ChangeSet(order = "051", id = "add-amazons3-plugin", author = "")
+    public void addAmazonS3Plugin (MongoTemplate mongoTemplate) {
+        Plugin plugin = new Plugin();
+        plugin.setName("Amazon S3");
+        plugin.setType(PluginType.DB);
+        plugin.setPackageName("amazons3-plugin");
+        plugin.setUiComponent("DbEditorForm");
+        plugin.setResponseType(Plugin.ResponseType.JSON);
+        plugin.setIconLocation("https://s3.us-east-2.amazonaws.com/assets.appsmith.com/AmazonS3.jpeg");
+        plugin.setDocumentationLink("https://docs.appsmith.com/datasource-reference/querying-amazon-s3");
+        plugin.setDefaultInstall(true);
+        try {
+            mongoTemplate.insert(plugin);
+        } catch (DuplicateKeyException e) {
+            log.warn(plugin.getPackageName() + " already present in database.");
+        }
+
+        installPluginToAllOrganizations(mongoTemplate, plugin.getId());
+    }
 }
