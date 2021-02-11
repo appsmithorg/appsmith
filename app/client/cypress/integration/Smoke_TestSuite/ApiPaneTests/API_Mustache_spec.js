d413ade4dda15c5200b20bd5b277d8b9a06b16ad
nandan.anantharamu <nandan.anantharamu@thoughtspot.com>
2021-02-11 12:40:12 +0530

    Added small wait

--- app/client/cypress/integration/Smoke_TestSuite/ApiPaneTests/API_Mustache_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ApiPaneTests/API_Mustache_spec.js
@@ -11,14 +11,14 @@ describe("Moustache test Functionality", function() {
   it("Moustache test Functionality", function() {
     cy.openPropertyPane("textwidget");
     cy.widgetText("Api", widgetsPage.textWidget, widgetsPage.textInputval);
-    cy.testCodeMirror("/users");
+    cy.testCodeMirror("users");
     cy.NavigateToAPI_Panel();
     cy.log("Navigation to API Panel screen successful");
     cy.CreateAPI("TestAPINew");
     cy.log("Creation of API Action successful");
     cy.enterDatasourceAndPath(testdata.baseUrl, testdata.moustacheMethod);
+    cy.wait(300);
     cy.RunAPI();
-    //cy.RunAPI();
     cy.ResponseStatusCheck(testdata.successStatusCode);
     cy.log("Response code check successful");
     cy.ResponseCheck("janet.weaver@reqres.in");
