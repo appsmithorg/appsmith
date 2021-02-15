6ff0870c0e8827f6ea9041cc95e21ed2ba36f1f1
Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>
2021-02-15 14:53:59 +0530

    remove negative margin

--- app/client/src/components/designSystems/blueprint/ModalComponent.tsx
+++ app/client/src/components/designSystems/blueprint/ModalComponent.tsx
@@ -26,7 +26,6 @@ const Container = styled.div<{
       justify-content: center;
       align-items: center;
       & .${Classes.OVERLAY_CONTENT} {
-        margin-top: -${(props) => props.theme.headerHeight};
         width: ${(props) => props.width}px;
         min-height: ${(props) => props.height}px;
         background: white;
