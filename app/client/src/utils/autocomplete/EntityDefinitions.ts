e48f6487f71e76e9e524e2190b13eb1776c136b1
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2021-02-16 15:52:42 +0530

    Switch documentation link correction

--- app/client/src/utils/autocomplete/EntityDefinitions.ts
+++ app/client/src/utils/autocomplete/EntityDefinitions.ts
@@ -135,7 +135,7 @@ export const entityDefinitions = {
   SWITCH_WIDGET: {
     "!doc":
       "Switch is a simple UI widget you can use when you want users to make a binary choice",
-    "!url": "https://docs.appsmith.com/widget-reference/checkbox",
+    "!url": "https://docs.appsmith.com/widget-reference/switch",
     isVisible: isVisible,
     isSwitchedOn: "bool",
     isDisabled: "bool",
