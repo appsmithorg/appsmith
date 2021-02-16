16fcb57ef50ba272c5d4066a4cd0490add4973c6
Nidhi <nidhi.nair93@gmail.com>
2021-02-16 19:17:41 +0530

    gSheets plugin skeleton from Rest API

---
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/connections/APIConnectionFactory.java
@@ -0,0 +1,23 @@
+package com.external.connections;
+
+import com.appsmith.external.models.AuthenticationDTO;
+import com.appsmith.external.models.OAuth2;
+import reactor.core.publisher.Mono;
+
+
+public class APIConnectionFactory {
+
+    public static Mono<APIConnection> createConnection(AuthenticationDTO authenticationType) {
+        if (authenticationType instanceof OAuth2) {
+            if (OAuth2.Type.CLIENT_CREDENTIALS.equals(((OAuth2) authenticationType).getGrantType())) {
+                return Mono.from(OAuth2ClientCredentials.create((OAuth2) authenticationType));
+            } else if (OAuth2.Type.AUTHORIZATION_CODE.equals(((OAuth2) authenticationType).getGrantType())) {
+                return Mono.from(OAuth2AuthorizationCode.create((OAuth2) authenticationType));
+            } else {
+                return Mono.empty();
+            }
+        } else {
+            return Mono.empty();
+        }
+    }
+}
