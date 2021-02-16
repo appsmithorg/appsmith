213b8295b52ebe59dc5763776bc8b28d438cdd4a
RIshabh Saxena <rishabh.robben@gmail.com>
2021-02-16 11:05:34 +0530

    documentation preview

--- app/client/src/components/editorComponents/GlobalSearch/Description.tsx
+++ app/client/src/components/editorComponents/GlobalSearch/Description.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-// import { Highlight as AlgoliaHighlight } from "react-instantsearch-dom";
 import styled from "styled-components";
 import { SEARCH_ITEM_TYPES } from "./utils";
 import { getTypographyByKey } from "constants/DefaultTheme";
-import ReactMarkdown from "react-markdown";
+import marked from "marked";
 
 type Props = {
   activeItem: any;
   activeItemType?: SEARCH_ITEM_TYPES;
 };
+
+const algoliaHighlightTag = "ais-highlight-0000000000";
+
+/**
+ * strip:
+ * image tags,
+ * gitbook plugin tags,
+ * description header (since it might be present for a lot of results)
+ */
+const strip = (text: string) =>
+  text.replace(/<img .*?>|description: &gt;-|description:|{% .*?%}/g, "");
 
 const Container = styled.div`
   flex: 1;
@@ -23,7 +33,7 @@ const Container = styled.div`
     `${props.theme.spaces[3]}px ${props.theme.spaces[4]}px`};
   border-radius: ${(props) => props.theme.radii[2]}px;
   color: ${(props) => props.theme.colors.globalSearch.searchItemText};
-  & .ais-Highlight-highlighted,
+  & ${algoliaHighlightTag}, & .ais-Highlight-highlighted,
   & .search-highlighted {
     background: unset;
     color: ${(props) => props.theme.colors.globalSearch.searchItemHighlight};
@@ -39,14 +49,54 @@ const Container = styled.div`
   }
 `;
 
+const getDocumentationPreviewContent = (
+  activeItem: any,
+): string | undefined => {
+  try {
+    const { value, matchedWords } = activeItem?._highlightResult?.document;
+    const parsedDocument = marked(value);
+    const domparser = new DOMParser();
+    const documentObj = domparser.parseFromString(parsedDocument, "text/html");
+    let content;
+
+    const hasMatches = matchedWords.length > 0;
+    if (hasMatches) {
+      content = documentObj?.querySelector(algoliaHighlightTag)?.parentElement
+        ?.innerHTML;
+    } else {
+      content = documentObj?.querySelector("p")?.innerHTML;
+    }
+    return content ? strip(content).trim() : undefined;
+  } catch (e) {
+    return;
+  }
+};
+
+const DocumentationDescription = ({ activeItem }: { activeItem: any }) => {
+  const content = getDocumentationPreviewContent(activeItem);
+  return content ? (
+    <div dangerouslySetInnerHTML={{ __html: content }} />
+  ) : (
+    <div>no preview</div>
+  );
+};
+
+const descriptionByType = {
+  [SEARCH_ITEM_TYPES.documentation]: DocumentationDescription,
+  [SEARCH_ITEM_TYPES.action]: () => null,
+  [SEARCH_ITEM_TYPES.widget]: () => null,
+};
+
 const Description = (props: Props) => {
   const { activeItem, activeItemType } = props;
 
+  if (!activeItemType || !activeItem) return null;
+
+  const Component = descriptionByType[activeItemType];
+
   return (
     <Container>
-      {activeItemType === SEARCH_ITEM_TYPES.documentation ? (
-        <ReactMarkdown>{activeItem.document}</ReactMarkdown>
-      ) : null}
+      <Component activeItem={activeItem} />
     </Container>
   );
 };
