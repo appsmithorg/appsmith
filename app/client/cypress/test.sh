80837294e74bf2ff889c021c9fcaf0750e6b0545
hetunandu <hetu@appsmith.com>
2021-02-19 16:01:46 +0530

    Update test.sh

--- app/client/cypress/test.sh
+++ app/client/cypress/test.sh
@@ -32,5 +32,5 @@ if [ "$target" == "ci" ]; then
     --parallel --group "Electrons on Gitlab CI" \
     --spec "cypress/integration/Smoke_TestSuite/**/*.js"
 else
-    $(npm bin)/cypress run --headless --browser chrome --spec "cypress/integration/Smoke_TestSuite/*/*"
+    $(npm bin)/cypress run --headless --browser chrome --spec "cypress/integration/Smoke_TestSuite/**/*.js"
 fi
