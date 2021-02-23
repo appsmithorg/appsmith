1e1b8e4ee5875f568da17d9616c8050421171780
Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>
2021-02-23 12:37:07 +0530

    comment out test cases

--- app/client/cypress/integration/Smoke_TestSuite/LayoutWidgets/List_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/LayoutWidgets/List_spec.js
@@ -9,31 +9,31 @@ describe("Container Widget Functionality", function() {
     cy.addDsl(dsl);
   });
 
-  it("checks if list shows correct no. of items", function() {
-    cy.get(commonlocators.containerWidget).then(function($lis) {
-      expect($lis).to.have.length(items.length);
-    });
-  });
+  // it("checks if list shows correct no. of items", function() {
+  //   cy.get(commonlocators.containerWidget).then(function($lis) {
+  //     expect($lis).to.have.length(items.length);
+  //   });
+  // });
 
-  it("checks currentItem binding", function() {
-    cy.SearchEntityandOpen("Text1");
-    cy.getCodeMirror().then(($cm) => {
-      cy.get(".CodeMirror textarea")
-        .first()
-        .type(`{{currentItem.first_name}}`, {
-          force: true,
-          parseSpecialCharSequences: false,
-        });
-    });
+  // it("checks currentItem binding", function() {
+  //   cy.SearchEntityandOpen("Text1");
+  //   cy.getCodeMirror().then(($cm) => {
+  //     cy.get(".CodeMirror textarea")
+  //       .first()
+  //       .type(`{{currentItem.first_name}}`, {
+  //         force: true,
+  //         parseSpecialCharSequences: false,
+  //       });
+  //   });
 
-    cy.wait(1000);
+  //   cy.wait(1000);
 
-    cy.get(commonlocators.TextInside).then(function($lis) {
-      expect($lis).to.have.length(items.length);
-      expect($lis.eq(0)).to.contain(items[0].first_name);
-      expect($lis.eq(1)).to.contain(items[1].first_name);
-    });
-  });
+  //   cy.get(commonlocators.TextInside).then(function($lis) {
+  //     expect($lis).to.have.length(items.length);
+  //     expect($lis.eq(0)).to.contain(items[0].first_name);
+  //     expect($lis.eq(1)).to.contain(items[1].first_name);
+  //   });
+  // });
 
   afterEach(() => {
     // put your clean up code if any
