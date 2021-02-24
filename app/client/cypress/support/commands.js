04701f70629c52a8e12358855eaa36dfc0ae5d62
Piyush <piyush@codeitout.com>
2021-02-24 11:33:19 +0530

    try mouseover solve

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -1810,7 +1810,9 @@ Cypress.Commands.add("deleteDatasource", (datasourceName) => {
 });
 
 Cypress.Commands.add("runQuery", () => {
-  cy.get(queryEditor.runQuery).click();
+  cy.get(queryEditor.runQuery)
+    .trigger("mouseover")
+    .click();
   cy.wait("@postExecute").should(
     "have.nested.property",
     "response.body.responseMeta.status",
