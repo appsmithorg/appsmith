807a38aea3bc684e4c194a8c9a2435647d2fd7c7
Trisha Anand <trisha@appsmith.com>
2021-03-01 14:00:50 +0530

    Removed the old analytics event. Now sending only the new one.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/NewActionServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/NewActionServiceImpl.java
@@ -13,7 +13,6 @@ import com.appsmith.external.models.ActionExecutionResult;
 import com.appsmith.external.models.DatasourceConfiguration;
 import com.appsmith.external.models.Param;
 import com.appsmith.external.models.Policy;
-import com.appsmith.external.models.Property;
 import com.appsmith.external.models.Provider;
 import com.appsmith.external.plugins.PluginExecutor;
 import com.appsmith.server.acl.AclPermission;
@@ -43,7 +42,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.ObjectUtils;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
@@ -61,13 +59,11 @@ import javax.validation.Validator;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
@@ -597,30 +593,25 @@ public class NewActionServiceImpl extends BaseService<NewActionRepository, NewAc
                             .flatMap(datasourceContextService::getDatasourceContext)
                             // Now that we have the context (connection details), execute the action.
                             .flatMap(
-                                    resourceContext -> pluginExecutor.executeParameterized(
+                                    resourceContext -> (Mono<ActionExecutionResult>) pluginExecutor.executeParameterized(
                                             resourceContext.getConnection(),
                                             executeActionDTO,
                                             datasourceConfiguration,
                                             actionConfiguration
                                     )
                             )
-                            .flatMap(result -> Mono.zip(actionMono, actionDTOMono, datasourceMono)
+                            // Now send the analytics event for this execution
+                            .flatMap(result ->
+                                    Mono.zip(actionMono, actionDTOMono, datasourceMono)
                                     .flatMap(tuple1 -> {
-                                        ActionExecutionResult actionExecutionResult = (ActionExecutionResult) result;
+                                        ActionExecutionResult actionExecutionResult = result;
 
                                         return Mono.when(sendExecuteAnalyticsEvent(tuple1.getT1(), tuple1.getT2(), tuple1.getT3(), executeActionDTO.getViewMode(), actionExecutionResult.getRequest()))
-                                                .thenReturn((ActionExecutionResult)result);
+                                                .thenReturn(result);
                                     })
-//                                    .thenReturn((ActionExecutionResult)result)
-                            )
-                            .map(result -> {
-                                log.debug("post analytics, result is : {}", result);
-                                return (ActionExecutionResult) result;
-                            });
+                            );
 
-                    return Mono.zip(actionMono, actionDTOMono, datasourceMono)
-                            .flatMap(tuple1 -> getAnalyticsMono(tuple1.getT1(), tuple1.getT2(), tuple1.getT3(), executeActionDTO))
-                            .then(executionMono)
+                    return executionMono
                             .onErrorResume(StaleConnectionException.class, error -> {
                                 log.info("Looks like the connection is stale. Retrying with a fresh context.");
                                 return datasourceContextService
@@ -675,111 +666,6 @@ public class NewActionServiceImpl extends BaseService<NewActionRepository, NewAc
                     );
                     return tuple.getT2();
                 });
-    }
-
-    private Mono<Void> getAnalyticsMono(NewAction action, ActionDTO actionDTO, Datasource datasource, ExecuteActionDTO executeActionDTO) {
-        // Since we're loading the application from DB *only* for analytics, we check if analytics is
-        // active before making the call to DB.
-        if (!analyticsService.isActive()) {
-            return Mono.empty();
-        }
-
-        return Mono.justOrEmpty(action.getApplicationId())
-                .flatMap(applicationService::findById)
-                .defaultIfEmpty(new Application())
-                .flatMap(application -> Mono.zip(
-                        Mono.just(application),
-                        sessionUserService.getCurrentUser(),
-                        newPageService.getNameByPageId(actionDTO.getPageId(), true)
-                ))
-                .map(tuple -> {
-                    final Application application = tuple.getT1();
-                    final User user = tuple.getT2();
-                    final String pageName = tuple.getT3();
-
-                    final PluginType pluginType = action.getPluginType();
-                    final ActionConfiguration actionConfiguration = actionDTO.getActionConfiguration();
-                    final ObjectMapper objectMapper = new ObjectMapper();
-
-                    final Map<String, Object> requestData = new HashMap<>();
-                    if (pluginType == PluginType.API) {
-
-                        String headersJson;
-                        try {
-                            headersJson = objectMapper.writeValueAsString(actionConfiguration
-                                    .getHeaders()
-                                    .stream()
-                                    .filter(p -> !StringUtils.isEmpty(p.getKey()))
-                                    .collect(Collectors.toMap(Property::getKey, Property::getValue, (a, b) -> b)));
-                        } catch (JsonProcessingException e) {
-                            log.warn("Couldn't serialize headers to JSON", e);
-                            headersJson = "";
-                        }
-
-                        String paramsJson;
-                        try {
-                            paramsJson = objectMapper.writeValueAsString(actionConfiguration
-                                    .getQueryParameters()
-                                    .stream()
-                                    .filter(p -> !StringUtils.isEmpty(p.getKey()))
-                                    .collect(Collectors.toMap(Property::getKey, Property::getValue, (a, b) -> b)));
-                        } catch (JsonProcessingException e) {
-                            log.warn("Couldn't serialize params to JSON", e);
-                            paramsJson = "";
-                        }
-
-                        requestData.putAll(Map.of(
-                                "url", actionDTO.getDatasource().getDatasourceConfiguration().getUrl() + actionConfiguration.getPath(),
-                                "headers", headersJson,
-                                "parameters", paramsJson,
-                                "body", ObjectUtils.defaultIfNull(actionConfiguration.getBody(), "")
-                        ));
-
-                    } else if (pluginType == PluginType.DB) {
-                        requestData.putAll(Map.of(
-                                "query", ObjectUtils.defaultIfNull(actionConfiguration.getBody(), ""),
-                                "properties", actionConfiguration.getPluginSpecifiedTemplates() == null
-                                        ? Collections.emptyMap()
-                                        : actionConfiguration.getPluginSpecifiedTemplates()
-                                            .stream()
-                                            .filter(Objects::nonNull)
-                                            .collect(Collectors.toMap(
-                                                    p -> ObjectUtils.defaultIfNull(p.getKey(), ""),
-                                                    p -> ObjectUtils.defaultIfNull(p.getValue(), "")
-                                            ))
-                        ));
-                    }
-
-                    final Map<String, Object> data = new HashMap<>();
-
-                    data.putAll(Map.of(
-                            "username", user.getUsername(),
-                            "type", pluginType,
-                            "name", actionDTO.getName(),
-                            "datasource", Map.of(
-                                    "name", datasource.getName()
-                            ),
-                            "orgId", application.getOrganizationId(),
-                            "appId", action.getApplicationId(),
-                            "appMode", Boolean.TRUE.equals(executeActionDTO.getViewMode()) ? "view" : "edit",
-                            "appName", application.getName(),
-                            "isExampleApp", application.isAppIsExample(),
-                            "request", requestData
-                    ));
-
-                    data.putAll(Map.of(
-                            "pageId", actionDTO.getPageId(),
-                            "pageName", pageName
-                    ));
-
-                    analyticsService.sendEvent(AnalyticsEvents.EXECUTE_ACTION.getEventName(), user.getUsername(), data);
-                    return user;
-                })
-                .onErrorResume(error -> {
-                    log.warn("Error sending action execution data point", error);
-                    return Mono.empty();
-                })
-                .then();
     }
 
     private Mono<ActionExecutionRequest> sendExecuteAnalyticsEvent(NewAction action, ActionDTO actionDTO, Datasource datasource, Boolean viewMode, ActionExecutionRequest actionExecutionRequest) {
