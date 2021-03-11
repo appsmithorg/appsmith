ab4eaf23dc126cfaac04cb114629bcb3961bda09
nandan.anantharamu <nandan.anantharamu@thoughtspot.com>
2021-03-11 22:52:45 +0530

    added test with switch widget within farm widget

---
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/FormWithSwitch_spec.js
@@ -0,0 +1,65 @@
+const commonlocators = require("../../../../locators/commonlocators.json");
+const formWidgetsPage = require("../../../../locators/FormWidgets.json");
+const widgetsPage = require("../../../../locators/Widgets.json");
+const publish = require("../../../../locators/publishWidgetspage.json");
+const dsl = require("../../../../fixtures/formSwitchDsl.json");
+const pages = require("../../../../locators/Pages.json");
+
+describe("Switch Widget within Form widget Functionality", function () {
+  before(() => {
+    cy.addDsl(dsl);
+  });
+  it("Switch Widget Functionality check with success message", function () {
+    cy.openPropertyPane("switchwidget");
+    cy.widgetText(
+      "Toggler",
+      formWidgetsPage.switchWidget,
+      widgetsPage.switchInput,
+    );
+    cy.testCodeMirror(this.data.switchInputName);
+    cy.get(widgetsPage.switchLabel).should("have.text", "Switch1");
+    cy.togglebar(widgetsPage.defaultcheck);
+    cy.getAlert(commonlocators.optionchangetextSwitch);
+    cy.closePropertyPane();
+  });
+
+  it("Form reset button valdiation with switch widget", function () {
+    cy.SearchEntityandOpen("FormButton2");
+    cy.get(widgetsPage.actionSelect).click();
+    cy.get(commonlocators.chooseAction)
+      .children()
+      .contains("Reset Widget")
+      .click();
+    cy.get(".t--open-dropdown-").click({ force: true });
+    cy.get(commonlocators.chooseAction)
+      .children()
+      .contains("Toggler")
+      .click();
+    cy.closePropertyPane();
+    cy.get(".t--widget-switchwidget").click();
+    cy.get(".t--toast-action span")
+      .last()
+      .invoke("text")
+      .then((text) => {
+        const toasttext = text;
+        cy.log(toasttext);
+        expect(text.trim()).to.equal(toasttext.trim());
+      });
+    cy.wait(7000);
+    cy.get("Button:contains('Reset')").click({ force: true })
+    /*
+    cy.get(".t--toast-action span")
+      .last()
+      .invoke("text")
+      .then((text) => {
+        const toasttext = text;
+        cy.log(toasttext);
+        expect(text.trim()).to.equal(toasttext.trim());
+      });
+      */
+  });
+
+});
+afterEach(() => {
+  // put your clean up code if any
+});
