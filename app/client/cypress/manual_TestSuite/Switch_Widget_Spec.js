cdbd63b8f31a5c9ae7d3dd3f284451f679bfaf74
prapullac <71753653+prapullac@users.noreply.github.com>
2021-03-11 12:45:32 +0530

    adding test ideas for Switch widget

---
+++ app/client/cypress/manual_TestSuite/Switch_Widget_Spec.js
@@ -0,0 +1,32 @@
+const dsl = require("../../../fixtures/switchWidgetDsl.json");
+
+describe("Test to add switch widget in canvas", function() {
+  it(" Add a switch widget and bind it to action", function() {
+    // Add a switch widget
+    // Click on the Property Pane
+    // Naviagte to Action Items
+    // Click on "onChange" Dropdown
+    // Select "Show a message"
+    // Add a message 
+    // now switch the button
+    // and observe the message is displyed to user
+  }
+  )
+
+  it(" Add a switch widget to a form to reset the widget", function() {
+    // Add a Form widget
+    // Add a switch widget
+    // Navigate to Reset button  of the Form
+    // Click on the Property Pane
+    // Naviagte to Action Items
+    // Click on "onChange" Dropdown
+    // Select "Reset a widget"
+    // Add a widget as "switch widget"
+    // Now "Reset Children to Yes "
+    // Ensure the Switch is "Flase"
+    // now Click on Reset 
+    // and observe the the button becomes active 
+  }
+  )
+}
+)
