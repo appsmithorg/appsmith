4e541ac851acdfefa46ab9b9fac1210173064fd6
Abhinav Jha <abhinav@appsmith.com>
2021-03-18 02:29:58 +0530

    Make filteredTableData and computed columns as derived properties

--- app/client/src/widgets/TableWidget/derived.js
+++ app/client/src/widgets/TableWidget/derived.js
@@ -92,4 +92,253 @@ export default {
     return [];
   },
   //
+  getTableColumns: (props) => {
+    let columns = [];
+    let allColumns = props.primaryColumns || {};
+
+    const sortColumn = props.sortedColumn?.column;
+    const sortOrder = props.sortedColumn?.asc;
+    if (
+      props.columnOrder &&
+      Array.isArray(props.columnOrder) &&
+      props.columnOrder.length > 0
+    ) {
+      const newColumnsInOrder = {};
+
+      props.columnOrder.forEach((id, index) => {
+        if (allColumns[id])
+          newColumnsInOrder[id] = { ...allColumns[id], index };
+      });
+      const remaining = _.without(
+        Object.keys(allColumns),
+        ...Object.keys(newColumnsInOrder),
+      );
+      const len = Object.keys(newColumnsInOrder).length;
+      if (remaining && remaining.length > 0) {
+        remaining.forEach((id, index) => {
+          newColumnsInOrder[id] = { ...allColumns[id], index: len + index };
+        });
+      }
+      allColumns = newColumnsInOrder;
+    }
+    const allColumnProperties = Object.values(allColumns);
+    for (let index = 0; index < allColumnProperties.length; index++) {
+      const columnProperties = allColumnProperties[index];
+      columnProperties.isAscOrder =
+        columnProperties.id === sortColumn ? sortOrder : undefined;
+      const columnData = columnProperties;
+      columns.push(columnData);
+    }
+    return columns;
+  },
+  //
+  getFilteredTableData: (props) => {
+    if (!props.sanitizedTableData || !props.sanitizedTableData.length) {
+      return [];
+    }
+    const derivedTableData = [...props.sanitizedTableData];
+    if (props.primaryColumns && _.isPlainObject(props.primaryColumns)) {
+      const primaryColumns = props.primaryColumns;
+      const columnIds = Object.keys(props.primaryColumns);
+      columnIds.forEach((columnId) => {
+        const column = primaryColumns[columnId];
+        let computedValues = [];
+
+        if (column && column.computedValue) {
+          if (_.isString(column.computedValue)) {
+            try {
+              computedValues = JSON.parse(column.computedValue);
+            } catch (e) {
+              console.log("Error parsing column value: ", column.computedValue);
+            }
+          } else if (Array.isArray(column.computedValue)) {
+            computedValues = column.computedValue;
+          }
+        }
+
+        if (computedValues.length === 0) {
+          if (props.derivedColumns) {
+            const derivedColumn = props.derivedColumns[columnId];
+            if (derivedColumn) {
+              computedValues = Array(derivedTableData.length).fill("");
+            }
+          }
+        }
+
+        for (let index = 0; index < computedValues.length; index++) {
+          derivedTableData[index] = {
+            ...derivedTableData[index],
+            [columnId]: computedValues[index],
+          };
+        }
+      });
+    }
+
+    const columns = props.columns;
+
+    let sortedTableData;
+    if (props.sortedColumn) {
+      const sortedColumn = props.sortedColumn.column;
+      const sortOrder = sortedColumn.asc;
+      const column = columns.find((column) => column.id === props.sortedColumn);
+      const columnType = column && column.type ? column.type : "text";
+
+      sortedTableData = derivedTableData.sort((a, b) => {
+        if (
+          _.isPlainObject(a) &&
+          _.isPlainObject(b) &&
+          !_.isNil(a[sortedColumn]) &&
+          !_.isNil(b[sortedColumn])
+        ) {
+          switch (columnType) {
+            case "number":
+              return sortOrder
+                ? Number(a[sortedColumn]) > Number(b[sortedColumn])
+                  ? 1
+                  : -1
+                : Number(b[sortedColumn]) > Number(a[sortedColumn])
+                ? 1
+                : -1;
+            case "date":
+              try {
+                return sortOrder
+                  ? moment(a[sortedColumn]).isAfter(b[sortedColumn])
+                    ? 1
+                    : -1
+                  : moment(b[sortedColumn]).isAfter(a[sortedColumn])
+                  ? 1
+                  : -1;
+              } catch (e) {
+                return -1;
+              }
+            default:
+              return sortOrder
+                ? a[sortedColumn].toString().toUpperCase() >
+                  b[sortedColumn].toString().toUpperCase()
+                  ? 1
+                  : -1
+                : b[sortedColumn].toString().toUpperCase() >
+                  a[sortedColumn].toString().toUpperCase()
+                ? 1
+                : -1;
+          }
+        } else {
+          return sortOrder ? 1 : 0;
+        }
+      });
+    } else {
+      sortedTableData = [...derivedTableData];
+    }
+    const ConditionFunctions = {
+      isExactly: (a, b) => {
+        return a.toString() === b.toString();
+      },
+      empty: (a) => {
+        return a === "" || a === undefined || a === null;
+      },
+      notEmpty: (a) => {
+        return a !== "" && a !== undefined && a !== null;
+      },
+      notEqualTo: (a, b) => {
+        return a.toString() !== b.toString();
+      },
+      isEqualTo: (a, b) => {
+        return a.toString() === b.toString();
+      },
+      lessThan: (a, b) => {
+        const numericB = Number(b);
+        const numericA = Number(a);
+        return numericA < numericB;
+      },
+      lessThanEqualTo: (a, b) => {
+        const numericB = Number(b);
+        const numericA = Number(a);
+        return numericA <= numericB;
+      },
+      greaterThan: (a, b) => {
+        const numericB = Number(b);
+        const numericA = Number(a);
+        return numericA > numericB;
+      },
+      greaterThanEqualTo: (a, b) => {
+        const numericB = Number(b);
+        const numericA = Number(a);
+        return numericA >= numericB;
+      },
+      contains: (a, b) => {
+        if (isString(a) && isString(b)) {
+          return a.includes(b);
+        }
+        return false;
+      },
+      doesNotContain: (a, b) => {
+        if (_.isString(a) && _.isString(b)) {
+          return !a.includes(b);
+        }
+        return false;
+      },
+      startsWith: (a, b) => {
+        if (_.isString(a) && _.isString(b)) {
+          return a.indexOf(b) === 0;
+        }
+        return false;
+      },
+      endsWith: (a, b) => {
+        if (_.isString(a) && _.isString(b)) {
+          return a.length === a.indexOf(b) + b.length;
+        }
+        return false;
+      },
+      is: (a, b) => {
+        return moment(a).isSame(moment(b), "d");
+      },
+      isNot: (a, b) => {
+        return !moment(a).isSame(moment(b), "d");
+      },
+      isAfter: (a, b) => {
+        return !moment(a).isAfter(moment(b), "d");
+      },
+      isBefore: (a, b) => {
+        return !moment(a).isBefore(moment(b), "d");
+      },
+    };
+
+    const searchKey = props.searchText ? props.searchText.toLowerCase() : "";
+
+    const finalTableData = sortedTableData.filter((item) => {
+      const searchFound = searchKey
+        ? Object.values(item)
+            .join(", ")
+            .toLowerCase()
+            .includes(searchKey)
+        : true;
+      if (!searchFound) return false;
+      if (!props.filters || props.filters.length === 0) return true;
+      const filterOperator = filters.length >= 2 ? filters[1].operator : "OR";
+      let filter = filterOperator === "AND";
+      for (let i = 0; i < filters.length; i++) {
+        let result = true;
+        try {
+          const conditionFunction = ConditionFunctions[filters[i].condition];
+          if (conditionFunction) {
+            result = conditionFunction(
+              item[filters[i].column],
+              filters[i].value,
+            );
+          }
+        } catch (e) {
+          console.log(e);
+        }
+        const filterValue = result;
+        if (filterOperator === "AND") {
+          filter = filter && filterValue;
+        } else {
+          filter = filter || filterValue;
+        }
+      }
+      return filter;
+    });
+    return finalTableData;
+  },
+  //
 };
