bb1d0059d3f8cc4b34614e34bad683f2d37f6809
Shri <shrikant@appsmith.com>
2021-03-18 21:08:56 +0530

    Clear OAuth tokens for forked datasources (#3609)
    * Clear OAuth tokens for forked datasources
    
    * Fix datasource duplicate finder in light of oAuth tokens
    
    * Fix potential NPE

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/AuthenticationResponse.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/AuthenticationResponse.java
@@ -1,13 +1,16 @@
 package com.appsmith.external.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthenticationResponse {
     String token;
 
