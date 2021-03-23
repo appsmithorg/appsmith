0c9ece83bd3b7f072b98a62b90ac9cd427214412
vicky-primathon.in <vicky.bansal@primathon.in>
2021-03-23 13:25:28 +0530

    Added unit test to text widget migrations

---
+++ app/client/src/utils/migrations/TextWidgetReplaceTextStyle.test.ts
@@ -0,0 +1,105 @@
+import { WidgetProps } from "widgets/BaseWidget";
+import { ContainerWidgetProps } from "widgets/ContainerWidget";
+import { migrateTextStyleFromTextWidget } from "utils/migrations/TextWidgetReplaceTextStyle";
+
+const inputDsl: ContainerWidgetProps<WidgetProps> = {
+  widgetName: "MainContainer",
+  backgroundColor: "none",
+  rightColumn: 1118,
+  snapColumns: 16,
+  detachFromLayout: true,
+  widgetId: "0",
+  topRow: 0,
+  bottomRow: 1280,
+  containerStyle: "none",
+  snapRows: 33,
+  parentRowSpace: 1,
+  type: "CANVAS_WIDGET",
+  canExtend: true,
+  version: 15,
+  minHeight: 1292,
+  parentColumnSpace: 1,
+  dynamicTriggerPathList: [],
+  dynamicBindingPathList: [],
+  leftColumn: 0,
+  isLoading: false,
+  parentId: "",
+  renderMode: "CANVAS",
+  children: [
+    {
+      isVisible: true,
+      text: "Label",
+      textStyle: "LABEL",
+      textAlign: "LEFT",
+      widgetName: "Text1",
+      version: 1,
+      type: "TEXT_WIDGET",
+      isLoading: false,
+      parentColumnSpace: 67.375,
+      parentRowSpace: 40,
+      leftColumn: 3,
+      rightColumn: 7,
+      topRow: 1,
+      bottomRow: 2,
+      parentId: "0",
+      widgetId: "yf8bhokz7d",
+      dynamicBindingPathList: [],
+      renderMode: "CANVAS",
+    },
+  ],
+};
+
+const outputDsl: ContainerWidgetProps<WidgetProps> = {
+  widgetName: "MainContainer",
+  backgroundColor: "none",
+  rightColumn: 1118,
+  snapColumns: 16,
+  detachFromLayout: true,
+  widgetId: "0",
+  topRow: 0,
+  bottomRow: 1280,
+  containerStyle: "none",
+  snapRows: 33,
+  parentRowSpace: 1,
+  type: "CANVAS_WIDGET",
+  canExtend: true,
+  version: 15,
+  minHeight: 1292,
+  parentColumnSpace: 1,
+  dynamicTriggerPathList: [],
+  dynamicBindingPathList: [],
+  leftColumn: 0,
+  isLoading: false,
+  parentId: "",
+  renderMode: "CANVAS",
+  children: [
+    {
+      isVisible: true,
+      text: "Label",
+      textAlign: "LEFT",
+      widgetName: "Text1",
+      version: 1,
+      type: "TEXT_WIDGET",
+      isLoading: false,
+      parentColumnSpace: 67.375,
+      parentRowSpace: 40,
+      leftColumn: 3,
+      rightColumn: 7,
+      topRow: 1,
+      bottomRow: 2,
+      parentId: "0",
+      widgetId: "yf8bhokz7d",
+      dynamicBindingPathList: [],
+      fontSize: "PARAGRAPH",
+      fontStyle: "BOLD",
+      renderMode: "CANVAS",
+    },
+  ],
+};
+
+describe("Text Widget Property Pane Upgrade", () => {
+  it("To test text widget textStyle property is migrated", () => {
+    const newDsl = migrateTextStyleFromTextWidget(inputDsl);
+    expect(JSON.stringify(newDsl) === JSON.stringify(outputDsl));
+  });
+});
