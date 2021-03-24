1ccece69e1199ecebe12ae6a8a571724845070fb
Abhinav Jha <abhinav@appsmith.com>
2021-03-25 03:35:04 +0530

    Custom FusionCharts Config (#2670)
    * Property pane enhancements
    
    - Property pane sections are collapsible
    - Property pane controls can be hidden conditionally
    - Property pane configurations now come from the widget instead of a global config file
    - Property pane property updates can be hooked with other related updates
    - Property pane control and section ids are generated dynamically now.
    
    * Add chart type: "Custom FusionChart" (#2996)
    
    Co-authored-by: Zeger Hoogeboom <zegerhoogeboom@users.noreply.github.com>
    Co-authored-by: zeger <zeger@equinoxai.com>

--- app/client/src/workers/validations.test.ts
+++ app/client/src/workers/validations.test.ts
@@ -164,6 +164,303 @@ describe("Validate Validators", () => {
   });
 });
 
+describe("Chart Custom Config validator", () => {
+  const validator = VALIDATORS.CUSTOM_FUSION_CHARTS_DATA;
+  it("correctly validates ", () => {
+    const cases = [
+      {
+        input: {
+          type: "area2d",
+          dataSource: {
+            chart: {
+              caption: "Countries With Most Oil Reserves [2017-18]",
+              subCaption: "In MMbbl = One Million barrels",
+              xAxisName: "Country",
+              yAxisName: "Reserves (MMbbl)",
+              numberSuffix: "K",
+            },
+            data: [
+              {
+                label: "Venezuela",
+                value: "290",
+              },
+              {
+                label: "Saudi",
+                value: "260",
+              },
+              {
+                label: "Canada",
+                value: "180",
+              },
+              {
+                label: "Iran",
+                value: "140",
+              },
+              {
+                label: "Russia",
+                value: "115",
+              },
+              {
+                label: "UAE",
+                value: "100",
+              },
+              {
+                label: "US",
+                value: "30",
+              },
+              {
+                label: "China",
+                value: "30",
+              },
+            ],
+          },
+        },
+
+        output: {
+          isValid: true,
+          parsed: {
+            type: "area2d",
+            dataSource: {
+              chart: {
+                caption: "Countries With Most Oil Reserves [2017-18]",
+                subCaption: "In MMbbl = One Million barrels",
+                xAxisName: "Country",
+                yAxisName: "Reserves (MMbbl)",
+                numberSuffix: "K",
+              },
+              data: [
+                {
+                  label: "Venezuela",
+                  value: "290",
+                },
+                {
+                  label: "Saudi",
+                  value: "260",
+                },
+                {
+                  label: "Canada",
+                  value: "180",
+                },
+                {
+                  label: "Iran",
+                  value: "140",
+                },
+                {
+                  label: "Russia",
+                  value: "115",
+                },
+                {
+                  label: "UAE",
+                  value: "100",
+                },
+                {
+                  label: "US",
+                  value: "30",
+                },
+                {
+                  label: "China",
+                  value: "30",
+                },
+              ],
+            },
+          },
+
+          transformed: {
+            type: "area2d",
+            dataSource: {
+              chart: {
+                caption: "Countries With Most Oil Reserves [2017-18]",
+                subCaption: "In MMbbl = One Million barrels",
+                xAxisName: "Country",
+                yAxisName: "Reserves (MMbbl)",
+                numberSuffix: "K",
+              },
+              data: [
+                {
+                  label: "Venezuela",
+                  value: "290",
+                },
+                {
+                  label: "Saudi",
+                  value: "260",
+                },
+                {
+                  label: "Canada",
+                  value: "180",
+                },
+                {
+                  label: "Iran",
+                  value: "140",
+                },
+                {
+                  label: "Russia",
+                  value: "115",
+                },
+                {
+                  label: "UAE",
+                  value: "100",
+                },
+                {
+                  label: "US",
+                  value: "30",
+                },
+                {
+                  label: "China",
+                  value: "30",
+                },
+              ],
+            },
+          },
+        },
+      },
+      {
+        input: {
+          type: "area2d",
+          dataSource: {
+            data: [
+              {
+                label: "Venezuela",
+                value: "290",
+              },
+              {
+                label: "Saudi",
+                value: "260",
+              },
+              {
+                label: "Canada",
+                value: "180",
+              },
+              {
+                label: "Iran",
+                value: "140",
+              },
+              {
+                label: "Russia",
+                value: "115",
+              },
+              {
+                label: "UAE",
+                value: "100",
+              },
+              {
+                label: "US",
+                value: "30",
+              },
+              {
+                label: "China",
+                value: "30",
+              },
+            ],
+          },
+        },
+        output: {
+          isValid: true,
+          parsed: {
+            type: "area2d",
+            dataSource: {
+              data: [
+                {
+                  label: "Venezuela",
+                  value: "290",
+                },
+                {
+                  label: "Saudi",
+                  value: "260",
+                },
+                {
+                  label: "Canada",
+                  value: "180",
+                },
+                {
+                  label: "Iran",
+                  value: "140",
+                },
+                {
+                  label: "Russia",
+                  value: "115",
+                },
+                {
+                  label: "UAE",
+                  value: "100",
+                },
+                {
+                  label: "US",
+                  value: "30",
+                },
+                {
+                  label: "China",
+                  value: "30",
+                },
+              ],
+            },
+          },
+          transformed: {
+            type: "area2d",
+            dataSource: {
+              data: [
+                {
+                  label: "Venezuela",
+                  value: "290",
+                },
+                {
+                  label: "Saudi",
+                  value: "260",
+                },
+                {
+                  label: "Canada",
+                  value: "180",
+                },
+                {
+                  label: "Iran",
+                  value: "140",
+                },
+                {
+                  label: "Russia",
+                  value: "115",
+                },
+                {
+                  label: "UAE",
+                  value: "100",
+                },
+                {
+                  label: "US",
+                  value: "30",
+                },
+                {
+                  label: "China",
+                  value: "30",
+                },
+              ],
+            },
+          },
+        },
+      },
+      {
+        input: {
+          type: undefined,
+          dataSource: undefined,
+        },
+        output: {
+          isValid: false,
+          message:
+            "Value does not match type: {type: string, dataSource: { chart: object, data: Array<{label: string, value: number}>}}",
+          parsed: {
+            type: undefined,
+            dataSource: undefined,
+          },
+          transformed: {
+            type: undefined,
+            dataSource: undefined,
+          },
+        },
+      },
+    ];
+    for (const testCase of cases) {
+      const response = validator(testCase.input, DUMMY_WIDGET, {});
+      expect(response).toStrictEqual(testCase.output);
+    }
+  });
+});
 describe("validateDateString test", () => {
   it("Check whether the valid date strings are recognized as valid", () => {
     const validDateStrings = [
