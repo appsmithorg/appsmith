45d7a5fe0b40e54e9baaf1352a6de3e33c43ff91
vicky-primathon.in <vicky.bansal@primathon.in>
2021-03-25 23:46:56 +0530

    Added unit tests

--- app/client/src/constants/DefaultTheme.test.tsx
+++ app/client/src/constants/DefaultTheme.test.tsx
@@ -10,6 +10,24 @@ describe("test getBorderCSSShorthand", () => {
       }),
     ).toEqual("2px solid #E0E0E0");
   });
+  it("test for expected value", () => {
+    expect(
+      getBorderCSSShorthand({
+        thickness: 2,
+        style: "dashed",
+        color: "#E0E0E0",
+      }),
+    ).toEqual("2px dashed #E0E0E0");
+  });
+  it("test for expected value", () => {
+    expect(
+      getBorderCSSShorthand({
+        thickness: 2,
+        style: "solid",
+        color: "#eeeeee",
+      }),
+    ).toEqual("2px solid #eeeeee");
+  });
   it("test for empty value", () => {
     expect(getBorderCSSShorthand(undefined)).toEqual("");
   });
