af223a4340d5cd0d8ce4d4d9ee5b60cdbb9564b9
Rishabh Saxena <rishabh.robben@gmail.com>
2021-03-31 19:44:38 +0530

    Fix circular depependency

--- app/client/src/mockResponses/WidgetConfigResponse.tsx
+++ app/client/src/mockResponses/WidgetConfigResponse.tsx
@@ -2,7 +2,7 @@ import { WidgetConfigReducerState } from "reducers/entityReducers/widgetConfigRe
 import { WidgetProps } from "widgets/BaseWidget";
 import moment from "moment-timezone";
 import { generateReactKey } from "utils/generators";
-import { FileDataTypes } from "widgets/FilepickerWidget";
+import FileDataTypes from "widgets/FileDataTypes";
 
 const WidgetConfigResponse: WidgetConfigReducerState = {
   config: {
