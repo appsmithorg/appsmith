2fc981aea9d8ae81845a690e8f3bbb717a35a631
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-04-01 22:33:41 +0530

    tests to make sure dependency helper functions are also called from the module we are testing

--- app/client/src/components/editorComponents/GlobalSearch/utils.test.ts
+++ app/client/src/components/editorComponents/GlobalSearch/utils.test.ts
@@ -1,20 +1,31 @@
-import { fetchDefaultDocs } from "./utils";
+import { fetchDefaultDocs, DocSearchItem } from "./utils";
+import * as githubHelper from "./githubHelper";
+
+// we mock the actual API call that uses "fetch"
+// and make sure that this is called from the
+// module we are testing(utils)
+jest.mock("./githubHelper", () => ({
+  fetchRawGithubContentList: jest.fn(),
+}));
 
 describe("globalsearch utils", () => {
   it("returns default github docs", async () => {
     let documents = [];
     await fetchDefaultDocs(
-      (truthy) => {
+      (truthy: boolean) => {
         // do nothing for now
         // to turn off errors
         expect(truthy === null || typeof truthy === "boolean").toBeTruthy();
       },
-      (docs) => {
+      (docs: DocSearchItem[]) => {
         documents = docs;
         expect(documents.length).toEqual(4);
       },
       0,
       2,
     );
+  });
+  it("makes sure that the helper dependency function is also called", async () => {
+    expect(githubHelper.fetchRawGithubContentList).toBeCalled();
   });
 });
--- app/client/src/components/editorComponents/GlobalSearch/utils.tsx
+++ app/client/src/components/editorComponents/GlobalSearch/utils.tsx
@@ -1,5 +1,6 @@
 import { Datasource } from "entities/Datasource";
 import { useEffect, useState } from "react";
+import { fetchRawGithubContentList } from "./githubHelper";
 
 export type RecentEntity = {
   type: string;
@@ -67,66 +68,6 @@ export const getItemTitle = (item: SearchItem): string => {
     default:
       return "";
   }
-};
-
-const defaultDocsConfig = [
-  {
-    link:
-      "https://raw.githubusercontent.com/appsmithorg/appsmith-docs/v1.2.1/tutorial-1/README.md",
-    title: "Tutorial",
-    path: "master/tutorial-1",
-    kind: "document",
-  },
-  {
-    link:
-      "https://raw.githubusercontent.com/appsmithorg/appsmith-docs/v1.2.1/core-concepts/connecting-to-data-sources/README.md",
-    title: "Connecting to Data Sources",
-    path: "master/core-concepts/connecting-to-data-sources",
-    kind: "document",
-  },
-  {
-    link:
-      "https://raw.githubusercontent.com/appsmithorg/appsmith-docs/v1.2.1/core-concepts/displaying-data-read/README.md",
-    title: "Displaying Data (Read)",
-    path: "master/core-concepts/displaying-data-read",
-    kind: "document",
-  },
-  {
-    link:
-      "https://raw.githubusercontent.com/appsmithorg/appsmith-docs/v1.2.1/core-concepts/writing-code/README.md",
-    title: "Writing Code",
-    path: "master/core-concepts/writing-code",
-    kind: "document",
-  },
-];
-
-const githubDocsAssetsPath =
-  "https://raw.githubusercontent.com/appsmithorg/appsmith-docs/v1.2.1/.gitbook";
-
-const fetchRawGithubContentList = async () => {
-  const data = await Promise.all(
-    defaultDocsConfig.map(async (doc: any) => {
-      const response = await fetch(doc.link);
-      let document = await response.text();
-      const assetRegex = new RegExp("[../]*?/.gitbook", "g");
-      document = document.replace(assetRegex, githubDocsAssetsPath);
-      return {
-        _highlightResult: {
-          document: {
-            value: document,
-          },
-          title: {
-            value: doc.title,
-          },
-        },
-        ...doc,
-      } as DocSearchItem;
-    }),
-  );
-  if (data) {
-    return data;
-  }
-  return [];
 };
 
 // Helper function to keep calling
