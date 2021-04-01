e3a7d7d2bf123d938b725595a904c72c2c18dde9
Nidhi <nidhi.nair93@gmail.com>
2021-04-01 10:12:04 +0530

    Another case for when state is present but not as expected

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/AuthenticationService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/AuthenticationService.java
@@ -186,10 +186,12 @@ public class AuthenticationService {
                 })
                 // We have no use of the datasource object during redirection, we merely send the response as a success state
                 .flatMap((datasource -> this.getPageRedirectUrl(state, null)))
-                .onErrorResume(e -> {
-                    log.debug("Error while retrieving access token: ", e);
-                    return this.getPageRedirectUrl(state, "appsmith_error");
-                });
+                .onErrorResume(
+                        e -> !(AppsmithError.UNAUTHORIZED_ACCESS.equals(((AppsmithException) e).getError())),
+                        e -> {
+                            log.debug("Error while retrieving access token: ", e);
+                            return this.getPageRedirectUrl(state, "appsmith_error");
+                        });
     }
 
     private Mono<String> getPageRedirectUrl(String state, String error) {
