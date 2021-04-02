41c60071c5212c424465f0fd40451c61e0b06256
root <root@DESKTOP-9GENCK0.localdomain>
2021-04-02 16:38:04 +0530

    fix test

--- app/client/src/utils/autocomplete/EntityDefinitions.test.ts
+++ app/client/src/utils/autocomplete/EntityDefinitions.test.ts
@@ -30,7 +30,7 @@ describe("EntityDefinitions", () => {
     const output = {
       "!doc":
         "Containers are used to group widgets together to form logical higher order widgets. Containers let you organize your page better and move all the widgets inside them together.",
-      "!url": "https://docs.appsmith.com/widget-reference/how-to-use-widgets",
+      "!url": "https://docs.appsmith.com/widget-reference/list",
       backgroundColor: {
         "!type": "string",
         "!url": "https://docs.appsmith.com/widget-reference/how-to-use-widgets",
