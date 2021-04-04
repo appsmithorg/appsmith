988efeaa69d378d943a387e1e73510334958adc5
Rishabh Saxena <rishabh.robben@gmail.com>
2021-04-04 21:54:44 +0530

    ENV var to set if comments are enabled

--- app/client/src/configs/index.ts
+++ app/client/src/configs/index.ts
@@ -41,6 +41,7 @@ export type INJECTED_CONFIGS = {
   intercomAppID: string;
   mailEnabled: boolean;
   disableTelemetry: boolean;
+  commentsEnabled: boolean;
 };
 declare global {
   interface Window {
@@ -113,6 +114,7 @@ const getConfigsFromEnvVars = (): INJECTED_CONFIGS => {
       ? process.env.REACT_APP_MAIL_ENABLED.length > 0
       : false,
     disableTelemetry: true,
+    commentsEnabled: !!process.env.REACT_APP_COMMENTS_ENABLED,
   };
 };
 
@@ -263,7 +265,7 @@ export const getAppsmithConfigs = (): AppsmithUIConfigs => {
       ENV_CONFIG.intercomAppID || APPSMITH_FEATURE_CONFIGS.intercomAppID,
     mailEnabled: ENV_CONFIG.mailEnabled || APPSMITH_FEATURE_CONFIGS.mailEnabled,
     disableTelemetry: APPSMITH_FEATURE_CONFIGS.disableTelemetry,
-    commentsEnabled: true,
+    commentsEnabled: APPSMITH_FEATURE_CONFIGS.commentsEnabled,
     commentsTestModeEnabled: false,
   };
 };
