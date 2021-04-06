1ab45d84f59d0eb779b257933377c9b61d3b7fc3
Nidhi <nidhi.nair93@gmail.com>
2021-04-06 20:48:19 +0530

    Removed comments

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/annotations/encryption/EncryptionMongoEventListener.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/annotations/encryption/EncryptionMongoEventListener.java
@@ -24,9 +24,6 @@ public class EncryptionMongoEventListener<E> extends AbstractMongoEventListener<
         E source = event.getSource();
 
         encryptionHandler.convertEncryption(source, encryptionService::encryptString);
-
-//        log.debug("onBeforeConvert for {}", event.getDocument());
-//        log.debug("onBeforeConvert for {}", source);
     }
 
     // This lifecycle event is after we retrieve a document from the DB,
@@ -36,13 +33,6 @@ public class EncryptionMongoEventListener<E> extends AbstractMongoEventListener<
         E source = event.getSource();
 
         encryptionHandler.convertEncryption(source, encryptionService::decryptString);
-
-        // At this point the list represents all possible candidate fields
-        // All we need to do is iterate through this list
-        // and encrypt the field if it is an annotation type
-        // or go deeper if it is an appsmith type
-//        log.debug("onAfterConvert for {}", event.getDocument());
-//        log.debug("onAfterConvert for {}", source);
     }
 
 }
