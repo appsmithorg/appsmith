c965eeb01f69b4df10b58f51eec2651ab9fc2d82
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-04-07 16:30:52 +0530

    added findbytext for sync call

--- app/client/src/pages/organization/__tests__/OrganizationSettingsForm.test.tsx
+++ app/client/src/pages/organization/__tests__/OrganizationSettingsForm.test.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { unmountComponentAtNode } from "react-dom";
-import { render } from "test/testUtils";
+import { findByText, render } from "test/testUtils";
 import "@testing-library/jest-dom";
-import { GeneralSettings } from "../../organization/General";
+import { GeneralSettings } from "../General";
 
 let container: any = null;
-describe("Applications", () => {
+describe("Application Settings", () => {
   beforeEach(async () => {
     // setup a DOM element as a render target
     container = document.createElement("div");
     document.body.appendChild(container);
   });
   it("checks that organization settings have correct styling", async (done) => {
-    const { getByText } = render(<GeneralSettings />);
+    const { findByText } = render(<GeneralSettings />);
 
-    const orgNameField = await getByText("Organization Name");
+    const orgNameField = await findByText("Organization Name");
     expect(orgNameField.closest("div")).toHaveStyle({ width: "150px;" });
 
     await done();
