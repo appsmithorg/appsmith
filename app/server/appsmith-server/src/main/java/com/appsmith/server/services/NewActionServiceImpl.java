1e6f451337926abc82e5ed2c56f195d8d29aa281
Sumit Kumar <sumit@appsmith.com>
2021-04-13 19:58:40 +0530

    fix NPE by adding null check conditional (#3989)
    Fix NPE seen on Sentry by adding null check for result body

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/NewActionServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/NewActionServiceImpl.java
@@ -710,6 +710,11 @@ public class NewActionServiceImpl extends BaseService<NewActionRepository, NewAc
             return result;
         }
 
+        if (result.getBody() == null) {
+            result.setDataTypes(new ArrayList<>());
+            return result;
+        }
+
         List<ParsedDataType> parsedDataTypeList = new ArrayList<>();
         Stream.of(ActionResultDataType.values())
                 .parallel()
