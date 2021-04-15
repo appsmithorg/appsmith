c279b54ac879a7ac300d729270e76187b76be200
root <root@DESKTOP-9GENCK0.localdomain>
2021-04-15 10:13:20 +0530

    start testing widget configresponse

--- app/client/src/mockResponses/WidgetConfigResponse.test.tsx
+++ app/client/src/mockResponses/WidgetConfigResponse.test.tsx
@@ -2,6 +2,6 @@ import WIDGET_CONFIG_RESPONSE from "./WidgetConfigResponse";
 
 describe("WidgetConfigResponse", () => {
   it("it checks if config contains list widget", () => {
-    expect(WIDGET_CONFIG_RESPONSE.config).toHaveProperty("LIST_WIDGET");
+    //
   });
 });
