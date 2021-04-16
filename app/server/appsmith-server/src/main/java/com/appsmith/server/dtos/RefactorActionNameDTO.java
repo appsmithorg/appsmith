fafbb92c293917e7372325335745503d9841a1b1
Trisha Anand <trisha@appsmith.com>
2021-04-16 13:41:00 +0530

    New dto for refactoring action name which includes actionId as well

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/RefactorActionNameDTO.java
@@ -0,0 +1,9 @@
+package com.appsmith.server.dtos;
+
+public class RefactorActionNameDTO {
+    String actionId;
+    String pageId;
+    String layoutId;
+    String oldName;
+    String newName;
+}
