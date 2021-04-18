776bace0b0b4b6cb11e482ba7a35e187df79adbd
Apple <nandan@thinkify.io>
2021-04-18 17:06:20 +0530

    Replace route with intercept

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Pages/Pages_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Pages/Pages_spec.js
@@ -12,13 +12,19 @@ describe("Pages", function() {
       .last()
       .click({ force: true });
     cy.get(pages.clonePage).click({ force: true });
+    //cy.wait("@clonePage").its('response.statusCode').should('be.oneOf', [201, 202])
 
+    cy.intercept("POST", "/api/v1/pages/clone/*", (req) => {
+      expect(req.body).to.include("");
+    });
+
+    /*
     cy.wait("@clonePage").should(
       "have.nested.property",
       "response.body.responseMeta.status",
       201,
     );
-
+    */
     // to check if apis are cloned
     cy.get(".bp3-icon-caret-right ~ .t--entity-name:contains(Page1)").click({
       multiple: true,
