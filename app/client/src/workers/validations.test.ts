813c82724fbdde01845c2bb951a54dc3355ae6a6
Akash N <akash@codemonk.in>
2021-04-20 13:50:49 +0530

    Update validation tests

--- app/client/src/workers/validations.test.ts
+++ app/client/src/workers/validations.test.ts
@@ -54,7 +54,8 @@ describe("Validate Validators", () => {
         ],
         output: {
           isValid: false,
-          message: '0##Value does not match type: [{ "x": "val", "y": "val" }]',
+          message:
+            '0##This value does not evaluate to type "Array<x:string, y:number>"',
           parsed: [
             {
               seriesName: "Sales",
@@ -85,7 +86,8 @@ describe("Validate Validators", () => {
         ],
         output: {
           isValid: false,
-          message: '0##Value does not match type: [{ "x": "val", "y": "val" }]',
+          message:
+            '0##This value does not evaluate to type "Array<x:string, y:number>"',
           parsed: [
             {
               seriesName: "Sales",
@@ -158,7 +160,7 @@ describe("Validate Validators", () => {
         parsed: expected[index],
       };
       if (invalidIndices.includes(index)) {
-        expectedResult.message = `Value does not match type: number[]`;
+        expectedResult.message = `This value does not evaluate to type: number[]`;
       }
       expect(result).toStrictEqual(expectedResult);
     });
@@ -444,7 +446,7 @@ describe("Chart Custom Config validator", () => {
         output: {
           isValid: false,
           message:
-            "Value does not match type: {type: string, dataSource: { chart: object, data: Array<{label: string, value: number}>}}",
+            'This value does not evaluate to type "{type: string, dataSource: { chart: object, data: Array<{label: string, value: number}>}}"',
           parsed: {
             type: undefined,
             dataSource: undefined,
