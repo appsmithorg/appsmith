be350ae1d2d171de0d7bbd628f16b76d9f8ff5fe
Sumit Kumar <sumit@appsmith.com>
2021-04-23 18:20:00 +0530

    cleanup

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/DatasourceServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/DatasourceServiceImpl.java
@@ -297,7 +297,7 @@ public class DatasourceServiceImpl extends BaseService<DatasourceRepository, Dat
     }
 
     private Mono<Datasource> validateAndSaveDatasourceToRepository(Datasource datasource) {
-            
+
         Mono<User> currentUserMono = sessionUserService.getCurrentUser();
 
         return Mono.just(datasource)
