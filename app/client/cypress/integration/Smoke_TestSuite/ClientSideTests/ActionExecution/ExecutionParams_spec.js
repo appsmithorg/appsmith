a4b9ef9798ad4b8b98a30b97f0346757035da322
Apple <nandan@thinkify.io>
2021-04-23 20:13:03 +0530

    updated test for executionparam

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/ExecutionParams_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/ExecutionParams_spec.js
@@ -1,11 +1,96 @@
 const dsl = require("../../../../fixtures/executionParamsDsl.json");
 const publishPage = require("../../../../locators/publishWidgetspage.json");
 const commonlocators = require("../../../../locators/commonlocators.json");
+const queryLocators = require("../../../../locators/QueryEditor.json");
+const datasource = require("../../../../locators/DatasourcesEditor.json");
+let datasourceName;
 
 describe("API Panel Test Functionality", function() {
   before(() => {
     cy.addDsl(dsl);
   });
+
+  beforeEach(() => {
+    cy.startRoutesForDatasource();
+  });
+
+  it("Create a postgres datasource", function() {
+    cy.NavigateToDatasourceEditor();
+    cy.get(datasource.PostgreSQL).click();
+
+    cy.getPluginFormsAndCreateDatasource();
+
+    cy.fillPostgresDatasourceForm();
+
+    cy.testSaveDatasource();
+
+    cy.get("@createDatasource").then((httpResponse) => {
+      datasourceName = httpResponse.response.body.data.name;
+    });
+  });
+
+  it("Create and run the query", () => {
+    cy.NavigateToQueryEditor();
+    cy.contains(".t--datasource-name", datasourceName)
+      .find(queryLocators.createQuery)
+      .click();
+
+    cy.get(queryLocators.templateMenu).click();
+    cy.get(".CodeMirror textarea")
+      .first()
+      .focus()
+      .type("SELECT * from {{this.params.tableName || 'users'}} LIMIT 10", {
+        parseSpecialCharSequences: false,
+      });
+    cy.EvaluateCurrentValue("SELECT * from undefined LIMIT 10");
+    cy.runQuery();
+  });
+
+  it("Bind Table and Button with query and validate", () => {
+    cy.SearchEntityandOpen("Table1");
+    cy.testJsontext("tabledata", "{{Query1.data", false);
+    // Assert 'posts' data (default)
+    cy.readTabledataPublish("0", "1").then((cellData) => {
+      expect(cellData).to.be.equal("Ximenez Kainz");
+      // Choose static button
+      cy.SearchEntityandOpen("StaticButton");
+      // toggle js of onClick
+      cy.get(".t--property-control-onclick")
+        .find(".t--js-toggle")
+        .click({ force: true });
+
+      cy.testJsontext(
+        "onclick",
+        "{{Query1.run(undefined, undefined, { tableName: 'orders' })}}",
+        false,
+      );
+      cy.get(commonlocators.editPropCrossButton).click({ force: true });
+      // Publish the app
+      cy.PublishtheApp();
+      cy.wait("@postExecute");
+
+      // Assert on load data in table
+      cy.readTabledataPublish("0", "1").then((cellData) => {
+        expect(cellData).to.be.equal("Ximenez Kainz");
+      });
+
+      cy.get(publishPage.buttonWidget)
+        .first()
+        .click();
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(2000);
+      cy.wait("@postExecute").should(
+        "have.nested.property",
+        "response.body.responseMeta.status",
+        200,
+      );
+      //Assert post button click
+      cy.readTabledataPublish("0", "1").then((cellData) => {
+        expect(cellData).to.be.equal("PAYMENT_PENDING");
+      });
+    });
+
+    /*
   it("Will pass execution params", function() {
     // Create the Api
     cy.NavigateToAPI_Panel();
@@ -97,5 +182,7 @@ describe("API Panel Test Functionality", function() {
     cy.readTabledataPublish("0", "2").then((cellData) => {
       expect(cellData).to.be.equal("delectus aut autem");
     });
+  });
+  */
   });
 });
