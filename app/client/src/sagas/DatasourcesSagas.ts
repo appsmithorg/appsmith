e1659c99262eb674f12435e7ea900d7f10c60e07
Nidhi <nidhi.nair93@gmail.com>
2021-04-26 19:11:57 +0530

    Error handling for unknown sheet when deleting row

--- app/client/src/sagas/DatasourcesSagas.ts
+++ app/client/src/sagas/DatasourcesSagas.ts
@@ -404,7 +404,7 @@ function* testDatasourceSaga(actionPayload: ReduxAction<Datasource>) {
             type: ENTITY_TYPE.DATASOURCE,
           },
           state: {
-            message: responseData.invalids[0],
+            message: responseData.invalids ? responseData.invalids[0] : "",
           },
         });
       } else {
