0b2bd452df3b61bcacc4fc3a8ae56cf00bd8e74f
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2021-04-27 11:04:09 +0530

    Query pane size adjusted as API pane field (#4162)

--- app/client/src/components/formControls/DynamicTextFieldControl.tsx
+++ app/client/src/components/formControls/DynamicTextFieldControl.tsx
@@ -25,6 +25,7 @@ import { actionPathFromName } from "components/formControls/utils";
 import { EvaluationSubstitutionType } from "entities/DataTree/dataTreeFactory";
 
 const Wrapper = styled.div`
+  width: 60%;
   .dynamic-text-field {
     border-radius: 4px;
     font-size: 14px;
