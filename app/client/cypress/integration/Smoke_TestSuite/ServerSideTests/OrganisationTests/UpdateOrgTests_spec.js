7d6dd40beaded30626b9042f4904854454eb58f3
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-05-05 10:57:16 +0530

    update tests according to new workflow

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/OrganisationTests/UpdateOrgTests_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/OrganisationTests/UpdateOrgTests_spec.js
@@ -13,16 +13,8 @@ describe("Update Organization", function() {
       cy.wait("@createOrg").then((interception) => {
         newOrganizationName = interception.response.body.data.name;
         cy.renameOrg(newOrganizationName, orgid);
+        cy.get(homePage.orgSettingOption).click({ force: true });
       });
-      cy.get(homePage.orgList.concat(orgid).concat(")"))
-        .scrollIntoView()
-        .should("be.visible")
-        .within(() => {
-          cy.get(".t--org-name span")
-            .first()
-            .click({ force: true });
-          cy.get(homePage.orgSettingOption).click({ force: true });
-        });
     });
     cy.generateUUID().then((uid) => {
       orgid = uid;
@@ -43,15 +35,12 @@ describe("Update Organization", function() {
   });
 
   it("Open the org general settings and update org email. The update should reflect in the org.", function() {
-    cy.get(homePage.orgList.concat(orgid).concat(")"))
-      .scrollIntoView()
-      .should("be.visible")
-      .within(() => {
-        cy.get(".t--org-name span")
-          .first()
-          .click({ force: true });
-        cy.get(homePage.orgSettingOption).click();
-      });
+    cy.createOrg();
+    cy.wait("@createOrg").then((interception) => {
+      newOrganizationName = interception.response.body.data.name;
+      cy.renameOrg(newOrganizationName, orgid);
+      cy.get(homePage.orgSettingOption).click({ force: true });
+    });
     cy.get(homePage.orgEmailInput).clear();
     cy.get(homePage.orgEmailInput).type(Cypress.env("TESTUSERNAME2"));
     cy.wait("@updateOrganization").should(
