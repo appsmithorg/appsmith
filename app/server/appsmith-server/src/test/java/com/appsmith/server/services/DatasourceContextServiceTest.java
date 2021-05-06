6c0668e74dd800ee49a43a5694b663dfc877e4bb
Nidhi <nidhi.nair93@gmail.com>
2021-05-06 13:36:37 +0530

    Tests for encryption

--- app/server/appsmith-server/src/test/java/com/appsmith/server/services/DatasourceContextServiceTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/DatasourceContextServiceTest.java
@@ -1,18 +1,28 @@
 package com.appsmith.server.services;
 
+import com.appsmith.external.models.DBAuth;
+import com.appsmith.external.models.DatasourceConfiguration;
+import com.appsmith.external.services.EncryptionService;
 import com.appsmith.server.acl.AclPermission;
+import com.appsmith.server.domains.Datasource;
 import com.appsmith.server.domains.Organization;
+import com.appsmith.server.domains.Plugin;
+import com.appsmith.server.helpers.MockPluginExecutor;
 import com.appsmith.server.helpers.PluginExecutorHelper;
 import com.appsmith.server.repositories.OrganizationRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,7 +30,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class DatasourceContextServiceTest {
 
     @Autowired
-    DatasourceContextService datasourceContextService;
+    EncryptionService encryptionService;
 
     @Autowired
     OrganizationRepository organizationRepository;
@@ -34,7 +44,7 @@ public class DatasourceContextServiceTest {
     @MockBean
     PluginExecutorHelper pluginExecutorHelper;
 
-    String orgId =  "";
+    String orgId = "";
 
     @Before
     @WithUserDetails(value = "api_user")
@@ -45,32 +55,81 @@ public class DatasourceContextServiceTest {
 
     @Test
     @WithUserDetails(value = "api_user")
+    public void checkDecryptionOfAuthenticationDTOTest() {
+        Mockito.when(pluginExecutorHelper.getPluginExecutor(Mockito.any())).thenReturn(Mono.just(new MockPluginExecutor()));
+
+        Mono<Plugin> pluginMono = pluginService.findByName("Installed Plugin Name");
+        Datasource datasource = new Datasource();
+        datasource.setName("test datasource name for authenticated fields decryption test");
+        DatasourceConfiguration datasourceConfiguration = new DatasourceConfiguration();
+        datasourceConfiguration.setUrl("http://test.com");
+        DBAuth authenticationDTO = new DBAuth();
+        String username = "username";
+        String password = "password";
+        authenticationDTO.setUsername(username);
+        authenticationDTO.setPassword(password);
+        datasourceConfiguration.setAuthentication(authenticationDTO);
+        datasource.setDatasourceConfiguration(datasourceConfiguration);
+        datasource.setOrganizationId(orgId);
+
+        final Datasource createdDatasource = pluginMono
+                .map(plugin -> {
+                    datasource.setPluginId(plugin.getId());
+                    return datasource;
+                })
+                .flatMap(datasourceService::create)
+                .block();
+
+        assert createdDatasource != null;
+        Mono<Datasource> datasourceMono = datasourceService.findById(createdDatasource.getId());
+
+        StepVerifier
+                .create(datasourceMono)
+                .assertNext(savedDatasource -> {
+                    DBAuth authentication = (DBAuth) savedDatasource.getDatasourceConfiguration().getAuthentication();
+                    Assert.assertEquals(password, authentication.getPassword());
+                    DBAuth encryptedAuthentication = (DBAuth) createdDatasource.getDatasourceConfiguration().getAuthentication();
+                    Assert.assertEquals(encryptionService.encryptString(password), encryptedAuthentication.getPassword());
+                })
+                .verifyComplete();
+    }
+
+
+    @Test
+    @WithUserDetails(value = "api_user")
     public void checkDecryptionOfAuthenticationDTONullPassword() {
-//        Mockito.when(pluginExecutorHelper.getPluginExecutor(Mockito.any())).thenReturn(Mono.just(new MockPluginExecutor()));
-//
-//        Mono<Plugin> pluginMono = pluginService.findByName("Installed Plugin Name");
-//        Datasource datasource = new Datasource();
-//        datasource.setName("test datasource name for authenticated fields decryption test null password");
-//        DatasourceConfiguration datasourceConfiguration = new DatasourceConfiguration();
-//        datasourceConfiguration.setUrl("http://test.com");
-//        DBAuth authenticationDTO = new DBAuth();
-//        datasourceConfiguration.setAuthentication(authenticationDTO);
-//        datasource.setDatasourceConfiguration(datasourceConfiguration);
-//        datasource.setOrganizationId(orgId);
-//
-//        Mono<Datasource> datasourceMono = pluginMono.map(plugin -> {
-//            datasource.setPluginId(plugin.getId());
-//            return datasource;
-//        }).flatMap(datasourceService::create);
-//
-//        StepVerifier
-//                .create(datasourceMono)
-//                .assertNext(savedDatasource -> {
-//                    DBAuth authentication = (DBAuth) savedDatasource.getDatasourceConfiguration().getAuthentication();
-//                    DBAuth decryptedAuthentication = (DBAuth) datasourceContextService.decryptSensitiveFields(authentication);
-//                    assertThat(decryptedAuthentication.getPassword()).isNull();
-//                })
-//                .verifyComplete();
+        Mockito.when(pluginExecutorHelper.getPluginExecutor(Mockito.any())).thenReturn(Mono.just(new MockPluginExecutor()));
+
+        Mono<Plugin> pluginMono = pluginService.findByName("Installed Plugin Name");
+        Datasource datasource = new Datasource();
+        datasource.setName("test datasource name for authenticated fields decryption test null password");
+        DatasourceConfiguration datasourceConfiguration = new DatasourceConfiguration();
+        datasourceConfiguration.setUrl("http://test.com");
+        DBAuth authenticationDTO = new DBAuth();
+        datasourceConfiguration.setAuthentication(authenticationDTO);
+        datasource.setDatasourceConfiguration(datasourceConfiguration);
+        datasource.setOrganizationId(orgId);
+
+        final Datasource createdDatasource = pluginMono
+                .map(plugin -> {
+                    datasource.setPluginId(plugin.getId());
+                    return datasource;
+                })
+                .flatMap(datasourceService::create)
+                .block();
+
+        assert createdDatasource != null;
+        Mono<Datasource> datasourceMono = datasourceService.findById(createdDatasource.getId());
+
+        StepVerifier
+                .create(datasourceMono)
+                .assertNext(savedDatasource -> {
+                    DBAuth authentication = (DBAuth) savedDatasource.getDatasourceConfiguration().getAuthentication();
+                    Assert.assertNull(authentication.getPassword());
+                    DBAuth encryptedAuthentication = (DBAuth) createdDatasource.getDatasourceConfiguration().getAuthentication();
+                    Assert.assertNull(encryptedAuthentication.getPassword());
+                })
+                .verifyComplete();
     }
 
 }
