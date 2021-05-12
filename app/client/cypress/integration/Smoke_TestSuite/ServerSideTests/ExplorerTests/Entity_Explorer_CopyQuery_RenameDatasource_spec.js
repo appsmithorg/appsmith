a1acccdcc95590a0cd9e254c012a88cc173317d1
arunvjn <arun@appsmith.com>
2021-05-12 17:17:53 +0530

    Modify cypress test case

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/ExplorerTests/Entity_Explorer_CopyQuery_RenameDatasource_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/ExplorerTests/Entity_Explorer_CopyQuery_RenameDatasource_spec.js
@@ -57,8 +57,8 @@ describe("Entity explorer tests related to copy query", function() {
         expect($lis).to.have.length(4);
         expect($lis.eq(0)).to.contain("{{Query1.isLoading}}");
         expect($lis.eq(1)).to.contain("{{Query1.data}}");
-        expect($lis.eq(2)).to.contain("{{Query1.run()}}");
-        expect($lis.eq(3)).to.contain("{{Query1.responseMeta}}");
+        expect($lis.eq(2)).to.contain("{{Query1.responseMeta}}");
+        expect($lis.eq(3)).to.contain("{{Query1.run()}}");
       });
     });
   });
@@ -81,8 +81,8 @@ describe("Entity explorer tests related to copy query", function() {
       expect($lis).to.have.length(4);
       expect($lis.eq(0)).to.contain("{{Query1Copy.isLoading}}");
       expect($lis.eq(1)).to.contain("{{Query1Copy.data}}");
-      expect($lis.eq(2)).to.contain("{{Query1Copy.run()}}");
-      expect($lis.eq(3)).to.contain("{{Query1.responseMeta}}");
+      expect($lis.eq(2)).to.contain("{{Query1Copy.responseMeta}}");
+      expect($lis.eq(3)).to.contain("{{Query1Copy.run()}}");
     });
   });
 
