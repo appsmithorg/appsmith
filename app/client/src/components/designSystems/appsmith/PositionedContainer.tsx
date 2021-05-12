1d3c2da455d0a58beb4189898464baf5096c1aee
Paul Li <wmdev0808@gmail.com>
2021-05-12 06:35:40 -0400

    FEATURE-2111 : Show/Hide Table Controls
    -- PositionedContainer : Add padding-bottom: 42px in case of Table widget

--- app/client/src/components/designSystems/appsmith/PositionedContainer.tsx
+++ app/client/src/components/designSystems/appsmith/PositionedContainer.tsx
@@ -1,6 +1,6 @@
 import React, { CSSProperties, ReactNode, useMemo } from "react";
 import { BaseStyle } from "widgets/BaseWidget";
-import { WIDGET_PADDING } from "constants/WidgetConstants";
+import { WidgetTypes, WIDGET_PADDING } from "constants/WidgetConstants";
 import { generateClassName } from "utils/generators";
 import styled from "styled-components";
 import { useClickOpenPropPane } from "utils/hooks/useClickOpenPropPane";
@@ -41,7 +41,10 @@ export function PositionedContainer(props: PositionedContainerProps) {
       top: y,
       height: props.style.componentHeight + (props.style.heightUnit || "px"),
       width: props.style.componentWidth + (props.style.widthUnit || "px"),
-      padding: padding + "px",
+      padding:
+        props.widgetType === WidgetTypes.TABLE_WIDGET
+          ? `${padding}px ${padding}px 42px`
+          : padding + "px",
     };
   }, [props.style]);
 
