2b7212da8c8714f80c8ef881f4acb2d24da1639a
Trisha Anand <trisha@appsmith.com>
2021-05-13 19:52:54 +0530

    Init minor comment

--- app/client/src/workers/DataTreeEvaluator.ts
+++ app/client/src/workers/DataTreeEvaluator.ts
@@ -417,6 +417,7 @@ export default class DataTreeEvaluator {
           }
 
           if (isWidget(entity)) {
+            // In case the widget has a scope, handle replacement of the variable name with the values from the scope.
             const widgetEntity = entity;
             const defaultPropertyMap = this.widgetConfigMap[widgetEntity.type]
               .defaultProperties;
