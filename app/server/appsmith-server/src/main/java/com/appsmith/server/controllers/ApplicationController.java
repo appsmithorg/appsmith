f1876b9ced58bd9d0eab57c016f089b4062a2496
Abhijeet <41686026+abhvsn@users.noreply.github.com>
2021-05-21 18:18:52 +0530

    logging for application controller modified
    Co-authored-by: Shrikant Sharat Kandula <shrikant@appsmith.com>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ApplicationController.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ApplicationController.java
@@ -135,7 +135,7 @@ public class ApplicationController extends BaseController<ApplicationService, Ap
     @PostMapping(value = "/import/{orgId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Mono<ResponseDTO<Application>> importApplicationFromFile(
             @RequestPart("file") Mono<Part> fileMono, @PathVariable String orgId) {
-        log.debug("Going to export import application in organization with id: {}", orgId);
+        log.debug("Going to import application in organization with id: {}", orgId);
         return fileMono
                 .flatMap(file -> importExportApplicationService.extractFileAndSaveApplication(orgId, file))
                 .map(fetchedResource -> new ResponseDTO<>(HttpStatus.OK.value(), fetchedResource, null));
