0e3ef7f238887bcf435b7c6e44f631efcead73cc
Nidhi <nidhi@appsmith.com>
2021-05-31 23:39:58 +0530

    Update app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/helpers/DatasourceValidator.java
    Co-authored-by: Shrikant Sharat Kandula <shrikant@appsmith.com>

--- app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/helpers/DatasourceValidator.java
+++ app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/helpers/DatasourceValidator.java
@@ -12,9 +12,8 @@ import java.util.regex.Pattern;
 public class DatasourceValidator {
 
     private static final String URL_REGEX =
-            "^(https?://" +
-                    "(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)" +
-                    "([).!';/?:,][[:blank:]])?$";
+            "^https?://" +
+                    "(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+$"
 
     private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
 
