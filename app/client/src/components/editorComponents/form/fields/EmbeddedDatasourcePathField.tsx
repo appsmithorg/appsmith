afb0522f2eacde9693bd886f04cfe7a98aa729e8
arunvjn <arun@appsmith.com>
2021-06-02 15:40:35 +0530

    Hide save/edit datasource button when url field is empty

--- app/client/src/components/editorComponents/form/fields/EmbeddedDatasourcePathField.tsx
+++ app/client/src/components/editorComponents/form/fields/EmbeddedDatasourcePathField.tsx
@@ -298,7 +298,7 @@ class EmbeddedDatasourcePathComponent extends React.Component<Props> {
     return (
       <DatasourceContainer>
         <CodeEditor {...props} />
-        {datasource && !("id" in datasource) ? (
+        {displayValue && datasource && !("id" in datasource) ? (
           <StoreAsDatasource enable={!!displayValue} />
         ) : datasource && "id" in datasource ? (
           <DatasourceIcon
