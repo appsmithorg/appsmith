f35355cd719f7c0c3f71202e8b432476fc7c7711
Abhijeet <abhi.nagarnaik@gmail.com>
2021-06-03 18:36:30 +0530

    WIP

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/CreateDBTablePageSolution.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/CreateDBTablePageSolution.java
@@ -1,33 +1,47 @@
 package com.appsmith.server.solutions;
 
+import com.appsmith.external.models.ActionConfiguration;
 import com.appsmith.external.models.DatasourceStructure;
 import com.appsmith.external.models.DatasourceStructure.Column;
 import com.appsmith.external.models.DatasourceStructure.Table;
+import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.constants.FieldName;
 import com.appsmith.server.domains.ApplicationPage;
+import com.appsmith.server.domains.Datasource;
 import com.appsmith.server.domains.NewPage;
+import com.appsmith.server.dtos.ActionDTO;
 import com.appsmith.server.dtos.PageDTO;
 import com.appsmith.server.exceptions.AppsmithError;
 import com.appsmith.server.exceptions.AppsmithException;
 import com.appsmith.server.services.ApplicationPageService;
 import com.appsmith.server.services.ApplicationService;
 import com.appsmith.server.services.ConfigService;
+import com.appsmith.server.services.DatasourceService;
+import com.appsmith.server.services.LayoutActionService;
 import com.appsmith.server.services.NewPageService;
-import lombok.RequiredArgsConstructor;
+import com.appsmith.server.services.PluginService;
+import com.sun.mail.imap.ACL;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.security.acl.Acl;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Slf4j
-public class CreateDBTablePageSolution {
+public class    CreateDBTablePageSolution {
     
     private DatasourceStructureSolution datasourceStructureSolution;
+    
+    private DatasourceService datasourceService;
+    
+    private PluginService pluginService;
     
     private NewPageService newPageService;
     
@@ -37,13 +51,19 @@ public class CreateDBTablePageSolution {
     
     private ConfigService configService;
     
+    private LayoutActionService layoutActionService;
+    
     private String templateDBName = "mock-DB-to-create-CRUD-page";
     
     private String templateApplicationName = "template-CRUD-DB-table-app";
     
-    private static String DATABASE_TABLE = "database table";
+    private final String DATABASE_TABLE = "database table";
     
-    public PageDTO createPageFromDBTable(String appId, String datasourceId, String tableName) {
+    private enum DBActionType {
+        CREATE, READ, SELECT, UPDATE, DELETE
+    }
+    
+    public PageDTO createPageFromDBTable(String appId, String pageId, String datasourceId, String tableName) {
         
         Mono<DatasourceStructure> templateDSStructureMono = configService.getTemplateDatasources()
             .filter(datasource -> StringUtils.equals(datasource.getName(), templateDBName))
@@ -65,41 +85,98 @@ public class CreateDBTablePageSolution {
                 
                 return newPageService.getById(defaultAppPage.getId());
             });
-    
+        
         return datasourceStructureSolution.getStructure(datasourceId, true)
             .switchIfEmpty(Mono.error(new AppsmithException(
-                AppsmithError.ACL_NO_RESOURCE_FOUND, FieldName.DATASOURCE, datasourceId))
+                AppsmithError.ACL_NO_RESOURCE_FOUND, FieldName.DATASOURCE_STRUCTURE, datasourceId))
             )
-            .flatMap(datasourceStructure -> {
-                Table table = datasourceStructure.getTables()
+            .zipWith(datasourceService.findById(datasourceId, AclPermission.MANAGE_DATASOURCES)
+                .switchIfEmpty(Mono.error(new AppsmithException(
+                    AppsmithError.ACL_NO_RESOURCE_FOUND, FieldName.DATASOURCE, datasourceId))
+                )
+            )
+            .flatMap(tuple -> {
+                DatasourceStructure datasourceStructure = tuple.getT1();
+                Datasource currDatasource = tuple.getT2();
+                
+                Table currTable = datasourceStructure.getTables()
                     .stream()
                     .filter(t -> StringUtils.equals(tableName, t.getName()))
                     .findFirst()
                     .orElse(null);
                 
-                if (table == null) {
+                if (currTable == null) {
                     return Mono.error(new AppsmithException(AppsmithError.ACL_NO_RESOURCE_FOUND, DATABASE_TABLE, tableName));
                 }
                 return Mono.zip(
-                    Mono.just(table),
+                    Mono.just(currTable),
                     templateDSStructureMono,
                     templatePageMono
                 );
             })
             .map(tuple -> {
-                Table table = tuple.getT1();
+                Table currTable = tuple.getT1();
                 //We are expecting only one table will be present
                 Table templateTable = tuple.getT2().getTables().get(0);
                 NewPage templatePage = tuple.getT3();
-                Map<String, String> mappedColumns = mapTableColumnNames(templateTable, table);
-                
+                Map<String, Column> templateTableNameToColumnMap = new HashMap<>();
+                Map<String, Column> currTableNameToColumnMap = new HashMap<>();
+                List<Map<String, Column>> columnMapList = List.of(templateTableNameToColumnMap, currTableNameToColumnMap);
+                Map<String, String> mappedColumns = mapTableColumnNames(templateTable, currTable, columnMapList);
+                cloneAction()
             });
     }
     
-    private Map<String, String> mapTableColumnNames(Table sourceTable, Table destTable) {
+    private Mono<ActionDTO> cloneAction(Datasource datasource, String pageId,
+                                        ActionDTO templateAction, DBActionType actionType) {
         
-        Map<Column, Column> mappedTableColumns = new HashMap<>();
+        ActionDTO action = templateAction;
+        action.setDatasource(datasource);
+        action.setPageId(pageId);
         
+        datasource.getStructure().getTables().get(0).getTemplates()
+        
+        return pluginService.findById(datasource.getPluginId())
+            .map(plugin -> {
+                String pluginPackageName = plugin.getPackageName();
+                
+            })
+        
+    }
+    
+    private ActionConfiguration createActionConfig
+    
+    private Map<String, String> mapTableColumnNames(Table sourceTable, Table destTable, List<Map<String, Column>> columnMapList) {
+        Map<String, String> mappedTableColumns = new HashMap<>();
+        Map<String, Column> templateColumnMap = columnMapList.get(0);
+        Map<String, Column> currTableColumnMap = columnMapList.get(1);
+        List<Column> sourceTableColumns = sourceTable.getColumns(), destTableColumns = destTable.getColumns();
+        
+        int idx = 0;
+        while (idx < sourceTableColumns.size() && idx < destTableColumns.size()) {
+            
+            templateColumnMap.put(sourceTableColumns.get(idx).getName(), sourceTableColumns.get(idx));
+            currTableColumnMap.put(destTableColumns.get(idx).getName(), destTableColumns.get(idx));
+            mappedTableColumns.put(sourceTableColumns.get(idx).getName(), destTableColumns.get(idx).getName());
+            idx++;
+        }
+        
+        if (idx < destTableColumns.size()) {
+            while (idx < destTableColumns.size()) {
+                currTableColumnMap.put(destTableColumns.get(idx).getName(), destTableColumns.get(idx));
+                idx++;
+            }
+        }
         return mappedTableColumns;
     }
 }
+
+
+/*
+String widgetType = dsl.getAsString(FieldName.WIDGET_TYPE);
+
+// Only Table widget has this behaviour.
+if (widgetType != null && widgetType.equals(FieldName.TABLE_WIDGET)) {
+    return WidgetSpecificUtils.escapeTableWidgetPrimaryColumns(dsl, escapedWidgetNames);
+}
+ */
