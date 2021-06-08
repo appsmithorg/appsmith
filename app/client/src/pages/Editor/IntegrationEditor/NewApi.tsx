bd45e42875886e95fcecd679823136e9ae4b27e9
Pranav Kanade <pranav@appsmith.com>
2021-06-08 18:10:44 +0530

    updated the styling for new api page

--- app/client/src/pages/Editor/IntegrationEditor/NewApi.tsx
+++ app/client/src/pages/Editor/IntegrationEditor/NewApi.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { Icon, Card } from "@blueprintjs/core";
+import { Icon } from "@blueprintjs/core";
 import styled from "styled-components";
 import { getCurlImportPageURL } from "constants/routes";
 import { SAAS_EDITOR_URL } from "pages/Editor/SaaSEditor/constants";
@@ -14,23 +13,14 @@ import { createNewApiAction } from "actions/apiPaneActions";
 import AnalyticsUtil, { EventLocation } from "utils/AnalyticsUtil";
 import { CURL } from "constants/AppsmithActionConstants/ActionConstants";
 import { PluginType } from "entities/Action";
+import Button, { Category, Size } from "components/ads/Button";
 
 const StyledContainer = styled.div`
   flex: 1;
-
-  .sectionHeadings {
-    font-weight: 500;
-    font-size: 16px;
-  }
-  .link {
-    color: ${Colors.OXFORD_BLUE};
-  }
-  .link:hover {
-    color: ${Colors.OXFORD_BLUE};
-    text-decoration: none !important;
-  }
   .textBtn {
-    font-size: 14px;
+    font-size: 16px;
+    line-height: 24px;
+    margin: 0;
     justify-content: center;
     text-align: center;
     letter-spacing: -0.17px;
@@ -46,69 +36,6 @@ const StyledContainer = styled.div`
     .textBtn {
       font-size: 18px;
     }
-  }
-  .link {
-    color: ${Colors.OXFORD_BLUE};
-  }
-  a:hover {
-    color: ${Colors.OXFORD_BLUE};
-    text-decoration: none;
-  }
-  .providerSearchCard {
-    display: flex;
-    color: ${Colors.BLACK};
-    padding: 5px;
-    cursor: pointer;
-  }
-  .providerSearchCard:hover {
-    background-color: ${Colors.CONCRETE};
-  }
-  .providerSearchResultImage {
-    height: 50px;
-    width: 60px;
-    object-fit: contain;
-  }
-  .providerSearchResultName {
-    display: flex;
-    align-self: center;
-    padding-left: 15px;
-    font-size: 16px;
-  }
-  .apiImage {
-    object-fit: contain;
-    height: 50px;
-    width: auto;
-    max-width: 100%;
-    margin-top: -5px;
-    margin-bottom: 10px;
-    min-height: 50px;
-  }
-  .curlImage {
-    width: 55px;
-  }
-  .saasImage.t--saas-google-sheets-plugin-image {
-    width: 40px;
-  }
-  .createIcon {
-    align-items: center;
-    margin-top: 15px;
-    margin-bottom: 25px;
-  }
-  .eachCard {
-    margin: 15px;
-    width: 140px;
-    height: 110px;
-    padding-bottom: 0px;
-    cursor: pointer;
-    border: 1px solid #e6e6e6;
-    box-shadow: none;
-  }
-  .eachCard:active {
-    border: 1px solid ${Colors.JAFFA_DARK};
-    background: rgba(242, 153, 74, 0.17);
-  }
-  .eachCard:hover {
-    border: 1px solid ${Colors.JAFFA_DARK};
   }
   @media (min-width: 2500px) {
     .eachCard {
@@ -129,20 +56,66 @@ const StyledContainer = styled.div`
   }
 `;
 
-const ApiCard = styled.div`
-  flex: 1;
+const ApiCardsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-  column-gap: 10px;
-  flex-wrap: wrap;
-  margin-left: -10px;
+  grid-template-columns: 1fr 1fr;
+  gap: 10px;
   text-align: center;
   min-width: 150px;
   border-radius: 4px;
-  width: 100%;
-  @media (min-width: 2500px) {
-    grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+  align-items: center;
+
+  .create-new-api {
+    &:hover {
+      cursor: pointer;
+    }
   }
+`;
+
+const ApiCard = styled.div`
+  border: 1px solid transparent;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 70px;
+  padding-left: 8px;
+  &:hover {
+    border: 1px solid #a9a7a7;
+  }
+
+  .content-icon-wrapper {
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    margin: 0 8px;
+    background: #f0f0f0;
+    display: flex;
+    align-items: center;
+
+    .content-icon {
+      height: 28px;
+      width: auto;
+      margin: 0 auto;
+      max-width: 100%;
+      margin-bottom: 2px;
+    }
+  }
+
+  .cta {
+    display: none;
+    margin-right: 32px;
+  }
+
+  &:hover {
+    .cta {
+      display: flex;
+    }
+  }
+`;
+
+const CardContentWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 type ApiHomeScreenProps = {
@@ -191,49 +164,75 @@ const newApiScreen = (props: Props) => {
 
   return (
     <StyledContainer>
-      <ApiCard>
-        <Card
-          className="eachCard t--createBlankApiCard"
-          interactive={false}
+      <ApiCardsContainer>
+        <ApiCard
+          className="t--createBlankApiCard create-new-api"
           onClick={handleCreateNew}
         >
-          <Icon className="createIcon" icon="plus" iconSize={20} />
-          <p className="textBtn">Create new</p>
-        </Card>
-        <Link
-          onClick={() => {
-            AnalyticsUtil.logEvent("IMPORT_API_CLICK", {
-              importSource: CURL,
-            });
-          }}
-          to={curlImportURL}
-        >
-          <Card className="eachCard" interactive={false}>
-            <img alt="CURL" className="curlImage t--curlImage" src={CurlLogo} />
+          <CardContentWrapper>
+            <div className="content-icon-wrapper">
+              <Icon className="content-icon" icon="plus" iconSize={28} />
+            </div>
+            <p className="textBtn">Create new</p>
+          </CardContentWrapper>
+        </ApiCard>
+        <ApiCard className="t--createBlankApiCard">
+          <CardContentWrapper>
+            <div className="content-icon-wrapper">
+              <img
+                alt="CURL"
+                className="curlImage t--curlImage content-icon"
+                src={CurlLogo}
+              />
+            </div>
             <p className="textBtn">CURL</p>
-          </Card>
-        </Link>
+          </CardContentWrapper>
+          <Button
+            category={Category.tertiary}
+            className="t--connect-to-btn cta"
+            href={curlImportURL}
+            onClick={() => {
+              AnalyticsUtil.logEvent("IMPORT_API_CLICK", {
+                importSource: CURL,
+              });
+            }}
+            size={Size.medium}
+            tag="a"
+            text="Create"
+          />
+        </ApiCard>
         {plugins
           .filter((p) => p.type === PluginType.SAAS)
           .map((p) => (
-            <Link
-              key={p.id}
-              to={
-                SAAS_EDITOR_URL(applicationId, pageId, p.packageName) +
-                location.search
-              }
-            >
-              <Card className="eachCard" interactive={false}>
-                <img
-                  alt={p.name}
-                  className={"saasImage t--saas-" + p.packageName + "-image"}
-                  src={p.iconLocation}
-                />
+            <ApiCard className="t--createBlankApiCard" key={p.id}>
+              <CardContentWrapper>
+                <div className="content-icon-wrapper">
+                  <img
+                    alt={p.name}
+                    className={
+                      "content-icon saasImage t--saas-" +
+                      p.packageName +
+                      "-image"
+                    }
+                    src={p.iconLocation}
+                  />
+                </div>
                 <p className="textBtn">{p.name}</p>
-              </Card>
-            </Link>
+              </CardContentWrapper>
+              <Button
+                category={Category.tertiary}
+                className="t--connect-to-btn cta"
+                href={
+                  SAAS_EDITOR_URL(applicationId, pageId, p.packageName) +
+                  location.search
+                }
+                size={Size.medium}
+                tag="a"
+                text="Connect"
+              />
+            </ApiCard>
           ))}
-      </ApiCard>
+      </ApiCardsContainer>
     </StyledContainer>
   );
 };
