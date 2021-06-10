206f637a41bd52ae3a334f813123d631385cfd23
root <root@DESKTOP-9GENCK0.localdomain>
2021-06-10 15:43:33 +0530

    add test for selectedItem

--- app/client/src/widgets/ListWidget/derived.test.js
+++ app/client/src/widgets/ListWidget/derived.test.js
@@ -3,7 +3,20 @@ import moment from "moment";
 import _ from "lodash";
 
 describe("Validates Derived Properties", () => {
-  it("validates items properties", () => {
+  it("validates selectedItem property", () => {
+    const { getSelectedItem } = derivedProperty;
+    const input = {
+      listData: [{ id: 1 }, { id: 2 }],
+      selectedItemIndex: 1,
+    };
+
+    const expected = { id: 2 };
+
+    let result = getSelectedItem(input, moment, _);
+    expect(result).toStrictEqual(expected);
+  });
+
+  it("validates items property", () => {
     const { getItems } = derivedProperty;
     const input = {
       listData: [1, 2],
