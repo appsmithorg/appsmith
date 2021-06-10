3f4b8ddb86157ea51d0907b1628b984f29b013fb
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-06-10 17:08:08 +0530

    Init tests

---
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/Bind_API_with_Text_Widget_spec.js
@@ -0,0 +1,25 @@
+const commonlocators = require("../../../../locators/commonlocators.json");
+const dsl = require("../../../../fixtures/textAPIBindingdsl.json");
+
+describe("Test Create Api and Bind to Text widget", function() {
+  let apiData;
+  before(() => {
+    cy.addDsl(dsl);
+  });
+
+  it("Test_Add users api and execute api", function() {
+    cy.createAndFillApi(this.data.userApi, "/users");
+    cy.RunAPI();
+    cy.get(apiPage.responseBody)
+      .contains("name")
+      .siblings("span")
+      .invoke("text")
+      .then((text) => {
+        const value = text.match(/"(.*)"/)[0];
+        cy.log(value);
+
+        apiData = value;
+        cy.log("val1:" + value);
+      });
+  });
+});
