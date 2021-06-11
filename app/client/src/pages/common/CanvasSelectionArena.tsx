6110848f606998f64697dc22afd54df05895904f
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2021-06-11 15:51:36 +0530

    restrict in deploy mode.

--- app/client/src/pages/common/CanvasSelectionArena.tsx
+++ app/client/src/pages/common/CanvasSelectionArena.tsx
@@ -7,6 +7,7 @@ import { throttle } from "lodash";
 import React, { memo, useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppState } from "reducers";
+import { APP_MODE } from "reducers/entityReducers/appReducer";
 import { getWidget } from "sagas/selectors";
 import { getAppMode } from "selectors/applicationSelectors";
 import {
@@ -67,161 +68,165 @@ export const CanvasSelectionArena = memo(
       [widgetId],
     );
     useEffect(() => {
-      const selectionCanvas: any = document.getElementById(
-        `canvas-${widgetId}`,
-      );
-      const { height, width } = selectionCanvas.getBoundingClientRect();
-      selectionCanvas.width = width;
-      selectionCanvas.height = height;
-      const canvasCtx = selectionCanvas.getContext("2d");
-      const initRectangle = (): SelectedArenaDimensions => ({
-        top: 0,
-        left: 0,
-        width: 0,
-        height: 0,
-      });
-      let selectionRectangle: SelectedArenaDimensions = initRectangle();
-      let isMultiSelect = false;
-      let isDragging = false;
+      if (appMode === APP_MODE.EDIT) {
+        const selectionCanvas: any = document.getElementById(
+          `canvas-${widgetId}`,
+        );
+        const { height, width } = selectionCanvas.getBoundingClientRect();
+        selectionCanvas.width = width;
+        selectionCanvas.height = height;
+        const canvasCtx = selectionCanvas.getContext("2d");
+        const initRectangle = (): SelectedArenaDimensions => ({
+          top: 0,
+          left: 0,
+          width: 0,
+          height: 0,
+        });
+        let selectionRectangle: SelectedArenaDimensions = initRectangle();
+        let isMultiSelect = false;
+        let isDragging = false;
 
-      const init = () => {
-        selectionCanvas.addEventListener("click", onClick, false);
-        selectionCanvas.addEventListener("mousedown", onMouseDown, false);
-        selectionCanvas.addEventListener("mouseup", onMouseUp, false);
-        selectionCanvas.addEventListener("mousemove", onMouseMove, false);
-        selectionCanvas.addEventListener("mouseleave", onMouseLeave, false);
-        selectionCanvas.addEventListener("mouseenter", onMouseEnter, false);
-      };
-
-      const getSelectionDimensions = () => {
-        return {
-          top:
-            selectionRectangle.height < 0
-              ? selectionRectangle.top - Math.abs(selectionRectangle.height)
-              : selectionRectangle.top,
-          left:
-            selectionRectangle.width < 0
-              ? selectionRectangle.left - Math.abs(selectionRectangle.width)
-              : selectionRectangle.left,
-          width: Math.abs(selectionRectangle.width),
-          height: Math.abs(selectionRectangle.height),
+        const init = () => {
+          selectionCanvas.addEventListener("click", onClick, false);
+          selectionCanvas.addEventListener("mousedown", onMouseDown, false);
+          selectionCanvas.addEventListener("mouseup", onMouseUp, false);
+          selectionCanvas.addEventListener("mousemove", onMouseMove, false);
+          selectionCanvas.addEventListener("mouseleave", onMouseLeave, false);
+          selectionCanvas.addEventListener("mouseenter", onMouseEnter, false);
         };
-      };
 
-      const selectWidgetsInit = (
-        selectionDimensions: SelectedArenaDimensions,
-        isMultiSelect: boolean,
-      ) => {
-        if (
-          selectionDimensions.left &&
-          selectionDimensions.top &&
-          selectionDimensions.width &&
-          selectionDimensions.height
-        ) {
-          const snapToNextColumn = selectionRectangle.height < 0;
-          const snapToNextRow = selectionRectangle.width < 0;
-          throttledWidgetSelection(
-            selectionDimensions,
-            snapToNextColumn,
-            snapToNextRow,
-            isMultiSelect,
-          );
-        }
-      };
+        const getSelectionDimensions = () => {
+          return {
+            top:
+              selectionRectangle.height < 0
+                ? selectionRectangle.top - Math.abs(selectionRectangle.height)
+                : selectionRectangle.top,
+            left:
+              selectionRectangle.width < 0
+                ? selectionRectangle.left - Math.abs(selectionRectangle.width)
+                : selectionRectangle.left,
+            width: Math.abs(selectionRectangle.width),
+            height: Math.abs(selectionRectangle.height),
+          };
+        };
 
-      const drawRectangle = (selectionDimensions: SelectedArenaDimensions) => {
-        const strokeWidth = 1;
-        canvasCtx.setLineDash([5]);
-        canvasCtx.strokeStyle = "rgb(84, 132, 236)";
-        canvasCtx.strokeRect(
-          selectionDimensions.left - strokeWidth,
-          selectionDimensions.top - strokeWidth,
-          selectionDimensions.width + 2 * strokeWidth,
-          selectionDimensions.height + 2 * strokeWidth,
-        );
-        canvasCtx.fillStyle = "rgb(84, 132, 236, 0.06)";
-        canvasCtx.fillRect(
-          selectionDimensions.left,
-          selectionDimensions.top,
-          selectionDimensions.width,
-          selectionDimensions.height,
-        );
-      };
-
-      const onMouseLeave = () => {
-        document.body.addEventListener("mouseup", onMouseUp, false);
-      };
-
-      const onMouseEnter = () => {
-        document.body.removeEventListener("mouseup", onMouseUp);
-      };
-
-      const onClick = (e: any) => {
-        if (
-          Math.abs(selectionRectangle.height) +
-            Math.abs(selectionRectangle.width) >
-          0
-        ) {
-          if (!isDragging) {
-            // cant set this in onMouseUp coz click seems to happen after onMouseUp.
-            selectionRectangle = initRectangle();
+        const selectWidgetsInit = (
+          selectionDimensions: SelectedArenaDimensions,
+          isMultiSelect: boolean,
+        ) => {
+          if (
+            selectionDimensions.left &&
+            selectionDimensions.top &&
+            selectionDimensions.width &&
+            selectionDimensions.height
+          ) {
+            const snapToNextColumn = selectionRectangle.height < 0;
+            const snapToNextRow = selectionRectangle.width < 0;
+            throttledWidgetSelection(
+              selectionDimensions,
+              snapToNextColumn,
+              snapToNextRow,
+              isMultiSelect,
+            );
           }
-          e.stopPropagation();
-        }
-      };
+        };
 
-      const onMouseDown = (e: any) => {
-        isMultiSelect = e.ctrlKey || e.metaKey || e.shiftKey;
-        selectionRectangle.left = e.offsetX - selectionCanvas.offsetLeft;
-        selectionRectangle.top = e.offsetY - selectionCanvas.offsetTop;
-        selectionRectangle.width = 0;
-        selectionRectangle.height = 0;
-        isDragging = true;
-        dispatch(setCanvasSelectionStateAction(true));
-        // bring the canvas to the top layer
-        selectionCanvas.style.zIndex = 2;
-      };
-      const onMouseUp = () => {
-        if (isDragging) {
-          isDragging = false;
-          canvasCtx.clearRect(
-            0,
-            0,
-            selectionCanvas.width,
-            selectionCanvas.height,
+        const drawRectangle = (
+          selectionDimensions: SelectedArenaDimensions,
+        ) => {
+          const strokeWidth = 1;
+          canvasCtx.setLineDash([5]);
+          canvasCtx.strokeStyle = "rgb(84, 132, 236)";
+          canvasCtx.strokeRect(
+            selectionDimensions.left - strokeWidth,
+            selectionDimensions.top - strokeWidth,
+            selectionDimensions.width + 2 * strokeWidth,
+            selectionDimensions.height + 2 * strokeWidth,
           );
-          selectionCanvas.style.zIndex = null;
-          dispatch(setCanvasSelectionStateAction(false));
-        }
-      };
-      const onMouseMove = (e: any) => {
-        if (isDragging) {
-          selectionRectangle.width =
-            e.offsetX - selectionCanvas.offsetLeft - selectionRectangle.left;
-          selectionRectangle.height =
-            e.offsetY - selectionCanvas.offsetTop - selectionRectangle.top;
-          canvasCtx.clearRect(
-            0,
-            0,
-            selectionCanvas.width,
-            selectionCanvas.height,
+          canvasCtx.fillStyle = "rgb(84, 132, 236, 0.06)";
+          canvasCtx.fillRect(
+            selectionDimensions.left,
+            selectionDimensions.top,
+            selectionDimensions.width,
+            selectionDimensions.height,
           );
-          const selectionDimensions = getSelectionDimensions();
-          drawRectangle(selectionDimensions);
-          selectWidgetsInit(selectionDimensions, isMultiSelect);
+        };
+
+        const onMouseLeave = () => {
+          document.body.addEventListener("mouseup", onMouseUp, false);
+        };
+
+        const onMouseEnter = () => {
+          document.body.removeEventListener("mouseup", onMouseUp);
+        };
+
+        const onClick = (e: any) => {
+          if (
+            Math.abs(selectionRectangle.height) +
+              Math.abs(selectionRectangle.width) >
+            0
+          ) {
+            if (!isDragging) {
+              // cant set this in onMouseUp coz click seems to happen after onMouseUp.
+              selectionRectangle = initRectangle();
+            }
+            e.stopPropagation();
+          }
+        };
+
+        const onMouseDown = (e: any) => {
+          isMultiSelect = e.ctrlKey || e.metaKey || e.shiftKey;
+          selectionRectangle.left = e.offsetX - selectionCanvas.offsetLeft;
+          selectionRectangle.top = e.offsetY - selectionCanvas.offsetTop;
+          selectionRectangle.width = 0;
+          selectionRectangle.height = 0;
+          isDragging = true;
+          dispatch(setCanvasSelectionStateAction(true));
+          // bring the canvas to the top layer
+          selectionCanvas.style.zIndex = 2;
+        };
+        const onMouseUp = () => {
+          if (isDragging) {
+            isDragging = false;
+            canvasCtx.clearRect(
+              0,
+              0,
+              selectionCanvas.width,
+              selectionCanvas.height,
+            );
+            selectionCanvas.style.zIndex = null;
+            dispatch(setCanvasSelectionStateAction(false));
+          }
+        };
+        const onMouseMove = (e: any) => {
+          if (isDragging) {
+            selectionRectangle.width =
+              e.offsetX - selectionCanvas.offsetLeft - selectionRectangle.left;
+            selectionRectangle.height =
+              e.offsetY - selectionCanvas.offsetTop - selectionRectangle.top;
+            canvasCtx.clearRect(
+              0,
+              0,
+              selectionCanvas.width,
+              selectionCanvas.height,
+            );
+            const selectionDimensions = getSelectionDimensions();
+            drawRectangle(selectionDimensions);
+            selectWidgetsInit(selectionDimensions, isMultiSelect);
+          }
+        };
+        if (appMode === APP_MODE.EDIT) {
+          init();
         }
-      };
-      if (appMode === "EDIT") {
-        init();
+        return () => {
+          selectionCanvas.removeEventListener("mousedown", onMouseDown);
+          selectionCanvas.removeEventListener("mouseup", onMouseUp);
+          selectionCanvas.removeEventListener("mousemove", onMouseMove);
+          selectionCanvas.removeEventListener("mouseleave", onMouseLeave);
+          selectionCanvas.removeEventListener("mouseenter", onMouseEnter);
+          selectionCanvas.removeEventListener("click", onClick);
+        };
       }
-      return () => {
-        selectionCanvas.removeEventListener("mousedown", onMouseDown);
-        selectionCanvas.removeEventListener("mouseup", onMouseUp);
-        selectionCanvas.removeEventListener("mousemove", onMouseMove);
-        selectionCanvas.removeEventListener("mouseleave", onMouseLeave);
-        selectionCanvas.removeEventListener("mouseenter", onMouseEnter);
-        selectionCanvas.removeEventListener("click", onClick);
-      };
     }, [
       appLayout,
       currentPageId,
@@ -229,12 +234,12 @@ export const CanvasSelectionArena = memo(
       mainContainer.bottomRow,
     ]);
 
-    return (
+    return appMode === APP_MODE.EDIT ? (
       <StyledSelectionCanvas
         data-testid={`canvas-${widgetId}`}
         id={`canvas-${widgetId}`}
       />
-    );
+    ) : null;
   },
 );
 CanvasSelectionArena.displayName = "CanvasSelectionArena";
