e1ded1413fa1461609fc162675a595285832ac4b
Rishabh Saxena <rishabh.robben@gmail.com>
2021-06-16 21:21:18 +0530

    Feature/signup success url (#5124)

--- app/client/src/pages/Applications/index.tsx
+++ app/client/src/pages/Applications/index.tsx
@@ -81,6 +81,7 @@ import { createOrganizationSubmitHandler } from "../organization/helpers";
 import ImportApplicationModal from "./ImportApplicationModal";
 import OnboardingForm from "./OnboardingForm";
 import { getAppsmithConfigs } from "configs";
+import { SIGNUP_SUCCESS_URL } from "constants/routes";
 
 const OrgDropDown = styled.div`
   display: flex;
@@ -887,13 +888,9 @@ type ApplicationProps = {
 };
 
 const getIsFromSignup = () => {
-  if (window.location.href) {
-    const url = new URL(window.location.href);
-    const searchParams = url.searchParams;
-    return !!searchParams.get("isFromSignup");
-  }
-  return false;
+  return window.location?.pathname === SIGNUP_SUCCESS_URL;
 };
+
 const { onboardingFormEnabled } = getAppsmithConfigs();
 class Applications extends Component<
   ApplicationProps,
@@ -912,9 +909,23 @@ class Applications extends Component<
     PerformanceTracker.stopTracking(PerformanceTransactionName.LOGIN_CLICK);
     PerformanceTracker.stopTracking(PerformanceTransactionName.SIGN_UP);
     this.props.getAllApplication();
+    const isFromSignUp = getIsFromSignup();
     this.setState({
-      showOnboardingForm: getIsFromSignup() && onboardingFormEnabled,
+      showOnboardingForm: isFromSignUp && onboardingFormEnabled,
     });
+
+    // Redirect directly in case we're not showing the onboarding form
+    if (isFromSignUp && !onboardingFormEnabled) {
+      const urlObject = new URL(window.location.href);
+      const redirectUrl = urlObject?.searchParams.get("redirectUrl");
+      if (redirectUrl) {
+        try {
+          window.location.replace(redirectUrl);
+        } catch (e) {
+          console.error("Error handling the redirect url");
+        }
+      }
+    }
   }
 
   public render() {
