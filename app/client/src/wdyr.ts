1a50dc4be11fb697ea8c35c35ba653a4fab62adb
Satish Gandham <satish@appsmith.com>
2021-06-17 11:30:36 +0530

    Add types for wdyr manually

--- app/client/src/wdyr.ts
+++ app/client/src/wdyr.ts
@@ -1,3 +1,5 @@
+/// <reference types="@welldone-software/why-did-you-render" />
+// ^ https://github.com/welldone-software/why-did-you-render/issues/161
 import React from "react";
 
 if (process.env.NODE_ENV === "development") {
