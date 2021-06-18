72ca726e48efe0f2c01eca01fd1915e3531af859
Kaushik Varanasi <kaushik.varanasi1@gmail.com>
2021-06-18 10:29:04 +0530

    allow all utf-8 characters in page name

--- app/client/src/utils/helpers.tsx
+++ app/client/src/utils/helpers.tsx
@@ -110,13 +110,8 @@ export const flashElementById = (id: string) => {
 };
 
 export const resolveAsSpaceChar = (value: string, limit?: number) => {
-  // ensures that all special characters are disallowed
-  // while allowing all utf-8 characters
-  const removeSpecialCharsRegex = /`|\~|\!|\@|\#|\$|\%|\^|\&|\*|\(|\)|\+|\=|\[|\{|\]|\}|\||\\|\'|\<|\,|\.|\>|\?|\/|\""|\;|\:|\s/;
   const duplicateSpaceRegex = /\s+/;
   return value
-    .split(removeSpecialCharsRegex)
-    .join(" ")
     .split(duplicateSpaceRegex)
     .join(" ")
     .slice(0, limit || 30);
