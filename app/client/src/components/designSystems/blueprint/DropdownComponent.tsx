7467e90b95852bcf71d05435d23b10d251c7feb7
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2021-06-23 08:13:33 +0100

    update Branch

--- app/client/src/components/designSystems/blueprint/DropdownComponent.tsx
+++ app/client/src/components/designSystems/blueprint/DropdownComponent.tsx
@@ -146,7 +146,7 @@ const DropdownContainer = styled.div`
 class DropDownComponent extends React.Component<DropDownComponentProps> {
   render() {
     return (
-      <DropdownContainer ref={this._menu as React.RefObject<HTMLDivElement>}>
+      <DropdownContainer>
         <DropdownStyles />
         <StyledControlGroup
           fill
