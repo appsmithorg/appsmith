5c3196df070891bfcfc35f0d77d6d686d5d0222e
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-06-25 19:46:36 +0530

    Use system installed mongo and redis servers

--- ci/scripts/1-install.sh
+++ ci/scripts/1-install.sh
@@ -2,7 +2,21 @@
 
 set -o errexit
 
+UBUNTU_RELEASE="$(lsb_release -dc | awk '$1 == "Codename:" { print $2 }')"
+echo "UBUNTU_RELEASE: $UBUNTU_RELEASE"
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \
+	| tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+
+sudo add-apt-repository ppa:redislabs/redis
+
 apt-get update -y
 
 # Installing `gettext-base` just for `envsubst` command.
-apt-get install -y maven gettext-base curl
+apt-get install -y maven gettext-base wget curl mongodb-org-{server,shell} redis
+
+service status mongod
+service status redis
+
+export APPSMITH_MONGODB_URI="mongodb://localhost:27017/appsmith"
+export APPSMITH_REDIS_URL="redis://localhost:6379"
