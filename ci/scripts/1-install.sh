21a19198e39dc582f0c01062952fda8c30b25853
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-06-25 20:09:10 +0530

    This is just sad

--- ci/scripts/1-install.sh
+++ ci/scripts/1-install.sh
@@ -8,14 +8,14 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \
 	| tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
-add-apt-repository ppa:redislabs/redis
+Remove sudoadd-apt-repository ppa:redislabs/redis
 
 apt-get update -y
 
 # Installing `gettext-base` just for `envsubst` command.
 apt-get install -y maven gettext-base wget curl mongodb-org-{server,shell} redis
 
-service status mongod
+service status mongodb
 service status redis
 
 export APPSMITH_MONGODB_URI="mongodb://localhost:27017/appsmith"
