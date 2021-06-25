03f65114269122e7a35fe6f421d3c88bbeaac76f
Rishabh Saxena <rishabh.robben@gmail.com>
2021-06-25 13:46:41 +0530

    Show onboarding form post login if not completed

--- app/client/src/pages/Applications/index.tsx
+++ app/client/src/pages/Applications/index.tsx
@@ -83,6 +83,10 @@ import ImportApplicationModal from "./ImportApplicationModal";
 import OnboardingForm from "./OnboardingForm";
 import { getAppsmithConfigs } from "configs";
 import { SIGNUP_SUCCESS_URL } from "constants/routes";
+import {
+  setOnboardingFormInProgress,
+  getOnboardingFormInProgress,
+} from "utils/storage";
 
 const OrgDropDown = styled.div`
   display: flex;
@@ -911,9 +915,21 @@ class Applications extends Component<
     PerformanceTracker.stopTracking(PerformanceTransactionName.LOGIN_CLICK);
     PerformanceTracker.stopTracking(PerformanceTransactionName.SIGN_UP);
     this.props.getAllApplication();
+    window.addEventListener("message", this.handleTypeFormMessage, false);
+    this.showOnboardingForm();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("message", this.handleTypeFormMessage);
+  }
+
+  showOnboardingForm = async () => {
     const isFromSignUp = getIsFromSignup();
+    const isOnboardingFormInProgress = await getOnboardingFormInProgress();
+    const showOnboardingForm =
+      onboardingFormEnabled && (isFromSignUp || isOnboardingFormInProgress);
     this.setState({
-      showOnboardingForm: isFromSignUp && onboardingFormEnabled,
+      showOnboardingForm: !!showOnboardingForm,
     });
 
     // Redirect directly in case we're not showing the onboarding form
@@ -928,7 +944,13 @@ class Applications extends Component<
         }
       }
     }
-  }
+  };
+
+  handleTypeFormMessage = (event: any) => {
+    if (event?.data?.type === "form-submit" && this.state.showOnboardingForm) {
+      setOnboardingFormInProgress();
+    }
+  };
 
   public render() {
     return (
