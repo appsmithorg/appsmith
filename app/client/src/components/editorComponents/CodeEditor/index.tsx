41bcad458acde2d43951bb56a796d37e7c67c259
hetunandu <hetu@appsmith.com>
2021-06-25 14:08:14 +0530

    Format for all times cause sometimes people can type this char as well

--- app/client/src/components/editorComponents/CodeEditor/index.tsx
+++ app/client/src/components/editorComponents/CodeEditor/index.tsx
@@ -294,14 +294,14 @@ class CodeEditor extends Component<Props, State> {
     cm: CodeMirror.Editor,
     change: CodeMirror.EditorChangeCancellable,
   ) {
-    if (change.origin === "paste") {
-      // Remove all non ASCII quotes since they are invalid in Javascript
-      const formattedText = change.text.map((line) =>
-        line.replace(/[“‘’”]/g, "'"),
-      );
-      if (change.update) {
-        change.update(undefined, undefined, formattedText);
-      }
+    // Remove all non ASCII quotes since they are invalid in Javascript
+    const formattedText = change.text.map((line) => {
+      let formattedLine = line.replace(/[‘’]/g, "'");
+      formattedLine = formattedLine.replace(/[“”]/g, '"');
+      return formattedLine;
+    });
+    if (change.update) {
+      change.update(undefined, undefined, formattedText);
     }
   }
 
