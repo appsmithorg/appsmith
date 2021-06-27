5632e4a4e211f2226cbd01e9ba2b112eea6984c5
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-06-27 13:47:10 +0530

    Run server in background

--- ci/2-scripts/3-build.sh
+++ ci/2-scripts/3-build.sh
@@ -18,6 +18,9 @@ curl-fail() {
 	echo
 }
 
+java -version
+node --version
+
 echo Building client code
 cd "$CODEBUILD_SRC_DIR/app/client"
 npm install -g yarn
@@ -28,10 +31,6 @@ fi
 
 REACT_APP_SHOW_ONBOARDING_FORM=true yarn run build
 
-# Serve the react bundle on a specific port. Nginx will proxy to this port
-echo "127.0.0.1	dev.appsmith.com" | tee -a /etc/hosts
-npx serve -s build -p 3000 &
-
 echo Building server code
 cd "$CODEBUILD_SRC_DIR/app/server"
 ./build.sh --batch-mode -DskipTests
@@ -40,17 +39,11 @@ APPSMITH_ENCRYPTION_SALT=ci-salt-is-white-like-radish \
 	APPSMITH_ENCRYPTION_PASSWORD=ci-password-is-red-like-carrot \
 	APPSMITH_CLOUD_SERVICES_BASE_URL='' \
 	APPSMITH_IS_SELF_HOSTED=false \
-	java -jar server-*.jar
+	java -jar server-*.jar > "$CODEBUILD_SRC_DIR/logs/server.log" &
 
-# docker build --tag 'appsmith-server:ci-local' .
-# docker run -d --network=host \
-# 	--name appsmith-server \
-# 	--env "APPSMITH_MONGODB_URI=$APPSMITH_MONGODB_URI" \
-# 	--env "APPSMITH_REDIS_URL=$APPSMITH_REDIS_URL" \
-# 	--env "APPSMITH_ENCRYPTION_SALT=$APPSMITH_ENCRYPTION_SALT" \
-# 	--env "APPSMITH_ENCRYPTION_PASSWORD=$APPSMITH_ENCRYPTION_PASSWORD" \
-# 	--env "APPSMITH_IS_SELF_HOSTED=false" \
-# 	'appsmith-server:ci-local'
+# Serve the react bundle on a specific port. Nginx will proxy to this port
+echo "127.0.0.1	dev.appsmith.com" | tee -a /etc/hosts
+npx serve -s build -p 3000 &
 
 cd "$CODEBUILD_SRC_DIR/app/client"
 
@@ -68,6 +61,7 @@ if ! curl --insecure --verbose localhost:8080; then
 fi
 
 # Random user names go here
+# Note: The USERNAME values must be valid email addresses, or the signup API calls will fail.
 export CYPRESS_USERNAME=cy@example.com
 export CYPRESS_PASSWORD=cypas
 export CYPRESS_TESTUSERNAME1=cy1@example.com
@@ -80,18 +74,19 @@ export APPSMITH_GOOGLE_MAPS_API_KEY=AIzaSyBOQFulljufGt3VDhBAwNjZN09KEFufVyg
 # Substitute all the env variables in nginx
 vars_to_substitute=$(printf '\$%s,' $(env | grep -o "^APPSMITH_[A-Z0-9_]\+" | xargs))
 echo "vars_to_substitute: $vars_to_substitute"
-cat ./docker/templates/nginx-app.conf.template \
+cat docker/templates/nginx-app.conf.template \
 	| sed -e "s|__APPSMITH_CLIENT_PROXY_PASS__|http://localhost:3000|g" \
 	| sed -e "s|__APPSMITH_SERVER_PROXY_PASS__|http://localhost:8080|g" \
 	| envsubst $vars_to_substitute \
 	| sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' \
 	| tee /etc/nginx/conf.d/app.conf
-cat ./docker/templates/nginx-root.conf.template \
+cat docker/templates/nginx-root.conf.template \
 	| envsubst ${vars_to_substitute} \
 	| sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' -e 's/user  *nginx;/user root;/' \
 	| tee /etc/nginx/nginx.conf
 
 # Create the SSL files for Nginx. Required for service workers to work properly.
+# This is a self-signed certificate, and so when using cURL, we need to add the `-k` or `--insecure` argument.
 mkdir -p /etc/certificate
 openssl req -x509 -newkey rsa:4096 -nodes \
 	-out /etc/certificate/dev.appsmith.com.pem \
@@ -105,10 +100,6 @@ if ! /etc/init.d/nginx reload; then
 fi
 
 sleep 5s
-if ! curl-fail dev.appsmith.com; then
-	cat /var/log/nginx/access.log
-	cat /var/log/nginx/error.log
-fi
 if ! curl-fail https://dev.appsmith.com; then
 	cat /var/log/nginx/access.log
 	cat /var/log/nginx/error.log
@@ -117,9 +108,6 @@ fi
 
 echo "Sleeping for 30 seconds to let the servers start"
 sleep 30
-
-echo "Checking if the containers have started"
-docker ps -a
 
 # Create test users.
 curl-fail -v -d "email=$CYPRESS_USERNAME" -d "password=$CYPRESS_PASSWORD" 'https://dev.appsmith.com/api/v1/users'
@@ -144,4 +132,5 @@ npx cypress run --headless --browser chrome \
 	--spec 'cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Input_spec.js'
 	# --spec 'cypress/integration/Smoke_TestSuite/**/*.js'
 
+# At end of this script, CodeBuild does some cleanup and without the below line, it throws an error.
 unset -f curl-fail
