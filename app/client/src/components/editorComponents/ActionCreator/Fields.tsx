4fdf14e490abc6f44200ad58ebe4953804fb869e
Pranav Kanade <pranav@appsmith.com>
2021-06-28 14:19:53 +0530

    split action creator file

--- app/client/src/components/editorComponents/ActionCreator/Fields.tsx
+++ app/client/src/components/editorComponents/ActionCreator/Fields.tsx
@@ -1,21 +1,5 @@
-import React, { useMemo } from "react";
-import { AppState } from "reducers";
-import {
-  getActionsForCurrentPage,
-  getDBDatasources,
-} from "selectors/entitiesSelector";
-import { createActionRequest } from "actions/actionActions";
-import {
-  getModalDropdownList,
-  getNextModalName,
-} from "selectors/widgetSelectors";
-import {
-  getCurrentApplicationId,
-  getCurrentPageId,
-} from "selectors/editorSelectors";
-import { Datasource } from "entities/Datasource";
+import React from "react";
 import { DropdownOption } from "widgets/DropdownWidget";
-import { useDispatch, useSelector } from "react-redux";
 import TreeDropdown, { TreeDropdownOption } from "components/ads/TreeDropdown";
 import {
   ControlWrapper,
@@ -23,25 +7,10 @@ import {
 } from "components/propertyControls/StyledControls";
 import { KeyValueComponent } from "components/propertyControls/KeyValueComponent";
 import { InputText } from "components/propertyControls/InputTextControl";
-import { createModalAction } from "actions/widgetActions";
-import { createNewQueryName } from "utils/AppsmithUtils";
 import { getDynamicBindings, isDynamicValue } from "utils/DynamicBindingUtils";
 import HightlightedCode from "components/editorComponents/HighlightedCode";
-import TreeStructure from "components/utils/TreeStructure";
 import { NavigationTargetType } from "sagas/ActionExecutionSagas";
-import { getWidgets } from "sagas/selectors";
-import { PluginType } from "entities/Action";
 import { Skin } from "constants/DefaultTheme";
-import { INTEGRATION_EDITOR_URL, INTEGRATION_TABS } from "constants/routes";
-import history from "utils/history";
-import { keyBy } from "lodash";
-import {
-  QueryIcon,
-  dbQueryIcon,
-  apiIcon,
-  MethodTag,
-  getPluginIcon,
-} from "pages/Editor/Explorer/ExplorerIcons";
 
 /* eslint-disable @typescript-eslint/ban-types */
 /* TODO: Function and object types need to be updated to enable the lint rule */
@@ -82,11 +51,11 @@ const NAVIGATION_TARGET_FIELD_OPTIONS = [
   },
 ];
 
-const FUNC_ARGS_REGEX = /((["][^"]*["])|([\[].*[\]])|([\{].*[\}])|(['][^']*['])|([\(].*[\)[=][>][{].*[}])|([^'",][^,"+]*[^'",]*))*/gi;
-const ACTION_TRIGGER_REGEX = /^{{([\s\S]*?)\(([\s\S]*?)\)}}$/g;
+export const FUNC_ARGS_REGEX = /((["][^"]*["])|([\[].*[\]])|([\{].*[\}])|(['][^']*['])|([\(].*[\)[=][>][{].*[}])|([^'",][^,"+]*[^'",]*))*/gi;
+export const ACTION_TRIGGER_REGEX = /^{{([\s\S]*?)\(([\s\S]*?)\)}}$/g;
 //Old Regex:: /\(\) => ([\s\S]*?)(\([\s\S]*?\))/g;
-const ACTION_ANONYMOUS_FUNC_REGEX = /\(\) => (({[\s\S]*?})|([\s\S]*?)(\([\s\S]*?\)))/g;
-const IS_URL_OR_MODAL = /^'.*'$/;
+export const ACTION_ANONYMOUS_FUNC_REGEX = /\(\) => (({[\s\S]*?})|([\s\S]*?)(\([\s\S]*?\)))/g;
+export const IS_URL_OR_MODAL = /^'.*'$/;
 const modalSetter = (changeValue: any, currentValue: string) => {
   const matches = [...currentValue.matchAll(ACTION_TRIGGER_REGEX)];
   let args: string[] = [];
@@ -224,13 +193,7 @@ const enumTypeGetter = (
   return defaultValue;
 };
 
-type ActionCreatorProps = {
-  value: string;
-  onValueChange: (newValue: string) => void;
-  additionalAutoComplete?: Record<string, Record<string, unknown>>;
-};
-
-const ActionType = {
+export const ActionType = {
   none: "none",
   integration: "integration",
   api: "api",
@@ -333,7 +296,7 @@ const views = {
   },
 };
 
-const FieldType = {
+export const FieldType = {
   ACTION_SELECTOR_FIELD: "ACTION_SELECTOR_FIELD",
   ON_SUCCESS_FIELD: "ON_SUCCESS_FIELD",
   ON_ERROR_FIELD: "ON_ERROR_FIELD",
@@ -551,201 +514,6 @@ const fieldConfigs: FieldConfigs = {
     view: ViewTypes.SELECTOR_VIEW,
   },
 };
-
-const baseOptions: any = [
-  {
-    label: "No Action",
-    value: ActionType.none,
-  },
-  {
-    label: "Execute an Integration",
-    value: ActionType.integration,
-  },
-
-  {
-    label: "Navigate To",
-    value: ActionType.navigateTo,
-  },
-  {
-    label: "Show Message",
-    value: ActionType.showAlert,
-  },
-  {
-    label: "Open Modal",
-    value: ActionType.showModal,
-  },
-  {
-    label: "Close Modal",
-    value: ActionType.closeModal,
-  },
-  {
-    label: "Store Value",
-    value: ActionType.storeValue,
-  },
-  {
-    label: "Download",
-    value: ActionType.download,
-  },
-  {
-    label: "Copy to Clipboard",
-    value: ActionType.copyToClipboard,
-  },
-  {
-    label: "Reset Widget",
-    value: ActionType.resetWidget,
-  },
-];
-
-function getFieldFromValue(
-  value: string | undefined,
-  getParentValue?: Function,
-): any[] {
-  const fields: any[] = [
-    {
-      field: FieldType.ACTION_SELECTOR_FIELD,
-      getParentValue,
-      value,
-    },
-  ];
-  if (!value) {
-    return fields;
-  }
-  if (value.indexOf("run") !== -1) {
-    const matches = [...value.matchAll(ACTION_TRIGGER_REGEX)];
-    if (matches.length) {
-      const funcArgs = matches[0][2];
-      const args = [...funcArgs.matchAll(ACTION_ANONYMOUS_FUNC_REGEX)];
-      const successArg = args[0];
-      const errorArg = args[1];
-      let sucesssValue;
-      if (successArg && successArg.length > 0) {
-        sucesssValue = successArg[1] !== "{}" ? `{{${successArg[1]}}}` : ""; //successArg[1] + successArg[2];
-      }
-      const successFields = getFieldFromValue(
-        sucesssValue,
-        (changeValue: string) => {
-          const matches = [...value.matchAll(ACTION_TRIGGER_REGEX)];
-          const args = [...matches[0][2].matchAll(ACTION_ANONYMOUS_FUNC_REGEX)];
-          let successArg = args[0] ? args[0][0] : "() => {}";
-          const errorArg = args[1] ? args[1][0] : "() => {}";
-          successArg = changeValue.endsWith(")")
-            ? `() => ${changeValue}`
-            : `() => ${changeValue}()`;
-
-          return value.replace(
-            ACTION_TRIGGER_REGEX,
-            `{{$1(${successArg}, ${errorArg})}}`,
-          );
-        },
-      );
-      successFields[0].label = "onSuccess";
-      fields.push(successFields);
-
-      let errorValue;
-      if (errorArg && errorArg.length > 0) {
-        errorValue = errorArg[1] !== "{}" ? `{{${errorArg[1]}}}` : ""; //errorArg[1] + errorArg[2];
-      }
-      const errorFields = getFieldFromValue(
-        errorValue,
-        (changeValue: string) => {
-          const matches = [...value.matchAll(ACTION_TRIGGER_REGEX)];
-          const args = [...matches[0][2].matchAll(ACTION_ANONYMOUS_FUNC_REGEX)];
-          const successArg = args[0] ? args[0][0] : "() => {}";
-          let errorArg = args[1] ? args[1][0] : "() => {}";
-          errorArg = changeValue.endsWith(")")
-            ? `() => ${changeValue}`
-            : `() => ${changeValue}()`;
-          return value.replace(
-            ACTION_TRIGGER_REGEX,
-            `{{$1(${successArg}, ${errorArg})}}`,
-          );
-        },
-      );
-      errorFields[0].label = "onError";
-      fields.push(errorFields);
-    }
-    return fields;
-  }
-  if (value.indexOf("navigateTo") !== -1) {
-    fields.push({
-      field: FieldType.URL_FIELD,
-    });
-    fields.push({
-      field: FieldType.QUERY_PARAMS_FIELD,
-    });
-    fields.push({
-      field: FieldType.NAVIGATION_TARGET_FIELD,
-    });
-  }
-
-  if (value.indexOf("showModal") !== -1) {
-    fields.push({
-      field: FieldType.SHOW_MODAL_FIELD,
-    });
-  }
-  if (value.indexOf("closeModal") !== -1) {
-    fields.push({
-      field: FieldType.CLOSE_MODAL_FIELD,
-    });
-  }
-  if (value.indexOf("showAlert") !== -1) {
-    fields.push(
-      {
-        field: FieldType.ALERT_TEXT_FIELD,
-      },
-      {
-        field: FieldType.ALERT_TYPE_SELECTOR_FIELD,
-      },
-    );
-  }
-  if (value.indexOf("storeValue") !== -1) {
-    fields.push(
-      {
-        field: FieldType.KEY_TEXT_FIELD,
-      },
-      {
-        field: FieldType.VALUE_TEXT_FIELD,
-      },
-    );
-  }
-  if (value.indexOf("resetWidget") !== -1) {
-    fields.push(
-      {
-        field: FieldType.WIDGET_NAME_FIELD,
-      },
-      {
-        field: FieldType.RESET_CHILDREN_FIELD,
-      },
-    );
-  }
-  if (value.indexOf("download") !== -1) {
-    fields.push(
-      {
-        field: FieldType.DOWNLOAD_DATA_FIELD,
-      },
-      {
-        field: FieldType.DOWNLOAD_FILE_NAME_FIELD,
-      },
-      {
-        field: FieldType.DOWNLOAD_FILE_TYPE_FIELD,
-      },
-    );
-  }
-  if (value.indexOf("copyToClipboard") !== -1) {
-    fields.push({
-      field: FieldType.COPY_TEXT_FIELD,
-    });
-  }
-  return fields;
-}
-
-function getPageDropdownOptions(state: AppState) {
-  return state.entities.pageList.pages.map((page) => ({
-    label: page.pageName,
-    id: page.pageId,
-    value: `'${page.pageName}'`,
-  }));
-}
 
 function renderField(props: {
   onValueChange: Function;
@@ -1036,191 +804,4 @@ function Fields(props: {
   }
 }
 
-function useModalDropdownList() {
-  const dispatch = useDispatch();
-  const nextModalName = useSelector(getNextModalName);
-
-  let finalList: TreeDropdownOption[] = [
-    {
-      label: "New Modal",
-      value: "Modal",
-      id: "create",
-      icon: "plus",
-      className: "t--create-modal-btn",
-      onSelect: (option: TreeDropdownOption, setter?: Function) => {
-        const modalName = nextModalName;
-        if (setter) {
-          setter({
-            value: `${modalName}`,
-          });
-          dispatch(createModalAction(nextModalName));
-        }
-      },
-    },
-  ];
-
-  finalList = finalList.concat(
-    (useSelector(getModalDropdownList) || []) as TreeDropdownOption[],
-  );
-
-  return finalList;
-}
-
-function useWidgetOptionTree() {
-  const widgets = useSelector(getWidgets) || {};
-  return Object.values(widgets)
-    .filter((w) => w.type !== "CANVAS_WIDGET" && w.type !== "BUTTON_WIDGET")
-    .map((w) => {
-      return {
-        label: w.widgetName,
-        id: w.widgetName,
-        value: `"${w.widgetName}"`,
-      };
-    });
-}
-
-function getIcon(action: any, plugin: any) {
-  if (plugin && plugin.type !== PluginType.API && plugin.iconLocation)
-    return <QueryIcon plugin={plugin} />;
-  else if (plugin && plugin.type === PluginType.DB) return dbQueryIcon;
-
-  const method = action.actionConfiguration.httpMethod;
-  if (!method) return apiIcon;
-  return <MethodTag type={method} />;
-}
-
-function getIntegrationOptionsWithChildren(
-  pageId: string,
-  plugins: any,
-  options: TreeDropdownOption[],
-  actions: any[],
-  datasources: Datasource[],
-  createIntegrationOption: TreeDropdownOption,
-  dispatch: any,
-) {
-  const queries = actions.filter(
-    (action) => action.config.pluginType === PluginType.DB,
-  );
-  const apis = actions.filter(
-    (action) =>
-      action.config.pluginType === PluginType.API ||
-      action.config.pluginType === PluginType.SAAS,
-  );
-  const option = options.find(
-    (option) => option.value === ActionType.integration,
-  );
-  if (option) {
-    option.children = [createIntegrationOption];
-    apis.forEach((api) => {
-      (option.children as TreeDropdownOption[]).push({
-        label: api.config.name,
-        id: api.config.id,
-        value: api.config.name,
-        type: option.value,
-        icon: getIcon(
-          api.config,
-          plugins[(api as any).config.datasource.pluginId],
-        ),
-      } as TreeDropdownOption);
-    });
-    queries.forEach((query) => {
-      (option.children as TreeDropdownOption[]).push({
-        label: query.config.name,
-        id: query.config.id,
-        value: query.config.name,
-        type: option.value,
-        icon: getIcon(
-          query.config,
-          plugins[(query as any).config.datasource.pluginId],
-        ),
-      } as TreeDropdownOption);
-    });
-    datasources.forEach((dataSource: Datasource) => {
-      (option.children as TreeDropdownOption[]).push({
-        label: dataSource.name,
-        id: dataSource.id,
-        value: dataSource.name,
-        type: option.value,
-        icon: getPluginIcon(plugins[dataSource.pluginId]) as React.ReactNode,
-        onSelect: () => {
-          const newQueryName = createNewQueryName(actions, pageId);
-          dispatch(
-            createActionRequest({
-              name: newQueryName,
-              pageId,
-              datasource: {
-                id: dataSource.id,
-              },
-              eventData: {
-                actionType: "Query",
-                from: "home-screen",
-                dataSource: dataSource.name,
-              },
-              pluginId: dataSource.pluginId,
-              actionConfiguration: {},
-            }),
-          );
-        },
-      } as TreeDropdownOption);
-    });
-  }
-  return options;
-}
-
-function useIntegrationsOptionTree() {
-  const pageId = useSelector(getCurrentPageId) || "";
-  const applicationId = useSelector(getCurrentApplicationId);
-  const datasources: Datasource[] = useSelector(getDBDatasources);
-  const dispatch = useDispatch();
-  const plugins = useSelector((state: AppState) => {
-    return state.entities.plugins.list;
-  });
-  const pluginGroups: any = useMemo(() => keyBy(plugins, "id"), [plugins]);
-  const actions = useSelector(getActionsForCurrentPage);
-
-  const integrationOptionTree = getIntegrationOptionsWithChildren(
-    pageId,
-    pluginGroups,
-    baseOptions,
-    actions,
-    datasources,
-    {
-      label: "Create New Integration",
-      value: "integration",
-      id: "create",
-      icon: "plus",
-      className: "t--create-integration-btn",
-      onSelect: () => {
-        history.push(
-          INTEGRATION_EDITOR_URL(applicationId, pageId, INTEGRATION_TABS.NEW),
-        );
-      },
-    },
-    dispatch,
-  );
-  return integrationOptionTree;
-}
-
-export function ActionCreator(props: ActionCreatorProps) {
-  const integrationOptionTree = useIntegrationsOptionTree();
-  const widgetOptionTree = useWidgetOptionTree();
-  const modalDropdownList = useModalDropdownList();
-  const pageDropdownOptions = useSelector(getPageDropdownOptions);
-  const fields = getFieldFromValue(props.value);
-  return (
-    <TreeStructure>
-      <Fields
-        additionalAutoComplete={props.additionalAutoComplete}
-        depth={1}
-        fields={fields}
-        integrationOptionTree={integrationOptionTree}
-        maxDepth={1}
-        modalDropdownList={modalDropdownList}
-        onValueChange={props.onValueChange}
-        pageDropdownOptions={pageDropdownOptions}
-        value={props.value}
-        widgetOptionTree={widgetOptionTree}
-      />
-    </TreeStructure>
-  );
-}
+export default Fields;
