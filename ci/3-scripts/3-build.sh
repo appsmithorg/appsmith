bc6c532e871cfa7b51765566d9a1e5339ad39bc2
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-06-28 11:48:12 +0530

    Ready to push to DockerHub

--- ci/3-scripts/3-build.sh
+++ ci/3-scripts/3-build.sh
@@ -3,6 +3,23 @@
 set -o errexit
 set -o xtrace
 
+aws --version
+java -version
+node --version
+docker version
+
+echo "$DOCKER_HUB_PASSWORD" | docker login --username "$DOCKER_HUB_USERNAME" --password-stdin
+docker info
+
+image_prefix=""
+if [[ -z $DOCKER_HUB_ORGANIZATION ]]; then
+	image_prefix="$DOCKER_HUB_ORGANIZATION/"
+fi
+
+if [[ -z $DOCKER_TAG_NAME ]]; then
+	DOCKER_TAG_NAME="${CODEBUILD_SOURCE_VERSION:-release}"
+fi
+
 echo Building client code
 cd "$CODEBUILD_SRC_DIR/app/client"
 npm install -g yarn
@@ -10,15 +27,13 @@ yarn install --frozen-lockfile
 
 REACT_APP_SHOW_ONBOARDING_FORM=true yarn run build
 
-docker build -t "appsmith-editor:$TAG_NAME" .
-docker tag "appsmith-editor:$TAG_NAME" "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/appsmith-editor:$TAG_NAME"
+docker build --tag "${image_prefix}appsmith-editor:$DOCKER_TAG_NAME" .
 
 echo Building server code
 cd "$CODEBUILD_SRC_DIR/app/server"
 ./build.sh --batch-mode --threads 1.0C -Dmaven.test.skip=true
 
-docker build --tag "appsmith-server:$TAG_NAME" .
-docker tag "appsmith-server:$TAG_NAME" "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/appsmith-server:$TAG_NAME"
+docker build --tag "${image_prefix}appsmith-server:$DOCKER_TAG_NAME" .
 
-docker push "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/appsmith-editor:$TAG_NAME"
-docker push "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/appsmith-server:$TAG_NAME"
+docker push "${image_prefix}appsmith-editor:$DOCKER_TAG_NAME"
+docker push "${image_prefix}appsmith-server:$DOCKER_TAG_NAME"
