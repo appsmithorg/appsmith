83ddfa5262afeab161a7f606db12be1bebdd9e1a
Trisha Anand <trisha@appsmith.com>
2021-06-29 18:03:01 +0530

    Moved helper functions to utils file to make the main file smaller

--- app/server/appsmith-plugins/mongoPlugin/src/main/java/com/external/plugins/MongoPlugin.java
+++ app/server/appsmith-plugins/mongoPlugin/src/main/java/com/external/plugins/MongoPlugin.java
@@ -24,15 +24,6 @@ import com.appsmith.external.models.SSLDetails;
 import com.appsmith.external.plugins.BasePlugin;
 import com.appsmith.external.plugins.PluginExecutor;
 import com.appsmith.external.plugins.SmartSubstitutionInterface;
-import com.external.plugins.commands.Aggregate;
-import com.external.plugins.commands.Count;
-import com.external.plugins.commands.Delete;
-import com.external.plugins.commands.Distinct;
-import com.external.plugins.commands.Find;
-import com.external.plugins.commands.Insert;
-import com.external.plugins.commands.MongoCommand;
-import com.external.plugins.commands.UpdateMany;
-import com.external.plugins.commands.UpdateOne;
 import com.mongodb.MongoCommandException;
 import com.mongodb.MongoSocketWriteException;
 import com.mongodb.MongoTimeoutException;
@@ -42,8 +33,6 @@ import com.mongodb.reactivestreams.client.MongoDatabase;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import org.bson.types.Decimal128;
-import org.bson.types.ObjectId;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.pf4j.Extension;
@@ -57,19 +46,14 @@ import reactor.core.scheduler.Schedulers;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -79,9 +63,13 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static com.appsmith.external.constants.ActionConstants.ACTION_CONFIGURATION_BODY;
+import static com.external.plugins.MongoPluginUtils.convertMongoFormInputToRawCommand;
+import static com.external.plugins.MongoPluginUtils.generateTemplatesAndStructureForACollection;
+import static com.external.plugins.MongoPluginUtils.getDatabaseName;
+import static com.external.plugins.MongoPluginUtils.isRawCommand;
+import static com.external.plugins.MongoPluginUtils.urlEncode;
 import static com.external.plugins.MongoPluginUtils.validConfigurationPresent;
 import static com.external.plugins.constants.ConfigurationIndex.AGGREGATE_PIPELINE;
-import static com.external.plugins.constants.ConfigurationIndex.COMMAND;
 import static com.external.plugins.constants.ConfigurationIndex.COUNT_QUERY;
 import static com.external.plugins.constants.ConfigurationIndex.DELETE_QUERY;
 import static com.external.plugins.constants.ConfigurationIndex.DISTINCT_QUERY;
@@ -95,7 +83,6 @@ import static com.external.plugins.constants.ConfigurationIndex.UPDATE_ONE_SORT;
 import static com.external.plugins.constants.ConfigurationIndex.UPDATE_ONE_UPDATE;
 import static com.external.plugins.constants.ConfigurationIndex.UPDATE_QUERY;
 import static com.external.plugins.constants.ConfigurationIndex.UPDATE_UPDATE;
-import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 
 public class MongoPlugin extends BasePlugin {
@@ -417,64 +404,6 @@ public class MongoPlugin extends BasePlugin {
                     .subscribeOn(scheduler);
         }
 
-        private Boolean isRawCommand(List<Property> templates) {
-            if ((templates.size() >= (1 + COMMAND)) &&
-                    (templates.get(COMMAND) != null) &&
-                    ("RAW".equals(templates.get(COMMAND).getValue()))) {
-                return TRUE;
-            }
-            return FALSE;
-        }
-
-        private String convertMongoFormInputToRawCommand(ActionConfiguration actionConfiguration) {
-            List<Property> templates = actionConfiguration.getPluginSpecifiedTemplates();
-            if (templates != null) {
-                // If its not raw command, then it must be one of the mongo form commands
-                if (!isRawCommand(templates)) {
-
-                    // Parse the commands into raw appropriately
-                    MongoCommand command = null;
-                    switch ((String) templates.get(COMMAND).getValue()) {
-                        case "INSERT":
-                            command = new Insert(actionConfiguration);
-                            break;
-                        case "FIND":
-                            command = new Find(actionConfiguration);
-                            break;
-                        case "UPDATE":
-                            command = new UpdateMany(actionConfiguration);
-                            break;
-                        case "UPDATE_ONE":
-                            command = new UpdateOne(actionConfiguration);
-                            break;
-                        case "DELETE":
-                            command = new Delete(actionConfiguration);
-                            break;
-                        case "COUNT":
-                            command = new Count(actionConfiguration);
-                            break;
-                        case "DISTINCT":
-                            command = new Distinct(actionConfiguration);
-                            break;
-                        case "AGGREGATE":
-                            command = new Aggregate(actionConfiguration);
-                            break;
-                        default:
-                            throw new AppsmithPluginException(AppsmithPluginError.PLUGIN_EXECUTE_ARGUMENT_ERROR, "No valid mongo command found. Please select a command from the \"Command\" dropdown and try again");
-                    }
-                    if (!command.isValid()) {
-                        throw new AppsmithPluginException(AppsmithPluginError.PLUGIN_EXECUTE_ARGUMENT_ERROR, "Try again after configuring the fields : " + command.getFieldNamesWithNoConfiguration());
-                    }
-
-                    return command.parseCommand().toJson();
-                }
-            }
-
-            // We reached here. This means either this is a RAW command input or some configuration error has happened
-            // in which case, we default to RAW
-            return actionConfiguration.getBody();
-        }
-
         private String smartSubstituteBSON(String rawQuery,
                                            List<Param> params,
                                            List<Map.Entry<String, String>> parameters) throws AppsmithPluginException {
@@ -505,19 +434,6 @@ public class MongoPlugin extends BasePlugin {
             }
 
             return templates;
-        }
-
-        private String getDatabaseName(DatasourceConfiguration datasourceConfiguration) {
-            // Explicitly set default database.
-            String databaseName = datasourceConfiguration.getConnection().getDefaultDatabaseName();
-
-            // If that's not available, pick the authentication database.
-            final DBAuth authentication = (DBAuth) datasourceConfiguration.getAuthentication();
-            if (StringUtils.isEmpty(databaseName) && authentication != null) {
-                databaseName = authentication.getDatabaseName();
-            }
-
-            return databaseName;
         }
 
         @Override
@@ -945,83 +861,6 @@ public class MongoPlugin extends BasePlugin {
                     .subscribeOn(scheduler);
         }
 
-        private static void generateTemplatesAndStructureForACollection(String collectionName,
-                                                                        Document document,
-                                                                        ArrayList<DatasourceStructure.Column> columns,
-                                                                        ArrayList<DatasourceStructure.Template> templates) {
-            String filterFieldName = null;
-            String filterFieldValue = null;
-            Map<String, String> sampleInsertValues = new LinkedHashMap<>();
-
-            for (Map.Entry<String, Object> entry : document.entrySet()) {
-                final String name = entry.getKey();
-                final Object value = entry.getValue();
-                String type;
-
-                if (value instanceof Integer) {
-                    type = "Integer";
-                    sampleInsertValues.put(name, "1");
-                } else if (value instanceof Long) {
-                    type = "Long";
-                    sampleInsertValues.put(name, "NumberLong(\"1\")");
-                } else if (value instanceof Double) {
-                    type = "Double";
-                    sampleInsertValues.put(name, "1");
-                } else if (value instanceof Decimal128) {
-                    type = "BigDecimal";
-                    sampleInsertValues.put(name, "NumberDecimal(\"1\")");
-                } else if (value instanceof String) {
-                    type = "String";
-                    sampleInsertValues.put(name, "\"new value\"");
-                    if (filterFieldName == null || filterFieldName.compareTo(name) > 0) {
-                        filterFieldName = name;
-                        filterFieldValue = (String) value;
-                    }
-                } else if (value instanceof ObjectId) {
-                    type = "ObjectId";
-                    if (!value.equals("_id")) {
-                        sampleInsertValues.put(name, "ObjectId(\"a_valid_object_id_hex\")");
-                    }
-                } else if (value instanceof Collection) {
-                    type = "Array";
-                    sampleInsertValues.put(name, "[1, 2, 3]");
-                } else if (value instanceof Date) {
-                    type = "Date";
-                    sampleInsertValues.put(name, "new Date(\"2019-07-01\")");
-                } else {
-                    type = "Object";
-                    sampleInsertValues.put(name, "{}");
-                }
-
-                columns.add(new DatasourceStructure.Column(name, type, null));
-            }
-
-            columns.sort(Comparator.naturalOrder());
-
-            Map<String, Object> templateConfiguration = new HashMap<>();
-            templateConfiguration.put("collectionName", collectionName);
-            templateConfiguration.put("filterFieldName", filterFieldName);
-            templateConfiguration.put("filterFieldValue", filterFieldValue);
-            templateConfiguration.put("sampleInsertValues", sampleInsertValues);
-
-            templates.addAll(
-                    new Find().generateTemplate(templateConfiguration)
-            );
-
-
-            templates.addAll(
-                    new Insert().generateTemplate(templateConfiguration)
-            );
-
-            templates.addAll(
-                    new UpdateMany().generateTemplate(templateConfiguration)
-            );
-
-            templates.addAll(
-                    new Delete().generateTemplate(templateConfiguration)
-            );
-        }
-
         @Override
         public Object substituteValueInInput(int index,
                                              String binding,
@@ -1040,10 +879,6 @@ public class MongoPlugin extends BasePlugin {
             // Unused function
             return Mono.error(new AppsmithPluginException(AppsmithPluginError.PLUGIN_ERROR, "Unsupported Operation"));
         }
-    }
-
-    private static String urlEncode(String text) {
-        return URLEncoder.encode(text, StandardCharsets.UTF_8);
     }
 
     private static Object cleanUp(Object object) {
