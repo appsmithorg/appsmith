5ee670c752d9fce51921fbc0d1fe7f6bba926208
Pranav Kanade <pranav@appsmith.com>
2021-07-01 17:21:14 +0530

    Revert "[Bug] Mock data section is missing (#5517)"
    This reverts commit be48f2f5

--- app/client/src/pages/Editor/IntegrationEditor/IntegrationsHomeScreen.tsx
+++ app/client/src/pages/Editor/IntegrationEditor/IntegrationsHomeScreen.tsx
@@ -276,6 +276,7 @@ class IntegrationsHomeScreen extends React.Component<
 > {
   constructor(props: Props) {
     super(props);
+    console.log("INtegrations Home screen");
 
     this.state = {
       page: 1,
@@ -386,9 +387,7 @@ class IntegrationsHomeScreen extends React.Component<
       currentScreen = (
         <NewIntegrationsContainer id="new-integrations-wrapper">
           {dataSources.length === 0 && <AddDatasourceSecurely />}
-          {dataSources.length === 0 &&
-            this.props.mockDatasources.length > 0 &&
-            mockDataSection}
+          {dataSources.length === 0 && mockDataSection}
           <CreateNewAPI
             active={activeSecondaryMenuId === SECONDARY_MENU_IDS.API}
             applicationId={applicationId}
@@ -405,9 +404,7 @@ class IntegrationsHomeScreen extends React.Component<
             location={location}
             pageId={pageId}
           />
-          {dataSources.length > 0 &&
-            this.props.mockDatasources.length > 0 &&
-            mockDataSection}
+          {dataSources.length === 0 && mockDataSection}
         </NewIntegrationsContainer>
       );
     } else {
