b917ab3bd37642395bccef86e10c88058c3ecea4
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-07-02 14:50:39 +0530

    Upload logs to S3 after build

--- ci/2-scripts/1-install.sh
+++ ci/2-scripts/1-install.sh
@@ -1,9 +1,6 @@
 set -o errexit
 set -o xtrace
 
-UBUNTU_RELEASE="$(lsb_release -dc | awk '$1 == "Codename:" { print $2 }')"
-echo "UBUNTU_RELEASE: $UBUNTU_RELEASE"
-
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" \
 	| tee /etc/apt/sources.list.d/mongodb-org-4.4.list
