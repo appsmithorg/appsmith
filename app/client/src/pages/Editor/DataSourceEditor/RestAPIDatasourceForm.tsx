69dc52625c8f4c064a2cdb6aabac8e9bfe286b59
Pranav Kanade <pranav@appsmith.com>
2021-07-02 13:08:49 +0530

    removed margin from header of generic datasource

--- app/client/src/pages/Editor/DataSourceEditor/RestAPIDatasourceForm.tsx
+++ app/client/src/pages/Editor/DataSourceEditor/RestAPIDatasourceForm.tsx
@@ -124,7 +124,7 @@ export const Header = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-top: 16px;
+  //margin-top: 16px;
 `;
 
 const SaveButtonContainer = styled.div`
