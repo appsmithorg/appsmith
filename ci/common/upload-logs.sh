6707558fac5a1ca2b2cb96242c443f5cb06752b5
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-07-02 18:20:41 +0530

    Push logs to S3 always

--- ci/common/upload-logs.sh
+++ ci/common/upload-logs.sh
@@ -1,3 +1,1 @@
-if [[ $CODEBUILD_BUILD_SUCCEEDING == 0 || $UPLOAD_LOGS_ON_SUCCESS == 1 ]]; then
-	aws s3 cp --no-progress --recursive "$CODEBUILD_SRC_DIR/ci/logs" "$S3_BUCKET_PREFIX/logs/$BATCH_ID/"
-fi
+aws s3 cp --no-progress --recursive "$CODEBUILD_SRC_DIR/ci/logs" "$S3_BUCKET_PREFIX/logs/$BATCH_ID/"
