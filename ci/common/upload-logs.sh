452d028ca0317c1c2fba5ee364eb3044bfc88a92
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-07-06 19:33:30 +0530

    Exit on failure

--- ci/common/upload-logs.sh
+++ ci/common/upload-logs.sh
@@ -3,3 +3,5 @@ set -o xtrace
 
 ls "$CODEBUILD_SRC_DIR/ci/logs"
 aws s3 cp --no-progress --recursive "$CODEBUILD_SRC_DIR/ci/logs" "$S3_LOGS_PREFIX/$BATCH_ID/"
+
+exit $((CODEBUILD_BUILD_SUCCEEDING ^ 1))
