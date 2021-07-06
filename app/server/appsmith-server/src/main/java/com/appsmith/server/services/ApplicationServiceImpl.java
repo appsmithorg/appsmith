4c9fafa732eb9765c6a7e939b5550ee33558df9c
Abhijeet <abhi.nagarnaik@gmail.com>
2021-07-06 12:20:29 +0530

    Error message comment added

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ApplicationServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ApplicationServiceImpl.java
@@ -130,6 +130,9 @@ public class ApplicationServiceImpl extends BaseService<ApplicationRepository, A
         return repository.updateById(id, application, AclPermission.MANAGE_APPLICATIONS)
             .onErrorResume(error -> {
                 if (error instanceof DuplicateKeyException) {
+                    // Error message : E11000 duplicate key error collection: appsmith.application index:
+                    // organization_application_deleted_compound_index dup key:
+                    // { organizationId: "******", name: "AppName", deletedAt: null }
                     if (error.getCause().getMessage().contains("name:")) {
                         return Mono.error(
                             new AppsmithException(AppsmithError.DUPLICATE_KEY_USER_ERROR, FieldName.APPLICATION, FieldName.NAME)
