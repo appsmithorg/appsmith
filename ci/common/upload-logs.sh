b729c562a5f6ffc34bd3fed0cabaed938cde2711
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-07-07 06:37:45 +0530

    No redirection

--- ci/common/upload-logs.sh
+++ ci/common/upload-logs.sh
@@ -4,4 +4,5 @@ set -o xtrace
 ls "$CODEBUILD_SRC_DIR/ci/logs"
 aws s3 cp --no-progress --recursive "$CODEBUILD_SRC_DIR/ci/logs" "$S3_LOGS_PREFIX/$BATCH_ID/"
 
+# Exit with status of 1 when build is succeeding, or 0 if failing.
 exit $((CODEBUILD_BUILD_SUCCEEDING ^ 1))
