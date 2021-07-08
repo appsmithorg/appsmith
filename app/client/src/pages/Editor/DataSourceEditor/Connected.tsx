df7e0d0ac9de2f84207612e74e9a8d6646907796
Pranav Kanade <pranav@appsmith.com>
2021-07-08 11:13:34 +0530

    Handling the action for apis as well

--- app/client/src/pages/Editor/DataSourceEditor/Connected.tsx
+++ app/client/src/pages/Editor/DataSourceEditor/Connected.tsx
@@ -4,18 +4,22 @@ import { useParams } from "react-router";
 import { AppState } from "reducers";
 import { isNil } from "lodash";
 import Button from "components/ads/Button";
-import { getDatasource } from "selectors/entitiesSelector";
+import { getDatasource, getPlugin } from "selectors/entitiesSelector";
 import { Colors } from "constants/Colors";
 import { HeaderIcons } from "icons/HeaderIcons";
 import styled from "styled-components";
 import { createActionRequest } from "actions/actionActions";
 import { createNewQueryName } from "utils/AppsmithUtils";
 import { getCurrentPageId } from "selectors/editorSelectors";
-import { QueryAction } from "entities/Action";
+import { ApiActionConfig, Action } from "entities/Action";
 import { renderDatasourceSection } from "./DatasourceSection";
 import { OnboardingStep } from "constants/OnboardingConstants";
 import { inOnboarding } from "sagas/OnboardingSagas";
 import OnboardingIndicator from "components/editorComponents/Onboarding/Indicator";
+import { Toaster } from "components/ads/Toast";
+import { Variant } from "components/ads/common";
+import { ERROR_ADD_API_INVALID_URL } from "constants/messages";
+import { DEFAULT_API_ACTION_CONFIG } from "constants/ApiEditorConstants";
 
 const ConnectedText = styled.div`
   color: ${Colors.OXFORD_BLUE};
@@ -69,8 +73,32 @@ function Connected() {
   const datasourceFormConfigs = useSelector(
     (state: AppState) => state.entities.plugins.formConfigs,
   );
+
+  const plugin = useSelector((state: AppState) =>
+    getPlugin(state, datasource?.pluginId ?? ""),
+  );
+
   const createQueryAction = useCallback(() => {
     const newQueryName = createNewQueryName(actions, currentPageId || "");
+
+    if (
+      plugin?.type === "API" &&
+      (!datasource ||
+        !datasource.datasourceConfiguration ||
+        !datasource.datasourceConfiguration.url)
+    ) {
+      Toaster.show({
+        text: ERROR_ADD_API_INVALID_URL(),
+        variant: Variant.danger,
+      });
+      return;
+    }
+
+    const headers = datasource?.datasourceConfiguration?.headers ?? [];
+    const defaultApiActionConfig: ApiActionConfig = {
+      ...DEFAULT_API_ACTION_CONFIG,
+      headers: headers.length ? headers : DEFAULT_API_ACTION_CONFIG.headers,
+    };
     let payload = {
       name: newQueryName,
       pageId: currentPageId,
@@ -78,12 +106,13 @@ function Connected() {
       datasource: {
         id: datasource?.id,
       },
-      actionConfiguration: {},
+      actionConfiguration: plugin?.type === "API" ? defaultApiActionConfig : {},
       eventData: {
         actionType: "Query",
         from: "datasource-pane",
       },
-    } as Partial<QueryAction>; // TODO: refactor later. Handle case for undefined datasource before we reach here.
+    } as Partial<Action>;
+
     if (datasource)
       if (isInOnboarding) {
         // If in onboarding and tooltip is being shown
