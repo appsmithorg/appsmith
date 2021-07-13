1ec9071cd2f806593e9f1057770291f5036fc82c
Abhinav Jha <abhinav@appsmith.com>
2021-07-13 10:36:27 +0530

    Re-enable unit tests

--- app/client/src/components/editorComponents/CodeEditor/CodeEditor.test.tsx
+++ app/client/src/components/editorComponents/CodeEditor/CodeEditor.test.tsx
@@ -1,76 +1,73 @@
-// // import CodeEditor from "./index";
-// // import store from "store";
-// // import TestRenderer from "react-test-renderer";
-// import React from "react";
-// // import { Provider } from "react-redux";
+import CodeEditor from "./index";
+import store from "store";
+import TestRenderer from "react-test-renderer";
+import React from "react";
+import { Provider } from "react-redux";
 
-// // import EvaluatedValuePopup from "./EvaluatedValuePopup";
-// // import { ThemeProvider } from "styled-components";
-// // import { theme, light } from "constants/DefaultTheme";
-// // import {
-// //   EditorSize,
-// //   EditorTheme,
-// //   TabBehaviour,
-// //   EditorModes,
-// // } from "./EditorConfig";
+import EvaluatedValuePopup from "./EvaluatedValuePopup";
+import { ThemeProvider } from "styled-components";
+import { theme, light } from "constants/DefaultTheme";
+import {
+  EditorSize,
+  EditorTheme,
+  TabBehaviour,
+  EditorModes,
+} from "./EditorConfig";
 
-// describe("CodeEditor", () => {
-//   it("should check EvaluatedValuePopup's hideEvaluatedValue  is false when hideEvaluatedValue is passed as false to codeditor", () => {
-//     // const finalTheme = { ...theme, colors: { ...theme.colors, ...light } };
-//     // const testRenderer = TestRenderer.create(
-//     //   <Provider store={store}>
-//     //     <ThemeProvider theme={finalTheme}>
-//     //       <CodeEditor
-//     //         additionalDynamicData={{}}
-//     //         hideEvaluatedValue={false}
-//     //         input={{
-//     //           value: "",
-//     //           onChange: () => {
-//     //             //
-//     //           },
-//     //         }}
-//     //         mode={EditorModes.TEXT}
-//     //         size={EditorSize.COMPACT}
-//     //         tabBehaviour={TabBehaviour.INDENT}
-//     //         theme={EditorTheme.LIGHT}
-//     //       />
-//     //     </ThemeProvider>
-//     //   </Provider>,
-//     // );
-//     // const testInstance = testRenderer.root;
-//     // expect(
-//     //   testInstance.findByType(EvaluatedValuePopup).props.hideEvaluatedValue,
-//     // ).toBe(false);
-//   });
+describe("CodeEditor", () => {
+  it("should check EvaluatedValuePopup's hideEvaluatedValue  is false when hideEvaluatedValue is passed as false to codeditor", () => {
+    const finalTheme = { ...theme, colors: { ...theme.colors, ...light } };
+    const testRenderer = TestRenderer.create(
+      <Provider store={store}>
+        <ThemeProvider theme={finalTheme}>
+          <CodeEditor
+            additionalDynamicData={{}}
+            hideEvaluatedValue={false}
+            input={{
+              value: "",
+              onChange: () => {
+                //
+              },
+            }}
+            mode={EditorModes.TEXT}
+            size={EditorSize.COMPACT}
+            tabBehaviour={TabBehaviour.INDENT}
+            theme={EditorTheme.LIGHT}
+          />
+        </ThemeProvider>
+      </Provider>,
+    );
+    const testInstance = testRenderer.root;
+    expect(
+      testInstance.findByType(EvaluatedValuePopup).props.hideEvaluatedValue,
+    ).toBe(false);
+  });
 
-//   it("should check EvaluatedValuePopup's hideEvaluatedValue is true when hideEvaluatedValue is passed as true to codeditor", () => {
-//     //   const finalTheme = { ...theme, colors: { ...theme.colors, ...light } };
-//     //   const testRenderer = TestRenderer.create(
-//     //     <Provider store={store}>
-//     //       <ThemeProvider theme={finalTheme}>
-//     //         <CodeEditor
-//     //           additionalDynamicData={{}}
-//     //           hideEvaluatedValue
-//     //           input={{
-//     //             value: "",
-//     //             onChange: () => {
-//     //               //
-//     //             },
-//     //           }}
-//     //           mode={EditorModes.TEXT}
-//     //           size={EditorSize.COMPACT}
-//     //           tabBehaviour={TabBehaviour.INDENT}
-//     //           theme={EditorTheme.LIGHT}
-//     //         />
-//     //       </ThemeProvider>
-//     //     </Provider>,
-//     //   );
-//     //   const testInstance = testRenderer.root;
-//     //   expect(
-//     //     testInstance.findByType(EvaluatedValuePopup).props.hideEvaluatedValue,
-//     //   ).toBe(true);
-//   });
-// });
-it("does nothing. needs implementing", () => {
-  expect(1 + 1).toEqual(2);
+  it("should check EvaluatedValuePopup's hideEvaluatedValue is true when hideEvaluatedValue is passed as true to codeditor", () => {
+    const finalTheme = { ...theme, colors: { ...theme.colors, ...light } };
+    const testRenderer = TestRenderer.create(
+      <Provider store={store}>
+        <ThemeProvider theme={finalTheme}>
+          <CodeEditor
+            additionalDynamicData={{}}
+            hideEvaluatedValue
+            input={{
+              value: "",
+              onChange: () => {
+                //
+              },
+            }}
+            mode={EditorModes.TEXT}
+            size={EditorSize.COMPACT}
+            tabBehaviour={TabBehaviour.INDENT}
+            theme={EditorTheme.LIGHT}
+          />
+        </ThemeProvider>
+      </Provider>,
+    );
+    const testInstance = testRenderer.root;
+    expect(
+      testInstance.findByType(EvaluatedValuePopup).props.hideEvaluatedValue,
+    ).toBe(true);
+  });
 });
