76562c0147043b8b2dfc26e0795844a43cfe6a99
bhavin <techbhavin@gmail.com>
2021-07-14 15:16:31 +0530

    updates on cypress tests

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Input_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Input_spec.js
@@ -139,18 +139,11 @@ describe("Input Widget Functionality", function() {
       .clear();
   });
 
-  it("Add new Input", () => {
-    cy.get(commonlocators.addWidget).click();
-    cy.dragAndDropToCanvas("inputwidget", { x: 300, y: 300 });
-    cy.get(".t--widget-inputwidget").should("exist");
-  });
-
   it("Input label wrapper do not show if lable and tooltip is empty", () => {
     cy.get(".t--input-label-wrapper").should("not.exist");
   });
 
   it("Input label renders if label prop is not empty", () => {
-    cy.openPropertyPane("inputwidget");
     // enter label in property pan
     cy.get(widgetsPage.inputLabelControl).type("Label1");
     // test if label shows up with correct text
@@ -158,7 +151,6 @@ describe("Input Widget Functionality", function() {
   });
 
   it("Input tooltip renders if tooltip prop is not empty", () => {
-    cy.openPropertyPane("inputwidget");
     // enter tooltip in property pan
     cy.get(widgetsPage.inputTooltipControl).type("Helpfull text for input");
     // tooltip help icon shows
