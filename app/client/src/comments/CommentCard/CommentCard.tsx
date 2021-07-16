d1abba0e12def0e6d7cfc4e7a700ce6dc0f79e38
Rishabh Saxena <rishabh.robben@gmail.com>
2021-07-16 11:21:54 +0530

    fix mentions list: set max height, overflow ellipses

--- app/client/src/comments/CommentCard/CommentCard.tsx
+++ app/client/src/comments/CommentCard/CommentCard.tsx
@@ -89,7 +89,7 @@ const UserName = styled.span`
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 2; /* number of lines to show */
+  -webkit-line-clamp: 1; /* number of lines to show */
   -webkit-box-orient: vertical;
 `;
 
