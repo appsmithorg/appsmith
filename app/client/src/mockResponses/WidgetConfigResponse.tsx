fa6c64347f4463c3d2de0bc327d7d04cf904d3e6
bhavin <techbhavin@gmail.com>
2021-07-17 17:26:21 +0530

    set defaultSelectedRow prop of table 0

--- app/client/src/mockResponses/WidgetConfigResponse.tsx
+++ app/client/src/mockResponses/WidgetConfigResponse.tsx
@@ -154,6 +154,7 @@ const WidgetConfigResponse: WidgetConfigReducerState = {
     TABLE_WIDGET: {
       rows: 7 * GRID_DENSITY_MIGRATION_V1,
       columns: 9 * GRID_DENSITY_MIGRATION_V1,
+      defaultSelectedRow: 0,
       label: "Data",
       widgetName: "Table",
       searchKey: "",
