137021e4920e61f877b8b99772df308436aed324
hetunandu <hetu@appsmith.com>
2021-07-19 15:11:23 +0530

    WIP

--- app/client/src/workers/Actions.ts
+++ app/client/src/workers/Actions.ts
@@ -1,17 +1,115 @@
+/*
+ * An AppsmithPromise is a mock promise class to replicate promise functionalities
+ * in the Appsmith world.
+ *
+ * To mimic the async nature of promises, we will return back
+ * action descriptors that get resolved in the main thread and then go back to action
+ * execution flow as the workflow is designed.
+ *
+ * Whenever an async call needs to run, it is wrapped around this promise descriptor
+ * and sent to the main thread to execute.
+ *
+ * new Promise(() => {
+ *  return Api1.run()
+ * })
+ * .then(() => {
+ *  return Api2.run()
+ * })
+ * .catch(() => {
+ *  return showMessage('An Error Occurred', 'error')
+ * })
+ *
+ * {
+ *  type: "APPSMITH_PROMISE",
+ *  payload: [{
+ *    type: "EXECUTE_ACTION",
+ *    payload: { actionId: "..." }
+ *   }],
+ *   then: "() => { return Api2.run() }",
+ *   catch: "() => { return showMessage('An Error Occurred', 'error) }"
+ * }
+ *
+ *
+ *
+ * */
+
 import { ActionDescription } from "entities/DataTree/dataTreeFactory";
 
+enum states {
+  pending = "Pending",
+  resolved = "Resolved",
+  rejected = "Rejected",
+}
+
+type Executor = (resolve: Resolve, reject: Reject) => unknown;
+type Resolve = (value: unknown) => void;
+type Reject = (value: unknown) => void;
+
 class AppsmithPromise {
-  action?: ActionDescription<any>;
-  constructor(action: ActionDescription<any>) {
-    this.action = action;
-  }
-  parsed = () => {
-    return this.action;
-  };
-  all = (promises: AppsmithPromise[]) => {
-    return {
-      type: "PROMISE_ALL",
-      payload: promises.map((promise) => promise.parsed()),
+  constructor(executor: Executor) {
+    const tryCall = (callback) =>
+      AppsmithPromise.try(() => callback(this.value));
+    const laterCalls = [];
+    const callLater = (getMember) => (callback) =>
+      new AppsmithPromise((resolve) =>
+        laterCalls.push(() => resolve(getMember()(callback))),
+      );
+    const members = {
+      [states.resolved]: {
+        state: states.resolved,
+        then: tryCall,
+        catch: (_) => this,
+      },
+      [states.rejected]: {
+        state: states.rejected,
+        then: (_) => this,
+        catch: tryCall,
+      },
+      [states.pending]: {
+        state: states.pending,
+        then: callLater(() => this.then),
+        catch: callLater(() => this.catch),
+      },
     };
-  };
+    const changeState = (state) => Object.assign(this, members[state]);
+    const apply = (value, state) => {
+      if (this.state === states.pending) {
+        this.value = value;
+        changeState(state);
+        for (const laterCall of laterCalls) {
+          laterCall();
+        }
+      }
+    };
+
+    const getCallback = (state) => (value) => {
+      if (value instanceof AppsmithPromise && state === states.resolved) {
+        value.then((value) => apply(value, states.resolved));
+        value.catch((value) => apply(value, states.rejected));
+      } else {
+        apply(value, state);
+      }
+    };
+
+    const resolve = getCallback(states.resolved);
+    const reject = getCallback(states.rejected);
+    changeState(states.pending);
+    try {
+      executor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+
+  static resolve(value) {
+    return new AppsmithPromise((resolve) => resolve(value));
+  }
+
+  static reject(value) {
+    return new AppsmithPromise((_, reject) => reject(value));
+  }
+
+  static try(callback) {
+    return new AppsmithPromise((resolve) => resolve(callback()));
+  }
 }
