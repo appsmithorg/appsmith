2b32d71d3c2b03ecfa649ce7cdbbdbf14ffedc78
Akash N <akash@codemonk.in>
2021-07-19 12:43:24 +0530

    Increase width to 90%

--- app/client/src/components/formControls/DynamicTextFieldControl.tsx
+++ app/client/src/components/formControls/DynamicTextFieldControl.tsx
@@ -25,7 +25,7 @@ import { actionPathFromName } from "components/formControls/utils";
 import { EvaluationSubstitutionType } from "entities/DataTree/dataTreeFactory";
 
 const Wrapper = styled.div`
-  width: 60%;
+  width: 90%;
   .dynamic-text-field {
     border-radius: 4px;
     font-size: 14px;
