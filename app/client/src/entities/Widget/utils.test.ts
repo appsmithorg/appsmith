71f10508706204b1215798fd79469ae025da065d
vicky.bansal@primathon.in <vicky.bansal@primathon.in>
2021-07-22 14:38:56 +0530

    Fix unit test

--- app/client/src/entities/Widget/utils.test.ts
+++ app/client/src/entities/Widget/utils.test.ts
@@ -159,6 +159,7 @@ describe("getAllPathsFromPropertyConfig", () => {
         onRowSelected: true,
         onPageChange: true,
         onSearchTextChanged: true,
+        onSort: true,
         onPageSizeChange: true,
         "primaryColumns.status.onClick": true,
       },
