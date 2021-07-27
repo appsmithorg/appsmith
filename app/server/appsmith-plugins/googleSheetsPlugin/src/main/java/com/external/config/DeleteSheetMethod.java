51c686c9d60e6cc29c5b741f87f2c19680eb204a
Nidhi <nidhi.nair93@gmail.com>
2021-07-27 13:12:12 +0530

    Delete method messages #5302

--- app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/config/DeleteSheetMethod.java
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/config/DeleteSheetMethod.java
@@ -138,7 +138,12 @@ public class DeleteSheetMethod implements Method {
                     "Missing a valid response object.");
         }
 
-        return this.objectMapper.valueToTree(Map.of("message", "Deleted sheet successfully!"));
+        String errorMessage = "Deleted spreadsheet successfully!";
+        if (GoogleSheets.SHEET.equalsIgnoreCase(methodConfig.getDeleteFormat())) {
+            errorMessage = "Deleted sheet " + methodConfig.getSheetName() + " successfully!";
+        }
+
+        return this.objectMapper.valueToTree(Map.of("message", errorMessage));
     }
 
 }
