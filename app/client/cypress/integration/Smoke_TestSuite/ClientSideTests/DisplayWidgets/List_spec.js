42ced2772bfa2804deef77ea6f3220b0be3b60b6
arslanhaiderbuttar <arslan@appsmith.com>
2021-07-27 17:24:47 +0500

    Update List_spec.js

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/List_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/List_spec.js
@@ -14,7 +14,7 @@ describe("Button Widget Functionality", function() {
     //Changing the style of the button from the property pane and verify it's color.
     // Change to Secondary button sytle
     cy.changeButtonStyle(2, "rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 0)");
-    cy.get(publishPage.backToEitor).click({ force: true });
+    cy.get(publishPage.backToEditor).click({ force: true });
     // Change to Danger button sytl
     cy.openPropertyPane("buttonwidget");
     cy.changeButtonStyle(3, "rgb(179, 3, 56)", "rgb(139, 2, 43)");
