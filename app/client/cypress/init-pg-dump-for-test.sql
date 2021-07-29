dbed59b68bacc47a11e80a5a6f7e0d6d8cd6f1d9
yatinappsmith <yatin@appsmith.com>
2021-07-29 15:23:59 +0530

    Using local Postgres database instead of remote

--- app/client/cypress/init-pg-dump-for-test.sql
+++ app/client/cypress/init-pg-dump-for-test.sql
@@ -50,4 +50,6 @@ values (3,	'New Config',	'{"key": "val1"}',	1,	'2020-08-26 11:14:28.458209+00', 
 insert into public.users (id, name, "createdAt", "updatedAt", status, gender, avatar, email, address, role, dob, "phoneNo") values 
 (7, 'Test user 7', '2019-08-07 21:36:27+00', '2019-10-21 03:23:42+00', 'APPROVED', 'Male', 'https://robohash.org/quiofficiadicta.jpg?size=100x100&set=set1' ,'xkainz6@ihg.com', '19624 Scofield Way', 'Admin','1993-08-14', ''),
 (8, 'Test user 8', '2019-08-07 21:36:27+00', '2019-10-21 03:23:42+00', 'APPROVED', 'Male', 'https://robohash.org/quiofficiadicta.jpg?size=100x100&set=set1' ,'xkainz6@ihg.com', '19624 Scofield Way', 'Admin','1993-08-14', ''),
-(9, 'Test user 9', '2019-08-07 21:36:27+00', '2019-10-21 03:23:42+00', 'APPROVED', 'Male', 'https://robohash.org/quiofficiadicta.jpg?size=100x100&set=set1' ,'xkainz6@ihg.com', '19624 Scofield Way', 'Admin','1993-08-14', '')
+(9, 'Test user 9', '2019-08-07 21:36:27+00', '2019-10-21 03:23:42+00', 'APPROVED', 'Male', 'https://robohash.org/quiofficiadicta.jpg?size=100x100&set=set1' ,'xkainz6@ihg.com', '19624 Scofield Way', 'Admin','1993-08-14', '');
+
+CREATE DATABASE fakeapi;
