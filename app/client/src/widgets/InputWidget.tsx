4536274f6e4fbbf9e91c69892d37a8deb8965045
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2021-07-31 04:03:17 +0530

    Remove type from InputTypes in InputWidget

--- app/client/src/widgets/InputWidget.tsx
+++ app/client/src/widgets/InputWidget.tsx
@@ -409,7 +409,7 @@ class InputWidget extends BaseWidget<InputWidgetProps, WidgetState> {
   }
 }
 
-export const InputTypes: { [key: string]: string } = {
+export const InputTypes = {
   TEXT: "TEXT",
   NUMBER: "NUMBER",
   INTEGER: "INTEGER",
