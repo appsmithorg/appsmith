69fce60f19dbc94ba220ba0feb5dcbd4b41448f7
Rishabh Saxena <rishabh.robben@gmail.com>
2021-08-03 00:39:07 +0530

    Dont show tour when skipped

--- app/client/src/comments/CommentsShowcaseCarousel/index.tsx
+++ app/client/src/comments/CommentsShowcaseCarousel/index.tsx
@@ -33,6 +33,8 @@ import { getCanManage } from "utils/helpers";
 import stepOneThumbnail from "assets/images/comments-onboarding/thumbnails/step-1.jpg";
 import stepTwoThumbnail from "assets/images/comments-onboarding/thumbnails/step-2.jpg";
 
+import { setCommentModeInUrl } from "pages/Editor/ToggleModeButton";
+
 const getBanner = (step: number) =>
   `${S3_BUCKET_URL}/comments/step-${step}.png`;
 
@@ -130,13 +132,13 @@ function IntroStep(props: {
 const IntroStepThemed = withTheme(IntroStep);
 
 const getSteps = (
-  onSubmitProfileForm: any,
   isSubmitProfileFormDisabled: boolean,
-  startTutorial: () => void,
+  finalSubmit: () => void,
   initialProfileFormValues: { emailAddress?: string; displayName?: string },
   emailDisabled: boolean,
   showEditorSteps: boolean,
   onSkip: () => void,
+  isSkipped?: boolean,
 ) => {
   const introSteps = showEditorSteps ? introStepsEditor : introStepsViewer;
 
@@ -154,11 +156,8 @@ const getSteps = (
         isSubmitDisabled: isSubmitProfileFormDisabled,
         initialValues: initialProfileFormValues,
         emailDisabled,
-        nextBtnText: "Start Tutorial",
-        onSubmit: () => {
-          startTutorial();
-          onSubmitProfileForm();
-        },
+        nextBtnText: isSkipped ? "Submit" : "Start Tutorial",
+        onSubmit: finalSubmit,
         hideBackBtn: true,
       },
     },
@@ -172,6 +171,7 @@ export default function CommentsShowcaseCarousel() {
   const profileFormErrors = useSelector(getFormSyncErrors("PROFILE_FORM"));
   const isSubmitDisabled = Object.keys(profileFormErrors).length !== 0;
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isSkipped, setIsSkipped] = useState(false);
 
   const currentUser = useSelector(getCurrentUser);
   const { email, name } = currentUser || {};
@@ -190,24 +190,35 @@ export default function CommentsShowcaseCarousel() {
   const currentOrg = useOrg(id);
   const canManage = getCanManage(currentOrg);
 
-  const startTutorial = () => {
-    const tourType = canManage
-      ? TourType.COMMENTS_TOUR_EDIT_MODE
-      : TourType.COMMENTS_TOUR_PUBLISHED_MODE;
-
-    dispatch(setActiveTour(tourType));
+  const finalSubmit = async () => {
     dispatch(hideCommentsIntroCarousel());
-    setCommentsIntroSeen(true);
+    await setCommentsIntroSeen(true);
+
+    if (!isSkipped) {
+      const tourType = canManage
+        ? TourType.COMMENTS_TOUR_EDIT_MODE
+        : TourType.COMMENTS_TOUR_PUBLISHED_MODE;
+      dispatch(setActiveTour(tourType));
+    } else {
+      setCommentModeInUrl(true);
+    }
+
+    onSubmitProfileForm();
+  };
+
+  const onSkip = () => {
+    setActiveIndex(2);
+    setIsSkipped(true);
   };
 
   const steps = getSteps(
-    onSubmitProfileForm,
     isSubmitDisabled,
-    startTutorial,
+    finalSubmit,
     initialProfileFormValues,
     !!email,
     canManage,
-    () => setActiveIndex(2),
+    onSkip,
+    isSkipped,
   );
 
   if (steps.length === 0 || !isIntroCarouselVisible) return null;
