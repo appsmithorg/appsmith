62bc166e5539da516d5eb2051a8d56bab3c96908
Anagh Hegde <anagh@appsmith.com>
2021-08-03 11:55:14 +0530

    Check the Authentication type before setting the password

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/MockDataServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/MockDataServiceImpl.java
@@ -182,7 +182,8 @@ public class MockDataServiceImpl implements MockDataService {
         return datasourceService.create(datasource)
                 .onErrorResume(DuplicateKeyException.class, error -> {
                     if (error.getMessage() != null
-                            && error.getMessage().contains("organization_datasource_deleted_compound_index")) {
+                            && error.getMessage().contains("organization_datasource_deleted_compound_index")
+                            && datasource.getDatasourceConfiguration().getAuthentication() instanceof DBAuth) {
                         ((DBAuth) datasource.getDatasourceConfiguration().getAuthentication()).setPassword(finalPassword);
                         return createSuffixedDatasource(datasource, name, 1 + suffix);
                     }
