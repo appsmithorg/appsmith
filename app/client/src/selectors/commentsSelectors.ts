d09d5f8b1b8bdbfcb627149a17b25880d3fafaa3
Rishabh Saxena <rishabh.robben@gmail.com>
2021-08-05 11:56:27 +0530

    Check if comments are enabled for comment intro modal (#6395)

--- app/client/src/selectors/commentsSelectors.ts
+++ app/client/src/selectors/commentsSelectors.ts
@@ -163,4 +163,5 @@ export const visibleCommentThreadSelector = (state: AppState) =>
   state.ui.comments.visibleCommentThreadId;
 
 export const isIntroCarouselVisibleSelector = (state: AppState) =>
-  state.ui.comments.isIntroCarouselVisible;
+  state.ui.comments.isIntroCarouselVisible &&
+  areCommentsEnabledForUserAndApp(state);
