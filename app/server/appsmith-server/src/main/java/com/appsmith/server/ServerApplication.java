9be3ae5e4a9905ab73beaca53a02fd8f71cf0e18
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-08-09 15:13:18 +0530

    Remove unused updates to app startup

--- app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
@@ -1,12 +1,7 @@
 package com.appsmith.server;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.env.StandardEnvironment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -14,38 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ServerApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ServerApplication.class)
-                // .initializers(new CustomInit())
-                .run(args);
-        // SpringApplication.run(ServerApplication.class, args);
-    }
-
-    static class CustomInit implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        @Override
-        public void initialize(ConfigurableApplicationContext context) {
-            final ConfigurableEnvironment environment = context.getEnvironment();
-            environment.getPropertySources().addAfter(
-                    StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME,
-                    new CustomPropertySource("custom-env")
-            );
-        }
-    }
-
-    static class CustomPropertySource extends PropertySource<CustomLoader> {
-        public CustomPropertySource(String name) {
-            super(name);
-        }
-
-        @Override
-        public Object getProperty(String s) {
-            return s + " value";
-        }
-    }
-
-    static class CustomLoader {
-        public Object getValue(String key) {
-            return key;
-        }
+        SpringApplication.run(ServerApplication.class, args);
     }
 
 }
