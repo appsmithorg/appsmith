aed86bb58cb2abcd317456f57876185a6da44c70
Pranav Kanade <pranav@appsmith.com>
2021-08-09 15:23:26 +0530

    updated collab event names

--- app/client/src/constants/AppCollabConstants.ts
+++ app/client/src/constants/AppCollabConstants.ts
@@ -1,5 +1,5 @@
 export const APP_COLLAB_EVENTS = {
-  START_EDITING_APP: "collab:start_edit_app",
-  STOP_EDITING_APP: "collab:leave_edit_app",
-  LIST_ONLINE_APP_EDITORS: "collab:online_app_editors",
+  START_EDITING_APP: "collab:start_edit",
+  STOP_EDITING_APP: "collab:leave_edit",
+  LIST_ONLINE_APP_EDITORS: "collab:online_editors",
 };
