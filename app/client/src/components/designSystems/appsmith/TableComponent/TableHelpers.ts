46b984ae1ad949883197fb62022fbc70b4b6b2c7
Hetu Nandu <hetunandu@gmail.com>
2021-08-10 15:51:27 +0530

    Hotfix Table Widget crashes when there is a cyclical dependency (#6505)

--- app/client/src/components/designSystems/appsmith/TableComponent/TableHelpers.ts
+++ app/client/src/components/designSystems/appsmith/TableComponent/TableHelpers.ts
@@ -10,16 +10,18 @@ export const removeSpecialChars = (value: string, limit?: number) => {
 };
 
 export const getAllTableColumnKeys = (
-  tableData: Array<Record<string, unknown>>,
+  tableData?: Array<Record<string, unknown>>,
 ) => {
   const columnKeys: string[] = [];
-  for (let i = 0, tableRowCount = tableData.length; i < tableRowCount; i++) {
-    const row = tableData[i];
-    for (const key in row) {
-      // Replace all special characters to _, limit key length to 200 characters.
-      const sanitizedKey = removeSpecialChars(key, 200);
-      if (!columnKeys.includes(sanitizedKey)) {
-        columnKeys.push(sanitizedKey);
+  if (tableData) {
+    for (let i = 0, tableRowCount = tableData.length; i < tableRowCount; i++) {
+      const row = tableData[i];
+      for (const key in row) {
+        // Replace all special characters to _, limit key length to 200 characters.
+        const sanitizedKey = removeSpecialChars(key, 200);
+        if (!columnKeys.includes(sanitizedKey)) {
+          columnKeys.push(sanitizedKey);
+        }
       }
     }
   }
