24c6104263bfcc801b4345bc8e658b0b3dd2e8da
Rishabh Saxena <rishabh.robben@gmail.com>
2021-08-17 23:31:04 +0530

    Components by tab

--- app/client/src/pages/Editor/gitSync/constants.ts
+++ app/client/src/pages/Editor/gitSync/constants.ts
@@ -8,25 +8,33 @@ import {
   SETTINGS,
 } from "constants/messages";
 
+export enum MENU_ITEM {
+  GIT_CONNECTION = "GIT_CONNECTION",
+  DEPLOY = "DEPLOY",
+  MERGE = "MERGE",
+  SHARE_APPLICATION = "SHARE_APPLICATION",
+  SETTINGS = "SETTINGS",
+}
+
 export const MENU_ITEMS: TabProp[] = [
   {
-    key: "GIT_CONNECTION",
+    key: MENU_ITEM.GIT_CONNECTION,
     title: createMessage(GIT_CONNECTION),
   },
   {
-    key: "DEPLOY",
+    key: MENU_ITEM.DEPLOY,
     title: createMessage(DEPLOY),
   },
   {
-    key: "MERGE",
+    key: MENU_ITEM.MERGE,
     title: createMessage(MERGE),
   },
   {
-    key: "SHARE_APPLICATION",
+    key: MENU_ITEM.SHARE_APPLICATION,
     title: createMessage(SHARE_APPLICATION),
   },
   {
-    key: "SETTINGS",
+    key: MENU_ITEM.SETTINGS,
     title: createMessage(SETTINGS),
   },
 ];
