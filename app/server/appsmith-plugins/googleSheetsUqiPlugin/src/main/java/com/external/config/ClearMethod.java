fe5f1191f7e67bba759491a7bbf4048a4182b308
Nidhi <nidhi.nair93@gmail.com>
2021-08-17 14:29:10 +0530

    Duplicate google sheets uqi plugin

---
+++ app/server/appsmith-plugins/googleSheetsUqiPlugin/src/main/java/com/external/config/ClearMethod.java
@@ -0,0 +1,50 @@
+package com.external.config;
+
+import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginError;
+import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * API reference: https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/clear
+ */
+public class ClearMethod implements Method {
+
+    ObjectMapper objectMapper;
+
+    public ClearMethod(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public boolean validateMethodRequest(MethodConfig methodConfig) {
+        if (methodConfig.getSpreadsheetId() == null || methodConfig.getSpreadsheetId().isBlank()) {
+            throw new AppsmithPluginException(AppsmithPluginError.PLUGIN_ERROR, "Missing required field Spreadsheet Url");
+        }
+        if (methodConfig.getSpreadsheetRange() == null || methodConfig.getSpreadsheetRange().isBlank()) {
+            throw new AppsmithPluginException(AppsmithPluginError.PLUGIN_ERROR, "Missing required field Data Range");
+        }
+        return true;
+
+    }
+
+    @Override
+    public WebClient.RequestHeadersSpec<?> getClient(WebClient webClient, MethodConfig methodConfig) {
+
+
+        UriComponentsBuilder uriBuilder = getBaseUriBuilder(this.BASE_SHEETS_API_URL,
+                methodConfig.getSpreadsheetId() /* spreadsheet Id */
+                        + "/values/"
+                        + methodConfig.getSpreadsheetRange() /* spreadsheet Range */
+                        + ":clear"
+        );
+
+        return webClient.method(HttpMethod.POST)
+                .uri(uriBuilder.build(true).toUri())
+                .body(BodyInserters.empty());
+    }
+
+}
---
+++ app/server/appsmith-plugins/googleSheetsUqiPlugin/src/main/java/com/external/constants/GoogleSheets.java
@@ -0,0 +1,8 @@
+package com.external.constants;
+
+public class GoogleSheets {
+
+    public static final String ROW_INDEX = "rowIndex";
+    public static final String SHEET = "SHEET";
+    public static final String SPREADSHEET = "SPREADSHEET";
+}
