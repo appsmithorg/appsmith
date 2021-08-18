18d151cb98c72e3b85b8f9869d8398654a17214a
Nidhi <nidhi.nair93@gmail.com>
2021-08-18 15:19:39 +0530

    Remove extra annotation

--- app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/dtos/MultipartFormDataDTO.java
+++ app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/dtos/MultipartFormDataDTO.java
@@ -1,6 +1,5 @@
 package com.external.dtos;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class MultipartFormDataDTO {
     String name;
     String type;
