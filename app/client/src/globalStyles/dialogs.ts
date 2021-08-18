fd0d2ed020c3816b58a3a66fbd9fac20870e4770
Rishabh Saxena <rishabh.robben@gmail.com>
2021-08-18 13:54:54 +0530

    typo

--- app/client/src/globalStyles/dialogs.ts
+++ app/client/src/globalStyles/dialogs.ts
@@ -3,7 +3,7 @@ import { Classes } from "@blueprintjs/core";
 import { Classes as GitSyncClasses } from "pages/Editor/gitSync/constants";
 
 export const DialogStyles = createGlobalStyle`
-  .${GitSyncClasses} .${Classes.DIALOG_BODY} {
-    padding: 0 !important;
+  .${GitSyncClasses.GIT_SYNC_MODAL} .${Classes.DIALOG_BODY}.${Classes.DIALOG_BODY} {
+    padding: 0;
   }
 `;
