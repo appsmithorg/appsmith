c58361eb65993b70435f0e6f351abb386b1fe0d5
Albin <albin@appsmith.com>
2021-08-19 10:00:22 +0530

    button story added

--- app/client/src/components/stories/Button.stories.tsx
+++ app/client/src/components/stories/Button.stories.tsx
@@ -1,31 +1,59 @@
 import React from "react";
-import Button, { Size, Category } from "components/ads/Button";
-import { withKnobs, select, boolean, text } from "@storybook/addon-knobs";
+import Button, { Size, Category, ButtonProps } from "components/ads/Button";
 import { withDesign } from "storybook-addon-designs";
-import { StoryWrapper, Variant } from "components/ads/common";
+import { Variant } from "components/ads/common";
 import { IconCollection, IconName } from "components/ads/Icon";
+import { storyName } from "./config/constants";
+import { controlType, statusType } from "./config/types";
 
 export default {
-  title: "Button",
+  title: storyName.platform.form.button.PATH,
   component: Button,
-  decorators: [withKnobs, withDesign],
+  decorators: [withDesign],
+  parameters: {
+    status: {
+      type: statusType.STABLE,
+    },
+  },
 };
 
-export const withDynamicProps = () => (
-  <StoryWrapper>
-    <Button
-      category={select("category", Object.values(Category), Category.primary)}
-      disabled={boolean("Disabled", false)}
-      fill={boolean("fill", false)}
-      icon={select(
-        "Icon name",
-        ["Select icon" as IconName, ...IconCollection],
-        "Select icon" as IconName,
-      )}
-      isLoading={boolean("Loading", false)}
-      size={select("size", Object.values(Size), Size.large)}
-      text={text("text", "Get")}
-      variant={select("variant", Object.values(Variant), Variant.info)}
-    />
-  </StoryWrapper>
-);
+export function Primary(args: ButtonProps) {
+  return <Button {...args} />;
+}
+
+Primary.args = {
+  category: Category.primary,
+  disabled: false,
+  fill: false,
+  icon: "Select icon" as IconName,
+  isLoading: false,
+  size: Size.large,
+  text: "Get",
+  variant: Variant.info,
+  tag: "button",
+};
+
+Primary.argTypes = {
+  category: {
+    control: controlType.SELECT,
+    options: Object.values(Category),
+  },
+  disabled: { control: controlType.BOOLEAN },
+  fill: { control: controlType.BOOLEAN },
+  icon: {
+    control: controlType.SELECT,
+    options: ["Select icon" as IconName, ...IconCollection],
+  },
+  isLoading: { control: controlType.BOOLEAN },
+  size: {
+    control: controlType.SELECT,
+    options: Object.values(Size),
+  },
+  text: { control: controlType.TEXT },
+  variant: {
+    control: controlType.SELECT,
+    options: Object.values(Variant),
+  },
+};
+
+Primary.storyName = storyName.platform.form.button.NAME;
