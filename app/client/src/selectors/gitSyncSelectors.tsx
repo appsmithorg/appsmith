d89bf5c487dcafa9c5cabf5ee1ddf27a6fb24463
Rishabh Saxena <rishabh.robben@gmail.com>
2021-08-19 21:04:43 +0530

    Default deploy section state (#6740)

--- app/client/src/selectors/gitSyncSelectors.tsx
+++ app/client/src/selectors/gitSyncSelectors.tsx
@@ -2,3 +2,5 @@ import { AppState } from "reducers";
 
 export const getIsGitSyncModalOpen = (state: AppState) =>
   state.ui.gitSync.isGitSyncModalOpen;
+
+export const getIsGitRepoSetup = () => false;
