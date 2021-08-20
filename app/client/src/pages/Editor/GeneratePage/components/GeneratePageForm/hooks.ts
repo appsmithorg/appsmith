12e7ee0c94cbfc81e5f7ef270ab08740378c36be
Rishabh Rathod <rishabh.rathod@appsmith.com>
2021-08-20 12:27:01 +0530

    Add s3 support for generate CRUD (#6264)
    * Add s3 support for generate CRUD
    - Dropdown enhancement to open options on initial load
    - Hide column selection option for s3
    
    * Refactor the prop name
    
    * Add useS3BucketList hook WIP
    
    * Dropdown enchancement & Fix small issues
    
    * Add fetch all sheets query
    
    * Add Query to get all S3 buckets
    
    * Fix dropdown open issue
    
    * Remove defaultIsOpen prop from dropdown
    
    * Resolve comments
    - Remove debugger
    - mockSheetUrl -> getSheetUrl
    
    * Add S3 cypress test
    
    * Fix cypress test yml config
    
    * Fix generate page cypress test

--- app/client/src/pages/Editor/GeneratePage/components/GeneratePageForm/hooks.ts
+++ app/client/src/pages/Editor/GeneratePage/components/GeneratePageForm/hooks.ts
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { DropdownOptions } from "../constants";
 import { Datasource } from "entities/Datasource";
 import { GenerateCRUDEnabledPluginMap } from "api/PluginApi";
 import { CONNECT_NEW_DATASOURCE_OPTION_ID } from "../DataSourceOption";
+import { useDispatch } from "react-redux";
+import { executeDatasourceQuery } from "actions/datasourceActions";
+import { ResponseMeta } from "api/ApiResponses";
+import { DropdownOption } from "components/ads/Dropdown";
 
 export const FAKE_DATASOURCE_OPTION = {
   CONNECT_NEW_DATASOURCE_OPTION: {
@@ -58,4 +62,75 @@ export const useDatasourceOptions = ({
     setDataSourceOptions(newDataSourceOptions);
   }, [datasources, setDataSourceOptions, generateCRUDSupportedPlugin]);
   return dataSourceOptions;
+};
+
+const payload = [
+  {
+    value: "LIST_BUCKETS",
+  },
+];
+
+export type executeDatasourceQuerySuccessPayload = {
+  responseMeta: ResponseMeta;
+  data: {
+    body: { bucketList: string[] };
+    headers: Record<string, string[]>;
+    statusCode: string;
+    isExecutionSuccess: boolean;
+  };
+};
+
+export const useS3BucketList = () => {
+  const dispatch = useDispatch();
+
+  const [bucketList, setBucketList] = useState<Array<string>>([]);
+  const [isFetchingBucketList, setIsFetchingBucketList] = useState<boolean>(
+    false,
+  );
+  const [failedFetchingBucketList, setFailedFetchingBucketList] = useState<
+    boolean
+  >(false);
+  const onFetchBucketSuccess = useCallback(
+    (payload: executeDatasourceQuerySuccessPayload) => {
+      setIsFetchingBucketList(false);
+      if (payload.data && payload.data.body) {
+        const payloadBody = payload.data.body;
+        const { bucketList: list = [] } = payloadBody;
+        setBucketList(list);
+      }
+    },
+    [setBucketList, setIsFetchingBucketList],
+  );
+
+  const onFetchBucketFailure = useCallback(() => {
+    setIsFetchingBucketList(false);
+    setFailedFetchingBucketList(true);
+  }, [setIsFetchingBucketList]);
+
+  const fetchBucketList = useCallback(
+    ({ selectedDatasource }: { selectedDatasource: DropdownOption }) => {
+      if (selectedDatasource.id) {
+        setIsFetchingBucketList(true);
+        setFailedFetchingBucketList(false);
+        dispatch(
+          executeDatasourceQuery({
+            payload: {
+              datasourceId: selectedDatasource.id,
+              data: payload,
+            },
+            onSuccessCallback: onFetchBucketSuccess,
+            onErrorCallback: onFetchBucketFailure,
+          }),
+        );
+      }
+    },
+    [onFetchBucketSuccess, onFetchBucketFailure, setIsFetchingBucketList],
+  );
+
+  return {
+    bucketList,
+    isFetchingBucketList,
+    failedFetchingBucketList,
+    fetchBucketList,
+  };
 };
