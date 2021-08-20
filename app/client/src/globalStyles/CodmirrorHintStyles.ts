6c6793a03dc78c5c24cd62c36333fbc81da11725
Vinod CG <vnodecg@gmail.com>
2021-08-20 10:44:09 +0530

    Update styling for warnings

--- app/client/src/globalStyles/CodmirrorHintStyles.ts
+++ app/client/src/globalStyles/CodmirrorHintStyles.ts
@@ -263,4 +263,7 @@ export const CodemirrorHintStyles = createGlobalStyle<{
     font-family: ${(props) => props.theme.fonts.text};
     color: #4B4848;
   }
+  .CodeMirror-lint-mark-warning{
+    background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAYAAAC09K7GAAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A/wD/oL2nkwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB9sJDw4cOCW1/KIAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAHElEQVQI12NggIL/DAz/GdA5/xkY/qPKMDAwAADLZwf5rvm+LQAAAABJRU5ErkJggg==")
+  }
 `;
