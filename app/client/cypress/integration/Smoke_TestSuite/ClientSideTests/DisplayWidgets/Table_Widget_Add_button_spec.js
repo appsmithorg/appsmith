cc4d8a2e4ff276e514d64e39cb817ad414d36c12
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2021-08-23 07:45:42 +0100

    fix: add test back

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Widget_Add_button_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Widget_Add_button_spec.js
@@ -10,41 +10,41 @@ describe("Table Widget property pane feature validation", function() {
     cy.addDsl(dsl);
   });
 
-  // it("Table widget with Add button test and validation", function() {
-  //   cy.openPropertyPane("tablewidget");
-  //   // Open column details of "id".
-  //   cy.editColumn("id");
-  //   cy.get(widgetsPage.tableBtn).should("not.exist");
-  //   // Changing column data type to "Button"
-  //   cy.changeColumnType("Button");
-  //   // Changing the computed value (data) to "orderAmount"
-  //   cy.updateComputedValue(testdata.currentRowOrderAmt);
-  //   // Selecting button action to show message
-  //   cy.get(widgetsPage.actionSelect).click();
-  //   cy.get(commonlocators.chooseAction)
-  //     .children()
-  //     .contains("Show Message")
-  //     .click();
-  //   cy.addSuccessMessage("Successful ".concat(testdata.currentRowEmail));
-  //   // Close Property pane
-  //   cy.get(commonlocators.editPropCrossButton).click({ force: true });
+  it("Table widget with Add button test and validation", function() {
+    cy.openPropertyPane("tablewidget");
+    // Open column details of "id".
+    cy.editColumn("id");
+    cy.get(widgetsPage.tableBtn).should("not.exist");
+    // Changing column data type to "Button"
+    cy.changeColumnType("Button");
+    // Changing the computed value (data) to "orderAmount"
+    cy.updateComputedValue(testdata.currentRowOrderAmt);
+    // Selecting button action to show message
+    cy.get(widgetsPage.actionSelect).click();
+    cy.get(commonlocators.chooseAction)
+      .children()
+      .contains("Show Message")
+      .click();
+    cy.addSuccessMessage("Successful ".concat(testdata.currentRowEmail));
+    // Close Property pane
+    cy.get(commonlocators.editPropCrossButton).click({ force: true });
 
-  //   // Validating the button action by clicking
-  //   cy.get(widgetsPage.tableBtn)
-  //     .last()
-  //     .click({ force: true });
-  //   // eslint-disable-next-line cypress/no-unnecessary-waiting
-  //   cy.wait(3000);
-  //   // Validating the toast message
-  //   cy.get(widgetsPage.toastAction).should("be.visible");
-  //   cy.get(widgetsPage.toastActionText)
-  //     .last()
-  //     .invoke("text")
-  //     .then((text) => {
-  //       const someText = text;
-  //       expect(someText).to.equal("Successful tobias.funke@reqres.in");
-  //     });
-  // });
+    // Validating the button action by clicking
+    cy.get(widgetsPage.tableBtn)
+      .last()
+      .click({ force: true });
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(3000);
+    // Validating the toast message
+    cy.get(widgetsPage.toastAction).should("be.visible");
+    cy.get(widgetsPage.toastActionText)
+      .last()
+      .invoke("text")
+      .then((text) => {
+        const someText = text;
+        expect(someText).to.equal("Successful tobias.funke@reqres.in");
+      });
+  });
   it("Table widget add new icon button column", function() {
     // click on Add new Column.
     cy.get(".t--add-column-btn").click();
