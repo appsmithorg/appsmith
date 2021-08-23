ffd40fab2c75d6cdb9123f21cdad3913ac6780f1
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2021-08-23 22:00:08 +0530

    error in console fix

--- app/client/src/sagas/DebuggerSagas.ts
+++ app/client/src/sagas/DebuggerSagas.ts
@@ -397,7 +397,7 @@ function* deleteDebuggerErrorLogSaga(
   const errors: Record<string, Log> = yield select(getDebuggerErrors);
   const error = errors[action.payload.id];
 
-  if (!error.source) return;
+  if (!error || !error.source) return;
 
   const analyticsPayload = {
     entityName: error.source.name,
