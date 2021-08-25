8f975576a882b917e69927ec9fc161458048ed71
hetunandu <hetu@appsmith.com>
2021-08-25 09:01:41 +0530

    protect string passing in copy actions

--- app/client/src/sagas/ActionExecution/CopyActionSaga.ts
+++ app/client/src/sagas/ActionExecution/CopyActionSaga.ts
@@ -1,10 +1,14 @@
 import copy from "copy-to-clipboard";
 import AppsmithConsole from "utils/AppsmithConsole";
 import { CopyToClipboardDescription } from "entities/DataTree/actionTriggers";
+import { TriggerFailureError } from "sagas/ActionExecution/PromiseActionSaga";
 
 export default function copySaga(
   payload: CopyToClipboardDescription["payload"],
 ) {
+  if (typeof payload.data !== "string") {
+    throw new TriggerFailureError("Value to copy is not a string");
+  }
   const result = copy(payload.data, payload.options);
   if (result) {
     AppsmithConsole.info({
