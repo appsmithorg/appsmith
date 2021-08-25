8aea797ff106229ece01fb40de8d63b0be3cdcd2
Albin <albin@appsmith.com>
2021-08-26 00:31:27 +0530

    Toast story UI fixes

--- app/client/.storybook/preview.js
+++ app/client/.storybook/preview.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { DefaultTheme, ThemeProvider } from "./configs";
 import store from "../src/store";
 import { Provider } from "react-redux";
+import GlobalStyles from "../src/globalStyles/index";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -18,6 +19,7 @@ export const decorators = [
     <Provider store={store}>
       <ThemeProvider theme={DefaultTheme}>
         <Story />
+        <GlobalStyles />
       </ThemeProvider>
     </Provider>
   ),
