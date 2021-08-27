42d01c04bf0508b58c2185c54ec5cf5f0eaf8c5b
Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>
2021-08-27 12:37:25 +0530

    add tests for text control

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Replay/Replay_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Replay/Replay_spec.js
@@ -1,5 +1,8 @@
 const commonlocators = require("../../../../locators/commonlocators.json");
 const widgetLocators = require("../../../../locators/publishWidgetspage.json");
+const formWidgetsPage = require("../../../../locators/FormWidgets.json");
+const widgetsPage = require("../../../../locators/Widgets.json");
+const publish = require("../../../../locators/publishWidgetspage.json");
 const dsl = require("../../../../fixtures/replay.json");
 
 describe("Undo/Redo functionality", function() {
@@ -15,20 +18,29 @@ describe("Undo/Redo functionality", function() {
 
     cy.get("body").type(`{${modifierKey}}z`);
     cy.wait(100);
-    cy.get(".t--property-control-disabled label").should(
-      "not.have.class",
-      "checked",
-    );
+    cy.get(`${widgetsPage.disable} label`).should("not.have.class", "checked");
     cy.get(widgetLocators.checkboxWidget + " " + "input").should(
       "not.be.disabled",
     );
 
     cy.get("body").type(`{${modifierKey}}{shift}z`);
     cy.wait(100);
-    cy.get(".t--property-control-disabled label").should(
-      "have.class",
-      "checked",
-    );
+    cy.get(`${widgetsPage.disable} label`).should("have.class", "checked");
     cy.get(widgetLocators.checkboxWidget + " " + "input").should("be.disabled");
+  });
+
+  it("checks undo/redo for input control in property pane", function() {
+    cy.get(widgetsPage.inputLabelControl).type("1");
+    cy.get(widgetsPage.inputLabelControl).contains("Label1");
+
+    cy.get("body").type(`{${modifierKey}}z`);
+    cy.wait(100);
+    cy.get(widgetsPage.inputLabelControl).contains("Label");
+    cy.get(`${publish.checkboxWidget} label`).should("have.text", "Label");
+
+    cy.get("body").type(`{${modifierKey}}{shift}z`);
+    cy.wait(100);
+    cy.get(widgetsPage.inputLabelControl).contains("Label1");
+    cy.get(`${publish.checkboxWidget} label`).should("have.text", "Label1");
   });
 });
