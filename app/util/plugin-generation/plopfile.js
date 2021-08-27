f32b0ee4719242c31406482b1de9d0a79649fce2
Trisha Anand <trisha@appsmith.com>
2021-08-27 17:39:30 +0530

    Completed the plugin generation for today's plugins

--- app/util/plugin-generation/plopfile.js
+++ app/util/plugin-generation/plopfile.js
@@ -1,55 +1,88 @@
-// The following code takes prompt input and generates two files from two different templates
+// The following code takes prompt input and generates all the required files.
 module.exports = function (plop) {
     plop.setGenerator('plugin', {
         prompts: [
-            {
-                type: 'list',
-                name: 'pluginType',
-                message: 'Enter the type of plugin you wish to contribute',
-                choices: ['SQL', 'NoSQL', 'File Storage', 'SAAS']
-            },
+            // {
+            //     type: 'list',
+            //     name: 'pluginType',
+            //     message: 'Enter the type of plugin you wish to contribute',
+            //     choices: ['SQL', 'NoSQL', 'File Storage', 'SAAS']
+            // },
             {
                 type: 'input',
                 name: 'name',
                 message: 'Integration Name here: '
-            },
-            {
-                type: 'list',
-                name: 'entityName',
-                message: 'Enter the name of the entity in the plugin, you want to query',
-                choices: ['Table', 'Row', 'Bucket', 'File', 'Collection']
+            // },
+            // {
+            //     type: 'list',
+            //     name: 'entityName',
+            //     message: 'Enter the name of the entity in the plugin, you want to query',
+            //     choices: ['Table', 'Row', 'Bucket', 'File', 'Collection']
             }
         ],
         actions: function(data) {
             var actions = [];
 
-            if (data.pluginType && data.name && data.entityName) {
-                // All the inputs were given. Generate the files 
-
-                /*
-                    Plugin Authentication UI File
-                    Plugin Authentication File
-                    Plugin Entity CRUD File
-                    Plugin Entity UI File
-                    Plugin Entity Test File
-                */
+            // if (data.pluginType && data.name && data.entityName) {
+            if (data.name) {
+                
                actions.push(
                 {
                     type: 'add',
-                    // path: '../../server/appsmith-plugins/{{pascalCase name}}/src/main/java/com.external.plugins/{{pascalCase name}}.java',
-                    path: '{{pascalCase name}}/{{pascalCase name}}.java',
-                    templateFile: 'plop-templates/controller.hbs'
+                    path: '../../server/appsmith-plugins/{{camelCase name}}/src/main/java/com.external.plugins/{{pascalCase name}}.java',
+                    templateFile: 'plop-templates/pluginJava.hbs'
                 }
                );
 
                actions.push(
                 {
                     type: 'add',
-                    // path: '../../server/appsmith-plugins/{{pascalCase name}}/src/main/java/com.external.plugins/{{pascalCase name}}.java',
-                    path: '{{pascalCase name}}/editor.json',
-                    templateFile: 'plop-templates/json-template.hbs'
+                    path: '../../server/appsmith-plugins/{{camelCase name}}/src/test/java/com.external.plugins/{{pascalCase name}}Test.java',
+                    templateFile: 'plop-templates/pluginTest.hbs'
                 }
                );
+
+               actions.push(
+                {
+                    type: 'add',
+                    path: '../../server/appsmith-plugins/{{pascalCase name}}/plugin.properties',
+                    templateFile: 'plop-templates/plugin-iml.hbs'
+                }
+               );
+
+               actions.push(
+                {
+                    type: 'add',
+                    path: '../../server/appsmith-plugins/{{pascalCase name}}/pom.xml',
+                    templateFile: 'plop-templates/plugin-pom.hbs'
+                }
+               );
+
+               actions.push(
+                {
+                    type: 'add',
+                    path: '../../server/appsmith-plugins/{{camelCase name}}/src/main/resources/editor.json',
+                    templateFile: 'plop-templates/editor-json.hbs'
+                }
+               );
+
+               actions.push(
+                {
+                    type: 'add',
+                    path: '../../server/appsmith-plugins/{{camelCase name}}/src/main/resources/form.json',
+                    templateFile: 'plop-templates/form-json.hbs'
+                }
+               );
+
+               actions.push(
+                {
+                    type: 'modify',
+                    pattern :/    <\/modules><\/project>/,
+                    path: '../../server/appsmith-plugins/pom.xml',
+                    templateFile: 'plop-templates/super-plugins-pom.hbs'
+                }
+               );
+
                actions.push(
                 {
                     type: 'modify',
@@ -59,8 +92,8 @@ module.exports = function (plop) {
                 }
                );
 
-
             }
+
             return actions;
         }
     });
