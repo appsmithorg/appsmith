bdc5d5df5e640fdeb5772141552774fa6d328b53
arunvjn <arun@appsmith.com>
2021-08-31 12:18:47 +0530

    UI changes to install modal

--- app/client/src/components/editorComponents/CustomLibs/index.tsx
+++ app/client/src/components/editorComponents/CustomLibs/index.tsx
@@ -1,3 +1,4 @@
+import { initializeInstallation } from "actions/cutomLibsActions";
 import Button from "components/ads/Button";
 import Dropdown from "components/ads/Dropdown";
 import { TabComponent } from "components/ads/Tabs";
@@ -6,12 +7,14 @@ import React from "react";
 import { useState } from "react";
 import { useCallback } from "react";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "reducers";
 import styled from "styled-components";
 
 const LibraryContainer = styled.div`
   background: white;
   padding: 25px 30px;
-  height: 535px;
+  height: 630px;
   width: 750px;
   display: flex;
   flex-direction: column;
@@ -21,10 +24,11 @@ const LibraryContainer = styled.div`
   input {
     position: absolute;
     border: 1.2px solid #e0dede;
-    top: 60px;
-    padding: 10px 12px;
+    top: 58px;
+    padding: 8px 12px;
     right: 45px;
-    width: 220px;
+    width: 240px;
+    height: 35px;
     font-size: 12px;
   }
 `;
@@ -45,9 +49,6 @@ const LibraryCard = styled.div`
   border: 1px solid #f0f0f0;
   margin-bottom: 18px;
   cursor: pointer;
-  &:hover {
-    border: 1px solid #cb4810;
-  }
 `;
 
 const LibraryInfo = styled.div`
@@ -75,8 +76,19 @@ const LibraryInfo = styled.div`
 
 const LibraryActions = styled.div`
   display: flex;
+  flex-shrink: 0;
   > * {
     margin: 0 5px;
+  }
+  .uninstall-btn {
+    background: #f22b2b;
+    color: white;
+    border: 1.2px solid #f22b2b;
+  }
+  .update-btn {
+    background: white;
+    border: 1.2px solid #716e6e;
+    color: #716e6e;
   }
 `;
 
@@ -94,28 +106,36 @@ const LibraryWrapper = styled.div`
 `;
 
 function InstalledLibraries() {
+  const libraries = useSelector(
+    (state: AppState) => state.ui.customLibs.defaultLibraries,
+  );
   return (
     <LibraryWrapper>
       <LibraryList>
-        <LibraryCard>
-          <LibraryInfo>
-            <span>Lodash</span>
-            <span>Lodash</span>
-            <span>Lodash</span>
-            <span>Lodash</span>
-          </LibraryInfo>
-          <LibraryActions>
-            <Button text="Install" />
-            <Button text="Uninstall" />
-          </LibraryActions>
-        </LibraryCard>
+        {(libraries || []).map((lib: any, idx: number) => (
+          <LibraryCard key={idx}>
+            <LibraryInfo>
+              <span className="lib-name">{lib.name}</span>
+              <span className="lib-desc">{lib.description}</span>
+              <span className="lib-desc">{lib.version}</span>
+            </LibraryInfo>
+            <LibraryActions>
+              <Button className="update-btn" text="Update to latest" />
+              <Button className="uninstall-btn" text="Uninstall" />
+            </LibraryActions>
+          </LibraryCard>
+        ))}
       </LibraryList>
     </LibraryWrapper>
   );
 }
 
 function AllLibraries({ libraries }: any) {
-  const [versions, setVersions] = useState<any>({});
+  const [versions, setVersions] = useState<any>({}),
+    dispatch = useDispatch();
+  const currentInstallations = useSelector(
+    (state: AppState) => state.ui.customLibs.currentInstallations,
+  );
   const fetchVersions = (name: string) => {
     if (typeof versions[name] === "undefined")
       fetch(`https://api.cdnjs.com/libraries/${name}?fields=versions`)
@@ -127,23 +147,35 @@ function AllLibraries({ libraries }: any) {
           }),
         );
   };
+  const installLibrary = (lib: any) => {
+    dispatch(initializeInstallation(lib));
+  };
   return (
     <LibraryWrapper>
       <LibraryList>
         {(libraries || []).map((lib: any, idx: number) => (
-          <LibraryCard key={idx} onMouseOver={(e) => fetchVersions(lib.name)}>
-            <LibraryInfo>
+          <LibraryCard key={idx}>
+            <LibraryInfo onMouseOver={() => fetchVersions(lib.name)}>
               <span className="lib-name">{lib.name}</span>
               <span className="lib-desc">{lib.description}</span>
               <Dropdown
                 className="lib-version"
+                enableSearch
                 options={versions[lib.name] || []}
                 selected={{ label: lib.version, value: lib.version }}
+                showLabelOnly
               />
             </LibraryInfo>
             <LibraryActions>
-              <Button text="Install" />
-              <Button text="Uninstall" />
+              {currentInstallations.indexOf(lib.name) === -1 ? (
+                <Button
+                  className="install-btn"
+                  onClick={() => installLibrary(lib)}
+                  text="Install"
+                />
+              ) : (
+                "Installing"
+              )}
             </LibraryActions>
           </LibraryCard>
         ))}
