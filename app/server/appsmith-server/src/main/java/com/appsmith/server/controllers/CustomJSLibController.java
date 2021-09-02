dd70985e9e7c9487609daaf8e250517ff23f7008
Sumit Kumar <sumit@appsmith.com>
2021-09-02 18:11:22 +0530

    - remove unused imports

--- app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/CustomJSLibController.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/CustomJSLibController.java
@@ -2,10 +2,9 @@ package com.appsmith.server.controllers;
 
 import com.appsmith.server.constants.Url;
 import com.appsmith.server.domains.JSLib;
-import com.appsmith.server.domains.UserData;
 import com.appsmith.server.services.CustomJSLibService;
-import com.appsmith.server.services.GitService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(Url.CUSTOM_JS_LIB_URL)
 public class CustomJSLibController extends BaseController<CustomJSLibService, JSLib, String> {
 
+    @Autowired
     public CustomJSLibController(CustomJSLibService service) {
         super(service);
     }
