28a749d61fa98a954de2866be023a120d29f1f22
Trisha Anand <trisha@appsmith.com>
2021-09-05 20:11:10 +0530

    Adding `APPSMITH_PLUGIN_MAX_RESPONSE_SIZE` which can be configured as an environment variable to increase plugin response size (currently only supported for Postgres Plugin)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/SharedConfigImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/SharedConfigImpl.java
@@ -12,8 +12,16 @@ public class SharedConfigImpl implements SharedConfig {
     @Value("${appsmith.codec.max-in-memory-size:10}")
     private int CODEC_SIZE;
 
+    @Value("${appsmith.plugin.response.size.max:5000000}")
+    private int maxPluginResponseSize = 5000000;
+
     @Override
     public int getCodecSize() {
         return this.CODEC_SIZE * 1024 * 1024;
+    }
+
+    @Override
+    public int getMaxResponseSize() {
+        return this.maxPluginResponseSize;
     }
 }
