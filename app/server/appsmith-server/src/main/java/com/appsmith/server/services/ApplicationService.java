e6e1123dca4c5d02ed4a080ee764929e9ef9bff7
Nayan <nayan@appsmith.com>
2021-09-07 11:46:20 +0600

    -added a blank line in application service class

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ApplicationService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ApplicationService.java
@@ -33,5 +33,6 @@ public interface ApplicationService extends CrudService<Application, String> {
     Mono<Application> getApplicationInViewMode(String applicationId);
 
     Mono<Application> saveLastEditInformation(String applicationId);
+
     Mono<String> generateSshKeyPair(String applicationId);
 }
