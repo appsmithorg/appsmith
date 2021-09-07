a72abe012a10c12f6386a39c08f81584f876ea0d
Anagh Hegde <anagh@appsmith.com>
2021-09-07 16:36:28 +0530

    Add tests for API to store the config at Application level

--- app/server/appsmith-server/src/test/java/com/appsmith/server/services/GitServiceTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/GitServiceTest.java
@@ -1,12 +1,23 @@
 package com.appsmith.server.services;
 
+import com.appsmith.server.acl.AclPermission;
+import com.appsmith.server.domains.Application;
+import com.appsmith.server.domains.GitApplicationMetadata;
+import com.appsmith.server.domains.GitAuth;
 import com.appsmith.server.domains.GitConfig;
+import com.appsmith.server.domains.Organization;
 import com.appsmith.server.domains.User;
 import com.appsmith.server.domains.UserData;
+import com.appsmith.server.dtos.GitConnectDTO;
+import com.appsmith.server.exceptions.AppsmithError;
 import com.appsmith.server.exceptions.AppsmithException;
 import com.appsmith.server.repositories.OrganizationRepository;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jgit.api.Git;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +39,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class GitServiceTest {
 
     @Autowired
-    private GitService gitDataService;
+    GitService gitDataService;
 
     @Autowired
     OrganizationRepository organizationRepository;
@@ -39,18 +50,50 @@ public class GitServiceTest {
     @SpyBean
     SessionUserService sessionUserService;
 
-    private GitConfig getConnectRequest( String commitEmail, String author) {
+    String orgId;
+
+    @Before
+    @WithUserDetails(value = "api_user")
+    public void setup() {
+        Organization testOrg = organizationRepository.findByName("Another Test Organization", AclPermission.READ_ORGANIZATIONS).block();
+        orgId = testOrg.getId();
+    }
+
+    private GitConfig getConfigRequest( String commitEmail, String author) {
         GitConfig gitConfig = new GitConfig();
         gitConfig.setAuthorEmail(commitEmail);
         gitConfig.setAuthorName(author);
         return gitConfig;
     }
 
+    private GitAuth getGitAuth() {
+        GitAuth gitAuth = new GitAuth();
+        gitAuth.setPrivateKey("privateKey");
+        gitAuth.setPublicKey("publicKey");
+        return gitAuth;
+    }
+
+    private GitConnectDTO getConnectRequest(String remoteUrl,
+                                            String applicationId,
+                                            GitAuth gitAuth,
+                                            String authorName,
+                                            String authorEmail,
+                                            String organizationId) {
+        GitConnectDTO gitConnectDTO = new GitConnectDTO();
+        gitConnectDTO.setRemoteUrl(remoteUrl);
+        gitConnectDTO.setApplicationId(applicationId);
+        gitConnectDTO.setGitAuth(gitAuth);
+        gitConnectDTO.setAuthorName(authorName);
+        gitConnectDTO.setAuthorEmail(authorEmail);
+        gitConnectDTO.setOrganizationId(organizationId);
+        return gitConnectDTO;
+    }
+
     @Test
     @WithUserDetails(value = "api_user")
     public void saveConfig_gitConfigValues_SaveToUserObject() {
         Mockito.when(userService.findByEmail(Mockito.anyString())).thenReturn(Mono.just(new User()));
-        GitConfig gitGlobalConfigDTO = getConnectRequest("test@appsmith.com", "Test 1");
+        GitConfig gitGlobalConfigDTO = getConfigRequest("test@appsmith.com", "Test 1");
         Mono<UserData> userDataMono = gitDataService.saveGitConfigData(gitGlobalConfigDTO);
 
         StepVerifier
@@ -65,7 +108,7 @@ public class GitServiceTest {
     @WithUserDetails(value = "api_user")
     public void saveConfig_gitConfigValues_updateUserObject() {
         Mockito.when(userService.findByEmail(Mockito.anyString())).thenReturn(Mono.just(new User()));
-        GitConfig gitGlobalConfigDTO = getConnectRequest("test@appsmith.com", "Test 1");
+        GitConfig gitGlobalConfigDTO = getConfigRequest("test@appsmith.com", "Test 1");
         Mono<UserData> userDataMono = gitDataService.saveGitConfigData(gitGlobalConfigDTO);
 
         StepVerifier
@@ -80,7 +123,7 @@ public class GitServiceTest {
     @WithUserDetails(value = "api_user")
     public void saveConfig_AuthorEmailNull_ThrowInvalidParameterError() {
         Mockito.when(userService.findByEmail(Mockito.anyString())).thenReturn(Mono.just(new User()));
-        GitConfig gitGlobalConfigDTO = getConnectRequest(null, "Test 1");
+        GitConfig gitGlobalConfigDTO = getConfigRequest(null, "Test 1");
 
         Mono<UserData> userDataMono = gitDataService.saveGitConfigData(gitGlobalConfigDTO);
         StepVerifier
@@ -94,7 +137,7 @@ public class GitServiceTest {
     @WithUserDetails(value = "api_user")
     public void saveConfig_AuthorNameEmptyString_ThrowInvalidParameterError() {
         Mockito.when(userService.findByEmail(Mockito.anyString())).thenReturn(Mono.just(new User()));
-        GitConfig gitGlobalConfigDTO = getConnectRequest("test@appsmith.com",  null);
+        GitConfig gitGlobalConfigDTO = getConfigRequest("test@appsmith.com",  null);
 
         Mono<UserData> userDataMono = gitDataService.saveGitConfigData(gitGlobalConfigDTO);
         StepVerifier
@@ -108,7 +151,7 @@ public class GitServiceTest {
     @WithUserDetails(value = "api_user")
     public void getConfig_ValueExistsInDB_Success() {
         Mockito.when(userService.findByEmail(Mockito.anyString())).thenReturn(Mono.just(new User()));
-        GitConfig gitGlobalConfigDTO = getConnectRequest("test@appsmith.com", "Test 1");
+        GitConfig gitGlobalConfigDTO = getConfigRequest("test@appsmith.com", "Test 1");
         UserData userData = gitDataService.saveGitConfigData(gitGlobalConfigDTO).block();
         Mono<GitConfig> gitConfigMono = gitDataService.getGitConfigForUser();
 
@@ -118,5 +161,102 @@ public class GitServiceTest {
                     assertThat(configData.getAuthorName()).isEqualTo(gitGlobalConfigDTO.getAuthorName());
                     assertThat(configData.getAuthorEmail()).isEqualTo(gitGlobalConfigDTO.getAuthorEmail());
                 });
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void connectApplicationToGit_validGitMetadata_Success() {
+        GitConnectDTO gitConnectDTO = getConnectRequest("test.url.git", "testID", getGitAuth(),
+                null, null, orgId);
+        Mono<Application> applicationMono = gitDataService.connectApplicationToGit(gitConnectDTO);
+
+        StepVerifier
+                .create(applicationMono)
+                .assertNext(application -> {
+                    GitApplicationMetadata gitApplicationMetadata = application.getGitApplicationMetadata();
+                    assertThat(gitApplicationMetadata.getAuthorEmail()).isNull();
+                    assertThat(gitApplicationMetadata.getAuthorName()).isNull();
+                    assertThat(gitApplicationMetadata.getBranchName()).isEqualTo("master");
+                    assertThat(gitApplicationMetadata.getRemoteUrl()).isEqualTo(gitConnectDTO.getRemoteUrl());
+                });
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void connectApplicationToGit_WithCustomUserConfig_Success() {
+        GitConnectDTO gitConnectDTO = getConnectRequest("test.url.git", "testID", getGitAuth(),
+                "test", "test@appsmith.com", orgId);
+        Mono<Application> applicationMono = gitDataService.connectApplicationToGit(gitConnectDTO);
+
+        StepVerifier
+                .create(applicationMono)
+                .assertNext(application -> {
+                    GitApplicationMetadata gitApplicationMetadata = application.getGitApplicationMetadata();
+                    assertThat(gitApplicationMetadata.getAuthorEmail()).isEqualTo(gitConnectDTO.getAuthorEmail());
+                    assertThat(gitApplicationMetadata.getAuthorName()).isEqualTo(gitConnectDTO.getAuthorName());
+                    assertThat(gitApplicationMetadata.getBranchName()).isEqualTo("master");
+                    assertThat(gitApplicationMetadata.getRemoteUrl()).isEqualTo(gitConnectDTO.getRemoteUrl());
+                    assertThat(gitApplicationMetadata.getDefaultApplicationId()).isEqualTo(gitConnectDTO.getApplicationId());
+                });
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void connectApplicationToGit_EmptyGitAuth_ThrowInvalidParameterException() {
+        GitConnectDTO gitConnectDTO = getConnectRequest("test.url.git", "testID", new GitAuth(),
+                "test", "test@appsmith.com", orgId);
+        Mono<Application> applicationMono = gitDataService.connectApplicationToGit(gitConnectDTO);
+
+        StepVerifier
+                .create(applicationMono)
+                .expectErrorMatches(throwable -> throwable instanceof AppsmithException
+                        && throwable.getMessage().contains(AppsmithError.INVALID_PARAMETER.getMessage("SSH Key", "")))
+                .verify();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void connectApplicationToGit_EmptyPublicSshKey_ThrowInvalidParameterException() {
+        GitAuth gitAuth = getGitAuth();
+        gitAuth.setPublicKey(null);
+        GitConnectDTO gitConnectDTO = getConnectRequest("test.url.git", "testID", gitAuth,
+                "test", "test@appsmith.com", orgId);
+        Mono<Application> applicationMono = gitDataService.connectApplicationToGit(gitConnectDTO);
+
+        StepVerifier
+                .create(applicationMono)
+                .expectErrorMatches(throwable -> throwable instanceof AppsmithException
+                        && throwable.getMessage().contains(AppsmithError.INVALID_PARAMETER.getMessage("SSH Key", "")))
+                .verify();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void connectApplicationToGit_EmptyPrivateSshKey_ThrowInvalidParameterException() {
+        GitAuth gitAuth = getGitAuth();
+        gitAuth.setPrivateKey(null);
+        GitConnectDTO gitConnectDTO = getConnectRequest("test.url.git", "testID", gitAuth,
+                "test", "test@appsmith.com", orgId);
+        Mono<Application> applicationMono = gitDataService.connectApplicationToGit(gitConnectDTO);
+
+        StepVerifier
+                .create(applicationMono)
+                .expectErrorMatches(throwable -> throwable instanceof AppsmithException
+                        && throwable.getMessage().contains(AppsmithError.INVALID_PARAMETER.getMessage("SSH Key", "")))
+                .verify();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void connectApplicationToGit_EmptyRemoteUrl_ThrowInvalidParameterException() {
+        GitConnectDTO gitConnectDTO = getConnectRequest(null, "testID", getGitAuth(),
+                "test", "test@appsmith.com", orgId);
+        Mono<Application> applicationMono = gitDataService.connectApplicationToGit(gitConnectDTO);
+
+        StepVerifier
+                .create(applicationMono)
+                .expectErrorMatches(throwable -> throwable instanceof AppsmithException
+                        && throwable.getMessage().contains(AppsmithError.INVALID_PARAMETER.getMessage("Remote Url", "")))
+                .verify();
     }
 }
