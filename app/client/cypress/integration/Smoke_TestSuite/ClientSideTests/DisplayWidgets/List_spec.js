7d255e0b520ee1e5e9ced09e0cd31c0fb0fc31b5
arslanhaiderbuttar <arslan@appsmith.com>
2021-09-07 14:37:23 +0500

    Listing Test Cases Fix

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/List_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/List_spec.js
@@ -13,14 +13,14 @@ describe("Button Widget Functionality", function() {
   it("Button-Style Validation", function() {
     //Changing the style of the button from the property pane and verify it's color.
     // Change to Secondary button sytle
-    cy.changeButtonStyle(2, "rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 0)");
+    cy.changeButtonStyle(2, "rgb(254, 184, 17)", "rgba(0, 0, 0, 0)");
     cy.get(publishPage.backToEditor).click({ force: true });
     // Change to Danger button sytl
     cy.openPropertyPane("buttonwidget");
-    cy.changeButtonStyle(3, "rgb(179, 3, 56)", "rgb(139, 2, 43)");
+    cy.changeButtonStyle(3, "rgb(242, 43, 43)", "rgb(139, 2, 43)");
   });
 
   afterEach(() => {
-    // cy.get(publishPage.backToEditor).click({ force: true });
+    cy.goToEditFromPublish();
   });
 });
