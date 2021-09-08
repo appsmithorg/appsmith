957db2245fa136e651c510289567906456244d8f
Abhijeet <ABHI.NAGARNAIK@GMAIL.COM>
2021-09-08 18:00:04 +0530

    Added commit api

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/GitService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/GitService.java
@@ -4,9 +4,13 @@ import com.appsmith.server.domains.GitConfig;
 import com.appsmith.server.domains.UserData;
 import reactor.core.publisher.Mono;
 
-public interface GitService extends CrudService<UserData, String> {
+public interface GitService {
 
     Mono<UserData> saveGitConfigData(GitConfig gitConfig);
 
     Mono<GitConfig> getGitConfigForUser();
+
+    Mono<String> commitApplication(String commitMessage, String applicationId);
+
+    Mono<String> commitApplication(String applicationId);
 }
