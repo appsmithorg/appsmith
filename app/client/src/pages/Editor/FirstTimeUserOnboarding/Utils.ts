92fdd6403e59a3fed280e665b7cb74bcd10575ed
sbalaji1192 <sbalaji1192@gmail.com>
2021-09-09 14:33:09 +0530

    Fix minor issue

--- app/client/src/pages/Editor/FirstTimeUserOnboarding/Utils.ts
+++ app/client/src/pages/Editor/FirstTimeUserOnboarding/Utils.ts
@@ -1,8 +1,11 @@
 import { ReduxActionTypes } from "constants/ReduxActionConstants";
+import { APPLICATIONS_URL } from "constants/routes";
 import { Dispatch } from "react";
 import AnalyticsUtil from "utils/AnalyticsUtil";
+import history from "utils/history";
 export const triggerWelcomeTour = (dispatch: Dispatch<any>) => {
   AnalyticsUtil.logEvent("SIGNPOSTING_WELCOME_TOUR_CLICK");
+  history.push(APPLICATIONS_URL);
   dispatch({
     type: ReduxActionTypes.SET_ENABLE_FIRST_TIME_USER_ONBOARDING,
     payload: false,
