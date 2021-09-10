c7e91d38133552ad1db04b5e461fefac574b93eb
arunvjn <arun@appsmith.com>
2021-09-10 10:23:07 +0530

    Code to prevent infinite re-render

--- app/client/src/components/editorComponents/GlobalSearch/SetSearchResults.tsx
+++ app/client/src/components/editorComponents/GlobalSearch/SetSearchResults.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from "react";
+import { useEffect, useCallback, useState } from "react";
 import { connectHits } from "react-instantsearch-dom";
 import { Hit as IHit } from "react-instantsearch-core";
 import { debounce } from "lodash";
@@ -20,8 +20,10 @@ type Props = {
 
 function SearchResults({ category, hits, setSearchResults }: Props) {
   const debouncedSetter = useCallback(debounce(setSearchResults, 100), []);
-
+  const [currentHits, setCurrentHits] = useState(hits);
   useEffect(() => {
+    if (JSON.stringify(hits) == JSON.stringify(currentHits)) return;
+    setCurrentHits(hits);
     const filteredHits = hits.filter((doc: SearchItem) => {
       return !doc.kind || doc.kind === SEARCH_ITEM_TYPES.document;
     });
