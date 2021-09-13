464a7f4cf9cf0c721d43bacb9eb9047d01d0c97e
Paul Li <wmdev0808@gmail.com>
2021-09-13 17:15:32 -0400

    Feat: JSON Editor Widget
    -- Fix failed cypress test cases

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/JsonEditor_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/JsonEditor_spec.js
@@ -1,33 +1,33 @@
 const dsl = require("../../../../fixtures/JsonEditorDsl.json");
 const commonlocators = require("../../../../locators/commonlocators.json");
-const explorer = require("../../../../locators/explorerlocators.json");
 
 describe("JSON Editor Widget Functionality", function() {
   beforeEach(() => {
     cy.addDsl(dsl);
   });
 
-  // it("If defaultText is changed, it will update the corresponding value in the editor, as well", function() {
-  //   cy.openPropertyPane("jsoneditorwidget");
-  //   cy.testJsontext(
-  //     "defaultjsonstring",
-  //     JSON.stringify(this.data.jsonDefaultText),
-  //   );
-  //   cy.get(".jsoneditor")
-  //     .contains("myname")
-  //     .should("have.text", "myname");
-  //   //Close edit prop
-  //   cy.get(commonlocators.editPropCrossButton).click({
-  //     force: true,
-  //   });
-  //   cy.PublishtheApp();
-  // });
+  it("If defaultText is changed, it will update the corresponding value in the editor, as well", function() {
+    cy.openPropertyPane("jsoneditorwidget");
+    cy.testJsontext(
+      "defaultjsonstring",
+      JSON.stringify(this.data.jsonDefaultText),
+    );
+    cy.get(".jsoneditor")
+      .contains("myname")
+      .should("have.text", "myname");
+    //Close edit prop
+    cy.get(commonlocators.editPropCrossButton).click({
+      force: true,
+    });
+    cy.PublishtheApp();
+  });
 
   it("If user change the value in view mode, it should reflect the changes in editor", function() {
-    cy.PublishtheApp();
-    cy.get(".json-editorvalue")
-      .eq(1)
-      .click()
+    cy.get("div.jsoneditor-value.jsoneditor-string")
+      .focus({ force: true })
+      .type("{uparrow}", { force: true })
+      .type("{ctrl}{shift}{downarrow}", { force: true })
+      .clear({ force: true })
       .type("namechanged");
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(1000);
