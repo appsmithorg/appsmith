92cf1c07fb018cf0d4e4c69f1569d8e16394686b
vicky.bansal@primathon.in <vicky.bansal@primathon.in>
2021-09-17 13:45:24 +0530

    Added cypress test to validate triggeredRow property value remain same after sorting on a table column

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Widget_Add_button_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Widget_Add_button_spec.js
@@ -48,6 +48,14 @@ describe("Table Widget property pane feature validation", function() {
   it("Table widget triggeredRow property should be accessible", function() {
     cy.get(commonlocators.TextInside).should("have.text", "Tobias Funke");
   });
+  it("Table widget triggeredRow property should be same even after sorting the table", function() {
+    //sort table date on second column
+    cy.get(".draggable-header ")
+      .first()
+      .click({ force: true });
+    cy.wait(1000);
+    cy.get(commonlocators.TextInside).should("have.text", "Tobias Funke");
+  });
   it("Table widget add new icon button column", function() {
     cy.openPropertyPane("tablewidget");
     // click on Add new Column.
