89f09132c41a9f66a73e2f3f0451003818de8f15
Abhijeet <ABHI.NAGARNAIK@GMAIL.COM>
2021-09-17 17:18:29 +0530

    Update execute method for DB metadata query for GSheet, Update asset urls

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/constants/Assets.java
@@ -0,0 +1,7 @@
+package com.appsmith.server.constants;
+
+public class Assets {
+
+    public static final String GENERATE_CRUD_PAGE_SUCCESS_URL_TABULAR = "https://assets.appsmith.com.s3.us-east-2.amazonaws.com/crud/workflow_sql.svg";
+    public static final String GENERATE_CRUD_PAGE_SUCCESS_URL_S3 = "https://assets.appsmith.com.s3.us-east-2.amazonaws.com/crud/workflow_s3.svg";
+}
