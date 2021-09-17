20b50998492f4411f191ae336b052a48f2731d1f
Albin <albin@appsmith.com>
2021-09-17 16:01:09 +0530

    * social login hover color fix

--- app/client/src/constants/DefaultTheme.tsx
+++ app/client/src/constants/DefaultTheme.tsx
@@ -1366,7 +1366,7 @@ const auth: any = {
   placeholder: lightShades[8],
   socialBtnText: "#000",
   socialBtnBorder: lightShades[13],
-  socialBtnHighlight: darkShades[1],
+  socialBtnHighlight: lightShades[12],
 };
 
 const helpModal = {
