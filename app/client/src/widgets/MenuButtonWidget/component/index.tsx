49551987fc04c9d4d4fc6dfd03a34d0acfd15e8f
Aswath K <aswath.sana@gmail.com>
2021-09-17 14:25:47 +0530

    fix: Transparent menu on MenuButton widget (#7268)

--- app/client/src/widgets/MenuButtonWidget/component/index.tsx
+++ app/client/src/widgets/MenuButtonWidget/component/index.tsx
@@ -340,7 +340,6 @@ const BaseMenuItem = styled(MenuItem)<ThemeProp & BaseStyleProps>`
 
 const StyledMenu = styled(Menu)`
   padding: 0;
-  background: none;
 `;
 
 export interface PopoverContentProps {
