ceee5060a29c19d9273e434ef7e6cdc98b78b829
Abhijeet <ABHI.NAGARNAIK@GMAIL.COM>
2021-09-20 16:01:06 +0530

    Update asset urls

--- app/server/appsmith-server/src/main/java/com/appsmith/server/constants/Assets.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/constants/Assets.java
@@ -2,6 +2,6 @@ package com.appsmith.server.constants;
 
 public class Assets {
 
-    public static final String GENERATE_CRUD_PAGE_SUCCESS_URL_TABULAR = "https://assets.appsmith.com.s3.us-east-2.amazonaws.com/crud/workflow_sql.svg";
-    public static final String GENERATE_CRUD_PAGE_SUCCESS_URL_S3 = "https://assets.appsmith.com.s3.us-east-2.amazonaws.com/crud/workflow_s3.svg";
+    public static final String GENERATE_CRUD_PAGE_SUCCESS_URL_TABULAR = "https://s3.us-east-2.amazonaws.com/assets.appsmith.com/crud/workflow_sql.svg";
+    public static final String GENERATE_CRUD_PAGE_SUCCESS_URL_S3 = "https://s3.us-east-2.amazonaws.com/assets.appsmith.com/crud/workflow_s3.svg";
 }
