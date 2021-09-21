dc5c9f4515ebff247ee1ca4334ec9d68cb49856f
vicky.bansal@primathon.in <vicky.bansal@primathon.in>
2021-09-21 16:06:48 +0530

    Fix countryCode property

--- app/client/src/widgets/InputWidget/widget/index.tsx
+++ app/client/src/widgets/InputWidget/widget/index.tsx
@@ -167,7 +167,7 @@ class InputWidget extends BaseWidget<InputWidgetProps, WidgetState> {
           },
           {
             helpText: "Changes the type of currency",
-            propertyName: "currencyCountryCode",
+            propertyName: "defaultCurrencyCountryCode",
             label: "Currency",
             enableSearch: true,
             dropdownHeight: "195px",
@@ -564,6 +564,8 @@ class InputWidget extends BaseWidget<InputWidgetProps, WidgetState> {
           }
         })()
       }}`,
+      countryCode: `{{this.selectedPhoneNumberCountryCode ? this.selectedPhoneNumberCountryCode : this.phoneNumberCountryCode}}`,
+      currencyCountryCode: `{{this.selectedCurrencyCountryCode ? this.selectedCurrencyCountryCode : this.defaultCurrencyCountryCode}}`,
       value: `{{this.text}}`,
     };
   }
@@ -600,7 +602,10 @@ class InputWidget extends BaseWidget<InputWidgetProps, WidgetState> {
   onCurrencyTypeChange = (code?: string) => {
     const currencyCountryCode = code;
     if (this.props.renderMode === RenderModes.CANVAS) {
-      super.updateWidgetProperty("currencyCountryCode", currencyCountryCode);
+      super.updateWidgetProperty(
+        "defaultCurrencyCountryCode",
+        currencyCountryCode,
+      );
     } else {
       this.props.updateWidgetMetaProperty(
         "selectedCurrencyCountryCode",
@@ -768,6 +773,8 @@ export interface InputWidgetProps extends WidgetProps {
   noOfDecimals?: number;
   allowCurrencyChange?: boolean;
   phoneNumberCountryCode?: string;
+  countryCode?: string;
+  defaultCurrencyCountryCode?: string;
   decimalsInCurrency?: number;
   defaultText?: string | number;
   tooltip?: string;
