23f6b63d4be9e51d9e31680467593625abde93b8
Albin <albin@appsmith.com>
2021-09-24 09:09:24 +0530

    * Removed storybook

--- app/client/.storybook/configs/index.js
+++
@@ -1,6 +0,0 @@
-import { ThemeProvider, theme } from "../../src/constants/DefaultTheme";
-import { light } from "constants/DefaultTheme";
-
-const DefaultTheme = { ...theme, colors: { ...theme.colors, ...light } };
-
-export { DefaultTheme, ThemeProvider }
