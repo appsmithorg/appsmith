09744ac18a28b9410396db68a2a4dd8758bcb9c4
haojin111 <haojin828@outlook.com>
2021-09-24 15:09:56 +0800

    fixed issue of reopen invite modal

--- app/client/src/pages/Applications/index.tsx
+++ app/client/src/pages/Applications/index.tsx
@@ -686,6 +686,7 @@ function ApplicationsSection(props: any) {
                   canEscapeKeyClose={false}
                   canOutsideClickClose={false}
                   isOpen={selectedOrgId === organization.id}
+                  onClose={() => setSelectedOrgId("")}
                   title={`Invite Users to ${organization.name}`}
                 >
                   <Form orgId={organization.id} />
