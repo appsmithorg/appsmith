7d262345a81499b6d4bbdf88c3086677c3619023
bhavin <techbhavin@gmail.com>
2021-09-24 16:28:05 +0530

    handling different url types with viewer

--- app/client/src/widgets/DocumentViewerWidget/component/index.tsx
+++ app/client/src/widgets/DocumentViewerWidget/component/index.tsx
@@ -1,19 +1,95 @@
-import React from "react";
+import React, { useMemo } from "react";
 import styled from "styled-components";
 import { DocumentViewer } from "react-documents";
+import { includes, replace, split, get } from "lodash";
+import { SUPPORTED_EXTENSIONS } from "../constants";
 
-const EmptyWrapper = styled.div`
+const ErrorWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-function DocumentViewerComponent(props: DocumentViewerComponentProps) {
-  if (!!props.docUrl) {
-    return <DocumentViewer url={props.docUrl} viewer="url" />;
+type viewerType = "google" | "office" | "mammoth" | "pdf" | "url";
+
+const checkUrlExtension = (docUrl: string) => {
+  // Remove everything to the last slash in URL
+  let url = docUrl.substr(1 + docUrl.lastIndexOf("/"));
+  // Break URL at ? and take first part (file name, extension)
+  url = url.split("?")[0];
+  // Sometimes URL doesn't have ? but #, so we should aslo do the same for #
+  url = url.split("#")[0];
+  // Now we have only filename and extension
+  const chunkList = url.split(".");
+  if (chunkList.length > 1) {
+    const ext = chunkList[chunkList.length - 1];
+    // check extension is valid or not
+    const validExtension = SUPPORTED_EXTENSIONS.includes(ext);
+    return {
+      hasExtension: true,
+      validExtension: validExtension,
+      extension: ext,
+    };
   } else {
-    return <EmptyWrapper>No document url provided for viewer</EmptyWrapper>;
+    // might be preview url
+    return { hasExtension: false };
   }
+};
+
+const getSettings = (docUrl: string) => {
+  let viewer = "url" as viewerType;
+  let url = docUrl;
+  let errorMessage = !!docUrl ? "" : "No document url provided for viewer";
+
+  // handled dropbox url
+  if (includes(url, "dropbox.com")) {
+    if (includes(url, "?dl=0") || !includes(url, "?raw=1")) {
+      url = replace(url, "?dl=0", "");
+      url = url + "?raw=1";
+    }
+    return { url, viewer };
+  }
+  // handled onedrive url
+  if (includes(url, "onedrive.live.com")) {
+    const onedriveUrl = new URL(url);
+    const onedriveUrlParamList = split(onedriveUrl.search, /[?&=]+/);
+    const cid = get(
+      onedriveUrlParamList,
+      onedriveUrlParamList.indexOf("cid") + 1,
+    );
+    const resid = get(
+      onedriveUrlParamList,
+      onedriveUrlParamList.indexOf("id") + 1,
+    );
+
+    url = `https://onedrive.live.com/embed?cid=${cid}&resid=${resid}&em=2`;
+    return { url, viewer };
+  }
+
+  // check url extension and if it is supported
+  const { extension, hasExtension, validExtension } = checkUrlExtension(url);
+  if (hasExtension) {
+    if (validExtension) {
+      if (!(extension === "txt" || extension === "pdf")) {
+        viewer = "office";
+      }
+    } else {
+      errorMessage = "Current file type is not supported";
+    }
+  }
+
+  return { url, viewer, errorMessage };
+};
+
+function DocumentViewerComponent(props: DocumentViewerComponentProps) {
+  const { errorMessage, url, viewer } = useMemo(
+    () => getSettings(props.docUrl),
+    [props.docUrl],
+  );
+  if (errorMessage) {
+    return <ErrorWrapper>{errorMessage}</ErrorWrapper>;
+  }
+  return <DocumentViewer url={url} viewer={viewer} />;
 }
 
 export interface DocumentViewerComponentProps {
--- app/client/src/widgets/DocumentViewerWidget/constants.ts
+++ app/client/src/widgets/DocumentViewerWidget/constants.ts
@@ -1,2 +1,12 @@
 // This file contains common constants which can be used across the widget configuration file (index.ts), widget and component folders.
 export const DOCUMENTVIEWER_WIDGET_CONSTANT = "";
+// txt and pdf handle by viewerType = "url"
+// and other types handle by viewerType = "office"
+export const SUPPORTED_EXTENSIONS = [
+  "txt",
+  "pdf",
+  "docx",
+  "ppt",
+  "pptx",
+  "xlsx",
+];
