898d7d2d7badd64f908f5c17e6f78e768f321555
Pranav Kanade <pranav@appsmith.com>
2021-09-26 23:37:37 +0530

    moved constants to a proper files

---
+++ app/client/src/sagas/WebsocketSagas/socketEvents.ts
@@ -0,0 +1,31 @@
+export const SOCKET_CONNECTION_EVENTS = {
+  CONNECT: "connect",
+  DISCONNECT: "disconnect",
+};
+
+export const COMMENTS_SOCKET_EVENTS = {
+  // comment events
+  // SET_COMMENTS: "SET_COMMENTS",
+  INSERT_COMMENT_THREAD: "insert:commentThread",
+  INSERT_COMMENT: "insert:comment",
+  UPDATE_COMMENT_THREAD: "update:commentThread",
+  REPLACE_COMMENT_THREAD: "replace:commentThread",
+  DELETE_COMMENT_THREAD: "delete:commentThread",
+  UPDATE_COMMENT: "update:comment",
+  DELETE_COMMENT: "delete:comment",
+
+  // notification events
+  INSERT_NOTIFICATION: "insert:notification",
+};
+
+export const EDITORS_PRESENCE_SOCKET_EVENTS = {
+  START_EDITING_APP: "collab:start_edit",
+  STOP_EDITING_APP: "collab:leave_edit",
+  LIST_ONLINE_APP_EDITORS: "collab:online_editors",
+};
+
+export const MULTI_PLAYER_SOCKET_EVENTS = {
+  START_EDITING_APP: "collab:start_edit",
+  STOP_EDITING_APP: "collab:leave_edit",
+  SHARE_USER_POINTER: "collab:mouse_pointer",
+};
