244bfaa7a1e40d79a6731c0a07e7525ecb5df3b6
Trisha Anand <trisha@appsmith.com>
2021-09-27 14:30:18 +0530

    Adding checks to ensure unique edges are added to the set of edges

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ActionDependencyEdge.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ActionDependencyEdge.java
@@ -3,6 +3,8 @@ package com.appsmith.server.domains;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 @Getter
 @Setter
@@ -10,4 +12,26 @@ import lombok.ToString;
 public class ActionDependencyEdge {
     String source;
     String target;
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(source)
+                .append(target)
+                .toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ActionDependencyEdge) {
+            final ActionDependencyEdge actionDependencyEdge = (ActionDependencyEdge) obj;
+
+            return new EqualsBuilder()
+                    .append(source, actionDependencyEdge.source)
+                    .append(target, actionDependencyEdge.target)
+                    .isEquals();
+        } else {
+            return false;
+        }
+    }
 }
