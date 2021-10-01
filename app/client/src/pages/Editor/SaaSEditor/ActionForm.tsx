527fcca42f94063e1c5e368d1837dff6b827ff30
Pranav Kanade <pranav@appsmith.com>
2021-10-01 11:24:11 +0530

    Revert "fixes to add proper routing around navigation between query pane, api pane and integration screen (#7782)"
    This reverts commit b3d7f08d4ee82a3a69fe9a63e32caee5332759c6.

--- app/client/src/pages/Editor/SaaSEditor/ActionForm.tsx
+++ app/client/src/pages/Editor/SaaSEditor/ActionForm.tsx
@@ -23,11 +23,7 @@ import {
 import { getConfigInitialValues } from "components/formControls/utils";
 import { merge } from "lodash";
 import { Datasource } from "entities/Datasource";
-import {
-  INTEGRATION_EDITOR_MODES,
-  INTEGRATION_EDITOR_URL,
-  INTEGRATION_TABS,
-} from "constants/routes";
+import { INTEGRATION_EDITOR_URL, INTEGRATION_TABS } from "constants/routes";
 
 type StateAndRouteProps = EditorJSONtoFormProps &
   RouteComponentProps<{
@@ -57,12 +53,7 @@ function ActionForm(props: Props) {
   };
   const onCreateDatasourceClick = () => {
     history.push(
-      INTEGRATION_EDITOR_URL(
-        applicationId,
-        pageId,
-        INTEGRATION_TABS.NEW,
-        INTEGRATION_EDITOR_MODES.AUTO,
-      ),
+      INTEGRATION_EDITOR_URL(applicationId, pageId, INTEGRATION_TABS.NEW),
     );
   };
 
