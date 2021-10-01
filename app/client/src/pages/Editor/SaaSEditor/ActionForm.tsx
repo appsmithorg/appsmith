0619e3798f3edff10de450a0dc54afde84220352
rashmi rai <89374972+rashmi-rai@users.noreply.github.com>
2021-10-01 19:32:52 +0530

    fix: missing where key in plugin specified templates (#8040)

--- app/client/src/pages/Editor/SaaSEditor/ActionForm.tsx
+++ app/client/src/pages/Editor/SaaSEditor/ActionForm.tsx
@@ -15,7 +15,11 @@ import {
   getPlugin,
 } from "selectors/entitiesSelector";
 import { RouteComponentProps } from "react-router";
-import { deleteAction, runAction } from "actions/pluginActionActions";
+import {
+  deleteAction,
+  runAction,
+  setActionProperty,
+} from "actions/pluginActionActions";
 import {
   EditorJSONtoForm,
   EditorJSONtoFormProps,
@@ -28,9 +32,11 @@ import {
   INTEGRATION_EDITOR_URL,
   INTEGRATION_TABS,
 } from "constants/routes";
+import { diff, Diff } from "deep-diff";
 
-type StateAndRouteProps = EditorJSONtoFormProps &
-  RouteComponentProps<{
+type StateAndRouteProps = EditorJSONtoFormProps & {
+  actionObjectDiff?: any;
+} & RouteComponentProps<{
     applicationId: string;
     pageId: string;
     pluginPackageName: string;
@@ -52,9 +58,56 @@ function ActionForm(props: Props) {
     dispatch(deleteAction({ id: apiId, name: actionName }));
   };
 
+  //Following if block is the fix for the missing where key
+  /**
+   * NOTE:
+   * Action object returned by getAction comes from state.entities.action
+   * action api's payload is created from state.entities.action and response is saved in the same key
+   * Data passed to redux form is the merge of values present in state.entities.action, editorConfig, settingsConfig and has the correct datastrucure
+   * Data structure in state.entities.action is not correct
+   * Q. What does the following fix do?
+   * A. It calculates the diff between merged values and state.entities.action and saves the same in state.entities.action
+   * There is another key form that holds the formData
+   */
+  if (!!props.actionObjectDiff) {
+    let path = "";
+    let value = "";
+    // Loop through the diff objects in difference Array
+    for (let i = 0; i < props.actionObjectDiff.length; i++) {
+      //kind = N indicates a newly added property/element
+      //This property is present in initialValues but not in action object
+      if (props.actionObjectDiff[i]?.kind === "N") {
+        // Calculate path from path[] in diff
+        path = props.actionObjectDiff[i].path.reduce(
+          (acc: string, item: number | string) => {
+            if (typeof item === "string" && acc) {
+              acc += `${path}.${item}`;
+            } else if (typeof item === "string" && !acc) {
+              acc += `${item}`;
+            } else acc += `${path}[${item}]`;
+            return acc;
+          },
+          "",
+        );
+        // get value from diff object
+        value = props.actionObjectDiff[i]?.rhs;
+      }
+    }
+    if (value && path) {
+      dispatch(
+        setActionProperty({
+          actionId: apiId,
+          propertyName: path,
+          value: value,
+        }),
+      );
+    }
+  }
+
   const onRunClick = () => {
     dispatch(runAction(apiId));
   };
+
   const onCreateDatasourceClick = () => {
     history.push(
       INTEGRATION_EDITOR_URL(
@@ -103,6 +156,14 @@ const mapStateToProps = (state: AppState, props: any) => {
   }
   merge(initialValues, getConfigInitialValues(settingConfig));
   merge(initialValues, action);
+  // initialValues contains merge of action, editorConfig, settingsConfig and will be passed to redux form
+  // getting diff between action and initialValues
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const actionObjectDiff: undefined | Diff<Action | undefined, Action>[] = diff(
+    action,
+    initialValues,
+  );
 
   const dataSources = getDatasourceByPluginId(state, pluginId);
   const DATASOURCES_OPTIONS = dataSources.map((dataSource: Datasource) => ({
@@ -129,6 +190,7 @@ const mapStateToProps = (state: AppState, props: any) => {
     executedQueryData: responses[apiId],
     runErrorMessage: runErrorMessage[apiId],
     formName: SAAS_EDITOR_FORM,
+    actionObjectDiff,
   };
 };
 
