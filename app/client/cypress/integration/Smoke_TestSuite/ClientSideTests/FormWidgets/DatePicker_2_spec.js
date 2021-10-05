68eee4f5ced7edfce32504d62a9130542d2f6ca9
Priyansh Singh <63330165+ps-19@users.noreply.github.com>
2021-10-05 20:39:55 +0530

    unused variable

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/DatePicker_2_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/DatePicker_2_spec.js
@@ -2,7 +2,6 @@ const commonlocators = require("../../../../locators/commonlocators.json");
 const formWidgetsPage = require("../../../../locators/FormWidgets.json");
 const dsl = require("../../../../fixtures/datePicker2dsl.json");
 const publishPage = require("../../../../locators/publishWidgetspage.json");
-const pages = require("../../../../locators/Pages.json");
 
 describe("DatePicker Widget Property pane tests with js bindings", function() {
   before(() => {
