ede0cb4c066b0456460070e96c464ea6e5b3ffe9
Anagh Hegde <anagh@appsmith.com>
2021-10-06 14:24:34 +0530

    Refactor Git operation tests to server

--- app/server/appsmith-server/src/test/java/com/appsmith/server/git/GitExecutorTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/git/GitExecutorTest.java
@@ -1,4 +1,4 @@
-package com.appsmith.git;
+package com.appsmith.server.git;
 
 import com.appsmith.external.git.GitExecutor;
 import com.appsmith.git.configurations.GitServiceConfig;
@@ -8,21 +8,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 
 @Slf4j
+@Import({GitExecutorImpl.class, GitServiceConfig.class})
 @RunWith(SpringRunner.class)
+@ContextConfiguration
+@RequiredArgsConstructor
 public class GitExecutorTest {
 
-    @Autowired
     GitExecutor gitExecutor;
 
-    @Autowired
     GitServiceConfig gitServiceConfig;
 
     private final String gitRemoteUrl = "git@github.com:appsmithorg/appsmith-docs.git";
