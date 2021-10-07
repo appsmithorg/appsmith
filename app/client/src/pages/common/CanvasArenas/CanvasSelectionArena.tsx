03469f3f0e8110a15a84877e4d66ecb13db3219d
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2021-10-07 11:14:29 +0530

    few more bug fixes in draw to select.

--- app/client/src/pages/common/CanvasArenas/CanvasSelectionArena.tsx
+++ app/client/src/pages/common/CanvasArenas/CanvasSelectionArena.tsx
@@ -232,10 +232,8 @@ export function CanvasSelectionArena({
       };
 
       const onMouseLeave = () => {
-        if (widgetId === MAIN_CONTAINER_WIDGET_ID) {
-          document.body.addEventListener("mouseup", onMouseUp, false);
-          document.body.addEventListener("click", onClick, false);
-        }
+        document.body.addEventListener("mouseup", onMouseUp, false);
+        document.body.addEventListener("click", onClick, false);
       };
 
       const onMouseEnter = (e: any) => {
@@ -246,7 +244,7 @@ export function CanvasSelectionArena({
         ) {
           firstRender(e, true);
           drawOnEnterObj.current = defaultDrawOnObj;
-        } else if (widgetId === MAIN_CONTAINER_WIDGET_ID) {
+        } else {
           document.body.removeEventListener("mouseup", onMouseUp);
           document.body.removeEventListener("click", onClick);
         }
@@ -328,7 +326,9 @@ export function CanvasSelectionArena({
       };
 
       const onMouseDown = (e: any) => {
+        const isNotRightClick = !(e.which === 3 || e.button === 2);
         if (
+          isNotRightClick &&
           slidingArenaRef.current &&
           (!isDraggableParent || e.ctrlKey || e.metaKey)
         ) {
