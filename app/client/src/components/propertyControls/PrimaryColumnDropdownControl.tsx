dc45cddbcbde08164387228dff6c38a19413bd3f
vicky.bansal@primathon.in <vicky.bansal@primathon.in>
2021-10-08 14:15:26 +0530

    Remove eslint suppressions

--- app/client/src/components/propertyControls/PrimaryColumnDropdownControl.tsx
+++ app/client/src/components/propertyControls/PrimaryColumnDropdownControl.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import React from "react";
 import BaseControl, { ControlProps } from "./BaseControl";
 import { ColumnProperties } from "widgets/TableWidget/component/Constants";
