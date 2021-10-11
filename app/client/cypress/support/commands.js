35cf61822128e8c6241493dee5a1bb4b3a723e8b
arunvjn <arun@appsmith.com>
2021-10-11 09:52:55 +0530

    Fixed cypress test

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -2389,13 +2389,6 @@ Cypress.Commands.add("CreateMockQuery", (queryName) => {
     force: true,
   });
   cy.assertPageSave();
-  cy.get(queryEditor.templateMenu + " div")
-    .contains("Select")
-    .click({ force: true });
-  cy.runQuery();
-  // cy.wait(3000);
-  // cy.get(queryEditor.runQuery)
-  //   .click({force: true});
 });
 
 Cypress.Commands.add("openPropertyPane", (widgetType) => {
