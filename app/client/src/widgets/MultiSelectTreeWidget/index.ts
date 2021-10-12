2d53da77066b2fc7aade69986324bf6563f89232
AR10X <arehamanofficial@gmail.com>
2021-10-12 19:24:50 +0530

    Removed space

--- app/client/src/widgets/MultiSelectTreeWidget/index.ts
+++ app/client/src/widgets/MultiSelectTreeWidget/index.ts
@@ -29,7 +29,7 @@ export const CONFIG = {
       { label: "Green", value: "GREEN" },
       { label: "Red", value: "RED" },
     ],
-    widgetName: "Multi TreeSelect",
+    widgetName: "MultiTreeSelect",
     defaultOptionValue: ["GREEN"],
     version: 1,
     isVisible: true,
