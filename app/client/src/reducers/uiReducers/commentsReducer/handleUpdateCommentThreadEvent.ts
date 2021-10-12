821aee6e4d92c7f087bb5af1d38d83e50e307770
Pranav Kanade <pranav@appsmith.com>
2021-10-12 17:42:16 +0530

    no need to deep copy the state

--- app/client/src/reducers/uiReducers/commentsReducer/handleUpdateCommentThreadEvent.ts
+++ app/client/src/reducers/uiReducers/commentsReducer/handleUpdateCommentThreadEvent.ts
@@ -1,6 +1,6 @@
 import { CommentThread } from "entities/Comments/CommentsInterfaces";
 import { ReduxAction } from "constants/ReduxActionConstants";
-import { cloneDeep, get, uniqBy } from "lodash";
+import { get, uniqBy } from "lodash";
 import { CommentsReduxState } from "./interfaces";
 
 // TODO verify cases where commentThread can be undefined for update event
@@ -9,8 +9,7 @@ const handleUpdateCommentThreadEvent = (
   action: ReduxAction<Partial<CommentThread & { _id: string }>>,
 ) => {
   const id = action.payload._id as string;
-  const copiedState = cloneDeep(state);
-  const commentThreadInStore = copiedState.commentThreadsMap[id];
+  const commentThreadInStore = state.commentThreadsMap[id];
   const existingComments = get(commentThreadInStore, "comments", []);
   const newComments = get(action.payload, "comments", []);
 
@@ -24,26 +23,26 @@ const handleUpdateCommentThreadEvent = (
 
   const shouldRefreshList = resolvedStateUpdated || pinnedStateChanged;
 
-  copiedState.commentThreadsMap[id] = {
+  state.commentThreadsMap[id] = {
     ...(commentThreadInStore || {}),
     ...action.payload,
     comments: uniqBy([...existingComments, ...newComments], "id"),
   };
 
   if (shouldRefreshList) {
-    copiedState.applicationCommentThreadsByRef[
+    state.applicationCommentThreadsByRef[
       action.payload.applicationId as string
     ] = {
-      ...copiedState.applicationCommentThreadsByRef[
+      ...state.applicationCommentThreadsByRef[
         action.payload.applicationId as string
       ],
     };
   }
 
-  const showUnreadIndicator = !copiedState.isCommentMode;
+  const showUnreadIndicator = !state.isCommentMode;
 
   return {
-    ...copiedState,
+    ...state,
     showUnreadIndicator,
     lastUpdatedCommentThreadId: id,
   };
