99459f3ebd2dfa8d1adfb5d726379e459f23580b
Avi Kathuria <avikathuria21@gmail.com>
2021-10-23 09:16:54 +0000

    Fixing Video URL Regex issue

--- app/client/src/widgets/VideoWidget/widget/index.tsx
+++ app/client/src/widgets/VideoWidget/widget/index.tsx
@@ -34,7 +34,7 @@ class VideoWidget extends BaseWidget<VideoWidgetProps, WidgetState> {
             validation: {
               type: ValidationTypes.TEXT,
               params: {
-                regex: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+                regex: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/=]*)/,
                 expected: {
                   type: "Video URL",
                   example: "https://www.youtube.com/watch?v=mzqK0QIZRLs",
