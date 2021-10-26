761e7b696770df138d694e776399913a9e96a6dd
sbalaji1192 <sbalaji1192@gmail.com>
2021-10-26 11:22:55 +0530

    wip

--- app/client/cypress/support/index.js
+++ app/client/cypress/support/index.js
@@ -43,11 +43,13 @@ before(function() {
   cy.url().then((url) => {
     if (url.indexOf("setup/welcome") > -1) {
       cy.createSuperUser();
+      cy.LogOut();
     }
   });
 
   cy.SignupFromAPI(Cypress.env("TESTUSERNAME1"), Cypress.env("TESTPASSWORD1"));
   cy.SignupFromAPI(Cypress.env("TESTUSERNAME2"), Cypress.env("TESTPASSWORD2"));
+  cy.LogOut();
   cy.wait(3000);
   const username = Cypress.env("USERNAME");
   const password = Cypress.env("PASSWORD");
