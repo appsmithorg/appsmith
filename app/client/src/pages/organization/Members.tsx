112a1c403c910d3d74593b63907c0898fff79475
haojin111 <haojin828@outlook.com>
2021-10-26 17:28:50 +0800

    replaced hard coded user name

--- app/client/src/pages/organization/Members.tsx
+++ app/client/src/pages/organization/Members.tsx
@@ -351,7 +351,7 @@ export default function MemberSettings(props: PageProps) {
                       className="avatar"
                       side={71}
                       source={`/api/${USER_PHOTO_URL}/${user.username}`}
-                      userName={"Test User"}
+                      userName={user.name || user.username}
                     />
                     <Text type={TextType.P1}>{user.name || user.username}</Text>
                     <Text className="email" type={TextType.P1}>
