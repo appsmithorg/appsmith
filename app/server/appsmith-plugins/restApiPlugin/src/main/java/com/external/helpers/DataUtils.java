ed4d142fbeefe0e370a9244e79f2f98db7194746
Nidhi <nidhi.nair93@gmail.com>
2021-11-06 19:07:36 +0530

    Review comment

--- app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/helpers/DataUtils.java
+++ app/server/appsmith-plugins/restApiPlugin/src/main/java/com/external/helpers/DataUtils.java
@@ -155,7 +155,10 @@ public class DataUtils {
                             try {
                                 populateFileTypeBodyBuilder(bodyBuilder, property, outputMessage);
                             } catch (JsonProcessingException e) {
-                                e.printStackTrace();
+                                throw new AppsmithPluginException(
+                                        AppsmithPluginError.PLUGIN_DATASOURCE_ARGUMENT_ERROR,
+                                        "Unable to parse content. Expected to receive an array or object of multipart data"
+                                );
                             }
                         }
                     }
