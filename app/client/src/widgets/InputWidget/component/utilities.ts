dc00515fd7b20584e4e1e25d0e4fad400c86becb
Vicky <vicky.bansal@primathon.in>
2021-11-09 13:41:56 +0530

    Fix currency number formating and add unit tests

--- app/client/src/widgets/InputWidget/component/utilities.ts
+++ app/client/src/widgets/InputWidget/component/utilities.ts
@@ -7,3 +7,40 @@ export const countryToFlag = (isoCode: string) => {
         )
     : isoCode;
 };
+
+export const formatCurrencyNumber = (decimalsInCurrency = 0, value: string) => {
+  const fractionDigits = decimalsInCurrency || 0;
+  const currentIndexOfDecimal = value.indexOf(".");
+  const indexOfDecimal = value.length - fractionDigits - 1;
+  const isDecimal =
+    value.includes(".") && currentIndexOfDecimal <= indexOfDecimal;
+  const locale = navigator.languages?.[0] || "en-US";
+  const formatter = new Intl.NumberFormat(locale, {
+    style: "decimal",
+    minimumFractionDigits: isDecimal ? fractionDigits : 0,
+  });
+  return formatter.format(parseFloat(value));
+};
+
+export const limitDecimalValue = (
+  decimalsInCurrency = 0,
+  valueAsString: string,
+) => {
+  let value = valueAsString.split(",").join("");
+  if (value) {
+    const decimalValueArray = value.split(".");
+    //remove extra digits after decimal point
+    if (
+      decimalsInCurrency &&
+      decimalValueArray[1].length > decimalsInCurrency
+    ) {
+      value =
+        decimalValueArray[0] +
+        "." +
+        decimalValueArray[1].substr(0, decimalsInCurrency);
+    }
+    return value;
+  } else {
+    return "";
+  }
+};
