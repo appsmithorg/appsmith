689b5bc5b70082f5430605dd116bf4cdc35fd507
hetunandu <hetu@appsmith.com>
2021-11-11 11:01:29 +0530

    add clear interval test

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/setInterval_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/setInterval_spec.js
@@ -13,7 +13,7 @@ describe("Test Create Api and Bind to Table widget", function() {
     cy.RunAPI();
   });
 
-  it("Fill button and test code generated", () => {
+  it("selects set interval function", () => {
     cy.SearchEntityandOpen("Button1");
     cy.get(widgetsPage.buttonOnClick)
       .last()
@@ -22,7 +22,9 @@ describe("Test Create Api and Bind to Table widget", function() {
       .children()
       .contains("Set interval")
       .click();
+  });
 
+  it("Fill setInterval action creator and test code generated", () => {
     cy.get("label")
       .contains("Callback function")
       .parent()
@@ -47,6 +49,10 @@ describe("Test Create Api and Bind to Table widget", function() {
         "{{setInterval(() => { Api1.run() }, 5000,'myInterval')}}",
       );
 
+    cy.get(widgetsPage.toggleOnClick).click();
+  });
+
+  it("works in the published version", () => {
     cy.PublishtheApp();
 
     cy.get(publishPage.buttonWidget).click();
@@ -62,5 +68,33 @@ describe("Test Create Api and Bind to Table widget", function() {
       "response.body.responseMeta.status",
       200,
     );
+    cy.get(publishPage.backToEditor).click({ force: true });
+  });
+
+  it("selects clear interval function", () => {
+    cy.SearchEntityandOpen("Button1");
+    cy.get(widgetsPage.buttonOnClick)
+      .last()
+      .click({ force: true });
+    cy.get(commonlocators.chooseAction)
+      .children()
+      .contains("Clear interval")
+      .click();
+  });
+
+  it("Fill clearInterval action creator and test code generated", () => {
+    cy.get("label")
+      .contains("Id")
+      .parent()
+      .then(($el) => {
+        cy.updateCodeInput($el, "myInterval");
+      });
+
+    cy.get(widgetsPage.toggleOnClick).click();
+
+    cy.get(".t--property-control-onclick")
+      .find(".CodeMirror-code")
+      .invoke("text")
+      .should("equal", "{{clearInterval('myInterval')}}");
   });
 });
