9b1794c40f19ca78ddeea4345a5d3641cf51914f
Aswath K <aswath@appsmith.com>
2021-11-12 18:00:45 +0530

    Adds position validation in test

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Button_Disabled_validation_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Button_Disabled_validation_spec.js
@@ -7,15 +7,22 @@ describe("Disabled Button Widget Functionality", function() {
   });
 
   it("Should be able to drag disabled button", function() {
-    cy.wait(500);
-    cy.get(".t--draggable-buttonwidget button")
-      .wait(500)
+    const selector = ".t--draggable-buttonwidget button";
+    cy.wait(1000);
+    cy.get(selector).then((button) => {
+      cy.wrap(button[0].getBoundingClientRect()).as("initialPosition");
+    });
+    cy.get(selector)
       .realHover()
       .trigger("dragstart", { force: true });
     cy.get(explorer.dropHere)
       .trigger("mousemove", 200, 400, { eventConstructor: "MouseEvent" })
       .trigger("mousemove", 200, 400, { eventConstructor: "MouseEvent" })
       .trigger("mouseup", 200, 400, { eventConstructor: "MouseEvent" });
+
+    cy.get(selector).then((button) => {
+      expect("initialPosition").not.equal(button[0].getBoundingClientRect());
+    });
     cy.wait("@updateLayout").should(
       "have.nested.property",
       "response.body.responseMeta.status",
