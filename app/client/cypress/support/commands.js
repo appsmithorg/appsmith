089f3a225f3ad5079adc03d6c6fc9c58df1a29f9
Albin <albin@appsmith.com>
2021-11-16 17:11:09 +0530

    * cypress fix

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -995,7 +995,7 @@ Cypress.Commands.add("CreationOfUniqueAPIcheck", (apiname) => {
     .focus()
     .type(apiname, { force: true, delay: 500 })
     .should("have.value", apiname);
-  cy.get(".error-message").should(($x) => {
+  cy.get(".t--action-name-edit-error").should(($x) => {
     expect($x).contain(
       apiname.concat(" is already being used or is a restricted keyword."),
     );
