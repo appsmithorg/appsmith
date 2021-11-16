c1adc140563078d28aa5c2cbf305e135ae4bbce1
Aswath K <aswath@appsmith.com>
2021-11-16 16:30:36 +0530

    Adds test for menutbutton and iconbutton

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Button_Disabled_validation_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Button_Disabled_validation_spec.js
@@ -16,9 +16,54 @@ describe("Disabled Button Widget Functionality", function() {
       .realHover()
       .trigger("dragstart", { force: true });
     cy.get(explorer.dropHere)
-      .trigger("mousemove", 200, 400, { eventConstructor: "MouseEvent" })
-      .trigger("mousemove", 200, 400, { eventConstructor: "MouseEvent" })
-      .trigger("mouseup", 200, 400, { eventConstructor: "MouseEvent" });
+      .trigger("mousemove", 200, 300, { eventConstructor: "MouseEvent" })
+      .trigger("mouseup", 200, 300, { eventConstructor: "MouseEvent" });
+
+    cy.get(selector).then((button) => {
+      expect("initialPosition").not.equal(button[0].getBoundingClientRect());
+    });
+    cy.wait("@updateLayout").should(
+      "have.nested.property",
+      "response.body.responseMeta.status",
+      200,
+    );
+  });
+
+  it("Should be able to drag disabled menu button", function() {
+    const selector = ".t--draggable-menubuttonwidget button";
+    cy.wait(1000);
+    cy.get(selector).then((button) => {
+      cy.wrap(button[0].getBoundingClientRect()).as("initialPosition");
+    });
+    cy.get(selector)
+      .realHover()
+      .trigger("dragstart", { force: true });
+    cy.get(explorer.dropHere)
+      .trigger("mousemove", 600, 300, { eventConstructor: "MouseEvent" })
+      .trigger("mouseup", 600, 300, { eventConstructor: "MouseEvent" });
+
+    cy.get(selector).then((button) => {
+      expect("initialPosition").not.equal(button[0].getBoundingClientRect());
+    });
+    cy.wait("@updateLayout").should(
+      "have.nested.property",
+      "response.body.responseMeta.status",
+      200,
+    );
+  });
+
+  it("Should be able to drag disabled icon button", function() {
+    const selector = ".t--draggable-iconbuttonwidget button";
+    cy.wait(1000);
+    cy.get(selector).then((button) => {
+      cy.wrap(button[0].getBoundingClientRect()).as("initialPosition");
+    });
+    cy.get(selector)
+      .realHover()
+      .trigger("dragstart", { force: true });
+    cy.get(explorer.dropHere)
+      .trigger("mousemove", 200, 200, { eventConstructor: "MouseEvent" })
+      .trigger("mouseup", 200, 200, { eventConstructor: "MouseEvent" });
 
     cy.get(selector).then((button) => {
       expect("initialPosition").not.equal(button[0].getBoundingClientRect());
