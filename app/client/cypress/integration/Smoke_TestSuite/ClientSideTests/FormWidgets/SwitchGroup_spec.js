34e21f01e537fd674bf7260530178412c6eb9921
Paul Li <wmdev0808@gmail.com>
2021-11-17 12:49:30 -0500

    feat: Switch Group Widget
    -- Rewrite a test case for onSelectionChange property

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/SwitchGroup_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/SwitchGroup_spec.js
@@ -1,5 +1,6 @@
 const commonlocators = require("../../../../locators/commonlocators.json");
 const formWidgetsPage = require("../../../../locators/FormWidgets.json");
+const modalWidgetPage = require("../../../../locators/ModalWidget.json");
 const publish = require("../../../../locators/publishWidgetspage.json");
 const dsl = require("../../../../fixtures/SwitchGroupWidgetDsl.json");
 
@@ -23,6 +24,7 @@ describe("Switch Group Widget Functionality", function() {
      * @param{RadioWidget}Mouseover
      * @param{RadioPre Css} Assertion
      */
+    cy.openPropertyPane("switchgroupwidget");
     cy.widgetText(
       "switchgrouptest",
       formWidgetsPage.switchGroupWidget,
@@ -69,18 +71,6 @@ describe("Switch Group Widget Functionality", function() {
       .contains("Green");
   });
 
-  it("Property: onSelectionChange", function() {
-    /**
-     * @param{Show Alert} Css for InputChange
-     */
-    cy.getAlert(commonlocators.optionchangeRadioselect);
-    cy.get(formWidgetsPage.radioOnSelectionChangeDropdown)
-      .get(commonlocators.dropdownSelectButton)
-      .click({ force: true })
-      .type("2");
-    cy.PublishtheApp();
-  });
-
   it("Property: isVisible === FALSE", function() {
     cy.togglebarDisable(commonlocators.visibleCheckbox);
     cy.PublishtheApp();
@@ -88,10 +78,26 @@ describe("Switch Group Widget Functionality", function() {
   });
 
   it("Property: isVisible === TRUE", function() {
+    cy.openPropertyPane("switchgroupwidget");
     cy.togglebar(commonlocators.visibleCheckbox);
     cy.PublishtheApp();
     cy.get(publish.checkboxGroupWidget + " " + "input")
       .eq(0)
       .should("exist");
+  });
+
+  it("Property: onSelectionChange", function() {
+    cy.openPropertyPane("switchgroupwidget");
+    // create an alert modal and verify its name
+    cy.createModal(this.data.ModalName);
+    cy.PublishtheApp();
+    cy.get(publish.switchGroupWidget)
+      .children("label.bp3-switch")
+      .first()
+      .click();
+    cy.get(modalWidgetPage.modelTextField).should(
+      "have.text",
+      this.data.ModalName,
+    );
   });
 });
