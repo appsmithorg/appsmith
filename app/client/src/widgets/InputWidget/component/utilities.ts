245c519ec1eeaa19767549ecb52686502f5fd636
vicky.bansal@primathon.in <vicky.bansal@primathon.in>
2021-11-19 14:46:16 +0530

    Add comments for utilities formatCurrencyNumber and limitDecimalValue

--- app/client/src/widgets/InputWidget/component/utilities.ts
+++ app/client/src/widgets/InputWidget/component/utilities.ts
@@ -8,6 +8,13 @@ export const countryToFlag = (isoCode: string) => {
     : isoCode;
 };
 
+/*
+ Returns formatted value with maximum number of decimals based on decimalsInCurrency value 
+ and add commas based on user's local browser
+  for eg:
+  a) (2, 1235.456) will return 1,234.56 
+  b) (1, 1234.456) will return 1,234.5
+*/
 export const formatCurrencyNumber = (decimalsInCurrency = 0, value: string) => {
   const fractionDigits = decimalsInCurrency || 0;
   const currentIndexOfDecimal = value.indexOf(".");
@@ -22,6 +29,12 @@ export const formatCurrencyNumber = (decimalsInCurrency = 0, value: string) => {
   return formatter.format(parseFloat(value));
 };
 
+/*
+ Returns number in string format with maximum number of decimals based on decimalsInCurrency value
+  for eg:
+  a) (2, 1235.456) will return 1234.56 
+  b) (1, 1234.456) will return 1234.5
+*/
 export const limitDecimalValue = (
   decimalsInCurrency = 0,
   valueAsString: string,
