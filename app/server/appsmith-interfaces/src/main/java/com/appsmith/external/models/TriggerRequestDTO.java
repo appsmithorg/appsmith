4fb1cb8101b7bb9cb12bfe2de31669bfc44e0733
Trisha Anand <trisha@appsmith.com>
2021-11-19 15:13:53 +0530

    Adding minor comments

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/TriggerRequestDTO.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/TriggerRequestDTO.java
@@ -12,7 +12,14 @@ import java.util.List;
 @Getter
 @Setter
 public class TriggerRequestDTO {
+
     TriggerRequestType requestType;
+
+    // Comma separated parameters in the correct order.
+    // e.g. for GSheets, it may look like the following :
+    // fileUrl, Sheet1, <HeaderRowIndex>
+    // The above parameters would return all the column names
     List<String> parameters;
+
     ClientDataDisplayType displayType;
 }
