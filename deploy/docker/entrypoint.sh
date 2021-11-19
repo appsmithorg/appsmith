615b6c2a322bdec627f9e96de46569587f60e36e
geekup-legodevops <prj_legodevops@geekup.vn>
2021-11-19 22:50:44 +0700

    Add confirmation step in importing DB command

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -141,7 +141,7 @@ configure_ssl() {
 configure_supervisord() {
   SUPERVISORD_CONF_PATH="/opt/appsmith/templates/supervisord"
   if [[ -z "$(ls -A /etc/supervisor/conf.d)" ]]; then
-    rm "/etc/supervisor/conf.d/"*
+    rm -f "/etc/supervisor/conf.d/"*
   fi
 
   cp -f "$SUPERVISORD_CONF_PATH/application_process/"*.conf /etc/supervisor/conf.d
@@ -152,8 +152,8 @@ configure_supervisord() {
   fi
   if [[ "$APPSMITH_REDIS_URL" = "redis://127.0.0.1:6379" ]]; then
     cp "$SUPERVISORD_CONF_PATH/redis.conf" /etc/supervisor/conf.d/
-		# Enable saving Redis session data to disk more often, so recent sessions aren't cleared on restart.
-		sed -i 's/^# save 60 10000$/save 60 1/g' /etc/redis/redis.conf
+    # Enable saving Redis session data to disk more often, so recent sessions aren't cleared on restart.
+    sed -i 's/^# save 60 10000$/save 60 1/g' /etc/redis/redis.conf
   fi
   if ! [[ -e "/appsmith-stacks/ssl/fullchain.pem" ]] || ! [[ -e "/appsmith-stacks/ssl/privkey.pem" ]]; then
     cp "$SUPERVISORD_CONF_PATH/cron.conf" /etc/supervisor/conf.d/
