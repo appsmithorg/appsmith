2c120cdbbea0901ef1a48ca33856f0577ee8a0fb
Nayan <nayan@appsmith.com>
2021-11-23 13:28:44 +0600

    -fetch themes by application

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomThemeRepository.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomThemeRepository.java
@@ -1,7 +1,8 @@
 package com.appsmith.server.repositories;
 
 import com.appsmith.server.domains.Theme;
+import reactor.core.publisher.Flux;
 
 public interface CustomThemeRepository extends AppsmithRepository<Theme> {
-
+    Flux<Theme> getApplicationThemes(String applicationId);
 }
