a5c46b4c68c2da52755842aa271d624543ac6a46
Akash N <akash@codemonk.in>
2021-11-23 14:23:38 +0530

    Update blueprint

--- app/client/src/pages/Editor/GuidedTour/constants.ts
+++ app/client/src/pages/Editor/GuidedTour/constants.ts
@@ -14,18 +14,82 @@ export const onboardingContainerBlueprint = {
         blueprint: {
           view: [
             {
-              type: "INPUT_WIDGET",
+              type: "TEXT_WIDGET",
               size: {
                 rows: 1 * GRID_DENSITY_MIGRATION_V1,
                 cols: 8 * GRID_DENSITY_MIGRATION_V1,
               },
               position: {
-                top: 0 * GRID_DENSITY_MIGRATION_V1,
+                top: 0,
+                left: 0,
+              },
+              props: {
+                text: "🧑‍🦱 Customer Update Form",
+                version: 1,
+                fontStyle: "BOLD",
+              },
+            },
+            {
+              type: "IMAGE_WIDGET",
+              size: {
+                rows: 4 * GRID_DENSITY_MIGRATION_V1,
+                cols: 6 * GRID_DENSITY_MIGRATION_V1,
+              },
+              position: {
+                top: 1.5 * GRID_DENSITY_MIGRATION_V1,
                 left: 0 * GRID_DENSITY_MIGRATION_V1,
               },
               props: {
-                label: "Customer Name",
                 version: 1,
+                objectFit: "cover",
+              },
+            },
+            {
+              type: "INPUT_WIDGET",
+              size: {
+                rows: 1 * GRID_DENSITY_MIGRATION_V1,
+                cols: 9.1 * GRID_DENSITY_MIGRATION_V1,
+              },
+              position: {
+                top: 2 * GRID_DENSITY_MIGRATION_V1,
+                left: 6.9 * GRID_DENSITY_MIGRATION_V1,
+              },
+              props: {
+                label: "Name",
+                version: 1,
+                labelStyle: "bold,BOLD",
+              },
+            },
+            {
+              type: "INPUT_WIDGET",
+              size: {
+                rows: 1 * GRID_DENSITY_MIGRATION_V1,
+                cols: 9 * GRID_DENSITY_MIGRATION_V1,
+              },
+              position: {
+                top: 3.5 * GRID_DENSITY_MIGRATION_V1,
+                left: 7 * GRID_DENSITY_MIGRATION_V1,
+              },
+              props: {
+                label: "Email",
+                version: 1,
+                labelStyle: "bold,BOLD",
+              },
+            },
+            {
+              type: "INPUT_WIDGET",
+              size: {
+                rows: 1 * GRID_DENSITY_MIGRATION_V1,
+                cols: 9.6 * GRID_DENSITY_MIGRATION_V1,
+              },
+              position: {
+                top: 5 * GRID_DENSITY_MIGRATION_V1,
+                left: 6.3 * GRID_DENSITY_MIGRATION_V1,
+              },
+              props: {
+                label: "Country",
+                version: 1,
+                labelStyle: "bold,BOLD",
               },
             },
           ],
