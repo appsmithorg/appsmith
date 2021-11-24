a6d982f982f3aa8e67a67ae535f3d003b08e29cc
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2021-11-24 12:17:44 +0530

    remove escape func from test

--- app/client/src/widgets/WidgetUtils.test.ts
+++ app/client/src/widgets/WidgetUtils.test.ts
@@ -124,14 +124,11 @@ describe("validate widget utils button style functions", () => {
   });
 
   it("validate escaping special characters", () => {
-    const escape = (value: string) => {
-      return value.replace(/\n/g, "\\n");
-    };
     const testString = `a\nb\nc
 hello! how are you?
 `;
     const result = escapeSpecialChars(testString);
-    const expectedResult = "a\\nb\\nc\\nhello! how are you?\\n";
-    expect(escape(result)).toStrictEqual(expectedResult);
+    const expectedResult = "a\nb\nc\nhello! how are you?\n";
+    expect(result).toStrictEqual(expectedResult);
   });
 });
