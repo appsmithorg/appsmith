0b18ea64f368169830e0b0012cf7b3310f4f338f
Confidence Okoghenun <cokoghenun@gmail.com>
2021-11-26 10:32:04 +0100

    chore: Renames actions to event on property pane (#8579)

--- app/client/src/widgets/MenuButtonWidget/widget/index.tsx
+++ app/client/src/widgets/MenuButtonWidget/widget/index.tsx
@@ -166,7 +166,7 @@ class MenuButtonWidget extends BaseWidget<MenuButtonWidgetProps, WidgetState> {
                   ],
                 },
                 {
-                  sectionName: "Actions",
+                  sectionName: "Events",
                   children: [
                     {
                       helpText:
