3cb4e850c70028f0fae5308e2b3d87a591d69b29
Nayan <nayan@appsmith.com>
2021-11-30 12:30:44 +0600

    -added constraint annotations

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/TestEmailConfigRequestDTO.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/TestEmailConfigRequestDTO.java
@@ -2,11 +2,29 @@ package com.appsmith.server.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class TestEmailConfigRequestDTO {
+    @NotNull
+    @NotEmpty
     private String smtpHost;
+
+    @NotNull
     private Integer smtpPort;
+
+    @NotNull
+    @NotEmpty
     private String username;
+
+    @NotNull
+    @NotEmpty
     private String password;
+
+    @NotNull
+    @NotEmpty
+    @Email
     private String fromEmail;
 }
