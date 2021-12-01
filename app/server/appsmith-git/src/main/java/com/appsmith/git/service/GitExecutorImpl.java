cfa0c75eb132da275d72273c25155d532cc27ba9
Anagh Hegde <anagh@appsmith.com>
2021-12-01 17:15:14 +0530

    Fix bad merge

--- app/server/appsmith-git/src/main/java/com/appsmith/git/service/GitExecutorImpl.java
+++ app/server/appsmith-git/src/main/java/com/appsmith/git/service/GitExecutorImpl.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.MergeCommand;
 import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.ResetCommand;
