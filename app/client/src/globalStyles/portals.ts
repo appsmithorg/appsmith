8f8e7e48b861cd6e1d50e0768ccaa92aaa96e124
Albin <albin@appsmith.com>
2021-12-01 12:16:57 +0530

    * Colors to constants

--- app/client/src/globalStyles/portals.ts
+++ app/client/src/globalStyles/portals.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from "styled-components";
 import { Layers } from "constants/Layers";
 import { Classes } from "@blueprintjs/core";
+import { Colors } from "constants/Colors";
 
 export const replayHighlightClass = "ur--has-border";
 
@@ -30,11 +31,11 @@ export const PortalStyles = createGlobalStyle`
   }
 
   .flash .${replayHighlightClass} {
-    border-color: #FEB811 !important;
+    border-color: ${Colors.WARNING_SOLID} !important;
     box-shadow: 0px 0px 4px 0.5px rgba(254, 184, 17, 0.7) !important;
 
     & > input, .CodeMirror, .appsmith-select__control {
-      border-color: #FEB811 !important;
+      border-color: ${Colors.WARNING_SOLID} !important;
       box-shadow: 0px 0px 4px 0.5px rgba(254, 184, 17, 0.7) !important;
     }
   }
