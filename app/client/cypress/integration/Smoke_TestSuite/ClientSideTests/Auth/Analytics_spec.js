156e4ec1dbcc55f1cefaa335365fd56afbb03ceb
sbalaji1192 <balaji@appsmith.com>
2021-12-03 00:50:06 +0530

    remove invalid test cases

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Auth/Analytics_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Auth/Analytics_spec.js
@@ -1,30 +1,5 @@
 import User from "../../../../fixtures/user.json";
 
-function mockAnalyticsAndTrackersKey() {
-  cy.window().then((window) => {
-    if (!window.APPSMITH_FEATURE_CONFIGS.segment) {
-      window.APPSMITH_FEATURE_CONFIGS.segment = {
-        apiKey: "test",
-        ceKey: "test",
-      };
-    }
-
-    if (!window.APPSMITH_FEATURE_CONFIGS.smartLook) {
-      window.APPSMITH_FEATURE_CONFIGS.smartLook = {
-        id: "test",
-      };
-    }
-
-    if (!window.APPSMITH_FEATURE_CONFIGS.sentry) {
-      window.APPSMITH_FEATURE_CONFIGS.sentry = {
-        dsn: "test",
-        release: "test",
-        environment: "test",
-      };
-    }
-  });
-}
-
 let appId;
 
 describe("Checks for analytics initialization", function() {
@@ -34,7 +9,6 @@ describe("Checks for analytics initialization", function() {
     }).as("getUsersWithoutTelemetry");
     cy.visit("/applications");
     cy.reload();
-    mockAnalyticsAndTrackersKey();
     cy.wait("@getUsersWithoutTelemetry");
     cy.window().then((window) => {
       expect(window.analytics).to.be.equal(undefined);
@@ -66,7 +40,6 @@ describe("Checks for analytics initialization", function() {
     }).as("getUsersWithTelemetry");
     cy.visit("/applications");
     cy.reload();
-    mockAnalyticsAndTrackersKey();
     cy.wait("@getUsersWithTelemetry");
     cy.wait(5000);
     cy.window().then((window) => {
@@ -95,29 +68,23 @@ describe("Checks for analytics initialization", function() {
     }).as("getUsersWithoutTelemetry");
     cy.visit("/applications");
     cy.reload();
-    mockAnalyticsAndTrackersKey();
     cy.wait("@getUsersWithoutTelemetry");
     cy.window().then((window) => {
       expect(window.smartlook).to.be.equal(undefined);
     });
-  });
-  it("Should check smartlook is initialised when enableTelemtry is true", function() {
-    cy.intercept("GET", "/api/v1/users/me", {
-      body: {
-        responseMeta: { status: 200, success: true },
-        data: {
-          ...User,
-          enableTelemetry: true,
-        },
-      },
-    }).as("getUsersWithTelemetry");
-    cy.visit("/applications");
-    cy.reload();
-    mockAnalyticsAndTrackersKey();
-    cy.wait("@getUsersWithTelemetry");
-    cy.wait(5000);
-    cy.window().then((window) => {
-      expect(window.smartlook).not.to.be.undefined;
+    let interceptFlag = false;
+    cy.intercept("POST", "https://**.smartlook.**", (req) => {
+      interceptFlag = true;
+      req.continue();
+    });
+    cy.generateUUID().then((id) => {
+      appId = id;
+      cy.CreateAppInFirstListedOrg(id);
+      localStorage.setItem("AppName", appId);
+    });
+    cy.wait(3000);
+    cy.window().then(() => {
+      cy.wrap(interceptFlag).should("eq", false);
     });
   });
 
@@ -127,29 +94,23 @@ describe("Checks for analytics initialization", function() {
     }).as("getUsersWithoutTelemetry");
     cy.visit("/applications");
     cy.reload();
-    mockAnalyticsAndTrackersKey();
     cy.wait("@getUsersWithoutTelemetry");
     cy.window().then((window) => {
       expect(window.Sentry).to.be.equal(undefined);
     });
-  });
-  it("Should check Sentry is initialised when enableTelemtry is true", function() {
-    cy.intercept("GET", "/api/v1/users/me", {
-      body: {
-        responseMeta: { status: 200, success: true },
-        data: {
-          ...User,
-          enableTelemetry: true,
-        },
-      },
-    }).as("getUsersWithTelemetry");
-    cy.visit("/applications");
-    cy.reload();
-    mockAnalyticsAndTrackersKey();
-    cy.wait("@getUsersWithTelemetry");
-    cy.wait(5000);
-    cy.window().then((window) => {
-      expect(window.Sentry).not.to.be.undefined;
+    let interceptFlag = false;
+    cy.intercept("POST", "https://**.sentry.io/**", (req) => {
+      interceptFlag = true;
+      req.continue();
+    });
+    cy.generateUUID().then((id) => {
+      appId = id;
+      cy.CreateAppInFirstListedOrg(id);
+      localStorage.setItem("AppName", appId);
+    });
+    cy.wait(3000);
+    cy.window().then(() => {
+      cy.wrap(interceptFlag).should("eq", false);
     });
   });
 });
