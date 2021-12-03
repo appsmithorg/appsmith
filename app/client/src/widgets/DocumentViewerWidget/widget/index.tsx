1760f250b96bc63f5648cecbade4a9b382c1011d
bhavin <techbhavin@gmail.com>
2021-12-03 20:38:38 +0530

    fix: animateLoading added into DocumentViewerWidget

--- app/client/src/widgets/DocumentViewerWidget/widget/index.tsx
+++ app/client/src/widgets/DocumentViewerWidget/widget/index.tsx
@@ -100,6 +100,16 @@ class DocumentViewerWidget extends BaseWidget<
             isTriggerProperty: false,
             validation: { type: ValidationTypes.BOOLEAN },
           },
+          {
+            propertyName: "animateLoading",
+            label: "Animate Loading",
+            controlType: "SWITCH",
+            helpText: "Controls the loading of the widget",
+            isJSConvertible: true,
+            isBindProperty: true,
+            isTriggerProperty: false,
+            validation: { type: ValidationTypes.BOOLEAN },
+          },
         ],
       },
     ];
