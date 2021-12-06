c5534526a55248a0c522aafe036a201be392fe7c
Trisha Anand <trisha@appsmith.com>
2021-12-06 19:54:13 +0530

    Config repo migrated

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepository.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepository.java
@@ -3,5 +3,5 @@ package com.appsmith.server.repositories;
 import com.appsmith.server.repositories.ce.CustomCommentThreadRepositoryCE;
 
 public interface CustomCommentThreadRepository extends CustomCommentThreadRepositoryCE {
-    
+
 }
