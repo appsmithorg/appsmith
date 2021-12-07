36dc18f5d2443243016a4e74660c052dda922de6
keyurparalkar <keyur@appsmith.com>
2021-12-07 13:41:16 +0530

    test: completed the integration tests for bug fixes

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/table_with_text_selRowIndices_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/table_with_text_selRowIndices_spec.js
@@ -1,13 +1,83 @@
 const dsl = require("../../../../fixtures/tableWithTextWidgetDsl.json");
-const apiWidgetslocator = require("../../../../locators/commonlocators.json");
+const widgetsPage = require("../../../../locators/Widgets.json");
+const commonlocators = require("../../../../locators/commonlocators.json");
 
 describe("Table widget edge case scenario testing", function() {
   before(() => {
     cy.addDsl(dsl);
   });
+
+  // it("Check if the selectedRowIndices does not contain 2d array", function() {
+  //   cy.openPropertyPane("tablewidget");
+
+  //   //Enable Multi row select
+  //   cy.get(widgetsPage.toggleEnableMultirowselection)
+  //     .first()
+  //     .click({force: true});
+
+  //   //Change the value of default selected row
+  //   cy.updateCodeInput(".t--property-control-defaultselectedrow", "1");
+
+  //   //Disable Multi row select
+  //   cy.get(widgetsPage.toggleEnableMultirowselection)
+  //   .first()
+  //   .click({force: true});
+
+  //   cy.get(`${widgetsPage.textWidget} .bp3-ui-text`).should("have.text", "[]");
+
+  //   //Enable Multi row select
+  //   cy.get(widgetsPage.toggleEnableMultirowselection)
+  //   .first()
+  //   .click({force: true});
+
+  //   cy.get(`${widgetsPage.textWidget} .bp3-ui-text`).should("have.text", "[  1]");
+
+  //   //Disable Multi row select
+  //   cy.get(widgetsPage.toggleEnableMultirowselection)
+  //     .first()
+  //     .click({force: true});
+
+  //   //Enable Multi row select
+  //   cy.get(widgetsPage.toggleEnableMultirowselection)
+  //   .first()
+  //   .click({force: true});
+
+  //   cy.get(`${widgetsPage.textWidget} .bp3-ui-text`).should("have.text", "[  1]");
+  // });
+
   it("Check if the selectedRowIndices does not contain -1", function() {
     cy.openPropertyPane("tablewidget");
 
-    cy.get(apiWidgetslocator.deflautSelectedRow).should("have.text", "0");
+    //Update the property default selected row to blank
+    cy.updateCodeInput(".t--property-control-defaultselectedrow", "");
+
+    //Check if the evaluated value is undefined
+    cy.get(commonlocators.evaluatedCurrentValue)
+      .first()
+      .should("be.visible")
+      .should("have.text", "undefined");
+
+    //Check the value present in the textfield which is selectedRowIndices is blank
+    cy.get(`${widgetsPage.textWidget} .bp3-ui-text`).should("have.text", "");
+
+    //Enable the "Enable Multi Row selection"
+    cy.get(widgetsPage.toggleEnableMultirowselection)
+      .first()
+      .click({ force: true });
+
+    //Check the value present in the textfield which is selectedRowIndices is []
+    cy.get(`${widgetsPage.textWidget} .bp3-ui-text`).should("have.text", "[]");
+
+    //Select the 1st, 2nd and 3rd row
+    cy.isSelectRow("0");
+    cy.isSelectRow("1");
+    cy.isSelectRow("2");
+
+    //Check the value present in the textfield which is selectedRowIndices is [0,1,2]
+    cy.get(`${widgetsPage.textWidget} .bp3-ui-text`).should(
+      "have.text",
+      "[  0,  1,  2]",
+    );
   });
 });
+//
