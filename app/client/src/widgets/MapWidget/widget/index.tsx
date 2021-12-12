08b0e8696751a806425e9fbd63c8d7232d44099e
Paul Li <82799722+wmdev0808@users.noreply.github.com>
2021-12-12 13:43:29 +0800

    feat: Change Map Pin Color on Map Widget (#8337)
    * feat: Change Map Pin Color on Map Widget
    
    -- Add a color property for a marker
    
    * feat: Change Map Pin Color on Map Widget
    
    -- Set #E0DEDE as default color
    
    * feat: Change Map Pin Color on Map Widget
    
    -- Revert to the original logic
    
    * feat: Change Map Pin Color on Map Widget
    
    -- Add a new base icon for a marker
    
    * feat: Change Map Pin Color on Map Widget
    
    -- Add anchor property for custom marker icon
    
    Co-authored-by: Arpit Mohan <arpit@appsmith.com>

--- app/client/src/widgets/MapWidget/widget/index.tsx
+++ app/client/src/widgets/MapWidget/widget/index.tsx
@@ -122,6 +122,10 @@ class MapWidget extends BaseWidget<MapWidgetProps, WidgetState> {
                         name: "title",
                         type: ValidationTypes.TEXT,
                       },
+                      {
+                        name: "color",
+                        type: ValidationTypes.TEXT,
+                      },
                     ],
                   },
                 },
@@ -374,6 +378,7 @@ export interface MapWidgetProps extends WidgetProps {
     lat: number;
     long: number;
     title?: string;
+    color?: string;
   };
   onMarkerClick?: string;
   onCreateMarker?: string;
