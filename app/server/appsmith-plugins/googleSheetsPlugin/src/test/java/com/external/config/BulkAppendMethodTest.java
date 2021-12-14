7fcd1d6fccb718a1ed388a6e8adf0ebae098b345
LeoThomas <leoweb2000@gmail.com>
2021-12-14 14:43:24 +0530

    Fixed code review comments

--- app/server/appsmith-plugins/googleSheetsPlugin/src/test/java/com/external/config/BulkAppendMethodTest.java
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/test/java/com/external/config/BulkAppendMethodTest.java
@@ -13,15 +13,6 @@ import java.util.List;
 public class BulkAppendMethodTest {
     final ObjectMapper objectMapper = new ObjectMapper();
 
-    private static String accessKey;
-    private static String secretKey;
-
-    @BeforeClass
-    public static void setUp() {
-        accessKey   = "access_key";
-        secretKey   = "secret_key";
-    }
-
     /**
      * To Test if it passes the empty Mono criteria,
      * else it should fail
@@ -40,38 +31,12 @@ public class BulkAppendMethodTest {
         }
     }
 
-    /**
-     * To Test if it passes the Non-empty Mono criteria,
-     * down the line the code may fail which is not the scope of testing
-     * that means, no code changes are done after empty Mono handling condition
-     * for which this test is intended to run.
-     */
-    @Test
-    public void testBulkAppendHandleNonEmptyMonoExecutePrerequisites() {
-        String[] testDataArray = {
-                "[{\"Sl #\":\"100\"}]",
-                "[{}]",
-                "[{\"Sl #\":\"100\"},{\"Sl#\":\"101\"}]",
-                "[{\"Sl #\":\"100\",\"Topic\":\"topic\"}]"};
-
-        for(int i=0; i<testDataArray.length; i++) {
-            MethodConfig methodConfig = getMethodConfigObject(testDataArray[i]);
-            BulkAppendMethod bulkAppend = new BulkAppendMethod(objectMapper);
-            Mono<Object> monoTest = bulkAppend.executePrerequisites(methodConfig, getOAuthObject());
-
-            StepVerifier
-                    .create(monoTest)
-                    .expectErrorMatches(throwable ->  throwable instanceof  java.lang.AssertionError);  }
-    }
-
-    /**
+   /**
      * Simulated oAuth2 object, just to bypass few case.
      * @return
      */
     private OAuth2 getOAuthObject(){
-        DatasourceConfiguration datasourceConfiguration = createDatasourceConfiguration();
         OAuth2 oAuth2 = new OAuth2();
-        oAuth2.setAuthenticationResponse(datasourceConfiguration.getAuthentication().getAuthenticationResponse());
         oAuth2.setAuthenticationResponse(new AuthenticationResponse() );
         oAuth2.getAuthenticationResponse().setToken("welcome123");
         return oAuth2;
@@ -96,20 +61,5 @@ public class BulkAppendMethodTest {
         methodConfig.setRowObjects(rowObject);
 
         return methodConfig;
-    }
-
-    /**
-     * DatasourceConfiguration object need for creating OAuth
-     * @return
-     */
-    private DatasourceConfiguration createDatasourceConfiguration() {
-        DBAuth authDTO = new DBAuth();
-        authDTO.setAuthType(DBAuth.Type.USERNAME_PASSWORD);
-        authDTO.setUsername(accessKey);
-        authDTO.setPassword(secretKey);
-
-        DatasourceConfiguration dsConfig = new DatasourceConfiguration();
-        dsConfig.setAuthentication(authDTO);
-        return dsConfig;
     }
 }
