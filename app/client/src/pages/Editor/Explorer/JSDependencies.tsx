c62820a96d0564d5d1bdcbd2b6c61650e88553a7
arunvjn <arun@appsmith.com>
2021-12-19 11:51:27 +0530

    Fixed warnings

--- app/client/src/pages/Editor/Explorer/JSDependencies.tsx
+++ app/client/src/pages/Editor/Explorer/JSDependencies.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import TooltipComponent from "components/ads/Tooltip";
 import { Colors } from "constants/Colors";
