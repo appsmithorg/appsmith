9867cdc011bf3aa2665abdd8f50b4641e96430ac
balajisoundar <balaji@appsmith.com>
2021-12-20 15:21:19 +0530

    cypress tests

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_spec.js
@@ -3,6 +3,7 @@ const widgetsPage = require("../../../../locators/Widgets.json");
 const commonlocators = require("../../../../locators/commonlocators.json");
 const publish = require("../../../../locators/publishWidgetspage.json");
 const dsl = require("../../../../fixtures/tableWidgetDsl.json");
+const explorer = require("../../../../locators/explorerlocators.json");
 
 describe("Table Widget Functionality", function() {
   before(() => {
@@ -359,6 +360,53 @@ describe("Table Widget Functionality", function() {
     });
   });
 */
+
+  it("Table Widget Functionality To Check with changing schema of tabledata", () => {
+    cy.visit("/applications");
+    cy.get(".t--new-button")
+      .first()
+      .click();
+    cy.get(".t--BuildFromScratch").click();
+    cy.get(explorer.addWidget).click();
+    cy.dragAndDropToCanvas("switchwidget", { x: 200, y: 200 });
+    cy.dragAndDropToCanvas("tablewidget", { x: 200, y: 300 });
+    cy.openPropertyPane("tablewidget");
+    cy.testJsontext(
+      "tabledata",
+      `
+      {{
+        Switch1.isSwitchedOn ? [
+            {
+              name: "joe"
+            }
+          ] : [
+            {
+              employee_name: "john"
+            },
+          ];
+      }}
+    `,
+    );
+    cy.wait("@updateLayout");
+    cy.PublishtheApp();
+    cy.getTableDataSelector("0", "0").then((element) => {
+      cy.get(element).should("be.visible");
+    });
+    cy.readTabledataPublish("0", "0").then((value) => {
+      expect(value).to.be.equal("joe");
+    });
+    cy.get(".t--switch-widget-active")
+      .first()
+      .click();
+    cy.wait(1000);
+    cy.getTableDataSelector("0", "0").then((element) => {
+      cy.get(element).should("be.visible");
+    });
+    cy.readTabledataPublish("0", "0").then((value) => {
+      expect(value).to.be.equal("john");
+    });
+  });
+
   afterEach(() => {
     // put your clean up code if any
   });
