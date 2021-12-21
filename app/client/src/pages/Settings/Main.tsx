22d73ec8557082f31e6edd580a7d277a373a3861
Pranav Kanade <pranav@appsmith.com>
2021-12-21 13:39:48 +0530

    separated save-restart bar to separate component

--- app/client/src/pages/Settings/Main.tsx
+++ app/client/src/pages/Settings/Main.tsx
@@ -1,9 +1,7 @@
 import React, { useCallback, useEffect } from "react";
 import { Icon } from "@blueprintjs/core";
 import { saveSettings } from "actions/settingsAction";
-import Button, { Category } from "components/ads/Button";
 import { SETTINGS_FORM_NAME } from "constants/forms";
-import { createMessage } from "constants/messages";
 import { ReduxActionTypes } from "constants/ReduxActionConstants";
 import {
   APPLICATIONS_URL,
@@ -31,6 +29,7 @@ import Group from "./Main/group";
 import RestartBanner from "./RestartBanner";
 import AdminConfig from "./config";
 import { CategoryType, SettingTypes } from "./config/types";
+import SaveAdminSettings from "./SaveSettings";
 
 const Wrapper = styled.div`
   flex-basis: calc(100% - ${(props) => props.theme.homePage.leftPane.width}px);
@@ -50,43 +49,6 @@ const BackButton = styled.div`
 const BackButtonText = styled.span``;
 
 const SettingsFormWrapper = styled.div``;
-
-const SettingsButtonWrapper = styled.div`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: ${(props) => props.theme.settings.footerHeight}px;
-  padding: ${(props) => props.theme.spaces[11]}px 0px 0px
-    ${(props) =>
-      props.theme.spaces[6] +
-      props.theme.homePage.leftPane.leftPadding +
-      props.theme.homePage.leftPane.rightMargin +
-      props.theme.homePage.leftPane.width}px;
-  box-shadow: ${(props) => props.theme.settings.footerShadow};
-  z-index: 2;
-  background-color: ${(props) => props.theme.colors.homepageBackground};
-`;
-
-const StyledButton = styled(Button)`
-  height: 24px;
-  display: inline-block;
-  margin-right: 16px;
-`;
-
-const StyledSaveButton = styled(StyledButton)`
-  width: 128px;
-  height: 38px;
-
-  & .cs-spinner {
-    top: 11px;
-  }
-`;
-
-const StyledClearButton = styled(StyledButton)`
-  width: 68px;
-  height: 38px;
-`;
 
 export const BottomSpace = styled.div`
   height: ${(props) => props.theme.settings.footerHeight + 20}px;
@@ -174,25 +136,13 @@ export function Main(
           subCategory={subCategory}
         />
         {isSavable && (
-          <SettingsButtonWrapper>
-            <StyledSaveButton
-              category={Category.primary}
-              className="t--admin-settings-save-button"
-              disabled={Object.keys(props.settings).length == 0 || !props.valid}
-              isLoading={props.isSaving}
-              onClick={onSave}
-              tag="button"
-              text={createMessage(() => "Save & Restart")}
-            />
-            <StyledClearButton
-              category={Category.tertiary}
-              className="t--admin-settings-reset-button"
-              disabled={Object.keys(props.settings).length == 0}
-              onClick={onClear}
-              tag="button"
-              text={createMessage(() => "Reset")}
-            />
-          </SettingsButtonWrapper>
+          <SaveAdminSettings
+            isSaving={props.isSaving}
+            onClear={onClear}
+            onSave={onSave}
+            settings={props.settings}
+            valid={props.valid}
+          />
         )}
         <BottomSpace />
       </SettingsFormWrapper>
