a66d4a613b2e0e3545cda45642b0a8e59b01c284
Apple <nandan@thinkify.io>
2021-12-21 18:50:41 +0530

    Updated unit test

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/UnitTest/LoginFromUIApp_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/UnitTest/LoginFromUIApp_spec.js
@@ -15,11 +15,13 @@ describe("Login from UI and check the functionality", function() {
       cy.Createpage(pageid);
       cy.DeletepageFromSideBar();
     });
-    cy.wait("@deletePage");
-    cy.get("@deletePage").should("have.property", "status", 200);
+    cy.wait("@deletePage").should((interception) => {
+      expect(interception.response.body.responseMeta.status).to.deep.eq(200);
+    });
     cy.DeleteApp(appname);
-    cy.wait("@deleteApplication");
-    cy.get("@deleteApplication").should("have.property", "status", 200);
+    cy.wait("@deleteApplication").should((interception) => {
+      expect(interception.response.body.responseMeta.status).to.deep.eq(200);
+    });
   });
 
   it("Login/Logout click Appsmith logo should route to login page", function() {
