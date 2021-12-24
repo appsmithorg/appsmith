2c4a00385bc93c005cb31ce792fef2f5973a74c2
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-12-24 21:48:52 +0530

    Fix MongoDB URI missing authSource
    Signed-off-by: Shrikant Sharat Kandula <shrikant@appsmith.com>

--- app/server/entrypoint.sh
+++ app/server/entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+
+# Add an `authSource` query param to MongoDB URI, if missing.
+if [ -n "$APPSMITH_MONGODB_URI" ]; then
+  if ! echo "$APPSMITH_MONGODB_URI" | grep -Fq "authSource="; then
+    if echo "$APPSMITH_MONGODB_URI" | grep -Fq '?'; then
+      APPSMITH_MONGODB_URI="$APPSMITH_MONGODB_URI&authSource=admin"
+    else
+      APPSMITH_MONGODB_URI="$APPSMITH_MONGODB_URI?authSource=admin"
+    fi
+  fi
+fi
+echo "$APPSMITH_MONGODB_URI"
+
 # Ref -Dlog4j2.formatMsgNoLookups=true https://spring.io/blog/2021/12/10/log4j2-vulnerability-and-spring-boot
 exec java -Djava.security.egd="file:/dev/./urandom" "$@" -Dlog4j2.formatMsgNoLookups=true -jar server.jar
