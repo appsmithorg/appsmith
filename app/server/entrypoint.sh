c0a9600146828e978354b9a8276e765155cdd0d5
Shrikant Sharat Kandula <shrikant@appsmith.com>
2021-12-24 22:01:39 +0530

    Remove echo MongoDB URI
    Signed-off-by: Shrikant Sharat Kandula <shrikant@appsmith.com>
    (cherry picked from commit 3f727cdfbb3fc896c69f1517a3b1b045b013cc6d)

--- app/server/entrypoint.sh
+++ app/server/entrypoint.sh
@@ -10,7 +10,6 @@ if [ -n "$APPSMITH_MONGODB_URI" ]; then
     fi
   fi
 fi
-echo "$APPSMITH_MONGODB_URI"
 
 # Ref -Dlog4j2.formatMsgNoLookups=true https://spring.io/blog/2021/12/10/log4j2-vulnerability-and-spring-boot
 exec java -Djava.security.egd="file:/dev/./urandom" "$@" -Dlog4j2.formatMsgNoLookups=true -jar server.jar
