651cf73ece22784a3bcbb06aab23a72eaaa82b85
geekup-legodevops <prj_legodevops@geekup.vn>
2021-12-27 16:51:32 +0700

    Refactor init mongodb flow to check and enable replica set for external mongodb based on mongo uri

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -2,7 +2,39 @@
 
 set -e
 
-check_initialized_db() {
+get_variable_mongodb() {
+  echo "Get variable mongodb"
+  MONGODB_PROTOCOL=$(echo "$APPSMITH_MONGODB_URI" | grep -oP "[a-z+]+(?=\:\/\/)") 
+  MONGODB_USERNAME=$(echo "$APPSMITH_MONGODB_URI" | grep -oP "(?<=\:\/\/)(.*\s?)(?=\:)")
+  MONGODB_PASSWORD=$(echo "$APPSMITH_MONGODB_URI" | grep -oP "\w+(?=@)")
+  MONGODB_HOST=$(echo "$MONGODB_URI" | grep -oP "(?<=\@)[a-z0-9.]+(?=\:)")
+  # If port is not defined in the URI, the above regex for the MongoDB Host will not be correct
+  if [[ -z $MONGODB_HOST ]]; then
+    MONGODB_HOST=$(echo "$MONGODB_URI" | grep -oP "[a-z0-9.]+(?=\/)")
+  fi
+  MONGODB_DATABASE=$(echo "$APPSMITH_MONGODB_URI" | grep -oP "\w+(?!\S)")
+}
+
+check_mongodb_uri() {
+  echo 'Check mongodb uri host'
+  isUriLocal=1
+  if [[ $MONGODB_HOST == "localhost" || $MONGODB_HOST == "127.0.0.1" ]]; then
+    isUriLocal=0
+  fi
+}
+
+init_mongodb() {
+  if [[ $isUriLocal -eq 0 ]]; then
+    echo "Init database"
+    MONGO_DB_PATH="/appsmith-stacks/data/mongodb"
+    MONGO_LOG_PATH="$MONGO_DB_PATH/log"
+    MONGO_DB_KEY="$MONGO_DB_PATH/key"
+    mkdir -p "$MONGO_DB_PATH"
+    touch "$MONGO_LOG_PATH"
+  fi
+}
+
+init_replica_set() {
   echo 'Check initialized database'
   shouldPerformInitdb=1
   for path in \
@@ -12,41 +44,44 @@ check_initialized_db() {
     "$MONGO_DB_PATH/storage.bson"; do
     if [ -e "$path" ]; then
       shouldPerformInitdb=0
-      return
+      break
     fi
   done
-  echo "Should initialize database"
-}
-
-init_mongodb() {
-  echo "Init database"
-  MONGO_DB_PATH="/appsmith-stacks/data/mongodb"
-  MONGO_LOG_PATH="$MONGO_DB_PATH/log"
-  MONGO_DB_KEY="$MONGO_DB_PATH/key"
-  mkdir -p "$MONGO_DB_PATH"
-  touch "$MONGO_LOG_PATH"
-
-  check_initialized_db
-
-  if [[ $shouldPerformInitdb -gt 0 ]]; then
+  
+  if [[ $shouldPerformInitdb -gt 0 && $isUriLocal -eq 0 ]]; then
+    echo 'Init replica set local'
     # Start installed MongoDB service - Dependencies Layer
     mongod --fork --port 27017 --dbpath "$MONGO_DB_PATH" --logpath "$MONGO_LOG_PATH"
     echo "Waiting 10s for mongodb init"
     sleep 10
-    bash "/opt/appsmith/templates/mongo-init.js.sh" "$MONGO_INITDB_ROOT_USERNAME" "$MONGO_INITDB_ROOT_PASSWORD" >"/appsmith-stacks/configuration/mongo-init.js"
-    mongo "127.0.0.1/${MONGO_INITDB_DATABASE}" /appsmith-stacks/configuration/mongo-init.js
+    bash "/opt/appsmith/templates/mongo-init.js.sh" "$MONGODB_USERNAME" "$MONGODB_PASSWORD" > "/appsmith-stacks/configuration/mongo-init.js"
+    mongo "${MONGODB_HOST}/${MONGODB_DATABASE}" /appsmith-stacks/configuration/mongo-init.js
     echo "Seeding db done"
-
     echo "Enable replica set"
     mongod --dbpath "$MONGO_DB_PATH" --shutdown || true
     echo "Fork process"
-    openssl rand -base64 756 >"$MONGO_DB_KEY"
+    openssl rand -base64 756 > "$MONGO_DB_KEY"
     chmod go-rwx,u-wx "$MONGO_DB_KEY"
     mongod --fork --port 27017 --dbpath "$MONGO_DB_PATH" --logpath "$MONGO_LOG_PATH" --replSet mr1 --keyFile "$MONGO_DB_KEY" --bind_ip localhost
     echo "Waiting 10s for mongodb init with replica set"
     sleep 10
     mongo "$APPSMITH_MONGODB_URI" --eval 'rs.initiate()'
     mongod --dbpath "$MONGO_DB_PATH" --shutdown || true
+  fi
+
+  if [[ $isUriLocal -gt 0 ]]; then
+    # Check mongodb cloud replica set
+    echo 'Check mongodb cloud replica set'
+    responseStatus=$(mongo "$APPSMITH_MONGODB_URI" --eval "rs.status()")
+    okString='"ok": 1'
+    indexString=${responseStatus%%$okString*}
+
+    if [[ ${#indexString} -gt 0 ]]; then
+      echo "Mongodb cloud replica set is enabled"
+    else
+      echo "Mongodb cloud init with replica set"
+      mongo "$APPSMITH_MONGODB_URI" --eval 'rs.initiate()'
+    fi
   fi
 }
 
@@ -67,7 +102,7 @@ configure_supervisord() {
   cp -f "$SUPERVISORD_CONF_PATH/application_process/"*.conf /etc/supervisor/conf.d
 
   # Disable services based on configuration
-  if [[ "$APPSMITH_MONGODB_URI" = "mongodb://appsmith:$MONGO_INITDB_ROOT_PASSWORD@localhost/appsmith" ]]; then
+  if [[ "$APPSMITH_MONGODB_URI" = "mongodb://appsmith:$MONGODB_PASSWORD@$MONGODB_HOST/appsmith" && $isUriLocal -eq 0 ]]; then
     cp "$SUPERVISORD_CONF_PATH/mongodb.conf" /etc/supervisor/conf.d/
   fi
   if [[ "$APPSMITH_REDIS_URL" = "redis://127.0.0.1:6379" ]]; then
@@ -98,7 +133,7 @@ if ! [[ -e "$ENV_PATH" ]]; then
     tr -dc A-Za-z0-9 </dev/urandom | head -c 13
     echo ''
   )
-  bash "/opt/appsmith/templates/docker.env.sh" "$AUTO_GEN_MONGO_PASSWORD" "$AUTO_GEN_ENCRYPTION_PASSWORD" "$AUTO_GEN_ENCRYPTION_SALT" >"$ENV_PATH"
+  bash "/opt/appsmith/templates/docker.env.sh" "$AUTO_GEN_MONGO_PASSWORD" "$AUTO_GEN_ENCRYPTION_PASSWORD" "$AUTO_GEN_ENCRYPTION_SALT" > "$ENV_PATH"
 fi
 
 if [[ -f "$ENV_PATH" ]]; then
@@ -136,7 +171,10 @@ if [[ -z "${APPSMITH_RECAPTCHA_SITE_KEY}" ]] || [[ -z "${APPSMITH_RECAPTCHA_SECR
 fi
 
 # Main Section
+get_variable_mongodb
+check_mongodb_uri
 init_mongodb
+init_replica_set
 mount_letsencrypt_directory
 configure_supervisord
 
