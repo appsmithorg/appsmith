628fb811246ece5b94156521a47d5e957a9b3f49
bhavin <techbhavin@gmail.com>
2021-12-27 15:39:08 +0530

    fix: test case updated

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Number_column_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Number_column_spec.js
@@ -1,9 +1,5 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
-const widgetsPage = require("../../../../locators/Widgets.json");
-const commonlocators = require("../../../../locators/commonlocators.json");
-const publish = require("../../../../locators/publishWidgetspage.json");
-const dsl = require("../../../../fixtures/basicTabledsl.json");
-const testData = require("../../../../fixtures/numberKeyTableData.json");
+const dsl = require("../../../../fixtures/basicNumberDataTabledsl.json");
 
 describe("Validate Table Widget Table Data", function() {
   before(() => {
@@ -12,12 +8,9 @@ describe("Validate Table Widget Table Data", function() {
 
   it("Check number key in table data convert table binding and header properly", function() {
     cy.openPropertyPane("tablewidget");
-    cy.testJsontext("tabledata", JSON.stringify(testData));
 
-    // check table header name
-    cy.get(".draggable-header")
-      .eq(0)
-      .should("have.text", "_1");
+    cy.contains('[role="columnheader"]', "_1").should("exist");
+    cy.contains('[role="columnheader"]', "_2").should("exist");
   });
 
   afterEach(() => {
