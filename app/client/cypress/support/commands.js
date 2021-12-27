910812b62a59ab56cdfa50919c2deee0639dc6a5
rashmi rai <rashmi.rr@yahoo.com>
2021-12-27 17:34:45 +0530

    feat: adding ads component for UQI (#8777)

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -390,9 +390,9 @@ Cypress.Commands.add(
   "addOauthAuthDetails",
   (accessTokenUrl, clientId, clientSecret, authURL) => {
     cy.get(datasource.authType).click();
-    cy.xpath(datasource.OAuth2).click();
+    cy.get(datasource.OAuth2).click();
     cy.get(datasource.grantType).click();
-    cy.xpath(datasource.authorisecode).click();
+    cy.get(datasource.authorisecode).click();
     cy.get(datasource.accessTokenUrl).type(accessTokenUrl);
     cy.get(datasource.clienID).type(clientId);
     cy.get(datasource.clientSecret).type(clientSecret);
@@ -3369,16 +3369,18 @@ Cypress.Commands.add(
   "validateNSelectDropdown",
   (ddTitle, currentValue, newValue) => {
     let toChange = false;
-    cy.xpath('//div[contains(text(),"' + currentValue + '")]').should(
+    cy.xpath('//span[contains(text(),"' + currentValue + '")]').should(
       "exist",
       currentValue + " dropdown value not present",
     );
     if (newValue) toChange = true;
     if (toChange) {
       cy.xpath(
-        "//p[text()='" + ddTitle + "']/following-sibling::div/div",
+        "//p[text()='" +
+          ddTitle +
+          "']/parent::label/following-sibling::div/div/div",
       ).click(); //to expand the dropdown
-      cy.xpath('//div[contains(text(),"' + newValue + '")]')
+      cy.xpath('//span[contains(text(),"' + newValue + '")]')
         .last()
         .click({ force: true }); //to select the new value
     }
@@ -3387,19 +3389,17 @@ Cypress.Commands.add(
 
 Cypress.Commands.add("typeValueNValidate", (valueToType, fieldName = "") => {
   if (fieldName) {
-    cy.xpath("//p[text()='" + fieldName + "']/following-sibling::div").then(
-      ($field) => {
-        cy.updateCodeInput($field, valueToType);
-      },
-    );
+    cy.xpath(
+      "//p[text()='" + fieldName + "']/parent::label/following-sibling::div",
+    ).then(($field) => {
+      cy.updateCodeInput($field, valueToType);
+    });
   } else {
     cy.xpath("//div[@class='CodeEditorTarget']").then(($field) => {
       cy.updateCodeInput($field, valueToType);
     });
   }
-
   cy.EvaluateCurrentValue(valueToType);
-
   // cy.xpath("//p[text()='" + fieldName + "']/following-sibling::div//div[@class='CodeMirror-code']//span/span").should((fieldValue) => {
   //   textF = fieldValue.innerText
   //   fieldValue.innerText = ""
@@ -3446,26 +3446,22 @@ Cypress.Commands.add(
   (fieldName = "", currentValue = "") => {
     let toValidate = false;
     if (currentValue) toValidate = true;
-
     if (fieldName) {
       cy.xpath(
         "//p[text()='" +
           fieldName +
-          "']/following-sibling::div//div[@class='CodeMirror-code']",
+          "']/parent::label/following-sibling::div//div[@class='CodeMirror-code']",
       ).click();
     } else {
       cy.xpath("//div[@class='CodeMirror-code']").click();
     }
-
     cy.wait(2000);
     const val = cy
       .get(commonlocators.evaluatedCurrentValue)
       .first()
       .should("be.visible")
       .invoke("text");
-
     if (toValidate) expect(val).to.eq(currentValue);
-
     return val;
   },
 );
