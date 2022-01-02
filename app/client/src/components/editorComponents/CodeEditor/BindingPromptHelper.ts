edc40518e5b84892a95587ca30f915b4bdc3f2b1
ohansFavour <fohanekwu@gmail.com>
2022-01-02 18:16:17 +0100

    improve slash command conditional

--- app/client/src/components/editorComponents/CodeEditor/BindingPromptHelper.ts
+++ app/client/src/components/editorComponents/CodeEditor/BindingPromptHelper.ts
@@ -1,14 +1,18 @@
 import { isString } from "lodash";
+import { isDynamicValue } from "utils/DynamicBindingUtils";
 
 export const showBindingPrompt = (
   showEvaluatedValue: boolean,
   inputValue: any,
   isHinterOpen: boolean,
 ): boolean => {
+  const isDynamicInputValue = inputValue && isDynamicValue(inputValue);
+  const lastCharacterOfSlash = inputValue && inputValue.slice(-1);
+
   return (
     showEvaluatedValue &&
     (!isString(inputValue) ||
       (!inputValue && !isHinterOpen) ||
-      (!inputValue?.includes("{{") && !inputValue?.includes("/")))
+      (!isDynamicInputValue && lastCharacterOfSlash !== "/"))
   );
 };
