8ed0fc957faf6611e67bab3c531411bdd8c7cd96
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-01-04 15:19:14 +0530

    WIP

--- app/client/src/components/editorComponents/CodeEditor/addon/matchMustaches.ts
+++ app/client/src/components/editorComponents/CodeEditor/addon/matchMustaches.ts
@@ -1,6 +1,12 @@
 import CodeMirror from "codemirror";
 import { throttle, findIndex } from "lodash";
 
+const getAllLines = (cm: CodeMirror.Editor) => {
+  const lines: Array<CodeMirror.LineHandle> = [];
+  cm.eachLine((line) => lines.push(line));
+  return lines;
+};
+
 interface CMEditor extends CodeMirror.Editor {
   findMatchingBracket: (
     pos: CodeMirror.Position,
@@ -14,25 +20,30 @@ interface CMEditor extends CodeMirror.Editor {
   };
 }
 
-function matchMustaches(cm: CodeMirror.Editor, tr: boolean) {
+function markMustaches(
+  cm: CodeMirror.Editor,
+  openMustacheStartPos: CodeMirror.Position,
+  closeMustacheStartPos: CodeMirror.Position,
+) {
   debugger;
-  // cm.markText(match.from, Pos(match.from.line, match.from.ch + 1), {
-  //   className: "binding-brackets",
-  // });
-  // editor.markText(
-  //   { ch: ending.start, line: lineNo },
-  //   { ch: ending.end, line: lineNo },
-  //   {
-  //     className: "binding-brackets",
-  //   },
-  // );
-  // editor.markText(
-  //   { ch: opening.start, line: lineNo },
-  //   { ch: opening.end, line: lineNo },
-  //   {
-  //     className: "binding-brackets",
-  //   },
-  // );
+
+  const openMustacheEndPos = CodeMirror.Pos(
+    openMustacheStartPos.line,
+    openMustacheStartPos.ch + 1,
+  );
+  // mark opening mustaches
+  cm.markText(openMustacheStartPos, openMustacheEndPos, {
+    className: "binding-brackets",
+  });
+
+  const closeMustacheEndPos = CodeMirror.Pos(
+    closeMustacheStartPos.line,
+    closeMustacheStartPos.ch + 1,
+  );
+  // mark closing mustaches
+  cm.markText(closeMustacheStartPos, closeMustacheEndPos, {
+    className: "binding-brackets",
+  });
 }
 
 const mustacheScanned = false;
@@ -56,19 +67,38 @@ function onMountHandler(cm: CMEditor) {
   // 2. find matching closed curly braces and check if it is mustache,
   // 3. if yes then mark starting and ending as bold.
   // 4. repeat
-  let lineCount = 0;
-  cm.eachLine((_line: CodeMirror.LineHandle) => {
-    const startMustacheIndex = _line.text.indexOf("{{");
 
-    if (startMustacheIndex > -1) {
-      const endMustacheIndex = startMustacheIndex + 1;
+  let lineIndex = 0;
+  const lines = getAllLines(cm);
+  for (lineIndex = 0; lineIndex < lines.length; lineIndex += 1) {
+    const currentLine = lines[lineIndex];
+    debugger;
+    // find open mustache in currentLine's text
+    const openMustacheTextStartIndex = currentLine.text.indexOf("{{");
+    if (openMustacheTextStartIndex > -1) {
+      // search of closing mustache when openMustache is found
+      const openMustacheEndIndex = openMustacheTextStartIndex + 1;
+      const openMustacheLineIndex = lineIndex;
       const match = cm.findMatchingBracket(
-        CodeMirror.Pos(lineCount, endMustacheIndex),
+        CodeMirror.Pos(openMustacheLineIndex, openMustacheEndIndex),
       );
-      debugger;
+
+      if (match.match) {
+        // found closingMustache
+
+        // let's mark / highlight mustaches
+        const closeMustacheLineIndex = match.to.line;
+        const closeMustacheTextStartIndex = match.to.ch - 1;
+        markMustaches(
+          cm,
+          CodeMirror.Pos(openMustacheLineIndex, openMustacheEndIndex),
+          CodeMirror.Pos(closeMustacheLineIndex, closeMustacheTextStartIndex),
+        );
+        // jump to line of closing mustache
+        lineIndex = closeMustacheLineIndex - 1;
+      }
     }
-    lineCount++;
-  });
+  }
 }
 
 CodeMirror.defineOption("matchMustaches", false, function(
@@ -79,11 +109,14 @@ CodeMirror.defineOption("matchMustaches", false, function(
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore: No types available
   if (old && old != CodeMirror.Init) {
+    // un-subscribing to all text changes in the currentEditor
     cm.off("change", matchAppsmithBindingBrackets);
   }
   if (val) {
+    // subscribing to all text changes in the currentEditor
     cm.on("change", matchAppsmithBindingBrackets);
   }
 });
 
+// This is called when currentEditor is initialized
 CodeMirror.defineInitHook(onMountHandler);
