9f0ea0818776f94e161034c4d3489e32df8fc631
Rishabh Saxena <rishabh@appsmith.com>
2022-01-06 14:18:54 +0530

    try increasing timeout further

--- app/server/appsmith-git/src/main/java/com/appsmith/git/constants/Constraint.java
+++ app/server/appsmith-git/src/main/java/com/appsmith/git/constants/Constraint.java
@@ -2,6 +2,6 @@ package com.appsmith.git.constants;
 
 public class Constraint {
     public static final int MAX_COMMIT_LOGS = 100;
-    public static final int REMOTE_TIMEOUT_MILLIS = 200000;
-    public static final int LOCAL_TIMEOUT_MILLIS = 50000;
+    public static final int REMOTE_TIMEOUT_MILLIS = 10000000;
+    public static final int LOCAL_TIMEOUT_MILLIS = 10000000;
 }
