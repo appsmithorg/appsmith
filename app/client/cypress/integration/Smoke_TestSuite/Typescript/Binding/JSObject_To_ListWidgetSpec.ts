c711e39849942cd1e82597926fb988e40d072434
Aishwarya-U-R <91450662+Aishwarya-U-R@users.noreply.github.com>
2022-01-10 09:51:43 +0530

    test: Automated tests for JSObjects in Typescript (#10223)
    * Bugs  #9069, #9201, #6975, #9922 - Scripted
    
    * Mongo DS update from Cloud to Localhost
    
    * Updating command.js
    
    * Ignoring Mongo until Localhost is fixed
    
    * S3 fixes
    
    * S3 fix
    
    * Mongo Cloud to Localhost transition
    
    * Bug #6375 scripted
    
    * Failure fix
    
    * S3 fix + testSaveDs method flow update
    
    * Refresh datasource structure script fix
    
    * Entity explorer datasource structure script fix
    
    * testSaveDs method flow update fixes
    
    * testSaveDs method flow update fixes - set 2
    
    * S3, Mongo Skipping cases fix
    
    * Custom Chart failure fix
    
    * Button-Call-Query Validation case script fix
    
    * Confirm run action failure fix
    
    * Confirm run action script fix
    
    * Dropdown-Call-Query Validation script fix
    
    * S3 - Delete icon issue fix
    
    * S3 - Delete icon locator updated
    
    * actionContextMenuByEntityName method call update
    
    * Table widget add new menu button column - rgb fix
    
    * Commenting BG color verify - to override flakiness
    
    * Mongo skipping script fix
    
    * Commenting Cypress.runner.stop()
    
    * Removing .skip
    
    * Bind_tableApi_spec.js failure fix
    
    * Slash command failure fix
    
    * Bugs # 8595 & 10049 - Implemented
    
    * Updating AGHelper methods to explicit public
    
    * Reverting tsconfig.json
    
    * tsconfig.json reverted
    
    * Adding local tsconfig.json for Cypress tests
    
    * fixture read updating for ts tests
    
    * Updating .yml files to pick up ts files execution
    
    * Including ts files for cypress run
    
    * Running only ts test files
    
    * Inclusion of TS files for cypress run
    
    * Revert cypress.json to run all tests
    
    * Added validation for Bug #10055 & commented
    
    * Flaky fix
    
    * JSObject_To_ListWidgetSpec - Implemented in TS
    
    * Entity_Explorer_JSEditor_spec implemented in TS
    
    * Uncommenting script for Bug 10049, 10055

---
+++ app/client/cypress/integration/Smoke_TestSuite/Typescript/Binding/JSObject_To_ListWidgetSpec.ts
@@ -0,0 +1,78 @@
+import { ApiPage } from "../../../../support/Pages/ApiPage";
+import { AggregateHelper } from "../../../../support/Pages/AggregateHelper";
+import { JSEditor } from "../../../../support/Pages/JSEditor";
+import { CommonLocators } from "../../../../support/Objects/CommonLocators";
+
+const apiPage = new ApiPage();
+const agHelper = new AggregateHelper();
+const jsEditor = new JSEditor();
+const locator = new CommonLocators();
+
+let dataSet: any, valueToTest: any;
+
+describe("Validate Create Api and Bind to Table widget via JSObject", () => {
+  before(() => {
+    cy.fixture('listwidgetdsl').then((val: any) => {
+      agHelper.AddDsl(val)
+    });
+
+    cy.fixture("example").then(function (data: any) {
+      dataSet = data;
+    });
+  });
+
+  it("1. Add users api and bind to JSObject", () => {
+    apiPage.CreateAndFillApi(dataSet.userApi + "/users")
+    apiPage.RunAPI()
+    apiPage.ReadApiResponsebyKey("name");
+    cy.get("@apiResp").then((value) => {
+      valueToTest = value;
+      cy.log("valueToTest to test returned is :" + valueToTest)
+      //cy.log("value to test returned is :" + value)
+    })
+    jsEditor.CreateJSObject("return Api1.data.users;");
+  });
+
+  it("2. Validate the Api data is updated on List widget", function () {
+    agHelper.SelectEntityByName("Widgets")//to expand widgets
+    agHelper.SelectEntityByName("List1");
+    jsEditor.EnterJSContext("items", "{{JSObject1.myFun1()}}")
+    cy.get(locator._textWidget).should("have.length", 8);
+    cy.get(locator._textWidget)
+      .first()
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal((valueToTest as string).trimEnd());
+      });
+    agHelper.DeployApp();
+    cy.get(locator._textWidgetInDeployed).should("have.length", 8);
+    cy.get(locator._textWidgetInDeployed)
+      .first()
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal((valueToTest as string).trimEnd());
+      });
+  });
+
+  it("3. Validate the List widget ", function () {
+    cy.get(locator._backToEditor).click({ force: true });
+    agHelper.SelectEntityByName("Widgets")//to expand widgets
+    agHelper.SelectEntityByName("List1");
+    jsEditor.EnterJSContext("itemspacing\\(px\\)", "50")
+    cy.get(locator._textWidget).should("have.length", 6);
+    cy.get(locator._textWidget)
+      .first()
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal((valueToTest as string).trimEnd());
+      });
+    agHelper.DeployApp();
+    cy.get(locator._textWidgetInDeployed).should("have.length", 6);
+    cy.get(locator._textWidgetInDeployed).first()
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal((valueToTest as string).trimEnd());
+      });
+  });
+
+});
