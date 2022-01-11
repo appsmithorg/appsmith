690109a0ff1b8d936d73b8368d62fc6a2c12ede3
arunvjn <arun@appsmith.com>
2022-01-11 19:06:01 +0530

    Rescued missing property pane

--- app/client/src/pages/Editor/WidgetsEditor/index.tsx
+++ app/client/src/pages/Editor/WidgetsEditor/index.tsx
@@ -29,6 +29,9 @@ import {
 import { useAllowEditorDragToSelect } from "utils/hooks/useAllowEditorDragToSelect";
 import { getIsFirstTimeUserOnboardingEnabled } from "selectors/onboardingSelectors";
 import EditorContextProvider from "components/editorComponents/EditorContextProvider";
+import { updateExplorerWidthAction } from "actions/explorerActions";
+import { DEFAULT_PROPERTY_PANE_WIDTH } from "constants/AppConstants";
+import { PropertyPaneSidebar } from "components/editorComponents/PropertyPaneSidebar";
 
 /* eslint-disable react/display-name */
 function WidgetsEditor() {
@@ -102,6 +105,26 @@ function WidgetsEditor() {
     [allowDragToSelect],
   );
 
+  const [propertyPaneWidth, setPropertyPaneWidth] = React.useState(
+    DEFAULT_PROPERTY_PANE_WIDTH,
+  );
+
+  /**
+   * on property pane sidebar drag end
+   *
+   * @return void
+   */
+  const onRightSidebarDragEnd = useCallback(() => {
+    dispatch(updateExplorerWidthAction(propertyPaneWidth));
+  }, [propertyPaneWidth]);
+
+  /**
+   * on property pane sidebar width change
+   */
+  const onRightSidebarWidthChange = useCallback((newWidth) => {
+    setPropertyPaneWidth(newWidth);
+  }, []);
+
   log.debug("Canvas rendered");
 
   PerformanceTracker.stopTracking();
@@ -123,6 +146,11 @@ function WidgetsEditor() {
           <CanvasContainer />
           <CrudInfoModal />
           <Debugger />
+          <PropertyPaneSidebar
+            onDragEnd={onRightSidebarDragEnd}
+            onWidthChange={onRightSidebarWidthChange}
+            width={propertyPaneWidth}
+          />
         </div>
       )}
     </EditorContextProvider>
