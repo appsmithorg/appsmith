4f301a45a22f94eb42a685e93c8aab03e1fa96dd
Albin <albin@appsmith.com>
2022-01-13 11:15:55 +0530

    * index file added

---
+++ app/client/src/components/ads/index.ts
@@ -0,0 +1,47 @@
+export { default as AppIcon } from "./AppIcon";
+export * from "./AppIcon";
+
+export { default as Button } from "./Button";
+export * from "./Button";
+
+export { default as ButtonTabComponent } from "./ButtonTabComponent";
+export * from "./ButtonTabComponent";
+
+export { default as Callout } from "./Callout";
+export * from "./Callout";
+
+export { default as Checkbox } from "./Checkbox";
+export * from "./Checkbox";
+
+export { default as ColorPickerComponent } from "./ColorPickerComponent";
+export * from "./ColorPickerComponent";
+
+export { default as ColorSelector } from "./ColorSelector";
+export * from "./ColorSelector";
+
+export * from "./common";
+
+export { default as CopyToClipboard } from "./CopyToClipBoard";
+export * from "./CopyToClipBoard";
+
+export { default as DatePickerComponent } from "./DatePickerComponent";
+export * from "./DatePickerComponent";
+
+export { default as DialogComponent } from "./DialogComponent";
+export * from "./DialogComponent";
+
+export { default as DisplayImageUpload } from "./DisplayImageUpload";
+export * from "./DisplayImageUpload";
+
+export { default as DraggableList } from "./DraggableList";
+
+export { DroppableComponent } from "./DraggableListComponent";
+
+export { default as Dropdown } from "./Dropdown";
+export * from "./Dropdown";
+
+export { default as EditableText } from "./EditableText";
+export * from "./EditableText";
+
+export { default as EditableTextSubComponent } from "./EditableTextSubComponent";
+// export * from "./EditableTextSubComponent";
