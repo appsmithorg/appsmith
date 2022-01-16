2c6f2953dae3534c8793c505cde667cc1102f379
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-01-17 02:14:03 +0530

    WIP

--- app/client/src/pages/Editor/WidgetsEditor/index.tsx
+++ app/client/src/pages/Editor/WidgetsEditor/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import * as log from "loglevel";
 
 import {
   getIsFetchingPage,
@@ -101,8 +100,6 @@ function WidgetsEditor() {
     },
     [allowDragToSelect],
   );
-
-  log.debug("Canvas rendered");
 
   PerformanceTracker.stopTracking();
   return (
