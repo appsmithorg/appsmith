77b9e765159d3ed330e8cf8737c448b86df77f51
geekup-legodevops <prj_legodevops@geekup.vn>
2022-01-19 01:21:15 +0700

    Refactor k8s installation to use fat container

--- deploy/k8s/scripts/appsmith-ingress.yaml.sh
+++ deploy/k8s/scripts/appsmith-ingress.yaml.sh
@@ -5,7 +5,7 @@ ssl_enable="$2"
 
 if [[ "$ssl_enable" == "true" ]]; then
   cat <<EOF
-    apiVersion: networking.k8s.io/v1beta1
+    apiVersion: networking.k8s.io/v1
     kind: Ingress
     metadata:
       name: appsmith-ingress
@@ -19,80 +19,37 @@ if [[ "$ssl_enable" == "true" ]]; then
         - hosts:
             - $custom_domain
           secretName: lego-tls
-      backend:
-        serviceName: "appsmith-editor"
-        servicePort: 80
       rules:
       - host: $custom_domain
         http:
           paths:
-          - path: /api
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-backend-service
-              servicePort: 8080
-          - path: /oauth2
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-backend-service
-              servicePort: 8080
-          - path: /login
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-backend-service
-              servicePort: 8080
-          - path: /static
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-editor
-              servicePort: 80
           - path: /
             pathType: Prefix
             backend:
-              serviceName: appsmith-editor
-              servicePort: 80
+              service:
+                name: appsmith-service
+                port:
+                  number: 80
 EOF
 else
   cat << EOF
-    apiVersion: networking.k8s.io/v1beta1
+    apiVersion: networking.k8s.io/v1
     kind: Ingress
     metadata:
       name: appsmith-ingress
       annotations:
         kubernetes.io/ingress.class: "nginx"
     spec:
-
-      backend:
-        serviceName: "appsmith-editor"
-        servicePort: 80
       rules:
       - host: $custom_domain
         http:
           paths:
-          - path: /api
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-backend-service
-              servicePort: 8080
-          - path: /oauth2
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-backend-service
-              servicePort: 8080
-          - path: /login
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-backend-service
-              servicePort: 8080
-          - path: /static
-            pathType: Prefix
-            backend:
-              serviceName: appsmith-editor
-              servicePort: 80
           - path: /
             pathType: Prefix
             backend:
-              serviceName: appsmith-editor
-              servicePort: 80
+              service:
+                name: appsmith-service
+                port:
+                  number: 80
 EOF
 fi
