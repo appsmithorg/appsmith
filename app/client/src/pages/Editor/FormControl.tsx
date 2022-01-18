b6a0350aeb207a2926e82d4e43b9ec7e6104cfec
Irongade <adeoluayangade@yahoo.com>
2022-01-18 11:20:46 +0100

    add unique identifier for error text message

--- app/client/src/pages/Editor/FormControl.tsx
+++ app/client/src/pages/Editor/FormControl.tsx
@@ -214,8 +214,8 @@ function renderFormConfigBottom(props: {
             (error) =>
               error.errorType === PropertyEvaluationErrorType.VALIDATION,
           )
-          .map((error) => (
-            <FormInputErrorText key={error?.errorMessage}>
+          .map((error, index) => (
+            <FormInputErrorText key={index}>
               {`* ${error?.errorMessage}`}
             </FormInputErrorText>
           ))}
