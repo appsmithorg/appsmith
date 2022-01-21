bb3dd77dbfa987496c2e7a3a2eb4f51d3c04d400
bhavin <techbhavin@gmail.com>
2022-01-21 16:09:24 +0530

    fix: cypress updated

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Number_column_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_Number_column_spec.js
@@ -1,13 +1,15 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
 const dsl = require("../../../../fixtures/basicNumberDataTableDsl.json");
+const explorer = require("../../../../locators/explorerlocators.json");
 
 describe("Validate Table Widget Table Data", function() {
-  before(() => {
-    cy.addDsl(dsl);
-  });
-
   it("Check number key in table data convert table binding and header properly", function() {
+    cy.get(explorer.addWidget).click();
+    cy.dragAndDropToCanvas("tablewidget", { x: 300, y: 300 });
+
     cy.openPropertyPane("tablewidget");
+
+    cy.testJsontext("tabledata", dsl.dsl.children[0].tableData);
 
     cy.contains('[role="columnheader"]', "_1").should("exist");
     cy.contains('[role="columnheader"]', "_2").should("exist");
