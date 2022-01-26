f03719e84f118b79bbb72ab5c1865e7e14664fc4
yatin <yatin@appsmith.com>
2022-01-26 19:24:22 +0530

    added always passing failing tests

---
+++ app/client/cypress/integration/Smoke_TestSuite/testingreuse/Always_fail1_spec.js
@@ -0,0 +1,27 @@
+const homePage = require("../../../locators/HomePage.json");
+const dsl = require("../../../fixtures/PgAdmindsl.json");
+const datasource = require("../../../locators/DatasourcesEditor.json");
+const queryLocators = require("../../../locators/QueryEditor.json");
+const widgetsPage = require("../../../locators/Widgets.json");
+const appPage = require("../../../locators/PgAdminlocators.json");
+
+describe("Always fai", function() {
+  let orgid;
+  let newOrganizationName;
+  let appname;
+  let datasourceName;
+
+  before(() => {
+    cy.addDsl(dsl);
+  });
+  beforeEach(() => {
+    cy.startRoutesForDatasource();
+  });
+
+  it("Always fail", function() {
+    cy.contains("Sorry, something went wrong").should('not.exist');
+
+
+  });
+
+});
