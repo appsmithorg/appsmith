4493e2d25803db5ba4efd3ea682461f29f9e596f
Leo Thomas <leoweb2010@gmail.com>
2022-02-01 01:12:56 +0530

    fix: 9414 Firestore where condition space in key

--- app/server/appsmith-plugins/firestorePlugin/src/main/java/com/external/plugins/FirestorePlugin.java
+++ app/server/appsmith-plugins/firestorePlugin/src/main/java/com/external/plugins/FirestorePlugin.java
@@ -627,7 +627,7 @@ public class FirestorePlugin extends BasePlugin {
                         requestParams.add(new RequestParamDTO(WHERE, conditionList, null, null, null));
 
                         for(Object condition : conditionList) {
-                            String path = ((Map<String, String>)condition).get("key");
+                            String path = (((Map<String, String>)condition).get("key")).trim();
                             String operatorString = ((Map<String, String>)condition).get("condition");
                             String value = ((Map<String, String>)condition).get("value");
 
