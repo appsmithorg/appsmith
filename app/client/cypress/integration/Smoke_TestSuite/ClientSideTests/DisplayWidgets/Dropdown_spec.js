77882bfa4eda9da0ee74f3cad97dba63b7665466
Vicky <vicky.bansal@primathon.in>
2022-02-02 15:35:09 +0530

    add cypress test for no focused option when no default value

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Dropdown_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Dropdown_spec.js
@@ -1,5 +1,6 @@
 const dsl = require("../../../../fixtures/emptyDSL.json");
 const explorer = require("../../../../locators/explorerlocators.json");
+const formWidget = require("../../../../locators/FormWidgets.json");
 
 describe("Dropdown Widget Functionality", function() {
   before(() => {
@@ -58,5 +59,32 @@ describe("Dropdown Widget Functionality", function() {
     cy.get(".t--property-control-options .t--codemirror-has-error").should(
       "exist",
     );
+  });
+  it("should check that no option should be focued when default option is empty", () => {
+    cy.openPropertyPane("dropdownwidget");
+    cy.updateCodeInput(
+      ".t--property-control-options",
+      `[
+        {
+          "label": "Blue",
+          "value": "BLUE"
+        },
+        {
+          "label": "Green",
+          "value": "GREEN"
+        },
+        {
+          "label": "Red",
+          "value": "RED"
+        }
+      ]`,
+    );
+    cy.wait(500);
+    cy.updateCodeInput(".t--property-control-defaultoption", "");
+    cy.wait(500);
+
+    cy.get(formWidget.dropdownWidgetInput).click({ force: true });
+    cy.wait(1000);
+    cy.get(".single-select").should("not.have.class", "is-focused");
   });
 });
--- app/client/src/widgets/DropdownWidget/component/index.tsx
+++ app/client/src/widgets/DropdownWidget/component/index.tsx
@@ -398,7 +398,9 @@ class DropDownComponent extends React.Component<
     return (
       <MenuItem
         active={isSelected}
-        className={`single-select ${isFocused && isSelected && "is-focused"}`}
+        className={`single-select ${
+          isFocused && isSelected ? "is-focused" : ""
+        }`}
         key={option.value}
         onClick={itemProps.handleClick}
         tabIndex={0}
