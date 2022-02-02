b443c035b8ab70e39bfb52f726f7052ac058245b
yatin <yatin@appsmith.com>
2022-02-02 18:59:25 +0530

    fix always tests

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/Always_failed4_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/Always_failed4_spec.js
@@ -1,9 +1,9 @@
-const homePage = require("../../../locators/HomePage.json");
-const dsl = require("../../../fixtures/PgAdmindsl.json");
-const datasource = require("../../../locators/DatasourcesEditor.json");
-const queryLocators = require("../../../locators/QueryEditor.json");
-const widgetsPage = require("../../../locators/Widgets.json");
-const appPage = require("../../../locators/PgAdminlocators.json");
+const homePage = require("../../../../locators/HomePage.json");
+const dsl = require("../../../../fixtures/PgAdmindsl.json");
+const datasource = require("../../../../locators/DatasourcesEditor.json");
+const queryLocators = require("../../../../locators/QueryEditor.json");
+const widgetsPage = require("../../../../locators/Widgets.json");
+const appPage = require("../../../../locators/PgAdminlocators.json");
 
 describe("Always fail4", function() {
   let orgid;
