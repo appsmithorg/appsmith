f2bba15064c7152cb9d892bb4cd984e4f24ad7e3
ohansFavour <fohanekwu@gmail.com>
2022-02-07 14:49:53 +0100

    keep linting in trigger field in sync with the state of the datatree

--- app/client/src/workers/DataTreeEvaluator.ts
+++ app/client/src/workers/DataTreeEvaluator.ts
@@ -502,12 +502,22 @@ export default class DataTreeEvaluator {
 
     if (isWidget(entity)) {
       // Adding the dynamic triggers in the dependency list as they need linting whenever updated
-      // we don't make it dependent on anything else
-      if (entity.dynamicTriggerPathList) {
-        Object.values(entity.dynamicTriggerPathList).forEach(({ key }) => {
-          dependencies[`${entityName}.${key}`] = [];
+      // To keep linting in trigger fields in sync, nodes they depend on need to be added to their dependencies
+      const dynamicTriggerPathlist = entity.dynamicTriggerPathList;
+
+      if (dynamicTriggerPathlist && dynamicTriggerPathlist.length) {
+        dynamicTriggerPathlist.forEach((dynamicPath) => {
+          const propertyPath = dynamicPath.key;
+          const unevalPropValue = _.get(entity, propertyPath);
+          const { jsSnippets } = getDynamicBindings(unevalPropValue);
+          const existingDeps =
+            dependencies[`${entityName}.${propertyPath}`] || [];
+          dependencies[`${entityName}.${propertyPath}`] = existingDeps.concat(
+            jsSnippets.filter((jsSnippet) => !!jsSnippet),
+          );
         });
       }
+
       const widgetDependencies = addWidgetPropertyDependencies({
         entity,
         entityName,
@@ -1307,7 +1317,12 @@ export default class DataTreeEvaluator {
                 entity,
                 entityPropertyPath,
               );
-              if (isABindingPath) {
+
+              const isATriggerPath =
+                isWidget(entity) &&
+                isPathADynamicTrigger(entity, entityPropertyPath);
+
+              if (isABindingPath || isATriggerPath) {
                 didUpdateDependencyMap = true;
 
                 const { jsSnippets } = getDynamicBindings(
