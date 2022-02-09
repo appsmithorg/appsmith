a59b895087b6c1d7c1218a77d4bfa3f864462d4b
Aswath K <aswath@appsmith.com>
2022-02-09 20:50:54 +0530

    Make step control keyboard accessible

--- app/client/src/components/ads/StepComponent.tsx
+++ app/client/src/components/ads/StepComponent.tsx
@@ -41,12 +41,16 @@ const StepWrapper = styled.div`
       fill: ${(props) => props.theme.colors.propertyPane.radioGroupText};
     }
   }
+
+  &:focus {
+    border: 1px solid var(--appsmith-input-focus-border-color);
+  }
 `;
 
 const InputWrapper = styled.div`
   width: calc(100% - 120px);
-  height: 32px;
-  line-height: 32px;
+  height: 30px;
+  line-height: 30px;
   font-size: 14px;
   text-align: center;
   letter-spacing: 1.44px;
@@ -78,8 +82,22 @@ export function StepComponent(props: StepComponentProps) {
     const value = props.value + props.steps;
     props.onChange(value);
   }
+  function handleKeydown(e: React.KeyboardEvent) {
+    switch (e.key) {
+      case "ArrowUp":
+      case "ArrowRight":
+        increase();
+        e.preventDefault();
+        break;
+      case "ArrowDown":
+      case "ArrowLeft":
+        decrease();
+        e.preventDefault();
+        break;
+    }
+  }
   return (
-    <StepWrapper>
+    <StepWrapper onKeyDown={handleKeydown} tabIndex={0}>
       <StyledDecreaseIcon height={2} onClick={decrease} width={12} />
       <InputWrapper>{props.displayFormat(props.value)}</InputWrapper>
       <StyledIncreaseIcon height={12} onClick={increase} width={12} />
