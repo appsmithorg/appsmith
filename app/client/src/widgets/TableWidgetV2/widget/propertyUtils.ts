3807f54f77ee139db607f07de0d6b24a66fcbd36
balajisoundar <balaji@appsmith.com>
2022-02-10 10:44:01 +0530

    i

--- app/client/src/widgets/TableWidgetV2/widget/propertyUtils.ts
+++ app/client/src/widgets/TableWidgetV2/widget/propertyUtils.ts
@@ -2,7 +2,7 @@ import { Alignment } from "@blueprintjs/core";
 import { ColumnProperties } from "../component/Constants";
 import { TableWidgetProps } from "../constants";
 import { Colors } from "constants/Colors";
-import { get } from "lodash";
+import _, { get } from "lodash";
 
 export enum ColumnTypes {
   TEXT = "text",
@@ -155,100 +155,70 @@ export function updateIconAlignment(
   return propertiesToUpdate;
 }
 
-// A hook for handling property updates when the primaryColumns
-// has changed and it is supposed to update the derivedColumns
-// For example, when we add a new column or update a derived column's name
-// The propertyPath will be of the type `primaryColumns.columnId`
-// Handling BindingProperty of derived columns
-export const updateDerivedColumnsHook = (
+/*
+ * Hooks that updates columns order when a new column
+ * gets added to the primaryColumns
+ */
+export const updateColumnOrderHook = (
   props: TableWidgetProps,
   propertyPath: string,
   propertyValue: any,
 ): Array<{ propertyPath: string; propertyValue: any }> | undefined => {
-  let propertiesToUpdate: Array<{
+  const propertiesToUpdate: Array<{
     propertyPath: string;
     propertyValue: any;
   }> = [];
-  if (props && propertyValue) {
-    // If we're adding a column, we need to add it to the `derivedColumns` property as well
-    if (/^primaryColumns\.\w+$/.test(propertyPath)) {
-      const newId = propertyValue.id;
-      if (newId) {
-        // sets default value for some properties
-        propertyValue.buttonColor = Colors.GREEN;
-        propertyValue.menuColor = Colors.GREEN;
-        propertyValue.labelColor = Colors.WHITE;
-
-        propertiesToUpdate = [
-          {
-            propertyPath: `derivedColumns.${newId}`,
-            propertyValue,
-          },
-        ];
-      }
-
-      const oldColumnOrder = props.columnOrder || [];
-      const newColumnOrder = [...oldColumnOrder, propertyValue.id];
-      propertiesToUpdate.push({
-        propertyPath: "columnOrder",
-        propertyValue: newColumnOrder,
-      });
-    }
-    // If we're updating a columns' name, we need to update the `derivedColumns` property as well.
-    const regex = /^primaryColumns\.(\w+)\.(.*)$/;
-    if (regex.test(propertyPath)) {
-      const matches = propertyPath.match(regex);
-      if (matches && matches.length === 3) {
-        // updated to use column keys
-        const columnId = matches[1];
-        const columnProperty = matches[2];
-        const primaryColumn = props.primaryColumns[columnId];
-        const isDerived = primaryColumn ? primaryColumn.isDerived : false;
-
-        const { derivedColumns = {} } = props;
-
-        if (isDerived && derivedColumns && derivedColumns[columnId]) {
-          propertiesToUpdate = [
-            {
-              propertyPath: `derivedColumns.${columnId}.${columnProperty}`,
-              propertyValue: propertyValue,
-            },
-          ];
-        }
-      }
-    }
-    if (propertiesToUpdate.length > 0) return propertiesToUpdate;
+  if (props && propertyValue && /^primaryColumns\.\w+$/.test(propertyPath)) {
+    const oldColumnOrder = props.columnOrder || [];
+    const newColumnOrder = [...oldColumnOrder, propertyValue.id];
+    propertiesToUpdate.push({
+      propertyPath: "columnOrder",
+      propertyValue: newColumnOrder,
+    });
   }
-  return;
+
+  if (propertiesToUpdate.length > 0) {
+    return propertiesToUpdate;
+  } else {
+    return;
+  }
 };
-// Gets the base property path excluding the current property.
-// For example, for  `primaryColumns[5].computedValue` it will return
-// `primaryColumns[5]`
+
+/*
+ * Gets the base property path excluding the current property.
+ * For example, for  `primaryColumns[5].computedValue` it will return
+ * `primaryColumns[5]`
+ */
 export const getBasePropertyPath = (
   propertyPath: string,
 ): string | undefined => {
-  try {
-    const propertyPathRegex = /^(.*)\.\w+$/g;
-    const matches = [...propertyPath.matchAll(propertyPathRegex)][0];
-    if (matches && Array.isArray(matches) && matches.length === 2) {
-      return matches[1];
-    }
-    return;
-  } catch (e) {
+  const propertyPathRegex = /^(.*)\.\w+$/g;
+  const matches = [...propertyPath.matchAll(propertyPathRegex)][0];
+  if (matches && _.isArray(matches) && matches.length === 2) {
+    return matches[1];
+  } else {
     return;
   }
 };
 
-// Hide column which are not included in the array params
+/*
+ * Function to check if column should be hidden, based on
+ * the given columnTypes
+ */
 export const hideByColumnType = (
   props: TableWidgetProps,
   propertyPath: string,
   columnTypes: ColumnTypes[],
   shouldUsePropertyPath?: boolean,
 ) => {
-  const baseProperty = shouldUsePropertyPath
-    ? propertyPath
-    : getBasePropertyPath(propertyPath);
+  let baseProperty;
+
+  if (shouldUsePropertyPath) {
+    baseProperty = propertyPath;
+  } else {
+    baseProperty = getBasePropertyPath(propertyPath);
+  }
+
   const columnType = get(props, `${baseProperty}.columnType`, "");
   return !columnTypes.includes(columnType);
 };
