55fe60df4176b845e8eb73d8f2c5293a7cf23630
Paul Li <wmdev0808@gmail.com>
2022-02-10 23:16:48 +0800

    fix: Issues and Enhancements in the camera widget
    -- Add a Cypress test case for onSaveImage property

---
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Camera_spec.js
@@ -0,0 +1,34 @@
+const modalWidgetPage = require("../../../../locators/ModalWidget.json");
+const dsl = require("../../../../fixtures/CameraDsl.json");
+
+describe("Camera Widget", () => {
+  before(() => {
+    cy.addDsl(dsl);
+  });
+
+  beforeEach(() => {
+    cy.openPropertyPane("camerawidget");
+  });
+
+  afterEach(() => {
+    cy.goToEditFromPublish();
+  });
+
+  it("Property: onImageSave, show modal", () => {
+    const widgetName = `camerawidget`;
+    const modalName = `modal`;
+    const mainControlSelector = `.t--widget-${widgetName} [class*="MainControlContainer"] button`;
+
+    cy.createModal(modalName);
+    // Take photo
+    cy.get(mainControlSelector).click();
+    cy.wait(200);
+    // Save photo
+    cy.get(mainControlSelector)
+      .first()
+      .click();
+
+    // Assert: should trigger onImageSave action - modal popup
+    cy.get(modalWidgetPage.modelTextField).should("have.text", modalName);
+  });
+});
