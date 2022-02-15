6110babe9baf5c4d2fc784464655567b0eea17e7
Nayan <nayan@appsmith.com>
2022-02-15 13:08:54 +0600

    -removed commented codes

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Theme.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Theme.java
@@ -30,16 +30,6 @@ public class Theme extends BaseDomain {
     @JsonProperty("isSystemTheme")  // manually setting property name to make sure it's compatible with Gson
     private boolean isSystemTheme = false;  // should be false by default
 
-//    @Data
-//    @AllArgsConstructor
-//    @NoArgsConstructor
-//    public static class Properties {
-//        private Colors colors;
-//        private BorderRadiusProperties borderRadius;
-//        private BoxShadowProperties boxShadow;
-//        private FontFamilyProperties fontFamily;
-//    }
-
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
@@ -47,87 +37,4 @@ public class Theme extends BaseDomain {
         private String primaryColor;
         private String backgroundColor;
     }
-
-//    @Data
-//    public static class Config {
-//        private Colors colors;
-//        private BorderRadius borderRadius;
-//        private BoxShadow boxShadow;
-//        private FontFamily fontFamily;
-//    }
-
-//    @Data
-//    public static class ResponsiveAttributes {
-//        @JsonProperty("none")
-//        @SerializedName("none")
-//        private String noneValue;
-//
-//        @JsonProperty("DEFAULT")
-//        @SerializedName("DEFAULT")
-//        private String defaultValue;
-//
-//        @JsonProperty("md")
-//        @SerializedName("md")
-//        private String mdValue;
-//
-//        @JsonProperty("lg")
-//        @SerializedName("lg")
-//        private String lgValue;
-//
-//        @JsonProperty("xl")
-//        @SerializedName("xl")
-//        private String xlValue;
-//
-//        @JsonProperty("2xl")
-//        @SerializedName("2xl")
-//        private String doubleXlValue;
-//
-//        @JsonProperty("3xl")
-//        @SerializedName("3xl")
-//        private String tripleXlValue;
-//
-//        @JsonProperty("full")
-//        @SerializedName("full")
-//        private String fullValue;
-//    }
-//
-//    @Data
-//    public static class BorderRadius {
-//        private ResponsiveAttributes appBorderRadius;
-//    }
-//
-//    @Data
-//    public static class BoxShadow {
-//        private ResponsiveAttributes appBoxShadow;
-//    }
-//
-//    @Data
-//    public static class FontFamily {
-//        private List<String> appFont;
-//    }
-//
-//    @Data
-//    public static class FontFamilyProperties {
-//        private String appFont;
-//    }
-//
-//    @Data
-//    public static class WidgetStyle {
-//        private String backgroundColor;
-//        private String borderRadius;
-//        private String boxShadow;
-//        private String primaryColor;
-//        private String menuColor;
-//        private String buttonColor;
-//    }
-//
-//    @Data
-//    public static class BorderRadiusProperties {
-//        private String appBorderRadius;
-//    }
-//
-//    @Data
-//    public static class BoxShadowProperties {
-//        private String appBoxShadow;
-//    }
 }
