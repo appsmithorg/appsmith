eed06ee9fb7b89ca38ea4f9e2c405b050cf5d538
Aishwarya UR <aishwarya@appsmith.com>
2022-02-17 14:38:41 +0530

    Camera case locator fix

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Camera_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Camera_spec.js
@@ -15,20 +15,20 @@ describe("Camera Widget", () => {
   });
 
   it("Property: onImageSave, show modal", () => {
-    const widgetName = `camerawidget`;
     const modalName = `modal`;
-    //const mainControlSelector = `.t--widget-${widgetName} [class*="MainControlContainer"] button`;
+    const mainControlSelector =
+      "//div[contains(@class, 't--widget-camerawidget')]//button";
 
     cy.createModal(modalName);
     // Take photo
-    cy.xpath("//div[contains(@class, 't--widget-camerawidget')]//button")
+    cy.xpath(mainControlSelector)
       .eq(2)
-      .click();
+      .click(); //taking photo
     cy.wait(2000);
     // Save photo
-    cy.xpath("//div[contains(@class, 't--widget-camerawidget')]//button")
+    cy.xpath(mainControlSelector)
       .eq(2)
-      .click();
+      .click(); //saving it
 
     // Assert: should trigger onImageSave action - modal popup
     cy.get(modalWidgetPage.modelTextField).should("have.text", modalName);
