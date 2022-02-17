b1b833f99564a52be19ccf26ac5328f2739f6308
Ankita Kinger <28362912+ankitakinger@users.noreply.github.com>
2022-02-17 15:43:47 +0530

    feat: OIDC admin settings config form (#220)
    * added OIDC config form on admin settings page
    
    * resolved featureflags error
    
    * minor change
    
    * removed scope field for now

--- app/client/src/ee/pages/AdminSettings/config/authentication/index.tsx
+++ app/client/src/ee/pages/AdminSettings/config/authentication/index.tsx
@@ -1,1 +1,115 @@
-export * from "ce/pages/AdminSettings/config/authentication/index";
+import {
+  config as CE_config,
+  Form_Auth_Callout,
+  Github_Auth_Callout,
+  Google_Auth_Callout,
+  Saml_Auth_Callout,
+} from "ce/pages/AdminSettings/config/authentication";
+import {
+  AdminConfigType,
+  SettingCategories,
+  SettingTypes,
+  SettingSubtype,
+} from "@appsmith/pages/AdminSettings/config/types";
+import { AuthMethodType, AuthPage } from "./AuthPage";
+import OIDC from "assets/images/oidc.svg";
+import React from "react";
+import { getAppsmithConfigs } from "@appsmith/configs";
+
+const { enableOidcOAuth } = getAppsmithConfigs();
+
+const OIDC_Auth: AdminConfigType = {
+  type: SettingCategories.OIDC_AUTH,
+  controlType: SettingTypes.GROUP,
+  title: "OpenID Connect",
+  subText:
+    "Enable your organization to sign in with your preferred OIDC compliant provider.",
+  canSave: true,
+  settings: [
+    {
+      id: "APPSMITH_OAUTH2_OIDC_READ_MORE",
+      category: SettingCategories.OIDC_AUTH,
+      subCategory: "oidc signup",
+      controlType: SettingTypes.LINK,
+      label: "How to configure?",
+    },
+    {
+      id: "APPSMITH_OAUTH2_OIDC_CLIENT_ID",
+      category: SettingCategories.OIDC_AUTH,
+      subCategory: "oidc signup",
+      controlType: SettingTypes.TEXTINPUT,
+      controlSubType: SettingSubtype.TEXT,
+      label: "Client ID",
+    },
+    {
+      id: "APPSMITH_OAUTH2_OIDC_CLIENT_SECRET",
+      category: SettingCategories.OIDC_AUTH,
+      subCategory: "oidc signup",
+      controlType: SettingTypes.TEXTINPUT,
+      controlSubType: SettingSubtype.TEXT,
+      label: "Client Secret",
+    },
+    {
+      id: "APPSMITH_OAUTH2_OIDC_AUTHORIZATION_URI",
+      category: SettingCategories.OIDC_AUTH,
+      subCategory: "oidc signup",
+      controlType: SettingTypes.TEXTINPUT,
+      controlSubType: SettingSubtype.TEXT,
+      label: "Authorization URL",
+    },
+    {
+      id: "APPSMITH_OAUTH2_OIDC_TOKEN_URI",
+      category: SettingCategories.OIDC_AUTH,
+      subCategory: "oidc signup",
+      controlType: SettingTypes.TEXTINPUT,
+      controlSubType: SettingSubtype.TEXT,
+      label: "Token URL",
+    },
+    {
+      id: "APPSMITH_OAUTH2_OIDC_USER_INFO",
+      category: SettingCategories.OIDC_AUTH,
+      subCategory: "oidc signup",
+      controlType: SettingTypes.TEXTINPUT,
+      controlSubType: SettingSubtype.TEXT,
+      label: "User Info URL",
+    },
+    {
+      id: "APPSMITH_OAUTH2_OIDC_JWK_SET_URI",
+      category: SettingCategories.OIDC_AUTH,
+      subCategory: "oidc signup",
+      controlType: SettingTypes.TEXTINPUT,
+      controlSubType: SettingSubtype.TEXT,
+      label: "JWK Set URL",
+    },
+  ],
+};
+
+export const Oidc_Auth_Callout: AuthMethodType = {
+  id: "APPSMITH_OIDC_AUTH",
+  category: SettingCategories.OIDC_AUTH,
+  label: "OIDC",
+  subText: `Enable your organization to sign in with your preferred OIDC compliant provider.`,
+  image: OIDC,
+  type: "LINK",
+  isConnected: enableOidcOAuth,
+};
+
+const AuthMethods = [
+  Oidc_Auth_Callout,
+  Saml_Auth_Callout,
+  Google_Auth_Callout,
+  Github_Auth_Callout,
+  Form_Auth_Callout,
+];
+
+function AuthMain() {
+  return <AuthPage authMethods={AuthMethods} />;
+}
+
+export const config: AdminConfigType = {
+  ...CE_config,
+  children: Array.isArray(CE_config.children)
+    ? [...CE_config.children, OIDC_Auth]
+    : [],
+  component: AuthMain,
+};
--- app/client/src/ee/pages/AdminSettings/config/types.ts
+++ app/client/src/ee/pages/AdminSettings/config/types.ts
@@ -1,1 +1,21 @@
-export * from "ce/pages/AdminSettings/config/types";
+import {
+  SettingCategories as CE_SettingCategories,
+  AdminConfigType as CE_AdminConfigType,
+  Category as CE_Category,
+  Setting as CE_Setting,
+  SettingSubtype,
+  SettingTypes,
+} from "ce/pages/AdminSettings/config/types";
+
+const EE_SettingCategories = {
+  OIDC_AUTH: "oidc-auth",
+};
+
+export const SettingCategories = {
+  ...CE_SettingCategories,
+  ...EE_SettingCategories,
+};
+export type AdminConfigType = CE_AdminConfigType;
+export type Category = CE_Category;
+export type Setting = CE_Setting;
+export { SettingSubtype, SettingTypes };
