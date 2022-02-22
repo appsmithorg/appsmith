af45193ecb4419d2e258ac1971d05259612f875e
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2022-02-22 19:07:08 +0530

    isLoading: check entity properties

--- app/client/src/sagas/WidgetLoadingSaga.ts
+++ app/client/src/sagas/WidgetLoadingSaga.ts
@@ -13,60 +13,77 @@ import {
 } from "../constants/ReduxActionConstants";
 import log from "loglevel";
 import * as Sentry from "@sentry/react";
-import { get } from "lodash";
+import { get, set } from "lodash";
 
-const createEntityDependencyMap = (dependencyMap: DependencyMap) => {
-  const entityDepMap: DependencyMap = {};
-  Object.entries(dependencyMap).forEach(([dependant, dependencies]) => {
-    const entityDependant = dependant.split(".")[0];
-    const existing = entityDepMap[entityDependant] || [];
-    entityDepMap[entityDependant] = existing.concat(
-      dependencies
-        .map((dep) => {
-          const value = dep.split(".")[0];
-          if (value !== entityDependant) {
-            return value;
-          }
-          return undefined;
-        })
-        .filter((value) => typeof value === "string") as string[],
+type ExtendedDependencyMap = Record<string, Array<string>>;
+
+type FullDependencyMap = Record<string, ExtendedDependencyMap>;
+
+const createEntityDependencyMap = (inverseMap: DependencyMap) => {
+  const entityDepMap: FullDependencyMap = {};
+  Object.entries(inverseMap).forEach(([dependantPath, dependencies]) => {
+    const entityDependant = dependantPath.split(".")[0];
+    const existing = entityDepMap[entityDependant] || {};
+    let existing2 = existing[dependantPath] || [];
+    existing2 = existing2.concat(
+      dependencies.filter((dep) => {
+        const value = dep.split(".")[0];
+        if (value === "Table1" && dep.split(".")[1] !== "tableData") return;
+        if (value !== entityDependant) {
+          return dep;
+        }
+        return;
+      }),
     );
+    if (!(existing2.length > 0)) return;
+    set(entityDepMap, [entityDependant, dependantPath], existing2);
   });
   return entityDepMap;
 };
 
 const getEntityDependencies = (
-  entityNames: string[],
-  inverseMap: DependencyMap,
+  fullEntityPaths: string[],
+  fullDepMap: FullDependencyMap,
   visited: Set<string>,
-): Set<string> => {
-  const dependantsEntities: Set<string> = new Set();
-  entityNames.forEach((entityName) => {
-    if (entityName in inverseMap) {
-      inverseMap[entityName].forEach((dependency) => {
-        const dependantEntityName = dependency.split(".")[0];
+): { names: Set<string>; fullPaths: Set<string> } => {
+  const dependantsEntityNames = new Set<string>();
+  const dependantEntityFullPaths = new Set<string>();
+  fullEntityPaths.forEach((fullEntityPath) => {
+    const entityName = fullEntityPath.split(".")[0];
+    if (!(entityName in fullDepMap)) return;
+    const extDepMap = fullDepMap[entityName];
+    Object.entries(extDepMap).forEach(([fullPath, dependencies]) => {
+      if (fullEntityPath.split(".").length > 1 && fullPath !== fullEntityPath)
+        return;
+      dependencies.forEach((dependantPath) => {
+        const dependantPathArray = dependantPath.split(".");
+        const dependantEntityName = dependantPathArray[0];
         // Example: For a dependency chain that looks like Dropdown1.selectedOptionValue -> Table1.tableData -> Text1.text -> Dropdown1.options
         // Here we're operating on
         // Dropdown1 -> Table1 -> Text1 -> Dropdown1
         // It looks like a circle, but isn't
         // So we need to mark the visited nodes and avoid infinite recursion in case we've already visited a node once.
-        if (visited.has(dependantEntityName)) {
+        if (visited.has(entityName)) {
           return;
         }
-        visited.add(dependantEntityName);
-        dependantsEntities.add(dependantEntityName);
+        visited.add(entityName);
+        dependantsEntityNames.add(dependantEntityName);
+        dependantEntityFullPaths.add(dependantPath);
         const childDependencies = getEntityDependencies(
-          Array.from(dependantsEntities),
-          inverseMap,
+          Array.from(dependantEntityFullPaths),
+          fullDepMap,
           visited,
         );
-        childDependencies.forEach((entityName) => {
-          dependantsEntities.add(entityName);
+        childDependencies.names.forEach((entityName) => {
+          dependantsEntityNames.add(entityName);
+        });
+        childDependencies.fullPaths.forEach((entityPath) => {
+          dependantEntityFullPaths.add(entityPath);
         });
       });
-    }
+    });
   });
-  return dependantsEntities;
+  return { names: dependantsEntityNames, fullPaths: dependantEntityFullPaths };
 };
 
 const ACTION_EXECUTION_REDUX_ACTIONS = [
@@ -83,6 +100,8 @@ const ACTION_EXECUTION_REDUX_ACTIONS = [
 function* setWidgetsLoadingSaga() {
   const inverseMap = yield select(getEvaluationInverseDependencyMap);
   const entityDependencyMap = createEntityDependencyMap(inverseMap);
+  console.log("Hello INVERSE_MAP", inverseMap);
+  console.log("Hello ENTITY_MAP", entityDependencyMap);
   const actions = yield select(getActions);
   const isLoadingActions: string[] = actions
     .filter((action: ActionData) => action.isLoading)
@@ -104,20 +123,20 @@ function* setWidgetsLoadingSaga() {
   Object.entries(dataTree).forEach(([entityName, entity]) => {
     if ("ENTITY_TYPE" in entity && entity.ENTITY_TYPE === ENTITY_TYPE.WIDGET)
       if (get(dataTree, [entityName, "animateLoading"]) === false) {
-        loadingEntities.delete(entityName);
+        loadingEntities.names.delete(entityName);
       }
   });
 
   yield put({
     type: ReduxActionTypes.SET_LOADING_ENTITIES,
-    payload: loadingEntities,
+    payload: loadingEntities.names,
   });
 }
 
 function* actionExecutionChangeListenerSaga() {
   while (true) {
     const takeEffect: TakeEffect = yield take(ACTION_EXECUTION_REDUX_ACTIONS);
-    console.log("Hello REDUX", takeEffect.type);
+    console.log("Hello REDUX", takeEffect.type, takeEffect);
     yield fork(setWidgetsLoadingSaga);
   }
 }
