10033aea098f03c05c1da8afc67881bc8ee3ff6e
Nayan <nayan@appsmith.com>
2022-02-24 16:43:11 +0600

    -Added new API to return list of default plugins (#11297)
    Added a new API that'll return a list of plugins which are available to install. In other words, it'll return list of default/system plugins. This returns the package name, name and icon only - which will be used by templates to show datasource name and icons.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomPluginRepositoryCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomPluginRepositoryCEImpl.java
@@ -1,13 +1,29 @@
 package com.appsmith.server.repositories.ce;
 
 import com.appsmith.server.domains.Plugin;
+import com.appsmith.server.domains.QPlugin;
 import com.appsmith.server.repositories.BaseAppsmithRepositoryImpl;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.core.query.Criteria;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 public class CustomPluginRepositoryCEImpl extends BaseAppsmithRepositoryImpl<Plugin> implements CustomPluginRepositoryCE {
 
     public CustomPluginRepositoryCEImpl(ReactiveMongoOperations mongoOperations, MongoConverter mongoConverter) {
         super(mongoOperations, mongoConverter);
+    }
+
+    @Override
+    public Flux<Plugin> findDefaultPluginIcons() {
+        Criteria criteria = Criteria.where(fieldName(QPlugin.plugin.defaultInstall)).is(Boolean.TRUE);
+        List<String> projections = List.of(
+                fieldName(QPlugin.plugin.name),
+                fieldName(QPlugin.plugin.packageName),
+                fieldName(QPlugin.plugin.iconLocation)
+        );
+        return this.queryAll(List.of(criteria), projections, null, null);
     }
 }
