b3a23f897aabba213208f5eb362f0adcbe297739
Arsalan <arsalanyaldram0211@outlook.com>
2022-02-28 18:35:35 +0530

    chore: Added Jest Test to check focus.

--- app/client/src/pages/Editor/PropertyPaneTitle.test.tsx
+++ app/client/src/pages/Editor/PropertyPaneTitle.test.tsx
@@ -9,21 +9,30 @@ import { Provider } from "react-redux";
 import store from "../../store";
 
 describe("<PropertyPaneTitle />", () => {
-  it("should focus when f2 is pressed", async () => {
-    const getTestComponent = () => (
-      <Provider store={store}>
-        <ThemeProvider theme={lightTheme}>
-          <PropertyPaneTitle
-            actions={[]}
-            isPanelTitle
-            // title="test"
-            widgetId="1"
-          />
-        </ThemeProvider>
-      </Provider>
-    );
+  const getTestComponent = () => (
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <PropertyPaneTitle
+          actions={[]}
+          isPanelTitle
+          title="test"
+          widgetId="1"
+        />
+      </ThemeProvider>
+    </Provider>
+  );
+
+  it("should focus on the component mount", () => {
     const component = getTestComponent();
     const renderResult = render(component);
+    expect(renderResult.container.querySelector("input")).toHaveFocus();
+  });
+
+  it("should focus when f2 is pressed", async () => {
+    const component = getTestComponent();
+    const renderResult = render(component);
+    await userEvent.keyboard("{Tab}");
+    await userEvent.keyboard("{Tab}");
     await userEvent.keyboard("{F2}");
     expect(renderResult.container.querySelector("input")).toBeVisible();
   });
