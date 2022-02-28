16664b72457b4ac6cde1a664f0e83af69769e036
Aswath K <aswath@appsmith.com>
2022-02-28 18:50:41 +0530

    fix: local ts issue regarding wdyr

--- app/client/src/wdyr.ts
+++ app/client/src/wdyr.ts
@@ -1,12 +1,10 @@
 import React from "react";
 
 if (process.env.NODE_ENV === "development") {
-  import("@welldone-software/why-did-you-render").then((module) => {
-    const whyDidYouRender = module.default;
-    whyDidYouRender(React, {
-      trackAllPureComponents: false,
-      trackExtraHooks: [[require("react-redux/lib"), "useSelector"]],
-    });
+  const whyDidYouRender = require("@welldone-software/why-did-you-render");
+  whyDidYouRender(React, {
+    trackAllPureComponents: false,
+    trackExtraHooks: [[require("react-redux/lib"), "useSelector"]],
   });
 }
 export default "";
