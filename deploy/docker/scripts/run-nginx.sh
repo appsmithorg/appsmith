018b1eb9efed5f1b8baca371cc7ba1412c854b06
Shrikant Sharat Kandula <shrikant@appsmith.com>
2022-03-01 20:50:53 +0530

    Apply env variables in NGINX config before startup
    Instead of doing env substitutions at request processing time, do it
    before NGINX even starts.

--- deploy/docker/scripts/run-nginx.sh
+++ deploy/docker/scripts/run-nginx.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-NGINX_SSL_CMNT="#"
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
 
-TEMPLATE_DIR="/opt/appsmith/templates"
-APP_TEMPLATE="$TEMPLATE_DIR/nginx-app-http.conf.template.sh"
+http_conf="/opt/appsmith/templates/nginx-app-http.conf.template.sh"
+https_conf="/opt/appsmith/templates/nginx-app-https.conf.template.sh"
+
+APP_TEMPLATE="$http_conf"
 
 # Check exist certificate with given custom domain
 if [[ -n $APPSMITH_CUSTOM_DOMAIN ]]; then
-  APP_TEMPLATE="$TEMPLATE_DIR/nginx-app-https.conf.template.sh"
-  if ! [[ -e "/etc/letsencrypt/live/$APPSMITH_CUSTOM_DOMAIN" ]]; then
-    source "/opt/appsmith/init_ssl_cert.sh"
-    init_ssl_cert "$APPSMITH_CUSTOM_DOMAIN"
-    
-    if (($? != 0)); then
-      echo $?
-      APP_TEMPLATE="$TEMPLATE_DIR/nginx-app-http.conf.template.sh"
-    fi
-  fi
+	APP_TEMPLATE="$https_conf"
+	if ! [[ -e "/etc/letsencrypt/live/$APPSMITH_CUSTOM_DOMAIN" ]]; then
+		source "/opt/appsmith/init_ssl_cert.sh"
+		if ! init_ssl_cert "$APPSMITH_CUSTOM_DOMAIN"; then
+			echo "Status code from init_ssl_cert is $?"
+			APP_TEMPLATE="$http_conf"
+		fi
+	fi
 fi
 
-echo "Re-generating nginx config template"
-bash "$APP_TEMPLATE" "$APPSMITH_CUSTOM_DOMAIN" >"/etc/nginx/conf.d/nginx_app.conf.template"
+bash "$APP_TEMPLATE" "$APPSMITH_CUSTOM_DOMAIN" > /etc/nginx/sites-available/default
 
-echo "Generating nginx configuration"
-cat /etc/nginx/conf.d/nginx_app.conf.template | envsubst "$(printf '$%s,' $(env | grep -Eo '^APPSMITH_[A-Z0-9_]+'))" | sed -e 's|\${\(APPSMITH_[A-Z0-9_]*\)}||g' >/etc/nginx/sites-available/default
+python3 -c '
+from pathlib import Path
+import re
+import os
+index_path = Path("/opt/appsmith/editor/index.html")
+content = re.sub(
+	r"\b__(APPSMITH_[A-Z0-9_]+?)__\b",
+	lambda match: [print(match.group(0), os.environ.get(match.group(1), None)), os.environ.get(match.group(1), match.group(0))][-1],
+	index_path.read_text(),
+)
+index_path.write_text(content)
+'
 
 exec nginx -g "daemon off;"
