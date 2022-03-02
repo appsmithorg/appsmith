4c1a6674fa0a9a5373c20094fe75481402d77b97
rahulramesha <71900764+rahulramesha@users.noreply.github.com>
2022-03-02 19:12:41 +0530

    fix: separating property pane out of widgets editor to avoid triggering unwanted events on widget canvas (#11396)
    * separating property pane out of widgets editor to avoid triggering unwanted events on widget canvas

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/WidgetSelection/WidgetSelection_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/WidgetSelection/WidgetSelection_spec.js
@@ -24,4 +24,29 @@ describe("Widget Selection", function() {
 
     cy.get(`.t--multi-selection-box`).should("have.length", 1);
   });
+
+  it("Select widgets using cmd + click and open property pane by clicking on the widget from right side panel", function() {
+    // Selection
+    cy.get(`#${dsl.dsl.children[0].widgetId}`).click({
+      ctrlKey: true,
+    });
+    cy.get(`#${dsl.dsl.children[2].widgetId}`).click({
+      ctrlKey: true,
+    });
+    cy.get(`div[data-testid='t--selected']`).should("have.length", 2);
+    cy.get(`.t--multi-selection-box`).should("have.length", 1);
+
+    //select on one of the widgets from the right side panel
+    cy.get(`.t-multi-widget-property-pane`).should("have.length", 1);
+    cy.get(`.t-multi-widget-button-${dsl.dsl.children[2].widgetId}`).should(
+      "have.length",
+      1,
+    );
+    cy.get(`.t-multi-widget-button-${dsl.dsl.children[2].widgetId}`).click({
+      force: true,
+    });
+
+    //should open property pane
+    cy.get(`.t--property-pane-view`).should("have.length", 1);
+  });
 });
