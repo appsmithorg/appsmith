b5a927df54f8416520031651a92f95890ca11b27
keyurparalkar <keyur@appsmith.com>
2022-03-03 17:27:52 +0530

    refactor: addressed review comments; removed border radius and box shadow for text widget; Updated migrations

--- app/client/src/widgets/TextWidget/component/index.tsx
+++ app/client/src/widgets/TextWidget/component/index.tsx
@@ -99,9 +99,6 @@ export const StyledText = styled(Text)<{
   textColor?: string;
   fontStyle?: string;
   fontSize?: TextSize;
-  borderRadius: string;
-  boxShadow?: string;
-
   borderColor?: Color;
   borderWidth?: number;
 }>`
@@ -119,8 +116,6 @@ export const StyledText = styled(Text)<{
   align-items: ${(props) =>
     props.scroll || props.truncate ? "flex-start" : "center"};
   background: ${(props) => props?.backgroundColor};
-  border-radius: ${({ borderRadius }) => borderRadius};
-  box-shadow: ${({ boxShadow }) => `${boxShadow}`} !important;
   border-width: ${(props) => props.borderWidth}px;
   border-color: ${(props) => props.borderColor || "transparent"};
   border-style: solid;
@@ -197,9 +192,6 @@ export interface TextComponentProps extends ComponentProps {
   textColor?: string;
   fontStyle?: string;
   disableLink: boolean;
-  borderRadius: string;
-  boxShadow?: string;
-
   borderColor?: Color;
   borderWidth?: number;
   shouldTruncate: boolean;
@@ -289,9 +281,7 @@ class TextComponent extends React.Component<TextComponentProps, State> {
             <StyledText
               backgroundColor={backgroundColor}
               borderColor={this.props.borderColor}
-              borderRadius={this.props.borderRadius}
               borderWidth={this.props.borderWidth}
-              boxShadow={this.props.boxShadow}
               className={this.props.isLoading ? "bp3-skeleton" : "bp3-ui-text"}
               ellipsize={ellipsize}
               fontSize={fontSize}
