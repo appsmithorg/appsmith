f4287a9567faa66c3ab8ae81f5244076cc6f02dc
Aishwarya UR <aishwarya@appsmith.com>
2022-03-04 16:54:41 +0530

    Migration spec locators fix

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_GeneralProperty_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_GeneralProperty_spec.js
@@ -194,7 +194,7 @@ describe("Table Widget property pane feature validation", function() {
       .contains("Short")
       .click({ force: true });
     cy.wait(1000);
-    cy.readTabledataValidateCSS("0", "1", "height", "19px", true);
+    cy.readTabledataValidateCSS("0", "1", "height", "28px", true);
   });
   it("Test to validate text color and text background", function() {
     // Open property pane
