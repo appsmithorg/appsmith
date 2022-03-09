2dfb86aa4cf207c09d89daaeb5eca0eaa3b3df2d
Mojtaba Zarezadeh <mojtab.zare@gmail.com>
2022-03-09 15:15:16 +0330

    fix: fix and update tests and test container and make it support newer algorithms like ed25519 Added org.bouncycastle:bcprov-jdk15on to support new algorithms Changed ssh keys in test with newer one with ed25519 algorithm. command : `ssh-keygen -t ed25519`

--- app/server/appsmith-plugins/sshPlugin/src/test/java/com/external/plugins/SSHPluginTest.java
+++ app/server/appsmith-plugins/sshPlugin/src/test/java/com/external/plugins/SSHPluginTest.java
@@ -9,17 +9,18 @@ import com.appsmith.external.models.SSHPrivateKey;
 import com.appsmith.external.models.UploadedFile;
 import com.jcraft.jsch.Session;
 import org.bson.internal.Base64;
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,23 +32,42 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class SSHPluginTest {
+    private static final String USERNAME = "appsmith";
+    private static final String PASSWORD = "securePass";
+    private static GenericContainer sshServer;
     private static String host;
     private static Integer port;
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
 
-    @ClassRule
-    public static final GenericContainer sshServer = new GenericContainer(CompletableFuture.completedFuture("sickp/alpine-sshd:7.9-r1"))
-            .withEnv(Map.of("CNTUSER", "root", "CNTPASS", "root"))
-            .withClasspathResourceMapping("ssh-test-key.pub", "/root/.ssh/authorized_keys", BindMode.READ_ONLY)
-            .withExposedPorts(22);
-
-    private SSHPlugin.SSHPluginExecutor pluginExecutor = new SSHPlugin.SSHPluginExecutor();
+    private final SSHPlugin.SSHPluginExecutor pluginExecutor = new SSHPlugin.SSHPluginExecutor();
 
     @BeforeClass
     public static void setup() {
+
+
+        String publicKey;
+        try {
+            Path path = Paths.get("src", "test", "resources", "ssh-test-key.pub");
+            publicKey = Files.readAllLines(path).get(0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        sshServer = new GenericContainer(CompletableFuture.completedFuture("linuxserver/openssh-server:8.6_p1-r3-ls72"))
+                .withEnv(Map.of(
+                        "USER_NAME", USERNAME,
+                        "USER_PASSWORD", PASSWORD,
+                        "PASSWORD_ACCESS", "true",
+                        "PUBLIC_KEY", publicKey
+                ))
+                .withExposedPorts(2222);
+        sshServer.start();
         host = sshServer.getContainerIpAddress();
         port = sshServer.getFirstMappedPort();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        // Manually controlling container lifecycle
+        sshServer.stop();
     }
 
     private DatasourceConfiguration createDSConfigurationForPasswordLogin() {
@@ -75,8 +95,7 @@ public class SSHPluginTest {
         sshAuth.setUsername(USERNAME);
         sshAuth.setAuthType(SSHAuth.AuthType.IDENTITY_FILE);
         // Read the private key from src/test/resources. The private key has been generated with passphrase: `password`
-        InputStream is = getClass().getClassLoader().getResourceAsStream("ssh-test-key");
-        String privateKey = new String(is.readAllBytes());
+        final String privateKey = Files.readString(Paths.get("src", "test", "resources", "ssh-test-key"));
         // We append data:<mime type>;base64, because the client uses Uppy for file uploads and this is how we get
         // the data from the client
         String encodedPrivateKey = "data:application/octet-stream;base64," + Base64.encode(privateKey.getBytes());
