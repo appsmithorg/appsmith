e1bc86b9e9921d663f9b7a868609817cc264aa1b
f0c1s <anubhav@appsmith.com>
2022-03-10 14:57:27 +0530

    fix: remove text area from commit message box (#11342)
    * fix: remove text area from commit message box
    * fix: multiline commit

--- app/client/src/components/ads/TextInput.tsx
+++ app/client/src/components/ads/TextInput.tsx
@@ -75,6 +75,7 @@ export type TextInputProps = CommonComponentProps & {
   $padding?: string;
   useTextArea?: boolean;
   isCopy?: boolean;
+  style?: any;
 };
 
 type boxReturnType = {
@@ -125,7 +126,6 @@ const InputLoader = styled.div<{
       : "100%"};
 
   height: ${(props) => props.$height || "36px"};
-  border-radius: 0;
 `;
 
 const StyledInput = styled((props) => {
@@ -217,7 +217,7 @@ const InputWrapper = styled.div<{
 }>`
   position: relative;
   display: flex;
-  align-items: center;  
+  align-items: center;
   width: ${(props) =>
     props.fill ? "100%" : props.width ? props.width : "260px"};
   height: ${(props) => props.height || "36px"};
@@ -241,7 +241,7 @@ const InputWrapper = styled.div<{
   .${Classes.TEXT} {
     color: ${(props) => props.theme.colors.danger.main};
   }
-  ​ .helper {
+  .helper {
     .${Classes.TEXT} {
       color: ${(props) => props.theme.colors.textInput.helper};
     }
