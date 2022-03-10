54e97d0555015118d40921724161e1782a03652d
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-03-10 17:14:53 +0530

    reverting to previous shortcut - "option" key doesn't work as expected when tried Shift-Option-F

--- app/client/src/components/editorComponents/CodeEditor/utils/autoIndentUtils.ts
+++ app/client/src/components/editorComponents/CodeEditor/utils/autoIndentUtils.ts
@@ -1,17 +1,17 @@
 import { PLATFORM_OS, getPlatformOS } from "utils/helpers";
 
 const autoIndentShortcut = {
-  [PLATFORM_OS.MAC]: "Shift-Option-F",
-  [PLATFORM_OS.IOS]: "Shift-Option-F",
-  [PLATFORM_OS.WINDOWS]: "Shift-Alt-F",
-  [PLATFORM_OS.ANDROID]: "Shift-Alt-F",
-  [PLATFORM_OS.LINUX]: "Ctrl-Shift-I",
+  [PLATFORM_OS.MAC]: "shift-cmd-p",
+  [PLATFORM_OS.IOS]: "shift-cmd-p",
+  [PLATFORM_OS.WINDOWS]: "shift-alt-f",
+  [PLATFORM_OS.ANDROID]: "shift-alt-f",
+  [PLATFORM_OS.LINUX]: "shift-ctrl-i",
 };
 
 export const getAutoIndentShortcutKey = () => {
   const platformOS = getPlatformOS();
   const autoIndentKey = platformOS
     ? autoIndentShortcut[platformOS]
-    : "Shift-Alt-F";
+    : "shift-alt-f";
   return autoIndentKey;
 };
