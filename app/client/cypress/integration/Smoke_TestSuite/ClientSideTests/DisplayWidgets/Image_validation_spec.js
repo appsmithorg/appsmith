3c54db0d17215bea26b0053d532e3fd516a35204
bhavin <techbhavin@gmail.com>
2022-03-11 14:20:54 +0530

    test: added cypress testcase

---
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Image_validation_spec.js
@@ -0,0 +1,57 @@
+const commonlocators = require("../../../../locators/commonlocators.json");
+const viewWidgetsPage = require("../../../../locators/ViewWidgets.json");
+const publish = require("../../../../locators/publishWidgetspage.json");
+const dsl = require("../../../../fixtures/displayWidgetDsl.json");
+const pages = require("../../../../locators/Pages.json");
+
+describe("Image Widget Validation Image Urls", function() {
+  before(() => {
+    cy.addDsl(dsl);
+  });
+
+  it("Check default image and new image", function() {
+    cy.openPropertyPane("imagewidget");
+    cy.testJsontext("defaultimage", this.data.defaultimage);
+    cy.get(viewWidgetsPage.imageinner)
+      .invoke("attr", "src")
+      .should("contain", this.data.defaultimage);
+    cy.wait(1000);
+    /**
+     * @param{URL} ImageUrl
+     */
+    cy.testCodeMirror(this.data.NewImage);
+    cy.get(viewWidgetsPage.imageinner)
+      .invoke("attr", "src")
+      .should("contain", this.data.NewImage);
+    cy.closePropertyPane();
+  });
+
+  it("Remove both images and check empty screen", function() {
+    cy.openPropertyPane("imagewidget");
+
+    cy.clearPropertyValue(0);
+    cy.clearPropertyValue(1);
+
+    cy.get(
+      `${viewWidgetsPage.imageWidget} div[data-testid=error-container]`,
+    ).should("not.exist");
+    cy.get(
+      `${viewWidgetsPage.imageWidget} div[data-testid=styledImage]`,
+    ).should("exist");
+
+    cy.get(viewWidgetsPage.imageinner)
+      .invoke("attr", "src")
+      .should("contain", "");
+  });
+
+  it("Add new image and check image src", function() {
+    cy.testCodeMirror(this.data.NewImage);
+    // if imageError flag not reset properly, this test will fail.
+    cy.get(viewWidgetsPage.imageinner)
+      .invoke("attr", "src")
+      .should("contain", this.data.NewImage);
+  });
+});
+afterEach(() => {
+  // put your clean up code if any
+});
