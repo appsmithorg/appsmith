64063f5fdcb37811addb8e3011d91ece74df1221
apple <nandan@thinkify.io>
2022-03-16 16:34:25 +0530

    Data and method for OIDC Form

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -20,6 +20,8 @@ const modalWidgetPage = require("../locators/ModalWidget.json");
 const widgetsPage = require("../locators/Widgets.json");
 const LayoutPage = require("../locators/Layout.json");
 const formWidgetsPage = require("../locators/FormWidgets.json");
+const oidcform = require("../locators/OIDCForm.json");
+const oidcData = require("../fixtures/oidcSource.json");
 import ApiEditor from "../locators/ApiEditor";
 const apiwidget = require("../locators/apiWidgetslocator.json");
 const dynamicInputLocators = require("../locators/DynamicInput.json");
@@ -2317,6 +2319,19 @@ Cypress.Commands.add(
     // });
   },
 );
+
+Cypress.Commands.add("fillOIDCform", () => {
+  cy.get(oidcform.clientID).type(oidcData.clientID);
+  cy.get(oidcform.clientSecret).type(oidcData.clientSecret);
+  cy.get(oidcform.authorizationURL).type(oidcData.authorizationURL);
+  cy.get(oidcform.tokenURL).type(oidcData.tokenURL);
+  cy.get(oidcform.userInfo).type(oidcData.userInfo);
+  cy.get(oidcform.jwtSetURI).type(oidcData.jwtSetURI);
+  cy.xpath(oidcform.AdvancedOptionExpand).click({ force: true });
+  cy.get(oidcform.scope).type(oidcData.scope);
+  cy.get(oidcform.userAttribute).type(oidcData.userAttribute);
+  cy.get(oidcform.saveBtn).click({ force: true });
+});
 
 Cypress.Commands.add(
   "fillPostgresDatasourceForm",
