d9c568fcec20688d84a5d97aa1061f6357a9eb2e
Sumesh Pradhan <sumesh@appsmith.com>
2022-03-16 15:48:43 +0530

    Update deploy/docker/scripts/run-java.sh
    Co-authored-by: Shrikant Sharat Kandula <shrikant@appsmith.com>

--- deploy/docker/scripts/run-java.sh
+++ deploy/docker/scripts/run-java.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Ref -Dlog4j2.formatMsgNoLookups=true https://spring.io/blog/2021/12/10/log4j2-vulnerability-and-spring-boot
-java_cmd="java ${APPSMITH_JAVA_ARGS:-} -Dserver.port=8082 -Djava.security.egd=file:/dev/./urandom -Dlog4j2.formatMsgNoLookups=true -jar server.jar"
-exec $java_cmd
+exec java ${APPSMITH_JAVA_ARGS:-} -Dserver.port=8082 -Djava.security.egd=file:/dev/./urandom -Dlog4j2.formatMsgNoLookups=true -jar server.jar
