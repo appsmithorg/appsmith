6bd181600e0262ca6053422a110b5cfbe4843b65
Aishwarya UR <aishwarya@appsmith.com>
2022-03-18 17:35:58 +0530

    HSEditor complete replace fix

--- app/client/cypress/support/Pages/JSEditor.ts
+++ app/client/cypress/support/Pages/JSEditor.ts
@@ -37,11 +37,8 @@ export class JSEditor {
         .first()
         .focus()
         .type("{uparrow}", { force: true })
-        .type("{ctrl}{shift}{downarrow}{del}", { force: true })
-        .wait(400)
-        .first()
-        .focus()
-        .type("{ctrl}{shift}{downarrow}{del}", { force: true })
+        .type("{ctrl}{shift}{downarrow}", { force: true })
+        .type("{del}", { force: true })
     }
 
     cy.get(locator._codeMirrorTextArea)
