8334d2b1133202e919f48fe3171b3cdf7cf102f1
Anagh Hegde <anagh@appsmith.com>
2022-03-21 10:09:03 +0530

    For appsmith default application, do not delete resources from db on delete branch

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/GitServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/GitServiceCEImpl.java
@@ -2169,7 +2169,14 @@ public class GitServiceCEImpl implements GitServiceCE {
                                     return Mono.error(new AppsmithException(AppsmithError.GIT_ACTION_FAILED, " delete branch. Branch does not exists in the repo"));
                                 }
                                 return applicationService.findByBranchNameAndDefaultApplicationId(branchName, defaultApplicationId, MANAGE_APPLICATIONS)
-                                        .flatMap(applicationPageService::deleteApplicationByResource)
+                                        .flatMap(application1 -> {
+                                            // If the user is trying to delete Appsmith default branch do not delete the resources from the db
+                                            if(application1.getId().equals(application1.getGitApplicationMetadata().getDefaultApplicationId())) {
+                                                return Mono.just(application1);
+                                            } else {
+                                                return applicationPageService.deleteApplicationByResource(application1);
+                                            }
+                                        })
                                         .onErrorResume(throwable -> {
                                             log.warn("Unable to find branch with name ", throwable);
                                             return addAnalyticsForGitOperation(
