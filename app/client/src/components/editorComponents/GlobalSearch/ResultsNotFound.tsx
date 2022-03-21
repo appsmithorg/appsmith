97ce831e43cd719913ca77cd839ddd91748e2400
arunvjn <arun@appsmith.com>
2022-03-21 13:06:02 +0530

    Temp commit need to add icon

--- app/client/src/components/editorComponents/GlobalSearch/ResultsNotFound.tsx
+++ app/client/src/components/editorComponents/GlobalSearch/ResultsNotFound.tsx
@@ -5,6 +5,8 @@ import { NO_SEARCH_DATA_TEXT } from "@appsmith/constants/messages";
 import { getTypographyByKey } from "constants/DefaultTheme";
 import { ReactComponent as DiscordIcon } from "assets/icons/help/discord.svg";
 import AnalyticsUtil from "utils/AnalyticsUtil";
+import { getAppsmithConfigs } from "ce/configs";
+import { Button, Category, Icon, IconPositions } from "components/ads";
 
 const Container = styled.div`
   display: flex;
@@ -33,6 +35,9 @@ const Container = styled.div`
     color: ${(props) => props.theme.colors.globalSearch.searchItemText};
     font-weight: 700;
   }
+  .intercom {
+    margin: 1rem 0;
+  }
 `;
 
 const StyledDiscordIcon = styled(DiscordIcon)`
@@ -42,25 +47,45 @@ const StyledDiscordIcon = styled(DiscordIcon)`
   vertical-align: -7px;
 `;
 
+const { intercomAppID } = getAppsmithConfigs();
+
 function ResultsNotFound() {
   return (
     <Container>
       <img alt="No data" src={NoSearchDataImage} />
       <div className="no-data-title">{NO_SEARCH_DATA_TEXT()}</div>
-      <span className="discord">
-        🤖 Join our{"  "}
-        <span
-          className="discord-link"
+      {(intercomAppID && window.Intercom) || true ? (
+        <Button
+          category={Category.tertiary}
+          className="intercom"
+          height="40"
+          iconPosition={IconPositions.left}
           onClick={() => {
-            window.open("https://discord.gg/rBTTVJp", "_blank");
-            AnalyticsUtil.logEvent("DISCORD_LINK_CLICK");
+            window.Intercom("show");
+            AnalyticsUtil.logEvent("INTERCOM_CLICK", {
+              from: "OMNIBAR",
+            });
           }}
-        >
-          <StyledDiscordIcon color="red" height={22} width={24} />
-          Discord Server
-        </span>{" "}
-        for more help.
-      </span>
+          text="Chat with us"
+          type="button"
+          width="100"
+        />
+      ) : (
+        <span className="discord">
+          🤖 Join our{"  "}
+          <span
+            className="discord-link"
+            onClick={() => {
+              window.open("https://discord.gg/rBTTVJp", "_blank");
+              AnalyticsUtil.logEvent("DISCORD_LINK_CLICK");
+            }}
+          >
+            <StyledDiscordIcon color="red" height={22} width={24} />
+            Discord Server
+          </span>{" "}
+          for more help.
+        </span>
+      )}
     </Container>
   );
 }
