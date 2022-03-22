5ca9bdbe98e1add9617d8f46d573e9b04a55b347
haojin111 <haojin828@outlook.com>
2022-03-22 17:53:14 +0800

    fixed issue of airtable authorize on reconnect modal

--- app/client/src/pages/Editor/gitSync/ReconnectDatasourceModal.tsx
+++ app/client/src/pages/Editor/gitSync/ReconnectDatasourceModal.tsx
@@ -425,7 +425,9 @@ function ReconnectDatasourceModal() {
       <ListItemWrapper
         ds={ds}
         key={ds.id}
-        onClick={onSelectDatasource}
+        onClick={() => {
+          onSelectDatasource(ds);
+        }}
         plugin={{
           name: pluginNames[ds.pluginId],
           image: pluginImages[ds.pluginId],
