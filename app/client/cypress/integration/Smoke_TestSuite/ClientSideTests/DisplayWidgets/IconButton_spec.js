11ae141bb8142fb54e91c63814cafb432154a68b
Arsalan <arsalanyaldram0211@outlook.com>
2022-03-23 12:05:45 +0530

    fix: Added test cases for the Icon Button disabled state.

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/IconButton_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/IconButton_spec.js
@@ -1,23 +1,26 @@
 const dsl = require("../../../../fixtures/iconButtonWidgetsDsl.json");
 const formWidgetsPage = require("../../../../locators/FormWidgets.json");
+const commonlocators = require("../../../../locators/commonlocators.json");
+const widgetsPage = require("../../../../locators/Widgets.json");
+const publishPage = require("../../../../locators/publishWidgetspage.json");
 
 describe("Icon Button Widget Functionality", function() {
   before(() => {
     cy.addDsl(dsl);
   });
 
-  it("check default buttonVariant with isJSConvertible", function() {
+  it.only("1. check default buttonVariant with isJSConvertible", function() {
     cy.openPropertyPane("iconbuttonwidget");
 
     cy.get(formWidgetsPage.toggleButtonVariant).click();
-    cy.get(".t--draggable-iconbuttonwidget button").should(
+    cy.get(widgetsPage.iconWidgetBtn).should(
       "have.css",
       "background-color",
       "rgb(3, 179, 101)",
     );
   });
 
-  it("add space into buttonVariant and validate", function() {
+  it("2. add space into buttonVariant and validate", function() {
     cy.get(".t--property-control-buttonvariant .CodeMirror  textarea")
       .first()
       .focus()
@@ -35,10 +38,38 @@ describe("Icon Button Widget Functionality", function() {
       "PRIMARY   ",
     );
 
-    cy.get(".t--draggable-iconbuttonwidget button").should(
+    cy.get(widgetsPage.iconWidgetBtn).should(
       "have.css",
       "background-color",
       "rgb(3, 179, 101)",
     );
+  });
+
+  it.only("3. show alert on button click", function() {
+    cy.get(".t--property-control-onclick")
+      .find(".t--js-toggle")
+      .click({ force: true });
+
+    cy.testJsontext(
+      "onclick",
+      "{{showAlert('Icon Button Clicked','success')}}",
+    );
+
+    cy.get(widgetsPage.iconWidgetBtn).click({ force: true });
+    cy.get(commonlocators.toastmsg).contains("Icon Button Clicked");
+    cy.PublishtheApp();
+    cy.get(publishPage.iconWidgetBtn).click({ force: true });
+    cy.get(commonlocators.toastmsg).contains("Icon Button Clicked");
+    cy.goToEditFromPublish();
+  });
+
+  it.only("4. should not show alert onclick if button is disabled", function() {
+    cy.openPropertyPane("iconbuttonwidget");
+    cy.CheckWidgetProperties(commonlocators.disableCheckbox);
+    cy.get(widgetsPage.iconWidgetBtn).click({ force: true });
+    cy.get(commonlocators.toastmsg).should("not.exist");
+    cy.PublishtheApp();
+    cy.get(publishPage.iconWidgetBtn).click({ force: true });
+    cy.get(commonlocators.toastmsg).should("not.exist");
   });
 });
