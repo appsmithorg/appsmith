d2833399506da3502904eff7a66b31393760175e
apple <nandan@thinkify.io>
2022-03-25 13:10:05 +0530

    Enhancement tests for captcha

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/BindButton_Text_WithRecaptcha_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/BindButton_Text_WithRecaptcha_spec.js
@@ -11,14 +11,20 @@ describe("Binding the Button widget with Text widget using Recpatcha v3", functi
     cy.addDsl(dsl);
   });
 
-  it("Validate the Button binding with Text Widget with Recaptcha Token", function() {
+  it("Validate the Button binding with Text Widget with Recaptcha Token with v2Key", function() {
     cy.get("button")
       .contains("Submit")
       .should("be.visible")
       .click({ force: true });
+    cy.testCodeMirrorLast(testdata.v2Key);
     cy.SearchEntityandOpen("Text1");
-    cy.get(".t--draggable-textwidget .bp3-ui-text").should("be.visible");
-    cy.get(".t--draggable-textwidget .bp3-ui-text").should("have.value", "");
+    cy.get(".t--draggable-textwidget span")
+      .last()
+      .invoke("text")
+      .then((x) => {
+        cy.log(x);
+        expect(x).to.be.empty;
+      });
     cy.SearchEntityandOpen("Button1");
     cy.get(".t--property-control-googlerecaptchaversion .bp3-popover-target")
       .last()
@@ -32,7 +38,50 @@ describe("Binding the Button widget with Text widget using Recpatcha v3", functi
       .should("be.visible")
       .click({ force: true });
     cy.SearchEntityandOpen("Text1");
-    cy.get(".t--draggable-textwidget .bp3-ui-text").should("be.visible");
-    cy.get(".t--draggable-textwidget .bp3-ui-text").should("have.value", "");
+    cy.wait(3000);
+    cy.get(".t--draggable-textwidget span")
+      .last()
+      .invoke("text")
+      .then((x) => {
+        cy.log(x);
+        expect(x).not.to.be.empty;
+      });
+  });
+
+  it("Validate the Button binding with Text Widget with Recaptcha Token with v3Key", function() {
+    cy.get("button")
+      .contains("Submit")
+      .should("be.visible")
+      .click({ force: true });
+    cy.testCodeMirrorLast(testdata.v3Key);
+    cy.SearchEntityandOpen("Text1");
+    cy.get(".t--draggable-textwidget span")
+      .last()
+      .invoke("text")
+      .then((x) => {
+        cy.log(x);
+        expect(x).not.to.be.empty;
+      });
+    cy.SearchEntityandOpen("Button1");
+    cy.get(".t--property-control-googlerecaptchaversion .bp3-popover-target")
+      .last()
+      .should("be.visible")
+      .click({ force: true });
+    cy.get(".t--dropdown-option:contains('reCAPTCHA v2')").click({
+      force: true,
+    });
+    cy.get("button")
+      .contains("Submit")
+      .should("be.visible")
+      .click({ force: true });
+    cy.SearchEntityandOpen("Text1");
+    cy.wait(3000);
+    cy.get(".t--draggable-textwidget span")
+      .last()
+      .invoke("text")
+      .then((x) => {
+        cy.log(x);
+        expect(x).not.to.be.empty;
+      });
   });
 });
