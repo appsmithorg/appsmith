e123a5f8dcb720b55c1d58a25ffc61be3b46d4ec
Aishwarya UR <aishwarya@appsmith.com>
2022-03-26 12:18:02 +0530

    API context menu flake fix

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ApiPaneTests/API_ContextMenu_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ApiPaneTests/API_ContextMenu_spec.js
@@ -9,13 +9,13 @@ const helper = new AggregateHelper();
 
 describe("API Panel Test Functionality ", function() {
   it("Test API copy/Move/delete feature", function() {
-    cy.log("Login Successful");
     cy.Createpage("SecondPage");
     cy.NavigateToAPI_Panel();
-    cy.log("Navigation to API Panel screen successful");
     cy.CreateAPI("FirstAPI");
     cy.enterDatasourceAndPath(testdata.baseUrl, "{{ '/random' }}");
-    cy.log("Creation of FirstAPI Action successful");
+    cy.assertPageSave();
+    cy.get("body").click(0, 0);
+    helper.expandCollapseEntity("QUERIES/JS");
     helper.ActionContextMenuByEntityName(
       "FirstAPI",
       "Copy to page",
