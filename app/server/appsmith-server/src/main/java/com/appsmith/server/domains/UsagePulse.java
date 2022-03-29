e74780aeb3b4bd4521a9256b5bdc67d859df3564
Shrikant Sharat Kandula <shrikant@appsmith.com>
2022-03-29 17:35:54 +0530

    Add UsagePulse API, create-only
    Signed-off-by: Shrikant Sharat Kandula <shrikant@appsmith.com>

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/UsagePulse.java
@@ -0,0 +1,21 @@
+package com.appsmith.server.domains;
+
+import com.appsmith.external.models.BaseDomain;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Document
+public class UsagePulse extends BaseDomain {
+
+    private String email;
+
+    private Instant time;
+
+}
---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/UsagePulseServiceCEImpl.java
@@ -0,0 +1,25 @@
+package com.appsmith.server.services.ce;
+
+import com.appsmith.server.domains.UsagePulse;
+import com.appsmith.server.repositories.ce.UsagePulseRepositoryCE;
+import com.appsmith.server.services.SessionUserService;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+
+@RequiredArgsConstructor
+public class UsagePulseServiceCEImpl implements UsagePulseServiceCE {
+
+    private final UsagePulseRepositoryCE repository;
+
+    private final SessionUserService sessionUserService;
+
+    @Override
+    public Mono<Void> createPulse() {
+        return sessionUserService.getCurrentUser()
+                .flatMap(user -> repository.save(new UsagePulse(user.getEmail(), Instant.now())))
+                .then();
+    }
+
+}
