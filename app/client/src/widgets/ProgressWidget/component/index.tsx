efbba7c0c42d286062cbc5b1894565bb32252fda
Paul Li <wmdev0808@gmail.com>
2022-03-30 06:53:28 +0800

    feat: Progress bar widget enhancements
    -- Show result without rounding for the circular type

--- app/client/src/widgets/ProgressWidget/component/index.tsx
+++ app/client/src/widgets/ProgressWidget/component/index.tsx
@@ -418,7 +418,7 @@ function CircularProgress(props: ProgressComponentProps) {
               x={VIEWBOX_CENTER_X}
               y={VIEWBOX_CENTER_Y}
             >
-              {`${Math.round((value / MAX_VALUE) * 100)}%`}
+              {`${(value / MAX_VALUE) * 100}%`}
             </Label>
           )}
         </>
