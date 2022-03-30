faba13dcaa08fdc00319db0edb56403ff567f6ad
bhavin <techbhavin@gmail.com>
2022-03-30 20:41:26 +0530

    fix: handle duplicate tab name

--- app/client/src/components/propertyControls/TabControl.tsx
+++ app/client/src/components/propertyControls/TabControl.tsx
@@ -8,7 +8,7 @@ import {
   DroppableComponent,
   RenderComponentProps,
 } from "components/ads/DraggableListComponent";
-import { getNextEntityName, noop } from "utils/AppsmithUtils";
+import { getNextEntityName } from "utils/AppsmithUtils";
 import _, { orderBy } from "lodash";
 import * as Sentry from "@sentry/react";
 import { Category, Size } from "components/ads/Button";
@@ -48,6 +48,7 @@ function AddTabButtonComponent({ widgetId }: any) {
     <StyledPropertyPaneButtonWrapper>
       <StyledPropertyPaneButton
         category={Category.tertiary}
+        className="t--add-tab-btn"
         icon="plus"
         onClick={addOption}
         size={Size.medium}
@@ -67,6 +68,7 @@ function TabControlComponent(props: RenderComponentProps<DroppableItem>) {
       type: ReduxActionTypes.WIDGET_DELETE_TAB_CHILD,
       payload: { ...item, index },
     });
+    if (props.deleteOption) props.deleteOption(index);
   };
 
   return (
@@ -81,6 +83,7 @@ function TabControlComponent(props: RenderComponentProps<DroppableItem>) {
 
 type State = {
   focusedIndex: number | null;
+  duplicateTabIds: string[];
 };
 
 class TabControl extends BaseControl<ControlProps, State> {
@@ -89,8 +92,27 @@ class TabControl extends BaseControl<ControlProps, State> {
 
     this.state = {
       focusedIndex: null,
+      duplicateTabIds: this.getDuplicateTabIds(props.propertyValue),
     };
   }
+
+  getDuplicateTabIds = (propertyValue: ControlProps["propertyValue"]) => {
+    const duplicateTabIds = [];
+    const tabIds = Object.keys(propertyValue);
+    const tabNames = _.map(propertyValue, "label");
+
+    for (let index = 0; index < tabNames.length; index++) {
+      const currLabel = tabNames[index] as string;
+      const duplicateValueIndex = tabNames.indexOf(currLabel);
+      if (duplicateValueIndex !== index) {
+        // get tab id from propertyValue index
+        duplicateTabIds.push(propertyValue[tabIds[index]].id);
+      }
+    }
+
+    return duplicateTabIds;
+  };
+
   componentDidMount() {
     this.migrateTabData(this.props.propertyValue);
   }
@@ -159,14 +181,17 @@ class TabControl extends BaseControl<ControlProps, State> {
     });
   };
   render() {
-    const tabs: DroppableItem[] = _.isString(this.props.propertyValue)
+    let tabs: DroppableItem[] = _.isString(this.props.propertyValue)
       ? []
       : Object.values(this.props.propertyValue);
-
+    tabs = tabs.map((tab: DroppableItem) => ({
+      ...tab,
+      isDuplicateLabel: _.includes(this.state.duplicateTabIds, tab.id),
+    }));
     return (
       <TabsWrapper>
         <DroppableComponent
-          deleteOption={noop}
+          deleteOption={this.deleteOption}
           fixedHeight={370}
           focusedIndex={this.state.focusedIndex}
           itemHeight={45}
@@ -205,6 +230,15 @@ class TabControl extends BaseControl<ControlProps, State> {
     this.updateProperty(this.props.propertyName, updatedTabs);
   };
 
+  deleteOption = (index: number) => {
+    const tabIds = Object.keys(this.props.propertyValue);
+    const newPropertyValue = { ...this.props.propertyValue };
+    // detele current item from propertyValue
+    delete newPropertyValue[tabIds[index]];
+    const duplicateTabIds = this.getDuplicateTabIds(newPropertyValue);
+    this.setState({ duplicateTabIds });
+  };
+
   updateOption = (index: number, updatedLabel: string) => {
     const tabsArray: any = Object.values(this.props.propertyValue);
     const { id: itemId } = tabsArray[index];
@@ -212,6 +246,17 @@ class TabControl extends BaseControl<ControlProps, State> {
       `${this.props.propertyName}.${itemId}.label`,
       updatedLabel,
     );
+    // check entered label is unique or duplicate
+    const tabNames = _.map(tabsArray, "label");
+    let duplicateTabIds = [...this.state.duplicateTabIds];
+    // if duplicate, add into array
+    if (_.includes(tabNames, updatedLabel)) {
+      duplicateTabIds.push(itemId);
+      this.setState({ duplicateTabIds });
+    } else {
+      duplicateTabIds = duplicateTabIds.filter((id) => id !== itemId);
+      this.setState({ duplicateTabIds });
+    }
   };
 
   addOption = () => {
