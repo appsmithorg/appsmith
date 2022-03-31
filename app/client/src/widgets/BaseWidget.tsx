f02aaef020085e3f14673978a08aff325ca25d21
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2022-03-31 12:26:54 +0530

    add comment

--- app/client/src/widgets/BaseWidget.tsx
+++ app/client/src/widgets/BaseWidget.tsx
@@ -75,6 +75,16 @@ abstract class BaseWidget<
     return {};
   }
 
+  /**
+   * getLoadingProperties returns a list of regexp's used to specify bindingPaths,
+   * which can set the isLoading prop of the widget.
+   * When:
+   * 1. the path is bound to an action (API/Query)
+   * 2. the action is currently in-progress
+   *
+   * if undefined, all paths can set the isLoading state
+   * if empty array, no paths can set the isLoading state
+   */
   static getLoadingProperties(): Array<RegExp> | undefined {
     return;
   }
