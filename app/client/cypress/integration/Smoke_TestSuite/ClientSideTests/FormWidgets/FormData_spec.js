6fe6a22cbeed99cc128a4e4ce5dafbd6f67b32e0
Paul Li <wmdev0808@gmail.com>
2022-04-05 03:00:39 +0800

    fix: Form widget should include the following widgets in data property when added in form
    -- Add InputWidgetV2 and CurrencyInputWidget

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/FormData_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/FormData_spec.js
@@ -5,7 +5,7 @@ describe("Form data", function() {
     cy.addDsl(dsl);
   });
 
-  it("CheckboxGroupWidget, MultiSelectTreeWidget, MultiSelectWidgetV2, SingleSelectTreeWidget, SwitchGroupWidget and PhoneInputWidget should have value props of which values are not null or undefined to be included into form data", function() {
+  it("CheckboxGroupWidget, MultiSelectTreeWidget, MultiSelectWidgetV2, SingleSelectTreeWidget, SwitchGroupWidget, PhoneInputWidget, InputWidgetV2 and CurrencyInputWidget should have value props of which values are not null or undefined to be included as a form data", function() {
     // Check form data
     cy.get("[data-testid='container-wrapper-vannrar7rd'] span", {
       timeout: 25000,
@@ -18,6 +18,8 @@ describe("Form data", function() {
         expect($formData).to.contain("TreeSelect");
         expect($formData).to.contain("SwitchGroup");
         expect($formData).to.contain("PhoneInput");
+        expect($formData).to.contain("Input");
+        expect($formData).to.contain("CurrencyInput");
       });
   });
 });
