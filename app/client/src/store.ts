b729d842dbb64355035e99e103ba68f03f2737f6
arunvjn <32433245+arunvjn@users.noreply.github.com>
2022-04-04 20:55:03 +0530

    fix: page launch error from dashboard (#12557)

--- app/client/src/store.ts
+++ app/client/src/store.ts
@@ -48,17 +48,23 @@ const routeParamsMiddleware: Middleware = () => (next: any) => (
       });
       break;
     }
-    case ReduxActionTypes.SWITCH_CURRENT_PAGE_ID:
+    case ReduxActionTypes.SWITCH_CURRENT_PAGE_ID: {
+      const id = action.payload.id;
+      const slug = action.payload.slug;
+      updateURLFactory({ pageId: id, pageSlug: slug });
+      break;
+    }
     case ReduxActionTypes.UPDATE_PAGE_SUCCESS: {
       const id = action.payload.id;
       const slug = action.payload.slug;
       const { pageId } = getRouteBuilderParams();
-      // Update page slug in URL only if the current page is renamed
-      if (pageId === id)
+      // Update route params and page slug in URL only if the current page is updated
+      if (pageId === id) {
+        updateURLFactory({ pageSlug: slug });
         updateSlugNamesInURL({
           pageSlug: slug,
         });
-      updateURLFactory({ pageId: id, pageSlug: slug });
+      }
       break;
     }
     case ReduxActionTypes.UPDATE_APPLICATION_SUCCESS:
