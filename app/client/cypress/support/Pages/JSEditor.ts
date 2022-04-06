2591ecaf5d7f61808175f3ed8246a4c9b327fadb
ohansFavour <fohanekwu@gmail.com>
2022-04-06 08:18:28 +0100

    Add cypress tests

--- app/client/cypress/support/Pages/JSEditor.ts
+++ app/client/cypress/support/Pages/JSEditor.ts
@@ -1,17 +1,15 @@
-import { ObjectsRegistry } from "../Objects/Registry"
+import { ObjectsRegistry } from "../Objects/Registry";
 export class JSEditor {
-
-  public agHelper = ObjectsRegistry.AggregateHelper
+  public agHelper = ObjectsRegistry.AggregateHelper;
   public locator = ObjectsRegistry.CommonLocators;
   public ee = ObjectsRegistry.EntityExplorer;
 
   private _runButton = "//li//*[local-name() = 'svg' and @class='run-button']";
   private _jsObjName = ".t--js-action-name-edit-field span";
   private _jsObjTxt = ".t--js-action-name-edit-field input";
-  private _newJSobj = "span:contains('New JS Object')"
-  private _bindingsClose = ".t--entity-property-close"
-  private _propertyList = ".t--entity-property"
-
+  private _newJSobj = "span:contains('New JS Object')";
+  private _bindingsClose = ".t--entity-property-close";
+  private _propertyList = ".t--entity-property";
 
   public NavigateToJSEditor() {
     cy.get(this.locator._createNew)
@@ -21,24 +19,33 @@ export class JSEditor {
 
     //cy.waitUntil(() => cy.get(this.locator._toastMsg).should('not.be.visible')) // fails sometimes
     //this.agHelper.WaitUntilEleDisappear(this.locator._toastMsg, 'created successfully')
-    this.agHelper.Sleep()
+    this.agHelper.Sleep();
   }
 
-  public CreateJSObject(JSCode: string, paste = true, completeReplace = false, toRun = true) {
+  public CreateJSObject(
+    JSCode: string,
+    paste = true,
+    completeReplace = false,
+    toRun = true,
+  ) {
     this.NavigateToJSEditor();
 
     if (!completeReplace) {
       cy.get(this.locator._codeMirrorTextArea)
         .first()
         .focus()
-        .type("{downarrow}{downarrow}{downarrow}{downarrow}  ")
-    }
-    else {
+        .type("{downarrow}{downarrow}{downarrow}{downarrow}  ");
+    } else {
       cy.get(this.locator._codeMirrorTextArea)
         .first()
         .focus()
-        .type("{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}")
-        .type("{shift}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}", { force: true })
+        .type(
+          "{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}",
+        )
+        .type(
+          "{shift}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}{uparrow}",
+          { force: true },
+        )
         .type("{backspace}", { force: true });
 
       // .type("{uparrow}", { force: true })
@@ -50,7 +57,6 @@ export class JSEditor {
       //   .type("{ctrl}{shift}{downarrow}", { force: true })
       //   .type("{backspace}",{ force: true });
       //.type("{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{downarrow} ")
-
     }
 
     cy.get(this.locator._codeMirrorTextArea)
@@ -59,15 +65,16 @@ export class JSEditor {
         const input = cy.get(el);
         if (paste) {
           //input.invoke("val", value);
-          this.agHelper.Paste(el, JSCode)
+          this.agHelper.Paste(el, JSCode);
         } else {
           input.type(JSCode, {
-            parseSpecialCharSequences: false, delay: 150
+            parseSpecialCharSequences: false,
+            delay: 150,
           });
         }
       });
 
-    this.agHelper.AssertAutoSave()//Ample wait due to open bug # 10284
+    this.agHelper.AssertAutoSave(); //Ample wait due to open bug # 10284
     //this.agHelper.Sleep(5000)//Ample wait due to open bug # 10284
 
     if (toRun) {
@@ -76,22 +83,31 @@ export class JSEditor {
         cy.xpath(this._runButton)
           .first()
           .click()
-          .wait(3000)
-      })
-      cy.get(this.locator._empty).should('not.exist')
-      cy.get(this.locator._toastMsg).should("have.length", 0)
+          .wait(3000);
+      });
+      cy.get(this.locator._empty).should("not.exist");
+      cy.get(this.locator._toastMsg).should("have.length", 0);
     }
-    this.GetJSObjectName()
+    this.GetJSObjectName();
   }
 
   //Not working - To improve!
   public EditJSObj(existingTxt: string, newTxt: string) {
-    cy.get(this.locator._codeEditorTarget).contains(existingTxt).dblclick()//.type("{backspace}").type(newTxt)
-    cy.get('body').type("{backspace}").type(newTxt)
-    this.agHelper.AssertAutoSave()//Ample wait due to open bug # 10284
+    cy.get(this.locator._codeEditorTarget)
+      .contains(existingTxt)
+      .dblclick(); //.type("{backspace}").type(newTxt)
+    cy.get("body")
+      .type("{backspace}")
+      .type(newTxt);
+    this.agHelper.AssertAutoSave(); //Ample wait due to open bug # 10284
   }
 
-  public EnterJSContext(endp: string, value: string, paste = true, toToggleOnJS = false) {
+  public EnterJSContext(
+    endp: string,
+    value: string,
+    paste = true,
+    toToggleOnJS = false,
+  ) {
     if (toToggleOnJS) {
       cy.get(this.locator._jsToggle(endp))
         .invoke("attr", "class")
@@ -103,7 +119,12 @@ export class JSEditor {
           }
         });
     }
-    cy.get(this.locator._propertyControl + endp + " " + this.locator._codeMirrorTextArea)
+    cy.get(
+      this.locator._propertyControl +
+        endp +
+        " " +
+        this.locator._codeMirrorTextArea,
+    )
       .first()
       .focus()
       .type("{uparrow}", { force: true })
@@ -113,7 +134,12 @@ export class JSEditor {
     cy.focused().then(($cm: any) => {
       if ($cm.contents != "") {
         cy.log("The field is not empty");
-        cy.get(this.locator._propertyControl + endp + " " + this.locator._codeMirrorTextArea)
+        cy.get(
+          this.locator._propertyControl +
+            endp +
+            " " +
+            this.locator._codeMirrorTextArea,
+        )
           .first()
           .click({ force: true })
           .focused()
@@ -121,14 +147,19 @@ export class JSEditor {
             force: true,
           });
       }
-      this.agHelper.Sleep()
-      cy.get(this.locator._propertyControl + endp + " " + this.locator._codeMirrorTextArea)
+      this.agHelper.Sleep();
+      cy.get(
+        this.locator._propertyControl +
+          endp +
+          " " +
+          this.locator._codeMirrorTextArea,
+      )
         .first()
         .then((el: any) => {
           const input = cy.get(el);
           if (paste) {
             //input.invoke("val", value);
-            this.agHelper.Paste(el, value)
+            this.agHelper.Paste(el, value);
           } else {
             input.type(value, {
               parseSpecialCharSequences: false,
@@ -136,7 +167,7 @@ export class JSEditor {
           }
         });
     });
-    this.agHelper.AssertAutoSave()//Allowing time for Evaluate value to capture value
+    this.agHelper.AssertAutoSave(); //Allowing time for Evaluate value to capture value
   }
 
   public RenameJSObjFromForm(renameVal: string) {
@@ -159,14 +190,16 @@ export class JSEditor {
   }
 
   public GetJSObjectName() {
-    cy.get(this._jsObjName).invoke("text").then((text) => cy.wrap(text).as("jsObjName")
-    );
+    cy.get(this._jsObjName)
+      .invoke("text")
+      .then((text) => cy.wrap(text).as("jsObjName"));
   }
 
   public validateDefaultJSObjProperties(jsObjName: string) {
     this.ee.ActionContextMenuByEntityName(jsObjName, "Show Bindings");
-    cy.get(this._propertyList).then(function ($lis) {
-      expect($lis).to.have.length(5);
+    cy.get(this._propertyList).then(function($lis) {
+      const bindingsLength = $lis.length;
+      expect(bindingsLength).to.be.at.least(4);
       expect($lis.eq(0).text()).to.be.oneOf([
         "{{" + jsObjName + ".myFun2()}}",
         "{{" + jsObjName + ".myFun1()}}",
@@ -174,11 +207,16 @@ export class JSEditor {
       expect($lis.eq(1).text()).to.be.oneOf([
         "{{" + jsObjName + ".myFun2()}}",
         "{{" + jsObjName + ".myFun1()}}",
+        "{{" + jsObjName + ".myFun2.data}}",
+        "{{" + jsObjName + ".myFun1.data}}",
       ]);
-      expect($lis.eq(2).text()).to.contain("{{" + jsObjName + ".myVar1}}");
-      expect($lis.eq(3).text()).to.contain("{{" + jsObjName + ".myVar2}}");
+      expect($lis.eq(bindingsLength - 2).text()).to.contain(
+        "{{" + jsObjName + ".myVar1}}",
+      );
+      expect($lis.eq(bindingsLength - 1).text()).to.contain(
+        "{{" + jsObjName + ".myVar2}}",
+      );
     });
     cy.get(this._bindingsClose).click({ force: true });
   }
-
 }
