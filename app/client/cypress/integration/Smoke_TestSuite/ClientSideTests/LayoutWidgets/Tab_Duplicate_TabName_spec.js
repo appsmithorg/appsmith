f7f0a02411f39f345e2059797a5c65d453c16ddc
bhavin <techbhavin@gmail.com>
2022-04-06 16:27:50 +0530

    test: updated test case

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/LayoutWidgets/Tab_Duplicate_TabName_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/LayoutWidgets/Tab_Duplicate_TabName_spec.js
@@ -13,8 +13,6 @@ describe("Tab widget test duplicate tab name validation", function() {
     cy.tabPopertyUpdate("tab4", "TestUpdated");
     cy.get(".t--has-duplicate-label-3").should("exist");
     cy.get(".t--has-duplicate-label-4").should("not.exist");
-    cy.tabPopertyUpdate("tab6", "Tab 5");
-    cy.get(".t--has-duplicate-label-5").should("exist");
 
     // detele column and re-validate duplicate column
     cy.deleteColumn("tab2");
