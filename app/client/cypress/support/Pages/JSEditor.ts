3424002d7b5596e5644abd323fa2f48fd76176b0
Aishwarya-U-R <91450662+Aishwarya-U-R@users.noreply.github.com>
2022-04-07 15:38:26 +0530

    JSOnPageLoad pop-up text msg fix (#12671)

--- app/client/cypress/support/Pages/JSEditor.ts
+++ app/client/cypress/support/Pages/JSEditor.ts
@@ -12,7 +12,7 @@ export class JSEditor {
   private _bindingsClose = ".t--entity-property-close"
   private _propertyList = ".t--entity-property"
   private _responseTabAction = (funName: string) => "//div[@class='function-name'][text()='" + funName + "']/following-sibling::div//*[local-name()='svg']"
-  private _functionSetting = (settingTxt: string) => "//span[text()='" + settingTxt + "']/parent::div/following-sibling::input[@type='checkbox']"
+  private _functionSetting = (settingTxt: string) => "//span[contains(text(),'" + settingTxt + "')]/parent::div/following-sibling::input[@type='checkbox']"
   _dialog = (dialogHeader: string) => "//div[contains(@class, 'bp3-dialog')]//h4[contains(text(), '" + dialogHeader + "')]"
   private _closeSettings = "span[icon='small-cross']"
 
