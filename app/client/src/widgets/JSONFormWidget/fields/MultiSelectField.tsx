765582cb77754bc91b0f2bd40854885639135047
Ashit Rath <ashit@appsmith.com>
2022-04-08 13:22:54 +0530

    fixed merge-conflict missing import

--- app/client/src/widgets/JSONFormWidget/fields/MultiSelectField.tsx
+++ app/client/src/widgets/JSONFormWidget/fields/MultiSelectField.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useMemo } from "react";
+import React, { useCallback, useContext, useMemo, useRef } from "react";
 import styled from "styled-components";
 import {
   DefaultValueType,
