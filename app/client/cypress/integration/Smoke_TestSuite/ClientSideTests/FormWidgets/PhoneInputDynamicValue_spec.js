2373f3bfa96e5ca8cd56362dee00fd0e6ccbed5d
balajisoundar <balaji@appsmith.com>
2022-04-08 19:55:13 +0530

    more cypress

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/PhoneInputDynamicValue_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/PhoneInputDynamicValue_spec.js
@@ -1,11 +1,33 @@
 const dynamicDSL = require("../../../../fixtures/PhoneInputDynamic.json");
 
+const widgetName = "phoneinputwidget";
+
 describe("Phone input widget - ", () => {
   before(() => {
     cy.addDsl(dynamicDSL);
   });
 
   it("should check that widget can be used with dynamic default dial code", () => {
+    cy.openPropertyPane(widgetName);
+    cy.get(".t--property-control-defaultcountrycode .CodeMirror-code").should(
+      "contain",
+      "{{appsmith.store.test}}",
+    );
+    cy.get(".t--property-control-allowcountrycodechange label")
+      .last()
+      .click({ force: true });
+    cy.get(".t--input-country-code-change")
+      .first()
+      .click();
+    cy.get(".t--search-input input").type("india");
+    cy.wait(500);
+    cy.get(".t--dropdown-option")
+      .last()
+      .click();
+    cy.get(".t--property-control-defaultcountrycode .CodeMirror-code").should(
+      "contain",
+      "{{appsmith.store.test}}",
+    );
     cy.PublishtheApp();
     cy.get(".bp3-button.select-button").click({ force: true });
     cy.get(".menu-item-text")
