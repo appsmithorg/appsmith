b4b529d5cb828b24422bbfe8e7e42fb99d6c82a8
arunvjn <arun@appsmith.com>
2022-04-10 10:22:43 +0530

    fetch published page asap

--- app/client/src/sagas/InitSagas.ts
+++ app/client/src/sagas/InitSagas.ts
@@ -400,53 +400,30 @@ export function* initializeAppViewerSaga(
   );
   yield put({ type: ReduxActionTypes.START_EVALUATION });
 
-  const resultOfPrimaryCalls: boolean = yield failFastApiCalls(
-    [
-      fetchActionsForView({ applicationId }),
-      fetchJSCollectionsForView({ applicationId }),
-    ],
-    [
-      ReduxActionTypes.FETCH_ACTIONS_VIEW_MODE_SUCCESS,
-      ReduxActionTypes.FETCH_JS_ACTIONS_VIEW_MODE_SUCCESS,
-    ],
-    [
-      ReduxActionErrorTypes.FETCH_ACTIONS_VIEW_MODE_ERROR,
-      ReduxActionErrorTypes.FETCH_JS_ACTIONS_VIEW_MODE_ERROR,
-    ],
-  );
-
-  if (!resultOfPrimaryCalls) return;
-
   const defaultPageId: string = yield select(getDefaultPageId);
   const toLoadPageId: string = pageId || defaultPageId;
 
   yield call(initiateURLUpdate, toLoadPageId, APP_MODE.PUBLISHED, pageId);
 
-  if (toLoadPageId) {
-    yield put(fetchPublishedPage(toLoadPageId, true));
+  const resultOfPrimaryCalls: boolean = yield failFastApiCalls(
+    [
+      fetchActionsForView({ applicationId }),
+      fetchJSCollectionsForView({ applicationId }),
+      fetchPublishedPage(toLoadPageId, true),
+    ],
+    [
+      ReduxActionTypes.FETCH_ACTIONS_VIEW_MODE_SUCCESS,
+      ReduxActionTypes.FETCH_JS_ACTIONS_VIEW_MODE_SUCCESS,
+      ReduxActionTypes.FETCH_PUBLISHED_PAGE_SUCCESS,
+    ],
+    [
+      ReduxActionErrorTypes.FETCH_ACTIONS_VIEW_MODE_ERROR,
+      ReduxActionErrorTypes.FETCH_JS_ACTIONS_VIEW_MODE_ERROR,
+      ReduxActionErrorTypes.FETCH_PUBLISHED_PAGE_ERROR,
+    ],
+  );
 
-    const resultOfFetchPage: {
-      success: boolean;
-      failure: boolean;
-    } = yield race({
-      success: take(ReduxActionTypes.FETCH_PUBLISHED_PAGE_SUCCESS),
-      failure: take(ReduxActionErrorTypes.FETCH_PUBLISHED_PAGE_ERROR),
-    });
-
-    if (resultOfFetchPage.failure) {
-      yield put({
-        type: ReduxActionTypes.SAFE_CRASH_APPSMITH_REQUEST,
-        payload: {
-          code: get(
-            resultOfFetchPage,
-            "failure.payload.error.code",
-            ERROR_CODES.SERVER_ERROR,
-          ),
-        },
-      });
-      return;
-    }
-  }
+  if (!resultOfPrimaryCalls) return;
 
   yield put(fetchCommentThreadsInit());
 
