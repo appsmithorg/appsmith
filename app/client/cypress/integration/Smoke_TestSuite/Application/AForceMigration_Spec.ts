38cadd6ae6c116018c98e72f9667a9af7ec076af
Aishwarya UR <aishwarya@appsmith.com>
2022-04-11 06:43:35 +0530

    Aforce - filter spec fix

--- app/client/cypress/integration/Smoke_TestSuite/Application/AForceMigration_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/Application/AForceMigration_Spec.ts
@@ -187,17 +187,17 @@ describe("AForce - Community Issues page validations", function () {
 
     //One filter
     table.FilterTable("Type", "is exactly", "Bug")
-    table.ReadTableRowColumnData(0, 1, 2000).then(($cellData) => {
-      expect($cellData).to.eq("[Bug]:  Postgres queries unable to execute with more than 9 placeholders");
+    table.ReadTableRowColumnData(0, 1).then(($cellData) => {
+      expect($cellData).to.eq("[Bug]: Postgres queries unable to execute with more than 9 placeholders");
     });
-    table.ReadTableRowColumnData(4, 1).then(($cellData) => {
-      expect($cellData).to.eq("[Bug] App header overlaps the Table filter pane when table is placed at top of the canvas ");
+    table.ReadTableRowColumnData(2, 1).then(($cellData) => {
+      expect($cellData).to.eq("[Bug]: Input updates with default values are not captured");
     });
     table.RemoveFilterNVerify("Question", true, false)
 
     //Two filters - OR
     table.FilterTable("Type", "starts with", "Trouble")
-    table.ReadTableRowColumnData(0, 0, 2000).then(($cellData) => {
+    table.ReadTableRowColumnData(0, 0).then(($cellData) => {
       expect($cellData).to.eq("Troubleshooting");
     });
     table.ReadTableRowColumnData(0, 1).then(($cellData) => {
@@ -205,22 +205,22 @@ describe("AForce - Community Issues page validations", function () {
     });
 
     table.FilterTable("Title", "contains", "query", 'OR', 1)
-    table.ReadTableRowColumnData(0, 0, 2000).then(($cellData) => {
+    table.ReadTableRowColumnData(1, 0).then(($cellData) => {
       expect($cellData).to.eq("Question");
     });
-    table.ReadTableRowColumnData(5, 1).then(($cellData) => {
+    table.ReadTableRowColumnData(7, 1).then(($cellData) => {
       expect($cellData).to.eq("Run storeValue commands before a Query.run()");
     });
     table.RemoveFilterNVerify("Question", true, false)
 
      //Two filters - AND
      table.FilterTable("Votes", "greater than", "3")
-     table.ReadTableRowColumnData(0, 1, 2000).then(($cellData) => {
+     table.ReadTableRowColumnData(1, 1).then(($cellData) => {
        expect($cellData).to.eq("Combine queries from different datasources");
      });
  
      table.FilterTable("Title", "contains", "button", 'AND', 1)
-     table.ReadTableRowColumnData(0, 1, 2000).then(($cellData) => {
+     table.ReadTableRowColumnData(0, 1).then(($cellData) => {
        expect($cellData).to.eq("Change the video in the video player with a button click");
      });
      table.RemoveFilterNVerify("Question", true, false)
