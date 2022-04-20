40dc5ea0090542fea4be067a028aed0805f96021
Ankur Singhal <ankur@appsmith.com>
2022-04-20 14:21:24 +0530

    made the Switch group component compatible to use ref from base widget

--- app/client/src/widgets/SwitchGroupWidget/component/index.tsx
+++ app/client/src/widgets/SwitchGroupWidget/component/index.tsx
@@ -50,7 +50,10 @@ export interface OptionProps {
   value: string;
 }
 
-function SwitchGroupComponent(props: SwitchGroupComponentProps) {
+const SwitchGroupComponent = React.forwardRef<
+  HTMLDivElement,
+  React.PropsWithChildren<SwitchGroupComponentProps>
+>((props, ref) => {
   const {
     alignment,
     compactMode,
@@ -77,6 +80,7 @@ function SwitchGroupComponent(props: SwitchGroupComponentProps) {
       compactMode={compactMode}
       data-testid="switchgroup-container"
       labelPosition={labelPosition}
+      ref={ref}
     >
       {labelText && (
         <LabelWithTooltip
@@ -119,7 +123,7 @@ function SwitchGroupComponent(props: SwitchGroupComponentProps) {
       </InputContainer>
     </SwitchGroupContainer>
   );
-}
+});
 
 export interface SwitchGroupComponentProps {
   alignment: Alignment;
@@ -141,5 +145,7 @@ export interface SwitchGroupComponentProps {
   widgetId: string;
   height: number;
 }
+
+SwitchGroupComponent.displayName = "SwitchGroupComponent";
 
 export default SwitchGroupComponent;
