4887e9374f08f68ae308cc70f5ba1b35f5eae4ee
balajisoundar <balaji@appsmith.com>
2022-04-22 10:14:13 +0530

    wip

--- app/client/src/widgets/WidgetUtils.ts
+++ app/client/src/widgets/WidgetUtils.ts
@@ -547,7 +547,7 @@ export const parseSchemaItem = (
  * - Often times we would wanna call more than one hook when a property is
  *   changed. Use this hook instead of nested calls
  *
- * Eack hook either returns `undefined` or an array of {propertyPath, propertyValue}
+ * Eack hook should either return `undefined` or an array of {propertyPath, propertyValue}
  * this function ignores the undefined and concats all the property update array.
  */
 export function composePropertyUpdateHook(
@@ -585,7 +585,7 @@ export function composePropertyUpdateHook(
         }
       });
 
-      return updates;
+      return updates.length ? updates : undefined;
     } else {
       return undefined;
     }
