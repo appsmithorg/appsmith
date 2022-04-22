c4ee44de150330bbe510f3786cd421e6c21a266f
Nayan <nayan@appsmith.com>
2022-04-22 11:40:32 +0600

    feat: log template app name to analytics service when template is forked (#12972)
    Logs application template name to analytics service when template is forked.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/ApplicationTemplateServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/ApplicationTemplateServiceCEImpl.java
@@ -24,6 +24,8 @@ import reactor.core.publisher.Mono;
 
 import java.lang.reflect.Type;
 import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ApplicationTemplateServiceCEImpl implements ApplicationTemplateServiceCE {
@@ -133,7 +135,9 @@ public class ApplicationTemplateServiceCEImpl implements ApplicationTemplateServ
         ).flatMap(application -> {
             ApplicationTemplate applicationTemplate = new ApplicationTemplate();
             applicationTemplate.setId(templateId);
-            return analyticsService.sendObjectEvent(AnalyticsEvents.FORK, applicationTemplate, null)
+            Map<String, Object>  extraProperties = new HashMap<>();
+            extraProperties.put("templateAppName", application.getName());
+            return analyticsService.sendObjectEvent(AnalyticsEvents.FORK, applicationTemplate, extraProperties)
                     .thenReturn(application);
         });
     }
