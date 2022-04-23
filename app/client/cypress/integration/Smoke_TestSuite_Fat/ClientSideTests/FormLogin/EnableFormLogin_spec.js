bb62c0169600ffd48244414d060054fc5476ac2c
haojin111 <haojin828@outlook.com>
2022-04-24 01:27:51 +0800

    resotre setting after checked

--- app/client/cypress/integration/Smoke_TestSuite_Fat/ClientSideTests/FormLogin/EnableFormLogin_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite_Fat/ClientSideTests/FormLogin/EnableFormLogin_spec.js
@@ -7,14 +7,7 @@ describe("Form Login test functionality", function() {
     cy.LogOut();
     cy.LoginFromAPI(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
     cy.visit("/applications");
-    cy.get(".t--profile-menu-icon").should("be.visible");
-    cy.get(".t--profile-menu-icon").click();
-    cy.get(".t--admin-settings-menu").should("be.visible");
-    cy.get(".t--admin-settings-menu").click();
-    cy.url().should("contain", "/settings/general");
-    // click authentication tab
-    cy.get(adminSettings.authenticationTab).click();
-    cy.url().should("contain", "/settings/authentication");
+    cy.openAuthentication();
     cy.get(adminSettings.formloginButton)
       .should("be.visible")
       .should("contain", "EDIT");
@@ -47,20 +40,29 @@ describe("Form Login test functionality", function() {
         "contain",
         "Signup is restricted on this instance of Appsmith",
       );
+      // restore setting
+      cy.LoginFromAPI(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
+      cy.visit("/applications");
+      cy.get(".t--profile-menu-icon").click();
+      cy.get(".t--admin-settings-menu").click();
+      cy.get(adminSettings.authenticationTab).click();
+      cy.get(adminSettings.formloginButton).click();
+      cy.wait(2000);
+      cy.get(adminSettings.formSignupDisabled).click({ force: true });
+      cy.wait(2000);
+      cy.get(adminSettings.saveButton).click();
+      cy.get(adminSettings.restartNotice).should("be.visible");
+      cy.wait(120000);
+      cy.waitUntil(() => cy.get(homePage.profileMenu).should("be.visible"));
+      cy.get(homePage.profileMenu).click();
+      cy.get(homePage.signOutIcon).click();
     });
   });
   it("2. Go to admin settings and disable Form Login", function() {
     cy.LogOut();
     cy.LoginFromAPI(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
     cy.visit("/applications");
-    cy.get(".t--profile-menu-icon").should("be.visible");
-    cy.get(".t--profile-menu-icon").click();
-    cy.get(".t--admin-settings-menu").should("be.visible");
-    cy.get(".t--admin-settings-menu").click();
-    cy.url().should("contain", "/settings/general");
-    // click authentication tab
-    cy.get(adminSettings.authenticationTab).click();
-    cy.url().should("contain", "/settings/authentication");
+    cy.openAuthentication();
     cy.get(adminSettings.formloginButton)
       .should("be.visible")
       .should("contain", "EDIT");
@@ -73,6 +75,7 @@ describe("Form Login test functionality", function() {
     cy.wait(120000);
     cy.waitUntil(() => cy.get(homePage.profileMenu).should("be.visible"));
     cy.wait(5000);
+    cy.reload();
     cy.get(adminSettings.authenticationTab).click();
     cy.get(adminSettings.formloginButton).click();
     cy.wait(2000);
@@ -88,7 +91,7 @@ describe("Form Login test functionality", function() {
     cy.get(adminSettings.saveButton).click();
     cy.get(adminSettings.restartNotice).should("be.visible");
     // adding wait for server to restart
-    cy.wait(12000);
+    cy.wait(120000);
     cy.waitUntil(() => cy.get(homePage.profileMenu).should("be.visible"));
     cy.get(homePage.profileMenu).click();
     cy.get(homePage.signOutIcon).click();
@@ -97,5 +100,45 @@ describe("Form Login test functionality", function() {
     cy.wait(5000);
     // validating form signup is disabled
     cy.get("form").should("not.exist");
+    cy.get(".t--sign-up").should("not.exist");
+    // cy.wait(30000); // restart nginx docker
+    cy.LoginFromAPI(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
+    cy.visit("/applications");
+    cy.openAuthentication();
+
+    // enable form login
+    cy.get(adminSettings.formloginButton)
+      .should("be.visible")
+      .should("contain", "ENABLE");
+    cy.get(adminSettings.formloginButton).click();
+    cy.get(adminSettings.formLoginDisabled).click({ force: true });
+    cy.wait(2000);
+    // assert server is restarting
+    cy.get(adminSettings.saveButton).should("be.visible");
+    cy.get(adminSettings.saveButton).should("not.be.disabled");
+    cy.get(adminSettings.saveButton).click();
+    cy.get(adminSettings.restartNotice).should("be.visible");
+    // adding wait for server to restart
+    cy.wait(120000);
+    cy.waitUntil(() => cy.get(homePage.profileMenu).should("be.visible"));
+    cy.reload();
+
+    // disable github
+    cy.get(adminSettings.authenticationTab).click();
+    cy.get(adminSettings.githubButton).click();
+    cy.wait(2000);
+    cy.get(adminSettings.disconnectBtn)
+      .click()
+      .should("contain", "Are you sure?");
+    cy.get(adminSettings.disconnectBtn).click();
+    cy.wait(120000);
+    cy.waitUntil(() => cy.get(homePage.profileMenu).should("be.visible"));
+    cy.reload();
+    cy.get(homePage.profileMenu).click();
+    cy.get(homePage.signOutIcon).click();
+    cy.wait(500);
+    cy.get(adminSettings.loginWithGithub).should("not.exist");
+    cy.get("form").should("exist");
+    cy.get(".t--sign-up").should("exist");
   });
 });
