d289be374943425e19e3169a628587f7e1d7e876
Sumesh <sumesh@appsmith.com>
2022-04-27 20:49:17 +0530

    updated health check script with curl cmds

--- deploy/docker/scripts/healthcheck.sh
+++ deploy/docker/scripts/healthcheck.sh
@@ -10,6 +10,32 @@ while read -r line
       echo "ERROR:- PROCESS: $process - STATUS: $status"
     else
       echo "PROCESS: $process - STATUS: $status"
+      if [[ "$process" == 'editor' ]]; then
+        if [[ $(curl -s -w "%{http_code}\n" http://localhost:80/ -o /dev/null) -ne 200 ]]; then
+           echo 'ERROR: Editor is down';
+           healthy=false
+        fi
+      elif [[ "$process" == "server" ]]; then
+        if [[ $(curl -s -w "%{http_code}\n" http://localhost:8080/api/v1/users/me/ -o /dev/null) -ne 200 ]]; then
+           echo 'ERROR: Server is down';
+           healthy=false
+        fi
+      elif [[ "$process" == "rts" ]]; then
+        if [[ $(curl -s -w "%{http_code}\n" http://localhost:8091/ -o /dev/null) -ne 302 ]]; then
+           echo 'ERROR: RTS is down';
+           healthy=false
+        fi
+      elif [[ "$process" == "mongo" ]]; then
+        if [[ $(mongo --eval  'db.runCommand("ping").ok') -ne 1 ]]; then
+            echo 'ERROR: Mongo is down';
+            healthy=false
+        fi
+       elif [[ "$process" == "redis" ]]; then
+        if [[ $(redis-cli ping) != 'PONG' ]]; then
+            echo 'ERROR: Redis is down';
+            healthy=false
+        fi
+      fi
     fi
   done <<< $(supervisorctl status all)
 if [ $healthy == true ]; then
