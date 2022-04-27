17c91c0d511b455fa6e014d8dd8b0a8e9be61658
Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>
2022-04-27 14:53:16 +0530

    fix qa bugs

--- app/client/src/widgets/ButtonGroupWidget/component/index.tsx
+++ app/client/src/widgets/ButtonGroupWidget/component/index.tsx
@@ -128,6 +128,7 @@ const PopoverStyles = createGlobalStyle<{
       background: none;
       box-shadow: 0 6px 20px 0px rgba(0, 0, 0, 0.15) !important;
       margin-top: 8px !important;
+      margin-bottom: 8px !important;
       border-radius: ${borderRadius === "1.5rem" ? `0.375rem` : borderRadius};
       box-shadow: none;
       overflow: hidden;
