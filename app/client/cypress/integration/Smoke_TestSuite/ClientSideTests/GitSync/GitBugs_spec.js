fc7f94b005e4d25b0e64d257aa0cf2bfc585feb7
haojin111 <haojin828@outlook.com>
2022-04-29 15:48:17 +0800

    enabled other test case

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/GitSync/GitBugs_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/GitSync/GitBugs_spec.js
@@ -166,7 +166,7 @@ describe("Git Bug: Fix clone page issue where JSObject are not showing up in des
   });
 });
 describe("Git synced app with JSObject", function() {
-  it.only("Create an app with JSObject, connect it to git and verify its data in edit and deploy mode", function() {
+  it("Create an app with JSObject, connect it to git and verify its data in edit and deploy mode", function() {
     cy.NavigateToHome();
     cy.createOrg();
     cy.wait("@createOrg").then((interception) => {
