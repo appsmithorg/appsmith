15ad40d3b5110abf1aafac130d94411044677bc3
Ankur Singhal <ankur@appsmith.com>
2022-05-02 12:05:18 +0530

    changed the MultiTreeSelect and component to accept ref from the BaseWidget

--- app/client/src/widgets/MultiSelectTreeWidget/component/index.tsx
+++ app/client/src/widgets/MultiSelectTreeWidget/component/index.tsx
@@ -98,203 +98,219 @@ const switcherIcon = (treeNode: TreeNodeProps) => {
 };
 const FOCUS_TIMEOUT = 500;
 
-function MultiTreeSelectComponent({
-  allowClear,
-  compactMode,
-  disabled,
-  dropdownStyle,
-  dropDownWidth,
-  expandAll,
-  filterText,
-  isFilterable,
-  isValid,
-  labelAlignment,
-  labelPosition,
-  labelStyle,
-  labelText,
-  labelTextColor,
-  labelTextSize,
-  labelWidth,
-  loading,
-  mode,
-  onChange,
-  options,
-  placeholder,
-  value,
-  widgetId,
-  width,
-}: TreeSelectProps): JSX.Element {
-  const [key, setKey] = useState(Math.random());
-  const [filter, setFilter] = useState(filterText ?? "");
+const MultiTreeSelectComponent = React.forwardRef<
+  HTMLDivElement,
+  TreeSelectProps
+>(
+  (
+    {
+      allowClear,
+      compactMode,
+      disabled,
+      dropdownStyle,
+      dropDownWidth,
+      expandAll,
+      filterText,
+      isFilterable,
+      isValid,
+      labelAlignment,
+      labelPosition,
+      labelStyle,
+      labelText,
+      labelTextColor,
+      labelTextSize,
+      labelWidth,
+      loading,
+      mode,
+      onChange,
+      options,
+      placeholder,
+      value,
+      widgetId,
+      width,
+    }: TreeSelectProps,
+    ref,
+  ): JSX.Element => {
+    const [key, setKey] = useState(Math.random());
+    const [filter, setFilter] = useState(filterText ?? "");
 
-  const _menu = useRef<HTMLElement | null>(null);
-  const labelRef = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [memoDropDownWidth, setMemoDropDownWidth] = useState(0);
+    const _menu = ref;
+    const labelRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [memoDropDownWidth, setMemoDropDownWidth] = useState(0);
 
-  // treeDefaultExpandAll is uncontrolled after first render,
-  // using this to force render to respond to changes in expandAll
-  useEffect(() => {
-    setKey(Math.random());
-  }, [expandAll]);
+    // treeDefaultExpandAll is uncontrolled after first render,
+    // using this to force render to respond to changes in expandAll
+    useEffect(() => {
+      setKey(Math.random());
+    }, [expandAll]);
 
-  const clearButton = useMemo(
-    () =>
-      filter ? (
-        <Button icon="cross" minimal onClick={() => setFilter("")} />
-      ) : null,
-    [filter],
-  );
-
-  const getDropdownPosition = useCallback(() => {
-    const node = _menu.current;
-    if (Boolean(node?.closest(`.${MODAL_PORTAL_CLASSNAME}`))) {
-      return document.querySelector(
-        `.${MODAL_PORTAL_CLASSNAME}`,
-      ) as HTMLElement;
-    }
-    return document.querySelector(`.${CANVAS_CLASSNAME}`) as HTMLElement;
-  }, []);
-  const onQueryChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-    event.stopPropagation();
-    setFilter(event.target.value);
-  }, []);
-
-  useEffect(() => {
-    const parentWidth = width - WidgetContainerDiff;
-    if (compactMode && labelRef.current) {
-      const labelWidth = labelRef.current.getBoundingClientRect().width;
-      const widthDiff = parentWidth - labelWidth - labelMargin;
-      setMemoDropDownWidth(
-        widthDiff > dropDownWidth ? widthDiff : dropDownWidth,
-      );
-      return;
-    }
-    setMemoDropDownWidth(
-      parentWidth > dropDownWidth ? parentWidth : dropDownWidth,
+    const clearButton = useMemo(
+      () =>
+        filter ? (
+          <Button icon="cross" minimal onClick={() => setFilter("")} />
+        ) : null,
+      [filter],
     );
-  }, [compactMode, dropDownWidth, width, labelText]);
-  const dropdownRender = useCallback(
-    (
-      menu: React.ReactElement<any, string | React.JSXElementConstructor<any>>,
-    ) => (
-      <>
-        {isFilterable ? (
-          <InputGroup
-            autoFocus
-            inputRef={inputRef}
-            leftIcon="search"
-            onChange={onQueryChange}
-            onKeyDown={(e) => e.stopPropagation()}
-            placeholder="Filter..."
-            rightElement={clearButton as JSX.Element}
-            small
-            type="text"
-            value={filter}
+
+    const getDropdownPosition = useCallback(() => {
+      const node = (_menu as React.MutableRefObject<HTMLDivElement>).current;
+      if (Boolean(node?.closest(`.${MODAL_PORTAL_CLASSNAME}`))) {
+        return document.querySelector(
+          `.${MODAL_PORTAL_CLASSNAME}`,
+        ) as HTMLElement;
+      }
+      return document.querySelector(`.${CANVAS_CLASSNAME}`) as HTMLElement;
+    }, []);
+    const onQueryChange = useCallback(
+      (event: ChangeEvent<HTMLInputElement>) => {
+        event.stopPropagation();
+        setFilter(event.target.value);
+      },
+      [],
+    );
+
+    useEffect(() => {
+      const parentWidth = width - WidgetContainerDiff;
+      if (compactMode && labelRef.current) {
+        const labelWidth = labelRef.current.getBoundingClientRect().width;
+        const widthDiff = parentWidth - labelWidth - labelMargin;
+        setMemoDropDownWidth(
+          widthDiff > dropDownWidth ? widthDiff : dropDownWidth,
+        );
+        return;
+      }
+      setMemoDropDownWidth(
+        parentWidth > dropDownWidth ? parentWidth : dropDownWidth,
+      );
+    }, [compactMode, dropDownWidth, width, labelText]);
+    const dropdownRender = useCallback(
+      (
+        menu: React.ReactElement<
+          any,
+          string | React.JSXElementConstructor<any>
+        >,
+      ) => (
+        <>
+          {isFilterable ? (
+            <InputGroup
+              autoFocus
+              inputRef={inputRef}
+              leftIcon="search"
+              onChange={onQueryChange}
+              onKeyDown={(e) => e.stopPropagation()}
+              placeholder="Filter..."
+              rightElement={clearButton as JSX.Element}
+              small
+              type="text"
+              value={filter}
+            />
+          ) : null}
+          <div className={`${loading ? Classes.SKELETON : ""}`}>{menu}</div>
+        </>
+      ),
+      [loading, isFilterable, filter, onQueryChange],
+    );
+
+    const onOpen = useCallback((open: boolean) => {
+      if (open) {
+        setTimeout(() => inputRef.current?.focus(), FOCUS_TIMEOUT);
+      }
+    }, []);
+
+    const onClear = useCallback(() => onChange([], []), []);
+
+    return (
+      <TreeSelectContainer
+        allowClear={allowClear}
+        compactMode={compactMode}
+        data-testid="multitreeselect-container"
+        isValid={isValid}
+        labelPosition={labelPosition}
+        ref={_menu as React.Ref<HTMLDivElement>}
+      >
+        <DropdownStyles dropDownWidth={memoDropDownWidth} id={widgetId} />
+        {labelText && (
+          <LabelWithTooltip
+            alignment={labelAlignment}
+            className={`multitree-select-label`}
+            color={labelTextColor}
+            compact={compactMode}
+            disabled={disabled}
+            fontSize={labelTextSize}
+            fontStyle={labelStyle}
+            loading={loading}
+            position={labelPosition}
+            ref={labelRef}
+            text={labelText}
+            width={labelWidth}
           />
-        ) : null}
-        <div className={`${loading ? Classes.SKELETON : ""}`}>{menu}</div>
-      </>
-    ),
-    [loading, isFilterable, filter, onQueryChange],
-  );
+        )}
+        <InputContainer compactMode={compactMode} labelPosition={labelPosition}>
+          <TreeSelect
+            allowClear={allowClear}
+            animation="slide-up"
+            choiceTransitionName="rc-tree-select-selection__choice-zoom"
+            className="rc-tree-select"
+            clearIcon={
+              <Icon
+                className="clear-icon"
+                fillColor={Colors.GREY_10}
+                name="close-x"
+              />
+            }
+            disabled={disabled}
+            dropdownClassName={`tree-multiselect-dropdown multiselecttree-popover-width-${widgetId}`}
+            dropdownRender={dropdownRender}
+            dropdownStyle={dropdownStyle}
+            filterTreeNode
+            getPopupContainer={getDropdownPosition}
+            inputIcon={
+              <Icon
+                className="dropdown-icon"
+                fillColor={disabled ? Colors.GREY_7 : Colors.GREY_10}
+                name="dropdown"
+              />
+            }
+            key={key}
+            loading={loading}
+            maxTagCount={"responsive"}
+            maxTagPlaceholder={(e) => `+${e.length} more`}
+            multiple
+            notFoundContent="No Results Found"
+            onChange={onChange}
+            onClear={onClear}
+            onDropdownVisibleChange={onOpen}
+            placeholder={placeholder}
+            removeIcon={
+              <Icon
+                className="remove-icon"
+                fillColor={Colors.GREY_10}
+                name="close-x"
+              />
+            }
+            searchValue={filter}
+            showArrow
+            showCheckedStrategy={mode}
+            showSearch={false}
+            style={{ width: "100%" }}
+            switcherIcon={switcherIcon}
+            transitionName="rc-tree-select-dropdown-slide-up"
+            treeCheckable={
+              <span className={`rc-tree-select-tree-checkbox-inner`} />
+            }
+            treeData={options}
+            treeDefaultExpandAll={expandAll}
+            treeIcon
+            treeNodeFilterProp="label"
+            value={value}
+          />
+        </InputContainer>
+      </TreeSelectContainer>
+    );
+  },
+);
 
-  const onOpen = useCallback((open: boolean) => {
-    if (open) {
-      setTimeout(() => inputRef.current?.focus(), FOCUS_TIMEOUT);
-    }
-  }, []);
-
-  const onClear = useCallback(() => onChange([], []), []);
-
-  return (
-    <TreeSelectContainer
-      allowClear={allowClear}
-      compactMode={compactMode}
-      data-testid="multitreeselect-container"
-      isValid={isValid}
-      labelPosition={labelPosition}
-      ref={_menu as React.RefObject<HTMLDivElement>}
-    >
-      <DropdownStyles dropDownWidth={memoDropDownWidth} id={widgetId} />
-      {labelText && (
-        <LabelWithTooltip
-          alignment={labelAlignment}
-          className={`multitree-select-label`}
-          color={labelTextColor}
-          compact={compactMode}
-          disabled={disabled}
-          fontSize={labelTextSize}
-          fontStyle={labelStyle}
-          loading={loading}
-          position={labelPosition}
-          ref={labelRef}
-          text={labelText}
-          width={labelWidth}
-        />
-      )}
-      <InputContainer compactMode={compactMode} labelPosition={labelPosition}>
-        <TreeSelect
-          allowClear={allowClear}
-          animation="slide-up"
-          choiceTransitionName="rc-tree-select-selection__choice-zoom"
-          className="rc-tree-select"
-          clearIcon={
-            <Icon
-              className="clear-icon"
-              fillColor={Colors.GREY_10}
-              name="close-x"
-            />
-          }
-          disabled={disabled}
-          dropdownClassName={`tree-multiselect-dropdown multiselecttree-popover-width-${widgetId}`}
-          dropdownRender={dropdownRender}
-          dropdownStyle={dropdownStyle}
-          filterTreeNode
-          getPopupContainer={getDropdownPosition}
-          inputIcon={
-            <Icon
-              className="dropdown-icon"
-              fillColor={disabled ? Colors.GREY_7 : Colors.GREY_10}
-              name="dropdown"
-            />
-          }
-          key={key}
-          loading={loading}
-          maxTagCount={"responsive"}
-          maxTagPlaceholder={(e) => `+${e.length} more`}
-          multiple
-          notFoundContent="No Results Found"
-          onChange={onChange}
-          onClear={onClear}
-          onDropdownVisibleChange={onOpen}
-          placeholder={placeholder}
-          removeIcon={
-            <Icon
-              className="remove-icon"
-              fillColor={Colors.GREY_10}
-              name="close-x"
-            />
-          }
-          searchValue={filter}
-          showArrow
-          showCheckedStrategy={mode}
-          showSearch={false}
-          style={{ width: "100%" }}
-          switcherIcon={switcherIcon}
-          transitionName="rc-tree-select-dropdown-slide-up"
-          treeCheckable={
-            <span className={`rc-tree-select-tree-checkbox-inner`} />
-          }
-          treeData={options}
-          treeDefaultExpandAll={expandAll}
-          treeIcon
-          treeNodeFilterProp="label"
-          value={value}
-        />
-      </InputContainer>
-    </TreeSelectContainer>
-  );
-}
+MultiTreeSelectComponent.displayName = "MultiTreeSelectComponent";
 
 export default MultiTreeSelectComponent;
