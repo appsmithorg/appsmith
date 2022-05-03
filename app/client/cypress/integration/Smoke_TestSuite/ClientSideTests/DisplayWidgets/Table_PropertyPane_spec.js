9ed5b3aef67e4be8861ac78fc5d492c4c28194b0
Pawan Kumar <pawankumar@Pawans-MacBook-Pro.local>
2022-05-03 14:19:01 +0530

    fix cypress tests

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_PropertyPane_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/DisplayWidgets/Table_PropertyPane_spec.js
@@ -387,7 +387,7 @@ describe("Table Widget property pane feature validation", function() {
       cy.get(selector + " button").should(
         "have.css",
         "border",
-        `1px solid rgb(3, 179, 101)`,
+        `1px solid rgb(22, 163, 74)`,
       );
     });
     cy.selectDropdownValue(commonlocators.tableButtonVariant, "Tertiary");
@@ -404,7 +404,7 @@ describe("Table Widget property pane feature validation", function() {
       cy.get(selector + " button > span").should(
         "have.css",
         "color",
-        "rgb(3, 179, 101)",
+        "rgb(22, 163, 74)",
       );
       cy.get(selector + " button").should(
         "have.css",
