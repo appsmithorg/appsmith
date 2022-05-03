bc0ce658bf10c0d3d69c5ffb02f0ab6138f9f81c
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-05-03 19:35:39 +0530

    Undo drag and drop wait

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -718,7 +718,6 @@ Cypress.Commands.add("dragAndDropToCanvas", (widgetType, { x, y }) => {
     .trigger("mousemove", x, y, { eventConstructor: "MouseEvent" })
     .trigger("mousemove", x, y, { eventConstructor: "MouseEvent" })
     .trigger("mouseup", x, y, { eventConstructor: "MouseEvent" });
-  cy.wait(500);
 });
 
 Cypress.Commands.add(
