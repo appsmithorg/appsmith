d9984253e842ab9b48f2923ef4d45c83c32073dd
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-05-03 16:59:02 +0530

    Fix Aforce cypress test

--- app/client/cypress/integration/Smoke_TestSuite/Application/AForceMigration_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/Application/AForceMigration_Spec.ts
@@ -265,9 +265,9 @@ describe("AForce - Community Issues page validations", function () {
   })
 
   it("9. Validate Updating issue from Details tab", () => {
-
-    agHelper.AssertElementAbsence(locator._widgetInDeployed('tabswidget'))
-    table.SelectTableRow(0)
+    // As the table row is already selected, tab widget is expected to be visible.
+    // agHelper.AssertElementAbsence(locator._widgetInDeployed('tabswidget'))
+    // table.SelectTableRow(0)
     agHelper.AssertElementPresence(locator._widgetInDeployed('tabswidget'))
     agHelper.Sleep(2000)//load time for tabs
     agHelper.GetNClick(locator._inputWidgetv1InDeployed).type("-updating title")
@@ -310,8 +310,8 @@ describe("AForce - Community Issues page validations", function () {
   })
 
   it("10. Validate Deleting the newly created issue", () => {
-    agHelper.AssertElementAbsence(locator._widgetInDeployed('tabswidget'))
-    table.SelectTableRow(0)
+    // agHelper.AssertElementAbsence(locator._widgetInDeployed('tabswidget'))
+    // table.SelectTableRow(0)
     agHelper.AssertElementPresence(locator._widgetInDeployed('tabswidget'))
     cy.get(table._trashIcon).closest('div').click()
     agHelper.AssertElementAbsence(locator._widgetInDeployed('tabswidget'))
