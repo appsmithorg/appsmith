appsmithorg/appsmith

commit 1d019d1376300af40f94fa1e4f0e343ee33b623e

fix: Modal padding and color fixes (#12189)
* * share modal fixes

* * modal ui fixes

app/client/src/pages/Editor/gitSync/GitSyncModal.tsx
@@ -35,7 +35,6 @@ const Container = styled.div`
   flex-direction: column;
   position: relative;
   overflow-y: hidden;
-  padding: 0px 8px 0px 8px;
 `;
 
 const BodyContainer = styled.div`
@@ -49,10 +48,17 @@ const MenuContainer = styled.div`
 
 const CloseBtnContainer = styled.div`
   position: absolute;
-  right: 0;
+  right: -5px;
   top: 0;
-  padding: ${(props) => props.theme.spaces[1]}px;
+  padding: ${(props) => props.theme.spaces[1]}px 0;
   border-radius: ${(props) => props.theme.radii[1]}px;
+
+  &:hover {
+    svg,
+    svg path {
+      fill: ${({ theme }) => get(theme, "colors.gitSyncModal.closeIconHover")};
+    }
+  }
 `;
 
 const ComponentsByTab = {

app/client/src/components/ads/CopyToClipBoard.tsx
@@ -19,7 +19,11 @@ const Wrapper = styled.div<{ offset?: string }>`
   }
 `;
 
-function CopyToClipboard(props: { copyText: string; btnWidth?: string }) {
+function CopyToClipboard(props: {
+  className?: string;
+  copyText: string;
+  btnWidth?: string;
+}) {
   const { copyText } = props;
   const copyURLInput = createRef<HTMLInputElement>();
   const [isCopied, setIsCopied] = useState(false);
@@ -38,7 +42,7 @@ function CopyToClipboard(props: { copyText: string; btnWidth?: string }) {
     }
   };
   return (
-    <Wrapper offset={props.btnWidth}>
+    <Wrapper className={props.className} offset={props.btnWidth}>
       <TextInput
         defaultValue={copyText}
         onChange={() => {

app/client/src/components/ads/DialogComponent.tsx
@@ -26,8 +26,9 @@ const StyledDialog = styled(Dialog)<{
       padding: 0;
       background: ${(props) => props.theme.colors.modal.bg};
       box-shadow: none;
-      .${Classes.ICON} {
-        color: ${(props) => props.theme.colors.modal.iconColor};
+      min-height: unset;
+      svg {
+        color: ${Colors.GREY_800};
       }
 
       .${Classes.BUTTON}.${Classes.MINIMAL}:hover {
@@ -39,19 +40,23 @@ const StyledDialog = styled(Dialog)<{
       color: ${(props) => props.theme.colors.modal.headerText};
       font-weight: ${(props) => props.theme.typography.h1.fontWeight};
       font-size: ${(props) => props.theme.typography.h1.fontSize}px;
-      line-height: ${(props) => props.theme.typography.h1.lineHeight}px;
+      line-height: unset;
       letter-spacing: ${(props) => props.theme.typography.h1.letterSpacing};
     }
 
     .${Classes.DIALOG_CLOSE_BUTTON} {
-      color: ${Colors.CHARCOAL};
+      color: ${Colors.SCORPION};
       min-width: 0;
       padding: 0;
 
       svg {
-        fill: ${Colors.CHARCOAL};
+        fill: ${Colors.SCORPION};
         width: 24px;
         height: 24px;
+
+        &:hover {
+          fill: ${Colors.COD_GRAY};
+        }
       }
     }
 
@@ -78,7 +83,7 @@ const StyledDialog = styled(Dialog)<{
 
     & .${Classes.DIALOG_BODY} {
       margin: 0;
-      margin-top: ${(props) => (props.noModalBodyMarginTop ? "0px" : "24px")};
+      margin-top: ${(props) => (props.noModalBodyMarginTop ? "0px" : "16px")};
       overflow: auto;
     }
 

app/client/src/constants/DefaultTheme.tsx
@@ -652,6 +652,7 @@ const lightShades = [
   "#F86A2B",
   "#FFDEDE",
   "#575757",
+  "#191919",
 ] as const;
 
 type ShadeColor = typeof darkShades[number] | typeof lightShades[number];
@@ -1373,13 +1374,14 @@ const editorBottomBar = {
 const gitSyncModal = {
   menuBackgroundColor: Colors.ALABASTER_ALT,
   separator: Colors.ALTO2,
-  closeIcon: "rgba(29, 28, 29, 0.7);",
+  closeIcon: Colors.SCORPION,
+  closeIconHover: Colors.COD_GRAY,
 };
 type GitSyncModalColors = typeof gitSyncModal;
 
 const tabItemBackgroundFill = {
   highlightBackground: Colors.Gallery,
-  highlightTextColor: Colors.CODE_GRAY,
+  highlightTextColor: Colors.COD_GRAY,
   textColor: Colors.CHARCOAL,
 };
 
@@ -2612,7 +2614,7 @@ export const light: ColorType = {
   },
   modal: {
     bg: lightShades[11],
-    headerText: lightShades[10],
+    headerText: lightShades[20],
     iconColor: lightShades[5],
     iconBg: lightShades[18],
     user: {

app/client/src/pages/Editor/gitSync/components/DeployedKeyUI.tsx
@@ -61,7 +61,7 @@ const KeyText = styled.span`
   font-size: 10px;
   font-weight: 600;
   text-transform: uppercase;
-  color: ${Colors.CODE_GRAY};
+  color: ${Colors.COD_GRAY};
 `;
 
 const MoreMenuWrapper = styled.div`

app/client/src/pages/Editor/gitSync/components/StyledComponents.tsx
@@ -6,11 +6,12 @@ export const Title = styled.p`
   ${(props) => getTypographyByKey(props, "h1")};
   margin: ${(props) =>
     `${props.theme.spaces[7]}px 0px ${props.theme.spaces[3]}px 0px`};
+  color: ${Colors.COD_GRAY};
 `;
 
 export const Subtitle = styled.span`
   ${(props) => getTypographyByKey(props, "p1")};
-  color: ${Colors.BLACK};
+  color: ${Colors.COD_GRAY};
 `;
 
 export const Caption = styled.span`

app/client/src/pages/organization/AppInviteUsersForm.tsx
@@ -17,25 +17,25 @@ import { ANONYMOUS_USERNAME } from "constants/userConstants";
 import { Colors } from "constants/Colors";
 import { viewerURL } from "RouteBuilder";
 
+const StyledCopyToClipBoard = styled(CopyToClipBoard)`
+  margin-bottom: 24px;
+`;
+
 const CommonTitleTextStyle = css`
   color: ${Colors.CHARCOAL};
   font-weight: normal;
 `;
 
 const Title = styled.div`
-  padding: 0 0 10px 0;
+  padding: 0 0 8px 0;
   & > span[type="h5"] {
     ${CommonTitleTextStyle}
   }
 `;
 
-const StyledCopyToClipBoard = styled(CopyToClipBoard)`
-  margin-bottom: 24px;
-`;
-
 const ShareWithPublicOption = styled.div`
   display: flex;
-  margin-bottom: 24px;
+  margin-bottom: 8px;
   align-items: center;
   justify-content: space-between;
 

app/client/src/pages/organization/ManageUsers.tsx
@@ -7,7 +7,7 @@ import { Classes } from "components/ads/common";
 import { useLocation } from "react-router-dom";
 
 const StyledManageUsers = styled("a")`
-  margin-top: 20px;
+  margin-top: 12px;
   display: inline-flex;
   &&&& {
     text-decoration: none;

commit 3bab0e5d16801ed34e9dde422cf53936d19a2d0b

fix: remove horizontal scrollbar
This scrollbar was introduced due to change in effective width of the
container.

The breaking change was introduced in PR #12189

The exact change that causes this bar to appear is https://github.com/appsmithorg/appsmith/pull/12189/files\#diff-a9e89817480e902f90382662965dfc200a72566de917626db002de7a7e2920ddR51

app/client/src/pages/Editor/gitSync/GitSyncModal.tsx
@@ -34,7 +34,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
-  overflow-y: hidden;
+  overflow: hidden;
 `;
 
 const BodyContainer = styled.div`