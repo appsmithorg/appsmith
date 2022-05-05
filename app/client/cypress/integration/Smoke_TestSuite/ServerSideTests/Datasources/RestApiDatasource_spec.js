556c85b5927b8c58f32c267025096c6c8dc98a48
Aishwarya UR <aishwarya@appsmith.com>
2022-05-05 15:54:28 +0530

    Flaky fixes

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/Datasources/RestApiDatasource_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/Datasources/RestApiDatasource_spec.js
@@ -9,7 +9,7 @@ describe("Create a rest datasource", function() {
     cy.NavigateToAPI_Panel();
     cy.CreateAPI("Testapi");
     cy.enterDatasourceAndPath(testdata.baseUrl, testdata.methods);
-    cy.get(".t--store-as-datasource").click({ force: true });
+    cy.get(".t--store-as-datasource").click();
     cy.saveDatasource();
     cy.contains(".datasource-highlight", "https://mock-api.appsmith.com");
     cy.SaveAndRunAPI();
