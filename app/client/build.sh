00579b621363f31bd764b7f209483b3a15f5f834
Arsalan <arsalanyaldram0211@outlook.com>
2022-05-18 13:00:36 +0530

    fix: upgrade vite require plugin add sentry checks

--- app/client/build.sh
+++ app/client/build.sh
@@ -6,7 +6,7 @@ GIT_SHA=$(eval git rev-parse HEAD)
 echo $GIT_SHA
 echo "Sentry Auth Token: $SENTRY_AUTH_TOKEN"
 
-REACT_APP_SENTRY_RELEASE=$GIT_SHA REACT_APP_CLIENT_LOG_LEVEL=ERROR EXTEND_ESLINT=true craco --max-old-space-size=4096 build --config craco.build.config.js
+REACT_APP_SENTRY_RELEASE=$GIT_SHA REACT_APP_CLIENT_LOG_LEVEL=ERROR EXTEND_ESLINT=true vite build
 
-rm ./build/static/js/*.js.map
+rm -rf build/**/*.js.map
 echo "build finished"
--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -5,22 +5,16 @@ import viteRawPlugin from "vite-raw-plugin";
 import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import viteCompression from "vite-plugin-compression";
-import viteSentry, { ViteSentryPluginOptions } from "vite-plugin-sentry";
+import viteSentry from "vite-plugin-sentry";
 import { VitePWA } from "vite-plugin-pwa";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-const sentryConfig: ViteSentryPluginOptions = {
-  sourceMaps: {
-    include: ["build"],
-    ignore: ["node_modules"],
-  },
-  release: "dfdf7fa46c5b483a944b4571554d6466da3c64a6ed8b46e3b8a4285183a6bcc3",
-  deploy: {
-    env: "Production",
-  },
-};
-
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  /*
+    Loading Env variables in vite
+    https://stackoverflow.com/questions/66389043/how-can-i-use-vite-env-variables-in-vite-config-js
+  */
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "") };
   const babelPlugins: any[] = [];
   const babelPresets: string[] = [];
   const configPlugins: any[] = [];
@@ -39,6 +33,34 @@ export default defineConfig(({ command }) => {
       },
     ]);
   } else {
+    if (
+      process.env.REACT_APP_ENVIRONMENT == "PRODUCTION" ||
+      process.env.REACT_APP_ENVIRONMENT === "STAGING"
+    ) {
+      if (
+        process.env.SENTRY_AUTH_TOKEN != null &&
+        process.env.SENTRY_AUTH_TOKEN !== ""
+      ) {
+        configPlugins.push(
+          viteSentry({
+            sourceMaps: {
+              include: ["build"],
+              ignore: ["node_modules"],
+            },
+            release: process.env.REACT_APP_SENTRY_RELEASE,
+            deploy: {
+              env: process.env.REACT_APP_SENTRY_ENVIRONMENT,
+            },
+          }),
+        );
+      } else {
+        // eslint-disable-next-line no-console
+        console.log(
+          "Sentry configuration missing in process environment. Sentry will be disabled.",
+        );
+      }
+    }
+
     // build configurations
     configPlugins.push(
       viteCompression(),
@@ -46,7 +68,6 @@ export default defineConfig(({ command }) => {
         algorithm: "brotliCompress",
         threshold: 10240,
       }),
-      viteSentry(sentryConfig),
       VitePWA({
         strategies: "injectManifest",
         srcDir: "src",
