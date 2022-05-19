621f5be22eaa794b6558d191dd67a100e51eeceb
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-05-19 17:59:18 +0530

    Add phone input widget and currencyInput widget

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/AllInputWidgets_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/AllInputWidgets_spec.js
@@ -1,4 +1,5 @@
 const explorer = require("../../../../locators/explorerlocators.json");
+const testdata = require("../../../../fixtures/testdata.json");
 
 const WIDGET = {
   INPUT_WIDGET_V2: "inputwidgetv2",
@@ -13,6 +14,9 @@ const PROPERTY_SELECTOR = {
   onSubmit: ".t--property-control-onsubmit",
   text: ".t--property-control-text",
 };
+
+const getWidgetSelector = (widget) => `.t--widget-${widget}`;
+const getWidgetInputSelector = (widget) => `.t--widget-${widget} input`;
 
 const widgetsToTest = {
   [WIDGET.INPUT_WIDGET_V2]: {
@@ -35,56 +39,69 @@ const widgetsToTest = {
     widgetName: "Input widget",
     widgetPrefixName: "Input",
   },
-  // [WIDGET.PHONE_INPUT_WIDGET]: {
-  //   testCases: [
-  //     {
-  //       input: "9999999999",
-  //       expected: "(999) 999-9999:US:+1",
-  //       clearBeforeType: true,
-  //     },
-  //     {
-  //       input: "{backspace}{backspace}{backspace}{backspace}12",
-  //       expected: "99999912:US:+1",
-  //       clearBeforeType: false,
-  //     },
-  //     {
-  //       input: "{backspace}{backspace}456",
-  //       expected: "999999456:US:+1",
-  //       clearBeforeType: false,
-  //     },
-  //   ],
-  //   widgetName: "Phone Input widget",
-  //   widgetPrefixName: "PhoneInput",
-  // },
-  // [WIDGET.CURRENCY_INPUT_WIDGET]: {
-  //   testCases: [
-  //     { input: "1233", expected: "1123", clearBeforeType: true },
-  //     {
-  //       input: "{backspace}{backspace}{backspace}{backspace}12",
-  //       expected: "12",
-  //       clearBeforeType: false,
-  //     },
-  //     {
-  //       input: "{backspace}{backspace}456",
-  //       expected: "456",
-  //       clearBeforeType: false,
-  //     },
-  //   ],
-  //   widgetName: "Currency Input widget",
-  //   widgetPrefixName: "CurrencyInput",
-  // },
+  [WIDGET.PHONE_INPUT_WIDGET]: {
+    testCases: [
+      {
+        input: "9999999999",
+        expected: "(999) 999-9999",
+        clearBeforeType: true,
+      },
+      {
+        input: "{backspace}{backspace}{backspace}{backspace}12",
+        expected: "99999912",
+        clearBeforeType: false,
+      },
+      {
+        input: "{backspace}{backspace}456",
+        expected: "999999456",
+        clearBeforeType: false,
+      },
+    ],
+    widgetName: "Phone Input widget",
+    widgetPrefixName: "PhoneInput",
+  },
+  [WIDGET.CURRENCY_INPUT_WIDGET]: {
+    testCases: [
+      { input: "1233", expected: "1,233", clearBeforeType: true },
+      {
+        input: "{backspace}{backspace}{backspace}{backspace}12",
+        expected: "12",
+        clearBeforeType: false,
+      },
+      {
+        input: "{backspace}{backspace}456",
+        expected: "456",
+        clearBeforeType: false,
+      },
+    ],
+    widgetName: "Currency Input widget",
+    widgetPrefixName: "CurrencyInput",
+  },
 };
 
-const getWidgetSelector = (widget) => `.t--widget-${widget}`;
-const getWidgetInputSelector = (widget) => `.t--widget-${widget} input`;
+function configureApi() {
+  cy.NavigateToAPI_Panel();
+  cy.log("Navigation to API Panel screen successful");
+  cy.CreateAPI("FirstAPI");
+  cy.get(".CodeMirror-placeholder")
+    .first()
+    .should("have.text", "https://mock-api.appsmith.com/users");
+  cy.log("Creation of FirstAPI Action successful");
+  cy.enterDatasourceAndPath(
+    testdata.baseUrl,
+    testdata.methods + `?user={{this.params.value}}`,
+  );
+  cy.SaveAndRunAPI();
+}
 
-Object.entries(widgetsToTest).forEach(([widgetSelector, testConfig]) => {
+Object.entries(widgetsToTest).forEach(([widgetSelector, testConfig], index) => {
   describe(`${testConfig.widgetName} widget test for storeValue save, Api Call params`, () => {
-    // const widgetSelector = WIDGET.INPUT_WIDGET_V2;
-    // const testConfig = widgetsToTest[widgetSelector];
+    it(`1. DragDrop widget & Label/Text widgets`, () => {
+      if (index === 0) {
+        // configureApi();
 
-    it(`1. DragDrop Input widget & Label/Text widgets`, () => {
-      cy.get(explorer.addWidget).click();
+        cy.get(explorer.addWidget).click();
+      }
       cy.dragAndDropToCanvas(widgetSelector, { x: 300, y: 200 });
       cy.get(getWidgetSelector(widgetSelector)).should("exist");
 
@@ -93,14 +110,16 @@ Object.entries(widgetsToTest).forEach(([widgetSelector, testConfig]) => {
       cy.dragAndDropToCanvas(WIDGET.TEXT, { x: 300, y: 600 });
     });
 
-    it("2. onSubmit should be triggered with the whole input value", () => {
-      cy.openPropertyPane(widgetSelector);
-      cy.selectDropdownValue(".t--property-control-datatype", "Text");
-      cy.get(".t--property-control-required label")
-        .last()
-        .click({ force: true });
+    it("2. StoreValue should have complete input value", () => {
+      if (widgetSelector === WIDGET.INPUT_WIDGET_V2) {
+        cy.openPropertyPane(widgetSelector);
+        cy.selectDropdownValue(".t--property-control-datatype", "Text");
+        cy.get(".t--property-control-required label")
+          .last()
+          .click({ force: true });
+        cy.closePropertyPane();
+      }
 
-      cy.closePropertyPane();
       // Set onClick action, storing value
       cy.openPropertyPane(WIDGET.BUTTON_WIDGET);
       cy.get(PROPERTY_SELECTOR.onClick)
@@ -139,10 +158,49 @@ Object.entries(widgetsToTest).forEach(([widgetSelector, testConfig]) => {
       });
     });
 
-    // after(() => {
-    //   cy.deleteWidget(widgetSelector);
-    //   cy.deleteWidget(WIDGET.BUTTON_WIDGET);
-    //   cy.deleteWidget(WIDGET.TEXT);
-    // });
+    it.skip("3. Api params getting correct input values", () => {
+      // Set onClick action, storing value
+      cy.openPropertyPane(WIDGET.BUTTON_WIDGET);
+      cy.get(PROPERTY_SELECTOR.onClick)
+        .find(".t--js-toggle")
+        .click();
+      cy.updateCodeInput(
+        PROPERTY_SELECTOR.onClick,
+        `{{FirstAPI.run({ value: ${testConfig.widgetPrefixName}1.text })}}`,
+      );
+
+      const inputs = testConfig.testCases;
+
+      inputs.forEach(({ clearBeforeType, expected, input }) => {
+        // Input text and hit enter key
+        if (clearBeforeType) {
+          cy.get(getWidgetInputSelector(widgetSelector))
+            .clear()
+            .type(`${input}`);
+        } else {
+          cy.get(getWidgetInputSelector(widgetSelector)).type(`${input}`);
+        }
+
+        cy.get(getWidgetSelector(WIDGET.BUTTON_WIDGET)).click();
+
+        // Assert if the Api request contains the expected value
+        cy.wait("@postExecute").then((interception) => {
+          expect(interception.request.body.data.request.url).to.equal(
+            `https://mock-api.appsmith.com/users?user=${expected}`,
+          );
+        });
+      });
+    });
+
+    it("4. Delete all the widgets on canvas", () => {
+      cy.get(getWidgetSelector(widgetSelector)).click();
+      cy.get("body").type(`{backspace}`, { force: true });
+
+      cy.get(getWidgetSelector(WIDGET.BUTTON_WIDGET)).click();
+      cy.get("body").type(`{backspace}`, { force: true });
+
+      cy.get(getWidgetSelector(WIDGET.TEXT)).click();
+      cy.get("body").type(`{backspace}`, { force: true });
+    });
   });
 });
