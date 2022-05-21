f1f42b099c7412b1b99bbbc9e46ad6861e1ef45e
ohansFavour <fohanekwu@gmail.com>
2022-05-21 15:54:44 +0100

    reposition import statement

--- app/client/src/components/editorComponents/CodeEditor/constants.ts
+++ app/client/src/components/editorComponents/CodeEditor/constants.ts
@@ -1,8 +1,7 @@
-// For these error types, we want to show a warning
-// All messages can be found here => https://github.com/jshint/jshint/blob/2.9.5/src/messages.js
-
 import { Position } from "codemirror";
 
+// For these error types, we want to show a warning
+// All messages can be found here => https://github.com/jshint/jshint/blob/2.9.5/src/messages.js
 export const WARNING_LINT_ERRORS = {
   W098: "'{a}' is defined but never used.",
   W014:
