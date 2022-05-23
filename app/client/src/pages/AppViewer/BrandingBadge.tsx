bf5e0c7aef931cb7e9bf9d842a57cb5c0bfa9112
Pawan Kumar <pawan.stardust@gmail.com>
2022-05-23 13:32:16 +0530

    open url in new tab;

--- app/client/src/pages/AppViewer/BrandingBadge.tsx
+++ app/client/src/pages/AppViewer/BrandingBadge.tsx
@@ -7,6 +7,8 @@ function BrandingBadge() {
     <a
       className="fixed items-center hidden p-1 px-2 space-x-2 bg-white border rounded-md md:flex z-2 hover:no-underline right-8 bottom-4 backdrop-blur-xl backdrop-filter"
       href="https://appsmith.com"
+      rel="noreferrer"
+      target="_blank"
     >
       <h4 className="text-xs text-gray-500">Built on</h4>
       <AppsmithLogo className="w-auto h-3" />
