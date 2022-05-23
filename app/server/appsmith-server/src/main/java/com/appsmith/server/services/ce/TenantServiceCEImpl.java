8685b7cf70fd48a00424019ad8c1fc718e217b00
Trisha Anand <trisha@appsmith.com>
2022-05-23 11:32:03 +0530

    WIP introdution of tenancy in workspace and user. Untested.

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/TenantServiceCEImpl.java
@@ -0,0 +1,28 @@
+package com.appsmith.server.services.ce;
+
+import com.appsmith.server.constants.FieldName;
+import com.appsmith.server.domains.Tenant;
+import com.appsmith.server.repositories.TenantRepository;
+import reactor.core.publisher.Mono;
+
+public class TenantServiceCEImpl implements TenantServiceCE {
+
+    private final TenantRepository tenantRepository;
+
+    private Mono<String> defaultTenantIdCacheMono = null;
+
+    public TenantServiceCEImpl(TenantRepository tenantRepository) {
+        this.tenantRepository = tenantRepository;
+    }
+
+    @Override
+    public Mono<String> getDefaultTenantId() {
+        // If the default tenant id does not exist in cache, find from repository and set the cache.
+        if (defaultTenantIdCacheMono == null) {
+            defaultTenantIdCacheMono = tenantRepository.findBySlug(FieldName.DEFAULT)
+                    .map(Tenant::getId);
+        }
+
+        return defaultTenantIdCacheMono;
+    }
+}
