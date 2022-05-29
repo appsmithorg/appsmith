26e56694413bb3da3a941413ed9b0e8047c2c61a
Abhijeet <abhijeet@appsmith.com>
2022-05-29 09:39:31 +0530

    Refactor

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/GitServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/GitServiceCEImpl.java
@@ -2424,29 +2424,25 @@ public class GitServiceCEImpl implements GitServiceCE {
                                                           Boolean isRepoPrivate,
                                                           Boolean isSystemGenerated) {
         GitApplicationMetadata gitData = application.getGitApplicationMetadata();
-        String defaultApplicationId = "", gitHostingProvider = "", branchName = "";
+        Map<String, Object> analyticsProps = new HashMap<>();
         if (gitData != null) {
-            defaultApplicationId = gitData.getDefaultApplicationId();
-            branchName = StringUtils.isEmptyOrNull(gitData.getBranchName()) ? "" : gitData.getBranchName();
-            gitHostingProvider = GitUtils.getGitProviderName(gitData.getRemoteUrl());
+            analyticsProps.put(FieldName.APPLICATION_ID, gitData.getDefaultApplicationId());
+            analyticsProps.put(FieldName.BRANCH_NAME, gitData.getBranchName());
+            analyticsProps.put("gitHostingProvider", GitUtils.getGitProviderName(gitData.getRemoteUrl()));
         }
-        Map<String, Object> analyticsProps = new HashMap<>(Map.of(
-                FieldName.APPLICATION_ID, defaultApplicationId,
+        // Do not include the error data points in the map for success states
+        if(!StringUtils.isEmptyOrNull(errorMessage) || !StringUtils.isEmptyOrNull(errorType)) {
+            analyticsProps.put("errorMessage", errorMessage);
+            analyticsProps.put("errorType", errorType);
+        }
+        analyticsProps.putAll(Map.of(
                 FieldName.ORGANIZATION_ID, defaultIfNull(application.getOrganizationId(), ""),
                 "branchApplicationId", defaultIfNull(application.getId(), ""),
                 "isRepoPrivate", defaultIfNull(isRepoPrivate, ""),
-                "gitHostingProvider", defaultIfNull(gitHostingProvider, ""),
-                "isSystemGenerated", defaultIfNull(isSystemGenerated, ""),
-                FieldName.BRANCH_NAME, branchName
+                "isSystemGenerated", defaultIfNull(isSystemGenerated, "")
         ));
-
         return sessionUserService.getCurrentUser()
                 .map(user -> {
-                    // Do not include the error data points in the map for success states
-                    if(!StringUtils.isEmptyOrNull(errorMessage) || !StringUtils.isEmptyOrNull(errorType)) {
-                        analyticsProps.put("errorMessage", errorMessage);
-                        analyticsProps.put("errorType", errorType);
-                    }
                     analyticsService.sendEvent(eventName, user.getUsername(), analyticsProps);
                     return application;
                 });
