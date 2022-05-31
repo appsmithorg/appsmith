2cd2b2b6655da76654437d670614aae27ed39b8f
Aishwarya UR <aishwarya@appsmith.com>
2022-05-31 11:55:34 +0530

    Mongo CRUD JSON start

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/CRUD_JSONForm/Mongo_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/CRUD_JSONForm/Mongo_Spec.ts
@@ -1,11 +1,14 @@
 import { ObjectsRegistry } from "../../../../support/Objects/Registry";
 
-let guid: any;
+let guid: any, dsName: any;
+
 let agHelper = ObjectsRegistry.AggregateHelper,
   ee = ObjectsRegistry.EntityExplorer,
   locator = ObjectsRegistry.CommonLocators,
   homePage = ObjectsRegistry.HomePage,
-  dataSources = ObjectsRegistry.DataSources;
+  dataSources = ObjectsRegistry.DataSources,
+  deployMode = ObjectsRegistry.DeployMode,
+  table = ObjectsRegistry.Table;
 
 describe("Validate Mongo CRUD with JSON Form", () => {
   it("1. Create DS & then Add new Page and generate CRUD template using created datasource", () => {
@@ -34,4 +37,82 @@ describe("Validate Mongo CRUD with JSON Form", () => {
       agHelper.GetNClick(dataSources._generatePageBtn);
     });
   });
+
+  it.only("2. Create new app and Generate CRUD page using a new datasource", () => {
+    homePage.NavigateToHome();
+    homePage.CreateNewApplication();
+    agHelper.GetNClick(homePage._buildFromDataTableActionCard);
+    agHelper.GetNClick(dataSources._selectDatasourceDropdown);
+    agHelper.GetNClickByContains(
+      dataSources._dropdownOption,
+      "Connect New Datasource",
+    );
+
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      dataSources.CreatePlugIn("MongoDB");
+      guid = uid;
+      agHelper.RenameWithInPane("Mongo " + guid, false);
+      dataSources.FillMongoDSForm();
+      dataSources.TestSaveDatasource();
+      cy.wrap("Mongo " + guid).as("dsName");
+    });
+
+    agHelper.ValidateNetworkStatus("@getDatasourceStructure"); //Making sure table dropdown is populated
+    agHelper.WaitUntilToastDisappear("datasource updated successfully");
+    agHelper.GetNClick(dataSources._selectTableDropdown);
+    agHelper.GetNClickByContains(dataSources._dropdownOption, "friends");
+
+    GenerateCRUDNValidateDeployPage(
+      "103",
+      "Atelier graphique",
+      "Schmitt",
+      "customerNumber",
+    );
+
+    agHelper.NavigateBacktoEditor();
+    cy.get("@dsName").then(($dsName) => {
+      dsName = $dsName;
+    });
+  });
+
+  function GenerateCRUDNValidateDeployPage(
+    col1Text: string,
+    col2Text: string,
+    col3Text: string,
+    jsonFromHeader: string,
+  ) {
+    agHelper.GetNClick(dataSources._generatePageBtn);
+    agHelper.ValidateNetworkStatus("@replaceLayoutWithCRUDPage", 201);
+    agHelper.ValidateToastMessage("Successfully generated a page");
+    agHelper.ValidateNetworkStatus("@getActions", 200);
+    agHelper.ValidateNetworkStatus("@postExecute", 200);
+    agHelper.ValidateNetworkStatus("@updateLayout", 200);
+
+    agHelper.GetNClick(dataSources._visibleTextSpan("GOT IT"));
+    deployMode.DeployApp();
+
+    //Validating loaded table
+    agHelper.AssertElementExist(dataSources._selectedRow);
+    table.ReadTableRowColumnData(0, 0, 2000).then(($cellData) => {
+      expect($cellData).to.eq(col1Text);
+    });
+    table.ReadTableRowColumnData(0, 1, 200).then(($cellData) => {
+      expect($cellData).to.eq(col2Text);
+    });
+    table.ReadTableRowColumnData(0, 2, 200).then(($cellData) => {
+      expect($cellData).to.eq(col3Text);
+    });
+
+    //Validating loaded JSON form
+    cy.xpath(locator._spanButton("Update")).then((selector) => {
+      cy.wrap(selector)
+        .invoke("attr", "class")
+        .then((classes) => {
+          //cy.log("classes are:" + classes);
+          expect(classes).not.contain("bp3-disabled");
+        });
+    });
+    dataSources.AssertJSONFormHeader(0, 0, jsonFromHeader);
+  }
 });
