51ef8fc69776037fd5c2415b1b316115ce95b76d
Trisha Anand <trisha@appsmith.com>
2022-05-31 19:28:35 +0530

    Adding repository functionalities for UserGroup

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/UserGroup.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/UserGroup.java
@@ -1,6 +1,7 @@
 package com.appsmith.server.domains;
 
 import com.appsmith.external.models.BaseDomain;
+import com.appsmith.server.dtos.UserProfileDTO;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
@@ -8,13 +9,14 @@ import java.util.List;
 
 @Document
 public class UserGroup extends BaseDomain {
+
     @NotNull
     String name;
 
     String description;
 
-    // Note : While storing the users information, ensure all the sensitive and unnecessary data is cleared out before
-    // storing inside the user group.
-    List<User> users;
-    
+    // Note : While storing the users' information, ensure all the sensitive and unnecessary data is cleared out before
+    // storing inside the user group
+    List<UserProfileDTO> users;
+
 }
