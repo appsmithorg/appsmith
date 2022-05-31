4f08a1d17766f2cf9730c8eebbfd9b9895946d69
Aishwarya UR <aishwarya@appsmith.com>
2022-05-31 19:20:12 +0530

    New snaps to match CI run

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/LayoutValidation/WidgetsLayout_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/LayoutValidation/WidgetsLayout_spec.js
@@ -1,10 +1,10 @@
 describe("Visual regression tests", () => {
   // for any changes in UI, update the screenshot in snapshot folder, to do so:
-  //  1. Delete the required screenshot which you want to update.
-  //  2. Run test in headless mode with any browser except chrome.(to maintain same resolution in CI)
-  //  3. New screenshot will be generated in the snapshot folder.
+  //  1. Delete the required screenshot which you want to update
+  //  2. Run test in headless mode with any browser (to maintain same resolution in CI)
+  //  3. New screenshot will be generated in the snapshot folder
 
-  it("Veirfy Switch group inline enable/disbale", () => {
+  it("Verify SwitchGroup inline enable/disbale", () => {
     cy.dragAndDropToCanvas("switchgroupwidget", { x: 300, y: 700 });
     cy.wait(1000);
 
