ab5896389fedcbe5790a36fc91a0227fdb62de11
Shrikant Sharat Kandula <shrikant@appsmith.com>
2022-06-01 23:02:50 +0530

    Pull back!

--- app/server/build.sh
+++ app/server/build.sh
@@ -3,6 +3,19 @@
 # Remove previous dist directory
 rm -rf dist/
 
+run_tests=false
+for i in "$@"; do
+  if [[ $i == "-DskipTests" ]]; then
+    run_tests=true
+    break
+  fi
+done
+
+if $run_tests; then
+  # If tests will be run, let's pull some required images that often fail to be pulled from inside Maven's test run.
+  docker image pull testcontainers/ryuk:0.3.0
+fi
+
 # Build the code. $@ accepts all the parameters from the input command line and uses it in the maven build command
 mvn clean package "$@"
 
