f6e1af8f90d95c186d079129f64b1e5aabf584cb
Trisha Anand <trisha@appsmith.com>
2022-06-01 14:33:16 +0530

    Adding RBAC Policy Domain data structure

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomRbacPolicyRepositoryCEImpl.java
@@ -0,0 +1,13 @@
+package com.appsmith.server.repositories.ce;
+
+import com.appsmith.server.domains.RbacPolicy;
+import com.appsmith.server.repositories.BaseAppsmithRepositoryImpl;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.convert.MongoConverter;
+
+public class CustomRbacPolicyRepositoryCEImpl extends BaseAppsmithRepositoryImpl<RbacPolicy> implements CustomRbacPolicyRepositoryCE {
+
+    public CustomRbacPolicyRepositoryCEImpl(ReactiveMongoOperations mongoOperations, MongoConverter mongoConverter) {
+        super(mongoOperations, mongoConverter);
+    }
+}
