940138e42680714d51f4f92b524a57c0c15726fa
Pawan Kumar <pawan@appsmith.com>
2022-06-10 16:02:11 +0530

    set false in selector for hiding watermark (#390)

--- app/client/src/ee/selectors/organizationSelectors.tsx
+++ app/client/src/ee/selectors/organizationSelectors.tsx
@@ -1,1 +1,7 @@
 export * from "ce/selectors/organizationSelectors";
+
+// Note: this value will come from a organisation setting later on
+// But for now, it will be false for EE
+export const getShowBrandingBadge = () => {
+  return false;
+};
