0eaf3aac8692ebd10091833a8f651277c9055947
Sidhant Goel <sidhant@appsmith.com>
2022-06-14 17:12:22 +0530

    added comment for returning Mono<Set>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/SessionUserServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/SessionUserServiceCEImpl.java
@@ -38,6 +38,8 @@ public class SessionUserServiceCEImpl implements SessionUserServiceCE {
                 .map(auth -> (User) auth.getPrincipal());
     }
 
+    // Used mono istead of flux because operation to convert flux into a set is synchronized and this function takes care if that
+    // and hence calling function doesn't need to do extra operations for converting multiple fluxes into a set
     @Override
     public Mono<Set<String>> getCurrentPermissionGroups() {
         // TODO implement this method
