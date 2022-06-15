32eab97260411d2dc57da25d65f4e04f378311f7
Sidhant Goel <sidhant@appsmith.com>
2022-06-15 13:46:06 +0530

    simplified embedded mongodb config

--- app/server/appsmith-server/src/test/java/com/appsmith/server/configurations/EmbeddedMongoConfig.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/configurations/EmbeddedMongoConfig.java
@@ -5,35 +5,16 @@ import java.io.IOException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import de.flapdoodle.embed.mongo.config.ImmutableMongoCmdOptions;
 import de.flapdoodle.embed.mongo.config.ImmutableMongodConfig;
-import de.flapdoodle.embed.mongo.config.MongoCmdOptions;
 import de.flapdoodle.embed.mongo.config.MongodConfig;
-import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
 
 @Configuration
 public class EmbeddedMongoConfig {
-    public static int mongodPort;
-    public static String defaultHost = "localhost";
-    static {
-        try {
-            mongodPort = Network.getFreeServerPort();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     @Bean
     public ImmutableMongodConfig prepareMongodConfig() throws IOException {
-        ImmutableMongoCmdOptions cmdOptions = MongoCmdOptions.builder()
-                .build();
-
         ImmutableMongodConfig mongoConfigConfig = MongodConfig.builder()
                 .version(Version.Main.V4_2)
-                .net(new Net(mongodPort, Network.localhostIsIPv6()))
-                .cmdOptions(cmdOptions)
                 .build();
         return mongoConfigConfig;
     }
