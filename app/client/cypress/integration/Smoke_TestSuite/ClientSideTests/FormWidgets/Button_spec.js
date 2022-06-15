bf20805ce21de5b8ff8de14ca1243594e7cf2a46
apple <nandan@thinkify.io>
2022-06-15 11:56:48 +0530

    updated some more tests

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Button_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/FormWidgets/Button_spec.js
@@ -185,6 +185,7 @@ describe("Button Widget Functionality", function() {
   it("Toggle JS - Button-Unckeck Visible field Validation", function() {
     //Uncheck the disabled checkbox using JS and validate
     cy.get(widgetsPage.toggleVisible).click({ force: true });
+    cy.EnableAllCodeEditors();
     cy.testJsontext("visible", "false");
     cy.PublishtheApp();
     cy.get(publishPage.buttonWidget).should("not.exist");
@@ -192,6 +193,7 @@ describe("Button Widget Functionality", function() {
 
   it("Toggle JS - Button-Check Visible field Validation", function() {
     //Check the disabled checkbox using JS and Validate
+    cy.EnableAllCodeEditors();
     cy.testJsontext("visible", "true");
     cy.PublishtheApp();
     cy.get(publishPage.buttonWidget).should("be.visible");
