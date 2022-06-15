d1d3512a75e54cf98e9bb917160e8b25f0bd419e
Abhijeet <abhijeet@appsmith.com>
2022-06-15 22:34:56 +0530

    Remove unwanted testcase

--- app/server/appsmith-server/src/test/java/com/appsmith/server/helpers/CompareDslActionDTOTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/helpers/CompareDslActionDTOTest.java
@@ -12,49 +12,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompareDslActionDTOTest {
 
-    List<String> sortedActionIds = new LinkedList<>();
-    TreeSet<DslActionDTO> orderedActionSet = new TreeSet<>(new CompareDslActionDTO());
 
     @Test
-    public void convert_whenNullActionIds_returnsSetInInputOrder() {
-        DslActionDTO action1 = new DslActionDTO();
-        DslActionDTO action2 = new DslActionDTO();
-        action1.setName("testName");
-        action2.setName("testName2");
-        Set<DslActionDTO> unorderedSet = Set.of(action1, action2);
-        orderedActionSet.addAll(unorderedSet);
+    public void convert_whenEmptyActionIdSet_elementsDoesNotMaintainOrder() {
+        TreeSet<DslActionDTO> orderedActionSet = new TreeSet<>(new CompareDslActionDTO());
         List<String> sortedActionNames = new LinkedList<>();
-        for (DslActionDTO dslActionDTO : orderedActionSet) {
-            sortedActionNames.add(dslActionDTO.getName());
-        }
-        List<String> actionNames = new LinkedList<>();
-        for (DslActionDTO dslActionDTO : unorderedSet) {
-            actionNames.add(dslActionDTO.getName());
-        }
-        assertThat(sortedActionNames).isEqualTo(actionNames);
-    }
-
-    @Test
-    public void convert_whenEmptyActionIdSet_returnsSetInInputOrder() {
         DslActionDTO action1 = new DslActionDTO();
         DslActionDTO action2 = new DslActionDTO();
-        action1.setId("");
-        action2.setId("abcd");
+        action1.setName("");
+        action2.setName("abcd");
         Set<DslActionDTO> actionSet = Set.of(action1, action2);
         orderedActionSet.addAll(actionSet);
         for (DslActionDTO dslActionDTO : orderedActionSet) {
-            sortedActionIds.add(dslActionDTO.getId());
-        }
-        List<String> actionIds = new LinkedList<>();
-        for (DslActionDTO dslActionDTO : actionSet) {
-            actionIds.add(dslActionDTO.getId());
+            sortedActionNames.add(dslActionDTO.getName());
         }
         // Two lists are defined to be equal if they contain the same elements in the same order.
-        assertThat(sortedActionIds).isEqualTo(actionIds);
+        assertThat(sortedActionNames).contains("abcd", "");
     }
 
     @Test
     public void convert_whenNonEmptySet_returnsOrderedResult() {
+        TreeSet<DslActionDTO> orderedActionSet = new TreeSet<>(new CompareDslActionDTO());
+        List<String> sortedActionIds = new LinkedList<>();
         DslActionDTO action1 = new DslActionDTO();
         DslActionDTO action2 = new DslActionDTO();
         DslActionDTO action3 = new DslActionDTO();
