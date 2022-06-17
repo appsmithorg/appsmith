825d792456ffa14ec622574c3baeabdb85399d91
Satish Gandham <satish@appsmith.com>
2022-06-17 17:49:24 +0530

    - Remove no-sandbox option in puppeteer launch

--- app/client/perf/tests/initial-setup.js
+++ app/client/perf/tests/initial-setup.js
@@ -4,7 +4,7 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 (async () => {
   const browser = await puppeteer.launch({
-    args: ["--window-size=1920,1080", "--no-sandbox"],
+    args: ["--window-size=1920,1080"],
     ignoreHTTPSErrors: true,
   });
   let page = await browser.newPage();
