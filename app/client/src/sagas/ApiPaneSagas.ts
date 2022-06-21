f58451aa5f5ef743016ee12abe009df1d577db3d
Arsalan Yaldram <arsalanyaldram0211@outlook.com>
2022-06-21 19:27:34 +0530

    feat: upgrade to create react app 5 (#14000)
    * Updated Typescript types.
    
    * Typefixes after merge with release.
    
    * chore: GenericApiResponse Removed alltogether.
    
    * chore: resolved ApiResponse unknown errors removed PageListPayload.
    
    * Added shouldBeDefined.
    
    * fix: Resolved type errors.
    
    * fix: Typescript upgrade to 4.5 and type fixes.
    
    * feat: upgrade to cra 5
    
    * feat: uncomment service worker registeration
    
    * force secure websocket protocol
    
    * jest test fixes
    
    * fix: react function lint rule removed
    
    * fix: klona test case.
    
    * fix: typescirpt issues resolved
    
    * fix: timeout for colorpicker test and change env.
    
    * feat: update client-build.yml file
    
    * fix: remove brotliplugin use compression plugin
    
    * fix: build config fixed
    
    * fix: upgrade webpack plugin
    
    * fix: add branchbutton test to todo.
    
    * fix: remove branch button test.
    
    * fix: Add tailwind theme values, fix cypress tests
    
    * fix: Typescript type fixes.
    
    * feat: run jest tests in silent mode
    
    * fix: cypress rgb values add branchbutton jest test
    
    * fix: review comments, fixes for error.message
    
    * fix: increase cache size for the workbox
    
    * fix: remove OrgApi.ts file
    
    * fix: cypress.json file remove credentials
    
    * fix: downgrade react and react-dom packages
    
    Co-authored-by: rahulramesha <rahul@appsmith.com>

--- app/client/src/sagas/ApiPaneSagas.ts
+++ app/client/src/sagas/ApiPaneSagas.ts
@@ -13,7 +13,7 @@ import {
   ReduxActionWithMeta,
   ReduxFormActionTypes,
 } from "@appsmith/constants/ReduxActionConstants";
-import { getFormData } from "selectors/formSelectors";
+import { GetFormData, getFormData } from "selectors/formSelectors";
 import { API_EDITOR_FORM_NAME, QUERY_EDITOR_FORM_NAME } from "constants/forms";
 import {
   DEFAULT_API_ACTION_CONFIG,
@@ -32,7 +32,10 @@ import { Property } from "api/ActionAPI";
 import { createNewApiName, getQueryParams } from "utils/AppsmithUtils";
 import { getPluginIdOfPackageName } from "sagas/selectors";
 import { getAction, getActions, getPlugin } from "selectors/entitiesSelector";
-import { ActionData } from "reducers/entityReducers/actionsReducer";
+import {
+  ActionData,
+  ActionDataState,
+} from "reducers/entityReducers/actionsReducer";
 import {
   createActionRequest,
   setActionProperty,
@@ -234,11 +237,11 @@ function* handleUpdateBodyContentType(
 }
 
 function* updateExtraFormDataSaga() {
-  const formData = yield select(getFormData, API_EDITOR_FORM_NAME);
+  const formData: GetFormData = yield select(getFormData, API_EDITOR_FORM_NAME);
   const { values } = formData;
 
   // when initializing, check if theres a display format present, if not use Json display format as default.
-  const extraFormData = yield select(getDisplayFormat, values.id);
+  const extraFormData: GetFormData = yield select(getDisplayFormat, values.id);
 
   const headers: Array<{ key: string; value: string }> =
     get(values, "actionConfiguration.headers") || [];
@@ -501,8 +504,8 @@ function* formValueChangeSaga(
 
 function* handleActionCreatedSaga(actionPayload: ReduxAction<Action>) {
   const { id, pluginType } = actionPayload.payload;
-  const action: Action = yield select(getAction, id);
-  const data = { ...action };
+  const action: Action | undefined = yield select(getAction, id);
+  const data = action ? { ...action } : {};
 
   if (pluginType === PluginType.API) {
     yield put(initialize(API_EDITOR_FORM_NAME, omit(data, "name")));
@@ -519,12 +522,12 @@ function* handleActionCreatedSaga(actionPayload: ReduxAction<Action>) {
 }
 
 function* handleDatasourceCreatedSaga(actionPayload: ReduxAction<Datasource>) {
-  const plugin: Plugin = yield select(
+  const plugin: Plugin | undefined = yield select(
     getPlugin,
     actionPayload.payload.pluginId,
   );
   // Only look at API plugins
-  if (plugin.type !== PluginType.API) return;
+  if (plugin && plugin.type !== PluginType.API) return;
 
   history.push(
     datasourcesEditorIdURL({
@@ -547,7 +550,7 @@ function* handleCreateNewApiActionSaga(
   );
   const { pageId } = action.payload;
   if (pageId && pluginId) {
-    const actions: ActionData[] = yield select(getActions);
+    const actions: ActionDataState = yield select(getActions);
     const pageActions = actions.filter(
       (a: ActionData) => a.config.pageId === pageId,
     );
@@ -582,7 +585,7 @@ function* handleApiNameChangeSuccessSaga(
   action: ReduxAction<{ actionId: string }>,
 ) {
   const { actionId } = action.payload;
-  const actionObj = yield select(getAction, actionId);
+  const actionObj: Action | undefined = yield select(getAction, actionId);
   yield take(ReduxActionTypes.FETCH_ACTIONS_FOR_PAGE_SUCCESS);
   if (!actionObj) {
     // Error case, log to sentry
