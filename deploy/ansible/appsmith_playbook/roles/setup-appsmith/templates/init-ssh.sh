9f2cb59ddab9811d5388f6400854fd9a1ae826de
pratapaprasanna <pratapagoutham@gmail.com>
2022-06-21 19:41:35 +0530

    Add default user and setup ssh key

--- deploy/ansible/appsmith_playbook/roles/setup-appsmith/templates/init-ssh.sh
+++ deploy/ansible/appsmith_playbook/roles/setup-appsmith/templates/init-ssh.sh
@@ -6,3 +6,12 @@ if [[ ! -e "$authorized_keys_path" ]]; then
 		sudo cp ~/.ssh/authorized_keys "$authorized_keys_path"
 		sudo chown appsmith:appsmith "$authorized_keys_path"
 fi
+
+authorized_keys_ubuntu_path=/home/ubuntu/.ssh/authorized_keys
+if [[ ! -e "$authorized_keys_ubuntu_path" ]]; then
+		echo "Setting SSH key for ubuntu user"
+		sudo mkdir -p /home/ubuntu/.ssh/
+		sudo chmod -R 700 /home/ubuntu/.ssh/
+		sudo cp ~/.ssh/authorized_keys "$authorized_keys_ubuntu_path"
+		sudo chown -R ubuntu:appsmith /home/ubuntu/.ssh/
+fi
