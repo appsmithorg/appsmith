26a0dc6f701ad786098f7e5bfba241e0ff43b8c0
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2022-06-21 17:15:01 +0530

    initial filter

--- app/client/src/selectors/debuggerSelectors.tsx
+++ app/client/src/selectors/debuggerSelectors.tsx
@@ -1,15 +1,29 @@
 import { matchDatasourcePath } from "constants/routes";
+import { DataTree } from "entities/DataTree/dataTreeFactory";
 import { AppState } from "reducers";
 import { createSelector } from "reselect";
+import { isWidget } from "workers/evaluationUtils";
+import { getDataTree } from "./dataTreeSelectors";
 export const getDebuggerErrors = (state: AppState) => state.ui.debugger.errors;
 export const hideErrors = (state: AppState) => state.ui.debugger.hideErrors;
 export const getFilteredErrors = createSelector(
   getDebuggerErrors,
   hideErrors,
-  (errors, hideErrors) => {
+  getDataTree,
+  (errors, hideErrors, dataTree: DataTree) => {
     if (hideErrors) return {};
 
-    return errors;
+    const filteredErrors = Object.fromEntries(
+      Object.entries(errors).filter(([, error]) => {
+        const entity = error?.source?.name && dataTree[error.source.name];
+        if (entity && isWidget(entity) && !entity.isVisible) {
+          return false;
+        }
+        return true;
+      }),
+    );
+    console.log("my errors", errors, filteredErrors);
+    return filteredErrors;
   },
 );
 export const getCurrentDebuggerTab = (state: AppState) =>
