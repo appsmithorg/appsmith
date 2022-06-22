a98a1832ff29f93ce7e1de5057db91ce50316642
Pawan Kumar <pawan.stardust@gmail.com>
2022-06-22 12:23:13 +0530

    change icon + add hover state

--- app/client/src/pages/AppViewer/BackToHomeButton.tsx
+++ app/client/src/pages/AppViewer/BackToHomeButton.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router";
-import AppsIcon from "remixicon-react/Apps2LineIcon";
+import AppsIcon from "remixicon-react/AppsLineIcon";
 
 import { getCurrentUser } from "selectors/usersSelectors";
 import { ANONYMOUS_USERNAME } from "constants/userConstants";
+import { getSelectedAppTheme } from "selectors/appThemingSelectors";
 
 function BackToHomeButton() {
   const history = useHistory();
   const currentUser = useSelector(getCurrentUser);
+  const selectedTheme = useSelector(getSelectedAppTheme);
 
   // if user is not logged in, don't render anything
   if (currentUser?.username == ANONYMOUS_USERNAME) return null;
 
   return (
     <button
-      className="flex items-center gap-2 group t--back-to-home"
+      className="flex items-center gap-1 group t--back-to-home"
       onClick={() => {
         history.push("/applications");
       }}
     >
-      <AppsIcon className="w-5 h-5" />
+      <AppsIcon
+        className="p-1 w-7 h-7 group-hover:bg-gray-100"
+        style={{
+          borderRadius: selectedTheme.properties.borderRadius.appBorderRadius,
+        }}
+      />
       <span className="hidden md:block">Apps</span>
     </button>
   );
