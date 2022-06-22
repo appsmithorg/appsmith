14eb9b4cb5e03ff6304896debc94e5525e704117
Anagh Hegde <anagh@appsmith.com>
2022-06-22 21:50:04 +0530

    feat: Add API for git docs and supported shh keys (#14641)

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/constants/ErrorReferenceDocUrl.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/constants/ErrorReferenceDocUrl.java
@@ -2,11 +2,20 @@ package com.appsmith.external.constants;
 
 import lombok.Getter;
 
-@Getter
-public class ErrorReferenceDocUrl {
-    public static final String GIT_MERGE_CONFLICT = "https://docs.appsmith.com/core-concepts/version-control-with-git/merging-branches";
-    public static final String GIT_PULL_CONFLICT = "https://docs.appsmith.com/core-concepts/version-control-with-git/pull-and-sync";
-    public static final String GIT_DEPLOY_KEY = "https://docs.appsmith.com/core-concepts/version-control-with-git/connecting-to-git-repository#generating-a-deploy-key";
-    public static final String FILE_PATH_NOT_SET = "https://docs.appsmith.com/core-concepts/version-control-with-git/updating-local-file-path";
-    public static final String GIT_UPSTREAM_CHANGES = "https://docs.appsmith.com/core-concepts/version-control-with-git/working-with-branches#syncing-local-with-remote-branch";
+public enum ErrorReferenceDocUrl {
+
+    GIT_MERGE_CONFLICT("https://docs.appsmith.com/core-concepts/version-control-with-git/merging-branches"),
+    GIT_PULL_CONFLICT("https://docs.appsmith.com/core-concepts/version-control-with-git/pull-and-sync"),
+    GIT_DEPLOY_KEY("https://docs.appsmith.com/core-concepts/version-control-with-git/connecting-to-git-repository#generating-a-deploy-key"),
+    FILE_PATH_NOT_SET("https://docs.appsmith.com/core-concepts/version-control-with-git/updating-local-file-path"),
+    GIT_UPSTREAM_CHANGES("https://docs.appsmith.com/core-concepts/version-control-with-git/working-with-branches#syncing-local-with-remote-branch"),
+    DEPLOY_KEY_DOC_URL("https://docs.github.com/en/developers/overview/managing-deploy-keys");
+
+    private final String docUrl;
+
+    ErrorReferenceDocUrl(String docUrl) {
+        this.docUrl = docUrl;
+    }
+
+    public String getDocUrl() { return this.docUrl;}
 }
