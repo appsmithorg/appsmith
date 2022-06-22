d0de60f5492f0852bf75c7303e6b46ae1f4b1bfa
Akash N <akash@codemonk.in>
2022-06-22 11:14:29 +0530

    fix: remove templates tab notification

--- app/client/src/pages/Templates/TemplatesTabItem.tsx
+++ app/client/src/pages/Templates/TemplatesTabItem.tsx
@@ -1,25 +1,13 @@
-import { Popover2, Classes as Popover2Classes } from "@blueprintjs/popover2";
-import { useLocation } from "react-router";
-import { setTemplateNotificationSeenAction } from "actions/templateActions";
 import { TextType, Text, Classes } from "components/ads";
 import Icon, { IconSize } from "components/ads/Icon";
 import { Colors } from "constants/Colors";
-import { matchTemplatesPath } from "constants/routes";
-import { isNull } from "lodash";
-import React, { ReactNode, Suspense, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { ReactNode, Suspense } from "react";
 import {
   INTRODUCING_TEMPLATES,
   createMessage,
   TEMPLATE_NOTIFICATION_DESCRIPTION,
 } from "@appsmith/constants/messages";
-import {
-  getIsFetchingApplications,
-  getUserApplicationsWorkspacesList,
-} from "selectors/applicationSelectors";
-import { showTemplateNotificationSelector } from "selectors/templatesSelectors";
 import styled from "styled-components";
-import { AppState } from "reducers";
 
 const NotificationWrapper = styled.div`
   background-color: ${Colors.SEA_SHELL};
@@ -43,13 +31,6 @@ const NotificationWrapper = styled.div`
   .description {
     margin-top: ${(props) => props.theme.spaces[0] + 2}px;
   }
-`;
-
-const StyledPopover = styled.div`
-  .${Popover2Classes.POPOVER2_TARGET} {
-    display: flex;
-  }
-  display: flex;
 `;
 
 export function TemplateFeatureNotification() {
@@ -77,53 +58,5 @@ interface TemplatesTabItemProps {
 }
 
 export function TemplatesTabItem(props: TemplatesTabItemProps) {
-  const hasSeenNotification = useSelector(showTemplateNotificationSelector);
-  const isFetchingApplications = useSelector(getIsFetchingApplications);
-  const workspaceListLength = useSelector(
-    (state: AppState) => getUserApplicationsWorkspacesList(state).length,
-  );
-  const location = useLocation();
-  const dispatch = useDispatch();
-
-  const showNotification =
-    !hasSeenNotification &&
-    !isFetchingApplications &&
-    !isNull(hasSeenNotification) &&
-    workspaceListLength;
-
-  const setNotificationSeenFlag = () => {
-    dispatch(setTemplateNotificationSeenAction(true));
-  };
-
-  useEffect(() => {
-    if (matchTemplatesPath(location.pathname) && !hasSeenNotification) {
-      setNotificationSeenFlag();
-    }
-  }, [location.pathname, hasSeenNotification]);
-
-  return (
-    <Suspense fallback={<div />}>
-      <StyledPopover>
-        <Popover2
-          content={<TemplateFeatureNotification />}
-          enforceFocus={false}
-          isOpen={!!showNotification}
-          modifiers={{
-            offset: {
-              enabled: true,
-              options: {
-                offset: [0, 0],
-              },
-            },
-          }}
-          onClose={setNotificationSeenFlag}
-          placement="bottom-start"
-          portalClassName="templates-notification"
-          targetTagName="div"
-        >
-          {props.children}
-        </Popover2>
-      </StyledPopover>
-    </Suspense>
-  );
+  return <Suspense fallback={<div />}>{props.children}</Suspense>;
 }
