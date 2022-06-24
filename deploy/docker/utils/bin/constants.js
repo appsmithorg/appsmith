7dca12354e726d6f39998475e75a27b5c64e0fa8
Sumesh <sumesh@appsmith.com>
2022-06-24 17:11:14 +0530

    added new constants

--- deploy/docker/utils/bin/constants.js
+++ deploy/docker/utils/bin/constants.js
@@ -5,8 +5,14 @@ const RESTORE_PATH = "/appsmith-stacks/data/restore"
 
 const DUMP_FILE_NAME = "appsmith-data.archive"
 
+const BACKUP_ERROR_LOG_PATH = '/appsmith-stacks/logs/backup'
+
+const MIN_REQUIRED_DISK_SPACE_IN_BYTES = 536870912099999999 // 5GB
+
 module.exports = {
     BACKUP_PATH,
     RESTORE_PATH,
     DUMP_FILE_NAME,
+    BACKUP_ERROR_LOG_PATH,
+    MIN_REQUIRED_DISK_SPACE_IN_BYTES
 }
