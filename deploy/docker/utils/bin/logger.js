ee2acf358cc65b400fb08c73d60373b1523ad86f
Sumesh <sumesh@appsmith.com>
2022-06-24 17:12:46 +0530

    created error logger module

---
+++ deploy/docker/utils/bin/logger.js
@@ -0,0 +1,16 @@
+const fsPromises = require('fs/promises');
+const Constants = require('./constants');
+
+async function backup_error(err){
+    try {
+      await fsPromises.access(Constants.BACKUP_ERROR_LOG_PATH);
+    } catch (error) {
+      await fsPromises.mkdir(Constants.BACKUP_ERROR_LOG_PATH)
+    }
+    await fsPromises.appendFile(Constants.BACKUP_ERROR_LOG_PATH + '/error.log', new Date().toISOString() + ' ' + err + '\n');
+  }
+
+module.exports = {
+    backup_error
+  };
+  
