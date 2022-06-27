57edd376d5516dacf351f175a6a0ea24d8693a20
Sumesh Pradhan <sumesh@appsmith.com>
2022-06-27 13:10:39 +0530

    fix: escaped $host var (#412)

--- deploy/docker/templates/nginx/nginx-app-https.conf.template.sh
+++ deploy/docker/templates/nginx/nginx-app-https.conf.template.sh
@@ -100,7 +100,7 @@ server {
 
   location /auth {
     proxy_pass http://localhost:8081;
-    proxy_set_header Host $host;
+    proxy_set_header Host \$host;
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-Proto \$scheme;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
