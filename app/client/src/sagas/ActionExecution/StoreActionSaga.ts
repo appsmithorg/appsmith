dc7023778508e7ee50c70d1f70124af4aa4972e1
ohansFavour <fohanekwu@gmail.com>
2022-06-27 20:01:53 +0100

    support paths as keys in storevalue

--- app/client/src/sagas/ActionExecution/StoreActionSaga.ts
+++ app/client/src/sagas/ActionExecution/StoreActionSaga.ts
@@ -12,6 +12,7 @@ import { getCurrentGitBranch } from "selectors/gitSyncSelectors";
 import { getCurrentApplicationId } from "selectors/editorSelectors";
 import { AppStoreState } from "reducers/entityReducers/appReducer";
 import { ReduxActionTypes } from "@appsmith/constants/ReduxActionConstants";
+import _ from "lodash";
 
 export default function* storeValueLocally(
   action: StoreValueActionDescription["payload"],
@@ -22,7 +23,7 @@ export default function* storeValueLocally(
     const appStoreName = getAppStoreName(applicationId, branch);
     const existingStore = localStorage.getItem(appStoreName) || "{}";
     const parsedStore = JSON.parse(existingStore);
-    parsedStore[action.key] = action.value;
+    _.set(parsedStore, action.key, action.value);
     const storeString = JSON.stringify(parsedStore);
     localStorage.setItem(appStoreName, storeString);
     yield put(updateAppPersistentStore(parsedStore, action));
@@ -33,8 +34,8 @@ export default function* storeValueLocally(
     const existingStore: AppStoreState = yield select(getAppStoreData);
     const newTransientStore = {
       ...existingStore.transient,
-      [action.key]: action.value,
     };
+    _.set(newTransientStore, action.key, action.value);
     yield put(updateAppTransientStore(newTransientStore, action));
     AppsmithConsole.info({
       text: `store('${action.key}', '${action.value}', false)`,
