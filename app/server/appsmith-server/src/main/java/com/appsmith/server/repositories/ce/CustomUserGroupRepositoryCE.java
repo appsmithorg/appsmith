87d75db683ebf28bfb4467013e92813fe404006b
Trisha Anand <trisha@appsmith.com>
2022-06-27 13:31:41 +0530

    Removed UserGroup

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomUserGroupRepositoryCE.java
+++
@@ -1,8 +0,0 @@
-package com.appsmith.server.repositories.ce;
-
-import com.appsmith.server.domains.UserGroup;
-import com.appsmith.server.repositories.AppsmithRepository;
-
-public interface CustomUserGroupRepositoryCE extends AppsmithRepository<UserGroup> {
-
-}
