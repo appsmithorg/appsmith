b1d456d2c556c53ea27b07f77d1382d8ed056675
Trisha Anand <trisha@appsmith.com>
2022-06-29 20:00:32 +0530

    feat: Create workspace

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/RbacPolicyServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/RbacPolicyServiceCEImpl.java
@@ -6,9 +6,13 @@ import com.appsmith.server.services.AnalyticsService;
 import com.appsmith.server.services.BaseService;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 
 import javax.validation.Validator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RbacPolicyServiceCEImpl extends BaseService<RbacPolicyRepository, RbacPolicy, String> implements RbacPolicyServiceCE{
 
@@ -20,5 +24,32 @@ public class RbacPolicyServiceCEImpl extends BaseService<RbacPolicyRepository, R
                                    AnalyticsService analyticsService) {
 
         super(scheduler, validator, mongoConverter, reactiveMongoTemplate, repository, analyticsService);
+    }
+
+    @Override
+    public Mono<RbacPolicy> create(RbacPolicy policy) {
+        String userId = policy.getUserId();
+        if (StringUtils.hasLength(userId)) {
+
+            return repository.findByUserId(userId)
+                    // if a policy for the uesr already exists, add the new permission groups to the policy instead
+                    // of creating a new one.
+                    .map(rbacPolicy -> {
+                        Set<String> permissionGroupIds = rbacPolicy.getPermissionGroupIds();
+                        if (permissionGroupIds == null) {
+                            permissionGroupIds = new HashSet<>();
+                        }
+                        permissionGroupIds.addAll(policy.getPermissionGroupIds());
+                        return rbacPolicy;
+                    })
+                    // If the policy with this userId does not exist, then validate the same before it gets saved.
+                    .switchIfEmpty(
+                            Mono.just(policy)
+                                    .flatMap(newPolicy -> super.validateObject(newPolicy))
+                    )
+                    .flatMap(repository::save);
+        } else {
+            return super.create(policy);
+        }
     }
 }
