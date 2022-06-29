d17b056449e45b804838eb082b0daf2e1f80db15
Preet <preetsidhu.bits@gmail.com>
2022-06-29 12:25:41 -0400

    refactor editor wrapper

---
+++ app/client/src/components/editorComponents/LazyCodeEditorWrapper/index.layout.tsx
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+
+import { Icon } from "components/ads";
+
+export const ReadOnlyInput = styled.input`
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0) !important;
+  font-family: monospace !important;
+  font-weight: 400 !important;
+  line-height: 21px !important;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  min-height: inherit;
+  height: -webkit-fill-available !important;
+`;
+
+export const HighlighedCodeContainer = styled("div")<{
+  containsObject: boolean;
+}>`
+  width: 100%;
+  background-color: #fff !important;
+  font-family: monospace !important;
+  font-weight: 400 !important;
+  line-height: 21px !important;
+
+  min-height: inherit;
+  padding: 6px 10px !important;
+
+  pre {
+    margin: 0 !important;
+    overflow: hidden !important;
+    font-family: monospace !important;
+    padding: 0 !important;
+    background: white !important;
+
+    word-wrap: break-word !important;
+    white-space: pre-wrap !important;
+    word-break: normal !important;
+
+    code {
+      background: white !important;
+      font-family: monospace !important;
+      line-height: 1.495 !important;
+    }
+  }
+`;
+
+export const LintErrorContainer = styled.div`
+  background-color: white;
+  padding: 8px;
+
+  position: absolute;
+  bottom: 30px;
+  left: 50px;
+  box-shadow: 0px 4px 8px 1px rgba(0, 0, 0, 0.3);
+
+  display: flex;
+`;
+
+export const IconWrapper = styled(Icon)`
+  margin-right: 4px;
+`;
+
+export const LazyEditorWrapper = styled("div")`
+  position: relative;
+`;
+
+export const ContentWrapper = styled("div")<{ containsCode: boolean }>`
+  overflow: hidden;
+  height: ${({ containsCode }) => (containsCode ? "auto" : "38px")};
+  min-height: 36px;
+  border: 1px solid;
+  border-color: inherit;
+
+  div:nth-child(1) {
+    min-height: 36px;
+  }
+`;
+
+export const NoCodeText = styled("div")`
+  color: #858282 !important;
+`;
--- app/client/src/components/editorComponents/LazyCodeEditorWrapper/index.tsx
+++ app/client/src/components/editorComponents/LazyCodeEditorWrapper/index.tsx
@@ -4,9 +4,10 @@ import { useSelector } from "react-redux";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 import { duotoneLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import ascetic from "react-syntax-highlighter/dist/esm/styles/hljs/ascetic";
 
 import Editor from "components/editorComponents/CodeEditor";
-import { EditorWrapper } from "./CodeEditor/styledComponents";
+import { EditorWrapper } from "../CodeEditor/styledComponents";
 import { replayHighlightClass } from "globalStyles/portals";
 import {
   EvaluationError,
@@ -14,90 +15,18 @@ import {
   PropertyEvaluationErrorType,
 } from "utils/DynamicBindingUtils";
 import { AppState } from "reducers";
-import styled from "styled-components";
-import { Icon, IconSize } from "components/ads";
+import { IconSize } from "components/ads";
+import {
+  ContentWrapper,
+  HighlighedCodeContainer,
+  IconWrapper,
+  LazyEditorWrapper,
+  LintErrorContainer,
+  NoCodeText,
+  ReadOnlyInput,
+} from "./index.layout";
 
 SyntaxHighlighter.registerLanguage("javascript", js);
-
-export const ReadOnlyInput = styled.input`
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0) !important;
-  font-family: monospace !important;
-  font-weight: 400 !important;
-  line-height: 21px !important;
-
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 2;
-  min-height: inherit;
-  height: -webkit-fill-available !important;
-`;
-
-export const HighlighedCodeContainer = styled("div")<{ containsCode: boolean }>`
-  width: 100%;
-  background-color: #fff !important;
-  font-family: monospace !important;
-  font-weight: 400 !important;
-  line-height: 21px !important;
-
-  min-height: inherit;
-  padding: 6px 10px !important;
-
-  pre {
-    margin: 0 !important;
-    overflow: hidden !important;
-    font-family: monospace !important;
-    padding: 0 !important;
-    background: white !important;
-
-    word-wrap: break-word !important;
-    white-space: pre-wrap !important;
-    word-break: normal !important;
-
-    code {
-      background: white !important;
-      font-family: monospace !important;
-      line-height: 1.495 !important;
-    }
-  }
-`;
-
-const LintErrorContainer = styled.div`
-  background-color: white;
-  padding: 8px;
-
-  position: absolute;
-  bottom: 30px;
-  left: 50px;
-  box-shadow: 0px 4px 8px 1px rgba(0, 0, 0, 0.3);
-
-  display: flex;
-`;
-
-const IconWrapper = styled(Icon)`
-  margin-right: 4px;
-`;
-
-const LazyEditorWrapper = styled("div")`
-  position: relative;
-`;
-
-const ContentWrapper = styled("div")<{ containsCode: boolean }>`
-  overflow: hidden;
-  height: ${({ containsCode }) => (containsCode ? "auto" : "38px")};
-  min-height: 36px;
-  border: 1px solid;
-  border-color: inherit;
-
-  div:nth-child(1) {
-    min-height: 36px;
-  }
-`;
-
-const NoCodeText = styled("div")`
-  color: #858282 !important;
-`;
 
 /**
  * Shimming request idle callback as it is not avaialble in Safari browser.
@@ -139,6 +68,7 @@ function CodeEditor(props: any) {
   const [lintError, setLintError] = useState<string>("");
   const [showLintError, setShowLintError] = useState<boolean>(false);
   const [containsCode, setContainsCode] = useState<boolean>(false);
+  const [containsObject, setContainsObject] = useState<boolean>(false);
   const [text, setText] = useState<string>("");
   const dynamicData = useSelector((state: AppState) => state.evaluations.tree);
   let handle: number;
@@ -177,17 +107,27 @@ function CodeEditor(props: any) {
 
   useEffect(() => {
     // Check if input value contains code
-    let str: string = props?.input?.value || "";
+    let str: string = getText();
     if (str && typeof str === "string" && str?.indexOf("{{") > -1)
       setContainsCode(true);
+    if (Array.isArray(str) || typeof str === "object") {
+      setText(JSON.stringify(str, null, 2));
+      setContainsCode(true);
+      setContainsObject(true);
+      return;
+    }
     // If !inputValue; then use placeholder
     if (!str?.toString()?.length) str = props?.placeholder || "";
-    setText(
-      Array.isArray(str) || typeof str === "object"
-        ? JSON.stringify(str, null, 2)
-        : str,
-    );
+    setText(str);
   }, [props?.input?.value, props?.placeholder]);
+
+  const getText = (): string => {
+    try {
+      return JSON.parse(props?.input?.value);
+    } catch (e) {
+      return props?.input?.value || "";
+    }
+  };
 
   const highlightedText = () => {
     if (!containsCode) {
@@ -196,7 +136,7 @@ function CodeEditor(props: any) {
     return (
       <SyntaxHighlighter
         language="javascript"
-        style={duotoneLight}
+        style={containsObject ? ascetic : duotoneLight}
         wrapLongLines
       >
         {text}
