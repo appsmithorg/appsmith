c25eaf5a8a1ed975e26d875a2362fe803b6d94cd
Abhijeet <abhijeet@appsmith.com>
2022-07-01 10:16:51 +0530

    Remove unrelated changes

--- app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 @EnableScheduling
 public class ServerApplication {
 
