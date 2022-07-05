d132b28729a2244383342f56ff1e5fd4baba421c
Parthvi Goswami <parthvigoswami@Parthvis-MacBook-Pro.local>
2022-07-05 21:58:01 +0530

    fix deleteBranch_spec

--- app/client/cypress/integration/Smoke_TestSuite/Application/PromisesApp_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/Application/PromisesApp_spec.js
@@ -7,6 +7,7 @@ const jsEditorLocators = require("../../../locators/JSEditor.json");
 let agHelper = ObjectsRegistry.AggregateHelper,
   ee = ObjectsRegistry.EntityExplorer,
   jsEditor = ObjectsRegistry.JSEditor;
+const newPage = "TableTest";
 
 describe("JSEditor tests", function() {
   before(() => {
@@ -17,29 +18,36 @@ describe("JSEditor tests", function() {
   });
   it("Testing promises with resetWidget, storeValue action and API call", () => {
     cy.NavigateToAPI_Panel();
-    cy.CreateAPI("Api12");
+    cy.CreateAPI("TC1api");
     cy.enterDatasourceAndPath("https://mock-api.appsmith.com/", "users");
-    cy.SaveAndRunAPI();
-    cy.ResponseStatusCheck("200");
+    cy.WaitAutoSave();
     cy.CheckAndUnfoldEntityItem("PAGES");
     jsEditor.CreateJSObject(
       `export default {
-        myFun1: () => {
-          resetWidget('Switch1')
-            .then(() => { 
-              storeValue('selectedValue', Select1.selectedOptionValue)
-              showAlert(appsmith.store.selectedValue)
-              return resetWidget('Select1')
+        myFun1: async () => { //comment
+          await this.clearStore()		//clear store value before running the case
+          return resetWidget('Switch1')
+            .then(() => {
+              resetWidget('Select1')
+              resetWidget('Table1')
+              return this.myFun2() 		//return a toast message
                 .then(() => {
-                  storeValue('defaultSelected', Select1.defaultOptionValue)
-                  showAlert(appsmith.store.defaultSelected)
-          }) })
-          return this.myFun2()
-        },  
+                  storeValue('selected', Select1.selectedOptionValue)
+                  showAlert(appsmith.store.selected) }) }) 		//return a second toast message
+            .catch(() =>  {
+              resetWidget('RadioGroup1')
+              showAlert("Couldn't execute all the success call steps, hence now in the fail part") })
+        },
         myFun2: () => {
-          return Api12.run()
-            .then (() => showAlert("Success running API query"))
-            .catch (() => showAlert("Couldn't run API query"))
+          return TC1api.run()
+            .then (() => {return showAlert("Success running API query")})
+            .catch (() => {return showAlert("Couldn't run API query")})
+        },
+        clearStore: async () => { //function to clear store values
+          Object.keys(appsmith.store).forEach((eachKey) => {
+            storeValue(eachKey, 'undefined')	
+            //return showAlert(eachKey)
+           })
         }
       }`,
       {
@@ -49,23 +57,32 @@ describe("JSEditor tests", function() {
         shouldCreateNewJSObj: true,
       },
     );
+    cy.wait(10000);
     // run the jsObject
     cy.get(jsEditorLocators.runButton)
       .first()
       .click();
-    //cy.wait(10000)
+    cy.wait(3000);
+    cy.wait("@postExecute").should(
+      "have.nested.property",
+      "response.body.responseMeta.status",
+      200,
+    );
     cy.pause();
+    cy.Createpage(newPage);
+    cy.get(`.t--entity-item:contains(${newPage})`).click();
+    cy.wait(1000);
     cy.get(".t--entity-item:contains(Page1)")
       .first()
       .click();
     cy.wait("@getPage");
     // verify text in the text widget
     cy.get(".t--draggable-textwidget span")
-      .last()
+      .eq(2)
       .invoke("text")
       .then((text) => {
         expect(text).to.equal(
-          "Step 4: Value is GREEN and will default to GREEN",
+          "Step 4: Value is Green and will default to undefined",
         );
       });
     // toggle off the switch
@@ -76,27 +93,45 @@ describe("JSEditor tests", function() {
       "contain",
       "Switch widget has changed",
     );
+    cy.readTabledataPublish("0", "1").then((cellData) => {
+      expect(cellData).to.be.empty;
+    });
     // select an option from select widget
     cy.get(".bp3-button.select-button").click({ force: true });
     cy.get(".menu-item-text")
       .eq(2)
       .click({ force: true });
+    // verify text is visible
+    cy.get(".t--draggable-textwidget span")
+      .eq(2)
+      .invoke("text")
+      .then((text) => {
+        expect(text).to.equal("Step 4: Value is Green and will default to Red");
+      });
+    // navigate to page2
+    cy.get(`.t--entity-item:contains(${newPage})`)
+      .first()
+      .click();
+    cy.wait("@getPage");
+    cy.get(".t--entity-item:contains(Page1)")
+      .first()
+      .click();
+    cy.wait("@getPage");
+    cy.readTabledataPublish("0", "1").then((cellData) => {
+      expect(cellData).not.to.be.empty;
+    });
     // hit audio play button and trigger actions
     cy.openPropertyPane("audiowidget");
-
-    // debug here, audio widget fails and also jsobject gives error sometimes!!
-
-    cy.widgetText("Audio1", widgetsPage.audioWidget, commonlocators.audioInner);
-    cy.get(commonlocators.onPlay).click();
+    cy.get(widgetsPage.autoPlay).click({ force: true });
     cy.wait("@postExecute").should(
       "have.nested.property",
       "response.body.responseMeta.status",
       200,
     );
-    cy.get(homePage.toastMessage).should("contain", "GREEN");
     cy.get(homePage.toastMessage).should(
       "contain",
       "Success running API query",
+      "GREEN",
     );
   });
 });
