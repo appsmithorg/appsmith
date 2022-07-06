4970bf3322e471099de69375e38120cabf5f4f2e
Pawan Kumar <pawan.stardust@gmail.com>
2022-07-06 13:09:28 +0530

    code split for hiding branding badge

--- app/client/src/ce/pages/AdminSettings/config/general.tsx
+++ app/client/src/ce/pages/AdminSettings/config/general.tsx
@@ -10,75 +10,85 @@ import {
 } from "@appsmith/pages/AdminSettings/config/types";
 import BrandingBadge from "pages/AppViewer/BrandingBadge";
 
+export const APPSMITH_INSTANCE_NAME_SETTING_SETTING = {
+  id: "APPSMITH_INSTANCE_NAME",
+  category: SettingCategories.GENERAL,
+  controlType: SettingTypes.TEXTINPUT,
+  controlSubType: SettingSubtype.TEXT,
+  label: "Instance Name",
+  placeholder: "appsmith/prod",
+};
+
+export const APPSMITH__ADMIN_EMAILS_SETTING = {
+  id: "APPSMITH_ADMIN_EMAILS",
+  category: SettingCategories.GENERAL,
+  controlType: SettingTypes.TEXTINPUT,
+  controlSubType: SettingSubtype.EMAIL,
+  label: "Admin Email",
+  subText:
+    "Emails of the users who can modify instance settings (Comma Separated)",
+  placeholder: "Jane@example.com",
+  validate: (value: string) => {
+    if (
+      value &&
+      !value
+        .split(",")
+        .reduce((prev, curr) => prev && isEmail(curr.trim()), true)
+    ) {
+      return "Please enter valid email id(s)";
+    }
+  },
+};
+
+export const APPSMITH_DOWNLOAD_DOCKER_COMPOSE_FILE_SETTING = {
+  id: "APPSMITH_DOWNLOAD_DOCKER_COMPOSE_FILE",
+  action: () => {
+    const { host, protocol } = window.location;
+    window.open(
+      `${protocol}//${host}${apiRequestConfig.baseURL}${UserApi.downloadConfigURL}`,
+      "_blank",
+    );
+  },
+  category: SettingCategories.GENERAL,
+  controlType: SettingTypes.BUTTON,
+  label: "Generated Docker Compose File",
+  text: "Download",
+};
+
+export const APPSMITH_DISABLE_TELEMETRY_SETTING = {
+  id: "APPSMITH_DISABLE_TELEMETRY",
+  category: SettingCategories.GENERAL,
+  controlType: SettingTypes.TOGGLE,
+  label: "Share anonymous usage data",
+  subText: "Share anonymous usage data to help improve the product",
+  toggleText: (value: boolean) =>
+    value ? "Don't share any data" : "Share Anonymous Telemetry",
+};
+
+export const APPSMITH_HIDE_WATERMARK_SETTING = {
+  id: "APPSMITH_HIDE_WATERMARK",
+  category: SettingCategories.GENERAL,
+  controlType: SettingTypes.CHECKBOX,
+  label: "Appsmith Watermark",
+  text: "Hide Appsmith Watermark",
+  needsUpgrade: true,
+  isDisabled: () => true,
+  textSuffix: <BrandingBadge />,
+  upgradeLogEventName: "ADMIN_SETTINGS_UPGRADE_WATERMARK",
+  upgradeLogMessage:
+    "Hello, I would like to upgrade and start using branding options.",
+};
+
 export const config: AdminConfigType = {
   type: SettingCategories.GENERAL,
   controlType: SettingTypes.GROUP,
   title: "General",
   canSave: true,
   settings: [
-    {
-      id: "APPSMITH_INSTANCE_NAME",
-      category: SettingCategories.GENERAL,
-      controlType: SettingTypes.TEXTINPUT,
-      controlSubType: SettingSubtype.TEXT,
-      label: "Instance Name",
-      placeholder: "appsmith/prod",
-    },
-    {
-      id: "APPSMITH_ADMIN_EMAILS",
-      category: SettingCategories.GENERAL,
-      controlType: SettingTypes.TEXTINPUT,
-      controlSubType: SettingSubtype.EMAIL,
-      label: "Admin Email",
-      subText:
-        "Emails of the users who can modify instance settings (Comma Separated)",
-      placeholder: "Jane@example.com",
-      validate: (value: string) => {
-        if (
-          value &&
-          !value
-            .split(",")
-            .reduce((prev, curr) => prev && isEmail(curr.trim()), true)
-        ) {
-          return "Please enter valid email id(s)";
-        }
-      },
-    },
-    {
-      id: "APPSMITH_DOWNLOAD_DOCKER_COMPOSE_FILE",
-      action: () => {
-        const { host, protocol } = window.location;
-        window.open(
-          `${protocol}//${host}${apiRequestConfig.baseURL}${UserApi.downloadConfigURL}`,
-          "_blank",
-        );
-      },
-      category: SettingCategories.GENERAL,
-      controlType: SettingTypes.BUTTON,
-      label: "Generated Docker Compose File",
-      text: "Download",
-    },
-    {
-      id: "APPSMITH_DISABLE_TELEMETRY",
-      category: SettingCategories.GENERAL,
-      controlType: SettingTypes.TOGGLE,
-      label: "Share anonymous usage data",
-      subText: "Share anonymous usage data to help improve the product",
-      toggleText: (value: boolean) =>
-        value ? "Don't share any data" : "Share Anonymous Telemetry",
-    },
-    {
-      id: "APPSMITH_HIDE_WATERMARK",
-      category: SettingCategories.GENERAL,
-      controlType: SettingTypes.CHECKBOX,
-      label: "Appsmith Watermark",
-      text: "Hide Appsmith Watermark",
-      needsUpgrade: true,
-      isDisabled: () => true,
-      textSuffix: <BrandingBadge />,
-      upgradeLogEventName: "ADMIN_SETTINGS_UPGRADE_WATERMARK",
-      upgradeLogMessage:
-        "Hello, I would like to upgrade and start using branding options.",
-    },
+    APPSMITH_INSTANCE_NAME_SETTING_SETTING,
+    APPSMITH__ADMIN_EMAILS_SETTING,
+    APPSMITH_DOWNLOAD_DOCKER_COMPOSE_FILE_SETTING,
+    APPSMITH_DISABLE_TELEMETRY_SETTING,
+    APPSMITH_HIDE_WATERMARK_SETTING,
   ],
 } as AdminConfigType;
