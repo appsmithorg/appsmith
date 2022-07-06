7819565a08c3eca082764f079d2f2cfa3a34d4db
Ayush Pahwa <ayush@appsmith.com>
2022-07-06 11:06:45 +0530

    Update: placeholder texts for where clause

--- app/client/src/components/formControls/WhereClauseControl.tsx
+++ app/client/src/components/formControls/WhereClauseControl.tsx
@@ -31,14 +31,14 @@ export type whereClauseValueType = {
 const valueFieldConfig: any = {
   key: "value",
   controlType: "QUERY_DYNAMIC_INPUT_TEXT",
-  placeholderText: "value",
+  placeholderText: "Value",
 };
 
 // Form config for the key field
 const keyFieldConfig: any = {
   key: "key",
   controlType: "QUERY_DYNAMIC_INPUT_TEXT",
-  placeholderText: "key",
+  placeholderText: "Column name",
 };
 
 // Form config for the condition field
