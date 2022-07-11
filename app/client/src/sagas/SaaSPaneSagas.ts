0ca5f6c011d144136738e51dc91cb57ab056f476
arunvjn <32433245+arunvjn@users.noreply.github.com>
2022-07-11 09:36:29 +0530

    chore: code changes to support clean urls v2 (#14757)

--- app/client/src/sagas/SaaSPaneSagas.ts
+++ app/client/src/sagas/SaaSPaneSagas.ts
@@ -9,18 +9,21 @@ import { Datasource } from "entities/Datasource";
 import { Action, PluginType } from "entities/Action";
 import { Plugin } from "api/PluginApi";
 import { saasEditorApiIdURL, saasEditorDatasourceIdURL } from "RouteBuilder";
+import { getCurrentPageId } from "selectors/editorSelectors";
 
 function* handleDatasourceCreatedSaga(actionPayload: ReduxAction<Datasource>) {
   const plugin: Plugin | undefined = yield select(
     getPlugin,
     actionPayload.payload.pluginId,
   );
+  const pageId: string = yield select(getCurrentPageId);
   // Only look at SAAS plugins
   if (!plugin) return;
   if (plugin.type !== PluginType.SAAS) return;
 
   history.push(
     saasEditorDatasourceIdURL({
+      pageId,
       pluginPackageName: plugin.packageName,
       datasourceId: actionPayload.payload.id,
       params: { from: "datasources" },
@@ -31,11 +34,13 @@ function* handleDatasourceCreatedSaga(actionPayload: ReduxAction<Datasource>) {
 function* handleActionCreatedSaga(actionPayload: ReduxAction<Action>) {
   const { id, pluginId } = actionPayload.payload;
   const plugin: Plugin | undefined = yield select(getPlugin, pluginId);
+  const pageId: string = yield select(getCurrentPageId);
 
   if (!plugin) return;
   if (plugin.type !== "SAAS") return;
   history.push(
     saasEditorApiIdURL({
+      pageId,
       pluginPackageName: plugin.packageName,
       apiId: id,
       params: {
