3824435f7d67d8216db4abf42296c37331711112
Aswath K <aswath.sana@gmail.com>
2022-07-14 10:30:30 +0530

    chore: Adds analytics for Property Pane keyboard navigation (#13703)
    * Adds analytics for DropdownControl
    
    * Add isUpdatedViaKeyboard to WIDGET_PROPERTY_UPDATE
    
    * fix: jest test for iconselectorcontrol
    
    * refactor: Interaction analytics event dispatching moved to a hook
    
    * refactor: event collection logic to HOC
    
    * Analytics for SwitchControl
    
    * Analytics for CodeEditor
    
    * Analytics for InputTextControl
    
    * Analytics for ColorPicker
    
    * fix issue with ColorPicker
    
    * refactor to re-use ref
    
    * rename the hook to ts
    
    * Analytics for StepControl
    
    * Analytics for ButtonTabComponent
    
    * Analytics for NumericInputControl
    
    * Analytics for IconSelector
    
    * fix: failed test
    
    * Analytics for ActionSelector
    
    * fix: failed jest test
    
    * fix: jest test for StepControl
    
    * Analytics for Option Control
    
    * Analytics for LocationSearchControl
    
    * Adds Analytics for Tab key
    
    * analytics for Property pane connections
    
    * Add analytics for widget name
    
    * Adds analytics for copy & delete button
    
    * fix issue with widget name analytics
    
    * add useCallback for tab keydown handler for button
    
    * Create separate Event handling for design system
    
    * removes unused imports
    
    * changes ADS_EVENT to DS_EVENT
    
    * Changes AdsEventTypes to DSEventTypes
    
    * Changes AdsEventDetail to DSEventDetail
    
    * changes occurences of Ads to DS
    
    * avoids creation of intermediate function
    
    * removes trace of 'analytics' from DS components
    
    * rename dispatchDSEvent to emitDSEvent
    
    * fix: Cypress selector

--- app/client/src/components/ads/Switch.tsx
+++ app/client/src/components/ads/Switch.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import styled from "constants/DefaultTheme";
 import { ISwitchProps, Switch } from "@blueprintjs/core";
 import { Colors } from "constants/Colors";
 import { replayHighlightClass } from "globalStyles/portals";
+import useDSEvent from "utils/hooks/useDSEvent";
+import { DSEventTypes } from "utils/AppsmithUtils";
 
 const StyledSwitch = styled(Switch)`
   &&&&& input:checked ~ span {
@@ -15,6 +17,35 @@ const StyledSwitch = styled(Switch)`
 `;
 
 export default function AdsSwitch(props: ISwitchProps) {
+  const { emitDSEvent, eventEmitterRefCallback } = useDSEvent<HTMLDivElement>(
+    true,
+  );
+
+  const emitKeyPressEvent = useCallback(
+    (key: string) => {
+      emitDSEvent({
+        component: "AdsSwitch",
+        event: DSEventTypes.KEYPRESS,
+        meta: {
+          key,
+        },
+      });
+    },
+    [emitDSEvent],
+  );
+
+  const handleKeydown = (e: React.KeyboardEvent) => {
+    switch (e.key) {
+      case "Enter":
+      case " ":
+        emitKeyPressEvent(e.key);
+        break;
+      case "Tab":
+        emitKeyPressEvent(`${e.shiftKey ? "Shift+" : ""}${e.key}`);
+        break;
+    }
+  };
+
   return (
     <StyledSwitch
       {...props}
@@ -23,6 +54,8 @@ export default function AdsSwitch(props: ISwitchProps) {
           ? props.className + " " + replayHighlightClass
           : replayHighlightClass
       }
+      inputRef={eventEmitterRefCallback}
+      onKeyDown={handleKeydown}
       tabIndex={0}
     />
   );
