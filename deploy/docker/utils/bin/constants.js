1355a62a4548d0965470a6f9d9082d5ed7f2a637
Sumesh <sumesh@appsmith.com>
2022-07-18 13:17:45 +0530

    chore: udpated min disk to 2gb from 5gb

--- deploy/docker/utils/bin/constants.js
+++ deploy/docker/utils/bin/constants.js
@@ -7,7 +7,7 @@ const DUMP_FILE_NAME = "appsmith-data.archive"
 
 const BACKUP_ERROR_LOG_PATH = '/appsmith-stacks/logs/backup'
 
-const MIN_REQUIRED_DISK_SPACE_IN_BYTES = 5368709120 // 5GB
+const MIN_REQUIRED_DISK_SPACE_IN_BYTES = 2147483648 // 2GB
 
 const DURATION_BETWEEN_BACKUP_ERROR_MAILS_IN_MILLI_SEC = 21600000 // 6 hrs
 
