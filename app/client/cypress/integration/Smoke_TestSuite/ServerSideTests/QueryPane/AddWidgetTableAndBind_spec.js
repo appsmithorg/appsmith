2d7f9f432f50d2db323ccafe61f986e8127c4164
balajisoundar <balaji@appsmith.com>
2022-07-19 22:41:12 +0530

    fix cypress

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/QueryPane/AddWidgetTableAndBind_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/QueryPane/AddWidgetTableAndBind_spec.js
@@ -83,7 +83,7 @@ describe("Addwidget from Query and bind with other widgets", function() {
   });
 
   it("5. Input widget test with default value from table widget[Bug#4136]", () => {
-    cy.openPropertyPane("tablewidget");
+    cy.openPropertyPane("tablewidgetv2");
     cy.get(".t--property-pane-title").click({ force: true });
     cy.get(".t--property-pane-title")
       .type("TableUpdated", { delay: 300 })
