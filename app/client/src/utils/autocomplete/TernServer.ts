f08640edf60f6c851c308fa429ad0fa7b0a59c98
arunvjn <arun@appsmith.com>
2022-07-19 17:58:16 +0530

    Some refactors

--- app/client/src/utils/autocomplete/TernServer.ts
+++ app/client/src/utils/autocomplete/TernServer.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // Heavily inspired from https://github.com/codemirror/CodeMirror/blob/master/addon/tern/tern.js
-import { Server, Def, CompletionsQuery } from "tern";
+import { Def } from "tern";
 import ecma from "constants/defs/ecmascript.json";
 import lodash from "constants/defs/lodash.json";
 import base64 from "constants/defs/base64-js.json";
@@ -8,22 +8,12 @@ import moment from "constants/defs/moment.json";
 import xmlJs from "constants/defs/xmlParser.json";
 import forge from "constants/defs/forge.json";
 import {
-  getDynamicBindings,
-  getDynamicStringSegments,
-  isDynamicValue,
-} from "utils/DynamicBindingUtils";
-import {
   GLOBAL_DEFS,
   GLOBAL_FUNCTIONS,
 } from "utils/autocomplete/EntityDefinitions";
 import { FieldEntityInformation } from "components/editorComponents/CodeEditor/EditorConfig";
 import { ENTITY_TYPE } from "entities/DataTree/dataTreeFactory";
 import SortRules from "./dataTypeSortRules";
-import {
-  Query,
-  Document,
-  QueryRegistry,
-} from "codemirror/node_modules/@types/tern";
 import CodeMirror, { Hint, Pos, cmpPos, TernOptions } from "codemirror";
 import "imports-loader?type=commonjs&imports=tern!codemirror/addon/tern/tern";
 import "tern/lib/infer";
@@ -125,7 +115,7 @@ class AppsmithTernServer extends CodeMirror.TernServer {
       },
     });
 
-    const sortAndFilterCompletions = this.sortAndFilterCompletions.bind(this);
+    const sortAndFilterCompletions = _sortAndFilterCompletions.bind(this);
 
     this.getHint = new Proxy(this.getHint, {
       apply(target, thisArg, argArray) {
@@ -133,7 +123,7 @@ class AppsmithTernServer extends CodeMirror.TernServer {
         argArray[1] = function(data: { from: any; to: any; list: any[] }) {
           data.list = sortAndFilterCompletions(data.list, true, "");
           console.log("Process further");
-          callback.apply(thisArg, [data]);
+          callback.call(thisArg, data);
         };
         //@ts-expect-error
         target.apply(thisArg, argArray);
@@ -154,161 +144,6 @@ class AppsmithTernServer extends CodeMirror.TernServer {
     else if (/^fn\(/.test(type)) return AutocompleteDataType.FUNCTION;
     else if (/^\[/.test(type)) return AutocompleteDataType.ARRAY;
     else return AutocompleteDataType.OBJECT;
-  }
-
-  sortAndFilterCompletions(
-    completions: any[],
-    findBestMatch: boolean,
-    bestMatchSearch: string,
-  ) {
-    const {
-      entityName,
-      entityType,
-      expectedType = AutocompleteDataType.UNKNOWN,
-    } = this.fieldEntityInformation;
-    type CompletionType =
-      | "DATA_TREE"
-      | "MATCHING_TYPE"
-      | "OTHER"
-      | "CONTEXT"
-      | "JS"
-      | "LIBRARY";
-    const completionType: Record<CompletionType, Completion[]> = {
-      MATCHING_TYPE: [],
-      DATA_TREE: [],
-      CONTEXT: [],
-      JS: [],
-      LIBRARY: [],
-      OTHER: [],
-    };
-    completions.forEach((completion) => {
-      if (entityName && completion.data.name === entityName) {
-        return;
-      }
-      if (completion.data.origin) {
-        if (
-          completion.data.origin &&
-          completion.data.origin.startsWith("DATA_TREE")
-        ) {
-          if (completion.data.name.includes(".")) {
-            // nested paths (with ".") should only be used for best match
-            if (completion.data.type === expectedType) {
-              completionType.MATCHING_TYPE.push(completion);
-            }
-          } else if (
-            completion.data.origin === "DATA_TREE.APPSMITH.FUNCTIONS"
-          ) {
-            // Global functions should be in best match as well as DataTree
-            if (
-              !entityType ||
-              ENTITY_TYPE.ACTION === entityType ||
-              ENTITY_TYPE.JSACTION === entityType ||
-              ENTITY_TYPE.WIDGET === entityType
-            ) {
-              completionType.MATCHING_TYPE.push(completion);
-              completionType.DATA_TREE.push(completion);
-            }
-          } else {
-            // All top level entities are set in data tree
-            completionType.DATA_TREE.push(completion);
-          }
-          return;
-        }
-        if (
-          completion.data.origin === "[doc]" ||
-          completion.data.origin === "customDataTree"
-        ) {
-          // [doc] are variables defined in the current context
-          // customDataTree are implicit context defined by platform
-          completionType.CONTEXT.push(completion);
-          return;
-        }
-        if (
-          completion.data.origin === "ecmascript" ||
-          completion.data.origin === "base64-js"
-        ) {
-          completionType.JS.push(completion);
-          return;
-        }
-        if (completion.data.origin.startsWith("LIB/")) {
-          completionType.LIBRARY.push(completion);
-          return;
-        }
-      }
-
-      // Generally keywords or other unCategorised completions
-      completionType.OTHER.push(completion);
-    });
-    completionType.DATA_TREE = completionType.DATA_TREE.sort(
-      (a: Completion, b: Completion) => {
-        if (a.type === "FUNCTION" && b.type !== "FUNCTION") {
-          return 1;
-        } else if (a.type !== "FUNCTION" && b.type === "FUNCTION") {
-          return -1;
-        }
-        return a.text.toLowerCase().localeCompare(b.text.toLowerCase());
-      },
-    );
-    completionType.MATCHING_TYPE = completionType.MATCHING_TYPE.filter((c) =>
-      c.text.toLowerCase().startsWith(bestMatchSearch.toLowerCase()),
-    );
-    if (findBestMatch && completionType.MATCHING_TYPE.length) {
-      const sortedMatches: Completion[] = [];
-      const groupedMatches = _.groupBy(completionType.MATCHING_TYPE, (c) => {
-        const name = c.text.split(".")[0];
-        const entityInfo = this.defEntityInformation.get(name);
-        if (!entityInfo) return c.text;
-        return c.text.replace(name, entityInfo.subType);
-      });
-
-      const expectedRules = SortRules[expectedType as AutocompleteDataType];
-      for (const [key, value] of Object.entries(groupedMatches)) {
-        const name = key.split(".")[0];
-        if (name === "JSACTION") {
-          sortedMatches.push(...value);
-        } else if (expectedRules.indexOf(key) !== -1) {
-          sortedMatches.push(...value);
-        }
-      }
-
-      sortedMatches.sort((a, b) => {
-        let aRank = 0;
-        let bRank = 0;
-        const aName = a.text.split(".")[0];
-        const bName = b.text.split(".")[0];
-        const aEntityInfo = this.defEntityInformation.get(aName);
-        const bEntityInfo = this.defEntityInformation.get(bName);
-        if (!aEntityInfo) return -1;
-        if (!bEntityInfo) return 1;
-        if (aEntityInfo.type === entityType) {
-          aRank = aRank + 1;
-        }
-        if (bEntityInfo.type === entityType) {
-          bRank = bRank + 1;
-        }
-        return aRank - bRank;
-      });
-      completionType.MATCHING_TYPE = _.take(sortedMatches, 3);
-      if (completionType.MATCHING_TYPE.length) {
-        completionType.MATCHING_TYPE.unshift(
-          createCompletionHeader("Best Match"),
-        );
-        completionType.DATA_TREE.unshift(
-          createCompletionHeader("Search Results"),
-        );
-      }
-    } else {
-      // Clear any matching type because we dont want to find best match
-      completionType.MATCHING_TYPE = [];
-    }
-    return [
-      ...completionType.CONTEXT,
-      ...completionType.MATCHING_TYPE,
-      ...completionType.DATA_TREE,
-      ...completionType.LIBRARY,
-      ...completionType.JS,
-      ...completionType.OTHER,
-    ];
   }
 
   updateDef(
@@ -349,3 +184,157 @@ export const createCompletionHeader = (name: string): Completion => ({
   type: AutocompleteDataType.UNKNOWN,
   isHeader: true,
 });
+
+function _sortAndFilterCompletions(
+  this: AppsmithTernServer,
+  completions: any[],
+  findBestMatch: boolean,
+  bestMatchSearch: string,
+) {
+  const {
+    entityName,
+    entityType,
+    expectedType = AutocompleteDataType.UNKNOWN,
+  } = this.fieldEntityInformation;
+  type CompletionType =
+    | "DATA_TREE"
+    | "MATCHING_TYPE"
+    | "OTHER"
+    | "CONTEXT"
+    | "JS"
+    | "LIBRARY";
+  const completionType: Record<CompletionType, Completion[]> = {
+    MATCHING_TYPE: [],
+    DATA_TREE: [],
+    CONTEXT: [],
+    JS: [],
+    LIBRARY: [],
+    OTHER: [],
+  };
+  completions.forEach((completion) => {
+    if (entityName && completion.data.name === entityName) {
+      return;
+    }
+    if (completion.data.origin) {
+      if (
+        completion.data.origin &&
+        completion.data.origin.startsWith("DATA_TREE")
+      ) {
+        if (completion.data.name.includes(".")) {
+          // nested paths (with ".") should only be used for best match
+          if (completion.data.type === expectedType) {
+            completionType.MATCHING_TYPE.push(completion);
+          }
+        } else if (completion.data.origin === "DATA_TREE.APPSMITH.FUNCTIONS") {
+          // Global functions should be in best match as well as DataTree
+          if (
+            !entityType ||
+            ENTITY_TYPE.ACTION === entityType ||
+            ENTITY_TYPE.JSACTION === entityType ||
+            ENTITY_TYPE.WIDGET === entityType
+          ) {
+            completionType.MATCHING_TYPE.push(completion);
+            completionType.DATA_TREE.push(completion);
+          }
+        } else {
+          // All top level entities are set in data tree
+          completionType.DATA_TREE.push(completion);
+        }
+        return;
+      }
+      if (
+        completion.data.origin === "[doc]" ||
+        completion.data.origin === "customDataTree"
+      ) {
+        // [doc] are variables defined in the current context
+        // customDataTree are implicit context defined by platform
+        completionType.CONTEXT.push(completion);
+        return;
+      }
+      if (
+        completion.data.origin === "ecmascript" ||
+        completion.data.origin === "base64-js"
+      ) {
+        completionType.JS.push(completion);
+        return;
+      }
+      if (completion.data.origin.startsWith("LIB/")) {
+        completionType.LIBRARY.push(completion);
+        return;
+      }
+    }
+
+    // Generally keywords or other unCategorised completions
+    completionType.OTHER.push(completion);
+  });
+  completionType.DATA_TREE = completionType.DATA_TREE.sort(
+    (a: Completion, b: Completion) => {
+      if (a.type === "FUNCTION" && b.type !== "FUNCTION") {
+        return 1;
+      } else if (a.type !== "FUNCTION" && b.type === "FUNCTION") {
+        return -1;
+      }
+      return a.text.toLowerCase().localeCompare(b.text.toLowerCase());
+    },
+  );
+  completionType.MATCHING_TYPE = completionType.MATCHING_TYPE.filter((c) =>
+    c.text.toLowerCase().startsWith(bestMatchSearch.toLowerCase()),
+  );
+  if (findBestMatch && completionType.MATCHING_TYPE.length) {
+    const sortedMatches: Completion[] = [];
+    const groupedMatches = _.groupBy(completionType.MATCHING_TYPE, (c) => {
+      const name = c.text.split(".")[0];
+      const entityInfo = this.defEntityInformation.get(name);
+      if (!entityInfo) return c.text;
+      return c.text.replace(name, entityInfo.subType);
+    });
+
+    const expectedRules = SortRules[expectedType as AutocompleteDataType];
+    for (const [key, value] of Object.entries(groupedMatches)) {
+      const name = key.split(".")[0];
+      if (name === "JSACTION") {
+        sortedMatches.push(...value);
+      } else if (expectedRules.indexOf(key) !== -1) {
+        sortedMatches.push(...value);
+      }
+    }
+
+    sortedMatches.sort((a, b) => {
+      let aRank = 0;
+      let bRank = 0;
+      const aName = a.text.split(".")[0];
+      const bName = b.text.split(".")[0];
+      const aEntityInfo = this.defEntityInformation.get(aName);
+      const bEntityInfo = this.defEntityInformation.get(bName);
+      if (!aEntityInfo) return -1;
+      if (!bEntityInfo) return 1;
+      if (aEntityInfo.type === entityType) {
+        aRank = aRank + 1;
+      }
+      if (bEntityInfo.type === entityType) {
+        bRank = bRank + 1;
+      }
+      return aRank - bRank;
+    });
+    completionType.MATCHING_TYPE = _.take(sortedMatches, 3);
+    if (completionType.MATCHING_TYPE.length) {
+      completionType.MATCHING_TYPE.unshift(
+        createCompletionHeader("Best Match"),
+      );
+      completionType.DATA_TREE.unshift(
+        createCompletionHeader("Search Results"),
+      );
+    }
+  } else {
+    // Clear any matching type because we dont want to find best match
+    completionType.MATCHING_TYPE = [];
+  }
+  return [
+    ...completionType.CONTEXT,
+    ...completionType.MATCHING_TYPE,
+    ...completionType.DATA_TREE,
+    ...completionType.LIBRARY,
+    ...completionType.JS,
+    ...completionType.OTHER,
+  ];
+}
