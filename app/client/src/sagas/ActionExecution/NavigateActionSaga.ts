a712d5eca1db9aa6adbdf5c2c57f1bcba8aa39a1
Rimil Dey <rimil@appsmith.com>
2022-07-21 15:06:02 +0530

    Add validations for url/page name, add error message

--- app/client/src/sagas/ActionExecution/NavigateActionSaga.ts
+++ app/client/src/sagas/ActionExecution/NavigateActionSaga.ts
@@ -11,6 +11,7 @@ import { setDataUrl } from "sagas/PageSagas";
 import AppsmithConsole from "utils/AppsmithConsole";
 import { NavigateActionDescription } from "entities/DataTree/actionTriggers";
 import { builderURL, viewerURL } from "RouteBuilder";
+import { TriggerFailureError } from "./errorUtils";
 
 export enum NavigationTargetType {
   SAME_WINDOW = "SAME_WINDOW",
@@ -30,25 +31,59 @@ const isValidUrlScheme = (url: string): boolean => {
   );
 };
 
+const isValidURL = (url: string): boolean => {
+  return (
+    url.match(
+      /\(?(?:(http|https|ftp|mailto|tel):\/\/)?(?:((?:[^\W\s]|\.|-|[:]{1})+)@{1})?((?:www.)?(?:[^\W\s]|\.|-)+[\.][^\W\s]{2,4}|localhost(?=\/)|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::(\d*))?([\/]?[^\s\?]*[\/]{1})*(?:\/?([^\s\n\?\[\]\{\}\#]*(?:(?=\.)){1}|[^\s\n\?\[\]\{\}\.\#]*)?([\.]{1}[^\s\?\#]*)?)?(?:\?{1}([^\s\n\#\[\]]*))?([\#][^\s\n]*)?\)?/g,
+    ) !== null
+  );
+};
+
+const isValidPageName = (
+  pageNameOrUrl: string,
+  pageList: Page[],
+): Page | undefined => {
+  return _.find(pageList, (page: Page) => page.pageName === pageNameOrUrl);
+};
+
 export default function* navigateActionSaga(
   action: NavigateActionDescription["payload"],
 ) {
   const pageList: Page[] = yield select(getPageList);
+
   const {
     pageNameOrUrl,
     params,
     target = NavigationTargetType.SAME_WINDOW,
   } = action;
-  const page = _.find(
-    pageList,
-    (page: Page) => page.pageName === pageNameOrUrl,
-  );
-  if (page) {
+
+  const page = isValidPageName(pageNameOrUrl, pageList);
+
+  if (isValidURL(pageNameOrUrl)) {
+    AnalyticsUtil.logEvent("NAVIGATE", {
+      navUrl: pageNameOrUrl,
+    });
+
+    let url = pageNameOrUrl + getQueryStringfromObject(params);
+
+    // Add a default protocol if it doesn't exist.
+    if (!isValidUrlScheme(url)) {
+      url = "https://" + url;
+    }
+
+    if (target === NavigationTargetType.SAME_WINDOW) {
+      window.location.assign(url);
+    } else if (target === NavigationTargetType.NEW_WINDOW) {
+      window.open(url, "_blank");
+    }
+  } else if (page) {
     const currentPageId: string = yield select(getCurrentPageId);
+
     AnalyticsUtil.logEvent("NAVIGATE", {
       pageName: pageNameOrUrl,
       pageParams: params,
     });
+
     const appMode: APP_MODE = yield select(getAppMode);
     const path =
       appMode === APP_MODE.EDIT
@@ -60,6 +95,7 @@ export default function* navigateActionSaga(
             pageId: page.pageId,
             params,
           });
+
     if (target === NavigationTargetType.SAME_WINDOW) {
       history.push(path);
       if (currentPageId === page.pageId) {
@@ -76,18 +112,6 @@ export default function* navigateActionSaga(
       },
     });
   } else {
-    AnalyticsUtil.logEvent("NAVIGATE", {
-      navUrl: pageNameOrUrl,
-    });
-    let url = pageNameOrUrl + getQueryStringfromObject(params);
-    // Add a default protocol if it doesn't exist.
-    if (!isValidUrlScheme(url)) {
-      url = "https://" + url;
-    }
-    if (target === NavigationTargetType.SAME_WINDOW) {
-      window.location.assign(url);
-    } else if (target === NavigationTargetType.NEW_WINDOW) {
-      window.open(url, "_blank");
-    }
+    throw new TriggerFailureError("Enter a valid URL or page name");
   }
 }
