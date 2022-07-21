e4a9c1c404c7fdc5520861a63af8e855006eeb42
keyurparalkar <keyur@appsmith.com>
2022-07-21 15:45:36 +0530

    fix: addressed feedback from dilip

--- app/client/src/widgets/FilePickerWidgetV2/widget/index.tsx
+++ app/client/src/widgets/FilePickerWidgetV2/widget/index.tsx
@@ -137,10 +137,10 @@ const FilePickerGlobalStyles = createGlobalStyle<{
         -webkit-mask-size: 20px;
         background: #858282;
         position: absolute;
-        top: 29%;
-        left: 27%;
-        width: 15px;
-        height: 15px;
+        top: 32%;
+        left: 32%;
+        width: 12px;
+        height: 12px;
       }
     }
   }
@@ -175,6 +175,8 @@ const FilePickerGlobalStyles = createGlobalStyle<{
   /* Style the + add more files button on top right corner of the upload modal */  
   .uppy-DashboardContent-addMore {
     color: var(--wds-accent-color);
+    width: 100px;
+
     &:hover {
       background-color: ${Colors.ATHENS_GRAY};
       color: var(--wds-accent-color);
@@ -182,6 +184,17 @@ const FilePickerGlobalStyles = createGlobalStyle<{
 
     & svg {
       fill: var(--wds-accent-color) !important;
+      position: absolute;
+      top: 38%;
+      right: 11%;
+
+      &:hover {
+        background-color: ${Colors.ATHENS_GRAY};
+      }
+    }
+
+    & .uppy-DashboardContent-addMoreCaption {
+      margin-left: 15px;
     }
   }
   /*********************************************************/
