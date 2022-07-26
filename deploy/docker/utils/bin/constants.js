bb947a4c6710500e6a7f85dd4daffac3c8c8e9d1
Sumesh <sumesh@appsmith.com>
2022-07-26 12:33:02 +0530

    feat: new constant for last err mail rs

--- deploy/docker/utils/bin/constants.js
+++ deploy/docker/utils/bin/constants.js
@@ -5,7 +5,9 @@ const RESTORE_PATH = "/appsmith-stacks/data/restore"
 
 const DUMP_FILE_NAME = "appsmith-data.archive"
 
-const BACKUP_ERROR_LOG_PATH = '/appsmith-stacks/logs/backup'
+const BACKUP_ERROR_LOG_PATH = "/appsmith-stacks/logs/backup"
+
+const LAST_ERROR_MAIL_TS = "/appsmith-stacks/data/backup/last-error-mail-ts"
 
 const MIN_REQUIRED_DISK_SPACE_IN_BYTES = 2147483648 // 2GB
 
@@ -15,6 +17,7 @@ module.exports = {
     BACKUP_PATH,
     RESTORE_PATH,
     DUMP_FILE_NAME,
+    LAST_ERROR_MAIL_TS,
     BACKUP_ERROR_LOG_PATH,
     MIN_REQUIRED_DISK_SPACE_IN_BYTES,
     DURATION_BETWEEN_BACKUP_ERROR_MAILS_IN_MILLI_SEC,
