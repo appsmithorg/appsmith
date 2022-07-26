f544427100fef8c841360bb28fcf1d63828b2291
balajisoundar <balaji@appsmith.com>
2022-07-26 16:26:40 +0530

    fix defaultValue

--- app/client/src/widgets/TableWidgetV2/widget/propertyConfig/PanelConfig/ButtonProperties.ts
+++ app/client/src/widgets/TableWidgetV2/widget/propertyConfig/PanelConfig/ButtonProperties.ts
@@ -298,6 +298,7 @@ export default {
       },
       isBindProperty: true,
       isTriggerProperty: false,
+      defaultValue: ButtonVariantTypes.PRIMARY,
       validation: {
         type: ValidationTypes.TABLE_PROPERTY,
         params: {
