29c17531e0b3bc27866087025cdb4b57ff231b0d
arunvjn <arun@appsmith.com>
2022-07-26 22:02:45 +0530

    Added cypress to assert autocompletion post copy pasting a widget

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/Autcomplete-bugs.spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/Autcomplete-bugs.spec.js
@@ -2,12 +2,12 @@ const explorer = require("../../../../locators/explorerlocators.json");
 const dynamicInputLocators = require("../../../../locators/DynamicInput.json");
 
 describe("Autocomplete bug fixes", function() {
+  const modifierKey = Cypress.platform === "darwin" ? "meta" : "ctrl";
   it("Verifies if selectedRow is in best match", function() {
     cy.get(explorer.addWidget).click();
     cy.dragAndDropToCanvas("tablewidgetv2", { x: 200, y: 200 });
     cy.dragAndDropToCanvas("textwidget", { x: 200, y: 600 });
     cy.openPropertyPane("textwidget");
-    cy.EnableAllCodeEditors();
     cy.EnableAllCodeEditors();
     cy.get(".CodeMirror textarea")
       .first()
@@ -35,6 +35,49 @@ describe("Autocomplete bug fixes", function() {
         cy.get(`${dynamicInputLocators.hints} li`)
           .eq(1)
           .should("have.text", "selectedRow");
+      });
+  });
+
+  it("Verifies if selectedRow is in best match", function() {
+    cy.get(explorer.addWidget).click();
+    cy.dragAndDropToCanvas("buttonwidget", { x: 200, y: 200 });
+    cy.get(`#div-selection-0`).click({
+      force: true,
+    });
+    cy.get("body").type(`{${modifierKey}}{a}`);
+    cy.wait(500);
+    cy.get("body").type(`{${modifierKey}}{c}`);
+    cy.wait(500);
+    cy.get("body").type(`{${modifierKey}}{v}`);
+    cy.wait(500);
+    cy.openPropertyPane("buttonwidget");
+    cy.EnableAllCodeEditors();
+    cy.get(".CodeMirror textarea")
+      .first()
+      .focus()
+      .type("{ctrl}{shift}{downarrow}")
+      .then(($cm) => {
+        if ($cm.val() !== "") {
+          cy.get(".CodeMirror textarea")
+            .first()
+            .clear({
+              force: true,
+            });
+        }
+        cy.get(".CodeMirror textarea")
+          .first()
+          .type("{{Butto", {
+            force: true,
+            parseSpecialCharSequences: false,
+          });
+        cy.wait(500);
+        cy.get(dynamicInputLocators.hints).should("exist");
+        cy.get(`${dynamicInputLocators.hints} li`)
+          .eq(0)
+          .should("have.text", "Best Match");
+        cy.get(`${dynamicInputLocators.hints} li`)
+          .eq(1)
+          .should("have.text", "Button1Copy.text");
       });
   });
 });
