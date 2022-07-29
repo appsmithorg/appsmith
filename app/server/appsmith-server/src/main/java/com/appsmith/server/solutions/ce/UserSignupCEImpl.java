56b268e229d74ef326fdc48a641af2b1f079a6cf
Shrikant Sharat Kandula <shrikant@appsmith.com>
2022-07-29 14:15:17 +0530

    Fix potential delay in sending setup completion

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce/UserSignupCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce/UserSignupCEImpl.java
@@ -204,9 +204,10 @@ public class UserSignupCEImpl implements UserSignupCE {
                     userData.setUseCase(userFromRequest.getUseCase());
 
                     return Mono.when(
-                            userDataService.updateForUser(user, userData)
-                                    .then(configService.getInstanceId())
-                                    .doOnSuccess(instanceId -> {
+                            userDataService.updateForUser(user, userData),
+                            configService.getInstanceId()
+                                    .map(instanceId -> {
+                                        log.debug("Installation setup complete.");
                                         analyticsService.sendEvent(
                                                 AnalyticsEvents.INSTALLATION_SETUP_COMPLETE.getEventName(),
                                                 instanceId,
