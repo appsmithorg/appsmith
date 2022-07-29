03a245247cf45c9c2c8b68b320653b744b8969c1
Aman Agarwal <aman@appsmith.com>
2022-07-29 13:54:53 +0530

    fix: preinstall script for bundling shared packages

--- packages/ast/build/index.es.js
+++
@@ -1,5 +0,0 @@
-const isNullOrWhitespace = (value) => value === undefined || value === null || !value.trim();
-const isNullX = (value) => value === undefined || value === null;
-
-export { isNullOrWhitespace, isNullX };
-//# sourceMappingURL=index.es.js.map
