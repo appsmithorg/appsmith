fff655c102a1fd19ce3e35e51d7cdff12d46d2f6
Pawan Kumar <pawan.stardust@gmail.com>
2022-08-01 17:13:31 +0530

    remove boot from useOnupgrade

--- app/client/src/utils/hooks/useOnUpgrade.ts
+++ app/client/src/utils/hooks/useOnUpgrade.ts
@@ -1,9 +1,4 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-
-import bootIntercom from "utils/bootIntercom";
 import { getAppsmithConfigs } from "@appsmith/configs";
-import { getCurrentUser } from "selectors/usersSelectors";
 import { createMessage, UPGRADE_TO_EE_GENERIC } from "ce/constants/messages";
 import AnalyticsUtil, { EventName } from "utils/AnalyticsUtil";
 
@@ -16,12 +11,6 @@ type Props = {
 };
 
 const useOnUpgrade = (props: Props) => {
-  const user = useSelector(getCurrentUser);
-
-  useEffect(() => {
-    bootIntercom(user);
-  }, [user?.email]);
-
   const { intercomMessage, logEventData, logEventName } = props;
 
   const triggerIntercom = (message: string) => {
