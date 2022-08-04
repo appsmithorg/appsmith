9ec4f2b371a3baa808f87ad624c24dd2a367434c
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-08-04 19:01:44 +0530

    Remove Menu from ads

--- app/client/src/components/ads/index.ts
+++ app/client/src/components/ads/index.ts
@@ -60,9 +60,6 @@ export * from "./IconSelector";
 
 export { default as ListSegmentHeader } from "./ListSegmentHeader";
 
-export { default as Menu } from "./Menu";
-export * from "./Menu";
-
 export { default as MenuDivider } from "./MenuDivider";
 
 export { default as MenuItem } from "./MenuItem";
