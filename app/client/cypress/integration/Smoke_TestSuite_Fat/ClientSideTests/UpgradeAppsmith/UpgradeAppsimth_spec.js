eb15af73452d8c6b34ca415b2787cef568b389db
Arslan <83570904+arslanhaiderbuttar@users.noreply.github.com>
2022-08-05 11:56:08 +0500

    add new workflow for upgrade scenarios

--- app/client/cypress/integration/Smoke_TestSuite_Fat/ClientSideTests/UpgradeAppsmith/UpgradeAppsimth_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite_Fat/ClientSideTests/UpgradeAppsmith/UpgradeAppsimth_spec.js
@@ -4,34 +4,25 @@ const testdata = require("../../../../fixtures/testdata.json");
 const testUrl = "http://localhost:5001/v1/parent/cmd";
 describe("Upgrade appsmith version", () => {
   it("Upgrade Appsmith version and verify the Applications", () => {
+    cy.GetCWD(testUrl);
+
     cy.log("Stop the container");
     cy.StopTheContainer(testUrl, "appsmith"); // stop the old container
     cy.wait(2000);
 
+    cy.GetCWD(testUrl);
+
     cy.log("Get path");
     cy.GetPath(testUrl, "appsmith").then((path) => {
+      cy.log(path);
       path = path.split("  ");
       path = path[1].split(" ");
-      path = path[0].slice(9, -7);
+      path = path[0].slice(0, -7);
       cy.log(path);
 
+      cy.GetCWD(testUrl);
+
       cy.log("Start old stack container");
-      cy.CreateAContainer(
-        testUrl,
-        path + "/oldstack/160",
-        "v1.6.0",
-        "appsmith-160",
-      );
-      cy.wait(45000);
-
-      cy.log("Verify Logs");
-      cy.GetAndVerifyLogs(testUrl, "appsmith"); // Get and verify the logs
-
-      cy.log("Stop the old stack container");
-      cy.StopTheContainer(testUrl, "appsmith-160"); // stop the old container
-      cy.wait(2000);
-
-      cy.log("Update old stack container");
       cy.CreateAContainer(
         testUrl,
         path + "/oldstack/160",
@@ -39,6 +30,9 @@ describe("Upgrade appsmith version", () => {
         "appsmith-160-updated",
       );
       cy.wait(45000);
+
+      cy.log("Verify Logs");
+      cy.GetAndVerifyLogs(testUrl, "appsmith"); // Get and verify the logs
     });
 
     //verify the Applications after upgrade
@@ -56,48 +50,31 @@ describe("Upgrade appsmith version", () => {
     cy.get(".t--buttongroup-widget")
       .children()
       .should("have.length", 3);
-
-    cy.log("Stop the container");
-    cy.StopTheContainer(testUrl, "appsmith-160-updated"); // stop the old container
-    cy.wait(2000);
   });
 
   it("Upgrade Appsmith from CE to EE and verify the Applications", () => {
     cy.log("Stop the container");
-    cy.StopTheContainer(testUrl, "appsmith"); // stop the old container
+    cy.StopTheContainer(testUrl, "appsmith-160-updated"); // stop the old container
     cy.wait(2000);
 
     cy.log("Get path");
     cy.GetPath(testUrl, "appsmith").then((path) => {
       path = path.split("  ");
       path = path[1].split(" ");
-      path = path[0].slice(9, -7);
+      path = path[0].slice(0, -7);
       cy.log(path);
 
       cy.log("Start old stack container");
       cy.CreateAContainer(
         testUrl,
         path + "/oldstack/ce",
-        "v1.6.0",
-        "appsmith-community",
+        "ee",
+        "appsmith-enterprise",
       );
       cy.wait(45000);
 
       cy.log("Verify Logs");
       cy.GetAndVerifyLogs(testUrl, "appsmith"); // Get and verify the logs
-
-      cy.log("Stop the old stack container");
-      cy.StopTheContainer(testUrl, "appsmith-community"); // stop the old container
-      cy.wait(2000);
-
-      cy.log("Update old stack container");
-      cy.CreateAContainer(
-        testUrl,
-        path + "/oldstack/ce",
-        "release",
-        "appsmith-enterprise",
-      );
-      cy.wait(45000);
     });
 
     //verify the Applications after upgrade
@@ -115,9 +92,5 @@ describe("Upgrade appsmith version", () => {
     cy.get(".t--buttongroup-widget")
       .children()
       .should("have.length", 3);
-
-    cy.log("Stop the container");
-    cy.StopTheContainer(testUrl, "appsmith-160-updated"); // stop the old container
-    cy.wait(2000);
   });
 });
