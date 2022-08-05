54a76e2e8a8c8565e69c4f84a63767bc689f3d1c
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-08-06 00:09:50 +0530

    fix cypress test merge issue

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/BugTests/Autocomplete_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/BugTests/Autocomplete_Spec.ts
@@ -1,40 +1,36 @@
 import { WIDGET } from "../../../../locators/WidgetLocators";
 import { ObjectsRegistry } from "../../../../support/Objects/Registry";
 const {
-  AggregateHelper,
-  CommonLocators,
-  EntityExplorer,
-  PropertyPane,
+  AggregateHelper: agHelper,
+  CommonLocators: locator,
+  EntityExplorer: ee,
+  PropertyPane: propPane,
 } = ObjectsRegistry;
 
 describe("Autocomplete bug fixes", function() {
   it("1. Bug #12790 Verifies if selectedRow is in best match", function() {
-    EntityExplorer.DragDropWidgetNVerify(WIDGET.TABLE, 200, 200);
-    EntityExplorer.DragDropWidgetNVerify(WIDGET.TEXT, 200, 600);
-    EntityExplorer.SelectEntityByName("Text1");
-    PropertyPane.UpdatePropertyFieldValue("Text", "{{Table1.}}");
-    AggregateHelper.AssertElementExist(CommonLocators._hints);
-    AggregateHelper.AssertElementText(CommonLocators._hints, "Best Match");
-    AggregateHelper.AssertElementText(CommonLocators._hints, "selectedRow", 1);
+    ee.DragDropWidgetNVerify(WIDGET.TABLE, 200, 200);
+    ee.DragDropWidgetNVerify(WIDGET.TEXT, 200, 600);
+    ee.SelectEntityByName("Text1");
+    propPane.TypeTextIntoField("Text", "{{Table1.");
+    agHelper.AssertElementExist(locator._hints);
+    agHelper.AssertElementText(locator._hints, "Best Match");
+    agHelper.AssertElementText(locator._hints, "selectedRow", 1);
   });
 
   it("2. Bug #14990 Checks if copied widget show up on autocomplete suggestions", function() {
-    EntityExplorer.CopyPasteWidget("Text1");
-    EntityExplorer.SelectEntityByName("Text1");
-    PropertyPane.UpdatePropertyFieldValue("Text", "");
-    PropertyPane.TypeTextIntoField("Text", "{{Te");
-    AggregateHelper.AssertElementExist(CommonLocators._hints);
-    AggregateHelper.AssertElementText(CommonLocators._hints, "Best Match");
-    AggregateHelper.AssertElementText(
-      CommonLocators._hints,
-      "Text1Copy.text",
-      1,
-    );
+    ee.CopyPasteWidget("Text1");
+    ee.SelectEntityByName("Text1");
+    propPane.UpdatePropertyFieldValue("Text", "");
+    propPane.TypeTextIntoField("Text", "{{Te");
+    agHelper.AssertElementExist(locator._hints);
+    agHelper.AssertElementText(locator._hints, "Best Match");
+    agHelper.AssertElementText(locator._hints, "Text1Copy.text", 1);
   });
 
   it("3. Bug #14100 Custom columns name label change should reflect in autocomplete", function() {
     // select table widget
-    EntityExplorer.SelectEntityByName("Table1");
+    ee.SelectEntityByName("Table1");
     // add new column
     cy.get(".t--add-column-btn").click();
     // edit column name
@@ -42,18 +38,13 @@ describe("Autocomplete bug fixes", function() {
       "[data-rbd-draggable-id='customColumn1'] .t--edit-column-btn",
     ).click();
 
-    PropertyPane.UpdatePropertyFieldValue("Property name", "columnAlias");
+    propPane.UpdatePropertyFieldValue("Property name", "columnAlias");
     cy.wait(500);
     // select text widget
-    EntityExplorer.SelectEntityByName("Text1");
+    ee.SelectEntityByName("Text1");
 
     // type {{Table1.selectedRow. and check for autocompletion suggestion having edited column name
-    PropertyPane.UpdatePropertyFieldValue("Text", "{{Table1.selectedRow.}}");
-    cy.get("body").type(`{end}{leftArrow}{leftArrow}`);
-    cy.get("body").type(`{ctrl} `);
-
-    cy.get(`${CommonLocators._hints} li`)
-      .eq(1)
-      .should("have.text", "columnAlias");
+    propPane.TypeTextIntoField("Text", "{{Table1.selectedRow.");
+    agHelper.AssertElementText(locator._hints, "columnAlias", 1);
   });
 });
