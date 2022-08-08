9c67967e2d740a8ed3ac97bd08b69bdab2c7271b
Aman Agarwal <aman@appsmith.com>
2022-08-08 16:03:38 +0530

    fix: added verification script, readme, moved scripts to shared

--- app/shared/build-shared-dep.js
+++ app/shared/build-shared-dep.js
@@ -1,31 +1,32 @@
 const { exec } = require("child_process");
+const path = require("path");
 const sharedJSON = require("./shared-dependencies.json");
 
-const CURRENT_DIRECTORY = __dirname;
+const CURRENT_DIRECTORY = path.join(__dirname, '..');
 const SCOPE_DIRECTORY = process.env.CURRENT_SCOPE;
 
 async function main() {
   console.log("\x1b[33m", "*******************************************");
-  console.log("\x1b[33m", "Linking Shared Dependencies");
+  console.log("\x1b[33m", "Bundling Shared Dependencies");
   console.log("\x1b[33m", "*******************************************");
 
   const dependencies = sharedJSON[SCOPE_DIRECTORY];
 
   if ((dependencies && dependencies.length > 0) || !SCOPE_DIRECTORY) {
     try {
-      // Linking Dependencies
       await Promise.all(
         dependencies.map(
           (dependencyFolder) =>
             new Promise((resolve, reject) => {
               console.log(
                 "\x1b[0m",
-                `Linking Dependency for \x1b[34m${dependencyFolder} to ${SCOPE_DIRECTORY}`
+                `Bundling Dependency for \x1b[34m${dependencyFolder}`
               );
               exec(
                 `
-            cd ${CURRENT_DIRECTORY}/${SCOPE_DIRECTORY};
-            yarn link ${dependencyFolder};
+            cd ${CURRENT_DIRECTORY}/${dependencyFolder.replace("@", "")};
+            yarn unlink;
+            yarn run link-package;
           `,
                 (err) => {
                   if (err) {
@@ -34,7 +35,7 @@ async function main() {
 
                   console.log(
                     "\x1b[0m",
-                    `Linked Dependency for \x1b[34m${dependencyFolder} to ${SCOPE_DIRECTORY} ✔`
+                    `Bundled Dependency for \x1b[34m${dependencyFolder}`
                   );
                   resolve();
                 }
@@ -43,15 +44,19 @@ async function main() {
         )
       );
     } catch (error) {
-      console.log("\x1b[31m", "Error in Linking Shared Dependencies ❌", error);
+      console.log(
+        "\x1b[31m",
+        "Error in Bundling Shared Dependencies ❌",
+        error
+      );
     }
 
     console.log("\x1b[32m", "*******************************************");
-    console.log("\x1b[32m", "Done Linking Shared Dependencies ✔");
+    console.log("\x1b[32m", "Done Bundling Shared Dependencies ✔");
     console.log("\x1b[32m", "*******************************************");
   } else {
     console.log("\x1b[34m", "*******************************************");
-    console.log("\x1b[34m", "No Shared Dependency to link");
+    console.log("\x1b[34m", "No Shared Dependency to bundle");
     console.log("\x1b[34m", "*******************************************");
   }
 }
