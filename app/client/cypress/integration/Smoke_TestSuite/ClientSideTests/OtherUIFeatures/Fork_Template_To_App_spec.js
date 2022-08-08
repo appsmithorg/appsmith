4596b1ef296c256cd0d1e139ad93cb0f7e886fc3
Akash N <akash@codemonk.in>
2022-08-08 11:30:05 +0530

    fix text

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/OtherUIFeatures/Fork_Template_To_App_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/OtherUIFeatures/Fork_Template_To_App_spec.js
@@ -1,8 +1,8 @@
 import templateLocators from "../../../../locators/TemplatesLocators.json";
 import widgetLocators from "../../../../locators/Widgets.json";
 
-describe("Fork a template to an workspace", () => {
-  it("Fork a template to an workspace", () => {
+describe("Fork a template to the current app", () => {
+  it("Fork a template to the current app", () => {
     cy.get("[data-cy=start-from-template]").click();
 
     cy.get(templateLocators.templateForkButton)
