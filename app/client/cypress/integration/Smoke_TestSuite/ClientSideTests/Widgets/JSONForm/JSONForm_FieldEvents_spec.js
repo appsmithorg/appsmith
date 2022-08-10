8ba0a8f6ceeccc5f3834610b3ad63ab56d19aca1
Aishwarya UR <aishwarya@appsmith.com>
2022-08-10 22:24:10 +0530

    cy fix

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/JSONForm/JSONForm_FieldEvents_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/JSONForm/JSONForm_FieldEvents_spec.js
@@ -237,6 +237,6 @@ describe("Date Field", () => {
       .type("10/08/2010");
 
     // Check for alert
-    cy.get(commonlocators.toastmsg).contains("10/08/2010");
+    cy.get(commonlocators.toastmsg).last().contains("10/08/2010");
   });
 });
