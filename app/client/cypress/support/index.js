b85d4005deb711aa315060b15a0dc1ba0bb16fc2
Aishwarya UR <aishwarya@appsmith.com>
2022-08-10 21:44:06 +0530

    Json field events spec failure fix

--- app/client/cypress/support/index.js
+++ app/client/cypress/support/index.js
@@ -97,10 +97,10 @@ before(function() {
 });
 
 beforeEach(function() {
-  //cy.window().then((win) => (win.onbeforeunload = undefined));
-  if (!navigator.userAgent.includes("Cypress")) {
-    window.addEventListener("beforeunload", this.beforeunloadFunction);
-  }
+  cy.window().then((win) => (win.onbeforeunload = undefined));
+  // if (!navigator.userAgent.includes("Cypress")) {
+  //   window.addEventListener("beforeunload", this.beforeunloadFunction);
+  // }
   initLocalstorage();
   Cypress.Cookies.preserveOnce("SESSION", "remember_token");
   cy.startServerAndRoutes();
