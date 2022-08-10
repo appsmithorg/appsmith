86baff366cfda1684eb6565d29d9f4c8f7cd3cbe
Ayangade Adeoluwa <adeoluayangade@yahoo.com>
2022-08-10 12:39:49 +0100

    Comment out query abort action execution

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/Abort_Action_Execution_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/ActionExecution/Abort_Action_Execution_spec.js
@@ -1,6 +1,7 @@
 const commonlocators = require("../../../../locators/commonlocators.json");
 const queryLocators = require("../../../../locators/QueryEditor.json");
 const datasource = require("../../../../locators/DatasourcesEditor.json");
+const formControls = require("../../../../locators/FormControl.json");
 
 const testApiUrl = "https://jsonplaceholder.typicode.com/photos";
 
@@ -15,22 +16,32 @@ describe("Abort Action Execution", function() {
     cy.VerifyErrorMsgPresence(ERROR_ACTION_EXECUTE_FAIL("Api1"));
   });
 
-  it("Cancel Request button should abort Query action execution", function() {
-    cy.NavigateToDatasourceEditor();
-    cy.get(datasource.MongoDB).click();
-    cy.fillMongoDatasourceForm();
-    cy.testSaveDatasource();
-    cy.get("@createDatasource").then(
-      (httpResponse) => httpResponse.response.body.data.name,
-    );
-    cy.NavigateToQueryEditor();
-    cy.NavigateToActiveTab();
-    cy.get(queryLocators.createQuery)
-      .last()
-      .click();
-    cy.get(queryLocators.queryNameField).type("AbortQuery");
-    cy.RunQueryWithoutWaitingForResolution();
-    cy.get(commonlocators.cancelActionExecution).click();
-    cy.VerifyErrorMsgPresence(ERROR_ACTION_EXECUTE_FAIL("Query1"));
-  });
+  // Queries were resolving quicker than we could cancel them
+  // Commenting this out till we can find a query that resolves slow enough for us to cancel its execution.
+
+  // it("Cancel Request button should abort Query action execution", function() {
+  //   cy.NavigateToDatasourceEditor();
+  //   cy.get(datasource.MongoDB).click();
+  //   cy.fillMongoDatasourceForm();
+  //   cy.testSaveDatasource();
+  //   cy.get("@createDatasource").then(
+  //     (httpResponse) => httpResponse.response.body.data.name,
+  //   );
+  //   cy.NavigateToQueryEditor();
+  //   cy.NavigateToActiveTab();
+  //   cy.get(queryLocators.createQuery)
+  //     .last()
+  //     .click();
+  //   cy.get(queryLocators.queryNameField).type("AbortQuery");
+  //   cy.ValidateAndSelectDropdownOption(
+  //     formControls.commandDropdown,
+  //     "Find Document(s)",
+  //   );
+
+  //   cy.typeValueNValidate("friends", formControls.mongoCollection);
+  //   cy.typeValueNValidate("300", formControls.mongoFindLimit);
+  //   cy.RunQueryWithoutWaitingForResolution();
+  //   cy.get(commonlocators.cancelActionExecution).click();
+  //   cy.VerifyErrorMsgPresence(ERROR_ACTION_EXECUTE_FAIL("AbortQuery"));
+  // });
 });
