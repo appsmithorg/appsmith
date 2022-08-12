359d2806d2337d9d54be031a90b43dfb764d3cae
keyurparalkar <keyur@appsmith.com>
2022-08-12 15:44:20 +0530

    feat: added filter for checkbox column type

--- app/client/src/widgets/TableWidgetV2/component/CascadeFields.tsx
+++ app/client/src/widgets/TableWidgetV2/component/CascadeFields.tsx
@@ -156,8 +156,8 @@ const typeOperatorsMap: Record<ReadOnlyColumnTypes, DropdownOption[]> = {
     { label: "not empty", value: "notEmpty", type: "" },
   ],
   [ColumnTypes.CHECKBOX]: [
-    { label: "is checked", value: "true", type: "" },
-    { label: "is unchecked", value: "false", type: "" },
+    { label: "is checked", value: "isChecked", type: "" },
+    { label: "is unchecked", value: "isUnChecked", type: "" },
   ],
 };
 
@@ -173,7 +173,7 @@ const columnTypeNameMap: Record<ReadOnlyColumnTypes, string> = {
   [ReadOnlyColumnTypes.NUMBER]: "Num",
   [ReadOnlyColumnTypes.DATE]: "Date",
   [ReadOnlyColumnTypes.URL]: "Url",
-  [ReadOnlyColumnTypes.CHECKBOX]: "Checkbox",
+  [ReadOnlyColumnTypes.CHECKBOX]: "Check",
 };
 
 function RenderOption(props: { type: string; title: string; active: boolean }) {
