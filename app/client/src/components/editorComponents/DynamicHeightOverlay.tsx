45b6dd520cbe908ba96e43c63a7d7806780cee29
Ankur Singhal <ankur@appsmith.com>
2022-08-22 15:23:53 +0530

    added a white border

--- app/client/src/components/editorComponents/DynamicHeightOverlay.tsx
+++ app/client/src/components/editorComponents/DynamicHeightOverlay.tsx
@@ -470,7 +470,7 @@ const DynamicHeightOverlay: React.FC<DynamicHeightOverlayProps> = memo(
       onAnyDotStop();
     }
 
-    ////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
 
     useEffect(() => {
       setMinY(minDynamicHeight * 10);
