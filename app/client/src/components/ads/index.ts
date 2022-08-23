11d6b20acb879208e5bdf3fb89b2473612fda7f8
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-08-23 09:21:38 +0530

    Fix issue with ads/index.ts

--- app/client/src/components/ads/index.ts
+++ app/client/src/components/ads/index.ts
@@ -1,9 +1,6 @@
 export { default as Callout } from "./Callout";
 export * from "./Callout";
 
-export { default as ColorPickerComponent } from "./ColorPickerComponent";
-export * from "./ColorPickerComponent";
-
 export * from "./common";
 
 export { default as DialogComponent } from "./DialogComponent";
@@ -24,35 +21,12 @@ export * from "./FilePicker";
 
 export { default as FilePickerV2 } from "./FilePickerV2";
 
-export { default as GifPlayerComponent } from "./GifPlayerComponent";
-
-export { default as ListSegmentHeader } from "./ListSegmentHeader";
-
-export { default as MenuDivider } from "./MenuDivider";
-
-export { default as NumberedStep } from "./NumberedStep";
-
-export { default as ProgressiveImage } from "./ProgressiveImage";
-export * from "./ProgressiveImage";
-
-export { default as RectangularComponent } from "./RectangularSwitcher";
-
-export { default as SearchInput } from "./SearchInput";
-export * from "./SearchInput";
-
 export { default as SearchSnippets } from "./SnippetButton";
-
-export { default as OnboardingStatusbar } from "./Statusbar";
-export * from "./Statusbar";
-
-export { default as TabItemBackgroundFill } from "./TabItemBackgroundFill";
 
 export { default as Table } from "./Table";
 export * from "./Table";
 
 // export * from "./Tabs"; conflict on names
-
-export { default as TagInputComponent } from "./TagInputComponent";
 
 export * from "./Toast";
 
