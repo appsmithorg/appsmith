06d8f198c8b243469dc067464c2f35309961be79
Dhruvik Neharia <dhruvik@appsmith.com>
2022-08-24 23:51:11 +0530

    style: qr code scanner modal and video container, remove futile code

--- app/client/src/widgets/QRScannerWidget/component/index.tsx
+++ app/client/src/widgets/QRScannerWidget/component/index.tsx
@@ -1,49 +1,81 @@
 import React, { useState } from "react";
 import { ComponentProps } from "widgets/BaseComponent";
-import "@uppy/core/dist/style.css";
-import "@uppy/dashboard/dist/style.css";
-import "@uppy/webcam/dist/style.css";
 import { BaseButton } from "widgets/ButtonWidget/component";
 import { Colors } from "constants/Colors";
 import Modal from "react-modal";
 import { QrReader } from "react-qr-reader";
-import { updateDatasource } from "actions/datasourceActions";
 import { ViewFinder } from "./ViewFinder";
+import { createGlobalStyle } from "styled-components";
+import CloseIcon from "assets/icons/ads/cross.svg";
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    zIndex: 2000,
-    minHeight: "50%",
-    width: "50%",
-  },
-  overlay: {
-    position: "fixed",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "rgba(0, 0, 0, 0.75)",
-    zIndex: 1000,
-  },
-};
+const QRScannerGlobalStyles = createGlobalStyle<{
+  borderRadius?: string;
+  boxShadow?: string;
+}>`
+  .qr-scanner-content {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2000;
+    height: 90%;
+    max-height: 500px;
+    width: 90%;
+    max-width: 500px;
+    border-radius: ${({ borderRadius }) => borderRadius};
+    box-shadow: ${({ boxShadow }) => boxShadow};
+    background-color: white;
+  }
+
+  .qr-scanner-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(16, 22, 26, 0.7);
+    z-index: 3;
+  }
+
+  .qr-scanner-close {
+    background-color: white;
+    width: 32px;
+    height: 32px;
+    text-align: center;
+    position: absolute;
+    top: -36px;
+    right: -2px;
+    border-radius: ${({ borderRadius }) => borderRadius};
+    background-image: url(${CloseIcon});
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  @media only screen and (min-width: 820px) {
+    .qr-scanner-close {
+      right: -36px;
+    }
+  }
+
+  .qr-camera-container {
+    border-radius: ${({ borderRadius }) => borderRadius};
+    overflow: hidden;
+    height: 100%;
+  }
+
+  .qr-camera {
+    height: 100%;
+  }
+`;
 
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
 Modal.setAppElement("#root");
-function FilePickerComponent(props: FilePickerComponentProps) {
+
+function QRScannerComponent(props: QRScannerComponentProps) {
   const [modalIsOpen, setIsOpen] = useState(false);
 
-  /**
-   * opens modal
-   */
   const openModal = () => {
     setIsOpen(true);
-    // props.uppy.getPlugin("Dashboard").openModal();
   };
 
   function closeModal() {
@@ -61,47 +93,52 @@ function FilePickerComponent(props: FilePickerComponentProps) {
         text={props.label}
       />
 
+      <QRScannerGlobalStyles
+        borderRadius={props.borderRadius}
+        boxShadow={props.boxShadow}
+      />
+
       <Modal
+        className="qr-scanner-content"
         isOpen={modalIsOpen}
-        // onAfterOpen={afterOpenModal}
-        // onRequestClose={closeModal}
-        /* @ts-expect-error:error */
-        style={customStyles}
-        // contentLabel="Example Modal"
+        overlayClassName="qr-scanner-overlay"
       >
         {modalIsOpen && (
-          <QrReader
-            ViewFinder={ViewFinder}
-            constraints={{ facingMode: "environment" }}
-            onResult={(result, error) => {
-              if (!!result) {
-                setIsOpen(false);
-                props.updateData(result.getText());
-              }
+          <div className="qr-camera-container">
+            <QrReader
+              ViewFinder={ViewFinder}
+              className="qr-camera"
+              constraints={{ facingMode: "environment" }}
+              onResult={(result, error) => {
+                if (!!result) {
+                  setIsOpen(false);
+                  props.updateValue(result.getText());
+                }
 
-              if (!!error) {
-                console.info(error);
-              }
-            }}
-          />
+                if (!!error) {
+                  console.info(error);
+                }
+              }}
+              videoContainerStyle={{ height: "100%" }}
+              videoStyle={{ objectFit: "cover" }}
+            />
+          </div>
         )}
-        <button onClick={closeModal}>close</button>
-
-        {/* <p>{data}</p> */}
+        <button className="qr-scanner-close" onClick={closeModal} />
       </Modal>
     </>
   );
 }
-export interface FilePickerComponentProps extends ComponentProps {
+export interface QRScannerComponentProps extends ComponentProps {
   label: string;
   buttonColor: string;
   borderRadius: string;
   boxShadow?: string;
-  updateData: (data: any) => void;
+  updateValue: (value: any) => void;
 }
 
-FilePickerComponent.defaultProps = {
+QRScannerComponent.defaultProps = {
   backgroundColor: Colors.GREEN,
 };
 
-export default FilePickerComponent;
+export default QRScannerComponent;
--- app/client/src/widgets/QRScannerWidget/widget/index.tsx
+++ app/client/src/widgets/QRScannerWidget/widget/index.tsx
@@ -1,183 +1,10 @@
 import React from "react";
 import BaseWidget, { WidgetProps, WidgetState } from "widgets/BaseWidget";
 import { WidgetType } from "constants/WidgetConstants";
-import FilePickerComponent from "../component";
+import QRScannerComponent from "../component";
 import { ValidationTypes } from "constants/WidgetValidation";
 import { DerivedPropertiesMap } from "utils/WidgetFactory";
 import FileDataTypes from "../constants";
-import { EvaluationSubstitutionType } from "entities/DataTree/dataTreeFactory";
-import { createGlobalStyle } from "styled-components";
-import UpIcon from "assets/icons/ads/up-arrow.svg";
-import CloseIcon from "assets/icons/ads/cross.svg";
-import { Colors } from "constants/Colors";
-
-const FilePickerGlobalStyles = createGlobalStyle<{
-  borderRadius?: string;
-}>`
-
-  /* Sets the font-family to theming font-family of the upload modal */
-  .uppy-Root {
-    font-family: var(--wds-font-family);
-  }
-
-  /*********************************************************/
-  /* Set the new dropHint upload icon */
-  .uppy-Dashboard-dropFilesHereHint {
-    background-image: none;
-    border-radius: ${({ borderRadius }) => borderRadius};
-  }
-
-  .uppy-Dashboard-dropFilesHereHint::before {
-    border: 2.5px solid var(--wds-accent-color);
-    width: 60px;
-    height: 60px;
-    border-radius: ${({ borderRadius }) => borderRadius};
-    display: inline-block;
-    content: ' ';
-    position: absolute;
-    top: 43%;
-  }
-
-  .uppy-Dashboard-dropFilesHereHint::after {
-    display: inline-block;
-    content: ' ';
-    position: absolute;
-    top: 46%;
-    width: 30px;
-    height: 30px;
-
-    -webkit-mask-image: url(${UpIcon});
-    -webkit-mask-repeat: no-repeat;
-    -webkit-mask-position: center;
-    -webkit-mask-size: 30px;
-    background: var(--wds-accent-color);
-  }
-  /*********************************************************/
-
-  /*********************************************************/
-  /* Set the styles for the upload button */
-  .uppy-StatusBar-actionBtn--upload {
-    background-color: var(--wds-accent-color) !important;
-    border-radius: ${({ borderRadius }) => borderRadius};
-  }
-
-  .uppy-Dashboard-Item-action--remove {
-
-    /* Sets the border radius of the button when it is focused */
-    &:focus {
-      border-radius: ${({ borderRadius }) =>
-        borderRadius === "0.375rem" ? "0.25rem" : borderRadius} !important;
-    }
-
-    .uppy-c-icon {
-      & path:first-child {
-      /* Sets the black background of remove file button hidden */
-        visibility: hidden;
-      }
-
-      & path:last-child {
-      /* Sets the cross mark color of remove file button */
-        fill: #858282;
-      }
-
-      background-color: #FFFFFF;
-      box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1);
-
-      & {
-      /* Sets the black background of remove file button hidden*/
-        border-radius: ${({ borderRadius }) =>
-          borderRadius === "0.375rem" ? "0.25rem" : borderRadius};
-      }
-    }
-  }
-  /*********************************************************/
-
-  /*********************************************************/
-  /* Sets the back cancel button color to match theming primary color */
-  .uppy-DashboardContent-back {
-    color: var(--wds-accent-color);
-
-    &:hover {
-      color: var(--wds-accent-color);
-      background-color: ${Colors.ATHENS_GRAY};
-    }
-  }
-  /*********************************************************/
-
-  /*********************************************************/
-  /* Sets the style according to reskinning for x button at the top right corner of the modal */
-  .uppy-Dashboard-close {
-    background-color: white;
-    width: 32px;
-    height: 32px;
-    text-align: center;
-    top: -33px;
-    border-radius: ${({ borderRadius }) => borderRadius};
-
-      & span {
-        font-size: 0;
-      }
-
-      & span::after {
-        content: ' ';
-        -webkit-mask-image: url(${CloseIcon});
-        -webkit-mask-repeat: no-repeat;
-        -webkit-mask-position: center;
-        -webkit-mask-size: 20px;
-        background: #858282;
-        position: absolute;
-        top: 32%;
-        left: 32%;
-        width: 12px;
-        height: 12px;
-      }
-    }
-  }
-  /*********************************************************/
-
-
-  /*********************************************************/
-  /* Sets the border radius of the upload modal */
-  .uppy-Dashboard-inner, .uppy-Dashboard-innerWrap {
-    border-radius: ${({ borderRadius }) => borderRadius} !important;
-  }
-
-  .uppy-Dashboard-AddFiles {
-    border-radius: ${({ borderRadius }) => borderRadius} !important;
-  }
-  /*********************************************************/
-
-  /*********************************************************/
-  /* Sets the error message style according to reskinning*/
-  .uppy-Informer {
-    bottom: 82px;
-    & p[role="alert"] {
-      border-radius: ${({ borderRadius }) => borderRadius};
-      background-color: transparent;
-      color: #D91921;
-      border: 1px solid #D91921;
-    }
-  }
-  /*********************************************************/
-
-  /*********************************************************/
-  /* Style the + add more files button on top right corner of the upload modal */
-  .uppy-DashboardContent-addMore {
-    color: var(--wds-accent-color);
-    font-weight: 400;
-    &:hover {
-      background-color: ${Colors.ATHENS_GRAY};
-      color: var(--wds-accent-color);
-    }
-
-    & svg {
-      fill: var(--wds-accent-color) !important;
-    }
-  }
-  /*********************************************************/
-
-}
-`;
 class QRScannerWidget extends BaseWidget<
   QRScannerWidgetProps,
   QRScannerWidgetState
@@ -197,120 +24,6 @@ class QRScannerWidget extends BaseWidget<
             isBindProperty: true,
             isTriggerProperty: false,
             validation: { type: ValidationTypes.TEXT },
-          },
-          {
-            propertyName: "maxNumFiles",
-            label: "Max No. files",
-            helpText:
-              "Sets the maximum number of files that can be uploaded at once",
-            controlType: "INPUT_TEXT",
-            placeholderText: "1",
-            inputType: "INTEGER",
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: { type: ValidationTypes.NUMBER },
-          },
-          {
-            propertyName: "maxFileSize",
-            helpText: "Sets the maximum size of each file that can be uploaded",
-            label: "Max file size(Mb)",
-            controlType: "INPUT_TEXT",
-            placeholderText: "5",
-            inputType: "INTEGER",
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: {
-              type: ValidationTypes.NUMBER,
-              params: { min: 1, max: 100, default: 5 },
-            },
-          },
-          {
-            propertyName: "allowedFileTypes",
-            helpText: "Restricts the type of files which can be uploaded",
-            label: "Allowed File Types",
-            controlType: "DROP_DOWN",
-            isMultiSelect: true,
-            placeholderText: "Select File types",
-            options: [
-              {
-                label: "Any File",
-                value: "*",
-              },
-              {
-                label: "Images",
-                value: "image/*",
-              },
-              {
-                label: "Videos",
-                value: "video/*",
-              },
-              {
-                label: "Audio",
-                value: "audio/*",
-              },
-              {
-                label: "Text",
-                value: "text/*",
-              },
-              {
-                label: "MS Word",
-                value: ".doc",
-              },
-              {
-                label: "JPEG",
-                value: "image/jpeg",
-              },
-              {
-                label: "PNG",
-                value: ".png",
-              },
-            ],
-            isJSConvertible: true,
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: {
-              type: ValidationTypes.ARRAY,
-              params: {
-                unique: true,
-                children: {
-                  type: ValidationTypes.TEXT,
-                },
-              },
-            },
-            evaluationSubstitutionType:
-              EvaluationSubstitutionType.SMART_SUBSTITUTE,
-          },
-          {
-            helpText: "Set the format of the data read from the files",
-            propertyName: "fileDataType",
-            label: "Data Format",
-            controlType: "DROP_DOWN",
-            options: [
-              {
-                label: FileDataTypes.Base64,
-                value: FileDataTypes.Base64,
-              },
-              {
-                label: FileDataTypes.Binary,
-                value: FileDataTypes.Binary,
-              },
-              {
-                label: FileDataTypes.Text,
-                value: FileDataTypes.Text,
-              },
-            ],
-            isBindProperty: false,
-            isTriggerProperty: false,
-          },
-          {
-            propertyName: "isRequired",
-            label: "Required",
-            helpText: "Makes input to the widget mandatory",
-            controlType: "SWITCH",
-            isJSConvertible: true,
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: { type: ValidationTypes.BOOLEAN },
           },
           {
             propertyName: "isVisible",
@@ -405,101 +118,6 @@ class QRScannerWidget extends BaseWidget<
   static getPropertyPaneContentConfig() {
     return [
       {
-        sectionName: "Basic",
-        children: [
-          {
-            propertyName: "allowedFileTypes",
-            helpText: "Restricts the type of files which can be uploaded",
-            label: "Allowed File Types",
-            controlType: "DROP_DOWN",
-            isMultiSelect: true,
-            placeholderText: "Select File types",
-            options: [
-              {
-                label: "Any File",
-                value: "*",
-              },
-              {
-                label: "Images",
-                value: "image/*",
-              },
-              {
-                label: "Videos",
-                value: "video/*",
-              },
-              {
-                label: "Audio",
-                value: "audio/*",
-              },
-              {
-                label: "Text",
-                value: "text/*",
-              },
-              {
-                label: "MS Word",
-                value: ".doc",
-              },
-              {
-                label: "JPEG",
-                value: "image/jpeg",
-              },
-              {
-                label: "PNG",
-                value: ".png",
-              },
-            ],
-            isJSConvertible: true,
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: {
-              type: ValidationTypes.ARRAY,
-              params: {
-                unique: true,
-                children: {
-                  type: ValidationTypes.TEXT,
-                },
-              },
-            },
-            evaluationSubstitutionType:
-              EvaluationSubstitutionType.SMART_SUBSTITUTE,
-          },
-          {
-            helpText: "Set the format of the data read from the files",
-            propertyName: "fileDataType",
-            label: "Data Format",
-            controlType: "DROP_DOWN",
-            options: [
-              {
-                label: FileDataTypes.Base64,
-                value: FileDataTypes.Base64,
-              },
-              {
-                label: FileDataTypes.Binary,
-                value: FileDataTypes.Binary,
-              },
-              {
-                label: FileDataTypes.Text,
-                value: FileDataTypes.Text,
-              },
-            ],
-            isBindProperty: false,
-            isTriggerProperty: false,
-          },
-          {
-            propertyName: "maxNumFiles",
-            label: "Max No. of files",
-            helpText:
-              "Sets the maximum number of files that can be uploaded at once",
-            controlType: "INPUT_TEXT",
-            placeholderText: "1",
-            inputType: "INTEGER",
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: { type: ValidationTypes.NUMBER },
-          },
-        ],
-      },
-      {
         sectionName: "Label",
         children: [
           {
@@ -512,35 +130,6 @@ class QRScannerWidget extends BaseWidget<
             isBindProperty: true,
             isTriggerProperty: false,
             validation: { type: ValidationTypes.TEXT },
-          },
-        ],
-      },
-      {
-        sectionName: "Validation",
-        children: [
-          {
-            propertyName: "isRequired",
-            label: "Required",
-            helpText: "Makes input to the widget mandatory",
-            controlType: "SWITCH",
-            isJSConvertible: true,
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: { type: ValidationTypes.BOOLEAN },
-          },
-          {
-            propertyName: "maxFileSize",
-            helpText: "Sets the maximum size of each file that can be uploaded",
-            label: "Max file size(Mb)",
-            controlType: "INPUT_TEXT",
-            placeholderText: "5",
-            inputType: "INTEGER",
-            isBindProperty: true,
-            isTriggerProperty: false,
-            validation: {
-              type: ValidationTypes.NUMBER,
-              params: { min: 1, max: 100, default: 5 },
-            },
           },
         ],
       },
@@ -658,27 +247,23 @@ class QRScannerWidget extends BaseWidget<
   }
 
   static getMetaPropertiesMap(): Record<string, any> {
-    return {
-      selectedFiles: [],
-      uploadedFileData: {},
-      isDirty: false,
-    };
+    return {};
   }
 
-  updateData = (data: any) => {
-    this.props.updateWidgetMetaProperty("data", data);
+  updateValue = (value: any) => {
+    this.props.updateWidgetMetaProperty("value", value);
   };
 
   getPageView() {
     return (
-      <FilePickerComponent
+      <QRScannerComponent
         borderRadius={this.props.borderRadius}
         boxShadow={this.props.boxShadow}
         buttonColor={this.props.buttonColor}
         isDisabled={this.props.isDisabled}
         key={this.props.widgetId}
         label={this.props.label}
-        updateData={this.updateData}
+        updateValue={this.updateValue}
         widgetId={this.props.widgetId}
       />
     );
@@ -691,7 +276,6 @@ class QRScannerWidget extends BaseWidget<
 
 interface QRScannerWidgetState extends WidgetState {
   isLoading: boolean;
-  uppy: any;
 }
 
 interface QRScannerWidgetProps extends WidgetProps {
