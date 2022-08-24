ab115f5227e3178956eb1bd78c1209d4f8220437
Aishwarya UR <aishwarya@appsmith.com>
2022-08-24 12:52:29 +0530

    MySQK CRUD fixes

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/GenerateCRUD/MySQL_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/GenerateCRUD/MySQL_Spec.ts
@@ -157,7 +157,7 @@ describe("Validate MySQL Generate CRUD with JSON Form", () => {
     agHelper.GetNClick(dataSources._selectTableDropdown);
     agHelper.GetNClickByContains(dataSources._dropdownOption, "productlines");
     agHelper.GetNClick(dataSources._generatePageBtn);
-    agHelper.ValidateToastMessage("Successfully generated a page");
+    agHelper.AssertContains("Successfully generated a page");
     agHelper.ValidateNetworkStatus("@replaceLayoutWithCRUDPage", 201);
     agHelper.ValidateNetworkStatus("@getActions", 200);
     agHelper.ValidateNetworkStatus("@postExecute", 200);
@@ -452,9 +452,10 @@ describe("Validate MySQL Generate CRUD with JSON Form", () => {
     //agHelper.AssertElementVisible(locator._jsonFormWidget, 1); //Insert Modal at index 1
     agHelper.AssertElementVisible(locator._visibleTextDiv("Insert Row"));
     agHelper.ClickButton("Submit");
-    agHelper.ValidateToastMessage("Column 'store_id' cannot be null");
-    agHelper.WaitUntilToastDisappear("error response", 1);
+    agHelper.AssertContains("Column 'store_id' cannot be null");
+    agHelper.AssertContains("error response");
 
+    agHelper.WaitUntilEleDisappear(locator._toastMsg);
     deployMode.EnterJSONInputValue("Store Id", "2106");
     deployMode.EnterJSONInputValue("Name", "Keokuk Spirits", 1);
     cy.xpath(deployMode._jsonFormRadioFieldByName("Store Status"))
@@ -471,7 +472,7 @@ describe("Validate MySQL Generate CRUD with JSON Form", () => {
       .should("eq", "password");
 
     agHelper.ClickButton("Submit");
-    agHelper.ValidateToastMessage("Duplicate entry '2106' for key 'PRIMARY'");
+    agHelper.AssertContains("Duplicate entry '2106' for key 'PRIMARY'");
 
     cy.xpath(deployMode._jsonFormFieldByName("Store Id", true))
       .clear()
@@ -595,7 +596,7 @@ describe("Validate MySQL Generate CRUD with JSON Form", () => {
   ) {
     agHelper.GetNClick(dataSources._generatePageBtn);
     agHelper.ValidateNetworkStatus("@replaceLayoutWithCRUDPage", 201);
-    agHelper.ValidateToastMessage("Successfully generated a page");
+    agHelper.AssertContains("Successfully generated a page");
     //agHelper.ValidateNetworkStatus("@getActions", 200);//Since failing sometimes
     agHelper.ValidateNetworkStatus("@postExecute", 200);
     agHelper.ValidateNetworkStatus("@updateLayout", 200);
