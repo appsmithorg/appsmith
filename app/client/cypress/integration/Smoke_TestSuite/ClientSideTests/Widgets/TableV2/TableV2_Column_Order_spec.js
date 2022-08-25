1baa7f0d738c2d39977881da71968a8e85233c9c
souma-ghosh <souma@appsmith.com>
2022-08-25 16:50:32 +0530

    Adds Cypress tests

---
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/TableV2/TableV2_Column_Order_spec.js
@@ -0,0 +1,19 @@
+const commonlocators = require("../../../../../locators/commonlocators.json");
+const dsl = require("../../../../../fixtures/tableV2ColumnOrderDsl.json");
+const widgetsPage = require("../../../../../locators/Widgets.json");
+
+describe("Table Widget V2 column order maintained on column change validation", function() {
+  before(() => {
+    cy.addDsl(dsl);
+  });
+
+  it("Table widget V2 column order should be same and new column should be at the end", function() {
+    cy.get(commonlocators.switchWidgetActive).click({ force: true });
+    cy.get(commonlocators.textWidgetContainer).each((item, index, list) => {
+      cy.wrap(item).should(
+        "contain.text",
+        `Column Order: [\\"action\\",\\"task\\",\\"status\\",\\"step1\\"]`,
+      );
+    });
+  });
+});
