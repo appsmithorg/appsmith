4908ecbc9f775e5ae5bda7ca0e4129739ae47be8
souma-ghosh <souma@appsmith.com>
2022-08-31 18:56:11 +0530

    Adds another assertion to table column order cypress test

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/TableV2/TableV2_Column_Order_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/TableV2/TableV2_Column_Order_spec.js
@@ -8,18 +8,26 @@ describe("Table Widget V2 column order maintained on column change validation", 
   });
 
   it("Table widget V2 column order should be maintained after reorder and new column should be at the end", function() {
-    cy.get(`${commonlocators.TableV2Head} .tr div:nth-child(3)`).trigger(
-      "dragstart",
-    );
-    cy.get(
-      `${commonlocators.TableV2Head} .tr div:nth-child(2) .draggable-header`,
-    ).trigger("drop");
-    cy.wait(2000);
+    const thirdColumnSelector = `${commonlocators.TableV2Head} .tr div:nth-child(3)`;
+    const secondColumnSelector = `${commonlocators.TableV2Head} .tr div:nth-child(2) .draggable-header`;
+
+    cy.get(thirdColumnSelector).trigger("dragstart");
+    cy.get(secondColumnSelector).trigger("drop");
     cy.get(commonlocators.switchWidgetActive).click({ force: true });
     cy.get(commonlocators.textWidgetContainer).each((item, index, list) => {
       cy.wrap(item).should(
         "contain.text",
         `Column Order: [\\"status\\",\\"task\\",\\"action\\",\\"step1\\"]`,
+      );
+    });
+    cy.wait(2000);
+    cy.get(thirdColumnSelector).trigger("dragstart");
+    cy.get(secondColumnSelector).trigger("drop");
+    cy.get(commonlocators.switchWidgetInActive).click({ force: true });
+    cy.get(commonlocators.textWidgetContainer).each((item, index, list) => {
+      cy.wrap(item).should(
+        "contain.text",
+        `Column Order: [\\"status\\",\\"action\\",\\"task\\",\\"step\\"]`,
       );
     });
   });
