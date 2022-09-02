395cd1717defdc5fd622895ba345694a5ac06711
hetunandu <hetunandu@gmail.com>
2022-09-02 15:28:01 +0530

    Test

--- app/client/src/serviceWorker.js
+++ app/client/src/serviceWorker.js
@@ -27,11 +27,11 @@ const regexMap = {
 };
 
 /* eslint-disable no-restricted-globals */
-// precacheAndRoute(self.__WB_MANIFEST || []);
-const toPrecache = self.__WB_MANIFEST.filter(
-  (file) => !file.url.includes("index.html"),
-);
-precacheAndRoute(toPrecache);
+precacheAndRoute(self.__WB_MANIFEST || []);
+// const toPrecache = self.__WB_MANIFEST.filter(
+//   (file) => !file.url.includes("index.html"),
+// );
+// precacheAndRoute(toPrecache);
 self.__WB_DISABLE_DEV_DEBUG_LOGS = false;
 skipWaiting();
 clientsClaim();
@@ -64,6 +64,6 @@ registerRoute(
   }),
 );
 
-registerRoute(({ url }) => {
-  return url.pathname.includes("index.html");
-}, new NetworkOnly());
+// registerRoute(({ url }) => {
+//   return url.pathname.includes("index.html");
+// }, new NetworkOnly());
