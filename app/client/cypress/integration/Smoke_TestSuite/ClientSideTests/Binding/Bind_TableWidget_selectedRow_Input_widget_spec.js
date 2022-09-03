763261716ba376c1c729e8864cd15ecd6f947721
Arpit Mohan <mohanarpit@users.noreply.github.com>
2022-09-03 20:27:41 +0200

    chore: Fixing lint errors in the Cypress code base (#16504)

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/Bind_TableWidget_selectedRow_Input_widget_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/Bind_TableWidget_selectedRow_Input_widget_spec.js
@@ -1,8 +1,6 @@
 /// <reference types="Cypress" />
 
-const commonlocators = require("../../../../locators/commonlocators.json");
 const dsl = require("../../../../fixtures/formInputTableDsl.json");
-const widgetsPage = require("../../../../locators/Widgets.json");
 const publish = require("../../../../locators/publishWidgetspage.json");
 const testdata = require("../../../../fixtures/testdata.json");
 
