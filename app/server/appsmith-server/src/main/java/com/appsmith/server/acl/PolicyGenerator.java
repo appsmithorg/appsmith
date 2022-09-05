8d6b09b6d34210a278c21e8545cbeda7f78ad652
Trisha Anand <trisha@appsmith.com>
2022-09-05 18:21:07 +0530

    Missed commit

--- app/server/appsmith-server/src/main/java/com/appsmith/server/acl/PolicyGenerator.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/acl/PolicyGenerator.java
@@ -3,8 +3,16 @@ package com.appsmith.server.acl;
 import com.appsmith.server.acl.ce.PolicyGeneratorCE;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 
 @Component
 public class PolicyGenerator extends PolicyGeneratorCE {
+
+    @Override
+    @PostConstruct
+    public void createPolicyGraph() {
+        super.createPolicyGraph();
+    }
 
 }
