7fc7de04c789b877887f4c1d7f2acbbf722e3c27
Dhruvik Neharia <dhruvik@appsmith.com>
2022-09-05 14:27:34 +0530

    test: add disabled and visibility cy tests for qr scanner

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/QRScanner/QRScanner_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/QRScanner/QRScanner_spec.js
@@ -1,5 +1,7 @@
 const explorer = require("../../../../../locators/explorerlocators.json");
 const widgetsPage = require("../../../../../locators/Widgets.json");
+const commonlocators = require("../../../../../locators/commonlocators.json");
+const publish = require("../../../../../locators/publishWidgetspage.json");
 
 const widgetName = "qrscannerwidget";
 
@@ -11,5 +13,37 @@ describe("QR Scanner widget", () => {
     cy.dragAndDropToCanvas("textwidget", { x: 300, y: 500 });
     cy.openPropertyPane("textwidget");
     cy.updateCodeInput(".t--property-control-text", `{{QRScanner1.value}}`);
+  });
+
+  it("2. QR Scanner functionality to check disabled widget", function() {
+    cy.openPropertyPane(widgetName);
+    cy.togglebar(commonlocators.disableCheckbox);
+    cy.PublishtheApp();
+    cy.get(publish.qrscannerwidget + " " + "button").should("be.disabled");
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("3. QR Scanner functionality to check enabled widget", function() {
+    cy.openPropertyPane(widgetName);
+    cy.togglebarDisable(commonlocators.disableCheckbox);
+    cy.PublishtheApp();
+    cy.get(publish.qrscannerwidget + " " + "button").should("be.enabled");
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("4. QR Scanner functionality to uncheck visible widget", function() {
+    cy.openPropertyPane(widgetName);
+    cy.togglebarDisable(commonlocators.visibleCheckbox);
+    cy.PublishtheApp();
+    cy.get(publish.qrscannerwidget + " " + "button").should("not.exist");
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("5. QR Scanner functionality to check visible widget", function() {
+    cy.openPropertyPane(widgetName);
+    cy.togglebar(commonlocators.visibleCheckbox);
+    cy.PublishtheApp();
+    cy.get(publish.qrscannerwidget + " " + "button").should("be.visible");
+    cy.get(publish.backToEditor).click();
   });
 });
