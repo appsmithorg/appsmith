42ddee89accb01cdd70bcf81a9ed6e9670be4328
Dhruvik Neharia <dhruvik@appsmith.com>
2022-09-06 16:48:18 +0530

    test: update tests from qr scanner to code scanner widget

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/CodeScanner/CodeScanner_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/CodeScanner/CodeScanner_spec.js
@@ -3,56 +3,56 @@ const widgetsPage = require("../../../../../locators/Widgets.json");
 const commonlocators = require("../../../../../locators/commonlocators.json");
 const publish = require("../../../../../locators/publishWidgetspage.json");
 
-const widgetName = "qrscannerwidget";
+const widgetName = "codescannerwidget";
 
-describe("QR Scanner widget", () => {
-  it("1. Drag & drop QR Scanner/Text widgets", () => {
+describe("Code Scanner widget", () => {
+  it("1. Drag & drop Code Scanner/Text widgets", () => {
     cy.get(explorer.addWidget).click();
     cy.dragAndDropToCanvas(widgetName, { x: 300, y: 300 });
-    cy.get(widgetsPage.qrscannerwidget).should("exist");
+    cy.get(widgetsPage.codescannerwidget).should("exist");
     cy.dragAndDropToCanvas("textwidget", { x: 300, y: 500 });
     cy.openPropertyPane("textwidget");
-    cy.updateCodeInput(".t--property-control-text", `{{QRScanner1.value}}`);
+    cy.updateCodeInput(".t--property-control-text", `{{CodeScanner1.value}}`);
   });
 
-  it("2. QR Scanner functionality to check disabled widget", function() {
+  it("2. Code Scanner functionality to check disabled widget", function() {
     cy.openPropertyPane(widgetName);
     cy.togglebar(commonlocators.disableCheckbox);
     cy.PublishtheApp();
-    cy.get(publish.qrscannerwidget + " " + "button").should("be.disabled");
+    cy.get(publish.codescannerwidget + " " + "button").should("be.disabled");
     cy.get(publish.backToEditor).click();
   });
 
-  it("3. QR Scanner functionality to check enabled widget", function() {
+  it("3. Code Scanner functionality to check enabled widget", function() {
     cy.openPropertyPane(widgetName);
     cy.togglebarDisable(commonlocators.disableCheckbox);
     cy.PublishtheApp();
-    cy.get(publish.qrscannerwidget + " " + "button").should("be.enabled");
+    cy.get(publish.codescannerwidget + " " + "button").should("be.enabled");
     cy.get(publish.backToEditor).click();
   });
 
-  it("4. QR Scanner functionality to uncheck visible widget", function() {
+  it("4. Code Scanner functionality to uncheck visible widget", function() {
     cy.openPropertyPane(widgetName);
     cy.togglebarDisable(commonlocators.visibleCheckbox);
     cy.PublishtheApp();
-    cy.get(publish.qrscannerwidget + " " + "button").should("not.exist");
+    cy.get(publish.codescannerwidget + " " + "button").should("not.exist");
     cy.get(publish.backToEditor).click();
   });
 
-  it("5. QR Scanner functionality to check visible widget", function() {
+  it("5. Code Scanner functionality to check visible widget", function() {
     cy.openPropertyPane(widgetName);
     cy.togglebar(commonlocators.visibleCheckbox);
     cy.PublishtheApp();
-    cy.get(publish.qrscannerwidget + " " + "button").should("be.visible");
+    cy.get(publish.codescannerwidget + " " + "button").should("be.visible");
     cy.get(publish.backToEditor).click();
   });
 
-  it("6. Open and close the QR Scanner modal", function() {
+  it("6. Open and close the Code Scanner modal", function() {
     // Open
-    cy.get(widgetsPage.qrscannerwidget).click();
+    cy.get(widgetsPage.codescannerwidget).click();
     //eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(2000);
     // Close
-    cy.get(".qr-scanner-close").click();
+    cy.get(".code-scanner-close").click();
   });
 });
