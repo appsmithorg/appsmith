5be106472ffff81dd509ca4c1db46c8d1c4bec12
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2022-09-07 17:29:36 +0530

    drag and drop init

--- app/client/src/pages/Editor/AppSettingsPane/AppSettings/index.tsx
+++ app/client/src/pages/Editor/AppSettingsPane/AppSettings/index.tsx
@@ -1,5 +1,11 @@
+import { setPageOrder } from "actions/pageActions";
+import { Page } from "ce/constants/ReduxActionConstants";
+import { ControlIcons, DraggableList } from "design-system";
 import { ThemePropertyPane } from "pages/Editor/ThemePropertyPane";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCurrentApplicationId } from "selectors/editorSelectors";
+import { selectAllPages } from "selectors/entitiesSelector";
 import styled from "styled-components";
 import GeneralSettings from "./GeneralSettings";
 import SectionHeader, { SectionHeaderProps } from "./SectionHeader";
@@ -27,7 +33,12 @@ const ThemeTabContentWrapper = styled.div`
 `;
 
 function AppSettings() {
+  const dispatch = useDispatch();
   const [selectedTab, setSelectedTab] = useState(Tabs.General);
+  const applicationId = useSelector(getCurrentApplicationId);
+  const pages: Page[] = useSelector(selectAllPages);
+
+  const DragIcon = ControlIcons.DRAG_CONTROL;
 
   const SectionHeadersConfig: SectionHeaderProps[] = [
     {
@@ -50,6 +61,13 @@ function AppSettings() {
     },
   ];
 
+  const setPageOrderCallback = useCallback(
+    (pageId: string, newOrder: number) => {
+      dispatch(setPageOrder(applicationId, pageId, newOrder));
+    },
+    [dispatch, applicationId],
+  );
+
   return (
     <Wrapper className="flex flex-row">
       <div className="w-1/2">
@@ -57,6 +75,25 @@ function AppSettings() {
           <SectionHeader key={config.name} {...config} />
         ))}
         <div className="border-t-[1px] border-[#d7d7d7]" />
+        <DraggableList
+          ItemRenderer={({ item }: { item: Page }) => (
+            <div className="cursor-pointer">
+              <DragIcon color="black" height={20} width={20} />
+              {item.pageName}
+            </div>
+          )}
+          itemHeight={70}
+          items={pages}
+          keyAccessor={"pageId"}
+          onUpdate={(
+            newOrder: any,
+            originalIndex: number,
+            newIndex: number,
+          ) => {
+            setPageOrderCallback(pages[originalIndex].pageId, newIndex);
+          }}
+          shouldReRender={false}
+        />
       </div>
       <SectionContent className="w-1/2">
         {(() => {
