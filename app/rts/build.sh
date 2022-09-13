736b73e4ffe62f5d1ae8d52ebfc2ae3b4d0e860b
Arpit Mohan <arpit@appsmith.com>
2022-09-14 02:46:09 +0530

    I think I found the problem

--- app/rts/build.sh
+++ app/rts/build.sh
@@ -5,6 +5,6 @@ set -o errexit
 cd "$(dirname "$0")"
 yarn install --frozen-lockfile
 npx tsc && npx tsc-alias
-ls -l node_modules
 # Copying node_modules directory into dist as rts server requires node_modules to run server build properly. This was previously being done in dockerfile which was copying the symlinks to image rather than the whole directory of shared modules (e.g. AST)
-cp -r node_modules ./dist
+cp -rL node_modules ./dist
+ls -al ./dist/node_modules/@shared
