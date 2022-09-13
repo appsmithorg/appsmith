5633b46dd5898b46df69845b0dc28395ed3a88b7
Aswath K <aswath@appsmith.com>
2022-09-13 10:51:01 +0530

    fix: Avoid unwanted evals on Action selector

--- app/client/src/components/propertyControls/ActionSelectorControl.tsx
+++ app/client/src/components/propertyControls/ActionSelectorControl.tsx
@@ -40,6 +40,7 @@ class ActionSelectorControl extends BaseControl<ControlProps> {
 
   handleValueUpdate = (newValue: string, isUpdatedViaKeyboard = false) => {
     const { propertyName, propertyValue } = this.props;
+    if (!propertyValue && !newValue) return;
     if (newValue !== propertyValue)
       this.updateProperty(propertyName, newValue, isUpdatedViaKeyboard);
   };
