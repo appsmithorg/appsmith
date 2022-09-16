c3bdb5fff15d5637eebf5421bf92175050a5b7ae
Shrikant Sharat Kandula <shrikant@appsmith.com>
2022-09-16 20:53:05 +0530

    Fix template filtering version mismatch
    Signed-off-by: Shrikant Sharat Kandula <shrikant@appsmith.com>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/ApplicationTemplateServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/ApplicationTemplateServiceCEImpl.java
@@ -63,7 +63,7 @@ public class ApplicationTemplateServiceCEImpl implements ApplicationTemplateServ
                 .fromUriString(cloudServicesConfig.getBaseUrl())
                 .pathSegment("api/v1/app-templates", templateId, "similar")
                 .queryParams(params)
-                .queryParam("version", releaseNotesService.getReleasedVersion())
+                .queryParam("version", releaseNotesService.getRunningVersion())
                 .build();
 
         String apiUrl = uriComponents.toUriString();
@@ -87,7 +87,7 @@ public class ApplicationTemplateServiceCEImpl implements ApplicationTemplateServ
         final String baseUrl = cloudServicesConfig.getBaseUrl();
 
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.newInstance()
-                .queryParam("version", releaseNotesService.getReleasedVersion());
+                .queryParam("version", releaseNotesService.getRunningVersion());
 
         if (!CollectionUtils.isEmpty(templateIds)) {
             uriComponentsBuilder.queryParam("id", templateIds);
