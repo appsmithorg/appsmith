dc219ef9512f1aa50f363e2e31ec081a2b65729b
Aman Agarwal <aman@appsmith.com>
2022-09-19 17:10:46 +0530

    fix: adding comments for copy command after ||

--- app/rts/build.sh
+++ app/rts/build.sh
@@ -8,4 +8,5 @@ npx tsc && npx tsc-alias
 # Copying node_modules directory into dist as rts server requires node_modules to run server build properly. 
 # This was previously being done in dockerfile which was copying the symlinks to image rather than the whole directory of shared modules (e.g. AST)
 # Also, we copy node_modules with -L flag in order to follow the symlinks for @shared folder and copy the contents instead of just the symlink
+# || command is added because on Mac -L flag doesn't work, so if the first command throws an error only then the command after || symbol will run
 cp -rL node_modules ./dist || cp -r node_modules ./dist
