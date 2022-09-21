b1127f635f23fe926e87f0bb74fcab7ffa6a756e
Nidhi Nair <cataclysm@Nidhis-MacBook-Pro.local>
2022-09-21 22:37:09 +0530

    Cleaned up comments all over

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/AstServiceCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/AstServiceCE.java
@@ -6,5 +6,15 @@ import java.util.List;
 
 public interface AstServiceCE {
 
+    /**
+     * If the RTS AST endpoints are accessible, use the service to find global references in the binding value
+     * If not, then fall back to the string comparison way of breaking down the binding value into words and looking
+     * for references
+     * In case the AST service returns with an error, throw an exception that propagates to the layout error messages,
+     * to let the user know that their on page load actions have not been updated.
+     * @param bindingValue    : The mustache binding value string to be analyzed
+     * @param evalVersion     : The evaluated value version of the current app to be used while AST parsing
+     * @return A mono of list of strings that represent all valid global references in thw binding string
+     */
     Mono<List<String>> getPossibleReferencesFromDynamicBinding(String bindingValue, int evalVersion);
 }
