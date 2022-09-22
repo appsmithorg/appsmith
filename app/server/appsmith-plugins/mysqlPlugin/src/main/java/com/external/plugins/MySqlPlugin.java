43e795ffca2a88a7f31de31e0545a332ae53da11
subrata <subrata@appsmith.com>
2022-09-22 21:14:05 +0600

    feat: Implement data type handling in MySQL (#16621)
    With this implementation we can now achieve the following under prepared statement:
    - Ability to distinguish between null object and "null" string
    - Ability to distinguish values like {{"098765"}} and {{098765}}. The former is identified as a string and the latter is identified as an integer

--- app/server/appsmith-plugins/mysqlPlugin/src/main/java/com/external/plugins/MySqlPlugin.java
+++ app/server/appsmith-plugins/mysqlPlugin/src/main/java/com/external/plugins/MySqlPlugin.java
@@ -1,11 +1,11 @@
 package com.external.plugins;
 
-import com.appsmith.external.constants.DataType;
+import com.appsmith.external.datatypes.*;
 import com.appsmith.external.dtos.ExecuteActionDTO;
 import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginError;
 import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginException;
 import com.appsmith.external.exceptions.pluginExceptions.StaleConnectionException;
-import com.appsmith.external.helpers.DataTypeStringUtils;
+import com.appsmith.external.helpers.DataTypeServiceUtils;
 import com.appsmith.external.helpers.MustacheHelper;
 import com.appsmith.external.models.ActionConfiguration;
 import com.appsmith.external.models.ActionExecutionRequest;
@@ -22,6 +22,9 @@ import com.appsmith.external.models.SSLDetails;
 import com.appsmith.external.plugins.BasePlugin;
 import com.appsmith.external.plugins.PluginExecutor;
 import com.appsmith.external.plugins.SmartSubstitutionInterface;
+import com.external.plugins.datatypes.MySQLBooleanType;
+import com.external.plugins.datatypes.MySQLDateTimeType;
+import com.external.plugins.datatypes.MySQLDateType;
 import com.external.utils.QueryUtils;
 import io.r2dbc.spi.ColumnMetadata;
 import io.r2dbc.spi.Connection;
@@ -133,6 +136,31 @@ public class MySqlPlugin extends BasePlugin {
             // "  and i.enforced = 'YES'\n" +  // Looks like this is not available on all versions of MySQL.
             "  and i.constraint_type in ('FOREIGN KEY', 'PRIMARY KEY')\n" +
             "order by i.table_name, i.constraint_name, k.position_in_unique_constraint;";
+
+    private static Map<ClientDataType, List<AppsmithType>> pluginSpecificTypes;
+
+    static {
+        pluginSpecificTypes = new HashMap<>();
+        pluginSpecificTypes.put(ClientDataType.NULL, List.of(new NullType()));
+
+        pluginSpecificTypes.put(ClientDataType.BOOLEAN, List.of(new MySQLBooleanType()));
+
+        pluginSpecificTypes.put(ClientDataType.NUMBER, List.of(
+                new IntegerType(),
+                new LongType(),
+                new DoubleType(),
+                new BigDecimalType()
+        ));
+
+        pluginSpecificTypes.put(ClientDataType.OBJECT, List.of(new JsonObjectType()));
+
+        pluginSpecificTypes.put(ClientDataType.STRING, List.of(
+                new TimeType(),
+                new MySQLDateType(),
+                new MySQLDateTimeType(),
+                new StringType()
+        ));
+    }
 
     public MySqlPlugin(PluginWrapper wrapper) {
         super(wrapper);
@@ -394,29 +422,36 @@ public class MySqlPlugin extends BasePlugin {
                                              Object... args) {
 
             Statement connectionStatement = (Statement) input;
-            DataType valueType = DataTypeStringUtils.stringToKnownDataTypeConverter(value);
+            ClientDataType clientDataType = (ClientDataType) args[0];
+            AppsmithType appsmithType = DataTypeServiceUtils.getAppsmithType(clientDataType, value, pluginSpecificTypes);
 
-            Map.Entry<String, String> parameter = new SimpleEntry<>(value, valueType.toString());
+            Map.Entry<String, String> parameter = new SimpleEntry<>(value, appsmithType.type().toString());
             insertedParams.add(parameter);
 
-            if (DataType.NULL.equals(valueType)) {
-                try {
-                    connectionStatement.bindNull((index - 1), Object.class);
-                } catch (UnsupportedOperationException e) {
-                    // Do nothing. Move on
-                }
-            } else if (DataType.INTEGER.equals(valueType)) {
-                /**
-                 * - NumberFormatException is NOT expected here since stringToKnownDataTypeConverter uses parseInt
-                 * method to detect INTEGER type.
-                 */
-                connectionStatement.bind((index - 1), Integer.parseInt(value));
-            } else if (DataType.BOOLEAN.equals(valueType)) {
-                connectionStatement.bind((index - 1), Boolean.parseBoolean(value) == TRUE ? 1 : 0);
-            } else {
-                connectionStatement.bind((index - 1), value);
+            switch (appsmithType.type()) {
+                case NULL:
+                    try {
+                        connectionStatement.bindNull((index - 1), Object.class);
+                    } catch (UnsupportedOperationException e) {
+                        // Do nothing. Move on
+                    }
+                    break;
+                case BOOLEAN:
+                    connectionStatement.bind((index - 1), appsmithType.performSmartSubstitution(value));
+                    break;
+                case INTEGER:
+                    connectionStatement.bind((index - 1), Integer.parseInt(value));
+                    break;
+                case LONG:
+                    connectionStatement.bind((index - 1), Long.parseLong(value));
+                    break;
+                case DOUBLE:
+                    connectionStatement.bind((index - 1), Double.parseDouble(value));
+                    break;
+                default:
+                    connectionStatement.bind((index - 1), value);
+                    break;
             }
-
             return connectionStatement;
         }
 
