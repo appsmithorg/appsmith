62253fc13d01a0483330efa4e3546ad623150084
“sneha122” <sneha@appsmith.com>
2022-09-28 16:09:33 +0530

    xlsx not getting listed fixed

--- app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/config/FileListMethod.java
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/config/FileListMethod.java
@@ -33,7 +33,7 @@ public class FileListMethod implements ExecutionMethod, TriggerMethod {
     @Override
     public WebClient.RequestHeadersSpec<?> getExecutionClient(WebClient webClient, MethodConfig methodConfig) {
         UriComponentsBuilder uriBuilder = getBaseUriBuilder(this.BASE_DRIVE_API_URL,
-                "?q=mimeType%3D'application%2Fvnd.google-apps.spreadsheet'%20and%20trashed%3Dfalse", true);
+                "?q=mimeType%3D'application%2Fvnd.google-apps.spreadsheet'%20or%20mimeType%3D'application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet'%20and%20trashed%3Dfalse", true);
 
         return webClient.method(HttpMethod.GET)
                 .uri(uriBuilder.build(true).toUri())
