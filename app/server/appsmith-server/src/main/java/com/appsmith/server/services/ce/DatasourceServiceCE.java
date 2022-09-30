8d3bf348256cef09d918338ca521f51a25b18be8
Nilesh Sarupriya <nilesh@appsmith.com>
2022-09-30 12:32:26 +0530

    Moving the Datasource search from NewActionService to DatasourceService

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/DatasourceServiceCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/DatasourceServiceCE.java
@@ -3,6 +3,7 @@ package com.appsmith.server.services.ce;
 import com.appsmith.external.models.Datasource;
 import com.appsmith.external.models.DatasourceTestResult;
 import com.appsmith.server.acl.AclPermission;
+import com.appsmith.server.dtos.ActionDTO;
 import com.appsmith.server.services.CrudService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -32,5 +33,5 @@ public interface DatasourceServiceCE extends CrudService<Datasource, String> {
 
     Mono<Datasource> populateHintMessages(Datasource datasource);
 
-    Mono<Datasource> getValidDatasourceMono(Datasource datasource);
+    Mono<Datasource> getValidDatasourceFromActionMono(ActionDTO actionDTO, AclPermission aclPermission);
 }
