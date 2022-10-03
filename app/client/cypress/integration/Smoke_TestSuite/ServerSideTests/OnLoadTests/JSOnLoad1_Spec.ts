c00e9c8f4dd9b07bf6e34bab8a6b7d9a1be7f2d0
Ayangade Adeoluwa <adeoluayangade@yahoo.com>
2022-10-03 16:32:36 +0100

    Fix failing cypress tests

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/OnLoadTests/JSOnLoad1_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/OnLoadTests/JSOnLoad1_Spec.ts
@@ -305,7 +305,8 @@ describe("JSObjects OnLoad Actions tests", function() {
       //One Quotes confirmation - for API true
       agHelper.AssertElementVisible(jsEditor._dialogBody("Quotes"));
       agHelper.ClickButton("No");
-      agHelper.WaitUntilToastDisappear('The action "Quotes" has failed');
+      agHelper.WaitUntilToastDisappear("Quotes was cancelled");
+      aza;
 
       //Another for API called via JS callQuotes()
       agHelper.AssertElementVisible(jsEditor._dialogBody("Quotes"));
@@ -357,7 +358,7 @@ describe("JSObjects OnLoad Actions tests", function() {
     deployMode.NavigateBacktoEditor();
     agHelper.AssertElementVisible(jsEditor._dialogBody("Quotes"));
     agHelper.ClickButton("No");
-    agHelper.AssertContains('The action "Quotes" has failed');
+    agHelper.AssertContains("Quotes was cancelled");
 
     agHelper.WaitUntilAllToastsDisappear();
     agHelper.AssertElementVisible(jsEditor._dialogBody("Quotes"));
@@ -392,9 +393,9 @@ describe("JSObjects OnLoad Actions tests", function() {
     deployMode.DeployApp();
     agHelper.AssertElementVisible(jsEditor._dialogBody("CatFacts"));
     agHelper.ClickButton("No");
-    agHelper.ValidateToastMessage('The action "CatFacts" has failed');
+    agHelper.ValidateToastMessage("CatFacts was cancelled");
 
-    agHelper.WaitUntilToastDisappear('The action "CatFacts" has failed');
+    agHelper.WaitUntilToastDisappear("CatFacts was cancelled");
     agHelper.GetNClick(locator._widgetInDeployed("imagewidget"));
     agHelper.AssertElementVisible(jsEditor._dialogBody("CatFacts"));
     agHelper.ClickButton("Yes");
@@ -525,7 +526,7 @@ describe("JSObjects OnLoad Actions tests", function() {
     deployMode.DeployApp();
     agHelper.AssertElementVisible(jsEditor._dialogBody("getBooks"));
     agHelper.ClickButton("No");
-    agHelper.ValidateToastMessage('The action "getBooks" has failed');
+    agHelper.ValidateToastMessage("getBooks was cancelled");
     agHelper
       .GetText(locator._jsonFormInputField("name"), "val")
       .should("be.empty");
@@ -555,7 +556,7 @@ describe("JSObjects OnLoad Actions tests", function() {
     deployMode.NavigateBacktoEditor();
     agHelper.AssertElementVisible(jsEditor._dialogBody("getBooks"));
     agHelper.ClickButton("No");
-    agHelper.ValidateToastMessage('The action "getBooks" has failed');
+    agHelper.ValidateToastMessage("getBooks was cancelled");
 
     ee.SelectEntityByName(jsName as string, "Queries/JS");
     ee.ActionContextMenuByEntityName(
