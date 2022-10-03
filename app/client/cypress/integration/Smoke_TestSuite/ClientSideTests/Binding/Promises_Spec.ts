209284b289de4a3ddc685391f3def43643576300
Aishwarya UR <aishwarya@appsmith.com>
2022-10-03 12:37:12 +0530

    fix

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/Promises_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Binding/Promises_Spec.ts
@@ -178,7 +178,7 @@ return InspiringQuotes.run().then((res) => { showAlert("Today's quote for " + us
   });
 
   it("7. Verify maintaining context via direct Promises", () => {
-    //deployMode.NavigateBacktoEditor();
+    deployMode.NavigateBacktoEditor();
     cy.fixture("promisesBtnListDsl").then((val: any) => {
       agHelper.AddDsl(val, locator._spanButton("Submit"));
     });
