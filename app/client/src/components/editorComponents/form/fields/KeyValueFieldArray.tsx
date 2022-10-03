38f650ecb930d6cb3569238b52c89b53ba75a590
“sneha122” <sneha@appsmith.com>
2022-10-03 15:16:51 +0530

    perf test failure check

--- app/client/src/components/editorComponents/form/fields/KeyValueFieldArray.tsx
+++ app/client/src/components/editorComponents/form/fields/KeyValueFieldArray.tsx
@@ -132,14 +132,14 @@ const expected = {
 
 function KeyValueRow(props: Props & WrappedFieldArrayProps) {
   useEffect(() => {
-    const allProps = props.fields?.getAll();
-    if (!!allProps) {
-      if (props.fields.length < 2 && props.pushFields) {
-        for (let i = props.fields.length; i < 2; i += 1) {
-          props.fields.push({ key: "", value: "" });
-        }
+    // const allProps = props.fields?.getAll();
+    // if (!!allProps) {
+    if (props.fields.length < 2 && props.pushFields) {
+      for (let i = props.fields.length; i < 2; i += 1) {
+        props.fields.push({ key: "", value: "" });
       }
     }
+    // }
   }, [props.fields, props.pushFields]);
 
   return (
