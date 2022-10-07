e6036c3b0111340b4a70cf99a40439b46134ed8b
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-10-07 17:41:52 +0530

    fix issue with merge

--- app/client/src/components/ads/index.ts
+++ app/client/src/components/ads/index.ts
@@ -6,8 +6,6 @@ export * from "./common";
 export { default as DialogComponent } from "./DialogComponent";
 export * from "./DialogComponent";
 
-export { DroppableComponent } from "./DraggableListComponent";
-
 export { default as FilePicker } from "./FilePicker";
 export * from "./FilePicker";
 
@@ -17,5 +15,3 @@ export { default as Table } from "./Table";
 export * from "./Table";
 
 // export * from "./Tabs"; conflict on names
-
-export { default as UserProfileImagePicker } from "./UserProfileImagePicker";
