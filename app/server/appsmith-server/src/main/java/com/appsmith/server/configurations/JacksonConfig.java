5efa76b29f355257cf44ada8ef04d6d128dabd65
Arpit Mohan <arpit@appsmith.com>
2022-10-14 20:49:27 +0530

    Ignoring unknown properties in Jackson object mapper

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/JacksonConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/JacksonConfig.java
@@ -1,5 +1,6 @@
 package com.appsmith.server.configurations;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,7 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
     }
 }
