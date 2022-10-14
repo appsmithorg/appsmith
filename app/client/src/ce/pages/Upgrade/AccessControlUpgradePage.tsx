89fa3cc41e1f12df357ab1a60a773d6af1f4b6cc
Anubhav <anubhav@appsmith.com>
2022-10-14 10:18:09 +0530

    chore: update access control page

--- app/client/src/ce/pages/Upgrade/AccessControlUpgradePage.tsx
+++ app/client/src/ce/pages/Upgrade/AccessControlUpgradePage.tsx
@@ -5,31 +5,31 @@ import UpgradePage from "./UpgradePage";
 export function AccessControlUpgradePage() {
   const header: Header = {
     heading: "Access Control",
-    subHeadings: ["First of all, how dare you?", "Oh God NO! No! No! God No!"],
+    subHeadings: ["sub heading 1", "sub heading 2"],
   };
   const carousel = {
     triggers: [
       {
         icon: "lock-2-line",
         heading: "heading",
-        details: ["blah blah blah"],
+        details: ["details"],
       },
       {
         icon: "search-eye-line",
         heading: "heading",
-        details: ["blah blah blah"],
+        details: ["details"],
       },
       {
         icon: "alert-line",
         heading: "heading",
-        details: ["blah blah blah"],
+        details: ["details"],
       },
     ],
     targets: ["first", "second", "third"],
   };
   const footer = {
     onClick: () => null,
-    message: "Access control no bueno!",
+    message: "Access control not published yet!",
   };
   const props = { header, carousel, footer };
   return <UpgradePage {...props} />;
