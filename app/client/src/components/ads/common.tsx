c0f198208e0f6b346fc4b6e2804406b9aff0df62
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-10-17 08:51:13 +0530

    Remove unused toast parameters from common

--- app/client/src/components/ads/common.tsx
+++ app/client/src/components/ads/common.tsx
@@ -1,28 +1,5 @@
 import { Theme } from "constants/DefaultTheme";
-import { toast } from "react-toastify";
-import { Variant } from "design-system";
 
 export type ThemeProp = {
   theme: Theme;
-};
-export enum ToastTypeOptions {
-  success = "success",
-  info = "info",
-  warning = "warning",
-  error = "error",
-}
-
-const TOAST_VARIANT_LOOKUP = {
-  [toast.TYPE.ERROR]: Variant.danger,
-  [toast.TYPE.INFO]: Variant.info,
-  [toast.TYPE.SUCCESS]: Variant.success,
-  [toast.TYPE.WARNING]: Variant.warning,
-  undefined: Variant.info,
-};
-
-export const ToastVariant = (type: any): Variant => {
-  return (
-    TOAST_VARIANT_LOOKUP[type as keyof typeof TOAST_VARIANT_LOOKUP] ||
-    Variant.info
-  );
 };
