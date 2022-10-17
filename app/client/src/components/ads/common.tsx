aa2b81ee289872cbe0b981f7c77b0f348bb700ab
Tanvi Bhakta <tanvibhakta@gmail.com>
2022-10-17 08:45:04 +0530

    Remove color utils, add import from design-system

--- app/client/src/components/ads/common.tsx
+++ app/client/src/components/ads/common.tsx
@@ -1,54 +1,9 @@
 import { Theme } from "constants/DefaultTheme";
-import tinycolor from "tinycolor2";
-import styled from "styled-components";
 import { toast } from "react-toastify";
 
 export type ThemeProp = {
   theme: Theme;
 };
-
-export const hexToRgb = (
-  hex: string,
-): {
-  r: number;
-  g: number;
-  b: number;
-} => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : {
-        r: -1,
-        g: -1,
-        b: -1,
-      };
-};
-
-export const hexToRgba = (color: string, alpha: number) => {
-  const value = hexToRgb(color);
-  return `rgba(${value.r}, ${value.g}, ${value.b}, ${alpha});`;
-};
-
-export const lighten = (color: string, amount: number) => {
-  return tinycolor(color)
-    .lighten(amount)
-    .toString();
-};
-
-export const darken = (color: string, amount: number) => {
-  return tinycolor(color)
-    .darken(amount)
-    .toString();
-};
-export const StoryWrapper = styled.div`
-  background: #ffffff;
-  height: 700px;
-  padding: 50px 100px;
-`;
 
 export enum Variant {
   success = "success",
