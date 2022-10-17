dbf3f383a7eda27b73c9112b91f9b7b6be3b7160
arunvjn <arun@appsmith.com>
2022-10-17 16:00:15 +0530

    set xhr withCredentials to false and credential option of fetch to false.

--- app/client/src/workers/HTTPRequestOverride.ts
+++ app/client/src/workers/HTTPRequestOverride.ts
@@ -6,30 +6,33 @@ const _originalXHR = self.XMLHttpRequest;
 const _originalXHROpen = _originalXHR.prototype.open;
 
 export default function interceptAndOverrideHttpRequest() {
-  const XHRPrototype = Object.getPrototypeOf(_originalXHR);
-  Object.assign(self, "XMLHttpRequest", {
-    value: function(args: any) {
-      const obj = new XHRPrototype.constructor(args);
-      obj.withCredentials = false;
-      return obj;
-    },
-  });
-  self.XMLHttpRequest.prototype = XHRPrototype;
-  self.XMLHttpRequest.prototype.open = function(
-    method,
+  const XHRPrototype = XMLHttpRequest.prototype;
+  //@ts-expect-error allow override
+  self.XMLHttpRequest = function(args) {
+    //@ts-expect-error allow override
+    const obj = new XHRPrototype.constructor(args);
+    obj.withCredentials = false;
+    return obj;
+  };
+  XHRPrototype.open = function(
+    method: string,
     url: string | URL,
     ...rest: any
   ) {
+    let requestDestination = "";
     try {
-      const requestDestination = new URL(url)?.origin;
-      if (location.origin === requestDestination) {
-        throw new DisallowedOriginError(
-          `Cannot make requests to ${location.origin}`,
-        );
-      }
-    } catch (e) {}
+      requestDestination = new URL(url)?.origin;
+    } catch (e) {
+      return _originalXHROpen.call(this, method, url as string, ...rest);
+    }
+    if (location.origin === requestDestination) {
+      throw new DisallowedOriginError(
+        `Cannot make requests to ${location.origin}`,
+      );
+    }
     return _originalXHROpen.call(this, method, url as string, ...rest);
   };
+  self.XMLHttpRequest.prototype = XHRPrototype;
 
   return fetchIntercept.register({
     request: function(url: string, config: any) {
@@ -42,7 +45,7 @@ export default function interceptAndOverrideHttpRequest() {
             ),
           );
       } catch (e) {}
-      config.credentials = "omit";
+      config = { ...config, credentials: "omit" };
       return [url, config];
     },
   });
