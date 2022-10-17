5ac775147a681fc392ac8635d0dff877a801811a
arunvjn <arun@appsmith.com>
2022-10-17 13:38:33 +0530

    override 'credentials' to false

--- app/client/src/workers/HTTPRequestOverride.ts
+++ app/client/src/workers/HTTPRequestOverride.ts
@@ -2,30 +2,47 @@ import * as fetchIntercept from "fetch-intercept";
 
 class DisallowedOriginError extends Error {}
 
-const _originalOpen = self.XMLHttpRequest.prototype.open;
+const _originalXHR = self.XMLHttpRequest;
+const _originalXHROpen = _originalXHR.prototype.open;
 
 export default function interceptAndOverrideHttpRequest() {
+  const XHRPrototype = Object.getPrototypeOf(_originalXHR);
+  Object.assign(self, "XMLHttpRequest", {
+    value: function(args: any) {
+      const obj = new XHRPrototype.constructor(args);
+      obj.withCredentials = false;
+      return obj;
+    },
+  });
+  self.XMLHttpRequest.prototype = XHRPrototype;
   self.XMLHttpRequest.prototype.open = function(
     method,
     url: string | URL,
     ...rest: any
   ) {
-    if (location.origin === new URL(url).origin) {
-      throw new DisallowedOriginError(
-        `Cannot make requests to ${location.origin}`,
-      );
-    }
-    return _originalOpen.call(this, method, url as string, ...rest);
+    try {
+      const requestDestination = new URL(url)?.origin;
+      if (location.origin === requestDestination) {
+        throw new DisallowedOriginError(
+          `Cannot make requests to ${location.origin}`,
+        );
+      }
+    } catch (e) {}
+    return _originalXHROpen.call(this, method, url as string, ...rest);
   };
 
   return fetchIntercept.register({
-    request: function(url, config) {
-      if (location.origin === new URL(url).origin)
-        return Promise.reject(
-          new DisallowedOriginError(
-            `Cannot make requests to ${location.origin}`,
-          ),
-        );
+    request: function(url: string, config: any) {
+      try {
+        const requestDestination = new URL(url).origin;
+        if (location.origin === requestDestination)
+          return Promise.reject(
+            new DisallowedOriginError(
+              `Cannot make requests to ${location.origin}`,
+            ),
+          );
+      } catch (e) {}
+      config.credentials = "omit";
       return [url, config];
     },
   });
