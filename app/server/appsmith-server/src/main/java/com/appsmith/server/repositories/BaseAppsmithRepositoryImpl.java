5f80671016a9812c64e1bc2657b7ce3cd80261a3
Trisha Anand <trisha@appsmith.com>
2022-10-17 14:38:04 +0530

    chore: Fix bad merge from ce to ee

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/BaseAppsmithRepositoryImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/BaseAppsmithRepositoryImpl.java
@@ -3,8 +3,16 @@ package com.appsmith.server.repositories;
 import com.appsmith.external.models.BaseDomain;
 import com.appsmith.server.repositories.ce.BaseAppsmithRepositoryCEImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.util.CollectionUtils;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public abstract class BaseAppsmithRepositoryImpl<T extends BaseDomain> extends BaseAppsmithRepositoryCEImpl<T> {
@@ -15,4 +23,37 @@ public abstract class BaseAppsmithRepositoryImpl<T extends BaseDomain> extends B
 
         super(mongoOperations, mongoConverter, cacheableRepositoryHelper);
     }
+
+    public Flux<T> queryAllWithoutPermissions(List<Criteria> criterias,
+                                              List<String> includeFields,
+                                              Sort sort,
+                                              int limit) {
+        final ArrayList<Criteria> criteriaList = new ArrayList<>(criterias);
+        Query query = new Query();
+        if (!CollectionUtils.isEmpty(includeFields)) {
+            for (String includeField : includeFields) {
+                query.fields().include(includeField);
+            }
+        }
+
+        if (limit != NO_RECORD_LIMIT) {
+            query.limit(limit);
+        }
+        Criteria andCriteria = new Criteria();
+
+        criteriaList.add(notDeleted());
+
+        andCriteria.andOperator(criteriaList.toArray(new Criteria[0]));
+
+        query.addCriteria(andCriteria);
+        if (sort != null) {
+            query.with(sort);
+        }
+
+        return mongoOperations.query(this.genericDomain)
+                .matching(query)
+                .all()
+                .map(obj -> obj);
+    }
+
 }
