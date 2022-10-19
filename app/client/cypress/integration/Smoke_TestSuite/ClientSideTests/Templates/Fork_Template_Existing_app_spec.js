9bd542645c1bc30e58568deb120b1fa76f3948b8
Parthvi Goswami <parthvigoswami@Parthvis-MacBook-Pro.local>
2022-10-19 10:20:57 +0530

    split the fork template spec in two in order to avoid failure

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Templates/Fork_Template_Existing_app_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Templates/Fork_Template_Existing_app_spec.js
@@ -2,19 +2,16 @@ import widgetLocators from "../../../../locators/Widgets.json";
 import template from "../../../../locators/TemplatesLocators.json";
 const publish = require("../../../../locators/publishWidgetspage.json");
 
-describe("Fork a template to the current app", () => {
-  it("1. Fork a template to the current app", () => {
-    cy.get(template.startFromTemplateCard).click();
-    cy.wait("@fetchTemplate").should(
-      "have.nested.property",
-      "response.body.responseMeta.status",
-      200,
-    );
+describe("Fork a template to the current app from new page popover", () => {
+  it("1. Fork template from page section", () => {
+    cy.AddPageFromTemplate();
     cy.wait(1000);
     cy.get(template.templateDialogBox).should("be.visible");
+    cy.wait(4000);
     cy.xpath(
       "//div[text()='Customer Support Dashboard']/following-sibling::div//button[contains(@class, 'fork-button')]",
     ).click();
+    cy.wait(1000);
     cy.wait("@getTemplatePages").should(
       "have.nested.property",
       "response.body.responseMeta.status",
@@ -24,30 +21,13 @@ describe("Fork a template to the current app", () => {
       "contain",
       "template added successfully",
     );
-    // [Bug]: Getting 'Resource not found' error on deploying template #17477
-    cy.PublishtheApp();
-    cy.get(".t--page-switch-tab")
-      .contains("Dashboard")
-      .click({ force: true });
-    cy.wait(4000);
-    cy.get(publish.backToEditor).click();
-    cy.wait(2000);
   });
 
-  it("2. Add selected pages from template to an app", () => {
-    cy.CheckAndUnfoldEntityItem("Pages");
-    cy.get(`.t--entity-name:contains(Page1)`)
-      .trigger("mouseover")
-      .click({ force: true });
-    cy.wait(1000);
-    cy.get(template.startFromTemplateCard).click();
-    cy.wait("@fetchTemplate").should(
-      "have.nested.property",
-      "response.body.responseMeta.status",
-      200,
-    );
+  it("2. Add selected page of template from page section", () => {
+    cy.AddPageFromTemplate();
     cy.wait(1000);
     cy.get(template.templateDialogBox).should("be.visible");
+    cy.wait(4000);
     cy.xpath("//div[text()='Customer Support Dashboard']").click();
     cy.wait("@getTemplatePages").should(
       "have.nested.property",
@@ -57,19 +37,16 @@ describe("Fork a template to the current app", () => {
     cy.xpath(template.selectAllPages)
       .next()
       .click();
-    cy.wait(1000);
-    cy.xpath("//span[text()='SEARCH']")
+    cy.xpath("//span[text()='DASHBOARD']")
       .parent()
       .next()
       .click();
-    // [Bug]: On forking selected pages from a template, resource not found error is shown #17270
     cy.get(template.templateViewForkButton).click();
     cy.wait("@fetchTemplate").should(
       "have.nested.property",
       "response.body.responseMeta.status",
       200,
     );
-    cy.wait(3000);
     cy.get(widgetLocators.toastAction).should(
       "contain",
       "template added successfully",
