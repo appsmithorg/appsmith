0f5639560efef058b3dfc68da0549c6f1d189cde
arunvjn <arun@appsmith.com>
2022-10-20 12:29:03 +0530

    Removes origin check

--- app/client/src/workers/HTTPRequestOverride.ts
+++ app/client/src/workers/HTTPRequestOverride.ts
@@ -1,10 +1,5 @@
 import * as fetchIntercept from "fetch-intercept";
 
-class DisallowedOriginError extends Error {}
-
-const _originalXHR = self.XMLHttpRequest;
-const _originalXHROpen = _originalXHR.prototype.open;
-
 export default function interceptAndOverrideHttpRequest() {
   const XHRPrototype = XMLHttpRequest.prototype;
   //@ts-expect-error allow override
@@ -14,39 +9,12 @@ export default function interceptAndOverrideHttpRequest() {
     obj.withCredentials = false;
     return obj;
   };
-  XHRPrototype.open = function(
-    method: string,
-    url: string | URL,
-    ...rest: any
-  ) {
-    let requestDestination = "";
-    try {
-      requestDestination = new URL(url)?.origin;
-    } catch (e) {
-      return _originalXHROpen.call(this, method, url as string, ...rest);
-    }
-    if (location.origin === requestDestination) {
-      throw new DisallowedOriginError(
-        `Cannot make requests to ${location.origin}`,
-      );
-    }
-    return _originalXHROpen.call(this, method, url as string, ...rest);
-  };
   self.XMLHttpRequest.prototype = XHRPrototype;
 
   return fetchIntercept.register({
-    request: function(url: string, config: any) {
-      try {
-        const requestDestination = new URL(url).origin;
-        if (location.origin === requestDestination)
-          return Promise.reject(
-            new DisallowedOriginError(
-              `Cannot make requests to ${location.origin}`,
-            ),
-          );
-      } catch (e) {}
-      config = { ...config, credentials: "omit" };
-      return [url, config];
+    request: function(...args) {
+      const request = new Request(args[0], { ...args[1], credentials: "omit" });
+      return [request];
     },
   });
 }
