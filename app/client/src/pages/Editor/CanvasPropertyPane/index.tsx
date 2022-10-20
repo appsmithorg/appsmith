322ce5778115b7952966db8fec44c9f371a39299
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2022-10-20 11:38:17 +0530

    Fixing the app level positioning drop down

--- app/client/src/pages/Editor/CanvasPropertyPane/index.tsx
+++ app/client/src/pages/Editor/CanvasPropertyPane/index.tsx
@@ -1,17 +1,17 @@
-import React, { useState } from "react";
 import * as Sentry from "@sentry/react";
+import React, { useState } from "react";
 
+import { addWrappers, removeWrappers } from "actions/autoLayoutActions";
+import { batchUpdateMultipleWidgetProperties } from "actions/controlActions";
+import { LayoutDirection, Positioning } from "components/constants";
+import { Colors } from "constants/Colors";
+import { Dropdown, DropdownOption, RenderOption } from "design-system";
+import { useDispatch } from "react-redux";
+import { getWidgets } from "sagas/selectors";
+import { useSelector } from "store";
+import styled from "styled-components";
 import { MainContainerLayoutControl } from "../MainContainerLayoutControl";
 import ThemeEditor from "../ThemePropertyPane/ThemeEditor";
-import styled from "styled-components";
-import { Colors } from "constants/Colors";
-import { LayoutDirection, Positioning } from "components/constants";
-import { useDispatch } from "react-redux";
-import { batchUpdateMultipleWidgetProperties } from "actions/controlActions";
-import { useSelector } from "store";
-import { getWidgets } from "sagas/selectors";
-import { Dropdown, DropdownOption, RenderOption } from "design-system";
-import { addWrappers, removeWrappers } from "actions/autoLayoutActions";
 
 const Title = styled.p`
   color: ${Colors.GRAY_800};
@@ -42,29 +42,31 @@ const PositioningOptions = () => {
         isSelectedNode ? "px-2 py-2" : "px-2 py-2 "
       }`}
       onClick={() => {
-        setSelectedOption(options.indexOf(option as DropdownOption));
-        const isVerticalStack =
-          (option as DropdownOption).value === Positioning.Vertical;
-        const widgetId = "0";
-        dispatch(
-          batchUpdateMultipleWidgetProperties([
-            {
-              widgetId,
-              updates: {
-                modify: {
-                  positioning: (option as DropdownOption).value,
-                  useAutoLayout:
-                    (option as DropdownOption).value !== Positioning.Fixed,
-                  direction: isVerticalStack
-                    ? LayoutDirection.Vertical
-                    : LayoutDirection.Horizontal,
+        if (!isSelectedNode) {
+          setSelectedOption(options.indexOf(option as DropdownOption));
+          const isVerticalStack =
+            (option as DropdownOption).value === Positioning.Vertical;
+          const widgetId = "0";
+          dispatch(
+            batchUpdateMultipleWidgetProperties([
+              {
+                widgetId,
+                updates: {
+                  modify: {
+                    positioning: (option as DropdownOption).value,
+                    useAutoLayout:
+                      (option as DropdownOption).value !== Positioning.Fixed,
+                    direction: isVerticalStack
+                      ? LayoutDirection.Vertical
+                      : LayoutDirection.Horizontal,
+                  },
                 },
               },
-            },
-          ]),
-        );
-        if (isVerticalStack) dispatch(addWrappers(widgetId));
-        else removeWrappers(widgetId);
+            ]),
+          );
+          if (isVerticalStack) dispatch(addWrappers(widgetId));
+          else removeWrappers(widgetId);
+        }
       }}
     >
       <div className="leading-normal">{(option as DropdownOption).label}</div>
