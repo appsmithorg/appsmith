6768087de04df9a5d6721f1909e346365abd4910
Trisha Anand <trisha@appsmith.com>
2022-10-21 14:00:19 +0530

    Making the file same as that on EE

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/InviteUsersDTO.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/InviteUsersDTO.java
@@ -1,11 +1,15 @@
 package com.appsmith.server.dtos;
 
 import com.appsmith.server.dtos.ce.InviteUsersCE_DTO;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class InviteUsersDTO extends InviteUsersCE_DTO {
 
 }
