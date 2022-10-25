b6ad03837c75f74afe99aa53f25533dec1f1ed94
Sumit Kumar <sumit@appsmith.com>
2022-10-26 00:28:21 +0530

    rebase with releasep

--- app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
@@ -2,11 +2,10 @@ package com.appsmith.server;
 
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
+@SpringBootApplication
 @EnableScheduling
 public class ServerApplication {
 
