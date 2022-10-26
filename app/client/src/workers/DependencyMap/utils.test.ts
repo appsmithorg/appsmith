76a7773a4b0852f9fd9eb7cfd320e91be118ea96
ChandanBalajiBP <chandan@appsmith.com>
2022-10-27 01:31:15 +0530

    updated w.r.t ast req and res body

--- app/client/src/workers/DependencyMap/utils.test.ts
+++ app/client/src/workers/DependencyMap/utils.test.ts
@@ -5,12 +5,14 @@ const entityRefactor = [
     script: "ApiNever",
     oldName: "ApiNever",
     newName: "ApiForever",
+    isJSObject: false,
     evalVersion: 2,
   },
   {
     script: "ApiNever.data",
     oldName: "ApiNever",
     newName: "ApiForever",
+    isJSObject: false,
     evalVersion: 2,
   },
   {
@@ -18,6 +20,7 @@ const entityRefactor = [
       "//   ApiNever  \n function ApiNever(abc) {let foo = \"I'm getting data from ApiNever but don't rename this string\" +     ApiNever.data; \n if(true) { return ApiNever }}",
     oldName: "ApiNever",
     newName: "ApiForever",
+    isJSObject: false,
     evalVersion: 2,
   },
   {
@@ -25,23 +28,42 @@ const entityRefactor = [
       "//ApiNever  \n function ApiNever(abc) {let ApiNever = \"I'm getting data from ApiNever but don't rename this string\" +     ApiNever.data; \n if(true) { return ApiNever }}",
     oldName: "ApiNever",
     newName: "ApiForever",
+    isJSObject: false,
+    evalVersion: 2,
+  },
+  {
+    script:
+      "export default {\n\tmyVar1: [],\n\tmyVar2: {},\n\t\tsearch: () => {\n\t\tif(Input1Copy.text.length==0){\n\t\t\treturn select_repair_db.data\n\t\t}\n\t\telse{\n\t\t\treturn(select_repair_db.data.filter(word => word.cust_name.toLowerCase().includes(Input1Copy.text.toLowerCase())))\n\t\t}\n\t},\n}",
+    oldName: "Input1Copy",
+    newName: "Input1",
+    isJSObject: true,
     evalVersion: 2,
   },
 ];
 const expectedResponse = [
-  { script: "ApiForever", count: 1 },
-  { script: "ApiForever.data", count: 1 },
+  { script: "ApiForever", refactorCount: 1 },
+  { script: "ApiForever.data", refactorCount: 1 },
   {
     script:
       "//   ApiNever  \n function ApiNever(abc) {let foo = \"I'm getting data from ApiNever but don't rename this string\" +     ApiForever.data; \n if(true) { return ApiForever }}",
-    count: 2,
+    refactorCount: 2,
   },
   {
     script:
       "//ApiNever  \n function ApiNever(abc) {let ApiNever = \"I'm getting data from ApiNever but don't rename this string\" +     ApiNever.data; \n if(true) { return ApiNever }}",
-    count: 0,
+    refactorCount: 0,
+  },
+  {
+    script:
+      "export default {\n\tmyVar1: [],\n\tmyVar2: {},\n\t\tsearch: () => {\n\t\tif(Input1.text.length==0){\n\t\t\treturn select_repair_db.data\n\t\t}\n\t\telse{\n\t\t\treturn(select_repair_db.data.filter(word => word.cust_name.toLowerCase().includes(Input1.text.toLowerCase())))\n\t\t}\n\t},\n}",
+    refactorCount: 2,
   },
 ];
+
+type EntityRefactorResponse = {
+  isSuccess: boolean;
+  body: { script: string; refactorCount: number };
+};
 
 describe("Check if shared/ast module is loaded", () => {
   const input = {
@@ -51,21 +73,20 @@ describe("Check if shared/ast module is loaded", () => {
     evalVersion: 2,
   };
 
-  type EntityRefactorReturnType = {
-    script: string;
-    count: number;
-  };
-
+  //These are integration test to verify the loading of @shared/ast module.
   entityRefactor.forEach(async (input, index) => {
     it(`Entity refactor test case ${index + 1}`, async () => {
       const res = entityRefactorFromCode(
         input.script,
         input.oldName,
         input.newName,
+        input.isJSObject,
         input.evalVersion,
-      ) as EntityRefactorReturnType;
-      expect(res.script).toEqual(expectedResponse[index].script);
-      expect(res.count).toEqual(expectedResponse[index].count);
+      ) as EntityRefactorResponse;
+      expect(res.body.script).toEqual(expectedResponse[index].script);
+      expect(res.body.refactorCount).toEqual(
+        expectedResponse[index].refactorCount,
+      );
     });
   });
 });
