d29a158d638199427b907aecaaaabc344f4512f9
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2022-10-27 20:07:05 +0530

    fix DateTime_spec

--- app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/Postgres_DataTypes/DateTime_Spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/ServerSideTests/Postgres_DataTypes/DateTime_Spec.ts
@@ -4,18 +4,21 @@ let dsName: any, query: string;
 const agHelper = ObjectsRegistry.AggregateHelper,
   ee = ObjectsRegistry.EntityExplorer,
   dataSources = ObjectsRegistry.DataSources,
-  propPane = ObjectsRegistry.PropertyPane,
   table = ObjectsRegistry.Table,
   locator = ObjectsRegistry.CommonLocators,
-  deployMode = ObjectsRegistry.DeployMode;
+  deployMode = ObjectsRegistry.DeployMode,
+  appSettings = ObjectsRegistry.AppSettings;
 
 describe("DateTime Datatype tests", function() {
   before(() => {
     cy.fixture("Datatypes/DateTimeDTdsl").then((val: any) => {
       agHelper.AddDsl(val);
     });
-    propPane.ChangeThemeColor(22, "Primary");
-    propPane.ChangeThemeColor(32, "Background");
+    appSettings.openPaneFromCta();
+    appSettings.goToThemeSettings();
+    appSettings.theme.ChangeThemeColor(22, "Primary");
+    appSettings.theme.ChangeThemeColor(32, "Background");
+    appSettings.closePane();
   });
 
   it("1. Create Postgress DS", function() {
