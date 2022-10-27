3f4c4f662d194d747f3d3b9f36368164ae8ea27e
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2022-10-27 21:27:19 +0100

    feat: add cypress test for serverSide pagination

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/ListV2/ListV2_PageNo_PageSize_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/ListV2/ListV2_PageNo_PageSize_spec.js
@@ -110,7 +110,51 @@ describe("List widget V2 page number and page size", () => {
     cy.get(commonlocators.bodyTextStyle)
       .first()
       .should("have.text", "Page Number 2");
+
+    cy.openPropertyPane("listwidgetv2");
+    cy.get(".t--delete-widget").click({ force: true });
   });
 
-  // TODO: Serverside Pagination
+  it("2. List widget V2 with server side pagination", () => {
+    cy.dragAndDropToCanvas("listwidgetv2", {
+      x: 300,
+      y: 300,
+    });
+    cy.openPropertyPane("listwidgetv2");
+
+    // toggle serversidepagination -> true
+    cy.togglebar(".t--property-control-serversidepagination input");
+    cy.wait("@updateLayout");
+
+    // toggle js of onPageSizeChange
+    cy.get(".t--property-control-onpagesizechange")
+      .find(".t--js-toggle")
+      .click({
+        force: true,
+      });
+    // Bind with MultiApi with static value
+    cy.testJsontext(
+      "onpagesizechange",
+      "{{showAlert('Page Size Changed ' + List1.pageSize)}}",
+    );
+    // toggle serversidepagination -> false
+    cy.togglebarDisable(".t--property-control-serversidepagination input");
+
+    cy.openPropertyPane("textwidget");
+    cy.testJsontext("text", `PageSize {{List1.pageSize}}`);
+    cy.wait("@updateLayout");
+
+    cy.get(commonlocators.bodyTextStyle)
+      .first()
+      .should("have.text", "PageSize 3");
+
+    // toggle serversidepagination -> true
+    cy.openPropertyPane("listwidgetv2");
+    cy.togglebar(".t--property-control-serversidepagination input");
+    cy.validateToastMessage("Page Size Changed 2");
+
+    cy.get(commonlocators.bodyTextStyle)
+      .first()
+      .should("have.text", "PageSize 2");
+  });
 });
