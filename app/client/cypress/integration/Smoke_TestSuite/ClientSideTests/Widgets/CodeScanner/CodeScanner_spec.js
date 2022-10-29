4ebb2c05db1b223d387421b6f68900ced56297ba
Dhruvik Neharia <dhruvik@appsmith.com>
2022-10-30 04:25:07 +0530

    test: Add Cypress tests for different scanner layouts for code scanner

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/CodeScanner/CodeScanner_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Widgets/CodeScanner/CodeScanner_spec.js
@@ -4,46 +4,206 @@ const commonlocators = require("../../../../../locators/commonlocators.json");
 const publish = require("../../../../../locators/publishWidgetspage.json");
 
 const widgetName = "codescannerwidget";
+const codeScannerVideoOnPublishPage = `${publish.codescannerwidget} ${commonlocators.codeScannerVideo}`;
+const codeScannerDisabledSVGIconOnPublishPage = `${publish.codescannerwidget} ${commonlocators.codeScannerDisabledSVGIcon}`;
 
 describe("Code Scanner widget", () => {
-  it("1. Drag & drop Code Scanner/Text widgets", () => {
+  it("1. Drag & drop Code Scanner widget", () => {
+    // Drop the widget
     cy.get(explorer.addWidget).click();
-    cy.dragAndDropToCanvas(widgetName, { x: 300, y: 300 });
+    cy.dragAndDropToCanvas(widgetName, { x: 300, y: 100 });
+
+    // Widget should be on the canvas
     cy.get(widgetsPage.codescannerwidget).should("exist");
-    cy.dragAndDropToCanvas("textwidget", { x: 300, y: 500 });
+
+    // Drop a text widget to test the code scanner value binding
+    cy.dragAndDropToCanvas("textwidget", { x: 300, y: 600 });
     cy.openPropertyPane("textwidget");
-    cy.updateCodeInput(".t--property-control-text", `{{CodeScanner1.value}}`);
+    cy.moveToContentTab();
+    cy.updateCodeInput(
+      ".t--property-control-text",
+      `CodeScanner Value - {{CodeScanner1.value}}`,
+    );
+
+    cy.wait(200);
+
+    // Value will be undefined initially
+    cy.get(commonlocators.TextInside).should(
+      "have.text",
+      `CodeScanner Value - undefined`,
+    );
   });
 
-  it("2. Code Scanner functionality to check disabled widget", function() {
+  it("2. Check if default scanner layout is ALWAYS_ON", () => {
+    // Update the text widget to check the value of scanner layout
+    cy.openPropertyPane("textwidget");
+    cy.moveToContentTab();
+    cy.updateCodeInput(
+      ".t--property-control-text",
+      `{{CodeScanner1.scannerLayout}}`,
+    );
+
+    cy.wait(200);
+
+    // Check the value of scanner layout
+    cy.get(commonlocators.TextInside).should("have.text", "ALWAYS_ON");
+  });
+
+  it("3. Check if the ALWAYS_ON scanner is DISABLED and (1) IS showing disabled icon (2) NOT streaming video ", function() {
     cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Disable and publish
     cy.togglebar(commonlocators.disableCheckbox);
     cy.PublishtheApp();
-    cy.get(publish.codescannerwidget + " " + "button").should("be.disabled");
+
+    // Video should NOT be streaming
+    cy.get(codeScannerVideoOnPublishPage).should("not.exist");
+
+    // Disabled icon should be there
+    cy.get(codeScannerDisabledSVGIconOnPublishPage).should("exist");
+
+    // Back to editor
     cy.get(publish.backToEditor).click();
   });
 
-  it("3. Code Scanner functionality to check enabled widget", function() {
+  it("4. Check if the ALWAYS_ON scanner is ENABLED and (1) NOT showing disabled icon (2) IS streaming video", () => {
     cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Enable and publish
     cy.togglebarDisable(commonlocators.disableCheckbox);
     cy.PublishtheApp();
-    cy.get(publish.codescannerwidget + " " + "button").should("be.enabled");
+
+    // Video should be streaming
+    cy.get(codeScannerVideoOnPublishPage).should("exist");
+
+    // Disabled icon should NOT be visible
+    cy.get(codeScannerDisabledSVGIconOnPublishPage).should("not.exist");
+
+    // Back to editor
     cy.get(publish.backToEditor).click();
   });
 
-  it("4. Code Scanner functionality to uncheck visible widget", function() {
+  it("5. Check if the ALWAYS_ON scanner is (1) NOT visible (2) NOT streaming video", function() {
     cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Visibilty OFF and publish
     cy.togglebarDisable(commonlocators.visibleCheckbox);
     cy.PublishtheApp();
-    cy.get(publish.codescannerwidget + " " + "button").should("not.exist");
+
+    // Video should NOT be streaming
+    cy.get(codeScannerVideoOnPublishPage).should("not.exist");
+
+    // Back to editor
     cy.get(publish.backToEditor).click();
   });
 
-  it("5. Code Scanner functionality to check visible widget", function() {
+  it("6. Check if the ALWAYS_ON scanner (1) IS visible (2) IS streaming video", function() {
     cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Visibilty ON and publish
     cy.togglebar(commonlocators.visibleCheckbox);
     cy.PublishtheApp();
+
+    // Video should be streaming
+    cy.get(codeScannerVideoOnPublishPage).should("be.visible");
+
+    // Back to editor
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("7. Change scanner layout from ALWAYS_ON to CLICK_TO_SCAN and verify layout change", function() {
+    cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Select scanner layout as CLICK_TO_SCAN
+    cy.get(
+      `${commonlocators.codeScannerScannerLayout} .t--button-tab-CLICK_TO_SCAN`,
+    )
+      .last()
+      .click({
+        force: true,
+      });
+
+    cy.wait(200);
+
+    // Check if previously dropped text widget with value {{CodeScanner1.scannerLayout}} is updated
+    cy.get(commonlocators.TextInside).should("have.text", "CLICK_TO_SCAN");
+
+    // Publish
+    cy.PublishtheApp();
+
+    // Check if a button is added to the canvas
     cy.get(publish.codescannerwidget + " " + "button").should("be.visible");
+    cy.get(publish.codescannerwidget + " " + "button").should("be.enabled");
+
+    // and video should not be streaming
+    cy.get(codeScannerVideoOnPublishPage).should("not.exist");
+
+    // Back to editor
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("8. Check if the CLICK_TO_SCAN scanner's button is DISABLED", function() {
+    cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Disable and publish
+    cy.togglebar(commonlocators.disableCheckbox);
+    cy.PublishtheApp();
+
+    // Button should be disabled
+    cy.get(publish.codescannerwidget + " " + "button").should("be.disabled");
+
+    // Back to editor
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("9. Check if the CLICK_TO_SCAN scanner's button is ENABLED", function() {
+    cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Enable and publish
+    cy.togglebarDisable(commonlocators.disableCheckbox);
+    cy.PublishtheApp();
+
+    // Button should be enabled
+    cy.get(publish.codescannerwidget + " " + "button").should("be.enabled");
+
+    // Back to editor
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("10. Check if the CLICK_TO_SCAN scanner's button is NOT visible", function() {
+    cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Visibilty OFF and publish
+    cy.togglebarDisable(commonlocators.visibleCheckbox);
+    cy.PublishtheApp();
+
+    // Button should NOT be visible
+    cy.get(publish.codescannerwidget + " " + "button").should("not.exist");
+
+    // Back to editor
+    cy.get(publish.backToEditor).click();
+  });
+
+  it("11. Check if the CLICK_TO_SCAN scanner's button is IS visible", function() {
+    cy.openPropertyPane(widgetName);
+    cy.moveToContentTab();
+
+    // Visibilty ON and publish
+    cy.togglebar(commonlocators.visibleCheckbox);
+    cy.PublishtheApp();
+
+    // Button should be visible
+    cy.get(publish.codescannerwidget + " " + "button").should("be.visible");
+
+    // Back to editor
     cy.get(publish.backToEditor).click();
   });
 
