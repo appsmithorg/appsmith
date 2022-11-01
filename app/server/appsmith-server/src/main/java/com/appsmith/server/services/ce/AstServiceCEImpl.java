f5061d94069d5a3b914ad2fda7deaebec03d405b
subrata <subrata@appsmith.com>
2022-11-01 16:23:28 +0600

    Change pending acquire queue timeout to 10 seconds (#17619)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/AstServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/AstServiceCEImpl.java
@@ -37,7 +37,7 @@ public class AstServiceCEImpl implements AstServiceCE {
             .maxConnections(1000)
             .maxIdleTime(Duration.ofSeconds(5))
             .maxLifeTime(Duration.ofSeconds(10))
-            .pendingAcquireTimeout(Duration.ofSeconds(5))
+            .pendingAcquireTimeout(Duration.ofSeconds(10))
             .pendingAcquireMaxCount(-1)
             .evictInBackground(Duration.ofSeconds(60))
             .build());
