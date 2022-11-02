84e6c1c19e21045d0f7c9b6f0abf2cec08d5f41d
Satish Gandham <hello@satishgandham.com>
2022-11-02 21:55:50 +0530

    Upload source maps to s3

--- app/client/build.sh
+++ app/client/build.sh
@@ -8,5 +8,7 @@ echo "Sentry Auth Token: $SENTRY_AUTH_TOKEN"
 
 REACT_APP_SENTRY_RELEASE=$GIT_SHA REACT_APP_CLIENT_LOG_LEVEL=ERROR EXTEND_ESLINT=true craco --max-old-space-size=4096 build --config craco.build.config.js
 
+node ./upload-source-maps.js
+
 rm ./build/static/js/*.js.map
 echo "build finished"
---
+++ app/client/upload-source-maps.js
@@ -0,0 +1,40 @@
+const AWS = require("aws-sdk");
+var fs = require("fs");
+const path = require("path");
+
+AWS.config.update({ region: "us-east-2" });
+const key = process.env.APPSMITH_PERFORMANCE_S3_KEY;
+const secret = process.env.APPSMITH_PERFORMANCE_S3_SECRET;
+
+const BUCKET_NAME = "performance-infra-artifacts-appsmith";
+// Create S3 service object
+const s3 = new AWS.S3({
+  accessKeyId: key,
+  secretAccessKey: secret,
+});
+
+uploadFiles = async () => {
+  if (!process.env.CI) {
+    return;
+  }
+  const files = fs.readdirSync("./build/static/js/");
+  files.forEach(async (file) => {
+    if (file.endsWith("js.map")) {
+      // upload to s3
+      const inputStream = fs.createReadStream(
+        path.join(__dirname, `build/static/js/${file}`),
+      );
+
+      const params = {
+        Key: `source-maps/${file}`,
+        Body: inputStream,
+        Bucket: BUCKET_NAME,
+      };
+
+      await s3.upload(params).promise();
+      return;
+    }
+  });
+};
+
+uploadFiles();
