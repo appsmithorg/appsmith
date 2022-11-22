ccd8b43778150510c47e280ac2a80a4ef7badd7c
Ankur Singhal <ankurrsinghal@gmail.com>
2022-11-22 18:43:42 +0530

    fixed auto height container test issue

--- app/client/src/components/autoHeight/AutoHeightContainer.test.tsx
+++ app/client/src/components/autoHeight/AutoHeightContainer.test.tsx
@@ -21,7 +21,7 @@ describe("<AutoHeightContainer />", () => {
         </AutoHeightContainer>,
       )
       .toJSON();
-    expect(tree).toHaveStyleRule("height", "auto");
+    expect(tree).toHaveStyleRule("height", "auto !important");
   });
 
   describe("when isAutoHeightWithLimits is false.", () => {
