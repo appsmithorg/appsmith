9aebf9fa383b88892b37d5e757d43b5d080cc558
Aswath K <aswath@appsmith.com>
2022-11-22 12:54:13 +0530

    Increments LATEST_PAGE_VERSION

--- app/client/src/constants/WidgetConstants.tsx
+++ app/client/src/constants/WidgetConstants.tsx
@@ -72,7 +72,7 @@ export const layoutConfigurations: LayoutConfigurations = {
   FLUID: { minWidth: -1, maxWidth: -1 },
 };
 
-export const LATEST_PAGE_VERSION = 67;
+export const LATEST_PAGE_VERSION = 68;
 
 export const GridDefaults = {
   DEFAULT_CELL_SIZE: 1,
