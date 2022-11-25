6046484c2de0c75570c2703a280dc69dd5e5d341
Pawan Kumar <pawan.stardust@gmail.com>
2022-11-25 08:36:08 +0530

    add config for list widget

--- app/client/src/widgets/ListWidget/index.ts
+++ app/client/src/widgets/ListWidget/index.ts
@@ -407,6 +407,7 @@ export const CONFIG = {
     config: Widget.getPropertyPaneConfig(),
     contentConfig: Widget.getPropertyPaneContentConfig(),
     styleConfig: Widget.getPropertyPaneStyleConfig(),
+    stylesheetConfig: Widget.getStylesheetConfig(),
   },
 };
 
