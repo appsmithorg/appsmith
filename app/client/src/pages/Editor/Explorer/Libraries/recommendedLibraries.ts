f475fefb212d2a679720aeb48e3ff85dbf3de73e
arunvjn <arun@appsmith.com>
2022-11-25 06:42:36 +0530

    Fixed library urls

--- app/client/src/pages/Editor/Explorer/Libraries/recommendedLibraries.ts
+++ app/client/src/pages/Editor/Explorer/Libraries/recommendedLibraries.ts
@@ -1,5 +1,3 @@
-const domain = window.location.origin;
-
 export default [
   {
     name: "uuidjs",
@@ -7,6 +5,7 @@ export default [
     description:
       "UUID.js is a JavaScript/ECMAScript library to generate RFC 4122 compliant Universally Unique IDentifiers (UUIDs). This library supports both version 4 UUIDs (UUIDs from random numbers) and version 1 UUIDs (time-based UUIDs), and provides an object-oriented interface to print a generated or parsed UUID in a variety of forms.",
     author: "LiosK",
+    docsURL: "https://github.com/uuidjs/uuid",
     version: "4.2.12",
     icon: "https://github.com/LiosK.png?s=20",
   },
@@ -14,14 +13,16 @@ export default [
     name: "jsonwebtoken",
     description: "JSON Web Token implementation (symmetric and asymmetric)",
     author: "auth0",
+    docsURL: "https://github.com/auth0/node-jsonwebtoken#readme",
     version: "8.5.1",
-    url: `${domain}/libraries/jsonwebtoken@8.5.1.js`,
+    url: `/libraries/jsonwebtoken@8.5.1.js`,
     icon: "https://github.com/auth0.png?s=20",
   },
   {
     name: "@supabase/supabase-js",
     description: "Isomorphic Javascript client for Supabase",
     author: "supabase",
+    docsURL: "https://supabase.com/docs/reference/javascript",
     version: "2.1.0",
     url:
       "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.1.0/dist/umd/supabase.min.js",
@@ -34,29 +35,35 @@ export default [
     description:
       "Analytics Next (aka Analytics 2.0) is the latest version of Segment’s JavaScript SDK - enabling you to send your data to any tool without having to learn, test, or use a new API every time.",
     author: "segmentio",
+    docsURL:
+      "https://github.com/segmentio/analytics-next/tree/master/packages/browser#readme",
     version: "1.46.1",
     icon: "https://github.com/segmentio.png?s=20",
   },
   {
     name: "Mixpanel",
     description: "Isomorphic Javascript client for Supabase",
-    author: "supabase",
+    author: "mixpanel",
+    docsURL: "https://developer.mixpanel.com/docs/javascript",
     version: "2.1.0",
-    url: `${domain}/libraries/mixpanel@0.17.0.js`,
+    url: `/libraries/mixpanel@0.17.0.js`,
     icon: "https://github.com/mixpanel.png?s=20",
   },
   {
     name: "fast-csv",
     description: "CSV parser and writer",
     author: "C2FO",
+    docsURL:
+      "https://c2fo.github.io/fast-csv/docs/introduction/getting-started/",
     version: "4.3.6",
-    url: `${domain}/libraries/fast-csv@4.3.6.js`,
+    url: `/libraries/fast-csv@4.3.6.js`,
     icon: "https://github.com/C2FO.png?s=20",
   },
   {
     name: "ky",
     description: "Tiny and elegant HTTP client based on the browser Fetch API",
     author: "sindresorhus",
+    docsURL: "https://github.com/sindresorhus/ky#usage",
     version: "0.25.0",
     url: "https://www.unpkg.com/ky@0.25.0/umd.js",
     icon: "https://github.com/sindresorhus.png?s=20",
@@ -65,6 +72,7 @@ export default [
     name: "jspdf",
     description: "PDF Document creation from JavaScript",
     author: "MrRio",
+    docsURL: "https://raw.githack.com/MrRio/jsPDF/master/docs/index.html",
     version: "2.5.1",
     url: "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
     icon: "https://github.com/MrRio.png?s=20",
@@ -73,6 +81,8 @@ export default [
     name: "@amplitude/analytics-browser",
     description: "Official Amplitude SDK for Web",
     author: "amplitude",
+    docsURL:
+      "https://github.com/amplitude/Amplitude-TypeScript/tree/main/packages/analytics-browser#usage",
     version: "1.6.1",
     url:
       "https://cdn.jsdelivr.net/npm/@amplitude/analytics-browser@1.6.1/lib/scripts/amplitude-min.umd.js",
@@ -82,14 +92,16 @@ export default [
     name: "uzip-module",
     description: "Module version of UZIP.js",
     author: "greggman",
+    docsURL: "https://github.com/greggman/uzip-module#uzip-module",
     version: "1.0.3",
-    url: `${domain}/libraries/uzip-module@1.0.3.js`,
+    url: `/libraries/uzip-module@1.0.3.js`,
     icon: "https://github.com/greggman.png?s=20",
   },
   {
     name: "@sentry/browser",
     description: "Official Sentry SDK for browsers",
     author: "getsentry",
+    docsURL: "https://docs.sentry.io/platforms/javascript/",
     version: "7.17.3",
     url: "https://browser.sentry-cdn.com/7.17.3/bundle.min.js",
     icon: "https://github.com/getsentry.png?s=20",
@@ -100,6 +112,8 @@ export default [
       "https://cdn.jsdelivr.net/npm/browser-image-compression@2.0.0/dist/browser-image-compression.min.js",
     version: "2.0.0",
     author: "Donaldcwl",
+    docsURL:
+      "https://github.com/Donaldcwl/browser-image-compression/#browser-image-compression",
     description: "Compress images in the browser",
     icon: "https://github.com/Donaldcwl.png?s=20",
   },
@@ -110,6 +124,7 @@ export default [
     description: "JavaScript library of crypto standards.",
     version: "4.1.1",
     author: "brix",
+    docsURL: "https://github.com/brix/crypto-js/#crypto-js",
     icon: "https://github.com/brix.png?s=20",
   },
   {
@@ -119,6 +134,7 @@ export default [
       "Query JavaScript objects with JSONPath expressions. Robust / safe JSONPath engine for Node.js.",
     version: "1.1.1",
     author: "dchester",
+    docsURL: "https://github.com/dchester/jsonpath/#jsonpath",
     icon: "https://github.com/dchester.png?s=20",
   },
 ];
