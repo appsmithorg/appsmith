64e284f21e76bd2dbc2ec2011f3cdd6122de7915
Abhinav Jha <abhinav@appsmith.com>
2022-11-25 21:01:47 +0530

    Remove console logs, and fix tests

--- app/client/src/reducers/entityReducers/canvasWidgetsReducer.ts
+++ app/client/src/reducers/entityReducers/canvasWidgetsReducer.ts
@@ -86,7 +86,6 @@ const canvasWidgetsReducer = createImmerReducer(initialState, {
     state: CanvasWidgetsReduxState,
     action: ReduxAction<UpdateWidgetsPayload>,
   ) => {
-    const start = performance.now();
     // For each widget whose properties we would like to update
     for (const [widgetId, propertyPathsToUpdate] of Object.entries(
       action.payload,
@@ -102,7 +101,6 @@ const canvasWidgetsReducer = createImmerReducer(initialState, {
           set(state, path, propertyValue);
       });
     }
-    console.log("Setting redux state: Time taken:", performance.now() - start);
   },
 });
 
