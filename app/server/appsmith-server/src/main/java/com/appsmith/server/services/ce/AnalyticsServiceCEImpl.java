36b6d170969c0f06e2b5bb54a0b21f8cbcb8ca1a
rahulramesha <71900764+rahulramesha@users.noreply.github.com>
2022-11-25 22:47:40 +0530

    fix: Overlapping of widgets while reflowing other widgets (#18460)
    * fix: api url z-index to show it above response panel (#18200)
    
    * chore: Switched to sequential checks instead of a parallel one for RTS check (#18440)
    
    fix: Switched to sequential checks instead of a parallel one
    
    * chore: Added size limit check for the message before sending the data to segment (#18453)
    
    * fix: Allowing multi form to json switching and eliminating json to form sw… (#18192)
    
    * Allowing multi form to json switching and eliminating json to form switching unless form data is cleared
    
    * Fix failing jest test case
    
    Co-authored-by: Aishwarya UR <aishwarya@appsmith.com>
    
    * add missed width and height limits instead of incrementing depth by 1
    
    Co-authored-by: Aman Agarwal <aman@appsmith.com>
    Co-authored-by: Nidhi <nidhi@appsmith.com>
    Co-authored-by: Anagh Hegde <anagh@appsmith.com>
    Co-authored-by: Ayangade Adeoluwa <37867493+Irongade@users.noreply.github.com>
    Co-authored-by: Aishwarya UR <aishwarya@appsmith.com>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/AnalyticsServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/AnalyticsServiceCEImpl.java
@@ -11,8 +11,11 @@ import com.appsmith.server.helpers.PolicyUtils;
 import com.appsmith.server.helpers.UserUtils;
 import com.appsmith.server.services.ConfigService;
 import com.appsmith.server.services.SessionUserService;
+import com.google.gson.Gson;
 import com.segment.analytics.Analytics;
+import com.segment.analytics.Log;
 import com.segment.analytics.messages.IdentifyMessage;
+import com.segment.analytics.messages.Message;
 import com.segment.analytics.messages.TrackMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
@@ -22,10 +25,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import static com.segment.analytics.internal.AnalyticsClient.getGsonInstance;
 
 @Slf4j
 public class AnalyticsServiceCEImpl implements AnalyticsServiceCE {
@@ -164,6 +170,15 @@ public class AnalyticsServiceCEImpl implements AnalyticsServiceCE {
             analyticsProperties.put("originService", "appsmith-server");
             analyticsProperties.put("instanceId", instanceId);
             messageBuilder = messageBuilder.properties(analyticsProperties);
+
+            // TODO Remove thise code block after finding the event that is causing the size limit issue for segment
+            Message message = messageBuilder.build();
+            Gson gson = getGsonInstance();
+            String stringifiedMessage = gson.toJson(message);
+            int sizeInBytes = stringifiedMessage.getBytes(Charset.forName("UTF-8")).length;
+            if (sizeInBytes  > 32768) {
+                log.error("Message was above individual limit. Message content {}, event {}", stringifiedMessage, event);
+            }
             analytics.enqueue(messageBuilder);
             return instanceId;
         }).subscribeOn(Schedulers.boundedElastic()).subscribe();
