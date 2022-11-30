3168bdfdc2cba789d99ee1c09d608748a5b66c43
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2022-11-30 11:45:50 +0530

    refactor as per review comments

--- app/client/src/workers/Evaluation/evaluation.worker.ts
+++ app/client/src/workers/Evaluation/evaluation.worker.ts
@@ -9,8 +9,6 @@ import {
 } from "utils/DynamicBindingUtils";
 import {
   CrashingError,
-  makeDataTreeEntityConfigAsProperty,
-  createUnEvalTreeForEval,
   DataTreeDiff,
   getSafeToRenderDataTree,
   removeFunctions,
@@ -37,6 +35,10 @@ import evaluate, {
 import { JSUpdate } from "utils/JSPaneUtils";
 import { validateWidgetProperty } from "workers/common/DataTreeEvaluator/validationUtils";
 import { initiateLinting } from "workers/Linting/utils";
+import {
+  createUnEvalTreeForEval,
+  makeEntityConfigsAsObjProperties,
+} from "./dataTreeUtils";
 
 const CANVAS = "canvas";
 
@@ -269,17 +271,14 @@ function eventRequestHandler({
 
           initiateLinting(
             lintOrder,
-            makeDataTreeEntityConfigAsProperty(
-              dataTreeEvaluator.oldUnEvalTree,
-              {
-                sanitizeDataTree: false,
-              },
-            ),
+            makeEntityConfigsAsObjProperties(dataTreeEvaluator.oldUnEvalTree, {
+              sanitizeDataTree: false,
+            }),
             requiresLinting,
           );
 
           const dataTreeResponse = dataTreeEvaluator.evalAndValidateFirstTree();
-          dataTree = makeDataTreeEntityConfigAsProperty(
+          dataTree = makeEntityConfigsAsObjProperties(
             dataTreeResponse.evalTree,
           );
         } else if (dataTreeEvaluator.hasCyclicalDependency) {
@@ -311,17 +310,14 @@ function eventRequestHandler({
 
           initiateLinting(
             lintOrder,
-            makeDataTreeEntityConfigAsProperty(
-              dataTreeEvaluator.oldUnEvalTree,
-              {
-                sanitizeDataTree: false,
-              },
-            ),
+            makeEntityConfigsAsObjProperties(dataTreeEvaluator.oldUnEvalTree, {
+              sanitizeDataTree: false,
+            }),
             requiresLinting,
           );
 
           const dataTreeResponse = dataTreeEvaluator.evalAndValidateFirstTree();
-          dataTree = makeDataTreeEntityConfigAsProperty(
+          dataTree = makeEntityConfigsAsObjProperties(
             dataTreeResponse.evalTree,
           );
         } else {
@@ -344,12 +340,9 @@ function eventRequestHandler({
 
           initiateLinting(
             lintOrder,
-            makeDataTreeEntityConfigAsProperty(
-              dataTreeEvaluator.oldUnEvalTree,
-              {
-                sanitizeDataTree: false,
-              },
-            ),
+            makeEntityConfigsAsObjProperties(dataTreeEvaluator.oldUnEvalTree, {
+              sanitizeDataTree: false,
+            }),
             requiresLinting,
           );
           nonDynamicFieldValidationOrder =
@@ -358,7 +351,7 @@ function eventRequestHandler({
             evalOrder,
             nonDynamicFieldValidationOrder,
           );
-          dataTree = makeDataTreeEntityConfigAsProperty(
+          dataTree = makeEntityConfigsAsObjProperties(
             dataTreeEvaluator.evalTree,
           );
           evalMetaUpdates = JSON.parse(
@@ -393,7 +386,7 @@ function eventRequestHandler({
         }
 
         dataTree = getSafeToRenderDataTree(
-          makeDataTreeEntityConfigAsProperty(unevalTree),
+          makeEntityConfigsAsObjProperties(unevalTree),
           widgetTypeConfigMap,
         );
 
