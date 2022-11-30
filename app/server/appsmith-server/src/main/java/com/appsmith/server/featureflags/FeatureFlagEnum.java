fbd3d14954b715b9eea709bd6f1d286b7c98d4af
Ankita Kinger <ankita@appsmith.com>
2022-11-30 16:20:36 +0530

    added a missing comma

--- app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/FeatureFlagEnum.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/FeatureFlagEnum.java
@@ -30,7 +30,7 @@ public enum FeatureFlagEnum {
     APP_TEMPLATE,
     TEMPLATES_PHASE_2,
     CONTEXT_SWITCHING,
-    DATASOURCE_ENVIRONMENTS
+    DATASOURCE_ENVIRONMENTS,
 
     // Put EE flags below this line, to avoid conflicts.
     RBAC,
