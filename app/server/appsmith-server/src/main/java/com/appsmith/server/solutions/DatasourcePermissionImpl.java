a002d67c7549c0612b3f08bbf4aa075e47ea7114
Trisha Anand <trisha@appsmith.com>
2022-12-01 19:31:57 +0530

    fix: Using delete permission to delete datasources in EE (#766)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/DatasourcePermissionImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/DatasourcePermissionImpl.java
@@ -1,8 +1,14 @@
 package com.appsmith.server.solutions;
 
+import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.solutions.ce.DatasourcePermissionCEImpl;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatasourcePermissionImpl extends DatasourcePermissionCEImpl implements DatasourcePermission {
+
+    @Override
+    public AclPermission getDeletePermission() {
+        return AclPermission.DELETE_DATASOURCES;
+    }
 }
