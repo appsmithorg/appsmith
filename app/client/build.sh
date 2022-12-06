d59aaddc4b069c529f3ab38a51d1d44a74b7e10f
Satish Gandham <hello@satishgandham.com>
2022-12-06 09:55:15 +0530

    Update if condition

--- app/client/build.sh
+++ app/client/build.sh
@@ -9,7 +9,7 @@ echo "Sentry Auth Token: $SENTRY_AUTH_TOKEN"
 REACT_APP_SENTRY_RELEASE=$GIT_SHA REACT_APP_CLIENT_LOG_LEVEL=ERROR EXTEND_ESLINT=true craco --max-old-space-size=4096 build --config craco.build.config.js
 
 
-if [[ -n "$CI" && "$GITHUB_REPOSITORY" == "appsmithorg/appsmith-ee" ]]; then
+if [ "$GITHUB_REPOSITORY" == "appsmithorg/appsmith-ee" ]; then
     echo "Deleting sourcemaps for EE"
     rm ./build/static/js/*.js.map
     rm ./build/static/js/*.js.map.gz
