485b6930342d578a504453ccfc96d560a21f73f6
Ravi Kumar Prasad <ravi@appsmith.com>
2022-12-07 10:56:12 +0530

    Address review comments

--- app/client/src/components/editorComponents/CodeEditor/utils/codeComment.ts
+++ app/client/src/components/editorComponents/CodeEditor/utils/codeComment.ts
@@ -9,23 +9,47 @@ export const getCodeCommentKeyMap = () => {
 // with minor modifications to support commenting in JS fields with {{ }} syntax
 // CodeMirror's APIs don't allow such things, so copied functions and overrode them
 
+/** Get end of line for line comment */
+function getEndLineForLineComment(
+  from: CodeMirror.Position,
+  to: CodeMirror.Position,
+  cm: CodeMirror.Editor,
+) {
+  return Math.min(
+    to.ch != 0 || to.line == from.line ? to.line + 1 : to.line,
+    cm.lastLine() + 1,
+  );
+}
+
+/** Get end of line for line comment */
+function getEndLineForLineUncomment(
+  from: CodeMirror.Position,
+  to: CodeMirror.Position,
+  cm: CodeMirror.Editor,
+) {
+  return Math.min(
+    to.ch != 0 || to.line == from.line ? to.line : to.line - 1,
+    cm.lastLine() + 1,
+  );
+}
+
 const JS_FIELD_BEGIN = "{{";
 
-const nonWS = /[^\s\u00a0]/;
+const nonWhitespace = /[^\s\u00a0]/;
 
 /**
  * Gives index of the first non whitespace character in the line
  **/
-function firstNonWS(str: string) {
-  const found = str.search(nonWS);
-  return found == -1 ? 0 : found;
+function firstNonWhitespace(str: string) {
+  const found = str.search(nonWhitespace);
+  return found === -1 ? 0 : found;
 }
 
 // Rough heuristic to try and detect lines that are part of multi-line string
 function probablyInsideString(
   cm: CodeMirror.Editor,
   pos: CodeMirror.Position,
-  line: any,
+  line: string,
 ) {
   return (
     /\bstring\b/.test(cm.getTokenTypeAt(CodeMirror.Pos(pos.line, 0))) &&
@@ -49,7 +73,7 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
     const self = this,
       mode = getMode(self, from);
     const firstLine = self.getLine(from.line);
-    if (firstLine == null || probablyInsideString(self, from, firstLine))
+    if (firstLine === null || probablyInsideString(self, from, firstLine))
       return;
 
     const commentString = options.lineComment || mode.lineComment;
@@ -61,75 +85,54 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
       return;
     }
 
-    const end = Math.min(
-      to.ch != 0 || to.line == from.line ? to.line + 1 : to.line,
-      self.lastLine() + 1,
-    );
-    const pad = options.padding == null ? " " : options.padding;
-    const blankLines = options.commentBlankLines || from.line == to.line;
+    const end = getEndLineForLineComment(from, to, self);
+    const padding = options.padding || " ";
+    const blankLines = options.commentBlankLines || from.line === to.line;
 
     self.operation(function() {
       if (options.indent) {
-        /* Commented below line to handle baseString individually for each line */
-        // let baseString = null;
-        // for (let i = from.line; i < end; ++i) {
-        //   const line = self.getLine(i);
-
-        //   const whitespace =
-        //     line.search(nonWS) === -1 ? line : line.slice(0, firstNonWS(line));
-        //   if (baseString == null || baseString.length > whitespace.length) {
-        //     baseString = whitespace;
-        //   }
-        // }
-        /* Changes end */
-
         for (let i = from.line; i < end; ++i) {
           const line = self.getLine(i);
 
-          /* Added below code to handle baseString individually for each line */
           const baseString =
-            line.search(nonWS) === -1 ? line : line.slice(0, firstNonWS(line));
-          /* Changes end */
+            line.search(nonWhitespace) === -1
+              ? line
+              : line.slice(0, firstNonWhitespace(line));
 
-          const cut = (baseString || "").length;
+          const offset = (baseString || "").length;
 
-          if (!blankLines && !nonWS.test(line)) continue;
-          /* Commented below line to handle baseString individually for each line */
-          // if (line.slice(0, cut) != baseString) cut = firstNonWS(line);
-          /* Changes end */
+          if (!blankLines && !nonWhitespace.test(line)) continue;
 
-          /* Handle JS field lines starting with {{ */
-          if (line.slice(cut).startsWith(JS_FIELD_BEGIN)) {
+          // Handle JS field lines starting with {{
+          if (line.slice(offset).startsWith(JS_FIELD_BEGIN)) {
             self.replaceRange(
-              baseString + JS_FIELD_BEGIN + commentString + pad,
+              baseString + JS_FIELD_BEGIN + commentString + padding,
               CodeMirror.Pos(i, 0),
-              CodeMirror.Pos(i, cut + JS_FIELD_BEGIN.length),
+              CodeMirror.Pos(i, offset + JS_FIELD_BEGIN.length),
             );
             continue;
           }
-          /* End changes for JS field handling */
 
           self.replaceRange(
-            baseString + commentString + pad,
+            baseString + commentString + padding,
             CodeMirror.Pos(i, 0),
-            CodeMirror.Pos(i, cut),
+            CodeMirror.Pos(i, offset),
           );
         }
       } else {
         for (let i = from.line; i < end; ++i) {
           const line = self.getLine(i);
-          if (blankLines || nonWS.test(line)) {
-            /* Handle JS field lines starting with {{ */
+          if (blankLines || nonWhitespace.test(line)) {
+            // Handle JS field lines starting with {{
             if (line.startsWith(JS_FIELD_BEGIN)) {
               self.replaceRange(
-                commentString + pad,
+                commentString + padding,
                 CodeMirror.Pos(i, JS_FIELD_BEGIN.length),
               );
               continue;
             }
-            /* End changes for JS field handling */
 
-            self.replaceRange(commentString + pad, CodeMirror.Pos(i, 0));
+            self.replaceRange(commentString + padding, CodeMirror.Pos(i, 0));
           }
         }
       }
@@ -141,37 +144,26 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
     // eslint-disable-next-line @typescript-eslint/no-this-alias
     const self = this;
     const mode = getMode(self, from);
-    const end = Math.min(
-      to.ch != 0 || to.line == from.line ? to.line : to.line - 1,
-      self.lastLine(),
-    );
+    const end = getEndLineForLineUncomment(from, to, self);
     const start = Math.min(from.line, end);
 
     // Try finding line comments
     const lineString = options.lineComment || mode.lineComment;
     const lines: string[] = [];
-    const pad = options.padding == null ? " " : options.padding;
-    let didSomething;
+    const padding = options.padding || " ";
+    let didCommentCode;
     lineComment: {
       if (!lineString) break lineComment;
       for (let i = start; i <= end; ++i) {
         const line = self.getLine(i);
         const found = line.indexOf(lineString);
-        /* Lines commented to handle JS fields with text without {{}} */
-        // if (
-        //   found > -1 &&
-        //   !/comment/.test(self.getTokenTypeAt(CodeMirror.Pos(i, found + 1)))
-        // )
-        //   found = -1;
-        /* Change ends */
 
-        if (found == -1 && nonWS.test(line)) break lineComment;
+        if (found == -1 && nonWhitespace.test(line)) break lineComment;
         if (
           found > -1 &&
-          /* Handle JS fields with {{}} */
+          // Handle JS fields with {{}}
           !line.trim().startsWith(JS_FIELD_BEGIN) &&
-          /* End changes for JS fields handling */
-          nonWS.test(line.slice(0, found))
+          nonWhitespace.test(line.slice(0, found))
         )
           break lineComment;
         lines.push(line);
@@ -182,9 +174,9 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
           const pos = line.indexOf(lineString);
           let endPos = pos + lineString.length;
           if (pos < 0) continue;
-          if (line.slice(endPos, endPos + pad.length) == pad)
-            endPos += pad.length;
-          didSomething = true;
+          if (line.slice(endPos, endPos + padding.length) == padding)
+            endPos += padding.length;
+          didCommentCode = true;
           self.replaceRange(
             "",
             CodeMirror.Pos(i, pos),
@@ -192,26 +184,26 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
           );
         }
       });
-      if (didSomething) return true;
+      if (didCommentCode) return true;
     }
 
     // Try block comments
     const startString = options.blockCommentStart || mode.blockCommentStart;
     const endString = options.blockCommentEnd || mode.blockCommentEnd;
     if (!startString || !endString) return false;
-    const lead = options.blockCommentLead || mode.blockCommentLead;
+    const blockCommentLead = options.blockCommentLead || mode.blockCommentLead;
     const startLine = self.getLine(start);
     const open = startLine.indexOf(startString);
     if (open == -1) return false;
-    const endLine = end == start ? startLine : self.getLine(end);
+    const endLine = end === start ? startLine : self.getLine(end);
     const close = endLine.indexOf(
       endString,
-      end == start ? open + startString.length : 0,
+      end === start ? open + startString.length : 0,
     );
     const insideStart = CodeMirror.Pos(start, open + 1),
       insideEnd = CodeMirror.Pos(end, close + 1);
     if (
-      close == -1 ||
+      close === -1 ||
       !/comment/.test(self.getTokenTypeAt(insideStart)) ||
       !/comment/.test(self.getTokenTypeAt(insideEnd)) ||
       self.getRange(insideStart, insideEnd, "\n").indexOf(endString) > -1
@@ -222,14 +214,14 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
     // Positions of the last startString before the start of the selection, and the first endString after it.
     let lastStart = startLine.lastIndexOf(startString, from.ch);
     let firstEnd =
-      lastStart == -1
+      lastStart === -1
         ? -1
         : startLine
             .slice(0, from.ch)
             .indexOf(endString, lastStart + startString.length);
     if (
-      lastStart != -1 &&
-      firstEnd != -1 &&
+      lastStart !== -1 &&
+      firstEnd !== -1 &&
       firstEnd + endString.length != from.ch
     )
       return false;
@@ -239,8 +231,8 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
       .slice(to.ch)
       .lastIndexOf(startString, firstEnd - to.ch);
     lastStart =
-      firstEnd == -1 || almostLastStart == -1 ? -1 : to.ch + almostLastStart;
-    if (firstEnd != -1 && lastStart != -1 && lastStart != to.ch) return false;
+      firstEnd === -1 || almostLastStart === -1 ? -1 : to.ch + almostLastStart;
+    if (firstEnd !== -1 && lastStart != -1 && lastStart !== to.ch) return false;
 
     self.operation(function() {
       self.replaceRange(
@@ -248,34 +240,41 @@ export const handleCodeComment = (cm: CodeMirror.Editor) => {
         CodeMirror.Pos(
           end,
           close -
-            (pad && endLine.slice(close - pad.length, close) == pad
-              ? pad.length
+            (padding && endLine.slice(close - padding.length, close) == padding
+              ? padding.length
               : 0),
         ),
         CodeMirror.Pos(end, close + endString.length),
       );
       let openEnd = open + startString.length;
-      if (pad && startLine.slice(openEnd, openEnd + pad.length) == pad)
-        openEnd += pad.length;
+      if (
+        padding &&
+        startLine.slice(openEnd, openEnd + padding.length) == padding
+      )
+        openEnd += padding.length;
       self.replaceRange(
         "",
         CodeMirror.Pos(start, open),
         CodeMirror.Pos(start, openEnd),
       );
-      if (lead)
+      if (blockCommentLead) {
         for (let i = start + 1; i <= end; ++i) {
           const line = self.getLine(i);
-          const found = line.indexOf(lead);
-          if (found == -1 || nonWS.test(line.slice(0, found))) continue;
-          let foundEnd = found + lead.length;
-          if (pad && line.slice(foundEnd, foundEnd + pad.length) == pad)
-            foundEnd += pad.length;
+          const found = line.indexOf(blockCommentLead);
+          if (found == -1 || nonWhitespace.test(line.slice(0, found))) continue;
+          let foundEnd = found + blockCommentLead.length;
+          if (
+            padding &&
+            line.slice(foundEnd, foundEnd + padding.length) == padding
+          )
+            foundEnd += padding.length;
           self.replaceRange(
             "",
             CodeMirror.Pos(i, found),
             CodeMirror.Pos(i, foundEnd),
           );
         }
+      }
     });
     return true;
   };
