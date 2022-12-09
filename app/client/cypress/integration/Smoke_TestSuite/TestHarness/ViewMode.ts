1ac56c825080dfc0438d90be9be0a0cef17455e9
Aishwarya UR <aishwarya@appsmith.com>
2022-12-09 10:59:31 +0530

    Testharness - started

---
+++ app/client/cypress/integration/Smoke_TestSuite/TestHarness/ViewMode.ts
@@ -0,0 +1,14 @@
+import { ObjectsRegistry } from "../../../support/Objects/Registry";
+const {
+  AggregateHelper: agHelper,
+  CommonLocators: locator,
+  DeployMode: deployMode,
+  EntityExplorer: ee,
+  PropertyPane: propPane,
+} = ObjectsRegistry;
+
+describe("Validate basic test harness", () => {
+  before(() => {});
+
+  it("1. Button with Input widget", () => {});
+});
