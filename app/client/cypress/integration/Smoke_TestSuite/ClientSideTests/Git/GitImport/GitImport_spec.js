78468c4e3bf8d3f2414d22bbd0d74a09e66b0a62
Aishwarya UR <aishwarya@appsmith.com>
2022-12-10 12:23:49 +0530

    Git import unskip

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Git/GitImport/GitImport_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Git/GitImport/GitImport_spec.js
@@ -142,8 +142,9 @@ describe("Git import flow ", function() {
     // verify js object binded to input widget
     cy.xpath("//input[@value='Success']").should("be.visible");
   });
-  // skipping these tests until bug #18776 is fixed
-  it.skip("4. Create a new branch, clone page and validate data on that branch in view and edit mode", () => {
+
+  // skipping this due to open bug #18776
+  it("4. Create a new branch, clone page and validate data on that branch in view and edit mode", () => {
     cy.createGitBranch(newBranch);
     cy.get(".tbody")
       .first()
@@ -213,7 +214,8 @@ describe("Git import flow ", function() {
     cy.wait(2000);
   });
 
-  it.skip("5. Switch to master and verify data in edit and view mode", () => {
+  // skipping this due to open bug #18776
+  it("5. Switch to master and verify data in edit and view mode", () => {
     cy.switchGitBranch("master");
     cy.wait(2000);
     // validate data binding in edit and deploy mode
@@ -236,7 +238,8 @@ describe("Git import flow ", function() {
     cy.wait(2000);
   });
 
-  it.skip("6. Add widget to master, merge then checkout to child branch and verify data", () => {
+  // skipping this due to open bug #18776
+  it("6. Add widget to master, merge then checkout to child branch and verify data", () => {
     cy.get(explorer.widgetSwitchId).click();
     cy.wait(2000); // wait for transition
     cy.dragAndDropToCanvas("buttonwidget", { x: 300, y: 600 });
