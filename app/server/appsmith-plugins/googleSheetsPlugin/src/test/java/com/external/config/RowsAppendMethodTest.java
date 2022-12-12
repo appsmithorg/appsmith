09a6f2a0edcd4c95f84f54a71f57e4bf1d6bf379
Vaibhav Tanwar <vaibhav@appsmith.com>
2022-12-12 12:00:39 +0530

    empty body test added in rowAppendMethodTest for GoogleSheetsPlugin

--- app/server/appsmith-plugins/googleSheetsPlugin/src/test/java/com/external/config/RowsAppendMethodTest.java
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/test/java/com/external/config/RowsAppendMethodTest.java
@@ -1,9 +1,15 @@
 package com.external.config;
 
+import com.appsmith.external.dtos.ExecuteActionDTO;
 import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginException;
+import com.appsmith.external.models.ActionConfiguration;
 import com.appsmith.external.models.AuthenticationResponse;
+import com.appsmith.external.models.DatasourceConfiguration;
 import com.appsmith.external.models.OAuth2;
+import com.appsmith.external.models.PaginationType;
 import com.external.constants.ErrorMessages;
+import com.external.plugins.GoogleSheetsPlugin;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -12,38 +18,62 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 public class RowsAppendMethodTest {
 
-    final ObjectMapper objectMapper = new ObjectMapper();
+    GoogleSheetsPlugin.GoogleSheetsPluginExecutor pluginExecutor = new GoogleSheetsPlugin.GoogleSheetsPluginExecutor();
 
     /**
-     * To Test if it passes the empty Mono criteria,
+     * To Test if it passes the expected exception criteria,
      * else it should fail
      */
     @Test
-    public void testBulkAppendHandleEmptyMonoExecutePrerequisites() {
-        String[] testDataArray = {"{}", ""};
+    public void testRowAppendMethodTestExecutePrerequisites() {
+
+        DatasourceConfiguration datasourceConfiguration = new DatasourceConfiguration();
+        datasourceConfiguration.setAuthentication(getOAuthObject());
+
+        Map<String,Object> formData = new HashMap<>();
+        formData.put("command", Collections.singletonMap("data","INSERT_ONE"));
+        formData.put("entityType", Collections.singletonMap("data","ROWS"));
+        formData.put("tableHeaderIndex", Collections.singletonMap("data","1"));
+        formData.put("projection", Collections.singletonMap("data",Collections.emptyList()));
+        formData.put("queryFormat", Collections.singletonMap("data","ROWS"));
+        formData.put("range", Collections.singletonMap("data",""));
+        formData.put("where", Collections.singletonMap("data",Map.of("condition","AND","children",Collections.singletonList(Collections.singletonMap("condition","LT")))));
+        formData.put("pagination",Collections.singletonMap("data",Map.of("limit",20,"offset",0)));
+        formData.put("smartSubstitution",Collections.singletonMap("data",true));
+        formData.put("sheetUrl",Collections.singletonMap("data","https://docs.google.com/spreadsheets/d/123/edit"));
+        formData.put("sheetName",Collections.singletonMap("data","portSheet"));
+        formData.put("sortBy",Collections.singletonMap("data",Collections.singletonList(Map.of("column","","order","Ascending"))));
+
+        ActionConfiguration actionConfiguration = new ActionConfiguration();
+
+        actionConfiguration.setTimeoutInMillisecond("10000");
+        actionConfiguration.setPaginationType(PaginationType.NONE);
+        actionConfiguration.setEncodeParamsToggle(true);
+        actionConfiguration.setFormData(formData);
+
+        String[] testDataArray = {null,"","{}"};
 
         for(int i=0; i<testDataArray.length; i++) {
-            RowsAppendMethod rowsAppendMethod = new RowsAppendMethod(objectMapper);
 
-            int currentIndex = i;
+            formData.put("rowObjects",new HashMap<>(Collections.singletonMap("data",testDataArray[i])));
+
             AppsmithPluginException appsmithPluginException = assertThrows(AppsmithPluginException.class, () -> {
-                rowsAppendMethod.executePrerequisites(getMethodConfigObject(testDataArray[currentIndex]), getOAuthObject());
+                pluginExecutor.executeParameterized(null,new ExecuteActionDTO(),datasourceConfiguration,actionConfiguration);
             });
 
             String actualMessage = appsmithPluginException.getMessage();
 
-            log.info("abc: {}",appsmithPluginException.getMessage());
-
             assertTrue(actualMessage.contains(ErrorMessages.EMPTY_ROW_OBJECT_MESSAGE));
-
         }
     }
-
 
     /**
      * Simulated oAuth2 object, just to bypass few case.
@@ -53,19 +83,9 @@ public class RowsAppendMethodTest {
         OAuth2 oAuth2 = new OAuth2();
         oAuth2.setAuthenticationResponse(new AuthenticationResponse() );
         oAuth2.getAuthenticationResponse().setToken("welcome123");
+        oAuth2.setGrantType(OAuth2.Type.AUTHORIZATION_CODE);
+        oAuth2.setScopeString("https://www.googleapis.com/auth/spreadsheets.readonly,https://www.googleapis.com/auth/drive.readonly");
+        oAuth2.setScope(Set.of("https://www.googleapis.com/auth/spreadsheets.readonly","https://www.googleapis.com/auth/drive.readonly"));
         return oAuth2;
-    }
-
-    /**
-     * Simulated MethodConfig object with testable data.
-     * @param rowObject
-     * @return
-     */
-    private  MethodConfig getMethodConfigObject(String rowObject) {
-        MethodConfig methodConfig = new MethodConfig(Map.of());
-
-        methodConfig.setRowObjects(rowObject);
-
-        return methodConfig;
     }
 }
