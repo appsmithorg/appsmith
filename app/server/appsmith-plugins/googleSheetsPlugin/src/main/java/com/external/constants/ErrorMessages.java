c718a20a9a996a1a85b09e216a8eed7caf08f708
Vaibhav Tanwar <vaibhav@appsmith.com>
2022-12-13 15:35:02 +0530

    new message added

--- app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/constants/ErrorMessages.java
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/constants/ErrorMessages.java
@@ -9,4 +9,6 @@ public class ErrorMessages {
     public static final String EXPECTED_ROW_OBJECT_MESSAGE = "Expected a row object. Please edit the Row Object field in the GSheet query form to provide a valid row object.";
 
     public static final String EXPECTED_ARRAY_OF_ROW_OBJECT_MESSAGE = "Expected an array of row object. Please edit the Row Object(s) field in the GSheet query form to provide a valid array of row object.";
+
+    public static final String REQUEST_BODY_NOT_ARRAY = "Request body was not an array.";
 }
