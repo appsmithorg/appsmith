dfd3a3b3b44733eb66de9c47e75aeec406e07754
Vaibhav Tanwar <vaibhav@appsmith.com>
2022-12-13 13:40:08 +0530

    errorMessages updated

--- app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/constants/ErrorMessages.java
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/constants/ErrorMessages.java
@@ -2,11 +2,11 @@ package com.external.constants;
 
 public class ErrorMessages {
 
-    public static final String EMPTY_ROW_OBJECT_MESSAGE = "Row object cannot be empty.";
+    public static final String EMPTY_ROW_OBJECT_MESSAGE = "Row object cannot be empty. Please edit the Row Object field in the GSheet query form to provide a row object.";
 
-    public static final String EMPTY_ROW_ARRAY_OBJECT_MESSAGE = "Row array object cannot be empty.";
+    public static final String EMPTY_ROW_ARRAY_OBJECT_MESSAGE = "Array of row object cannot be empty. Please edit the Row Object(s) field in the GSheet query form to provide an array of row object.";
 
-    public static final String EXPECTED_ROW_OBJECT_MESSAGE = "Expected a row object.";
+    public static final String EXPECTED_ROW_OBJECT_MESSAGE = "Expected a row object. Please edit the Row Object field in the GSheet query form to provide a valid row object.";
 
-    public static final String EXPECTED_ARRAY_OF_ROW_OBJECT_MESSAGE = "Expected an array of row objects.";
+    public static final String EXPECTED_ARRAY_OF_ROW_OBJECT_MESSAGE = "Expected an array of row object. Please edit the Row Object(s) field in the GSheet query form to provide a valid array of row object.";
 }
