6e6c39ded82b7a7bd33a5a3ca0c367f2aef7f379
Trisha Anand <trisha@appsmith.com>
2022-12-15 19:19:02 +0530

    fixing ci test case failure

--- app/server/appsmith-server/src/test/java/com/appsmith/server/services/ActionCollectionServiceImplTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/ActionCollectionServiceImplTest.java
@@ -347,7 +347,7 @@ public class ActionCollectionServiceImplTest {
                 .thenReturn(Flux.empty());
 
         Mockito
-                .when(layoutActionService.createSingleAction(Mockito.any(), Boolean.FALSE))
+                .when(layoutActionService.createSingleAction(Mockito.any(), Mockito.any()))
                 .thenAnswer(invocation -> {
                     final ActionDTO argument = (ActionDTO) invocation.getArguments()[0];
                     argument.setId("testActionId");
