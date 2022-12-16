5107d4f1f87f92f911de11bcaea891a16824509b
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2022-12-16 20:30:19 +0530

    fix: set soft focus on property pane inputs

--- app/client/src/reducers/uiReducers/propertyPaneReducer.tsx
+++ app/client/src/reducers/uiReducers/propertyPaneReducer.tsx
@@ -76,6 +76,12 @@ const propertyPaneReducer = createImmerReducer(initialState, {
   ) => {
     state.width = action.payload;
   },
+  [ReduxActionTypes.SET_FOCUSABLE_PROPERTY_FIELD]: (
+    state: PropertyPaneReduxState,
+    action: ReduxAction<{ path: string }>,
+  ) => {
+    state.focusedProperty = action.payload.path;
+  },
   [ReduxActionTypes.SET_SELECTED_PANEL_PROPERTY]: (
     state: PropertyPaneReduxState,
     action: {
