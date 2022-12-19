89e31aa3863a1e8572db4de8aae28cdbb87d00df
Trisha Anand <trisha@appsmith.com>
2022-12-19 21:21:56 +0530

    fix: Server test case failure caused by a bad merge

--- app/server/appsmith-server/src/test/java/com/appsmith/server/solutions/roles/WorkspaceResourcesTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/solutions/roles/WorkspaceResourcesTest.java
@@ -54,6 +54,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.test.context.support.WithUserDetails;
@@ -81,6 +82,7 @@ import static com.appsmith.server.constants.FieldName.VIEWER;
 import static java.lang.Boolean.TRUE;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class WorkspaceResourcesTest {
