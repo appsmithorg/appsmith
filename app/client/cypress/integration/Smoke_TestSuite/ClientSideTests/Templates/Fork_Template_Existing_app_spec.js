0d4f5c154498b504c034a3afb4da3bc3cae38476
Vijetha-Kaja <vijetha@appsmith.com>
2022-12-20 11:32:17 +0530

    Fixed flaky tests

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Templates/Fork_Template_Existing_app_spec.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Templates/Fork_Template_Existing_app_spec.js
@@ -10,8 +10,8 @@ describe("Fork a template to the current app from new page popover", () => {
     cy.get(template.templateDialogBox).should("be.visible");
     cy.wait(4000);
     cy.xpath(
-      "//div[text()='Customer Support Dashboard']/following-sibling::div//button[contains(@class, 'fork-button')]",
-    ).click();
+      "//div[text()='Customer Support Dashboard']/following-sibling::div//button[contains(@class, 'fork-button')]//span[contains(@class, 't--left-icon')]",
+      ).click();
     cy.wait(1000);
     cy.wait("@getTemplatePages").should(
       "have.nested.property",
