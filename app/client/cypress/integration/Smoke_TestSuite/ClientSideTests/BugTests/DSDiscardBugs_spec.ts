609cb947f6fb84a9e17108a8b2cf4c40bb8ab704
“sneha122” <sneha@appsmith.com>
2022-12-22 15:21:46 +0530

    cypress code reivew updates

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/BugTests/DSDiscardBugs_spec.ts
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/BugTests/DSDiscardBugs_spec.ts
@@ -9,15 +9,17 @@ const testString = "test";
 
 describe("datasource unsaved changes popup shows even without changes", function() {
   // In case of postgres and other plugins, host address and port key values are initialized by default making form dirty
-  it("1. Create postgres datasource, save it and edit it and go back, now unsaved changes popup should not be shown", () => {
+  it("1. Bug 18664: Create postgres datasource, save it and edit it and go back, now unsaved changes popup should not be shown", () => {
     dataSources.NavigateToDSCreateNew();
     agHelper.GenerateUUID();
     cy.get("@guid").then((uid) => {
+      // using CreatePlugIn function instead of CreateDatasource,
+      // because I do not need to fill the datasource form and use the same default data
       dataSources.CreatePlugIn("PostgreSQL");
       dsName = "Postgres" + uid;
       agHelper.RenameWithInPane(dsName, false);
       dataSources.SaveDatasource();
-      cy.wait(1000);
+      agHelper.Sleep();
       dataSources.EditDatasource();
       agHelper.GoBack();
       agHelper.AssertElementVisible(dataSources._activeDS);
@@ -26,16 +28,18 @@ describe("datasource unsaved changes popup shows even without changes", function
   });
 
   // In case of Auth DS, headers, query parameters and custom query parameters are being initialized, which makes form dirty
-  it("2. Create REST API datasource, save it and edit it and go back, now unsaved changes popup should not be shown", () => {
+  it("2. Bug 18962: Create REST API datasource, save it and edit it and go back, now unsaved changes popup should not be shown", () => {
     dataSources.NavigateToDSCreateNew();
     agHelper.GenerateUUID();
     cy.get("@guid").then((uid) => {
+      // using CreatePlugIn function instead of CreateDatasource,
+      // because I do not need to fill the datasource form and use the same default data
       dataSources.CreatePlugIn("Authenticated API");
       dsName = "AuthDS" + uid;
       agHelper.RenameWithInPane(dsName, false);
-      dataSources.FillAuthenticatedAPIForm();
+      dataSources.FillAuthAPIUrl();
       dataSources.SaveDatasource();
-      cy.wait(1000);
+      agHelper.Sleep();
 
       // Edit DS for the first time, we shouldnt see discard popup on back button
       // Even if headers, and query parameters are being initialized, we shouldnt see the popup
@@ -52,8 +56,8 @@ describe("datasource unsaved changes popup shows even without changes", function
         testString,
         testString,
       );
-      dataSources.updateDatasource();
-      cy.wait(1000);
+      dataSources.UpdateDatasource();
+      agHelper.Sleep();
 
       // Now edit DS, and ensure that discard popup is not shown on back button click
       // Even if custom authentication params are being initialized, we shouldnt see the popup
@@ -70,12 +74,14 @@ describe("datasource unsaved changes popup shows even without changes", function
     dataSources.NavigateToDSCreateNew();
     agHelper.GenerateUUID();
     cy.get("@guid").then((uid) => {
+      // using CreatePlugIn function instead of CreateDatasource,
+      // because I do not need to fill the datasource form and use the same default data
       dataSources.CreatePlugIn("MongoDB");
       dsName = "Mongo" + uid;
       agHelper.RenameWithInPane(dsName, false);
       dataSources.FillMongoDSForm();
       dataSources.SaveDatasource();
-      cy.wait(1000);
+      agHelper.Sleep();
 
       // Edit datasource, change connection string uri param and click on back button
       dataSources.EditDatasource();
@@ -83,9 +89,7 @@ describe("datasource unsaved changes popup shows even without changes", function
       agHelper.GoBack();
 
       // Assert that popup is visible
-      agHelper.AssertElementVisible(dataSources._datasourceModalDoNotSave);
-      agHelper.AssertElementVisible(dataSources._datasourceModalSave);
-      agHelper.GetNClick(dataSources._datasourceModalSave);
+      dataSources.SaveDSFromDialog();
 
       dataSources.DeleteDatasouceFromActiveTab(dsName);
     });
