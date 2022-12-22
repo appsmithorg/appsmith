c379ceff7bfbc4b62c28577409c7afb58e3d0b5c
Nidhi Nair <nidhi@appsmith.com>
2022-12-22 19:26:31 +0530

    Send Mono complete on cloud services failure as well

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/InstanceConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/InstanceConfig.java
@@ -39,7 +39,10 @@ public class InstanceConfig implements ApplicationListener<ApplicationReadyEvent
         configService.getByName(Appsmith.APPSMITH_REGISTERED)
                 .filter(config -> Boolean.TRUE.equals(config.getConfig().get("value")))
                 .switchIfEmpty(registerInstance())
-                .doOnError(errorSignal -> log.debug("Instance registration failed with error: \n{}", errorSignal.getMessage()))
+                .onErrorResume(errorSignal -> {
+                    log.debug("Instance registration failed with error: \n{}", errorSignal.getMessage());
+                    return Mono.empty();
+                })
                 .then(performRtsHealthCheck())
                 .doFinally(ignored -> this.printReady())
                 .subscribe(null, e -> {
