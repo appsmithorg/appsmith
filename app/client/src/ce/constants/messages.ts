8889b98ee81585b43b73dc1d8429a77cd10cbdbc
sneha122 <sneha@appsmith.com>
2022-12-22 17:48:45 +0530

    fix: discard popup for gsheet updates (#18719)
    ## Description
    
    This PR contains:
    - In case of google sheet datasource, when we make some changes in the
    datasource form and click on back button, The discard popup should have
    "Save and Authorize" button instead of "Save" button as in case of
    google sheets save and authorize happens as one single action.
    - In case of google sheet datasource, I have replaced "Save and
    Re-authorize" to "Save and Authorize" to maintain consistency.
    
    > Add a TL;DR when description is extra long (helps content team)
    
    Fixes #18035 
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)
    
    
    ## How Has This Been Tested?
    
    - Manual
    - Cypress
    
    ### Test Plan
    > Add Testsmith test cases links that relate to this PR
    
    ### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    
    
    ## Checklist:
    ### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    ### QA activity:
    - [ ] Test plan has been approved by relevant developers
    - [ ] Test plan has been peer reviewed by QA
    - [ ] Cypress test cases have been added and approved by either SDET or
    manual QA
    - [ ] Organized project review call with relevant stakeholders after
    Round 1/2 of QA
    - [ ] Added Test Plan Approved label after reveiwing all Cypress test
    
    Co-authored-by: “sneha122” <“sneha@appsmith.com”>

--- app/client/src/ce/constants/messages.ts
+++ app/client/src/ce/constants/messages.ts
@@ -1403,10 +1403,14 @@ export const PAGE_SETTINGS_SET_AS_HOMEPAGE_TOOLTIP_NON_HOME_PAGE = () =>
 export const PAGE_SETTINGS_ACTION_NAME_CONFLICT_ERROR = (name: string) =>
   `${name} is already being used.`;
 
+// Datasources
 export const NEW_QUERY_BUTTON_TEXT = () => "New Query";
 export const NEW_API_BUTTON_TEXT = () => "New API";
 export const GENERATE_NEW_PAGE_BUTTON_TEXT = () => "GENERATE NEW PAGE";
 export const RECONNECT_BUTTON_TEXT = () => "RECONNECT";
+export const SAVE_BUTTON_TEXT = () => "SAVE";
+export const SAVE_AND_AUTHORIZE_BUTTON_TEXT = () => "SAVE AND AUTHORIZE";
+export const DISCARD_POPUP_DONT_SAVE_BUTTON_TEXT = () => "DON'T SAVE";
 
 // Alert options and labels for showMessage types
 export const ALERT_STYLE_OPTIONS = [
