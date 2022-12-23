5cfc563af4df3d995653afd8c8cd6ba7cd295312
Rimil Dey <rimil@appsmith.com>
2022-12-23 15:19:45 +0530

    Improve error msg when no name is given for download action

--- app/client/src/sagas/ActionExecution/DownloadActionSaga.ts
+++ app/client/src/sagas/ActionExecution/DownloadActionSaga.ts
@@ -2,11 +2,8 @@ import { getType, Types } from "utils/TypeHelpers";
 import downloadjs from "downloadjs";
 import AppsmithConsole from "utils/AppsmithConsole";
 import Axios from "axios";
-import {
-  ActionTriggerType,
-  DownloadActionDescription,
-} from "entities/DataTree/actionTriggers";
-import { ActionValidationError } from "sagas/ActionExecution/errorUtils";
+import { DownloadActionDescription } from "entities/DataTree/actionTriggers";
+import { TriggerFailureError } from "sagas/ActionExecution/errorUtils";
 import { isBase64String, isUrlString } from "./downloadActionUtils";
 
 export default async function downloadSaga(
@@ -14,12 +11,7 @@ export default async function downloadSaga(
 ) {
   const { data, name, type } = action;
   if (!name) {
-    throw new ActionValidationError(
-      ActionTriggerType.DOWNLOAD,
-      "name",
-      Types.STRING,
-      getType(name),
-    );
+    throw new TriggerFailureError("Please enter a file name");
   }
   const dataType = getType(data);
   if (dataType === Types.ARRAY || dataType === Types.OBJECT) {
