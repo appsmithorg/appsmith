3ea3e128ba48ec14ba9bc1cde023979e12e11439
Aishwarya UR <aishwarya@appsmith.com>
2022-12-24 18:29:58 +0530

    ForkTemplateToGitConnectedApp.js locator fix

--- app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Templates/ForkTemplateToGitConnectedApp.js
+++ app/client/cypress/integration/Smoke_TestSuite/ClientSideTests/Templates/ForkTemplateToGitConnectedApp.js
@@ -38,7 +38,7 @@ describe("Fork a template to the current app", () => {
     cy.wait(1000);
     cy.get(template.templateDialogBox).should("be.visible");
     cy.xpath(
-      "//div[text()='Customer Support Dashboard']/following-sibling::div//button[contains(@class, 'fork-button')//span[contains(@class, 't--left-icon')]",
+      "//div[text()='Customer Support Dashboard']/following-sibling::div//button[contains(@class, 'fork-button')]//span[contains(@class, 't--left-icon')]",
     )
       .scrollIntoView()
       .click();
