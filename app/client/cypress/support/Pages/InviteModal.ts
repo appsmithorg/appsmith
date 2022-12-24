7b041189c46e86c82d5cd502eb92b66f6408727a
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2022-12-24 09:53:09 +0530

    feat: update cypress tests

---
+++ app/client/cypress/support/Pages/InviteModal.ts
@@ -0,0 +1,53 @@
+import { ObjectsRegistry } from "../Objects/Registry";
+
+// Edit mode modal
+export class InviteModal {
+  private agHelper = ObjectsRegistry.AggregateHelper;
+  private embedSettings = ObjectsRegistry.EmbedSettings;
+  private deployPage = ObjectsRegistry.DeployMode;
+  private commonLocators = ObjectsRegistry.CommonLocators;
+
+  private locators = {
+    _inviteTab: "[data-cy='t--tab-INVITE']",
+    _embedTab: "[data-cy='t--tab-EMBED']",
+    _shareButton: ".t--application-share-btn",
+    _closeButton: ".t--close-form-dialog",
+    _previewEmbed: "[data-cy='preview-embed']",
+  };
+
+  public SelectInviteTab() {
+    this.agHelper.GetNClick(this.locators._inviteTab);
+  }
+
+  public SelectEmbedTab() {
+    this.agHelper.GetNClick(this.locators._embedTab);
+  }
+
+  public OpenShareModal() {
+    this.agHelper.GetNClick(this.locators._shareButton);
+  }
+
+  public CloseModal() {
+    this.agHelper.GetNClick(this.locators._closeButton);
+  }
+
+  public ValidatePreviewEmbed() {
+    this.embedSettings.ToggleShowNavigationBar();
+    cy.get(this.locators._previewEmbed)
+      .invoke("removeAttr", "target")
+      .click();
+    this.agHelper.AssertElementExist(this.commonLocators._backToEditor);
+
+    this.deployPage.NavigateBacktoEditor();
+
+    this.OpenShareModal();
+    this.SelectEmbedTab();
+    this.embedSettings.ToggleShowNavigationBar();
+    cy.get(this.locators._previewEmbed)
+      .invoke("removeAttr", "target")
+      .click();
+    this.agHelper.AssertElementAbsence(this.commonLocators._backToEditor);
+
+    cy.go("back");
+  }
+}
