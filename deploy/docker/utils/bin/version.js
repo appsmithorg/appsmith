1ec121e6cae4440de6a15e3ba11f9d9f457f0139
Sumesh <sumesh@appsmith.com>
2022-12-28 09:54:42 +0530

    chore: use utils get version

--- deploy/docker/utils/bin/version.js
+++ deploy/docker/utils/bin/version.js
@@ -1,9 +1,9 @@
-const bk = require('./backup');
+const utils = require('./utils');
 
 async function exec() {
   let version = null;
   try {
-    version = await bk.getCurrentVersion();
+    version = await utils.getCurrentAppsmithVersion();
   } catch (err) {
     console.error("Error fetching current Appsmith version", err);
     process.exit(1);
