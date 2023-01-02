83d996b34d6eac2b94bb8b1d6a267c80f1a8a489
arunvjn <arun@appsmith.com>
2023-01-02 10:41:48 +0530

    Make store fns appsmith async

--- app/client/src/workers/Evaluation/fns/storeFns.ts
+++ app/client/src/workers/Evaluation/fns/storeFns.ts
@@ -5,61 +5,51 @@ import {
 } from "ce/entities/DataTree/actionTriggers";
 import set from "lodash/set";
 import { MAIN_THREAD_ACTION } from "../evalWorkerActions";
+import { addFn, isAsyncGuard } from "./utils/fnGuard";
 import { TriggerCollector } from "./utils/TriggerCollector";
 
 export function initStoreFns(ctx: typeof globalThis) {
   const triggerCollector = new TriggerCollector(
     MAIN_THREAD_ACTION.PROCESS_STORE_UPDATES,
   );
-  Object.defineProperty(ctx, "storeValue", {
-    value: function storeValue(key: string, value: string, persist = true) {
-      const requestPayload: StoreValueActionDescription = {
-        type: ActionTriggerType.STORE_VALUE,
-        payload: {
-          key,
-          value,
-          persist,
-        },
-      };
-      set(self, ["appsmith", "store", key], value);
-      triggerCollector.collect(requestPayload);
-      return Promise.resolve({});
-    },
-    configurable: false,
-    writable: false,
-    enumerable: false,
-  });
+  function storeValue(key: string, value: string, persist = true) {
+    const requestPayload: StoreValueActionDescription = {
+      type: ActionTriggerType.STORE_VALUE,
+      payload: {
+        key,
+        value,
+        persist,
+      },
+    };
+    set(self, ["appsmith", "store", key], value);
+    triggerCollector.collect(requestPayload);
+    return Promise.resolve({});
+  }
 
-  Object.defineProperty(ctx, "removeValue", {
-    value: function removeValue(key: string) {
-      const requestPayload: RemoveValueActionDescription = {
-        type: ActionTriggerType.REMOVE_VALUE,
-        payload: {
-          key,
-        },
-      };
-      //@ts-expect-error no types for store
-      delete self.appsmith.store[key];
-      triggerCollector.collect(requestPayload);
-      return Promise.resolve({});
-    },
-    configurable: false,
-    writable: false,
-    enumerable: false,
-  });
+  function removeValue(key: string) {
+    const requestPayload: RemoveValueActionDescription = {
+      type: ActionTriggerType.REMOVE_VALUE,
+      payload: {
+        key,
+      },
+    };
+    //@ts-expect-error no types for store
+    delete self.appsmith.store[key];
+    triggerCollector.collect(requestPayload);
+    return Promise.resolve({});
+  }
 
-  Object.defineProperty(ctx, "clearStore", {
-    value: function clearStore() {
-      //@ts-expect-error no types for store
-      self.appsmith.store = {};
-      triggerCollector.collect({
-        type: ActionTriggerType.CLEAR_STORE,
-        payload: null,
-      });
-      return Promise.resolve({});
-    },
-    configurable: false,
-    writable: false,
-    enumerable: false,
-  });
+  function clearStore() {
+    //@ts-expect-error no types for store
+    self.appsmith.store = {};
+    triggerCollector.collect({
+      type: ActionTriggerType.CLEAR_STORE,
+      payload: null,
+    });
+    return Promise.resolve({});
+  }
+
+  addFn(ctx, storeValue, [isAsyncGuard]);
+  addFn(ctx, removeValue, [isAsyncGuard]);
+  addFn(ctx, clearStore, [isAsyncGuard]);
 }
---
+++ app/client/src/workers/Evaluation/fns/utils/fnGuard.ts
@@ -0,0 +1,26 @@
+import { ActionCalledInSyncFieldError } from "workers/Evaluation/errorModifier";
+
+export function addFn(
+  ctx: typeof globalThis,
+  fn: (...args: any[]) => any,
+  fnGuards: Array<(func: typeof fn) => void>,
+) {
+  Object.defineProperty(ctx, fn.name, {
+    value: function(...args: any[]) {
+      for (const guard of fnGuards) {
+        guard(fn);
+      }
+      return fn(...args);
+    },
+    configurable: false,
+    writable: false,
+    enumerable: false,
+  });
+}
+
+export function isAsyncGuard(fn: (...args: any[]) => any) {
+  if (!self.ALLOW_ASYNC) {
+    self.IS_ASYNC = true;
+    throw new ActionCalledInSyncFieldError(fn.name);
+  }
+}
