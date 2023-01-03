4bf22cd28bce14f945ee444adbf375f149051e9c
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2023-01-03 18:31:09 +0530

    fix: cta updates

---
+++ app/client/src/pages/Editor/BottomBar/styles.ts
@@ -0,0 +1,10 @@
+import { Colors } from "constants/Colors";
+import { css } from "styled-components";
+
+export const BottomBarCTAStyles = css`
+  height: 34px;
+  width: 32px;
+  :hover {
+    background-color: ${Colors.GRAY_100};
+  }
+`;
