33a0998ffc14399e4b81c522b3c58b0df3cc0d4e
Arsalan <arsalanyaldram0211@outlook.com>
2023-01-05 16:18:22 +0530

    fix: add context menu icon height.

--- app/client/src/pages/Editor/Explorer/ContextMenuTrigger.tsx
+++ app/client/src/pages/Editor/Explorer/ContextMenuTrigger.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ControlIcons } from "icons/ControlIcons";
 import { EntityTogglesWrapper } from "./ExplorerStyledComponents";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import { Colors } from "constants/Colors";
 import { TooltipComponent } from "design-system";
 import {
@@ -9,12 +9,11 @@ import {
   ENTITY_MORE_ACTIONS_TOOLTIP,
 } from "@appsmith/constants/messages";
 import { TOOLTIP_HOVER_ON_DELAY } from "constants/AppConstants";
+import { Theme } from "constants/DefaultTheme";
 
 const ToggleIcon = styled(ControlIcons.MORE_VERTICAL_CONTROL)`
   &&& {
     flex-grow: 0;
-    width: ${(props) => props.theme.fontSizes[3]}px;
-    height: ${(props) => props.theme.fontSizes[3]}px;
     g {
       path {
         fill: ${Colors.GRAY};
@@ -23,6 +22,8 @@ const ToggleIcon = styled(ControlIcons.MORE_VERTICAL_CONTROL)`
   }
 `;
 export function ContextMenuTrigger(props: { className?: string }) {
+  const theme = useTheme() as Theme;
+
   return (
     <EntityTogglesWrapper
       className={props.className + " entity-context-menu-icon"}
@@ -33,7 +34,7 @@ export function ContextMenuTrigger(props: { className?: string }) {
         hoverOpenDelay={TOOLTIP_HOVER_ON_DELAY}
         position="right"
       >
-        <ToggleIcon />
+        <ToggleIcon height={theme.fontSizes[3]} width={theme.fontSizes[3]} />
       </TooltipComponent>
     </EntityTogglesWrapper>
   );
