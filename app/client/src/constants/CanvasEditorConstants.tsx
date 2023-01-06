e1a72bda5837378ce743b09b81cb186a29816846
Hetu Nandu <hetu@appsmith.com>
2023-01-06 16:56:08 +0530

    Update CanvasEditorConstants.tsx

--- app/client/src/constants/CanvasEditorConstants.tsx
+++ app/client/src/constants/CanvasEditorConstants.tsx
@@ -23,29 +23,3 @@ export const zIndexLayers = {
   ENTITY_EXPLORER: "z-[3]",
   RESIZER: "z-[4]",
 };
-
-export type PaneConfig = {
-  tabsPanelWidth: number;
-  propertyPaneWidth: number;
-  canvasPaneWidth: number;
-};
-
-export enum ScreenSize {
-  SMALL = "SMALL", // smaller than 13 inch
-  // MEDIUM = "MEDIUM", // 13 - 15 inch
-  // LARGE = "LARGE", // 15 to 17 inch
-  // EXTRA_LARGE = "EXTRA_LARGE", // more than 17 inch
-}
-
-export type ScreenSizePaneConfigType = Record<ScreenSize, PaneConfig>;
-
-export const ScreenSizePaneConfig: ScreenSizePaneConfigType = {
-  // EXTRA_LARGE: { max: undefined, min: undefined },
-  // LARGE: { max: undefined, min: undefined },
-  // MEDIUM: { max: undefined, min: undefined },
-  [ScreenSize.SMALL]: {
-    tabsPanelWidth: 4,
-    propertyPaneWidth: 0,
-    canvasPaneWidth: 6,
-  },
-};
