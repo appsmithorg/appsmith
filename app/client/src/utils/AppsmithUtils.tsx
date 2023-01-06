071b9927107e451dcac4afb821e6c7447239aaa2
Anand Srinivasan <66776129+eco-monk@users.noreply.github.com>
2023-01-06 19:39:38 +0530

    chore: send segment anonymous id (#19122)
    Add segment's `anonymousId` as a header in all API calls.
    
    cached id ->
    [details](https://segment.com/docs/connections/sources/catalog/libraries/website/javascript/identity/#segment-id-persistence)
    
    On Page load actions:
    - If segment is enabled:
      - and cached id exists -> trigger with cached id
      - if cached id doesn’t exist, we wait for max 2 seconds.
        - if segment init is success -> trigger with anonymous id
        - if failed/delayed -> trigger without anonymous id
    - If segment is disabled we don’t wait at all and anonymous id is not
    sent.
    
    Signed-off-by: Shrikant Sharat Kandula <shrikant@appsmith.com>
    Co-authored-by: Shrikant Sharat Kandula <shrikant@appsmith.com>
    Co-authored-by: Hetu Nandu <hetu@appsmith.com>

--- app/client/src/utils/AppsmithUtils.tsx
+++ app/client/src/utils/AppsmithUtils.tsx
@@ -72,10 +72,10 @@ export const initializeAnalyticsAndTrackers = () => {
     if (appsmithConfigs.segment.enabled && !(window as any).analytics) {
       if (appsmithConfigs.segment.apiKey) {
         // This value is only enabled for Appsmith's cloud hosted version. It is not set in self-hosted environments
-        AnalyticsUtil.initializeSegment(appsmithConfigs.segment.apiKey);
+        return AnalyticsUtil.initializeSegment(appsmithConfigs.segment.apiKey);
       } else if (appsmithConfigs.segment.ceKey) {
         // This value is set in self-hosted environments. But if the analytics are disabled, it's never used.
-        AnalyticsUtil.initializeSegment(appsmithConfigs.segment.ceKey);
+        return AnalyticsUtil.initializeSegment(appsmithConfigs.segment.ceKey);
       }
     }
   } catch (e) {
