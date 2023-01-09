b5f785c6989719c63b81e8e0899107e4a76e1329
Sumit Kumar <sumit@appsmith.com>
2023-01-09 15:58:38 +0530

    update merge conflict resolution

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce/ImportExportApplicationServiceCEImplV2.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce/ImportExportApplicationServiceCEImplV2.java
@@ -1230,7 +1230,6 @@ public class ImportExportApplicationServiceCEImplV2 implements ImportExportAppli
                                 return application;
                             });
                 })
-                .then(installedJSLibMono)
                 .onErrorResume(throwable -> {
                     log.error("Error while importing the application ", throwable.getMessage());
                     if (importedApplication.getId() != null && applicationId == null) {
