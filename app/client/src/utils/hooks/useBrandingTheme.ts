4b0c18dd3532089ddcb6154ffe8f3410c85a7b56
Pawan Kumar <pawan.stardust@gmail.com>
2023-01-09 13:47:37 +0530

    add brand light color

--- app/client/src/utils/hooks/useBrandingTheme.ts
+++ app/client/src/utils/hooks/useBrandingTheme.ts
@@ -1,38 +1,52 @@
+import tinycolor from "tinycolor2";
 import { useSelector } from "react-redux";
+
 import { getTenantConfig } from "@appsmith/selectors/tenantSelectors";
+import { useEffect } from "react";
 
 const useBrandingTheme = () => {
   const config = useSelector(getTenantConfig);
 
-  const cssVariables = {
-    brand: "primary",
-    "background-secondary": "background",
-    "brand-hover": "hover",
-    "brand-text": "font",
-    "brand-disabled": "disabled",
-  };
+  useEffect(() => {
+    const hsl = tinycolor(config.brandColors.primary).toHsl();
+    const hue = hsl.h;
+    const saturation = hsl.s;
 
-  // Create a CSS variable for each color
-  for (let i = 0; i < Object.keys(cssVariables).length; i++) {
-    const key = Object.keys(cssVariables)[i];
-    const value = Object.values(cssVariables)[i];
+    const cssVariables: Record<string, string> = {
+      brand: config.brandColors.primary,
+      "background-secondary": config.brandColors.background,
+      "brand-hover": config.brandColors.hover,
+      "brand-text": config.brandColors.font,
+      "brand-disabled": config.brandColors.disabled,
+      "brand-light": `#${tinycolor(`hsl ${hue} ${saturation} ${98}}`).toHex()}`,
+    };
 
-    document.documentElement.style.setProperty(
-      `--ads-color-${key}`,
-      config.brandColors[value],
-    );
-  }
+    // Create a CSS variable for each color
+    for (let i = 0; i < Object.keys(cssVariables).length; i++) {
+      const key = Object.keys(cssVariables)[i];
+      const value = Object.values(cssVariables)[i];
 
-  // Set the favicon
-  let favicon: any = document.querySelector("link[rel='shortcut icon']");
-  if (!favicon) {
-    favicon = document.createElement("link");
-    favicon.rel = "shortcut icon";
-    favicon.className = "t--branding-favicon";
-    document.getElementsByTagName("head")[0].appendChild(favicon);
-  }
+      document.documentElement.style.setProperty(`--ads-color-${key}`, value);
+    }
 
-  favicon.href = config.brandFaviconUrl;
+    // Set the favicon
+    let favicon: any = document.querySelector("link[rel='shortcut icon']");
+    if (!favicon) {
+      favicon = document.createElement("link");
+      favicon.rel = "shortcut icon";
+      favicon.className = "t--branding-favicon";
+      document.getElementsByTagName("head")[0].appendChild(favicon);
+    }
+
+    favicon.href = config.brandFaviconUrl;
+  }, [
+    config.brandColors.primary,
+    config.brandColors.background,
+    config.brandColors.hover,
+    config.brandColors.font,
+    config.brandColors.disabled,
+    config.brandFaviconUrl,
+  ]);
 };
 
 export default useBrandingTheme;
