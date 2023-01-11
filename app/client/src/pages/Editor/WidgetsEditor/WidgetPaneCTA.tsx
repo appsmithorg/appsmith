038e78dbe0e4936836d2297ecd8a667aeb94367a
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2023-01-11 16:20:52 +0530

    wip

--- app/client/src/pages/Editor/WidgetsEditor/WidgetPaneCTA.tsx
+++ app/client/src/pages/Editor/WidgetsEditor/WidgetPaneCTA.tsx
@@ -1,5 +1,5 @@
 import { Popover } from "@blueprintjs/core";
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import WidgetSidebar from "pages/Editor/WidgetSidebar";
 import { Button } from "design-system";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,15 +7,43 @@ import { selectForceOpenWidgetPanel } from "../Explorer";
 import { forceOpenWidgetPanel } from "actions/widgetSidebarActions";
 import { getDragDetails } from "sagas/selectors";
 import { AppState } from "@appsmith/reducers";
+import { useMouseLocation } from "../GlobalHotKeys/useMouseLocation";
+import { getExplorerWidth } from "selectors/explorerSelector";
+import { ThemeContext } from "styled-components";
+
+const WIDGET_PANE_WIDTH = 246;
+const WIDGET_PANE_HEIGHT = 600;
 
 function WidgetPaneTrigger() {
   const dispatch = useDispatch();
+  const theme = useContext(ThemeContext);
   const openWidgetPanel = useSelector(selectForceOpenWidgetPanel);
   const dragDetails = useSelector(getDragDetails);
+  const explorerWidth = useSelector(getExplorerWidth);
+  const getMousePosition = useMouseLocation();
   const isDragging = useSelector(
     (state: AppState) => state.ui.widgetDragResize.isDragging,
   );
   const toOpen = useRef(false);
+
+  const isOverlappingWithPane = () => {
+    const { x, y } = getMousePosition();
+
+    const hbufferOffset = 100 * 2;
+    const vbufferOffset = 0 * 1;
+    const hOffset = explorerWidth + WIDGET_PANE_WIDTH + hbufferOffset;
+    const vOffset =
+      parseInt(theme.smallHeaderHeight.slice(0, -2)) +
+      40 +
+      WIDGET_PANE_HEIGHT +
+      vbufferOffset;
+    console.log({ x, y, hOffset, vOffset }, "offsets");
+    if (x < hOffset && y < vOffset) {
+      return true;
+    }
+
+    return false;
+  };
 
   useEffect(() => {
     if (isDragging && dragDetails.newWidget) {
@@ -26,7 +54,9 @@ function WidgetPaneTrigger() {
 
   useEffect(() => {
     if (!isDragging && toOpen.current) {
-      dispatch(forceOpenWidgetPanel(true));
+      if (!isOverlappingWithPane()) {
+        dispatch(forceOpenWidgetPanel(true));
+      }
       toOpen.current = false;
     }
   }, [isDragging]);
