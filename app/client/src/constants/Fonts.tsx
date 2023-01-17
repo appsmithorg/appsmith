cb4dd9c1962f54e4d9cc2fad23cea6ea45abf263
arunvjn <arun@appsmith.com>
2023-01-17 14:23:00 +0530

    Reorder fonts for windows 10

--- app/client/src/constants/Fonts.tsx
+++ app/client/src/constants/Fonts.tsx
@@ -1,4 +1,4 @@
 export const TextFonts =
   "-apple-system, BlinkMacSystemFont, “Segoe UI”, “Roboto”, “Oxygen”, “Ubuntu”, “Cantarell”, “Fira Sans”, “Droid Sans”, “Helvetica Neue”, sans-serif";
 export const CodeFonts =
-  'ui-monospace, "Menlo", "Monaco", "Cascadia Code", "Cascadia Mono", "Segoe UI Mono", "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro", "Fira Mono", "Droid Sans Mono", "Courier New", monospace, Consolas';
+  'ui-monospace, "Menlo", "Monaco", "Cascadia Code", "Cascadia Mono", "Segoe UI Mono", "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro", "Fira Mono", "Droid Sans Mono", Consolas, "Courier New", monospace';
