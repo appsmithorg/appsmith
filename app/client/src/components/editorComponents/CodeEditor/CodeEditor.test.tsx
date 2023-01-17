a8f0e631fdcb94828e64357ac5771f86d6d6e5fc
arunvjn <arun@appsmith.com>
2023-01-17 19:01:40 +0530

    Revert "Added test for line highlight"
    This reverts commit 1356f5936ed11a095374aabdfbff3976f6f96a72.

--- app/client/src/components/editorComponents/CodeEditor/CodeEditor.test.tsx
+++ app/client/src/components/editorComponents/CodeEditor/CodeEditor.test.tsx
@@ -3,6 +3,7 @@ import store from "store";
 import TestRenderer from "react-test-renderer";
 import React from "react";
 import { Provider } from "react-redux";
+
 import EvaluatedValuePopup from "./EvaluatedValuePopup";
 import { ThemeProvider } from "styled-components";
 import { theme, light } from "constants/DefaultTheme";
@@ -12,7 +13,6 @@ import {
   TabBehaviour,
   EditorModes,
 } from "./EditorConfig";
-import { render } from "@testing-library/react";
 
 describe("CodeEditor", () => {
   it("should check EvaluatedValuePopup's hideEvaluatedValue  is false when hideEvaluatedValue is passed as false to codeditor", () => {
@@ -69,37 +69,5 @@ describe("CodeEditor", () => {
     expect(
       testInstance.findByType(EvaluatedValuePopup).props.hideEvaluatedValue,
     ).toBe(true);
-  });
-  it("Should highlight the current line", async () => {
-    const finalTheme = { ...theme, colors: { ...theme.colors, ...light } };
-    const component = render(
-      <Provider store={store}>
-        <ThemeProvider theme={finalTheme}>
-          <CodeEditor
-            additionalDynamicData={{}}
-            hideEvaluatedValue
-            input={{
-              value: `export default {
-                myFun1: () => {}
-              }`,
-              onChange: () => {
-                //
-              },
-            }}
-            mode={EditorModes.TEXT}
-            size={EditorSize.COMPACT}
-            tabBehaviour={TabBehaviour.INDENT}
-            theme={EditorTheme.LIGHT}
-          />
-        </ThemeProvider>
-      </Provider>,
-    );
-    const line1 = component.container.querySelector(".CodeMirror-line");
-    expect(line1).toBeDefined();
-    (line1 as HTMLSpanElement)?.click();
-    const highlight = component.container.querySelector(
-      ".CodeMirror-activeline-background",
-    );
-    expect(highlight).toBeDefined();
   });
 });
