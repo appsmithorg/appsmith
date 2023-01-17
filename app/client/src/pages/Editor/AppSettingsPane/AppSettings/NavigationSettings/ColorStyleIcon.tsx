6f82a669ac23b7ac11e015086b26f9d9dad19fe0
Dhruvik Neharia <dhruvik@appsmith.com>
2023-01-18 01:27:42 +0530

    refactor: use styled components in ColorStyleIcon

--- app/client/src/pages/Editor/AppSettingsPane/AppSettings/NavigationSettings/ColorStyleIcon.tsx
+++ app/client/src/pages/Editor/AppSettingsPane/AppSettings/NavigationSettings/ColorStyleIcon.tsx
@@ -3,12 +3,23 @@ import { NavigationSetting, NAVIGATION_SETTINGS } from "constants/AppConstants";
 import { Colors } from "constants/Colors";
 import { useSelector } from "react-redux";
 import { getSelectedAppTheme } from "selectors/appThemingSelectors";
+import styled from "styled-components";
+
+const StyledIcon = styled.div<{
+  backgroundColor: string;
+}>`
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  height: 14px;
+  width: 14px;
+  border-radius: 100%;
+  margin-right: 4px;
+  border: 1px solid ${Colors.GREY_200};
+`;
 
 const ColorStyleIcon = (props: {
   colorStyle: NavigationSetting["colorStyle"];
 }) => {
   const selectedTheme = useSelector(getSelectedAppTheme);
-
   let backgroundColor = Colors.WHITE;
 
   if (props.colorStyle === NAVIGATION_SETTINGS.COLOR_STYLE.SOLID) {
@@ -20,18 +31,7 @@ const ColorStyleIcon = (props: {
     backgroundColor = Colors.WHITE;
   }
 
-  return (
-    <div
-      style={{
-        backgroundColor: backgroundColor,
-        height: 14,
-        width: 14,
-        borderRadius: "100%",
-        marginRight: 4,
-        border: `1px solid ${Colors.GREY_200}`,
-      }}
-    />
-  );
+  return <StyledIcon backgroundColor={backgroundColor} />;
 };
 
 export default ColorStyleIcon;
