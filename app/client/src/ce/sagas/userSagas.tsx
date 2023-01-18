977b09be3a5760b742f6fd12bf6ff1803a76a8a6
balajisoundar <balaji@appsmith.com>
2023-01-18 18:39:36 +0530

    chore: send anonymous id from segment for anonymous users in usage pulse call

--- app/client/src/ce/sagas/userSagas.tsx
+++ app/client/src/ce/sagas/userSagas.tsx
@@ -1,4 +1,4 @@
-import { call, put, race, select, take } from "redux-saga/effects";
+import { call, fork, put, race, select, take } from "redux-saga/effects";
 import {
   ReduxAction,
   ReduxActionWithPromise,
@@ -55,7 +55,10 @@ import {
   getFirstTimeUserOnboardingApplicationId,
   getFirstTimeUserOnboardingIntroModalVisibility,
 } from "utils/storage";
-import { initializeAnalyticsAndTrackers } from "utils/AppsmithUtils";
+import {
+  initializeAnalyticsAndTrackers,
+  initializeSegmentWithoutTracking,
+} from "utils/AppsmithUtils";
 import { getAppsmithConfigs } from "ce/configs";
 import { getSegmentState } from "selectors/analyticsSelectors";
 import {
@@ -124,6 +127,32 @@ export function* waitForSegmentInit(skipWithAnonymousId: boolean) {
   }
 }
 
+/*
+ * Function to initiate usage tracking
+ *  - For anonymous users we need segement id, so if telemetry is off
+ *    we're intiating segment without tracking and once we get the id,
+ *    analytics object is purged.
+ */
+export function* initiateUsageTracking(payload: {
+  isAnonymousUser: boolean;
+  enableTelemetry: boolean;
+}) {
+  //To make sure that we're not tracking from previous session.
+  UsagePulse.stopTrackingActivity();
+
+  if (payload.isAnonymousUser) {
+    if (payload.enableTelemetry) {
+      UsagePulse.userAnonymousId = AnalyticsUtil.getAnonymousId();
+    } else {
+      yield initializeSegmentWithoutTracking();
+      UsagePulse.userAnonymousId = AnalyticsUtil.getAnonymousId();
+      AnalyticsUtil.removeAnalytics();
+    }
+  }
+
+  UsagePulse.startTrackingActivity();
+}
+
 export function* getCurrentUserSaga() {
   try {
     PerformanceTracker.startAsyncTracking(
@@ -132,6 +161,7 @@ export function* getCurrentUserSaga() {
     const response: ApiResponse = yield call(UserApi.getCurrentUser);
 
     const isValidResponse: boolean = yield validateResponse(response);
+
     if (isValidResponse) {
       //@ts-expect-error: response is of type unknown
       const { enableTelemetry } = response.data;
@@ -148,22 +178,7 @@ export function* getCurrentUserSaga() {
             yield put(segmentInitUncertain());
           }
         }
-      } else if (
-        //@ts-expect-error: response is of type unknown
-        response.data.isAnonymous &&
-        //@ts-expect-error: response is of type unknown
-        response.data.username === ANONYMOUS_USERNAME
-      ) {
-        /*
-         * We're initializing the segment api regardless of the enableTelemetry flag
-         * So we can use segement Id to fingerprint anonymous user in usage pulse call
-         */
-        //NOTE: commenting for now to fix a flaky cypress issue
-        // yield initializeSegmentWithoutTracking();
       }
-
-      //To make sure that we're not tracking from previous session.
-      UsagePulse.stopTrackingActivity();
 
       if (
         //@ts-expect-error: response is of type unknown
@@ -173,16 +188,16 @@ export function* getCurrentUserSaga() {
       ) {
         //@ts-expect-error: response is of type unknown
         enableTelemetry && AnalyticsUtil.identifyUser(response.data);
-      } else {
-        UsagePulse.userAnonymousId = "anonymousId";
-
-        if (!enableTelemetry) {
-          AnalyticsUtil.removeAnalytics();
-        }
       }
 
-      UsagePulse.startTrackingActivity();
-
+      /*
+       * Forking it as we don't want to block application flow
+       */
+      yield fork(initiateUsageTracking, {
+        //@ts-expect-error: response is of type unknown
+        isAnonymousUser: response.data.isAnonymous,
+        enableTelemetry,
+      });
       yield put(initAppLevelSocketConnection());
       yield put(initPageLevelSocketConnection());
       yield put({
