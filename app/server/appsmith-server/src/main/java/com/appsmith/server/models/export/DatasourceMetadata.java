2bcd9441c2872d6a751760bddfff95775a18ebbc
Sidhant Goel <sidhant@appsmith.com>
2023-01-21 10:03:06 +0530

    added models for export

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/models/export/DatasourceMetadata.java
@@ -0,0 +1,10 @@
+package com.appsmith.server.modals;
+
+import lombok.Data;
+
+@Data
+public class DatasourceMetadata {
+    private String name;
+    private String pluginId;
+    private String gitSyncId;
+}
