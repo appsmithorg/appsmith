93ed45e16e634453700d4741868a162e1ee9ece4
Saroj <saroj@appsmith.com>
2023-01-24 18:21:18 +0530

    commented the skipped test

--- app/client/cypress/UpgradeAppsmith/UpgradeAppsimth_spec.js
+++ app/client/cypress/UpgradeAppsmith/UpgradeAppsimth_spec.js
@@ -69,54 +69,54 @@ describe("Upgrade appsmith version", () => {
     cy.wait(2000);
   });
 
-  it.skip("Upgrade Appsmith from CE to EE and verify the Applications", () => {
-    cy.log("Stop the appsmith container");
-    cy.StopTheContainer(testUrl, "appsmith"); // stop the old container
-    cy.wait(2000);
+  // it.skip("Upgrade Appsmith from CE to EE and verify the Applications", () => {
+  //   cy.log("Stop the appsmith container");
+  //   cy.StopTheContainer(testUrl, "appsmith"); // stop the old container
+  //   cy.wait(2000);
 
-    const uuid = () => Cypress._.random(0, 10000);
-    const name = uuid();
-    cy.wait(2000);
+  //   const uuid = () => Cypress._.random(0, 10000);
+  //   const name = uuid();
+  //   cy.wait(2000);
 
-    cy.log("Get path");
-    cy.GetPath(testUrl, "appsmith").then((path) => {
-      path = path.split("  ");
-      path = path[1].split(" ");
-      path = path[0].slice(0, -7);
-      cy.log(path);
+  //   cy.log("Get path");
+  //   cy.GetPath(testUrl, "appsmith").then((path) => {
+  //     path = path.split("  ");
+  //     path = path[1].split(" ");
+  //     path = path[0].slice(0, -7);
+  //     cy.log(path);
 
-      localStorage.setItem(
-        "ContainerName",
-        `appsmith-160-ce-${name}-enterprise`,
-      );
+  //     localStorage.setItem(
+  //       "ContainerName",
+  //       `appsmith-160-ce-${name}-enterprise`,
+  //     );
 
-      cy.log("Start old stack container");
-      cy.CreateEEContainer(
-        testUrl,
-        path + "/oldstack",
-        "fatcontainer",
-        `appsmith-160-ce-${name}-enterprise`,
-      );
-      cy.wait(45000);
+  //     cy.log("Start old stack container");
+  //     cy.CreateEEContainer(
+  //       testUrl,
+  //       path + "/oldstack",
+  //       "fatcontainer",
+  //       `appsmith-160-ce-${name}-enterprise`,
+  //     );
+  //     cy.wait(45000);
 
-      cy.log("Verify Logs");
-      cy.GetAndVerifyLogs(testUrl, `appsmith-160-ce-${name}-enterprise`); // Get and verify the logs
-    });
+  //     cy.log("Verify Logs");
+  //     cy.GetAndVerifyLogs(testUrl, `appsmith-160-ce-${name}-enterprise`); // Get and verify the logs
+  //   });
 
-    //verify the Applications after upgrade
-    cy.LoginFromAPI(testdata.UPGRADEUSERNAME, testdata.UPGRADEPASSWORD);
-    cy.visit(testdata.APPURL);
+  //   //verify the Applications after upgrade
+  //   cy.LoginFromAPI(testdata.UPGRADEUSERNAME, testdata.UPGRADEPASSWORD);
+  //   cy.visit(testdata.APPURL);
 
-    cy.get(".t--buttongroup-widget").should("exist");
-    cy.get(".t--buttongroup-widget")
-      .children()
-      .should("have.length", 3);
+  //   cy.get(".t--buttongroup-widget").should("exist");
+  //   cy.get(".t--buttongroup-widget")
+  //     .children()
+  //     .should("have.length", 3);
 
-    cy.get(publishPage.backToEditor).click({ force: true });
+  //   cy.get(publishPage.backToEditor).click({ force: true });
 
-    cy.get(".t--buttongroup-widget").should("exist");
-    cy.get(".t--buttongroup-widget")
-      .children()
-      .should("have.length", 3);
-  });
+  //   cy.get(".t--buttongroup-widget").should("exist");
+  //   cy.get(".t--buttongroup-widget")
+  //     .children()
+  //     .should("have.length", 3);
+  // });
 });
