9b4bf326326b8d32b3a2d2fd355b314a016a5557
Saroj <saroj@appsmith.com>
2023-01-24 19:02:12 +0530

    update import path

--- app/client/cypress/integration/UpgradeAppsmith/UpgradeAppsimth_spec.js
+++ app/client/cypress/integration/UpgradeAppsmith/UpgradeAppsimth_spec.js
@@ -1,5 +1,5 @@
-const publishPage = require("../locators/publishWidgetspage.json");
-const testdata = require("../fixtures/testdata.json");
+const publishPage = require("../../locators/publishWidgetspage.json");
+const testdata = require("../../fixtures/testdata.json");
 
 const testUrl = "http://localhost:5001/v1/parent/cmd";
 describe("Upgrade appsmith version", () => {
