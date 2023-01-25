fa8fb92ecf00497925d4d2f5d459b0acbf4370b9
Sangeeth Sivan <sangeeth@appsmith.com>
2023-01-25 16:49:58 +0530

    chore: revert removing intercom and put redirection to pricing page behind feature flag

--- app/client/src/utils/hooks/useOnUpgrade.ts
+++ app/client/src/utils/hooks/useOnUpgrade.ts
@@ -3,6 +3,8 @@ import {
   createMessage,
   UPGRADE_TO_EE_GENERIC,
 } from "@appsmith/constants/messages";
+import { useSelector } from "react-redux";
+import { selectFeatureFlags } from "selectors/usersSelectors";
 import AnalyticsUtil, { EventName } from "utils/AnalyticsUtil";
 
 const { intercomAppID } = getAppsmithConfigs();
@@ -15,6 +17,7 @@ type Props = {
 
 const useOnUpgrade = (props: Props) => {
   const { intercomMessage, logEventData, logEventName } = props;
+  const features = useSelector(selectFeatureFlags);
 
   const triggerIntercom = (message: string) => {
     if (intercomAppID && window.Intercom) {
@@ -27,7 +30,14 @@ const useOnUpgrade = (props: Props) => {
       logEventName || "ADMIN_SETTINGS_UPGRADE",
       logEventData,
     );
-    triggerIntercom(intercomMessage || createMessage(UPGRADE_TO_EE_GENERIC));
+    if (features.USAGE_AND_BILLING) {
+      window.open(
+        "https://www.appsmith.com/pricing?source=CE&instance=",
+        "_blank",
+      );
+    } else {
+      triggerIntercom(intercomMessage || createMessage(UPGRADE_TO_EE_GENERIC));
+    }
   };
 
   return { onUpgrade };
