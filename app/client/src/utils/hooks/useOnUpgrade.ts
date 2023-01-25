be00986fa5b2e716f0c7201e78f3117d6af56d66
Sangeeth Sivan <sangeeth@appsmith.com>
2023-01-25 17:36:18 +0530

    chore: get instance id from tenant api

--- app/client/src/utils/hooks/useOnUpgrade.ts
+++ app/client/src/utils/hooks/useOnUpgrade.ts
@@ -3,6 +3,7 @@ import {
   createMessage,
   UPGRADE_TO_EE_GENERIC,
 } from "@appsmith/constants/messages";
+import { getTenantConfig } from "@appsmith/selectors/tenantSelectors";
 import { useSelector } from "react-redux";
 import { selectFeatureFlags } from "selectors/usersSelectors";
 import AnalyticsUtil, { EventName } from "utils/AnalyticsUtil";
@@ -18,6 +19,7 @@ type Props = {
 const useOnUpgrade = (props: Props) => {
   const { intercomMessage, logEventData, logEventName } = props;
   const features = useSelector(selectFeatureFlags);
+  const tenantConfig = useSelector(getTenantConfig);
 
   const triggerIntercom = (message: string) => {
     if (intercomAppID && window.Intercom) {
@@ -32,7 +34,7 @@ const useOnUpgrade = (props: Props) => {
     );
     if (features.USAGE_AND_BILLING) {
       window.open(
-        "https://www.appsmith.com/pricing?source=CE&instance=",
+        `https://www.appsmith.com/pricing?source=CE&instance=${tenantConfig?.instanceId}`,
         "_blank",
       );
     } else {
