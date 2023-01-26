44efe49640f377d123d2f91d32660128175ec2ee
“sneha122” <sneha@appsmith.com>
2023-01-26 19:23:18 +0530

    fix: datasource popup blinking issue fixed

--- app/client/src/pages/Editor/DataSourceEditor/index.tsx
+++ app/client/src/pages/Editor/DataSourceEditor/index.tsx
@@ -390,17 +390,19 @@ class DatasourceEditorRouter extends React.Component<Props, State> {
   blockRoutes() {
     this.setState({
       unblock: this.props?.history?.block((tx: any) => {
-        this.setState(
-          {
-            // need to pass in query params as well as state, when user navigates away from ds form page
-            navigation: () =>
-              this.props.history.push(tx.pathname + tx.search, tx.state),
-            showDialog: true,
-            routesBlocked: true,
-          },
-          this.routesBlockFormChangeCallback.bind(this),
-        );
-        return false;
+        if (this.props.isFormDirty) {
+          this.setState(
+            {
+              // need to pass in query params as well as state, when user navigates away from ds form page
+              navigation: () =>
+                this.props.history.push(tx.pathname + tx.search, tx.state),
+              showDialog: true,
+              routesBlocked: true,
+            },
+            this.routesBlockFormChangeCallback.bind(this),
+          );
+          return false;
+        }
       }),
     });
   }
