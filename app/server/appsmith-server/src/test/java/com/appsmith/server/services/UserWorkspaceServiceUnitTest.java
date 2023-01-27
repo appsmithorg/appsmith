651048896bb2f53f5086415cd9ad4abb51354113
Nayan <nayan@appsmith.com>
2023-01-27 11:11:44 +0600

    -removed unnecessary dependencies

--- app/server/appsmith-server/src/test/java/com/appsmith/server/services/UserWorkspaceServiceUnitTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/UserWorkspaceServiceUnitTest.java
@@ -6,12 +6,8 @@ import com.appsmith.server.domains.Workspace;
 import com.appsmith.server.dtos.PermissionGroupInfoDTO;
 import com.appsmith.server.dtos.WorkspaceMemberInfoDTO;
 import com.appsmith.server.exceptions.AppsmithError;
-import com.appsmith.server.helpers.PolicyUtils;
-import com.appsmith.server.notifications.EmailSender;
 import com.appsmith.server.repositories.PermissionGroupRepository;
 import com.appsmith.server.repositories.UserDataRepository;
-import com.appsmith.server.repositories.UserRepository;
-import com.appsmith.server.repositories.WorkspaceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,7 +22,9 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.List;
+import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
@@ -34,16 +32,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DirtiesContext
 @Slf4j
 public class UserWorkspaceServiceUnitTest {
-
-    @Autowired SessionUserService sessionUserService;
-    @Autowired WorkspaceRepository workspaceRepository;
-    @Autowired UserRepository userRepository;
     @Autowired UserDataRepository userDataRepository;
-    @Autowired PolicyUtils policyUtils;
-    @Autowired EmailSender emailSender;
     @Autowired UserDataService userDataService;
-    @Autowired PermissionGroupService permissionGroupService;
-    @Autowired TenantService tenantService;
     @Autowired WorkspaceService workspaceService;
     @Autowired PermissionGroupRepository permissionGroupRepository;
     @Autowired UserWorkspaceService userWorkspaceService;
@@ -108,5 +98,25 @@ public class UserWorkspaceServiceUnitTest {
                 .create(workspaceMembers)
                 .expectErrorMessage(AppsmithError.NO_RESOURCE_FOUND.getMessage(FieldName.WORKSPACE, sampleWorkspaceId))
                 .verify();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void getWorkspaceMembers_WhenUserHasProfilePhoto_ProfilePhotoIncluded() {
+        // create workspace
+        Workspace workspace = new Workspace();
+        workspace.setName("workspace_" + UUID.randomUUID());
+        Mono<Workspace> workspaceMono = workspaceService.create(workspace);
+
+        Mono<List<WorkspaceMemberInfoDTO>> listMono = userDataService.getForCurrentUser().flatMap(userData -> {
+                    userData.setProfilePhotoAssetId("sample-photo-id");
+                    return userDataRepository.save(userData);
+                }).then(workspaceMono)
+                .flatMap(createdWorkspace -> userWorkspaceService.getWorkspaceMembers(createdWorkspace.getId()));
+
+        StepVerifier.create(listMono).assertNext(workspaceMemberInfoDTOS -> {
+            assertThat(workspaceMemberInfoDTOS.size()).isEqualTo(1);
+            assertThat(workspaceMemberInfoDTOS.get(0).getPhotoId()).isEqualTo("sample-photo-id");
+        }).verifyComplete();
     }
 }
