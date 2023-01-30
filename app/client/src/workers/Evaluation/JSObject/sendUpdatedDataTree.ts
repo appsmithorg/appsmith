2615a0e408922d64182661cd08d2f5fdc0117cb0
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2023-01-30 15:32:33 +0530

    create sendUpdatedDataTree file

---
+++ app/client/src/workers/Evaluation/JSObject/sendUpdatedDataTree.ts
@@ -0,0 +1,22 @@
+/**
+ * Input - differences
+ * - evaluate using setupDataTree + evalAndValidate
+ * - initiate linting
+ *
+ */
+
+// import { sendMessage } from "utils/MessageUtil";
+
+// sendMessage()
+
+// sendMessage.call(self, {
+//   messageType: MessageType.DEFAULT,
+//   body: {
+//     data: {
+//       logs,
+//       eventType: this.requestInfo?.eventType,
+//       triggerMeta: this.requestInfo?.triggerMeta,
+//     },
+//     method: MAIN_THREAD_ACTION.PROCESS_LOGS,
+//   },
+// });
