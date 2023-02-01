4fbbf17c7db570eebb5f70a011553f967d69a3aa
Rishabh Rathod <rishabh.rathod@appsmith.com>
2023-02-01 17:28:06 +0530

    feat: Add jsFunction audit logs (#941)
    ## Description
    
    This PR adds logging for the execution of JS Action (JSObject functions that are marked as "Async" ). This logs would be used in audit logs feature. 
    
    Fixes https://github.com/appsmithorg/appsmith/issues/18821
    
    ## Type of change
    
    
    - Feature enhancement
    
    
    ## How Has This Been Tested?
    
    - Manual
    https://github.com/appsmithorg/TestSmith/issues/2170
    - Cypress

--- app/client/src/ee/workers/Evaluation/JSObject/postJSFunctionExecution.ts
+++ app/client/src/ee/workers/Evaluation/JSObject/postJSFunctionExecution.ts
@@ -1,1 +1,16 @@
 export * from "ce/workers/Evaluation/JSObject/postJSFunctionExecution";
+
+import { MAIN_THREAD_ACTION } from "@appsmith/workers/Evaluation/evalWorkerActions";
+import { MessageType, sendMessage } from "utils/MessageUtil";
+
+export function postJSFunctionExecutionLog(fullName: string) {
+  sendMessage.call(self, {
+    messageType: MessageType.DEFAULT,
+    body: {
+      data: {
+        fullPath: fullName,
+      },
+      method: MAIN_THREAD_ACTION.LOG_JS_FUNCTION_EXECUTION,
+    },
+  });
+}
--- app/client/src/selectors/entitiesSelector.ts
+++ app/client/src/selectors/entitiesSelector.ts
@@ -17,7 +17,10 @@ import ImageAlt from "assets/images/placeholder-image.svg";
 import { CanvasWidgetsReduxState } from "reducers/entityReducers/canvasWidgetsReducer";
 import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
 import { AppStoreState } from "reducers/entityReducers/appReducer";
-import { JSCollectionDataState } from "reducers/entityReducers/jsActionsReducer";
+import {
+  JSCollectionData,
+  JSCollectionDataState,
+} from "reducers/entityReducers/jsActionsReducer";
 import { DefaultPlugin, GenerateCRUDEnabledPluginMap } from "api/PluginApi";
 import { JSAction, JSCollection } from "entities/JSCollection";
 import { APP_MODE } from "entities/App";
@@ -376,6 +379,38 @@ export const getJSCollectionsForCurrentPage = createSelector(
     return actions.filter((a) => a.config.pageId === pageId);
   },
 );
+
+export const getJSCollectionFromName = createSelector(
+  [
+    getJSCollectionsForCurrentPage,
+    (_state: AppState, JSObjectName: string) => JSObjectName,
+  ],
+  (jsCollections, JSObjectName) => {
+    let currentJSCollection = null;
+    for (const jsCollection of jsCollections) {
+      if (JSObjectName === jsCollection.config.name) {
+        currentJSCollection = jsCollection;
+        break;
+      }
+    }
+    return currentJSCollection;
+  },
+);
+
+export const getJSActionFromJSCollection = (
+  JSCollection: JSCollectionData,
+  functionName: string,
+) => {
+  const actions = JSCollection.config.actions;
+  let currentAction = null;
+  for (const jsAction of actions) {
+    if (functionName === jsAction.name) {
+      currentAction = jsAction;
+      break;
+    }
+  }
+  return currentAction;
+};
 
 export const getPlugin = (state: AppState, pluginId: string) => {
   return state.entities.plugins.list.find((plugin) => plugin.id === pluginId);
