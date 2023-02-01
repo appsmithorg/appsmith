933775aab505f0071bf93d3cc59db38c45572a7e
Aishwarya UR <aishwarya@appsmith.com>
2023-02-01 23:34:34 +0530

    forktogit fix

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Templates/ForkTemplateToGitConnectedApp.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Templates/ForkTemplateToGitConnectedApp.js
@@ -49,23 +49,25 @@ describe("Fork a template to the current app", () => {
     }).then(($ele) => {
       cy.wrap($ele).should("have.length", 0);
     });
-    cy.get("body").then(($ele) => {
-      if (
-        $ele.find(
-          _.locators._specificToast(
-            Cypress.env("MESSAGES").SERVER_API_TIMEOUT_ERROR(),
-          ),
-        ).length <= 0
-      ) {
-        if ($ele.find(template.templateViewForkButton).length > 0) {
-          cy.get(template.templateViewForkButton).click();
-        }
-      }
-    });
-    cy.get(widgetLocators.toastAction, { timeout: 40000 }).should(
-      "contain",
-      "template added successfully",
-    );
+    cy.wait(10000); //wait 10 seconds & click on fork button
+    cy.get(template.templateViewForkButton).click();
+    // cy.get("body").then(($ele) => {
+    //   // if (
+    //   //   $ele.find(
+    //   //     _.locators._specificToast(
+    //   //       Cypress.env("MESSAGES").SERVER_API_TIMEOUT_ERROR()
+    //   //     ), {timeout: 20000}
+    //   //   ).length > 0
+    //   // ) {
+    //     if ($ele.find(template.templateViewForkButton).length > 0) {
+    //       cy.get(template.templateViewForkButton).click();
+    //     }
+    //   //}
+    // });
+    // cy.get(widgetLocators.toastAction, { timeout: 40000 }).should(
+    //   "contain",
+    //   "template added successfully",
+    // );
     cy.commitAndPush();
   });
 
