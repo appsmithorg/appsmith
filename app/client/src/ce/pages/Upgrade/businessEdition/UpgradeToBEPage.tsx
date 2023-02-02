a0c5f2986d71dfee43f1c5bbb3447c027b5f056e
Sangeeth Sivan <sangeeth@appsmith.com>
2023-02-02 12:29:20 +0530

    chore: upgrade hook link changed and image added

--- app/client/src/ce/pages/Upgrade/businessEdition/UpgradeToBEPage.tsx
+++ app/client/src/ce/pages/Upgrade/businessEdition/UpgradeToBEPage.tsx
@@ -6,41 +6,56 @@ import {
   MOVE_TO_BUSINESS_EDITION,
 } from "@appsmith/constants/messages";
 import { FooterComponent } from "../Footer";
+import useOnUpgrade from "utils/hooks/useOnUpgrade";
 
 export const UpgradeToBEPageWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
+`;
+
+export const ImageContainer = styled.div`
+  display: flex;
+  height: calc(100% - 86px);
+  width: 100%;
   img {
+    object-fit: fill;
     width: 100%;
     height: 100%;
-    object-fit: contain;
   }
-  footer {
-    height: 86px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 40px;
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  .upgrade-page-footer-container {
+    margin-left: 256px;
+    height: 90px;
   }
 `;
 
 export const UpgradeToBEPage = () => {
+  const { onUpgrade } = useOnUpgrade({
+    logEventName: "ADMIN_SETTINGS_UPGRADE_HOOK",
+    logEventData: { source: "Upgrade to BE" },
+  });
+
   return (
     <UpgradeToBEPageWrapper>
-      <img
-        alt="Upgrade to Business Edition"
-        key="upgrade-to-business-edition"
-        src={UpgradeToBusinessEdition}
-      />
-      <FooterComponent
-        message={createMessage(MOVE_TO_BUSINESS_EDITION, "?")}
-        onClick={() =>
-          window.open("https://www.appsmith.com/pricing", "_blank")
-        }
-        showHeading={false}
-      />
+      <ImageContainer>
+        <img
+          alt="Upgrade to Business Edition"
+          key="upgrade-to-business-edition"
+          src={UpgradeToBusinessEdition}
+        />
+      </ImageContainer>
+      <FooterContainer>
+        <FooterComponent
+          message={createMessage(MOVE_TO_BUSINESS_EDITION, "?")}
+          onClick={() => onUpgrade()}
+          showHeading={false}
+        />
+      </FooterContainer>
     </UpgradeToBEPageWrapper>
   );
 };
