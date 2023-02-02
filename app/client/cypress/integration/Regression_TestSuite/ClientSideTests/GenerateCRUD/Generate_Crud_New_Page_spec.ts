4751c057281f2bbafe728cd9ca16a5ff021416d0
Aishwarya UR <aishwarya@appsmith.com>
2023-02-02 15:15:43 +0530

    Client/Branding+CodeComment+GenerateCRUD grouping

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/GenerateCRUD/Generate_Crud_New_Page_spec.ts
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/GenerateCRUD/Generate_Crud_New_Page_spec.ts
@@ -2,10 +2,10 @@ import { ObjectsRegistry } from "../../../../support/Objects/Registry";
 const { ApiPage, DataSources, EntityExplorer } = ObjectsRegistry;
 
 describe("Validate generate CRUD operation by creating a datasource from generate CRUD form", () => {
-  it("Generated CRUD app should work when there are no entities in the page", () => {
+  it("Generated CRUD app should work when there are no entities in the page & when there are entities in the current page", () => {
     DataSources.GeneratePageWithMockDB();
-  });
-  it("Generated CRUD app should work when there are entities in the current page", () => {
+
+    //when there are entities in the current page
     EntityExplorer.AddNewPage("add-page");
     ApiPage.CreateApi();
     DataSources.GeneratePageWithMockDB();
