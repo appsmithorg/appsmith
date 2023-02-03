8d92a9e33e0ba0c49858e7cbf2bcfa6481510acc
Druthi Polisetty <druthi@appsmith.com>
2023-02-03 15:59:41 +0530

    Added another check to see if text widget shows user.email

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/BugTests/Bug20275_Spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/BugTests/Bug20275_Spec.js
@@ -1,8 +1,12 @@
 import { ObjectsRegistry } from "../../../../support/Objects/Registry";
+import { WIDGET } from "../../../../locators/WidgetLocators";
 
 const jsEditor = ObjectsRegistry.JSEditor,
   agHelper = ObjectsRegistry.AggregateHelper,
-  deployMode = ObjectsRegistry.DeployMode;
+  deployMode = ObjectsRegistry.DeployMode,
+  locator = ObjectsRegistry.CommonLocators,
+  ee = ObjectsRegistry.EntityExplorer,
+  propPane = ObjectsRegistry.PropertyPane;
 
 describe("Testing if user.email is avaible on page load", function() {
   it("Bug: 20275: {{appsmith.user.email}} is not available on page load", function() {
@@ -22,8 +26,16 @@ describe("Testing if user.email is avaible on page load", function() {
 
     jsEditor.EnableDisableAsyncFuncSettings("myFun1", true, false);
 
+    ee.DragDropWidgetNVerify(WIDGET.TEXT, 200, 600);
+    ee.SelectEntityByName("Text1");
+    propPane.TypeTextIntoField("Text", "{{appsmith.user.email}}");
+
     deployMode.DeployApp();
 
     agHelper.ValidateToastMessage(Cypress.env("USERNAME"));
+
+    agHelper
+      .GetText(locator._textWidgetInDeployed)
+      .then(($userEmail) => expect($userEmail).to.eq(Cypress.env("USERNAME")));
   });
 });
