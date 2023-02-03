5119bb517249a0c25381ec592f174a07f5fed22b
Aishwarya UR <aishwarya@appsmith.com>
2023-02-03 22:42:49 +0530

    git import removed wait

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Git/GitImport/GitImport_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Git/GitImport/GitImport_spec.js
@@ -202,7 +202,7 @@ describe("Git import flow ", function() {
     cy.get(gitSyncLocators.commitCommentInput).type("Initial Commit");
     cy.get(gitSyncLocators.commitButton).click();
     cy.intercept("POST", "api/v1/git/commit/app/*").as("commit");
-    cy.wait(10000);
+    _.agHelper.AssertElementExist(_.gitSync._bottomBarPull);
     cy.get(gitSyncLocators.closeGitSyncModal).click();
     cy.wait(2000);
     cy.merge(mainBranch);
