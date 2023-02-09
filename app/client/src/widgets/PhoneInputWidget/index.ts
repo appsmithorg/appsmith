35fb011466e39b295c69823b14f3235028d22da9
Aswath K <aswath@appsmith.com>
2023-02-09 17:30:51 +0530

    Increase minWidth for Currency & Phone input

--- app/client/src/widgets/PhoneInputWidget/index.ts
+++ app/client/src/widgets/PhoneInputWidget/index.ts
@@ -47,7 +47,7 @@ export const CONFIG = {
         viewportMinWidth: 0,
         configuration: () => {
           return {
-            minWidth: "60px",
+            minWidth: "180px",
           };
         },
       },
