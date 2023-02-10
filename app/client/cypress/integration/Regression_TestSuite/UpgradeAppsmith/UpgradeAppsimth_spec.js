be07bea4c59e812ee4dd826e20a5a494e0c43bc3
Saroj <saroj@appsmith.com>
2023-02-10 10:32:38 +0530

    skip the test in case of BE

--- app/client/cypress/integration/Regression_TestSuite/UpgradeAppsmith/UpgradeAppsimth_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/UpgradeAppsmith/UpgradeAppsimth_spec.js
@@ -32,62 +32,63 @@ describe("Upgrade appsmith version", () => {
       cy.GetAndVerifyLogs(tedUrl, `appsmith-${name}`);
     });
 
-    //verify the Applications after upgrade
-    cy.forceVisit(testdata.APPURL);
-    agHelper.GetNClick(".t--widget-iconbuttonwidget button", 0, true);
-    agHelper.Sleep(1000);
-    agHelper.GetNAssertElementText(
-      ".tbody>div",
-      "1Developmentexpertise",
-      "have.text",
-      1,
-    );
+    //verify the Applications after upgrade only on CE and skip for BE
+    if (Cypress.env("Edition") !== 0) {
+      cy.forceVisit(testdata.APPURL);
+      agHelper.GetNClick(".t--widget-iconbuttonwidget button", 0, true);
+      agHelper.Sleep(1000);
+      agHelper.GetNAssertElementText(
+        ".tbody>div",
+        "1Developmentexpertise",
+        "have.text",
+        1,
+      );
 
-    agHelper.GetNClick(".tbody>div", 1, true, 1000);
-    agHelper.GetNAssertElementText(
-      ".t--jsonformfield-label input",
-      "Development",
-      "have.value",
-    );
-    agHelper.GetNAssertElementText(
-      ".t--jsonformfield-type input",
-      "expertise",
-      "have.value",
-    );
-    agHelper.GetNAssertElementText(
-      ".t--jsonformfield-rowIndex input",
-      "1",
-      "have.value",
-    );
+      agHelper.GetNClick(".tbody>div", 1, true, 1000);
+      agHelper.GetNAssertElementText(
+        ".t--jsonformfield-label input",
+        "Development",
+        "have.value",
+      );
+      agHelper.GetNAssertElementText(
+        ".t--jsonformfield-type input",
+        "expertise",
+        "have.value",
+      );
+      agHelper.GetNAssertElementText(
+        ".t--jsonformfield-rowIndex input",
+        "1",
+        "have.value",
+      );
 
-    cy.get(".t--jsonformfield-label input")
-      .clear()
-      .type("DevelopmentUpdate");
-    agHelper.GetNClick(".t--jsonform-footer button", 1, true);
-    agHelper.Sleep(2000);
-    agHelper.GetNClick(".t--widget-iconbuttonwidget button", 0, true, 1000);
-    agHelper.GetNAssertElementText(
-      ".tbody>div",
-      "1DevelopmentUpdateexpertise",
-      "have.text",
-      1,
-    );
+      cy.get(".t--jsonformfield-label input")
+        .clear()
+        .type("DevelopmentUpdate");
+      agHelper.GetNClick(".t--jsonform-footer button", 1, true);
+      agHelper.Sleep(2000);
+      agHelper.GetNClick(".t--widget-iconbuttonwidget button", 0, true, 1000);
+      agHelper.GetNAssertElementText(
+        ".tbody>div",
+        "1DevelopmentUpdateexpertise",
+        "have.text",
+        1,
+      );
 
-    //Resetting the data
-    agHelper.GetNClick(".tbody>div", 1, true, 1000);
-    cy.get(".t--jsonformfield-label input")
-      .clear()
-      .type("Development");
-    agHelper.GetNClick(".t--jsonform-footer button", 1, true);
-    agHelper.Sleep(2000);
-    agHelper.GetNClick(".t--widget-iconbuttonwidget button", 0, true, 1000);
-    agHelper.GetNAssertElementText(
-      ".tbody>div",
-      "1Developmentexpertise",
-      "have.text",
-      1,
-    );
-
+      //Resetting the data
+      agHelper.GetNClick(".tbody>div", 1, true, 1000);
+      cy.get(".t--jsonformfield-label input")
+        .clear()
+        .type("Development");
+      agHelper.GetNClick(".t--jsonform-footer button", 1, true);
+      agHelper.Sleep(2000);
+      agHelper.GetNClick(".t--widget-iconbuttonwidget button", 0, true, 1000);
+      agHelper.GetNAssertElementText(
+        ".tbody>div",
+        "1Developmentexpertise",
+        "have.text",
+        1,
+      );
+    }
     // stop the container
     cy.StopContainer(tedUrl, localStorage.getItem("ContainerName"));
     agHelper.Sleep(2000);
