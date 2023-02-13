1791782639046c36cb906dd0f2f92f20fa7d7183
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-02-13 12:24:56 +0530

    import

---
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/PeekOverlay/PeekOverlay.spec.ts
@@ -0,0 +1,7 @@
+import * as _ from "../../../../support/Objects/ObjectsCore";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+describe("peek overlay", () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  cy.get("@guid").then((guid) => {});
+});
