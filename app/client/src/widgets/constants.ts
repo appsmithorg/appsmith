fc77cc1a319b7cd9aaffe9b5f26c5e71d9a75440
Tolulope Adetula <31691737+Tooluloope@users.noreply.github.com>
2023-02-13 11:57:59 +0000

    feat: update BlueprintOperationTypes

--- app/client/src/widgets/constants.ts
+++ app/client/src/widgets/constants.ts
@@ -42,6 +42,9 @@ export enum BlueprintOperationTypes {
   MODIFY_PROPS = "MODIFY_PROPS",
   ADD_ACTION = "ADD_ACTION",
   CHILD_OPERATIONS = "CHILD_OPERATIONS",
+  BEFORE_DROP = "BEFORE_DROP",
+  BEFORE_COPY = "BEFORE_COPY",
+  BEFORE_ADD = "BEFORE_ADD",
 }
 
 export type FlattenedWidgetProps = WidgetProps & {
