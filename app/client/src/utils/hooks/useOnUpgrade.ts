bff816df29bc511eec366308c69d47fe04a411d7
Sangeeth Sivan <sangeeth@appsmith.com>
2023-02-14 13:21:34 +0530

    chore: remove feature flags

--- app/client/src/utils/hooks/useOnUpgrade.ts
+++ app/client/src/utils/hooks/useOnUpgrade.ts
@@ -1,14 +1,6 @@
-import { getAppsmithConfigs } from "@appsmith/configs";
-import {
-  createMessage,
-  UPGRADE_TO_EE_GENERIC,
-} from "@appsmith/constants/messages";
-import { getInstanceId } from "@appsmith/selectors/tenantSelectors";
+import { getTenantConfig } from "@appsmith/selectors/tenantSelectors";
 import { useSelector } from "react-redux";
-import { selectFeatureFlags } from "selectors/usersSelectors";
 import AnalyticsUtil, { EventName } from "utils/AnalyticsUtil";
-
-const { intercomAppID } = getAppsmithConfigs();
 
 type Props = {
   intercomMessage?: string;
@@ -16,30 +8,22 @@ type Props = {
   logEventData?: any;
 };
 
-const useOnUpgrade = (props: Props) => {
-  const { intercomMessage, logEventData, logEventName } = props;
-  const features = useSelector(selectFeatureFlags);
-  const instanceId = useSelector(getInstanceId);
+const PRICING_PAGE_URL =
+  "https://www.appsmith.com/api/preview?secret=8JPsJRnSkt6Va8FzxUPFhZezxZuHRnSU&slug=pricing-preview";
 
-  const triggerIntercom = (message: string) => {
-    if (intercomAppID && window.Intercom) {
-      window.Intercom("showNewMessage", message);
-    }
-  };
+const useOnUpgrade = (props: Props) => {
+  const { logEventData, logEventName } = props;
+  const tenantConfig = useSelector(getTenantConfig);
 
   const onUpgrade = () => {
     AnalyticsUtil.logEvent(
       logEventName || "ADMIN_SETTINGS_UPGRADE",
       logEventData,
     );
-    if (features.USAGE_AND_BILLING) {
-      window.open(
-        `https://www.appsmith.com/api/preview?secret=8JPsJRnSkt6Va8FzxUPFhZezxZuHRnSU&slug=pricing-preview?source=CE&instance=${instanceId}`,
-        "_blank",
-      );
-    } else {
-      triggerIntercom(intercomMessage || createMessage(UPGRADE_TO_EE_GENERIC));
-    }
+    window.open(
+      `${PRICING_PAGE_URL}?source=CE&instance=${tenantConfig?.instanceId}`,
+      "_blank",
+    );
   };
 
   return { onUpgrade };
