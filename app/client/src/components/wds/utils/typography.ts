28dec74d44c623977aa7907130c0bcec2cd1f80b
vadim <vadim@appsmith.com>
2023-02-14 12:38:58 +0100

    Clean up font stack

--- app/client/src/components/wds/utils/typography.ts
+++ app/client/src/components/wds/utils/typography.ts
@@ -50,7 +50,7 @@ export const createTypographyStyles = (props: createTypographyStylesProps) => {
     });
 
     return {
-      fontFamily: `-apple-system, "-apple-system Fallback: BlinkMacSy…back: Segoe UI", "-apple-system Fallback: Roboto"`,
+      fontFamily: `-apple-system, BlinkMacSystemFont, "-apple-system Fallback: Segoe UI", "-apple-system Fallback: Roboto"`,
       ...styles,
     };
   }
