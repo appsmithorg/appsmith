c09403542b37473cece88456e8d43420474a30d9
Favour Ohans <fohanekwu@gmail.com>
2023-02-14 18:09:58 +0100

    Update annotation

--- app/client/src/entities/DataTree/dataTreeWidget.ts
+++ app/client/src/entities/DataTree/dataTreeWidget.ts
@@ -216,7 +216,7 @@ export const generateDataTreeWidget = (
   const overridingMetaProps: Record<string, unknown> = {};
 
   // overridingMetaProps maps properties that can be overriden by either default values or meta changes to initial values.
-  // initial value is set to metaProps value or undefined (set to undefined to ensure that its path is present in the unevalTree).
+  // initial value is set to metaProps value or defaultMetaProps value.
   Object.entries(defaultMetaProps).forEach(([key, value]) => {
     if (overridingMetaPropsMap[key]) {
       overridingMetaProps[key] =
