043dfd95330ab59b4aef113473cd265e17ceb022
Druthi Polisetty <druthi@appsmith.com>
2023-02-15 21:56:55 +0530

    Fixed cypress tests to follow new format

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/OtherUIFeatures/Widget_Error_spec.ts
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/OtherUIFeatures/Widget_Error_spec.ts
@@ -2,6 +2,8 @@ import { ObjectsRegistry } from "../../../../support/Objects/Registry";
 
 const dsl = require("../../../../fixtures/buttondsl.json");
 const widgetLocators = require("../../../../locators/Widgets.json");
+import * as _ from "../../../../support/Objects/ObjectsCore";
+import { WIDGET } from "../../../../locators/WidgetLocators";
 
 const {
   DebuggerHelper: debuggerHelper,
@@ -15,7 +17,7 @@ describe("Widget error state", function() {
     cy.addDsl(dsl);
   });
 
-  it("Check widget error state", function() {
+  it("1. Check widget error state", function() {
     cy.openPropertyPane("buttonwidget");
 
     cy.get(".t--property-control-visible")
@@ -28,14 +30,14 @@ describe("Widget error state", function() {
     cy.contains(".t--widget-error-count", 1);
   });
 
-  it("Check if the current value is shown in the debugger", function() {
+  it("2. Check if the current value is shown in the debugger", function() {
     debuggerHelper.ClickDebuggerIcon();
     cy.contains(".react-tabs__tab", "Errors").click();
 
     debuggerHelper.LogStateContains("Test");
   });
 
-  it("Switch to error tab when clicked on the debug button", function() {
+  it("3. Switch to error tab when clicked on the debug button", function() {
     cy.get("[data-cy=t--tab-LOGS_TAB]").click();
     cy.get(".t--property-control-onclick")
       .find(".t--js-toggle")
@@ -48,35 +50,33 @@ describe("Widget error state", function() {
     cy.contains(".react-tabs__tab--selected", "Errors");
   });
 
-  it("All errors should be expanded by default", function() {
+  it("4. All errors should be expanded by default", function() {
     debuggerHelper.AssertVisibleErrorMessagesCount(2);
   });
 
-  it("Recent errors are shown at the top of the list", function() {
+  it("5. Recent errors are shown at the top of the list", function() {
     cy.testJsontext("label", "{{[]}}");
     debuggerHelper.LogStateContains("text", 0);
   });
 
-  it("Clicking on a message should open the search menu", function() {
+  it("6. Clicking on a message should open the search menu", function() {
     debuggerHelper.ClickErrorMessage(0);
     debuggerHelper.AssertContextMenuItemVisible();
   });
 
-  it("Undoing widget deletion should show errors if present", function() {
+  it("7. Undoing widget deletion should show errors if present", function() {
     cy.deleteWidget();
     debuggerHelper.AssertVisibleErrorMessagesCount(0);
     cy.get("body").type(`{${modifierKey}}z`);
     debuggerHelper.AssertVisibleErrorMessagesCount(2);
   });
 
-  it("Bug-2760: Error log on a widget property not clearing out when the widget property is deleted", function() {
-    cy.dragAndDropToCanvas("tablewidgetv2", {
-      x: 150,
-      y: 300,
-    });
-    cy.openPropertyPane("tablewidgetv2");
-    cy.addColumnV2("customColumn1");
-    cy.editColumn("customColumn1");
+  it("8. Bug-2760: Error log on a widget property not clearing out when the widget property is deleted", function() {
+    _.ee.DragDropWidgetNVerify(WIDGET.TABLE, 150, 300);
+    _.ee.SelectEntityByName("Table1", "Widgets");
+
+    _.table.AddColumn("customColumn1");
+    _.table.EditColumn("customColumn1");
     propPane.UpdatePropertyFieldValue("Computed Value", "{{test}}");
 
     debuggerHelper.AssertDebugError(
@@ -85,7 +85,7 @@ describe("Widget error state", function() {
       false,
     );
 
-    cy.deleteColumn("customColumn1");
+    _.table.DeleteColumn("customColumn1");
 
     debuggerHelper.DebuggerListDoesnotContain(
       "ReferenceError: test is not defined",
