37adeef62f3d799a9366ba2cbc31d37e9a1607df
Arsalan <arsalanyaldram0211@outlook.com>
2023-02-16 17:43:35 +0530

    feat: added autolayout config to group widgets.

--- app/client/src/widgets/RadioGroupWidget/index.ts
+++ app/client/src/widgets/RadioGroupWidget/index.ts
@@ -1,7 +1,8 @@
-import IconSVG from "./icon.svg";
 import { Alignment } from "@blueprintjs/core";
-import Widget from "./widget";
 import { LabelPosition } from "components/constants";
+
+import IconSVG from "./icon.svg";
+import Widget from "./widget";
 
 export const CONFIG = {
   type: Widget.getWidgetType(),
@@ -44,6 +45,18 @@ export const CONFIG = {
     contentConfig: Widget.getPropertyPaneContentConfig(),
     styleConfig: Widget.getPropertyPaneStyleConfig(),
     stylesheetConfig: Widget.getStylesheetConfig(),
+  },
+  autoLayout: {
+    widgetSize: [
+      {
+        viewportMinWidth: 0,
+        configuration: () => {
+          return {
+            minWidth: "240px",
+          };
+        },
+      },
+    ],
   },
 };
 
