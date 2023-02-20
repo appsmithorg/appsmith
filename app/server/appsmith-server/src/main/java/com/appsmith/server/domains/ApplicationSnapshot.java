bde8b7192f09c3c08df161844bd5b975ab09ae97
Nayan <nayan@appsmith.com>
2023-02-21 00:11:49 +0600

    added last updated time in the response

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ApplicationSnapshot.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ApplicationSnapshot.java
@@ -2,6 +2,7 @@ package com.appsmith.server.domains;
 
 import com.appsmith.external.models.BaseDomain;
 import com.appsmith.server.dtos.ApplicationJson;
+import com.appsmith.server.helpers.DateUtils;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,4 +18,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class ApplicationSnapshot extends BaseDomain {
     private String applicationId;
     private ApplicationJson applicationJson;
+
+    /**
+     * Adding this method as updatedAt field in BaseDomain is annotated with @JsonIgnore
+     * @return Updated at timestamp in ISO format
+     */
+    public String getUpdatedTime() {
+        return DateUtils.ISO_FORMATTER.format(this.getUpdatedAt());
+    }
 }
