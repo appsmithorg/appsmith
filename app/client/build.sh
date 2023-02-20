a7350d8d2568d934cc336f663945c8867dba9181
Pawan Kumar <pawan.stardust@gmail.com>
2023-02-20 19:42:36 +0530

    change order storybook build and cra build

--- app/client/build.sh
+++ app/client/build.sh
@@ -6,6 +6,12 @@ GIT_SHA=$(eval git rev-parse HEAD)
 echo $GIT_SHA
 echo "Sentry Auth Token: $SENTRY_AUTH_TOKEN"
 
+# build storybook and move to the static folder
+cd ./packages/wds
+yarn build-storybook
+mv -f ./storybook-static ../../build/static/wds
+cd ../../
+
 # build cra app
 REACT_APP_SENTRY_RELEASE=$GIT_SHA REACT_APP_CLIENT_LOG_LEVEL=ERROR EXTEND_ESLINT=true craco --max-old-space-size=4096 build --config craco.build.config.js
 
@@ -16,9 +22,3 @@ if [ "$GITHUB_REPOSITORY" == "appsmithorg/appsmith-ee" ]; then
 fi
 
 echo "build finished"
-
-# build storybook and move to the static folder
-cd ./packages/wds
-yarn build-storybook
-
-mv -f ./storybook-static ../../build/static/wds
