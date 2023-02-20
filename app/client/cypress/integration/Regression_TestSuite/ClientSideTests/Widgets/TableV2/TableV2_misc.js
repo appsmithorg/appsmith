f541f9afec79a1f5c25137a26b06a958e38a87ed
souma-ghosh <souma@appsmith.com>
2023-02-20 15:11:54 +0530

    Adds test case for undefine and object type values

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/TableV2/TableV2_misc.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/TableV2/TableV2_misc.js
@@ -3,12 +3,12 @@ import { ObjectsRegistry } from "../../../../../support/Objects/Registry";
 
 const table = ObjectsRegistry.TableV2;
 
-describe("Table Widget V2 row multi select validation", function() {
+describe("tests bug 20663 TypeError: Cannot read properties of undefined", function() {
   before(() => {
     cy.addDsl(dsl);
   });
 
-  it("tests bug 20663 TypeError: Cannot read properties of undefined", function() {
+  it("test bug when the evaluated column label value is an object", function() {
     cy.openPropertyPane("tablewidgetv2");
     cy.get(
       ".tablewidgetv2-primarycolumn-list div[data-rbd-draggable-id='id'] input[type=text]",
@@ -17,6 +17,18 @@ describe("Table Widget V2 row multi select validation", function() {
       ".tablewidgetv2-primarycolumn-list div[data-rbd-draggable-id='id'] input[type=text]",
       // eslint-disable-next-line prettier/prettier
     ).type("{{}{{}appsmith{}}{}}");
+    cy.get(table._tableRow(0, 0)).should("exist");
+  });
+
+  it("test bug when the evaluated column label value is undefined", function() {
+    cy.openPropertyPane("tablewidgetv2");
+    cy.get(
+      ".tablewidgetv2-primarycolumn-list div[data-rbd-draggable-id='id'] input[type=text]",
+    ).clear();
+    cy.get(
+      ".tablewidgetv2-primarycolumn-list div[data-rbd-draggable-id='id'] input[type=text]",
+      // eslint-disable-next-line prettier/prettier
+    ).type("{{}{{}ap{}}{}}");
     cy.get(table._tableRow(0, 0)).should("exist");
   });
 });
