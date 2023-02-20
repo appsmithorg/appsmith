27f62123587b60443bbeda6f324da84d2ae26b5c
Aishwarya UR <aishwarya@appsmith.com>
2023-02-21 00:59:40 +0530

    PgAdmin spec fix

--- app/client/cypress/integration/Regression_TestSuite/Application/PgAdmin_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/Application/PgAdmin_spec.js
@@ -5,7 +5,7 @@ const widgetsPage = require("../../../locators/Widgets.json");
 const appPage = require("../../../locators/PgAdminlocators.json");
 
 describe("PgAdmin Clone App", function() {
-  let datasourceName;
+  let datasourceName, tableName;
 
   before("Add dsl and create datasource", () => {
     cy.addDsl(dsl);
@@ -19,9 +19,8 @@ describe("PgAdmin Clone App", function() {
     cy.NavigateToQueryEditor();
     cy.NavigateToActiveTab();
     // clicking on new query to write a query
-    cy.contains(".t--datasource-name", datasourceName)
-      .find(queryLocators.createQuery)
-      .click();
+
+    _.dataSources.CreateQuery(datasourceName);
     cy.get(queryLocators.queryNameField).type("get_schema");
     // switching off Use Prepared Statement toggle
     _.dataSources.ToggleUsePreparedStatement(false);
@@ -34,9 +33,9 @@ describe("PgAdmin Clone App", function() {
     _.dataSources.RunQuery();
     // clicking on chevron icon to go back to the _.dataSources page
     cy.get(appPage.dropdownChevronLeft).click();
+
     // clicking on new query to write a query
     _.dataSources.CreateQuery(datasourceName);
-
     cy.get(queryLocators.queryNameField).type("get_tables");
     cy.get(queryLocators.templateMenu).click();
     // writing query to get all the tables
@@ -47,6 +46,7 @@ describe("PgAdmin Clone App", function() {
     _.dataSources.RunQuery();
     // clicking on chevron icon to go back to the _.dataSources page
     cy.get(appPage.dropdownChevronLeft).click();
+
     // clicking on new query to write a query
     _.dataSources.CreateQuery(datasourceName);
     cy.get(queryLocators.queryNameField).type("get_columns");
@@ -56,33 +56,7 @@ describe("PgAdmin Clone App", function() {
       `SELECT column_name, data_type, table_name, ordinal_position, is_nullable FROM information_schema.COLUMNS`,
     );
     _.dataSources.RunQuery();
-    _.ee.SelectEntityByName("Page1", "Pages");
-    _.agHelper.GetNClick(appPage.viewButton, 0, true);
-    // adding new table
-    _.agHelper.GetNClick(appPage.addNewtable, 0, true);
-    _.agHelper.GenerateUUID();
-    cy.get("@guid").then((uid) => {
-      cy.xpath(appPage.addTablename)
-        .clear()
-        .type(`table${uid}`);
-    });
-    // adding column to the table
-    _.agHelper.GetNClick(appPage.addColumn, 0, true);
-    _.agHelper.AssertElementVisible(appPage.columnNamefield);
-    _.agHelper.AssertElementVisible(appPage.datatypefield);
-    _.agHelper.GetNClick(appPage.addColumnName);
-    _.agHelper.UpdateInput(appPage.addColumnName, "id");
-    cy.get(appPage.dropdownChevronDown)
-      .last()
-      .click();
-    _.agHelper.GetNClick(appPage.selectDatatype);
-    // switching on the Not Null toggle
-    cy.get(widgetsPage.switchWidgetInactive)
-      .last()
-      .click();
-    _.agHelper.GetNClick(appPage.submitButton, 0, true);
-    _.agHelper.AssertElementVisible(appPage.addColumn);
-    _.agHelper.GetNClick(appPage.closeButton, 0, true);
+
     _.dataSources.NavigateToActiveTab();
     _.dataSources.CreateQuery(datasourceName);
     _.agHelper.RenameWithInPane("create_table");
@@ -93,9 +67,9 @@ describe("PgAdmin Clone App", function() {
     _.dataSources.EnterQuery(
       `CREATE TABLE {{schema_select.selectedOptionValue}}.{{nt_name.text.replaceAll(" ","_")}}({{appsmith.store.nt_col.map((c)=>c.name.replaceAll(" ","_") + " " + c.dtype + (c.nnull ? " NOT NULL " :  "") + (c.pkey ? " PRIMARY KEY " : "")).join(" , ")}})`,
     );
-    _.dataSources.RunQuery();
     // clicking on chevron icon to go back to the _.dataSources page
     cy.get(appPage.dropdownChevronLeft).click();
+
     // clicking on new query to write a query
     _.dataSources.CreateQuery(datasourceName);
     _.agHelper.RenameWithInPane("drop_table");
@@ -104,16 +78,14 @@ describe("PgAdmin Clone App", function() {
     _.dataSources.ToggleUsePreparedStatement(false);
     // creating query to delete the table
     _.dataSources.EnterQuery(
-      `DROP TABLE {{schema_select.selectedOptionValue}}.{{List1.selectedItem.tablename}};`,
+      `DROP TABLE {{schema_select.selectedOptionValue}}.{{nt_name.text.replaceAll(" ","_")}}({{appsmith.store.nt_col.map((c)=>c.name.replaceAll(" ","_") + " " + c.dtype + (c.nnull ? " NOT NULL " :  "") + (c.pkey ? " PRIMARY KEY " : "")).join(" , ")}});`,
     );
-    _.dataSources.RunQuery();
     // clicking on chevron icon to go back to the _.dataSources page
     cy.get(appPage.dropdownChevronLeft).click();
   });
 
-  it("2. Add new table", function() {
-    // clicking on chevron to go back to the application page
-    cy.get(appPage.dropdownChevronLeft).click();
+  it("2. Add new table from app page, View and Delete table", function() {
+    _.deployMode.DeployApp();
     // adding new table
     cy.xpath(appPage.addNewtable).click({ force: true });
     cy.wait(500);
@@ -121,17 +93,15 @@ describe("PgAdmin Clone App", function() {
       cy.xpath(appPage.addTablename)
         .clear()
         .type(`table${UUID}`);
+      tableName = `table${UUID}`;
     });
     // adding column to the table
     cy.xpath(appPage.addColumn).click({ force: true });
     cy.xpath(appPage.columnNamefield).should("be.visible");
     cy.xpath(appPage.datatypefield).should("be.visible");
     _.agHelper.GetNClick(appPage.addColumnName);
-    _.agHelper.UpdateInput(appPage.addColumnName, "id2");
-    cy.get(appPage.dropdownChevronDown)
-      .last()
-      .click();
-    cy.xpath(appPage.selectDatatype).click();
+    _.agHelper.UpdateInput(appPage.addColumnName, "ID");
+    _.agHelper.SelectFromDropDown("Varchar", "", 1);
     // switching on the Primary Key toggle
     cy.get(widgetsPage.switchWidgetInactive)
       .first()
@@ -147,9 +117,6 @@ describe("PgAdmin Clone App", function() {
       .first()
       .click({ force: true });
     cy.xpath(appPage.closeButton).click({ force: true });
-  });
-
-  it("3.View and Delete table", function() {
     cy.xpath(appPage.addNewtable).should("be.visible");
     // viewing the table's columns by clicking on view button
     cy.xpath(appPage.viewButton)
