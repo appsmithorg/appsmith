efb63e7393beef48a31a06c435fd059dc86ebdd0
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2023-02-21 09:34:58 +0530

    chore: undo multipane change

--- app/client/src/selectors/multiPaneSelectors.ts
+++ app/client/src/selectors/multiPaneSelectors.ts
@@ -4,7 +4,7 @@ export const getTabsPaneWidth = (state: AppState) =>
   state.ui.multiPaneConfig.tabsPaneWidth;
 
 export const isMultiPaneActive = (state: AppState) =>
-  state.ui.users.featureFlag.data.MULTIPLE_PANES === false;
+  state.ui.users.featureFlag.data.MULTIPLE_PANES === true;
 
 export const getPaneCount = (state: AppState) =>
   state.ui.multiPaneConfig.paneCount;
