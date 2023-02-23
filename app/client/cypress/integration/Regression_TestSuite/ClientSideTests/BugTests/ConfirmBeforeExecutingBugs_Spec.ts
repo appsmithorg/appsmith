87b3fa0742506a91d427804a5e6ecd64943435d0
Druthi Polisetty <druthi@appsmith.com>
2023-02-23 10:54:08 +0530

    fix: Function execution does not wait for user confirmation before running and does not display response when run on page laod and added tests

---
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/BugTests/ConfirmBeforeExecutingBugs_Spec.ts
@@ -0,0 +1,110 @@
+const dsl = require("../../../../fixtures/buttondsl.json");
+
+import * as _ from "../../../../support/Objects/ObjectsCore";
+import { WIDGET } from "../../../../locators/WidgetLocators";
+
+const jsObjectBody1 = `export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1(){
+
+	},
+	myFun2: async () => {
+		showAlert("ran successfully");
+	}
+}`;
+
+const jsObjectBody2 = `export default {
+	myVar1: [],
+	myVar2: {},
+  myFun1: async function() {
+		return showAlert('hi');
+	},
+	myFun2: function() {
+		showAlert("myFun2 is running");
+
+		return this.myFun1();
+	},
+}`;
+
+describe("Confirm before executing a js function should always show a confirmation modal before executing", function() {
+  it("1. Bug-17500: `Confirm before executing` setting does not work when a JS function is trigged on a button click", function() {
+    _.jsEditor.CreateJSObject(jsObjectBody1, {
+      paste: true,
+      completeReplace: true,
+      toRun: false,
+      shouldCreateNewJSObj: true,
+      prettify: false,
+    });
+
+    _.jsEditor.EnableDisableAsyncFuncSettings("myFun2", false, true);
+
+    _.ee.DragDropWidgetNVerify(WIDGET.BUTTON);
+    _.ee.SelectEntityByName("Button1", "Widgets");
+
+    _.propPane.EnterJSContext("onClick", `{{JSObject1.myFun2()}}`, true, false);
+
+    _.agHelper.ClickButton("Submit");
+    _.agHelper.AssertContains("Confirmation Dialog");
+    _.agHelper.ClickButton("Yes");
+    _.agHelper.ValidateToastMessage("ran successfully");
+
+    _.agHelper.WaitUntilAllToastsDisappear();
+
+    _.agHelper.ClickButton("Submit");
+    _.agHelper.ClickButton("No");
+    _.agHelper.ValidateToastMessage("JSObject1.myFun2 was cancelled");
+  });
+
+  it("2. Bug-13646: Function execution does not wait for user confirmation when a nested function", function() {
+    _.jsEditor.CreateJSObject(jsObjectBody2, {
+      paste: true,
+      completeReplace: true,
+      toRun: false,
+      shouldCreateNewJSObj: true,
+      prettify: false,
+    });
+
+    _.jsEditor.EnableDisableAsyncFuncSettings("myFun1", false, true);
+
+    _.ee.SelectEntityByName("Button1", "Widgets");
+    _.propPane.EnterJSContext("onClick", `{{JSObject2.myFun2()}}`, true, false);
+
+    _.agHelper.ClickButton("Submit");
+    _.agHelper.ValidateToastMessage("myFun2 is running");
+    _.agHelper.AssertContains("Confirmation Dialog");
+
+    _.agHelper.WaitUntilAllToastsDisappear();
+
+    _.agHelper.ClickButton("Yes");
+    _.agHelper.ValidateToastMessage("hi");
+
+    _.agHelper.WaitUntilAllToastsDisappear();
+
+    _.agHelper.ClickButton("Submit");
+    _.agHelper.ClickButton("No");
+    _.agHelper.ValidateToastMessage("JSObject2.myFun1 was cancelled", 1);
+  });
+
+  it("3. Bug-13646: Function execution does not wait for user confirmation when in a nested function and is run on page load", function() {
+    _.ee.SelectEntityByName("JSObject2", "Queries/JS");
+    _.jsEditor.EnableDisableAsyncFuncSettings("myFun2", true, false);
+
+    _.agHelper.RefreshPage();
+
+    _.agHelper.ValidateToastMessage("myFun2 is running");
+    _.agHelper.AssertContains("Confirmation Dialog");
+
+    _.agHelper.WaitUntilAllToastsDisappear();
+
+    _.agHelper.ClickButton("Yes");
+    _.agHelper.ValidateToastMessage("hi");
+
+    _.agHelper.WaitUntilAllToastsDisappear();
+
+    _.agHelper.RefreshPage();
+
+    _.agHelper.ClickButton("No");
+    _.agHelper.ValidateToastMessage("JSObject2.myFun1 was cancelled", 1);
+  });
+});
