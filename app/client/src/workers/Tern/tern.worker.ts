c35bd3f515bf23aefa1e43fc221d5471e4619126
Ivan Akulov <mail@iamakulov.com>
2023-02-27 00:26:00 +0100

    Move Tern definitions to the Tern worker
    This avoids bundling them in the main bundle

--- app/client/src/workers/Tern/tern.worker.ts
+++ app/client/src/workers/Tern/tern.worker.ts
@@ -1,5 +1,16 @@
 import tern, { Server, Def } from "tern";
 import { CallbackFn, TernWorkerAction } from "utils/autocomplete/types";
+import ecma from "constants/defs/ecmascript.json";
+import lodash from "constants/defs/lodash.json";
+import base64 from "constants/defs/base64-js.json";
+import moment from "constants/defs/moment.json";
+import xmlJs from "constants/defs/xmlParser.json";
+import forge from "constants/defs/forge.json";
+import browser from "constants/defs/browser.json";
+import {
+  GLOBAL_DEFS,
+  GLOBAL_FUNCTIONS,
+} from "@appsmith/utils/autocomplete/EntityDefinitions";
 
 let server: Server;
 
@@ -10,7 +21,7 @@ self.onmessage = function(e) {
   const data = e.data;
   switch (data.type) {
     case TernWorkerAction.INIT:
-      return startServer(data.defs, data.plugins, data.scripts);
+      return startServer(data.plugins, data.scripts);
     case TernWorkerAction.ADD_FILE:
       return server.addFile(data.name, data.text);
     case TernWorkerAction.DELETE_FILE:
@@ -37,13 +48,23 @@ function getFile(file: string, c: CallbackFn) {
   pending[nextId] = c;
 }
 
-function startServer(defs: Def[], plugins = {}, scripts?: string[]) {
+function startServer(plugins = {}, scripts?: string[]) {
   if (scripts) self.importScripts.apply(null, scripts);
 
   server = new tern.Server({
     getFile: getFile,
     async: true,
-    defs: defs,
+    defs: [
+      ecma,
+      browser,
+      GLOBAL_FUNCTIONS,
+      GLOBAL_DEFS,
+      lodash,
+      base64,
+      moment,
+      xmlJs,
+      forge,
+    ] as Def[],
     plugins: plugins,
   });
 }
