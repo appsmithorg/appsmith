34df8df15d1e007d19883e31d0c0ce36be2b411f
Aswath K <aswath@appsmith.com>
2023-02-28 14:28:04 +0530

    poc: Text widget hugs the content

--- app/client/src/widgets/TextWidget/component/index.tsx
+++ app/client/src/widgets/TextWidget/component/index.tsx
@@ -124,6 +124,9 @@ export const StyledText = styled(Text)<StyledTextProps>`
   }
   display: flex;
   width: 100%;
+  .auto-layout & {
+    width: max-content;
+  }
   justify-content: flex-start;
   flex-direction: ${(props) => (props.isTruncated ? "column" : "unset")};
   align-items: ${(props) =>
