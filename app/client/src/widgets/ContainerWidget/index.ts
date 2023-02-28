d0a726b33dde82382f67ae4a98fdcef2c14f37a5
Aswath K <aswath@appsmith.com>
2023-02-28 19:25:23 +0530

    Adds min dimension for ContainerWidget

--- app/client/src/widgets/ContainerWidget/index.ts
+++ app/client/src/widgets/ContainerWidget/index.ts
@@ -60,6 +60,19 @@ export const CONFIG = {
     responsiveBehavior: getDefaultResponsiveBehavior(Widget.getWidgetType()),
     minWidth: FILL_WIDGET_MIN_WIDTH,
   },
+  autoLayout: {
+    widgetSize: [
+      {
+        viewportMinWidth: 0,
+        configuration: () => {
+          return {
+            minWidth: "280px",
+            minHeight: "50px",
+          };
+        },
+      },
+    ],
+  },
   properties: {
     derived: Widget.getDerivedPropertiesMap(),
     default: Widget.getDefaultPropertiesMap(),
