68c10fbab9c2ebadaae5f9e049c68723ae12ebcb
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-03-01 18:29:53 +0530

    build fix

--- app/client/src/selectors/multiPaneSelectors.ts
+++ app/client/src/selectors/multiPaneSelectors.ts
@@ -3,6 +3,7 @@ import { AppState } from "ce/reducers";
 export const getTabsPaneWidth = (state: AppState) =>
   state.ui.multiPaneConfig.tabsPaneWidth;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const isMultiPaneActive = (state: AppState) => true;
 
 export const getPaneCount = (state: AppState) =>
