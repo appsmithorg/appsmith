1d0eeb687e8545a01e650fe541d05992362e7973
Nidhi Nair <nidhi@appsmith.com>
2023-03-01 17:44:29 +0530

    Added comment in migration

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration004ResetOnPageLoadEdgesInLayouts.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration004ResetOnPageLoadEdgesInLayouts.java
@@ -9,7 +9,15 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.data.mongodb.core.query.UpdateDefinition;
 
-
+/**
+ * Since an Appsmith application is not limited by the number of interconnections it can make,
+ * it is possible for users to create graphs so complex that we end up having MBs of data in a
+ * relatively simple page as well.
+ * This was causing failures in a user's app since they were now unable to update their app
+ * any further. We do not really use this property anywhere in our logic today,
+ * and this is essentially redundant information.
+ * This migration gets rid of the edges property from the layout in all existing pages.
+ */
 @ChangeUnit(order = "004", id = "reset-on-page-load-edges-in-layouts")
 public class Migration004ResetOnPageLoadEdgesInLayouts {
     private final MongoTemplate mongoTemplate;
