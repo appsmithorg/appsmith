35805862bcb4774e424d64201861b5167a39e073
Preet <preetsidhu.bits@gmail.com>
2023-03-02 15:38:29 +0530

    build fix

--- app/client/src/pages/Editor/AppPositionTypeControl.tsx
+++ app/client/src/pages/Editor/AppPositionTypeControl.tsx
@@ -1,69 +1,69 @@
-import classNames from "classnames";
-import React, { useEffect, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
+// import classNames from "classnames";
+import React from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 
-import { batchUpdateMultipleWidgetProperties } from "actions/controlActions";
-import { ReactComponent as DesktopIcon } from "assets/icons/ads/app-icons/monitor-alt.svg";
-import { ReactComponent as MultiDeviceIcon } from "assets/icons/ads/app-icons/monitor-smartphone-alt.svg";
+// import { batchUpdateMultipleWidgetProperties } from "actions/controlActions";
+// import { ReactComponent as DesktopIcon } from "assets/icons/ads/app-icons/monitor-alt.svg";
+// import { ReactComponent as MultiDeviceIcon } from "assets/icons/ads/app-icons/monitor-smartphone-alt.svg";
 import { Colors } from "constants/Colors";
-import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
-import { IconName, TooltipComponent } from "design-system-old";
+// import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
+// import { IconName } from "design-system-old";
 import {
   AppPositioningTypeConfig,
   AppPositioningTypes,
 } from "reducers/entityReducers/pageListReducer";
 import {
   getCurrentAppPositioningType,
-  isAutoLayoutEnabled,
+  // isAutoLayoutEnabled,
 } from "selectors/editorSelectors";
-import { LayoutDirection, Positioning } from "utils/autoLayout/constants";
+// import { LayoutDirection, Positioning } from "utils/autoLayout/constants";
 import { MainContainerLayoutControl } from "./MainContainerLayoutControl";
-interface ApplicationPositionTypeConfigOption {
-  name: string;
-  type: AppPositioningTypes;
-  icon?: IconName;
-}
-const IconObj: any = {
-  fluid: <MultiDeviceIcon />,
-  desktop: <DesktopIcon />,
-};
+// interface ApplicationPositionTypeConfigOption {
+//   name: string;
+//   type: AppPositioningTypes;
+//   icon?: IconName;
+// }
+// const IconObj: any = {
+//   fluid: <MultiDeviceIcon />,
+//   desktop: <DesktopIcon />,
+// };
 export const AppsmithDefaultPositionType: AppPositioningTypeConfig = {
   type: AppPositioningTypes.FIXED,
 };
 
-const AppsmithLayoutTypes: ApplicationPositionTypeConfigOption[] = [
-  {
-    name: "Fixed Layout",
-    type: AppPositioningTypes.FIXED,
-    icon: "desktop",
-  },
-  {
-    name: "Auto Layout",
-    type: AppPositioningTypes.AUTO,
-    icon: "fluid",
-  },
-];
+// const AppsmithLayoutTypes: ApplicationPositionTypeConfigOption[] = [
+//   {
+//     name: "Fixed Layout",
+//     type: AppPositioningTypes.FIXED,
+//     icon: "desktop",
+//   },
+//   {
+//     name: "Auto Layout",
+//     type: AppPositioningTypes.AUTO,
+//     icon: "fluid",
+//   },
+// ];
 
 export const Title = styled.p`
   color: ${Colors.GRAY_800};
 `;
 
 export function AppPositionTypeControl() {
-  const dispatch = useDispatch();
-  const buttonRefs: Array<HTMLButtonElement | null> = [];
+  // const dispatch = useDispatch();
+  // const buttonRefs: Array<HTMLButtonElement | null> = [];
   const selectedOption = useSelector(getCurrentAppPositioningType);
-  const isAutoLayoutActive = useSelector(isAutoLayoutEnabled);
+  // const isAutoLayoutActive = useSelector(isAutoLayoutEnabled);
   /**
    * return selected layout index. if there is no app
    * layout, use the default one ( fluid )
    */
-  const selectedIndex = useMemo(() => {
-    return AppsmithLayoutTypes.findIndex(
-      (each) =>
-        each.type === (selectedOption || AppsmithDefaultPositionType.type),
-    );
-  }, [selectedOption]);
+  // const selectedIndex = useMemo(() => {
+  //   return AppsmithLayoutTypes.findIndex(
+  //     (each) =>
+  //       each.type === (selectedOption || AppsmithDefaultPositionType.type),
+  //   );
+  // }, [selectedOption]);
 
   // const [focusedIndex, setFocusedIndex] = React.useState(selectedIndex);
 
@@ -76,28 +76,28 @@ export function AppPositionTypeControl() {
   //   }
   // }, [isAutoLayoutActive]);
 
-  const updateAppPositioningLayout = (
-    layoutOption: ApplicationPositionTypeConfigOption,
-  ) => {
-    const selectedType =
-      layoutOption.type !== AppPositioningTypes.AUTO
-        ? Positioning.Fixed
-        : Positioning.Vertical;
-    dispatch(
-      batchUpdateMultipleWidgetProperties([
-        {
-          widgetId: MAIN_CONTAINER_WIDGET_ID,
-          updates: {
-            modify: {
-              positioning: selectedType,
-              useAutoLayout: selectedType !== Positioning.Fixed,
-              direction: LayoutDirection.Vertical,
-            },
-          },
-        },
-      ]),
-    );
-  };
+  // const updateAppPositioningLayout = (
+  //   layoutOption: ApplicationPositionTypeConfigOption,
+  // ) => {
+  //   const selectedType =
+  //     layoutOption.type !== AppPositioningTypes.AUTO
+  //       ? Positioning.Fixed
+  //       : Positioning.Vertical;
+  //   dispatch(
+  //     batchUpdateMultipleWidgetProperties([
+  //       {
+  //         widgetId: MAIN_CONTAINER_WIDGET_ID,
+  //         updates: {
+  //           modify: {
+  //             positioning: selectedType,
+  //             useAutoLayout: selectedType !== Positioning.Fixed,
+  //             direction: LayoutDirection.Vertical,
+  //           },
+  //         },
+  //       },
+  //     ]),
+  //   );
+  // };
 
   // const handleKeyDown = (event: React.KeyboardEvent, index: number) => {
   //   if (!buttonRefs.length) return;
