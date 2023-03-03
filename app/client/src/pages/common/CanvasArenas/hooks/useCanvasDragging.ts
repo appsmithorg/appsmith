e2c2996bc28c1747029ec0de0253c72552efa1d7
Hetu Nandu <hetu@appsmith.com>
2023-03-03 13:43:17 +0530

    fix: Optimise Modal open performance (#21103)
    ## Description
    
    #20202 had caused a performance regression in widget selection
    specifically around opening and closing modals. It was noticed that
    focusing on widget caused the increased rendering time.
    Focusing involved:
    - scrolling a widget into view
    - flashing the widget
    
    These two functions are not really needed when a modal is selected since
    the modal already comes top and centre. Removing these two functions for
    widgets solved the rendering performance issue.
    
    Numbers:
    
    | Test Case | Before Widget Selection | With Widget Selection | Current
    PR |
    
    |--------------------------|-------------------------|-----------------------|------------|
    | Misc Open Complex Modal | 2035.65 | 2267.84 | 2101.03 |
    | Misc Open Simple Modal | 1191.92 | 1156.78 | 1082.88 |
    | Open Modal | 558.62 | 813.64 | 576.45 |
    | Misc Close Complex Modal | 294.21 | 431.76 | 278.43 |
    | Misc Close Simple Modal | 953.42 | 963.77 | 938.77 |
    | Close Modal | 384.86 | 673 | 674.61 |
    
    
    > Reverse performance regression introduced in #20202
    
    Fixes #20901
    
    ## Type of change
    
    - Bug fix (non-breaking change which fixes an issue)
    
    
    ## How Has This Been Tested?
    
    - Performance Tests
    
    ### Test Plan
    > Add Testsmith test cases links that relate to this PR
    
    ### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    
    
    ## Checklist:
    ### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    ### QA activity:
    - [ ] Test plan has been approved by relevant developers
    - [ ] Test plan has been peer reviewed by QA
    - [ ] Cypress test cases have been added and approved by either SDET or
    manual QA
    - [ ] Organized project review call with relevant stakeholders after
    Round 1/2 of QA
    - [ ] Added Test Plan Approved label after reveiwing all Cypress test

--- app/client/src/pages/common/CanvasArenas/hooks/useCanvasDragging.ts
+++ app/client/src/pages/common/CanvasArenas/hooks/useCanvasDragging.ts
@@ -574,7 +574,7 @@ export const useCanvasDragging = (
         resetCanvasState();
       }
     }
-  }, [isDragging, isResizing, blocksToDraw, snapRows, canExtend, scale]);
+  }, [isDragging, isResizing, blocksToDraw, snapRows, canExtend]);
   return {
     showCanvas: isDragging && !isResizing,
   };
