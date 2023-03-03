2f435d90e79786b55246ebfd469f7203ab3a0fb6
ChandanBalajiBP <104058110+ChandanBalajiBP@users.noreply.github.com>
2023-03-03 18:14:41 +0530

    fix: UI inconsistency in log tab (#21037)
    ## Description
    
    > This includes fixes to Ui inconsistencies in the
    Error|Respionse|header|lLog tab for the entity.
    
    
    ## Type of change
    
    > Please delete options that are not relevant.
    
    - Bug fix (non-breaking change which fixes an issue)
    
    - Manual
    - Not including cypress as we are going to change the UI in the upcoming
    phases of error handling
    
    ### Test Plan
    > Add Testsmith test cases links that relate to this PR
    
    ### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    
    
    ## Checklist:
    ### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    ### QA activity:
    - [ ] Test plan has been approved by relevant developers
    - [ ] Test plan has been peer reviewed by QA
    - [ ] Cypress test cases have been added and approved by either SDET or
    manual QA
    - [ ] Organized project review call with relevant stakeholders after
    Round 1/2 of QA
    - [ ] Added Test Plan Approved label after reveiwing all Cypress test

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Workspace/DeleteWorkspace_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Workspace/DeleteWorkspace_spec.js
@@ -3,20 +3,20 @@ import homePage from "../../../../locators/HomePage";
 import * as _ from "../../../../support/Objects/ObjectsCore";
 
 describe("Delete workspace test spec", function() {
-  let newWorkspaceName
+  let newWorkspaceName;
 
   it("1. Should delete the workspace", function() {
     cy.visit("/applications");
     cy.generateUUID().then((uid) => {
       newWorkspaceName = uid;
       _.homePage.CreateNewWorkspace(newWorkspaceName);
-        cy.wait(500);
-        cy.contains("Delete Workspace").click();
-        cy.contains("Are you sure").click();
-        cy.wait("@deleteWorkspaceApiCall").then((httpResponse) => {
-          expect(httpResponse.status).to.equal(200);
-        });
-        cy.get(newWorkspaceName).should("not.exist");
+      cy.wait(500);
+      cy.contains("Delete Workspace").click();
+      cy.contains("Are you sure").click();
+      cy.wait("@deleteWorkspaceApiCall").then((httpResponse) => {
+        expect(httpResponse.status).to.equal(200);
+      });
+      cy.get(newWorkspaceName).should("not.exist");
     });
   });
 
@@ -26,24 +26,21 @@ describe("Delete workspace test spec", function() {
     cy.generateUUID().then((uid) => {
       newWorkspaceName = uid;
       _.homePage.CreateNewWorkspace(newWorkspaceName);
-        cy.wait(500);
-        cy.contains("Delete Workspace");
-        _.homePage.InviteUserToWorkspace(
-          newWorkspaceName,
-          Cypress.env("TESTUSERNAME1"),
-          "App Viewer",
-        );
-        cy.LogOut();
-        cy.LogintoApp(
-          Cypress.env("TESTUSERNAME1"),
-          Cypress.env("TESTPASSWORD1"),
-        );
-        cy.visit("/applications");
-        cy.openWorkspaceOptionsPopup(newWorkspaceName);
-        cy.get(homePage.workspaceNamePopoverContent)
-          .contains("Delete Workspace")
-          .should("not.exist");
-        cy.LogOut();
+      cy.wait(500);
+      cy.contains("Delete Workspace");
+      _.homePage.InviteUserToWorkspace(
+        newWorkspaceName,
+        Cypress.env("TESTUSERNAME1"),
+        "App Viewer",
+      );
+      cy.LogOut();
+      cy.LogintoApp(Cypress.env("TESTUSERNAME1"), Cypress.env("TESTPASSWORD1"));
+      cy.visit("/applications");
+      cy.openWorkspaceOptionsPopup(newWorkspaceName);
+      cy.get(homePage.workspaceNamePopoverContent)
+        .contains("Delete Workspace")
+        .should("not.exist");
+      cy.LogOut();
     });
   });
 });
