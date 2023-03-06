460ba0109c141b47075f07614e7ed91ee11860a1
Hetu Nandu <hetunandu@gmail.com>
2023-03-06 12:36:52 +0530

    Fix empty state text

--- app/client/src/pages/Editor/TabsPane/EmptyState.tsx
+++ app/client/src/pages/Editor/TabsPane/EmptyState.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const TabsEmptyState = () => {
   return (
     <div className={"p-2"}>
-      <h4>Empty State</h4>
+      <h2 className="absolute top-0 left-0 right-0 flex items-end h-108 justify-center text-2xl font-bold text-gray-300">
+        Select an entity from the explorer
+      </h2>
     </div>
   );
 };
