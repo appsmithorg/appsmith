c3f986feb5beee6d71d44bc9fb3532afe930217f
Hetu Nandu <hetunandu@gmail.com>
2023-03-06 14:09:54 +0530

    Hide canvas CTAs

--- app/client/src/pages/Editor/WidgetsEditor/index.tsx
+++ app/client/src/pages/Editor/WidgetsEditor/index.tsx
@@ -26,7 +26,6 @@ import {
 import EditorContextProvider from "components/editorComponents/EditorContextProvider";
 import Guide from "../GuidedTour/Guide";
 import PropertyPaneContainer from "./PropertyPaneContainer";
-import CanvasTopSection from "./EmptyCanvasSection";
 import { useAutoHeightUIState } from "utils/hooks/autoHeightUIHooks";
 import { isMultiPaneActive } from "selectors/multiPaneSelectors";
 
@@ -104,20 +103,17 @@ function WidgetsEditor() {
         <>
           {guidedTourEnabled && <Guide />}
           <div className="relative flex flex-row w-full overflow-hidden">
-            <div className="relative flex flex-col w-full overflow-hidden">
-              <CanvasTopSection />
-              <div
-                className="relative flex flex-row w-full overflow-hidden"
-                data-testid="widgets-editor"
-                draggable
-                onClick={handleWrapperClick}
-                onDragStart={onDragStart}
-              >
-                <PageTabs />
-                <CanvasContainer />
-                <CrudInfoModal />
-                <Debugger />
-              </div>
+            <div
+              className="relative flex flex-row w-full overflow-hidden"
+              data-testid="widgets-editor"
+              draggable
+              onClick={handleWrapperClick}
+              onDragStart={onDragStart}
+            >
+              <PageTabs />
+              <CanvasContainer />
+              <CrudInfoModal />
+              <Debugger />
             </div>
             {!isMultiPane && <PropertyPaneContainer />}
           </div>
