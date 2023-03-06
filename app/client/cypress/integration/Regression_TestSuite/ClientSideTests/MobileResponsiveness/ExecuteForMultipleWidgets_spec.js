9e3aae4fc6c387c77def62282706eb96f4a2c879
Apple <nandan@thinkify.io>
2023-03-06 19:28:08 +0530

    updated another usecase

---
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/MobileResponsiveness/ExecuteForMultipleWidgets_spec.js
@@ -0,0 +1,96 @@
+const dsl = require("../../../../fixtures/mobileresponsiveness_multipleDsl.json");
+const commonlocators = require("../../../../locators/commonlocators.json");
+import { ObjectsRegistry } from "../../../../support/Objects/Registry";
+const agHelper = ObjectsRegistry.AggregateHelper;
+let cheight;
+let theight;
+let cwidth;
+let twidth;
+
+describe("Validating Mobile Views", function() {
+
+  afterEach(() => {
+    agHelper.SaveLocalStorageCache();
+  });
+
+  beforeEach(() => {
+    agHelper.RestoreLocalStorageCache();
+  });
+  it("Validate change with height width for widgets", function() {
+    cy.addDsl(dsl);
+    cy.wait(5000); //for dsl to settle
+    //cy.openPropertyPane("containerwidget");
+    cy.get(".t--entity-name:contains('Container1')").click({force: true});
+    cy.get(".t--widget-containerwidget")
+      .first()
+      .invoke("css", "height")
+      .then((height) => {
+        cy.get(".t--widget-inputwidgetv2")
+          .invoke("css", "height")
+          .then((newheight) => {
+        
+            cy.PublishtheApp();
+            cy.get(".t--widget-containerwidget")
+            .first()
+            .invoke("css", "height")
+            .then((height) => {
+              cy.get(".t--widget-inputwidgetv2")
+                .invoke("css", "height")
+                .then((newheight) => {
+                  cheight=height;
+                  theight=newheight;
+                });
+              });
+              cy.get(".t--widget-containerwidget")
+              .first()
+              .invoke("css", "width")
+              .then((width) => {
+                cy.get(".t--widget-inputwidgetv2")
+                  .invoke("css", "width")
+                  .then((newwidth) => {
+                    cwidth=width;
+                    twidth=newwidth;
+                  });
+                });
+            //expect(height).to.equal(newheight);
+          });
+      });
+  });
+
+    let phones = ["iphone-3","iphone-4", "iphone-5", "iphone-6", "iphone-6+", "iphone-7",
+    "iphone-8", "iphone-x", "samsung-note9", "samsung-s10",[1024, 768]]
+    phones.forEach((phone) => {
+        it(`${phone} port execution`, function() {
+
+            if (Cypress._.isArray(phone)) {
+                cy.viewport(phone[0], phone[1])
+              } else {
+                cy.viewport(phone)
+              }
+            cy.wait(15000);
+            cy.get(".t--widget-containerwidget")
+            .first()
+            .invoke("css", "height")
+            .then((height) => {
+              cy.get(".t--widget-inputwidgetv2")
+                .invoke("css", "height")
+                .then((newheight) => {
+                expect(cheight).to.not.equal(height);
+                expect(theight).to.equal(newheight);
+                });
+              });
+              cy.get(".t--widget-containerwidget")
+              .first()
+              .invoke("css", "width")
+              .then((width) => {
+                cy.get(".t--widget-inputwidgetv2")
+                  .first()
+                  .invoke("css", "width")
+                  .then((newwidth) => {
+                  expect(cwidth).to.not.equal(width);
+                  expect(twidth).to.not.equal(newwidth);
+                  });
+                });
+        });
+    })
+});
