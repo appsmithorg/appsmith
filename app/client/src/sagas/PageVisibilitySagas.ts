ec6b191eff049f098a8a978dced38f0a1ffed9af
Ivan Akulov <mail@iamakulov.com>
2023-03-08 19:52:19 +0100

    Bump TypeScript to 4.9.5
    This resolves typing issues in loadable() [to be introduced in the next commit]
    
    Note:
    - the triggerReducer.ts change: is the code there correct? TypeScript
      is now (rightfully) complaining that we’re spreading `...value` into
      the object, which doesn’t match the FormEvalOutput type.

--- app/client/src/sagas/PageVisibilitySagas.ts
+++ app/client/src/sagas/PageVisibilitySagas.ts
@@ -18,9 +18,9 @@ function listenToVisibilityEvents() {
 function* handleTabVisibilityConnection() {
   const channel: EventChannel<unknown> = yield call(listenToVisibilityEvents);
   while (true) {
-    const event: { target: { visibilityState: VisibilityState } } = yield take(
-      channel,
-    );
+    const event: {
+      target: { visibilityState: DocumentVisibilityState };
+    } = yield take(channel);
     // Only invoke when page gets visible
     if (event.target && event.target.visibilityState === "visible") {
       yield put(pageVisibilityAppEvent(event.target.visibilityState));
