58c6c6745dfc75061bbe2cca5e2cd2bbea3c0da0
Favour Ohans <fohanekwu@gmail.com>
2023-03-09 14:47:32 +0100

    Fix path resolve

--- app/shared/ast/src/utils.ts
+++ app/shared/ast/src/utils.ts
@@ -1,5 +1,5 @@
 import unescapeJS from "unescape-js";
-import { PropertyNode, isLiteralNode } from "..";
+import { isLiteralNode, PropertyNode } from "../index";
 
 const beginsWithLineBreakRegex = /^\s+|\s+$/;
 
