ffb59bfd5f1982c0f18d00fd2a9e26c8dd97f1e5
Dhruvik Neharia <dhruvik@appsmith.com>
2023-03-11 19:27:26 +0530

    fix: remove top header's bottom border when nav color style is set to theme

--- app/client/src/pages/AppViewer/Navigation/components/TopHeader.styled.tsx
+++ app/client/src/pages/AppViewer/Navigation/components/TopHeader.styled.tsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 import { NavigationSetting, NAVIGATION_SETTINGS } from "constants/AppConstants";
-import {
-  getMenuContainerBackgroundColor,
-  getMenuItemBackgroundColorWhenActive,
-} from "../../utils";
+import { getMenuContainerBackgroundColor } from "../../utils";
 
 export const HeaderRow = styled.div<{
   primaryColor: string;
@@ -13,20 +10,13 @@ export const HeaderRow = styled.div<{
   display: flex;
   flex-direction: row;
 
-  ${({ navColorStyle, primaryColor, theme }) => {
-    const isThemeColorStyle =
-      navColorStyle === NAVIGATION_SETTINGS.COLOR_STYLE.THEME;
+  ${({ navColorStyle, theme }) => {
+    const isLightColorStyle =
+      navColorStyle === NAVIGATION_SETTINGS.COLOR_STYLE.LIGHT;
 
-    return isThemeColorStyle
-      ? `
-        border-bottom: 1px solid ${getMenuItemBackgroundColorWhenActive(
-          primaryColor,
-          navColorStyle,
-        )};
-      `
-      : `
-        border-bottom: 1px solid ${theme.colors.header.tabsHorizontalSeparator};
-      `;
+    return isLightColorStyle
+      ? `border-bottom: 1px solid ${theme.colors.header.tabsHorizontalSeparator}`
+      : "";
   }}
 `;
 
