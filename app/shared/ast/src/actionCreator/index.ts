5b1d6e0c9e0a456c8851ec59c2bc7ee0a24e284c
arunvjn <arun@appsmith.com>
2023-03-13 13:23:52 +0530

    Remove deep clone on ast to stack overflow issues

--- app/shared/ast/src/actionCreator/index.ts
+++ app/shared/ast/src/actionCreator/index.ts
@@ -93,19 +93,17 @@ export const setTextArgumentAtPosition = (currentValue: string, changeValue: any
     try {
         // sanitize to remove unnecessary characters which might lead to invalid ast
         const changeValueScript = sanitizeScript(rawValue, evaluationVersion);
-        const changeValueAst = getAST(changeValueScript, {
+        getAST(changeValueScript, {
             locations: true,
             ranges: true,
         });
         const sanitizedScript = sanitizeScript(currentValue, evaluationVersion);
-        const __ast = getAST(sanitizedScript, {
+        ast = getAST(sanitizedScript, {
             locations: true,
             ranges: true,
             // collect all comments as they are not part of the ast, we will attach them back on line 46
             onComment: commentArray,
         });
-        // clone ast to avoid mutating original ast
-        ast = klona(__ast);
     } catch (error) {
         // if ast is invalid return original string
         throw error;
@@ -227,14 +225,12 @@ export const setObjectAtPosition = (currentValue: string, changeValue: any, argN
     try {
         // sanitize to remove unnecessary characters which might lead to invalid ast
         const sanitizedScript = sanitizeScript(currentValue, evaluationVersion);
-        const __ast = getAST(sanitizedScript, {
+        ast = getAST(sanitizedScript, {
             locations: true,
             ranges: true,
             // collect all comments as they are not part of the ast, we will attach them back on line 46
             onComment: commentArray,
         });
-        // clone ast to avoid mutating original ast
-        ast = klona(__ast);
     } catch (error) {
         // if ast is invalid throw error
         throw error;
@@ -316,14 +312,12 @@ export const setEnumArgumentAtPosition = (currentValue: string, changeValue: str
     try {
         // sanitize to remove unnecessary characters which might lead to invalid ast
         const sanitizedScript = sanitizeScript(currentValue, evaluationVersion);
-        const __ast = getAST(sanitizedScript, {
+        ast = getAST(sanitizedScript, {
             locations: true,
             ranges: true,
             // collect all comments as they are not part of the ast, we will attach them back on line 46
             onComment: commentArray,
         });
-        // clone ast to avoid mutating original ast
-        ast = klona(__ast);
     } catch (error) {
         // if ast is invalid throw error
         throw error;
@@ -335,8 +329,8 @@ export const setEnumArgumentAtPosition = (currentValue: string, changeValue: str
     const node = findRootCallExpression(astWithComments);
 
     if(node && isCallExpressionNode(node)) {
-                       // add 1 to get the starting position of the next
-                // node to ending position of previous
+        // add 1 to get the starting position of the next
+        // node to ending position of previous
         const startPosition = node.callee.end + NEXT_POSITION;
         node.arguments[argNum] = {
             type: NodeTypes.Literal,
@@ -396,14 +390,12 @@ export const setModalName = (currentValue: string, changeValue: string, evaluati
     try {
         // sanitize to remove unnecessary characters which might lead to invalid ast
         const sanitizedScript = sanitizeScript(currentValue, evaluationVersion);
-        const __ast = getAST(sanitizedScript, {
+        ast = getAST(sanitizedScript, {
             locations: true,
             ranges: true,
             // collect all comments as they are not part of the ast, we will attach them back on line 46
             onComment: commentArray,
         });
-        // clone ast to avoid mutating original ast
-        ast = klona(__ast);
     } catch (error) {
         // if ast is invalid throw error
         throw error;
