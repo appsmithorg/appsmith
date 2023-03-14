78b1ab54dfa400e7d3fc9568f02f74ff8af4f05f
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-03-14 23:42:28 +0530

    Revert "merge fix."
    This reverts commit a8ddd7850fd33685e00c4cd5bc4d9e9b56fcd55b.

--- app/client/src/pages/Editor/AppPositionTypeControl.tsx
+++ app/client/src/pages/Editor/AppPositionTypeControl.tsx
@@ -12,14 +12,12 @@ import {
   AppPositioningTypes,
 } from "reducers/entityReducers/pageListReducer";
 import {
-  getCurrentApplicationId,
   getCurrentAppPositioningType,
   isAutoLayoutEnabled,
 } from "selectors/editorSelectors";
 import { MainContainerLayoutControl } from "./MainContainerLayoutControl";
 import { updateLayoutPositioning } from "actions/autoLayoutActions";
 
-import { updateApplication } from "actions/applicationActions";
 interface ApplicationPositionTypeConfigOption {
   name: string;
   type: AppPositioningTypes;
@@ -55,7 +53,6 @@ export const AppPositionTypeControl = () => {
   const buttonRefs: Array<HTMLButtonElement | null> = [];
   const selectedOption = useSelector(getCurrentAppPositioningType);
   const isAutoLayoutActive = useSelector(isAutoLayoutEnabled);
-  const appId = useSelector(getCurrentApplicationId);
   /**
    * return selected layout index. if there is no app
    * layout, use the default one ( fluid )
@@ -82,13 +79,6 @@ export const AppPositionTypeControl = () => {
     layoutOption: ApplicationPositionTypeConfigOption,
   ) => {
     dispatch(updateLayoutPositioning(layoutOption.type));
-    dispatch(
-      updateApplication(appId || "", {
-        appPositioning: {
-          type: layoutOption.type,
-        },
-      }),
-    );
   };
 
   const handleKeyDown = (event: React.KeyboardEvent, index: number) => {
