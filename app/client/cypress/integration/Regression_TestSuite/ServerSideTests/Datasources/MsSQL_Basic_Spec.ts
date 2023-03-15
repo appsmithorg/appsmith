fb6962bfc52c2acac87999815f36218f031ec4c4
Aishwarya UR <aishwarya@appsmith.com>
2023-03-16 00:30:22 +0530

    update

--- app/client/cypress/integration/Regression_TestSuite/ServerSideTests/Datasources/MsSQL_Basic_Spec.ts
+++ app/client/cypress/integration/Regression_TestSuite/ServerSideTests/Datasources/MsSQL_Basic_Spec.ts
@@ -1,14 +1,12 @@
 import * as _ from "../../../../support/Objects/ObjectsCore";
 
-let dsName: any, query : string;
-
+let dsName: any, query: string;
 
 describe("Validate MySQL query UI flows - Bug 14054", () => {
   it("1. Create a new MySQL DS", () => {
     _.dataSources.CreateDataSource("MsSql");
     cy.get("@dsName").then(($dsName) => {
       dsName = $dsName;
-      cy.log("dsname is "+ dsName);
     });
   });
 
@@ -16,12 +14,10 @@ describe("Validate MySQL query UI flows - Bug 14054", () => {
     _.dataSources.NavigateFromActiveDS(dsName, true);
     _.agHelper.GetNClick(_.dataSources._templateMenu);
     _.agHelper.RenameWithInPane("MsSQL_queries");
-    runQueryNValidate("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE';", [
-      "TABLE_CATALOG",
-      "TABLE_SCHEMA",
-      "TABLE_NAME",
-      "TABLE_TYPE",
-    ]);
+    runQueryNValidate(
+      "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE';",
+      ["TABLE_CATALOG", "TABLE_SCHEMA", "TABLE_NAME", "TABLE_TYPE"],
+    );
     runQueryNValidate("exec sp_columns Amazon_Sales;", [
       "TABLE_QUALIFIER",
       "TABLE_OWNER",
@@ -41,25 +37,28 @@ describe("Validate MySQL query UI flows - Bug 14054", () => {
       "CHAR_OCTET_LENGTH",
       "ORDINAL_POSITION",
       "IS_NULLABLE",
-      "SS_DATA_TYPE"
+      "SS_DATA_TYPE",
     ]);
     _.agHelper.ActionContextMenuWithInPane("Delete");
   });
 
-    it("3. Run a Select query & validate response", () => {
-      query = `Select * from Simpsons;`;
-      _.entityExplorer.CreateNewDsQuery(dsName);
-      _.agHelper.RenameWithInPane("selectSimpsons");
-      _.agHelper.GetNClick(_.dataSources._templateMenu);
-      _.dataSources.EnterQuery(query);
-     _.dataSources.RunQueryNVerifyResponseViews(99);//Could be 100 in CI, to check
-     _.agHelper.ActionContextMenuWithInPane("Delete");
-    });
-
+  it("3. Run a Select query & validate response", () => {
+    query = `Select * from Simpsons;`;
+    _.entityExplorer.CreateNewDsQuery(dsName);
+    _.agHelper.RenameWithInPane("selectSimpsons");
+    _.agHelper.GetNClick(_.dataSources._templateMenu);
+    _.dataSources.EnterQuery(query);
+    _.dataSources.RunQueryNVerifyResponseViews(99); //Could be 100 in CI, to check
+    _.agHelper.ActionContextMenuWithInPane("Delete");
+  });
 
   after("Verify Deletion of the datasource", () => {
     _.entityExplorer.SelectEntityByName(dsName, "Datasources");
-    _.entityExplorer.ActionContextMenuByEntityName(dsName, "Delete", "Are you sure?");
+    _.entityExplorer.ActionContextMenuByEntityName(
+      dsName,
+      "Delete",
+      "Are you sure?",
+    );
     _.agHelper.ValidateNetworkStatus("@deleteDatasource", 200);
   });
 
