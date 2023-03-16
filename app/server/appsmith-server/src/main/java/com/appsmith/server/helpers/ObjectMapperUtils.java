bbbf9d0ac8ac7b68a238a7cda1cb59c21ea05f94
Nayan <nayan@appsmith.com>
2023-03-16 11:20:46 +0600

    fixed method in objectmapperutils

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ObjectMapperUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ObjectMapperUtils.java
@@ -1,18 +1,12 @@
 package com.appsmith.server.helpers;
 
-import com.appsmith.external.views.Views;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
 
-public class ObjectMapperUtils {
-    private final ObjectMapper objectMapper;
-
-    public ObjectMapperUtils(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+public record ObjectMapperUtils(ObjectMapper objectMapper) {
 
     public <T> T readFromString(String src, Class viewsClass, Class<T> resultClass) throws IOException {
         return objectMapper
@@ -28,7 +22,7 @@ public class ObjectMapperUtils {
 
     public String writeAsString(Object src, Class viewsClass) throws JsonProcessingException {
         return objectMapper
-                .writerWithView(Views.Public.class)
+                .writerWithView(viewsClass)
                 .writeValueAsString(src);
     }
 }
