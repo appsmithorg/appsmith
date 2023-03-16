d54640e84f512db4a3e7e0b0aaef21fa7830b9b4
Anagh Hegde <anagh@appsmith.com>
2023-03-16 16:36:47 +0530

    revert

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Git/cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
