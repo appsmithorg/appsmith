1d951f153402b314c7e86282782bebed5b84b105
arunvjn <arun@appsmith.com>
2023-03-21 10:32:49 +0530

    removes invalid test case

--- app/shared/ast/src/actionCreator/index.test.ts
+++ app/shared/ast/src/actionCreator/index.test.ts
@@ -48,14 +48,6 @@ describe("getFuncExpressionAtPosition", () => {
     expect(result).toEqual("location => {}");
   })
 
-  it("should return the arguments for function without properties", () => {
-    const value = "showAlert('Hello');"
-
-    const result = getFuncExpressionAtPosition(value, 0, 2);
-
-    expect(result).toEqual("'Hello'");
-  })
-
   it("should return the callback function for setInterval", () => {
     const value = "setInterval(() => { console.log('hello'); }, 1000);";
 
