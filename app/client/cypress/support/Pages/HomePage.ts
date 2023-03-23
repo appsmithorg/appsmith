fde7cd03e200fd62544897289edb6644f5024dc0
Vijetha-Kaja <vijetha@appsmith.com>
2023-03-23 20:55:05 +0530

    bug-21191 automation

--- app/client/cypress/support/Pages/HomePage.ts
+++ app/client/cypress/support/Pages/HomePage.ts
@@ -40,6 +40,7 @@ export class HomePage {
 
   private _manageUsers = ".manageUsers";
   private _appHome = "//a[@href='/applications']";
+  public _closeBtn=".bp3-dialog-close-button";
   _applicationCard = ".t--application-card";
   private _homeIcon = ".t--appsmith-logo";
   private _homeAppsmithImage = "a.t--appsmith-logo";
