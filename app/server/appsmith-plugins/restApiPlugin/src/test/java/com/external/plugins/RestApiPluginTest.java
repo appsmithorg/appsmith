1e33237bfee906342af07579c76ae837f3fabb46
PiyushPushkar02 <118709669+PiyushPushkar02@users.noreply.github.com>
2023-03-23 13:29:46 +0530

    review comment fix

--- app/server/appsmith-plugins/restApiPlugin/src/test/java/com/external/plugins/RestApiPluginTest.java
+++ app/server/appsmith-plugins/restApiPlugin/src/test/java/com/external/plugins/RestApiPluginTest.java
@@ -139,7 +139,7 @@ public class RestApiPluginTest {
     public void testValidApiExecutionWithWhitespacesInUrl() {
         DatasourceConfiguration dsConfig = new DatasourceConfiguration();
         //added whitespaces in url to validate successful execution of the same
-        dsConfig.setUrl("    https://postman-echo.com/post");
+        dsConfig.setUrl("    https://postman-echo.com/post       ");
 
         ActionConfiguration actionConfig = new ActionConfiguration();
         final List<Property> headers = List.of(new Property("content-type", "application/json"));
@@ -153,18 +153,6 @@ public class RestApiPluginTest {
                 .assertNext(result -> {
                     assertTrue(result.getIsExecutionSuccess());
                     assertNotNull(result.getBody());
-                    JsonNode data = ((ObjectNode) result.getBody()).get("data");
-                    assertEquals(requestBody, data.toString());
-                    final ActionExecutionRequest request = result.getRequest();
-                    assertEquals("https://postman-echo.com/post", request.getUrl());
-                    assertEquals(HttpMethod.POST, request.getHttpMethod());
-                    assertEquals(requestBody, request.getBody().toString());
-                    final Iterator<Map.Entry<String, JsonNode>> fields = ((ObjectNode) result.getRequest().getHeaders()).fields();
-                    fields.forEachRemaining(field -> {
-                        if (HttpHeaders.CONTENT_TYPE.equalsIgnoreCase(field.getKey())) {
-                            assertEquals("application/json", field.getValue().get(0).asText());
-                        }
-                    });
                 })
                 .verifyComplete();
     }
