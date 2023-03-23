2698c52a0ae4fdb19a5817fd236be9c0f1a7891e
Pawan Kumar <pawan@appsmith.com>
2023-03-23 10:11:40 +0530

    fix chart spec

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/Chart/Chart_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/Chart/Chart_spec.js
@@ -315,19 +315,19 @@ describe("Chart Widget Functionality", function () {
     cy.get(".fusioncharts-container").should(
       "have.css",
       "font-family",
-      "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Open Sans,Helvetica Neue,Icons16,sans-serif",
+      '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",Icons16,sans-serif',
     );
 
     cy.UpdateChartType("Pie Chart");
     cy.get("g[class$='item'] text").should(
       "have.css",
       "font-family",
-      "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Open Sans,Helvetica Neue,Icons16,sans-serif",
+      '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",Icons16,sans-serif',
     );
     cy.get("g[class$='labels'] text").should(
       "have.css",
       "font-family",
-      "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Open Sans,Helvetica Neue,Icons16,sans-serif",
+      '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",Icons16,sans-serif',
     );
   });
 
