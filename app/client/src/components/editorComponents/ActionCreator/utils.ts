ce3921c9ab84e17b7513e189afcaa295aba4234a
arunvjn <arun@appsmith.com>
2023-03-24 20:35:46 +0530

    Fixed JS arguments bug

--- app/client/src/components/editorComponents/ActionCreator/utils.ts
+++ app/client/src/components/editorComponents/ActionCreator/utils.ts
@@ -407,8 +407,8 @@ export function actionToCode(
   }
   /**
    * Unfortunately, we have to do this because the integration action could be represented with success and error callbacks
-   * and then/catch blocks. We need to check if the action is an integration action and if it had a success or error callback
-   * defined already, to preserve the positions of params object which should first param when using then/catch and 3rd param when using
+   * or then/catch blocks. We need to check if the action is an integration action and if it had a success or error callback
+   * defined already to preserve the positions of params object which should first param when using then/catch and 3rd param when using
    * callbacks.
    */
   const supportsCallback = actionType === AppsmithFunction.integration;
