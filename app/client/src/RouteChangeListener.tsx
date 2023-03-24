a7a4165a65dfc8970645ac8ea8a7d3ffcd2b9c4f
Hetu Nandu <hetunandu@gmail.com>
2023-03-24 13:46:58 +0530

    Test case fix

--- app/client/src/RouteChangeListener.tsx
+++ app/client/src/RouteChangeListener.tsx
@@ -1,23 +1,20 @@
 import { routeChanged } from "actions/focusHistoryActions";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import history from "utils/history";
 import type { AppsmithLocationState } from "utils/history";
+import history from "utils/history";
 import type { LocationListener } from "history";
 
 export default function RouteChangeListener() {
   const dispatch = useDispatch();
+  const handler: LocationListener<AppsmithLocationState> = (
+    location,
+    action,
+  ) => {
+    dispatch(routeChanged(location, action));
+  };
   useEffect(() => {
-    const handler: LocationListener<AppsmithLocationState> = (
-      location,
-      action,
-    ) => {
-      if (window.location.pathname !== location.pathname) {
-        dispatch(routeChanged(location, action));
-      }
-    };
-    const historyUnregister = history.listen(handler);
-    return () => historyUnregister();
+    return history.listen(handler);
   }, []);
   return null;
 }
