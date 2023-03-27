098fc8a211b219b257806faa26834f49dda1a113
Druthi Polisetty <druthi@appsmith.com>
2023-03-27 10:19:39 +0530

    Missing configTree

--- app/client/src/workers/Evaluation/fns/mock.ts
+++ app/client/src/workers/Evaluation/fns/mock.ts
@@ -1,6 +1,7 @@
 import { ENTITY_TYPE } from "design-system-old";
-import type { DataTree } from "entities/DataTree/dataTreeFactory";
+import type { DataTree, ConfigTree } from "entities/DataTree/dataTreeFactory";
 import { createEvaluationContext } from "workers/Evaluation/evaluate";
+import { PluginType } from "entities/Action";
 
 const dataTree: DataTree = {
   action1: {
@@ -13,6 +14,21 @@ const dataTree: DataTree = {
     clear: {},
     responseMeta: { isExecutionSuccess: false },
     ENTITY_TYPE: ENTITY_TYPE.ACTION,
+  },
+};
+
+const configTree: ConfigTree = {
+  action1: {
+    pluginId: "",
+    actionId: "123",
+    pluginType: PluginType.API,
+    dynamicBindingPathList: [],
+    name: "action1",
+    bindingPaths: {},
+    reactivePaths: {},
+    ENTITY_TYPE: ENTITY_TYPE.ACTION,
+    dependencyMap: {},
+    logBlackList: {},
   },
 };
 
