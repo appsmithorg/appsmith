1e5ce141e4161c624f7f93da0fa136972e0f5c52
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-03-27 23:00:06 +0530

    login failure spec

---
+++ app/client/cypress/integration/Regression_TestSuite/ServerSideTests/LoginTests/LoginFailure_spec.js
@@ -0,0 +1,26 @@
+import * as _ from "../../../../support/Objects/ObjectsCore";
+const loginPage = require("../../../../locators/LoginPage.json");
+
+describe("Login failure", function () {
+  it("preserves redirectUrl param on login failure", function () {
+    let appUrl;
+    _.deployMode.DeployApp();
+    cy.location()
+      .then((location) => {
+        cy.LogOutUser();
+        appUrl = location.href;
+        cy.visit(appUrl);
+        cy.get(loginPage.username).should("be.visible");
+      })
+      .then(() => cy.GetUrlQueryParams())
+      .then((queryParams) => {
+        expect(decodeURIComponent(queryParams.redirectUrl)).to.eq(appUrl);
+        cy.LoginUser("user@error.com", "pwd_error");
+      })
+      .then(() => cy.GetUrlQueryParams())
+      .then((queryParams) => {
+        expect(decodeURIComponent(queryParams.error)).to.eq("true");
+        expect(decodeURIComponent(queryParams.redirectUrl)).to.eq(appUrl);
+      });
+  });
+});
