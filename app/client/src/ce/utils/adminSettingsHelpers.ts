dc435687589f0a4cc51450328aa229008936d70e
Albin <albin@appsmith.com>
2023-03-30 01:16:03 +0530

    Revert: mistake commit of admin flag

--- app/client/src/ce/utils/adminSettingsHelpers.ts
+++ app/client/src/ce/utils/adminSettingsHelpers.ts
@@ -38,7 +38,6 @@ export const getDefaultAdminSettingsPath = (
 };
 
 export const showAdminSettings = (user?: User): boolean => {
-  return true;
   return (user?.isSuperUser && user?.isConfigurable) || false;
 };
 
