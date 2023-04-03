313e107d2b6481e1f202bb61eb5f74cc6d45655f
arunvjn <arun@appsmith.com>
2023-04-03 09:12:16 +0530

    Revert "Beginning of good times"
    This reverts commit 748f643cadcc93e0bbfa47b57848bf0c50636ed0.

--- app/client/src/lib/dependencyMap/graph/index.ts
+++
@@ -1,8 +0,0 @@
-import type { TDependencyGenerator } from "../generators";
-
-export default class Graph {
-  private graph: Record<string, string[]>;
-  constructor(target: unknown, generator: TDependencyGenerator) {
-    this.graph = generator.generate(target);
-  }
-}
