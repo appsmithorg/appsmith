3789a24791df70ac3cb9b3c1c05b16eb2397a68c
Abhijeet <abhijeet@appsmith.com>
2023-04-05 14:17:54 +0530

    Revert "Refactor annotations for Plugin domain class"
    This reverts commit 2a4969f459046ec5216e5f64d9067915cbedb2d9.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Plugin.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/Plugin.java
@@ -1,17 +1,7 @@
 package com.appsmith.server.domains;
 
 import com.appsmith.server.domains.ce.PluginCE;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
-@Document
 public class Plugin extends PluginCE {
 
 }
