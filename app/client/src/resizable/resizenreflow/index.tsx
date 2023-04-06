5fc995cea6838d5c0b2ad59bb9ae58292a8f43c8
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-04-06 19:23:24 +0530

    Revert "fix: outline wasnt rendering."
    This reverts commit 2d5114c1f067aaede27eaee35fe7b6210cf05461.

--- app/client/src/resizable/resizenreflow/index.tsx
+++ app/client/src/resizable/resizenreflow/index.tsx
@@ -446,7 +446,6 @@ export function ReflowResizable(props: ResizableProps) {
           className={props.className}
           id={`resize-${props.widgetId}`}
           ref={resizableRef}
-          showBoundaries={props.showResizeBoundary}
           style={{ ..._props, ...resizeWrapperStyle }}
         >
           {props.children}
