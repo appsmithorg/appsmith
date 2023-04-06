92cbf2c45d728d34bcc260a353d3b38e12f782e4
Preet <preetsidhu.bits@gmail.com>
2023-04-06 15:37:19 -0400

    static outline color

--- app/client/src/resizable/common.tsx
+++ app/client/src/resizable/common.tsx
@@ -31,8 +31,7 @@ export const ResizeWrapper = styled(animated.div)<{
   outline-offset: 1px;
   outline: 1px solid !important;
   will-change: outline-color;
-  outline-color: ${({ showBoundaries }) =>
-    showBoundaries ? Colors.GREY_1 : "transparent"} !important;
+  outline-color: ${Colors.GREY_1} !important;
   & {
     * {
       pointer-events: ${(props) => !props.$prevents && "none"};
