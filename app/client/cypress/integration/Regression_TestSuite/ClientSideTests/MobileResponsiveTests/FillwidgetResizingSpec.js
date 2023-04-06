dfd2301c6b6d1e7b6e2c074a95522a35f38b7725
Preet <preetsidhu.bits@gmail.com>
2023-04-06 12:01:16 -0400

    fix prettier issues

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/MobileResponsiveTests/FillwidgetResizingSpec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/MobileResponsiveTests/FillwidgetResizingSpec.js
@@ -5,53 +5,61 @@ let theight;
 let twidth;
 
 describe("Validating Mobile Views", function () {
+  afterEach(() => {
+    agHelper.SaveLocalStorageCache();
+  });
 
-    afterEach(() => {
-        agHelper.SaveLocalStorageCache();
-    });
+  beforeEach(() => {
+    agHelper.RestoreLocalStorageCache();
+  });
+  it("Validate change with height width for widgets", function () {
+    cy.addDsl(dsl);
+    cy.wait(5000); //for dsl to settle
+    //cy.openPropertyPane("containerwidget");
+    cy.PublishtheApp();
+    cy.wait(2000);
+    cy.get(".t--widget-inputwidgetv2")
+      .invoke("css", "height")
+      .then((newheight) => {
+        theight = newheight;
+      });
+    cy.get(".t--widget-inputwidgetv2")
+      .invoke("css", "width")
+      .then((newwidth) => {
+        twidth = newwidth;
+      });
+  });
 
-    beforeEach(() => {
-        agHelper.RestoreLocalStorageCache();
-    });
-    it("Validate change with height width for widgets", function () {
-        cy.addDsl(dsl);
-        cy.wait(5000); //for dsl to settle
-        //cy.openPropertyPane("containerwidget");
-        cy.PublishtheApp();
-        cy.wait(2000);
-        cy.get(".t--widget-inputwidgetv2")
-            .invoke("css", "height")
-            .then((newheight) => {
-                theight = newheight;
-            });
-        cy.get(".t--widget-inputwidgetv2")
-            .invoke("css", "width")
-            .then((newwidth) => {
-                twidth = newwidth;
-            });
-    })
-
-    let phones = ["iphone-3", "iphone-4", "iphone-5", "iphone-6", "iphone-6+", "iphone-7",
-        "iphone-8", "iphone-x", "samsung-note9", "samsung-s10"]
-    phones.forEach((phone) => {
-        it(`${phone} port execution`, function () {
-
-            if (Cypress._.isArray(phone)) {
-                cy.viewport(phone[0], phone[1])
-            } else {
-                cy.viewport(phone)
-            }
-            cy.wait(2000);
-            cy.get(".t--widget-inputwidgetv2")
-                .invoke("css", "height")
-                .then((newheight) => {
-                    expect(theight).to.not.equal(newheight);
-                });
-            cy.get(".t--widget-inputwidgetv2")
-                .invoke("css", "width")
-                .then((newwidth) => {
-                    expect(twidth).to.not.equal(newwidth);
-                });
+  let phones = [
+    "iphone-3",
+    "iphone-4",
+    "iphone-5",
+    "iphone-6",
+    "iphone-6+",
+    "iphone-7",
+    "iphone-8",
+    "iphone-x",
+    "samsung-note9",
+    "samsung-s10",
+  ];
+  phones.forEach((phone) => {
+    it(`${phone} port execution`, function () {
+      if (Cypress._.isArray(phone)) {
+        cy.viewport(phone[0], phone[1]);
+      } else {
+        cy.viewport(phone);
+      }
+      cy.wait(2000);
+      cy.get(".t--widget-inputwidgetv2")
+        .invoke("css", "height")
+        .then((newheight) => {
+          expect(theight).to.not.equal(newheight);
+        });
+      cy.get(".t--widget-inputwidgetv2")
+        .invoke("css", "width")
+        .then((newwidth) => {
+          expect(twidth).to.not.equal(newwidth);
         });
     });
-})
+  });
+});
