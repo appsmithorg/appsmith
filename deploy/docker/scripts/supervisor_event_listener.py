1b51729054543e6427ae8508b9b1678808cefe50
Sumesh <sumesh@appsmith.com>
2023-04-11 16:24:21 +0530

    refactor indent

--- deploy/docker/scripts/supervisor_event_listener.py
+++ deploy/docker/scripts/supervisor_event_listener.py
@@ -1,31 +1,39 @@
-import sys, shutil, requests, time, os
+import os
+import requests
+import sys
+import shutil
+import time
 
 LOADING_TEMPLATE_PAGE = r'/opt/appsmith/templates/appsmith_starting.html'
 LOADING_PAGE_EDITOR = r'/opt/appsmith/editor/loading.html'
 BACKEND_HEALTH_ENDPOINT = "http://localhost/api/v1/health"
-LOADING_PAGE_TIMEOUT_IN_SEC = 45
 
-def write_stdout(*args):
+def write_stdout(s):
     # only eventlistener protocol messages may be sent to stdout
-    print(*args, flush=True)
+    sys.stdout.write(s)
+    sys.stdout.flush()
 
-def write_stderr(*args):
-    print(*args, flush=True, file=sys.stderr)
+def write_stderr(s):
+    sys.stderr.write(s)
+    sys.stderr.flush()
 
-def wait_until_backend_healthy(timeout):
-    for _ in range(3):
+def wait_until_backend_healthy():
+    sleep_sec = 3
+    timeout_sec = 45
+    for _ in range(timeout_sec//sleep_sec):
         if requests.get(BACKEND_HEALTH_ENDPOINT).ok:
             write_stderr('\nBackend is healthy\n')
             break
-        time.sleep(3)
+        time.sleep(sleep_sec)
+
     else:
-        write_stderr('\nBackend  health timeout\n')
+        write_stderr('\nBackend health check timed out\n')
 
     remove_loading_page()
 
 def remove_loading_page():
-   if os.path.exists(LOADING_PAGE_EDITOR):
-      os.remove(LOADING_PAGE_EDITOR)
+    if os.path.exists(LOADING_PAGE_EDITOR):
+        os.remove(LOADING_PAGE_EDITOR)
 
 def main():
     while 1:
@@ -40,24 +48,24 @@ def main():
         headers = dict([ x.split(':') for x in line.split() ])
         data = sys.stdin.read(int(headers['len']))
 
-        if (line.__contains__('PROCESS_STATE_STARTING')):
-          data_params = dict([ x.split(':') for x in data.split()])
-          if data_params['groupname'] == 'backend':
-            write_stderr('\nBackend State: STARTING\n')
-            shutil.copyfile(LOADING_TEMPLATE_PAGE, LOADING_PAGE_EDITOR)
+        if line.__contains__('PROCESS_STATE_STARTING'):
+            data_params = dict([ x.split(':') for x in data.split()])
+            if data_params['groupname'] == 'backend':
+                write_stderr('\nBackend State: STARTING\n')
+                shutil.copyfile(LOADING_TEMPLATE_PAGE, LOADING_PAGE_EDITOR)
 
-        elif (line.__contains__('PROCESS_STATE_RUNNING')):
-          data_params = dict([ x.split(':') for x in data.split()])
-          if data_params['groupname'] == 'backend':
-            write_stderr('\nBackend State: RUNNING\n')
-            wait_until_backend_healthy(timeout=LOADING_PAGE_TIMEOUT_IN_SEC)
-            write_stderr(data)
+        elif line.__contains__('PROCESS_STATE_RUNNING'):
+            data_params = dict([ x.split(':') for x in data.split()])
+            if data_params['groupname'] == 'backend':
+                write_stderr('\nBackend State: RUNNING\n')
+                wait_until_backend_healthy()
+                write_stderr(data)
 
-        elif (line.__contains__('PROCESS_STATE_FATAL')):
-          data_params = dict([ x.split(':') for x in data.split()])
-          if data_params['groupname'] == 'backend':
-            write_stderr('\nBackend State: FATAL\n')
-            remove_loading_page()
+        elif line.__contains__('PROCESS_STATE_FATAL'):
+            data_params = dict([ x.split(':') for x in data.split()])
+            if data_params['groupname'] == 'backend':
+                write_stderr('\nBackend State: FATAL\n')
+                remove_loading_page()
 
         # transition from READY to ACKNOWLEDGED
         write_stdout('RESULT 2\nOK')
