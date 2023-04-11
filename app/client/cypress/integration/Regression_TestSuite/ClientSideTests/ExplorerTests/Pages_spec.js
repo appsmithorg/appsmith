40d3a9047149a8992ac22e5b9a5a9cb0a9745c78
Apple <nandan@thinkify.io>
2023-04-11 15:12:29 +0530

    updated one of the tests

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/ExplorerTests/Pages_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/ExplorerTests/Pages_spec.js
@@ -33,8 +33,8 @@ describe("Pages", function () {
   it("3. Check for Refrsh page and validate and 404 is showing correct route", () => {
     //Automated as part Bug19654
     cy.get(publish.backToEditor).click();
-    cy.get(".t--page-switch-tab").contains("Page1").click({ force: true });
     cy.reload();
+    _.entityExplorer.SelectEntityByName("Page1 Copy", "Pages");
     //Checks if 404 is showing correct route
     cy.visit("/route-that-does-not-exist");
     cy.get(locators.errorPageTitle).should(($x) => {
