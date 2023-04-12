ab794dab1f0871c41f996eb8a191df8a775d9fa5
Hetu Nandu <hetunandu@gmail.com>
2023-04-12 13:31:05 +0530

    Changes in bottom bar icons

--- app/client/src/components/editorComponents/Debugger/index.tsx
+++ app/client/src/components/editorComponents/Debugger/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import styled from "styled-components";
 import DebuggerTabs from "./DebuggerTabs";
 import type { AppState } from "@appsmith/reducers";
 import {
@@ -14,11 +13,8 @@ import {
   getMessageCount,
   hideDebuggerIconSelector,
 } from "selectors/debuggerSelectors";
-import { getTypographyByKey, TooltipComponent } from "design-system-old";
 import { DEBUGGER_TAB_KEYS } from "./helpers";
-import { BottomBarCTAStyles } from "pages/Editor/BottomBar/styles";
-import { Button } from "design-system";
-import { Colors } from "constants/Colors";
+import { Button, Tooltip } from "design-system";
 
 function Debugger() {
   const showDebugger = useSelector(
@@ -27,57 +23,6 @@ function Debugger() {
 
   return showDebugger ? <DebuggerTabs /> : null;
 }
-
-const TriggerContainer = styled.div<{
-  errorCount: number;
-  warningCount: number;
-}>`
-  position: relative;
-  overflow: visible;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 4px;
-  padding: 9px 16px;
-  border-left: 1px solid ${Colors.GRAY_200};
-  cursor: pointer;
-  ${BottomBarCTAStyles}
-
-  .debugger-count {
-    color: ${(props) =>
-      props.errorCount
-        ? props.theme.colors.debugger.floatingButton.errorCount
-        : Colors.GRAY_700};
-    ${getTypographyByKey("btnSmall")}
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 14px;
-    -webkit-user-select: none; /* Safari */
-    -ms-user-select: none; /* IE 10 and IE 11 */
-    user-select: none; /* Standard syntax */
-  }
-`;
-
-export const ResizerMainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: calc(100% - 50px);
-  overflow: hidden;
-  gap: 10px;
-  .db-form-resizer-content {
-    flex-direction: column;
-  }
-`;
-
-export const ResizerContentContainer = styled.div`
-  overflow: auto;
-  flex: 1;
-  position: relative;
-  display: flex;
-`;
 
 export function DebuggerTrigger() {
   const dispatch = useDispatch();
@@ -90,7 +35,7 @@ export function DebuggerTrigger() {
   dispatch(setErrorCount(totalMessageCount));
 
   const onClick = (e: any) => {
-    //Removed canavs condition
+    //Removed canvas condition
     //Because we want to show debugger in all pages.
     //Updated in PR #21753 and commit id ee87fa2
     dispatch(showDebuggerAction(!showDebugger));
@@ -113,30 +58,18 @@ export function DebuggerTrigger() {
   if (hideDebuggerIcon) return null;
 
   return (
-    <TooltipComponent
-      content={tooltipContent}
-      modifiers={{
-        preventOverflow: { enabled: true },
-      }}
-    >
-      <TriggerContainer
-        className="t--debugger"
-        errorCount={messageCounters.errors}
+    <Tooltip content={tooltipContent}>
+      <Button
+        className="t--debugger-count"
+        color={"red"}
+        kind="tertiary"
         onClick={onClick}
-        warningCount={messageCounters.warnings}
+        size="md"
+        startIcon={totalMessageCount ? "close-circle" : "close-circle-line"}
       >
-        <Button
-          isIconButton
-          kind="tertiary"
-          onClick={onClick}
-          size="md"
-          startIcon={totalMessageCount ? "close-circle" : "close-circle-line"}
-        />
-        <div className="debugger-count t--debugger-count">
-          {totalMessageCount > 99 ? "99+" : totalMessageCount}
-        </div>
-      </TriggerContainer>
-    </TooltipComponent>
+        {totalMessageCount > 99 ? "99+" : totalMessageCount}
+      </Button>
+    </Tooltip>
   );
 }
 
