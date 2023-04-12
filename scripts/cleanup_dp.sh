734ec13ef9eb54e463e5f5b68dcaaa5c8174d6fd
Goutham Pratapa <goutham@appsmith.com>
2023-04-12 09:56:59 +0530

    Update cleanup_dp.sh

--- scripts/cleanup_dp.sh
+++ scripts/cleanup_dp.sh
@@ -22,7 +22,7 @@ export AWS_ACCESS_KEY_ID="$(echo "$sts_output" | jq -r '.Credentials.AccessKeyId
 export AWS_SECRET_ACCESS_KEY="$(echo "$sts_output" | jq -r '.Credentials.SecretAccessKey')"
 export AWS_SESSION_TOKEN="$(echo "$sts_output" | jq -r '.Credentials.SessionToken')"
 
-aws eks update-kubeconfig --name "$cluster_name" --role-arn "$AWS_ROLE_ARN"
+aws eks update-kubeconfig --region $region --name $cluster_name --profile eksci
 
 ### Get list of helm charts
 deployed_charts="$(helm ls -A --filter 'ce[0-9]+' --output json | jq -r '.[].namespace')"
