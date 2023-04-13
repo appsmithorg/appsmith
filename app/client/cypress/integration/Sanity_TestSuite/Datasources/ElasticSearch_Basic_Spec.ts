3ca7af9e9080c84e186e624fbb0db650a1e855d6
Vijetha-Kaja <vijetha@appsmith.com>
2023-04-13 15:24:09 +0530

    reverted previous changes and moved specs to sanity

--- app/client/cypress/integration/Sanity_TestSuite/Datasources/ElasticSearch_Basic_Spec.ts
+++ app/client/cypress/integration/Sanity_TestSuite/Datasources/ElasticSearch_Basic_Spec.ts
@@ -1,4 +1,4 @@
-import * as _ from "../../../../support/Objects/ObjectsCore";
+import * as _ from "../../../support/Objects/ObjectsCore";
 
 let dsName: any, books: any;
 describe("Validate Elasticsearch DS", () => {
