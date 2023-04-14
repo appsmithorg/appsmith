a54216f07b1e29521661ce3c1e6190ca8f76f508
Vemparala Surya Vamsi <vamsi@appsmith.com>
2023-04-14 13:40:16 +0530

    action saga updates...the action can be triggered from another orchestrator saga and will generate the actions pertaining to the form config

--- app/client/src/WidgetQueryGenerators/MongoDB.ts
+++ app/client/src/WidgetQueryGenerators/MongoDB.ts
@@ -1,4 +1,4 @@
-import { isEmpty } from "lodash";
+import { isEmpty, merge } from "lodash";
 import { BaseQueryGenerator } from "./BaseQueryGenerator";
 import type { CombinedConfig } from "./types";
 
@@ -7,11 +7,13 @@ enum COMMAND_TYPES {
   "INSERT" = "INSERT",
   "UPDATE" = "UPDATE",
 }
-export default class MongoDB extends BaseQueryGenerator {
-  private buildBasicConig(command: COMMAND_TYPES, tableName: string) {
+const ALLOWED_INITAL_VALUE_KEYS = ["aggregate", "smartSubstitution"];
+
+export default abstract class MongoDB extends BaseQueryGenerator {
+  private static buildBasicConfig(command: COMMAND_TYPES, tableName: string) {
     return { command: { data: command }, collection: { data: tableName } };
   }
-  private buildSelect(combinedConfig: CombinedConfig) {
+  private static buildSelect(combinedConfig: CombinedConfig) {
     const { config, select } = combinedConfig;
 
     if (!select) return;
@@ -39,46 +41,108 @@ export default class MongoDB extends BaseQueryGenerator {
     if (isEmpty(buildCommand)) return;
 
     return {
-      find: buildCommand,
-      ...this.buildBasicConig(COMMAND_TYPES.FIND, config.tableName),
+      actionTitle: "Find_query",
+      actionPayload: {
+        find: buildCommand,
+        ...this.buildBasicConfig(COMMAND_TYPES.FIND, config.tableName),
+      },
     };
   }
-  private buildUpdate(combinedConfig: CombinedConfig) {
+  private static buildUpdate(combinedConfig: CombinedConfig) {
     const { config, insert } = combinedConfig;
 
     if (!insert || !insert.where) return;
 
     return {
-      updateMany: {
-        query: { data: insert.where },
-        update: { data: insert.value },
+      actionTitle: "Update_query",
+      actionPayload: {
+        updateMany: {
+          query: { data: insert.where },
+          update: { data: insert.value },
+        },
+        ...this.buildBasicConfig(COMMAND_TYPES.UPDATE, config.tableName),
       },
-      ...this.buildBasicConig(COMMAND_TYPES.UPDATE, config.tableName),
     };
   }
-  private buildInsert(combinedConfig: CombinedConfig) {
+  private static buildInsert(combinedConfig: CombinedConfig) {
     const { config, create } = combinedConfig;
 
     if (!create || !create.value) return;
 
     return {
-      insert: {
-        documents: { data: create.value },
+      actionTitle: "Insert_query",
+      actionPayload: {
+        insert: {
+          documents: { data: create.value },
+        },
+        ...this.buildBasicConfig(COMMAND_TYPES.INSERT, config.tableName),
       },
-      ...this.buildBasicConig(COMMAND_TYPES.INSERT, config.tableName),
     };
   }
 
-  build(combinedConfig: CombinedConfig) {
+  private static createPayload(
+    formDataInitialValues: Record<string, any>,
+    commandKey: string,
+    builtValues: Record<string, any> | undefined,
+  ) {
+    if (!builtValues || isEmpty(builtValues)) {
+      return;
+    }
+    // if no initial values return payload with builtin values
+    if (!formDataInitialValues || isEmpty(formDataInitialValues)) {
+      return builtValues;
+    }
+
+    const scrubedOutInitalValues = [...ALLOWED_INITAL_VALUE_KEYS, commandKey]
+      .filter((key) => formDataInitialValues[key])
+      .reduce((acc, key) => {
+        acc[key] = formDataInitialValues[key];
+        return acc;
+      }, {} as Record<string, any>);
+
+    const { actionPayload, actionTitle } = builtValues;
+    return {
+      actionPayload: {
+        formData: merge({}, scrubedOutInitalValues, actionPayload),
+      },
+      actionTitle,
+    };
+  }
+  static build(
+    combinedConfig: CombinedConfig,
+    pluginInitalValues: { actionConfiguration: any },
+  ) {
     const allBuildConfigs = [];
+
+    const formDataInitialValues =
+      pluginInitalValues?.actionConfiguration?.formData;
+
     if (combinedConfig.select) {
-      allBuildConfigs.push(this.buildSelect(combinedConfig));
+      allBuildConfigs.push(
+        this.createPayload(
+          formDataInitialValues,
+          "find",
+          this.buildSelect(combinedConfig),
+        ),
+      );
     }
     if (combinedConfig.insert) {
-      allBuildConfigs.push(this.buildUpdate(combinedConfig));
+      allBuildConfigs.push(
+        this.createPayload(
+          formDataInitialValues,
+          "updateMany",
+          this.buildUpdate(combinedConfig),
+        ),
+      );
     }
     if (combinedConfig.create) {
-      allBuildConfigs.push(this.buildInsert(combinedConfig));
+      allBuildConfigs.push(
+        this.createPayload(
+          formDataInitialValues,
+          "insert",
+          this.buildInsert(combinedConfig),
+        ),
+      );
     }
     //remove falsey build configs
     return allBuildConfigs.filter((val) => !!val);
