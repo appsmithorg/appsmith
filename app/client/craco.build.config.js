44161003c72cd605fcc2d5b19b809192bd4b2ec5
Ivan Akulov <iamakulov@outlook.com>
2023-04-14 10:03:31 +0200

    Revert "perf: work around the V8 service worker regression" (#22290)
    fixed in #21996 was just

--- app/client/craco.build.config.js
+++ app/client/craco.build.config.js
@@ -16,20 +16,6 @@ plugins.push(
     mode: "development",
     swDest: "./pageService.js",
     maximumFileSizeToCacheInBytes: 11 * 1024 * 1024,
-    exclude: [
-      // Don’t cache source maps and PWA manifests.
-      // (These are the default values of the `exclude` option: https://developer.chrome.com/docs/workbox/reference/workbox-build/#type-WebpackPartial,
-      // so we need to specify them explicitly if we’re extending this array.)
-      /\.map$/,
-      /^manifest.*\.js$/,
-      // Don’t cache the root html file
-      /index\.html/,
-      // Don’t cache LICENSE.txt files emitted by CRA
-      // when a chunk includes some license comments
-      /LICENSE\.txt/,
-    ],
-    // Don’t cache-bust JS and CSS chunks
-    dontCacheBustURLsMatching: /\.[0-9a-zA-Z]{8}\.chunk\.(js|css)$/,
   }),
 );
 
