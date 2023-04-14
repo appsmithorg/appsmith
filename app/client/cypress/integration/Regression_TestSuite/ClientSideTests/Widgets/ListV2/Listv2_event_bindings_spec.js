e1ffdcedd1a0adfb935ec07c4f503aed566fc1dd
Ashit Rath <ashit@appsmith.com>
2023-04-14 12:31:44 +0530

    fix: List v2 child widgets event defined with action selector

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/ListV2/Listv2_event_bindings_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/ListV2/Listv2_event_bindings_spec.js
@@ -1,10 +1,60 @@
 const simpleListWithInputAndButtonDSL = require("../../../../../fixtures/Listv2/simpleListWithInputAndButton.json");
 const commonlocators = require("../../../../../locators/commonlocators.json");
+import { ObjectsRegistry } from "../../../../../support/Objects/Registry";
 
 const toggleJSButton = (name) => `.t--property-control-${name} .t--js-toggle`;
 const widgetSelector = (name) => `[data-widgetname-cy="${name}"]`;
+let agHelper = ObjectsRegistry.AggregateHelper;
+
+const LOAD_TIMEOUT = 10000;
+
+const listData = [
+  {
+    id: "000",
+    name: "Yellow",
+    img: "https://assets.appsmith.com/widgets/default.png",
+  },
+  {
+    id: "001",
+    name: "Blue",
+    img: "https://assets.appsmith.com/widgets/default.png",
+  },
+  {
+    id: "002",
+    name: "Green",
+    img: "https://assets.appsmith.com/widgets/default.png",
+  },
+  {
+    id: "003",
+    name: "Red",
+    img: "https://assets.appsmith.com/widgets/default.png",
+  },
+  {
+    id: "004",
+    name: "Black",
+    img: "https://assets.appsmith.com/widgets/default.png",
+  },
+  {
+    id: "005",
+    name: "Yellow",
+    img: "https://assets.appsmith.com/widgets/default.png",
+  },
+  {
+    id: "006",
+    name: "Indigo",
+    img: "https://assets.appsmith.com/widgets/default.png",
+  },
+];
 
 describe("Listv2 - Event bindings", () => {
+  beforeEach(() => {
+    agHelper.RestoreLocalStorageCache();
+  });
+
+  afterEach(() => {
+    agHelper.SaveLocalStorageCache();
+  });
+
   it("1. simple list widget should have access to currentItem, currentIndex and currentView", () => {
     cy.addDsl(simpleListWithInputAndButtonDSL);
     cy.wait(4000);
@@ -22,9 +72,11 @@ describe("Listv2 - Event bindings", () => {
     cy.get(widgetSelector("Button1")).find("button").click({ force: true });
 
     cy.get(commonlocators.toastmsg).contains("Input _ 000 _ 0");
-  });
 
-  it("2. simple list widget should get updated values of currentView", () => {
+    /**
+     *  update value of currentView
+     */
+
     // Enter text in the parent list widget's text input
     cy.get(widgetSelector("Input1"))
       .find("input")
@@ -37,5 +89,38 @@ describe("Listv2 - Event bindings", () => {
     cy.wait(1000);
 
     cy.get(commonlocators.toastmsg).contains("Updated Input _ 000 _ 0");
+  });
+
+  it("2. widget events should get defined using action selector", () => {
+    cy.addDsl(simpleListWithInputAndButtonDSL);
+    cy.wait(2000);
+
+    // wait for list widget to load
+    cy.get(".t--widget-listwidgetv2", { timeout: LOAD_TIMEOUT });
+    // Wait for List widget to load other items
+    cy.get(".t--widget-buttonwidget", { timeout: LOAD_TIMEOUT }).should(
+      "have.length.at.least",
+      2,
+    );
+
+    cy.openPropertyPane("buttonwidget");
+
+    // Clear the onClick event binding
+    cy.testJsontext("onclick", "");
+    // Disable the JS mode
+    cy.get(toggleJSButton("onclick")).click({ force: true });
+
+    // Define 2 actions chained together
+    cy.addAction("{{currentItem.name}}", "onClick");
+    cy.wait(2000);
+    cy.addAction("{{currentIndex.toString()}}", "onClick");
+    cy.wait(2000);
+
+    // Find the button and click
+    cy.get(widgetSelector("Button1")).find("button").click({ force: true });
+
+    // Verify alerts for both actions
+    cy.get(commonlocators.toastmsg).contains(listData[0].name);
+    cy.get(commonlocators.toastmsg).contains("0");
   });
 });
