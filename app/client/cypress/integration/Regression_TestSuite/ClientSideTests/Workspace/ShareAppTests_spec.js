e0c82319afa04358c972509247d71543ae20f4b6
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-04-18 19:56:50 +0530

    clean up

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Workspace/ShareAppTests_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Workspace/ShareAppTests_spec.js
@@ -100,6 +100,7 @@ describe("Create new workspace and share with a user", function () {
 
   it("5. login as uninvited user and then validate public access of Application", function () {
     cy.LoginFromAPI(Cypress.env("TESTUSERNAME2"), Cypress.env("TESTPASSWORD2"));
+    cy.visit(currentUrl);
     cy.wait("@getPagesForViewApp").should(
       "have.nested.property",
       "response.body.responseMeta.status",
@@ -135,6 +136,7 @@ describe("Create new workspace and share with a user", function () {
 
   it("6. login as uninvited user, validate public access disable feature ", function () {
     cy.LoginFromAPI(Cypress.env("TESTUSERNAME2"), Cypress.env("TESTPASSWORD2"));
+    cy.visit(currentUrl);
     cy.wait("@getPagesForViewApp").should(
       "have.nested.property",
       "response.body.responseMeta.status",
