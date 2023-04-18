d6b65336e25a232f8b40685c0044f8e5f924aa91
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-04-18 19:35:53 +0530

    update login command to submit form

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -268,21 +268,21 @@ Cypress.Commands.add("Signup", (uname, pword) => {
 });
 
 Cypress.Commands.add("LoginFromAPI", (uname, pword) => {
-  cy.request({
-    method: "POST",
-    url: "api/v1/login",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-    },
-    followRedirect: false,
-    form: true,
-    body: {
-      username: uname,
-      password: pword,
-    },
-  }).then((response) => {
-    expect(response.status).equal(302);
-    //cy.log(response.body);
+  cy.location().then((loc) => {
+    cy.visit({
+      method: "POST",
+      url: "api/v1/login",
+      headers: {
+        origin: loc.origin,
+      },
+      followRedirect: true,
+      body: {
+        username: uname,
+        password: pword,
+      },
+    }).then(() => {
+      cy.url().should("eq", loc.origin + "/applications");
+    });
   });
 });
 
