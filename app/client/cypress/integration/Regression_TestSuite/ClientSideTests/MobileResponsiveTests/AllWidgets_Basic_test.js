e2e59d42e5a57c5d05cbf43209f91cf3f5d61456
Apple <nandan@thinkify.io>
2023-04-18 19:43:43 +0530

    updated test with parametrisation

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/MobileResponsiveTests/AllWidgets_Basic_test.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/MobileResponsiveTests/AllWidgets_Basic_test.js
@@ -4,6 +4,25 @@ import { ObjectsRegistry } from "../../../../support/Objects/Registry";
 const agHelper = ObjectsRegistry.AggregateHelper;
 let theight;
 let twidth;
+ let widgets = ["switchwidget", "currencyinputwidget", "codescannerwidget","listwidgetv2","tablewidgetv2","audiowidget","checkboxwidget"];
+    let height = {
+      switchwidget: 0,
+      currencyinputwidget: 0,
+      codescannerwidget: 0,
+      listwidgetv2: 0,
+      tablewidgetv2: 0,
+      audiowidget: 0,
+      checkboxwidget: 0,
+    };
+    let width = {
+      switchwidget: 0,
+      currencyinputwidget: 0,
+      codescannerwidget: 0,
+      listwidgetv2: 0,
+      tablewidgetv2: 0,
+      audiowidget: 0,
+      checkboxwidget: 0,
+    };
 
 describe("Validating Mobile Views", function () {
   afterEach(() => {
@@ -32,21 +51,8 @@ describe("Validating Mobile Views", function () {
     //cy.openPropertyPane("containerwidget");
     cy.PublishtheApp();
     cy.wait(2000);
-    let widgets = ["switchwidget", "currencyinputwidget", "codescannerwidget"];
-    let height = {
-      switchwidget: 0,
-      currencyinputwidget: 0,
-      codescannerwidget: 0,
-    };
-    let width = {
-      switchwidget: 0,
-      currencyinputwidget: 0,
-      codescannerwidget: 0,
-    };
-
     let heightPromises = [];
     let widthPromises = [];
-
     for (let i = 0; i < widgets.length; i++) {
       let heightPromise = cy
         .get(".t--widget-".concat(widgets[i]))
@@ -57,7 +63,6 @@ describe("Validating Mobile Views", function () {
           cy.log(i);
         });
       heightPromises.push(heightPromise);
-
       let widthPromise = cy
         .get(".t--widget-".concat(widgets[i]))
         .invoke("css", "width")
@@ -68,13 +73,10 @@ describe("Validating Mobile Views", function () {
         });
       widthPromises.push(widthPromise);
     }
-
-    Promise.all([...heightPromises, ...widthPromises]).then(() => {
       for (let i = 0; i < widgets.length; i++) {
-        cy.log(width[widgets[i]]);
         cy.log(height[widgets[i]]);
+        cy.log(width[widgets[i]]);
       }
-    });
   });
 
   let phones = ["iphone-4", "samsung-s10", [390, 844], [360, 780]];
@@ -86,15 +88,35 @@ describe("Validating Mobile Views", function () {
         cy.viewport(phone);
       }
       cy.wait(2000);
-      cy.get(".t--widget-inputwidgetv2")
+      cy.get(".t--widget-switchwidget")
         .invoke("css", "height")
         .then((newheight) => {
-          expect(theight).to.equal(newheight);
+          expect(height[widgets[0]]).to.equal(newheight);
         });
-      cy.get(".t--widget-inputwidgetv2")
+      cy.get(".t--widget-switchwidget")
         .invoke("css", "width")
         .then((newwidth) => {
-          expect(twidth).to.not.equal(newwidth);
+          expect(width[widgets[0]]).to.not.equal(newwidth);
+        });
+       cy.get(".t--widget-currencyinputwidget")
+        .invoke("css", "height")
+        .then((newheight) => {
+          expect(height[widgets[1]]).to.equal(newheight);
+        });
+       cy.get(".t--widget-currencyinputwidget")
+        .invoke("css", "width")
+        .then((newwidth) => {
+          expect(width[widgets[1]]).to.not.equal(newwidth);
+        });
+        cy.get(".t--widget-codescannerwidget")
+        .invoke("css", "height")
+        .then((newheight) => {
+          expect(height[widgets[2]]).to.equal(newheight);
+        });
+       cy.get(".t--widget-codescannerwidget")
+        .invoke("css", "width")
+        .then((newwidth) => {
+          expect(width[widgets[2]]).to.not.equal(newwidth);
         });
     });
   });
