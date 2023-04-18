832a46cdbcf22a356462ada2366eee4afcab1911
Sangeeth Sivan <sangeeth@appsmith.com>
2023-04-19 00:28:25 +0530

    fix: proper fix for scripts , but hacky for css

--- app/client/src/widgets/RichTextEditorWidget/component/index.tsx
+++ app/client/src/widgets/RichTextEditorWidget/component/index.tsx
@@ -1,3 +1,27 @@
+import "tinymce/tinymce";
+import "tinymce/icons/default";
+import "tinymce/plugins/paste";
+import "tinymce/plugins/link";
+import "tinymce/plugins/image";
+import "tinymce/plugins/table";
+import "tinymce/plugins/code";
+import "tinymce/plugins/help";
+import "tinymce/plugins/insertdatetime";
+import "tinymce/plugins/media";
+import "tinymce/plugins/advlist";
+import "tinymce/plugins/autolink";
+import "tinymce/plugins/lists";
+import "tinymce/plugins/charmap";
+import "tinymce/plugins/preview";
+import "tinymce/plugins/anchor";
+import "tinymce/plugins/searchreplace";
+import "tinymce/plugins/visualblocks";
+import "tinymce/plugins/fullscreen";
+import "tinymce/plugins/emoticons";
+import "tinymce/plugins/print";
+import "tinymce/themes/silver";
+import "tinymce/skins/ui/oxide/skin.min.css";
+import "tinymce/skins/ui/oxide/content.min.css";
 import React, { useRef, useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import { Editor } from "@tinymce/tinymce-react";
@@ -350,7 +374,69 @@ function RichtextEditorComponent(props: RichtextEditorComponentProps) {
             branding: false,
             resize: false,
             browser_spellcheck: true,
-            content_style: `${cssVariables}
+            content_style: `
+            body {
+              font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+              line-height: 1.4;
+              margin: 1rem;
+            }
+            table {
+              border-collapse: collapse;
+            }
+            /* Apply a default padding if legacy cellpadding attribute is missing */
+            table:not([cellpadding]) th,
+            table:not([cellpadding]) td {
+              padding: 0.4rem;
+            }
+            /* Set default table styles if a table has a positive border attribute
+               and no inline css */
+            table[border]:not([border="0"]):not([style*="border-width"]) th,
+            table[border]:not([border="0"]):not([style*="border-width"]) td {
+              border-width: 1px;
+            }
+            /* Set default table styles if a table has a positive border attribute
+               and no inline css */
+            table[border]:not([border="0"]):not([style*="border-style"]) th,
+            table[border]:not([border="0"]):not([style*="border-style"]) td {
+              border-style: solid;
+            }
+            /* Set default table styles if a table has a positive border attribute
+               and no inline css */
+            table[border]:not([border="0"]):not([style*="border-color"]) th,
+            table[border]:not([border="0"]):not([style*="border-color"]) td {
+              border-color: #ccc;
+            }
+            figure {
+              display: table;
+              margin: 1rem auto;
+            }
+            figure figcaption {
+              color: #999;
+              display: block;
+              margin-top: 0.25rem;
+              text-align: center;
+            }
+            hr {
+              border-color: #ccc;
+              border-style: solid;
+              border-width: 1px 0 0 0;
+            }
+            code {
+              background-color: #e8e8e8;
+              border-radius: 3px;
+              padding: 0.1rem 0.2rem;
+            }
+            .mce-content-body:not([dir=rtl]) blockquote {
+              border-left: 2px solid #ccc;
+              margin-left: 1.5rem;
+              padding-left: 1rem;
+            }
+            .mce-content-body[dir=rtl] blockquote {
+              border-right: 2px solid #ccc;
+              margin-right: 1.5rem;
+              padding-right: 1rem;
+            }
+              ${cssVariables}
               ${
                 props.isDisabled
                   ? `* {
@@ -392,7 +478,6 @@ function RichtextEditorComponent(props: RichtextEditorComponentProps) {
           }}
           key={`editor_${props.isToolbarHidden}_${props.isDisabled}`}
           onEditorChange={handleEditorChange}
-          tinymceScriptSrc="https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.10.1/tinymce.min.js"
           toolbar={props.isToolbarHidden ? false : toolbarConfig}
           value={editorValue}
         />
