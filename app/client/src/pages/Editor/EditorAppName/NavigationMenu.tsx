2ff6a936a9cff56a710d54e23c9f86cf42ee16bd
Rohit Agarwal <rohit_agarwal@live.in>
2023-04-19 11:08:49 +0530

    editor dropdown menu dedup

--- app/client/src/pages/Editor/EditorAppName/NavigationMenu.tsx
+++ app/client/src/pages/Editor/EditorAppName/NavigationMenu.tsx
@@ -1,41 +1,78 @@
 import React from "react";
 
-import type { noop } from "lodash";
+// import type { noop } from "lodash";
 
 import type { MenuItemData } from "./NavigationMenuItem";
-import { NavigationMenuItem, MenuTypes } from "./NavigationMenuItem";
+import { MenuTypes } from "./NavigationMenuItem";
+import {
+  MenuItem,
+  MenuSub,
+  MenuSubTrigger,
+  MenuSubContent,
+  MenuContent,
+  MenuSeparator,
+} from "design-system";
 
 type NavigationMenuProps = {
   menuItems: MenuItemData[] | undefined;
-  setIsPopoverOpen: typeof noop;
+  // setIsPopoverOpen: typeof noop;
 };
 
 export function NavigationMenu(props: NavigationMenuProps) {
-  const { menuItems, setIsPopoverOpen } = props;
-
+  const { menuItems } = props;
+  // const [confirm, setConfirm] = useState({
+  //   isConfirm: false,
+  //   text: "",
+  // });
+  // const handleReconfirmClick = (e: React.SyntheticEvent) => {
+  //   if (!confirm.isConfirm && menuItems?men.confirmText) {
+  //     setConfirm({
+  //       isConfirm: true,
+  //       text: confirmText,
+  //     });
+  //     e.preventDefault();
+  //     e.stopPropagation();
+  //   } else if (onClick) {
+  //     // setIsPopoverOpen(false);
+  //     onClick(e);
+  //     AnalyticsUtil.logEvent("APP_MENU_OPTION_CLICK", {
+  //       option: text,
+  //     });
+  //     setConfirm({
+  //       isConfirm: false,
+  //       text: text,
+  //     });
+  //   }
+  // };
   return (
-    // eslint-disable-next-line
-    <>
+    <MenuContent width="214px">
       {menuItems?.map((item, idx) => {
         return item.type === MenuTypes.PARENT ? (
-          <NavigationMenuItem
-            key={idx}
-            menuItemData={item}
-            setIsPopoverOpen={setIsPopoverOpen}
-          >
-            <NavigationMenu
-              menuItems={item?.children}
-              setIsPopoverOpen={setIsPopoverOpen}
-            />
-          </NavigationMenuItem>
+          <MenuSub>
+            <MenuSubTrigger>{item.text}</MenuSubTrigger>
+            <MenuSubContent width="214px">
+              {item?.children?.map((subitem, idx) => (
+                <MenuItem
+                  endIcon={subitem?.isOpensNewWindow ? "share-box-line" : ""}
+                  key={idx}
+                  onClick={subitem.onClick}
+                >
+                  <div className="flex justify-between">
+                    {subitem.text}
+                    {subitem?.labelElement}
+                  </div>
+                </MenuItem>
+              ))}
+            </MenuSubContent>
+          </MenuSub>
+        ) : item.type === MenuTypes.MENU_DIVIDER ? (
+          <MenuSeparator />
         ) : (
-          <NavigationMenuItem
-            key={idx}
-            menuItemData={item}
-            setIsPopoverOpen={setIsPopoverOpen}
-          />
+          <MenuItem key={idx} onClick={item.onClick}>
+            {item.text}
+          </MenuItem>
         );
       })}
-    </>
+    </MenuContent>
   );
 }
