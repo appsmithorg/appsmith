5aa50ee03845dc4cc18cde5f84266e4dc305d47d
Rimil Dey <rimil@appsmith.com>
2023-04-20 13:38:53 +0530

    Fix cypress tests

--- app/client/cypress/integration/Regression_TestSuite/ServerSideTests/OnLoadTests/JSOnLoad2_Spec.ts
+++ app/client/cypress/integration/Regression_TestSuite/ServerSideTests/OnLoadTests/JSOnLoad2_Spec.ts
@@ -34,7 +34,7 @@ describe("JSObjects OnLoad Actions tests", function () {
       AssertJSOnPageLoad(
         "runWorldCountries",
         false,
-        "ReferenceError: getWorldCountries is not defined",
+        "getWorldCountries is not defined",
       );
     });
   });
@@ -53,7 +53,7 @@ describe("JSObjects OnLoad Actions tests", function () {
     AssertJSOnPageLoad(
       "runWorldCountries",
       false,
-      "ReferenceError: getWorldCountries is not defined",
+      "getWorldCountries is not defined",
     );
   });
 
