bf56890540ace7efb4c5d6bd35cc23d71a081574
Tanvi Bhakta <tanvibhakta@gmail.com>
2023-04-21 19:14:11 +0530

    deduplicate db callout

--- app/client/src/components/designSystems/appsmith/help/CollapsibleHelp.tsx
+++ app/client/src/components/designSystems/appsmith/help/CollapsibleHelp.tsx
@@ -1,24 +1,14 @@
 import type { ReactNode } from "react";
 import React from "react";
 import styled from "styled-components";
-import { Icon } from "@blueprintjs/core";
+import { Callout } from "design-system";
+import type { CalloutProps } from "design-system";
 interface CollapsibleHelpProps {
   children?: ReactNode;
 }
 
-const HelpIcon = styled(Icon)`
-  margin-right: 14.5px;
-  margin-left: 4px;
-  height: 100%;
-  background-color: #ef7541;
-  border: 2px solid rgba(239, 123, 99);
-  border-radius: 50%;
-`;
-
 const Container = styled.div`
-  background-color: rgba(239, 123, 99, 0.15);
   padding: 10px 10px;
-  width: 100%;
   position: relative;
   display: flex;
   align-items: center;
@@ -29,21 +19,14 @@ const Container = styled.div`
   width: 89%;
 `;
 
-const LeftBar = styled.div`
-  width: 2px;
-  background-color: #ef7541;
-  position: absolute;
-  left: 0;
-  height: 100%;
-`;
-
-export default function CollapsibleHelp(props: CollapsibleHelpProps) {
-  //   const [isOpen, setIsOpen] = useState(false);
+export default function CollapsibleHelp(
+  props: CollapsibleHelpProps & CalloutProps,
+) {
   return (
     <Container>
-      <LeftBar />
-      <HelpIcon color={"#FDEBE8"} icon="help" />
-      <div>{props.children}</div>
+      <Callout kind="warning" {...props}>
+        {props.children}
+      </Callout>
     </Container>
   );
 }
