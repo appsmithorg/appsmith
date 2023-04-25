d4f1ae03d71ceff6ba0a4e1882cb6d42b59d35ee
Ankita Kinger <ankita@appsmith.com>
2023-04-25 15:16:15 +0530

    updating the spinner

--- app/client/src/pages/Settings/components.tsx
+++ app/client/src/pages/Settings/components.tsx
@@ -51,10 +51,6 @@ export const LoaderContainer = styled.div`
   justify-content: center;
   width: 100%;
   align-items: center;
-  .cs-spinner {
-    width: 32px;
-    height: 32px;
-  }
 `;
 
 export const ContentBox = styled.div`
