13a3dc34300ab2c813149f405a2ca4b5daca5ec6
Ravi Kumar Prasad <ravi@appsmith.com>
2023-04-25 09:52:40 +0530

    Address review comment on cypress test

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/BugTests/Bug6732_Spec.ts
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/BugTests/Bug6732_Spec.ts
@@ -1,22 +1,18 @@
 import * as _ from "../../../../support/Objects/ObjectsCore";
 
-const { agHelper, apiPage, locators } = _;
+const { agHelper, apiPage } = _;
 
 describe("this.params in IIFE function in API editor", () => {
   it("1. this.params should be available in IIFE function in API editor", () => {
     apiPage.CreateApi("Api1", "GET");
 
-    _.agHelper.GetNClick(_.locators._paramsTab);
-    cy.get(locators._queryParamsKey(0)).then(($el) => {
-      agHelper.UpdateCodeInput($el, "page");
-    });
-
-    cy.get(locators._queryParamsValue(0)).then(($el) => {
-      agHelper.UpdateCodeInput(
-        $el,
-        "{{(function () { return this.params.key; })()}}",
-      );
-    });
+    apiPage.SelectPaneTab("Params");
+    apiPage.EnterParams(
+      "page",
+      "{{(function () { return this.params.key; })()}}",
+      0,
+      false,
+    );
 
     agHelper.VerifyEvaluatedValue("page");
   });
