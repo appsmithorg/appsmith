a7f4da3124f0776114719863c9ae0f81fff93cb2
Aishwarya UR <aishwarya@appsmith.com>
2023-04-25 23:44:41 +0530

    Added delete Hkey

--- app/client/cypress/integration/Regression_TestSuite/ServerSideTests/Datasources/Redis_Basic_Spec.ts
+++ app/client/cypress/integration/Regression_TestSuite/ServerSideTests/Datasources/Redis_Basic_Spec.ts
@@ -12,12 +12,13 @@ describe("Validate Redis DS", () => {
 
   it("Create HAST set (Multiple key value pair under single key name) in redis DB, Read, Delete", () => {
     let hSetReceipe = `HSET recipe:1 name "Vegetable Stir Fry" ingredients "2 cups mixed vegetables (broccoli, carrots, bell peppers, mushrooms, snow peas), 2 cloves garlic, minced" instructions "1. Heat vegetable oil in a large skillet over medium-high heat. 2. Add mixed vegetables and garlic to the skillet and cook for 3-4 minutes. 3. In a small bowl, whisk together soy sauce and cornstarch. 4. Pour the soy sauce mixture over the vegetables and stir until the vegetables are coated. 5. Cook for an additional 1-2 minutes. 6. Serve hot." difficulty "easy"`;
-    let hGetKeys = "HMGET recipe:1 name";
+    let hGetKeys = "HGET recipe:1 name";
     let hMGet = "HMGET recipe:1 difficulty name"; // getting multiple keys
     let hUpdate = "HSET recipe:1 difficulty medium";
     let getUpdatedKey = "HGET recipe:1 difficulty";
     let getAll = "HGETALL recipe:1";
     let addNewKeyValue = `HSET recipe:1 prep_time "10 minutes"`;
+    let deletehKey = "DEL recipe:1";
 
     //Add HSET
     _.dataSources.CreateQueryAfterDSSaved();
@@ -69,11 +70,22 @@ describe("Validate Redis DS", () => {
     _.dataSources.EnterQuery(addNewKeyValue);
     _.dataSources.RunQueryNVerifyResponseViews(1);
 
-    //verify new key/value also added to HSET
+    //Verify new key/value also added to HSET
     _.dataSources.EnterQuery(getAll);
     _.dataSources.RunQueryNVerifyResponseViews(10); //5 keys, 5 values
     _.dataSources.ReadQueryTableResponse(0).then(($cellData: any) => {
       expect($cellData).to.eq("ingredients");
+    });
+
+    //Deleting the Hash key
+    _.dataSources.EnterQuery(deletehKey);
+    _.dataSources.RunQueryNVerifyResponseViews(1);
+
+    //Verify Deletion is success
+    _.dataSources.EnterQuery(hGetKeys);
+    _.dataSources.RunQueryNVerifyResponseViews(); //5 keys, 5 values
+    _.dataSources.ReadQueryTableResponse(0).then(($cellData: any) => {
+      expect($cellData).to.eq("null");
     });
   });
 
