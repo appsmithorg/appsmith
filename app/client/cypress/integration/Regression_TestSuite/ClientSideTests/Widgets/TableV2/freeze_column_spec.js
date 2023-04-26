d85186b0e18be56eb617eed9ed1bda1b9ef16861
keyurparalkar <keyur@appsmith.com>
2023-04-26 13:31:44 +0530

    fix: cypress failures

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/TableV2/freeze_column_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/TableV2/freeze_column_spec.js
@@ -3,10 +3,7 @@ import {
   PROPERTY_SELECTOR,
   WIDGET,
 } from "../../../../../locators/WidgetLocators";
-import {
-  TABLE_DATA_DYNAMIC,
-  TABLE_DATA_DYNAMIC_STATIC,
-} from "../../../../../support/Constants";
+import { TABLE_DATA_DYNAMIC } from "../../../../../support/Constants";
 import { ObjectsRegistry } from "../../../../../support/Objects/Registry";
 
 const widgetsPage = require("../../../../../locators/Widgets.json");
@@ -481,114 +478,5 @@ describe("3. Server-side pagination when turned on test of re-ordering columns",
     cy.get(PROPERTY_SELECTOR.tableColumnNames)
       .eq(1)
       .should("have.value", "productName");
-  });
-});
-
-describe("4. Check frozen common and/or custom columns retain position on query change", () => {
-  before(() => {
-    cy.dragAndDropToCanvas(WIDGET.TABLE, { x: 600, y: 200 });
-    cy.dragAndDropToCanvas(WIDGET.TEXT, { x: 600, y: 50 });
-
-    cy.openPropertyPane(WIDGET.TABLE);
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC_STATIC);
-
-    cy.openPropertyPane(WIDGET.TEXT);
-    cy.updateCodeInput(PROPERTY_SELECTOR.text, `{{Table1.columnOrder}}`);
-
-    /**
-     * Create two table data with common columns
-     */
-  });
-  it("4.1 Check if common frozen columns retain position", () => {
-    // Freeze the common column: id
-    cy.freezeColumnFromDropdown("id", "right");
-
-    //Change the table data:
-    cy.openPropertyPane(WIDGET.TABLE);
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC);
-
-    //Check the id column is still frozen to the right:
-    cy.wait(500);
-    cy.checkColumnPosition("id", 4);
-    cy.freezeColumnFromDropdown("id", "right");
-  });
-
-  it("4.2 Check if custom frozen columns retain position", () => {
-    // Add a custom column
-    cy.get(widgetsPage.addColumn).scrollIntoView();
-    cy.get(widgetsPage.addColumn).should("be.visible").click({ force: true });
-    cy.wait(300);
-    cy.freezeColumnFromDropdown("customColumn1", "right");
-
-    // Change the table data:
-    cy.openPropertyPane(WIDGET.TABLE);
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC_STATIC);
-
-    //Check the id column is still frozen to the right:
-    cy.checkColumnPosition("customColumn1", 5);
-  });
-
-  it("4.3 Check if the custom + common columns retain their positon", () => {
-    cy.wait(500);
-    // Freeze the common column:
-    cy.freezeColumnFromDropdown("id", "right");
-
-    // Expect id should come before customColumn1 and both should be frozen:
-    cy.checkColumnPosition("id", 3);
-    cy.checkColumnPosition("customColumn1", 4);
-
-    // Change table data:
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC);
-
-    // Re-check the frozen column order:
-    cy.checkColumnPosition("id", 3);
-    cy.checkColumnPosition("customColumn1", 4);
-  });
-
-  it("4.4 Check if frozen column retains its position and unfrozen goes at the first position", () => {
-    // Freeze the common column:
-    cy.freezeColumnFromDropdown("id", "right");
-
-    // Change table data:
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC_STATIC);
-
-    cy.wait(500);
-
-    // Re-check the frozen column order:
-    cy.checkColumnPosition("id", 0);
-    cy.checkColumnPosition("customColumn1", 4);
-  });
-
-  it("4.5 Check if the order of the forzen columns remains the same", () => {
-    cy.freezeColumnFromDropdown("id", "right");
-
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC);
-
-    cy.wait(500);
-
-    cy.checkColumnPosition("id", 4);
-    cy.checkColumnPosition("customColumn1", 5);
-
-    // Check the order for left frozen columns:
-    cy.freezeColumnFromDropdown("id", "left");
-    cy.freezeColumnFromDropdown("customColumn1", "left");
-
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC_STATIC);
-
-    cy.wait(500);
-
-    cy.checkColumnPosition("id", 0);
-    cy.checkColumnPosition("customColumn1", 1);
-
-    // Check if the common unfrozen column comes after the left frozen column
-    cy.freezeColumnFromDropdown("customColumn1", "left");
-    cy.dragAndDropColumn("customColumn1", "action");
-
-    cy.updateCodeInput(PROPERTY_SELECTOR.tableData, TABLE_DATA_DYNAMIC);
-
-    cy.wait(500);
-
-    cy.checkColumnPosition("id", 0);
-    cy.checkColumnPosition("customColumn1", 1);
   });
 });
