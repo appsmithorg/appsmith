c3b73da2fa673fac56a2af4c0dc3ed3b0624482d
arunvjn <32433245+arunvjn@users.noreply.github.com>
2023-05-01 20:54:45 +0530

    chore: ai feedback v2 (#1377)
    ## Description
    Contains UI changes and some enhancements for GPT AI window.
    - Fixed space in mustache binding.
    - Fixed duplicated binding syntax on successive copy action.
    - Added Run button to execute generated script.
    - Enabled AI window in SQL pages.
    - Added like/dislike icons for feedback
    - Fuzzy match user prompt with appsmith entities
    - Added meta in request body to contain datasource ids
    - Enables SQL query generation
    
    ## Type of change
    - New feature (non-breaking change which adds functionality)
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## How Has This Been Tested?
    - Manual
    
    ### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    
    ## Checklist:
    ### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    ### QA activity:
    - [ ] Test plan has been approved by relevant developers
    - [ ] Test plan has been peer reviewed by QA
    - [ ] Cypress test cases have been added and approved by either SDET or
    manual QA
    - [ ] Organized project review call with relevant stakeholders after
    Round 1/2 of QA
    - [ ] Added Test Plan Approved label after reveiwing all Cypress test

--- app/client/src/ee/reducers/AIReducer.ts
+++ app/client/src/ee/reducers/AIReducer.ts
@@ -1,23 +1,57 @@
+import type {
+  TAssistantPrompt,
+  TChatGPTPrompt,
+} from "@appsmith/components/editorComponents/GPT/utils";
+import { isAssistantPrompt } from "@appsmith/components/editorComponents/GPT/utils";
 import type { ReduxAction } from "@appsmith/constants/ReduxActionConstants";
 import { ReduxActionTypes } from "@appsmith/constants/ReduxActionConstants";
-import { createReducer } from "utils/ReducerUtils";
+import { createImmerReducer } from "utils/ReducerUtils";
 
 export interface AIReduxState {
   isAIWindowOpen: boolean;
+  evaluationResults: Record<string, any>;
+  messages: TChatGPTPrompt[];
 }
 
 const initialGPTState: AIReduxState = {
   isAIWindowOpen: false,
+  evaluationResults: {},
+  messages: [],
 };
 
 const handlers = {
   [ReduxActionTypes.TOGGLE_AI_WINDOW]: (
     state: AIReduxState,
     action: ReduxAction<boolean>,
-  ): AIReduxState => ({
-    ...state,
-    isAIWindowOpen: action.payload,
-  }),
+  ) => {
+    state.isAIWindowOpen = action.payload;
+  },
+  [ReduxActionTypes.EVALUATE_GPT_RESPONSE_COMPLETE]: (
+    state: AIReduxState,
+    action: ReduxAction<{ error: string; result: any; messageId: string }>,
+  ) => {
+    state.evaluationResults[action.payload.messageId] = action.payload.result;
+  },
+  [ReduxActionTypes.ADD_GPT_MESSAGE]: (
+    state: AIReduxState,
+    action: ReduxAction<TChatGPTPrompt>,
+  ) => {
+    state.messages.push(action.payload);
+  },
+  [ReduxActionTypes.UPDATE_GPT_MESSAGE]: (
+    state: AIReduxState,
+    action: ReduxAction<Partial<TAssistantPrompt>>,
+  ) => {
+    const { messageId } = action.payload;
+    const messageIndex = state.messages.findIndex(
+      (message) =>
+        isAssistantPrompt(message) && message.messageId === messageId,
+    );
+    state.messages[messageIndex] = {
+      ...(state.messages[messageIndex] as TAssistantPrompt),
+      ...action.payload,
+    };
+  },
 };
 
-export default createReducer(initialGPTState, handlers);
+export default createImmerReducer(initialGPTState, handlers);
