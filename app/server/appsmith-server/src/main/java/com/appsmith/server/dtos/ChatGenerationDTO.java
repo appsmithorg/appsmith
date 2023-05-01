4b42f7e37220156134a1771588c8f49f970e6978
Nidhi <nidhi@appsmith.com>
2023-05-01 20:55:02 +0530

    feat: SQL generation for Ask AI (#1376)
    Adds the ability to send SQL generation requests, along with datasource
    schema info.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ChatGenerationDTO.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ChatGenerationDTO.java
@@ -1,6 +1,9 @@
 package com.appsmith.server.dtos;
 
+import com.appsmith.external.models.DatasourceStructure;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,4 +20,27 @@ public class ChatGenerationDTO {
 
     @JsonProperty("api_context")
     Object apiContext;
+
+    ChatGenerationMeta meta;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class ChatGenerationMeta {
+        String datasourceId;
+
+        ChatGenerationDatasourceStructure datasourceStructure;
+
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public static class ChatGenerationDatasourceStructure {
+        String pluginName;
+
+        String datasourceName;
+
+        DatasourceStructure structure;
+    }
 }
