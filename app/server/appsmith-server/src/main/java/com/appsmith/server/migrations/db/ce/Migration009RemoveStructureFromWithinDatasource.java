f51f7443322b611a93e67daadb447b9511fd6858
Nidhi Nair <nidhi@appsmith.com>
2023-05-01 10:37:40 +0530

    Added indices

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration009RemoveStructureFromWithinDatasource.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration009RemoveStructureFromWithinDatasource.java
@@ -2,10 +2,13 @@ package com.appsmith.server.migrations.db.ce;
 
 
 import com.appsmith.external.models.Datasource;
+import com.appsmith.external.models.DatasourceConfigurationStructure;
+import com.appsmith.server.migrations.DatabaseChangelog1;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
@@ -17,8 +20,12 @@ public class Migration009RemoveStructureFromWithinDatasource {
 
     private final MongoOperations mongoOperations;
 
-    public Migration009RemoveStructureFromWithinDatasource(MongoOperations mongoOperations) {
+    private final MongoTemplate mongoTemplate;
+
+    public Migration009RemoveStructureFromWithinDatasource(MongoOperations mongoOperations,
+                                                           MongoTemplate mongoTemplate) {
         this.mongoOperations = mongoOperations;
+        this.mongoTemplate = mongoTemplate;
     }
 
     @RollbackExecution
@@ -28,6 +35,13 @@ public class Migration009RemoveStructureFromWithinDatasource {
 
     @Execution
     public void executeMigration() {
+        DatabaseChangelog1.dropIndexIfExists(mongoTemplate, DatasourceConfigurationStructure.class, "dsConfigStructure_datasourceId_envId_compound_index");
+
+        DatabaseChangelog1.ensureIndexes(mongoTemplate, DatasourceConfigurationStructure.class,
+                DatabaseChangelog1.makeIndex("datasourceId", "envId")
+                        .unique().named("dsConfigStructure_datasourceId_envId_compound_index")
+        );
+
         Query query = query(where("structure").exists(true));
 
         Update update = new Update().unset("structure");
