598b127f7efe740ff127e44e084b9863f8ce4445
Anagh Hegde <anagh@appsmith.com>
2023-05-02 12:06:25 +0530

    WIP

--- app/server/appsmith-git/src/main/java/com/appsmith/git/constants/CommonConstants.java
+++ app/server/appsmith-git/src/main/java/com/appsmith/git/constants/CommonConstants.java
@@ -2,7 +2,7 @@ package com.appsmith.git.constants;
 
 public class CommonConstants {
     // This field will be useful when we migrate fields within JSON files (currently this will be useful for Git feature)
-    public static Integer fileFormatVersion = 4;
+    public static Integer fileFormatVersion = 5;
     public static String FILE_FORMAT_VERSION = "fileFormatVersion";
     public static final String CANVAS = "canvas";
     public static final String APPLICATION = "application";
