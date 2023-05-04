d7a6051f3d54e6c8f003c9c508d1be206c2c635c
Aishwarya UR <aishwarya@appsmith.com>
2023-05-04 09:53:22 +0530

    patch fix reapply

--- app/client/cypress/apply-patches.js
+++ app/client/cypress/apply-patches.js
@@ -1,4 +1,4 @@
-const { getVersionDir } = require("../node_modules/cypress/lib/tasks/state");
+const { getVersionDir } = require("cypress/lib/tasks/state/version");
 const chalk = require("chalk");
 const Diff = require("diff");
 const fs = require("fs/promises");
