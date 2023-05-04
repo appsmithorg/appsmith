de626bb548790f136888e237c9a861204c5ae599
Apple <nandan@thinkify.io>
2023-05-04 17:51:43 +0530

    updated workflow logic

---
+++ app/client/cypress_ci.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  defaultCommandTimeout: 20000,
+  requestTimeout: 21000,
+  responseTimeout: 20000,
+  pageLoadTimeout: 30000,
+  videoUploadOnPasses: false,
+  videoCompression: false,
+  numTestsKeptInMemory: 10,
+  reporterOptions: {
+    reportDir: 'results',
+    overwrite: false,
+    html: true,
+    json: false,
+  },
+  chromeWebSecurity: false,
+  viewportHeight: 1100,
+  viewportWidth: 1400,
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost/',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
