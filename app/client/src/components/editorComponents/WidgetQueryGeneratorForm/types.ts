9aa41b2f8b78a1e81ec25e482f556759b0723ed6
balajisoundar <balaji@appsmith.com>
2023-05-04 09:55:34 +0530

    fix type

--- app/client/src/components/editorComponents/WidgetQueryGeneratorForm/types.ts
+++ app/client/src/components/editorComponents/WidgetQueryGeneratorForm/types.ts
@@ -1,10 +1,8 @@
-import type { DefaultOptionType } from "rc-select/lib/Select";
-
 export interface DropdownOptionType {
   id?: string;
   label?: string;
   value?: string;
   icon?: JSX.Element;
-  onSelect?: (value: string, option: DefaultOptionType) => void;
+  onSelect?: (value: string, option: DropdownOptionType) => void;
   data?: any;
 }
