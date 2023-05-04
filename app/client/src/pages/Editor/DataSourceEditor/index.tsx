e85c1f7d58dabdfb4a36abb0a00f5c86449389e3
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-05-04 08:29:23 +0700

    update: moved action button to the main header

--- app/client/src/pages/Editor/DataSourceEditor/index.tsx
+++ app/client/src/pages/Editor/DataSourceEditor/index.tsx
@@ -33,6 +33,7 @@ import DatasourceSaasForm from "../SaaSEditor/DatasourceForm";
 
 import {
   getCurrentApplicationId,
+  getPagePermissions,
   selectURLSlugs,
 } from "selectors/editorSelectors";
 import { saasEditorDatasourceIdURL } from "RouteBuilder";
@@ -49,18 +50,25 @@ import { getQueryParams } from "utils/URLUtils";
 import { TEMP_DATASOURCE_ID } from "constants/Datasource";
 import SaveOrDiscardDatasourceModal from "./SaveOrDiscardDatasourceModal";
 import {
+  ActionWrapper,
   EditDatasourceButton,
   FormTitleContainer,
   Header,
   PluginImage,
 } from "./JSONtoForm";
 import { getAssetUrl } from "@appsmith/utils/airgapHelpers";
-import { hasManageDatasourcePermission } from "@appsmith/utils/permissionHelpers";
+import {
+  hasCreateDatasourceActionPermission,
+  hasManageDatasourcePermission,
+} from "@appsmith/utils/permissionHelpers";
 import FormTitle from "./FormTitle";
 import styled from "styled-components";
 import CloseEditor from "components/editorComponents/CloseEditor";
+import NewActionButton from "./NewActionButton";
+import { isDatasourceAuthorizedForQueryCreation } from "utils/editorContextUtils";
 
 interface ReduxStateProps {
+  canCreateDatasourceActions: boolean;
   canManageDatasource: boolean;
   datasourceId: string;
   formData: Datasource;
@@ -69,6 +77,7 @@ interface ReduxStateProps {
   formConfig: any[];
   isDeleting: boolean;
   isNewDatasource: boolean;
+  isPluginAuthorized: boolean;
   pageId: string;
   pluginImages: Record<string, string>;
   pluginId: string;
@@ -466,11 +475,15 @@ class DatasourceEditorRouter extends React.Component<Props, State> {
 
   renderHeader() {
     const {
+      canCreateDatasourceActions,
       canManageDatasource,
+      datasource,
       datasourceId,
       isNewDatasource,
+      isPluginAuthorized,
       pluginId,
       pluginImages,
+      pluginType,
       setDatasourceViewMode,
       viewMode,
     } = this.props;
@@ -487,14 +500,25 @@ class DatasourceEditorRouter extends React.Component<Props, State> {
           />
         </FormTitleContainer>
         {viewMode && (
-          <EditDatasourceButton
-            category={Category.secondary}
-            className="t--edit-datasource"
-            onClick={() => {
-              setDatasourceViewMode(false);
-            }}
-            text="EDIT"
-          />
+          <ActionWrapper>
+            <EditDatasourceButton
+              category={Category.secondary}
+              className="t--edit-datasource"
+              onClick={() => {
+                setDatasourceViewMode(false);
+              }}
+              text="EDIT"
+            />
+            <NewActionButton
+              datasource={datasource}
+              disabled={!canCreateDatasourceActions || !isPluginAuthorized}
+              eventFrom="datasource-pane"
+              pluginType={pluginType}
+              style={{
+                marginLeft: "16px",
+              }}
+            />
+          </ActionWrapper>
         )}
       </Header>
     );
@@ -541,13 +565,23 @@ const mapStateToProps = (state: AppState, props: any): ReduxStateProps => {
   const defaultKeyValueArrayConfig =
     datasourcePane?.defaultKeyValueArrayConfig as any;
 
-  const datasourcePermissions = datasource.userPermissions || [];
+  const datasourcePermissions = datasource?.userPermissions || [];
 
   const canManageDatasource = hasManageDatasourcePermission(
     datasourcePermissions,
   );
 
+  const pagePermissions = getPagePermissions(state);
+  const canCreateDatasourceActions = hasCreateDatasourceActionPermission([
+    ...datasourcePermissions,
+    ...pagePermissions,
+  ]);
+
+  const isPluginAuthorized =
+    plugin && isDatasourceAuthorizedForQueryCreation(formData, plugin);
+
   return {
+    canCreateDatasourceActions,
     canManageDatasource,
     datasourceId,
     pluginImages: getPluginImages(state),
@@ -556,6 +590,7 @@ const mapStateToProps = (state: AppState, props: any): ReduxStateProps => {
     pluginId,
     isSaving: datasources.loading,
     isDeleting: !!datasource?.isDeleting,
+    isPluginAuthorized: !!isPluginAuthorized,
     isTesting: datasources.isTesting,
     formConfig: formConfigs[pluginId] || [],
     isNewDatasource: datasourcePane.newDatasource === TEMP_DATASOURCE_ID,
