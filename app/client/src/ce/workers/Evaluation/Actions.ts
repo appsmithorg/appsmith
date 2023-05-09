f2498d0ccce4f41128900ab9158c7ded6fed7de1
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2023-05-09 10:55:43 +0530

    remove log

--- app/client/src/ce/workers/Evaluation/Actions.ts
+++ app/client/src/ce/workers/Evaluation/Actions.ts
@@ -125,8 +125,6 @@ export const addDataTreeToContext = (args: {
       funcObj,
     );
   }
-
-  console.log("$$$", { EVAL_CONTEXT });
 };
 
 export const addPlatformFunctionsToEvalContext = (context: any) => {
