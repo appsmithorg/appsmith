3c2bb541a51837f2328d7293df41db1285c83c5c
manish kumar <manish@appsmith.com>
2023-05-11 14:43:48 +0530

    added test cases

---
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/DatasourceConfigurationStorageServiceTest.java
@@ -0,0 +1,111 @@
+package com.appsmith.server.services;
+
+import com.appsmith.external.models.DatasourceConfiguration;
+import com.appsmith.external.models.DatasourceConfigurationStorage;
+import com.appsmith.external.models.Endpoint;
+import com.appsmith.server.domains.User;
+import com.appsmith.server.domains.Workspace;
+import com.appsmith.server.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Comparator;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@DirtiesContext
+@Slf4j
+public class DatasourceConfigurationStorageServiceTest {
+
+    @Autowired
+    UserRepository userRepository;
+    @SpyBean
+    WorkspaceService workspaceService;
+    @Autowired
+    DatasourceConfigurationStorageService datasourceConfigurationStorageService;
+
+
+
+    @BeforeEach
+    public void setup() {
+        Mono<User> userMono = userRepository.findByEmail("api_user").cache();
+        Workspace workspace =
+                userMono.flatMap(user -> workspaceService.createDefault(new Workspace(), user))
+                        .switchIfEmpty(Mono.error(new Exception("createDefault is returning empty!!")))
+                        .block();
+
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void verifyFindOneByDatasourceId() {
+        String datasourceId = "mockDatasourceId";
+        DatasourceConfiguration datasourceConfiguration = new DatasourceConfiguration();
+        datasourceConfiguration.setEndpoints(List.of(new Endpoint("mockEndpoints", 000L)));
+        DatasourceConfigurationStorage datasourceConfigurationStorage =
+                new DatasourceConfigurationStorage(datasourceId, null, datasourceConfiguration, null, null);
+
+        DatasourceConfigurationStorage savedDatasourceConfigurationStorage =
+                datasourceConfigurationStorageService.save(datasourceConfigurationStorage).block();
+
+        Mono<DatasourceConfigurationStorage> datasourceConfigurationStorageMono =
+                datasourceConfigurationStorageService.findOneByDatasourceId(datasourceId);
+
+        StepVerifier.create(datasourceConfigurationStorageMono)
+                .assertNext(datasourceConfigurationStorage1 -> {
+                    assert(datasourceConfigurationStorage1 != null);
+                    assert(datasourceId.equals(datasourceConfigurationStorage1.getDatasourceId()));
+                    assert(datasourceConfigurationStorage1.getDatasourceConfiguration().getEndpoints().size() == 1);
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void verifyFindByDatasourceId() {
+
+        String datasourceId = "mockDatasourceId";
+        String environmentIdOne = "mockEnvironmentIdOne";
+        String environmentIdTwo = "mockEnvironmentIdTwo";
+        DatasourceConfiguration datasourceConfiguration = new DatasourceConfiguration();
+        datasourceConfiguration.setEndpoints(List.of(new Endpoint("mockEndpoints", 000L)));
+        DatasourceConfigurationStorage datasourceConfigurationStorageOne =
+                new DatasourceConfigurationStorage(datasourceId, environmentIdOne, datasourceConfiguration, null, null);
+
+        DatasourceConfigurationStorage datasourceConfigurationStorageTwo =
+                new DatasourceConfigurationStorage(datasourceId, environmentIdTwo, datasourceConfiguration, null, null);
+
+
+        datasourceConfigurationStorageService.save(datasourceConfigurationStorageOne).block();
+        datasourceConfigurationStorageService.save(datasourceConfigurationStorageTwo).block();
+
+        Flux<DatasourceConfigurationStorage> datasourceConfigurationStorageFlux =
+                datasourceConfigurationStorageService.findByDatasourceId(datasourceId)
+                        .sort(Comparator.comparing(DatasourceConfigurationStorage::getEnvironmentId));
+
+        StepVerifier.create(datasourceConfigurationStorageFlux)
+                .assertNext(datasourceConfigurationStorage -> {
+                    assert(datasourceConfigurationStorage != null);
+                    assert(datasourceId.equals(datasourceConfigurationStorage.getDatasourceId()));
+                    assert(environmentIdOne.equals(datasourceConfigurationStorage.getEnvironmentId()));
+                })
+                .assertNext(datasourceConfigurationStorage -> {
+                    assert(datasourceConfigurationStorage != null);
+                    assert(datasourceId.equals(datasourceConfigurationStorage.getDatasourceId()));
+                    assert(environmentIdTwo.equals(datasourceConfigurationStorage.getEnvironmentId()));
+                })
+                .verifyComplete();
+    }
+}
