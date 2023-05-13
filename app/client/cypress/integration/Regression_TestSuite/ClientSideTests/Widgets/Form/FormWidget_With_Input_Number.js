28a0dcc3694ffc8f6ef3699f55c90e26674fc13b
Aishwarya UR <aishwarya@appsmith.com>
2023-05-13 21:51:43 +0530

    FormWidget_With_Input_Number fix

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/Form/FormWidget_With_Input_Number.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Widgets/Form/FormWidget_With_Input_Number.js
@@ -1,5 +1,6 @@
 const dsl = require("../../../../../fixtures/formWidgetWithInputValCheckDsl.json");
 const widgetsPage = require("../../../../../locators/Widgets.json");
+import * as _ from "../../../../../support/Objects/ObjectsCore";
 
 describe("Form Widget with Input Functionality", function () {
   before(() => {
@@ -10,25 +11,11 @@ describe("Form Widget with Input Functionality", function () {
     //Check if the Input widget is visible
     cy.get(widgetsPage.inputWidget).should("be.visible");
 
-    //Check if the submit button is visible;
-    cy.get(widgetsPage.formButtonWidget)
-      .contains("Submit")
-      .scrollIntoView()
-      .should("be.visible");
-
     //Do Submission
-    cy.get(widgetsPage.formButtonWidget)
-      .contains("Submit")
-      .closest("div")
-      .click();
+    _.agHelper.ClickButton("Submit");
 
     //Check if on submission if the notification toast appears with text containing input1: 0
-    cy.get(widgetsPage.toastActionText)
-      .last()
-      .invoke("text")
-      .then((text) => {
-        expect(text).to.equal('{"Text1":"Form","Input1":0}');
-      });
+    _.agHelper.ValidateToastMessage('{"Text1":"Form","Input1":0}');
 
     cy.get(widgetsPage.formButtonWidget)
       .contains("Reset")
