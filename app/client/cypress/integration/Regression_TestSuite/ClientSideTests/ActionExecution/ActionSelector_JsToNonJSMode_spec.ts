10eb6f5590881c78dda21cb46d18617c36765350
Aishwarya UR <aishwarya@appsmith.com>
2023-05-13 11:38:28 +0530

    ActionSelector_JsToNonJSMode_spec fix

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/ActionExecution/ActionSelector_JsToNonJSMode_spec.ts
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/ActionExecution/ActionSelector_JsToNonJSMode_spec.ts
@@ -1359,7 +1359,7 @@ describe("JS to non-JS mode in Action Selector", () => {
 
     _.agHelper.GetNAssertElementText(
       _.propPane._actionCard,
-      "Watch Geolocation",
+      "Watch geolocation",
       "have.text",
       0,
     );
@@ -1383,7 +1383,7 @@ describe("JS to non-JS mode in Action Selector", () => {
 
     _.agHelper.GetNAssertElementText(
       _.propPane._actionCard,
-      "Stop watching Geolocation",
+      "Stop watching geolocation",
       "have.text",
       0,
     );
@@ -1407,7 +1407,7 @@ describe("JS to non-JS mode in Action Selector", () => {
 
     _.agHelper.GetNAssertElementText(
       _.propPane._actionCard,
-      "Get GeolocationAdd callback",
+      "Get geolocationAdd callback",
       "have.text",
       0,
     );
