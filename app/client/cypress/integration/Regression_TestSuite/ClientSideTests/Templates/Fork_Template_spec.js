701d48ecfabe88b3471b8a61c4a55e881bc064c7
Aishwarya UR <aishwarya@appsmith.com>
2023-05-14 17:10:49 +0530

    Chart specs fixed

--- app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Templates/Fork_Template_spec.js
+++ app/client/cypress/integration/Regression_TestSuite/ClientSideTests/Templates/Fork_Template_spec.js
@@ -58,7 +58,7 @@ describe("excludeForAirgap", "Fork a template to an workspace", () => {
     );
   });
 
-  it.only("4. Check if tooltip is working in 'Reconnect Datasources'", () => {
+  it("4. Check if tooltip is working in 'Reconnect Datasources'", () => {
     cy.NavigateToHome();
     cy.get(templateLocators.templatesTab).click();
     cy.wait(1000);
