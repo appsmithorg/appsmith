0b729e02cfe9d90d3de84c78ac0448b4a88ea681
Ankita Kinger <ankita@appsmith.com>
2023-05-15 18:07:57 +0530

    fix: Entity pane spacing & deduplicating spinner on app editor page (#23336)
    ## Description
    
    1. Fixing Entity pane spacing
    2. Deduplicating spinner on app editor page
    
    #### PR fixes following issue(s)
    Fixes 
    
    https://www.notion.so/appsmith/entity-explorer-all-the-plus-icons-are-not-on-the-same-line-mainly-for-pages-5482bbe8fa9442af9721c0ee7bd8a377?pvs=4
    
    https://www.notion.so/appsmith/Replace-old-loading-icon-with-the-new-one-when-editing-apps-4b7cd08012d24f2e8ab977cfefff7d1f?pvs=4
    
    #### Type of change
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## Testing
    
    #### How Has This Been Tested?
    - [x] Manual
    - [ ] Jest
    - [ ] Cypress
    
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Test-plan-implementation#speedbreaker-features-to-consider-for-every-change)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans/_edit#areas-of-interest)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/pages/Editor/gitSync/ReconnectDatasourceModal.tsx
+++ app/client/src/pages/Editor/gitSync/ReconnectDatasourceModal.tsx
@@ -505,7 +505,7 @@ function ReconnectDatasourceModal() {
   return (
     <Modal onOpenChange={handleClose} open={isModalOpen}>
       <ModalContentWrapper>
-        <ModalHeader> Reconnect datasources</ModalHeader>
+        <ModalHeader>Reconnect datasources</ModalHeader>
         <ModalBodyWrapper>
           <BodyContainer>
             <Title>
