30ff30650933c5cef9af255d3d834d81123cae26
Dhruvik Neharia <dhruvik@appsmith.com>
2023-05-16 15:44:39 +0530

    fix: truncate app nav page name string instead of hiding it behind overflow

--- app/client/src/pages/AppViewer/Navigation/components/MenuText.tsx
+++ app/client/src/pages/AppViewer/Navigation/components/MenuText.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import type { NavigationSetting } from "constants/AppConstants";
 import { TooltipComponent } from "design-system-old";
-import { isEllipsisActive } from "utils/helpers";
+import { isEllipsisActive, truncateString } from "utils/helpers";
 import { StyledMenuItemText } from "./MenuText.styled";
 
 type MenuTextProps = {
@@ -39,8 +39,9 @@ const MenuText = ({ name, navColorStyle, primaryColor }: MenuTextProps) => {
         primaryColor={primaryColor}
       >
         <div className="relative flex items-center justify-center flex-grow menu-item-text">
-          <span ref={tabNameRef}>{name}</span>
-          {ellipsisActive && "..."}
+          <span ref={tabNameRef}>
+            {ellipsisActive ? truncateString(name, 24) : name}
+          </span>
         </div>
       </StyledMenuItemText>
     </TooltipComponent>
