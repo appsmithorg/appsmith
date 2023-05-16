99b3f9866d412ab848120b54d9cfccedebf7af01
PiyushPushkar02 <118709669+PiyushPushkar02@users.noreply.github.com>
2023-05-16 20:24:52 +0530

    chore: analytics event for schema fetch and datasource test (#23387)
    ## Description
    
    - Added events for Data source test
    - Added events for Data source schema fetch event.
    - For data source schema fetch event failure, in case there is an
    exception thrown the event will not be captured as that requires a big
    change. This is being tracked here #23131
    
    #### PR fixes following issue(s)
    Fixes #23128 
    
    #### Type of change
    
    - New feature (non-breaking change which adds functionality)
    
    #### How Has This Been Tested?
    - [x] Manual
    
    
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Test-plan-implementation#speedbreaker-features-to-consider-for-every-change)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans/_edit#areas-of-interest)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/DatasourceStructureSolutionImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/DatasourceStructureSolutionImpl.java
@@ -2,10 +2,11 @@ package com.appsmith.server.solutions;
 
 import com.appsmith.server.helpers.PluginExecutorHelper;
 import com.appsmith.server.services.AuthenticationValidator;
-import com.appsmith.server.services.DatasourceConfigurationStructureService;
 import com.appsmith.server.services.DatasourceContextService;
 import com.appsmith.server.services.DatasourceService;
 import com.appsmith.server.services.PluginService;
+import com.appsmith.server.services.DatasourceConfigurationStructureService;
+import com.appsmith.server.services.AnalyticsService;
 import com.appsmith.server.solutions.ce.DatasourceStructureSolutionCEImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -20,9 +21,10 @@ public class DatasourceStructureSolutionImpl extends DatasourceStructureSolution
                                            DatasourceContextService datasourceContextService,
                                            AuthenticationValidator authenticationValidator,
                                            DatasourcePermission datasourcePermission,
-                                           DatasourceConfigurationStructureService datasourceConfigurationStructureService) {
+                                           DatasourceConfigurationStructureService datasourceConfigurationStructureService,
+                                           AnalyticsService analyticsService) {
 
         super(datasourceService, pluginExecutorHelper, pluginService, datasourceContextService,
-                authenticationValidator, datasourcePermission, datasourceConfigurationStructureService);
+                authenticationValidator, datasourcePermission, datasourceConfigurationStructureService,analyticsService);
     }
 }
