849ca18448ab5d34bc7915de21c287d7653169df
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2023-05-17 17:55:26 +0530

    fix: Entity_explorer_widgets_spec

--- app/client/cypress/e2e/Regression_TestSuite/ClientSideTests/ExplorerTests/Entity_Explorer_Widgets_spec.js
+++ app/client/cypress/e2e/Regression_TestSuite/ClientSideTests/ExplorerTests/Entity_Explorer_Widgets_spec.js
@@ -11,7 +11,9 @@ describe("Entity explorer tests related to widgets and validation", function () 
     cy.selectEntityByName("Container4");
     cy.get(".t--entity-collapse-toggle").eq(4).click({ force: true });
     cy.get(".t--entity-name").contains("Text1").trigger("mouseover");
-    cy.hoverAndClickParticularIndex(4);
+    cy.get("[data-testid='t--entity-item-Text1'] .entity-context-menu").click({
+      force: true,
+    });
     cy.selectAction("Show bindings");
     cy.get(apiwidget.propertyList).then(function ($lis) {
       expect($lis).to.have.length(2);
@@ -19,11 +21,15 @@ describe("Entity explorer tests related to widgets and validation", function () 
       expect($lis.eq(1)).to.contain("{{Text1.text}}");
     });
     cy.get(".t--entity-name").contains("Text1").trigger("mouseover");
-    cy.hoverAndClickParticularIndex(4);
+    cy.get("[data-testid='t--entity-item-Text1'] .entity-context-menu").click({
+      force: true,
+    });
     cy.selectAction("Edit name");
     cy.EditApiNameFromExplorer("TextUpdated");
     cy.get(".t--entity-name").contains("TextUpdated").trigger("mouseover");
-    cy.hoverAndClickParticularIndex(4);
+    cy.get(
+      "[data-testid='t--entity-item-TextUpdated'] .entity-context-menu",
+    ).click({ force: true });
     cy.selectAction("Show bindings");
     cy.get(apiwidget.propertyList).then(function ($lis) {
       expect($lis).to.have.length(2);
