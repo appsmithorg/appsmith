b27ee2ad05c4eb33b72498c25da9e88443c1d24c
Pawan Kumar <pawan.stardust@gmail.com>
2023-05-17 19:55:09 +0530

    removing tooltip hoc for now

--- app/client/packages/design-system/widgets/src/components/Tooltip/index.tsx
+++ app/client/packages/design-system/widgets/src/components/Tooltip/index.tsx
@@ -1,4 +1,3 @@
-export { withTooltip } from "./withTooltip";
 export { Tooltip } from "@design-system/headless";
 export { TooltipTrigger } from "./TooltipTrigger";
 export { TooltipContent } from "./TooltipContent";
