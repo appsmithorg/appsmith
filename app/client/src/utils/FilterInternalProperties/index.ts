0ccec86abe25af4cec971d9c0ba20ac82f78976c
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-05-17 10:05:21 +0530

    add type checks

--- app/client/src/utils/FilterInternalProperties/index.ts
+++ app/client/src/utils/FilterInternalProperties/index.ts
@@ -1,35 +1,42 @@
-import { ENTITY_TYPE } from "design-system-old";
 import { getActionChildrenPeekData } from "./Action";
-import type { DataTree } from "entities/DataTree/dataTreeFactory";
+import type {
+  DataTree,
+  DataTreeEntity,
+} from "entities/DataTree/dataTreeFactory";
 import type { JSCollectionDataState } from "reducers/entityReducers/jsActionsReducer";
 import { getWidgetChildrenPeekData } from "./Widget";
 import { getJsActionPeekData } from "./JsAction";
 import { getAppsmithPeekData } from "./Appsmith";
+import {
+  isActionEntity,
+  isWidgetEntity,
+} from "components/editorComponents/CodeEditor/codeEditorUtils";
+import {
+  isAppsmithEntity,
+  isJSAction,
+} from "ce/workers/Evaluation/evaluationUtils";
 
 export const filterInternalProperties = (
   objectName: string,
-  data: any,
+  dataTreeEntity: DataTreeEntity,
   jsActions: JSCollectionDataState,
   dataTree: DataTree,
 ) => {
-  if (!data) return;
-  const entityType: ENTITY_TYPE = data.ENTITY_TYPE;
-  switch (entityType) {
-    case ENTITY_TYPE.ACTION:
-      return getActionChildrenPeekData(objectName, dataTree)?.peekData;
-    case ENTITY_TYPE.APPSMITH:
-      return getAppsmithPeekData(dataTree).peekData;
-    case ENTITY_TYPE.JSACTION:
-      const jsAction = jsActions.find(
-        (jsAction) => jsAction.config.id === data.actionId,
-      );
-      return jsAction
-        ? getJsActionPeekData(jsAction, dataTree)?.peekData
-        : data;
-    case ENTITY_TYPE.WIDGET:
-      return getWidgetChildrenPeekData(objectName, data.type, dataTree)
-        ?.peekData;
-    default:
-      return data;
+  if (!dataTreeEntity) return;
+  if (isActionEntity(dataTreeEntity)) {
+    return getActionChildrenPeekData(objectName, dataTree)?.peekData;
+  } else if (isAppsmithEntity(dataTreeEntity)) {
+    return getAppsmithPeekData(dataTree).peekData;
+  } else if (isJSAction(dataTreeEntity)) {
+    const jsAction = jsActions.find(
+      (jsAction) => jsAction.config.id === dataTreeEntity.actionId,
+    );
+    return jsAction
+      ? getJsActionPeekData(jsAction, dataTree)?.peekData
+      : dataTreeEntity;
+  } else if (isWidgetEntity(dataTreeEntity)) {
+    return getWidgetChildrenPeekData(objectName, dataTreeEntity.type, dataTree)
+      ?.peekData;
   }
+  return dataTreeEntity;
 };
