c316cfe7838d7635972203a16f662c57f7f7abf1
Pawan Kumar <pawan.stardust@gmail.com>
2023-05-19 13:10:46 +0530

    remove unnecessary code

--- app/client/packages/design-system/widgets/src/components/Tooltip/TooltipContent.tsx
+++ app/client/packages/design-system/widgets/src/components/Tooltip/TooltipContent.tsx
@@ -8,14 +8,13 @@ import type {
 import { StyledTooltipContent } from "./index.styled";
 
 // (TODO) [Pawan] Move this constant to a common place
-const PORTAL_ID = "wds-theme-provider";
 
 export const TooltipContent = forwardRef(
   (props: HeadlessTooltipContentProps, ref: HeadlessTooltipContentRef) => {
-    const { children, portalId = PORTAL_ID, ...rest } = props;
+    const { children, ...rest } = props;
 
     return (
-      <StyledTooltipContent portalId={portalId} ref={ref} {...rest}>
+      <StyledTooltipContent ref={ref} {...rest}>
         {typeof children === "string" ? <Text>{children}</Text> : children}
       </StyledTooltipContent>
     );
