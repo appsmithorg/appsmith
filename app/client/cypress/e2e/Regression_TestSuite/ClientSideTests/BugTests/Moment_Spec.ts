ac87ec8281a710525723e3ef198761d82fd0944f
Vijetha-Kaja <vijetha@appsmith.com>
2023-05-19 16:17:38 +0530

    moment_spec.ts flaky fix

--- app/client/cypress/e2e/Regression_TestSuite/ClientSideTests/BugTests/Moment_Spec.ts
+++ app/client/cypress/e2e/Regression_TestSuite/ClientSideTests/BugTests/Moment_Spec.ts
@@ -20,6 +20,7 @@ describe("Bug #14299 - The data from the query does not show up on the widget", 
     _.agHelper.GetNClick(_.dataSources._templateMenu);
     _.agHelper.RenameWithInPane("getAstronauts");
     _.dataSources.EnterQuery(query);
+    _.agHelper.RenameWithInPane("getAstronauts");
     _.jsEditor.CreateJSObject(
       `export default {
       runAstros: () => {
@@ -107,16 +108,13 @@ describe("Bug #14299 - The data from the query does not show up on the widget", 
       _.agHelper.AssertContains("ran successfully"); //runAstros triggered on PageLaoad of Edit page!
       _.entityExplorer.ExpandCollapseEntity("Queries/JS");
       _.entityExplorer.ActionContextMenuByEntityName(
-        "getAstronauts",
-        "Delete",
-        "Are you sure?",
-      );
-      _.entityExplorer.ActionContextMenuByEntityName(
         "JSObject1",
         "Delete",
         "Are you sure?",
         true,
       );
+      _.entityExplorer.DeleteAllQueriesForDB(dsName);
+      _.agHelper.WaitUntilAllToastsDisappear();
       _.deployMode.DeployApp(
         _.locators._widgetInDeployed("tablewidget"),
         false,
