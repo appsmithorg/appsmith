7ae238d8c392b5406e4692a199a937782def5349
manish kumar <manish@appsmith.com>
2023-05-22 18:05:15 +0530

    refactoring source and dest environmentId

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce/ApplicationForkingServiceCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce/ApplicationForkingServiceCE.java
@@ -6,7 +6,7 @@ import reactor.core.publisher.Mono;
 
 public interface ApplicationForkingServiceCE {
 
-    Mono<Application> forkApplicationToWorkspaceWithEnvironment(String srcApplicationId, String targetWorkspaceId, String environmentId);
+    Mono<Application> forkApplicationToWorkspaceWithEnvironment(String srcApplicationId, String targetWorkspaceId, String sourceEnvironmentId);
 
     Mono<ApplicationImportDTO> forkApplicationToWorkspace(String srcApplicationId,
                                                           String targetWorkspaceId,
