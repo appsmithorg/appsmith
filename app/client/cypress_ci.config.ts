6811f810c32ed4d434f5d9246ba2bcb6c2c68cbf
Aishwarya UR <aishwarya@appsmith.com>
2023-05-24 08:46:14 +0530

    MySQl- trial fix, alter viewports for CI runs

--- app/client/cypress_ci.config.ts
+++ app/client/cypress_ci.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
     json: false,
   },
   chromeWebSecurity: false,
-  viewportHeight: 1200,
-  viewportWidth: 1600,
+  viewportHeight: 1000,
+  viewportWidth: 1500,
   retries: {
     runMode: 1,
     openMode: 0,
