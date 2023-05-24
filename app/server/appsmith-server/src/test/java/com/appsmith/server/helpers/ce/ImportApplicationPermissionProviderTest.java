b154d972d190ced1e240ad930624ce102567d246
Nayan <nayan@appsmith.com>
2023-05-24 13:13:20 +0600

    added tests to check create permission with permission provider

--- app/server/appsmith-server/src/test/java/com/appsmith/server/helpers/ce/ImportApplicationPermissionProviderTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/helpers/ce/ImportApplicationPermissionProviderTest.java
@@ -3,6 +3,7 @@ package com.appsmith.server.helpers.ce;
 import com.appsmith.external.models.BaseDomain;
 import com.appsmith.external.models.Datasource;
 import com.appsmith.external.models.Policy;
+import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.domains.ActionCollection;
 import com.appsmith.server.domains.Application;
 import com.appsmith.server.domains.NewAction;
@@ -17,6 +18,8 @@ import com.appsmith.server.solutions.DatasourcePermissionImpl;
 import com.appsmith.server.solutions.DomainPermission;
 import com.appsmith.server.solutions.PagePermission;
 import com.appsmith.server.solutions.PagePermissionImpl;
+import com.appsmith.server.solutions.WorkspacePermission;
+import com.appsmith.server.solutions.WorkspacePermissionImpl;
 import org.junit.jupiter.api.Test;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
@@ -33,6 +36,7 @@ class ImportApplicationPermissionProviderTest {
     PagePermission pagePermission = new PagePermissionImpl();
     ActionPermission actionPermission = new ActionPermissionImpl();
     DatasourcePermission datasourcePermission = new DatasourcePermissionImpl();
+    WorkspacePermission workspacePermission = new WorkspacePermissionImpl();
 
     @Test
     public void testCheckPermissionMethods_WhenNoPermissionProvided_ReturnsTrue() {
@@ -52,7 +56,11 @@ class ImportApplicationPermissionProviderTest {
     }
 
     @Test
-    public void tesHasEditPermissionDomains_WhenPermissionGroupDoesNotMatch_ReturnsFalse() {
+    public void tesHasEditPermissionOnDomains_WhenPermissionGroupDoesNotMatch_ReturnsFalse() {
+        // The tests are very similar, just the permission provider and domain changes, using list of tuples
+        // to iterate over the domains and related permission
+        // we'll create a permission provider for each domain and check if the edit permission is false
+
         List<Tuple2<BaseDomain, DomainPermission>> domainAndPermissionList = new ArrayList<>();
         domainAndPermissionList.add(Tuples.of(new NewPage(), pagePermission));
         domainAndPermissionList.add(Tuples.of(new NewAction(), actionPermission));
@@ -61,7 +69,9 @@ class ImportApplicationPermissionProviderTest {
 
         for(Tuple2<BaseDomain, DomainPermission> domainAndPermission : domainAndPermissionList) {
             BaseDomain domain = domainAndPermission.getT1();
+            // create a permission provider that sets edit permission on the domain
             ImportApplicationPermissionProvider provider = createPermissionProviderForDomainEditPermission(domain, domainAndPermission.getT2());
+
             if(domain instanceof NewPage) {
                 assertFalse(provider.hasEditPermission((NewPage) domain));
             } else if (domain instanceof NewAction) {
@@ -72,9 +82,33 @@ class ImportApplicationPermissionProviderTest {
                 assertFalse(provider.hasEditPermission((Datasource) domain));
             }
         }
-        NewAction domain = new NewAction();
-        ImportApplicationPermissionProvider provider = createPermissionProviderForDomainEditPermission(domain, actionPermission);
-        assertFalse(provider.hasEditPermission(domain));
+    }
+
+    @Test
+    public void tesHasCreatePermissionOnDomains_WhenPermissionGroupDoesNotMatch_ReturnsFalse() {
+        // The tests are very similar, just the permission provider and domain changes, using list of tuples
+        // to iterate over the domains and related permission
+        // we'll create a permission provider for each domain and check if the create permission is false
+
+        List<Tuple2<BaseDomain, AclPermission>> domainAndPermissionList = new ArrayList<>();
+        domainAndPermissionList.add(Tuples.of(new Application(), applicationPermission.getPageCreatePermission()));
+        domainAndPermissionList.add(Tuples.of(new NewPage(), pagePermission.getActionCreatePermission()));
+        domainAndPermissionList.add(Tuples.of(new Workspace(), workspacePermission.getDatasourceCreatePermission()));
+
+        for(Tuple2<BaseDomain, AclPermission> domainAndPermission : domainAndPermissionList) {
+            BaseDomain domain = domainAndPermission.getT1();
+            // create a permission provider that sets edit permission on the domain
+            ImportApplicationPermissionProvider provider = createPermissionProviderForDomainCreatePermission(domain, domainAndPermission.getT2());
+
+            if(domain instanceof Application) {
+                assertFalse(provider.canCreatePage((Application) domain));
+            } else if (domain instanceof NewPage) {
+                assertFalse(provider.canCreateAction((NewPage) domain));
+                assertFalse(provider.canCreateActionCollection((NewPage) domain));
+            } else if (domain instanceof Workspace) {
+                assertFalse(provider.canCreateDatasource((Workspace) domain));
+            }
+        }
     }
 
     /**
@@ -90,14 +124,7 @@ class ImportApplicationPermissionProviderTest {
      * @return
      */
     private ImportApplicationPermissionProvider createPermissionProviderForDomainEditPermission(BaseDomain baseDomain, DomainPermission domainPermission) {
-        Set<String> permissionGroups = Set.of("group1", "group2");
-
-        Policy policy = new Policy();
-        policy.setPermission(domainPermission.getEditPermission().getValue());
-        policy.getPermissionGroups().addAll(permissionGroups);
-
-        Set<Policy> policies = Set.of(policy);
-        baseDomain.setPolicies(policies);
+        setPoliciesToDomain(baseDomain, domainPermission.getEditPermission());
 
         ImportApplicationPermissionProvider.Builder builder = ImportApplicationPermissionProvider
                 .builder()
@@ -113,5 +140,46 @@ class ImportApplicationPermissionProviderTest {
             builder.editDatasourcePermission(domainPermission.getEditPermission());
         }
         return builder.build();
+    }
+
+    /**
+     * Prepares the domain and creates a permission provider to test create resource permission check on domain.
+     * The method does the following:
+     * 1. Adds a policy with the edit permission and dummy groups to the domain.
+     * 2. Creates a permission provider with the create resource permission on the domain and another permission groups.
+     * 3. Returns the permission provider.
+     * The permission group in the domain policy and the permission provider are different.
+     * Hence, the create resource permission check should fail.
+     * @param baseDomain
+     * @param permission
+     * @return
+     */
+    private ImportApplicationPermissionProvider createPermissionProviderForDomainCreatePermission(BaseDomain baseDomain, AclPermission permission) {
+        setPoliciesToDomain(baseDomain, permission);
+
+        ImportApplicationPermissionProvider.Builder builder = ImportApplicationPermissionProvider
+                .builder()
+                .userPermissionGroups(Set.of());
+
+        if(baseDomain instanceof Application) {
+            builder.createPagePermission(permission);
+        } else if(baseDomain instanceof NewPage) {
+            builder.createActionPermission(permission);
+            builder.createActionCollectionPermission(permission);
+        } else if(baseDomain instanceof Workspace) {
+            builder.createDatasourcePermission(permission);
+        }
+        return builder.build();
+    }
+
+    private void setPoliciesToDomain(BaseDomain baseDomain, AclPermission aclPermission) {
+        Set<String> permissionGroups = Set.of("group1", "group2");
+
+        Policy policy = new Policy();
+        policy.setPermission(aclPermission.getValue());
+        policy.getPermissionGroups().addAll(permissionGroups);
+
+        Set<Policy> policies = Set.of(policy);
+        baseDomain.setPolicies(policies);
     }
 }
