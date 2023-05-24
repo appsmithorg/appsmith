b6e69bb615a93d684e4b69607914aa4a7e1afc9e
Anand Srinivasan <anand.a.srinivasan@outlook.com>
2023-05-24 16:41:10 +0530

    clean up tests

--- app/client/packages/ast/src/peekOverlay/index.test.ts
+++ app/client/packages/ast/src/peekOverlay/index.test.ts
@@ -11,319 +11,168 @@ describe("extractExpressionAtPositionWholeDoc", () => {
     thisExpressionReplacement: "JsObject",
   });
 
-  let result: string | undefined;
+  const checkExpressionAtScript = async (
+    pos: number,
+    resultString?: string,
+  ) => {
+    let result;
+    try {
+      result = await scriptIdentifier.extractExpressionAtPosition(pos);
+    } catch (e) {
+      expect(e).toBe(
+        "PeekOverlayExpressionIdentifier - No expression found at position",
+      );
+    }
+    expect(result).toBe(resultString);
+  };
+
+  const checkExpressionAtJsObject = async (
+    pos: number,
+    resultString?: string,
+  ) => {
+    let result;
+    try {
+      result = await jsObjectIdentifier.extractExpressionAtPosition(pos);
+    } catch (e) {
+      expect(e).toBe(
+        "PeekOverlayExpressionIdentifier - No expression found at position",
+      );
+    }
+    expect(result).toBe(resultString);
+  };
 
   it("handles MemberExpressions", async () => {
     // nested properties
     scriptIdentifier.updateScript("Api1.data[0].id");
-
     // at position 'A'
     // 'A'pi1.data[0].id
-    result = await scriptIdentifier.extractExpressionAtPosition(0);
-    expect(result).toBe("Api1");
-
+    checkExpressionAtScript(0, "Api1");
     // Ap'i'1.data[0].id
-    result = await scriptIdentifier.extractExpressionAtPosition(2);
-    expect(result).toBe("Api1");
-
+    checkExpressionAtScript(2, "Api1");
     // Api1.'d'ata[0].id
-    result = await scriptIdentifier.extractExpressionAtPosition(6);
-    expect(result).toBe("Api1.data");
-
+    checkExpressionAtScript(6, "Api1.data");
     // Api1.data['0'].id
-    result = await scriptIdentifier.extractExpressionAtPosition(11);
-    expect(result).toBe("Api1.data[0]");
-
+    checkExpressionAtScript(11, "Api1.data[0]");
     // Api1.data[0].i'd'
-    result = await scriptIdentifier.extractExpressionAtPosition(14);
-    expect(result).toBe("Api1.data[0].id");
+    checkExpressionAtScript(14, "Api1.data[0].id");
 
-    // function call - argument hover
-    result = undefined;
+    // function call
+    // argument hover - Api1
     scriptIdentifier.updateScript(`storeValue("abc", Api1.run)`);
-    result = await scriptIdentifier.extractExpressionAtPosition(18);
-    expect(result).toBe("Api1");
-
+    checkExpressionAtScript(18, "Api1");
     scriptIdentifier.updateScript("Api1.check.run()");
     // Ap'i'1.check.run()
-    result = await scriptIdentifier.extractExpressionAtPosition(2);
-    expect(result).toBe("Api1");
-
+    checkExpressionAtScript(2, "Api1");
     // Api1.check.'r'un()
-    result = await scriptIdentifier.extractExpressionAtPosition(12);
-    expect(result).toBe("Api1.check.run");
+    checkExpressionAtScript(12, "Api1.check.run");
 
     // local varibles are filtered
     scriptIdentifier.updateScript("Api1.check.data[x].id");
-
     // Api1
-    result = await scriptIdentifier.extractExpressionAtPosition(2);
-    expect(result).toBe("Api1");
-
+    checkExpressionAtScript(2, "Api1");
     // check
-    result = await scriptIdentifier.extractExpressionAtPosition(7);
-    expect(result).toBe("Api1.check");
-
+    checkExpressionAtScript(7, "Api1.check");
     // data
-    result = await scriptIdentifier.extractExpressionAtPosition(12);
-    expect(result).toBe("Api1.check.data");
-
+    checkExpressionAtScript(12, "Api1.check.data");
     // x
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(16);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBe(undefined);
-
+    checkExpressionAtScript(16);
     // id
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(19);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBe(undefined);
+    checkExpressionAtScript(19);
   });
 
   it("handles ExpressionStatements", async () => {
     // simple statement
     scriptIdentifier.updateScript("Api1");
     // Ap'i'1
-    result = await scriptIdentifier.extractExpressionAtPosition(2);
-    expect(result).toBe("Api1");
+    checkExpressionAtScript(2, "Api1");
 
     // function call
     scriptIdentifier.updateScript(`storeValue("abc", 123)`);
     // storeValue("a'b'c", 123)
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(13);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-
+    checkExpressionAtScript(13);
     // st'o'reValue("abc", 123) - functionality not supported now
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(2);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBe(undefined);
+    checkExpressionAtScript(2);
 
     // consequent function calls
     scriptIdentifier.updateScript(`Api1.data[0].id.toFixed().toString()`);
     // toFixed
-    result = await scriptIdentifier.extractExpressionAtPosition(16);
-    expect(result).toBe("Api1.data[0].id.toFixed");
-
+    checkExpressionAtScript(16, "Api1.data[0].id.toFixed");
     // toString
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(26);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBe(undefined);
+    checkExpressionAtScript(26);
 
     // function call argument hover
-    result = undefined;
     scriptIdentifier.updateScript(`storeValue("abc", Api1)`);
-    result = await scriptIdentifier.extractExpressionAtPosition(18);
-    expect(result).toBe("Api1");
+    checkExpressionAtScript(18, "Api1");
   });
 
   it("handles BinaryExpressions", async () => {
+    // binary expression
     scriptIdentifier.updateScript(
       `Api1.data.users[0].id === "myData test" ? "Yes" : "No"`,
     );
 
     // id
-    result = await scriptIdentifier.extractExpressionAtPosition(19);
-    expect(result).toBe("Api1.data.users[0].id");
-
+    checkExpressionAtScript(19, "Api1.data.users[0].id");
     // myData
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(27);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBeUndefined();
-
+    checkExpressionAtScript(27);
     // ?
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(40);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBeUndefined();
-
+    checkExpressionAtScript(40);
     // Yes
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(43);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBeUndefined();
-
+    checkExpressionAtScript(43);
     // :
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(48);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBeUndefined();
-
+    checkExpressionAtScript(48);
     // No
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(51);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBeUndefined();
+    checkExpressionAtScript(51);
 
     // hardcoded LHS
     scriptIdentifier.updateScript(`"sample" === "myData test" ? "Yes" : "No"`);
     // sample
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(1);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
+    checkExpressionAtScript(1);
 
     // nested expressions
     scriptIdentifier.updateScript(
       `"sample" === "myData test" ? "nested" === "nested check" ? "Yes" : "No" : "No"`,
     );
     // nested
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(31);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-
+    checkExpressionAtScript(31);
     // nested check
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(44);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-
+    checkExpressionAtScript(44);
     // Yes
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(61);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-
+    checkExpressionAtScript(61);
     // No
-    try {
-      result = undefined;
-      result = await scriptIdentifier.extractExpressionAtPosition(69);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
+    checkExpressionAtScript(69);
   });
 
   it("handles JsObject cases", async () => {
     jsObjectIdentifier.updateScript(JsObjectWithThisKeyword);
 
     // this keyword cases
-    result = await jsObjectIdentifier.extractExpressionAtPosition(140);
-    expect(result).toBe("JsObject");
-
-    result = await jsObjectIdentifier.extractExpressionAtPosition(159);
-    expect(result).toBe("JsObject.numArray");
-
-    result = await jsObjectIdentifier.extractExpressionAtPosition(180);
-    expect(result).toBe("JsObject.objectArray");
-
-    result = await jsObjectIdentifier.extractExpressionAtPosition(183);
-    expect(result).toBe("JsObject.objectArray[0]");
-
-    result = await jsObjectIdentifier.extractExpressionAtPosition(186);
-    expect(result).toBe("JsObject.objectArray[0].x");
-
-    result = await jsObjectIdentifier.extractExpressionAtPosition(208);
-    expect(result).toBe("JsObject.objectData['x']");
-
-    result = await jsObjectIdentifier.extractExpressionAtPosition(238);
-    expect(result).toBe("JsObject.objectData['x']['a']");
-
-    result = await jsObjectIdentifier.extractExpressionAtPosition(243);
-    expect(result).toBe("JsObject.objectData['x']['a'].b");
+    // this
+    checkExpressionAtJsObject(140, "JsObject");
+    // numArray
+    checkExpressionAtJsObject(159, "JsObject.numArray");
+    // objectArray
+    checkExpressionAtJsObject(180, "JsObject.objectArray");
+    // [0]
+    checkExpressionAtJsObject(183, "JsObject.objectArray[0]");
+    // x
+    checkExpressionAtJsObject(186, "JsObject.objectArray[0].x");
+    // 'x'
+    checkExpressionAtJsObject(208, "JsObject.objectData['x']");
+    // 'a'
+    checkExpressionAtJsObject(238, "JsObject.objectData['x']['a']");
+    // b
+    checkExpressionAtJsObject(243, "JsObject.objectData['x']['a'].b");
 
     // await keyword cases
     // resetWidget
-    try {
-      result = undefined;
-      result = await jsObjectIdentifier.extractExpressionAtPosition(255);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBeUndefined();
-
+    checkExpressionAtJsObject(255);
     // "Switch1"
-    try {
-      result = undefined;
-      result = await jsObjectIdentifier.extractExpressionAtPosition(266);
-    } catch (e) {
-      expect(e).toBe(
-        "PeekOverlayExpressionIdentifier - No expression found at position",
-      );
-    }
-    expect(result).toBeUndefined();
-
+    checkExpressionAtJsObject(266);
     // Api1
-    result = await jsObjectIdentifier.extractExpressionAtPosition(287);
-    expect(result).toBe("Api1");
-
+    checkExpressionAtJsObject(287, "Api1");
     // run
-    result = await jsObjectIdentifier.extractExpressionAtPosition(292);
-    expect(result).toBe("Api1.run");
+    checkExpressionAtJsObject(292, "Api1.run");
   });
 });
 
