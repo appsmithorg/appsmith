80031cbeb1d2b585780f1d7951002b4303a779cc
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-05-29 14:00:23 +0800

    fix: merge conflicts

--- app/client/src/pages/Editor/DataSourceEditor/index.tsx
+++ app/client/src/pages/Editor/DataSourceEditor/index.tsx
@@ -654,7 +654,6 @@ class DatasourceEditorRouter extends React.Component<Props, State> {
                     datasourceButtonConfiguration={
                       datasourceButtonConfiguration
                     }
-                    deleteTempDSFromDraft={deleteTempDSFromDraft}
                     formData={formData}
                     getSanitizedFormData={memoize(this.getSanitizedData)}
                     isFormDirty={this.props.isFormDirty}
