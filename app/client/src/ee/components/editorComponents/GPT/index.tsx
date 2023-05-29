2bba66b05ece608f873f1fdf3e324c7e7385cd8d
arunvjn <32433245+arunvjn@users.noreply.github.com>
2023-05-29 23:20:15 +0530

    feat: ai sql generation (#1522)
    ## Description
    Adds AI powered SQL generation (Beta) for postgres queries
    - Moved accept/reject suggestions to the right.
    - Increased padding x for AI popover
    - Added 2 sizes to popover, 360px for property pane and 520px for SQL
    editors
    
    #### Type of change
    - New feature (non-breaking change which adds functionality)
    >
    >
    ## Testing
    >
    #### How Has This Been Tested?
    - [x] Manual
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Test-plan-implementation#speedbreaker-features-to-consider-for-every-change)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans/_edit#areas-of-interest)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/ee/components/editorComponents/GPT/index.tsx
+++ app/client/src/ee/components/editorComponents/GPT/index.tsx
@@ -1,13 +1,10 @@
 export * from "ce/components/editorComponents/GPT";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { AskAI } from "./AskAI";
-import type { TAssistantPrompt } from "./utils";
-import { useGPTTask } from "./utils";
 import type { TAIWrapperProps } from "ce/components/editorComponents/GPT";
 import { Popover2 } from "@blueprintjs/popover2";
-import AnalyticsUtil from "utils/AnalyticsUtil";
-import { usePrevious } from "@mantine/hooks";
+import { editorSQLModes } from "components/editorComponents/CodeEditor/sql/config";
 
 export function AIWindow(props: TAIWrapperProps) {
   if (!props.enableAIAssistance) {
@@ -18,90 +15,23 @@ export function AIWindow(props: TAIWrapperProps) {
 }
 
 function FloatingAIWindow(props: TAIWrapperProps) {
-  /**
-   * Store the AI response
-   * `null` value represents the state when AI response is either not generated or rejected.
-   */
-  const [response, setResponse] = React.useState<TAssistantPrompt | null>(null);
-  const task = useGPTTask();
+  const popoverClassName =
+    props.mode === editorSQLModes.POSTGRESQL_WITH_BINDING
+      ? "w-[520px]"
+      : "w-[380px]";
 
-  /**
-   * Called when AI response is received
-   * Stores the response and calls the update method
-   * @param value
-   */
-  const updateResponse = (value: TAssistantPrompt | null) => {
-    setResponse(value);
-    props.update?.(`{{${value?.content}}}`);
-  };
-
-  const prevOpen = usePrevious(props.isOpen);
-
-  /**
-   * Holds the default value of the property
-   */
-  const defaultValue = React.useRef<string>(props.currentValue);
-
-  useEffect(() => {
-    if (prevOpen !== props.isOpen && props.isOpen) {
-      defaultValue.current = props.currentValue;
-    }
-  }, [props.isOpen, props.currentValue, prevOpen]);
-
-  /**
-   * When this is invoked the field should already be updated.
-   * Logs analytics and closes the popover.
-   * @param query
-   * @param task
-   */
-  const acceptResponse = (query: string) => {
-    if (response) {
-      AnalyticsUtil.logEvent("AI_RESPONSE_FEEDBACK", {
-        responseId: response.messageId,
-        requestedOutputType: task,
-        liked: true,
-        generatedCode: response.content,
-        userQuery: query,
-      });
-    }
-    defaultValue.current = props.currentValue;
-    setResponse(null);
-    props.close();
-  };
-
-  /**
-   * Sets the AI response to null and restore the property to its default value.
-   * @param query
-   * @param task
-   */
-  const rejectResponse = (query: string, implicit = false) => {
-    // If response is empty, all changes are committed.
-    if (!response) return;
-    if (!query) return;
-    AnalyticsUtil.logEvent("AI_RESPONSE_FEEDBACK", {
-      responseId: response.messageId,
-      requestedOutputType: task.id,
-      liked: false,
-      implicit,
-      generatedCode: response.content,
-      userQuery: query,
-    });
-    setResponse(null);
-    props.update?.(defaultValue.current || "");
-  };
   return (
     <Popover2
       autoFocus={false}
       className="w-full"
       content={
         <AskAI
-          acceptResponse={acceptResponse}
           close={props.close}
+          currentValue={props.currentValue}
           dataTreePath={props.dataTreePath}
-          rejectResponse={rejectResponse}
-          response={response}
+          isOpen={props.isOpen}
           triggerContext={props.triggerContext}
-          updateResponse={updateResponse}
+          update={props.update}
         />
       }
       enforceFocus={false}
@@ -113,7 +43,7 @@ function FloatingAIWindow(props: TAIWrapperProps) {
         },
       }}
       placement="bottom-end"
-      popoverClassName="w-[320px]"
+      popoverClassName={popoverClassName}
       portalClassName="ai-window"
     >
       {props.children}
