1284e6be5f52e1d705788f88be79734250ea8d8c
Rajat Agrawal <rajat@appsmith.com>
2023-05-30 19:23:31 +0530

    Fix jest test failures

--- app/client/src/widgets/TableWidgetV2/widget/utilities.test.ts
+++ app/client/src/widgets/TableWidgetV2/widget/utilities.test.ts
@@ -55,7 +55,7 @@ describe("getOriginalRowIndex", () => {
       selectedRowIndex,
       "step",
     );
-    const expected = -1;
+    const expected = 1;
     expect(result).toStrictEqual(expected);
   });
 
@@ -86,7 +86,7 @@ describe("getOriginalRowIndex", () => {
       selectedRowIndex,
       "step",
     );
-    const expected = -1;
+    const expected = 1;
     expect(result).toStrictEqual(expected);
   });
 
@@ -157,7 +157,7 @@ describe("getOriginalRowIndex", () => {
       selectedRowIndex,
       "step",
     );
-    const expected = -1;
+    const expected = 1;
     expect(result).toStrictEqual(expected);
   });
 
@@ -260,8 +260,14 @@ describe("getOriginalRowIndex", () => {
         __primaryKey__: "#1",
       },
     ];
-    const result = getOriginalRowIndex(oldTableData, newTableData, 0, "");
-    const expected = -1;
+    const selectedRowIndex = 0;
+    const result = getOriginalRowIndex(
+      oldTableData,
+      newTableData,
+      selectedRowIndex,
+      "",
+    );
+    const expected = 0;
     expect(result).toStrictEqual(expected);
   });
 
@@ -312,8 +318,14 @@ describe("getOriginalRowIndex", () => {
         __primaryKey__: "#11",
       },
     ];
-    const result = getOriginalRowIndex(oldTableData, newTableData, 0, "");
-    const expected = -1;
+    const selectedRowIndex = 0;
+    const result = getOriginalRowIndex(
+      oldTableData,
+      newTableData,
+      selectedRowIndex,
+      "",
+    );
+    const expected = 0;
     expect(result).toStrictEqual(expected);
   });
 });
