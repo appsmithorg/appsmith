43c6a7aeeeba5cf4d33ee71695ee9c7279517cba
Ankit Srivastava <ankit@appsmith.com>
2023-05-31 20:14:33 +0530

    updated cypress tests for selctors through locators

--- app/client/cypress/e2e/Regression/ClientSide/Editor/HelpButton_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Editor/HelpButton_spec.ts
@@ -2,10 +2,15 @@ import * as _ from "../../../../support/Objects/ObjectsCore";
 
 describe("Help Button on editor", function () {
   it("1. Chat with us and Intercom consent should be visible on Help Menu", () => {
-    _.agHelper.GetNClick("[data-testid='t--help-button']", 0, true, 1000);
-    _.agHelper.GetNClick("#intercom-trigger", 0, true, 1000);
+    _.agHelper.GetNClick(_.debuggerHelper.locators._helpButton, 0, true, 1000);
+    _.agHelper.GetNClick(
+      _.debuggerHelper.locators._intercomOption,
+      0,
+      true,
+      1000,
+    );
     _.agHelper.GetNAssertElementText(
-      "[data-testid='t--intercom-consent-text']",
+      _.debuggerHelper.locators._intercomConsentText,
       "Can we have your email for better support?",
       "contain.text",
     );
