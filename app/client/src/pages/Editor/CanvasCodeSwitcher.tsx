3c2c087e7f9ed1e42ced108e5ea8d1e49c51ba7f
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2023-05-31 15:32:41 +0530

    fix: set tab state based on url

--- app/client/src/pages/Editor/CanvasCodeSwitcher.tsx
+++ app/client/src/pages/Editor/CanvasCodeSwitcher.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import type { SegmentedControlOption } from "design-system";
 import { SegmentedControl } from "design-system";
 import history from "utils/history";
 import { builderURL } from "RouteBuilder";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
+import { matchBuilderPath } from "constants/routes";
+import { getIsEditorInitialized } from "selectors/editorSelectors";
 
 type CanvasCodeSwitcherProps = {
   pageId: string;
@@ -11,6 +14,8 @@ type CanvasCodeSwitcherProps = {
 
 function CanvasCodeSwitcher(props: CanvasCodeSwitcherProps) {
   const dispatch = useDispatch();
+  const isEditorInitialized = useSelector(getIsEditorInitialized);
+  const location = useLocation();
   const options: SegmentedControlOption[] = [
     {
       label: "Canvas",
@@ -23,7 +28,17 @@ function CanvasCodeSwitcher(props: CanvasCodeSwitcherProps) {
   ];
   const [switcher, setSwitcher] = useState("CANVAS");
 
+  useEffect(() => {
+    if (matchBuilderPath(location.pathname)) {
+      setSwitcher("CANVAS");
+    } else {
+      setSwitcher("CODE");
+    }
+  }, [location]);
+
   const onChange = (value: string) => {
+    if (value === switcher) return;
+
     if (value === "CANVAS") {
       history.push(
         builderURL({
@@ -38,6 +53,8 @@ function CanvasCodeSwitcher(props: CanvasCodeSwitcherProps) {
 
     setSwitcher(value);
   };
+
+  if (!isEditorInitialized) return null;
 
   return (
     <SegmentedControl onChange={onChange} options={options} value={switcher} />
