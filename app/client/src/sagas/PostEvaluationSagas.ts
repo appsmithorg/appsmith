859702d60d27e95d21d46a3bef3aaa67ff874c31
Ankita Kinger <ankita@appsmith.com>
2023-05-31 16:35:25 +0530

    adding back changes of PR #23112

--- app/client/src/sagas/PostEvaluationSagas.ts
+++ app/client/src/sagas/PostEvaluationSagas.ts
@@ -336,6 +336,7 @@ export function* logSuccessfulBindings(
   isCreateFirstTree: boolean,
   isNewWidgetAdded: boolean,
   configTree: ConfigTree,
+  undefinedEvalValuesMap: Record<string, boolean>,
 ) {
   const appMode: APP_MODE | undefined = yield select(getAppMode);
   if (appMode === APP_MODE.PUBLISHED) return;
@@ -354,6 +355,10 @@ export function* logSuccessfulBindings(
       | ActionEntityConfig;
     if (isAction(entity) || isWidget(entity)) {
       const unevalValue = get(unEvalTree, evaluatedPath);
+      let isUndefined = false;
+
+      isUndefined = get(undefinedEvalValuesMap, evaluatedPath) || false;
+
       const entityType = isAction(entity)
         ? entityConfig.pluginType
         : entity.type;
@@ -398,9 +403,11 @@ export function* logSuccessfulBindings(
               unevalValue,
               entityType,
               propertyPath,
+              isUndefined,
             });
           }
         }
+
         successfulBindingPaths[evaluatedPath] = unevalValue;
       } else {
         /**Remove the binding from the map so that in case it is added again, we log it*/
