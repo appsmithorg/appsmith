0f955dc15286895906e323e7a473b0a124a118a2
Aishwarya UR <aishwarya@appsmith.com>
2023-06-01 05:18:06 +0530

    Few more fixes

--- app/client/cypress/e2e/Regression/ClientSide/Binding/MomentBasic_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Binding/MomentBasic_Spec.ts
@@ -1,44 +1,36 @@
-import { ObjectsRegistry } from "../../../../support/Objects/Registry";
-
-let dataSet: any;
-let agHelper = ObjectsRegistry.AggregateHelper,
-  ee = ObjectsRegistry.EntityExplorer,
-  propPane = ObjectsRegistry.PropertyPane,
-  locator = ObjectsRegistry.CommonLocators,
-  deployMode = ObjectsRegistry.DeployMode;
+import * as _ from "../../../../support/Objects/ObjectsCore";
 
 describe("Validate basic binding of Input widget to Input widget", () => {
   before(() => {
     cy.fixture("inputBindingdsl").then((val: any) => {
-      agHelper.AddDsl(val);
-    });
-
-    cy.fixture("testdata").then(function (data: any) {
-      dataSet = data;
+      _.agHelper.AddDsl(val);
     });
   });
 
   it("1. Input widget test with default value from another Input widget", () => {
-    ee.SelectEntityByName("Input1", "Widgets");
-    propPane.UpdatePropertyFieldValue(
-      "Default value",
-      dataSet.defaultInputBinding + "}}",
-    );
-    agHelper.ValidateNetworkStatus("@updateLayout");
-    //Binding second input widget with first input widget and validating
-    ee.SelectEntityByName("Input2");
-    propPane.UpdatePropertyFieldValue(
-      "Default value",
-      dataSet.momentInput + "}}",
-    );
-    agHelper.ValidateNetworkStatus("@updateLayout");
+    cy.fixture("testdata").then(function (dataSet: any) {
+      //dataSet = data;
+      _.entityExplorer.SelectEntityByName("Input1", "Widgets");
+      _.propPane.UpdatePropertyFieldValue(
+        "Default value",
+        dataSet.defaultInputBinding + "}}",
+      );
+      _.agHelper.ValidateNetworkStatus("@updateLayout");
+      //Binding second input widget with first input widget and validating
+      _.entityExplorer.SelectEntityByName("Input2");
+      _.propPane.UpdatePropertyFieldValue(
+        "Default value",
+        dataSet.momentInput + "}}",
+      );
+    });
+    _.agHelper.ValidateNetworkStatus("@updateLayout");
     //Publish widget and validate the data displayed in input widgets
     let currentTime = new Date();
-    deployMode.DeployApp(locator._widgetInputSelector("inputwidgetv2"));
-    cy.get(locator._widgetInputSelector("inputwidgetv2"))
+    _.deployMode.DeployApp(_.locators._widgetInputSelector("inputwidgetv2"));
+    cy.get(_.locators._widgetInputSelector("inputwidgetv2"))
       .first()
       .should("contain.value", currentTime.getFullYear());
-    cy.get(locator._widgetInputSelector("inputwidgetv2"))
+    cy.get(_.locators._widgetInputSelector("inputwidgetv2"))
       .last()
       .should("contain.value", currentTime.getFullYear());
   });
--- app/client/cypress_ci.config.ts
+++ app/client/cypress_ci.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
     json: false,
   },
   chromeWebSecurity: false,
-  viewportHeight: 1200,
-  viewportWidth: 1600,
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   retries: {
     runMode: 1,
     openMode: 0,
