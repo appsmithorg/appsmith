a72e3347f57b6268b0b8e5f6e09cbbdf6687aa94
balajisoundar <balaji@appsmith.com>
2023-06-01 22:56:05 +0530

    feat: Table one click binding for MongoDB and Postgres (#23629)
    > Pull Request Template
    >
    > Use this template to quickly create a well written pull request.
    Delete all quotes before creating the pull request.
    >
    ## Description
    > Add a TL;DR when description is extra long (helps content team)
    >
    > Please include a summary of the changes and which issue has been
    fixed. Please also include relevant motivation
    > and context. List any dependencies that are required for this change
    >
    > Links to Notion, Figma or any other documents that might be relevant
    to the PR
    >
    >
    #### PR fixes following issue(s)
    Fixes # (issue number)
    > if no issue exists, please create an issue and ask the maintainers
    about this first
    >
    >
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    > Please delete options that are not relevant.
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - Chore (housekeeping or task changes that don't impact user perception)
    - This change requires a documentation update
    >
    >
    >
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [x] Manual
    - [x] Jest
    - [x] Cypress
    >
    >
    #### Test Plan
    > One Click Binding -
    https://github.com/appsmithorg/TestSmith/issues/2390
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Test-plan-implementation#speedbreaker-features-to-consider-for-every-change)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans/_edit#areas-of-interest)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    ---------
    
    Co-authored-by: Vemparala Surya Vamsi <vamsi@appsmith.com>

---
+++ app/client/cypress/e2e/Regression/ClientSide/OneClickBinding/PropertyControl_spec.ts
@@ -0,0 +1,145 @@
+import * as _ from "../../../../support/Objects/ObjectsCore";
+import { ChooseAndAssertForm } from "./spec_utility";
+import locator from "../../../../locators/OneClickBindingLocator";
+import CommonLocators from "../../../../locators/commonlocators.json";
+import DatasourceEditor from "../../../../locators/DatasourcesEditor.json";
+
+describe("One click binding control", () => {
+  before(() => {
+    _.entityExplorer.DragDropWidgetNVerify("tablewidgetv2");
+  });
+
+  it("1.should check the datasource selector and the form", () => {
+    _.agHelper.AssertElementExist(locator.datasourceDropdownSelector);
+    _.agHelper.GetNClick(locator.datasourceDropdownSelector);
+    _.agHelper.AssertElementAbsence(locator.datasourceQueryBindHeaderSelector);
+    _.agHelper.AssertElementExist(locator.datasourceGenerateAQuerySelector);
+    _.agHelper.AssertElementExist(locator.datasourceOtherActionsSelector);
+
+    _.entityExplorer.NavigateToSwitcher("Explorer");
+    cy.wait(500);
+
+    _.dataSources.CreateMockDB("Users");
+
+    cy.wait(500);
+
+    _.dataSources.CreateQueryAfterDSSaved();
+
+    _.entityExplorer.NavigateToSwitcher("Widgets");
+
+    _.entityExplorer.NavigateToSwitcher("Explorer");
+
+    _.agHelper.GetNClick(locator.datasourceDropdownSelector);
+
+    _.agHelper.AssertElementExist(locator.datasourceQueryBindHeaderSelector);
+
+    _.agHelper.AssertElementLength(locator.datasourceQuerySelector, 1);
+
+    _.agHelper.AssertElementExist(locator.datasourceGenerateAQuerySelector);
+
+    _.agHelper.AssertElementExist(locator.datasourceSelector());
+
+    _.agHelper.AssertElementExist(locator.datasourceOtherActionsSelector);
+
+    _.agHelper.AssertElementExist(locator.otherActionSelector());
+
+    _.agHelper.AssertElementExist(
+      locator.otherActionSelector("Connect new datasource"),
+    );
+
+    _.agHelper.GetNClick(locator.otherActionSelector("Connect new datasource"));
+
+    _.agHelper.AssertElementExist(locator.datasourcePage);
+
+    _.agHelper.GetNClick(locator.backButton);
+
+    _.agHelper.GetNClick(locator.datasourceDropdownSelector);
+
+    _.agHelper.AssertElementExist(
+      locator.otherActionSelector("Insert snippet"),
+    );
+
+    _.agHelper.GetNClick(locator.otherActionSelector("Insert snippet"));
+
+    _.agHelper.AssertElementExist(CommonLocators.globalSearchModal);
+
+    _.agHelper.TypeText(CommonLocators.globalSearchInput, "{esc}", 0, true);
+
+    _.agHelper.GetNClick(locator.datasourceDropdownSelector);
+
+    _.agHelper.AssertElementExist(
+      locator.otherActionSelector("Insert binding"),
+    );
+
+    _.agHelper.GetNClick(locator.otherActionSelector("Insert binding"));
+
+    _.propPane.ValidatePropertyFieldValue("Table data", "{{}}");
+
+    _.propPane.UpdatePropertyFieldValue("Table data", "");
+
+    _.propPane.ToggleJsMode("Table data");
+
+    _.agHelper.GetNClick(locator.datasourceDropdownSelector);
+
+    _.agHelper.AssertElementAbsence(
+      locator.datasourceDropdownOptionSelector("Query1"),
+    );
+
+    _.agHelper.GetNClick(locator.datasourceQuerySelector, 0);
+
+    _.agHelper.AssertElementExist(locator.dropdownOptionSelector("Query1"));
+
+    _.propPane.ToggleJsMode("Table data");
+
+    _.propPane.ValidatePropertyFieldValue("Table data", "{{Query1.data}}");
+
+    _.propPane.UpdatePropertyFieldValue("Table data", "");
+
+    _.propPane.ToggleJsMode("Table data");
+
+    ChooseAndAssertForm("New from Users", "Users", "public.users", "gender");
+
+    _.propPane.MoveToTab("Style");
+
+    _.propPane.MoveToTab("Content");
+
+    ChooseAndAssertForm("New from sample Movies", "movies", "movies", "status");
+
+    _.entityExplorer.NavigateToSwitcher("Explorer");
+
+    _.agHelper.GetNClick("#entity-add_new_datasource");
+
+    _.agHelper.GetNClick(".t--plugin-name:contains('MongoDB')");
+
+    _.agHelper.TypeText(
+      DatasourceEditor.datasourceTitleInputLocator,
+      "myinvalidds",
+    );
+
+    _.agHelper.TypeText(
+      `[name="datasourceConfiguration.endpoints[0].host"]`,
+      "127.0.0.1",
+    );
+
+    _.agHelper.TypeText(
+      `[name="datasourceConfiguration.endpoints[0].port"]`,
+      "8000",
+    );
+
+    _.dataSources.SaveDatasource();
+
+    _.entityExplorer.NavigateToSwitcher("Widgets");
+
+    _.agHelper.GetNClick(locator.datasourceDropdownSelector);
+
+    _.agHelper.GetNClick(locator.datasourceSelector("myinvalidds"));
+
+    cy.wait("@getDatasourceStructure", { timeout: 20000 });
+
+    _.agHelper.AssertElementExist(
+      locator.tableError(
+        "Appsmith server timed out when fetching structure. Please reach out to appsmith customer support to resolve this.",
+      ),
+    );
+  });
+});
