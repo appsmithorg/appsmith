a69ee194d1b0e17b3c3fa2eb55ee3fce00a9561a
tkAppsmith <tushar@appsmith.com>
2023-06-01 16:22:27 +0530

    nullified additional entries that come up in serilization for git export

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/Datasource.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/Datasource.java
@@ -183,4 +183,10 @@ public class Datasource extends BranchAwareDomain implements Forkable<Datasource
 
         return newDs;
     }
+
+    @Override
+    public void sanitiseToExportDBObject() {
+        this.setDatasourceStorages(null);
+        super.sanitiseToExportDBObject();
+    }
 }
