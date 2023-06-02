a1b2aa51dc84522493536268378eec17fe09b1f4
Aishwarya UR <aishwarya@appsmith.com>
2023-06-02 11:43:35 +0530

    viewportWidth revert

--- app/client/cypress_ci.config.ts
+++ app/client/cypress_ci.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   chromeWebSecurity: false,
   viewportHeight: 1200,
-  viewportWidth: 1500,
+  viewportWidth: 1600,
   retries: {
     runMode: 1,
     openMode: 0,
