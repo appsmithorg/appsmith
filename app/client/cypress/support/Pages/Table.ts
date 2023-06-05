7b73390496a84d9b2975d076643e1e70a1e8fbee
Aishwarya UR <aishwarya@appsmith.com>
2023-06-05 20:56:50 +0530

    TableBugs_Spec trial fix

--- app/client/cypress/support/Pages/Table.ts
+++ app/client/cypress/support/Pages/Table.ts
@@ -498,8 +498,12 @@ export class Table {
   ) {
     this.deployMode.StubbingWindow();
     cy.url().then(($currentUrl) => {
-      this.agHelper.GetNClick(this._tableRowColumnData(row, col, tableVersion));
-      this.agHelper.Sleep(4000); //for new url to settle loading
+      this.agHelper.GetNClick(
+        this._tableRowColumnData(row, col, tableVersion),
+        0,
+        false,
+        4000,
+      ); //timeout new url to settle loading
       cy.get("@windowStub").should("be.calledOnce");
       cy.url().should("eql", expectedURL);
       this.agHelper.AssertDocumentReady();
