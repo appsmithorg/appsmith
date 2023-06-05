b14ea29a23e98c595ed6ad04a96ce22e4b19f355
Aishwarya UR <aishwarya@appsmith.com>
2023-06-05 17:35:54 +0530

    Renaming_spec fix

--- app/client/cypress/e2e/Regression/ClientSide/ExplorerTests/Renaming_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/ExplorerTests/Renaming_spec.js
@@ -1,22 +1,16 @@
 const explorer = require("../../../../locators/explorerlocators.json");
-import { ObjectsRegistry } from "../../../../support/Objects/Registry";
-let ee = ObjectsRegistry.EntityExplorer;
-let jsEditor = ObjectsRegistry.JSEditor;
+import * as _ from "../../../../support/Objects/ObjectsCore";
 
 const firstApiName = "First";
 const secondApiName = "Second";
 
 describe("Api Naming conflict on a page test", function () {
   it("1. Expects actions on the same page cannot have identical names", function () {
-    cy.log("Login Successful");
     // create an API
-    cy.NavigateToAPI_Panel();
-    cy.CreateAPI(firstApiName);
-
+    _.apiPage.CreateApi(firstApiName);
     // create another API
-    cy.NavigateToAPI_Panel();
-    cy.CreateAPI(secondApiName);
-    ee.ExpandCollapseEntity("Queries/JS", true);
+    _.apiPage.CreateApi(secondApiName);
+    _.entityExplorer.ExpandCollapseEntity("Queries/JS");
     // try to rename one of the APIs with an existing API name
     cy.get(`.t--entity-item:contains(${secondApiName})`).within(() => {
       cy.get(".t--context-menu").click({ force: true });
@@ -26,28 +20,22 @@ describe("Api Naming conflict on a page test", function () {
     cy.get(explorer.editEntity).last().type(firstApiName, { force: true });
     //cy.RenameEntity(firstApiName);
     cy.validateMessage(firstApiName);
-    cy.ClearSearch();
-    cy.get(`.t--entity-item:contains(${secondApiName})`).within(() => {
-      cy.get(".t--context-menu").click({ force: true });
-    });
-    cy.deleteActionAndConfirm();
-    cy.get(`.t--entity-item:contains(${firstApiName})`).within(() => {
-      cy.get(".t--context-menu").click({ force: true });
-    });
-    cy.deleteActionAndConfirm();
+    _.agHelper.PressEnter();
+    _.entityExplorer.ActionContextMenuByEntityName(secondApiName, "Delete");
+    _.entityExplorer.ActionContextMenuByEntityName(firstApiName, "Delete");
   });
 });
 
 describe("Api Naming conflict on different pages test", function () {
   it("2. It expects actions on different pages can have identical names", function () {
-    cy.log("Login Successful");
     // create a new API
     cy.CreateAPI(firstApiName);
-    ee.ExpandCollapseEntity("Queries/JS", true);
+    _.entityExplorer.ExpandCollapseEntity("Queries/JS", true);
+
     // create a new page and an API on that page
-    ee.AddNewPage();
+    _.entityExplorer.AddNewPage();
     cy.CreateAPI(firstApiName);
-    ee.ExpandCollapseEntity("Queries/JS", true);
+    _.entityExplorer.ExpandCollapseEntity("Queries/JS", true);
     cy.get(".t--entity-name").contains(firstApiName).should("exist");
     cy.get(`.t--entity-item:contains(${firstApiName})`).within(() => {
       cy.get(".t--context-menu").click({ force: true });
@@ -67,10 +55,9 @@ describe("Api Naming conflict on different pages test", function () {
 
 describe("Entity Naming conflict test", function () {
   it("3. Expects JS objects and actions to not have identical names on the same page.", function () {
-    cy.log("Login Successful");
-    ee.ExpandCollapseEntity("Queries/JS", true);
+    _.entityExplorer.ExpandCollapseEntity("Queries/JS", true);
     // create JS object and name it
-    jsEditor.CreateJSObject('return "Hello World";');
+    _.jsEditor.CreateJSObject('return "Hello World";');
     cy.get(`.t--entity-item:contains('JSObject1')`).within(() => {
       cy.get(".t--context-menu").click({ force: true });
     });
@@ -89,7 +76,7 @@ describe("Entity Naming conflict test", function () {
     cy.selectAction("Edit name");
 
     cy.get(explorer.editEntity).last().type(firstApiName, { force: true });
-    ee.ValidateDuplicateMessageToolTip(firstApiName);
+    _.entityExplorer.ValidateDuplicateMessageToolTip(firstApiName);
     cy.get("body").click(0, 0);
     cy.wait(2000);
     cy.get(`.t--entity-item:contains(${firstApiName})`).within(() => {
