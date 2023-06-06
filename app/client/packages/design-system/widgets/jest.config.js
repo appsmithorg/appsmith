eba0d8af6dc503f08bbe043200a51b5691f113c2
Ivan Akulov <mail@iamakulov.com>
2023-06-06 15:21:47 +0200

    design-system/widgets: inline icons similarly to design-system/headless

--- app/client/packages/design-system/widgets/jest.config.js
+++ app/client/packages/design-system/widgets/jest.config.js
@@ -2,14 +2,4 @@ module.exports = {
   preset: "ts-jest",
   roots: ["<rootDir>/src"],
   testEnvironment: "jsdom",
-  moduleNameMapper: {
-    // Mocks out all these file formats when tests are run
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "identity-obj-proxy",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  transform: {
-    "node_modules/@blueprintjs/core/.+\\.(j|t)sx?$": "ts-jest",
-  },
-  transformIgnorePatterns: ["node_modules/(?!@blueprintjs/core/.*)"],
 };
