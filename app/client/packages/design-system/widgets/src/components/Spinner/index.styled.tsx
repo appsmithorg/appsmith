cd82a701d74b7fec132eb9d5a53c5026f9835595
Ivan Akulov <mail@iamakulov.com>
2023-06-06 20:33:11 +0200

    Simplify the Spinner icon implementation
    https://github.com/appsmithorg/appsmith/pull/24010#discussion_r1220123213

--- app/client/packages/design-system/widgets/src/components/Spinner/index.styled.tsx
+++ app/client/packages/design-system/widgets/src/components/Spinner/index.styled.tsx
@@ -2,29 +2,14 @@ import React from "react";
 import styled from "styled-components";
 
 // Adapted from remixicon-react/Loader2FillIcon (https://github.com/Remix-Design/RemixIcon/blob/f88a51b6402562c6c2465f61a3e845115992e4c6/icons/System/loader-2-fill.svg)
-function LoaderIcon({
-  // children are intentionally omitted from the props, to copy the behavior of remixicon-react
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  children,
-  color = "currentColor",
-  size = 24,
-  ...props
-}: {
-  children?: React.ReactNode;
-  color?: string;
-  size?: number;
-  [key: string]: any;
-}) {
-  const className = "remixicon-icon " + (props.className || "");
-
+function LoaderIcon({ className }: { className?: string }) {
   return (
     <svg
-      {...props}
       className={className}
-      fill={color}
-      height={size}
+      fill="currentColor"
+      height={24}
       viewBox="0 0 24 24"
-      width={size}
+      width={24}
     >
       <path d="M12 2a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0V3a1 1 0 0 1 1-1Zm0 15a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Zm10-5a1 1 0 0 1-1 1h-3a1 1 0 1 1 0-2h3a1 1 0 0 1 1 1ZM7 12a1 1 0 0 1-1 1H3a1 1 0 1 1 0-2h3a1 1 0 0 1 1 1Zm12.071 7.071a1 1 0 0 1-1.414 0l-2.121-2.121a1 1 0 0 1 1.414-1.414l2.121 2.12a1 1 0 0 1 0 1.415ZM8.464 8.464a1 1 0 0 1-1.414 0L4.93 6.344a1 1 0 0 1 1.414-1.415L8.464 7.05a1 1 0 0 1 0 1.414ZM4.93 19.071a1 1 0 0 1 0-1.414l2.121-2.121a1 1 0 0 1 1.414 1.414l-2.12 2.121a1 1 0 0 1-1.415 0ZM15.536 8.464a1 1 0 0 1 0-1.414l2.12-2.121a1 1 0 1 1 1.415 1.414L16.95 8.464a1 1 0 0 1-1.414 0Z" />
     </svg>
