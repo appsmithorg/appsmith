6094dd6a8cff0feb0717925ce2a26afaa811794b
Aishwarya UR <aishwarya@appsmith.com>
2023-06-06 21:20:18 +0530

    Bug 23641 spec split

--- app/client/cypress/e2e/Regression/ClientSide/Autocomplete/Bugs_AC_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Autocomplete/Bugs_AC_Spec.ts
@@ -161,37 +161,4 @@ describe("Autocomplete bug fixes", function () {
       "console.log('hello')",
     );
   });
-
-  it("11. Bug #23641 Verifies if 'children' shows up in autocomplete list", function () {
-    _.entityExplorer.NavigateToSwitcher("Widgets");
-    _.agHelper.SelectAllWidgets();
-    _.agHelper.PressDelete();
-    _.entityExplorer.DragDropWidgetNVerify(
-      _.draggableWidgets.MULTITREESELECT,
-      200,
-      200,
-    );
-    _.entityExplorer.DragDropWidgetNVerify(
-      _.draggableWidgets.TREESELECT,
-      200,
-      400,
-    );
-    _.entityExplorer.DragDropWidgetNVerify(_.draggableWidgets.TEXT, 200, 600);
-    _.entityExplorer.SelectEntityByName("Text1");
-    _.propPane.TypeTextIntoField("Text", "{{TreeSelect1.options[0].c");
-    _.agHelper.AssertElementExist(_.locators._hints);
-    _.agHelper.GetNAssertElementText(
-      _.locators._hints,
-      "children",
-      "contain.text",
-    );
-
-    _.propPane.TypeTextIntoField("Text", "{{MultiTreeSelect1.options[0].c");
-    _.agHelper.AssertElementExist(_.locators._hints);
-    _.agHelper.GetNAssertElementText(
-      _.locators._hints,
-      "children",
-      "contain.text",
-    );
-  });
 });
