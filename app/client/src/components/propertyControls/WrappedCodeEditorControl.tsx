8d08a62f16fd622bd3a26320ffb8544fa4129cfb
balajisoundar <balaji@appsmith.com>
2023-06-06 15:58:26 +0530

    test

---
+++ app/client/src/components/propertyControls/WrappedCodeEditorControl.tsx
@@ -0,0 +1,231 @@
+import React from "react";
+import type { ControlProps } from "./BaseControl";
+import BaseControl from "./BaseControl";
+import { StyledDynamicInput } from "./StyledControls";
+import type { CodeEditorExpected } from "components/editorComponents/CodeEditor";
+import type { EditorTheme } from "components/editorComponents/CodeEditor/EditorConfig";
+import {
+  EditorModes,
+  EditorSize,
+  TabBehaviour,
+} from "components/editorComponents/CodeEditor/EditorConfig";
+import type { ColumnProperties } from "widgets/TableWidgetV2/component/Constants";
+import { isDynamicValue } from "utils/DynamicBindingUtils";
+import styled from "styled-components";
+import { isString } from "utils/helpers";
+import { JSToString, stringToJS } from "./utils";
+import type { AdditionalDynamicDataTree } from "utils/autocomplete/customTreeTypeDefCreator";
+import LazyCodeEditor from "components/editorComponents/LazyCodeEditor";
+import type { WidgetProps } from "widgets/BaseWidget";
+
+const PromptMessage = styled.span`
+  line-height: 17px;
+
+  > .code-wrapper {
+    font-family: var(--ads-v2-font-family-code);
+    display: inline-flex;
+    align-items: center;
+  }
+`;
+const CurlyBraces = styled.span`
+  color: var(--ads-v2-color-fg);
+  background-color: var(--ads-v2-color-bg-muted);
+  border-radius: 2px;
+  padding: 2px;
+  margin: 0 2px 0 0;
+  font-size: 10px;
+  font-weight: var(--ads-v2-font-weight-bold);
+`;
+
+type InputTextProp = {
+  label: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement> | string) => void;
+  evaluatedValue?: any;
+  expected?: CodeEditorExpected;
+  placeholder?: string;
+  dataTreePath?: string;
+  additionalDynamicData: AdditionalDynamicDataTree;
+  theme: EditorTheme;
+};
+
+function InputText(props: InputTextProp) {
+  const {
+    additionalDynamicData,
+    dataTreePath,
+    evaluatedValue,
+    expected,
+    onChange,
+    placeholder,
+    theme,
+    value,
+  } = props;
+  return (
+    <StyledDynamicInput>
+      <LazyCodeEditor
+        AIAssisted
+        additionalDynamicData={additionalDynamicData}
+        dataTreePath={dataTreePath}
+        evaluatedValue={evaluatedValue}
+        expected={expected}
+        input={{
+          value: value,
+          onChange: onChange,
+        }}
+        mode={EditorModes.TEXT_WITH_BINDING}
+        placeholder={placeholder}
+        promptMessage={
+          <PromptMessage>
+            Access the current cell using{" "}
+            <span className="code-wrapper">
+              <CurlyBraces>{"{{"}</CurlyBraces>
+              currentRow.columnName
+              <CurlyBraces>{"}}"}</CurlyBraces>
+            </span>
+          </PromptMessage>
+        }
+        size={EditorSize.EXTENDED}
+        tabBehaviour={TabBehaviour.INDENT}
+        theme={theme}
+      />
+    </StyledDynamicInput>
+  );
+}
+
+class WrappedCodeEditorControl extends BaseControl<WrappedCodeEditorControlProps> {
+  wrapperCode: {
+    prefix: "";
+    suffix: "";
+  };
+
+  static getBindingPrefix(tableName: string) {
+    return `{{${tableName}.processedTableData.map((currentRow, currentIndex) => ( `;
+  }
+
+  static bindingSuffix = `))}}`;
+
+  constructor(props: ControlProps) {
+    super(props);
+
+    this.wrapperCode = {
+      prefix: "",
+      suffix: "",
+    };
+  }
+
+  render() {
+    const {
+      dataTreePath,
+      defaultValue,
+      expected,
+      label,
+      propertyValue,
+      theme,
+    } = this.props;
+    const tableName = this.props.widgetProperties.widgetName;
+    const value =
+      propertyValue && isDynamicValue(propertyValue)
+        ? ComputeTablePropertyControlV2.getInputComputedValue(
+            propertyValue,
+            tableName,
+          )
+        : propertyValue
+        ? propertyValue
+        : defaultValue;
+    const evaluatedProperties = this.props.widgetProperties;
+
+    const columns: Record<string, ColumnProperties> =
+      evaluatedProperties.primaryColumns || {};
+    const currentRow: { [key: string]: any } = {};
+    Object.values(columns).forEach((column) => {
+      currentRow[column.alias || column.originalId] = undefined;
+    });
+    // Load default value in evaluated value
+    if (value && !propertyValue) {
+      this.onTextChange(value);
+    }
+    return (
+      <InputText
+        additionalDynamicData={{
+          currentRow,
+          currentIndex: -1,
+        }}
+        dataTreePath={dataTreePath}
+        expected={expected}
+        label={label}
+        onChange={this.onTextChange}
+        theme={theme}
+        value={value}
+      />
+    );
+  }
+
+  static getInputComputedValue = (propertyValue: string, tableName: string) => {
+    const bindingPrefix =
+      ComputeTablePropertyControlV2.getBindingPrefix(tableName);
+
+    if (propertyValue.includes(bindingPrefix)) {
+      const value = `${propertyValue.substring(
+        bindingPrefix.length,
+        propertyValue.length -
+          ComputeTablePropertyControlV2.bindingSuffix.length,
+      )}`;
+      return JSToString(value);
+    } else {
+      return propertyValue;
+    }
+  };
+
+  getComputedValue = (value: string, tableName: string) => {
+    if (
+      !isDynamicValue(value) &&
+      !this.props.additionalControlData?.isArrayValue
+    ) {
+      return value;
+    }
+
+    const stringToEvaluate = stringToJS(value);
+
+    if (stringToEvaluate === "") {
+      return stringToEvaluate;
+    }
+
+    return `${ComputeTablePropertyControlV2.getBindingPrefix(
+      tableName,
+    )}${stringToEvaluate}${ComputeTablePropertyControlV2.bindingSuffix}`;
+  };
+
+  onTextChange = (event: React.ChangeEvent<HTMLTextAreaElement> | string) => {
+    let value = "";
+
+    if (typeof event !== "string") {
+      value = event.target?.value;
+    } else {
+      value = event;
+    }
+
+    if (isString(value)) {
+      const output = this.getComputedValue(
+        value,
+        this.props.widgetProperties.widgetName,
+      );
+
+      this.updateProperty(this.props.propertyName, output);
+    } else {
+      this.updateProperty(this.props.propertyName, value);
+    }
+  };
+
+  static getControlType() {
+    return "WRAPPED_CODE_EDITOR";
+  }
+}
+
+export interface WrappedCodeEditorControlProps extends ControlProps {
+  wrapperCode?: {
+    prefix: string | ((widget: WidgetProps) => string);
+    suffix: string | ((widget: WidgetProps) => string);
+  };
+}
+
+export default WrappedCodeEditorControl;
