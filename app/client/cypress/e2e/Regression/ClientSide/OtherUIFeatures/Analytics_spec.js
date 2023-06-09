12bd7ad9cc7e2b96841abe2eb9ee35cdde89c482
Aishwarya UR <aishwarya@appsmith.com>
2023-06-09 14:26:17 +0530

    Analytics_spec fix

--- app/client/cypress/e2e/Regression/ClientSide/OtherUIFeatures/Analytics_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/OtherUIFeatures/Analytics_spec.js
@@ -1,5 +1,5 @@
 let appId;
-import * as _ from "../../../../support/Objects/ObjectsCore";
+import { agHelper } from "../../../../support/Objects/ObjectsCore";
 
 describe(
   "excludeForAirgap",
@@ -7,12 +7,12 @@ describe(
   function () {
     before(() => {
       cy.visit("/applications");
+      agHelper.AssertNetworkStatus("@getReleaseItems");
       cy.get(".admin-settings-menu-option").click();
       cy.get("[data-testid='APPSMITH_DISABLE_TELEMETRY']").should("be.checked"); //Bug 21191
       cy.get("[data-testid='APPSMITH_DISABLE_TELEMETRY']").uncheck({
         force: true,
-      });
-      //disabling sharing of anonymous data
+      }); //disabling sharing of anonymous data
       cy.get(".t--admin-settings-save-button").click();
       cy.wait(2000);
       cy.get(".t--admin-settings-restart-notice", { timeout: 120000 }).should(
@@ -23,8 +23,8 @@ describe(
 
     it("1. Should check analytics is not initialised when enableTelemtry is false", function () {
       cy.visit("/applications");
-      _.agHelper.AssertNetworkStatus("@getReleaseItems");
-      // _.agHelper.RefreshPage();
+      agHelper.AssertNetworkStatus("@getReleaseItems");
+      // agHelper.RefreshPage();
       // cy.wait(3000);
       // cy.wait("@getMe")
       //   //.wait("@getMe")
@@ -55,7 +55,7 @@ describe(
 
     it("2. Should check smartlook is not initialised when enableTelemtry is false", function () {
       cy.visit("/applications");
-      _.agHelper.RefreshPage(true, "getReleaseItems");
+      agHelper.RefreshPage(true, "getReleaseItems");
       cy.wait(3000);
       cy.wait("@getMe");
       cy.window().then((window) => {
@@ -79,7 +79,7 @@ describe(
 
     it("3. Should check Sentry is not initialised when enableTelemtry is false", function () {
       cy.visit("/applications");
-      _.agHelper.RefreshPage(true, "getReleaseItems");
+      agHelper.RefreshPage(true, "getReleaseItems");
       cy.wait(3000);
       cy.wait("@getMe");
       cy.window().then((window) => {
