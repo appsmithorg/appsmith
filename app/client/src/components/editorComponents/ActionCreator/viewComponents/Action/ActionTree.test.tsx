eb51866dfb21d3a53a8c2a479d692a0b122776d3
arunvjn <32433245+arunvjn@users.noreply.github.com>
2023-06-09 14:55:52 +0530

    fix: show callback button in action selector cards for chainable actions by default (#24071)
    ## Description
    show callback button in action selector cards for chain-able actions by
    default
    - Removed touched state from ActionCard that tracks interaction on card.
    - Added unit test to assert callback button render
    
    #### Fixes #23857
    
    #### Media
    <img width="272" alt="Screenshot 2023-06-06 at 13 13 27"
    src="https://github.com/appsmithorg/appsmith/assets/32433245/17a98382-988f-4a62-b15f-a026aebf45b0">
    
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    >
    >
    ## Testing
    >
    #### How Has This Been Tested?
    - [x] Manual
    - [x] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Test-plan-implementation#speedbreaker-features-to-consider-for-every-change)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans/_edit#areas-of-interest)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

---
+++ app/client/src/components/editorComponents/ActionCreator/viewComponents/Action/ActionTree.test.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { testStore } from "store";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "selectors/themeSelectors";
+import { render } from "@testing-library/react";
+import ActionTree from "./ActionTree";
+import type { TActionBlock } from "../../types";
+import { APPSMITH_GLOBAL_FUNCTIONS } from "../../constants";
+
+describe("tests for Action Tree in Action Selector", () => {
+  const store = testStore({});
+  it("callback button is rendered for chainable actions", function () {
+    const actionBlock: TActionBlock = {
+      code: "showAlert('Hello')",
+      actionType: APPSMITH_GLOBAL_FUNCTIONS.showAlert,
+      success: {
+        blocks: [],
+      },
+      error: {
+        blocks: [],
+      },
+    };
+    const component = render(
+      <Provider store={store}>
+        <ThemeProvider theme={lightTheme}>
+          <ActionTree
+            actionBlock={actionBlock}
+            id="xyz"
+            level={0}
+            onChange={() => {
+              return;
+            }}
+          />
+        </ThemeProvider>
+      </Provider>,
+    );
+
+    const callbackBtn = component.queryByTestId("t--callback-btn-xyz");
+    expect(callbackBtn).not.toBeNull();
+  });
+
+  it("callback button should not be rendered for actions that are not chainable", function () {
+    const actionBlock: TActionBlock = {
+      code: "setInterval(() => showAlert('Hello'), 1000, 'test')",
+      actionType: APPSMITH_GLOBAL_FUNCTIONS.setInterval,
+      success: {
+        blocks: [],
+      },
+      error: {
+        blocks: [],
+      },
+    };
+    const component = render(
+      <Provider store={store}>
+        <ThemeProvider theme={lightTheme}>
+          <ActionTree
+            actionBlock={actionBlock}
+            id="xyz"
+            level={0}
+            onChange={() => {
+              return;
+            }}
+          />
+        </ThemeProvider>
+      </Provider>,
+    );
+
+    const callbackBtn = component.queryByTestId("t--callback-btn-xyz");
+    expect(callbackBtn).toBeNull();
+  });
+});
