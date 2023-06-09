a3f69ec4dfc58face2daddc97ae22d2d2f0e966a
brayn003 <rudra@appsmith.com>
2023-06-09 19:19:32 +0530

    fix: temp handling git based stringification in dsl

--- app/client/packages/rts/src/services/DSLService.ts
+++ app/client/packages/rts/src/services/DSLService.ts
@@ -1,12 +1,40 @@
+import type { NestedDSL } from "@shared/dsl";
 import { flattenDSLByName, unflattenDSLByName } from "@shared/dsl";
 
-export const getFlattenedDSLForGit = (nestedDSL) => {
-  const gitFlattenedDSL = flattenDSLByName(nestedDSL);
-  return gitFlattenedDSL;
+type DSLWidget = Record<string, any>;
+
+export const getFlattenedDSLForGit = (nestedDSL: NestedDSL<DSLWidget>) => {
+  const { entities } = flattenDSLByName(nestedDSL);
+
+  const serializedWidgets = {};
+  Object.keys(entities.canvasWidgets).forEach((widgetName) => {
+    const widget = entities.canvasWidgets[widgetName];
+    let sw: string;
+    try {
+      sw = JSON.stringify(widget);
+    } catch (err) {
+      throw new Error(`Widget is not a valid object`);
+    }
+    serializedWidgets[widgetName] = sw;
+  });
+
+  return serializedWidgets;
 };
 
 export const getNestedDSLFromGit = (flattenedDSL) => {
-  const { entities, result = "0" } = flattenedDSL;
-  const nestedDSL = unflattenDSLByName(result, entities);
+  const canvasWidgets = {};
+  Object.keys(flattenedDSL).forEach((widgetName) => {
+    const sw = flattenedDSL[widgetName];
+    let widget;
+    try {
+      widget = JSON.parse(sw);
+    } catch {
+      throw new Error(`Widget structure is not valid`);
+    }
+    canvasWidgets[widgetName] = widget;
+  });
+  const entities = { canvasWidgets };
+
+  const nestedDSL = unflattenDSLByName("MainContainer", entities);
   return nestedDSL;
 };
