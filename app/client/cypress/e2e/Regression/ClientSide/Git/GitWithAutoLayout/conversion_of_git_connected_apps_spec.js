97afe3a950756a1b9949a2a740836918fbfd1108
rahulramesha <rahul@appsmith.com>
2023-06-10 01:04:57 +0530

    unskip tests since the bug has been fixed

--- app/client/cypress/e2e/Regression/ClientSide/Git/GitWithAutoLayout/conversion_of_git_connected_apps_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/Git/GitWithAutoLayout/conversion_of_git_connected_apps_spec.js
@@ -1,4 +1,11 @@
-import * as _ from "../../../../../support/Objects/ObjectsCore";
+import {
+  homePage,
+  agHelper,
+  autoLayout,
+  gitSync,
+  entityExplorer,
+  draggableWidgets,
+} from "../../../../../support/Objects/ObjectsCore";
 
 let parentBranchKey = "ParentBranch",
   childBranchKey = "ChildBranch";
@@ -6,77 +13,73 @@ let parentBranchKey = "ParentBranch",
 let repoName;
 describe("Git sync:", function () {
   before(() => {
-    _.homePage.NavigateToHome();
-    _.agHelper.GenerateUUID();
+    homePage.NavigateToHome();
+    agHelper.GenerateUUID();
     cy.get("@guid").then((uid) => {
-      _.homePage.CreateNewWorkspace("AutoLayoutGit" + uid);
-      _.homePage.CreateAppInWorkspace("AutoLayoutGit" + uid);
+      homePage.CreateNewWorkspace("AutoLayoutGit" + uid);
+      homePage.CreateAppInWorkspace("AutoLayoutGit" + uid);
     });
 
-    _.gitSync.CreateNConnectToGit();
+    gitSync.CreateNConnectToGit();
     cy.get("@gitRepoName").then((repName) => {
       repoName = repName;
     });
     cy.wait(3000);
 
-    _.gitSync.CreateGitBranch(parentBranchKey, true);
+    gitSync.CreateGitBranch(parentBranchKey, true);
     cy.get("@gitbranchName").then((branName) => {
       parentBranchKey = branName;
     });
 
-    _.gitSync.CreateGitBranch(childBranchKey, true);
+    gitSync.CreateGitBranch(childBranchKey, true);
     cy.get("@gitbranchName").then((branName) => {
       childBranchKey = branName;
     });
   });
 
   it("1. when snapshot is restored from a page created before Conversion, it should refresh in the same page", () => {
-    _.entityExplorer.DragDropWidgetNVerify(
-      _.draggableWidgets.CONTAINER,
-      100,
-      100,
-    );
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.CONTAINER, 100, 100);
 
-    _.entityExplorer.AddNewPage("New blank page");
+    entityExplorer.AddNewPage("New blank page");
 
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
+    autoLayout.ConvertToAutoLayoutAndVerify();
 
-    _.autoLayout.UseSnapshotFromBanner();
+    autoLayout.UseSnapshotFromBanner();
 
-    _.entityExplorer.VerifyIsCurrentPage("Page2");
+    entityExplorer.VerifyIsCurrentPage("Page2");
 
-    _.entityExplorer.SelectEntityByName("Page1", "Pages");
+    entityExplorer.SelectEntityByName("Page1", "Pages");
 
     cy.wait(1000);
 
-    _.entityExplorer.ActionContextMenuByEntityName("Page2");
+    entityExplorer.ActionContextMenuByEntityName("Page2");
   });
 
-  //Skipped these tests as they seemed to have regressed again, will enable them once it is fixed. #22956
-  it.skip("2. when snapshot is restored from a page created after Conversion, it should redirected to home page", () => {
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
+  it("2. when snapshot is restored from a page created after Conversion, it should redirected to home page", () => {
+    autoLayout.ConvertToAutoLayoutAndVerify();
 
-    _.entityExplorer.AddNewPage("New blank page");
+    entityExplorer.AddNewPage("New blank page");
 
-    _.autoLayout.UseSnapshotFromBanner();
+    autoLayout.UseSnapshotFromBanner();
 
-    _.entityExplorer.VerifyIsCurrentPage("Page1");
+    entityExplorer.VerifyIsCurrentPage("Page1");
   });
 
-  //Skipped these tests as they seemed to have regressed again, will enable them once it is fixed. #22956
-  it.skip("3. Switch to parentBranch and when snapshot is restored from a page created after Conversion, it should redirected to home page", () => {
+  it("3. Switch to parentBranch and when snapshot is restored from a page created after Conversion, it should redirected to home page", () => {
     cy.switchGitBranch(parentBranchKey);
 
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.CONTAINER, 100, 100);
 
-    _.entityExplorer.AddNewPage("New blank page");
+    autoLayout.ConvertToAutoLayoutAndVerify();
 
-    _.autoLayout.useSnapshotFromBanner();
+    entityExplorer.AddNewPage("New blank page");
 
-    _.entityExplorer.VerifyIsCurrentPage("Page1");
+    autoLayout.UseSnapshotFromBanner();
+
+    entityExplorer.VerifyIsCurrentPage("Page1");
   });
 
   after(() => {
-    _.gitSync.DeleteTestGithubRepo(repoName);
+    gitSync.DeleteTestGithubRepo(repoName);
   });
 });
