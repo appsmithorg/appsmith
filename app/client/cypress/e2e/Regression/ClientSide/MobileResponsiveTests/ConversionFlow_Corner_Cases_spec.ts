de9eeb0857576c5e8c5b3c365acf16e5d4643a1a
rahulramesha <71900764+rahulramesha@users.noreply.github.com>
2023-06-11 20:59:30 +0530

    chore: Exclude failing auto-layout test cases for air gapped instances (#24329)
    ## Description
    This PR is to exclude failing auto-layout test cases for air gapped
    instances and some housekeeping cleanup
    
    #### Type of change
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## Testing
    #### How Has This Been Tested?
    - [ ] Cypress
    
    
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Test-plan-implementation#speedbreaker-features-to-consider-for-every-change)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans/_edit#areas-of-interest)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/cypress/e2e/Regression/ClientSide/MobileResponsiveTests/ConversionFlow_Corner_Cases_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/MobileResponsiveTests/ConversionFlow_Corner_Cases_spec.ts
@@ -1,79 +1,85 @@
-import * as _ from "../../../../support/Objects/ObjectsCore";
+import {
+  entityExplorer,
+  draggableWidgets,
+  autoLayout,
+  homePage,
+  agHelper,
+} from "../../../../support/Objects/ObjectsCore";
 import template from "../../../../locators/TemplatesLocators.json";
 import widgetLocators from "../../../../locators/Widgets.json";
 
 describe("Handle Cases while conversion", () => {
   it("1. when snapshot is restored from a page created before Conversion, it should refresh in the same page", () => {
-    _.entityExplorer.DragDropWidgetNVerify(
-      _.draggableWidgets.CONTAINER,
-      100,
-      200,
-    );
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.CONTAINER, 100, 200);
 
-    _.entityExplorer.AddNewPage("New blank page");
+    entityExplorer.AddNewPage("New blank page");
 
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
+    autoLayout.ConvertToAutoLayoutAndVerify();
 
-    _.autoLayout.UseSnapshotFromBanner();
+    autoLayout.UseSnapshotFromBanner();
 
-    _.entityExplorer.VerifyIsCurrentPage("Page2");
+    entityExplorer.VerifyIsCurrentPage("Page2");
 
-    _.entityExplorer.SelectEntityByName("Page1", "Pages");
+    entityExplorer.SelectEntityByName("Page1", "Pages");
 
     cy.wait(1000);
 
-    _.entityExplorer.ActionContextMenuByEntityName("Page2");
+    entityExplorer.ActionContextMenuByEntityName("Page2");
   });
 
   it("2. when snapshot is restored from a page created after Conversion, it should redirected to home page", () => {
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
+    autoLayout.ConvertToAutoLayoutAndVerify();
 
-    _.entityExplorer.AddNewPage("New blank page");
+    entityExplorer.AddNewPage("New blank page");
 
-    _.autoLayout.UseSnapshotFromBanner();
+    autoLayout.UseSnapshotFromBanner();
 
-    _.entityExplorer.VerifyIsCurrentPage("Page1");
+    entityExplorer.VerifyIsCurrentPage("Page1");
   });
 
   it("3. #21969 - when app has null values in some widget's property actions or bindings, it should still convert without errors", () => {
-    _.homePage.NavigateToHome();
-    _.homePage.CreateNewApplication();
+    homePage.NavigateToHome();
+    homePage.CreateNewApplication();
 
     cy.fixture("templatePageWithNullbindings").then((val) => {
-      _.agHelper.AddDsl(val);
+      agHelper.AddDsl(val);
     });
 
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
+    autoLayout.ConvertToAutoLayoutAndVerify();
   });
 
   it("4. when app has widgets with dynamic Bindings, which have default values that are to be defined during conversion, it should convert without errors", () => {
-    _.homePage.NavigateToHome();
-    _.homePage.CreateNewApplication();
+    homePage.NavigateToHome();
+    homePage.CreateNewApplication();
 
     cy.fixture("conversionDslWithDynamicBindings").then((val) => {
-      _.agHelper.AddDsl(val);
+      agHelper.AddDsl(val);
     });
 
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
-    _.autoLayout.UseSnapshotFromBanner();
+    autoLayout.ConvertToAutoLayoutAndVerify();
+    autoLayout.UseSnapshotFromBanner();
   });
 
-  it("5. #23367 when app imports pages from a template, it should convert without any errors before refreshing the page after load", () => {
-    _.entityExplorer.AddNewPage("Add page from template");
-    _.agHelper.AssertElementVisible(template.templateDialogBox);
-    _.agHelper.GetNClick("//h1[text()='Marketing Dashboard']");
-    cy.wait(10000); // for templates page to load fully
-    _.agHelper.GetNClick(template.selectCheckbox);
-    cy.wait(1000);
-    _.agHelper.GetNClick(template.selectCheckbox, 1);
-    _.agHelper.GetNClick(template.templateViewForkButton);
-    cy.wait(5000);
-    _.agHelper.AssertContains(
-      "template added successfully",
-      "exist",
-      widgetLocators.toastAction,
-    );
+  it(
+    "excludeForAirgap",
+    "5. #23367 when app imports pages from a template, it should convert without any errors before refreshing the page after load",
+    () => {
+      entityExplorer.AddNewPage("Add page from template");
+      agHelper.AssertElementVisible(template.templateDialogBox);
+      agHelper.GetNClick("//h1[text()='Marketing Dashboard']");
+      cy.wait(10000); // for templates page to load fully
+      agHelper.GetNClick(template.selectCheckbox);
+      cy.wait(1000);
+      agHelper.GetNClick(template.selectCheckbox, 1);
+      agHelper.GetNClick(template.templateViewForkButton);
+      cy.wait(5000);
+      agHelper.AssertContains(
+        "template added successfully",
+        "exist",
+        widgetLocators.toastAction,
+      );
 
-    _.autoLayout.ConvertToAutoLayoutAndVerify();
-  });
+      autoLayout.ConvertToAutoLayoutAndVerify();
+    },
+  );
 });
