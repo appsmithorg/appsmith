1a0664530879ce40e68ded1c4830b2c04a270908
Rahul Barwal <rahul.barwal@appsmith.com>
2023-06-13 09:51:29 +0530

    Adds check to make sure that we show fork button only when user has valid workspaces to fork.

--- app/client/src/pages/Templates/TemplateViewHeader.tsx
+++ app/client/src/pages/Templates/TemplateViewHeader.tsx
@@ -8,7 +8,10 @@ import { Button, Link, Text } from "design-system";
 import { useQuery } from "pages/Editor/utils";
 import React from "react";
 import { useSelector } from "react-redux";
-import { getActiveTemplateSelector } from "selectors/templatesSelectors";
+import {
+  getActiveTemplateSelector,
+  getForkableWorkspaces,
+} from "selectors/templatesSelectors";
 import styled from "styled-components";
 import history from "utils/history";
 import ForkTemplate from "./ForkTemplate";
@@ -32,6 +35,7 @@ const SHOW_FORK_MODAL_PARAM = "showForkTemplateModal";
 function TemplateViewHeader({ templateId }: Props) {
   const currentTemplate = useSelector(getActiveTemplateSelector);
   const query = useQuery();
+  const workspaceList = useSelector(getForkableWorkspaces);
   const goBack = () => {
     history.goBack();
   };
@@ -51,21 +55,25 @@ function TemplateViewHeader({ templateId }: Props) {
       <Title kind="heading-l" renderAs="h1">
         {currentTemplate?.title}
       </Title>
-      <ForkTemplate
-        onClose={onForkModalClose}
-        showForkModal={!!query.get(SHOW_FORK_MODAL_PARAM)}
-        templateId={templateId}
-      >
-        <Button
-          className="template-fork-button"
-          data-testid="template-fork-button"
-          onClick={onForkButtonTrigger}
-          size="md"
-          startIcon="fork-2"
-        >
-          {createMessage(FORK_THIS_TEMPLATE)}
-        </Button>
-      </ForkTemplate>
+      <section>
+        {!!workspaceList.length && (
+          <ForkTemplate
+            onClose={onForkModalClose}
+            showForkModal={!!query.get(SHOW_FORK_MODAL_PARAM)}
+            templateId={templateId}
+          >
+            <Button
+              className="template-fork-button"
+              data-testid="template-fork-button"
+              onClick={onForkButtonTrigger}
+              size="md"
+              startIcon="fork-2"
+            >
+              {createMessage(FORK_THIS_TEMPLATE)}
+            </Button>
+          </ForkTemplate>
+        )}
+      </section>
     </HeaderWrapper>
   );
 }
