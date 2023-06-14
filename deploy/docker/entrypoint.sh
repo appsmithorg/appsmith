19f95588a490f0c30a5072fde77e1ecb554c7fa8
Goutham Pratapa <goutham@appsmith.com>
2023-06-14 13:59:51 +0530

    create stackpath in cloudrun based deployment

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -28,6 +28,8 @@ if [[ -n "${FILESTORE_IP_ADDRESS-}" ]]; then
   FILE_SHARE_NAME="$(echo "$FILE_SHARE_NAME" | xargs)"
 
   echo "Running appsmith for cloudRun"
+  echo "creating mount point"
+  mkdir -p /appsmith-stacks
   echo "Mounting File Sytem"
   mount -t nfs -o nolock "$FILESTORE_IP_ADDRESS:/$FILE_SHARE_NAME" /appsmith-stacks
   echo "Mounted File Sytem"
