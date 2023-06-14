76c37748020effa939f8aa76a5a667a3fa23bb70
nilansh <nilansh@appsmith.com>
2023-06-14 23:49:33 +0530

    added env for setting ff refresh rate

--- app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/FeatureFlagScheduledTaskImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/FeatureFlagScheduledTaskImpl.java
@@ -21,7 +21,7 @@ public class FeatureFlagScheduledTaskImpl implements FeatureFlagScheduledTask {
     // Number of milliseconds between the start of each scheduled calls to this method.
     @Scheduled(
             initialDelay = 2 * 60 * 60 * 1000 /* two hours */,
-            fixedRate = 2 * 60 * 60 * 1000 /* two hours */)
+            fixedDelayString = "${appsmith.feature_flag.refreshtime.minutes}")
     @Override
     public void updateFeatureFlags() {
         featureFlagService.refreshFeatureFlagsForAllUsers()
