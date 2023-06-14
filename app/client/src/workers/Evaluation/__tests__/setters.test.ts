db7f8595a3d966f1eb63a4d2c107bc69a6a973cc
Rishabh Rathod <rishabh.rathod@appsmith.com>
2023-06-14 16:02:45 +0530

    chore:  WPS unit test (#24413)
    ## Description
    
    
    Add jest test 
    
    #### PR fixes following issue(s)
    Fixes https://github.com/appsmithorg/appsmith/issues/23648
    
    #### Type of change
    
    ## Testing
    >
    #### How Has This Been Tested?
    
    - [x] Jest
    
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Test-plan-implementation#speedbreaker-features-to-consider-for-every-change)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans/_edit#areas-of-interest)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

---
+++ app/client/src/workers/Evaluation/__tests__/setters.test.ts
@@ -0,0 +1,90 @@
+import { generateDataTreeWidget } from "entities/DataTree/dataTreeWidget";
+import setters from "../setters";
+import TableWidget from "widgets/TableWidgetV2/widget";
+import { CONFIG } from "widgets/TableWidgetV2/index";
+import { RenderModes } from "constants/WidgetConstants";
+import type { ConfigTree, DataTree } from "entities/DataTree/dataTreeFactory";
+import { registerWidget } from "utils/WidgetRegisterHelpers";
+import { createEvaluationContext } from "../evaluate";
+
+registerWidget(TableWidget, CONFIG);
+
+const evalTree: DataTree = {};
+const configTree: ConfigTree = {};
+
+const tableWidgetDataTree = generateDataTreeWidget({
+  type: TableWidget.getWidgetType(),
+  widgetId: "random",
+  widgetName: "Table1",
+  children: [],
+  bottomRow: 0,
+  isLoading: false,
+  parentColumnSpace: 0,
+  parentRowSpace: 0,
+  version: 1,
+  leftColumn: 0,
+  renderMode: RenderModes.CANVAS,
+  rightColumn: 0,
+  topRow: 0,
+  primaryColumns: [],
+  tableData: [],
+});
+
+evalTree["Table1"] = tableWidgetDataTree.unEvalEntity;
+configTree["Table1"] = tableWidgetDataTree.configEntity;
+
+const evalContext = createEvaluationContext({
+  dataTree: evalTree,
+  isTriggerBased: true,
+  context: {},
+  configTree,
+});
+
+jest.mock("workers/Evaluation/handlers/evalTree", () => ({
+  get dataTreeEvaluator() {
+    return {
+      evalTree: evalContext,
+      getEvalTree: () => evalContext,
+      getConfigTree: () => configTree,
+    };
+  },
+}));
+
+jest.mock("../evalTreeWithChanges", () => ({
+  evalTreeWithChanges: () => {
+    //
+  },
+}));
+
+describe("Setter class test", () => {
+  it("Setters init method ", () => {
+    setters.init(configTree);
+
+    expect(setters.getMap()).toEqual({
+      Table1: {
+        setData: true,
+        setSelectedRowIndex: true,
+        setVisibility: true,
+      },
+    });
+  });
+
+  it("getEntitySettersFromConfig method ", async () => {
+    const methodMap = setters.getEntitySettersFromConfig(
+      tableWidgetDataTree.configEntity,
+      "Table1",
+    );
+
+    Object.assign(self, evalContext);
+
+    expect(Object.keys(methodMap)).toEqual([
+      "setVisibility",
+      "setSelectedRowIndex",
+      "setData",
+    ]);
+
+    self.Table1.setData([{ a: 1 }, { a: 2 }, { a: 3 }]);
+
+    expect(self.Table1.tableData).toEqual([{ a: 1 }, { a: 2 }, { a: 3 }]);
+  });
+});
