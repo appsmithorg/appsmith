55b168da50914e89a98884dfb3198d7abb7c557f
arunvjn <arun@appsmith.com>
2023-06-15 18:37:57 +0530

    Fixed typo

--- app/client/src/ce/components/editorComponents/GPT/trigger.tsx
+++ app/client/src/ce/components/editorComponents/GPT/trigger.tsx
@@ -3,6 +3,6 @@ import type FeatureFlags from "entities/FeatureFlags";
 export const APPSMITH_AI = "AI";
 
 /* eslint-disable-next-line */
-export function isAIEnabled(ff: FeatureFlags, modes: TEditorModes) {
+export function isAIEnabled(ff: FeatureFlags, mode: TEditorModes) {
   return false;
 }
