4d5859b35d074da4b60d6d938c6470d89a6226d7
vadim <vadim@appsmith.com>
2023-06-15 09:35:31 +0200

    feat: WDS color positive (#24086)
    ## Description
    
    tl;dr Checking whether seed falls into the interval and picking most
    distant green
    
    Fixes #23803   
    
    ## Media
    
    
    https://github.com/appsmithorg/appsmith/assets/80973/9c3510f6-1a11-4cc3-b90b-778f9c380e41
    
    
    
    ## Type of change
    
    - New feature (non-breaking change which adds functionality)
    - Chore (housekeeping or task changes that don't impact user perception)
    
    
    ## How Has This Been Tested?
    
    - Manual
    
    ### Test Plan
    Initial POC refinement, no testing necessary
    
    ## Checklist:
    ### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    ### QA activity:
    - [ ] Test plan has been approved by relevant developers
    - [ ] Test plan has been peer reviewed by QA
    - [ ] Cypress test cases have been added and approved by either SDET or
    manual QA
    - [ ] Organized project review call with relevant stakeholders after
    Round 1/2 of QA
    - [ ] Added Test Plan Approved label after reveiwing all Cypress test

--- app/client/packages/design-system/theming/src/color/DarkModeTheme.ts
+++ app/client/packages/design-system/theming/src/color/DarkModeTheme.ts
@@ -11,6 +11,7 @@ export class DarkModeTheme implements ColorModeTheme {
   private readonly seedHue: number;
   private readonly seedIsAchromatic: boolean;
   private readonly seedIsCold: boolean;
+  private readonly seedIsGreen: boolean;
   private readonly seedIsRed: boolean;
   private readonly seedIsVeryDark: boolean;
 
@@ -21,6 +22,7 @@ export class DarkModeTheme implements ColorModeTheme {
       hue,
       isAchromatic,
       isCold,
+      isGreen,
       isRed,
       isVeryDark,
       lightness,
@@ -31,6 +33,7 @@ export class DarkModeTheme implements ColorModeTheme {
     this.seedHue = hue;
     this.seedIsAchromatic = isAchromatic;
     this.seedIsCold = isCold;
+    this.seedIsGreen = isGreen;
     this.seedIsRed = isRed;
     this.seedIsVeryDark = isVeryDark;
   }
@@ -46,6 +49,7 @@ export class DarkModeTheme implements ColorModeTheme {
       bgAccentSubtleActive: this.bgAccentSubtleActive.toString(),
       bgAssistive: this.bgAssistive.toString(),
       bgNegative: this.bgNegative.toString(),
+      bgPositive: this.bgPositive.toString(),
       // fg
       fg: this.fg.toString(),
       fgAccent: this.fgAccent.toString(),
@@ -53,7 +57,7 @@ export class DarkModeTheme implements ColorModeTheme {
       fgNeutral: this.fgNeutral.toString(),
       fgOnAccent: this.fgOnAccent.toString(),
       fgOnAssistive: this.fgOnAssistive.toString(),
-      fgPositive: this.fgPositive,
+      fgPositive: this.fgPositive.toString(),
       fgWarn: this.fgWarn,
       // bd
       bdAccent: this.bdAccent.toString(),
@@ -62,6 +66,8 @@ export class DarkModeTheme implements ColorModeTheme {
       bdNegativeHover: this.bdNegativeHover.toString(),
       bdNeutral: this.bdNeutral.toString(),
       bdNeutralHover: this.bdNeutralHover.toString(),
+      bdPositive: this.bdPositive.toString(),
+      bdPositiveHover: this.bdPositiveHover.toString(),
     };
   };
 
@@ -222,6 +228,26 @@ export class DarkModeTheme implements ColorModeTheme {
 
     return color;
   }
+
+  private get bgPositive() {
+    const color = this.bgAccent.clone();
+
+    color.oklch.l = 0.62;
+    color.oklch.c = 0.17;
+    color.oklch.h = 145;
+
+    if (this.seedIsGreen && this.seedColor.oklch.c > 0.09) {
+      if (this.seedColor.oklch.h < 145) {
+        color.oklch.h = 155;
+      }
+      if (this.seedColor.oklch.h >= 145) {
+        color.oklch.h = 135;
+      }
+    }
+
+    return color;
+  }
+
   /*
    * Foreground colors
    */
@@ -256,37 +282,6 @@ export class DarkModeTheme implements ColorModeTheme {
     return color;
   }
 
-  private get fgOnAccent() {
-    const tint = this.seedColor.clone();
-    const shade = this.seedColor.clone();
-
-    if (this.seedIsAchromatic) {
-      tint.oklch.c = 0;
-      shade.oklch.c = 0;
-    }
-
-    tint.oklch.l = 0.94;
-    shade.oklch.l = 0.27;
-
-    if (-this.bgAccent.contrastAPCA(tint) < this.bgAccent.contrastAPCA(shade)) {
-      return shade;
-    }
-
-    return tint;
-  }
-
-  private get fgNeutral() {
-    return this.bdNeutral.clone();
-  }
-
-  private get fgPositive() {
-    return "#4ade80";
-  }
-
-  private get fgWarn() {
-    return "#facc15";
-  }
-
   private get fgNegative() {
     const color = this.bgNegative.clone();
     color.oklch.l = color.oklch.l + 0.05;
@@ -307,8 +302,52 @@ export class DarkModeTheme implements ColorModeTheme {
     return color;
   }
 
+  private get fgNeutral() {
+    return this.bdNeutral.clone();
+  }
+
+  private get fgOnAccent() {
+    const tint = this.seedColor.clone();
+    const shade = this.seedColor.clone();
+
+    if (this.seedIsAchromatic) {
+      tint.oklch.c = 0;
+      shade.oklch.c = 0;
+    }
+
+    tint.oklch.l = 0.94;
+    shade.oklch.l = 0.27;
+
+    if (-this.bgAccent.contrastAPCA(tint) < this.bgAccent.contrastAPCA(shade)) {
+      return shade;
+    }
+
+    return tint;
+  }
+
   private get fgOnAssistive() {
     return this.bg.clone();
+  }
+
+  private get fgPositive() {
+    const color = this.bgPositive.clone();
+
+    color.oklch.l = color.oklch.l + 0.08;
+
+    if (
+      this.seedIsGreen &&
+      !this.seedIsAchromatic &&
+      this.fgAccent.oklch.l > 0.5 &&
+      this.fgAccent.oklch.h < 145
+    ) {
+      color.oklch.h = color.oklch.h - 5;
+    }
+
+    return color;
+  }
+
+  private get fgWarn() {
+    return "#facc15";
   }
 
   /*
@@ -420,6 +459,37 @@ export class DarkModeTheme implements ColorModeTheme {
 
     if (this.bdNeutral.oklch.l >= 0.9) {
       color.oklch.l = color.oklch.l - 0.25;
+    }
+
+    return color;
+  }
+
+  private get bdPositive() {
+    const color = this.bgPositive.clone();
+
+    color.oklch.l = color.oklch.l + 0.05;
+    color.oklch.c = color.oklch.c + 0.05;
+
+    return color;
+  }
+
+  private get bdPositiveHover() {
+    const color = this.bdPositive.clone();
+
+    if (this.bdPositive.oklch.l < 0.06) {
+      color.oklch.l = color.oklch.l + 0.6;
+    }
+
+    if (this.bdPositive.oklch.l >= 0.06 && this.bdPositive.oklch.l < 0.25) {
+      color.oklch.l = color.oklch.l + 0.4;
+    }
+
+    if (this.bdPositive.oklch.l >= 0.25 && this.bdPositive.oklch.l < 0.5) {
+      color.oklch.l = color.oklch.l + 0.25;
+    }
+
+    if (this.bdPositive.oklch.l >= 0.5) {
+      color.oklch.l = color.oklch.l + 0.1;
     }
 
     return color;
