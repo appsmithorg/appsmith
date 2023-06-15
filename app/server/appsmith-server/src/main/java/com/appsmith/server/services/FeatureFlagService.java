18d936341d0860a4c38d7c50de91fec1a19c8b9c
nilansh <nilansh@appsmith.com>
2023-06-15 15:20:39 +0530

    refactoring ff to ce files

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/FeatureFlagService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/FeatureFlagService.java
@@ -1,14 +1,6 @@
 package com.appsmith.server.services;
 
-import com.appsmith.server.featureflags.FeatureFlagTrait;
 import com.appsmith.server.services.ce.FeatureFlagServiceCE;
-import reactor.core.publisher.Mono;
-
-import java.util.List;
-import java.util.Map;
 
 public interface FeatureFlagService extends FeatureFlagServiceCE {
-    Mono<Void> refreshFeatureFlagsForAllUsers();
-
-    Mono<Void> remoteSetUserTraits(List<FeatureFlagTrait> featureFlagTraits);
 }
--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/FeatureFlagServiceCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/FeatureFlagServiceCE.java
@@ -2,8 +2,10 @@ package com.appsmith.server.services.ce;
 
 import com.appsmith.server.domains.User;
 import com.appsmith.server.featureflags.FeatureFlagEnum;
+import com.appsmith.server.featureflags.FeatureFlagTrait;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Map;
 
 public interface FeatureFlagServiceCE {
@@ -35,4 +37,8 @@ public interface FeatureFlagServiceCE {
      * @return Mono<Map<String, Boolean>>
      */
     Mono<Map<String, Boolean>> getAllFeatureFlagsForUser();
+
+    Mono<Void> refreshFeatureFlagsForAllUsers();
+
+    Mono<Void> remoteSetUserTraits(List<FeatureFlagTrait> featureFlagTraits);
 }
