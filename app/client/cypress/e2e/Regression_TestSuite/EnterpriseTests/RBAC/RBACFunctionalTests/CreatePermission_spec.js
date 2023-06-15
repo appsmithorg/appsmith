7a150fa24925c6a4a97835ee013ab353253854a2
Aishwarya-U-R <91450662+Aishwarya-U-R@users.noreply.github.com>
2023-06-15 16:24:02 +0530

    test: Cypress | CreatePermission_spec fix (#1643)
    ## Description
     - This PR fixes the CreatePermission_spec 7th case - text fix
    
    #### Type of change
    - Script fix (non-breaking change which fixes an issue)
    
    ## Testing
    
    #### How Has This Been Tested?
    - [X] Cypress
    
    ## Checklist:
    #### QA activity:
    - [X] Added `Test Plan Approved` label after changes are reviewed

--- app/client/cypress/e2e/Regression_TestSuite/EnterpriseTests/RBAC/RBACFunctionalTests/CreatePermission_spec.js
+++ app/client/cypress/e2e/Regression_TestSuite/EnterpriseTests/RBAC/RBACFunctionalTests/CreatePermission_spec.js
@@ -379,8 +379,7 @@ describe("Create Permission flow ", function () {
     );
 
     // Assert can create JS action from omnibar
-    cy.get(omnibar.createNew).contains("New JS Object").click();
-    cy.wait(1000);
+    cy.get(omnibar.createNew).contains("New JS Object").click().wait(1000);
     cy.wait("@createNewJSCollection");
     cy.wait(1000);
     cy.get(".t--js-action-name-edit-field").type(jsObjectName).wait(1000);
