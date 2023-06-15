3adfacbb4368285234f0ffcf988770bc58b3c57d
Pawan Kumar <pawan.stardust@gmail.com>
2023-06-15 18:51:54 +0530

    chore: Breakdown semantic options for button (#24416)
    Fixes #24311

--- app/client/packages/design-system/theming/src/color/types.ts
+++ app/client/packages/design-system/theming/src/color/types.ts
@@ -4,30 +4,55 @@ export type ColorMode = "light" | "dark";
 
 export interface ColorModeTheme {
   getColors: () => {
+    // bg
     bg: string;
     bgAccent: string;
     bgAccentHover: string;
     bgAccentActive: string;
     bgAccentSubtleHover: string;
     bgAccentSubtleActive: string;
-    bgAssistive: string;
-    bgNegative: string;
     bgPositive: string;
+    bgPositiveHover: string;
+    bgPositiveActive: string;
+    bgPositiveSubtleHover: string;
+    bgPositiveSubtleActive: string;
+    bgNegative: string;
+    bgNegativeHover: string;
+    bgNegativeActive: string;
+    bgNegativeSubtleHover: string;
+    bgNegativeSubtleActive: string;
+    bgWarning: string;
+    bgWarningHover: string;
+    bgWarningActive: string;
+    bgWarningSubtleHover: string;
+    bgWarningSubtleActive: string;
+    bgNeutral: string;
+    bgNeutralHover: string;
+    bgNeutralActive: string;
+    bgNeutralSubtleHover: string;
+    bgNeutralSubtleActive: string;
+
+    // fg
     fg: string;
     fgAccent: string;
     fgNegative: string;
     fgNeutral: string;
-    fgOnAccent: string;
-    fgOnAssistive: string;
     fgPositive: string;
-    fgWarn: string;
+    fgWarning: string;
+    fgOnAccent: string;
+    fgOnNeutral: string;
+    fgOnPositive: string;
+    fgOnWarning: string;
+    fgOnNegative: string;
+    fgOnAssistive: string;
+    // bd
     bdAccent: string;
-    bdFocus: string;
-    bdNegative: string;
-    bdNegativeHover: string;
     bdNeutral: string;
-    bdNeutralHover: string;
     bdPositive: string;
-    bdPositiveHover: string;
+    bdWarning: string;
+    bdNegative: string;
+    bdFocus: string;
+    bdNeutralHover: string;
+    bdNegativeHover: string;
   };
 }
