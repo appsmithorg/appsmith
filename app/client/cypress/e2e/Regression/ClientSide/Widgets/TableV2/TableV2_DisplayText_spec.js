c716bd19729d0f3326a0816ae5aabbff51916645
Sangeeth Sivan <sangeeth@appsmith.com>
2023-06-16 11:55:43 +0530

    fix: table search input clear

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/TableV2_DisplayText_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/TableV2_DisplayText_spec.js
@@ -41,7 +41,7 @@ describe("Table V2 sort & filter using display text functionality", () => {
     table.ReadTableRowColumnData(0, 0, "v2").then(($cellData) => {
       expect($cellData).to.eq("X");
     });
-    cy.get(publish.searchInput).first().clear();
+    cy.get(table._searchInput).clear();
     cy.backFromPropertyPanel();
   });
 
