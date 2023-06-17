a04b77d04577ab824dd9b40421a583d2727c376d
Aishwarya UR <aishwarya@appsmith.com>
2023-06-17 21:25:19 +0530

    review comments - MySQL_Spec.ts

--- app/client/cypress/e2e/Regression/ServerSide/QueryPane/MySQL_Spec.ts
+++ app/client/cypress/e2e/Regression/ServerSide/QueryPane/MySQL_Spec.ts
@@ -15,7 +15,6 @@ describe("Validate MySQL query UI flows - Bug 14054", () => {
 
   it("2. Validate Describe & verify query response", () => {
     dataSources.NavigateFromActiveDS(dsName, true);
-    dataSources.EnterQuery("SELECT * FROM users LIMIT 10");
     agHelper.RenameWithInPane("verifyDescribe");
     runQueryNValidate("Describe customers;", [
       "Field",
@@ -49,7 +48,6 @@ describe("Validate MySQL query UI flows - Bug 14054", () => {
 
   it("3. Validate SHOW & verify query response", () => {
     dataSources.NavigateFromActiveDS(dsName, true);
-    dataSources.EnterQuery("SELECT * FROM users LIMIT 10");
     agHelper.RenameWithInPane("verifyShow");
     runQueryNValidate("SHOW tables;", ["Tables_in_fakeapi"]);
     runQueryNValidate("SHOW databases", ["Database"]);
