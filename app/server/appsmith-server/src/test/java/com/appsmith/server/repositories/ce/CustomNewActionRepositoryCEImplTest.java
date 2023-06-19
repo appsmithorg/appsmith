1c940e9ad0d28330a8a2a275a47045726c881e66
Nayan <nayan@appsmith.com>
2023-06-19 13:00:16 +0600

    added unit tests for the bulk insert and bulk update methods

--- app/server/appsmith-server/src/test/java/com/appsmith/server/repositories/ce/CustomNewActionRepositoryCEImplTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/repositories/ce/CustomNewActionRepositoryCEImplTest.java
@@ -2,12 +2,14 @@ package com.appsmith.server.repositories.ce;
 
 import com.appsmith.server.domains.NewAction;
 import com.appsmith.server.repositories.NewActionRepository;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
@@ -54,5 +56,49 @@ public class CustomNewActionRepositoryCEImplTest {
                 })
                 .expectComplete()
                 .verify();
+    }
+
+    @Test
+    public void bulkInsert_WhenDuplicateId_ExceptionThrown() {
+        String duplicateId = new ObjectId().toString();
+        List<NewAction> actionList = new ArrayList<>();
+
+        for(int i = 0; i < 2; i++) {
+            NewAction action = new NewAction();
+            action.setId(duplicateId);
+            actionList.add(action);
+        }
+
+        StepVerifier.create(newActionRepository.bulkInsert(actionList))
+                .verifyError();
+    }
+
+    @Test
+    public void bulkInsert_WhenInsertedWithProvidedId_InsertedWithProvidedId() {
+        List<NewAction> actionList = new ArrayList<>();
+       String applicationId = UUID.randomUUID().toString();
+
+        for(int i = 0; i < 5; i++) {
+            String generatedId = new ObjectId().toString();
+            NewAction action = new NewAction();
+            action.setId(generatedId);
+            action.setApplicationId(applicationId);
+            // set the id to be the workspaceId so that we can check later bulk insert used the provided id
+            action.setWorkspaceId("workspace-" + generatedId);
+            actionList.add(action);
+        }
+
+        Mono<List<NewAction>> newActionsMono = newActionRepository.bulkInsert(actionList)
+                .thenMany(newActionRepository.findByApplicationId(applicationId))
+                .collectList();
+
+        StepVerifier.create(newActionsMono)
+                .assertNext(newActions -> {
+                    assertThat(newActions.size()).isEqualTo(5);
+                    newActions.forEach(newAction -> {
+                        assertThat(newAction.getWorkspaceId()).isEqualTo("workspace-" + newAction.getId());
+                    });
+                })
+                .verifyComplete();
     }
 }
