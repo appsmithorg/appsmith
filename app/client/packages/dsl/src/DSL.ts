094c8d59c7769de5664cbcd707ef1fb5e83bc196
brayn003 <rudra@appsmith.com>
2023-06-20 21:22:29 +0530

    fix: adding more jest test cases

--- app/client/packages/dsl/src/DSL.ts
+++ app/client/packages/dsl/src/DSL.ts
@@ -1,24 +1,14 @@
-import {
-  ROOT_CONTAINER_WIDGET_ID,
-  ROOT_CONTAINER_WIDGET_NAME,
-} from "./constants";
+import { ROOT_CONTAINER_WIDGET_ID } from "./constants";
 import type { NormalizedSchema } from "normalizr";
 import { schema, normalize, denormalize } from "normalizr";
-
-// type WidgetProps = Record<string, any> & {
-//   widgetId: string;
-//   widgetName: string;
-// };
 
 export type NestedDSLWidget<W> = W & { children?: NestedDSLWidget<W>[] };
 export type NestedDSL<W> = NestedDSLWidget<W>;
 
 export type UnnestedDSLWidget<W> = W & { children?: string[] };
 export type UnnestedDSL<W> = { [widgetId: string]: UnnestedDSLWidget<W> };
-export type UnnestedGitDSL<W> = { [widgetName: string]: UnnestedDSLWidget<W> };
 
 export type UnnestedDSLEntities<W> = { canvasWidgets: UnnestedDSL<W> };
-export type UnnestedGitDSLEntities<W> = { canvasWidgets: UnnestedGitDSL<W> };
 
 // Schema by widgetId
 const SCHEMA_BY_ID = new schema.Entity(
@@ -27,14 +17,6 @@ const SCHEMA_BY_ID = new schema.Entity(
   { idAttribute: "widgetId" },
 );
 SCHEMA_BY_ID.define({ children: [SCHEMA_BY_ID] });
-
-// Schema by widgetName
-const SCHEMA_BY_NAME = new schema.Entity(
-  "canvasWidgets",
-  {},
-  { idAttribute: "widgetName" },
-);
-SCHEMA_BY_NAME.define({ children: [SCHEMA_BY_NAME] });
 
 // Normalising using widgetId
 export function unnestDSL<W>(nestedDSL: NestedDSL<W>): UnnestedDSL<W> {
@@ -54,24 +36,4 @@ export function nestDSL<W>(
 ): NestedDSL<W> {
   const entities = { canvasWidgets: unnestedDSL };
   return denormalize(widgetId, SCHEMA_BY_ID, entities);
-}
-
-// Normalising using widgetName
-export function unnestGitDSL<W>(nestedDSL: NestedDSL<W>): UnnestedGitDSL<W> {
-  const {
-    entities,
-  }: NormalizedSchema<UnnestedGitDSLEntities<W>, string> = normalize(
-    nestedDSL,
-    SCHEMA_BY_NAME,
-  );
-  return entities.canvasWidgets;
-}
-
-// Denormalising using widgetName
-export function nestGitDSL<W>(
-  unnestedDSL: UnnestedDSL<W>,
-  widgetName: string = ROOT_CONTAINER_WIDGET_NAME,
-): NestedDSL<W> {
-  const entities = { canvasWidgets: unnestedDSL };
-  return denormalize(widgetName, SCHEMA_BY_NAME, entities);
 }
--- app/client/packages/dsl/src/index.ts
+++ app/client/packages/dsl/src/index.ts
@@ -1,1 +1,10 @@
-export * from "./DSL";
+export { nestDSL, unnestDSL } from "./DSL";
+export { ROOT_CONTAINER_WIDGET_ID } from "./constants";
+
+export type {
+  NestedDSLWidget,
+  NestedDSL,
+  UnnestedDSLWidget,
+  UnnestedDSL,
+  UnnestedDSLEntities,
+} from "./DSL";
