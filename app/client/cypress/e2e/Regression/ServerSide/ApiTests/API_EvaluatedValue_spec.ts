4d77e40b41cb352da966ad710b1f96a08e255077
Ayangade Adeoluwa <adeoluayangade@yahoo.com>
2023-06-21 07:29:30 +0100

    Add cypress tests

--- app/client/cypress/e2e/Regression/ServerSide/ApiTests/API_EvaluatedValue_spec.ts
+++ app/client/cypress/e2e/Regression/ServerSide/ApiTests/API_EvaluatedValue_spec.ts
@@ -7,4 +7,14 @@ describe("Validate API URL Evaluated value", () => {
     apiPage.CreateApi("FirstAPI");
     apiPage.EnterURL(`{{{"key": "value"}}}`, true, `{"key":"value"}`);
   });
+
+  // https://github.com/appsmithorg/appsmith/issues/24696
+  it("1. Check if path field strings have not been JSON.stringified - #24696", () => {
+    apiPage.CreateApi("SecondAPI");
+    apiPage.EnterURL(
+      `https://jsonplaceholder.typicode/{{Api1.isLoading}}`,
+      true,
+      `https://jsonplaceholder.typicode/false`,
+    );
+  });
 });
