86671b1ce2e74e71cb8d7773a5177123d4d5443b
Ayangade Adeoluwa <adeoluayangade@yahoo.com>
2023-06-21 07:39:43 +0100

    Fix failing cypress test

--- app/client/cypress/e2e/Regression/ServerSide/ApiTests/API_EvaluatedValue_spec.ts
+++ app/client/cypress/e2e/Regression/ServerSide/ApiTests/API_EvaluatedValue_spec.ts
@@ -12,7 +12,7 @@ describe("Validate API URL Evaluated value", () => {
   it("1. Check if path field strings have not been JSON.stringified - #24696", () => {
     apiPage.CreateApi("SecondAPI");
     apiPage.EnterURL(
-      `https://jsonplaceholder.typicode/{{Api1.isLoading}}`,
+      `https://jsonplaceholder.typicode/{{SecondAPI.isLoading}}`,
       true,
       `https://jsonplaceholder.typicode/false`,
     );
