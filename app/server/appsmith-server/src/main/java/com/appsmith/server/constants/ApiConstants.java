638eeb79a326274eec03a73670f3e708cad04b0c
Abhijeet <abhi.nagarnaik@gmail.com>
2023-06-23 00:12:40 +0530

    Add class for API constants

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/constants/ApiConstants.java
@@ -0,0 +1,7 @@
+package com.appsmith.server.constants;
+
+public class ApiConstants {
+    public static final String INSTANCE_VERSION = "Appsmith-Version";
+    public static final String DATE = "Date";
+    public static final String APPSMITH_SIGNATURE = "Appsmith-Signature";
+}
