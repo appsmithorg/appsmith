5d853f30352376049b113f2ce55a91eba866a062
Manish Kumar <107841575+sondermanish@users.noreply.github.com>
2023-06-23 12:48:15 +0530

    chore: adding workspace-id index to environment (#1684)
    ## Description
    > TL;DR : WorkspaceId index added to environment collection.
    `getTrueEnvironmentId` call enabled for all environments
    
    Fixes #1654
    
    #### Type of change
    - Chore (housekeeping or task changes that don't impact user perception)
    
    #### How Has This Been Tested?
    - [ ] Manual
    
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/Migration110EEAddWorkspaceIdIndexInEnvironmentCollection.java
@@ -0,0 +1,38 @@
+package com.appsmith.server.migrations.db;
+
+import com.appsmith.external.models.Environment;
+import com.appsmith.server.constants.FieldName;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import static com.appsmith.server.migrations.DatabaseChangelog1.dropIndexIfExists;
+import static com.appsmith.server.migrations.DatabaseChangelog1.ensureIndexes;
+import static com.appsmith.server.migrations.DatabaseChangelog1.makeIndex;
+
+@ChangeUnit(order = "110-ee", id = "add-ws-id-index-to-environment")
+public class Migration110EEAddWorkspaceIdIndexInEnvironmentCollection {
+
+    private final MongoTemplate mongoTemplate;
+
+    private static final String workspaceIdIndexName = "environment_wsId_deleted_deletedAt";
+
+    public Migration110EEAddWorkspaceIdIndexInEnvironmentCollection(MongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @RollbackExecution
+    public void rollBackExecution() {
+        // We don't need a rollback strategy because we don't care about this value anymore
+    }
+    @Execution
+    public void executeMigration() {
+        dropIndexIfExists(mongoTemplate, Environment.class, workspaceIdIndexName);
+
+        ensureIndexes(mongoTemplate, Environment.class,
+                makeIndex(FieldName.WORKSPACE_ID, FieldName.DELETED, FieldName.DELETED_AT)
+                        .named(workspaceIdIndexName)
+        );
+    }
+}
