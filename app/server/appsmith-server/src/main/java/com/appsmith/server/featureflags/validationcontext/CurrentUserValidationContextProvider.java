0302d99bb08a15da211525db4230954722504059
tkAppsmith <tushar@appsmith.com>
2023-06-26 13:21:37 +0530

    added missing component annotation resulting in autowiring failure

--- app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/validationcontext/CurrentUserValidationContextProvider.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/validationcontext/CurrentUserValidationContextProvider.java
@@ -3,8 +3,10 @@ package com.appsmith.server.featureflags.validationcontext;
 import com.appsmith.server.domains.User;
 import com.appsmith.server.services.SessionUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Component
 public class CurrentUserValidationContextProvider implements FeatureFlagValidationContextProvider<Mono<User>> {
 
     private final SessionUserService sessionUserService;
