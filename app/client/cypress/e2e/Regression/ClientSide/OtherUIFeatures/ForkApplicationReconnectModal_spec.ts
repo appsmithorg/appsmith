9ffae12be3fdafe0c0388b4f89c897149515838d
“sneha122” <sneha@appsmith.com>
2023-06-26 17:55:19 +0530

    fix: cypress test added for forking critical issue

---
+++ app/client/cypress/e2e/Regression/ClientSide/OtherUIFeatures/ForkApplicationReconnectModal_spec.ts
@@ -0,0 +1,52 @@
+import {
+  dataSources,
+  homePage,
+  agHelper,
+} from "../../../../support/Objects/ObjectsCore";
+
+let currentWorkspace: string, currentAppName: string, forkWorkspaceName: string;
+
+describe("Fork application across workspaces", function () {
+  it("1. Signed user should be able to fork a public forkable app & Check if the forked application has the same dsl as the original", function () {
+    homePage.NavigateToHome();
+    // Create new workspace to create App in
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      currentWorkspace = "CurrentWorkspace " + uid;
+      currentAppName = "MongoQueryApp " + uid;
+      homePage.CreateNewWorkspace(currentWorkspace);
+      homePage.CreateAppInWorkspace(currentWorkspace, currentAppName);
+
+      // Create datasource and query
+      dataSources.CreateDataSource("Mongo", true, false);
+      dataSources.CreateQueryAfterDSSaved("", "Query1");
+
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(2000);
+      homePage.NavigateToHome();
+
+      agHelper.GenerateUUID();
+      cy.get("@guid").then((uid) => {
+        forkWorkspaceName = "ForkApplication" + uid;
+        homePage.CreateNewWorkspace(forkWorkspaceName);
+        homePage.FilterApplication(currentAppName);
+
+        agHelper.Sleep(500);
+
+        homePage.ForkApplication(currentAppName, forkWorkspaceName);
+
+        cy.get("@dsName").then(($dsName) => {
+          dataSources.FillMongoDSForm();
+          dataSources.TestDatasource();
+        });
+      });
+    });
+  });
+
+  after(() => {
+    homePage.DeleteApplication(currentAppName);
+    homePage.DeleteApplication(currentAppName);
+    homePage.DeleteWorkspace(currentWorkspace);
+    homePage.DeleteWorkspace(forkWorkspaceName);
+  });
+});
