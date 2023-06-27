93df86a52a43a5244d4da48bf76809ad6805a828
arunvjn <arun@appsmith.com>
2023-06-27 18:11:42 +0530

    temp commit

---
+++ app/client/src/Linting/lib/entity/EntityTree.ts
@@ -0,0 +1,96 @@
+import type {
+  ConfigTree,
+  DataTree,
+  DataTreeEntity,
+} from "entities/DataTree/dataTreeFactory";
+import type { IEntity } from ".";
+import type { Diff } from "deep-diff";
+import EntityFactory from ".";
+import { PathUtils } from "Linting/utils/pathUtils";
+import { isJSAction } from "ce/workers/Evaluation/evaluationUtils";
+import type { EntityParser } from "Linting/utils/entityParser";
+import { jsLintEntityParser } from "Linting/utils/entityParser";
+import type { EntityDiffGenerator } from "Linting/utils/diffGenerator";
+import { jsLintDiffGenerator } from "Linting/utils/diffGenerator";
+
+export abstract class EntityTree {
+  protected tree = new Map<string, IEntity>();
+  protected unEvalTree: DataTree = {};
+  protected configTree: ConfigTree = {};
+  constructor(unEvalTree: DataTree, configTree: ConfigTree) {
+    this.unEvalTree = unEvalTree;
+    this.configTree = configTree;
+  }
+  abstract buildTree(unEvalTree: DataTree, configTree: ConfigTree): void;
+  computeDifferences(tree?: EntityTree) {
+    const differences: Diff<unknown>[] = [];
+    if (!tree) return differences;
+    for (const [name, entity] of this.tree.entries()) {
+      const otherEntity = tree.getEntityByName(name);
+      if (!otherEntity) continue;
+      const difference = entity.computeDifference(otherEntity);
+      if (!difference) continue;
+      differences.push(...difference);
+    }
+    return differences;
+  }
+
+  getAllPaths = (): Record<string, true> => {
+    return PathUtils.getAllPaths(this.unEvalTree);
+  };
+
+  getRawTree() {
+    const rawTree: DataTree = {};
+    for (const [name, entity] of this.tree.entries()) {
+      rawTree[name] = entity.getRawEntity() as DataTreeEntity;
+    }
+    return rawTree as DataTree;
+  }
+
+  getEntityByName(name: string) {
+    return this.tree.get(name);
+  }
+
+  getEntities() {
+    const entities = Array.from(this.tree.values());
+    return entities;
+  }
+}
+
+export interface EntityClassLoader {
+  load(entity: DataTreeEntity): {
+    parser?: EntityParser;
+    diffGenerator?: EntityDiffGenerator;
+  };
+}
+
+class LintEntityClassLoader implements EntityClassLoader {
+  load(entity: DataTreeEntity) {
+    if (isJSAction(entity)) {
+      return {
+        parser: jsLintEntityParser,
+        diffGenerator: jsLintDiffGenerator,
+      };
+    }
+    return {
+      parser: undefined,
+      diffGenerator: undefined,
+    };
+  }
+}
+
+export class LintEntityTree extends EntityTree {
+  constructor(unEvalTree: DataTree, configTree: ConfigTree) {
+    super(unEvalTree, configTree);
+    this.buildTree(unEvalTree, configTree);
+  }
+  buildTree(unEvalTree: DataTree, configTree: ConfigTree): void {
+    const entities = Object.entries(unEvalTree);
+    const classLoader = new LintEntityClassLoader();
+    for (const [name, entity] of entities) {
+      const config = configTree[name];
+      if (!config) continue;
+      this.tree.set(name, EntityFactory.getEntity(entity, config, classLoader));
+    }
+  }
+}
