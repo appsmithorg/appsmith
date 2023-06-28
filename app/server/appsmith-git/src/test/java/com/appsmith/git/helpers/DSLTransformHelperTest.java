826a6281350e3884f4bea44eb4c96bcb20008d62
Anagh Hegde <anagh@appsmith.com>
2023-06-28 20:13:50 +0530

    fix: Delete widgets when the parent is updated

---
+++ app/server/appsmith-git/src/test/java/com/appsmith/git/helpers/DSLTransformHelperTest.java
@@ -0,0 +1,9 @@
+package com.appsmith.git.helpers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class DSLTransformHelperTest {
+
+}
