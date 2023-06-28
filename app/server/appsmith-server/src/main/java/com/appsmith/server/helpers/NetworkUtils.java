e2cd4badfe5929673d92da81fad5b1dca00f4c68
Abhijeet <41686026+abhvsn@users.noreply.github.com>
2023-06-28 14:56:05 +0530

    chore: Code split for network utils (#1708)
    ## Description
    PR to code split the Network utils class which is being used and
    modified in both CE and EE repo. This will fix the merge conflicts we
    were seeing recently for NetworkUtils class.
    
    CE PR: https://github.com/appsmithorg/appsmith/pull/24849
    
    Fixes https://github.com/appsmithorg/appsmith/issues/24271
    
    #### Type of change
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## Testing
    #### How Has This Been Tested?
    - [x] Manual
    
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/NetworkUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/NetworkUtils.java
@@ -1,57 +1,26 @@
 package com.appsmith.server.helpers;
 
 import com.appsmith.server.configurations.AirgapInstanceConfig;
-import com.appsmith.util.WebClientUtils;
+import com.appsmith.server.helpers.ce.NetworkUtilsCE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.time.Duration;
-
 @Component
-@Slf4j
-public class NetworkUtils {
+public class NetworkUtils extends NetworkUtilsCE {
 
-    private static final URI GET_IP_URI = URI.create("https://api64.ipify.org");
+    private final AirgapInstanceConfig airgapInstanceConfig;
 
-    private static String cachedAddress = null;
-
-    private static final String FALLBACK_IP = "unknown";
-
-    @Autowired
-    public NetworkUtils(@Autowired AirgapInstanceConfig airgapInstanceConfig) {
-        if (airgapInstanceConfig.isAirgapEnabled()) {
-            cachedAddress = "unknown";
-        }
+    public NetworkUtils(AirgapInstanceConfig airgapInstanceConfig) {
+        this.airgapInstanceConfig = airgapInstanceConfig;
     }
 
-    /**
-     * This method hits an API endpoint that returns the external IP address of this server instance.
-     *
-     * @return A publisher that yields the IP address.
-     */
-    public static Mono<String> getExternalAddress() {
-        if (cachedAddress != null) {
-            return Mono.just(cachedAddress);
+    @Override
+    public Mono<String> getExternalAddress() {
+        if (airgapInstanceConfig.isAirgapEnabled()) {
+            return Mono.just(FALLBACK_IP);
         }
-
-       return WebClientUtils
-               .create()
-               .get()
-               .uri(GET_IP_URI)
-               .retrieve()
-               .bodyToMono(String.class)
-               .map(address -> {
-                   cachedAddress = address;
-                   return address;
-               })
-               .timeout(Duration.ofSeconds(60))
-               .onErrorResume(throwable -> {
-                   log.debug("Unable to get the machine ip: ", throwable);
-                   return Mono.just(FALLBACK_IP);
-               });
+        return super.getExternalAddress();
     }
 
 }
