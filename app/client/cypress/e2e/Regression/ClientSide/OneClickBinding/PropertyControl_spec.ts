de53a6abf4d7339af0e4a581a67c88d7d19a2e31
Ayangade Adeoluwa <37867493+Irongade@users.noreply.github.com>
2023-06-29 17:19:31 +0100

    fix: mock db creation flow (#24840)
    When a new Mock DB is created, we navigate to the Active Tab in the
    Datasources page. This PR changes that behaviour and instead navigates
    the user to the Datasource view page.

--- app/client/cypress/e2e/Regression/ClientSide/OneClickBinding/PropertyControl_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/OneClickBinding/PropertyControl_spec.ts
@@ -34,7 +34,7 @@ describe("excludeForAirgap", "One click binding control", () => {
     entityExplorer.NavigateToSwitcher("Explorer", 0, true);
 
     dataSources.CreateMockDB("Users").then(($createdMockUsers) => {
-      dataSources.CreateQueryFromActiveTab($createdMockUsers, false);
+      dataSources.CreateQueryFromActiveTab($createdMockUsers, true);
     });
 
     cy.wait(500);
