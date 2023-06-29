3b4a7a60508535ec2243bfb65deab7cf3a6b951f
Aishwarya UR <aishwarya@appsmith.com>
2023-06-29 12:09:32 +0530

    API_EvaluatedValue_spec.ts fix

--- app/client/cypress/e2e/Regression/ServerSide/ApiTests/API_EvaluatedValue_spec.ts
+++ app/client/cypress/e2e/Regression/ServerSide/ApiTests/API_EvaluatedValue_spec.ts
@@ -1,19 +1,17 @@
-import { ObjectsRegistry } from "../../../../support/Objects/Registry";
-
-const apiPage = ObjectsRegistry.ApiPage;
+import { apiPage, hostPort } from "../../../../support/Objects/ObjectsCore";
 
 describe("Validate API URL Evaluated value", () => {
   it("1. Check if url object does not crash App", () => {
     apiPage.CreateApi("FirstAPI");
-    apiPage.EnterURL(`{{{"key": "value"}}}`, true, `{"key":"value"}`);
+    apiPage.EnterURL(`{{{"key": "value"}}}`, `{"key":"value"}`);
   });
 
   // https://github.com/appsmithorg/appsmith/issues/24696
   it("2. Check if path field strings have not been JSON.stringified - #24696", () => {
     apiPage.CreateApi("SecondAPI");
     apiPage.EnterURL(
-      `https://jsonplaceholder.typicode/{{SecondAPI.isLoading}}`,
-      `https://jsonplaceholder.typicode/false`,
+      hostPort.mockApiUrl + `/{{SecondAPI.isLoading}}`,
+      hostPort.mockApiUrl + `/false`,
     );
   });
 });
