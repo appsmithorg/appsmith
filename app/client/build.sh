d7505e77c9cc5aff689eed5931fec887339ba5db
Valera Melnikov <valera@appsmith.com>
2023-06-30 12:45:07 +0300

    chore: disable cra eslint (#24953)
    ## Description
    Disable CRA built-in ESLint checks since we have our own config and a
    separate step for this
    
    #### Type of change
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [x] Manual
    - [ ] Jest
    - [ ] Cypress
    
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    Co-authored-by: Valera Melnikov <melnikov.vv@greendatasoft.ru>

--- app/client/build.sh
+++ app/client/build.sh
@@ -18,6 +18,8 @@ fi
 # build cra app
 export REACT_APP_SENTRY_RELEASE=$GIT_SHA
 export REACT_APP_CLIENT_LOG_LEVEL=ERROR
+# Disable CRA built-in ESLint checks since we have our own config and a separate step for this
+export DISABLE_ESLINT_PLUGIN=true
 craco --max-old-space-size=7168 build --config craco.build.config.js
 
 if [ "$GITHUB_REPOSITORY" == "appsmithorg/appsmith-ee" ]; then
