dd142c38e6eb61796f9a72ebad41eb41c078b6b4
Sumit Kumar <sumit@appsmith.com>
2023-07-04 17:55:15 +0530

    remove redundant code from cypress tc.

--- app/client/cypress/e2e/Sanity/Datasources/Port_Number_Placeholder_Spec.ts
+++ app/client/cypress/e2e/Sanity/Datasources/Port_Number_Placeholder_Spec.ts
@@ -3,12 +3,7 @@ import { agHelper, dataSources } from "../../../support/Objects/ObjectsCore";
 describe("Test placeholder value for port number for all datasources - tests #24960", () => {
   it("1. Test MsSQL datasource port number placeholder", () => {
     dataSources.NavigateToDSCreateNew();
-    agHelper.GenerateUUID();
-    cy.get("@guid").then((uid) => {
-      // using CreatePlugIn function instead of CreateDatasource,
-      // because I do not need to fill the datasource form and use the same default data
-      dataSources.CreatePlugIn("Microsoft SQL Server");
-    });
+    dataSources.CreatePlugIn("Microsoft SQL Server");
 
     const expectedPlaceholderValue = "1433";
     agHelper.AssertAttribute(
@@ -21,12 +16,7 @@ describe("Test placeholder value for port number for all datasources - tests #24
 
   it("2. Test Oracle plugin datasource port number placeholder", () => {
     dataSources.NavigateToDSCreateNew();
-    agHelper.GenerateUUID();
-    cy.get("@guid").then((uid) => {
-      // using CreatePlugIn function instead of CreateDatasource,
-      // because I do not need to fill the datasource form and use the same default data
-      dataSources.CreatePlugIn("Oracle");
-    });
+    dataSources.CreatePlugIn("Oracle");
 
     const expectedPlaceholderValue = "1521";
     agHelper.AssertAttribute(
@@ -39,12 +29,7 @@ describe("Test placeholder value for port number for all datasources - tests #24
 
   it("3. Test SMTP plugin datasource port number placeholder", () => {
     dataSources.NavigateToDSCreateNew();
-    agHelper.GenerateUUID();
-    cy.get("@guid").then((uid) => {
-      // using CreatePlugIn function instead of CreateDatasource,
-      // because I do not need to fill the datasource form and use the same default data
-      dataSources.CreatePlugIn("SMTP");
-    });
+    dataSources.CreatePlugIn("SMTP");
 
     const expectedPlaceholderValue = "25";
     agHelper.AssertAttribute(
