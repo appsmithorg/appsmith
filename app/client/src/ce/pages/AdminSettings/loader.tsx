549930b53d2693c206fdd6688514724f1018d1f5
Ivan Akulov <mail@iamakulov.com>
2023-07-04 14:51:19 +0200

    Ensure a correct AdminSettings version is loaded

--- app/client/src/ce/pages/AdminSettings/loader.tsx
+++ app/client/src/ce/pages/AdminSettings/loader.tsx
@@ -1,3 +1,6 @@
+// ⚠️ Please keep this file in sync with ee/pages/AdminSettings/loader.tsx.
+// We can’t just re-export ce/pages/AdminSettings/loader.tsx in ee/pages/AdminSettings/loader.tsx
+// as the `import("./index")` path is relative to the file it’s in.
 import React from "react";
 import PageLoadingBar from "pages/common/PageLoadingBar";
 import { retryPromise } from "utils/AppsmithUtils";
--- app/client/src/ee/pages/AdminSettings/loader.tsx
+++ app/client/src/ee/pages/AdminSettings/loader.tsx
@@ -1,2 +1,20 @@
-export * from "ce/pages/AdminSettings/loader";
-export { default } from "ce/pages/AdminSettings/loader";
+// ⚠️ Please keep this file in sync with ce/pages/AdminSettings/loader.tsx.
+// We can’t just re-export ce/pages/AdminSettings/loader.tsx in ee/pages/AdminSettings/loader.tsx
+// as the `import("./index")` path is relative to the file it’s in.
+import React from "react";
+import PageLoadingBar from "pages/common/PageLoadingBar";
+import { retryPromise } from "utils/AppsmithUtils";
+
+const Page = React.lazy(() =>
+  retryPromise(() => import(/* webpackChunkName: "settings" */ "./index")),
+);
+
+const AdminSettingsLoader = (props: any) => {
+  return (
+    <React.Suspense fallback={<PageLoadingBar />}>
+      <Page {...props} />
+    </React.Suspense>
+  );
+};
+
+export default AdminSettingsLoader;
