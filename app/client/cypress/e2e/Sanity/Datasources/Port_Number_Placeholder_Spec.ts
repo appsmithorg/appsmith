50828064c54ff8ff4f255f6da6c6e789be497fa6
Sumit Kumar <sumit@appsmith.com>
2023-07-04 18:08:59 +0530

    merge TC into one

--- app/client/cypress/e2e/Sanity/Datasources/Port_Number_Placeholder_Spec.ts
+++ app/client/cypress/e2e/Sanity/Datasources/Port_Number_Placeholder_Spec.ts
@@ -1,37 +1,36 @@
 import { agHelper, dataSources } from "../../../support/Objects/ObjectsCore";
 
 describe("Test placeholder value for port number for all datasources - tests #24960", () => {
-  it("1. Test MsSQL datasource port number placeholder", () => {
+  it("1. Test datasource port number placeholder", () => {
+    // MsSQL
     dataSources.NavigateToDSCreateNew();
     dataSources.CreatePlugIn("Microsoft SQL Server");
 
-    const expectedPlaceholderValue = "1433";
+    let expectedPlaceholderValue = "1433";
     agHelper.AssertAttribute(
       dataSources._port,
       "placeholder",
       expectedPlaceholderValue,
     );
     dataSources.SaveDSFromDialog(false);
-  });
 
-  it("2. Test Oracle plugin datasource port number placeholder", () => {
+    // Oracle
     dataSources.NavigateToDSCreateNew();
     dataSources.CreatePlugIn("Oracle");
 
-    const expectedPlaceholderValue = "1521";
+    expectedPlaceholderValue = "1521";
     agHelper.AssertAttribute(
       dataSources._port,
       "placeholder",
       expectedPlaceholderValue,
     );
     dataSources.SaveDSFromDialog(false);
-  });
 
-  it("3. Test SMTP plugin datasource port number placeholder", () => {
+    // SMTP
     dataSources.NavigateToDSCreateNew();
     dataSources.CreatePlugIn("SMTP");
 
-    const expectedPlaceholderValue = "25";
+    expectedPlaceholderValue = "25";
     agHelper.AssertAttribute(
       dataSources._port,
       "placeholder",
