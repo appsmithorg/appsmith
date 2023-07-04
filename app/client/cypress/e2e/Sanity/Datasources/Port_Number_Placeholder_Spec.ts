2ebe5866d546cb38d3f7fabdf73f68b99208699f
Sumit Kumar <sumit@appsmith.com>
2023-07-04 16:00:21 +0530

    add Cypress TC

---
+++ app/client/cypress/e2e/Sanity/Datasources/Port_Number_Placeholder_Spec.ts
@@ -0,0 +1,64 @@
+import { agHelper, dataSources } from "../../../support/Objects/ObjectsCore";
+
+describe("Test placeholder value for port number for all datasources - tests #24960", () => {
+  let dsName: any;
+  it("1. Test MsSQL datasource port number placeholder", () => {
+    dataSources.NavigateToDSCreateNew();
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      // using CreatePlugIn function instead of CreateDatasource,
+      // because I do not need to fill the datasource form and use the same default data
+      dataSources.CreatePlugIn("Microsoft SQL Server");
+      dsName = "MsSql" + uid;
+      agHelper.RenameWithInPane(dsName, false);
+    });
+
+    const expectedPlaceholderValue = "1433";
+    agHelper.AssertAttribute(
+      dataSources._port,
+      "placeholder",
+      expectedPlaceholderValue,
+    );
+    dataSources.SaveDSFromDialog(false);
+  });
+
+  it("2. Test Oracle plugin datasource port number placeholder", () => {
+    dataSources.NavigateToDSCreateNew();
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      // using CreatePlugIn function instead of CreateDatasource,
+      // because I do not need to fill the datasource form and use the same default data
+      dataSources.CreatePlugIn("Oracle");
+      dsName = "Oracle" + uid;
+      agHelper.RenameWithInPane(dsName, false);
+    });
+
+    const expectedPlaceholderValue = "1521";
+    agHelper.AssertAttribute(
+      dataSources._port,
+      "placeholder",
+      expectedPlaceholderValue,
+    );
+    dataSources.SaveDSFromDialog(false);
+  });
+
+  it("3. Test SMTP plugin datasource port number placeholder", () => {
+    dataSources.NavigateToDSCreateNew();
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      // using CreatePlugIn function instead of CreateDatasource,
+      // because I do not need to fill the datasource form and use the same default data
+      dataSources.CreatePlugIn("SMTP");
+      dsName = "Smtp" + uid;
+      agHelper.RenameWithInPane(dsName, false);
+    });
+
+    const expectedPlaceholderValue = "25";
+    agHelper.AssertAttribute(
+      dataSources._port,
+      "placeholder",
+      expectedPlaceholderValue,
+    );
+    dataSources.SaveDSFromDialog(false);
+  });
+});
