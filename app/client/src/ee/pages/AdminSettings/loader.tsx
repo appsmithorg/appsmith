21c4090f5e7e0cfb057b2bcb7eb0c50e197f37d9
Ivan Akulov <mail@iamakulov.com>
2023-07-05 17:57:06 +0200

    Remove the AdminSettings loader in CE

--- app/client/src/ee/pages/AdminSettings/loader.tsx
+++ app/client/src/ee/pages/AdminSettings/loader.tsx
@@ -1,6 +1,3 @@
-// ⚠️ Please keep this file in sync with ce/pages/AdminSettings/loader.tsx.
-// We can’t just re-export ce/pages/AdminSettings/loader.tsx in ee/pages/AdminSettings/loader.tsx
-// as the `import("./index")` path is relative to the file it’s in.
 import React from "react";
 import PageLoadingBar from "pages/common/PageLoadingBar";
 import { retryPromise } from "utils/AppsmithUtils";
