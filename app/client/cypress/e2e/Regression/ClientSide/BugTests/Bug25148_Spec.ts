058002e7deb4789fc39f1ac3b7c895c6241eac51
Aman Agarwal <aman@appsmith.com>
2023-07-10 14:26:47 +0530

    fix: added cypress test case

--- app/client/cypress/e2e/Regression/ClientSide/BugTests/Bug25148_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/BugTests/Bug25148_Spec.ts
@@ -3,8 +3,8 @@ import * as _ from "../../../../support/Objects/ObjectsCore";
 const { agHelper, apiPage, dataSources } = _;
 let dsName: any;
 
-describe("Bug 6732 - this.params in IIFE function in API editor", () => {
-  it("1. this.params should be available in IIFE function in API editor", () => {
+describe("Bug 25148 - Edit Datasource button was disabled on Authentication tab of Api action", () => {
+  it("1. Checking if the Edit datasource button is enabled or not", () => {
     dataSources.NavigateToDSCreateNew();
     agHelper.GenerateUUID();
     cy.get("@guid").then((uid) => {
