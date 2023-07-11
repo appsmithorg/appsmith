f98b9cd7608a9fe661324be5d329c2e0bdfc3331
manish kumar <manish@appsmith.com>
2023-07-11 13:18:40 +0530

    test case fix

--- app/server/appsmith-server/src/test/java/com/appsmith/server/solutions/DatasourceTriggerSolutionTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/solutions/DatasourceTriggerSolutionTest.java
@@ -146,14 +146,12 @@ public class DatasourceTriggerSolutionTest {
         Mockito.when(datasourceStructureSolution.getStructure(Mockito.anyString(), Mockito.anyBoolean(), Mockito.any()))
                 .thenReturn(Mono.just(testStructure));
 
-        Datasource datasource = datasourceService
-                .findById(datasourceId, datasourcePermission.getReadPermission())
-                .block();
+        datasourceService.findById(datasourceId, datasourcePermission.getReadPermission()).block();
         Mockito.doReturn(Mono.just(Boolean.TRUE)).when(featureFlagService).check(Mockito.any());
 
         Mono<TriggerResultDTO> tableNameMono = datasourceTriggerSolution.trigger(
                 datasourceId,
-                null,
+                defaultEnvironmentId,
                 new TriggerRequestDTO("ENTITY_SELECTOR", Map.of(), ClientDataDisplayType.DROP_DOWN));
 
         Mono<TriggerResultDTO> columnNamesMono = datasourceTriggerSolution.trigger(
