a047ea2c0da0dcdd15ca0d4fca302aa1ebb10d1e
Abhijeet <abhi.nagarnaik@gmail.com>
2023-07-14 00:26:46 +0530

    Add comment

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ServerSideExecutionRequestDTO.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ServerSideExecutionRequestDTO.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class ServerSideExecutionRequestDTO {
 
+    // This can be captured from web exchange but keeping this flexibility to client side in case the server is hosted
+    // under the VPN and APIs are routed via nginx
     String baseUrl;
 
     String collectionId;
