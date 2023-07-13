73be864c6f9baa709f39d696c34b66d25e285608
Nilansh Bansal <nilansh@appsmith.com>
2023-07-13 15:23:29 +0530

    feat: Only local flags served if Airgapped instance (#1820)
    ## Description
    > This PR eliminates calling remote ff service if the instance is
    airgapped.
    
    Fixes #[24814](https://github.com/appsmithorg/appsmith/issues/24814)
    
    #### Type of change
    
    - New feature (non-breaking change which adds functionality)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/FeatureFlagService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/FeatureFlagService.java
@@ -1,5 +1,10 @@
 package com.appsmith.server.services;
 
 import com.appsmith.server.services.ce.FeatureFlagServiceCE;
+import reactor.core.publisher.Mono;
 
-public interface FeatureFlagService extends FeatureFlagServiceCE {}
+import java.util.Map;
+
+public interface FeatureFlagService extends FeatureFlagServiceCE {
+    Mono<Map<String, Boolean>> getAllFeatureFlagsForUser();
+}
