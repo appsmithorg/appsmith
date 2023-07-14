ae95ddbb501e7ea892b0c8e3bcd9c50dad4e7150
Saroj <saroj@appsmith.com>
2023-07-14 12:52:04 +0530

    fixing the error in after spec

--- app/client/cypress/plugins/index.js
+++ app/client/cypress/plugins/index.js
@@ -154,14 +154,13 @@ module.exports = (on, config) => {
   });
 
   on("after:spec", (spec, results) => {
+    // delete the video if the spec passed and no tests retried
     if (results && results.video) {
-      // Do we have failures for any retry attempts?
-      const failures = results.tests.some((test) =>
-        test.attempts.some((attempt) => attempt.state === "failed"),
-      );
-      if (!failures) {
-        // delete the video if the spec passed and no tests retried
-        fs.unlinkSync(results.video);
+      const failed = results.tests.some((test) => test.state === "failed");
+      if (!failed) {
+        unlink(results.video, () => {
+          console.log(`${results.video} deleted`);
+        });
       }
     }
   });
