a86a8b00572a56cb80eeb26dd8c5a11c3445b90d
GAUTAM MANAK <106014185+GAUTAMMANAK1@users.noreply.github.com>
2023-07-18 01:42:03 +0530

    Update index.js
    Replaced fs.accesSync with fs.accessSync to fix the typo.
    Moved the variable widgetPath inside the plop.addHelper("directory") function for improved readability.
    Fixed the try-catch block to correctly handle the existence check of the widget path.
    Aligned the indentation of the code for better readability.

--- app/client/generators/index.js
+++ app/client/generators/index.js
@@ -4,17 +4,21 @@ const widgetGenerator = require("./widget/index.js");
 
 module.exports = (plop) => {
   plop.setGenerator("widget", widgetGenerator);
+
   plop.addHelper("directory", (widget) => {
+    const widgetPath = path.join(__dirname, `../src/widgets/${widget}`);
     try {
-      fs.accesSync(path.join(__dirname, `../src/widgets/${widget}`), fs.F_OK);
-      return `widgets/${widget}`;
+      fs.accessSync(widgetPath, fs.constants.F_OK);
     } catch (e) {
       return `widgets/${widget}`;
     }
+    return `widgets/${widget}`;
   });
+
   plop.addHelper("suffixed", (name) => {
     return `${name}Widget`;
   });
+
   plop.addHelper("widgetTypeFormat", (name) => {
     return `${name.toUpperCase()}_WIDGET`;
   });
