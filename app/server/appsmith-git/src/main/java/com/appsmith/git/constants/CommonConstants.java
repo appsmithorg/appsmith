795a204ec33053d576f1539425ae77b0442b3ce4
Anagh Hegde <anagh@appsmith.com>
2023-07-18 02:41:06 +0530

    fix: tabs widget reading from file system

--- app/server/appsmith-git/src/main/java/com/appsmith/git/constants/CommonConstants.java
+++ app/server/appsmith-git/src/main/java/com/appsmith/git/constants/CommonConstants.java
@@ -26,4 +26,8 @@ public class CommonConstants {
     public static final String EMPTY_STRING = "";
     public static final String FILE_MIGRATION_MESSAGE =
             "Some of the changes above are due to an improved file structure designed to reduce merge conflicts. You can safely commit them to your repository.";
+
+    public static final String TABS_WIDGET = "TABS_WIDGET";
+
+    public static final String TAB_NAME = "tabName";
 }
