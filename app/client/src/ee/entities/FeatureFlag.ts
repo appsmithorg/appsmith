fa2df2a3841b8862a2e045dfabdb52fb1cb44075
Hetu Nandu <hetunandu@gmail.com>
2023-07-19 09:10:16 +0530

    fix: Adds more boilerplate for EE feature flagging support (#1851)
    Creating objects for better splitting between EE and CE for EE only
    feature flags

--- app/client/src/ee/entities/FeatureFlag.ts
+++ app/client/src/ee/entities/FeatureFlag.ts
@@ -1,1 +1,29 @@
 export * from "ce/entities/FeatureFlag";
+import {
+  FEATURE_FLAG as CE_FEATURE_FLAG,
+  DEFAULT_FEATURE_FLAG_VALUE as CE_DEFAULT_FEATURE_FLAG_VALUE,
+} from "ce/entities/FeatureFlag";
+
+const EE_FEATURE_FLAG = {
+  TEST_EE_FLAG: "TEST_EE_FLAG",
+} as const;
+
+export const EE_DEFAULT_FEATURE_FLAG_VALUE: EE_FeatureFlags = {
+  TEST_EE_FLAG: true,
+};
+
+export type EE_FeatureFlag = keyof typeof EE_FEATURE_FLAG;
+export type EE_FeatureFlags = Record<EE_FeatureFlag, boolean>;
+
+export const FEATURE_FLAG = {
+  ...CE_FEATURE_FLAG,
+  ...EE_FEATURE_FLAG,
+} as const;
+
+export const DEFAULT_FEATURE_FLAG_VALUE = {
+  ...CE_DEFAULT_FEATURE_FLAG_VALUE,
+  ...EE_DEFAULT_FEATURE_FLAG_VALUE,
+};
+
+export type FeatureFlag = keyof typeof FEATURE_FLAG;
+export type FeatureFlags = Record<FeatureFlag, boolean>;
