cc50a10aa83b82de276a479902a7e7fe474b7929
Ayangade Adeoluwa <adeoluayangade@yahoo.com>
2023-07-19 09:34:13 +0100

    Make schema test skipped for airgapped

--- app/client/cypress/e2e/Regression/ClientSide/BugTests/DatasourceSchema_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/BugTests/DatasourceSchema_spec.ts
@@ -42,37 +42,45 @@ describe("Datasource form related tests", function () {
     dataSources.DeleteDatasouceFromWinthinDS(dataSourceName);
   });
 
-  it("3. Verify if schema (table and column) exist in query editor and searching works", () => {
-    featureFlagIntercept(
-      {
-        ab_ds_schema_enabled: true,
-      },
-      false,
-    );
-    agHelper.RefreshPage();
-    dataSources.CreateMockDB("Users");
-    dataSources.CreateQueryAfterDSSaved();
-    dataSources.VerifyTableSchemaOnQueryEditor("public.users");
-    ee.ExpandCollapseEntity("public.users");
-    dataSources.VerifyColumnSchemaOnQueryEditor("id");
-    dataSources.FilterAndVerifyDatasourceSchemaBySearch(
-      "gender",
-      true,
-      "column",
-    );
-  });
+  it(
+    "excludeForAirgap",
+    "3. Verify if schema (table and column) exist in query editor and searching works",
+    () => {
+      featureFlagIntercept(
+        {
+          ab_ds_schema_enabled: true,
+        },
+        false,
+      );
+      agHelper.RefreshPage();
+      dataSources.CreateMockDB("Users");
+      dataSources.CreateQueryAfterDSSaved();
+      dataSources.VerifyTableSchemaOnQueryEditor("public.users");
+      ee.ExpandCollapseEntity("public.users");
+      dataSources.VerifyColumnSchemaOnQueryEditor("id");
+      dataSources.FilterAndVerifyDatasourceSchemaBySearch(
+        "gender",
+        true,
+        "column",
+      );
+    },
+  );
 
   // the full list for schema-less plugins can be found here. https://www.notion.so/appsmith/Don-t-show-schema-section-for-plugins-that-don-t-support-it-78f82b6abf7948c5a7d596ae583ed8a4?pvs=4#3862343ca2564f7e83a2c8279965ca61
-  it("3. Verify schema does not show up in schema-less plugins", () => {
-    featureFlagIntercept(
-      {
-        ab_ds_schema_enabled: true,
-      },
-      false,
-    );
-    agHelper.RefreshPage();
-    dataSources.CreateDataSource("Airtable");
-    dataSources.CreateQueryAfterDSSaved();
-    dataSources.VerifySchemaAbsenceInQueryEditor();
-  });
+  it(
+    "excludeForAirgap",
+    "4. Verify schema does not show up in schema-less plugins",
+    () => {
+      featureFlagIntercept(
+        {
+          ab_ds_schema_enabled: true,
+        },
+        false,
+      );
+      agHelper.RefreshPage();
+      dataSources.CreateDataSource("Airtable");
+      dataSources.CreateQueryAfterDSSaved();
+      dataSources.VerifySchemaAbsenceInQueryEditor();
+    },
+  );
 });
