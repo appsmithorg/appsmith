ab6cb4576b02d86bfef308f8533490ddd7fd2089
Valera Melnikov <valera@appsmith.com>
2023-07-19 12:51:40 +0300

    fix: spotless server errors (#25483)
    ## Description
    hotfix for spotless server errors
    
    Co-authored-by: Valera Melnikov <melnikov.vv@greendatasoft.ru>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ProductAlertService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ProductAlertService.java
@@ -2,5 +2,4 @@ package com.appsmith.server.services;
 
 import com.appsmith.server.services.ce.ProductAlertServiceCE;
 
-public interface ProductAlertService extends ProductAlertServiceCE {
-}
+public interface ProductAlertService extends ProductAlertServiceCE {}
