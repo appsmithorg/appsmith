163aecf5c3df9d3db6d9b3762a71eddda2985e46
NandanAnantharamu <67676905+NandanAnantharamu@users.noreply.github.com>
2023-07-19 10:31:15 +0530

    test: cypress - updated dateColumn test from js to ts (#25043)
    Updated Flaky tests Date_column_editing_1_spec.js and
    Date_column_editing_2_spec.js to ts.

--- app/client/cypress/support/Objects/CommonLocators.ts
+++ app/client/cypress/support/Objects/CommonLocators.ts
@@ -253,11 +253,10 @@ export class CommonLocators {
   _appThemeCard = ".t--theme-card";
   _appNavigationSettings = "#t--navigation-settings-header";
   _appNavigationSettingsShowTitle = "#t--navigation-settings-application-title";
-  _switchGroupControl =
-    ".t--draggable-switchgroupwidget .bp3-control-indicator";
   _fontSelect = "fontsize .rc-select";
   _fontInput = "fontsize input";
   _pagination = ".rc-pagination";
   _controlOption = ".t--property-control-options";
   _canvasBody = "[data-testid='div-selection-0']";
+  _svg = "svg";
 }
