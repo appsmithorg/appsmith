8b0dc78731930c037710f66467954988c6a848da
ChandanBalajiBP <104058110+ChandanBalajiBP@users.noreply.github.com>
2023-07-20 19:49:48 +0530

    remove empty payload

--- app/client/src/actions/datasourceActions.ts
+++ app/client/src/actions/datasourceActions.ts
@@ -488,7 +488,6 @@ export const datasourceDiscardAction = (pluginId: string) => {
 
 export const softRefreshDatasourceStructure = () => ({
   type: ReduxActionTypes.SOFT_REFRESH_DATASOURCE_STRUCTURE,
-  payload: {},
 });
 
 export default {
