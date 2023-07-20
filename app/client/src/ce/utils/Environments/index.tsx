b52c540f7e548e756ae4562124f579c3fb415ab3
ChandanBalajiBP <104058110+ChandanBalajiBP@users.noreply.github.com>
2023-07-20 14:49:11 +0530

    remove default env name

--- app/client/src/ce/utils/Environments/index.tsx
+++ app/client/src/ce/utils/Environments/index.tsx
@@ -19,7 +19,7 @@ export const getCurrentEnvironment = () => {
 
 // function to get the current environment from the URL
 export const getCurrentEnvName = () => {
-  return "unused_env";
+  return "";
 };
 
 // function to check if the datasource is configured for the current environment
