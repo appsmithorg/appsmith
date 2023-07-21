c9e646989ea3677d8a36c012d10cdb75601bf318
Pawan Kumar <pawan.stardust@gmail.com>
2023-07-21 16:23:06 +0530

    remove switch group

--- app/client/packages/design-system/widgets/src/components/SwitchGroup/index.styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { CheckboxGroup as HeadlessSwitchGroup } from "@design-system/headless";
-
-import { fieldStyles } from "../../styles/fieldStyles";
-import type { SwitchGroupProps } from "./SwitchGroup";
-
-export const StyledSwitchGroup = styled(HeadlessSwitchGroup)<SwitchGroupProps>`
-  ${fieldStyles}
-`;
--- app/client/packages/design-system/widgets/src/components/SwitchGroup/index.tsx
+++ app/client/packages/design-system/widgets/src/components/SwitchGroup/index.tsx
@@ -1,2 +1,2 @@
-export { SwitchGroup } from "./SwitchGroup";
-export type { SwitchGroupProps } from "./SwitchGroup";
+export { CheckboxGroup as SwitchGroup } from "../CheckboxGroup";
+export type { CheckboxGroupProps as SwitchGroupProps } from "../CheckboxGroup";
