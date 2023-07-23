697e2a6f548339720695af0ffbd3519307a77d54
Goutham Pratapa <goutham@appsmith.com>
2023-07-23 21:22:07 +0530

    Migrate dp from sts to deployments

--- scripts/cleanup_dp.sh
+++ scripts/cleanup_dp.sh
@@ -14,10 +14,12 @@ output = json
 region=ap-south-1
 source_profile = default" > ~/.aws/config
 
+
 sts_output=$(aws sts assume-role --role-arn env.AWS_ROLE_ARN --role-session-name ekscisession)
 export AWS_ACCESS_KEY_ID=$(echo $sts_output | jq -r '.Credentials''.AccessKeyId');\
 export AWS_SECRET_ACCESS_KEY=$(echo $sts_output | jq -r '.Credentials''.SecretAccessKey');\
 export AWS_SESSION_TOKEN=$(echo $sts_output | jq -r '.Credentials''.SessionToken');
+export DP_EFS_ID="fs-0b282500b38dc08c6"
 
 aws eks update-kubeconfig --region ap-south-1 --name uat-cluster --profile eksci
 
@@ -28,7 +30,7 @@ echo "Getting the pods"
 kubectl get pods
 
 ### Get list of helm charts
-deployed_charts="$(helm ls -A --filter 'ce[0-9]+' --output json | jq -r '.[].namespace')"
+deployed_charts="$(helm ls -A --filter 'ee[0-9]+' --output json | jq -r '.[].namespace')"
 
 for i in $deployed_charts
   do 
@@ -40,5 +42,7 @@ for i in $deployed_charts
       helm uninstall $i -n $i
       kubectl delete ns $i || true
       mongosh "mongodb+srv://$DB_USERNAME:$DB_PASSWORD@$DB_URL/$i?retryWrites=true&minPoolSize=1&maxPoolSize=10&maxIdleTimeMS=900000&authSource=admin" --eval 'db.dropDatabase()'
+      ACCESS_POINT_ID=$(aws efs describe-access-points --file-system-id "$DP_EFS_ID" | jq -r '.AccessPoints[] | select(.Name=="$pr") | .AccessPointId')
+      aws efs delete-access-point --access-point-id $ACCESS_POINT_ID
     fi
   done
--- scripts/deploy_preview.sh
+++ scripts/deploy_preview.sh
@@ -27,15 +27,24 @@ sts_output=$(aws sts assume-role --role-arn env.AWS_ROLE_ARN --role-session-name
 export AWS_ACCESS_KEY_ID=$(echo $sts_output | jq -r '.Credentials''.AccessKeyId');\
 export AWS_SECRET_ACCESS_KEY=$(echo $sts_output | jq -r '.Credentials''.SecretAccessKey');\
 export AWS_SESSION_TOKEN=$(echo $sts_output | jq -r '.Credentials''.SessionToken');
+export DP_EFS_ID="fs-058bd95facaa277c8"
 
-export NAMESPACE=ce"$PULL_REQUEST_NUMBER"
-export CHARTNAME=ce"$PULL_REQUEST_NUMBER"
-export SECRET=ce"$PULL_REQUEST_NUMBER"
-export DBNAME=ce"$PULL_REQUEST_NUMBER"
-export DOMAINNAME=ce-"$PULL_REQUEST_NUMBER".dp.appsmith.com
+## Use DP-EFS and create ACCESS_POINT
+ACCESS_POINT=$(aws efs create-access-point --file-system-id $DP_EFS_ID \
+               --tags Key=Name,Value=$PULL_REQUEST_NUMBER --posix-user Uid=1003,Gid=1003 \
+               --root-directory "{\"Path\": \"/\",\"CreationInfo\":{\"OwnerUid\":1003,\"OwnerGid\":1003, \"Permissions\":\"700\"}}")
+
+export ACCESS_POINT_ID=$(echo $ACCESS_POINT | jq '.AccessPointId' | tr -d '"')
+export NAMESPACE=ee"$PULL_REQUEST_NUMBER"
+export CHARTNAME=ee"$PULL_REQUEST_NUMBER"
+export SECRET=ee"$PULL_REQUEST_NUMBER"
+export DBNAME=ee"$PULL_REQUEST_NUMBER"
+export DOMAINNAME=ee-"$PULL_REQUEST_NUMBER".dp.appsmith.com
+
+
 export HELMCHART="appsmith"
-export HELMCHART_URL="http://helm.appsmith.com"
-export HELMCHART_VERSION="2.0.2"
+export HELMCHART_URL="http://helm-ee.appsmith.com"
+export HELMCHART_VERSION="3.0.3"
 
 aws eks update-kubeconfig --region $region --name $cluster_name --profile eksci
 
@@ -63,14 +72,18 @@ echo "Add appsmith-ce to helm repo"
 AWS_REGION=ap-south-1 helm repo add $HELMCHART $HELMCHART_URL
 
 echo "Deploy appsmith helm chart"
-helm upgrade -i $CHARTNAME appsmith/appsmith -n $NAMESPACE \
+helm upgrade -i $CHARTNAME appsmith-ee/$HELMCHART -n $NAMESPACE \
   --create-namespace --recreate-pods --set image.repository=$DOCKER_HUB_ORGANIZATION/appsmith-dp --set image.tag=$IMAGE_HASH \
-  --set image.pullSecrets=$SECRET --set redis.enabled=false --set mongodb.enabled=false --set ingress.enabled=true \
+  --set image.pullSecrets=$SECRET --set autoscaling.enabled=true --set autoscaling.minReplicas=1 --set autoscaling.maxReplicas=1 \
+  --set redis.enabled=false --set mongodb.enabled=false --set ingress.enabled=true \
   --set "ingress.annotations.service\.beta\.kubernetes\.io/aws-load-balancer-ssl-cert=$AWS_RELEASE_CERT" \
   --set "ingress.hosts[0].host=$DOMAINNAME, ingress.hosts[0].paths[0].path=/, ingress.hosts[0].paths[0].pathType=Prefix" \
   --set ingress.className="nginx" --set applicationConfig.APPSMITH_CLOUD_SERVICES_BASE_URL="https://release-cs.appsmith.com" \
-  --set image.pullPolicy="Always" --set autoupdate.enabled="true" --set persistence.size=2Gi \
+  --set image.pullPolicy="Always" --set autoupdate.enabled="true" --set persistence.efs.enabled=true \
+  --set persistence.efs.driver="efs.csi.aws.com" --set persistence.efs.volumeHandle="$DP_EFS_ID::$ACCESS_POINT_ID" \
+  --set storageClass.enabled= true --set storageClass.provisioner="efs.csi.aws.com" \
   --set applicationConfig.APPSMITH_SENTRY_DSN="https://abf15a075d1347969df44c746cca7eaa@o296332.ingest.sentry.io/1546547" \
   --set applicationConfig.APPSMITH_SENTRY_ENVIRONMENT="$NAMESPACE" \
   --set applicationConfig.APPSMITH_MONGODB_URI="mongodb+srv://$DB_USERNAME:$DB_PASSWORD@$DB_URL/$DBNAME?retryWrites=true&minPoolSize=1&maxPoolSize=10&maxIdleTimeMS=900000&authSource=admin" \
+  --set applicationConfig.APPSMITH_REDIS_URL="redis-cluster://release-redis-cluster.ihh5sj.clustercfg.aps1.cache.amazonaws.com:6379" \
   --version $HELMCHART_VERSION
