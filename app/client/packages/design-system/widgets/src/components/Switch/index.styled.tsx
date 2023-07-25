276bdc1fb24f628811fa27a29c6cc40cc64daa68
Pawan Kumar <pawan.stardust@gmail.com>
2023-07-25 17:08:06 +0530

    add switch

--- app/client/packages/design-system/widgets/src/components/Switch/index.styled.tsx
+++ app/client/packages/design-system/widgets/src/components/Switch/index.styled.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Checkbox as HeadlessCheckbox } from "@design-system/headless";
+import { Switch as HeadlessCheckbox } from "@design-system/headless";
 
 import type { SwitchProps } from ".";
 import { inlineLabelStyles } from "../../styles/inlineLabelStyles";
