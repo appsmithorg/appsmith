0ff085084f15dc04de6d7c510940a768a36c2ba6
Druthi Polisetty <druthi@appsmith.com>
2023-07-31 15:19:58 +0530

    fix: UI fixes for the Signposting component (#1912)
    ## Description
    fix: UI fixes for the Signposting component
    
    #### PR fixes following issue(s)
    Fixes # (issue number)
    > if no issue exists, please create an issue and ask the maintainers
    about this first
    >
    >
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    > Please delete options that are not relevant.
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - Chore (housekeeping or task changes that don't impact user perception)
    - This change requires a documentation update
    >
    >
    >
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/ee/components/editorComponents/AiSignPosting.tsx
+++ app/client/src/ee/components/editorComponents/AiSignPosting.tsx
@@ -14,29 +14,39 @@ const SubContent = styled.div`
   color: var(--ads-v2-color-fg-muted);
 `;
 
+const Content = styled.span`
+  color: #364652;
+`;
+
 export function AIPost(props: { title: string }) {
   return (
-    <div className="">
-      <div className="text-sm mb-2">{props.title}</div>
-      <SubContent className="text-xs">
-        Use <span className="font-semibold">/ai</span>, to open Appsmith AI
+    <div className="text-sm">
+      <Content>
+        Use <span className="font-semibold">/ai</span> command
+      </Content>
+      <SubContent className="text-xs mt-2">
+        <div className="">{props.title}</div>
       </SubContent>
     </div>
   );
 }
 
+const Container = styled.div`
+  margin-bottom: -7px;
+`;
+
 function AISignPosting(props: AISignPostingProps) {
   const isJavascriptMode = props.mode === EditorModes.TEXT_WITH_BINDING;
 
-  let containerClasses = "absolute -bottom-[2px] translate-y-full w-full";
+  let containerClasses = "absolute bottom-[4px] translate-y-full w-full";
 
   if (!isJavascriptMode) {
-    containerClasses = "mb-2 w-fit";
+    containerClasses = "w-fit";
   }
 
   const title = isJavascriptMode
-    ? "Generate javascript code"
-    : "Generate SQL query";
+    ? "To generate js code using AI"
+    : "To generate queries using AI";
 
   /** We show this prompt on the editor only for TEXT_WITH_BINDING   */
   if (
@@ -47,7 +57,7 @@ function AISignPosting(props: AISignPostingProps) {
   }
 
   return (
-    <div
+    <Container
       className={`${containerClasses} ${
         props.isOpen ? "visible" : "invisible"
       } t--no-binding-prompt`}
@@ -56,7 +66,7 @@ function AISignPosting(props: AISignPostingProps) {
         content={<AIPost title={title} />}
         iconName="magic-line"
       />
-    </div>
+    </Container>
   );
 }
 
