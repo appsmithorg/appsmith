780af04c3f4b2d61983456df011ce97a58b04dd0
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-08-02 21:03:03 +0530

    Update generate_info_json.sh

--- scripts/generate_info_json.sh
+++ scripts/generate_info_json.sh
@@ -9,7 +9,7 @@ commit_sha="$(git rev-parse HEAD)"
 base_url="$(git remote get-url origin | sed 's,^git@github\.com:,https://github.com/,; s/\.git$//')"
 
 # Tag, if any, on the HEAD commit.
-git_tag="$(git describe --exact-match --tags --match 'v*' --dirty --broken || true)"
+git_tag="$(git describe --exact-match --tags --match 'v*' --dirty --broken || echo)"
 
 jq -n \
   --arg commitSha "$commit_sha" \
