8c8826245c538eab3bc86c8c351bd5e22713f362
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-08-02 17:53:24 +0530

    update: moved super user role only if workspace is present

--- app/client/src/utils/ProductRamps/index.ts
+++ app/client/src/utils/ProductRamps/index.ts
@@ -34,9 +34,9 @@ export const getUserRoleInWorkspace = () => {
   const state = store.getState();
   const { currentUser } = state?.ui?.users;
   const isSuperUser = currentUser?.isSuperUser;
-  if (isSuperUser) return RAMP_FOR_ROLES.SUPER_USER;
   const workspaceUsers = state?.ui?.workspaces?.workspaceUsers;
   if (workspaceUsers?.length) {
+    if (isSuperUser) return RAMP_FOR_ROLES.SUPER_USER;
     const workspaceUser = workspaceUsers.find(
       (user: any) => user?.username === currentUser?.username,
     );
@@ -46,7 +46,7 @@ export const getUserRoleInWorkspace = () => {
         return role.trim();
       }
     }
-  }
+  } else return RAMP_FOR_ROLES.APP_VIEWER;
 };
 
 export const showProductRamps = (rampName: string) => {
