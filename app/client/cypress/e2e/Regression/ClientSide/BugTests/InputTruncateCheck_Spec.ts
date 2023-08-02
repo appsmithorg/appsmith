846624c465831d97e236681db6d8d9778e27d14d
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2023-08-02 08:10:36 +0530

    chore: skip interacting with the explorer

--- app/client/cypress/e2e/Regression/ClientSide/BugTests/InputTruncateCheck_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/BugTests/InputTruncateCheck_Spec.ts
@@ -88,12 +88,12 @@ function configureApi() {
 }
 
 Object.entries(widgetsToTest).forEach(([widgetSelector, testConfig], index) => {
-  describe.skip(`${testConfig.widgetName} widget test for storeValue save, Api Call params`, () => {
+  describe(`${testConfig.widgetName} widget test for storeValue save, Api Call params`, () => {
     it(`1. DragDrop widget & Label/Text widgets`, () => {
       if (index === 0) {
         configureApi();
       }
-      entityExplorer.PinUnpinEntityExplorer(false);
+      // entityExplorer.PinUnpinEntityExplorer(false);
       entityExplorer.DragDropWidgetNVerify(widgetSelector, 300, 200);
       entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON, 400, 400);
       //entityExplorer.SelectEntityByName(draggableWidgets.BUTTONNAME("1"));
@@ -110,7 +110,7 @@ Object.entries(widgetsToTest).forEach(([widgetSelector, testConfig], index) => {
         PROPERTY_SELECTOR.TextFieldName,
         `{{appsmith.store.textPayloadOnSubmit}}`,
       );
-      entityExplorer.PinUnpinEntityExplorer(true);
+      // entityExplorer.PinUnpinEntityExplorer(true);
     });
 
     it("2. StoreValue should have complete input value", () => {
