3ae42784c267d3df140355c8b0f9080fbe0bb66a
Vijetha-Kaja <vijetha@appsmith.com>
2023-08-02 15:47:34 +0530

    restapi & authenticated api fix

---
+++ app/client/cypress/e2e/Sanity/Datasources/RestApiOAuth2Validation_spec.ts
@@ -0,0 +1,71 @@
+import testdata  from "../../../fixtures/testdata.json";
+import datasource from "../../../locators/DatasourcesEditor.json";
+import {
+  agHelper,
+  assertHelper,
+  entityExplorer,
+  entityItems,
+  apiPage,
+  dataSources,
+} from "../../../support/Objects/ObjectsCore";
+let clientId, clientSecret
+
+describe("Datasource form OAuth2 client credentials related tests", function () {
+  it("1. Create an API with app url and save as Datasource for Client Credentials test", function () {
+    cy.fixture("datasources").then((datasourceFormData) => {
+      dataSources.CreateOAuthClient("authorization_code");
+      apiPage.CreateAndFillApi(datasourceFormData["OAuth_ApiUrl"] + "/api/echo/get?ASDSA=ASDSA", "TestOAuth");
+      agHelper.GetNClick(apiPage._saveAsDS);
+
+      // Add Oauth details to datasource and save
+      agHelper.AssertElementEnabledDisabled(datasource.saveBtn,0,false);
+      cy.get("@OAuthClientID").then((id) => {
+        cy.get("@OAuthClientSecret").then((secret) => {
+          clientId=id
+          clientSecret=secret
+          dataSources.AddOAuth2AuthorizationCodeDetails(
+            datasourceFormData["OAUth_AccessTokenUrl"],
+            clientId,
+            clientSecret,
+            testdata.oauth2Scopes,
+          );
+        });
+      });
+      // since we are moving to different, it will show unsaved changes dialog
+      // save datasource and then proceed
+      dataSources.SaveDatasource();
+      agHelper.ValidateToastMessage("datasource created"); //verifying there is no error toast, Bug 14566
+      entityExplorer.SelectEntityByName("TestOAuth", "Queries/JS");
+      agHelper.ActionContextMenuWithInPane({
+        action: "Delete",
+        entityType: entityItems.Api,
+      });
+    });
+  });
+
+  it("2. Create an API with app url and save as Datasource for Authorization code details test", function () {
+    cy.fixture("datasources").then((datasourceFormData) => {
+    apiPage.CreateAndFillApi(testdata.appUrl, "TestOAuth");
+    agHelper.GetNClick(apiPage._saveAsDS);
+    //Add Oauth details to datasource and save
+    agHelper.AssertElementEnabledDisabled(datasource.saveBtn,0,false);
+    dataSources.AddOAuth2AuthorizationCodeDetails(
+      datasourceFormData["OAUth_AccessTokenUrl"],
+      clientId,
+      clientSecret,
+      datasourceFormData["OAuth_AuthUrl"],
+    );
+    })
+  });
+
+  it("3. Validate save and Authorise", function () {
+    agHelper.GetNClick(dataSources._saveAndAuthorizeDS);
+
+    //Accept consent
+    agHelper.GetNClick(dataSources._consent);
+    agHelper.GetNClick(dataSources._consentSubmit);
+
+    //Validate save
+    assertHelper.AssertNetworkStatus("@saveDatasource", 201);
+  });
+});
