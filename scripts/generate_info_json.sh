024f7cdd277e9295709105e00b8e7eba7f3f8a13
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-08-02 21:51:55 +0530

    Fix indentation

--- scripts/generate_info_json.sh
+++ scripts/generate_info_json.sh
@@ -13,12 +13,12 @@ git_branch="$(git rev-parse --abbrev-ref HEAD)"
 # Tag, if any, on the HEAD commit.
 # Output of this `describe` command is described at https://git-scm.com/docs/git-describe#_examples.
 git_tag="$(
-	# If HEAD is a commit in the `master` branch, then `git describe --tags` will give a relevant tag name.
-	if git merge-base --is-ancestor HEAD origin/master; then
-		git describe --tags --match 'v*' --dirty --broken | sed 's/-g.*$//'
-	fi
-	# This will look like `v1.9.30` when HEAD is the tag `v1.9.30`, or something like `v1.9.30-10` when HEAD is 10 commits
-	# ahead of the tag `v1.9.30`.
+  # If HEAD is a commit in the `master` branch, then `git describe --tags` will give a relevant tag name.
+  if git merge-base --is-ancestor HEAD origin/master; then
+    git describe --tags --match 'v*' --dirty --broken | sed 's/-g.*$//'
+  fi
+  # This will look like `v1.9.30` when HEAD is the tag `v1.9.30`, or something like `v1.9.30-10` when HEAD is 10 commits
+  # ahead of the tag `v1.9.30`.
 )"
 
 jq -n \
