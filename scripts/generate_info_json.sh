53844d2e964399001d78af32159fd576f3e1b927
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-08-02 21:00:38 +0530

    Include tag in info.json

--- scripts/generate_info_json.sh
+++ scripts/generate_info_json.sh
@@ -5,11 +5,16 @@ set -o nounset
 
 commit_sha="$(git rev-parse HEAD)"
 
+# Base URL of the current repository on GitHub.
 base_url="$(git remote get-url origin | sed 's,^git@github\.com:,https://github.com/,; s/\.git$//')"
+
+# Tag, if any, on the HEAD commit.
+git_tag="$(git describe --exact-match --tags --match 'v*' --dirty --broken || true)"
 
 jq -n \
   --arg commitSha "$commit_sha" \
   --arg commitUrl "$base_url/commit/$commit_sha" \
+  --arg gitTag "$git_tag" \
   --arg branch "$(git rev-parse --abbrev-ref HEAD)" \
   --arg date "$(date -u -Iseconds)" \
   --argjson isCI "${CI:-false}" \
