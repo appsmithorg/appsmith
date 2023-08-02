fc543cb2b01825cb0e4e1e2f913f654258bde4ad
ChandanBalajiBP <104058110+ChandanBalajiBP@users.noreply.github.com>
2023-08-03 00:49:46 +0530

    fix build

--- app/client/src/ce/reducers/environmentReducer.ts
+++ app/client/src/ce/reducers/environmentReducer.ts
@@ -1,4 +1,4 @@
 export interface EnvironmentType {
-  id?: string;
-  name?: string;
+  id: string;
+  name: string;
 }
--- app/client/src/ce/selectors/environmentSelectors.tsx
+++ app/client/src/ce/selectors/environmentSelectors.tsx
@@ -1,8 +1,9 @@
 import type { AppState } from "@appsmith/reducers";
+import type { EnvironmentType } from "@appsmith/reducers/environmentReducer";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const areEnvironmentsFetched = (state: AppState, workspaceId: string) =>
   true;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getEnvironments = (state: AppState) => [];
+export const getEnvironments = (state: AppState): Array<EnvironmentType> => [];
