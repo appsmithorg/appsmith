5323ced61776d486c5661c04b03eef1bc1d0bab3
Sharanya <sharanya@appsmith.com>
2023-08-03 16:33:55 +0530

    test: Cypress - progress widget cases

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/Others/Progress_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Others/Progress_spec.js
@@ -1,11 +1,23 @@
 const explorer = require("../../../../../locators/explorerlocators.json");
+const widgets = require("../../../../../locators/Widgets.json");
+import {
+  agHelper,
+  deployMode,
+  draggableWidgets,
+  entityExplorer,
+  fakerHelper,
+  propPane,
+} from "../../../../../support/Objects/ObjectsCore";
+import { progressWidgetProgress } from "../../../../../locators/WidgetLocators";
+import { faker } from "@faker-js/faker";
 
 describe("Progress Widget", function () {
+  before(() => {
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.PROGRESS);
+  });
+
   it("Add a new Progress widget and text widget", function () {
-    cy.get(explorer.addWidget).click();
-    cy.dragAndDropToCanvas("progresswidget", { x: 300, y: 300 });
-    cy.get(".t--widget-progresswidget").should("exist");
-    cy.dragAndDropToCanvas("textwidget", { x: 300, y: 500 });
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.TEXT, 500, 200);
     cy.openPropertyPane("textwidget");
     cy.updateCodeInput(".t--property-control-text", "");
   });
@@ -137,5 +149,54 @@ describe("Progress Widget", function () {
       { force: true },
     );
     cy.get("ul.CodeMirror-hints").contains("progress").should("exist");
+  });
+
+  it("Check Progress, Number of steps, Counterclockwise, Show result disabled on infite looping enablement", function () {
+    //  on enabling infite loading assert the absence of elements
+    agHelper.GetNClick(widgets.progressWidget);
+    propPane.TogglePropertyState("Infinite loading", "On");
+    agHelper.AssertElementAbsence(propPane._labelContains("Progress"));
+    agHelper.AssertElementAbsence(propPane._labelContains("Number of steps"));
+    agHelper.AssertElementAbsence(propPane._labelContains("Counterclockwise"));
+    agHelper.AssertElementAbsence(propPane._labelContains("Show result"));
+    propPane.TogglePropertyState("Infinite loading", "Off");
+  });
+
+  it("Check binding in Progress ", function () {
+    agHelper.GetNClick(widgets.linearProgressWidget);
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.INPUT_V2, 600, 100);
+    agHelper.GetNClick(widgets.progressWidget);
+    // assert error texts in Progress property
+    propPane.UpdatePropertyFieldValue("Progress", "-1");
+    agHelper.VerifyEvaluatedErrorMessage("Minimum allowed value: 0");
+    propPane.UpdatePropertyFieldValue("Progress", fakerHelper.GetRandomText());
+    agHelper.VerifyEvaluatedErrorMessage(
+      "This value does not evaluate to type number Max: 100",
+    );
+    propPane.UpdatePropertyFieldValue("Progress", "101");
+    agHelper.VerifyEvaluatedErrorMessage("Maximum allowed value: 100");
+
+    // set input entered in text widget to progress
+    propPane.UpdatePropertyFieldValue("Progress", "{{Input1.text}}");
+    deployMode.DeployApp();
+    agHelper.EnterInputText("Label", "10");
+    // assert if the text in text widget is reflected in progress bar
+    agHelper.AssertElementExist(progressWidgetProgress("10"));
+    agHelper.EnterInputText("Label", "20", true);
+    agHelper.AssertElementExist(progressWidgetProgress("20"));
+    deployMode.NavigateBacktoEditor();
+  });
+
+  it("Check progress widget visibility based on visible state - Deploy mode", function () {
+    agHelper.GetNClick(widgets.progressWidget);
+    propPane.TogglePropertyState("Visible", "Off");
+    deployMode.DeployApp();
+    agHelper.AssertElementAbsence(widgets.progressWidget);
+    deployMode.NavigateBacktoEditor();
+    agHelper.GetNClick(widgets.progressWidget);
+    propPane.TogglePropertyState("Visible", "On");
+    deployMode.DeployApp();
+    agHelper.AssertElementExist(widgets.progressWidget);
+    agHelper.GetNClick(widgets.progressWidget);
   });
 });
