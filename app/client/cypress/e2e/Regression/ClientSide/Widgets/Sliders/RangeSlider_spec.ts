b173bf2bef38bcb55d61b547399e90f438641af4
nandan <nandan@thinkify.io>
2023-08-04 15:19:47 +0530

    reoganised tests for Range Slider

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/Sliders/RangeSlider_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Sliders/RangeSlider_spec.ts
@@ -8,8 +8,6 @@ import {
   deployMode,
 } from "../../../../../support/Objects/ObjectsCore";
 
-
-
 describe("Range Slider spec", () => {
   before(() => {
     entityExplorer.DragDropWidgetNVerify("rangesliderwidget", 550, 100);
@@ -21,7 +19,7 @@ describe("Range Slider spec", () => {
     propPane.UpdatePropertyFieldValue("Text", "{{RangeSlider1.start}}");
   });
 
-  it("1. Verify properties and default values", () => {
+  it("1. Verify property visibility and default values", () => {
     const dataSectionProperties = [
       "min\\.value",
       "max\\.value",
@@ -44,19 +42,13 @@ describe("Range Slider spec", () => {
     // Data Section properties
     dataSectionProperties.forEach((dataSectionProperty) => {
       agHelper.AssertElementVisible(
-        propPane._propertyPanePropertyControl(
-          "data",
-          `${dataSectionProperty}`,
-        ),
+        propPane._propertyPanePropertyControl("data", `${dataSectionProperty}`),
       );
     });
     // General Section Properties
     generalProperties.forEach((generalProperty) => {
       agHelper.AssertElementVisible(
-        propPane._propertyPanePropertyControl(
-          "general",
-          `${generalProperty}`,
-        ),
+        propPane._propertyPanePropertyControl("general", `${generalProperty}`),
       );
     });
     // Events Section properties
@@ -96,7 +88,7 @@ describe("Range Slider spec", () => {
       });
   });
 
-  it("2. Validates Min. value", () => {
+  it("2. Validate accepted and unaccepted 'Min. value' values", () => {
     propPane.UpdatePropertyFieldValue("Min. value", "110");
 
     agHelper.VerifyEvaluatedErrorMessage(
@@ -111,12 +103,66 @@ describe("Range Slider spec", () => {
 
     agHelper.VerifyEvaluatedErrorMessage("This value must be a number");
 
-    propPane.UpdatePropertyFieldValue("Min. value", "0");
+    propPane.UpdatePropertyFieldValue("Min. range", "10");
+    propPane.UpdatePropertyFieldValue("Step size", "10");
+    propPane.UpdatePropertyFieldValue("Min. value", "-10");
     agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
 
+    // Verify in Preview mode negative value
+    agHelper.GetNClick(locators._enterPreviewMode);
+    agHelper
+      .GetElement(locators._sliderThumb)
+      .eq(0)
+      .focus()
+      .type("{leftArrow}{leftArrow}{leftArrow}");
+    agHelper.Sleep(2000);
+    agHelper
+      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 1)
+      .then(($label) => {
+        expect($label).to.eq("-10");
+      });
+    agHelper.GetNClick(locators._exitPreviewMode);
+
+    // Verify in Deploy mode negative value
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._sliderThumb)
+      .eq(0)
+      .focus()
+      .type("{leftArrow}{leftArrow}");
+    agHelper
+      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 1)
+      .then(($label) => {
+        expect($label).to.eq("-10");
+      });
+    deployMode.NavigateBacktoEditor();
+
+    // Allows decimal value
+    entityExplorer.SelectEntityByName("RangeSlider1");
+    propPane.UpdatePropertyFieldValue("Min. value", "10.5");
+
+    // Verify Decimal value
+    agHelper
+      .GetElement(locators._sliderThumb)
+      .eq(0)
+      .focus()
+      .type("{leftArrow}");
+    agHelper
+      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 1)
+      .then(($label) => {
+        expect($label).to.eq("10.5");
+      });
+    // Does not allow value greater than Max value
+    propPane.UpdatePropertyFieldValue("Min. value", "110");
+    agHelper.VerifyEvaluatedErrorMessage(
+      "This value must be less than max value",
+    );
+    // Updating to allowed value
+    propPane.UpdatePropertyFieldValue("Min. value", "10");
+    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
   });
 
-  it("3. Validates Max. value", () => {
+  it("3. Validate accepted and unaccepted 'Max. value' values", () => {
     propPane.UpdatePropertyFieldValue("Max. value", "0");
 
     agHelper.VerifyEvaluatedErrorMessage(
@@ -131,12 +177,78 @@ describe("Range Slider spec", () => {
 
     agHelper.VerifyEvaluatedErrorMessage("This value must be a number");
 
-    propPane.UpdatePropertyFieldValue("Max. value", "100");
+    // Allows decimal value
+    propPane.UpdatePropertyFieldValue("Max. value", "100.5");
+    // Updating step size to verify Max value
+    propPane.UpdatePropertyFieldValue("Step size", "90");
+    // Verify decimal value
+    agHelper
+      .GetElement(locators._sliderThumb)
+      .eq(1)
+      .focus()
+      .type("{rightArrow}");
+    agHelper
+      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
+      .then(($label) => {
+        expect($label).to.eq("100.5");
+      });
+
+    // Does not allow value less than min value
+    propPane.UpdatePropertyFieldValue("Max. value", "-20");
+    agHelper.VerifyEvaluatedErrorMessage(
+      "This value must be greater than min value",
+    );
+
+    // Accepts negative value
+    propPane.UpdatePropertyFieldValue("Min. value", "-50");
+    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
+    propPane.UpdatePropertyFieldValue("Max. value", "-30");
     agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
 
+    agHelper
+      .GetElement(locators._sliderThumb)
+      .eq(1)
+      .focus()
+      .type("{rightArrow}{rightArrow}{rightArrow}");
+    agHelper
+      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
+      .then(($label) => {
+        expect($label).to.eq("-30");
+      });
+
+    // Verify in Preview mode negative value
+    agHelper.GetNClick(locators._enterPreviewMode);
+    agHelper
+      .GetElement(locators._sliderThumb)
+      .eq(1)
+      .focus()
+      .type("{rightArrow}");
+    agHelper.Sleep(2000);
+    agHelper
+      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
+      .then(($label) => {
+        expect($label).to.eq("-30");
+      });
+    agHelper.GetNClick(locators._exitPreviewMode);
+
+    // Verify in Deploy mode negative value
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._sliderThumb)
+      .eq(1)
+      .focus()
+      .type("{rightArrow}");
+    agHelper
+      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
+      .then(($label) => {
+        expect($label).to.eq("-30");
+      });
+    deployMode.NavigateBacktoEditor();
   });
 
-  it("4. Validates Step size", () => {
+  it("4. Validate accepted and unaccepted 'Step size' values", () => {
+    entityExplorer.SelectEntityByName("RangeSlider1");
+    propPane.UpdatePropertyFieldValue("Min. range", "5");
     propPane.UpdatePropertyFieldValue("Step size", "10");
 
     agHelper.VerifyEvaluatedErrorMessage(
@@ -151,23 +263,35 @@ describe("Range Slider spec", () => {
 
     agHelper.VerifyEvaluatedErrorMessage("This value must be a number");
 
-    propPane.UpdatePropertyFieldValue("Step size", "1");
+    // Does not allow value less than 0.1
+    entityExplorer.SelectEntityByName("RangeSlider1");
+    propPane.UpdatePropertyFieldValue("Step size", "0");
+    agHelper.VerifyEvaluatedErrorMessage("This value must be greater than 0.1");
+    // Does not allow negative value
+    propPane.UpdatePropertyFieldValue("Step size", "-10");
+    agHelper.VerifyEvaluatedErrorMessage("This value must be greater than 0.1");
+    propPane.UpdatePropertyFieldValue("Step size", "5");
+    // Does not allow value greater than max value
+    propPane.UpdatePropertyFieldValue("Max. value", "100");
     agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
+    propPane.UpdatePropertyFieldValue("Min. value", "10");
+    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
+    propPane.UpdatePropertyFieldValue("Step size", "110");
+    agHelper.VerifyEvaluatedErrorMessage("This value must be less than 90");
 
+    // Updating to allowed value
+    propPane.UpdatePropertyFieldValue("Step size", "5");
+    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
   });
 
-  it("5. Validates Min Range", () => {
+  it("5. Validate accepted and unaccepted 'Min Range' values", () => {
     propPane.UpdatePropertyFieldValue("Min. range", "0");
 
-    agHelper.VerifyEvaluatedErrorMessage(
-      "This value must be greater than 0.1",
-    );
+    agHelper.VerifyEvaluatedErrorMessage("This value must be greater than 0.1");
 
     propPane.UpdatePropertyFieldValue("Min. range", "-10");
 
-    agHelper.VerifyEvaluatedErrorMessage(
-      "This value must be greater than 0.1",
-    );
+    agHelper.VerifyEvaluatedErrorMessage("This value must be greater than 0.1");
 
     propPane.UpdatePropertyFieldValue("Min. range", "asd");
 
@@ -175,20 +299,27 @@ describe("Range Slider spec", () => {
 
     propPane.UpdatePropertyFieldValue("Min. range", "110");
 
-    agHelper.VerifyEvaluatedErrorMessage("This value must be less than 100");
+    agHelper.VerifyEvaluatedErrorMessage("This value must be less than 90");
 
-    propPane.UpdatePropertyFieldValue("Min. range", "10");
+    // Does not accept value less than step size
+    propPane.UpdatePropertyFieldValue("Min. range", "2");
+    agHelper.VerifyEvaluatedErrorMessage(
+      "This value must be greater than or equal to step size",
+    );
+
+    // Updating to allowed value
+    propPane.UpdatePropertyFieldValue("Min. range", "5");
     agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
-
   });
 
-  it("6. Validates Default start value", () => {
+  it("6. Validate accepted and unaccepted 'Default start' value", () => {
     propPane.UpdatePropertyFieldValue("Default start value", "-100");
 
     agHelper.VerifyEvaluatedErrorMessage(
       "This value must be greater than or equal to the min value",
     );
 
+    propPane.UpdatePropertyFieldValue("Default end value", "100");
     propPane.UpdatePropertyFieldValue("Default start value", "110");
 
     agHelper.VerifyEvaluatedErrorMessage(
@@ -203,7 +334,7 @@ describe("Range Slider spec", () => {
     agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
   });
 
-  it("7. Validates Default end value", () => {
+  it("7. Validate accepted and unaccepted 'Default end' value", () => {
     propPane.UpdatePropertyFieldValue("Default end value", "-10");
 
     agHelper.VerifyEvaluatedErrorMessage(
@@ -277,169 +408,9 @@ describe("Range Slider spec", () => {
     entityExplorer.NavigateToSwitcher("Widgets");
     agHelper.ClearTextField(locators._entityExplorersearch);
     agHelper.TypeText(locators._entityExplorersearch, "Range");
-    agHelper.AssertElementExist(
-      locators._widgetPageIcon("rangesliderwidget"),
-    );
+    agHelper.AssertElementExist(locators._widgetPageIcon("rangesliderwidget"));
     agHelper.ClearTextField(locators._entityExplorersearch);
     agHelper.TypeText(locators._entityExplorersearch, "slider");
-    agHelper.AssertElementExist(
-      locators._widgetPageIcon("rangesliderwidget"),
-    );
-  });
-
-  it("10. Min value tests", () => {
-    // Allows negative value and validate
-    entityExplorer.SelectEntityByName("RangeSlider1");
-    propPane.UpdatePropertyFieldValue("Min. value", "-10");
-    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
-
-    // Verify in Preview mode negative value
-    agHelper.GetNClick(locators._enterPreviewMode);
-    agHelper
-      .GetElement(locators._sliderThumb)
-      .eq(0)
-      .focus()
-      .type("{leftArrow}{leftArrow}{leftArrow}");
-    agHelper.Sleep(2000);
-    agHelper
-      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 1)
-      .then(($label) => {
-        expect($label).to.eq("-10");
-      });
-    agHelper.GetNClick(locators._exitPreviewMode);
-
-    // Verify in Deploy mode negative value
-    deployMode.DeployApp();
-    agHelper
-      .GetElement(locators._sliderThumb)
-      .eq(0)
-      .focus()
-      .type("{leftArrow}{leftArrow}");
-    agHelper
-      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 1)
-      .then(($label) => {
-        expect($label).to.eq("-10");
-      });
-    deployMode.NavigateBacktoEditor();
-
-    // Allows decimal value
-    entityExplorer.SelectEntityByName("RangeSlider1");
-    propPane.UpdatePropertyFieldValue("Min. value", "10.5");
-
-    // Verify Decimal value
-    agHelper
-      .GetElement(locators._sliderThumb)
-      .eq(0)
-      .focus()
-      .type("{leftArrow}");
-    agHelper
-      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 1)
-      .then(($label) => {
-        expect($label).to.eq("10.5");
-      });
-    // Does not allow value greater than Max value
-    propPane.UpdatePropertyFieldValue("Min. value", "110");
-    agHelper.VerifyEvaluatedErrorMessage(
-      "This value must be less than max value",
-    );
-    // Updating to allowed value
-    propPane.UpdatePropertyFieldValue("Min. value", "10");
-  });
-
-  it("11. Max value tests", () => {
-    // Allows decimal value
-    propPane.UpdatePropertyFieldValue("Max. value", "100.5");
-    // Verify decimal value
-    agHelper
-      .GetElement(locators._sliderThumb)
-      .eq(1)
-      .focus()
-      .type("{rightArrow}");
-    agHelper
-      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
-      .then(($label) => {
-        expect($label).to.eq("100.5");
-      });
-    // Does not allow value less than min value
-    propPane.UpdatePropertyFieldValue("Max. value", "-20");
-    agHelper.VerifyEvaluatedErrorMessage(
-      "This value must be greater than min value",
-    );
-    // Accepts negative value
-    propPane.UpdatePropertyFieldValue("Min. value", "-50");
-    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
-    propPane.UpdatePropertyFieldValue("Max. value", "-30");
-    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
-
-    agHelper
-      .GetElement(locators._sliderThumb)
-      .eq(1)
-      .focus()
-      .type("{rightArrow}{rightArrow}{rightArrow}");
-    agHelper
-      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
-      .then(($label) => {
-        expect($label).to.eq("-30");
-      });
-
-    // Verify in Preview mode negative value
-    agHelper.GetNClick(locators._enterPreviewMode);
-    agHelper
-      .GetElement(locators._sliderThumb)
-      .eq(1)
-      .focus()
-      .type("{rightArrow}");
-    agHelper.Sleep(2000);
-    agHelper
-      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
-      .then(($label) => {
-        expect($label).to.eq("-30");
-      });
-    agHelper.GetNClick(locators._exitPreviewMode);
-
-    // Verify in Deploy mode negative value
-    deployMode.DeployApp();
-    agHelper
-      .GetElement(locators._sliderThumb)
-      .eq(1)
-      .focus()
-      .type("{rightArrow}");
-    agHelper
-      .GetText(getWidgetSelector(draggableWidgets.TEXT), "text", 0)
-      .then(($label) => {
-        expect($label).to.eq("-30");
-      });
-    deployMode.NavigateBacktoEditor();
-  });
-
-  it("12. Step size tests", () => {
-    // Does not allow value less than 0.1
-    entityExplorer.SelectEntityByName("RangeSlider1");
-    propPane.UpdatePropertyFieldValue("Step size", "0");
-    agHelper.VerifyEvaluatedErrorMessage(
-      "This value must be greater than 0.1",
-    );
-    // Does not allow negative value
-    propPane.UpdatePropertyFieldValue("Step size", "-10");
-    agHelper.VerifyEvaluatedErrorMessage(
-      "This value must be greater than 0.1",
-    );
-    propPane.UpdatePropertyFieldValue("Step size", "5");
-    // Does not allow value greater than max value
-    propPane.UpdatePropertyFieldValue("Max. value", "100");
-    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
-    propPane.UpdatePropertyFieldValue("Min. value", "10");
-    agHelper.AssertElementAbsence(locators._evaluatedErrorMessage);
-    propPane.UpdatePropertyFieldValue("Step size", "110");
-    agHelper.VerifyEvaluatedErrorMessage("This value must be less than 90");
-  });
-
-  it("13. Min range test", () => {
-    // Does not accept value less than step size
-    propPane.UpdatePropertyFieldValue("Step size", "5");
-    propPane.UpdatePropertyFieldValue("Min. range", "2");
-    agHelper.VerifyEvaluatedErrorMessage(
-      "This value must be greater than or equal to step size",
-    );
+    agHelper.AssertElementExist(locators._widgetPageIcon("rangesliderwidget"));
   });
 });
