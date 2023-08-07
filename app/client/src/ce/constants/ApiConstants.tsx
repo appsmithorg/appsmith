c6f25e6c1e8d349f17b335c136fce207ea03cb2b
brayn003 <rudra@appsmith.com>
2023-08-07 20:05:59 +0530

    fix: adding branchname as url param

--- app/client/src/ce/constants/ApiConstants.tsx
+++ app/client/src/ce/constants/ApiConstants.tsx
@@ -40,8 +40,10 @@ export const LOGIN_SUBMIT_PATH = "login";
 export const SIGNUP_SUBMIT_PATH = "users";
 export const SUPER_USER_SUBMIT_PATH = `${SIGNUP_SUBMIT_PATH}/super`;
 
-export const getExportAppAPIRoute = (applicationId: string) =>
-  `/api/v1/applications/export/${applicationId}`;
-
+export const getExportAppAPIRoute = (applicationId: string) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const branch = urlParams.get("branch");
+  return `/api/v1/applications/export/${applicationId}?branchName=${branch}`;
+};
 export const getSnapShotAPIRoute = (applicationId: string) =>
   `/v1/applications/snapshot/${applicationId}`;
