86a33c8e0123e4b2db5a469d9afddba2b1fd534c
“sneha122” <sneha@appsmith.com>
2023-08-08 13:29:37 +0530

    fix: property pane table binding issue fixed

--- app/client/cypress/e2e/Regression/ClientSide/MobileResponsiveTests/SuggestedWidgets_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/MobileResponsiveTests/SuggestedWidgets_spec.js
@@ -3,16 +3,21 @@ import {
   dataSources,
   table,
   agHelper,
+  apiPage,
+  assertHelper,
+  entityExplorer,
 } from "../../../../support/Objects/ObjectsCore";
 import { Widgets } from "../../../../support/Pages/DataSources";
 import { featureFlagIntercept } from "../../../../support/Objects/FeatureFlags";
+import { PROPERTY_SELECTOR } from "../../../../locators/WidgetLocators";
+import { EntityItems } from "../../../../support/Pages/AssertHelper";
 
 describe("Check Suggested Widgets Feature in auto-layout", function () {
   before(() => {
     autoLayout.ConvertToAutoLayoutAndVerify(false);
     featureFlagIntercept(
       {
-        ab_ds_binding_enabled: true,
+        ab_ds_binding_enabled: false,
       },
       false,
     );
@@ -29,6 +34,28 @@ describe("Check Suggested Widgets Feature in auto-layout", function () {
     dataSources.AddSuggestedWidget(Widgets.Table);
     table.ReadTableRowColumnData(1, 0, "v2").then((cellData) => {
       expect(cellData).to.eq("5");
+    });
+  });
+
+  it("Bug 25432. Table widget bound to API with mock api url", () => {
+    let apiName = "mockAPI";
+    apiPage.CreateAndFillApi("https://mock-api.appsmith.com/users", apiName);
+    apiPage.RunAPI();
+    assertHelper.AssertNetworkStatus("@postExecute", 200);
+
+    dataSources.AddSuggestedWidget(Widgets.Table);
+    agHelper.AssertContains(
+      apiName + ".data.users",
+      "exist",
+      PROPERTY_SELECTOR.tableData + " .CodeMirror",
+    );
+
+    agHelper.Sleep(500);
+    entityExplorer.ExpandCollapseEntity("Queries/JS");
+    entityExplorer.ActionContextMenuByEntityName({
+      entityNameinLeftSidebar: apiName,
+      action: "Delete",
+      entityType: EntityItems.Api,
     });
   });
 });
--- app/client/src/components/editorComponents/ActionRightPane/SuggestedWidgets.tsx
+++ app/client/src/components/editorComponents/ActionRightPane/SuggestedWidgets.tsx
@@ -249,6 +249,10 @@ function getWidgetProps(
         props: {
           [fieldName]: `{{${actionName}.${suggestedWidget.bindingQuery}}}`,
           dynamicBindingPathList: [{ key: "tableData" }],
+          dynamicPropertyPathList:
+            suggestedWidget.bindingQuery === "data"
+              ? []
+              : [{ key: "tableData" }],
         },
         parentRowSpace: 10,
       };
