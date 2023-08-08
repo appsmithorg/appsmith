9a0aca12845801d78d5b7fe16f176c1913fcbfe2
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-08-08 20:25:14 +0530

    Add comments

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/SecurityConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/SecurityConfig.java
@@ -154,8 +154,10 @@ public class SecurityConfig {
                 // Default security headers configuration from
                 // https://docs.spring.io/spring-security/site/docs/5.0.x/reference/html/headers.html
                 .headers()
+                // Disabled here because add it in NGINX instead.
                 .contentTypeOptions()
                 .disable()
+                // Disabled because we use CSP's `frame-ancestors` instead.
                 .frameOptions()
                 .disable()
                 .and()
