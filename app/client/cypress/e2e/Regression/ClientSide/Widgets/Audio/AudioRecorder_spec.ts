067da7530a1e00d04aab268f830f6886237388f1
Parthvi <80334441+Parthvi12@users.noreply.github.com>
2023-08-09 15:30:26 +0530

    update test

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/Audio/AudioRecorder_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Audio/AudioRecorder_spec.ts
@@ -92,46 +92,56 @@ describe("Audio Recorder functionality tests", () => {
     propPane.TogglePropertyState("Disabled", "Off");
     propPane.SelectPlatformFunction("onRecordingStart", "Show alert");
     agHelper.EnterActionValue("Message", "Recording Started");
-    propPane.SelectPlatformFunction("onRecordingStart", "Show alert");
+    propPane.SelectPlatformFunction("onRecordingComplete", "Show alert");
     agHelper.EnterActionValue("Message", "Recording Completed");
     agHelper.GetNClick(".bp3-button");
     agHelper.GetNClick(".bp3-button");
     agHelper.ValidateToastMessage("Recording Started");
     agHelper.WaitUntilAllToastsDisappear();
-    agHelper.GetNClick(".bp3-button bp3-minimal");
-    agHelper.ValidateToastMessage("Recording Completed");
-    // verify in previwew mode
-    agHelper.GetNClick(".bp3-button");
-    agHelper.ValidateToastMessage("Recording Started");
-    agHelper.WaitUntilAllToastsDisappear();
-    agHelper.GetNClick(".bp3-button bp3-minimal");
+    agHelper.Sleep(2000); //for recorder to record
+    agHelper.GetNClick(".bp3-minimal");
     agHelper.ValidateToastMessage("Recording Completed");
     // verify in deploy mode
     deployMode.DeployApp();
     agHelper.Sleep(2000);
     cy.xpath("//button[@status='PERMISSION_PROMPT']").click();
+    cy.xpath("//button[@status='DEFAULT']").click();
+
     agHelper.ValidateToastMessage("Recording Started");
     agHelper.WaitUntilAllToastsDisappear();
-    agHelper.GetNClick(".bp3-button bp3-minimal");
+    agHelper.GetNClick(".bp3-minimal");
     agHelper.ValidateToastMessage("Recording Completed");
+    agHelper.WaitUntilAllToastsDisappear();
+    deployMode.NavigateBacktoEditor();
+    // verify in preview mode
+    agHelper.GetNClick(locators._enterPreviewMode);
+    cy.xpath("//button[@status='PERMISSION_PROMPT']").click();
+    cy.xpath("//button[@status='DEFAULT']").click();
+    agHelper.ValidateToastMessage("Recording Started");
+    agHelper.WaitUntilAllToastsDisappear();
+    agHelper.GetNClick(".bp3-minimal");
+    agHelper.ValidateToastMessage("Recording Completed");
+    //Exit preview mode
+    agHelper.GetNClick(locators._exitPreviewMode);
   });
 
   it("Verify style tab's proprties", () => {
+    entityExplorer.SelectEntityByName("AudioRecorder1", "Widgets");
     propPane.MoveToTab("Style");
     propPane.EnterJSContext("Button color", "#FFC13D");
     agHelper.Sleep(1000);
     propPane.SelectColorFromColorPicker("iconcolor", -15);
-    cy.xpath("//button[@status='PERMISSION_PROMPT']").should(
+    cy.xpath("//button[@status='COMPLETE']").should(
       "have.css",
       "background-color",
       "rgb(255, 193, 61)",
     );
     propPane.EnterJSContext("Box shadow", "Small");
-    cy.xpath("//button[@status='PERMISSION_PROMPT']")
+    cy.xpath("//button[@status='COMPLETE']")
       .should("have.css", "box-shadow")
       .and("not.eq", "none");
     propPane.EnterJSContext("Border radius", "none");
-    cy.xpath("//button[@status='PERMISSION_PROMPT']")
+    cy.xpath("//button[@status='COMPLETE']")
       .should("have.css", "border-radius")
       .and("eq", "3px");
   });
