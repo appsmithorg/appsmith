979b41f62e14760861c782c91caab6e64aa92152
Aswath K <aswath.sana@gmail.com>
2023-08-09 09:52:15 +0530

    chore: Add id & ref to Flex component

--- app/client/packages/design-system/widgets/src/components/Flex/Flex.tsx
+++ app/client/packages/design-system/widgets/src/components/Flex/Flex.tsx
@@ -2,93 +2,102 @@ import React from "react";
 import { StyledContainerFlex, StyledFlex } from "./index.styled";
 import type { FlexProps } from "./types";
 
-export const Flex = (props: FlexProps) => {
-  const {
-    alignContent,
-    alignItems,
-    alignSelf,
-    children,
-    className,
-    columnGap,
-    direction,
-    flex,
-    flexBasis,
-    flexGrow,
-    flexShrink,
-    gap,
-    height,
-    isContainer = false,
-    isHidden = false,
-    justifyContent,
-    justifySelf,
-    margin,
-    marginBottom,
-    marginLeft,
-    marginRight,
-    marginTop,
-    maxHeight,
-    maxWidth,
-    minHeight,
-    minWidth,
-    order,
-    padding,
-    paddingBottom,
-    paddingLeft,
-    paddingRight,
-    paddingTop,
-    rowGap,
-    style,
-    width,
-    wrap,
-  } = props;
+export const Flex = React.forwardRef(
+  (props: FlexProps, ref: React.Ref<HTMLDivElement>) => {
+    const {
+      alignContent,
+      alignItems,
+      alignSelf,
+      children,
+      className,
+      columnGap,
+      direction,
+      flex,
+      flexBasis,
+      flexGrow,
+      flexShrink,
+      gap,
+      height,
+      id,
+      isContainer = false,
+      isHidden = false,
+      justifyContent,
+      justifySelf,
+      margin,
+      marginBottom,
+      marginLeft,
+      marginRight,
+      marginTop,
+      maxHeight,
+      maxWidth,
+      minHeight,
+      minWidth,
+      order,
+      padding,
+      paddingBottom,
+      paddingLeft,
+      paddingRight,
+      paddingTop,
+      rowGap,
+      style,
+      width,
+      wrap,
+    } = props;
 
-  const renderFlex = () => {
+    const renderFlex = () => {
+      return (
+        <StyledFlex
+          $alignContent={alignContent}
+          $alignItems={alignItems}
+          $alignSelf={alignSelf}
+          $columnGap={columnGap}
+          $direction={direction}
+          $flex={flex}
+          $flexBasis={flexBasis}
+          $flexGrow={flexGrow}
+          $flexShrink={flexShrink}
+          $gap={gap}
+          $height={height}
+          $isHidden={isHidden}
+          $justifyContent={justifyContent}
+          $justifySelf={justifySelf}
+          $margin={margin}
+          $marginBottom={marginBottom}
+          $marginLeft={marginLeft}
+          $marginRight={marginRight}
+          $marginTop={marginTop}
+          $maxHeight={maxHeight}
+          $maxWidth={maxWidth}
+          $minHeight={minHeight}
+          $minWidth={minWidth}
+          $order={order}
+          $padding={padding}
+          $paddingBottom={paddingBottom}
+          $paddingLeft={paddingLeft}
+          $paddingRight={paddingRight}
+          $paddingTop={paddingTop}
+          $rowGap={rowGap}
+          $width={width}
+          $wrap={wrap}
+          className={className}
+          id={id}
+          ref={ref}
+          style={style}
+        >
+          {children}
+        </StyledFlex>
+      );
+    };
+
     return (
-      <StyledFlex
-        $alignContent={alignContent}
-        $alignItems={alignItems}
-        $alignSelf={alignSelf}
-        $columnGap={columnGap}
-        $direction={direction}
-        $flex={flex}
-        $flexBasis={flexBasis}
-        $flexGrow={flexGrow}
-        $flexShrink={flexShrink}
-        $gap={gap}
-        $height={height}
-        $isHidden={isHidden}
-        $justifyContent={justifyContent}
-        $justifySelf={justifySelf}
-        $margin={margin}
-        $marginBottom={marginBottom}
-        $marginLeft={marginLeft}
-        $marginRight={marginRight}
-        $marginTop={marginTop}
-        $maxHeight={maxHeight}
-        $maxWidth={maxWidth}
-        $minHeight={minHeight}
-        $minWidth={minWidth}
-        $order={order}
-        $padding={padding}
-        $paddingBottom={paddingBottom}
-        $paddingLeft={paddingLeft}
-        $paddingRight={paddingRight}
-        $paddingTop={paddingTop}
-        $rowGap={rowGap}
-        $width={width}
-        $wrap={wrap}
-        className={className}
-        style={style}
-      >
-        {children}
-      </StyledFlex>
+      <>
+        {isContainer && (
+          <StyledContainerFlex>{renderFlex()}</StyledContainerFlex>
+        )}
+        {!isContainer && <>{renderFlex()}</>}
+      </>
     );
-  };
+  },
+);
 
-  return (
-    <>
-      {isContainer && <StyledContainerFlex>{renderFlex()}</StyledContainerFlex>}
-      {!isContainer && <>{renderFlex()}</>}
-    </>
-  );
-};
+Flex.displayName = "Flex";
