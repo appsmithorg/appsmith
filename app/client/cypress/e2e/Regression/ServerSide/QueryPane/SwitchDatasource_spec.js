4f75cd6dbeba0cab52776d01ea526fe7c37a56ca
Parthvi <80334441+Parthvi12@users.noreply.github.com>
2023-08-09 23:52:38 +0530

    update SwitchDatasource test

--- app/client/cypress/e2e/Regression/ServerSide/QueryPane/SwitchDatasource_spec.js
+++ app/client/cypress/e2e/Regression/ServerSide/QueryPane/SwitchDatasource_spec.js
@@ -1,103 +1,79 @@
 const datasource = require("../../../../locators/DatasourcesEditor.json");
 const queryLocators = require("../../../../locators/QueryEditor.json");
-import { dataSources } from "../../../../support/Objects/ObjectsCore";
+import { dataSources, agHelper } from "../../../../support/Objects/ObjectsCore";
 describe("Switch datasource", function () {
   let postgresDatasourceName;
   let postgresDatasourceNameSecond;
   let mongoDatasourceName;
-
+  let guid, dsName_1, dsName_2, MongoDB;
   beforeEach(() => {
     cy.startRoutesForDatasource();
   });
 
   it("1. Create postgres datasource", function () {
-    cy.NavigateToDatasourceEditor();
-    cy.get(datasource.PostgreSQL).click({ force: true });
-    cy.generateUUID().then((uid) => {
-      postgresDatasourceName = uid;
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      dataSources.NavigateToDSCreateNew();
+      dataSources.CreatePlugIn("PostgreSQL");
+      guid = uid.toLowerCase();
+      agHelper.RenameWithInPane("Postgres_1_" + guid, false);
+      dataSources.FillPostgresDSForm();
+      dataSources.TestSaveDatasource();
 
-      cy.get(".t--edit-datasource-name").click();
-      cy.get(".t--edit-datasource-name input")
-        .clear()
-        .type(postgresDatasourceName, { force: true })
-        .should("have.value", postgresDatasourceName)
-        .blur();
+      cy.wrap("Postgres_1_" + guid).as("dsName_1");
+      cy.get("@dsName_1").then(($dsName) => {
+        dsName_1 = $dsName;
+      });
     });
-    cy.fillPostgresDatasourceForm();
-    cy.testSaveDatasource();
   });
-
   it("2. Create another postgres datasource", function () {
-    cy.NavigateToDatasourceEditor();
-    cy.get(datasource.PostgreSQL).click({ force: true });
-    cy.generateUUID().then((uid) => {
-      postgresDatasourceNameSecond = uid;
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      dataSources.NavigateToDSCreateNew();
+      dataSources.CreatePlugIn("PostgreSQL");
+      guid = uid.toLowerCase();
+      agHelper.RenameWithInPane("Postgres_2_" + guid, false);
+      dataSources.FillPostgresDSForm();
+      dataSources.TestSaveDatasource();
 
-      cy.get(".t--edit-datasource-name").click();
-      cy.get(".t--edit-datasource-name input")
-        .clear()
-        .type(postgresDatasourceNameSecond, { force: true })
-        .should("have.value", postgresDatasourceNameSecond)
-        .blur();
+      cy.wrap("Postgres_2_" + guid).as("dsName_2");
+      cy.get("@dsName_2").then(($dsName) => {
+        dsName_2 = $dsName;
+      });
     });
-    cy.fillPostgresDatasourceForm();
-    cy.testSaveDatasource();
   });
 
   it("3. Create mongo datasource", function () {
-    cy.NavigateToDatasourceEditor();
-    cy.get(datasource.MongoDB).click();
-    cy.generateUUID().then((uid) => {
-      mongoDatasourceName = uid;
-
-      cy.get(".t--edit-datasource-name").click();
-      cy.get(".t--edit-datasource-name input")
-        .clear()
-        .type(mongoDatasourceName, { force: true })
-        .should("have.value", mongoDatasourceName)
-        .blur();
+    agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      dataSources.NavigateToDSCreateNew();
+      dataSources.CreatePlugIn("Mongo");
+      guid = uid.toLowerCase();
+      agHelper.RenameWithInPane("Mongo" + guid, false);
+      dataSources.FillMongoDSForm();
+      dataSources.TestSaveDatasource();
+      cy.wrap("Mongo" + guid).as("MongoDB");
+      cy.get("@MongoDB").then(($dsName) => {
+        MongoDB = $dsName;
+      });
     });
-    cy.fillMongoDatasourceForm();
-    cy.testSaveDatasource();
   });
 
   it("4. By switching datasources execute a query with both the datasources", function () {
     dataSources.NavigateToActiveTab();
-    dataSources.CreateQueryFromActiveTab(postgresDatasourceName);
-
-    // Update Action API is not being called after run, we dont need this validation here
-    // cy.wait("@saveAction").should(
-    //   "have.nested.property",
-    //   "response.body.data.isValid",
-    //   true,
-    // );
+    dataSources.CreateQueryFromActiveTab(dsName_1);
     cy.get(".rc-select-show-arrow").click();
-    cy.contains(".rc-select-item-option-content", postgresDatasourceNameSecond)
-      .click()
-      .wait(1000);
+    cy.contains(".rc-select-item-option-content", dsName_2).click().wait(1000);
     cy.runQuery();
-
-    // Update Action API is not being called after run, we dont need this validation here
-    // cy.wait("@saveAction").should(
-    //   "have.nested.property",
-    //   "response.body.data.isValid",
-    //   true,
-    // );
-  });
-
-  it("5. Confirm mongo datasource is not present in the switch datasources dropdown", function () {
+    // Confirm mongo datasource is not present in the switch datasources dropdown
     cy.get(".rc-select-show-arrow").click();
-    cy.get(".rc-select-item-option-content").should(
-      "not.have",
-      mongoDatasourceName,
-    );
+    cy.get(".rc-select-item-option-content").should("not.have", MongoDB);
   });
 
-  it("6. Delete the query and datasources", function () {
+  it("5. Delete the query and datasources", function () {
     dataSources.DeleteQuery("Query1");
-
-    cy.deleteDatasource(postgresDatasourceName);
-    cy.deleteDatasource(postgresDatasourceNameSecond);
-    cy.deleteDatasource(mongoDatasourceName);
+    cy.deleteDatasource(MongoDB);
+    cy.deleteDatasource(dsName_1);
+    cy.deleteDatasource(dsName_2);
   });
 });
