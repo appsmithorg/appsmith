f1bdc7c5221bf703e0fc839e5c358925a900c4f1
Aishwarya UR <aishwarya@appsmith.com>
2023-08-10 11:10:04 +0530

    propPane.EnterJSContext() used

--- app/client/cypress/e2e/Regression/ServerSide/Params/PassingParams_Spec.ts
+++ app/client/cypress/e2e/Regression/ServerSide/Params/PassingParams_Spec.ts
@@ -18,20 +18,13 @@ describe("Bug #10784 - Passing params from JS to SQL query should not break", ()
   before(() => {
     entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON, 100, 100);
     entityExplorer.DragDropWidgetNVerify(draggableWidgets.SELECT, 500, 100);
-    propPane.ToggleJSMode("sourcedata");
-    propPane.UpdatePropertyFieldValue(
+    propPane.EnterJSContext(
       "Source Data",
       `[\n  {\n    \"label\": \"7\",\n    \"value\": \"7\"\n  },\n  {\n    \"label\": \"8\",\n    \"value\": \"8\"\n  },\n  {\n    \"label\": \"9\",\n    \"value\": \"9\"\n  }\n]`,
     );
 
-    propPane.ToggleJSMode("labelkey");
-    (cy as any).updateCodeInput(
-      ".t--property-control-wrapper.t--property-control-labelkey",
-      `label`,
-    );
-
-    propPane.ToggleJSMode("valuekey");
-    (cy as any).updateCodeInput(".t--property-control-valuekey", `value`);
+    propPane.EnterJSContext("Label key", "label");
+    propPane.EnterJSContext("Value key", "value");
 
     propPane.UpdatePropertyFieldValue(
       "Default selected value",
