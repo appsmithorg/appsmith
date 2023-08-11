a884baaa5ee18f9adedbbcd98cdca188db031f63
Pawan Kumar <pawan.stardust@gmail.com>
2023-08-11 17:31:35 +0530

    update tests

--- app/client/packages/design-system/theming/src/color/LightModeTheme.test.tsx
+++ app/client/packages/design-system/theming/src/color/LightModeTheme.test.tsx
@@ -2,21 +2,29 @@ import { LightModeTheme } from "./LightModeTheme";
 
 describe("@design-system/theming/color/LightModeTheme", () => {
   it("checks bg color", () => {
-    // seed is very light
-    const veryLightSeed = new LightModeTheme("oklch(94% 0.03 231)").bg;
-    expect(veryLightSeed.oklch.l).toBe(0.9);
+    const { bg: veryLightSeedBg } = new LightModeTheme(
+      "oklch(0.95 0.09 231)",
+    ).getColors();
+    expect(veryLightSeedBg).toBe("oklch(0.9 0.009 231)");
 
-    // seed is not very light
-    const notVeryLightSeed = new LightModeTheme("oklch(90% 0.03 231)").bg;
-    expect(notVeryLightSeed.oklch.l).toBe(0.985);
+    const { bg: notVerylightSeedBg } = new LightModeTheme(
+      "oklch(0.92 0.09 231)",
+    ).getColors();
+    expect(notVerylightSeedBg).toBe("oklch(0.985 0.009 231)");
 
-    const coldSeed = new LightModeTheme("oklch(90% 0.07 231)").bg;
-    expect(coldSeed.oklch.c).toBe(0.009);
+    const { bg: coldSeedBg } = new LightModeTheme(
+      "oklch(0.95 0.07 231)",
+    ).getColors();
+    expect(coldSeedBg).toBe("oklch(0.9 0.009 231)");
 
-    const notColdSeed = new LightModeTheme("oklch(90% 0.07 110)").bg;
-    expect(notColdSeed.oklch.c).toBe(0.007);
+    const { bg: notColdSeedBg } = new LightModeTheme(
+      "oklch(0.92 0.07 110)",
+    ).getColors();
+    expect(notColdSeedBg).toBe("oklch(0.985 0.007 110)");
 
-    const aChromaticSeed = new LightModeTheme("oklch(90% 0.02 110)").bg;
-    expect(aChromaticSeed.oklch.c).toBe(0);
+    const { bg: aChromaticSeedBg } = new LightModeTheme(
+      "oklch(0.92 0.02 110)",
+    ).getColors();
+    expect(aChromaticSeedBg).toBe("oklch(0.985 0 110)");
   });
 });
