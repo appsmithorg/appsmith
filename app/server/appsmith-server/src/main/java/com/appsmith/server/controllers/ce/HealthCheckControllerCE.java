de65c778f527b740b32e77e50ad00eb27af1f6d2
Shubham Saxena <shubhamsaxenagithub@gmail.com>
2023-08-17 18:15:47 +0530

    remove rate limit annotation from health check controller

--- app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ce/HealthCheckControllerCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ce/HealthCheckControllerCE.java
@@ -3,7 +3,6 @@ package com.appsmith.server.controllers.ce;
 import com.appsmith.external.views.Views;
 import com.appsmith.server.constants.Url;
 import com.appsmith.server.dtos.ResponseDTO;
-import com.appsmith.server.ratelimiting.annotations.RateLimit;
 import com.appsmith.server.services.HealthCheckService;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
@@ -18,7 +17,6 @@ public class HealthCheckControllerCE {
     private final HealthCheckService healthCheckService;
 
     @JsonView(Views.Public.class)
-    @RateLimit(api = "health-check")
     @GetMapping
     public Mono<ResponseDTO<String>> getHealth() {
         return healthCheckService.getHealth().map(health -> new ResponseDTO<>(HttpStatus.OK.value(), health, null));
