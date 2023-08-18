fff1e422d8c23158766eddd1853ada1836a2025b
Dhruvik Neharia <dhruvik@appsmith.com>
2023-08-18 20:40:18 +0530

    fix: chart onDataPointClick now uses GetNClickByContains

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/Chart/ChartDataPoint_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Chart/ChartDataPoint_Spec.ts
@@ -63,16 +63,17 @@ describe("Input widget test with default value from chart datapoint", () => {
   it("2. onDataPointClick should work and respond with x, y, seriesTitle, and rawEventData (in case of custom fusion chart).", () => {
     assertHelper.AssertNetworkStatus("@updateLayout");
     entityExplorer.SelectEntityByName("Chart1");
+    propPane.TogglePropertyState("Show Labels", "On");
     propPane.SelectPlatformFunction("onDataPointClick", "Show alert");
     agHelper.EnterActionValue("Message", dataSet.bindingDataPoint);
 
     deployMode.DeployApp();
     agHelper.Sleep(1500); //waiting for chart to load!
 
-    agHelper.GetNClick(widgetLocators.chartDataPoint);
+    agHelper.GetNClickByContains(widgetLocators.chartDataPoint, "36000");
     cy.get(locators._toastMsg).should(
       "have.text",
-      `{"x":"Mon","y":10000,"seriesTitle":"Sales"}`,
+      `{"x":"Sun","y":36000,"seriesTitle":"Sales"}`,
     );
     deployMode.NavigateBacktoEditor();
 
@@ -92,7 +93,10 @@ describe("Input widget test with default value from chart datapoint", () => {
     deployMode.DeployApp();
 
     agHelper.Sleep(1500); //waiting for chart to load!
-    agHelper.GetNClick(widgetLocators.sankeyChartDataPoint, 0, true);
+    agHelper.GetNClickByContains(
+      widgetLocators.chartDataPoint,
+      "European Union",
+    );
     cy.get(locators._toastMsg).should(
       "have.text",
       `{"x":-1,"y":-1,"seriesTitle":"","rawEventData":{"color":"#FFC533","alpha":100,"labelFill":"#666","labelAlpha":100,"value":4747591,"label":"European Union","sourceLinks":["France","United States","United Kingdom","Switzerland","Austria","Sweden"],"targetLinks":["Netherlands","Germany","Belgium","China","Italy","Russia","Spain"]}}`,
