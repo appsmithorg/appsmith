184304765c867aa2725ba05c3bf7d29c5850aa2b
Aishwarya UR <aishwarya@appsmith.com>
2023-08-18 12:13:38 +0530

    ClientSide/EmbedSettings/EmbedSettings_spec.js fix

--- app/client/cypress/support/AdminSettingsCommands.js
+++ app/client/cypress/support/AdminSettingsCommands.js
@@ -73,9 +73,10 @@ Cypress.Commands.add("waitForServerRestart", () => {
   cy.get(adminSettings.restartNotice).should("be.visible");
   // Wait for restart notice to not be visible with a timeout
   // Cannot use cy.get as mentioned in https://github.com/NoriSte/cypress-wait-until/issues/75#issuecomment-572685623
-  cy.waitUntil(() => !Cypress.$(adminSettings.restartNotice).length, {
-    timeout: 180000,
-  });
+  // cy.waitUntil(() => !Cypress.$(adminSettings.restartNotice).length, {
+  //   timeout: 180000,
+  // });
+  cy.get(adminSettings.restartNotice, { timeout: 180000 }).should("not.exist");
   cy.window().then((win) => {
     win.location.reload();
   });
