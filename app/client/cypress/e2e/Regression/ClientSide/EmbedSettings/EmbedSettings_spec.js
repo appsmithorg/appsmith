dc285f150f80a4347e16135eddca0c07386ed03b
Aishwarya UR <aishwarya@appsmith.com>
2023-08-21 15:43:03 +0530

    waitForServerRestart() update

--- app/client/cypress/e2e/Regression/ClientSide/EmbedSettings/EmbedSettings_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/EmbedSettings/EmbedSettings_spec.js
@@ -84,7 +84,6 @@ describe("Embed settings options", function () {
     });
     cy.get(adminSettings.saveButton).click();
     cy.waitForServerRestart();
-    cy.get(adminSettings.restartNotice).should("not.exist");
     cy.get("@deployUrl").then((depUrl) => {
       cy.log("deployUrl is " + depUrl);
       deployUrl = depUrl;
@@ -110,7 +109,7 @@ describe("Embed settings options", function () {
     //   } = interception[1].response.body.data;
     //   expect(APPSMITH_ALLOWED_FRAME_ANCESTORS).to.equal("*");
     // });
-    cy.get(adminSettings.restartNotice).should("not.exist");
+    cy.log("deployUrl is " + depUrl);
     cy.visit(deployUrl, { timeout: 60000 });
     getIframeBody().contains("Submit").should("exist");
     ValidateEditModeSetting(_.embedSettings.locators._allowAllText);
@@ -124,7 +123,7 @@ describe("Embed settings options", function () {
     });
     cy.get(adminSettings.saveButton).click();
     cy.waitForServerRestart();
-    cy.get(adminSettings.restartNotice).should("not.exist");
+    cy.log("deployUrl is " + depUrl);
     cy.visit(deployUrl, { timeout: 60000 });
     // TODO: Commented out as it is flaky
     // cy.wait(["@getEnvVariables", "@getEnvVariables"]).then((interception) => {
