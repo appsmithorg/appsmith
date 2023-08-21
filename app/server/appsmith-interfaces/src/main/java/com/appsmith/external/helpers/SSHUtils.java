c496b3406e0eaaf52ee3dd49a8426ce8b4353250
Sumit Kumar <sumit@appsmith.com>
2023-08-21 15:35:13 +0530

    add comments

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/helpers/SSHUtils.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/helpers/SSHUtils.java
@@ -14,15 +14,27 @@ import static com.appsmith.external.constants.PluginConstants.HostName.LOCALHOST
 
 @NoArgsConstructor
 public class SSHUtils {
-    static Object monitor = new Object();
-    public static final int RANDOM_FREE_PORT_NUM = 0;
+    static Object monitor = new Object(); // monitor object to be used for synchronization lock
+    public static final int RANDOM_FREE_PORT_NUM = 0; // using port 0 indicates `bind` method to acquire random free
+    // port
 
-    // TODO: add comments
+    /**
+     * This class is meant to provide methods that should help with the creation and management of SSH tunnel by
+     * various plugins.
+     *
+     * @param sshHost : host address of remote SSH server
+     * @param sshPort : port number for remote SSH server
+     * @param username : login username for the remote SSH server account
+     * @param key : client private key
+     * @param dbHost : host address on which the DB is hosted relative to the SSH host
+     * @param dbPort : port address on which the DB is hosted relative to the SSH host
+     * @return
+     * @throws IOException
+     */
     public static SSHTunnelContext createSSHTunnel(String sshHost, int sshPort, String username, String key,
                                                    String dbHost, int dbPort) throws IOException {
         final SSHClient client = new SSHClient();
 
-        client.loadKnownHosts();
         client.connect(sshHost, sshPort);
         PKCS8KeyFile keyFile = new PKCS8KeyFile();
         keyFile.init(key, null);
@@ -31,6 +43,12 @@ public class SSHUtils {
         final ServerSocket serverSocket = new ServerSocket();
         final Parameters params = new Parameters(LOCALHOST, RANDOM_FREE_PORT_NUM, dbHost, dbPort);
         serverSocket.setReuseAddress(true);
+
+        /**
+         * This method has been synchronized via a lock because it is not documented if the `bind` method is thread
+         * safe or not. In particular, this is a concern because we are acquiring a free port address via the `bind`
+         * method. Hypothetically, there could be a race condition when acquiring the free port.
+         */
         synchronized (monitor) {
             serverSocket.bind(new InetSocketAddress(params.getLocalHost(), params.getLocalPort()));
         }
