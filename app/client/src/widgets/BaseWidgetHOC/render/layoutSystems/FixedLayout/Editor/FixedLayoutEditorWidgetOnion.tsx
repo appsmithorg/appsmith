559ed0a99657c85f4496465a29bc9f824b155684
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-08-23 17:53:48 +0530

    chore: add auto height overlay to fixed widget onion.

--- app/client/src/widgets/BaseWidgetHOC/render/layoutSystems/FixedLayout/Editor/FixedLayoutEditorWidgetOnion.tsx
+++ app/client/src/widgets/BaseWidgetHOC/render/layoutSystems/FixedLayout/Editor/FixedLayoutEditorWidgetOnion.tsx
@@ -4,26 +4,29 @@ import DraggableComponent from "widgets/BaseWidgetHOC/render/common/DraggableCom
 import SnipeableComponent from "widgets/BaseWidgetHOC/render/common/SnipeableComponent";
 import { WidgetNameLayer } from "widgets/BaseWidgetHOC/render/common/WidgetNameLayer";
 import type { BaseWidgetProps } from "widgets/BaseWidgetHOC/withBaseWidgetHOC";
+import { AutoHeightOverlayLayer } from "../common/AutoHeightOverlayLayer";
 import { FixedLayoutWigdetComponent } from "../common/FixedLayoutWidgetComponent";
 import { PositionedComponentLayer } from "../PositionedComponentLayer";
 import { ResizableLayer } from "../ResizableLayer";
 
 export const FixedLayoutEditorWidgetOnion = (props: BaseWidgetProps) => {
   return (
-    <PositionedComponentLayer {...props}>
-      <SnipeableComponent {...props}>
-        <DraggableComponent {...props}>
-          <WidgetNameLayer {...props}>
-            <ResizableLayer {...props}>
-              <ErrorBoundary>
-                <FixedLayoutWigdetComponent {...props}>
-                  {props.children}
-                </FixedLayoutWigdetComponent>
-              </ErrorBoundary>
-            </ResizableLayer>
-          </WidgetNameLayer>
-        </DraggableComponent>
-      </SnipeableComponent>
-    </PositionedComponentLayer>
+    <AutoHeightOverlayLayer {...props}>
+      <PositionedComponentLayer {...props}>
+        <SnipeableComponent {...props}>
+          <DraggableComponent {...props}>
+            <WidgetNameLayer {...props}>
+              <ResizableLayer {...props}>
+                <ErrorBoundary>
+                  <FixedLayoutWigdetComponent {...props}>
+                    {props.children}
+                  </FixedLayoutWigdetComponent>
+                </ErrorBoundary>
+              </ResizableLayer>
+            </WidgetNameLayer>
+          </DraggableComponent>
+        </SnipeableComponent>
+      </PositionedComponentLayer>
+    </AutoHeightOverlayLayer>
   );
 };
