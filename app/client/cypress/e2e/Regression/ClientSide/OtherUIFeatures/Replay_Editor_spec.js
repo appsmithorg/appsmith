8ecec949f28ef19ebfbf1eb67d4eba19a7116789
Vijetha-Kaja <vijetha@appsmith.com>
2023-08-24 21:34:15 +0530

    replay editor fix

--- app/client/cypress/e2e/Regression/ClientSide/OtherUIFeatures/Replay_Editor_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/OtherUIFeatures/Replay_Editor_spec.js
@@ -9,10 +9,11 @@ import {
   agHelper,
   jsEditor,
   dataSources,
+  entityExplorer,
 } from "../../../../support/Objects/ObjectsCore";
 
 describe("Undo/Redo functionality", function () {
-  const modifierKey = Cypress.platform === "darwin" ? "meta" : "ctrl";
+  const modifierKey = Cypress.platform === "darwin" ? "meta" : "cmd";
   let postgresDatasourceName;
 
   it("1. Checks undo/redo in datasource forms", () => {
@@ -157,25 +158,22 @@ describe("Undo/Redo functionality", function () {
     });
     // cy.get(".function-name").should("not.contain.text", "test");
   });
-
+ 
   it("5. Checks undo/redo for Authenticated APIs", () => {
     cy.NavigateToAPI_Panel();
     cy.get(apiwidget.createAuthApiDatasource).click({ force: true });
     cy.wait(2000);
-    agHelper.TypeText("input[name='url']", testdata.baseUrl);
     agHelper.TypeText("input[name='headers[0].key']", testdata.headerKey);
-    agHelper.GetNClick("body");
-    agHelper.TypeText("body", `{${modifierKey}}z`);
-    agHelper.TypeText("body", `{${modifierKey}}z`);
-    cy.wait(2000);
+    agHelper.TypeText("input[name='url']", testdata.baseUrl);
+    agHelper.Sleep(1000);
+    cy.get("body").click(0, 0);
+    cy.get("body").type(`{${modifierKey}}z`);
     cy.get("input[name='headers[0].key']").should("have.value", "");
     cy.get("input[name='url']").should("have.value", "");
+    cy.get("body").click(0, 0);
     cy.get("body").type(`{${modifierKey}}{shift}z`);
     cy.get("body").type(`{${modifierKey}}{shift}z`);
-    cy.get("input[name='url']").should(
-      "have.value",
-      "https://mock-api.appsmith.com/",
-    );
+    cy.get("input[name='url']").should("have.value", testdata.baseUrl);
     cy.get("input[name='headers[0].key']").should("have.value", "Content-Type");
   });
 });
