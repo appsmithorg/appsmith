cfe36b822fdfe09bfeffe5638e649ae8b06acdb3
Hetu Nandu <hetunandu@gmail.com>
2023-08-24 12:40:31 +0530

    update cypress test

--- app/client/cypress/e2e/Regression/ClientSide/FormLogin/EmailVerfication_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/FormLogin/EmailVerfication_spec.ts
@@ -1,70 +1,6 @@
 import adminsSettings from "../../../../locators/AdminsSettings";
 
 describe("Email verification", () => {
-  it("Email verification settings test", () => {
-    cy.LogOut();
-    cy.LoginFromAPI(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
-    cy.get(".admin-settings-menu-option").should("be.visible");
-    cy.get(".admin-settings-menu-option").click();
-    cy.url().should("contain", "/settings/general");
-    cy.wait("@getEnvVariables");
-    cy.get(adminsSettings.authenticationTab).click();
-    cy.get(adminsSettings.formloginButton).click();
-    cy.pause();
-    // Assert verification is disabled
-    cy.get(adminsSettings.enableEmailVerificationInput).should("be.disabled");
-    // Assert callout
-    cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
-    // go to email settings
-    cy.get(adminsSettings.emailTab).click();
-    // add email settings
-    cy.generateUUID().then((uuid) => {
-      cy.get(adminsSettings.fromAddress).clear().type(`${uuid}@appsmith.com`);
-    });
-    // save
-    cy.get(adminsSettings.saveButton).click();
-    cy.waitUntil(() =>
-      cy.contains("General", { timeout: 180000 }).should("be.visible"),
-    );
-    cy.wait("@getEnvVariables");
-    cy.get(adminsSettings.authenticationTab).click();
-    cy.get(adminsSettings.formloginButton).click();
-    // Assert verification is enabled
-    cy.get(adminsSettings.enableEmailVerificationInput).should(
-      "not.be.disabled",
-    );
-    // Assert callout
-    cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
-    // turn on setting
-    cy.get(adminsSettings.enableEmailVerificationInput).click();
-    // Assert callout
-    cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
-    cy.get(adminsSettings.saveButton).click();
-    cy.wait("@postTenantConfig").then((interception) => {
-      expect(interception.request.body.enableEmailVerification).to.equal(true);
-    });
-    cy.get(adminsSettings.emailTab).click();
-    // remove email settings
-    cy.get(adminsSettings.saveButton).click();
-    cy.waitUntil(() =>
-      cy.contains("General", { timeout: 180000 }).should("be.visible"),
-    );
-    cy.wait("@getEnvVariables");
-    cy.get(adminsSettings.authenticationTab).click();
-    cy.get(adminsSettings.formloginButton).click();
-    // Assert verification is enabled
-    cy.get(adminsSettings.enableEmailVerificationInput).should(
-      "not.be.disabled",
-    );
-    // Assert callout
-    cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
-    // turn off setting
-    cy.get(adminsSettings.enableEmailVerificationInput).click();
-    // assert verification is disabled
-    cy.get(adminsSettings.enableEmailVerificationInput).should("be.disabled");
-    // assert callout
-    cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
-  });
   it("Shows the email verification pending page correctly", () => {
     cy.LogOut();
     cy.visit("/user/verificationPending?email=test@appsmith.com");
@@ -108,5 +44,75 @@ describe("Email verification", () => {
     cy.get("[data-testid='verification-error']").matchImageSnapshot(
       "Unknown error",
     );
+  });
+  it("Email verification settings test", () => {
+    cy.LogOut();
+    cy.LoginFromAPI(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
+    cy.get(".admin-settings-menu-option").should("be.visible");
+    cy.get(".admin-settings-menu-option").click();
+    cy.url().should("contain", "/settings/general");
+    cy.wait("@getEnvVariables");
+    cy.get(adminsSettings.authenticationTab).click();
+    cy.get(adminsSettings.formloginButton).click();
+    cy.pause();
+    // Assert verification is disabled
+    cy.get(adminsSettings.enableEmailVerificationInput).should("be.disabled");
+    // Assert callout
+    cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+    // go to email settings
+    cy.get(adminsSettings.emailTab).click();
+    // add email settings
+    cy.generateUUID().then((uuid) => {
+      cy.get(adminsSettings.fromAddress).clear().type(`${uuid}@appsmith.com`);
+    });
+    // save
+    cy.get(adminsSettings.saveButton).click();
+    cy.waitUntil(() =>
+      cy.contains("General", { timeout: 180000 }).should("be.visible"),
+    ).then(() => {
+      cy.wait("@getEnvVariables");
+      cy.get(adminsSettings.authenticationTab).click();
+      cy.get(adminsSettings.formloginButton).click();
+      // Assert verification is enabled
+      cy.get(adminsSettings.enableEmailVerificationInput).should(
+        "not.be.disabled",
+      );
+      // Assert callout
+      cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+      // turn on setting
+      cy.get(adminsSettings.enableEmailVerificationInput).click();
+      // Assert callout
+      cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+      cy.get(adminsSettings.saveButton).click();
+      cy.wait("@postTenantConfig").then((interception) => {
+        expect(interception.request.body.enableEmailVerification).to.equal(
+          true,
+        );
+      });
+      cy.get(adminsSettings.emailTab).click();
+      // remove email settings
+      cy.get(adminsSettings.saveButton).click();
+      cy.waitUntil(() =>
+        cy.contains("General", { timeout: 180000 }).should("be.visible"),
+      ).then(() => {
+        cy.wait("@getEnvVariables");
+        cy.get(adminsSettings.authenticationTab).click();
+        cy.get(adminsSettings.formloginButton).click();
+        // Assert verification is enabled
+        cy.get(adminsSettings.enableEmailVerificationInput).should(
+          "not.be.disabled",
+        );
+        // Assert callout
+        cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+        // turn off setting
+        cy.get(adminsSettings.enableEmailVerificationInput).click();
+        // assert verification is disabled
+        cy.get(adminsSettings.enableEmailVerificationInput).should(
+          "be.disabled",
+        );
+        // assert callout
+        cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+      });
+    });
   });
 });
