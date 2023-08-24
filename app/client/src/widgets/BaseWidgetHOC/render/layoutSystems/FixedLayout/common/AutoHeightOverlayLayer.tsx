7634cfa6d009cedb3c6e2a1d659f9379ee17e427
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-08-24 20:56:34 +0530

    fix: typo "children" in overlay component.

--- app/client/src/widgets/BaseWidgetHOC/render/layoutSystems/FixedLayout/common/AutoHeightOverlayLayer.tsx
+++ app/client/src/widgets/BaseWidgetHOC/render/layoutSystems/FixedLayout/common/AutoHeightOverlayLayer.tsx
@@ -66,7 +66,7 @@ export const AutoHeightOverlayLayer = (props: BaseWidgetProps) => {
         onMaxHeightSet={onMaxHeightSet}
         onMinHeightSet={onMinHeightSet}
       />
-      {props.childern}
+      {props.children}
     </>
   );
 };
