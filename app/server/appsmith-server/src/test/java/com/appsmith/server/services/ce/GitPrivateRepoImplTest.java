f02f95b4187b0bdf676781a1c152b8e3052a20d6
Abhijeet <abhi.nagarnaik@gmail.com>
2023-08-25 22:56:06 +0530

    Create private repo helper class to avoid self invoking from gitService class

---
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/ce/GitPrivateRepoImplTest.java
@@ -0,0 +1,78 @@
+package com.appsmith.server.services.ce;
+
+import com.appsmith.server.helpers.GitCloudServicesUtils;
+import com.appsmith.server.helpers.GitPrivateRepoHelper;
+import com.appsmith.server.services.ApplicationService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Slf4j
+@DirtiesContext
+public class GitPrivateRepoImplTest {
+
+    @Autowired
+    GitPrivateRepoHelper gitPrivateRepoHelper;
+
+    @MockBean
+    GitCloudServicesUtils gitCloudServicesUtils;
+
+    @MockBean
+    ApplicationService applicationService;
+
+    @BeforeEach
+    void setup() {
+        Mockito.when(gitCloudServicesUtils.getPrivateRepoLimitForOrg(anyString(), anyBoolean()))
+                .thenReturn(Mono.just(3));
+    }
+
+    @Test
+    public void isRepoLimitReached_connectedAppCountIsLessThanLimit_Success() {
+
+        Mockito.when(applicationService.getGitConnectedApplicationsCountWithPrivateRepoByWorkspaceId(anyString()))
+                .thenReturn(Mono.just(1L));
+
+        StepVerifier.create(gitPrivateRepoHelper.isRepoLimitReached("workspaceId", false))
+                .assertNext(aBoolean -> assertEquals(false, aBoolean))
+                .verifyComplete();
+    }
+
+    @Test
+    public void isRepoLimitReached_connectedAppCountIsSameAsLimit_Success() {
+
+        Mockito.when(applicationService.getGitConnectedApplicationsCountWithPrivateRepoByWorkspaceId(anyString()))
+                .thenReturn(Mono.just(3L));
+
+        StepVerifier.create(gitPrivateRepoHelper.isRepoLimitReached("workspaceId", true))
+                .assertNext(aBoolean -> assertEquals(true, aBoolean))
+                .verifyComplete();
+    }
+
+    // This test is to check if the limit is reached when the count of connected apps is more than the limit
+    // This happens when public visible git repo is synced with application and then the visibility is changed
+    @Test
+    public void isRepoLimitReached_connectedAppCountIsMoreThanLimit_Success() {
+
+        Mockito.when(applicationService.getGitConnectedApplicationsCountWithPrivateRepoByWorkspaceId(anyString()))
+                .thenReturn(Mono.just(4L));
+
+        StepVerifier.create(gitPrivateRepoHelper.isRepoLimitReached("workspaceId", false))
+                .assertNext(aBoolean -> assertEquals(true, aBoolean))
+                .verifyComplete();
+    }
+}
