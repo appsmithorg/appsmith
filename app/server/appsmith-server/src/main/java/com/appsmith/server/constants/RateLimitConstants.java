458391fd2abdef7070e24380d459f65bee6eed6e
Shubham Saxena <shubhamsaxenagithub@gmail.com>
2023-08-25 14:04:08 +0530

    fix messaging for account suspension

--- app/server/appsmith-server/src/main/java/com/appsmith/server/constants/RateLimitConstants.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/constants/RateLimitConstants.java
@@ -1,9 +1,7 @@
 package com.appsmith.server.constants;
 
 public class RateLimitConstants {
-    public static final String RATE_LIMIT_REACHED_ACCOUNT_SUSPENDED = "Your account is suspended for 24 hours.";
-    public static final String RATE_LIMIT_ACCOUNT_SUSPENDED_WARNING_MESSAGE =
-            "After 5 consecutive failed logins, your account will be suspended for 24 hours.";
-
+    public static final String RATE_LIMIT_REACHED_ACCOUNT_SUSPENDED =
+            "Your account is suspended for 24 hours. Please reset your password to continue";
     public static final String BUCKET_KEY_FOR_LOGIN_API = "login";
 }
