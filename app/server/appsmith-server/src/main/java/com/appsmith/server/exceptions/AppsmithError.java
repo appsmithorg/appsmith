9dd029746c697a318efd2748da9fe23e8a821405
Abhijeet <abhi.nagarnaik@gmail.com>
2023-08-25 11:01:24 +0530

    Add base structure in server module

--- app/server/appsmith-server/src/main/java/com/appsmith/server/exceptions/AppsmithError.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/exceptions/AppsmithError.java
@@ -906,6 +906,14 @@ public enum AppsmithError {
             "Invalid application property configuration",
             ErrorType.INTERNAL_ERROR,
             null),
+    INVALID_METHOD_LEVEL_ANNOTATION_USAGE(
+            403,
+            AppsmithErrorCode.INVALID_METHOD_LEVEL_ANNOTATION_USAGE.getCode(),
+            "Invalid usage for {0} annotation from class {1} on method {2}. {3}. Please contact Appsmith support for more details!",
+            AppsmithErrorAction.LOG_EXTERNALLY,
+            "Invalid usage for custom annotation",
+            ErrorType.CONFIGURATION_ERROR,
+            null);
     ;
 
     private final Integer httpErrorCode;
