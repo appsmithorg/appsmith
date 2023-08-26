8036132649698d29a00ff22a2093bad7ad6afd7c
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-08-26 16:25:06 +0530

    fix: cypress errors.

--- app/client/src/widgets/BaseWidgetHOC/render/common/WidgetNameLayer.tsx
+++ app/client/src/widgets/BaseWidgetHOC/render/common/WidgetNameLayer.tsx
@@ -1,14 +1,24 @@
 import WidgetNameComponent from "components/editorComponents/WidgetNameComponent";
-import { get } from "lodash";
+import { get, memoize } from "lodash";
 import React from "react";
 import { EVAL_ERROR_PATH } from "utils/DynamicBindingUtils";
+import type { EvaluationError } from "utils/DynamicBindingUtils";
 import type { WidgetProps } from "widgets/BaseWidget";
 
+const getErrorCount = memoize(
+  (evalErrors: Record<string, EvaluationError[]>) => {
+    return Object.values(evalErrors).reduce(
+      (prev, curr) => curr.length + prev,
+      0,
+    );
+  },
+  JSON.stringify,
+);
 export const WidgetNameLayer = (props: WidgetProps) => {
   return !props.disablePropertyPane ? (
     <>
       <WidgetNameComponent
-        errorCount={get(props, EVAL_ERROR_PATH, {})}
+        errorCount={getErrorCount(get(props, EVAL_ERROR_PATH, {}))}
         parentId={props.parentId}
         showControls={props.type === "MODAL_WIDGET"}
         topRow={props.detachFromLayout ? 4 : props.topRow}
