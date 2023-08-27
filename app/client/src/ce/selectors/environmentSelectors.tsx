2ddae6bb16512aa97f84a65f9cdabf860c0e6b1f
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-08-27 15:09:24 +0700

    update: code split for environment selectors

--- app/client/src/ce/selectors/environmentSelectors.tsx
+++ app/client/src/ce/selectors/environmentSelectors.tsx
@@ -1,5 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { AppState } from "@appsmith/reducers";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const areEnvironmentsFetched = (state: AppState, workspaceId: string) =>
   true;
+
+export const getCurrentEnvironmentId = (state: AppState) => "unused_env";
+
+export const getCurrentEnvironmentName = (state: AppState) => "";
+
+export const getCurrentEditingEnvironmentId = (state: AppState) => "unused_env";
