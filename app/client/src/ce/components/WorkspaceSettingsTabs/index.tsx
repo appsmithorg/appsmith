3032a49aa9f771c01a678110ad0136bf57664411
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-08-30 11:40:56 +0700

    create: separate folder for workspace settings

---
+++ app/client/src/ce/components/WorkspaceSettingsTabs/index.tsx
@@ -0,0 +1,126 @@
+import React, { useCallback, useEffect } from "react";
+import {
+  useRouteMatch,
+  Route,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
+import MemberSettings from "@appsmith/pages/workspace/Members";
+import { GeneralSettings } from "pages/workspace/General";
+import { Tabs, Tab, TabsList, TabPanel } from "design-system";
+import { navigateToTab } from "@appsmith/pages/workspace/helpers";
+import styled from "styled-components";
+
+import * as Sentry from "@sentry/react";
+const SentryRoute = Sentry.withSentryRouting(Route);
+
+export const TabsWrapper = styled.div`
+  padding-top: var(--ads-v2-spaces-4);
+
+  .ads-v2-tabs {
+    height: 100%;
+    overflow: hidden;
+
+    .tab-panel {
+      overflow: auto;
+      height: calc(100% - 46px);
+    }
+  }
+`;
+
+type TabProp = {
+  key: string;
+  title: string;
+  count?: number;
+  panelComponent?: JSX.Element;
+};
+
+type Props = {
+  currentTab: string | undefined;
+  isMemberofTheWorkspace: boolean;
+  searchValue: string;
+  setTabArrLen: (tabArrLen: number) => void;
+};
+
+export const WorkspaceSettingsTabs = ({
+  currentTab,
+  isMemberofTheWorkspace,
+  searchValue,
+  setTabArrLen,
+}: Props) => {
+  const { path } = useRouteMatch();
+  const location = useLocation();
+  const history = useHistory();
+
+  const GeneralSettingsComponent = (
+    <SentryRoute
+      component={GeneralSettings}
+      location={location}
+      path={`${path}/general`}
+    />
+  );
+
+  const MemberSettingsComponent = (
+    <SentryRoute
+      component={useCallback(
+        (props: any) => (
+          <MemberSettings {...props} searchValue={searchValue} />
+        ),
+        [location, searchValue],
+      )}
+      location={location}
+      path={`${path}/members`}
+    />
+  );
+
+  const tabArr: TabProp[] = [
+    isMemberofTheWorkspace && {
+      key: "members",
+      title: "Members",
+      panelComponent: MemberSettingsComponent,
+    },
+    {
+      key: "general",
+      title: "General Settings",
+      panelComponent: GeneralSettingsComponent,
+    },
+  ].filter(Boolean) as TabProp[];
+
+  useEffect(() => {
+    setTabArrLen(tabArr.length);
+  }, [tabArr.length, setTabArrLen]);
+
+  return (
+    <TabsWrapper
+      className="tabs-wrapper"
+      data-testid="t--user-edit-tabs-wrapper"
+    >
+      <Tabs
+        defaultValue={currentTab}
+        onValueChange={(key: string) => navigateToTab(key, location, history)}
+        value={currentTab}
+      >
+        <TabsList>
+          {tabArr.map((tab) => {
+            return (
+              <Tab
+                data-testid={`t--tab-${tab.key}`}
+                key={tab.key}
+                value={tab.key}
+              >
+                <div className="tab-item">{tab.title}</div>
+              </Tab>
+            );
+          })}
+        </TabsList>
+        {tabArr.map((tab) => {
+          return (
+            <TabPanel className="tab-panel" key={tab.key} value={tab.key}>
+              {tab.panelComponent}
+            </TabPanel>
+          );
+        })}
+      </Tabs>
+    </TabsWrapper>
+  );
+};
