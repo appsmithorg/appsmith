63d072989d2b16b4d06184aa0d59a58dd2cf356e
“sneha122” <sneha@appsmith.com>
2023-08-30 15:43:17 +0530

    fix: spec excluded from airgap

--- app/client/cypress/e2e/Regression/ClientSide/BugTests/Bug26716_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/BugTests/Bug26716_Spec.ts
@@ -5,42 +5,46 @@ import {
 } from "../../../../support/Objects/ObjectsCore";
 let dsName;
 
-describe("Bug 26726: Datasource selected from entity explorer should be correctly highlighted", function () {
-  it("1. Create users and movies mock datasources and switch between them through entity explorer, check the active state", function () {
-    dataSources.CreateMockDB("Users");
-    dataSources.CreateMockDB("Movies");
-    dataSources.NavigateToDSCreateNew();
-    dataSources.CreateDataSource("Postgres");
-    cy.get("@dsName").then(($dsName) => {
-      dsName = $dsName + "";
-      // Select Users
-      entityExplorer.SelectEntityByName("Users", "Datasources");
-      agHelper.Sleep(200);
-      agHelper.AssertClassExists(
-        dataSources._entityExplorerID("Users"),
-        "active",
-      );
+describe(
+  "excludeForAirgap",
+  "Bug 26726: Datasource selected from entity explorer should be correctly highlighted",
+  function () {
+    it("1. Create users and movies mock datasources and switch between them through entity explorer, check the active state", function () {
+      dataSources.CreateMockDB("Users");
+      dataSources.CreateMockDB("Movies");
+      dataSources.NavigateToDSCreateNew();
+      dataSources.CreateDataSource("Postgres");
+      cy.get("@dsName").then(($dsName) => {
+        dsName = $dsName + "";
+        // Select Users
+        entityExplorer.SelectEntityByName("Users", "Datasources");
+        agHelper.Sleep(200);
+        agHelper.AssertClassExists(
+          dataSources._entityExplorerID("Users"),
+          "active",
+        );
 
-      // Switch to Movies
-      entityExplorer.SelectEntityByName("Movies", "Datasources");
-      agHelper.Sleep(200);
-      agHelper.AssertClassExists(
-        dataSources._entityExplorerID("Movies"),
-        "active",
-      );
+        // Switch to Movies
+        entityExplorer.SelectEntityByName("Movies", "Datasources");
+        agHelper.Sleep(200);
+        agHelper.AssertClassExists(
+          dataSources._entityExplorerID("Movies"),
+          "active",
+        );
 
-      // Switch to custom DS
-      entityExplorer.SelectEntityByName(dsName, "Datasources");
-      agHelper.Sleep(200);
-      agHelper.AssertClassExists(
-        dataSources._entityExplorerID(dsName),
-        "active",
-      );
+        // Switch to custom DS
+        entityExplorer.SelectEntityByName(dsName, "Datasources");
+        agHelper.Sleep(200);
+        agHelper.AssertClassExists(
+          dataSources._entityExplorerID(dsName),
+          "active",
+        );
 
-      // Delete all datasources
-      dataSources.DeleteDatasouceFromActiveTab("Users");
-      dataSources.DeleteDatasouceFromActiveTab("Movies");
-      dataSources.DeleteDatasouceFromActiveTab(dsName);
+        // Delete all datasources
+        dataSources.DeleteDatasouceFromActiveTab("Users");
+        dataSources.DeleteDatasouceFromActiveTab("Movies");
+        dataSources.DeleteDatasouceFromActiveTab(dsName);
+      });
     });
-  });
-});
+  },
+);
