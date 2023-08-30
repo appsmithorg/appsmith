c85fa210f7c13f94813c69b7974c1b72b3facee1
Sumit Kumar <sumit@appsmith.com>
2023-08-30 16:27:04 +0530

    add new test file for datasource validation tests

---
+++ app/server/appsmith-plugins/mysqlPlugin/src/test/java/com/external/plugins/MySQLDatasourceValidationTest.java
@@ -0,0 +1,37 @@
+package com.external.plugins;
+
+import com.appsmith.external.models.DBAuth;
+import com.appsmith.external.models.DatasourceConfiguration;
+import com.appsmith.external.models.Endpoint;
+import com.appsmith.external.models.Property;
+import com.appsmith.external.models.SSHConnection;
+import com.appsmith.external.models.SSHPrivateKey;
+import com.appsmith.external.models.UploadedFile;
+
+import java.util.ArrayList;
+
+public class MySQLDatasourceValidationTest {
+    static MySqlPlugin.MySqlPluginExecutor pluginExecutor = new MySqlPlugin.MySqlPluginExecutor();
+
+    private DatasourceConfiguration getDatasourceConfigurationWithSSHConnectionMethod() {
+        DatasourceConfiguration datasourceConfiguration = new DatasourceConfiguration();
+        ArrayList<Property> properties = new ArrayList<>();
+        properties.add(null);
+        properties.add(new Property("Connection method", "SSH"));
+        datasourceConfiguration.setProperties(properties);
+        ArrayList<Endpoint> mysqlEndpoints = new ArrayList<>();
+        mysqlEndpoints.add(new Endpoint("mysqlHost", 3306L));
+        datasourceConfiguration.setEndpoints(mysqlEndpoints);
+        ArrayList<Endpoint> sshEndpoints = new ArrayList<>();
+        sshEndpoints.add(new Endpoint("sshHost", 22L));
+        SSHConnection sshConnection = new SSHConnection();
+        sshConnection.setEndpoints(sshEndpoints);
+        sshConnection.setUsername("sshUser");
+        sshConnection.setPrivateKey(new SSHPrivateKey(new UploadedFile("key", "base64Key"), null));
+        datasourceConfiguration.setSshProxy(sshConnection);
+        datasourceConfiguration.setAuthentication(new DBAuth(null, "username", "password", "dbname"));
+
+        return datasourceConfiguration;
+    }
+
+}
