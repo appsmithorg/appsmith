6e60153753cb9153e424939869a104f39e99cff2
Laveena Enid <laveena@appsmith.com>
2023-08-31 23:16:20 +0530

    First set - Statbox tests
    Signed-off-by: Laveena Enid <laveena@appsmith.com>

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/Statbox/Statbox_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Statbox/Statbox_spec.ts
@@ -6,6 +6,7 @@ import {
   draggableWidgets,
   entityExplorer,
   locators,
+  deployMode,
   propPane,
 } from "../../../../../support/Objects/ObjectsCore";
 
@@ -14,18 +15,61 @@ describe("Statbox spec", () => {
     /**
      * On the canvas we have a Statbox Widget
      */
-    cy.fixture("StatboxDsl").then((dsl: string) => {
-      agHelper.AddDsl(dsl);
-    });
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.STATBOX, 550, 100);
   });
 
-  it("1. Validate if the Visible, Animate Loading and Height properties are present in the property pane", () => {
-    entityExplorer.SelectEntityByName("Statbox1", "Widgets");
-    //agHelper.GetNClick(appSettings.locators)
+  it("1. Validate all the respective properties are present on the Content and Style sections in the property pane", () => {
     agHelper.AssertContains("Visible", "exist", "label");
-    agHelper.AssertContains("Animate Loading", "exist", "label");
+    agHelper.AssertContains("Animate loading", "exist", "label");
     agHelper.AssertContains("Height", "exist", "label");
     // Switch to the Style Tab
     propPane.MoveToTab("Style");
+    agHelper.AssertContains("Background color", "exist", "label");
+    agHelper.AssertContains("Border color", "exist", "label");
+    agHelper.AssertContains("Border width", "exist", "label");
+    agHelper.AssertContains("Border radius", "exist", "label");
+    agHelper.AssertContains("Box shadow", "exist", "label");
   });
+
+  // it("2. Rename, copy-paste and delete the widget", () => {
+  //   entityExplorer.RenameEntityFromExplorer("Statbox1", "Stats", true);
+  //   propPane.CopyPasteWidgetFromPropertyPane("Stats");
+  //   entityExplorer.DeleteWidgetFromEntityExplorer("Stats");
+  // });
+
+  it("3. Validate default widgets presence inside the statbox", () => {
+    entityExplorer.NavigateToSwitcher("Explorer");
+    entityExplorer.AssertEntityPresenceInExplorer("Statbox1");
+    entityExplorer.ExpandCollapseEntity("Statbox1");
+    entityExplorer.AssertEntityPresenceInExplorer("Text1");
+    entityExplorer.AssertEntityPresenceInExplorer("Text2");
+    entityExplorer.AssertEntityPresenceInExplorer("IconButton1");
+    entityExplorer.AssertEntityPresenceInExplorer("Text3");
+  });
+
+  it("4. Validate visibility", () => {
+    propPane.MoveToTab("Content");
+    propPane.TogglePropertyState("Visible", "Off");
+    deployMode.DeployApp();
+    agHelper.AssertElementAbsence(
+      locators._widgetInCanvas(draggableWidgets.STATBOX),
+    );
+    deployMode.NavigateBacktoEditor();
+    agHelper.GetNClick(locators._enterPreviewMode);
+    agHelper.AssertElementAbsence(
+      locators._widgetInCanvas(draggableWidgets.STATBOX),
+    );
+    agHelper.GetNClick(locators._exitPreviewMode);
+    entityExplorer.SelectEntityByName("Statbox1", "Widgets");
+    propPane.TogglePropertyState("Visible", "On");
+  });
+
+  // it("5. Validate if widgets can be D&D inside the Statbox widget", () => {
+  //   entityExplorer.DragDropWidgetNVerify(
+  //     draggableWidgets.BUTTON,
+  //     550,
+  //     100,
+  //     draggableWidgets.STATBOX,
+  // );
+  //});
 });
