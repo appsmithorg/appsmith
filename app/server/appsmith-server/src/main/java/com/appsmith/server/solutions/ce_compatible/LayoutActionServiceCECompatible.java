0ab272f6f7d6f521218956200bc1563e67c4a66a
Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>
2023-09-04 19:45:40 +0530

    refactor: LayoutActionService

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce_compatible/LayoutActionServiceCECompatible.java
@@ -0,0 +1,5 @@
+package com.appsmith.server.solutions.ce_compatible;
+
+import com.appsmith.server.services.ce.LayoutActionServiceCE;
+
+public interface LayoutActionServiceCECompatible extends LayoutActionServiceCE {}
--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce_compatible/LayoutActionServiceCECompatibleImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/ce_compatible/LayoutActionServiceCECompatibleImpl.java
@@ -1,19 +1,25 @@
-package com.appsmith.server.services;
+package com.appsmith.server.solutions.ce_compatible;
 
 import com.appsmith.server.helpers.ResponseUtils;
+import com.appsmith.server.services.ActionCollectionService;
+import com.appsmith.server.services.AnalyticsService;
+import com.appsmith.server.services.ApplicationService;
+import com.appsmith.server.services.CollectionService;
+import com.appsmith.server.services.DatasourceService;
+import com.appsmith.server.services.NewActionService;
+import com.appsmith.server.services.NewPageService;
+import com.appsmith.server.services.SessionUserService;
+import com.appsmith.server.services.ce.LayoutActionServiceCEImpl;
 import com.appsmith.server.solutions.ActionPermission;
 import com.appsmith.server.solutions.PageLoadActionsUtil;
 import com.appsmith.server.solutions.PagePermission;
-import com.appsmith.server.solutions.ce_compatible.LayoutActionServiceCECompatibleImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
-public class LayoutActionServiceImpl extends LayoutActionServiceCECompatibleImpl implements LayoutActionService {
-
-    public LayoutActionServiceImpl(
+public class LayoutActionServiceCECompatibleImpl extends LayoutActionServiceCEImpl
+        implements LayoutActionServiceCECompatible {
+    public LayoutActionServiceCECompatibleImpl(
             ObjectMapper objectMapper,
             AnalyticsService analyticsService,
             NewPageService newPageService,
@@ -27,7 +33,6 @@ public class LayoutActionServiceImpl extends LayoutActionServiceCECompatibleImpl
             DatasourceService datasourceService,
             PagePermission pagePermission,
             ActionPermission actionPermission) {
-
         super(
                 objectMapper,
                 analyticsService,
