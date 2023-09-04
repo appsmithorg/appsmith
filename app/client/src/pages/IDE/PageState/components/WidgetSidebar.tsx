022e5ec300031788079113c329cd256971faf29d
akash-codemonk <67054171+akash-codemonk@users.noreply.github.com>
2023-09-04 09:54:19 +0530

    chore: remove change

--- app/client/src/pages/IDE/PageState/components/WidgetSidebar.tsx
+++ app/client/src/pages/IDE/PageState/components/WidgetSidebar.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import WidgetSidebarWithTags from "../../../Editor/WidgetSidebarWithTags";
 import { tailwindLayers } from "../../../../constants/Layers";
-import { setIdeSidebarWidth } from "pages/IDE/ideActions";
-import { useDispatch } from "react-redux";
 
 const Container = styled.div`
   height: 100%;
@@ -11,11 +9,6 @@ const Container = styled.div`
 `;
 
 const WidgetSidebar = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(setIdeSidebarWidth(300));
-  }, []);
-
   return (
     <Container
       className={`flex-1 flex flex-col overflow-hidden pt-2 ${tailwindLayers.entityExplorer}`}
