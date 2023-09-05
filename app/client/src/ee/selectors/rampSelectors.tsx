7f5878fbc088a14f1afbb3f16748d1d5182dd9a0
Ankita Kinger <ankita@appsmith.com>
2023-09-05 20:36:09 +0530

    fix: Updating logic to show ramps on Cloud instances (#2246)
    ## Description
    
    Updating logic to show ramps on Cloud instances
    
    #### PR fixes following issue(s)
    Fixes [#26964](https://github.com/appsmithorg/appsmith/issues/26964)
    
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Testing
    
    #### How Has This Been Tested?
    - [x] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/ee/selectors/rampSelectors.tsx
+++ app/client/src/ee/selectors/rampSelectors.tsx
@@ -1,11 +1,15 @@
 export * from "ce/selectors/rampSelectors";
 import { showProductRamps as CE_showProductRamps } from "ce/selectors/rampSelectors";
 import { createSelector } from "reselect";
+import { getAppsmithConfigs } from "@appsmith/configs";
 
-// useCESelector is a boolean flag that is used to determine whether to use the CE selector or the EE selector
-// this is introduced since some ramps need to be shown in EE also
-export const showProductRamps = (rampName: string, useCESelector = false) =>
-  useCESelector
+const { cloudHosting } = getAppsmithConfigs();
+
+export const showProductRamps = (
+  rampName: string,
+  isEnterpriseOnlyFeature = false,
+) =>
+  cloudHosting || isEnterpriseOnlyFeature
     ? CE_showProductRamps(rampName)
     : createSelector(
         () => null,
