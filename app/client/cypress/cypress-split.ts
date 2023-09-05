52e31b5357b5a24412b924581bbed887a71d2cbe
Saroj <saroj@appsmith.com>
2023-09-05 19:02:02 +0530

    reverting un-necessary changes

--- app/client/cypress/cypress-split.ts
+++ app/client/cypress/cypress-split.ts
@@ -82,8 +82,6 @@ async function getSpecsToRun(
   ignorePattern: string | string[],
 ): Promise<string[]> {
   try {
-    console.log(specPattern);
-    console.log(ignorePattern);
     const specFilePaths = await sortSpecFilesByTestCount(
       await getSpecFilePaths(specPattern, ignorePattern),
     );
@@ -96,7 +94,6 @@ async function getSpecsToRun(
       totalRunnersCount,
       currentRunner,
     );
-    console.log("SPECS TO RUN =======>", specsToRun);
     return specsToRun;
   } catch (err) {
     console.error(err);
@@ -137,65 +134,8 @@ function getArgs() {
     totalRunners: getEnvValue("TOTAL_RUNNERS", { required: false }),
     thisRunner: getEnvValue("THIS_RUNNER", { required: false }),
     cypressSpecs: getEnvValue("CYPRESS_SPECS", { required: false }),
-    // specsPattern: getEnvValue("CYPRESS_SPEC_PATTERN", { required: true }),
-    // env: getEnvValue("CYPRESS_ENV", { required: false }),
-    // configFile: getEnvValue("CYPRESS_CONFIG_FILE", { required: false }),
-    // headless: getEnvValue("CYPRESS_HEADLESS", { required: false }),
-    // browser: getEnvValue("CYPRESS_BROWSER", { required: false }),
   };
 }
-
-// This will finally segregate the specs and run the command to execute cypress
-// (async () => {
-//   try {
-//     const {
-//       browser,
-//       configFile,
-//       env,
-//       headless,
-//       specsPattern,
-//       thisRunner,
-//       totalRunners,
-//     } = getArgs();
-
-//     let command = "yarn cypress run ";
-//     command += browser != "" ? `--browser ${browser} ` : "";
-//     command += configFile != "" ? `--config-file ${configFile} ` : "";
-//     command += env != "" ? `--env ${env} ` : "";
-//     command += headless == "false" ? `--headed ` : "";
-
-//     const specs = await getSpecsToRun(
-//       totalRunners,
-//       thisRunner,
-//       specsPattern?.split(","),
-//     );
-
-//     let commandProcess;
-//     if (specs.length > 0) {
-//       const final_command = `${command} --spec "${specs}"`;
-//       commandProcess = exec(final_command);
-//     } else {
-//       console.log("No specs to run!");
-//       process.exit(0);
-//     }
-
-//     // pipe output because we want to see the results of the run
-//     if (commandProcess?.stdout) {
-//       commandProcess.stdout.pipe(process.stdout);
-//     }
-
-//     if (commandProcess?.stderr) {
-//       commandProcess.stderr.pipe(process.stderr);
-//     }
-
-//     commandProcess?.on("exit", (code) => {
-//       process.exit(code || 0);
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-// })();
 
 export async function cypressSplit(on: any, config: any) {
   try {
