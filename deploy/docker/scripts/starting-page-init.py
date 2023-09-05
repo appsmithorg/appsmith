a9e9705d3ac049f6ffd00fc35986a87702b1f2ef
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-09-05 13:25:43 +0530

    Remove python-requests from Docker image

--- deploy/docker/scripts/starting-page-init.py
+++ deploy/docker/scripts/starting-page-init.py
@@ -5,7 +5,8 @@ import time
 import shutil
 import subprocess
 import logging
-import traceback
+import urllib.request
+import urllib.error
 import atexit
 
 
@@ -21,8 +22,8 @@ try:
    import requests
 except ModuleNotFoundError as e:
    logging.error("Module Not Found: " , e)
-   
-   
+
+
 def get_backend_status():
     try:
         return subprocess.getoutput("supervisorctl status backend").split()[1]
@@ -30,17 +31,15 @@ def get_backend_status():
         logging.error("Subprocess Error ", e)
     except ValueError as e:
         logging.error("Value Error ", e)
-        
+
 def check_health_endpoint(url,sleep_sec = 3,timeout_sec = 180):
     for _ in range(timeout_sec//sleep_sec):
         try:
-            if requests.get(url).ok:
-                logging.info('Backend health check successful.')
-                break
-        except ImportError as e:
-            logging.error("Import Error:  ", e)
-            sys.exit(1)
-        except requests.RequestException:
+            with urllib.request.urlopen(url) as response:
+                if response.status == 200:
+                    logging.info('Backend health check successful.')
+                    break
+        except urllib.error.URLError:
             pass # retry after sleep_sec
         finally:
             time.sleep(sleep_sec)
@@ -70,7 +69,7 @@ def add_loading_page():
 @atexit.register
 def failsafe():
     remove_loading_page()
-    
+
 def main():
     add_loading_page()
     check_health_endpoint(BACKEND_HEALTH_ENDPOINT)
@@ -78,4 +77,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    
