f80b6f8cac5b8f9a5bc04f311efe5d384dceee6e
Aishwarya UR <aishwarya@appsmith.com>
2023-09-05 20:46:35 +0530

    Change position so users sample case will run

--- app/client/cypress/e2e/Sanity/Datasources/MockDBs_Spec.ts
+++ app/client/cypress/e2e/Sanity/Datasources/MockDBs_Spec.ts
@@ -12,7 +12,40 @@ describe(
   "excludeForAirgap",
   "Validate Mock Query Active Ds querying & count",
   () => {
-    it("1. Create Query from Mock Mongo DB & verify active queries count", () => {
+    it("1. Create Query from Mock Postgres DB & verify active queries count", () => {
+      dataSources.CreateMockDB("Users").then((mockDBName) => {
+        dsName = mockDBName;
+        cy.log("Mock DB Name: " + mockDBName);
+
+        assertHelper.AssertNetworkStatus("@getDatasourceStructure", 200);
+        dataSources.CreateQueryAfterDSSaved();
+
+        // This will validate that query populated in editor uses existing table name
+        agHelper.VerifyCodeInputValue(
+          formControls.postgreSqlBody,
+          'SELECT * FROM public."users" LIMIT 10;',
+        );
+
+        dataSources.RunQueryNVerifyResponseViews(5); //minimum 5 rows are expected
+        dataSources.NavigateToActiveTab();
+        agHelper
+          .GetText(dataSources._queriesOnPageText(mockDBName))
+          .then(($queryCount) =>
+            expect($queryCount).to.eq("1 query on this page"),
+          );
+
+        entityExplorer.CreateNewDsQuery(mockDBName);
+        dataSources.RunQueryNVerifyResponseViews(10, true);
+        dataSources.NavigateToActiveTab();
+        agHelper
+          .GetText(dataSources._queriesOnPageText(mockDBName))
+          .then(($queryCount) =>
+            expect($queryCount).to.eq("2 queries on this page"),
+          );
+      });
+    });
+
+    it("2. Create Query from Mock Mongo DB & verify active queries count", () => {
       dataSources.CreateMockDB("Movies").then((mockDBName) => {
         dsName = mockDBName;
         cy.log("Mock DB Name: " + mockDBName);
@@ -40,39 +73,6 @@ describe(
         dataSources.ValidateNSelectDropdown("Commands", "Find document(s)");
         dataSources.ValidateNSelectDropdown("Collection", "movies");
         dataSources.RunQueryNVerifyResponseViews(1, false);
-        dataSources.NavigateToActiveTab();
-        agHelper
-          .GetText(dataSources._queriesOnPageText(mockDBName))
-          .then(($queryCount) =>
-            expect($queryCount).to.eq("2 queries on this page"),
-          );
-      });
-    });
-
-    it("2. Create Query from Mock Postgres DB & verify active queries count", () => {
-      dataSources.CreateMockDB("Users").then((mockDBName) => {
-        dsName = mockDBName;
-        cy.log("Mock DB Name: " + mockDBName);
-
-        assertHelper.AssertNetworkStatus("@getDatasourceStructure", 200);
-        dataSources.CreateQueryAfterDSSaved();
-
-        // This will validate that query populated in editor uses existing table name
-        agHelper.VerifyCodeInputValue(
-          formControls.postgreSqlBody,
-          'SELECT * FROM public."users" LIMIT 10;',
-        );
-
-        dataSources.RunQueryNVerifyResponseViews(5); //minimum 5 rows are expected
-        dataSources.NavigateToActiveTab();
-        agHelper
-          .GetText(dataSources._queriesOnPageText(mockDBName))
-          .then(($queryCount) =>
-            expect($queryCount).to.eq("1 query on this page"),
-          );
-
-        entityExplorer.CreateNewDsQuery(mockDBName);
-        dataSources.RunQueryNVerifyResponseViews(10, true);
         dataSources.NavigateToActiveTab();
         agHelper
           .GetText(dataSources._queriesOnPageText(mockDBName))
