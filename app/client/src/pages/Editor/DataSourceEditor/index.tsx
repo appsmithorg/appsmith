1c21e324a27725db039bacabfa5890917000df23
Ayangade Adeoluwa <adeoluayangade@yahoo.com>
2023-09-06 13:25:01 +0100

    Segregrate feature flags for mongo and postgresql and sql

--- app/client/src/pages/Editor/DataSourceEditor/index.tsx
+++ app/client/src/pages/Editor/DataSourceEditor/index.tsx
@@ -988,10 +988,18 @@ const mapStateToProps = (state: AppState, props: any): ReduxStateProps => {
   );
 
   //   A/B feature flag for datasource view mode preview data.
-  const isEnabledForDSViewModeSchema = selectFeatureFlagCheck(
+  let isEnabledForDSViewModeSchema = selectFeatureFlagCheck(
     state,
     FEATURE_FLAG.ab_gsheet_schema_enabled,
   );
+
+  // for mongoDB, the feature flag should be based on ab_mock_mongo_schema_enabled.
+  if (plugin?.name === PluginName.MONGO) {
+    isEnabledForDSViewModeSchema = selectFeatureFlagCheck(
+      state,
+      FEATURE_FLAG.ab_mock_mongo_schema_enabled,
+    );
+  }
 
   // should plugin be able to preview data
   const isPluginAllowedToPreviewData =
