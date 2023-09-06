e927d1c9ca71165a9c9e2bd708cdd926240c8bc9
Preet Sidhu <preetsidhu.bits@gmail.com>
2023-09-06 15:49:01 -0400

    [Task]: Update DataTreeFactory to include widget resizes and remove references to layout mode and viewport. (#26810)
    ## Description
    
    - Remove ```appPositioningType``` and ```isMobile``` props from widget
    DataTree.
    - Include width and height as ```derivedHeight and derivedWidth``` in
    widget DataTree. Prefer width and height if available, else calculate
    using position props.
    - Update TableWidgetV2 to use the ```derivedHeight``` prop in
    derivedProps calculation to get pageSize.
    
    #### PR fixes following issue(s)
    Fixes # [26629](https://github.com/appsmithorg/appsmith/issues/26629)
    
    
    #### Type of change
    > Please delete options that are not relevant.
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [x] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    1. Create an app.
    2. Add a Table widget.
    3. Connect the Table widget to a datasource.
    4. Assert that the Table widget works as expected. The number of rows
    per page are calculated accurately based on initial height of the
    widget.
    
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    ---------
    
    Co-authored-by: Ashok Kumar M <35134347+marks0351@users.noreply.github.com>

--- app/client/src/layoutSystems/fixedlayout/index.ts
+++ app/client/src/layoutSystems/fixedlayout/index.ts
@@ -2,32 +2,12 @@ import { RenderModes } from "constants/WidgetConstants";
 import { FixedLayoutEditorWrapper } from "./editor/FixedLayoutEditorWrapper";
 import { FixedLayoutViewerWrapper } from "./viewer/FixedLayoutViewerWrapper";
 import type { BaseWidgetProps } from "widgets/BaseWidgetHOC/withBaseWidgetHOC";
-
-/**
- * getFixedLayoutComponentDimensions
- *
- * utiltiy function to compute a widgets dimensions in Fixed layout system
- *
- */
-
-const getFixedLayoutComponentDimensions = ({
-  bottomRow,
-  leftColumn,
-  parentColumnSpace,
-  parentRowSpace,
-  rightColumn,
-  topRow,
-}: BaseWidgetProps) => {
-  return {
-    componentWidth: (rightColumn - leftColumn) * parentColumnSpace,
-    componentHeight: (bottomRow - topRow) * parentRowSpace,
-  };
-};
+import { getFixedLayoutComponentDimensions } from "utils/ComponentSizeUtils";
 
 /**
  * getFixedLayoutSystemPropsEnhancer
  *
- * utiltiy function to enhance BaseWidgetProps with Fixed Layout system specific props
+ * utility function to enhance BaseWidgetProps with Fixed Layout system specific props
  *
  */
 
@@ -44,7 +24,7 @@ const getFixedLayoutSystemPropsEnhancer = (props: BaseWidgetProps) => {
 /**
  * getFixedLayoutSystemWrapper
  *
- * utiltiy function to return the fixed layout system wrapper based on render mode.
+ * utility function to return the fixed layout system wrapper based on render mode.
  * wrapper is the component that wraps around a widget to provide layouting ability and enable editing experience.
  *
  */
@@ -59,8 +39,8 @@ const getFixedLayoutSystemWrapper = (renderMode: RenderModes) => {
 /**
  * getFixedLayoutSystem
  *
- * utiltiy function to return the fixed layout system config for
- * wrapper based on render mode and property enhancer funciton
+ * utility function to return the fixed layout system config for
+ * wrapper based on render mode and property enhancer function
  *
  */
 export function getFixedLayoutSystem(renderMode: RenderModes) {
