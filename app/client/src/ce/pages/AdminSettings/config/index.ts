60fcd46f808bff60a77d0d5ce955d9f93831a5fa
ChandanBalajiBP <104058110+ChandanBalajiBP@users.noreply.github.com>
2023-09-08 00:00:04 +0530

    restructure client code

--- app/client/src/ce/pages/AdminSettings/config/index.ts
+++ app/client/src/ce/pages/AdminSettings/config/index.ts
@@ -2,7 +2,7 @@ import { ConfigFactory } from "pages/AdminSettings/config/ConfigFactory";
 
 import { config as GeneralConfig } from "@appsmith/pages/AdminSettings/config/general";
 import { config as EmailConfig } from "pages/AdminSettings/config/email";
-import { config as DeveloperSettings } from "@appsmith/pages/AdminSettings/config/developerSettings";
+import { config as DeveloperSettings } from "@appsmith/pages/AdminSettings/config/DeveloperSettings";
 import { config as VersionConfig } from "pages/AdminSettings/config/version";
 import { config as AdvancedConfig } from "pages/AdminSettings/config/advanced";
 import { config as Authentication } from "@appsmith/pages/AdminSettings/config/authentication";
