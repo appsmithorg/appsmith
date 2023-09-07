3af52671d2988bacb0ff867cc621b0fdc8b9b95a
Rajat Agrawal <rajat@appsmith.com>
2023-09-07 14:24:24 +0530

    rename functions

--- app/client/src/widgets/ChartWidget/component/index.tsx
+++ app/client/src/widgets/ChartWidget/component/index.tsx
@@ -185,30 +185,17 @@ class ChartComponent extends React.Component<
     );
   };
 
-  findKey = (obj : any, id : any) : any => {
-    const myFun = () => { 1+2 }
-    const myFunString = myFun.toString()
-    // console.log("***", "my func string is ", myFunString)
-
-    const evaluatedFunc = new Function("return " + myFunString)
-    // console.log("***", "evaluated func is ", evaluatedFunc()())
-    // console.log("find key called for obj ", JSON.stringify(obj))
-    
+  findObjectWithID = (obj : any, id : any) : any => {    
     if (typeof(obj) !== "object") {
         return null;
     }
 
-    // console.log("obj.id is ", obj.id, " id is ", id)
     if (obj.id == id) {
-        // console.log("coming in if")
         return obj
     } else {
         const keys = Object.keys(obj)
-        // console.log("keys is ", keys)
         for (const key of keys) {
-            // console.log("calling find key for ", obj[key])
-            const result = this.findKey(obj[key], id)
-            // console.log("result for obj ", JSON.stringify(obj[key]), " is ", result)
+            const result = this.findObjectWithID(obj[key], id)
             if (result) {
                 return result
             }
@@ -227,18 +214,10 @@ class ChartComponent extends React.Component<
       const fn = c.function
       const values = c.values
 
-      const config = this.findKey(options, id)
+      const config = this.findObjectWithID(options, id)
       if(config) {
         if (fn) {
           const assignFunc = (new Function("return " + c.function))()
-          // console.log("***", "assign func is ", assignFunc)
-          config[key] = assignFunc
-        } else if (values) {
-          const assignFunc = (new Function("return " + `(value, params) => {
-              return ${JSON.stringify(values)}[params.dataIndex]
-            }
-          `))()
-          // console.log("***", "assign func is ", assignFunc)
           config[key] = assignFunc
         }
       }
