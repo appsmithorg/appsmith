5bfc49bc12027d03886aa490606f93e6166e60b9
Sumit Kumar <sumit@appsmith.com>
2023-09-07 16:31:53 +0530

    revert test change

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/DatabaseChangelog2.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/DatabaseChangelog2.java
@@ -114,6 +114,7 @@ import static com.appsmith.server.acl.AclPermission.READ_THEMES;
 import static com.appsmith.server.acl.AppsmithRole.TENANT_ADMIN;
 import static com.appsmith.server.constants.FieldName.DEFAULT_PERMISSION_GROUP;
 import static com.appsmith.server.constants.FieldName.PERMISSION_GROUP_ID;
+import static com.appsmith.server.helpers.CollectionUtils.findSymmetricDiff;
 import static com.appsmith.server.migrations.DatabaseChangelog1.dropIndexIfExists;
 import static com.appsmith.server.migrations.DatabaseChangelog1.ensureIndexes;
 import static com.appsmith.server.migrations.DatabaseChangelog1.getUpdatedDynamicBindingPathList;
@@ -2543,7 +2544,7 @@ public class DatabaseChangelog2 {
      * @param mongoTemplate
      * @param cacheableRepositoryHelper
      */
-    /*@ChangeSet(order = "10000", id = "update-super-users", author = "", runAlways = true)
+    @ChangeSet(order = "10000", id = "update-super-users", author = "", runAlways = true)
     public void updateSuperUsers(
             MongoTemplate mongoTemplate,
             CacheableRepositoryHelper cacheableRepositoryHelper,
@@ -2598,7 +2599,7 @@ public class DatabaseChangelog2 {
 
         Update update = new Update().set(fieldName(QPermissionGroup.permissionGroup.assignedToUserIds), userIds);
         mongoTemplate.updateFirst(permissionGroupQuery, update, PermissionGroup.class);
-    }*/
+    }
 
     @ChangeSet(order = "034", id = "update-bad-theme-state", author = "")
     public void updateBadThemeState(
