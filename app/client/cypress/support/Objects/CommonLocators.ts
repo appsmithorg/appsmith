fcd7685491faecbcaae5945011f552ed85cbf76f
Aishwarya UR <aishwarya@appsmith.com>
2023-09-07 22:50:26 +0530

    Don't save handle

--- app/client/cypress/support/Objects/CommonLocators.ts
+++ app/client/cypress/support/Objects/CommonLocators.ts
@@ -82,7 +82,7 @@ export class CommonLocators {
     "']/ancestor::div[@role='menuitem']";
   _visibleTextDiv = (divText: string) => "//div[text()='" + divText + "']";
   _visibleTextSpan = (spanText: string, isCss = false) =>
-    isCss ? `span:contains('${spanText}')` : `//span[text()='${spanText}']`;
+    isCss ? `span:contains("${spanText}")` : `//span[text()="${spanText}"]`;
   _openWidget = ".widgets .t--entity-add-btn";
   _dropHere = ".t--drop-target";
   _editPage = "[data-testid=onboarding-tasks-datasource-text], .t--drop-target";
