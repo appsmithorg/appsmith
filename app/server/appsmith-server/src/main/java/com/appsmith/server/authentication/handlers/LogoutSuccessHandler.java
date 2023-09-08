6d622c821d3840600ed1386180e5d7a382e5d376
Shubham Saxena <shubhamsaxenagithub@gmail.com>
2023-09-08 17:32:00 +0530

    Revert "fix: reset counter while logging out user"
    This reverts commit 306a648a8bbf8733db980219974dfe6ddb98a9cf.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/authentication/handlers/LogoutSuccessHandler.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/authentication/handlers/LogoutSuccessHandler.java
@@ -1,7 +1,6 @@
 package com.appsmith.server.authentication.handlers;
 
 import com.appsmith.server.authentication.handlers.ce.LogoutSuccessHandlerCE;
-import com.appsmith.server.ratelimiting.RateLimitService;
 import com.appsmith.server.services.AnalyticsService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -9,8 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LogoutSuccessHandler extends LogoutSuccessHandlerCE {
 
-    public LogoutSuccessHandler(
-            ObjectMapper objectMapper, AnalyticsService analyticsService, RateLimitService rateLimitService) {
-        super(objectMapper, analyticsService, rateLimitService);
+    public LogoutSuccessHandler(ObjectMapper objectMapper, AnalyticsService analyticsService) {
+        super(objectMapper, analyticsService);
     }
 }
--- app/server/appsmith-server/src/main/java/com/appsmith/server/authentication/handlers/ce/LogoutSuccessHandlerCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/authentication/handlers/ce/LogoutSuccessHandlerCE.java
@@ -1,10 +1,8 @@
 package com.appsmith.server.authentication.handlers.ce;
 
 import com.appsmith.external.constants.AnalyticsEvents;
-import com.appsmith.server.constants.RateLimitConstants;
 import com.appsmith.server.domains.User;
 import com.appsmith.server.dtos.ResponseDTO;
-import com.appsmith.server.ratelimiting.RateLimitService;
 import com.appsmith.server.services.AnalyticsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -25,13 +23,10 @@ public class LogoutSuccessHandlerCE implements ServerLogoutSuccessHandler {
 
     private final ObjectMapper objectMapper;
     private final AnalyticsService analyticsService;
-    private final RateLimitService rateLimitService;
 
-    public LogoutSuccessHandlerCE(
-            ObjectMapper objectMapper, AnalyticsService analyticsService, RateLimitService rateLimitService) {
+    public LogoutSuccessHandlerCE(ObjectMapper objectMapper, AnalyticsService analyticsService) {
         this.objectMapper = objectMapper;
         this.analyticsService = analyticsService;
-        this.rateLimitService = rateLimitService;
     }
 
     @Override
@@ -47,12 +42,8 @@ public class LogoutSuccessHandlerCE implements ServerLogoutSuccessHandler {
             String responseStr = objectMapper.writeValueAsString(responseBody);
             DataBuffer buffer =
                     exchange.getResponse().bufferFactory().allocateBuffer().write(responseStr.getBytes());
-
-            User loggedInUser = (User) authentication.getPrincipal();
-            rateLimitService.resetCounter(RateLimitConstants.BUCKET_KEY_FOR_LOGIN_API, loggedInUser.getEmail());
-
             return analyticsService
-                    .sendObjectEvent(AnalyticsEvents.LOGOUT, loggedInUser)
+                    .sendObjectEvent(AnalyticsEvents.LOGOUT, (User) authentication.getPrincipal())
                     .then(response.writeWith(Mono.just(buffer)));
         } catch (JsonProcessingException e) {
             log.error("Unable to write to response json. Cause: ", e);
