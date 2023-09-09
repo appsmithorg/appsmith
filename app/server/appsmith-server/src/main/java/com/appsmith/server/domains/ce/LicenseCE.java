3f0fb80ae0cbc10ca3a50222066278a456bf8e11
Abhijeet <abhi.nagarnaik@gmail.com>
2023-09-09 23:55:43 +0530

    Remove previous plan from CE

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/LicenseCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/LicenseCE.java
@@ -6,7 +6,4 @@ import lombok.Data;
 @Data
 public class LicenseCE {
     LicensePlan plan;
-
-    // Field to detect previous license plan which will be used to provide the appropriate messaging on UI
-    LicensePlan previousPlan = LicensePlan.FREE;
 }
