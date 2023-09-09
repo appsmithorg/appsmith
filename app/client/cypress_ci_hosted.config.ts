5cd9343904a2d9df3e75510121745c816dc577c1
Saroj <saroj@appsmith.com>
2023-09-09 07:58:04 +0530

    added video support for cy 13

--- app/client/cypress_ci_hosted.config.ts
+++ app/client/cypress_ci_hosted.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   responseTimeout: 60000,
   pageLoadTimeout: 60000,
   videoCompression: false,
-  videoUploadOnPasses: false,
+  video: true,
   numTestsKeptInMemory: 5,
   experimentalMemoryManagement: true,
   reporter: "cypress-mochawesome-reporter",
