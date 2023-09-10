c6145c6b10080b144383dddbae37f2d78fac3ec2
tkAppsmith <tushar@appsmith.com>
2023-09-11 03:11:13 +0530

    reverted code to enable logs

--- app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/ServerApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @EnableScheduling
 public class ServerApplication {
 
     public static void main(String[] args) {
-
-        Hooks.onOperatorDebug();
 
         new SpringApplicationBuilder(ServerApplication.class)
                 .bannerMode(Banner.Mode.OFF)
