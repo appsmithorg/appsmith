077b4a188091bf8804dc68d218a9bad9cede5f7e
brayn003 <rudra@appsmith.com>
2023-09-10 20:11:59 +0530

    fix: fixing scroll issue

--- app/client/src/pages/Editor/gitSync/Tabs/GitSettings/index.tsx
+++ app/client/src/pages/Editor/gitSync/Tabs/GitSettings/index.tsx
@@ -2,17 +2,21 @@ import React from "react";
 import GitUserSettings from "./GitUserSettings";
 import GitDisconnect from "./GitDisconnect";
 import styled from "styled-components";
+import { ModalBody } from "design-system";
 
 const Container = styled.div`
-  min-height: calc(360px + 52px + 16px);
+  overflow: auto;
+  min-height: calc(360px + 52px);
 `;
 
 function GitSettings() {
   return (
-    <Container>
-      <GitUserSettings />
-      <GitDisconnect />
-    </Container>
+    <ModalBody>
+      <Container>
+        <GitUserSettings />
+        <GitDisconnect />
+      </Container>
+    </ModalBody>
   );
 }
 
