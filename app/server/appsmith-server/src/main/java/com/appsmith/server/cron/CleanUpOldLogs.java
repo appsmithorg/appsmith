f5a9a2dc3cb970785f5104963c62d5fbec0d69cd
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-09-11 18:17:51 +0530

    Add comment to cutoff age
    Co-authored-by: Nidhi <nidhi@appsmith.com>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/cron/CleanUpOldLogs.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/cron/CleanUpOldLogs.java
@@ -14,7 +14,7 @@ import java.util.stream.Stream;
 @Component
 public class CleanUpOldLogs {
 
-    private static final int CUTOFF_AGE = 7 * 24 * 60 * 60 * 1000;
+    private static final int CUTOFF_AGE = 7 * 24 * 60 * 60 * 1000; // 7 days
 
     @Scheduled(cron = "0 0 0 * * SAT")
     public void cleanUpOldLogs() throws IOException {
