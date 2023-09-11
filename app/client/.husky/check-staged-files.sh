a9912269864278aea9e32d85c0b7ef40e855023b
Ankita Kinger <ankita@appsmith.com>
2023-09-11 18:25:37 +0530

    updating pre-commit hook for staged files to not run spotless check if its merge commit

--- app/client/.husky/check-staged-files.sh
+++ app/client/.husky/check-staged-files.sh
@@ -3,7 +3,9 @@
 is_server_change=$(git diff --cached --name-only | grep -c "app/server")
 is_client_change=$(git diff --cached --name-only | grep -c "app/client")
 
-if [ "$is_server_change" -ge 1 ]; then
+is_merge_commit=$(git rev-parse -q --verify MERGE_HEAD)
+
+if [[ "$is_server_change" -ge 1 && ! "$is_merge_commit" ]]; then
   echo "Running Spotless check ..."
     pushd app/server > /dev/null
     if (mvn spotless:check 1> /dev/null && popd > /dev/null) then
