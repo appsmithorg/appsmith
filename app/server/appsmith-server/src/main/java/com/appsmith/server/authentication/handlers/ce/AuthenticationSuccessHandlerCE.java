appsmithorg/appsmith

commit 31bca0b123f5aa7660c337df6c28c581b447b95a

feat: Email Verification (#25854)
## Description

Adds a setting for Admin Users to enable Email verification of users who
are signing up using "Form login" When enabled, it will send a
verification email to a user who is signing up on a tenant and only when
they verify (by clicking on the link in email) they will be allowed to
proceed to the rest of the sign up process.

Corresponding EE PR for the email template:
https://github.com/appsmithorg/appsmith-ee/pull/2153

#### PR fixes following issue(s)
Fixes #21387
Fixes #25552

#### Media
> A video or a GIF is preferred. when using Loom, don’t embed because it
looks like it’s a GIF. instead, just link to the video
>
>
#### Type of change

- New feature (non-breaking change which adds functionality)
- This change requires a documentation update

## Testing
>
#### How Has This Been Tested?
- [x] Manual
- [x] Jest
- [x] Cypress
We have mocked server apis to respond with different states and tested
the ui on that change

#### Test Plan
https://github.com/appsmithorg/TestSmith/issues/2459
>
>
#### Issues raised during DP testing
> Link issues raised during DP testing for better visiblity and tracking
(copy link from comments dropped on this PR)
>
>
>
## Checklist:
#### Dev activity
- [x] My code follows the style guidelines of this project
- [x] I have performed a self-review of my own code
- [x] I have commented my code, particularly in hard-to-understand areas
- [ ] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [x] I have added tests that prove my fix is effective or that my
feature works
- [x] New and existing unit tests pass locally with my changes
- [x] PR is being merged under a feature flag


#### QA activity:
- [ ] [Speedbreak
features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
have been covered
- [ ] Test plan covers all impacted features and [areas of

app/server/appsmith-server/src/main/java/com/appsmith/server/authentication/handlers/ce/AuthenticationSuccessHandlerCE.java
@@ -9,6 +9,7 @@ import com.appsmith.server.domains.Application;
 import com.appsmith.server.domains.LoginSource;
 import com.appsmith.server.domains.User;
 import com.appsmith.server.domains.Workspace;
+import com.appsmith.server.dtos.ResendEmailVerificationDTO;
 import com.appsmith.server.helpers.RedirectHelper;
 import com.appsmith.server.repositories.UserRepository;
 import com.appsmith.server.repositories.WorkspaceRepository;
@@ -17,8 +18,10 @@ import com.appsmith.server.services.ApplicationPageService;
 import com.appsmith.server.services.ConfigService;
 import com.appsmith.server.services.FeatureFlagService;
 import com.appsmith.server.services.SessionUserService;
+import com.appsmith.server.services.TenantService;
 import com.appsmith.server.services.UserDataService;
 import com.appsmith.server.services.UserIdentifierService;
+import com.appsmith.server.services.UserService;
 import com.appsmith.server.services.WorkspaceService;
 import com.appsmith.server.solutions.ForkExamplesWorkspace;
 import com.appsmith.server.solutions.WorkspacePermission;
@@ -37,10 +40,17 @@ import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.net.URI;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository.DEFAULT_SPRING_SECURITY_CONTEXT_ATTR_NAME;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -60,8 +70,65 @@ public class AuthenticationSuccessHandlerCE implements ServerAuthenticationSucce
     private final ConfigService configService;
     private final FeatureFlagService featureFlagService;
     private final CommonConfig commonConfig;
-
     private final UserIdentifierService userIdentifierService;
+    private final TenantService tenantService;
+
+    private final UserService userService;
+
+    private Mono<Boolean> isVerificationRequired(String userEmail, String method) {
+        Mono<Boolean> emailVerificationEnabledMono = tenantService
+                .getTenantConfiguration()
+                .map(tenant -> tenant.getTenantConfiguration().getEmailVerificationEnabled())
+                .cache();
+
+        Mono<User> userMono = userRepository.findByEmail(userEmail).cache();
+        Mono<Boolean> verificationRequiredMono = null;
+
+        if (method == "signup") {
+            verificationRequiredMono = emailVerificationEnabledMono.flatMap(emailVerificationEnabled -> {
+                // email verification is not enabled at the tenant, so verification not required
+                if (!TRUE.equals(emailVerificationEnabled)) {
+                    return userMono.flatMap(user -> {
+                        user.setEmailVerificationRequired(FALSE);
+                        return userRepository.save(user).then(Mono.just(FALSE));
+                    });
+                } else {
+                    return userMono.flatMap(user -> {
+                        user.setEmailVerificationRequired(TRUE);
+                        return userRepository.save(user).then(Mono.just(TRUE));
+                    });
+                }
+            });
+        } else if (method == "login") {
+            verificationRequiredMono = userMono.flatMap(user -> {
+                Boolean emailVerified = user.getEmailVerified();
+                // email already verified
+                if (TRUE.equals(emailVerified)) {
+                    return Mono.just(FALSE);
+                } else {
+                    return emailVerificationEnabledMono.flatMap(emailVerificationEnabled -> {
+                        // email verification not enabled at the tenant
+                        if (!TRUE.equals(emailVerificationEnabled)) {
+                            user.setEmailVerificationRequired(FALSE);
+                            return userRepository.save(user).then(Mono.just(FALSE));
+                        } else {
+                            // scenario when at the time of signup, the email verification was disabled at the tenant
+                            // but later on turned on, now when this user logs in, it will not be prompted to verify
+                            // as the configuration at time of signup is considered for any user.
+                            // for old users, the login works as expected, without the need to verify
+                            if (!TRUE.equals(user.getEmailVerificationRequired())) {
+                                return Mono.just(FALSE);
+                            } else {
+                                user.setEmailVerificationRequired(TRUE);
+                                return userRepository.save(user).then(Mono.just(TRUE));
+                            }
+                        }
+                    });
+                }
+            });
+        }
+        return verificationRequiredMono;
+    }
 
     /**
      * On authentication success, we send a redirect to the endpoint that serve's the user's profile.
@@ -77,6 +144,117 @@ public class AuthenticationSuccessHandlerCE implements ServerAuthenticationSucce
         return onAuthenticationSuccess(webFilterExchange, authentication, false, false, null);
     }
 
+    private Mono<String> extractRedirectUrlAndSendVerificationMail(
+            WebFilterExchange webFilterExchange, User user, String redirectUrl) {
+        String baseUrl =
+                webFilterExchange.getExchange().getRequest().getHeaders().getOrigin();
+        ResendEmailVerificationDTO resendEmailVerificationDTO = new ResendEmailVerificationDTO();
+        resendEmailVerificationDTO.setEmail(user.getEmail());
+        resendEmailVerificationDTO.setBaseUrl(baseUrl);
+        // This is the case post signup when the url is /signup-success?redirectUrl=<>
+        // After verification we redirect the user to /signup-success always and use the redirect url
+        // to navigate to next page after signup-success
+        try {
+            redirectUrl = redirectUrl.split("redirectUrl=")[1];
+        } catch (Exception e) {
+            log.error(String.valueOf(e));
+        }
+        redirectUrl = URLDecoder.decode(redirectUrl, StandardCharsets.UTF_8);
+        return userService
+                .resendEmailVerification(resendEmailVerificationDTO, redirectUrl)
+                .then(Mono.just(redirectUrl));
+    }
+
+    private Mono<Void> postVerificationRequiredHandler(
+            WebFilterExchange webFilterExchange, User user, Application defaultApplication, Boolean isFromSignup) {
+        /**
+         * This function:
+         * Removes the user session as only post verification login is allowed
+         * Extracts the redirect url post signup/login
+         * Send email verification mail with the redirect url
+         * Redirects the user to verificationPending screen
+         */
+        return webFilterExchange.getExchange().getSession().flatMap(webSession -> {
+            webSession.getAttributes().remove(DEFAULT_SPRING_SECURITY_CONTEXT_ATTR_NAME);
+            return redirectHelper
+                    .getAuthSuccessRedirectUrl(webFilterExchange, defaultApplication, true)
+                    .flatMap(redirectUrl -> extractRedirectUrlAndSendVerificationMail(
+                                    webFilterExchange, user, redirectUrl)
+                            .map(url -> String.format(
+                                    "/user/verificationPending?email=%s",
+                                    URLEncoder.encode(user.getEmail(), StandardCharsets.UTF_8)))
+                            .flatMap(redirectUri -> redirectStrategy.sendRedirect(
+                                    webFilterExchange.getExchange(), URI.create(redirectUri))));
+        });
+    }
+
+    /**
+     * This function handles the redirection in case of form type signup/login.
+     * It constructs the redirect uri based on user's request, and if email verification is required
+     * then redirects the user to /verificationPending and sends the magic link with the user's redirectUrl
+     * in the email.
+     * @param webFilterExchange
+     * @param defaultWorkspaceId
+     * @param authentication
+     * @param isFromSignup
+     * @param createDefaultApplication
+     * @return
+     */
+    private Mono<Void> formEmailVerificationRedirectionHandler(
+            WebFilterExchange webFilterExchange,
+            String defaultWorkspaceId,
+            Authentication authentication,
+            Boolean isFromSignup,
+            Boolean createDefaultApplication,
+            String originHeader) {
+        Mono<Void> redirectionMono = null;
+        User user = (User) authentication.getPrincipal();
+        Mono<Boolean> isVerificationRequiredMono = Mono.just(FALSE);
+
+        if (isFromSignup) {
+            isVerificationRequiredMono = isVerificationRequired(user.getEmail(), "signup");
+            if (createDefaultApplication) {
+                Mono<Boolean> finalIsVerificationRequiredMono = isVerificationRequiredMono;
+                redirectionMono = finalIsVerificationRequiredMono.flatMap(isVerificationRequired -> {
+                    if (TRUE.equals(isVerificationRequired)) {
+                        return createDefaultApplication(defaultWorkspaceId, authentication)
+                                .flatMap(defaultApplication -> postVerificationRequiredHandler(
+                                        webFilterExchange, user, defaultApplication, TRUE));
+                    } else {
+                        return Mono.zip(
+                                        userService.sendWelcomeEmail(user, originHeader),
+                                        createDefaultApplication(defaultWorkspaceId, authentication))
+                                .flatMap(obj -> redirectHelper.handleRedirect(webFilterExchange, obj.getT2(), true));
+                    }
+                });
+            } else {
+                Mono<Boolean> finalIsVerificationRequiredMono1 = isVerificationRequiredMono;
+                redirectionMono = finalIsVerificationRequiredMono1.flatMap(isVerificationRequired -> {
+                    if (TRUE.equals(isVerificationRequired)) {
+                        return postVerificationRequiredHandler(webFilterExchange, user, null, TRUE);
+                    } else {
+                        return userService
+                                .sendWelcomeEmail(user, originHeader)
+                                .then(redirectHelper.handleRedirect(webFilterExchange, null, true));
+                    }
+                });
+            }
+        } else {
+            isVerificationRequiredMono = isVerificationRequired(user.getEmail(), "login");
+            Mono<Boolean> finalIsVerificationRequiredMono2 = isVerificationRequiredMono;
+            redirectionMono = finalIsVerificationRequiredMono2.flatMap(isVerificationRequired -> {
+                if (TRUE.equals(isVerificationRequired)) {
+                    return postVerificationRequiredHandler(webFilterExchange, user, null, FALSE);
+                } else {
+                    return userService
+                            .sendWelcomeEmail(user, originHeader)
+                            .then(redirectHelper.handleRedirect(webFilterExchange, null, false));
+                }
+            });
+        }
+        return redirectionMono;
+    }
+
     public Mono<Void> onAuthenticationSuccess(
             WebFilterExchange webFilterExchange,
             Authentication authentication,
@@ -84,10 +262,15 @@ public class AuthenticationSuccessHandlerCE implements ServerAuthenticationSucce
             boolean isFromSignup,
             String defaultWorkspaceId) {
         log.debug("Login succeeded for user: {}", authentication.getPrincipal());
-        Mono<Void> redirectionMono;
+        Mono<Void> redirectionMono = null;
         User user = (User) authentication.getPrincipal();
+        String originHeader =
+                webFilterExchange.getExchange().getRequest().getHeaders().getOrigin();
 
         if (authentication instanceof OAuth2AuthenticationToken) {
+            // for oauth type signups, we don't need to verify email
+            user.setEmailVerificationRequired(FALSE);
+
             // In case of OAuth2 based authentication, there is no way to identify if this was a user signup (new user
             // creation) or if this was a login (existing user). What we do here to identify this, is an approximation.
             // If and when we find a better way to do identify this, let's please move away from this approximation.
@@ -112,51 +295,39 @@ public class AuthenticationSuccessHandlerCE implements ServerAuthenticationSucce
             if (isFromSignup) {
                 boolean finalIsFromSignup = isFromSignup;
                 redirectionMono = workspaceService
-                        .isCreateWorkspaceAllowed(Boolean.TRUE)
-                        .elapsed()
-                        .map(pair -> {
-                            log.debug(
-                                    "AuthenticationSuccessHandlerCE::Time taken to check if workspace creation allowed: {} ms",
-                                    pair.getT1());
-                            return pair.getT2();
-                        })
+                        .isCreateWorkspaceAllowed(TRUE)
                         .flatMap(isCreateWorkspaceAllowed -> {
                             if (isCreateWorkspaceAllowed) {
-                                return createDefaultApplication(defaultWorkspaceId, authentication)
-                                        .elapsed()
-                                        .map(pair -> {
-                                            log.debug(
-                                                    "AuthenticationSuccessHandlerCE::Time taken to create default application: {} ms",
-                                                    pair.getT1());
-                                            return pair.getT2();
-                                        })
-                                        .flatMap(defaultApplication -> handleOAuth2Redirect(
-                                                webFilterExchange, defaultApplication, finalIsFromSignup));
+
+                                return Mono.zip(
+                                                userService.sendWelcomeEmail(user, originHeader),
+                                                createDefaultApplication(defaultWorkspaceId, authentication))
+                                        .flatMap(objects -> handleOAuth2Redirect(
+                                                webFilterExchange, objects.getT2(), finalIsFromSignup));
                             }
-                            return handleOAuth2Redirect(webFilterExchange, null, finalIsFromSignup);
+                            return userService
+                                    .sendWelcomeEmail(user, originHeader)
+                                    .then(handleOAuth2Redirect(webFilterExchange, null, finalIsFromSignup));
                         });
             } else {
-                redirectionMono = handleOAuth2Redirect(webFilterExchange, null, isFromSignup);
+                redirectionMono = userService
+                        .sendWelcomeEmail(user, originHeader)
+                        .then(handleOAuth2Redirect(webFilterExchange, null, isFromSignup));
             }
         } else {
-            boolean finalIsFromSignup = isFromSignup;
-            if (createDefaultApplication && isFromSignup) {
-                redirectionMono = createDefaultApplication(defaultWorkspaceId, authentication)
-                        .elapsed()
-                        .map(pair -> {
-                            log.debug(
-                                    "AuthenticationSuccessHandlerCE::Time taken to create default application: {} ms",
-                                    pair.getT1());
-                            return pair.getT2();
-                        })
-                        .flatMap(defaultApplication ->
-                                redirectHelper.handleRedirect(webFilterExchange, defaultApplication, true));
-            } else {
-                redirectionMono = redirectHelper.handleRedirect(webFilterExchange, null, finalIsFromSignup);
-            }
+            // form type signup/login handler
+            redirectionMono = formEmailVerificationRedirectionHandler(
+                    webFilterExchange,
+                    defaultWorkspaceId,
+                    authentication,
+                    isFromSignup,
+                    createDefaultApplication,
+                    originHeader);
         }
 
         final boolean isFromSignupFinal = isFromSignup;
+
+        Mono<Void> finalRedirectionMono = redirectionMono;
         return sessionUserService
                 .getCurrentUser()
                 .flatMap(currentUser -> {
@@ -200,7 +371,7 @@ public class AuthenticationSuccessHandlerCE implements ServerAuthenticationSucce
 
                     return Mono.whenDelayError(monos);
                 })
-                .then(redirectionMono);
+                .then(finalRedirectionMono);
     }
 
     protected Mono<Application> createDefaultApplication(String defaultWorkspaceId, Authentication authentication) {

app/client/cypress/e2e/Regression/ClientSide/FormLogin/EmailVerfication_spec.ts
@@ -1,0 +1,118 @@
+import adminsSettings from "../../../../locators/AdminsSettings";
+
+describe("Email verification", () => {
+  it("Shows the email verification pending page correctly", () => {
+    cy.LogOut();
+    cy.visit("/user/verificationPending?email=test@appsmith.com");
+    cy.wait(1000);
+    cy.get("[data-testid='verification-pending']").matchImageSnapshot(
+      "VerificationPendingScreen",
+    );
+  });
+  it("Verification error pages", () => {
+    const errorCode = {
+      ALREADY_VERIFIED: "AE-EMV-4095",
+      EXPIRED: "AE-EMV-4096",
+      MISMATCH: "AE-EMV-4098",
+      UNKNOWN: "UNKNOWN",
+    };
+    cy.visit(
+      `/user/verify-error?email=abc&code=${errorCode.MISMATCH}&message=xyz`,
+    );
+    cy.wait(1000);
+    cy.get("[data-testid='verification-error']").matchImageSnapshot(
+      "WrongToken",
+    );
+    cy.visit(
+      `/user/verify-error?email=abc&code=${errorCode.ALREADY_VERIFIED}&message=xyz`,
+    );
+    cy.wait(1000);
+    cy.get("[data-testid='verification-error']").matchImageSnapshot(
+      "Verified Token",
+    );
+    cy.visit(
+      `/user/verify-error?email=abc&code=${errorCode.EXPIRED}&message=xyz`,
+    );
+    cy.wait(1000);
+    cy.get("[data-testid='verification-error']").matchImageSnapshot(
+      "Expired Token",
+    );
+    cy.visit(
+      `/user/verify-error?email=abc&code=${errorCode.UNKNOWN}&message=xyz`,
+    );
+    cy.wait(1000);
+    cy.get("[data-testid='verification-error']").matchImageSnapshot(
+      "Unknown error",
+    );
+  });
+  it.skip("Email verification settings test", () => {
+    cy.LogOut();
+    cy.LoginFromAPI(Cypress.env("USERNAME"), Cypress.env("PASSWORD"));
+    cy.get(".admin-settings-menu-option").should("be.visible");
+    cy.get(".admin-settings-menu-option").click();
+    cy.url().should("contain", "/settings/general");
+    cy.wait("@getEnvVariables");
+    cy.get(adminsSettings.authenticationTab).click();
+    cy.get(adminsSettings.formloginButton).click();
+    cy.pause();
+    // Assert verification is disabled
+    cy.get(adminsSettings.enableEmailVerificationInput).should("be.disabled");
+    // Assert callout
+    cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+    // go to email settings
+    cy.get(adminsSettings.emailTab).click();
+    // add email settings
+    cy.generateUUID().then((uuid) => {
+      cy.get(adminsSettings.fromAddress).clear().type(`${uuid}@appsmith.com`);
+    });
+    // save
+    cy.get(adminsSettings.saveButton).click();
+    cy.waitUntil(() =>
+      cy.contains("General", { timeout: 180000 }).should("be.visible"),
+    ).then(() => {
+      cy.wait("@getEnvVariables");
+      cy.get(adminsSettings.authenticationTab).click();
+      cy.get(adminsSettings.formloginButton).click();
+      // Assert verification is enabled
+      cy.get(adminsSettings.enableEmailVerificationInput).should(
+        "not.be.disabled",
+      );
+      // Assert callout
+      cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+      // turn on setting
+      cy.get(adminsSettings.enableEmailVerificationInput).click();
+      // Assert callout
+      cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+      cy.get(adminsSettings.saveButton).click();
+      cy.wait("@postTenantConfig").then((interception) => {
+        expect(interception.request.body.enableEmailVerification).to.equal(
+          true,
+        );
+      });
+      cy.get(adminsSettings.emailTab).click();
+      // remove email settings
+      cy.get(adminsSettings.saveButton).click();
+      cy.waitUntil(() =>
+        cy.contains("General", { timeout: 180000 }).should("be.visible"),
+      ).then(() => {
+        cy.wait("@getEnvVariables");
+        cy.get(adminsSettings.authenticationTab).click();
+        cy.get(adminsSettings.formloginButton).click();
+        // Assert verification is enabled
+        cy.get(adminsSettings.enableEmailVerificationInput).should(
+          "not.be.disabled",
+        );
+        // Assert callout
+        cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+        // turn off setting
+        cy.get(adminsSettings.enableEmailVerificationInput).click();
+        // assert verification is disabled
+        cy.get(adminsSettings.enableEmailVerificationInput).should(
+          "be.disabled",
+        );
+        // assert callout
+        cy.get(adminsSettings.adminSettingsCallout).should("be.visible");
+      });
+    });
+  });
+});

app/client/cypress/locators/AdminsSettings.js
@@ -34,4 +34,6 @@ export default {
   brandingSubmitButton: ".t--settings-branding-submit-button",
   businessTag: "[data-testid='t--business-tag']",
   enterpriseTag: "[data-testid='t--enterprise-tag']",
+  enableEmailVerificationInput: "[data-testid='emailVerificationEnabled']",
+  adminSettingsCallout: "[data-testid='admin-settings-group-link']",
 };

commit 005399e18acfa29ffac6ee5772a98d504a2b7e8a

fix: welcome mail trigger (#27166)
## Description
> This PR fixes the error introduced in
https://github.com/appsmithorg/appsmith/pull/25854 which lead to sending
of welcome mails for form-type login requests.

#### PR fixes following issue(s)
Fixes #27054 

#### Media
> A video or a GIF is preferred. when using Loom, don’t embed because it
looks like it’s a GIF. instead, just link to the video
>
>
#### Type of change
> Please delete options that are not relevant.
- Bug fix (non-breaking change which fixes an issue)

## Testing
>
#### How Has This Been Tested?
> Please describe the tests that you ran to verify your changes. Also
list any relevant details for your test configuration.
> Delete anything that is not relevant
- [x] Manual
- [ ] JUnit
- [ ] Jest
- [ ] Cypress
>
>
#### Test Plan
> Add Testsmith test cases links that relate to this PR
>
>
#### Issues raised during DP testing
> Link issues raised during DP testing for better visiblity and tracking
(copy link from comments dropped on this PR)
>
>
>
## Checklist:
#### Dev activity
- [x] My code follows the style guidelines of this project
- [x] I have performed a self-review of my own code
- [ ] I have commented my code, particularly in hard-to-understand areas
- [ ] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [ ] I have added tests that prove my fix is effective or that my
feature works
- [x] New and existing unit tests pass locally with my changes
- [ ] PR is being merged under a feature flag


#### QA activity:
- [ ] [Speedbreak
features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
have been covered
- [ ] Test plan covers all impacted features and [areas of
interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
- [ ] Test plan has been peer reviewed by project stakeholders and other
QA members

app/server/appsmith-server/src/main/java/com/appsmith/server/authentication/handlers/ce/AuthenticationSuccessHandlerCE.java
@@ -248,9 +248,7 @@ public class AuthenticationSuccessHandlerCE implements ServerAuthenticationSucce
                 if (TRUE.equals(isVerificationRequired)) {
                     return postVerificationRequiredHandler(webFilterExchange, user, null, FALSE);
                 } else {
-                    return userService
-                            .sendWelcomeEmail(user, originHeader)
-                            .then(redirectHelper.handleRedirect(webFilterExchange, null, false));
+                    return redirectHelper.handleRedirect(webFilterExchange, null, false);
                 }
             });
         }