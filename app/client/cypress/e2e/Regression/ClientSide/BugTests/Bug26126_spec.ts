e6440dcf320a4f686d8f34ff85d894e7b6019199
Ayangade Adeoluwa <37867493+Irongade@users.noreply.github.com>
2023-09-11 10:54:32 +0100

    fix: fix ds required fields (#26940)
    When user toggles between different datasource configuration options in
    S3, the required fields was not being updated which prevents the user
    from saving the datasource. This also solves this problem for all
    possible datasources that have different configuration options.

---
+++ app/client/cypress/e2e/Regression/ClientSide/BugTests/Bug26126_spec.ts
@@ -0,0 +1,26 @@
+import {
+  dataSources,
+  entityExplorer,
+} from "../../../../support/Objects/ObjectsCore";
+
+describe("Bug 26126: Fix DS button disability", function () {
+  it("ensures save button is correctly updated when DS required fields change", function () {
+    dataSources.NavigateToDSCreateNew();
+    dataSources.CreatePlugIn("S3");
+    dataSources.FillS3DSForm();
+    dataSources.AssertSaveDSButtonDisability(false);
+    dataSources.ValidateNSelectDropdown(
+      "S3 service provider",
+      "Amazon S3",
+      "Upcloud",
+    );
+    dataSources.AssertSaveDSButtonDisability(true);
+    dataSources.ValidateNSelectDropdown(
+      "S3 service provider",
+      "Upcloud",
+      "Amazon S3",
+    );
+
+    dataSources.AssertSaveDSButtonDisability(false);
+  });
+});
