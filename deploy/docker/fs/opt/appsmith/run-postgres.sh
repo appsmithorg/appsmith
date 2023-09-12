5f4e970b99a99b58ccbe550cf67aff3d59c9aa22
Sumesh <sumesh@appsmith.com>
2023-09-12 07:50:12 +0530

    postgres core file cleanup

--- deploy/docker/fs/opt/appsmith/run-postgres.sh
+++ deploy/docker/fs/opt/appsmith/run-postgres.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-set -o errexit
-set -o pipefail
-set -o nounset
-set -o noglob
-
-rm -f /appsmith-stacks/data/postgres/core.*
+rm -f /appsmith-stacks/data/postgres/main/core.*
 exec /usr/lib/postgresql/13/bin/postgres -D "/appsmith-stacks/data/postgres/main" -c listen_addresses=127.0.0.1 -c stats_temp_directory=/tmp
