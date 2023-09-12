827582edee88cbebec0a82af07d665596e52acfa
Aishwarya-U-R <91450662+Aishwarya-U-R@users.noreply.github.com>
2023-09-12 17:33:51 +0530

    resolve merge conflicts

--- app/client/cypress/e2e/Regression/ServerSide/Datasources/ElasticSearch_Basic_Spec.ts
+++ app/client/cypress/e2e/Regression/ServerSide/Datasources/ElasticSearch_Basic_Spec.ts
@@ -90,7 +90,7 @@ describe.skip("Validate Elasticsearch DS", () => {
 
     agHelper.PressEnter();
 
-    agHelper.Sleep();
+    agHelper.Sleep(2000);
     dataSources.RunQuery();
     cy.get("@postExecute").then((resObj: any) => {
       expect(
@@ -141,6 +141,13 @@ describe.skip("Validate Elasticsearch DS", () => {
       directInput: false,
       inputFieldName: "Body",
     });
+
+    agHelper
+      .GetElement(dataSources._bodyCodeMirror)
+      .type("{downarrow}".repeat(5));
+
+    agHelper.PressEnter();
+    agHelper.Sleep(2000);
     dataSources.RunQuery();
 
     cy.get("@postExecute").then((resObj: any) => {
