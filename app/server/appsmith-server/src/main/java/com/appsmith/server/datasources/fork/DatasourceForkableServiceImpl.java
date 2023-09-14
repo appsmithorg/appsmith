d3510397fc26243e4176f0e9482b022a126833bd
Nidhi Nair <nidhi@appsmith.com>
2023-09-15 00:12:07 +0530

    fixed duplicate code

--- app/server/appsmith-server/src/main/java/com/appsmith/server/datasources/fork/DatasourceForkableServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/datasources/fork/DatasourceForkableServiceImpl.java
@@ -5,12 +5,11 @@ import com.appsmith.external.models.DatasourceStorage;
 import com.appsmith.server.datasources.base.DatasourceService;
 import com.appsmith.server.datasourcestorages.base.DatasourceStorageService;
 import com.appsmith.server.fork.forkable.ForkableService;
-import com.appsmith.server.fork.forkable.ForkableServiceCE;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DatasourceForkableServiceImpl extends DatasourceForkableServiceCEImpl
-        implements ForkableServiceCE<Datasource> {
+        implements ForkableService<Datasource> {
 
     public DatasourceForkableServiceImpl(
             DatasourceService datasourceService,
