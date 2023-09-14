a993e6f4f772fc81abbe866234263e8bdd658e38
Nidhi Nair <nidhi@appsmith.com>
2023-09-14 15:27:45 +0530

    chore: Refactored forking related classes to new struct

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/fork/forkable/ForkableService.java
@@ -0,0 +1,5 @@
+package com.appsmith.server.fork.forkable;
+
+import com.appsmith.external.models.BaseDomain;
+
+public interface ForkableService<T extends BaseDomain> extends ForkableServiceCE<T> {}
---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/NewActionService.java
@@ -0,0 +1,5 @@
+package com.appsmith.server.services;
+
+import com.appsmith.server.services.ce.NewActionServiceCE;
+
+public interface NewActionService extends NewActionServiceCE {}
