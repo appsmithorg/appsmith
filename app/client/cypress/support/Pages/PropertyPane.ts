f23b574791ddd33a4159189764887cbc6d56e156
nandan <nandan@thinkify.io>
2023-09-15 17:05:49 +0530

    resolved lint errors

--- app/client/cypress/support/Pages/PropertyPane.ts
+++ app/client/cypress/support/Pages/PropertyPane.ts
@@ -162,7 +162,7 @@ export class PropertyPane {
     ".currency-change-dropdown-trigger .remixicon-icon";
   _countryCodeChangeDropDown = ".t--input-country-code-change .remixicon-icon";
   _searchCountryPlaceHolder = "[placeholder='Search by ISD code or country']";
-  _dataIcon = (icon: string) => `[data-icon="${icon}"]`
+  _dataIcon = (icon: string) => `[data-icon="${icon}"]`;
 
   public OpenJsonFormFieldSettings(fieldName: string) {
     this.agHelper.GetNClick(this._jsonFieldEdit(fieldName));
