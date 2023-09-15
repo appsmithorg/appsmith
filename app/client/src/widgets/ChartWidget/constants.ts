765aaa1f37a9799264ee7bd71df112f99e2cb595
Rajat Agrawal <rajat@appsmith.com>
2023-09-15 16:20:17 +0530

    Whitespace commit

--- app/client/src/widgets/ChartWidget/constants.ts
+++ app/client/src/widgets/ChartWidget/constants.ts
@@ -43,7 +43,7 @@ export const messages = {
   ErrorTitle: "Error in Chart Data/Configuration",
   MoreDetails: "More Details",
   EmptyData: "No chart data to display",
-  Undefined: "Undefined",
+  Undefined: "Undefined 123",
   customFusionChartDeprecationMessage:
     "Custom Fusion Charts will stop being supported on March 1st 2024. Change the chart type to E-charts Custom to switch.",
   customFusionChartOptionLabel: (showDeprecationMessage: boolean) => {
