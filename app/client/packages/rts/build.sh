67afb64107d0172cd082042ca946b39db724f396
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2023-09-16 17:19:23 +0530

    Don't rename built file

--- app/client/packages/rts/build.sh
+++ app/client/packages/rts/build.sh
@@ -3,11 +3,20 @@
 set -o errexit
 
 cd "$(dirname "$0")"
-rm -rf dist/
 
 yarn install --immutable
 yarn run tsc --noEmit
 
-esbuild src/server.ts --bundle --minify --sourcemap --platform=node --target="$(node --version | sed s/v/node/)" --outfile=dist/rts.js
+rm -rf dist/
+esbuild src/server.ts \
+  --bundle \
+  --minify \
+  --sourcemap \
+  --platform=node \
+  --target="$(node --version | sed s/v/node/)" \
+  --outdir=dist \
+  --external:dtrace-provider
 
-cp -r dist "$(git rev-parse --show-toplevel)"/deploy/docker/fs/opt/appsmith/rts
+cp -vr \
+  dist \
+  "$(git rev-parse --show-toplevel)"/deploy/docker/fs/opt/appsmith/rts
