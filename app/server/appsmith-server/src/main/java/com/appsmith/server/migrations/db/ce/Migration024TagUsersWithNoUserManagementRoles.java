249fef49bc2370b902fd8fb3dc1dead23166b6e6
Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>
2023-09-18 16:31:03 +0530

    chore: migration for creating user management roles.

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration024TagUsersWithNoUserManagementRoles.java
@@ -0,0 +1,60 @@
+package com.appsmith.server.migrations.db.ce;
+
+import com.appsmith.external.models.QBaseDomain;
+import com.appsmith.server.domains.PermissionGroup;
+import com.appsmith.server.domains.User;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.util.List;
+
+import static com.appsmith.server.repositories.ce.BaseAppsmithRepositoryCEImpl.fieldName;
+
+@Slf4j
+@RequiredArgsConstructor
+@ChangeUnit(order = "023", id = "tag-users-with-no-user-management-roles")
+public class Migration024TagUsersWithNoUserManagementRoles {
+
+    private final MongoTemplate mongoTemplate;
+
+    public static final String MIGRATION_FLAG_024_TAG_USER_WITHOUT_USER_MANAGEMENT_ROLE =
+            "tagUserWithoutUserManagementRole";
+
+    @RollbackExecution
+    public void rollbackExecution() {}
+
+    @Execution
+    public void tagUsersWithNoUserManagementRoles() {
+        Criteria criteriaDefaultDomainTypeExists =
+                Criteria.where("defaultDomainType").exists(true);
+        Criteria criteriaDefaultDomainTypeIsUser =
+                Criteria.where("defaultDomainType").is(User.class.getSimpleName());
+
+        Criteria criteriaUserManagementRoles =
+                new Criteria().andOperator(criteriaDefaultDomainTypeExists, criteriaDefaultDomainTypeIsUser);
+        Query queryUserManagementRoles = new Query(criteriaUserManagementRoles);
+        queryUserManagementRoles.fields().include("defaultDomainId");
+
+        List<PermissionGroup> userManagementRoles = mongoTemplate.find(queryUserManagementRoles, PermissionGroup.class);
+
+        List<String> userIdsWithUserManagementRoles = userManagementRoles.stream()
+                .map(PermissionGroup::getDefaultDomainId)
+                .toList();
+
+        Criteria criteriaUsersWithNoUserManagementRoles =
+                Criteria.where(fieldName(QBaseDomain.baseDomain.id)).nin(userIdsWithUserManagementRoles);
+        Query queryUsersWithNoUserManagementRoles = new Query(criteriaUsersWithNoUserManagementRoles);
+
+        Update updateSetMigrationFlag = new Update();
+        updateSetMigrationFlag.set(MIGRATION_FLAG_024_TAG_USER_WITHOUT_USER_MANAGEMENT_ROLE, Boolean.TRUE);
+
+        mongoTemplate.updateMulti(queryUsersWithNoUserManagementRoles, updateSetMigrationFlag, User.class);
+    }
+}
---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration025CreateUserManagementRolesForUsersTaggedIn024.java
@@ -0,0 +1,98 @@
+package com.appsmith.server.migrations.db.ce;
+
+import com.appsmith.external.models.Policy;
+import com.appsmith.server.constants.FieldName;
+import com.appsmith.server.domains.PermissionGroup;
+import com.appsmith.server.domains.QUser;
+import com.appsmith.server.domains.User;
+import com.appsmith.server.dtos.Permission;
+import com.appsmith.server.exceptions.AppsmithError;
+import com.appsmith.server.exceptions.AppsmithException;
+import com.appsmith.server.migrations.utils.CompatibilityUtils;
+import com.appsmith.server.solutions.PolicySolution;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import java.util.Map;
+import java.util.Set;
+
+import static com.appsmith.server.acl.AclPermission.MANAGE_USERS;
+import static com.appsmith.server.repositories.ce.BaseAppsmithRepositoryCEImpl.fieldName;
+import static java.lang.Boolean.TRUE;
+
+@Slf4j
+@RequiredArgsConstructor
+@ChangeUnit(order = "023", id = "create-user-management-roles-for-users-tagged-in-migration-024")
+public class Migration025CreateUserManagementRolesForUsersTaggedIn024 {
+    private final MongoTemplate mongoTemplate;
+    private final PolicySolution policySolution;
+
+    private static final int migrationRetries = 5;
+    private static final String migrationNote = "This will not have any adverse effects on the Data. Restarting the "
+            + "server will begin the migration from where it left.";
+    private static final String migrationId = "create-user-management-roles-for-users-tagged-in-migration-024";
+
+    @RollbackExecution
+    public void rollbackExecution() {}
+
+    @Execution
+    public void createUserManagementRolesForUsersTaggedInMigration024() {
+        Criteria criteriaUsersTaggedInMigration024 = Criteria.where(
+                        Migration024TagUsersWithNoUserManagementRoles
+                                .MIGRATION_FLAG_024_TAG_USER_WITHOUT_USER_MANAGEMENT_ROLE)
+                .is(TRUE);
+
+        Query queryUsersTaggedInMigration024 = new Query(criteriaUsersTaggedInMigration024);
+        queryUsersTaggedInMigration024.fields().include(fieldName(QUser.user.id));
+        queryUsersTaggedInMigration024.fields().include(fieldName(QUser.user.policies));
+        queryUsersTaggedInMigration024.fields().include(fieldName(QUser.user.email));
+
+        Query optimisedQueryUsersTaggedInMigration024 = CompatibilityUtils.optimizeQueryForNoCursorTimeout(
+                mongoTemplate, queryUsersTaggedInMigration024, User.class);
+
+        int attempt = 0;
+        long countUsersTaggedInMigration024 = mongoTemplate.count(optimisedQueryUsersTaggedInMigration024, User.class);
+
+        while (attempt < migrationRetries && countUsersTaggedInMigration024 > 0) {
+            mongoTemplate.stream(optimisedQueryUsersTaggedInMigration024, User.class)
+                    .forEach(user -> {
+                        User userWithUpdatedPolicies = createUserManagementRoleAndGetUserWithUpdatedPolicies(user);
+                        mongoTemplate.save(userWithUpdatedPolicies);
+                    });
+            attempt += 1;
+            countUsersTaggedInMigration024 = mongoTemplate.count(optimisedQueryUsersTaggedInMigration024, User.class);
+        }
+
+        if (countUsersTaggedInMigration024 > 0) {
+            String reasonForFailure = "All user management roles were not created.";
+            throw new AppsmithException(AppsmithError.MIGRATION_FAILED, migrationId, reasonForFailure, migrationNote);
+        }
+    }
+
+    private User createUserManagementRoleAndGetUserWithUpdatedPolicies(User user) {
+        // Create user management permission group
+        PermissionGroup userManagementRole = new PermissionGroup();
+        userManagementRole.setName(user.getUsername() + FieldName.SUFFIX_USER_MANAGEMENT_ROLE);
+        // Add CRUD permissions for user to the group
+        userManagementRole.setPermissions(Set.of(new Permission(user.getId(), MANAGE_USERS)));
+
+        // Assign the permission group to the user
+        userManagementRole.setAssignedToUserIds(Set.of(user.getId()));
+
+        PermissionGroup savedUserManagementRole =
+                mongoTemplate.save(userManagementRole, PermissionGroup.class.getSimpleName());
+
+        Map<String, Policy> crudUserPolicies =
+                policySolution.generatePolicyFromPermissionGroupForObject(savedUserManagementRole, user.getId());
+
+        User updatedWithPolicies = policySolution.addPoliciesToExistingObject(crudUserPolicies, user);
+
+        return updatedWithPolicies;
+    }
+}
