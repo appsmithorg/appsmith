2b9d3bde20e2800204e73283a19865c7e6615dd8
Favour Ohans <fohanekwu@gmail.com>
2023-09-18 13:20:29 +0100

    Wait for data population before setting isLoading

--- app/client/src/sagas/WidgetLoadingSaga.ts
+++ app/client/src/sagas/WidgetLoadingSaga.ts
@@ -10,6 +10,7 @@ import type {
   ActionData,
   ActionDataState,
 } from "reducers/entityReducers/actionsReducer";
+import type { ReduxAction } from "@appsmith/constants/ReduxActionConstants";
 import {
   ReduxActionErrorTypes,
   ReduxActionTypes,
@@ -18,27 +19,62 @@ import log from "loglevel";
 import * as Sentry from "@sentry/react";
 import { findLoadingEntities } from "utils/WidgetLoadingStateUtils";
 
+const actionExecutionRequestActions = [
+  ReduxActionTypes.EXECUTE_PLUGIN_ACTION_REQUEST,
+  ReduxActionTypes.RUN_ACTION_REQUEST,
+];
+
+const actionExecutionCompletionActions = [
+  ReduxActionTypes.EXECUTE_PLUGIN_ACTION_SUCCESS,
+  ReduxActionTypes.RUN_ACTION_SUCCESS,
+  ReduxActionErrorTypes.RUN_ACTION_ERROR,
+  ReduxActionErrorTypes.EXECUTE_PLUGIN_ACTION_ERROR,
+  ReduxActionTypes.RUN_ACTION_CANCELLED,
+];
+
 const ACTION_EXECUTION_REDUX_ACTIONS = [
   // Actions
-  ReduxActionTypes.RUN_ACTION_REQUEST,
-  ReduxActionTypes.RUN_ACTION_SUCCESS,
-  ReduxActionTypes.EXECUTE_PLUGIN_ACTION_REQUEST,
-  ReduxActionTypes.EXECUTE_PLUGIN_ACTION_SUCCESS,
-  ReduxActionErrorTypes.EXECUTE_PLUGIN_ACTION_ERROR,
+  ...actionExecutionRequestActions,
+  ...actionExecutionCompletionActions,
+
   // Widget evalution
   ReduxActionTypes.SET_EVALUATED_TREE,
 ];
 
-function* setWidgetsLoadingSaga() {
+function* dispatchSetLoadingAction({
+  loadingEntities,
+  loadingTriggerAction,
+}: {
+  loadingEntities: Set<string>;
+  loadingTriggerAction: ReduxAction<unknown>;
+}) {
+  yield put({
+    type: ReduxActionTypes.SET_LOADING_ENTITIES,
+    payload: loadingEntities,
+  });
+
+  if (loadingTriggerAction.type !== ReduxActionTypes.SET_EVALUATED_TREE) {
+    yield put({
+      type: ReduxActionTypes.TRIGGER_EVAL,
+    });
+  }
+}
+
+function* setWidgetsLoadingSaga(action: ReduxAction<unknown>) {
+  if (actionExecutionCompletionActions.includes(action.type)) {
+    // Ensure that data is already available in the dataTree and all
+    // dependent entities have been re-evaluated
+    yield take(ReduxActionTypes.SET_EVALUATED_TREE);
+  }
   const actions: ActionDataState = yield select(getActions);
   const isLoadingActions: string[] = actions
     .filter((action: ActionData) => action.isLoading)
     .map((action: ActionData) => action.config.name);
 
   if (isLoadingActions.length === 0) {
-    yield put({
-      type: ReduxActionTypes.SET_LOADING_ENTITIES,
-      payload: new Set<string>(),
+    yield call(dispatchSetLoadingAction, {
+      loadingEntities: new Set<string>(),
+      loadingTriggerAction: action,
     });
   } else {
     const inverseMap: DependencyMap = yield select(
@@ -52,17 +88,19 @@ function* setWidgetsLoadingSaga() {
       inverseMap,
     );
 
-    yield put({
-      type: ReduxActionTypes.SET_LOADING_ENTITIES,
-      payload: loadingEntities,
+    yield call(dispatchSetLoadingAction, {
+      loadingEntities,
+      loadingTriggerAction: action,
     });
   }
 }
 
 function* actionExecutionChangeListenerSaga() {
   while (true) {
-    yield take(ACTION_EXECUTION_REDUX_ACTIONS);
-    yield fork(setWidgetsLoadingSaga);
+    const action: ReduxAction<unknown> = yield take(
+      ACTION_EXECUTION_REDUX_ACTIONS,
+    );
+    yield fork(setWidgetsLoadingSaga, action);
   }
 }
 
