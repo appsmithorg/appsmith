439fd5af33602a442cf71bdeb7ee3321d4545033
Druthi Polisetty <druthi@appsmith.com>
2023-09-19 15:14:52 +0530

    fix: resetWidget doesn't work when a value is set with a setter

--- app/client/src/sagas/ActionExecution/ResetWidgetActionSaga.ts
+++ app/client/src/sagas/ActionExecution/ResetWidgetActionSaga.ts
@@ -1,4 +1,4 @@
-import { put, select, take } from "redux-saga/effects";
+import { call, put, select, take } from "redux-saga/effects";
 import { getWidgetByName } from "sagas/selectors";
 import {
   resetChildrenMetaProperty,
@@ -12,20 +12,33 @@ import {
 import { getType, Types } from "utils/TypeHelpers";
 import type { FlattenedWidgetProps } from "WidgetProvider/constants";
 import { ReduxActionTypes } from "@appsmith/constants/ReduxActionConstants";
-import { getDataTree, getConfigTree } from "selectors/dataTreeSelectors";
+import {
+  getDataTree,
+  getConfigTree,
+  getUnevaluatedDataTree,
+} from "selectors/dataTreeSelectors";
 import type {
   DataTree,
   ConfigTree,
   WidgetEntityConfig,
+  UnEvalTree,
+  UnEvalTreeEntity,
 } from "entities/DataTree/dataTreeFactory";
 import { isWidget } from "@appsmith/workers/Evaluation/evaluationUtils";
 import type { TResetWidgetDescription } from "workers/Evaluation/fns/resetWidget";
+import type { PropertyOverrideDependency } from "entities/DataTree/types";
+import { evalWorker } from "sagas/EvaluationsSaga";
+import { EVAL_WORKER_ACTIONS } from "@appsmith/workers/Evaluation/evalWorkerActions";
+import { isDynamicValue } from "utils/DynamicBindingUtils";
+import { getCodeFromMoustache } from "components/editorComponents/ActionCreator/utils";
+import { klona } from "klona";
 
 export default function* resetWidgetActionSaga(
   action: TResetWidgetDescription,
 ) {
   const { payload } = action;
   const { widgetName } = payload;
+
   if (getType(widgetName) !== Types.STRING) {
     throw new ActionValidationError(
       "RESET_WIDGET_META_RECURSIVE_BY_NAME",
@@ -36,6 +49,8 @@ export default function* resetWidgetActionSaga(
   }
   const dataTree: DataTree = yield select(getDataTree);
   const configTree: ConfigTree = yield select(getConfigTree);
+  const unEvalAndConfigTree: ReturnType<typeof getUnevaluatedDataTree> =
+    yield select(getUnevaluatedDataTree);
 
   const widget: FlattenedWidgetProps | undefined = yield select(
     getWidgetByName,
@@ -45,12 +60,63 @@ export default function* resetWidgetActionSaga(
     throw new TriggerFailureError(`Widget ${payload.widgetName} not found`);
   }
   const evaluatedEntity = dataTree[widget.widgetName];
-  const evaluatedEntityConfig = configTree[widget.widgetName];
+  const evaluatedEntityConfig = configTree[
+    widget.widgetName
+  ] as WidgetEntityConfig;
+
+  const unEvalEntity = unEvalAndConfigTree.unEvalTree[
+    widget.widgetName
+  ] as UnEvalTree;
+
   if (isWidget(evaluatedEntity)) {
+    const evaluatedEntityClone = klona(evaluatedEntity);
+
+    if (evaluatedEntity) {
+      const { propertyOverrideDependency } = evaluatedEntityConfig;
+      // propertyOverrideDependency has defaultProperty name for each meta property of widget
+      const propertyOverrideDependencyEntries = Object.entries(
+        propertyOverrideDependency as PropertyOverrideDependency,
+      );
+
+      for (let i = 0; i < propertyOverrideDependencyEntries.length; i++) {
+        const [, dependency] = propertyOverrideDependencyEntries[i];
+        const defaultPropertyPath = dependency.DEFAULT;
+
+        if (!defaultPropertyPath) continue;
+
+        const expressionToEvaluate: string | UnEvalTreeEntity | undefined =
+          defaultPropertyPath && unEvalEntity[defaultPropertyPath];
+
+        let finalValue: unknown;
+        if (
+          expressionToEvaluate &&
+          typeof expressionToEvaluate === "string" &&
+          isDynamicValue(expressionToEvaluate)
+        ) {
+          const codeFromMoustache = getCodeFromMoustache(expressionToEvaluate);
+          const workerResponse: {
+            errors: Array<unknown>;
+            result: unknown;
+          } = yield call(
+            evalWorker.request,
+            EVAL_WORKER_ACTIONS.EVAL_EXPRESSION,
+            {
+              expression: codeFromMoustache,
+            },
+          );
+          finalValue = workerResponse.result;
+        } else {
+          finalValue = expressionToEvaluate;
+        }
+
+        evaluatedEntityClone[defaultPropertyPath] = finalValue;
+      }
+    }
+
     yield put(
       resetWidgetMetaProperty(
         widget.widgetId,
-        evaluatedEntity,
+        evaluatedEntityClone,
         evaluatedEntityConfig as WidgetEntityConfig,
       ),
     );
