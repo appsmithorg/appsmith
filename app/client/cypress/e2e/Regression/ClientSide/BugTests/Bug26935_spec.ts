31f7fd25d55924a062e7c054cfab8115e233b5f2
Favour Ohans <fohanekwu@gmail.com>
2023-09-19 09:42:31 +0100

    Add  tests for widget isLoading

---
+++ app/client/cypress/e2e/Regression/ClientSide/BugTests/Bug26935_spec.ts
@@ -0,0 +1,43 @@
+import {
+  agHelper,
+  locators,
+  entityExplorer,
+  jsEditor,
+  propPane,
+  apiPage,
+  dataManager,
+  draggableWidgets,
+} from "../../../../support/Objects/ObjectsCore";
+
+describe("Assistive Binding", function () {
+  before(() => {
+    // Create Api1
+    apiPage.CreateAndFillApi(dataManager.paginationUrl(100, 1));
+    // Table1
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.TABLE, 100, 100);
+
+    // Button1
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON, 200, 500);
+
+    // Text1
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.TEXT, 200, 600);
+  });
+  it("Updates isLoading property of widgets when API/Query is executing", () => {
+    entityExplorer.SelectEntityByName("Button1", "Widgets");
+    propPane.EnterJSContext("onClick", `{{Api1.run()}}`, true, false);
+    entityExplorer.SelectEntityByName("Text1", "Widgets");
+    propPane.TypeTextIntoField(
+      "Text",
+      "Table1 isLoading: {{Table1.isLoading}}",
+    );
+    entityExplorer.SelectEntityByName("Table1", "Widgets");
+    propPane.EnterJSContext("Table data", "{{Api1.data}}", true, false);
+
+    agHelper.AssertContains("Table1 isLoading: false");
+    agHelper.ClickButton("Submit");
+    // After triggering API execution, check that isLoading is set to true
+    agHelper.AssertContains("Table1 isLoading: true");
+    agHelper.Sleep(2000);
+    agHelper.AssertContains("Table1 isLoading: false");
+  });
+});
