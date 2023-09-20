fceb1ffb748376bf87858b7a4c69e68a51c9de72
Pawan Kumar <pawan.stardust@gmail.com>
2023-09-20 13:18:26 +0530

    minor refactor

--- app/client/packages/design-system/widgets/src/styles/src/index.ts
+++ app/client/packages/design-system/widgets/src/styles/src/index.ts
@@ -1,2 +1,4 @@
 export { default as fieldStyles } from "./field.module.css";
 export { default as inlineLabelStyles } from "./inline-label.module.css";
+
+export { textInputStyles } from "./textInputStyles";
