414dcf51f50be32db899de4affd58f64dada840b
Saroj <saroj@appsmith.com>
2023-09-21 10:30:39 +0530

    resolving review comments

--- app/client/cypress_ci_hosted.config.ts
+++ app/client/cypress_ci_hosted.config.ts
@@ -52,8 +52,8 @@ export default defineConfig({
     specPattern: [
       "cypress/e2e/Sanity/Datasources/Airtable_Basic_Spec.ts",
       "cypress/e2e/GSheet/**/**/*",
-      "cypress/e2e/Regression/ServerSide/Datasources/Oracle_Spec.ts",
       "cypress/e2e/Regression/ServerSide/Datasources/ElasticSearch_Basic_Spec.ts",
+      "cypress/e2e/Regression/ServerSide/Datasources/Oracle_Spec.ts",
     ],
     testIsolation: false,
     excludeSpecPattern: ["cypress/e2e/**/spec_utility.ts"],
