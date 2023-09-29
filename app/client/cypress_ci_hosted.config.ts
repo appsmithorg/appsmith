a30da7abaadd0a50d15627d1642fe594c7c82baa
Saroj <saroj@appsmith.com>
2023-09-29 13:19:19 +0530

    running only gsheet specs

--- app/client/cypress_ci_hosted.config.ts
+++ app/client/cypress_ci_hosted.config.ts
@@ -49,12 +49,7 @@ export default defineConfig({
       );
       return require("./cypress/plugins/index.js")(on, config);
     },
-    specPattern: [
-      "cypress/e2e/Sanity/Datasources/Airtable_Basic_Spec.ts",
-      "cypress/e2e/GSheet/**/**/*",
-      "cypress/e2e/Regression/ServerSide/Datasources/ElasticSearch_Basic_Spec.ts",
-      "cypress/e2e/Regression/ServerSide/Datasources/Oracle_Spec.ts",
-    ],
+    specPattern: ["cypress/e2e/GSheet/**/**/*"],
     testIsolation: false,
     excludeSpecPattern: ["cypress/e2e/**/spec_utility.ts"],
   },
