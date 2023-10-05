a916b23c08fecaf82150f7027be84b91ded81c25
Sharanya <sharanya@appsmith.com>
2023-10-05 14:42:29 +0530

    chart widget cases

---
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Chart/Chart_widget_spec_2.ts
@@ -0,0 +1,74 @@
+import {
+  agHelper,
+  entityExplorer,
+  deployMode,
+  draggableWidgets,
+  propPane,
+  locators,
+} from "../../../../../support/Objects/ObjectsCore";
+
+describe("", () => {
+  before(() => {
+    entityExplorer.DragNDropWidget(draggableWidgets.CHART);
+  });
+
+  afterEach(() => {
+    deployMode.NavigateBacktoEditor();
+    entityExplorer.SelectEntityByName("Chart1");
+  });
+
+  it("1. Test Series tile chart", () => {
+    propPane.SelectPropertiesDropDown("Chart Type", "Pie chart");
+    propPane.UpdatePropertyFieldValue(
+      "Title",
+      "Data of anime at 2020 @ December",
+    );
+    agHelper.AssertAutoSave();
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._widgetInDeployed(draggableWidgets.CHART))
+      .matchImageSnapshot("chartwidget/piechartsnapshotwithtitle");
+  });
+
+  it("2. Test Adaptive axis", () => {
+    propPane.SelectPropertiesDropDown("Chart Type", "Column chart");
+    agHelper.AssertAutoSave();
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._widgetInDeployed(draggableWidgets.CHART))
+      .matchImageSnapshot("chartwidget/columnchartsnapshotwithoutadaptiveaxis");
+    deployMode.NavigateBacktoEditor();
+    entityExplorer.SelectEntityByName("Chart1");
+    propPane.TogglePropertyState("Adaptive axis", "On");
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._widgetInDeployed(draggableWidgets.CHART))
+      .matchImageSnapshot("chartwidget/columnchartsnapshotwithadaptiveaxis");
+  });
+
+  it("3. Test x axis label orientation chart", () => {
+    propPane.SelectPropertiesDropDown("Chart Type", "Line chart");
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._widgetInDeployed(draggableWidgets.CHART))
+      .matchImageSnapshot("chartwidget/linechartWithAutoXAxisLabelOrientation");
+    deployMode.NavigateBacktoEditor();
+    entityExplorer.SelectEntityByName("Chart1");
+    propPane.SelectPropertiesDropDown("x-axis label orientation", "Slant");
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._widgetInDeployed(draggableWidgets.CHART))
+      .matchImageSnapshot(
+        "chartwidget/linechartWithSlantXAxisLabelOrientation",
+      );
+    deployMode.NavigateBacktoEditor();
+    entityExplorer.SelectEntityByName("Chart1");
+    propPane.SelectPropertiesDropDown("x-axis label orientation", "Rotate");
+    deployMode.DeployApp();
+    agHelper
+      .GetElement(locators._widgetInDeployed(draggableWidgets.CHART))
+      .matchImageSnapshot(
+        "chartwidget/linechartWithRotateXAxisLabelOrientation",
+      );
+  });
+});
