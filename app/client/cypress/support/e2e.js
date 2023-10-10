70e91a63c71bc214f50185010b19399652762637
Aishwarya UR <aishwarya@appsmith.com>
2023-10-10 12:41:52 +0530

    Cypress.on exception & fail updated

--- app/client/cypress/support/e2e.js
+++ app/client/cypress/support/e2e.js
@@ -44,13 +44,14 @@ import {
 
 installLogsCollector();
 
-Cypress.on("uncaught:exception", () => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false;
+Cypress.on("uncaught:exception", (error) => {
+  cy.log(error.message);
+  //return true; // returning false here prevents Cypress from failing the test
+  throw error; // throw error to have test still fail
 });
 
 Cypress.on("fail", (error) => {
+  cy.log(error.message);
   throw error; // throw error to have test still fail
 });
 
