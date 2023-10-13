9c78853b45b4173a47a3e8cd96ee20c558c748a4
Manish Kumar <107841575+sondermanish@users.noreply.github.com>
2023-10-13 16:41:19 +0530

    chore: added support changes (#28045)
    ## Description
    > CE support for PR https://github.com/appsmithorg/appsmith-ee/pull/2634
    - Added compatibility classes for EE support
    
    Fixes https://github.com/appsmithorg/appsmith-ee/issues/2621

--- app/server/appsmith-server/src/main/java/com/appsmith/server/datasourcestorages/base/DatasourceStorageServiceCECompatibleImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/datasourcestorages/base/DatasourceStorageServiceCECompatibleImpl.java
@@ -1,18 +1,17 @@
 package com.appsmith.server.datasourcestorages.base;
 
 import com.appsmith.server.helpers.PluginExecutorHelper;
-import com.appsmith.server.plugins.base.PluginService;
 import com.appsmith.server.repositories.DatasourceStorageRepository;
 import com.appsmith.server.services.AnalyticsService;
+import com.appsmith.server.services.PluginService;
 import com.appsmith.server.solutions.DatasourcePermission;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
-public class DatasourceStorageServiceImpl extends DatasourceStorageServiceCECompatibleImpl
-        implements DatasourceStorageService {
-    public DatasourceStorageServiceImpl(
+public class DatasourceStorageServiceCECompatibleImpl extends DatasourceStorageServiceCEImpl
+        implements DatasourceStorageServiceCECompatible {
+
+    public DatasourceStorageServiceCECompatibleImpl(
             DatasourceStorageRepository repository,
             DatasourcePermission datasourcePermission,
             PluginService pluginService,
