c85f47abf4246f4ef9a9599418b2f04ddd4cb789
Diljit VJ <diljit@appsmith.com>
2023-10-13 11:37:38 +0530

    Return default ai context from getAIContext function

--- app/client/src/ce/components/editorComponents/GPT/trigger.tsx
+++ app/client/src/ce/components/editorComponents/GPT/trigger.tsx
@@ -2,7 +2,10 @@
 import type { TEditorModes } from "components/editorComponents/CodeEditor/EditorConfig";
 export const APPSMITH_AI = "Appsmith AI";
 import type { FeatureFlags } from "@appsmith/entities/FeatureFlag";
-import type { ENTITY_TYPE } from "@appsmith/entities/DataTree/types";
+import {
+  ENTITY_TYPE_VALUE,
+  type ENTITY_TYPE,
+} from "@appsmith/entities/DataTree/types";
 
 export function isAIEnabled(ff: FeatureFlags, mode: TEditorModes) {
   return false;
@@ -13,17 +16,21 @@ export const isAISlashCommand = (editor: CodeMirror.Editor) => {
 };
 
 export const getAIContext = ({
-  currentLineValue,
   cursorPosition,
   editor,
-  entityType,
-  slashIndex,
 }: {
   entityType?: ENTITY_TYPE;
-  slashIndex: number;
-  currentLineValue: string;
+  slashIndex?: number;
+  currentLineValue?: string;
   cursorPosition: CodeMirror.Position;
   editor: CodeMirror.Editor;
 }) => {
-  return null;
+  return {
+    functionName: "",
+    cursorLineNumber: cursorPosition.line,
+    functionString: "",
+    mode: editor.getMode().name,
+    cursorPosition,
+    cursorCoordinates: editor.cursorCoords(true, "local"),
+  };
 };
