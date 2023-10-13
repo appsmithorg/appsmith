780867957383bd28a1b8e91c3d2037d68f517a7e
nandan <nandan@thinkify.io>
2023-10-13 22:28:50 +0530

    iframe regression tests

--- app/client/cypress/support/Objects/CommonLocators.ts
+++ app/client/cypress/support/Objects/CommonLocators.ts
@@ -320,4 +320,9 @@ export class CommonLocators {
   _treeSelectedContent = ".rc-tree-select-selection-item-content";
   _switcherIcon = ".switcher-icon";
   _root = "#root";
+  _pageHeaderToggle = ".mobile-ui-page-header-toggle-icon";
+  _pageHeaderMenuList = ".mobile-ui-page-header-menu-list";
+  _enterFullScreen = ".application-demo-new-dashboard-control-enter-fullscreen";
+  _dashboardContainer = ".application-demo-new-dashboard-container";
+  _exitFullScreen = ".application-demo-new-dashboard-control-exit-fullscreen";
 }
