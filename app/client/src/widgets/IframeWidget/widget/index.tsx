a6f50802c70a366739ec1a8cd33c41161557476c
Kyle Zhang <u6133716@anu.edu.au>
2023-10-15 21:13:12 +1100

    Implemented iFrame dynamic visibility control.

--- app/client/src/widgets/IframeWidget/widget/index.tsx
+++ app/client/src/widgets/IframeWidget/widget/index.tsx
@@ -50,6 +50,7 @@ class IframeWidget extends BaseWidget<IframeWidgetProps, WidgetState> {
       widgetName: "Iframe",
       version: 1,
       animateLoading: true,
+      isVisible: true,
       responsiveBehavior: ResponsiveBehavior.Fill,
       flexVerticalAlignment: FlexVerticalAlignment.Top,
     };
@@ -166,6 +167,17 @@ class IframeWidget extends BaseWidget<IframeWidgetProps, WidgetState> {
             label: "Animate loading",
             controlType: "SWITCH",
             helpText: "Controls the loading of the widget",
+            defaultValue: true,
+            isJSConvertible: true,
+            isBindProperty: true,
+            isTriggerProperty: false,
+            validation: { type: ValidationTypes.BOOLEAN },
+          },
+          {
+            propertyName: "isVisible",
+            label: "Visible",
+            controlType: "SWITCH",
+            helpText: "Controls the visibility of the widget",
             defaultValue: true,
             isJSConvertible: true,
             isBindProperty: true,
@@ -345,6 +357,7 @@ class IframeWidget extends BaseWidget<IframeWidgetProps, WidgetState> {
       borderColor,
       borderOpacity,
       borderWidth,
+      isVisible,
       renderMode,
       source,
       srcDoc,
@@ -360,6 +373,7 @@ class IframeWidget extends BaseWidget<IframeWidgetProps, WidgetState> {
         borderRadius={this.props.borderRadius}
         borderWidth={borderWidth}
         boxShadow={this.props.boxShadow}
+        isVisible={isVisible}
         onMessageReceived={this.handleMessageReceive}
         onSrcDocChanged={this.handleSrcDocChange}
         onURLChanged={this.handleUrlChange}
