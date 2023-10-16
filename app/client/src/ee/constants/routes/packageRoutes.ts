4eeee80ca5ac45dcac56014460b81e7c896db0a5
ashit-rath <ashit@appsmith.com>
2023-10-16 19:07:59 +0530

    chore: Route builder for packages (#2659)
    ## Description
    PR extends the URLAssembly class to add support for building Package
    editor based routes.
    
    The EE class, looks into the current route to identify if the current
    editor is an app or a package based on whether the current url starts
    with `/pkg` (Package Editor) or not.
    Based on the above result, if it's an app editor then the app based
    baseUrl generation logic is call otherwise the package baseUrl
    generation logic is called.
    Post this the rest of the url building pattern and attributes are same.
    
    #### PR fixes following issue(s)
    Fixes appsmithorg/appsmith#28006
    
    #### Type of change
    > Please delete options that are not relevant.
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## Testing
    >
    #### How Has This Been Tested?
    - [x] Manual
    - [ ] JUnit
    - [x] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/ee/constants/routes/packageRoutes.ts
+++ app/client/src/ee/constants/routes/packageRoutes.ts
@@ -1,2 +1,3 @@
 export const BASE_PACKAGE_EDITOR_URL = "/pkg";
 export const PACKAGE_EDITOR_URL = `${BASE_PACKAGE_EDITOR_URL}/:packageId`;
+export const MODULE_EDITOR_BASE_PATH = `${PACKAGE_EDITOR_URL}/:moduleId`;
