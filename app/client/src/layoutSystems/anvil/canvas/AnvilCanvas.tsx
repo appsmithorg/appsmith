078dcf62e07f7b5da48a4d0ef563533849b34b65
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-10-17 16:21:40 +0530

    chore: update canvasid utils

--- app/client/src/layoutSystems/anvil/canvas/AnvilCanvas.tsx
+++ app/client/src/layoutSystems/anvil/canvas/AnvilCanvas.tsx
@@ -4,22 +4,23 @@ import type { BaseWidgetProps } from "widgets/BaseWidgetHOC/withBaseWidgetHOC";
 import type { LayoutComponentProps } from "../utils/anvilTypes";
 import type { WidgetProps } from "widgets/BaseWidget";
 import { renderLayouts } from "../utils/layouts/renderUtils";
-import { getCanvasId } from "./utils";
-import { RenderModes } from "constants/WidgetConstants";
+import { getAnvilCanvasId } from "./utils";
+import {
+  MAIN_CONTAINER_WIDGET_ID,
+  RenderModes,
+} from "constants/WidgetConstants";
 
 export const AnvilCanvas = (props: BaseWidgetProps) => {
   const map: LayoutComponentProps["childrenMap"] = {};
   props.children.forEach((child: WidgetProps) => {
     map[child.widgetId] = child;
   });
-  const style = {
-    minHeight: props.minHeight + "px",
-  };
   return (
     <div
-      className="anvil-canvas"
-      id={getCanvasId(props.widgetId)}
-      style={style}
+      className={`anvil-canvas ${
+        props.widgetId === MAIN_CONTAINER_WIDGET_ID ? "overflowY" : ""
+      }`}
+      id={getAnvilCanvasId(props.widgetId)}
     >
       {renderLayouts(
         props.layout,
