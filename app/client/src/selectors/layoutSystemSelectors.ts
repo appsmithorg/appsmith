830928697ab93309d61c5e8056a41a173ba16db6
Preet Sidhu <preetsidhu.bits@gmail.com>
2023-10-17 08:26:49 -0400

    remove hard coding

--- app/client/src/selectors/layoutSystemSelectors.ts
+++ app/client/src/selectors/layoutSystemSelectors.ts
@@ -5,15 +5,14 @@ import { LayoutSystemTypes } from "layoutSystems/types";
  * selector to fetch the application's layout type
  */
 export const getLayoutSystemType = (state: AppState) => {
-  return LayoutSystemTypes.ANVIL;
-  // if (
-  //   state.ui.applications?.currentApplication?.applicationDetail?.appPositioning
-  //     ?.type
-  // ) {
-  //   return LayoutSystemTypes[
-  //     state.ui.applications.currentApplication?.applicationDetail
-  //       ?.appPositioning?.type
-  //   ];
-  // }
-  // return LayoutSystemTypes.FIXED;
+  if (
+    state.ui.applications?.currentApplication?.applicationDetail?.appPositioning
+      ?.type
+  ) {
+    return LayoutSystemTypes[
+      state.ui.applications.currentApplication?.applicationDetail
+        ?.appPositioning?.type
+    ];
+  }
+  return LayoutSystemTypes.FIXED;
 };
