7359fe5e8e88d6fb1ababa362264fc66365f3949
Aishwarya UR <aishwarya@appsmith.com>
2023-10-18 22:23:43 +0530

    remove cypress.runne.stop()

--- app/client/cypress/support/e2e.js
+++ app/client/cypress/support/e2e.js
@@ -49,21 +49,9 @@ Cypress.on("uncaught:exception", (error) => {
   return false; // returning false here prevents Cypress from failing the test
 });
 
-let testFailed = false; //used to track test status, resetting
-
-// Cypress.on("fail", (error) => {
-//   testFailed = true;
-//   cy.log(error.message);
-//   throw error; // throw error to have test still fail
-// });
-
-beforeEach(() => {
-  testFailed = false;
-  Cypress.on("fail", (error) => {
-    testFailed = true;
-    cy.log(error.message);
-    throw error; // throw error to have test fail
-  });
+Cypress.on("fail", (error) => {
+  cy.log(error.message);
+  throw error; // throw error to have test fail
 });
 
 Cypress.env("MESSAGES", MESSAGES);
@@ -191,12 +179,6 @@ beforeEach(function () {
   cy.intercept("api/v1/admin/env", (req) => {
     req.headers["origin"] = Cypress.config("baseUrl");
   });
-});
-
-afterEach(function () {
-  if (testFailed) {
-    Cypress.runner.stop(); // This stops the Cypress run.
-  }
 });
 
 after(function () {
