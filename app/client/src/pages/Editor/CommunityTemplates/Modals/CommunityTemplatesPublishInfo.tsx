bbcb323f1369ad8928dc3ac21f4674032b10b79a
Rahul Barwal <rahul.barwal@appsmith.com>
2023-10-19 13:15:59 +0530

    Adds constants for templates
    * reduces whitespace in showcase tab.
    * adds constants

--- app/client/src/pages/Editor/CommunityTemplates/Modals/CommunityTemplatesPublishInfo.tsx
+++ app/client/src/pages/Editor/CommunityTemplates/Modals/CommunityTemplatesPublishInfo.tsx
@@ -9,6 +9,7 @@ import { Button, Icon, Text } from "design-system";
 import React, { useCallback, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import { COMMUNITY_PORTAL } from "constants/TemplatesConstants";
 
 interface Props {
   onPublishClick: () => void;
@@ -40,13 +41,12 @@ const CommunityTemplatesPublishInfo = ({
 };
 
 export default CommunityTemplatesPublishInfo;
-const COMMUNITY_PORTAL_BASE_URL = "https://community.appsmith.com";
 
 const PublishedAppInstructions = () => {
   const currentApplication = useSelector(getCurrentApplication);
   const onVisitTemplateClick = useCallback(() => {
     openUrlInNewPage(
-      `${COMMUNITY_PORTAL_BASE_URL}/template/${currentApplication?.id}`,
+      `${COMMUNITY_PORTAL.BASE_URL}/template/${currentApplication?.id}`,
     );
   }, [currentApplication?.id]);
 
@@ -96,7 +96,7 @@ const UnPublishedAppInstructions = ({
       <InfoFooter>
         <Button
           endIcon="link"
-          href="https://community.appsmith.com/library"
+          href={`${COMMUNITY_PORTAL.BASE_URL}/library`}
           kind="tertiary"
           renderAs="a"
           size="md"
@@ -131,7 +131,7 @@ const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 4px;
-  min-height: 250px;
+  min-height: 75px;
   padding-top: var(--ads-v2-spaces-2);
 `;
 const InfoFooter = styled.footer`
