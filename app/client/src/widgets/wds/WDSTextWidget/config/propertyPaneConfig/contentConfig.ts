468fff707e575be6284bcbbb626715fa223ea7ec
Pawan Kumar <pawan@appsmith.com>
2023-10-20 14:32:30 +0530

    chore: Create WDS Text Widget (#28211)
    Fixes #28210

---
+++ app/client/src/widgets/wds/WDSTextWidget/config/propertyPaneConfig/contentConfig.ts
@@ -0,0 +1,58 @@
+import { ValidationTypes } from "constants/WidgetValidation";
+
+export const propertyPaneContentConfig = [
+  {
+    sectionName: "General",
+    children: [
+      {
+        propertyName: "text",
+        helpText: "Sets the text of the widget",
+        label: "Text",
+        controlType: "INPUT_TEXT",
+        placeholderText: "Name:",
+        isBindProperty: true,
+        isTriggerProperty: false,
+        validation: {
+          type: ValidationTypes.TEXT,
+          params: { limitLineBreaks: true },
+        },
+      },
+      {
+        propertyName: "lineClamp",
+        helpText: "Controls the number of lines displayed",
+        label: "Line clamp (max lines)",
+        controlType: "INPUT_TEXT",
+        placeholderText: "No. of lines to display",
+        isBindProperty: true,
+        isTriggerProperty: false,
+        validation: {
+          type: ValidationTypes.NUMBER,
+          params: {
+            min: 1,
+          },
+        },
+      },
+      {
+        propertyName: "isVisible",
+        helpText: "Controls the visibility of the widget",
+        label: "Visible",
+        controlType: "SWITCH",
+        isJSConvertible: true,
+        isBindProperty: true,
+        isTriggerProperty: false,
+        validation: { type: ValidationTypes.BOOLEAN },
+      },
+      {
+        propertyName: "animateLoading",
+        label: "Animate loading",
+        controlType: "SWITCH",
+        helpText: "Controls the loading of the widget",
+        defaultValue: true,
+        isJSConvertible: true,
+        isBindProperty: true,
+        isTriggerProperty: false,
+        validation: { type: ValidationTypes.BOOLEAN },
+      },
+    ],
+  },
+];
