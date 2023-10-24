640afd3ec3d0cbcde76fc1b7483d5e34e9001137
Hetu Nandu <hetunandu@gmail.com>
2023-10-24 19:30:38 +0530

    Lib side pane

--- app/client/src/pages/Editor/IDE/LeftPane/AddLibraryPopover.tsx
+++ app/client/src/pages/Editor/IDE/LeftPane/AddLibraryPopover.tsx
@@ -1,10 +1,22 @@
-import { Button, Popover, PopoverTrigger } from "design-system";
+import {
+  Button,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+} from "design-system";
 import React, { useState } from "react";
+import {
+  createMessage,
+  customJSLibraryMessages,
+} from "@appsmith/constants/messages";
+import { Installer } from "../../Explorer/Libraries/Installer";
 
 const AddLibraryPopover = () => {
   const [open, setOpen] = useState(false);
   return (
-    <Popover open={open}>
+    <Popover onOpenChange={setOpen} open={open}>
       <PopoverTrigger>
         <Button
           isIconButton
@@ -14,6 +26,14 @@ const AddLibraryPopover = () => {
           startIcon="add-line"
         />
       </PopoverTrigger>
+      <PopoverContent align="start" className="z-[25]" side="left" size="md">
+        <PopoverHeader className="sticky top-0" isClosable>
+          {createMessage(customJSLibraryMessages.ADD_JS_LIBRARY)}
+        </PopoverHeader>
+        <PopoverBody className={"!overflow-y-clip"}>
+          <Installer />
+        </PopoverBody>
+      </PopoverContent>
     </Popover>
   );
 };
--- app/client/src/pages/Editor/IDE/LeftPane/LibrarySidePane.tsx
+++ app/client/src/pages/Editor/IDE/LeftPane/LibrarySidePane.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import AddLibraryPopover from "./AddLibraryPopover";
 import PaneHeader from "./PaneHeader";
+import { useSelector } from "react-redux";
+import { selectLibrariesForExplorer } from "@appsmith/selectors/entitiesSelector";
+import { List } from "design-system";
 
 const LibrarySidePane = () => {
+  const libraries = useSelector(selectLibrariesForExplorer);
+  const onClick = () => {};
   return (
     <div>
       <PaneHeader
         rightIcon={<AddLibraryPopover />}
         title="Installed Libraries"
+      />
+      <List
+        items={libraries.map((lib) => ({
+          title: lib.name,
+          description: lib.version || "",
+          onClick,
+          descriptionType: "inline",
+        }))}
       />
     </div>
   );
