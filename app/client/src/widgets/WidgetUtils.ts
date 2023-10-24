8bf2a229e5728f384115b295b1cf401457061cc6
Victor Kostyuk <victor@kosty.uk>
2023-10-24 15:34:57 +0200

    fix: revert whitespace changes

--- app/client/src/widgets/WidgetUtils.ts
+++ app/client/src/widgets/WidgetUtils.ts
@@ -394,8 +394,8 @@ export const PopoverStyles = createGlobalStyle<{
     }
 
     .${portalClassName} .${DTClasses.DATEPICKER_FOOTER} .${
-    Classes.BUTTON
-  }:hover {
+      Classes.BUTTON
+    }:hover {
       background-color: ${lightenColor(accentColor)};
     }
 
@@ -408,10 +408,10 @@ export const PopoverStyles = createGlobalStyle<{
     }
 
     .${portalClassName} .${DTClasses.DATEPICKER_YEAR_SELECT} select + .${
-    Classes.ICON
-  }, .${portalClassName} .${DTClasses.DATEPICKER_MONTH_SELECT} select + .${
-    Classes.ICON
-  } {
+      Classes.ICON
+    }, .${portalClassName} .${DTClasses.DATEPICKER_MONTH_SELECT} select + .${
+      Classes.ICON
+    } {
       color: var(--wds-color-icon) !important;
     }
 
@@ -424,8 +424,8 @@ export const PopoverStyles = createGlobalStyle<{
     }
 
     .${portalClassName} .${DTClasses.DATERANGEPICKER_SHORTCUTS} li a.${
-    Classes.ACTIVE
-  } {
+      Classes.ACTIVE
+    } {
       color: ${getComplementaryGrayscaleColor(accentColor)};
       background-color: ${accentColor};
     }
