3d42333539f06935edfc7cfb5dd27d01a872d50a
Abhinav Jha <abhinav@appsmith.com>
2023-10-25 11:10:43 +0530

    fix: Fix widget name canvas issues (#28314)
    ## Description
    Fixes the following from #28310
    - Use LayoutSystemType or LayoutSystemFeatures instead of feature flags
    in
    [app/client/src/pages/Editor/CanvasPropertyPane/index.tsx](https://github.com/appsmithorg/appsmith/pull/27966/files/e7603fd21350415d384a68039fa1abc4001b7e59#diff-6ffc62fd8fff7633da84c1bbf822394ff49f1d89dc4ad073fff16e2f5e5d41fd)
    - In WidgetNamesCanvas use SetDragginStateFnType or
    SetDraggingStateActionPayload and remove the other from the codebase.
    
    #### PR fixes following issue(s)
    Fixes #28312
    Fixes #28310
    
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    
    #### How Has This Been Tested?
    - [x] Manual
    - [ ] JUnit
    - [x] Jest
    - [ ] Cypress
    
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/layoutSystems/common/WidgetNamesCanvas/index.tsx
+++ app/client/src/layoutSystems/common/WidgetNamesCanvas/index.tsx
@@ -51,6 +51,7 @@ const OverlayCanvasContainer = (props: { canvasWidth: number }) => {
   const focusedWidgetNameData: WidgetNameData | undefined = useSelector(
     getFocusedWidgetNameData,
   );
+
   // should we allow dragging of widgets
   const shouldAllowDrag = useSelector(getShouldAllowDrag);
   // When we begin dragging, the drag and resize hooks need a few details to take over
@@ -61,8 +62,8 @@ const OverlayCanvasContainer = (props: { canvasWidth: number }) => {
   const wrapperRef = useRef<HTMLDivElement>(null);
   // used to keep track of positions of widgetName drawn on canvas to make it intractable
   const widgetNamePositions = useRef<WidgetNamePositionType>({
-    selected: undefined,
-    focused: undefined,
+    selected: {},
+    focused: {},
   });
   //Positions of canvas
   const canvasPositions = useRef<CanvasPositions>({
@@ -159,14 +160,23 @@ const OverlayCanvasContainer = (props: { canvasWidth: number }) => {
   // Reset the canvas if no widgets are focused or selected
   // Update the widget name positions if there are widgets focused or selected
   // and they've changed.
-
-  // Note: If the selector for `selectWidgetNameData` reference changes
-  // Then this will run on every render. We should be careful about this.
   useEffect(() => {
     if (!selectedWidgetNameData && !focusedWidgetNameData) {
       resetCanvas(widgetNamePositions, stageRef);
     } else {
-      updateFn();
+      // The following is a hack, where if the widget name data is an empty array
+      // The source is the fact that we're moving a widget
+      // In this case, we don't want to lose the references we have right now,
+      // because after dropping, the selectedWidgetNameData will come back as it was before
+      // the move. We only want to recompute widget name positions if the widget name data
+      // has changed after the layout element positions have been computed
+      // In the case of layout element positions being recomputed, the actual widget name data
+      // will be different from the widget name data we have right now.
+      if (selectedWidgetNameData?.length === 0) {
+        resetCanvas(widgetNamePositions, stageRef, true);
+      } else {
+        updateFn();
+      }
     }
   }, [selectedWidgetNameData, focusedWidgetNameData]);
 
