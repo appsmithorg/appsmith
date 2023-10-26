f2527b7c9e63dfc4c4be377f48c5dfa8a34dd55e
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-10-26 18:17:50 +0530

    wip: show drag images on drag

--- app/client/src/layoutSystems/anvil/canvasArenas/hooks/useCanvasActivation.ts
+++ app/client/src/layoutSystems/anvil/canvasArenas/hooks/useCanvasActivation.ts
@@ -1,11 +1,15 @@
 import { CANVAS_ART_BOARD } from "constants/componentClassNameConstants";
 import { Indices } from "constants/Layers";
 import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
+import type { DraggedWidget } from "layoutSystems/anvil/utils/anvilTypes";
 import type { LayoutElementPosition } from "layoutSystems/common/types";
 import { positionObserver } from "layoutSystems/common/utils/LayoutElementPositionsObserver";
 import { getAnvilLayoutDOMId } from "layoutSystems/common/utils/LayoutElementPositionsObserver/utils";
+import { throttle } from "lodash";
 import { useEffect, useRef } from "react";
+import type { DragDetails } from "reducers/uiReducers/dragResizeReducer";
 import { useWidgetDragResize } from "utils/hooks/dragResizeHooks";
+import WidgetFactory from "WidgetProvider/factory";
 import type { AnvilDnDStates } from "./useAnvilDnDStates";
 
 export const AnvilCanvasZIndex = {
@@ -28,6 +32,81 @@ const checkIfMousePositionIsInsideBlock = (
   );
 };
 
+const createNewWidgetPreview = (dragDetails: DragDetails): HTMLDivElement => {
+  const type = `${dragDetails.newWidget.type
+    .split("_")
+    .join("")
+    .toLowerCase()}`;
+  const cardDiv = document.getElementById(`widget-card-draggable-${type}`);
+  const clonedDiv = cardDiv?.cloneNode(true) as HTMLDivElement;
+  clonedDiv.style.position = "absolute";
+  clonedDiv.style.zIndex = AnvilCanvasZIndex.activated;
+  clonedDiv.style.pointerEvents = "none";
+  clonedDiv.style.top = "-100px";
+  clonedDiv.style.position = "-100px";
+  clonedDiv.style.border = "2px solid black";
+  clonedDiv.style.background = "white";
+  clonedDiv.style.width = cardDiv?.clientWidth + "px";
+  clonedDiv.style.height = cardDiv?.clientHeight + "px";
+  return clonedDiv;
+};
+
+const createSelectedWidgetsPreview = (
+  draggedBlocks: DraggedWidget[],
+): HTMLDivElement => {
+  const parentWrapperDiv = document.createElement("div");
+  draggedBlocks.forEach((each, i) => {
+    if (i < 2) {
+      const wrapperDiv = document.createElement("div");
+      wrapperDiv.style.display = "flex";
+      wrapperDiv.style.justifyContent = "center";
+      wrapperDiv.style.alignItems = "center";
+      const textDiv = document.createElement("text");
+      textDiv.innerText =
+        WidgetFactory.getConfig(each.type)?.widgetName || each.type;
+      wrapperDiv.appendChild(textDiv);
+      wrapperDiv.style.border = "1px solid black";
+      wrapperDiv.style.minHeight = "50px";
+      wrapperDiv.style.minWidth = "100px";
+      wrapperDiv.style.width = "100%";
+      wrapperDiv.style.height = "100%";
+      wrapperDiv.style.padding = "5px 5px";
+      wrapperDiv.style.pointerEvents = "none";
+      parentWrapperDiv.appendChild(wrapperDiv);
+    }
+  });
+
+  parentWrapperDiv.style.display = "flex";
+  parentWrapperDiv.style.flexDirection = "row";
+  parentWrapperDiv.style.position = "absolute";
+  parentWrapperDiv.style.zIndex = AnvilCanvasZIndex.activated;
+  parentWrapperDiv.style.pointerEvents = "none";
+  parentWrapperDiv.style.top = "-100px";
+  parentWrapperDiv.style.position = "-100px";
+  parentWrapperDiv.style.border = "1px solid black";
+  parentWrapperDiv.style.background = "white";
+  parentWrapperDiv.style.maxWidth = "300px";
+  if (draggedBlocks.length > 2) {
+    const notifDiv = document.createElement("div");
+    notifDiv.style.display = "inline-block";
+    notifDiv.style.textAlign = "center";
+    notifDiv.style.position = "absolute";
+    notifDiv.style.borderRadius = "50%";
+    notifDiv.style.height = "25px";
+    notifDiv.style.width = "25px";
+    notifDiv.style.top = "-12.5px";
+    notifDiv.style.left = "calc(100% - 12.5px)";
+    notifDiv.style.lineHeight = "25px";
+    notifDiv.style.background = "white";
+    notifDiv.style.border = "1px solid orange";
+    const textDiv = document.createElement("text");
+    textDiv.innerText = `${+draggedBlocks.length - 2}`;
+    notifDiv.appendChild(textDiv);
+    parentWrapperDiv.appendChild(notifDiv);
+  }
+  return parentWrapperDiv;
+};
+
 export const useCanvasActivation = (
   anvilDragStates: AnvilDnDStates,
   layoutId: string,
@@ -35,6 +114,7 @@ export const useCanvasActivation = (
   const {
     activateOverlayWidgetDrop,
     dragDetails,
+    draggedBlocks,
     isDragging,
     isNewWidget,
     layoutElementPositions,
@@ -43,17 +123,27 @@ export const useCanvasActivation = (
   const mainContainerDOMNode = document.getElementById(CANVAS_ART_BOARD);
   const { setDraggingCanvas, setDraggingNewWidget, setDraggingState } =
     useWidgetDragResize();
+  const throttleSetDraggingCanvas = throttle(setDraggingCanvas, 50);
   const draggedWidgetPositions = anvilDragStates.selectedWidgets.map((each) => {
     return layoutElementPositions[each];
   });
+  const currentActivatedLayout = useRef("");
   /**
    * boolean ref that indicates if the mouse position is outside of main canvas while dragging
    * this is being tracked in order to activate/deactivate canvas.
    */
   const isMouseOutOfMainCanvas = useRef(false);
   const mouseOutOfCanvasArtBoard = () => {
-    isMouseOutOfMainCanvas.current = true;
-    setDraggingCanvas();
+    if (!isMouseOutOfMainCanvas.current) {
+      isMouseOutOfMainCanvas.current = true;
+      if (dragPreviewRef.current) {
+        dragPreviewRef.current.style.zIndex = "unset";
+      }
+      if (currentActivatedLayout.current !== "") {
+        currentActivatedLayout.current = "";
+        throttleSetDraggingCanvas();
+      }
+    }
   };
 
   /**
@@ -107,6 +197,7 @@ export const useCanvasActivation = (
       );
     },
   );
+
   /**
    * Callback function to handle mouse move events while dragging state is set.
    * The function uses the mouse position and checks through smallToLargeSortedDroppableLayoutIds
@@ -127,7 +218,7 @@ export const useCanvasActivation = (
         draggedWidgetPositions.find((each) => {
           return checkIfMousePositionIsInsideBlock(e, mainCanvasRect, each);
         });
-      const hoveredCanvas = isMousePositionOutsideOfDraggingWidgets
+      const hoveredLayoutId = isMousePositionOutsideOfDraggingWidgets
         ? dragDetails.dragGroupActualParent
         : smallToLargeSortedDroppableLayoutIds.find((each) => {
             const currentCanvasPositions = layoutElementPositions[each];
@@ -139,10 +230,11 @@ export const useCanvasActivation = (
               );
             }
           });
-      if (dragDetails.draggedOn !== hoveredCanvas) {
-        if (hoveredCanvas) {
+      if (currentActivatedLayout.current !== hoveredLayoutId) {
+        if (hoveredLayoutId) {
           isMouseOutOfMainCanvas.current = false;
-          setDraggingCanvas(hoveredCanvas);
+          currentActivatedLayout.current = hoveredLayoutId;
+          throttleSetDraggingCanvas(hoveredLayoutId);
         } else {
           mouseOutOfCanvasArtBoard();
         }
@@ -164,15 +256,42 @@ export const useCanvasActivation = (
       }
     }
   };
+  const dragPreviewRef = useRef<HTMLDivElement>();
+  const renderDragPreview = (e: MouseEvent) => {
+    if (isDragging) {
+      if (dragPreviewRef.current) {
+        dragPreviewRef.current.style.left = `${e.clientX}px`;
+        dragPreviewRef.current.style.top = `${e.clientY}px`;
+        if (
+          dragPreviewRef.current.style.zIndex !== AnvilCanvasZIndex.activated
+        ) {
+          dragPreviewRef.current.style.zIndex = AnvilCanvasZIndex.activated;
+        }
+      }
+    }
+  };
   useEffect(() => {
     if (isDragging && layoutId === mainCanvasLayoutId) {
+      currentActivatedLayout.current = dragDetails.draggedOn || "";
+      if (!dragPreviewRef.current) {
+        dragPreviewRef.current = isNewWidget
+          ? createNewWidgetPreview(dragDetails)
+          : createSelectedWidgetsPreview(draggedBlocks);
+        document.body.appendChild(dragPreviewRef.current);
+      }
       document?.addEventListener("mousemove", onMouseMoveWhileDragging);
+      document?.addEventListener("mousemove", renderDragPreview);
       document.body.addEventListener("mouseup", onMouseUp, false);
       window.addEventListener("mouseup", onMouseUp, false);
       return () => {
         document?.removeEventListener("mousemove", onMouseMoveWhileDragging);
+        document?.removeEventListener("mousemove", renderDragPreview);
         document.body.removeEventListener("mouseup", onMouseUp);
         window.removeEventListener("mouseup", onMouseUp);
+        if (dragPreviewRef.current) {
+          document.body.removeChild(dragPreviewRef.current);
+          dragPreviewRef.current = undefined;
+        }
       };
     }
   }, [
