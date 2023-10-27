936be8ed8fa5339f72aed048d3bd30c621eaf2cc
Rahul Barwal <rahul.barwal@appsmith.com>
2023-10-27 13:06:38 +0530

    Better checkboxes placement with grid

--- app/client/src/components/editorComponents/PartialImportExport/PartialExportModal/StyledSheet.ts
+++ app/client/src/components/editorComponents/PartialImportExport/PartialExportModal/StyledSheet.ts
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export const CheckboxWrapper = styled.div`
-  display: flex;
-  align-items: start;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   padding: 16px;
   background-color: var(--ads-v2-color-gray-100);
   gap: 16px;
-  flex-wrap: wrap;
   border-radius: 4px;
 `;
