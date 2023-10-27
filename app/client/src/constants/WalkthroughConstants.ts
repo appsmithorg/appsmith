8d68d627f460c1cad1b39d522d2ffd5c43186ade
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-10-27 17:07:52 +0700

    update: constants for walkthrough

--- app/client/src/constants/WalkthroughConstants.ts
+++ app/client/src/constants/WalkthroughConstants.ts
@@ -2,6 +2,8 @@ export const FEATURE_WALKTHROUGH_KEYS = {
   ds_binding: "ab_ds_binding_enabled",
   ds_schema: "ab_ds_schema_enabled",
   binding_widget: "binding_widget",
+  env_walkthrough_a: "ab_env_walkthrough_a_enabled",
+  env_walkthrough_b: "ab_env_walkthrough_b_enabled",
   // Signposting keys
   back_to_canvas: "back_to_canvas",
   add_table_widget: "add_table_widget",
