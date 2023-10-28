46eaed9b7cda391f2ba05c43793f4e00fd16fc23
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-10-28 13:57:17 +0700

    update: remove redundant code

--- app/client/src/ee/pages/Applications/WorkspaceMenu.tsx
+++ app/client/src/ee/pages/Applications/WorkspaceMenu.tsx
@@ -1,6 +1,3 @@
 export * from "ce/pages/Applications/WorkspaceMenu";
 import { default as CE_WorkspaceMenu } from "ce/pages/Applications/WorkspaceMenu";
 export default CE_WorkspaceMenu;
-export const EEWorkspaceMenuItems = ({}: { workspaceId: string }) => {
-  return null;
-};
