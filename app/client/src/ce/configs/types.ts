c314269a460b011f7c0b0acaa830cb06b4d2e743
Ayush Pahwa <ayushpahwa96@gmail.com>
2023-10-30 22:44:14 +0700

    update: type definitions for environment

--- app/client/src/ce/configs/types.ts
+++ app/client/src/ce/configs/types.ts
@@ -65,11 +65,18 @@ export interface AppsmithUIConfigs {
   customerPortalUrl: string;
 }
 
+export interface DatasourceMeta {
+  configuredDatasources: number;
+  totalDatasources: number;
+}
+
 // Type for one environment
 export interface EnvironmentType {
   id: string;
   name: string;
   workspaceId: string;
   isDefault?: boolean;
+  isLocked: boolean; // Whether the environment is locked (disables editing and deleting of the env)
   userPermissions?: string[];
+  datasourceMeta?: DatasourceMeta;
 }
