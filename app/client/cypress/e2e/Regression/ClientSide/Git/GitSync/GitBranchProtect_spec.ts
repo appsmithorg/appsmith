b59a0a6a66cf79247b6d2e135989bdb8c5a09754
brayn003 <rudra@appsmith.com>
2023-11-01 14:40:31 +0530

    fix: fixing test case

--- app/client/cypress/e2e/Regression/ClientSide/Git/GitSync/GitBranchProtect_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Git/GitSync/GitBranchProtect_spec.ts
@@ -1,75 +1,81 @@
 import { featureFlagIntercept } from "../../../../../support/Objects/FeatureFlags";
 import * as _ from "../../../../../support/Objects/ObjectsCore";
 
-let wsName: string;
-let appName: string;
+let guid: any;
 let repoName1: any;
 let repoName2: any;
 
-describe("Git Branch Protectioon", function () {
-  before(() => {
+describe("Git Branch Protection", function () {
+  it("Issue 28056 - 1 : Check if protection is not enabled when feature flag is disabled", function () {
     _.agHelper.GenerateUUID();
     cy.get("@guid").then((uid) => {
-      wsName = "GitBranchProtect" + uid;
-      appName = "GitBranchProtect" + uid;
+      guid = uid;
+      const wsName = "GitBranchProtect-1" + uid;
+      const appName = "GitBranchProtect-1" + uid;
       _.homePage.CreateNewWorkspace(wsName, true);
       _.homePage.CreateAppInWorkspace(wsName, appName);
-    });
-  });
-
-  it("Issue 28056 - 1 : Check if protection is not enabled when feature flag is disabled", function () {
-    featureFlagIntercept({
-      release_git_connect_v2_enabled: true,
-      release_git_branch_protection_enabled: false,
-    });
-    cy.wait(1000);
-    _.gitSync.CreateNConnectToGitV2();
-    cy.get("@gitRepoName").then((repName) => {
-      repoName1 = repName;
-      _.agHelper.AssertElementExist(_.entityExplorer._entityExplorerWrapper);
-      _.agHelper.AssertElementExist(_.propPane._propertyPaneSidebar);
-      _.agHelper.AssertElementEnabledDisabled(
-        _.gitSync._bottomBarCommit,
-        0,
-        false,
-      );
+      featureFlagIntercept({
+        release_git_connect_v2_enabled: true,
+        release_git_branch_protection_enabled: false,
+      });
+      cy.wait(1000);
+      _.gitSync.CreateNConnectToGitV2();
+      cy.get("@gitRepoName").then((repName) => {
+        repoName1 = repName;
+        _.agHelper.AssertElementExist(_.entityExplorer._entityExplorerWrapper);
+        _.agHelper.AssertElementExist(_.propPane._propertyPaneSidebar);
+        _.agHelper.AssertElementEnabledDisabled(
+          _.gitSync._bottomBarCommit,
+          0,
+          false,
+        );
+      });
     });
   });
 
   it("Issue 28056 - 2 : Check if protection is enabled when feature flag is enabled", function () {
-    featureFlagIntercept({
-      release_git_connect_v2_enabled: true,
-      release_git_branch_protection_enabled: true,
-    });
-    cy.wait(1000);
+    _.agHelper.GenerateUUID();
+    cy.get("@guid").then((uid) => {
+      guid = uid;
+      const wsName = "GitBranchProtect-2" + uid;
+      const appName = "GitBranchProtect-2" + uid;
+      _.homePage.CreateNewWorkspace(wsName, true);
+      _.homePage.CreateAppInWorkspace(wsName, appName);
+      featureFlagIntercept({
+        release_git_connect_v2_enabled: true,
+        release_git_branch_protection_enabled: true,
+      });
+      cy.wait(1000);
 
-    cy.intercept({
-      method: "POST",
-      url: "/api/v1/git/branvh/app/**/protected",
-    }).as("gitProtectApi");
+      cy.intercept({
+        method: "POST",
+        url: "/api/v1/git/branvh/app/**/protected",
+      }).as("gitProtectApi");
 
-    _.gitSync.CreateNConnectToGitV2();
-    cy.get("@gitRepoName").then((repName) => {
-      repoName2 = repName;
+      _.gitSync.CreateNConnectToGitV2();
+      cy.get("@gitRepoName").then((repName) => {
+        repoName2 = repName;
 
-      cy.wait("@gitProtectApi").then((res1) => {
-        expect(res1.response).to.have.property("statusCode", 200);
-        _.agHelper.GetNClick(_.locators._dialogCloseButton);
+        cy.wait("@gitProtectApi").then((res1) => {
+          expect(res1.response).to.have.property("statusCode", 200);
+          _.agHelper.GetNClick(_.locators._dialogCloseButton);
 
-        _.agHelper.AssertElementAbsence(
-          _.entityExplorer._entityExplorerWrapper,
-        );
-        _.agHelper.AssertElementAbsence(_.propPane._propertyPaneSidebar);
-        _.agHelper.AssertElementEnabledDisabled(
-          _.gitSync._bottomBarCommit,
-          0,
-          true,
-        );
+          _.agHelper.AssertElementAbsence(
+            _.entityExplorer._entityExplorerWrapper,
+          );
+          _.agHelper.AssertElementAbsence(_.propPane._propertyPaneSidebar);
+          _.agHelper.AssertElementEnabledDisabled(
+            _.gitSync._bottomBarCommit,
+            0,
+            true,
+          );
+        });
       });
     });
   });
 
   after(() => {
     _.gitSync.DeleteTestGithubRepo(repoName1);
+    _.gitSync.DeleteTestGithubRepo(repoName2);
   });
 });
