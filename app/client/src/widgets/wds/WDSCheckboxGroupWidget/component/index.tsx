55e4f7dac041f974305e3a540dd405343fa1f3cd
Dhruvik Neharia <dhruvik@appsmith.com>
2023-11-01 05:44:36 +0530

    feat: add wds checkbox group widget

---
+++ app/client/src/widgets/wds/WDSCheckboxGroupWidget/component/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { CheckboxGroup, Checkbox } from "@design-system/widgets";
+import type { CheckboxGroupComponentProps } from "./types";
+import type { OptionProps } from "../widget/types";
+
+export const CheckboxGroupComponent = (props: CheckboxGroupComponentProps) => {
+  return (
+    <CheckboxGroup
+      isDisabled={props.isDisabled}
+      isRequired={props.isRequired}
+      label={props.labelText}
+      onChange={(value) => {
+        props.onChange(value);
+      }}
+      orientation={props.orientation}
+      value={props.selectedValues}
+    >
+      {props.options.map((option: OptionProps, index: number) => {
+        return (
+          <Checkbox
+            key={`${props.widgetId}-checkbox-${index}`}
+            labelPosition={props.labelPosition}
+            validationState={props.isValid ? "valid" : "invalid"}
+            value={option.value}
+          >
+            {option.label}
+          </Checkbox>
+        );
+      })}
+    </CheckboxGroup>
+  );
+};
---
+++ app/client/src/widgets/wds/WDSCheckboxGroupWidget/config/index.ts
@@ -0,0 +1,7 @@
+export * from "./propertyPaneConfig";
+export { metaConfig } from "./metaConfig";
+export { settersConfig } from "./settersConfig";
+export { defaultsConfig } from "./defaultsConfig";
+export { featuresConfig } from "./featuresConfig";
+export { autocompleteConfig } from "./autocompleteConfig";
+export { anvilConfig } from "./anvilConfig";
