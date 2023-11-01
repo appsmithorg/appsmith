c2bee0e2e7601461896fa859dc56eba13ea80aa4
Sumit Kumar <sumit@appsmith.com>
2023-11-01 19:02:40 +0530

    replace progress bar with spinner as recommended by ADS team

--- app/client/src/pages/Editor/DatasourceInfo/RenderInterimDataState.tsx
+++ app/client/src/pages/Editor/DatasourceInfo/RenderInterimDataState.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import EmptyTableSVG from "assets/images/empty-table-in-display-preview.svg";
-import { Text } from "design-system";
+import { Spinner, Text } from "design-system";
 import {
   EMPTY_TABLE_TITLE_TEXT,
   EMPTY_TABLE_MESSAGE_TEXT,
@@ -11,41 +11,12 @@ import {
   ERR_FETCHING_DATASOURCE_PREVIEW_DATA,
 } from "@appsmith/constants/messages";
 import { MessageWrapper, SchemaStateMessageWrapper } from "./SchemaViewModeCSS";
-import styled, { keyframes } from "styled-components";
 
 type InterimState = "LOADING" | "NODATA" | "FAILED";
 
 interface RenderInterimDataStateProps {
   state: InterimState;
 }
-
-export const indeterminateProgressBarAnimation = keyframes`
-  0% {
-    background-position: -50px 0;
-  }
-  100% {
-    background-position: 0px 0px;
-  }
-`;
-
-export const IndeterminateProgressBarDiv = styled.div`
-  border: 2px solid var(--ads-v2-colors-response-label-default-fg);
-  border-radius: 0.5em;
-  height: 12px;
-  margin-bottom: 2em;
-  width: 200px;
-  background-image: repeating-linear-gradient(
-    -45deg,
-    var(--ads-v2-colors-response-surface-default-border),
-    var(--ads-v2-colors-response-surface-default-border) 9px,
-    var(--ads-v2-colors-response-surface-default-bg) 12px,
-    var(--ads-v2-colors-response-surface-default-bg) 15px
-  );
-  -webkit-animation: ${indeterminateProgressBarAnimation} 1s linear infinite;
-  -moz-animation: ${indeterminateProgressBarAnimation} 1s linear infinite;
-  animation: ${indeterminateProgressBarAnimation} 1.5s linear infinite;
-  background-size: 200% 100%;
-`;
 
 const RenderInterimDataState = ({ state }: RenderInterimDataStateProps) => {
   return (
@@ -72,8 +43,8 @@ const RenderInterimDataState = ({ state }: RenderInterimDataStateProps) => {
           </Text>
         ) : state === "LOADING" ? (
           <>
-            {/* Show progress bar */}
-            <IndeterminateProgressBarDiv />
+            {/* Show spinner */}
+            <Spinner size="md" />
             {/* Show title */}
             <Text style={{ fontWeight: "bold" }}>
               {createMessage(LOADING_RECORDS_TITLE_TEXT)}
