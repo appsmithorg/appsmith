9bcae54d63bf21f130d5821f80f05e9d7a5b2422
balajisoundar <balaji@appsmith.com>
2023-11-02 12:48:52 +0530

    fix: null check before accessing dsl of page in getCurrentPageWidgets selector

--- app/client/src/ce/selectors/entitiesSelector.ts
+++ app/client/src/ce/selectors/entitiesSelector.ts
@@ -684,7 +684,9 @@ export const getCurrentPageWidgets = createSelector(
   getPageWidgets,
   getCurrentPageId,
   (widgetsByPage, currentPageId) =>
-    currentPageId ? widgetsByPage[currentPageId].dsl : {},
+    currentPageId && widgetsByPage[currentPageId]
+      ? widgetsByPage[currentPageId].dsl
+      : {},
 );
 
 export const getParentModalId = (
