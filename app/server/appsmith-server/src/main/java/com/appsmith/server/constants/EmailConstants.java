aa088c71acc46637240f1426e5cac6ca09a2ec36
Nilesh Sarupriya <nilesh@appsmith.com>
2023-11-02 10:54:48 +0530

    fix: subject and email templates (#2791)
    ## Description
    > Fix email subjects and templates
    > Related CE PR: https://github.com/appsmithorg/appsmith/pull/28531
    
    #### PR fixes following issue(s)
    Fixes # (issue number)
    > if no issue exists, please create an issue and ask the maintainers
    about this first
    >
    >
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Testing
    >
    #### How Has This Been Tested?
    - [x] Manual
    
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    ---------
    
    Co-authored-by: Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/constants/EmailConstants.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/constants/EmailConstants.java
@@ -8,13 +8,13 @@ public class EmailConstants extends EmailConstantsCE {
     public static final String GROUP_NAME = "groupName";
     public static final String ROLE_NAME = "roleName";
     public static final String INVITER_APPLICATION_NAME = "inviterApplicationName";
-    public static final String APPLICATION_EMAIL_SUBJECT_FOR_NEW_USER = "You're invited to the app %s.";
-    public static final String APPLICATION_EMAIL_SUBJECT_FOR_EXISTING_USER = "You're invited to the app %s.";
-    public static final String PRIMARY_LINK_TEXT_INSTANCE_INVITE = "Join your instance";
-    public static final String PRIMARY_LINK_TEXT_APPLICATION_REDIRECTION = "See the app";
-    public static final String PRIMARY_LINK_TEXT_USER_SIGNUP_APPLICATION_INVITE = "Get access";
+    public static final String APPLICATION_EMAIL_SUBJECT_FOR_NEW_USER = "You're invited to the app %s. \uD83E\uDD73";
+    public static final String PRIMARY_LINK_TEXT_JOIN_YOUR_INSTANCE = "Join your instance";
+    public static final String PRIMARY_LINK_TEXT_GO_TO_YOUR_INSTANCE = "Go to your instance";
+    public static final String PRIMARY_LINK_TEXT_GET_ACCESS = "Get access";
     public static final String INVITE_TO_INSTANCE_EMAIL_SUBJECT = "You’re invited to %s.";
-    public static final String INVITE_TO_INSTANCE_EMAIL_SUBJECT_VIA_GROUP = "You’re invited to the user group %s.";
+    public static final String INVITE_TO_INSTANCE_EMAIL_SUBJECT_VIA_GROUP =
+            "You’re invited to the user group %s. \uD83E\uDD73";
     public static final String INVITE_TO_INSTANCE_EMAIL_TEMPLATE = "email/ee/inviteInstanceTemplateViaRole.html";
     public static final String INVITE_TO_INSTANCE_ADMIN_EMAIL_TEMPLATE = "email/ee/instanceAdminInviteTemplate.html";
     public static final String INVITE_TO_INSTANCE_EMAIL_TEMPLATE_VIA_GROUPS =
@@ -30,4 +30,8 @@ public class EmailConstants extends EmailConstantsCE {
     public static final String BRAND_PRIMARY_COLOR = "brandPrimaryColor";
     public static final String BRAND_BACKGROUND_COLOR = "brandBackgroundColor";
     public static final String BRAND_FONT_COLOR = "brandFontColor";
+    public static final String PRIMARY_LINK_TEXT_INVITE_TO_INSTANCE_EE = "Join your Instance";
+    public static final String INSTANCE_ADMIN_INVITE_EMAIL_SUBJECT_EE = "You're invited to %s. \uD83E\uDD73";
+    public static final String INVITE_TO_WORKSPACE_EMAIL_SUBJECT_EE =
+            "You’re invited to the workspace %s. \uD83E\uDD73";
 }
