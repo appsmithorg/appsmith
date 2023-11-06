036dbcedda49fcf09acb9a5c54f6b3b914d957d2
Aishwarya UR <aishwarya@appsmith.com>
2023-11-06 12:17:48 +0530

    widge properties more cases

--- app/client/cypress/e2e/Regression/ClientSide/SetProperty/SetOptions_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/SetProperty/SetOptions_Spec.ts
@@ -7,6 +7,8 @@ import {
   draggableWidgets,
   deployMode,
   debuggerHelper,
+  apiPage,
+  dataManager,
 } from "../../../../support/Objects/ObjectsCore";
 
 describe("Widget Property Setters - Part III - Tc #2409 - Validates SetOptions", () => {
@@ -270,6 +272,113 @@ describe("Widget Property Setters - Part III - Tc #2409 - Validates SetOptions",
           .to.include("monday")
           .and.to.include("weekday")
           .and.not.to.include("1");
+      });
+    deployMode.NavigateBacktoEditor();
+  });
+
+  it("5. Update 'setOptions' property - during onPage load - via CallBack", () => {
+    apiPage.CreateAndFillApi(
+      dataManager.dsValues[dataManager.defaultEnviorment].mockApiUrl,
+    );
+    entityExplorer.SelectEntityByName("JSObject1");
+    jsEditor.EditJSObj(`export default {
+      async myFunc1 () {
+        await Api1.run(() => {Select1.setOptions([
+          {
+            "label": "Api callback",
+            "value": "Api callback"
+          }
+        ])}, () => {showAlert('unable to run API')});
+      }
+    }`);
+    jsEditor.EnableDisableAsyncFuncSettings("myFunc1", true, false); //for on page load execution, since sync function is updated to async
+    deployMode.DeployApp();
+    agHelper
+      .GetText(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) +
+          " " +
+          locators._input,
+        "val",
+      )
+      .then((val) => {
+        expect(val).to.include("Api callback");
+      });
+    deployMode.NavigateBacktoEditor();
+  });
+
+  it("6. Update 'setOptions' property - during onPage load - via contcat", () => {
+    entityExplorer.SelectEntityByName("Select1");
+    propPane.EnterJSContext(
+      "Source Data",
+      `[
+        {
+          "name": "monday",
+          "code": "Weekday"
+        }
+      ]`,
+    ); // By JS function
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.SELECT, 300, 400);
+    entityExplorer.DragDropWidgetNVerify(draggableWidgets.SELECT, 300, 500);
+    entityExplorer.SelectEntityByName("JSObject1");
+    jsEditor.EditJSObj(`export default {
+      myFunc1 () {
+        Select3.setOptions(Select1.options.concat(Select2.options));
+      }
+    }`);
+    jsEditor.EnableDisableAsyncFuncSettings("myFunc1", true, false); //for on page load execution, since sync function is updated to async
+    entityExplorer.SelectEntityByName("Input1");
+    propPane.UpdatePropertyFieldValue("Default value", "{{Select3.options}}");
+    deployMode.DeployApp();
+    agHelper
+      .GetText(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) +
+          " " +
+          locators._input,
+        "val",
+      )
+      .then((val) => {
+        expect(val)
+          .to.include("monday")
+          .and.to.include("Blue")
+          .and.to.include("Red")
+          .and.to.include("Green");
+      });
+    deployMode.NavigateBacktoEditor();
+  });
+
+  it("7. Update 'setOptions' property - via SetTimeout framework function", () => {
+    entityExplorer.SelectEntityByName("Input1");
+    propPane.UpdatePropertyFieldValue("Default value", "{{Select1.options}}");
+    entityExplorer.SelectEntityByName("JSObject1");
+    jsEditor.EditJSObj(`export default {
+      myFun1() {
+        let localValue = {"label": 'local label', "value": 'local value'};
+        setTimeout(() => {Select1.setOptions(localValue)}, 1000);
+      }
+    }`);
+    jsEditor.EnableDisableAsyncFuncSettings("myFun1", false, false); //for on page load execution
+    deployMode.DeployApp();
+    agHelper
+      .GetText(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) +
+          " " +
+          locators._input,
+        "val",
+      )
+      .then((val) => {
+        expect(val).to.contain("monday");
+      });
+    agHelper.ClickButton("Submit");
+    agHelper.Sleep(); //settimeout timer!
+    agHelper
+      .GetText(
+        locators._widgetInDeployed(draggableWidgets.INPUT_V2) +
+          " " +
+          locators._input,
+        "val",
+      )
+      .then((val) => {
+        expect(val).to.include("local label").and.to.include("local value");
       });
     deployMode.NavigateBacktoEditor();
   });
