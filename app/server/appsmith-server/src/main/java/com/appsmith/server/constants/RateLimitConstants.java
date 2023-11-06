89facbeb10663457711c9909a68cd2b89b8fb913
Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>
2023-11-06 15:17:58 +0530

    rename variables for rate limit durations

--- app/server/appsmith-server/src/main/java/com/appsmith/server/constants/RateLimitConstants.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/constants/RateLimitConstants.java
@@ -9,16 +9,22 @@ public class RateLimitConstants {
             "Your account is suspended for 24 hours. Please reset your password to continue";
     public static final String BUCKET_KEY_FOR_LOGIN_API = "login";
     public static final String BUCKET_KEY_FOR_TEST_DATASOURCE_API = "test_datasource_or_execute_query";
-    public static final Duration DURATION_5_SECONDS = Duration.ofSeconds(5);
-    public static final Duration DURATION_1_DAY = Duration.ofDays(1);
-    public static final Duration DURATION_1_NANOSECOND = Duration.ofNanos(1);
-    public static final RateLimit DEFAULT_PRESET_RATE_LIMIT_TEST_DATASOURCE_API =
-            RateLimit.builder().refillDuration(DURATION_5_SECONDS).limit(3).build();
+    public static final Duration DEFAULT_PRESET_REFILL_DURATION_DATASOURCE_API = Duration.ofSeconds(5);
+    public static final Duration DEFAULT_PRESET_REFILL_DURATION_LOGIN_API = Duration.ofDays(1);
+    public static final Duration DEFAULT_MIN_REFILL_DURATION_LOGIN_API = Duration.ofNanos(1);
+    public static final RateLimit DEFAULT_PRESET_RATE_LIMIT_TEST_DATASOURCE_API = RateLimit.builder()
+            .refillDuration(DEFAULT_PRESET_REFILL_DURATION_DATASOURCE_API)
+            .limit(3)
+            .build();
 
-    public static final RateLimit DEFAULT_PRESET_RATE_LIMIT_LOGIN_API =
-            RateLimit.builder().refillDuration(DURATION_1_DAY).limit(5).build();
+    public static final RateLimit DEFAULT_PRESET_RATE_LIMIT_LOGIN_API = RateLimit.builder()
+            .refillDuration(DEFAULT_PRESET_REFILL_DURATION_LOGIN_API)
+            .limit(5)
+            .build();
 
     // Bucket4j's highest supported rate is 1 token/nanosecond.
-    public static final RateLimit DEFAULT_MAX_RATE_LIMIT_LOGIN_API =
-            RateLimit.builder().refillDuration(DURATION_1_NANOSECOND).limit(1).build();
+    public static final RateLimit DEFAULT_MAX_RATE_LIMIT_LOGIN_API = RateLimit.builder()
+            .refillDuration(DEFAULT_MIN_REFILL_DURATION_LOGIN_API)
+            .limit(1)
+            .build();
 }
