a9df740f0a08ee6ef2b6518078ab181ca9885d39
ashit-rath <ashit@appsmith.com>
2023-11-06 08:07:19 +0530

    fix: module editor default redirection (#2805)
    ## Description
    Adds support to other public action type redirection
    
    #### PR fixes following issue(s)
    Fixes appsmithorg/appsmith#28570
    
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    - 
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] JUnit
    - [x] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/ee/pages/Editor/ModuleEditor/ModuleEditorDefaultRedirect.tsx
+++ app/client/src/ee/pages/Editor/ModuleEditor/ModuleEditorDefaultRedirect.tsx
@@ -1,22 +1,67 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Redirect, matchPath, useLocation } from "react-router";
 
 import { MODULE_EDITOR_PATH } from "@appsmith/constants/routes/packageRoutes";
+import { useSelector } from "react-redux";
+import { getAction, getPlugins } from "@appsmith/selectors/entitiesSelector";
 import type { Module } from "@appsmith/constants/ModuleConstants";
+import type { Action } from "entities/Action";
+import { PluginType } from "entities/Action";
+import {
+  apiEditorIdURL,
+  queryEditorIdURL,
+  saasEditorApiIdURL,
+} from "@appsmith/RouteBuilder";
+import { keyBy } from "lodash";
+import type { Plugin } from "api/PluginApi";
 
 interface ModuleEditorDefaultRedirectProps {
   module: Module;
 }
+
+interface GetUrlProps {
+  id: string;
+  plugin: Plugin;
+}
+
+const getURL = ({ id, plugin }: GetUrlProps) => {
+  if (!!plugin && plugin.type === PluginType.SAAS) {
+    return saasEditorApiIdURL({
+      pluginPackageName: plugin.packageName,
+      apiId: id,
+    });
+  } else if (
+    plugin.type === PluginType.DB ||
+    plugin.type === PluginType.REMOTE
+  ) {
+    return queryEditorIdURL({
+      queryId: id,
+    });
+  } else {
+    return apiEditorIdURL({ apiId: id });
+  }
+};
 
 function ModuleEditorDefaultRedirect({
   module,
 }: ModuleEditorDefaultRedirectProps) {
   const { pathname } = useLocation();
   const { isExact } = matchPath(pathname, MODULE_EDITOR_PATH) || {};
+  const action: Action | undefined = useSelector((state) =>
+    getAction(state, module?.publicEntityId),
+  );
+  const plugins = useSelector(getPlugins);
+  const pluginGroups = useMemo(() => keyBy(plugins, "id"), [plugins]);
 
-  const defaultRedirectUrl = `${pathname}/queries/${module.publicEntityId}`;
+  if (!action) return null;
 
-  if (!isExact) return null;
+  const plugin = pluginGroups[action?.pluginId];
+
+  if (!isExact || !plugin) return null;
+  const defaultRedirectUrl = getURL({
+    id: action.id,
+    plugin: pluginGroups[action.pluginId],
+  });
 
   return <Redirect to={defaultRedirectUrl} />;
 }
