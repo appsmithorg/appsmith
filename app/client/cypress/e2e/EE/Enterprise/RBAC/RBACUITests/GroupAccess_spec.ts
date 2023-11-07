de3716f367722046c5e4d49058e48205b8490aed
sharanya-appsmith <135708039+sharanya-appsmith@users.noreply.github.com>
2023-11-07 17:00:18 +0530

    test: Cypress - pageaccess spec fix (#2823)

--- app/client/cypress/e2e/EE/Enterprise/RBAC/RBACUITests/GroupAccess_spec.ts
+++ app/client/cypress/e2e/EE/Enterprise/RBAC/RBACUITests/GroupAccess_spec.ts
@@ -106,10 +106,10 @@ describe.skip("Create group, check if users in group has group roles accessess",
     cy.wait(5000);
     homePage.SearchAndOpenApp(appName);
     entityExplorer.SelectEntityByName("Page1", "Pages");
-    entityExplorer.DragNDropWidget("checkboxwidget");
+    entityExplorer.DragNDropWidget("checkboxwidget", 300, 100, "", "", true);
     agHelper.AssertElementAbsence(locators._saveStatusError);
     entityExplorer.SelectEntityByName(pageName, "Pages");
-    entityExplorer.DragNDropWidget("checkboxwidget");
+    entityExplorer.DragNDropWidget("checkboxwidget", 300, 100, "", "", true);
     agHelper.AssertElementExist(locators._saveStatusError);
     entityExplorer.SelectEntityByName(queryName, "Queries/JS");
     agHelper.GetNClick(entityExplorer._contextMenu(queryName), 0, true, 500);
