f425d2d7d295429c427be465f99956caf4c85694
Dipyaman Biswas <dipyaman@appsmith.com>
2023-11-08 17:57:02 +0530

    fix: unskip usage pulse Cypress Test (#2830)
    ## Description
    
    Unskip Usage Pulse Test case.
    
    #### PR fixes following issue(s)
    Fixes #2602 
    
    
    
    #### Type of change
    > Please delete options that are not relevant.
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] JUnit
    - [ ] Jest
    - [x] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/cypress/e2e/EE/Enterprise/LicenseAndBilling/UsagePulse_spec.js
+++ app/client/cypress/e2e/EE/Enterprise/LicenseAndBilling/UsagePulse_spec.js
@@ -9,9 +9,7 @@ describe("excludeForAirgap", "Usage pulse", function () {
   beforeEach(() => {
     cy.intercept("POST", "/api/v1/usage-pulse").as("usagePulse");
   });
-  // skipping test to unblock CI
-  // To do: unskip once fixed by Dipyaman
-  it.skip("1. Should send usage pulse", function () {
+  it("1. Should send usage pulse", function () {
     agHelper.VisitNAssert("/applications", "getReleaseItems");
     agHelper.Sleep(2000);
     cy.get(homePageLocators.applicationCard).first().trigger("mouseover");
