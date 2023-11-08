38b2117cea1c07c7555d3dc7521d17c81fd403be
Ankit Srivastava <ankit@appsmith.com>
2023-11-08 11:17:53 +0530

    updated logic

--- app/client/cypress/support/Objects/FeatureFlags.ts
+++ app/client/cypress/support/Objects/FeatureFlags.ts
@@ -1,39 +1,37 @@
 export const featureFlagIntercept = (
   flags: Record<string, boolean>,
-  shouldIntercept = true,
+  reload = true,
 ) => {
 
-  if (shouldIntercept) {
-    cy.intercept("GET", "/api/v1/users/features").as("getFeatures");
-  }
-
-  cy.reload();
-  cy.wait(2000);
-
-  if (shouldIntercept) {
-    cy.wait("@getFeatures").then((interception) => {
-      if (interception && interception.response) {
-        const originalResponse = interception?.response.body;
-        let modifiedResponse: any = {}
-        Object.keys(originalResponse.data).forEach(flag => {
-          if (flag.startsWith('license_')) {
-            modifiedResponse[flag] = originalResponse.data[flag]
+  cy.intercept(
+    {
+      method: "GET",
+      url: "/api/v1/users/features",
+    },
+    (req) => {
+      req.reply((res) => {
+        if (res) {
+          const originalResponse = res.body;
+          let modifiedResponse: any = {}
+          Object.keys(originalResponse.data).forEach(flag => {
+            if (flag.startsWith('license_')) {
+              modifiedResponse[flag] = originalResponse.data[flag]
+            }
+          })
+          modifiedResponse = {
+            ...modifiedResponse,
+            ...flags
           }
-        })
-        modifiedResponse = {
-          ...modifiedResponse,
-          ...flags
+          res.send({
+            responseMeta: {
+              status: 200,
+              success: true,
+            },
+            data: { ...modifiedResponse },
+            errorDisplay: "",
+          });
         }
-        const response = {
-          responseMeta: {
-            status: 200,
-            success: true,
-          },
-          data: { ...modifiedResponse },
-          errorDisplay: "",
-        };
-        cy.intercept("GET", "/api/v1/users/features", response);
-      }
-    })
-  }
+      });
+    },
+  ).as("getFeatures");
 };
