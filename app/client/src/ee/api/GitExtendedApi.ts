f062557382d2d5c67aee52c8cd34d5ce7a4b568e
Rudraprasad Das <rudra@appsmith.com>
2023-11-09 12:04:55 +0530

    fix: adding feature to change the default branch (#2816)

--- app/client/src/ee/api/GitExtendedApi.ts
+++ app/client/src/ee/api/GitExtendedApi.ts
@@ -1,1 +1,17 @@
 export * from "ce/api/GitExtendedApi";
+import Api from "api/Api";
+
+class GitExtendedApi extends Api {
+  static baseURL = `/v1/git`;
+
+  static async updateDefaultBranch(applicationId: string, branchName: string) {
+    return Api.patch(
+      `${GitExtendedApi.baseURL}/branch/app/${applicationId}/default`,
+      {},
+      undefined,
+      { params: { branchName } },
+    );
+  }
+}
+
+export default GitExtendedApi;
