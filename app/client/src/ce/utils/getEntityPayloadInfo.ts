2bb223bd250952b4239448092d899f637c33692d
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2023-11-14 10:25:59 +0530

    fix type

--- app/client/src/ce/utils/getEntityPayloadInfo.ts
+++ app/client/src/ce/utils/getEntityPayloadInfo.ts
@@ -1,43 +1,43 @@
 import type {
   ActionEntityConfig,
+  EntityConfig,
   JSActionEntityConfig,
 } from "@appsmith/entities/DataTree/types";
 import {
-  ENTITY_TYPE_VALUE,
-  type WidgetEntity,
   type WidgetEntityConfig,
+  ENTITY_TYPE_VALUE,
 } from "@appsmith/entities/DataTree/types";
 import type { PluginType } from "entities/Action";
 
 export const getEntityPayloadInfo: Record<
   string,
-  (
-    entity: any,
-    entityConfig: any,
-  ) => {
+  (entityConfig: EntityConfig) => {
     iconId: string;
     id: string;
     pluginType?: PluginType;
   }
 > = {
-  [ENTITY_TYPE_VALUE.WIDGET]: (
-    entity: WidgetEntity,
-    entityConfig: WidgetEntityConfig,
-  ) => ({
-    iconId: entityConfig.widgetId,
-    id: entityConfig.widgetId,
-  }),
-  [ENTITY_TYPE_VALUE.JSACTION]: (
-    entity,
-    entityConfig: JSActionEntityConfig,
-  ) => ({
-    iconId: entityConfig.actionId,
-    id: entityConfig.actionId,
-    pluginType: entityConfig.pluginType,
-  }),
-  [ENTITY_TYPE_VALUE.ACTION]: (entity, entityConfig: ActionEntityConfig) => ({
-    iconId: entityConfig.pluginId,
-    id: entityConfig.actionId,
-    pluginType: entityConfig.pluginType,
-  }),
+  [ENTITY_TYPE_VALUE.WIDGET]: (entityConfig) => {
+    const config = entityConfig as WidgetEntityConfig;
+    return {
+      iconId: config.widgetId,
+      id: config.widgetId,
+    };
+  },
+  [ENTITY_TYPE_VALUE.JSACTION]: (entityConfig) => {
+    const config = entityConfig as JSActionEntityConfig;
+    return {
+      iconId: config.actionId,
+      id: config.actionId,
+      pluginType: config.pluginType,
+    };
+  },
+  [ENTITY_TYPE_VALUE.ACTION]: (entityConfig) => {
+    const config = entityConfig as ActionEntityConfig;
+    return {
+      iconId: config.pluginId,
+      id: config.actionId,
+      pluginType: config.pluginType,
+    };
+  },
 };
