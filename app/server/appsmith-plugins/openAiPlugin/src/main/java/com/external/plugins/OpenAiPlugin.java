9ef5578bfa399e19d11d2c42e62ff9241c0e36bb
Nirmal Sarswat <nirmal@appsmith.com>
2023-11-15 14:16:56 +0530

    adding logs

--- app/server/appsmith-plugins/openAiPlugin/src/main/java/com/external/plugins/OpenAiPlugin.java
+++ app/server/appsmith-plugins/openAiPlugin/src/main/java/com/external/plugins/OpenAiPlugin.java
@@ -96,6 +96,8 @@ public class OpenAiPlugin extends BasePlugin {
             URI uri = openAICommand.createExecutionUri();
             HttpMethod httpMethod = openAICommand.getExecutionMethod();
 
+            log.info("OpenAI request object DTO is {}", gson.toJson(openAIRequestDTO));
+
             ActionExecutionRequest actionExecutionRequest =
                     RequestCaptureFilter.populateRequestFields(actionConfiguration, uri, insertedParams, objectMapper);
 
@@ -107,6 +109,7 @@ public class OpenAiPlugin extends BasePlugin {
                     .flatMap(responseEntity -> {
                         HttpStatusCode statusCode = responseEntity.getStatusCode();
                         HttpHeaders headers = responseEntity.getHeaders();
+                        log.info("OpenAI response object DTO is {}", new String(responseEntity.getBody()));
 
                         ActionExecutionResult actionExecutionResult = new ActionExecutionResult();
                         actionExecutionResult.setRequest(actionExecutionRequest);
