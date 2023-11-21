57a508fe064ae93b0d0eecf89c394c1ab4492c01
Anagh Hegde <anagh@appsmith.com>
2023-11-21 13:57:16 +0530

    chore: remove the redirection for mockdb to localhost

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration035RemoveMockDbEndPointInDatasourceInSelfHostedInstance.java
@@ -0,0 +1,79 @@
+package com.appsmith.server.migrations.db.ce;
+
+import com.appsmith.external.models.DatasourceStorage;
+import com.appsmith.server.configurations.CommonConfig;
+import com.appsmith.server.solutions.EnvManager;
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.Map;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Update.update;
+
+@Slf4j
+@ChangeUnit(order = "035", id = "remove-mockdb-endpoint-in-datasource-self-hosted-instance", author = " ")
+public class Migration035RemoveMockDbEndPointInDatasourceInSelfHostedInstance {
+
+    private final MongoTemplate mongoTemplate;
+
+    private final CommonConfig commonConfig;
+
+    private final EnvManager envManager;
+
+    public Migration035RemoveMockDbEndPointInDatasourceInSelfHostedInstance(
+            MongoTemplate mongoTemplate, CommonConfig commonConfig, EnvManager envManager) {
+        this.mongoTemplate = mongoTemplate;
+        this.commonConfig = commonConfig;
+        this.envManager = envManager;
+    }
+
+    @RollbackExecution
+    public void rollbackExecution() {}
+
+    @Execution
+    public void removeMockDbEndpointInDatasource() {
+        // This migration is applicable for self-hosted instances only.
+        // The end point used in templates is redirected to internal postgres db
+        // We are now switching to RDS and hence want to remove this endpoint from the datasource and replace it with
+        // localhost, so that the existing queries do not break. And the data stored in the db by users is not lost
+
+        // APPSMITH_ENABLE_EMBEDDED_DB && isCloudHosting
+        Map<String, String> envVariable = getEnvVariable();
+        if (Boolean.FALSE.equals(commonConfig.isCloudHosting())
+                && envVariable.containsKey("APPSMITH_ENABLE_EMBEDDED_DB")) {
+
+            Query query = query(where("structure").exists(true));
+
+            Update update = new Update().unset("structure");
+
+            mongoTemplate.updateMulti(
+                    query(where("datasourceConfiguration.endpoints.host").is("mockdb.internal.appsmith.com")),
+                    update("datasourceConfiguration.endpoints.$.host", "127.0.0.1"),
+                    DatasourceStorage.class);
+        }
+    }
+
+    public Map<String, String> getEnvVariable() {
+        String originalContent = "";
+        final Path envFilePath = Path.of(commonConfig.getEnvFilePath());
+        try {
+            originalContent = Files.readString(envFilePath);
+        } catch (NoSuchFileException e) {
+            log.error("Env file not found at " + commonConfig.getEnvFilePath(), e);
+        } catch (IOException e) {
+            log.error("Unable to read env file " + commonConfig.getEnvFilePath(), e);
+        }
+        return envManager.parseToMap(originalContent);
+    }
+}
