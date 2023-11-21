24498b878734f9ec7b30bdf0ed7bfd3ca8d1d1c7
Saroj <saroj@appsmith.com>
2023-11-21 11:00:11 +0530

    updated the dime defenders to skip flaky tests on demand

--- app/client/cypress/scripts/util.ts
+++ app/client/cypress/scripts/util.ts
@@ -34,6 +34,7 @@ export default class util {
       thisRunner: this.getEnvValue("THIS_RUNNER", { required: true }),
       cypressSpecs: this.getEnvValue("CYPRESS_SPECS", { required: false }),
       cypressRerun: this.getEnvValue("CYPRESS_RERUN", { required: false }),
+      cypressSkipFlaky: this.getEnvValue("CYPRESS_SKIP_FLAKY", { required: false }),
     };
   }
 
