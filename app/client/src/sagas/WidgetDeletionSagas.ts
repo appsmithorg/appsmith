25f1bdcfefe4a56ae05c06c95fd209158da50b81
Preet <preetsidhu.bits@gmail.com>
2023-11-21 15:05:00 -0500

    remove empty canvas upon widget deletion

--- app/client/src/sagas/WidgetDeletionSagas.ts
+++ app/client/src/sagas/WidgetDeletionSagas.ts
@@ -55,6 +55,7 @@ import { LayoutSystemTypes } from "layoutSystems/types";
 import { getLayoutSystemType } from "selectors/layoutSystemSelectors";
 import { updateAnvilParentPostWidgetDeletion } from "layoutSystems/anvil/utils/layouts/update/deletionUtils";
 import { getCurrentApplication } from "@appsmith/selectors/applicationSelectors";
+import { updateWidgetsToBeDeleted } from "layoutSystems/anvil/utils/widgetDeletionUtils";
 
 const WidgetTypes = WidgetFactory.widgetTypes;
 
@@ -152,16 +153,30 @@ function* deleteSagaInit(deleteAction: ReduxAction<WidgetDelete>) {
     return;
   }
 
+  const allWidgets: CanvasWidgetsReduxState = yield select(getWidgets);
+  const widgetsToBeDeleted: string[] = yield call(
+    updateWidgetsToBeDeleted,
+    selectedWidgets,
+  );
+
   if (selectedWidgets.length > 1) {
     yield put({
       type: WidgetReduxActionTypes.WIDGET_BULK_DELETE,
-      payload: deleteAction.payload,
+      payload: widgetsToBeDeleted?.length
+        ? { ...deleteAction.payload, widgetIds: widgetsToBeDeleted }
+        : deleteAction.payload,
     });
   }
   if (!!widgetId || !!selectedWidget) {
     yield put({
       type: WidgetReduxActionTypes.WIDGET_SINGLE_DELETE,
-      payload: deleteAction.payload,
+      payload: widgetsToBeDeleted?.length
+        ? {
+            ...deleteAction.payload,
+            widgetId: widgetsToBeDeleted[0],
+            parentId: allWidgets[widgetsToBeDeleted[0]].parentId,
+          }
+        : deleteAction.payload,
     });
   }
 }
