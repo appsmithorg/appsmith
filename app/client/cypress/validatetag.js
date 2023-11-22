4b188a63ad774a799d43458f01e5d8f030ca5d53
Sharanya <sharanya@appsmith.com>
2023-11-22 14:55:51 +0530

    test: Cypress - added script for validating tags added on cypress tests

--- app/client/cypress/validatetag.js
+++ app/client/cypress/validatetag.js
@@ -1,9 +1,6 @@
 const fs = require('fs');
 const { Tag } = require('./tags');
 
-const filePath = ''; // Replace with your Cypress test file path
-
-
 function extractTagsFromTestFile(filePath) {
   const fileContent = fs.readFileSync(filePath, 'utf-8');
 
@@ -14,24 +11,29 @@ function extractTagsFromTestFile(filePath) {
   if (matches && matches.length >= 3) {
     const tagsObj = eval(`(${matches[2]})`); // Parse the tags object
     if (tagsObj.tags && Array.isArray(tagsObj.tags)) {
-      return tagsObj.tags;
+      return { filePath, tags: tagsObj.tags };
     }
   }
-  return [];
+  return { filePath, tags: [] };
 }
 
-function checkTagsValidity(tags) {
-  const invalidTags = tags.filter(tag => !Tag.includes(tag));
+function checkTagsValidity(fileInfo) {
+  const invalidTags = fileInfo.tags.filter(tag => !Tag.includes(tag));
   if (invalidTags.length > 0) {
-    throw new Error(`Invalid tag(s) found: ${invalidTags.join(", ")}. Allowed tags are: ${Tag.join(", ")}`);
+    return { filePath: fileInfo.filePath, invalidTags };
   }
+  return null;
 }
 
-// Extract tags from the test file
-try {
-  const extractedTags = extractTagsFromTestFile(filePath);
-  checkTagsValidity(extractedTags);
-  console.log("Tags are valid.");
-} catch (error) {
-  console.error(error.message);
-}
+// Extract tags from the test files passed as command-line arguments
+const fileInfos = process.argv.slice(2).map(filePath => extractTagsFromTestFile(filePath));
+
+// Check validity for each file
+fileInfos.forEach(fileInfo => {
+  const error = checkTagsValidity(fileInfo);
+  if (error) {
+    console.error(`Invalid tag(s) found in ${error.filePath}: ${error.invalidTags.join(', ')}. Refer to tag.js for allowed tags.`);
+  } else {
+    console.log(`Tags in ${fileInfo.filePath} are valid.`);
+  }
+});
