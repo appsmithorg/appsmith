c6850e75343c9b9bf607a7ba28fafcf5bf5e3854
Pawan Kumar <pawan.stardust@gmail.com>
2023-11-22 13:37:52 +0530

    refactor

--- app/client/packages/design-system/headless/src/components/Radio/src/types.ts
+++ app/client/packages/design-system/headless/src/components/Radio/src/types.ts
@@ -1,17 +1,13 @@
 import type { StyleProps } from "@react-types/shared";
 import type { SpectrumRadioGroupProps } from "@react-types/radio";
+import type { TextInputProps } from "../../TextInput";
 
 export interface RadioGroupProps
   extends Omit<
-    SpectrumRadioGroupProps,
-    keyof StyleProps | "labelPosition" | "labelAlign" | "isEmphasized"
-  > {
-  /** classname for label */
-  labelClassName?: string;
-  /** classname for errorMessage or description */
-  helpTextClassName?: string;
-  /** classname for the field */
-  fieldClassName?: string;
-  /** className for the text input. */
-  className?: string;
-}
+      SpectrumRadioGroupProps,
+      keyof StyleProps | "labelPosition" | "labelAlign" | "isEmphasized"
+    >,
+    Pick<
+      TextInputProps,
+      "fieldClassName" | "labelClassName" | "helpTextClassName" | "className"
+    > {}
