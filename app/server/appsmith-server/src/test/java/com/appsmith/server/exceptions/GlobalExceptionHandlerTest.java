ae44c544ae29923a6a85db73c835ad8af95fd2d4
“sneha122” <sneha@appsmith.com>
2023-11-23 12:22:54 +0530

    fix: self review changes

--- app/server/appsmith-server/src/test/java/com/appsmith/server/exceptions/GlobalExceptionHandlerTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/exceptions/GlobalExceptionHandlerTest.java
@@ -5,11 +5,8 @@ import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginError;
 import com.appsmith.external.exceptions.pluginExceptions.AppsmithPluginException;
 import com.appsmith.server.dtos.ResponseDTO;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
 import org.springframework.mock.web.server.MockServerWebExchange;
-import org.springframework.util.LinkedMultiValueMap;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -19,10 +16,7 @@ public class GlobalExceptionHandlerTest {
     // Refer issue: https://github.com/appsmithorg/appsmith/pull/29009 for more context
     @Test
     public void testCatchPluginException_onAppsmithPluginException_throwsInternalServerError() {
-        LinkedMultiValueMap<String, String> mockHeaders = new LinkedMultiValueMap<>(1);
-        mockHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        MockServerHttpRequest httpRequest =
-                MockServerHttpRequest.get("").headers(mockHeaders).build();
+        MockServerHttpRequest httpRequest = MockServerHttpRequest.get("").build();
         MockServerWebExchange exchange = MockServerWebExchange.from(httpRequest);
 
         GlobalExceptionHandler handler = new GlobalExceptionHandler(null, null, null, null);
