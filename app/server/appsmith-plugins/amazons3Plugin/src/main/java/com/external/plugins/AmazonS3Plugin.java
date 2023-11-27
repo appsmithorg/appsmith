2bd00fe655b242888937118a544fbc94f84c4a74
Nilansh Bansal <nilansh@appsmith.com>
2023-11-27 14:17:27 +0530

    feat: added unsigned url for single and multi-file uploads (#28098) (#29066)
    ## Description
    > This PR adds unsigned URLs in the response for single and multiple
    file uploads in s3.
    
    #### PR fixes following issue(s)
    Fixes #28098 
    
    
    #### Type of change
    - New feature (non-breaking change which adds functionality)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [x] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/server/appsmith-plugins/amazons3Plugin/src/main/java/com/external/plugins/AmazonS3Plugin.java
+++ app/server/appsmith-plugins/amazons3Plugin/src/main/java/com/external/plugins/AmazonS3Plugin.java
@@ -214,6 +214,36 @@ public class AmazonS3Plugin extends BasePlugin {
             return urlList;
         }
 
+        /**
+         * This function returns the unsigned file urls for the files present in the body
+         */
+        ArrayList<String> createFileUrlsFromBody(AmazonS3 connection, String bucketName, String path, String body)
+                throws AppsmithPluginException {
+            List<MultipartFormDataDTO> multipartFormDataDTOs;
+            ArrayList<String> urlList = new ArrayList<>();
+            try {
+                multipartFormDataDTOs = Arrays.asList(objectMapper.readValue(body, MultipartFormDataDTO[].class));
+            } catch (IOException e) {
+                throw new AppsmithPluginException(
+                        AppsmithPluginError.PLUGIN_EXECUTE_ARGUMENT_ERROR,
+                        S3ErrorMessages.UNPARSABLE_CONTENT_ERROR_MSG,
+                        e.getMessage());
+            }
+            multipartFormDataDTOs.forEach(multipartFormDataDTO -> {
+                final String filePath = path + multipartFormDataDTO.getName();
+
+                urlList.add(connection.getUrl(bucketName, filePath).toString());
+            });
+            return urlList;
+        }
+
+        /**
+         * This function returns the unsigned file url for the file path
+         */
+        String createFileUrl(AmazonS3 connection, String bucketName, String path) {
+            return connection.getUrl(bucketName, path).toString();
+        }
+
         /*
          * - Throws exception on upload failure.
          * - Returns signed url of the created file on success.
@@ -643,9 +673,12 @@ public class AmazonS3Plugin extends BasePlugin {
                                     signedUrl = uploadFileFromBody(
                                             connection, bucketName, path, body, false, expiryDateTime);
                                 }
+                                // gets the unsigned url for the file
+                                String url = createFileUrl(connection, bucketName, path);
                                 actionResult = new HashMap<String, Object>();
                                 ((HashMap<String, Object>) actionResult).put("signedUrl", signedUrl);
                                 ((HashMap<String, Object>) actionResult).put("urlExpiryDate", expiryDateTimeString);
+                                ((HashMap<String, Object>) actionResult).put("url", url);
                                 requestParams.add(
                                         new RequestParamDTO(CREATE_EXPIRY, expiryDateTimeString, null, null, null));
                                 requestParams.add(
@@ -694,6 +727,9 @@ public class AmazonS3Plugin extends BasePlugin {
                                 actionResult = new HashMap<String, Object>();
                                 ((HashMap<String, Object>) actionResult).put("signedUrls", signedUrls);
                                 ((HashMap<String, Object>) actionResult).put("urlExpiryDate", expiryDateTimeString);
+                                // Adds the unsigned urls in the response
+                                ((HashMap<String, Object>) actionResult)
+                                        .put("urls", createFileUrlsFromBody(connection, bucketName, path, body));
 
                                 requestParams.add(
                                         new RequestParamDTO(CREATE_EXPIRY, expiryDateTimeString, null, null, null));
