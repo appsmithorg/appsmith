1450bbf0e1e7ec8a16f9b2463e7e4dbff85add2d
Preet <preetsidhu.bits@gmail.com>
2023-11-27 13:42:43 -0500

    update min-width of fill widgets

--- app/client/src/layoutSystems/anvil/utils/widgetUtils.ts
+++ app/client/src/layoutSystems/anvil/utils/widgetUtils.ts
@@ -26,7 +26,7 @@ export const getResponsiveMinWidth = (
   const minWidth = config["base"];
   return {
     ...config,
-    base: "100%",
+    base: `max(100%, ${config[`${MOBILE_BREAKPOINT}px`] ?? minWidth})`,
     [`${MOBILE_BREAKPOINT}px`]: config[`${MOBILE_BREAKPOINT}px`] ?? minWidth,
   };
 };
