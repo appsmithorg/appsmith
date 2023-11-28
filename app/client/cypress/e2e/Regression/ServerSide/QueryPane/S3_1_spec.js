27c374e61cd6b13d08f7dc018d23ff840b335e62
Aishwarya UR <aishwarya@appsmith.com>
2023-11-28 14:15:58 +0530

    ServerSide/QueryPane/S3_1_spec.js fix with dynamic wait

--- app/client/cypress/e2e/Regression/ServerSide/QueryPane/S3_1_spec.js
+++ app/client/cypress/e2e/Regression/ServerSide/QueryPane/S3_1_spec.js
@@ -10,6 +10,7 @@ import {
   entityItems,
   draggableWidgets,
   propPane,
+  assertHelper,
 } from "../../../../support/Objects/ObjectsCore";
 
 let datasourceName;
@@ -421,6 +422,7 @@ describe("Validate CRUD queries for Amazon S3 along with UI flow verifications",
       cy.setQueryTimeout(30000);
 
       dataSources.RunQuery({ toValidateResponse: false });
+      assertHelper.AssertReduxLoad("RUN_ACTION_SUCCESS");
       cy.wait("@postExecute").then(({ response }) => {
         expect(response.body.data.isExecutionSuccess).to.eq(true);
       });
