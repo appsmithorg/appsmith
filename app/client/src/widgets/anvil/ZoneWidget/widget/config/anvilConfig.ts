01cdc350c12b78ebd222388a185ca968c489e779
Preet <preetsidhu.bits@gmail.com>
2023-11-28 11:45:36 -0500

    add minwidth for zones

--- app/client/src/widgets/anvil/ZoneWidget/widget/config/anvilConfig.ts
+++ app/client/src/widgets/anvil/ZoneWidget/widget/config/anvilConfig.ts
@@ -3,7 +3,7 @@ import type { AnvilConfig } from "WidgetProvider/constants";
 export const anvilConfig: AnvilConfig = {
   isLargeWidget: true,
   widgetSize: {
-    minWidth: { base: "min-content" },
+    minWidth: { base: "240px" },
     minHeight: { base: "50px" },
   },
 };
