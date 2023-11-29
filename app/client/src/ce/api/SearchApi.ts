7fb45062184464e8a6b0cf9059750784b87d6702
Ankit Srivastava <ankit@appsmith.com>
2023-11-29 21:56:53 +0530

    commented not required changes

--- app/client/src/ce/api/SearchApi.ts
+++ app/client/src/ce/api/SearchApi.ts
@@ -1,4 +1,3 @@
-import type { AxiosPromise } from "axios";
 import Api from "api/Api";
 import type { ApiResponse } from "api/ApiResponses";
 import type { FetchApplicationsResponse } from "./ApplicationApi";
@@ -28,37 +27,16 @@ export interface MockedSearchApiResponse extends ApiResponse {
 export class SearchApi extends Api {
   static searchURL = "v1/search-entities";
 
-  static async searchAllEntities() // keyword: string,
-  : Promise<AxiosPromise<SearchEntitiesResponse>> {
+  static async searchAllEntities() {
+    // keyword: string,
+    // : Promise<AxiosPromise<SearchEntitiesResponse>> {
     // const request: SearchEntitiesRequest = {
     //   keyword,
     //   entities: ["workspaces", "applications", "packages"],
     //   page: 1,
     //   limit: 100,
     // };
-
-    // Mocked response structure based on the provided data
-    const mockedResponse: MockedSearchApiResponse = {
-      success: true,
-      data: {},
-      // Other properties in ApiResponse...
-    };
-
-    // Simulate API call delay using setTimeout (remove this in a real API call)
-    return new Promise<AxiosPromise<SearchEntitiesResponse>>((resolve) => {
-      setTimeout(() => {
-        resolve({
-          data: mockedResponse,
-          // Simulated Axios response object structure
-          // You might need to adjust this based on your Axios response structure
-          status: 200,
-          statusText: "OK",
-          headers: {},
-          config: {},
-          request: null,
-        });
-      }, 1000); // Simulate a delay of 1 second (remove in actual implementation)
-    });
+    // return new Promise();
   }
 }
 
