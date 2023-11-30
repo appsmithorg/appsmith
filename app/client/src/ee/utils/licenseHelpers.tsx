ea83244da87a367aae199196f5e821eae9493d69
fs01c1 <ankit@appsmith.com>
2023-11-30 15:43:24 +0530

    fixed header button

--- app/client/src/ee/utils/licenseHelpers.tsx
+++ app/client/src/ee/utils/licenseHelpers.tsx
@@ -10,25 +10,16 @@ import {
 import { isAirgapped } from "@appsmith/utils/airgapHelpers";
 import store from "store";
 import { customerPortalPlansUrl } from "@appsmith/utils/billingUtils";
-import capitalize from "lodash/capitalize";
 import { createMessage, UPGRADE } from "@appsmith/constants/messages";
 import { useRouteMatch } from "react-router";
 import PageBannerMessage from "@appsmith/pages/common/PageWrapperBanner";
-import { Link } from "design-system";
-import styled from "styled-components";
+import { Button } from "design-system";
 
 export const getLicenseKey = () => {
   const state = store.getState();
   const licenseKey = state?.tenant?.tenantConfiguration?.license?.key;
   return licenseKey || "";
 };
-
-const StyledLink = styled(Link)`
-  align-items: center;
-  height: 38px;
-  gap: 8px;
-  padding: 14px;
-`;
 
 export const pricingPageUrlSource = "BE";
 
@@ -38,16 +29,15 @@ export const ShowUpgradeMenuItem = () => {
   const isFree = useSelector(isFreePlan);
   const isAirgappedInstance = isAirgapped();
   return (isTrial || isFree) && isAdmin && !isAirgappedInstance ? (
-    <StyledLink
-      className="business-plan-menu-option"
+    <Button
+      className="business-plan-menu-option mr-2"
       data-testid="t--upgrade-to-business"
-      kind="primary"
-      startIcon="upload-cloud"
+      href={customerPortalPlansUrl}
+      kind="secondary"
       target="_blank"
-      to={customerPortalPlansUrl}
     >
-      {capitalize(createMessage(UPGRADE))}
-    </StyledLink>
+      {createMessage(UPGRADE)}
+    </Button>
   ) : null;
 };
 
