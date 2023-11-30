df3cf2c387fdc592e3306c5818e6cedf06316ccd
Rajat Agrawal <rajat@appsmith.com>
2023-11-30 14:40:15 +0530

    Rename the event name to Server startup for instance config

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/InstanceConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/InstanceConfig.java
@@ -22,7 +22,7 @@ import static java.lang.Boolean.TRUE;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-@Observed(name = "License Verification")
+@Observed(name = "Server startup")
 public class InstanceConfig implements ApplicationListener<ApplicationReadyEvent> {
 
     private final ConfigService configService;
