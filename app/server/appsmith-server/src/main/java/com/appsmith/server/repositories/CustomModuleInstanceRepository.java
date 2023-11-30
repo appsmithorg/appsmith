2352914b4c4b0855ebb6587d58ad33f3215800f1
subratadeypappu <subrata@appsmith.com>
2023-11-30 14:16:15 +0600

    chore: Deploy application with module instances (#2976)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomModuleInstanceRepository.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomModuleInstanceRepository.java
@@ -3,6 +3,7 @@ package com.appsmith.server.repositories;
 import com.appsmith.external.models.CreatorContextType;
 import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.domains.ModuleInstance;
+import com.mongodb.client.result.UpdateResult;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -21,4 +22,8 @@ public interface CustomModuleInstanceRepository extends AppsmithRepository<Modul
     Flux<ModuleInstance> findAllByRootModuleInstanceId(String rootModuleInstanceId, Optional<AclPermission> permission);
 
     Flux<ModuleInstance> findAllByApplicationIds(List<String> applicationIds, List<String> includedFields);
+
+    Flux<ModuleInstance> findAllByApplicationId(String applicationId, Optional<AclPermission> permission);
+
+    Mono<UpdateResult> archiveDeletedUnpublishedModuleInstances(String applicationId, AclPermission permission);
 }
