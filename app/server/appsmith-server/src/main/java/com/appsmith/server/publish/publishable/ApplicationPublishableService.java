bf4c1140eb63f81d406cf5b2fd0288551292aba0
subrata <subrata@appsmith.com>
2023-11-30 11:33:03 +0600

    Add missing T

--- app/server/appsmith-server/src/main/java/com/appsmith/server/publish/publishable/ApplicationPublishableService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/publish/publishable/ApplicationPublishableService.java
@@ -2,4 +2,4 @@ package com.appsmith.server.publish.publishable;
 
 import com.appsmith.external.models.BaseDomain;
 
-public interface ApplicationPublishableService<T extends BaseDomain> extends ApplicationPublishableServiceCE {}
+public interface ApplicationPublishableService<T extends BaseDomain> extends ApplicationPublishableServiceCE<T> {}
