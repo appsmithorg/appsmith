bee5f0f672af6d69a2b48573e4147a2b10be7dd4
Ankita Kinger <ankita@appsmith.com>
2023-12-01 12:03:29 +0530

    fix: Refactoring package editor DOM structure to fix UI (#2995)
    ## Description
    
    Refactoring package editor DOM structure to fix UI
    
    #### PR fixes following issue(s)
    Fixes # (issue number)
    
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Testing
    
    #### How Has This Been Tested?
    - [x] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/ee/pages/Editor/PackageEditor/PackageIDE/MainPane/index.tsx
+++ app/client/src/ee/pages/Editor/PackageEditor/PackageIDE/MainPane/index.tsx
@@ -1,23 +1,45 @@
 import React from "react";
-import { Route, Switch, useRouteMatch } from "react-router";
+import { Route, Switch } from "react-router";
 import * as Sentry from "@sentry/react";
-import useRoutes from "@appsmith/pages/Editor/IDE/MainPane/useRoutes";
-import { PACKAGE_EDITOR_PATH } from "@appsmith/constants/routes/packageRoutes";
+import {
+  MODULE_EDITOR_PATH,
+  PACKAGE_EDITOR_PATH,
+  SAAS_EDITOR_DATASOURCE_ID_PATH,
+} from "@appsmith/constants/routes/packageRoutes";
+import ModuleEditor from "@appsmith/pages/Editor/ModuleEditor";
+import IntegrationEditor from "pages/Editor/IntegrationEditor";
+import {
+  DATA_SOURCES_EDITOR_ID_PATH,
+  INTEGRATION_EDITOR_PATH,
+} from "constants/routes";
+import DataSourceEditor from "pages/Editor/DataSourceEditor";
+import DatasourceForm from "pages/Editor/SaaSEditor/DatasourceForm";
 
 const SentryRoute = Sentry.withSentryRouting(Route);
 export const MainPane = (props: { id: string }) => {
-  const { path } = useRouteMatch();
-  const routes = useRoutes(path);
-
   return (
     <div
       className="relative flex flex-col flex-1 overflow-auto z-2"
       id={props.id}
     >
-      <Switch key={PACKAGE_EDITOR_PATH}>
-        {routes.map((route) => (
-          <SentryRoute {...route} key={route.key} />
-        ))}
+      <Switch>
+        {/* All subroutes go here */}
+        <SentryRoute component={ModuleEditor} path={MODULE_EDITOR_PATH} />
+        <SentryRoute
+          component={IntegrationEditor}
+          exact
+          path={`${PACKAGE_EDITOR_PATH}${INTEGRATION_EDITOR_PATH}`}
+        />
+        <SentryRoute
+          component={DataSourceEditor}
+          exact
+          path={`${PACKAGE_EDITOR_PATH}${DATA_SOURCES_EDITOR_ID_PATH}`}
+        />
+        <SentryRoute
+          component={DatasourceForm}
+          exact
+          path={`${PACKAGE_EDITOR_PATH}${SAAS_EDITOR_DATASOURCE_ID_PATH}`}
+        />
       </Switch>
     </div>
   );
