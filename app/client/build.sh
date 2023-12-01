3f6351c90b7961f01cd98057acc2833ab65f993e
Rahul Barwal <rahul.barwal@appsmith.com>
2023-12-01 11:25:52 +0530

    Revert "Dummy commit"
    This reverts commit a9dcbda904d8df7ece506251f052cc01497da13d.

--- app/client/build.sh
+++ app/client/build.sh
@@ -18,7 +18,6 @@ fi
 # build cra app
 export REACT_APP_SENTRY_RELEASE=$GIT_SHA
 export REACT_APP_CLIENT_LOG_LEVEL=ERROR
-export REACT_APP_CLOUD_HOSTING=true
 # Disable CRA built-in ESLint checks since we have our own config and a separate step for this
 export DISABLE_ESLINT_PLUGIN=true
 craco --max-old-space-size=7168 build --config craco.build.config.js
