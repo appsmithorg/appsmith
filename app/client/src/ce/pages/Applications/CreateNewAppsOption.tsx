288b52a10d78c54a628c19f21eeee8f0544ae804
Aman Agarwal <aman@appsmith.com>
2023-12-04 16:32:29 +0530

    fix: added fetch env config to imitate the ee repo functionality (#29266)
    ## Description
    - Added the fetching of environment config for ee repo on create new app
    onboarding flow.
    - Imitated the `fetchingEnvironmentConfigs` function from ee to ce to
    provide consistency on both repos
    - Avoid the redirection from datasource sagas for changing the
    datasource form which was throwing error trying to create a redirection
    url.
    
    #### PR fixes following issue(s)
    Fixes #29231 
    > if no issue exists, please create an issue and ask the maintainers
    about this first
    >
    >
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    > Please delete options that are not relevant.
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - Chore (housekeeping or task changes that don't impact user perception)
    - This change requires a documentation update
    >
    >
    >
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Implemented a new action to fetch environment configurations,
    enhancing the app's data management capabilities.
    - Updated application creation flow to automatically fetch environment
    configurations when a workspace ID is present.
    
    - **Enhancements**
    - Improved the logic for redirecting users in the data source creation
    process to be more context-aware.
    
    - **Bug Fixes**
    - Addressed an issue where users were being redirected incorrectly
    during the data source creation process.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/ce/pages/Applications/CreateNewAppsOption.tsx
+++ app/client/src/ce/pages/Applications/CreateNewAppsOption.tsx
@@ -61,6 +61,7 @@ import { TEMP_DATASOURCE_ID } from "constants/Datasource";
 import { fetchMockDatasources } from "actions/datasourceActions";
 import DatasourceForm from "pages/Editor/SaaSEditor/DatasourceForm";
 import type { Datasource } from "entities/Datasource";
+import { fetchingEnvironmentConfigs } from "@appsmith/actions/environmentAction";
 
 const SectionWrapper = styled.div`
   display: flex;
@@ -242,6 +243,9 @@ const CreateNewAppsOption = ({
       if (isEnabledForStartWithData) {
         dispatch(fetchPlugins());
         dispatch(fetchMockDatasources());
+        if (application?.workspaceId) {
+          dispatch(fetchingEnvironmentConfigs(application?.workspaceId, true));
+        }
         setUseType(START_WITH_TYPE.DATA);
       }
     } else {
