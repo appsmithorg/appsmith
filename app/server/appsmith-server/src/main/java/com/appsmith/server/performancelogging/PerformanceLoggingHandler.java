83ce0e95eabe6b359b679c6ec36e537ef1fc8277
Rajat Agrawal <rajat@appsmith.com>
2023-12-05 18:40:13 +0530

    Improve logging format for debugging OOM issues

--- app/server/appsmith-server/src/main/java/com/appsmith/server/performancelogging/PerformanceLoggingHandler.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/performancelogging/PerformanceLoggingHandler.java
@@ -1,5 +1,6 @@
 package com.appsmith.server.performancelogging;
 
+import io.micrometer.common.KeyValue;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.Observation.Context;
 import io.micrometer.observation.ObservationHandler;
@@ -7,13 +8,44 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
 @Component
 @Slf4j
 @ConditionalOnExpression("${logging.verbose.enabled}")
 class PerformanceLoggingHandler implements ObservationHandler<Observation.Context> {
     @Override
     public void onStart(Context context) {
-        context.put("executionTime", System.currentTimeMillis());
+        if (needsLogStatement(context)) {
+            context.put("executionTime", System.currentTimeMillis());
+
+            KeyValue url = context.getHighCardinalityKeyValue("http.url");
+            String urlName = url == null ? "null" : url.getValue();
+            log.info("Execution Started : {}, Context : {}, {}", context.getName(), urlName, memoryFootprint());
+        }
+    }
+
+    private String memoryFootprint() {
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+
+        return String.format(
+                "Initial memory: %.2f GB, Used heap memory: %.2f GB, Max heap memory: %.2f GB, Committed memory: %.2f GB",
+                (double) memoryMXBean.getHeapMemoryUsage().getInit() / 1073741824,
+                (double) memoryMXBean.getHeapMemoryUsage().getUsed() / 1073741824,
+                (double) memoryMXBean.getHeapMemoryUsage().getMax() / 1073741824,
+                (double) memoryMXBean.getHeapMemoryUsage().getCommitted() / 1073741824);
+    }
+
+    private Boolean needsLogStatement(Context context) {
+        String name = context.getName();
+        if (name.equals("spring.security.authorizations")
+                || name.equals("spring.security.filterchains")
+                || name.equals("spring.security.http.secured.requests")) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
@@ -23,13 +55,22 @@ class PerformanceLoggingHandler implements ObservationHandler<Observation.Contex
 
     @Override
     public void onStop(Context context) {
-        long startTime = context.getOrDefault("executionTime", 0L);
-        long executionTime = System.currentTimeMillis() - startTime;
-        log.info(
-                "Execution Complete: {} | Total Time Taken: {} ms | Context: {}",
-                context.getName(),
-                executionTime,
-                context.toString());
+        if (needsLogStatement(context)) {
+            long startTime = context.getOrDefault("executionTime", 0L);
+            long executionTime = System.currentTimeMillis() - startTime;
+
+            KeyValue url = context.getHighCardinalityKeyValue("http.url");
+            String urlName = url == null ? "null" : url.getValue();
+
+            log.info(
+                    "Execution Complete: {} | Total Time Taken: {} ms | Context: {}, {}",
+                    context.getName(),
+                    executionTime,
+                    urlName,
+                    memoryFootprint()
+                    // context.toString()
+                    );
+        }
     }
 
     @Override
