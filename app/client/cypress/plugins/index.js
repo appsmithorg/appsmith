cb9eefd1c0791d585d99d628d405cae1b8744eaa
Sharanya <sharanya@appsmith.com>
2023-12-06 18:23:49 +0530

    moved change to separate function

--- app/client/cypress/plugins/index.js
+++ app/client/cypress/plugins/index.js
@@ -226,19 +226,30 @@ module.exports = async (on, config) => {
   console.log("Whats in config before:" + config.specPattern);
   console.log("Type of 'config.specPattern':", typeof config.specPattern);
 
-    const splitSpecs = JSON.stringify(config.specPattern).split(',').map((pattern) => {
-      console.log("Pattern:", pattern);
-      const index = pattern.indexOf('***');
-      return index !== -1 ? pattern.slice(index) : pattern;
-    }); 
-    config.specPattern = splitSpecs;
-  
+  config.specPattern = trimPathsBeforeWildcard(config.specPattern);
+
   console.log("Whats in splitSpecs:" + splitSpecs);
   console.log("Whats in config after:" + config.specPattern);
 
   if (process.env["RUNID"]) {
     config = await new cypressSplit().splitSpecs(on, config);
     cypressHooks(on, config);
+  }
+
+
+  function trimPathsBeforeWildcard(pathsString) {
+    console.log("In trimPathsBeforeWildcard")
+    const paths = pathsString.split(',');
+    console.log("paths:", paths)
+    const trimmedPaths = paths.map(path => {
+      const wildcardIndex = path.indexOf('***');
+      if (wildcardIndex !== -1) {
+        return path.slice(wildcardIndex);
+      }
+      console.log("return paths:", paths)
+      return path;
+    });
+    return trimmedPaths.join(',');
   }
 
   return config;
