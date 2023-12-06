5673e5b546f6c1ebbb3de340ded2dff4fc75081f
Aishwarya UR <aishwarya@appsmith.com>
2023-12-07 00:13:44 +0530

    run limit

--- app/client/cypress_ci_custom.config.ts
+++ app/client/cypress_ci_custom.config.ts
@@ -57,7 +57,6 @@ export default defineConfig({
       "cypress/e2e/Sanity/Datasources/Airtable_Basic_Spec.ts",
       "cypress/e2e/EE/Enterprise/MultipleEnv/ME_airtable_spec.ts",
       "cypress/e2e/Regression/ServerSide/Datasources/ElasticSearch_Basic_Spec.ts",
-      "cypress/e2e/Regression/ServerSide/Datasources/Oracle_Spec.ts",
       "cypress/e2e/Regression/ClientSide/Widgets/Others/MapWidget_Spec.ts",
       "cypress/e2e/Sanity/Datasources/MsSQL_Basic_Spec.ts",
     ],
