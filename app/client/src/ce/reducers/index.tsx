bb189392847f7dacd356aa3513b16ba9bda66d34
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2023-12-06 16:46:35 +0530

    remove unused import

--- app/client/src/ce/reducers/index.tsx
+++ app/client/src/ce/reducers/index.tsx
@@ -59,7 +59,6 @@ import type { SettingsReduxState } from "@appsmith/reducers/settingsReducer";
 import SettingsReducer from "@appsmith/reducers/settingsReducer";
 import type { GuidedTourState } from "reducers/uiReducers/guidedTourReducer";
 import type { TriggerValuesEvaluationState } from "reducers/evaluationReducers/triggerReducer";
-import type { CanvasWidgetStructure } from "WidgetProvider/constants";
 import type { AppSettingsPaneReduxState } from "reducers/uiReducers/appSettingsPaneReducer";
 import type { TenantReduxState } from "@appsmith/reducers/tenantReducer";
 import tenantReducer from "@appsmith/reducers/tenantReducer";
