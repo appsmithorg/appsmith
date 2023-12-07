c87adea3f6864978ae73099076d2b8cb8884ac86
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-12-07 16:15:44 +0530

    adding comments to redistributeSectionSpace func function

--- app/client/src/layoutSystems/anvil/sectionSpaceDistributor/utils.ts
+++ app/client/src/layoutSystems/anvil/sectionSpaceDistributor/utils.ts
@@ -1,9 +1,16 @@
 import { SectionColumns, ZoneMinColumnWidth } from "../utils/constants";
 
+/**
+ * Redistributes space among zones in a section while preserving column ratios.
+ * @param spaceDistributedObj - Object containing the current space distribution for each zone.
+ * @param zoneOrder - Array defining the order of zones in the section.
+ * @param zoneChangeFactor - Amount of space to add or remove (positive for addition, negative for removal).
+ * @param index - Index of the zone where space is added or removed.
+ * @param addedViaStepper - Indicates whether the change was initiated via stepper.
+ * @returns Updated space distribution after the change.
+ */
 export const redistributeSectionSpace = (
-  spaceDistributedObj: {
-    [key: string]: number;
-  },
+  spaceDistributedObj: { [key: string]: number },
   zoneOrder: string[],
   zoneChangeFactor: number,
   index: number,
@@ -12,22 +19,41 @@ export const redistributeSectionSpace = (
   const spaceDistributedArray = zoneOrder.map(
     (zone) => spaceDistributedObj[zone],
   );
-  if (Math.abs(zoneChangeFactor) < ZoneMinColumnWidth)
+
+  // Check if the change is smaller than the minimum zone column width
+  if (Math.abs(zoneChangeFactor) < ZoneMinColumnWidth) {
     return spaceDistributedArray;
+  }
+
+  // Calculate the maximum space that can be added or removed
   const spaceWelcomed =
     SectionColumns - ZoneMinColumnWidth * spaceDistributedArray.length;
+
+  // Determine the actual change considering the maximum space and direction of change
   const zoneChange =
-    zoneChangeFactor > spaceWelcomed ? spaceWelcomed : zoneChangeFactor;
+    zoneChangeFactor > spaceWelcomed && zoneChangeFactor > 0
+      ? spaceWelcomed
+      : zoneChangeFactor;
+
+  // Calculate the even distribution space for comparison
   const evenDistributionSpace = SectionColumns / spaceDistributedArray.length;
+
+  // Check if the layout is evenly distributed
   const evenlyDistributedLayout =
     (addedViaStepper || spaceDistributedArray.length > 1) &&
     spaceDistributedArray.every((each) => each === evenDistributionSpace);
+
+  // Determine if adding or removing space
   const isAddingZone = zoneChange > 0;
+
+  // Modify the array based on the operation
   if (isAddingZone) {
     spaceDistributedArray.splice(index, 0, zoneChange);
   } else {
     spaceDistributedArray.splice(index, 1);
   }
+
+  // Check if the layout is still evenly distributed after the change
   if (evenlyDistributedLayout) {
     const updatedEvenDistributionSpace =
       SectionColumns / spaceDistributedArray.length;
@@ -35,12 +61,16 @@ export const redistributeSectionSpace = (
       updatedEvenDistributionSpace,
     );
   }
+
+  // Initialize indices and space variables
   let leftIndex = index - 1;
   let rightIndex = isAddingZone ? index + 1 : index;
   let spaceRightNow = spaceDistributedArray.reduce(
     (result, each) => each + result,
     0,
   );
+
+  // Determine the direction and adjust space until the target is reached
   const changeFactor = isAddingZone ? -1 : 1;
   while (spaceRightNow !== SectionColumns) {
     if (leftIndex !== -1) {
@@ -72,6 +102,7 @@ export const redistributeSectionSpace = (
       break;
     }
   }
+
   return spaceDistributedArray;
 };
 
