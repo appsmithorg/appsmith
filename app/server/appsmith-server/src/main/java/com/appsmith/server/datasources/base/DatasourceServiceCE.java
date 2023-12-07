2ac08bf651f9a8b03f77a75c852b8ccccdd55cbf
Manish Kumar <107841575+sondermanish@users.noreply.github.com>
2023-12-07 21:09:50 +0700

    Revert "fix: added changes for mustache binding (#29264)"
    This reverts commit d1eaeffe76c07f0c41fc9735ce71456bf5581272.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/datasources/base/DatasourceServiceCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/datasources/base/DatasourceServiceCE.java
@@ -36,7 +36,7 @@ public interface DatasourceServiceCE {
 
     Mono<Datasource> findById(String id);
 
-    Mono<Set<MustacheBindingToken>> extractKeysFromDatasource(Datasource datasource);
+    Set<MustacheBindingToken> extractKeysFromDatasource(Datasource datasource);
 
     Mono<Datasource> save(Datasource datasource);
 
