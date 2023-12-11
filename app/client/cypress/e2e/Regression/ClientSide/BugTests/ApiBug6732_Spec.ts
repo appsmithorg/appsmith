b0fcf260cf046fd4ac8be842d7330f43cb1cdb85
Sharanya <sharanya@appsmith.com>
2023-12-11 17:19:19 +0530

    fixing prettier issues

--- app/client/cypress/e2e/Regression/ClientSide/BugTests/ApiBug6732_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/BugTests/ApiBug6732_Spec.ts
@@ -2,21 +2,25 @@ import * as _ from "../../../../support/Objects/ObjectsCore";
 
 const { apiPage } = _;
 
-describe("Bug 6732 - this.params in IIFE function in API editor", { tags: ["@tag.Datasource"] }, () => {
-  it("1. this.params should be available in IIFE function in API editor", () => {
-    apiPage.CreateApi("Api1", "GET");
+describe(
+  "Bug 6732 - this.params in IIFE function in API editor",
+  { tags: ["@tag.Datasource"] },
+  () => {
+    it("1. this.params should be available in IIFE function in API editor", () => {
+      apiPage.CreateApi("Api1", "GET");
 
-    apiPage.SelectPaneTab("Params");
-    apiPage.EnterParams(
-      "page",
-      "{{(function () { return this.params.key; })()}}",
-      0,
-      false,
-    );
+      apiPage.SelectPaneTab("Params");
+      apiPage.EnterParams(
+        "page",
+        "{{(function () { return this.params.key; })()}}",
+        0,
+        false,
+      );
 
-    cy.get(apiPage._paramValue(0)).should(
-      "not.have.class",
-      "t--codemirror-has-error",
-    );
-  });
-});
+      cy.get(apiPage._paramValue(0)).should(
+        "not.have.class",
+        "t--codemirror-has-error",
+      );
+    });
+  },
+);
