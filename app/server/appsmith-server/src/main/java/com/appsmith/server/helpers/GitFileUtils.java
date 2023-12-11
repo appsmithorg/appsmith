d3442e9555e0fc4f9d746ff0e804f11e4a7c339f
Nidhi Nair <nidhi@appsmith.com>
2023-12-12 01:02:13 +0530

    Spotless fixes

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/GitFileUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/GitFileUtils.java
@@ -48,7 +48,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
