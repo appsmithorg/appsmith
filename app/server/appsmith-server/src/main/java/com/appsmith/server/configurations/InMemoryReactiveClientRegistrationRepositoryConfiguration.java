bd9f0bddfab25eba998d137910ca44ac81ff8ed2
Nilesh Sarupriya <nilesh@appsmith.com>
2023-12-11 05:35:06 -0600

    chore: remove the issuer URI check (#3055)
    ## Description
    > Make the issuer uri check more resilient. Add special handling for
    Keycloak Issuer URI.
    
    #### PR fixes following issue(s)
    Fixes # (issue number)
    > if no issue exists, please create an issue and ask the maintainers
    about this first
    >
    >
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Testing
    >
    #### How Has This Been Tested?
    - [x] Manual
    
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    ---------
    
    Co-authored-by: Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/InMemoryReactiveClientRegistrationRepositoryConfiguration.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/InMemoryReactiveClientRegistrationRepositoryConfiguration.java
@@ -1,15 +1,23 @@
 package com.appsmith.server.configurations;
 
-import org.apache.commons.lang3.StringUtils;
+import com.appsmith.server.domains.LoginSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientPropertiesRegistrationAdapter;
+import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.oauth2.core.AuthenticationMethod;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,29 +37,98 @@ import java.util.Map;
  * </ol>
  */
 @Configuration
+@Slf4j
 public class InMemoryReactiveClientRegistrationRepositoryConfiguration {
 
+    private static final String KEYCLOAK = LoginSource.KEYCLOAK.name().toLowerCase();
+
+    /**
+     * <p>
+     * Configures and creates an ReactiveClientRegistrationRepository bean for Spring Security OAuth2
+     * based on the provided OAuth2 client properties.
+     * </p>
+     *
+     * <p>
+     * This method removes the KEYCLOAK registration and provider from the properties, builds a ClientRegistration
+     * specifically for Keycloak, and adds it to the list of registrations. Finally, creates and returns an
+     * InMemoryReactiveClientRegistrationRepository with the configured list of client registrations.
+     * </p>
+     *
+     * @param properties The OAuth2 client properties used to configure client registrations.
+     * @return An InMemoryReactiveClientRegistrationRepository bean initialized with the configured client registrations.
+     * @see org.springframework.context.annotation.Bean
+     * @see org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository
+     */
     @Bean
     InMemoryReactiveClientRegistrationRepository clientRegistrationRepository(OAuth2ClientProperties properties) {
-        removeIssuerURIFromOAuth2ClientPropertiesIfNotReachable(properties);
+        OAuth2ClientProperties.Registration registration =
+                properties.getRegistration().get(KEYCLOAK);
+        OAuth2ClientProperties.Provider provider = properties.getProvider().get(KEYCLOAK);
+        properties.getRegistration().remove(KEYCLOAK);
+        properties.getProvider().remove(KEYCLOAK);
         List<ClientRegistration> registrations =
                 new ArrayList<>(OAuth2ClientPropertiesRegistrationAdapter.getClientRegistrations(properties)
                         .values());
+
+        ClientRegistration clientRegistrationForKeycloak = buildClientRegistrationForKeycloak(registration, provider);
+        registrations.add(clientRegistrationForKeycloak);
         return new InMemoryReactiveClientRegistrationRepository(registrations);
     }
 
-    private void removeIssuerURIFromOAuth2ClientPropertiesIfNotReachable(OAuth2ClientProperties properties) {
-        Map<String, OAuth2ClientProperties.Provider> providers = properties.getProvider();
-        RestTemplate restTemplate = new RestTemplate();
-        providers.forEach((key, value) -> {
-            if (!StringUtils.isEmpty(value.getIssuerUri())) {
-                String issuerUri = value.getIssuerUri();
-                try {
-                    restTemplate.getForObject(issuerUri, String.class);
-                } catch (Exception exception) {
-                    value.setIssuerUri(null);
-                }
-            }
-        });
+    /**
+     * <p>
+     * Builds a ClientRegistration specifically for Keycloak based on the provided registration and provider information.
+     * </p>
+     *
+     * <p>
+     * This method constructs a ClientRegistration.Builder with the necessary configuration for Keycloak.
+     * It maps the properties from the registration and provider objects to the corresponding builder methods.
+     * Additionally, it sets the end session endpoint in the provider configuration metadata.
+     * </p>
+     *
+     * @param registration The registration information for the OAuth2 client.
+     * @param provider The provider information for the OAuth2 client.
+     * @return A ClientRegistration specifically configured for Keycloak.
+     * @see org.springframework.security.oauth2.client.registration.ClientRegistration
+     */
+    ClientRegistration buildClientRegistrationForKeycloak(
+            OAuth2ClientProperties.Registration registration, OAuth2ClientProperties.Provider provider) {
+        PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
+        String authorizationUri = provider.getAuthorizationUri();
+        URI uri = URI.create(authorizationUri);
+        String endSessionEndpoint = UriComponentsBuilder.newInstance()
+                .scheme(uri.getScheme())
+                .host(uri.getHost())
+                .path("/auth/realms/appsmith/protocol/openid-connect/logout")
+                .build()
+                .toUriString();
+        Map<String, Object> configurationMetadata = new HashMap<>();
+        configurationMetadata.put("end_session_endpoint", endSessionEndpoint);
+        ClientRegistration.Builder builder = ClientRegistration.withRegistrationId(KEYCLOAK)
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .providerConfigurationMetadata(configurationMetadata);
+        map.from(registration::getClientSecret).to(builder::clientSecret);
+        map.from(registration::getRedirectUri).to(builder::redirectUri);
+        map.from(registration::getScope)
+                .as(org.springframework.util.StringUtils::toStringArray)
+                .to(builder::scope);
+        map.from(registration::getClientSecret).to(builder::clientSecret);
+        map.from(registration::getClientAuthenticationMethod)
+                .as(ClientAuthenticationMethod::new)
+                .to(builder::clientAuthenticationMethod);
+        map.from(registration::getAuthorizationGrantType)
+                .as(AuthorizationGrantType::new)
+                .to(builder::authorizationGrantType);
+        map.from(registration::getClientName).to(builder::clientName);
+        map.from(registration::getClientId).to(builder::clientId);
+        map.from(provider::getAuthorizationUri).to(builder::authorizationUri);
+        map.from(provider::getTokenUri).to(builder::tokenUri);
+        map.from(provider::getJwkSetUri).to(builder::jwkSetUri);
+        map.from(provider::getUserInfoUri).to(builder::userInfoUri);
+        map.from(provider::getUserInfoAuthenticationMethod)
+                .as(AuthenticationMethod::new)
+                .to(builder::userInfoAuthenticationMethod);
+        map.from(provider::getUserNameAttribute).to(builder::userNameAttributeName);
+        return builder.build();
     }
 }
