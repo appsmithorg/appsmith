071af654ccec5d0bfc6307c9419dd2fda732196e
Aman Agarwal <aman@appsmith.com>
2023-12-12 13:54:38 +0530

    fix: sorting for gsheet spreadsheets & sheets

--- app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/config/FileInfoMethod.java
+++ app/server/appsmith-plugins/googleSheetsPlugin/src/main/java/com/external/config/FileInfoMethod.java
@@ -17,6 +17,8 @@ import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -161,6 +163,7 @@ public class FileInfoMethod implements ExecutionMethod, TriggerMethod {
                         "value", properties.get("title").asText()));
             }
         }
+        Collections.sort(sheetsList, Comparator.comparing(sheet -> sheet.get("label")));
         return this.objectMapper.valueToTree(sheetsList);
     }
 }
