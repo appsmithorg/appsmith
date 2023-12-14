43da585a9ebb1f5355cfdacb87d955b7063a01e2
Preet Sidhu <preetsidhu.bits@gmail.com>
2023-12-14 11:53:35 -0500

    fix lint error

--- app/client/src/layoutSystems/anvil/utils/layouts/typeUtils.test.ts
+++ app/client/src/layoutSystems/anvil/utils/layouts/typeUtils.test.ts
@@ -5,14 +5,14 @@ import type { LayoutComponentProps } from "../anvilTypes";
 describe("Layouts - typeUtils tests", () => {
   describe("doesLayoutRenderWidgets", () => {
     it("should return true if layout renders widgets", () => {
-      const layout: LayoutComponentProps = generateLayoutComponentMock();
+      const layout: LayoutComponentProps = generateLayoutComponentMock().layout;
       expect(doesLayoutRenderWidgets(layout)).toBeTruthy();
     });
     it("should return false if layout renders widgets", () => {
       const layout: LayoutComponentProps = generateLayoutComponentMock(
         {},
         false,
-      );
+      ).layout;
       expect(doesLayoutRenderWidgets(layout)).toBeFalsy();
     });
   });
