960d3278e0284c92a6293d62f9582f786d7c1dd8
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2023-12-14 19:55:03 +0530

    chore: code clean up

--- app/client/src/layoutSystems/anvil/integrations/sagas/anvilSpaceDistributionSagas.ts
+++ app/client/src/layoutSystems/anvil/integrations/sagas/anvilSpaceDistributionSagas.ts
@@ -6,6 +6,10 @@ import type { CanvasWidgetsReduxState } from "reducers/entityReducers/canvasWidg
 import { updateAndSaveLayout } from "actions/pageActions";
 import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
 
+/**
+ * function to redistribute spaces among zones within a section.
+ * @param action - Redux action containing the section layout ID and the distributed space for each zone.
+ */
 function* reDistributeZoneSpaces(
   action: ReduxAction<{
     sectionLayoutId: string;
@@ -15,13 +19,24 @@ function* reDistributeZoneSpaces(
   }>,
 ) {
   const { zonesDistributed } = action.payload;
+
+  // Retrieve the current state of all widgets
   const allWidgets: CanvasWidgetsReduxState = yield select(getWidgets);
+
+  // Create a copy of the widgets to update
   let updatedWidgets = { ...allWidgets };
+
+  // Extract allocated zone IDs from the action payload
   const allocatedZoneIds = Object.keys(zonesDistributed);
+
+  // Determine the section widget ID based on the first allocated zone ID or use a default if none are available
   const sectionWidgetId =
-    updatedWidgets[allocatedZoneIds[0]].parentId || MAIN_CONTAINER_WIDGET_ID;
+    updatedWidgets[allocatedZoneIds[0]]?.parentId || MAIN_CONTAINER_WIDGET_ID;
+
+  // Retrieve the section parent widget
   const sectionParent = updatedWidgets[sectionWidgetId];
-  const allZoneIds = sectionParent.children || [];
+
+  // Initialize the updated widgets with cleared space distribution for the section parent
   updatedWidgets = {
     ...updatedWidgets,
     [sectionWidgetId]: {
@@ -29,8 +44,13 @@ function* reDistributeZoneSpaces(
       spaceDistributed: {},
     },
   };
+
+  // Iterate through all zone IDs in the section
+  const allZoneIds = sectionParent.children || [];
   allZoneIds.forEach((zoneId) => {
     let zoneWidget = updatedWidgets[zoneId];
+
+    // If the zone widget exists, update its flexGrow based on the distributed spaces
     if (zoneWidget) {
       const spaces = zonesDistributed[zoneId];
       if (spaces) {
@@ -40,6 +60,8 @@ function* reDistributeZoneSpaces(
         };
       }
     }
+
+    // Update the widgets with the changes for the current zone
     updatedWidgets = {
       ...updatedWidgets,
       [zoneId]: {
@@ -49,11 +71,13 @@ function* reDistributeZoneSpaces(
         ...updatedWidgets[sectionWidgetId],
         spaceDistributed: {
           ...updatedWidgets[sectionWidgetId].spaceDistributed,
-          [zoneId]: zoneWidget.flexGrow,
+          [zoneId]: zoneWidget?.flexGrow,
         },
       },
     };
   });
+
+  // Dispatch an action to update and save the layout with the modified widgets
   yield put(updateAndSaveLayout(updatedWidgets));
 }
 
