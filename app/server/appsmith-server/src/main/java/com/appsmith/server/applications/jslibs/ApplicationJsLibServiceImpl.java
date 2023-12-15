48bb641f9eb910454b4d643fc371e8adb8a39c1e
Nidhi Nair <nidhi@appsmith.com>
2023-12-16 00:40:25 +0530

    test: introduce conflict

--- app/server/appsmith-server/src/main/java/com/appsmith/server/applications/jslibs/ApplicationJsLibServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/applications/jslibs/ApplicationJsLibServiceImpl.java
@@ -3,12 +3,14 @@ package com.appsmith.server.applications.jslibs;
 import com.appsmith.server.applications.base.ApplicationService;
 import com.appsmith.server.domains.Application;
 import com.appsmith.server.jslibs.context.ContextBasedJsLibService;
+import com.appsmith.server.repositories.ApplicationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationJsLibServiceImpl extends ApplicationJsLibServiceCEImpl
         implements ContextBasedJsLibService<Application> {
-    public ApplicationJsLibServiceImpl(ApplicationService applicationService) {
+    public ApplicationJsLibServiceImpl(
+            ApplicationService applicationService, ApplicationRepository applicationRepository) {
         super(applicationService);
     }
 }
