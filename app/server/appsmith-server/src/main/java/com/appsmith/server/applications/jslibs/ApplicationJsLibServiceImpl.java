9155bc41633239f4a83934104cde4c2635b0d65b
Nidhi Nair <nidhi@appsmith.com>
2023-12-16 01:08:06 +0530

    Revert test changes

--- app/server/appsmith-server/src/main/java/com/appsmith/server/applications/jslibs/ApplicationJsLibServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/applications/jslibs/ApplicationJsLibServiceImpl.java
@@ -3,14 +3,12 @@ package com.appsmith.server.applications.jslibs;
 import com.appsmith.server.applications.base.ApplicationService;
 import com.appsmith.server.domains.Application;
 import com.appsmith.server.jslibs.context.ContextBasedJsLibService;
-import com.appsmith.server.repositories.ApplicationRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ApplicationJsLibServiceImpl extends ApplicationJsLibServiceCEImpl
         implements ContextBasedJsLibService<Application> {
-    public ApplicationJsLibServiceImpl(
-            ApplicationService applicationService, ApplicationRepository applicationRepository) {
+    public ApplicationJsLibServiceImpl(ApplicationService applicationService) {
         super(applicationService);
     }
 }
