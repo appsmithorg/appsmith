acf2ed59d19badcbb3b2e5e7116ac449cfff6353
Aishwarya UR <aishwarya@appsmith.com>
2023-12-15 13:50:33 +0530

    BracketNotation_AC_spec fix

--- app/client/cypress/e2e/Regression/ClientSide/Autocomplete/BracketNotation_AC_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Autocomplete/BracketNotation_AC_spec.ts
@@ -31,8 +31,10 @@ describe(
 
       agHelper.GetNClick(jsEditor._lineinJsEditor(5));
       agHelper.TypeText(locators._codeMirrorTextArea, "this.");
+      agHelper.WaitUntilEleAppear(locators._hints);
       agHelper.GetElementsNAssertTextPresence(locators._hints, "my variable 1");
-      agHelper.GetNClickByContains(locators._hints, "my variable 1", 0, false);
+      agHelper.Sleep();//is needed for hint to stay on screen & then click, tried other checks, still failing locally & in CI
+      agHelper.GetNClickByContains(locators._hints, "my variable 1", 0);
       agHelper.GetNAssertElementText(
         jsEditor._lineinJsEditor(5),
         'this["my variable 1"]',
@@ -40,7 +42,9 @@ describe(
       );
       agHelper.GetNClick(jsEditor._lineinJsEditor(6));
       agHelper.TypeText(locators._codeMirrorTextArea, 'this["');
+      agHelper.WaitUntilEleAppear(locators._hints);
       agHelper.GetElementsNAssertTextPresence(locators._hints, "my variable 1");
+      agHelper.Sleep();//is needed for hint to stay on screen & then click, tried other checks, still failing locally & in CI
       agHelper.GetNClickByContains(locators._hints, "my variable 1", 0, false);
       agHelper.GetNAssertElementText(
         jsEditor._lineinJsEditor(5),
