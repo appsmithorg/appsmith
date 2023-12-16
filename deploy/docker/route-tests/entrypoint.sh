67a1045fa002c02b00b8414e25b61be719bb0d82
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-12-16 16:10:04 +0530

    Switch to alpine

--- deploy/docker/route-tests/entrypoint.sh
+++ deploy/docker/route-tests/entrypoint.sh
@@ -14,28 +14,28 @@ new-spec() {
 
 reload-caddy() {
   sed -i 's/127.0.0.1:{args\[0]}/127.0.0.1:5050/' "$TMP/Caddyfile"
-  /opt/caddy/caddy fmt --overwrite "$TMP/Caddyfile"
-  /opt/caddy/caddy reload --config "$TMP/Caddyfile"
+  caddy fmt --overwrite "$TMP/Caddyfile"
+  caddy reload --config "$TMP/Caddyfile"
   sleep 1
 }
 
 run-hurl() {
-  /opt/hurl/hurl --test \
+  hurl --test \
     --resolve local.com:80:127.0.0.1 \
     --resolve custom-domain.com:80:127.0.0.1 \
     --resolve custom-domain.com:443:127.0.0.1 \
     "$@"
 }
 
-if [[ "${OPEN_SHELL-}" == 1 ]]; then
+#if [[ "${OPEN_SHELL-}" == 1 ]]; then
   # Open shell for debugging after this script is done.
   trap bash EXIT
-fi
+#fi
 
 echo
-echo "caddy version: $(/opt/caddy/caddy --version)"
-echo "hurl version: $(/opt/hurl/hurl --version)"
-echo "mkcert version: $(/opt/mkcert --version)"
+echo "caddy version: $(caddy --version)"
+echo "hurl version: $(hurl --version)"
+echo "mkcert version: $(mkcert --version)"
 echo
 
 export TMP=/tmp/appsmith
@@ -43,18 +43,19 @@ export WWW_PATH="$TMP/www"
 
 mkdir -p "$WWW_PATH"
 echo -n 'index.html body' > "$WWW_PATH/index.html"
+mkcert -install
 
 # Start echo server
 (
   export XDG_DATA_HOME="$TMP/echo-data"
   export XDG_CONFIG_HOME="$TMP/echo-conf"
   mkdir -p "$XDG_DATA_HOME" "$XDG_CONFIG_HOME"
-  /opt/caddy/caddy start --config echo.caddyfile --adapter caddyfile >> "$TMP/echo-caddy.log" 2>&1
+  caddy start --config echo.caddyfile --adapter caddyfile >> "$TMP/echo-caddy.log" 2>&1
 )
 
 # Start Caddy for use with our config to test
 echo localhost > "$TMP/Caddyfile"
-/opt/caddy/caddy start --config "$TMP/Caddyfile" >> "$TMP/caddy.log" 2>&1
+caddy start --config "$TMP/Caddyfile" >> "$TMP/caddy.log" 2>&1
 
 sleep 1
 
@@ -69,14 +70,14 @@ new-spec "Spec 2: With a custom domain, cert obtained (because of internal CA)"
 export APPSMITH_CUSTOM_DOMAIN=custom-domain.com
 node /caddy-reconfigure.mjs
 #sed -i '2i acme_ca https://acme-staging-v02.api.letsencrypt.org/directory' "$TMP/Caddyfile"
-sed -i '/^https:\/\//a tls internal' "$TMP/Caddyfile"
+sed -i '/https:\/\/'"$APPSMITH_CUSTOM_DOMAIN"' {$/a tls internal' "$TMP/Caddyfile"
 reload-caddy
 run-hurl common/*.hurl common-https/*.hurl spec-2/*.hurl
 
 
 new-spec "Spec 3: With a custom domain, certs given in ssl folder"
 export APPSMITH_CUSTOM_DOMAIN=custom-domain.com
-/opt/mkcert -cert-file "/appsmith-stacks/ssl/fullchain.pem" -key-file "/appsmith-stacks/ssl/privkey.pem" "$APPSMITH_CUSTOM_DOMAIN"
+mkcert -cert-file "/appsmith-stacks/ssl/fullchain.pem" -key-file "/appsmith-stacks/ssl/privkey.pem" "$APPSMITH_CUSTOM_DOMAIN"
 node /caddy-reconfigure.mjs
 reload-caddy
 run-hurl common/*.hurl spec-3/*.hurl
--- deploy/docker/route-tests/run.sh
+++ deploy/docker/route-tests/run.sh
@@ -4,11 +4,13 @@ set -o errexit
 set -o nounset
 
 loc="$(dirname "$0")"
-docker build -f "$loc/Dockerfile" --tag ar "$loc/.."
+#docker build -f "$loc/Dockerfile" --tag ar "$loc/.."
 docker run \
   --name ar \
   --rm \
   -it \
+  --hostname ar \
+  -e OPEN_SHELL=${OPEN_SHELL-} \
   --volume "$loc/../fs/opt/appsmith/caddy-reconfigure.mjs:/caddy-reconfigure.mjs:ro" \
   --volume "$loc:/code:ro" \
   ar
