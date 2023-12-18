af1c3be1bc0eb45c998498b306cea053667c21ea
Hetu Nandu <hetu@appsmith.com>
2023-12-18 16:10:25 +0530

    chore: Editor Pane Segments completion (#29688)
    Completes the List and Add states of Queries / JS / UI of Page Pane
    Segments
    
    fixes #29081
    fixes #29501 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
      - Introduced new URL building functions for widgets and queries.
      - Added new UI elements for creating widgets and queries.
      - Implemented new routing paths for widget and query creation.
    
    - **Enhancements**
      - Updated text labels for clarity in various components.
      - Improved sorting logic for page pane data.
      - Enhanced global search with additional display titles.
    
    - **Bug Fixes**
      - Corrected styling inconsistencies in the IDE's main pane.
      - Fixed routing issues for adding new queries and widgets.
    
    - **Documentation**
      - Updated button text to reflect new features for end-users.
    
    - **Style**
      - Applied new styles for the search bar and other UI components.
    
    Please note that some internal code changes and refactorings have been
    omitted from these notes to focus on user-facing features and
    improvements.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/pages/Editor/IDE/PagesPane/JS_Section.tsx
+++ app/client/src/pages/Editor/IDE/PagesPane/JS_Section.tsx
@@ -15,10 +15,12 @@ import { useFeatureFlag } from "utils/hooks/useFeatureFlag";
 import { FEATURE_FLAG } from "@appsmith/entities/FeatureFlag";
 import { getHasCreateActionPermission } from "@appsmith/utils/BusinessFeatures/permissionPageHelpers";
 import { createNewJSCollection } from "actions/jsPaneActions";
+import { createMessage, PAGES_PANE_TEXTS } from "@appsmith/constants/messages";
 
 const JSContainer = styled(Flex)`
   & .t--entity-item {
-    grid-template-columns: 4px auto 1fr auto auto auto auto auto;
+    grid-template-columns: 0 auto 1fr auto auto auto auto auto;
+    height: 32px;
 
     & .t--entity-name {
       padding-left: var(--ads-v2-spaces-3);
@@ -48,11 +50,11 @@ const JSSection = () => {
 
   return (
     <JSContainer
-      className="ide-pages-pane__content-queries"
+      className="ide-pages-pane__content-js"
       flexDirection="column"
       gap="spaces-3"
-      overflow="scroll"
-      padding="spaces-4"
+      overflow="hidden"
+      padding="spaces-3"
     >
       {canCreateActions && (
         <Button
@@ -61,24 +63,26 @@ const JSSection = () => {
           size={"sm"}
           startIcon={"add-line"}
         >
-          New JS object
+          {createMessage(PAGES_PANE_TEXTS.js_add_button)}
         </Button>
       )}
-      {JSObjects &&
-        JSObjects.map((JSobject) => {
-          return (
-            <Flex flexDirection={"column"} key={JSobject.id}>
-              <ExplorerJSCollectionEntity
-                id={JSobject.id}
-                isActive={JSobject.id === activeActionId}
-                key={JSobject.id}
-                searchKeyword={""}
-                step={2}
-                type={JSobject.type as PluginType}
-              />
-            </Flex>
-          );
-        })}
+      <Flex flex="1" flexDirection="column" gap="spaces-2" overflow="scroll">
+        {JSObjects &&
+          JSObjects.map((JSobject) => {
+            return (
+              <Flex flexDirection={"column"} key={JSobject.id}>
+                <ExplorerJSCollectionEntity
+                  id={JSobject.id}
+                  isActive={JSobject.id === activeActionId}
+                  key={JSobject.id}
+                  searchKeyword={""}
+                  step={2}
+                  type={JSobject.type as PluginType}
+                />
+              </Flex>
+            );
+          })}
+      </Flex>
 
       {!JSObjects ||
         (JSObjects.length === 0 && (
