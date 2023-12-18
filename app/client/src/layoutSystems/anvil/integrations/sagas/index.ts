03a03ba0eff17746e6f1e6fb10fc62e8cba1a016
Preet <preetsidhu.bits@gmail.com>
2023-12-18 12:55:19 -0500

    add stepper functionality

--- app/client/src/layoutSystems/anvil/integrations/sagas/index.ts
+++ app/client/src/layoutSystems/anvil/integrations/sagas/index.ts
@@ -1,8 +1,10 @@
 import { fork } from "redux-saga/effects";
 import anvilDraggingSagas from "./anvilDraggingSagas";
 import LayoutElementPositionsSaga from "./LayoutElementPositionsSaga";
+import anvilSectionSagas from "./sectionSagas";
 
 export default function* anvilSagas() {
   yield fork(LayoutElementPositionsSaga);
   yield fork(anvilDraggingSagas);
+  yield fork(anvilSectionSagas);
 }
--- app/client/src/layoutSystems/anvil/utils/constants.ts
+++ app/client/src/layoutSystems/anvil/utils/constants.ts
@@ -1,5 +1,5 @@
 import { FlexLayerAlignment } from "layoutSystems/common/utils/constants";
-import type { HighlightPayload } from "./anvilTypes";
+import type { HighlightPayload, HighlightRenderInfo } from "./anvilTypes";
 
 export const MOBILE_BREAKPOINT = 480;
 
@@ -18,3 +18,16 @@ export const defaultHighlightPayload: HighlightPayload = {
   highlights: [],
   skipEntity: false,
 };
+
+export const defaultHighlightRenderInfo: HighlightRenderInfo = {
+  dropZone: {},
+  height: 0,
+  isVertical: false,
+  posX: 0,
+  posY: 0,
+  width: 0,
+};
+
+// Constants for the minimum and maximum zone count
+export const MIN_ZONE_COUNT = 1;
+export const MAX_ZONE_COUNT = 4;
