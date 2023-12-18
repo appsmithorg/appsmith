666a5a5dc04032ef50f68fc7eff32a942d8c2f73
Preet <preetsidhu.bits@gmail.com>
2023-12-18 13:41:18 -0500

    move anvil checks to separate file

--- app/client/src/layoutSystems/anvil/integrations/sagas/index.ts
+++ app/client/src/layoutSystems/anvil/integrations/sagas/index.ts
@@ -1,8 +1,10 @@
 import { fork } from "redux-saga/effects";
 import anvilDraggingSagas from "./anvilDraggingSagas";
 import LayoutElementPositionsSaga from "./LayoutElementPositionsSaga";
+import anvilChecksSagas from "./anvilChecksSagas";
 
 export default function* anvilSagas() {
   yield fork(LayoutElementPositionsSaga);
   yield fork(anvilDraggingSagas);
+  yield fork(anvilChecksSagas);
 }
