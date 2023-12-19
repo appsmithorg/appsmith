6b6c9c9770d80e2aafcf2fa6c79432f4f1470453
Preet Sidhu <preetsidhu.bits@gmail.com>
2023-12-19 14:16:17 -0500

    temp: crash fix

--- app/client/src/widgets/wds/WDSButtonGroupWidget/config/anvilConfig.ts
+++ app/client/src/widgets/wds/WDSButtonGroupWidget/config/anvilConfig.ts
@@ -5,7 +5,7 @@ export const anvilConfig: AnvilConfig = {
   isLargeWidget: false,
   widgetSize: (props: ButtonGroupWidgetProps): SizeConfig => {
     let minWidth = 120;
-    const buttonLength = Object.keys(props.buttonsList).length;
+    const buttonLength = Object.keys(props?.buttonsList ?? 3).length;
     if (props.orientation === "horizontal") {
       // 120 is the width of the button, 8 is widget padding, 1 is the gap between buttons
       minWidth = 120 * buttonLength + 8 + (buttonLength - 1) * 1;
