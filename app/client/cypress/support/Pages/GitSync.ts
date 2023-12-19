1e3cfcf7d5690cdb846e3223eb6fa5a16abe3d1e
Aishwarya UR <aishwarya@appsmith.com>
2023-12-19 19:23:33 +0530

    trial

--- app/client/cypress/support/Pages/GitSync.ts
+++ app/client/cypress/support/Pages/GitSync.ts
@@ -360,7 +360,7 @@ export class GitSync {
     this.agHelper.AssertElementExist(this._bottomBarCommit);
     cy.waitUntil(
       () => {
-        this.agHelper.GetNClick(this._branchButton);
+        this.agHelper.GetElement(this._branchButton, "noVerify").click();
         if (this.agHelper.IsElementVisible(this._branchSearchInput)) {
           // If visible, return true to stop waiting
           return true;
