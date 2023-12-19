2e7b7648142593ace27676c07eea5ae59a2e1b45
Preet <preetsidhu.bits@gmail.com>
2023-12-19 12:47:13 -0500

    remove commented code

--- app/client/src/selectors/layoutSystemSelectors.ts
+++ app/client/src/selectors/layoutSystemSelectors.ts
@@ -1,19 +1,8 @@
-import type { AppState } from "@appsmith/reducers";
 import { LayoutSystemTypes } from "layoutSystems/types";
 
 /**
  * selector to fetch the application's layout type
  */
-export const getLayoutSystemType = (state: AppState) => {
+export const getLayoutSystemType = () => {
   return LayoutSystemTypes.ANVIL;
-  if (
-    state.ui.applications?.currentApplication?.applicationDetail?.appPositioning
-      ?.type
-  ) {
-    return LayoutSystemTypes[
-      state.ui.applications.currentApplication?.applicationDetail
-        ?.appPositioning?.type
-    ];
-  }
-  return LayoutSystemTypes.FIXED;
 };
