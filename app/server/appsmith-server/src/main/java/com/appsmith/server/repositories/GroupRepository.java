9efdb787e1444c4e95141487978303a65099b643
Shrikant Sharat Kandula <shrikant@appsmith.com>
2023-12-20 11:34:46 +0530

    chore: Cleanup of some deprecated objects (#29577)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.appsmith.server.repositories;
-
-import com.appsmith.server.repositories.ce.GroupRepositoryCE;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GroupRepository extends GroupRepositoryCE, CustomGroupRepository {}
