bc76490785f3d73adfb22abccf23f4c8ca54f389
Hetu Nandu <hetu@appsmith.com>
2023-12-20 12:26:16 +0530

    fix: Widget Context Switching (#29735)
    ## Description
    Fixes context switching issues related to Widget List ensuring it also
    works for current prod
    
    
    #### PR fixes following issue(s)
    Fixes #29695
    
    
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
      - Added a search functionality to the app.
    - Integrated a search bar at the top of the `Hero` component and a
    `Search` component to the `App` component.
      - Included styles for the search bar in the application.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/navigation/FocusUtils.ts
+++ app/client/src/navigation/FocusUtils.ts
@@ -1,30 +1,38 @@
 import type { FocusEntityInfo } from "./FocusEntity";
 import { FocusEntity, identifyEntityFromPath } from "./FocusEntity";
 import {
-  builderURL,
   datasourcesEditorURL,
   jsCollectionListURL,
   queryListURL,
+  widgetListURL,
 } from "@appsmith/RouteBuilder";
 
 export const getEntityParentUrl = (
   entityInfo: FocusEntityInfo,
   parentEntity: FocusEntity,
 ): string => {
-  if (parentEntity === FocusEntity.CANVAS) {
-    const canvasUrl = builderURL({ pageId: entityInfo.pageId ?? "" });
-    return canvasUrl.split("?")[0];
+  let parentUrl: string = "";
+  if (parentEntity === FocusEntity.WIDGET_LIST) {
+    parentUrl = widgetListURL({
+      pageId: entityInfo.pageId,
+    });
   }
   if (parentEntity === FocusEntity.DATASOURCE_LIST) {
-    return datasourcesEditorURL({ pageId: entityInfo.pageId });
+    parentUrl = datasourcesEditorURL({
+      pageId: entityInfo.pageId,
+    });
   }
   if (parentEntity === FocusEntity.JS_OBJECT_LIST) {
-    return jsCollectionListURL({ pageId: entityInfo.pageId });
+    parentUrl = jsCollectionListURL({
+      pageId: entityInfo.pageId,
+    });
   }
   if (parentEntity === FocusEntity.QUERY_LIST) {
-    return queryListURL({ pageId: entityInfo.pageId });
+    parentUrl = queryListURL({
+      pageId: entityInfo.pageId,
+    });
   }
-  return "";
+  return parentUrl.split("?")[0];
 };
 export const isPageChange = (prevPath: string, currentPath: string) => {
   const prevFocusEntityInfo = identifyEntityFromPath(prevPath);
@@ -34,27 +42,3 @@ export const isPageChange = (prevPath: string, currentPath: string) => {
   }
   return prevFocusEntityInfo.pageId !== currFocusEntityInfo.pageId;
 };
-
-export const isAppStateChange = (prevPath: string, currentPath: string) => {
-  const prevFocusEntityInfo = identifyEntityFromPath(prevPath);
-  const currFocusEntityInfo = identifyEntityFromPath(currentPath);
-  return prevFocusEntityInfo.appState !== currFocusEntityInfo.appState;
-};
-
-/**
- * Method to indicate if the URL is of type API, Query etc.,
- * and not anything else
- * @param path
- * @returns
- */
-export function shouldStorePageURLForFocus(path: string) {
-  const entityTypesToStore = [
-    FocusEntity.QUERY,
-    FocusEntity.API,
-    FocusEntity.JS_OBJECT,
-  ];
-
-  const entity = identifyEntityFromPath(path)?.entity;
-
-  return entityTypesToStore.indexOf(entity) >= 0;
-}
--- app/client/src/sagas/ContextSwitchingSaga.ts
+++ app/client/src/sagas/ContextSwitchingSaga.ts
@@ -22,14 +22,8 @@ import type { Action } from "entities/Action";
 import { getAction, getPlugin } from "@appsmith/selectors/entitiesSelector";
 import type { Plugin } from "api/PluginApi";
 import { getCurrentGitBranch } from "selectors/gitSyncSelectors";
-import {
-  getEntityParentUrl,
-  isAppStateChange,
-  isPageChange,
-} from "../navigation/FocusUtils";
+import { getEntityParentUrl, isPageChange } from "../navigation/FocusUtils";
 import { EditorState } from "../entities/IDE/constants";
-import { getCurrentApplicationId } from "../selectors/editorSelectors";
-import { get } from "lodash";
 
 /**
  * Context switching works by restoring the states of ui elements to as they were
@@ -54,7 +48,7 @@ export function* contextSwitchingSaga(
     const storePaths: Array<{
       key: string;
       entityInfo: FocusEntityInfo;
-    }> = yield call(getEntitiesForStore, previousPath, currentPath);
+    }> = yield call(getEntitiesForStore, previousPath);
     for (const storePath of storePaths) {
       yield call(
         storeStateOfPath,
@@ -180,43 +174,22 @@ function shouldSetState(
   }
   const prevFocusEntityInfo = identifyEntityFromPath(prevPath);
   const currFocusEntityInfo = identifyEntityFromPath(currPath);
+  const isSamePage = !isPageChange(prevPath, currPath);
 
   // While switching from selected widget state to canvas,
   // it should not be restored stored state for canvas
   return !(
     prevFocusEntityInfo.entity === FocusEntity.PROPERTY_PANE &&
-    currFocusEntityInfo.entity === FocusEntity.CANVAS &&
-    prevFocusEntityInfo.pageId === currFocusEntityInfo.pageId
+    (currFocusEntityInfo.entity === FocusEntity.WIDGET_LIST ||
+      currFocusEntityInfo.entity === FocusEntity.CANVAS) &&
+    isSamePage
   );
 }
 
-function* getEntitiesForStore(previousPath: string, currentPath: string) {
+function* getEntitiesForStore(previousPath: string) {
   const branch: string | undefined = yield select(getCurrentGitBranch);
   const entities: Array<{ entityInfo: FocusEntityInfo; key: string }> = [];
   const prevFocusEntityInfo = identifyEntityFromPath(previousPath);
-  if (isAppStateChange(previousPath, currentPath)) {
-    const currentAppId: string = yield select(getCurrentApplicationId);
-    entities.push({
-      key: `${prevFocusEntityInfo.appState}.${currentAppId}#${branch}`,
-      entityInfo: {
-        entity: FocusEntity.APP_STATE,
-        id: prevFocusEntityInfo.appState,
-        appState: prevFocusEntityInfo.appState,
-      },
-    });
-  }
-  if (isPageChange(previousPath, currentPath)) {
-    if (prevFocusEntityInfo.pageId) {
-      entities.push({
-        key: `${prevFocusEntityInfo.pageId}#${branch}`,
-        entityInfo: {
-          entity: FocusEntity.PAGE,
-          id: prevFocusEntityInfo.pageId,
-          appState: EditorState.EDITOR,
-        },
-      });
-    }
-  }
 
   if (prevFocusEntityInfo.entity in FocusStoreHierarchy) {
     const parentEntity = FocusStoreHierarchy[prevFocusEntityInfo.entity];
@@ -232,6 +205,18 @@ function* getEntitiesForStore(previousPath: string, currentPath: string) {
         key: `${parentPath}#${branch}`,
       });
     }
+  }
+
+  if (prevFocusEntityInfo.appState === EditorState.EDITOR) {
+    entities.push({
+      entityInfo: {
+        entity: FocusEntity.EDITOR,
+        id: `EDITOR.${prevFocusEntityInfo.pageId}`,
+        pageId: prevFocusEntityInfo.pageId,
+        appState: EditorState.EDITOR,
+      },
+      key: `EDITOR_STATE.${prevFocusEntityInfo.pageId}#${branch}`,
+    });
   }
 
   entities.push({
@@ -254,42 +239,21 @@ function* getEntitiesForSet(
   }
   const branch: string | undefined = yield select(getCurrentGitBranch);
   const entities: Array<{ entityInfo: FocusEntityInfo; key: string }> = [];
+  const prevEntityInfo = identifyEntityFromPath(previousPath);
   const currentEntityInfo = identifyEntityFromPath(currentPath);
   if (
-    isAppStateChange(previousPath, currentPath) &&
-    state?.invokedBy === NavigationMethod.AppSidebar
+    currentEntityInfo.entity === FocusEntity.CANVAS &&
+    (prevEntityInfo.pageId !== currentEntityInfo.pageId ||
+      prevEntityInfo.appState !== currentEntityInfo.appState)
   ) {
-    const currentAppId: string = yield select(getCurrentApplicationId);
-    const key = `${currentEntityInfo.appState}.${currentAppId}.${currentEntityInfo.pageId}#${branch}`;
     entities.push({
-      key,
+      key: `EDITOR_STATE.${currentEntityInfo.pageId}#${branch}`,
       entityInfo: {
-        entity: FocusEntity.APP_STATE,
-        id: currentEntityInfo.appState,
-        appState: currentEntityInfo.appState,
+        id: `EDITOR.${currentEntityInfo.pageId}`,
+        appState: EditorState.EDITOR,
+        entity: FocusEntity.EDITOR,
       },
     });
-    const focusHistory: FocusState = yield select(getCurrentFocusInfo, key);
-    if (get(focusHistory, "state.AppUrl")) {
-      return entities;
-    }
-  }
-  if (isPageChange(previousPath, currentPath)) {
-    const key = `${currentEntityInfo.pageId}#${branch}`;
-    if (currentEntityInfo.pageId) {
-      entities.push({
-        key,
-        entityInfo: {
-          entity: FocusEntity.PAGE,
-          id: currentEntityInfo.pageId,
-          appState: EditorState.EDITOR,
-        },
-      });
-      const focusHistory: FocusState = yield select(getCurrentFocusInfo, key);
-      if (get(focusHistory, "state.PageUrl")) {
-        return entities;
-      }
-    }
   }
 
   entities.push({
