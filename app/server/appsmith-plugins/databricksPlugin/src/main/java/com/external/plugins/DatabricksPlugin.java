2241369b5ff2d6007dd239d2c68a9eb714115d98
Trisha Anand <trisha@appsmith.com>
2023-12-20 22:01:14 +0530

    Working version with non-reviewed code

--- app/server/appsmith-plugins/databricksPlugin/src/main/java/com/external/plugins/DatabricksPlugin.java
+++ app/server/appsmith-plugins/databricksPlugin/src/main/java/com/external/plugins/DatabricksPlugin.java
@@ -6,13 +6,30 @@ import com.appsmith.external.models.DatasourceConfiguration;
 import com.appsmith.external.plugins.BasePlugin;
 import com.appsmith.external.plugins.PluginExecutor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.ObjectUtils;
 import org.pf4j.Extension;
 import org.pf4j.PluginWrapper;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.appsmith.external.helpers.PluginUtils.getColumnsListForJdbcPlugin;
 
 public class DatabricksPlugin extends BasePlugin {
+
+    private static final String JDBC_DRIVER = "com.databricks.client.jdbc.Driver";
 
     public DatabricksPlugin(PluginWrapper wrapper) {
         super(wrapper);
@@ -20,17 +37,89 @@ public class DatabricksPlugin extends BasePlugin {
 
     @Slf4j
     @Extension
-    public static class DatabricksPluginExecutor implements PluginExecutor<Object> {
+    public static class DatabricksPluginExecutor implements PluginExecutor<Connection> {
         @Override
         public Mono<ActionExecutionResult> execute(
-                Object connection,
+                Connection connection,
                 DatasourceConfiguration datasourceConfiguration,
                 ActionConfiguration actionConfiguration) {
-            return null;
+            String query = actionConfiguration.getBody();
+
+            List<Map<String, Object>> rowsList = new ArrayList<>(50);
+            final List<String> columnsList = new ArrayList<>();
+
+            return Mono.fromCallable(() -> {
+                        try {
+                            Statement statement = connection.createStatement();
+                            ResultSet resultSet = statement.executeQuery(query);
+
+                            ResultSetMetaData metaData = resultSet.getMetaData();
+                            int colCount = metaData.getColumnCount();
+                            columnsList.addAll(getColumnsListForJdbcPlugin(metaData));
+
+                            while (resultSet.next()) {
+                                // Use `LinkedHashMap` here so that the column ordering is preserved in the response.
+                                Map<String, Object> row = new LinkedHashMap<>(colCount);
+
+                                for (int i = 1; i <= colCount; i++) {
+                                    Object value;
+                                    final String typeName = metaData.getColumnTypeName(i);
+
+                                    if (resultSet.getObject(i) == null) {
+                                        value = null;
+                                    } else {
+                                        value = resultSet.getObject(i);
+                                    }
+
+                                    row.put(metaData.getColumnName(i), value);
+                                }
+
+                                rowsList.add(row);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        ActionExecutionResult result = new ActionExecutionResult();
+                        result.setBody(objectMapper.valueToTree(rowsList));
+                        result.setIsExecutionSuccess(true);
+                        return Mono.just(result);
+                    })
+                    .flatMap(obj -> obj)
+                    .subscribeOn(Schedulers.boundedElastic());
         }
 
         @Override
-        public void datasourceDestroy(Object connection) {}
+        public Mono<Connection> datasourceCreate(DatasourceConfiguration datasourceConfiguration) {
+
+            // Set up the connection URL
+            StringBuilder urlBuilder = new StringBuilder("jdbc:databricks://");
+
+            List<String> hosts = datasourceConfiguration.getEndpoints().stream()
+                    .map(endpoint -> endpoint.getHost() + ":" + ObjectUtils.defaultIfNull(endpoint.getPort(), 443L))
+                    .collect(Collectors.toList());
+
+            urlBuilder.append(String.join(",", hosts)).append(";");
+
+            String url = urlBuilder.toString();
+
+            Properties p = new java.util.Properties();
+            p.put("httpPath", datasourceConfiguration.getProperties().get(0).getValue());
+            p.put("AuthMech", "3");
+            p.put("UID", "token");
+            p.put("PWD", datasourceConfiguration.getProperties().get(1).getValue());
+
+            try {
+                Class.forName(JDBC_DRIVER);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            return Mono.fromCallable(() -> DriverManager.getConnection(url, p));
+        }
+
+        @Override
+        public void datasourceDestroy(Connection connection) {}
 
         @Override
         public Set<String> validateDatasource(DatasourceConfiguration datasourceConfiguration) {
