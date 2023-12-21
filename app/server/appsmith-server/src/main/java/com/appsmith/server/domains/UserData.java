e72da1151fa8b19ebf729dcd97f6a02a75cafcc1
Aman Agarwal <aman@appsmith.com>
2023-12-21 13:11:07 +0530

    fix: deprecated the role property

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/UserData.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/UserData.java
@@ -33,6 +33,7 @@ public class UserData extends BaseDomain {
 
     // Role of the user in their workspace, example, Designer, Developer, Product Lead etc.
     @JsonView(Views.Public.class)
+    @Deprecated
     private String role;
 
     // The development proficiency of the user for example, Beginner, Novice, Intermediate, Advanced.
