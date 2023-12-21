56715832f5edc4998ee3935dbc0b78c0ce0db5ca
manish kumar <manish@appsmith.com>
2023-12-21 15:33:53 +0530

    added more changes

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ce/ImportableContextJsonCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ce/ImportableContextJsonCE.java
@@ -3,13 +3,5 @@ package com.appsmith.server.dtos.ce;
 import com.appsmith.server.constants.ImportableJsonType;
 
 public abstract class ImportableContextJsonCE {
-
-    /**
-     * These are variables for demonstration only, it might change as we decide more on implementation
-     */
-    public int clientSchemaVersion;
-
-    public int serverSchemaVersion;
-
     public abstract ImportableJsonType getImportableJsonType();
 }
