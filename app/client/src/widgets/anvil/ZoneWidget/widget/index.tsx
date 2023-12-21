9d8baf8ae5a5238ed7257bb7683080dc753ddcda
Preet <preetsidhu.bits@gmail.com>
2023-12-21 14:02:22 -0500

    minor update

--- app/client/src/widgets/anvil/ZoneWidget/widget/index.tsx
+++ app/client/src/widgets/anvil/ZoneWidget/widget/index.tsx
@@ -15,10 +15,8 @@ import {
   propertyPaneContent,
   propertyPaneStyle,
 } from "./config";
-import BaseWidget, {
-  type WidgetProps,
-  type WidgetState,
-} from "widgets/BaseWidget";
+import BaseWidget from "widgets/BaseWidget";
+import type { WidgetProps, WidgetState } from "widgets/BaseWidget";
 import type { LayoutProps } from "layoutSystems/anvil/utils/anvilTypes";
 import type { ContainerWidgetProps } from "widgets/ContainerWidget/widget";
 import ContainerComponent from "widgets/ContainerWidget/component";
