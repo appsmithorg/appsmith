1a1ee3d643a3d29b8d02a213caeec186817d17bb
Sharanya <sharanya@appsmith.com>
2023-12-25 15:12:26 +0530

    prettier fix

--- app/client/cypress/e2e/Regression/ClientSide/JSLibrary/Library_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/JSLibrary/Library_spec.ts
@@ -93,12 +93,12 @@ describe(
       }
     });
 
-  it("6. Checks installation in exported/forked app", () => {
-    homePage.NavigateToHome();
-    homePage.ImportApp("library_export.json");
-    homePage.AssertImportToast(0);
-    agHelper.ValidateToastMessage("true");
-    agHelper.WaitUntilAllToastsDisappear();
+    it("6. Checks installation in exported/forked app", () => {
+      homePage.NavigateToHome();
+      homePage.ImportApp("library_export.json");
+      homePage.AssertImportToast(0);
+      agHelper.ValidateToastMessage("true");
+      agHelper.WaitUntilAllToastsDisappear();
 
       //Checks installation in forked app
       homePage.NavigateToHome();
