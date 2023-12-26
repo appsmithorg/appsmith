011cc46bf54ce821b19b089d4833c4777640734f
Sharanya <sharanya@appsmith.com>
2023-12-26 15:08:35 +0530

    added tag.widget to map spec and set Canvas_Context_Property_Pane_1_spec.js to excludeforarigap

--- app/client/cypress/manual_TestSuite/CommentedScriptFiles/Map_spec.js
+++ app/client/cypress/manual_TestSuite/CommentedScriptFiles/Map_spec.js
@@ -6,7 +6,7 @@ import * as _ from "../../support/Objects/ObjectsCore";
 if (Cypress.env("APPSMITH_GOOGLE_MAPS_API_KEY")) {
   describe(
     "Map Widget Functionality",
-    { tags: ["@tag.Maps", "@tag.excludeForAirgap"] },
+    { tags: ["@tag.Maps", "@tag.Widget", "@tag.excludeForAirgap"] },
     function () {
       before(() => {
         _.agHelper.AddDsl("Mapdsl");
