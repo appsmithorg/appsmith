44bf505b3c824f7651e5fb0ef744ab1dff44f105
Nayan <nayan@appsmith.com>
2023-12-26 11:42:28 +0600

    updated the migration, get permissions from default permission groups

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration041AddPermissionsForGitOperations.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration041AddPermissionsForGitOperations.java
@@ -1,13 +1,15 @@
 package com.appsmith.server.migrations.db.ce;
 
+import com.appsmith.external.models.BaseDomain;
 import com.appsmith.external.models.Policy;
 import com.appsmith.external.models.QBaseDomain;
 import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.constants.FieldName;
 import com.appsmith.server.domains.Application;
+import com.appsmith.server.domains.PermissionGroup;
 import com.appsmith.server.domains.QApplication;
+import com.appsmith.server.domains.QWorkspace;
 import com.appsmith.server.domains.Workspace;
-import com.appsmith.server.repositories.ce.BaseAppsmithRepositoryCEImpl;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
@@ -18,11 +20,13 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.appsmith.server.constants.ce.FieldNameCE.ADMINISTRATOR;
+import static com.appsmith.server.constants.ce.FieldNameCE.DEVELOPER;
 import static com.appsmith.server.migrations.db.ce.Migration040TagWorkspacesForGitOperationsPermissionMigration.MIGRATION_FLAG_WORKSPACE_WITHOUT_GIT_PERMISSIONS;
+import static com.appsmith.server.repositories.ce.BaseAppsmithRepositoryCEImpl.fieldName;
 
 @Slf4j
 @ChangeUnit(order = "041", id = "add-permissions-for-git-operations", author = " ")
@@ -53,21 +57,20 @@ public class Migration041AddPermissionsForGitOperations {
          4. Add the new policy for git permissions to set of application.policies and save application
          5. Unset the flag for the workspace and save when all the applications are migrated
         */
-        String policiesFieldPath = BaseAppsmithRepositoryCEImpl.fieldName(QBaseDomain.baseDomain.policies);
-        String idFieldPath = BaseAppsmithRepositoryCEImpl.fieldName(QBaseDomain.baseDomain.id);
-        String workspaceIdFieldPath = BaseAppsmithRepositoryCEImpl.fieldName(QApplication.application.workspaceId);
+        String policiesFieldPath = fieldName(QBaseDomain.baseDomain.policies);
+        String idFieldPath = fieldName(QBaseDomain.baseDomain.id);
+        String workspaceIdFieldPath = fieldName(QApplication.application.workspaceId);
+        String defaultPermissionGroupsFieldPath = fieldName(QWorkspace.workspace.defaultPermissionGroups);
 
         int batchSize = 10000, updatedCount = 0, updatedCountInCurrentBatch;
 
         do {
             // get workspaces which are not deleted and are tagged for migration
-            Criteria criteria = Criteria.where(FieldName.DELETED_AT)
-                    .exists(false)
-                    .and(MIGRATION_FLAG_WORKSPACE_WITHOUT_GIT_PERMISSIONS)
+            Criteria criteria = Criteria.where(MIGRATION_FLAG_WORKSPACE_WITHOUT_GIT_PERMISSIONS)
                     .exists(true);
             Query selectWorkspacesQuery = Query.query(criteria).limit(batchSize);
             selectWorkspacesQuery.skip(updatedCount);
-            selectWorkspacesQuery.fields().include(policiesFieldPath); // we only need the policies
+            selectWorkspacesQuery.fields().include(defaultPermissionGroupsFieldPath); // we only need the policies
 
             // fetch the workspaes with the above criteria
             List<Workspace> workspaceList = mongoTemplate.find(selectWorkspacesQuery, Workspace.class);
@@ -77,36 +80,37 @@ public class Migration041AddPermissionsForGitOperations {
                 updatedCount++;
                 updatedCountInCurrentBatch++;
 
-                // get the permission groups from the parent of the new permissions i.e. WORKSPACE_CREATE_APPLICATION
-                Optional<Set<String>> optionalPermissionGroupsForManagePermission = workspace.getPolicies().stream()
-                        .filter(policy ->
-                                policy.getPermission().equals(AclPermission.WORKSPACE_CREATE_APPLICATION.getValue()))
-                        .map(Policy::getPermissionGroups)
-                        .findFirst();
+                // get the default permission groups for this workspace
+                Set<String> defaultPermissionGroups = workspace.getDefaultPermissionGroups();
+                Query permissionGroupsQuery =
+                        new Query(Criteria.where(idFieldPath).in(defaultPermissionGroups));
+                List<PermissionGroup> permissionGroups =
+                        mongoTemplate.find(permissionGroupsQuery, PermissionGroup.class);
 
-                if (optionalPermissionGroupsForManagePermission.isPresent()) {
-                    Set<String> parentPolicyPermissionGroups = optionalPermissionGroupsForManagePermission.get();
+                Set<String> adminAndDeveloperPermissionGroupIds = permissionGroups.stream()
+                        .filter(permissionGroup -> permissionGroup.getName().startsWith(ADMINISTRATOR)
+                                || permissionGroup.getName().startsWith(DEVELOPER))
+                        .map(BaseDomain::getId)
+                        .collect(Collectors.toSet());
 
-                    // fetch all the applications under this workspace
-                    Query selectApplicationQuery = Query.query(Criteria.where(FieldName.DELETED_AT)
-                            .exists(false)
-                            .and(workspaceIdFieldPath)
-                            .is(workspace.getId()));
-                    selectApplicationQuery.fields().include(policiesFieldPath); // we only need the policies
+                // fetch all the applications under this workspace
+                Query selectApplicationQuery = Query.query(Criteria.where(FieldName.DELETED_AT)
+                        .exists(false)
+                        .and(workspaceIdFieldPath)
+                        .is(workspace.getId()));
+                selectApplicationQuery.fields().include(policiesFieldPath); // we only need the policies
 
-                    List<Application> applicationList = mongoTemplate.find(selectApplicationQuery, Application.class);
-                    for (Application application : applicationList) {
-                        // add a new policy for git permissions and allow parentPolicyPermissionGroups
-                        addGitPoliciesToPolicySet(application.getPolicies(), parentPolicyPermissionGroups);
+                List<Application> applicationList = mongoTemplate.find(selectApplicationQuery, Application.class);
+                for (Application application : applicationList) {
+                    // add a new policy for git permissions and allow adminAndDeveloperPermissionGroupIds
+                    addGitPoliciesToPolicySet(application.getPolicies(), adminAndDeveloperPermissionGroupIds);
 
-                        // update the application object
-                        Update update = new Update();
-                        update.set(policiesFieldPath, application.getPolicies());
+                    // update the application object
+                    Update update = new Update();
+                    update.set(policiesFieldPath, application.getPolicies());
 
-                        Query updateQuery =
-                                Query.query(Criteria.where(idFieldPath).is(application.getId()));
-                        mongoTemplate.updateFirst(updateQuery, update, Application.class);
-                    }
+                    Query updateQuery = Query.query(Criteria.where(idFieldPath).is(application.getId()));
+                    mongoTemplate.updateFirst(updateQuery, update, Application.class);
                 }
 
                 // all the applications of this workspace have been migrated, unset the flag for this workspace
