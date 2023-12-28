ffbf6a7e25400b23c627574849c65921d2ef1754
Dipyaman Biswas <dipyaman@appsmith.com>
2023-12-29 03:56:11 +0530

    fix: set timeout to 50

--- app/client/cypress/support/Pages/HomePage.ts
+++ app/client/cypress/support/Pages/HomePage.ts
@@ -454,7 +454,7 @@ export class HomePage {
     this.agHelper.GetNClick(this._backToEditor);
     this.agHelper.AssertElementAbsence(this.locator._loading);
     this.assertHelper.AssertNetworkStatus("getWorkspace");
-    this.agHelper.AssertElementAbsence(this.locator._btnSpinner, 70000);
+    this.agHelper.AssertElementAbsence(this.locator._btnSpinner, 50000);
   }
 
   public EditAppFromAppHover(appName = "") {
