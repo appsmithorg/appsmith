eeced9e1c80eefd98221e2084d260a558c1e0fec
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-01-04 15:55:27 +0530

    Revert "feat: show only nearest vertical highlights."
    This reverts commit e198bd840c2345eb1b75c1cb71bdf4b42bc1fd29.

--- app/client/src/layoutSystems/anvil/canvasArenas/hooks/utils.ts
+++ app/client/src/layoutSystems/anvil/canvasArenas/hooks/utils.ts
@@ -168,26 +168,11 @@ export const getVerticalHighlights = (
   const verticalHighlights = highlights.filter(
     (highlight: AnvilHighlightInfo) => highlight.isVertical,
   );
-  const allValidVerticalHighlights = verticalHighlights
-    .filter((highlight: AnvilHighlightInfo) => {
-      return (
-        pos.y >= highlight.posY && pos.y <= highlight.posY + highlight.height
-      );
-    })
-    .sort((each1, each2) => each1.posX - each2.posX);
-  const allHighlightsLeftToPos = allValidVerticalHighlights.filter((each) => {
-    return each.posX < pos.x;
+  return verticalHighlights.filter((highlight: AnvilHighlightInfo) => {
+    return (
+      pos.y >= highlight.posY && pos.y <= highlight.posY + highlight.height
+    );
   });
-  const allHighlightsRightToPos = allValidVerticalHighlights.filter((each) => {
-    return each.posX > pos.x;
-  });
-  const nearestLeftHighlightToPos = allHighlightsLeftToPos.length
-    ? [allHighlightsLeftToPos[allHighlightsLeftToPos.length - 1]]
-    : [];
-  const nearestRightHighlightToPos = allHighlightsRightToPos.length
-    ? [allHighlightsRightToPos[0]]
-    : [];
-  return [...nearestLeftHighlightToPos, ...nearestRightHighlightToPos];
 };
 
 export function getViableDropPositions(
