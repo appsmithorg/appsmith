4032c46122918bff9de0d7f28aaff57d74dd49b7
Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>
2024-01-04 15:31:13 -0800

    chore: update parameters of PluginTriggerSolution constructor

--- app/server/appsmith-server/src/main/java/com/appsmith/server/plugins/solutions/PluginTriggerSolutionCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/plugins/solutions/PluginTriggerSolutionCEImpl.java
@@ -6,61 +6,31 @@ import com.appsmith.external.plugins.PluginExecutor;
 import com.appsmith.server.exceptions.AppsmithError;
 import com.appsmith.server.exceptions.AppsmithException;
 import com.appsmith.server.helpers.PluginExecutorHelper;
-import com.appsmith.server.plugins.base.PluginServiceImpl;
 import com.appsmith.server.repositories.PluginRepository;
-import com.appsmith.server.services.AnalyticsService;
-import com.appsmith.server.services.WorkspaceService;
 import com.appsmith.server.solutions.DatasourceTriggerSolution;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.validation.Validator;
 import org.apache.commons.lang3.StringUtils;
-import org.pf4j.PluginManager;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
 
 import java.util.Map;
 
 import static com.appsmith.server.constants.ce.FieldNameCE.DATASOURCE_ID;
 
 @Component
-public class PluginTriggerSolutionCEImpl extends PluginServiceImpl implements PluginTriggerSolutionCE {
+public class PluginTriggerSolutionCEImpl implements PluginTriggerSolutionCE {
     private final DatasourceTriggerSolution datasourceTriggerSolution;
     private final PluginExecutorHelper pluginExecutorHelper;
+    private final PluginRepository pluginRepository;
 
     public PluginTriggerSolutionCEImpl(
-            Scheduler scheduler,
-            Validator validator,
-            MongoConverter mongoConverter,
-            ReactiveMongoTemplate reactiveMongoTemplate,
-            PluginRepository repository,
-            AnalyticsService analyticsService,
-            WorkspaceService workspaceService,
-            PluginManager pluginManager,
-            ReactiveRedisTemplate<String, String> reactiveTemplate,
-            ChannelTopic topic,
-            ObjectMapper objectMapper,
             DatasourceTriggerSolution datasourceTriggerSolution,
-            PluginExecutorHelper pluginExecutorHelper) {
-        super(
-                scheduler,
-                validator,
-                mongoConverter,
-                reactiveMongoTemplate,
-                repository,
-                analyticsService,
-                workspaceService,
-                pluginManager,
-                reactiveTemplate,
-                topic,
-                objectMapper);
+            PluginExecutorHelper pluginExecutorHelper,
+            PluginRepository pluginRepository) {
+
         this.datasourceTriggerSolution = datasourceTriggerSolution;
         this.pluginExecutorHelper = pluginExecutorHelper;
+        this.pluginRepository = pluginRepository;
     }
 
     @Override
@@ -75,8 +45,9 @@ public class PluginTriggerSolutionCEImpl extends PluginServiceImpl implements Pl
             return datasourceTriggerSolution.trigger(datasourceId, environmentId, triggerRequestDTO);
         }
 
-        Mono<PluginExecutor> pluginExecutorMono =
-                this.findById(pluginId).flatMap(plugin -> pluginExecutorHelper.getPluginExecutor(Mono.just(plugin)));
+        Mono<PluginExecutor> pluginExecutorMono = pluginRepository
+                .findById(pluginId)
+                .flatMap(plugin -> pluginExecutorHelper.getPluginExecutor(Mono.just(plugin)));
 
         /*
          * Since there is no datasource provided, we are passing the Datasource Context connection and datasourceConfiguration as null.
--- app/server/appsmith-server/src/main/java/com/appsmith/server/plugins/solutions/PluginTriggerSolutionImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/plugins/solutions/PluginTriggerSolutionImpl.java
@@ -1,45 +1,16 @@
 package com.appsmith.server.plugins.solutions;
 
+import com.appsmith.server.helpers.PluginExecutorHelper;
 import com.appsmith.server.repositories.PluginRepository;
-import com.appsmith.server.services.AnalyticsService;
-import com.appsmith.server.services.WorkspaceService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.validation.Validator;
-import org.pf4j.PluginManager;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
+import com.appsmith.server.solutions.DatasourceTriggerSolution;
 import org.springframework.stereotype.Component;
-import reactor.core.scheduler.Scheduler;
 
 @Component
 public class PluginTriggerSolutionImpl extends PluginTriggerSolutionCEImpl implements PluginTriggerSolution {
     public PluginTriggerSolutionImpl(
-            Scheduler scheduler,
-            Validator validator,
-            MongoConverter mongoConverter,
-            ReactiveMongoTemplate reactiveMongoTemplate,
-            PluginRepository repository,
-            AnalyticsService analyticsService,
-            WorkspaceService workspaceService,
-            PluginManager pluginManager,
-            ReactiveRedisTemplate<String, String> reactiveTemplate,
-            ChannelTopic topic,
-            ObjectMapper objectMapper) {
-        super(
-                scheduler,
-                validator,
-                mongoConverter,
-                reactiveMongoTemplate,
-                repository,
-                analyticsService,
-                workspaceService,
-                pluginManager,
-                reactiveTemplate,
-                topic,
-                objectMapper,
-                datasourceTriggerSolution,
-                pluginExecutorHelper);
+            DatasourceTriggerSolution datasourceTriggerSolution,
+            PluginExecutorHelper pluginExecutorHelper,
+            PluginRepository pluginRepository) {
+        super(datasourceTriggerSolution, pluginExecutorHelper, pluginRepository);
     }
 }
