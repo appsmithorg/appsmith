57ac6f163df2e19c9c4244da691abbd028815735
Aishwarya UR <aishwarya@appsmith.com>
2024-01-09 16:59:11 +0530

    AutoFillWidgets_Reflow_spec fix

--- app/client/cypress/e2e/Regression/ClientSide/MobileResponsiveTests/AutoFillWidgets_Reflow_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/MobileResponsiveTests/AutoFillWidgets_Reflow_spec.ts
@@ -40,20 +40,19 @@ describe(
         currentUrl = url;
         cy.log(currentUrl);
       });
-      homePage.Signout(false).then(() => {
-        agHelper.VisitNAssert(currentUrl, "getPagesForViewApp");
-        agHelper.AssertCSS(
-          autoLayout.getAutoLayoutLayerClassName("0", 0),
-          "flex-wrap",
-          "nowrap",
-        );
-        cy.viewport("iphone-4");
-        agHelper.AssertCSS(
-          autoLayout.getAutoLayoutLayerClassName("0", 0),
-          "flex-wrap",
-          "wrap",
-        );
-      });
+      homePage.Signout(false);
+      agHelper.VisitNAssert(currentUrl, "getPagesForViewApp");
+      agHelper.AssertCSS(
+        autoLayout.getAutoLayoutLayerClassName("0", 0),
+        "flex-wrap",
+        "nowrap",
+      );
+      cy.viewport("iphone-4");
+      agHelper.AssertCSS(
+        autoLayout.getAutoLayoutLayerClassName("0", 0),
+        "flex-wrap",
+        "wrap",
+      );
     });
   },
 );
