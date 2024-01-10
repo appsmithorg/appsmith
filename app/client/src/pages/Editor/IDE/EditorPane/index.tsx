4b7e0178e9c23f331cca27cc9199b41bff9c65eb
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-01-10 16:55:07 +0530

    fix: lifted the condition ablove render method

--- app/client/src/pages/Editor/IDE/EditorPane/index.tsx
+++ app/client/src/pages/Editor/IDE/EditorPane/index.tsx
@@ -21,6 +21,17 @@ const EditorPane = ({ match: { path } }: RouteComponentProps) => {
   const pagesActive = useSelector(getPagesActiveStatus);
   const isSideBySideEnabled = useSelector(getIsSideBySideEnabled);
 
+  const PagesRender = () => {
+    if (!isSideBySideEnabled) {
+      return <Pages />;
+    } else if (isSideBySideEnabled && pagesActive) {
+      /* This below pages component will get changed */
+      return <Pages />;
+    } else {
+      return null;
+    }
+  };
+
   return (
     <Flex
       className="ide-editor-left-pane"
@@ -33,9 +44,7 @@ const EditorPane = ({ match: { path } }: RouteComponentProps) => {
       {/** Entity Properties component is needed to render
         the Bindings popover in the context menu. Will be removed eventually **/}
       <EntityProperties />
-      {!isSideBySideEnabled ? <Pages /> : null}
-      {/* This below pages component will get changed */}
-      {isSideBySideEnabled && pagesActive ? <Pages /> : null}
+      <PagesRender />
       {/* divider is inside the Pages component */}
       <Switch>
         <SentryRoute component={GlobalAdd} exact path={`${path}${ADD_PATH}`} />
