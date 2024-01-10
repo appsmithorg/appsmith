a743e89451315c186427e93e9bb382d3bdea86c2
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-01-10 22:31:45 +0530

    Revert "chore: enable anvil to test hypothesis"
    This reverts commit 11a65e6f2699e8dd9fd559908844f46c61d3b9c3.

--- app/client/src/utils/hooks/useFeatureFlag.ts
+++ app/client/src/utils/hooks/useFeatureFlag.ts
@@ -4,9 +4,6 @@ import { selectFeatureFlags } from "@appsmith/selectors/featureFlagsSelectors";
 
 export function useFeatureFlag(flagName: FeatureFlag): boolean {
   const flagValues = useSelector(selectFeatureFlags);
-  if (flagName === "ab_wds_enabled") {
-    return true;
-  }
   if (flagName in flagValues) {
     return flagValues[flagName];
   }
