d1ea4a59c4534ff65d398986419358556a5a7afe
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-01-10 16:41:56 +0530

    feat: completed minimal segment in left pane

--- app/client/src/pages/Editor/IDE/EditorPane/MinimalSegment.tsx
+++ app/client/src/pages/Editor/IDE/EditorPane/MinimalSegment.tsx
@@ -1,23 +1,71 @@
 import React from "react";
-import { Flex, Text } from "design-system";
+import { Flex, Text, Tag } from "design-system";
+import { useSelector } from "react-redux";
+
 import { createMessage, EDITOR_PANE_TEXTS } from "@appsmith/constants/messages";
+import {
+  getActionsCount,
+  getJsActionsCount,
+  getWidgetsCount,
+} from "selectors/ideSelectors";
 
 const MinimalSegment = () => {
+  const actionsCount = useSelector(getActionsCount);
+  const jsActionsCount = useSelector(getJsActionsCount);
+  const widgetsCount = useSelector(getWidgetsCount);
+
   return (
     <Flex
       alignItems={"center"}
       borderTop={"1px solid var(--ads-v2-color-border)"}
+      gap={"spaces-2"}
       height={"36px"}
       justifyContent={"space-between"}
       p={"spaces-2"}
       px={"spaces-3"}
       width={"100%"}
     >
-      <Text kind={"body-m"}>
-        {createMessage(EDITOR_PANE_TEXTS.queries_tab)}
-      </Text>
-      <Text kind={"body-m"}>{createMessage(EDITOR_PANE_TEXTS.js_tab)}</Text>
-      <Text kind={"body-m"}>{createMessage(EDITOR_PANE_TEXTS.ui_tab)}</Text>
+      <Flex
+        alignItems={"center"}
+        flex={"1"}
+        gap={"spaces-2"}
+        height={"100%"}
+        justifyContent={"center"}
+        width={"100%"}
+      >
+        <Text kind={"body-m"}>
+          {createMessage(EDITOR_PANE_TEXTS.queries_tab)}
+        </Text>
+        <Tag isClosable={false} size="md">
+          {actionsCount}
+        </Tag>
+      </Flex>
+      <Flex
+        alignItems={"center"}
+        flex={"1"}
+        gap={"spaces-2"}
+        height={"100%"}
+        justifyContent={"center"}
+        width={"100%"}
+      >
+        <Text kind={"body-m"}>{createMessage(EDITOR_PANE_TEXTS.js_tab)}</Text>
+        <Tag isClosable={false} size="md">
+          {jsActionsCount}
+        </Tag>
+      </Flex>
+      <Flex
+        alignItems={"center"}
+        flex={"1"}
+        gap={"spaces-2"}
+        height={"100%"}
+        justifyContent={"center"}
+        width={"100%"}
+      >
+        <Text kind={"body-m"}>{createMessage(EDITOR_PANE_TEXTS.ui_tab)}</Text>
+        <Tag isClosable={false} size="md">
+          {widgetsCount}
+        </Tag>
+      </Flex>
     </Flex>
   );
 };
