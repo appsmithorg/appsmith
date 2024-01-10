efe23c582cafdc3c0846004bcf33a54ad522759b
Hetu Nandu <hetunandu@gmail.com>
2024-01-10 16:41:10 +0530

    only deselect under feature flag

--- app/client/src/pages/Editor/WidgetCard.tsx
+++ app/client/src/pages/Editor/WidgetCard.tsx
@@ -4,8 +4,11 @@ import styled from "styled-components";
 import { useWidgetDragResize } from "utils/hooks/dragResizeHooks";
 import AnalyticsUtil from "utils/AnalyticsUtil";
 import { generateReactKey } from "utils/generators";
+import { useWidgetSelection } from "utils/hooks/useWidgetSelection";
 import { IconWrapper } from "constants/IconConstants";
 import { Text } from "design-system";
+import { useFeatureFlag } from "utils/hooks/useFeatureFlag";
+import { FEATURE_FLAG } from "@appsmith/entities/FeatureFlag";
 
 interface CardProps {
   details: WidgetCardProps;
@@ -59,6 +62,10 @@ export const BetaLabel = styled.div`
 
 function WidgetCard(props: CardProps) {
   const { setDraggingNewWidget } = useWidgetDragResize();
+  const { deselectAll } = useWidgetSelection();
+  const isEditorPaneSegmentsEnabled = useFeatureFlag(
+    FEATURE_FLAG.release_show_new_sidebar_pages_pane_enabled,
+  );
 
   const onDragStart = (e: any) => {
     e.preventDefault();
@@ -72,6 +79,9 @@ function WidgetCard(props: CardProps) {
         ...props.details,
         widgetId: generateReactKey(),
       });
+    if (!isEditorPaneSegmentsEnabled) {
+      deselectAll();
+    }
   };
 
   const type = `${props.details.type.split("_").join("").toLowerCase()}`;
