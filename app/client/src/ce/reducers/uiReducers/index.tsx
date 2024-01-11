d3820cd710c587c4a3efed686c3dd60be9e02b75
Vemparala Surya Vamsi <vamsi@appsmith.com>
2024-01-11 23:17:09 +0530

    only feature flag changes

--- app/client/src/ce/reducers/uiReducers/index.tsx
+++ app/client/src/ce/reducers/uiReducers/index.tsx
@@ -50,6 +50,7 @@ import layoutConversionReducer from "reducers/uiReducers/layoutConversionReducer
 import oneClickBindingReducer from "reducers/uiReducers/oneClickBindingReducer";
 import activeFieldReducer from "reducers/uiReducers/activeFieldEditorReducer";
 import ideReducer from "../../../reducers/uiReducers/ideReducer";
+import consolidatedPageLoadReducer from "reducers/uiReducers/consolidatedPageLoadReducer";
 
 export const uiReducerObject = {
   analytics: analyticsReducer,
@@ -104,4 +105,5 @@ export const uiReducerObject = {
   oneClickBinding: oneClickBindingReducer,
   activeField: activeFieldReducer,
   ide: ideReducer,
+  consolidatedPageLoad: consolidatedPageLoadReducer,
 };
---
+++ app/client/src/reducers/uiReducers/consolidatedPageLoadReducer.ts
@@ -0,0 +1,21 @@
+import { createReducer } from "utils/ReducerUtils";
+import { ReduxActionTypes } from "@appsmith/constants/ReduxActionConstants";
+
+const initialState: ConsolidatedPageLoadState = {
+  isLoading: true,
+};
+
+const consolidatedPageLoadReducer = createReducer(initialState, {
+  [ReduxActionTypes.START_CONSOLIDATED_PAGE_LOAD]: () => ({
+    isLoading: true,
+  }),
+  [ReduxActionTypes.END_CONSOLIDATED_PAGE_LOAD]: () => ({
+    isLoading: false,
+  }),
+});
+
+export interface ConsolidatedPageLoadState {
+  isLoading: boolean;
+}
+
+export default consolidatedPageLoadReducer;
