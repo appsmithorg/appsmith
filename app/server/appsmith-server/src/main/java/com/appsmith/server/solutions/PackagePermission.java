02684cd9ceedaf7bd6cec16e521afa298edd75a3
manish kumar <manish@appsmith.com>
2024-01-12 14:39:20 +0530

    deleted pacakge permission

--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/PackagePermission.java
+++
@@ -1,5 +0,0 @@
-package com.appsmith.server.solutions;
-
-import com.appsmith.server.solutions.ce.PackagePermissionCE;
-
-public interface PackagePermission extends PackagePermissionCE {}
--- app/server/appsmith-server/src/main/java/com/appsmith/server/solutions/PackagePermissionImpl.java
+++
@@ -1,5 +0,0 @@
-package com.appsmith.server.solutions;
-
-import com.appsmith.server.solutions.ce.PackagePermissionCEImpl;
-
-public class PackagePermissionImpl extends PackagePermissionCEImpl implements PackagePermission {}
