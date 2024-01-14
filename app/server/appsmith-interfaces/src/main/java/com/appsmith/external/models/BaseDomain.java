57cee7dfe9d629e08df1ebb8aa161ca90a48747b
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-01-14 11:14:32 +0530

    Update BaseDomain.java

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/BaseDomain.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/BaseDomain.java
@@ -58,9 +58,9 @@ public abstract class BaseDomain implements Persistable<String>, AppsmithDomain,
 
     /** @deprecated to rely only on `deletedAt` for all domain models.
      * This field only exists here because its removal will cause a huge diff on all entities in git-connected
-     * applications. So, instead, we keep it, deprecated, transient (no read/write to DB), query-transient (no
-     * corresponding field in Q* class), no getter/setter methods and only use it for reflection-powered services, like
-     * the git sync implementation. For all other practical purposes, this field doesn't exist.
+     * applications. So, instead, we keep it, deprecated, query-transient (no corresponding field in Q* class),
+     * no getter/setter methods and only use it for reflection-powered services, like the git sync
+     * implementation. For all other practical purposes, this field doesn't exist.
      */
     @Deprecated(forRemoval = true)
     @JsonView(Views.Internal.class)
