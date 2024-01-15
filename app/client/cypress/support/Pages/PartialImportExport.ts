a8368fbc81c22538e39e8e8af03f36cea2421a17
Rahul Barwal <rahul.barwal@appsmith.com>
2024-01-15 17:18:39 +0530

    Fix file deletion bug in PartialImportExport class

--- app/client/cypress/support/Pages/PartialImportExport.ts
+++ app/client/cypress/support/Pages/PartialImportExport.ts
@@ -118,7 +118,7 @@ export default class PartialImportExport {
         },
       );
     });
-    // cy.exec(`rm cypress/downloads/${fixtureName}`);
+    cy.exec(`rm cypress/downloads/${fixtureName}`);
   }
 
   ImportPartiallyExportedFile(
