8b65f40c91d3a057bf9eb281d6866221874a9920
ashit-rath <ashit@appsmith.com>
2024-01-16 12:07:41 +0530

    fix: module routing and dynamic fetching for SaaS DS in modules (#3286)
    ## Description
    Include `MODULE_EDITOR_PATH` in matchBasePath to fix mongo issue
    Reuse existing routing resolver to resolve paths for module editors.
    
    #### PR fixes following issue(s)
    Fixes https://github.com/appsmithorg/appsmith/issues/30258
    
    #### Type of change
    - Bug fix (non-breaking change which fixes an issue)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed

--- app/client/src/ee/entities/IDE/utils.ts
+++ app/client/src/ee/entities/IDE/utils.ts
@@ -1,1 +1,5 @@
 export * from "ce/entities/IDE/utils";
+import { EDITOR_PATHS as CE_EDITOR_PATHS } from "ce/entities/IDE/utils";
+import { MODULE_EDITOR_PATH } from "@appsmith/constants/routes/packageRoutes";
+
+export const EDITOR_PATHS = [...CE_EDITOR_PATHS, MODULE_EDITOR_PATH];
