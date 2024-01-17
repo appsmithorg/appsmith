f376d362e4f6e11dce23063a04d692b28cb5eae9
balajisoundar <balaji@appsmith.com>
2024-01-17 19:44:14 +0530

    chore: custom widget onReady warning and template updates (#30335)
    ## Description
    Update tempaltes and add warning message when onReady function is
    missing.
    
    > if no issue exists, please create an issue and ask the maintainers
    about this first
    >
    >
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    > Please delete options that are not relevant.
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - Chore (housekeeping or task changes that don't impact user perception)
    - This change requires a documentation update
    >
    >
    >
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [ ] Manual
    - [ ] JUnit
    - [ ] Jest
    - [ ] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    	- Introduced a new Vanilla JavaScript template for custom widgets.
    - Enhanced Vue.js custom widget template with new design and
    functionality.
    - Added a warning system to alert users when certain expected code
    patterns are missing.
    
    - **Bug Fixes**
    	- Updated the `appsmithConsole` to include error handling.
    
    - **Documentation**
    	- Added new documentation URLs for custom widget development guidance.
    
    - **Refactor**
    	- Replaced the `blank` module with `vanillaJs` in code templates.
    	- Removed unused styles and code comments from React template.
    	- Streamlined default context values for widget development.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/pages/Editor/CustomWidgetBuilder/constants.ts
+++ app/client/src/pages/Editor/CustomWidgetBuilder/constants.ts
@@ -21,14 +21,14 @@ export const DEFAULT_CONTEXT_VALUE = {
   name: "",
   widgetId: "",
   srcDoc: {
-    html: "<div>Hello World</div>",
-    js: "function test() {console.log('Hello World');}",
-    css: "div {color: red;}",
+    html: "",
+    js: "",
+    css: "",
   },
   uncompiledSrcDoc: {
-    html: "<div>Hello World</div>",
-    js: "function test() {console.log('Hello World');}",
-    css: "div {color: red;}",
+    html: "",
+    js: "",
+    css: "",
   },
   model: {},
   events: {},
@@ -59,3 +59,6 @@ export const CUSTOM_WIDGET_DOC_URL =
 
 export const CUSTOM_WIDGET_DEFAULT_MODEL_DOC_URL =
   "https://docs.appsmith.com/reference/widgets/custom#default-model";
+
+export const CUSTOM_WIDGET_ONREADY_DOC_URL =
+  "https://docs.appsmith.com/reference/widgets/custom#onready";
