0a3b65bc4822cc7c7ab2044db23a091f3a6a0d72
“sneha122” <sneha@appsmith.com>
2024-01-18 15:53:06 +0530

    revert header size fix

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/util/WebClientUtils.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/util/WebClientUtils.java
@@ -68,11 +68,11 @@ public class WebClientUtils {
     }
 
     public static WebClient.Builder builder() {
-        return builder(HttpClient.create().httpResponseDecoder(spec -> spec.maxHeaderSize(16 * 1024)));
+        return builder(HttpClient.create());
     }
 
     public static WebClient.Builder builder(ConnectionProvider provider) {
-        return builder(HttpClient.create(provider).httpResponseDecoder(spec -> spec.maxHeaderSize(16 * 1024)));
+        return builder(HttpClient.create(provider));
     }
 
     public static WebClient.Builder builder(HttpClient httpClient) {
