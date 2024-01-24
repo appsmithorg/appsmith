e96998f996adce500235132e433fdecd94df7509
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-01-24 16:58:23 +0530

    chore: clean up AnvilMainCanvas

--- app/client/src/layoutSystems/anvil/canvas/AnvilMainCanvas.tsx
+++ app/client/src/layoutSystems/anvil/canvas/AnvilMainCanvas.tsx
@@ -1,11 +1,16 @@
 import type { BaseWidgetProps } from "widgets/BaseWidgetHOC/withBaseWidgetHOC";
 import { AnvilCanvas } from "./AnvilCanvas";
 import React from "react";
-import { useCanvasActivationStates } from "../canvasArenas/hooks/mainCanvas/useCanvasActivationStates";
 import { useCanvasActivation } from "../canvasArenas/hooks/mainCanvas/useCanvasActivation";
 
+/**
+ * Anvil Main Canvas is just a wrapper around AnvilCanvas.
+ * Why do we need this?
+ * Because we need to use useCanvasActivation hook which is only needed to be used once and it is also exclusive to edit mode.
+ * checkout useCanvasActivation for more details.
+ */
+
 export const AnvilMainCanvas = (props: BaseWidgetProps) => {
-  const anvilCanvasActivationStates = useCanvasActivationStates();
-  useCanvasActivation(anvilCanvasActivationStates);
+  useCanvasActivation();
   return <AnvilCanvas {...props} />;
 };
