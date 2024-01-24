74ba9e29033c8815366b3e52954579456471dca0
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-01-24 12:57:38 +0530

    fix: adding Anvil tags

--- app/client/cypress/e2e/Regression/ClientSide/Anvil/AnvilAppNavigation_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Anvil/AnvilAppNavigation_spec.ts
@@ -12,56 +12,63 @@ import { MAIN_CONTAINER_WIDGET_ID } from "../../../../../src/constants/WidgetCon
 import { getAnvilCanvasId } from "../../../../../src/layoutSystems/anvil/canvas/utils";
 import { featureFlagIntercept } from "../../../../support/Objects/FeatureFlags";
 
-describe(`${ANVIL_EDITOR_TEST}: Validating multiple widgets in anvil layout mode with App navigation settings`, function () {
-  beforeEach(() => {
-    // intercept features call for Anvil + WDS tests
-    featureFlagIntercept({ release_anvil_enabled: true, ab_wds_enabled: true });
-    // Cleanup the canvas before each test
-    agHelper.SelectAllWidgets();
-    agHelper.PressDelete();
-  });
-  it("1. Change App navigation settings and valdiate the layout settings", () => {
-    const mainCanvasId = `#${getAnvilCanvasId(MAIN_CONTAINER_WIDGET_ID)}`;
-    const paddingBetweenZoneAndMainCanvas = 35;
-    agHelper.AssertElementExist(mainCanvasId).then((mainCanvas) => {
-      const x = mainCanvas.position().left;
-      const y = mainCanvas.position().top;
-      anvilLayout.DragDropAnvilWidgetNVerify(
-        WIDGET.WDSINPUT,
-        x + 10,
-        y + paddingBetweenZoneAndMainCanvas,
-        {
-          skipWidgetSearch: true,
-        },
-      );
-      anvilLayout.DragDropAnvilWidgetNVerify(
-        WIDGET.WDSINPUT,
-        x + 10,
-        y + paddingBetweenZoneAndMainCanvas,
-        {
-          skipWidgetSearch: true,
-          widgetNameToDropInto: "Section1",
-        },
-      );
-      anvilLayout.DragDropAnvilWidgetNVerify(
-        WIDGET.WDSBUTTON,
-        x + 10,
-        y + paddingBetweenZoneAndMainCanvas,
-        {
-          skipWidgetSearch: true,
-          widgetNameToDropInto: "Section1",
-        },
-      );
+describe(
+  `${ANVIL_EDITOR_TEST}: Validating multiple widgets in anvil layout mode with App navigation settings`,
+  { tags: ["@tag.Anvil"] },
+  function () {
+    beforeEach(() => {
+      // intercept features call for Anvil + WDS tests
+      featureFlagIntercept({
+        release_anvil_enabled: true,
+        ab_wds_enabled: true,
+      });
+      // Cleanup the canvas before each test
+      agHelper.SelectAllWidgets();
+      agHelper.PressDelete();
     });
-    propPane.NavigateToPage("Page1", "onClick");
-    appSettings.OpenAppSettings();
-    agHelper.GetNClick(appSettings.locators._navigationSettingsTab);
-    agHelper.GetNClick(
-      appSettings.locators._navigationSettings._orientationOptions._side,
-    );
-    agHelper.AssertElementExist(appSettings.locators._sideNavbar);
-    agHelper.GetNClick(locators._canvas);
-    agHelper.AssertElementExist(locators._widgetInCanvas(WIDGET.WDSINPUT));
-    agHelper.AssertElementExist(locators._widgetInCanvas(WIDGET.WDSINPUT), 1);
-  });
-});
+    it("1. Change App navigation settings and valdiate the layout settings", () => {
+      const mainCanvasId = `#${getAnvilCanvasId(MAIN_CONTAINER_WIDGET_ID)}`;
+      const paddingBetweenZoneAndMainCanvas = 35;
+      agHelper.AssertElementExist(mainCanvasId).then((mainCanvas) => {
+        const x = mainCanvas.position().left;
+        const y = mainCanvas.position().top;
+        anvilLayout.DragDropAnvilWidgetNVerify(
+          WIDGET.WDSINPUT,
+          x + 10,
+          y + paddingBetweenZoneAndMainCanvas,
+          {
+            skipWidgetSearch: true,
+          },
+        );
+        anvilLayout.DragDropAnvilWidgetNVerify(
+          WIDGET.WDSINPUT,
+          x + 10,
+          y + paddingBetweenZoneAndMainCanvas,
+          {
+            skipWidgetSearch: true,
+            widgetNameToDropInto: "Section1",
+          },
+        );
+        anvilLayout.DragDropAnvilWidgetNVerify(
+          WIDGET.WDSBUTTON,
+          x + 10,
+          y + paddingBetweenZoneAndMainCanvas,
+          {
+            skipWidgetSearch: true,
+            widgetNameToDropInto: "Section1",
+          },
+        );
+      });
+      propPane.NavigateToPage("Page1", "onClick");
+      appSettings.OpenAppSettings();
+      agHelper.GetNClick(appSettings.locators._navigationSettingsTab);
+      agHelper.GetNClick(
+        appSettings.locators._navigationSettings._orientationOptions._side,
+      );
+      agHelper.AssertElementExist(appSettings.locators._sideNavbar);
+      agHelper.GetNClick(locators._canvas);
+      agHelper.AssertElementExist(locators._widgetInCanvas(WIDGET.WDSINPUT));
+      agHelper.AssertElementExist(locators._widgetInCanvas(WIDGET.WDSINPUT), 1);
+    });
+  },
+);
--- app/client/cypress/e2e/Regression/ClientSide/Anvil/AnvilDnD_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Anvil/AnvilDnD_spec.ts
@@ -9,54 +9,61 @@ import { getAnvilCanvasId } from "../../../../../src/layoutSystems/anvil/canvas/
 import { ANVIL_EDITOR_TEST } from "../../../../support/Constants";
 import { featureFlagIntercept } from "../../../../support/Objects/FeatureFlags";
 
-describe(`${ANVIL_EDITOR_TEST}: Anvil tests for DnD Module`, () => {
-  beforeEach(() => {
-    // intercept features call for Anvil + WDS tests
-    featureFlagIntercept({ release_anvil_enabled: true, ab_wds_enabled: true });
-    // Cleanup the canvas before each test
-    agHelper.SelectAllWidgets();
-    agHelper.PressDelete();
-  });
-  it("1. Drag and Drop widget onto Empty Canvas", () => {
-    const mainCanvasId = `#${getAnvilCanvasId(MAIN_CONTAINER_WIDGET_ID)}`;
-    agHelper.AssertElementExist(mainCanvasId).then((mainCanvas) => {
-      const x = mainCanvas.position().left;
-      const y = mainCanvas.position().top;
-      const width = mainCanvas.width() || 0;
-      const paddingBetweenZoneAndMainCanvas = 35;
-      // start align
-      anvilLayout.DragDropAnvilWidgetNVerify(
-        WIDGET.WDSBUTTON,
-        x + 10,
-        y + paddingBetweenZoneAndMainCanvas * 0.5,
-        {
-          skipWidgetSearch: true,
-        },
-      );
-      // center align
-      anvilLayout.DragDropAnvilWidgetNVerify(
-        WIDGET.WDSBUTTON,
-        x + (width - 2 * paddingBetweenZoneAndMainCanvas) / 2,
-        y + paddingBetweenZoneAndMainCanvas * 0.5,
-        {
-          skipWidgetSearch: true,
-          widgetNameToDropInto: "Zone1",
-        },
-      );
-      // end align
-      anvilLayout.DragDropAnvilWidgetNVerify(
-        WIDGET.WDSBUTTON,
-        x + (width - 2 * paddingBetweenZoneAndMainCanvas),
-        y + paddingBetweenZoneAndMainCanvas * 0.5,
-        {
-          skipWidgetSearch: true,
-          widgetNameToDropInto: "Zone1",
-        },
-      );
-      agHelper.AssertElementLength(
-        locators._widgetInCanvas(WIDGET.WDSBUTTON),
-        3,
-      );
+describe(
+  `${ANVIL_EDITOR_TEST}: Anvil tests for DnD Module`,
+  { tags: ["@tag.Anvil"] },
+  () => {
+    beforeEach(() => {
+      // intercept features call for Anvil + WDS tests
+      featureFlagIntercept({
+        release_anvil_enabled: true,
+        ab_wds_enabled: true,
+      });
+      // Cleanup the canvas before each test
+      agHelper.SelectAllWidgets();
+      agHelper.PressDelete();
     });
-  });
-});
+    it("1. Drag and Drop widget onto Empty Canvas", () => {
+      const mainCanvasId = `#${getAnvilCanvasId(MAIN_CONTAINER_WIDGET_ID)}`;
+      agHelper.AssertElementExist(mainCanvasId).then((mainCanvas) => {
+        const x = mainCanvas.position().left;
+        const y = mainCanvas.position().top;
+        const width = mainCanvas.width() || 0;
+        const paddingBetweenZoneAndMainCanvas = 35;
+        // start align
+        anvilLayout.DragDropAnvilWidgetNVerify(
+          WIDGET.WDSBUTTON,
+          x + 10,
+          y + paddingBetweenZoneAndMainCanvas * 0.5,
+          {
+            skipWidgetSearch: true,
+          },
+        );
+        // center align
+        anvilLayout.DragDropAnvilWidgetNVerify(
+          WIDGET.WDSBUTTON,
+          x + (width - 2 * paddingBetweenZoneAndMainCanvas) / 2,
+          y + paddingBetweenZoneAndMainCanvas * 0.5,
+          {
+            skipWidgetSearch: true,
+            widgetNameToDropInto: "Zone1",
+          },
+        );
+        // end align
+        anvilLayout.DragDropAnvilWidgetNVerify(
+          WIDGET.WDSBUTTON,
+          x + (width - 2 * paddingBetweenZoneAndMainCanvas),
+          y + paddingBetweenZoneAndMainCanvas * 0.5,
+          {
+            skipWidgetSearch: true,
+            widgetNameToDropInto: "Zone1",
+          },
+        );
+        agHelper.AssertElementLength(
+          locators._widgetInCanvas(WIDGET.WDSBUTTON),
+          3,
+        );
+      });
+    });
+  },
+);
