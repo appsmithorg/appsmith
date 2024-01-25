1c0dabe45423e3a9c10b358a91c1cf6417c4a410
Valera Melnikov <valera@appsmith.com>
2024-01-25 13:22:25 +0300

    fix: fix modal styles (#30627)

--- app/client/packages/design-system/widgets/src/components/Modal/src/Modal.tsx
+++ app/client/packages/design-system/widgets/src/components/Modal/src/Modal.tsx
@@ -4,13 +4,6 @@ import styles from "./styles.module.css";
 import type { ModalProps } from "./types";
 import clsx from "clsx";
 
-const style: React.CSSProperties = {
-  width: `calc(var(--provider-width) - var(--sizing-6))`,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
 export const Modal = (props: ModalProps) => {
   const {
     children,
@@ -26,7 +19,6 @@ export const Modal = (props: ModalProps) => {
       <PopoverModalContent
         data-size={size}
         overlayClassName={clsx(styles.overlay, overlayClassName)}
-        style={style}
       >
         {children}
       </PopoverModalContent>
