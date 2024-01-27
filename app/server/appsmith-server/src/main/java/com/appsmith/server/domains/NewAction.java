359e44dba1ecbbf47710baec7089e4aa4312cd40
Nidhi Nair <nidhi@appsmith.com>
2024-01-27 15:33:25 +0530

    chore: Split for git with mi in apps

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/NewAction.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/NewAction.java
@@ -9,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
-@ToString
+@ToString(callSuper = true)
 @NoArgsConstructor
 @Document
 public class NewAction extends NewActionCE {}
