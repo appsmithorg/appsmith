2e4a0e71010aad8e942f9e7ad926610d07a795d2
Pawan Kumar <pawan.stardust@gmail.com>
2024-01-30 18:34:10 +0530

    add temporary feature flag check

--- app/client/src/widgets/wds/WDSStatBoxWidget/config/anvilConfig.ts
+++ app/client/src/widgets/wds/WDSStatBoxWidget/config/anvilConfig.ts
@@ -3,8 +3,6 @@ import type { AnvilConfig } from "WidgetProvider/constants";
 export const anvilConfig: AnvilConfig = {
   isLargeWidget: true,
   widgetSize: {
-    minWidth: {
-      base: "100%",
-    },
+    minWidth: "100%",
   },
 };
