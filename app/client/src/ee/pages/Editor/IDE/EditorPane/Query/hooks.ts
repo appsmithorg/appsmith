6b3afa703ff05b93735cb2a52fae2fadfb30d734
Hetu Nandu <hetunandu@gmail.com>
2024-01-31 18:17:19 +0530

    fix: update query add url implementation (#3444)
    PR in tandem with https://github.com/appsmithorg/appsmith/pull/30790

--- app/client/src/ee/pages/Editor/IDE/EditorPane/Query/hooks.ts
+++ app/client/src/ee/pages/Editor/IDE/EditorPane/Query/hooks.ts
@@ -3,50 +3,9 @@ export * from "ce/pages/Editor/IDE/EditorPane/Query/hooks";
 import { useGroupedAddQueryOperations as CE_useGroupedAddQueryOperations } from "ce/pages/Editor/IDE/EditorPane/Query/hooks";
 import type { GroupedAddOperations } from "ce/pages/Editor/IDE/EditorPane/Query/hooks";
 
-import {
-  apiEditorIdURL,
-  queryAddURL,
-  queryEditorIdURL,
-} from "@appsmith/RouteBuilder";
-import { useLocation } from "react-router";
-import { FocusEntity, identifyEntityFromPath } from "navigation/FocusEntity";
-import { useCallback } from "react";
-import { EditorEntityTabState } from "@appsmith/entities/IDE/constants";
-import { moduleInstanceEditorURL } from "@appsmith/RouteBuilder";
-import { useCurrentEditorState } from "pages/Editor/IDE/hooks";
-import { MODULE_TYPE } from "@appsmith/constants/ModuleConstants";
-import history from "utils/history";
+import { FocusEntity } from "navigation/FocusEntity";
 import { groupBy } from "lodash";
 import { useModuleOptions } from "@appsmith/utils/moduleInstanceHelpers";
-
-export const useQueryAdd = () => {
-  const location = useLocation();
-  const currentEntityInfo = identifyEntityFromPath(location.pathname);
-  const { segmentMode } = useCurrentEditorState();
-
-  const addButtonClickHandler = useCallback(() => {
-    let url = queryAddURL({});
-    if (segmentMode === EditorEntityTabState.Edit) {
-      switch (currentEntityInfo.entity) {
-        case FocusEntity.QUERY:
-          url = queryEditorIdURL({ queryId: currentEntityInfo.id, add: true });
-          break;
-        case FocusEntity.API:
-          url = apiEditorIdURL({ apiId: currentEntityInfo.id, add: true });
-          break;
-        case FocusEntity.QUERY_MODULE_INSTANCE:
-          url = moduleInstanceEditorURL({
-            moduleInstanceId: currentEntityInfo.id,
-            add: true,
-            moduleType: MODULE_TYPE.QUERY,
-          });
-      }
-    }
-    history.push(url);
-  }, [currentEntityInfo.id, location, segmentMode]);
-
-  return addButtonClickHandler;
-};
 
 export const useGroupedAddQueryOperations = (): GroupedAddOperations => {
   const [fromSourceGroup, fromBlankGroup] = CE_useGroupedAddQueryOperations();
--- app/client/src/ee/pages/Editor/IDE/EditorPane/Query/utils.ts
+++ app/client/src/ee/pages/Editor/IDE/EditorPane/Query/utils.ts
@@ -1,9 +1,13 @@
 export * from "ce/pages/Editor/IDE/EditorPane/Query/utils";
 
-import { getQueryEntityItemUrl as CE_getQueryEntityItemUrl } from "ce/pages/Editor/IDE/EditorPane/Query/utils";
+import {
+  getQueryEntityItemUrl as CE_getQueryEntityItemUrl,
+  getQueryAddUrl as CE_getQueryAddUrl,
+} from "ce/pages/Editor/IDE/EditorPane/Query/utils";
 import type { EntityItem } from "@appsmith/entities/IDE/constants";
 import { moduleInstanceEditorURL } from "@appsmith/RouteBuilder";
 import { MODULE_TYPE } from "@appsmith/constants/ModuleConstants";
+import type { FocusEntityInfo } from "navigation/FocusEntity";
 
 export const getQueryEntityItemUrl = (
   item: EntityItem,
@@ -17,4 +21,16 @@ export const getQueryEntityItemUrl = (
     });
   }
   return CE_getQueryEntityItemUrl(item, pageId);
+};
+
+export const getQueryAddUrl = (item: FocusEntityInfo): string => {
+  if (item.params.moduleInstanceId) {
+    return moduleInstanceEditorURL({
+      pageId: item.params.pageId,
+      moduleType: MODULE_TYPE.QUERY,
+      moduleInstanceId: item.params.moduleInstanceId,
+      add: true,
+    });
+  }
+  return CE_getQueryAddUrl(item);
 };
