d69271796b04ec84fb41103389e9f9abc0608b63
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-01-31 12:33:56 +0530

    chore: use default spring query instead of custom one

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomUserRepositoryCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomUserRepositoryCE.java
@@ -9,8 +9,6 @@ public interface CustomUserRepositoryCE extends AppsmithRepository<User> {
 
     Mono<User> findByEmail(String email, AclPermission aclPermission);
 
-    Mono<User> findByCaseInsensitiveEmail(String email);
-
     Mono<User> findByEmailAndTenantId(String email, String tenantId);
 
     Mono<Boolean> isUsersEmpty();
