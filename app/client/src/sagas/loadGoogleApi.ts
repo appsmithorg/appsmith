563142c36eb3665a58d06585baf14bf728abe759
Vemparala Surya Vamsi <vamsi@appsmith.com>
2024-02-01 18:25:43 +0530

    Small fix

--- app/client/src/sagas/loadGoogleApi.ts
+++ app/client/src/sagas/loadGoogleApi.ts
@@ -14,7 +14,7 @@ async function loadScript(src: string) {
 }
 
 export const executeGoogleApi = async () => {
-  const { airGapped } = isAirgapped();
+  const airGapped = isAirgapped();
   if (airGapped) {
     return;
   }
