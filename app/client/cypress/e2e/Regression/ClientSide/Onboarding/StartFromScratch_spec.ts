6e13f6becbcd239052091e0aec0fe7788691f13a
Aishwarya UR <aishwarya@appsmith.com>
2024-02-05 21:13:29 +0530

    fix

--- app/client/cypress/e2e/Regression/ClientSide/Onboarding/StartFromScratch_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Onboarding/StartFromScratch_spec.ts
@@ -26,11 +26,10 @@ describe(
       agHelper.GenerateUUID();
       cy.get("@guid").then((uid) => {
         homePage.SignUp(`${uid}@appsmithtest.com`, uid as unknown as string);
-        onboarding.closeIntroModal();
       });
       agHelper.GetNClick(onboarding.locators.startFromScratchCard);
 
-      agHelper.GetNClick(FirstTimeUserOnboardingLocators.introModalCloseBtn);
+      onboarding.closeIntroModal();
 
       featureFlagIntercept({
         ab_show_templates_instead_of_blank_canvas_enabled: true,
