f14b40cef69da23a236a3a02492139b51ed314e8
Valera Melnikov <valera@appsmith.com>
2024-02-06 10:26:47 +0300

    fix: fix modal position and styles (#30805)
    ## Description
    Fixes for the modal widget:
    1. Added support for clickOutside. The modal is closed only by clicking
    on the backdrop overlay. A click on the widget name has been added to
    the exceptions for close event.
    2. Fixed the positioning of the modal. Now it is located in the center
    of the provider.
    3. For the correct positioning of the modal, it was necessary to make
    fixes for canvas height. The fixes also affect fixed and autolayout.
    
    #### PR fixes following issue(s)
    Fixes #30788
    
    Also fixed this
    https://www.notion.so/appsmith/Canvas-gets-cut-off-on-preview-mode-525b95f26c6e4644bf5ab7389c02e434
    
    #### Media
    
    
    https://github.com/appsmithorg/appsmith/assets/11555074/6db9ecde-595b-4fa4-a21b-9ed08930d58f
    
    
    #### Type of change
    > Please delete options that are not relevant.
    - Bug fix (non-breaking change which fixes an issue)
    - Chore (housekeeping or task changes that don't impact user perception)
    >
    >
    >
    ## Testing
    >
    #### How Has This Been Tested?
    > Please describe the tests that you ran to verify your changes. Also
    list any relevant details for your test configuration.
    > Delete anything that is not relevant
    - [x] Manual
    - [ ] JUnit
    - [x] Jest
    - [x] Cypress
    >
    >
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Popover and Modal components now support dismissing by clicking
    outside. Custom logic and selectors can be specified to control this
    behavior.
    - **Enhancements**
    	- Simplified logic for closing Modals by directly setting open state.
    - Enhanced Modal styling options, allowing for better customization of
    width and height.
    	- ErrorBoundary component now supports custom styles.
    - **Refactor**
    - Removed redundant code and unused properties across various components
    and layout systems.
    - Simplified state management and styling adjustments in editor
    components.
    - **Style**
    - Updated Modal component styles for improved layout and responsiveness.
    - **Chores**
    - Codebase cleanup including removal of unused classes, variables, and
    adjustments for more efficient layout rendering.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/packages/design-system/widgets/src/components/Modal/src/types.ts
+++ app/client/packages/design-system/widgets/src/components/Modal/src/types.ts
@@ -8,7 +8,12 @@ import type { SIZES } from "../../../shared";
 export interface ModalProps
   extends Pick<
       PopoverProps,
-      "isOpen" | "setOpen" | "onClose" | "triggerRef" | "initialFocus"
+      | "isOpen"
+      | "setOpen"
+      | "onClose"
+      | "triggerRef"
+      | "initialFocus"
+      | "dismissClickOutside"
     >,
     Pick<PopoverModalContentProps, "overlayClassName"> {
   /** Size of the Modal
