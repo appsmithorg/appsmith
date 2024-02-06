e69ca2c700e1c77c9554c5f9ab003dda6f731c1b
vadim <vadim@appsmith.com>
2024-02-06 13:46:02 +0100

    Adjust corner radii in elevation testing components (harmonized ones in comments)

--- app/client/packages/design-system/widgets/src/testing/Elevation.tsx
+++ app/client/packages/design-system/widgets/src/testing/Elevation.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Flex } from "@design-system/widgets";
+import { Flex, Button } from "@design-system/widgets";
 
 export const Elevation = () => {
   return (
@@ -9,6 +9,8 @@ export const Elevation = () => {
       style={{
         background: "var(--color-bg-elevation-1)",
         boxShadow: "var(--box-shadow-1)",
+        // borderRadius: "calc(var(--border-radius-1) + var(--outer-spacing-6)*2)",
+        borderRadius: "var(--border-radius-1)",
       }}
     >
       <Flex
@@ -17,6 +19,8 @@ export const Elevation = () => {
         style={{
           background: "var(--color-bg-elevation-2)",
           boxShadow: "var(--box-shadow-2)",
+          // borderRadius: "calc(var(--border-radius-1) + var(--outer-spacing-6))",
+          borderRadius: "var(--border-radius-1)",
         }}
       >
         <Flex
@@ -25,8 +29,11 @@ export const Elevation = () => {
           style={{
             background: "var(--color-bg-elevation-3)",
             boxShadow: "var(--box-shadow-3)",
+            borderRadius: "var(--border-radius-1)",
           }}
-        />
+        >
+          <Button>Do the Thing</Button>
+        </Flex>
       </Flex>
     </Flex>
   );
