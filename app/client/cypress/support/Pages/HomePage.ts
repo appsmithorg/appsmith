49e2c3de0c06eee4bc4948d2f0bd039e7b7a9bbb
Aishwarya UR <aishwarya@appsmith.com>
2024-02-06 16:41:10 +0530

    fix

--- app/client/cypress/support/Pages/HomePage.ts
+++ app/client/cypress/support/Pages/HomePage.ts
@@ -425,7 +425,7 @@ export class HomePage {
   }
 
   public SignUp(uname: string, pswd: string) {
-    this.agHelper.VisitNAssert("/user/signup");
+    this.agHelper.VisitNAssert("/user/signup", "getConsolidatedData");
     this.agHelper.AssertElementVisibility(this.signupUsername);
     this.agHelper.AssertAttribute(this._submitBtn, "data-disabled", "true");
     this.agHelper.TypeText(this.signupUsername, uname);
