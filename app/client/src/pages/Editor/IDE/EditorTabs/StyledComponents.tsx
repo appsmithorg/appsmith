8570a9b8e61e50fe8073ddb93a1c2c85a7af27e6
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-02-07 18:06:31 +0530

    feat: Tabs new design

--- app/client/src/pages/Editor/IDE/EditorTabs/StyledComponents.tsx
+++ app/client/src/pages/Editor/IDE/EditorTabs/StyledComponents.tsx
@@ -2,15 +2,34 @@ import styled from "styled-components";
 import { Flex } from "design-system";
 
 export const StyledTab = styled(Flex)`
-  border-radius: var(--ads-v2-border-radius);
+  position: relative;
+  top: 1px;
+  border-top: 1px solid transparent;
+  border-top-left-radius: var(--ads-v2-border-radius);
+  border-top-right-radius: var(--ads-v2-border-radius);
   padding: var(--ads-v2-spaces-2) var(--ads-v2-spaces-4);
   font-size: 12px;
   color: var(--ads-v2-colors-text-default);
   cursor: pointer;
   gap: var(--ads-v2-spaces-2);
+
+  // After element - the seperator in between tabs
+  &:not(&.active):not(:has(+ .active)):not(:last-child):after {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: 8px;
+    width: 1px;
+    height: 40%;
+    background-color: var(--ads-v2-color-border);
+  }
+
   &.active {
     background-color: var(--ads-v2-colors-control-knob-default-bg);
     color: var(--ads-v2-colors-text-default);
-    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.12);
+    border-top: 1px solid var(--ads-v2-color-bg-brand);
+    box-shadow:
+      1px 0px 0px 0px var(--ads-v2-color-border) inset,
+      -1px 0px 0px 0px var(--ads-v2-color-border) inset;
   }
 `;
