260c5dc827ab21f2a83792b17185f697db362099
Rahul Barwal <rahul.barwal@appsmith.com>
2024-02-07 10:19:37 +0530

    Add onForkTemplateClick function to StartWithTemplatesWrapper

--- app/client/src/ce/pages/Applications/StartWithTemplatesWrapper.tsx
+++ app/client/src/ce/pages/Applications/StartWithTemplatesWrapper.tsx
@@ -3,59 +3,25 @@ import {
   START_WITH_TEMPLATE_CONNECT_SUBHEADING,
   createMessage,
 } from "@appsmith/constants/messages";
-import { getApplicationByIdFromWorkspaces } from "@appsmith/selectors/applicationSelectors";
-import {
-  importTemplateIntoApplicationViaOnboardingFlow,
-  setActiveLoadingTemplateId,
-} from "actions/templateActions";
 import type { Template } from "api/TemplatesApi";
 import { Flex, Text } from "design-system";
 import StartWithTemplates from "pages/Templates/StartWithTemplates";
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  getForkableWorkspaces,
-  isImportingTemplateToAppSelector,
-} from "selectors/templatesSelectors";
+import { useSelector } from "react-redux";
+import { getForkableWorkspaces } from "selectors/templatesSelectors";
 import styled from "styled-components";
-import AnalyticsUtil from "utils/AnalyticsUtil";
 
 interface Props {
-  currentApplicationIdForCreateNewApp: string;
   setSelectedTemplate: (id: string) => void;
+  onForkTemplateClick: (template: Template) => void;
 }
 
 const StartWithTemplatesWrapper = ({
-  currentApplicationIdForCreateNewApp,
+  onForkTemplateClick,
   setSelectedTemplate,
 }: Props) => {
-  const dispatch = useDispatch();
-  const isImportingTemplate = useSelector(isImportingTemplateToAppSelector);
   const workspaceList = useSelector(getForkableWorkspaces);
 
-  const application = useSelector((state) =>
-    getApplicationByIdFromWorkspaces(
-      state,
-      currentApplicationIdForCreateNewApp,
-    ),
-  );
-  const onForkTemplateClick = (template: Template) => {
-    const title = template.title;
-    AnalyticsUtil.logEvent("FORK_TEMPLATE_WHEN_ONBOARDING", { title });
-    // When fork template is clicked to add a new app using the template
-    if (!isImportingTemplate && application) {
-      dispatch(setActiveLoadingTemplateId(template.id));
-      dispatch(
-        importTemplateIntoApplicationViaOnboardingFlow(
-          template.id,
-          template.title,
-          template.pages.map((p) => p.name),
-          application.id,
-          application.workspaceId,
-        ),
-      );
-    }
-  };
   return (
     <Flex flexDirection="column" pl="spaces-3" pr="spaces-3">
       <Header
