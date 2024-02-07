91f70170dd2d5046b8f59f021976b779281fcff1
Aishwarya UR <aishwarya@appsmith.com>
2024-02-07 09:41:04 +0530

    trial

--- app/client/cypress/e2e/Regression/ClientSide/Onboarding/StartFromScratch3_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Onboarding/StartFromScratch3_spec.ts
@@ -15,7 +15,7 @@ describe(
   "Start with scratch userflow",
   { tags: ["@tag.excludeForAirgap", "@tag.Templates"] },
   function () {
-    before(() => {
+    beforeEach(() => {
       homePage.Signout(true);
       featureFlagIntercept({
         ab_show_templates_instead_of_blank_canvas_enabled: true,
