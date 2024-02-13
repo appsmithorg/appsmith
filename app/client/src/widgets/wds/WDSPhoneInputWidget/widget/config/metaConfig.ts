9ffd8c25181214dfba85b43fff2ffe02a5f28584
Pawan Kumar <pawan.stardust@gmail.com>
2024-02-13 13:18:43 +0530

    add new icon set

--- app/client/src/widgets/wds/WDSPhoneInputWidget/widget/config/metaConfig.ts
+++ app/client/src/widgets/wds/WDSPhoneInputWidget/widget/config/metaConfig.ts
@@ -1,7 +1,7 @@
 import { WIDGET_TAGS } from "constants/WidgetConstants";
 
 import IconSVG from "../../icon.svg";
-import ThumbnailSVG from "../thumbnail.svg";
+import ThumbnailSVG from "../../thumbnail.svg";
 
 export const metaConfig = {
   name: "Phone Input",
