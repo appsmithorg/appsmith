5e00ac88a0af9f9b7632717c49a73f915a808494
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-02-14 17:35:21 +0530

    chore: adding tests for widget clicks so that this wont regress again.

--- app/client/cypress/support/Objects/ObjectsCore.ts
+++ app/client/cypress/support/Objects/ObjectsCore.ts
@@ -36,4 +36,5 @@ export const gsheetHelper = ObjectsRegistry.GSheetHelper;
 export const widgetLocators = WIDGETSKIT;
 export const communityTemplates = ObjectsRegistry.CommunityTemplates;
 export const anvilLayout = ObjectsRegistry.AnvilLayout;
+export const wdsWidgets = ObjectsRegistry.WDSWidgets;
 export const partialImportExport = ObjectsRegistry.PartialImportExport;
