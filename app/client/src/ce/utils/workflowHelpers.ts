4ce7d24e2b387a145efe3318b8274ca6447a8a03
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-02-15 21:30:27 +0530

    Add workflowAutocompleteDefinitions to entityDefinitions

--- app/client/src/ce/utils/workflowHelpers.ts
+++ app/client/src/ce/utils/workflowHelpers.ts
@@ -1,3 +1,8 @@
+import type { Def } from "tern";
+
 export const useWorkflowOptions = () => {
   return [];
 };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const workflowAutocompleteDefinitions = (generatedTypeDef: Def) => ({});
