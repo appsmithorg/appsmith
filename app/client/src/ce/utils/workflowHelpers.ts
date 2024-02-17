6c2216a58a07645e2413433669d9f7186cb77865
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-02-17 12:08:22 +0530

    update: remove imports from workflow helpers

--- app/client/src/ce/utils/workflowHelpers.ts
+++ app/client/src/ce/utils/workflowHelpers.ts
@@ -1,8 +1,3 @@
-import type { Def } from "tern";
-
 export const useWorkflowOptions = () => {
   return [];
 };
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const workflowAutocompleteDefinitions = (generatedTypeDef: Def) => ({});
