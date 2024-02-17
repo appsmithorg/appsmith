8d2686983ffa5d88bfff47481bb816ad7ffa6114
Rahul Barwal <rahul.barwal@appsmith.com>
2024-02-17 15:19:15 +0530

    Add import for FEATURE_FLAG and update useFeatureFlag call

--- app/client/src/ce/pages/Applications/CreateNewAppFromTemplateModal/CreateNewAppFromTemplatesWrapper.tsx
+++ app/client/src/ce/pages/Applications/CreateNewAppFromTemplateModal/CreateNewAppFromTemplatesWrapper.tsx
@@ -1,22 +1,16 @@
+import { hideCreateAppFromTemplatesModal } from "actions/templateActions";
 import React from "react";
-import CreateNewAppFromTemplatesModal from ".";
-import { useFeatureFlag } from "utils/hooks/useFeatureFlag";
 import { useDispatch, useSelector } from "react-redux";
 import { createAppFromTemplatesModalSelector } from "selectors/templatesSelectors";
-import { hideCreateAppFromTemplatesModal } from "actions/templateActions";
+import CreateNewAppFromTemplatesModal from ".";
 
 interface Props {
   currentWorkspaceId: string;
 }
 
 const CreateNewAppFromTemplatesWrapper = ({ currentWorkspaceId }: Props) => {
-  const isCreateAppFromTemplatesEnabled = useFeatureFlag(
-    "release_show_create_app_from_templates_enabled",
-  );
   const isOpen = useSelector(createAppFromTemplatesModalSelector);
   const dispatch = useDispatch();
-
-  if (!isCreateAppFromTemplatesEnabled) return null;
 
   const handleClose = () => {
     dispatch(hideCreateAppFromTemplatesModal());
