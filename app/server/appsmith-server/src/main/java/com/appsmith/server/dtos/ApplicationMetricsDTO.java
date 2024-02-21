a2101cced99cfec8c9e68ec06538f2f2b0d9f708
nilansh <nilansh@appsmith.com>
2024-02-21 17:49:39 +0530

    Added Action and Application metric DTOs

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ApplicationMetricsDTO.java
@@ -0,0 +1,13 @@
+package com.appsmith.server.dtos;
+
+import com.appsmith.server.dtos.ce.ApplicationMetricsCE_DTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class ApplicationMetricsDTO extends ApplicationMetricsCE_DTO {}
---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/ce/ApplicationMetricsCE_DTO.java
@@ -0,0 +1,27 @@
+package com.appsmith.server.dtos.ce;
+
+import com.appsmith.external.views.Views;
+import com.appsmith.server.dtos.ActionMetricsDTO;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class ApplicationMetricsCE_DTO {
+
+    @JsonView(Views.Public.class)
+    String pageId;
+
+    @JsonView(Views.Public.class)
+    String pageName;
+
+    @JsonView(Views.Public.class)
+    List<ActionMetricsDTO> actionMetricsDTOList;
+}
