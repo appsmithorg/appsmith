5bcc827305a4df30bcd0122e015765fc10489f2a
Valera Melnikov <valera@appsmith.com>
2024-02-26 13:58:43 +0300

    Revert "fix: check cypress config"
    This reverts commit 810677a8c312f3092715c420304a0c329349ef7c.

--- app/client/cypress_ci.config.ts
+++ app/client/cypress_ci.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   chromeWebSecurity: false,
   viewportHeight: 1200,
   viewportWidth: 1400,
-  scrollBehavior: false,
+  scrollBehavior: "center",
   retries: {
     runMode: 1,
     openMode: 0,
