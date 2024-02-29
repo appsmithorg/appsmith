a739c026e33ee537231efeb3e81bcf3f2620cbb5
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2024-02-29 16:12:24 +0530

    fixed the import after moving types

--- app/client/src/workers/Evaluation/formEval.ts
+++ app/client/src/workers/Evaluation/formEval.ts
@@ -8,13 +8,13 @@ import type {
 } from "reducers/evaluationReducers/formEvaluationReducer";
 import { ReduxActionTypes } from "@appsmith/constants/ReduxActionConstants";
 import type { ActionConfig } from "entities/Action";
-import type { FormEvalActionPayload } from "sagas/FormEvaluationSaga";
 import type { FormConfigType } from "components/formControls/BaseControl";
 import { isArray, isEmpty, isString, merge, uniq } from "lodash";
 import { extractEvalConfigFromFormConfig } from "components/formControls/utils";
 import { isDynamicValue } from "utils/DynamicBindingUtils";
 import { isTrueObject } from "@appsmith/workers/Evaluation/evaluationUtils";
 import type { DatasourceConfiguration } from "entities/Datasource";
+import type { FormEvalActionPayload } from "./types";
 
 export enum ConditionType {
   HIDE = "hide", // When set, the component will be shown until condition is true
