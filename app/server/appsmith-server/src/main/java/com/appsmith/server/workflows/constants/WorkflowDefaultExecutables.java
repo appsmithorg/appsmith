852e1fbf70ca7797feeb2534681f28b3b225827a
Nilesh Sarupriya <nilesh@appsmith.com>
2024-03-01 02:52:04 -0600

    chore: update the workflows template code (#3624)
    ## Description
    > Update the template code of Main JS object in workflows.
    
    #### PR fixes following issue(s)
    Fixes [[Task]: Update the default script and comments for Workflow's
    main js object](https://github.com/appsmithorg/appsmith/issues/31383)
    
    #### Media
    > A video or a GIF is preferred. when using Loom, don’t embed because it
    looks like it’s a GIF. instead, just link to the video
    >
    >
    #### Type of change
    - Chore (housekeeping or task changes that don't impact user perception)
    
    ## Testing
    >
    #### How Has This Been Tested?
    > No testing is needed as we are just changing the template code of the
    Workflow's main JS Object
    
    #### Test Plan
    > Add Testsmith test cases links that relate to this PR
    >
    >
    #### Issues raised during DP testing
    > Link issues raised during DP testing for better visiblity and tracking
    (copy link from comments dropped on this PR)
    >
    >
    >
    ## Checklist:
    #### Dev activity
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] PR is being merged under a feature flag
    
    
    #### QA activity:
    - [ ] [Speedbreak
    features](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#speedbreakers-)
    have been covered
    - [ ] Test plan covers all impacted features and [areas of
    interest](https://github.com/appsmithorg/TestSmith/wiki/Guidelines-for-test-plans#areas-of-interest-)
    - [ ] Test plan has been peer reviewed by project stakeholders and other
    QA members
    - [ ] Manually tested functionality on DP
    - [ ] We had an implementation alignment call with stakeholders post QA
    Round 2
    - [ ] Cypress test cases have been added and approved by SDET/manual QA
    - [ ] Added `Test Plan Approved` label after Cypress tests were reviewed
    - [ ] Added `Test Plan Approved` label after JUnit tests were reviewed
    
    Co-authored-by: Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>

--- app/server/appsmith-server/src/main/java/com/appsmith/server/workflows/constants/WorkflowDefaultExecutables.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/workflows/constants/WorkflowDefaultExecutables.java
@@ -1,30 +1,24 @@
 package com.appsmith.server.workflows.constants;
 
 public class WorkflowDefaultExecutables {
-    public static final String MAIN_JS_OBJECT_BODY =
-            """
-            export default {
-                // This is a main file for building your workflows. All activities to be executed should be defined within the executeWorkflow
-                // function.
-                async executeWorkflow(data) {
-                    // This function takes in a json object as arguments (data) which can be passed when you trigger the workflow.
-                    // Complete the following line to set up your first activity. Place the cursor after activities, and select the action you'd like"
-                    // to execute from the list menu that appears.
-
-                    // await Query1.run();
-                    // await Api1.run();
-
-                    return true;
-                    }
-                }
-            """;
-    public static final String EXECUTE_WORKFLOW_ACTION_BODY =
-            """
-            async executeWorkflow(data) {
-                // await Query1.run();
-                // await Api1.run();
-
-                return true;
-            }
-            """;
+    public static final String MAIN_JS_OBJECT_BODY = "export default {"
+            + "\n" + "\t/**"
+            + "\n" + "\t* Entry point for Workflow execution. All activities to be executed should be defined here."
+            + "\n"
+            + "\t* @param data  This function takes in a json object as arguments (data) which can be passed when you trigger the workflow."
+            + "\n" + "\t* @returns boolean Shall return true or false."
+            + "\n" + "\t*/"
+            + "\n" + "\tasync executeWorkflow(data) {"
+            + "\n" + "\t\t// start writing your code here."
+            + "\n" + "\n"
+            + "\t\treturn true;"
+            + "\n" + "\t}"
+            + "\n" + "}"
+            + "\n";
+    public static final String EXECUTE_WORKFLOW_ACTION_BODY = "\tasync executeWorkflow(data) {"
+            + "\n" + "\t\t// start writing your code here."
+            + "\n" + "\n"
+            + "\t\treturn true;"
+            + "\n" + "\t}"
+            + "\n";
 }
