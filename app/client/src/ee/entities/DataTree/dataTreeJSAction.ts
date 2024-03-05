1c5d487da323e0b67812c2677b7df0dbe83359cc
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2024-03-05 11:31:47 +0530

    fix: unblock app for js module import when package is not available in the workspace (#3645)

--- app/client/src/ee/entities/DataTree/dataTreeJSAction.ts
+++ app/client/src/ee/entities/DataTree/dataTreeJSAction.ts
@@ -2,6 +2,7 @@ export * from "ce/entities/DataTree/dataTreeJSAction";
 import type { JSCollectionData } from "@appsmith/reducers/entityReducers/jsActionsReducer";
 import type { JSActionEntity, JSActionEntityConfig } from "./types";
 import { generateDataTreeJSAction as CE_generateDataTreeJSAction } from "ce/entities/DataTree/dataTreeJSAction";
+import { klona } from "klona";
 
 export const generateDataTreeJSAction = (
   js: JSCollectionData,
@@ -9,7 +10,19 @@ export const generateDataTreeJSAction = (
   unEvalEntity: JSActionEntity;
   configEntity: JSActionEntityConfig;
 } => {
-  const { configEntity, unEvalEntity } = CE_generateDataTreeJSAction(js);
+  const isPublic = js.config.isPublic;
+  let newJS = js;
+  if (isPublic && !js.config.body) {
+    newJS = klona(js);
+    const jsBody = isPublic && !newJS.config.body ? "" : newJS.config.body;
+    const variables =
+      isPublic && !newJS.config.variables ? [] : newJS.config.variables;
+
+    newJS.config["body"] = jsBody;
+    newJS.config["variables"] = variables;
+  }
+
+  const { configEntity, unEvalEntity } = CE_generateDataTreeJSAction(newJS);
 
   if (js.config.moduleId || js.config.moduleInstanceId) {
     configEntity["moduleInstanceId"] = js.config.moduleInstanceId;
