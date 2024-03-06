c345ccc1966c358c4b0d9f850db04ff262bbb972
Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>
2024-03-06 18:43:38 +0530

    chore: update the code comment for RequestAppsmithTokenDTO

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/RequestAppsmithTokenDTO.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/RequestAppsmithTokenDTO.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 
 /**
  * This is used by the SaaS Controller to get a Appsmith token for a Datasource, based on the contextId and contextType.
+ * The contextId can be pageId if the contextType is PAGE, or workflowId if the contextType is WORKFLOW or
+ * moduleId if the contextType is MODULE.
  * @author nsarupr
  */
 @Data
