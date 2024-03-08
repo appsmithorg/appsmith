583c478ab3d493ee119ce53395675dfaab574d33
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-03-08 13:27:11 +0530

    chore: Use Bridge API for user, config, theme repos (#31460)
    Managed to remove the `Criteria` and `where` imported from MongoDB APIs.
    The `Bridge` API calls should work the same on Postgres as well as here.
    We'll be moving more repository methods over to this API soon.
    
    Notice that the `Update` import is still there. Bridge API for `Update`
    objects is also almost ready, and is coming up soon.

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomUserRepositoryCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomUserRepositoryCEImpl.java
@@ -4,18 +4,16 @@ import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.constants.FieldName;
 import com.appsmith.server.domains.User;
 import com.appsmith.server.helpers.ce.bridge.Bridge;
+import com.appsmith.server.helpers.ce.bridge.BridgeQuery;
 import com.appsmith.server.repositories.BaseAppsmithRepositoryImpl;
 import com.appsmith.server.repositories.CacheableRepositoryHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.mongodb.core.query.Criteria;
 import reactor.core.publisher.Mono;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @Slf4j
 public class CustomUserRepositoryCEImpl extends BaseAppsmithRepositoryImpl<User> implements CustomUserRepositoryCE {
@@ -29,7 +27,7 @@ public class CustomUserRepositoryCEImpl extends BaseAppsmithRepositoryImpl<User>
 
     @Override
     public Mono<User> findByEmail(String email, AclPermission aclPermission) {
-        Criteria emailCriteria = where(User.Fields.email).is(email);
+        BridgeQuery<User> emailCriteria = Bridge.equal(User.Fields.email, email);
         return queryBuilder().criteria(emailCriteria).permission(aclPermission).one();
     }
 
