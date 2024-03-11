31a40fa5c98a1bad8352f2303075684d6f376267
Anagh Hegde <anagh@appsmith.com>
2024-03-11 12:00:01 +0530

    add compound index for user class

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration048AddIndexToUserCollection.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration048AddIndexToUserCollection.java
@@ -1,10 +1,15 @@
 package com.appsmith.server.migrations.db.ce;
 
+import com.appsmith.server.domains.User;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
+
+import static com.appsmith.server.migrations.DatabaseChangelog1.ensureIndexes;
+import static com.appsmith.server.migrations.DatabaseChangelog1.makeIndex;
 
 @Slf4j
 @ChangeUnit(order = "047", id = "add-missing-index-in-user-collection", author = " ")
@@ -19,14 +24,12 @@ public class Migration048AddIndexToUserCollection {
     @RollbackExecution
     public void rollbackExecution() {}
 
-    /**
-     * Steps of the migration:
-     * 0. Find the plugin for Appsmith ai datasource type
-     * 1. Find all Appsmith AI datasources without createdAt field created from Migration 045
-     * 2. For each datasource, create policies based on default workspace permission groups and application specific permission groups
-     * 3. create a datasource storage
-     * 4. set createdAt and updatedAt across
-     */
     @Execution
-    public void addMissingIndexInUserCollection() {}
+    public void addMissingIndexInUserCollection() {
+
+        Index userEmailCreatedAtIndex =
+                makeIndex("deleted", "deletedAt", "email", "createdAt").unique().named("user_compound_index");
+
+        ensureIndexes(mongoTemplate, User.class, userEmailCreatedAtIndex);
+    }
 }
