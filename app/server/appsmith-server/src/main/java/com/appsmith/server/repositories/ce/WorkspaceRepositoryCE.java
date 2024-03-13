099ba74eb20bfb4b5e5b0acd3279a41933c6f15b
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-03-13 22:50:49 +0530

    Migrate CustomWorkspaceRepositoryCEImpl to Bridge API

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/WorkspaceRepositoryCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/WorkspaceRepositoryCE.java
@@ -3,11 +3,12 @@ package com.appsmith.server.repositories.ce;
 import com.appsmith.server.domains.Workspace;
 import com.appsmith.server.repositories.BaseRepository;
 import com.appsmith.server.repositories.CustomWorkspaceRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface WorkspaceRepositoryCE extends BaseRepository<Workspace, String>, CustomWorkspaceRepository {
 
-    Mono<Workspace> findBySlug(String slug);
+    Flux<Workspace> findAllWorkspaces();
 
     Mono<Workspace> findByIdAndPluginsPluginId(String workspaceId, String pluginId);
 
