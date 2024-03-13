be9b751539edbd88d4b4639287f7675b13e2bf4a
Ankita Kinger <ankita@appsmith.com>
2024-03-13 11:11:36 +0530

    chore: Refactoring code to show plugin based icons for module instances on EE (#31713)
    ## Description
    
    Refactoring code to show plugin based icons for module instances on EE
    
    Fixes [#30163](https://github.com/appsmithorg/appsmith/issues/30163)
    
    ## Automation
    
    /ok-to-test tags="@tag.All"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!IMPORTANT]  
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/8256707865>
    > Commit: `897cc28f1c8d15efb7789788aba89687b85a4916`
    > Cypress dashboard url: <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=8256707865&attempt=2"
    target="_blank">Click here!</a>
    > All cypress tests have passed 🎉🎉🎉
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    
    
    
    
    
    
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new interface for module metadata, including properties
    like module and plugin identifiers.
    - Added a new type property to the Module interface to distinguish
    module types.
    - Enhanced resource list loading with unique identifiers for each
    resource.
    - Implemented functionality to dynamically resolve and display icons
    based on module or plugin properties.
    - **Enhancements**
    - Updated the applications page to support workspaces more effectively,
    including active workspace identification.
    - Improved module selection and display across the application, allowing
    for a more integrated module management experience.
    - **Refactor**
    - Removed unused functions and components related to module icon
    resolution.
    - Streamlined the logic for fetching and displaying module-related
    information, including in action selectors and query editors.
    - **Bug Fixes**
    - Fixed an issue with resource object identification by introducing
    unique `id` fields.
    - **Style**
    - Added new class selector for module instance name editing, enhancing
    the styling options.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/components/editorComponents/WidgetQueryGeneratorForm/CommonControls/DatasourceDropdown/useSource/useConnectToOptions.tsx
+++ app/client/src/components/editorComponents/WidgetQueryGeneratorForm/CommonControls/DatasourceDropdown/useSource/useConnectToOptions.tsx
@@ -18,8 +18,6 @@ import type {
   ActionData,
   ActionDataState,
 } from "@appsmith/reducers/entityReducers/actionsReducer";
-import { EntityIcon } from "pages/Editor/Explorer/ExplorerIcons";
-import { Icon } from "design-system";
 import type {
   ModuleInstanceData,
   ModuleInstanceDataState,
@@ -27,6 +25,11 @@ import type {
 import { selectFeatureFlagCheck } from "@appsmith/selectors/featureFlagsSelectors";
 import { FEATURE_FLAG } from "@appsmith/entities/FeatureFlag";
 import type { AppState } from "@appsmith/reducers";
+import type { Module } from "@appsmith/constants/ModuleConstants";
+import { getAllModules } from "@appsmith/selectors/modulesSelector";
+import { resolveIcon } from "pages/Editor/utils";
+import { Icon } from "design-system";
+import { EntityIcon } from "pages/Editor/Explorer/ExplorerIcons";
 
 enum SortingWeights {
   alphabetical = 1,
@@ -97,12 +100,23 @@ interface ConnectToOptionsProps {
 export const getQueryIcon = (
   query: ActionData | ModuleInstanceData,
   pluginImages: Record<string, string>,
+  modules: Record<string, Module>,
 ) => {
   if (query.config.hasOwnProperty("type")) {
+    const q = query as ModuleInstanceData;
+    const module = modules[q.config.sourceModuleId];
+    const icon = resolveIcon({
+      iconLocation: pluginImages[module.pluginId] || "",
+      pluginType: module.pluginType,
+      moduleType: module.type,
+    });
+
     return (
-      <EntityIcon>
-        <Icon name="module" />
-      </EntityIcon>
+      icon || (
+        <EntityIcon>
+          <Icon name="module" />
+        </EntityIcon>
+      )
     );
   } else {
     const action = query as ActionData;
@@ -172,6 +186,7 @@ function useConnectToOptions(props: ConnectToOptionsProps) {
   const { pluginImages, widget } = props;
 
   const queryModuleInstances = useSelector(getQueryModuleInstances);
+  const modules = useSelector(getAllModules);
   let filteredQueries: ActionData[] | ModuleInstanceData[] = queries;
 
   /* Exclude Gsheets from query options till this gets resolved https://github.com/appsmithorg/appsmith/issues/27102*/
@@ -193,7 +208,7 @@ function useConnectToOptions(props: ConnectToOptionsProps) {
       id: query.config.id,
       label: query.config.name,
       value: getBindingValue(widget, query),
-      icon: getQueryIcon(query, pluginImages),
+      icon: getQueryIcon(query, pluginImages, modules),
       onSelect: function (value?: string, valueOption?: DropdownOptionType) {
         addBinding(
           valueOption?.value,
