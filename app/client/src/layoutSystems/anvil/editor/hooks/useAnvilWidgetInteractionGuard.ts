42b5e08d0a7257edc3f155486c397a49e1c84e53
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-03-13 17:30:23 +0530

    fix: allow click interactions only on pressing alt/option button.

---
+++ app/client/src/layoutSystems/anvil/editor/hooks/useAnvilWidgetInteractionGuard.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { getIsDragging } from "selectors/widgetDragSelectors";
+const EventsToBlock = ["click", "pointerup", "mouseup", "touchend"];
+
+export const useAnvilWidgetInteractionGuard = (
+  ref: React.RefObject<HTMLDivElement>,
+) => {
+  const isDragging = useSelector(getIsDragging);
+  const allowEventPropagation = useRef(isDragging);
+  useEffect(() => {
+    allowEventPropagation.current = isDragging;
+  }, [isDragging]);
+
+  const handleFirstInteraction = (e: any) => {
+    if (!(e.altKey || allowEventPropagation.current)) {
+      e.stopPropagation();
+    }
+  };
+
+  useEffect(() => {
+    // allow click events to pass only if alt key is pressed in capture phase
+    EventsToBlock.forEach((event) => {
+      ref.current?.addEventListener(event, handleFirstInteraction, true);
+    });
+
+    return () => {
+      EventsToBlock.forEach((event) => {
+        ref.current?.removeEventListener(event, handleFirstInteraction, true);
+      });
+    };
+  }, []);
+};
