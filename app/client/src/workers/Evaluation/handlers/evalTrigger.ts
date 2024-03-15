6673651e48f145114b9b1315e974d11da7133001
Rajat Agrawal <rajat@appsmith.com>
2024-03-15 17:52:52 +0530

    Use getter for getting evalTree instead of accessing the property directly

--- app/client/src/workers/Evaluation/handlers/evalTrigger.ts
+++ app/client/src/workers/Evaluation/handlers/evalTrigger.ts
@@ -33,7 +33,7 @@ export default async function (request: EvalWorkerASyncRequest) {
 
   return dataTreeEvaluator.evaluateTriggers(
     dynamicTrigger,
-    dataTreeEvaluator.evalTree,
+    dataTreeEvaluator.getEvalTree(),
     unEvalTree.configTree,
     callbackData,
     {
