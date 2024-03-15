60997e909b758c605e061b26f6ce882510dfc311
sbalaji1192 <sbalaji1192@gmail.com>
2024-03-15 11:27:09 +0530

    fix: [map chart widget] create unique instances of map loader to avoid cross cancelling loader calls

---
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Others/MapWidget_loading_Spec.ts
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+import {
+  agHelper,
+  entityExplorer,
+  propPane,
+  draggableWidgets,
+  deployMode,
+  locators,
+} from "../../../../../support/Objects/ObjectsCore";
+import EditorNavigation, {
+  EntityType,
+} from "../../../../../support/Pages/EditorNavigation";
+
+describe(
+  "Map chart Widget",
+  { tags: ["@tag.Widget", "@tag.Maps"] },
+  function () {
+    it("1.Drag two Map Widget and Verify the Map Widget is loading", () => {
+      //Add map and verify
+      entityExplorer.DragDropWidgetNVerify(draggableWidgets.MAPCHART, 200, 200);
+      entityExplorer.DragDropWidgetNVerify(draggableWidgets.MAPCHART, 600, 200);
+      agHelper.RefreshPage();
+      agHelper.AssertElementLength(
+        ".t--draggable-mapchartwidget svg text:contains('Global Population')",
+        2,
+      );
+    });
+  },
+);
