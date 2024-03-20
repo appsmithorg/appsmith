f76ad22dda73ba8e6a241a3ad8775ea2a4f5adbb
Ankita Kinger <ankita@appsmith.com>
2024-03-20 18:16:48 +0530

    adding tests for upgrade ramps on general settings page

--- app/client/cypress/e2e/Regression/Enterprise/AdminSettings/Admin_settings_spec.js
+++ app/client/cypress/e2e/Regression/Enterprise/AdminSettings/Admin_settings_spec.js
@@ -22,7 +22,44 @@ describe("Admin settings page", { tags: ["@tag.Settings"] }, function () {
     cy.url().should("contain", "/settings/general");
   });
 
-  it("2. should test that authentication and branding page shows upgrade button and redirects to pricing page", () => {
+  it("2. should test that all business and enterprise general settings should have resp. tag and should be disabled", () => {
+    cy.visit("/settings/general", { timeout: 60000 });
+    cy.get(".t--admin-settings-hideWatermark").within(() => {
+      cy.get(adminsSettings.businessTag)
+        .should("exist")
+        .should("contain", "Business");
+      cy.get("[data-testid='hideWatermark']").should("have.attr", "disabled");
+    });
+    cy.get(".t--admin-settings-showRolesAndGroups").within(() => {
+      cy.get(adminsSettings.businessTag)
+        .should("exist")
+        .should("contain", "Business");
+      cy.get("[data-testid='showRolesAndGroups']").should(
+        "have.attr",
+        "disabled",
+      );
+    });
+    cy.get(".t--admin-settings-singleSessionPerUserEnabled").within(() => {
+      cy.get(adminsSettings.businessTag)
+        .should("exist")
+        .should("contain", "Business");
+      cy.get("[data-testid='singleSessionPerUserEnabled']").should(
+        "have.attr",
+        "disabled",
+      );
+    });
+    cy.get(".t--admin-settings-userSessionTimeoutInMinutes").within(() => {
+      cy.get(adminsSettings.enterpriseTag)
+        .should("exist")
+        .should("contain", "Enterprise");
+      cy.get("[name='userSessionTimeoutInMinutes']").should(
+        "have.attr",
+        "disabled",
+      );
+    });
+  });
+
+  it("3. should test that authentication and branding page shows upgrade button and redirects to pricing page", () => {
     cy.visit("/settings/general", { timeout: 60000 });
     cy.get(adminsSettings.authenticationTab).click();
     cy.url().should("contain", "/settings/authentication");
@@ -54,7 +91,7 @@ describe("Admin settings page", { tags: ["@tag.Settings"] }, function () {
     }
   });
 
-  it("3. should test that Business features shows upgrade button and direct to pricing page", () => {
+  it("4. should test that Business features shows upgrade button and direct to pricing page", () => {
     cy.visit("/settings/general", { timeout: 60000 });
     if (CURRENT_REPO === REPO.CE) {
       cy.get(adminsSettings.accessControl).within(() => {
