31db4207be1c94445c7237a6131c43987459adf4
Anagh Hegde <anagh@appsmith.com>
2024-03-20 11:08:58 +0530

    review comment

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomJSLibRepositoryCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomJSLibRepositoryCEImpl.java
@@ -3,11 +3,11 @@ package com.appsmith.server.repositories.ce;
 import com.appsmith.server.domains.CustomJSLib;
 import com.appsmith.server.dtos.CustomJSLibContextDTO;
 import com.appsmith.server.helpers.ce.bridge.Bridge;
+import com.appsmith.server.helpers.ce.bridge.BridgeQuery;
 import com.appsmith.server.repositories.BaseAppsmithRepositoryImpl;
 import com.appsmith.server.repositories.CacheableRepositoryHelper;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
-import org.springframework.data.mongodb.core.query.Criteria;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -26,9 +26,9 @@ public class CustomJSLibRepositoryCEImpl extends BaseAppsmithRepositoryImpl<Cust
 
     @Override
     public Mono<CustomJSLib> findUniqueCustomJsLib(CustomJSLib customJSLib) {
-        Criteria criteria = Bridge.equal(CustomJSLib.Fields.uidString, customJSLib.getUidString());
+        BridgeQuery<CustomJSLib> bridgeQuery = Bridge.equal(CustomJSLib.Fields.uidString, customJSLib.getUidString());
 
-        return queryBuilder().criteria(criteria).one();
+        return queryBuilder().criteria(bridgeQuery).one();
     }
 
     @Override
@@ -38,8 +38,8 @@ public class CustomJSLibRepositoryCEImpl extends BaseAppsmithRepositoryImpl<Cust
                 .map(CustomJSLibContextDTO::getUidString)
                 .collect(Collectors.toSet());
 
-        Criteria criteria = Bridge.in(CustomJSLib.Fields.uidString, uidStrings);
+        BridgeQuery<CustomJSLib> bridgeQuery = Bridge.in(CustomJSLib.Fields.uidString, uidStrings);
 
-        return queryBuilder().criteria(criteria).all();
+        return queryBuilder().criteria(bridgeQuery).all();
     }
 }
