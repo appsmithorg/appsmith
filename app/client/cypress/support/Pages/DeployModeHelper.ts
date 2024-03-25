eb6e899f9464032eab81253e90d5be6456d0b0e0
Vemparala Surya Vamsi <vamsi@appsmith.com>
2024-03-26 00:45:01 +0530

    test if test case passes

--- app/client/cypress/support/Pages/DeployModeHelper.ts
+++ app/client/cypress/support/Pages/DeployModeHelper.ts
@@ -46,7 +46,6 @@ export class DeployMode {
     toValidateSavedState && this.agHelper.AssertAutoSave();
     this.assertHelper.AssertDocumentReady();
     this.StubbingDeployPage(addDebugFlag);
-    this.agHelper.WaitUntilEleAppear(this.locator._buttonByText("Deploy"))
     this.agHelper.ClickButton("Deploy");
     this.agHelper.AssertElementAbsence(this.locator._btnSpinner, 10000);
     this.assertHelper.AssertDocumentReady();
