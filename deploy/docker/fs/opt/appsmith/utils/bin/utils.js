305d4d7fab29cf45504e871904e07e125601e731
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-03-27 19:58:44 +0530

    chore: Remove old undocumented migrate command (#32114)
    This removes the `appsmithctl migrate` command which can migrate an
    Appsmith instance from on EC2 instance to another, using SSH. Why are we
    removing it?
    
    1. It's not documented on docs.appsmith.com at all.
    2. The problem is better solved with a combination of `appsmithctl
    backup` and `appsmithctl restore`, with much _more_ flexibility.

--- deploy/docker/fs/opt/appsmith/utils/bin/utils.js
+++ deploy/docker/fs/opt/appsmith/utils/bin/utils.js
@@ -9,9 +9,8 @@ function showHelp() {
     "\nUsage: appsmith <command> to interact with appsmith utils tool"
   );
   console.log("\nOptions:\r");
-  console.log("\tex, export_db\t\tExport interal database.\r");
-  console.log("\tim, import_db\t\tImport interal database.\r");
-  console.log("\tmi, migrate\t\tMigrate new server.\r");
+  console.log("\tex, export_db\t\tExport internal database.\r");
+  console.log("\tim, import_db\t\tImport internal database.\r");
   console.log("\tcrs, check_replica_set\tCheck replica set mongoDB.\r");
   console.log("\tbackup\t\t\tTake a backup of Appsmith instance.\r");
   console.log("\trestore\t\t\tRestore Appsmith instance from a backup.\r");
