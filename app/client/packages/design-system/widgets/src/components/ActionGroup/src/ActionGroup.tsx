f0a4f19d68320d49594781958deaccfe1522dabd
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-03-28 10:36:30 +0530

    code review fixes

--- app/client/packages/design-system/widgets/src/components/ActionGroup/src/ActionGroup.tsx
+++ app/client/packages/design-system/widgets/src/components/ActionGroup/src/ActionGroup.tsx
@@ -27,10 +27,7 @@ const _ActionGroup = <T extends object>(
     ...others
   } = props;
   const domRef = useDOMRef(ref);
-  const state = useListState({
-    ...props,
-    suppressTextValueWarning: true,
-  });
+  const state = useListState({ ...props, suppressTextValueWarning: true });
   const { actionGroupProps, visibleItems } = useActionGroup(
     props,
     state,
