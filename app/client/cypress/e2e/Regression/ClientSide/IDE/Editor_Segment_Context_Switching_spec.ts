995f09a541a9162e79ff4a54f5fe445b56dd69df
Hetu Nandu <hetunandu@gmail.com>
2024-03-28 18:35:31 +0530

    Chore: Add Editor Pane segments context switch tests

---
+++ app/client/cypress/e2e/Regression/ClientSide/IDE/Editor_Segment_Context_Switching_spec.ts
@@ -0,0 +1,94 @@
+import {
+  agHelper,
+  dataSources,
+  homePage,
+} from "../../../../support/Objects/ObjectsCore";
+
+import EditorNavigation, {
+  EntityType,
+  PageLeftPane,
+  PagePaneSegment,
+} from "../../../../support/Pages/EditorNavigation";
+import datasourceEditor from "../../../../locators/DatasourcesEditor.json";
+
+describe("Editor Segment Context Switch", { tags: ["@tag.IDE"] }, function () {
+  before("Import the test application", () => {
+    homePage.CreateNewWorkspace("IDETest", true);
+    homePage.ImportApp("SampleAppIDETest.json", "IDETest");
+    cy.wait("@importNewApplication").then((interception) => {
+      agHelper.Sleep();
+      const { isPartialImport } = interception.response?.body.data;
+      cy.log("isPartialImport is", isPartialImport);
+      if (isPartialImport) {
+        // should reconnect modal
+        cy.fillPostgresDatasourceForm();
+        cy.get(datasourceEditor.sectionAuthentication).click();
+        cy.testDatasource(true);
+        agHelper.GetNClick(dataSources._saveDs);
+        cy.wait("@getWorkspace");
+        homePage.AssertImportToast();
+      } else {
+        homePage.AssertImportToast();
+      }
+    });
+  });
+  it("will select an item when switched to it", () => {
+    // Check JS item is selected
+    PageLeftPane.switchSegment(PagePaneSegment.JS);
+    PageLeftPane.selectedItem().should("be.visible");
+
+    // Check Query item is selected
+    PageLeftPane.switchSegment(PagePaneSegment.Queries);
+    PageLeftPane.selectedItem().should("be.visible");
+
+    // Check UI item is selected
+    PageLeftPane.switchSegment(PagePaneSegment.UI);
+    PageLeftPane.selectedItem().should("be.visible");
+  });
+  it("will select previously selected item when switched to it", () => {
+    // Select a UI item
+    EditorNavigation.SelectEntityByName(
+      "txt_longitudeValue",
+      EntityType.Widget,
+      {},
+      ["con_userDetails"],
+    );
+
+    // Select a JS item
+    EditorNavigation.SelectEntityByName("Transformers", EntityType.JSObject);
+
+    // Select a Query item
+    EditorNavigation.SelectEntityByName("addUser", EntityType.Query);
+
+    // Check UI item is selected
+    PageLeftPane.switchSegment(PagePaneSegment.UI);
+    PageLeftPane.selectedItem().contains("txt_longitudeValue");
+
+    // Check JS item is selected
+    PageLeftPane.switchSegment(PagePaneSegment.JS);
+    PageLeftPane.selectedItem().contains("Transformers");
+
+    // Check Query item is selected
+    PageLeftPane.switchSegment(PagePaneSegment.Queries);
+    PageLeftPane.selectedItem().contains("addUser");
+  });
+  it("will not retain the add state of the segment", () => {
+    // Switch to add new in UI
+    PageLeftPane.switchSegment(PagePaneSegment.UI);
+    PageLeftPane.switchToAddNew();
+
+    // Switch to add new in Query
+    PageLeftPane.switchSegment(PagePaneSegment.Queries);
+    PageLeftPane.switchToAddNew();
+
+    // Assert add state not in UI
+    PageLeftPane.switchSegment(PagePaneSegment.UI);
+    // Assumes that selected item is not visible when in add state
+    PageLeftPane.selectedItem().should("be.visible");
+
+    // Assert add state not in Query
+    PageLeftPane.switchSegment(PagePaneSegment.Queries);
+    // Assumes that selected item is not visible when in add state
+    PageLeftPane.selectedItem().should("be.visible");
+  });
+});
