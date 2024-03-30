134a3f79cf54d808fd64847423d441f8fd9660e2
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-03-30 09:01:33 +0530

    Smaller diff? 🤔

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/views/FromRequest.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/views/FromRequest.java
@@ -4,4 +4,4 @@ package com.appsmith.external.views;
  * Intended to annotate fields that can be set by HTTP request payloads, but should NOT be included
  * in HTTP responses sent back to the client.
  */
-public interface FromRequest {}
+public interface FromRequest extends Views.Public {}
--- app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ce/ActionControllerCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ce/ActionControllerCE.java
@@ -3,6 +3,7 @@ package com.appsmith.server.controllers.ce;
 import com.appsmith.external.models.ActionDTO;
 import com.appsmith.external.models.ActionExecutionResult;
 import com.appsmith.external.views.FromRequest;
+import com.appsmith.external.views.ToResponse;
 import com.appsmith.external.views.Views;
 import com.appsmith.server.constants.FieldName;
 import com.appsmith.server.constants.Url;
@@ -61,7 +62,7 @@ public class ActionControllerCE {
         this.actionExecutionSolution = actionExecutionSolution;
     }
 
-    @JsonView(Views.Public.class)
+    @JsonView(ToResponse.class)
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<ResponseDTO<ActionDTO>> createAction(
@@ -73,7 +74,7 @@ public class ActionControllerCE {
                 .map(created -> new ResponseDTO<>(HttpStatus.CREATED.value(), created, null));
     }
 
-    @JsonView(Views.Public.class)
+    @JsonView(ToResponse.class)
     @PutMapping("/{defaultActionId}")
     public Mono<ResponseDTO<ActionDTO>> updateAction(
             @PathVariable String defaultActionId,
