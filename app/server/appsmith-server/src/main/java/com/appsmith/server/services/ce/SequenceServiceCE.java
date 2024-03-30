fe4f7d5d88df62ac824a549aa1d61ba4f05d69fd
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-03-30 16:32:20 +0530

    undo changes in SequenceServiceCE

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/SequenceServiceCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/SequenceServiceCE.java
@@ -5,5 +5,9 @@ import reactor.core.publisher.Mono;
 
 public interface SequenceServiceCE {
 
+    Mono<Long> getNext(String name);
+
+    Mono<Long> getNext(Class<? extends BaseDomain> domainClass, String suffix);
+
     Mono<String> getNextAsSuffix(Class<? extends BaseDomain> domainClass, String suffix);
 }
--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/SequenceServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/SequenceServiceCEImpl.java
@@ -2,7 +2,7 @@ package com.appsmith.server.services.ce;
 
 import com.appsmith.external.models.BaseDomain;
 import com.appsmith.server.domains.Sequence;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Update;
 import reactor.core.publisher.Mono;
@@ -11,12 +11,17 @@ import static org.springframework.data.mongodb.core.FindAndModifyOptions.options
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 
-@RequiredArgsConstructor
 public class SequenceServiceCEImpl implements SequenceServiceCE {
 
     private final ReactiveMongoTemplate mongoTemplate;
 
-    private Mono<Long> getNext(String name) {
+    @Autowired
+    public SequenceServiceCEImpl(ReactiveMongoTemplate mongoTemplate) {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @Override
+    public Mono<Long> getNext(String name) {
         return mongoTemplate
                 .findAndModify(
                         query(where("name").is(name)),
@@ -27,9 +32,13 @@ public class SequenceServiceCEImpl implements SequenceServiceCE {
     }
 
     @Override
+    public Mono<Long> getNext(Class<? extends BaseDomain> domainClass, String suffix) {
+        return getNext(mongoTemplate.getCollectionName(domainClass) + suffix);
+    }
+
+    @Override
     public Mono<String> getNextAsSuffix(Class<? extends BaseDomain> domainClass, String suffix) {
-        final String className = domainClass.getName();
-        final String name = className.substring(0, 1).toLowerCase() + className.substring(1) + suffix;
-        return getNext(name).map(number -> number > 1 ? " " + number : "");
+        return getNext(mongoTemplate.getCollectionName(domainClass) + suffix)
+                .map(number -> number > 1 ? " " + number : "");
     }
 }
