4b27a59b1c43881babc6dad9e92b1eb6c86350fa
Vemparala Surya Vamsi <121419957+vsvamsi1@users.noreply.github.com>
2024-04-01 17:45:09 +0530

    chore: remove feature flag code of consolidated api (#32248)
    Removed feature flag related code to consolidated api, also corrections
    in feature flags are made here.
    
    Fixes #31100
    
    ## Automation
    
    /ok-to-test tags="@tag.All"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > Tests running at:
    <https://github.com/appsmithorg/appsmith/actions/runs/8490467344>
    > Commit: `fce48b210983689994152b7ae77c773cfdca854a`
    > Workflow: `PR Automation test suite`
    > Tags: ``
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    
    
    
    
    
    
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced new feature flags to enhance user interface and default
    settings.
    - **Refactor**
    	- Simplified page loading logic for better performance.
    	- Enhanced error handling and reporting for a smoother user experience.
    - **Chores**
    	- Cleaned up unused imports and code for better maintainability.
    - **Bug Fixes**
    - Removed redundant checks and updated functionality to include new
    feature flags.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/ce/AppRouter.tsx
+++ app/client/src/ce/AppRouter.tsx
@@ -46,20 +46,13 @@ import { useDispatch, useSelector } from "react-redux";
 import * as Sentry from "@sentry/react";
 import { getSafeCrash, getSafeCrashCode } from "selectors/errorSelectors";
 import UserProfile from "pages/UserProfile";
-import {
-  getCurrentUserLoading,
-  getFeatureFlagsFetching,
-} from "selectors/usersSelectors";
 import Setup from "pages/setup";
 import SettingsLoader from "pages/AdminSettings/loader";
 import SignupSuccess from "pages/setup/SignupSuccess";
 import type { ERROR_CODES } from "@appsmith/constants/ApiConstants";
 import TemplatesListLoader from "pages/Templates/loader";
 import { getCurrentUser as getCurrentUserSelector } from "selectors/usersSelectors";
-import {
-  getTenantPermissions,
-  isTenantLoading,
-} from "@appsmith/selectors/tenantSelectors";
+import { getTenantPermissions } from "@appsmith/selectors/tenantSelectors";
 import useBrandingTheme from "utils/hooks/useBrandingTheme";
 import RouteChangeListener from "RouteChangeListener";
 import { initCurrentPage } from "../actions/initActions";
@@ -154,14 +147,8 @@ export function Routes() {
 export default function AppRouter() {
   const safeCrash: boolean = useSelector(getSafeCrash);
   const safeCrashCode: ERROR_CODES | undefined = useSelector(getSafeCrashCode);
-  const tenantIsLoading = useSelector(isTenantLoading);
-  const currentUserIsLoading = useSelector(getCurrentUserLoading);
-  const featuresIsLoading = useSelector(getFeatureFlagsFetching);
   const isConsolidatedPageLoading = useSelector(getIsConsolidatedPageLoading);
   const dispatch = useDispatch();
-  const isConsolidatedFetchEnabled = useFeatureFlag(
-    FEATURE_FLAG.rollout_consolidated_page_load_fetch_enabled,
-  );
 
   useEffect(() => {
     dispatch(initCurrentPage());
@@ -169,12 +156,7 @@ export default function AppRouter() {
 
   useBrandingTheme();
 
-  let isLoading: boolean;
-  if (isConsolidatedFetchEnabled) {
-    isLoading = isConsolidatedPageLoading;
-  } else {
-    isLoading = tenantIsLoading || currentUserIsLoading || featuresIsLoading;
-  }
+  const isLoading = isConsolidatedPageLoading;
   // hide the top loader once the tenant is loaded
   useEffect(() => {
     if (!isLoading) {
