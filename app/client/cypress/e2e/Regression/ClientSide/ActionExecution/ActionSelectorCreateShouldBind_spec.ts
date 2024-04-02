57c38440a4d32e9b07d30e45c512441e3b764a00
Hetu Nandu <hetunandu@gmail.com>
2024-04-02 18:18:01 +0530

    Add correct tag to test

--- app/client/cypress/e2e/Regression/ClientSide/ActionExecution/ActionSelectorCreateShouldBind_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/ActionExecution/ActionSelectorCreateShouldBind_spec.ts
@@ -8,30 +8,34 @@ import EditorNavigation, {
   EntityType,
 } from "../../../../support/Pages/EditorNavigation";
 
-describe("Creations via action selector should bind to the property", () => {
-  it("binds newly created query / api to the button onClick", () => {
-    entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON);
-    propPane.SelectPlatformFunction("onClick", "Execute a query");
-    agHelper.GetNClick(".t--create-datasources-query-btn");
-    agHelper.GetElement(".t--new-blank-api").first().click();
-    cy.wait("@createNewApi");
-    EditorNavigation.SelectEntityByName("Button1", EntityType.Widget);
-    agHelper.GetNAssertElementText(
-      propPane._actionCard,
-      "GETExecute a queryApi1.run",
-    );
-    entityExplorer.DeleteWidgetFromEntityExplorer("Button1");
-  });
+describe(
+  "Creations via action selector should bind to the property",
+  { tags: ["@tag.JS", "@tag.PropertyPane"] },
+  () => {
+    it("binds newly created query / api to the button onClick", () => {
+      entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON);
+      propPane.SelectPlatformFunction("onClick", "Execute a query");
+      agHelper.GetNClick(".t--create-datasources-query-btn");
+      agHelper.GetElement(".t--new-blank-api").first().click();
+      cy.wait("@createNewApi");
+      EditorNavigation.SelectEntityByName("Button1", EntityType.Widget);
+      agHelper.GetNAssertElementText(
+        propPane._actionCard,
+        "GETExecute a queryApi1.run",
+      );
+      entityExplorer.DeleteWidgetFromEntityExplorer("Button1");
+    });
 
-  it("binds newly created JSObject to the button onClick", () => {
-    entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON);
-    propPane.SelectPlatformFunction("onClick", "Execute a JS function");
-    agHelper.GetNClick(".t--create-js-object-btn");
-    cy.wait("@createNewJSCollection");
-    EditorNavigation.SelectEntityByName("Button1", EntityType.Widget);
-    agHelper.GetNAssertElementText(
-      propPane._actionCard,
-      "Execute a JS functionJSObject1.Button1onClick()",
-    );
-  });
-});
+    it("binds newly created JSObject to the button onClick", () => {
+      entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON);
+      propPane.SelectPlatformFunction("onClick", "Execute a JS function");
+      agHelper.GetNClick(".t--create-js-object-btn");
+      cy.wait("@createNewJSCollection");
+      EditorNavigation.SelectEntityByName("Button1", EntityType.Widget);
+      agHelper.GetNAssertElementText(
+        propPane._actionCard,
+        "Execute a JS functionJSObject1.Button1onClick()",
+      );
+    });
+  },
+);
