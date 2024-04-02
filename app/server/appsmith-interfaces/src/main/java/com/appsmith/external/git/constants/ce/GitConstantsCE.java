c6649751dfb5be996af5fd2104ae77364524ba4a
sondermanish <sonder.manish@gmail.com>
2024-04-02 17:27:17 +0530

    removed git commit

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/git/constants/ce/GitConstantsCE.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/git/constants/ce/GitConstantsCE.java
@@ -25,7 +25,5 @@ public class GitConstantsCE {
     public class GitMetricConstantsCE {
         public static final String CHECKOUT_REMOTE = "checkout-remote";
         public static final String HARD_RESET = "hard-reset";
-
-        public static final String GIT_COMMIT = "git-commit";
     }
 }
