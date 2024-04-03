ee777b05279269f918cea81a71d9b90cfce32284
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-04-03 10:38:28 +0530

    feat: Added test for canvas view mode

--- app/client/cypress/support/Pages/Canvas.ts
+++ app/client/cypress/support/Pages/Canvas.ts
@@ -1,3 +1,4 @@
+import { agHelper } from "../Objects/ObjectsCore";
 import EditorNavigation from "./EditorNavigation";
 
 class Canvas {
@@ -5,14 +6,24 @@ class Canvas {
     EditorNavigation.ShowCanvas();
     for (const widget of widgetNames) {
       // Ctrl click on widget name component
-      cy.get(`div[data-widgetname-cy='${widget}']`).realHover();
-      cy.get(`div[data-testid="t--settings-controls-positioned-wrapper"]`)
-        .contains(widget)
-        .click({
-          force: true,
-          ctrlKey: true,
-        });
+      this.commandClickWidget(widget);
     }
+  }
+
+  hoverOnWidget(widgetType: string) {
+    const selector = `.t--draggable-${widgetType}`;
+    agHelper.Sleep(500);
+    cy.get(selector).first().trigger("mouseover", { force: true }).wait(500);
+  }
+
+  commandClickWidget(widgetName: string) {
+    cy.get(`div[data-widgetname-cy='${widgetName}']`).realHover();
+    cy.get(`div[data-testid="t--settings-controls-positioned-wrapper"]`)
+      .contains(widgetName)
+      .click({
+        force: true,
+        ctrlKey: true,
+      });
   }
 }
 
