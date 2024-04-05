7623801fa5bdbff6639d51317e1aaf37da2367bf
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-04-05 17:47:10 +0530

    add comment + remove feature flag

--- app/client/src/ce/selectors/featureFlagsSelectors.ts
+++ app/client/src/ce/selectors/featureFlagsSelectors.ts
@@ -12,9 +12,6 @@ export const selectFeatureFlagCheck = (
 ): boolean => {
   const flagValues = selectFeatureFlags(state);
 
-  if (flagName === "ab_wds_enabled") return true;
-  if (flagName === "release_anvil_enabled") return true;
-
   if (flagName in flagValues) {
     return flagValues[flagName];
   }
