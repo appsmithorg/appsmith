94429a2cf0156f6584451bf5047fef2c170677ac
Hetu Nandu <hetunandu@gmail.com>
2024-04-09 14:32:07 +0530

    Revert "Revert "chore: Make pages section into a dropdown""
    This reverts commit ccb397717d1c4d84a2616ad2c5633364e3a970a1.

--- app/client/src/pages/Editor/IDE/Header/EditorTitle.tsx
+++ app/client/src/pages/Editor/IDE/Header/EditorTitle.tsx
@@ -1,52 +1,52 @@
-import React from "react";
-import { Flex, Text, Icon } from "design-system";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import {
+  Flex,
+  Text,
+  Icon,
+  Menu,
+  MenuTrigger,
+  MenuContent,
+} from "design-system";
 
 import { createMessage, HEADER_TITLES } from "@appsmith/constants/messages";
-import { setIdeEditorPagesActiveStatus } from "actions/ideActions";
-import { getPagesActiveStatus } from "selectors/ideSelectors";
+import { PagesSection } from "../EditorPane/PagesSection";
 
 const EditorTitle = ({ title }: { title: string }) => {
-  const dispatch = useDispatch();
-  const active = useSelector(getPagesActiveStatus);
-
-  const onClickHandler = () => {
-    dispatch(setIdeEditorPagesActiveStatus(!active));
-  };
+  const [active, setActive] = useState(false);
 
   return (
-    <Flex
-      alignItems={"center"}
-      borderRadius={"var(--ads-v2-border-radius)"}
-      className={"hover:bg-[var(--ads-v2-color-bg-subtle)] cursor-pointer"}
-      justifyContent={"center"}
-      onClick={onClickHandler}
-      p={"spaces-2"}
-    >
-      <Text
-        color={"var(--ads-v2-colors-content-label-inactive-fg)"}
-        kind="body-m"
-      >
-        {createMessage(HEADER_TITLES.EDITOR) + " /"}
-      </Text>
-      <Flex
-        alignItems={"center"}
-        className={"t--pages-switcher"}
-        data-active={active}
-        gap={"spaces-1"}
-        height={"100%"}
-        justifyContent={"center"}
-        paddingLeft={"spaces-2"}
-      >
-        <Text isBold kind={"body-m"}>
-          {title}
-        </Text>
-        <Icon
-          name={active ? "arrow-up-s-line" : "arrow-down-s-line"}
-          size={"md"}
-        />
-      </Flex>
-    </Flex>
+    <Menu onOpenChange={setActive}>
+      <MenuTrigger>
+        <div className="flex align-center justify-center py-2 hover:bg-[var(--ads-v2-color-bg-subtle)] cursor-pointer">
+          <Text
+            color={"var(--ads-v2-colors-content-label-inactive-fg)"}
+            kind="body-m"
+          >
+            {createMessage(HEADER_TITLES.EDITOR) + " /"}
+          </Text>
+          <Flex
+            alignItems={"center"}
+            className={"t--pages-switcher"}
+            data-active={active}
+            gap={"spaces-1"}
+            height={"100%"}
+            justifyContent={"center"}
+            paddingLeft={"spaces-2"}
+          >
+            <Text isBold kind={"body-m"}>
+              {title}
+            </Text>
+            <Icon
+              name={active ? "arrow-up-s-line" : "arrow-down-s-line"}
+              size={"md"}
+            />
+          </Flex>
+        </div>
+      </MenuTrigger>
+      <MenuContent align="start" width="300px">
+        <PagesSection />
+      </MenuContent>
+    </Menu>
   );
 };
 
