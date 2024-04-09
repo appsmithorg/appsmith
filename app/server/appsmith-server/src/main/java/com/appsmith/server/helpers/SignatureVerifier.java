5020002c35349338b67848b7529c55aa90910ebc
Abhijeet <abhi.nagarnaik@gmail.com>
2024-04-09 17:24:19 +0530

    chore: Rotate sigining keys used for signature verification from CS API calls

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/SignatureVerifier.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/SignatureVerifier.java
@@ -5,13 +5,15 @@ import org.apache.commons.collections.CollectionUtils;
 import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
 import org.bouncycastle.crypto.signers.Ed25519Signer;
 import org.bouncycastle.crypto.util.OpenSSHPublicKeyUtil;
-import org.pf4j.util.StringUtils;
 import org.springframework.http.HttpHeaders;
+import org.springframework.util.StringUtils;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 import static com.appsmith.server.constants.ApiConstants.CLOUD_SERVICES_SIGNATURE;
 import static com.appsmith.server.constants.ApiConstants.DATE;
@@ -19,19 +21,26 @@ import static com.appsmith.server.constants.ApiConstants.DATE;
 @Slf4j
 public class SignatureVerifier {
 
-    // Public key for verifying signatures with ED25519 scheme.
-    private static final String PUBLIC_VERIFICATION_KEY =
+    // Public key for verifying signatures with ED25519 scheme. We are using two keys to support key rotation. Once the
+    // keys are rotated on CS side, we can remove the previous key in the upcoming versions.
+    private static final String CURRENT_PUBLIC_VERIFICATION_KEY =
+            "AAAAC3NzaC1lZDI1NTE5AAAAIAt5shghJhvGR0JQHBoOZ73N8JjRo31MPpY1y67O1C2G";
+    private static final String PREVIOUS_PUBLIC_VERIFICATION_KEY =
             "AAAAC3NzaC1lZDI1NTE5AAAAICNwJ+zx2opXjjOga/YyzRxb2czvNgQ/twA+miCKDIX3";
 
     private static final String TIMESTAMP = "timestamp";
 
     private static final String EQUAL = "=";
 
-    private static final AsymmetricKeyParameter publicKeyParameters;
+    private static final List<AsymmetricKeyParameter> publicKeyParameters = new ArrayList<>(2);
 
     static {
-        publicKeyParameters =
-                OpenSSHPublicKeyUtil.parsePublicKey(Base64.getDecoder().decode(PUBLIC_VERIFICATION_KEY));
+        if (StringUtils.hasLength(PREVIOUS_PUBLIC_VERIFICATION_KEY)) {
+            publicKeyParameters.add(
+                    OpenSSHPublicKeyUtil.parsePublicKey(Base64.getDecoder().decode(PREVIOUS_PUBLIC_VERIFICATION_KEY)));
+        }
+        publicKeyParameters.add(
+                OpenSSHPublicKeyUtil.parsePublicKey(Base64.getDecoder().decode(CURRENT_PUBLIC_VERIFICATION_KEY)));
     }
 
     /**
@@ -46,13 +55,10 @@ public class SignatureVerifier {
         }
         String signature = headers.get(CLOUD_SERVICES_SIGNATURE).get(0);
         String date = headers.get(DATE).get(0);
-        if (StringUtils.isNullOrEmpty(signature) || StringUtils.isNullOrEmpty(date)) {
+        if (!StringUtils.hasLength(signature) || !StringUtils.hasLength(date)) {
             return false;
         }
         try {
-            if (publicKeyParameters == null) {
-                return false;
-            }
             return isSignatureValid(signature, date);
         } catch (Exception exception) {
             log.debug("Error occurred while verifying CS signature.", exception);
@@ -72,23 +78,31 @@ public class SignatureVerifier {
         byte[] signatureBytes = Base64.getUrlDecoder().decode(encodedSignature);
         byte[] signingDataBytes = signingData.getBytes(StandardCharsets.UTF_8);
 
-        // Set up Ed25519 verifier
-        Ed25519Signer verifier = new Ed25519Signer();
-        verifier.init(false, publicKeyParameters);
-        verifier.update(signingDataBytes, 0, signingDataBytes.length);
+        for (AsymmetricKeyParameter publicKeyParameter : publicKeyParameters) {
+            // Set up Ed25519 verifier
+            Ed25519Signer verifier = setupVerifier(signingDataBytes, publicKeyParameter);
 
-        // Verify the signature to check if the data is tampered
-        if (verifier.verifySignature(signatureBytes)) {
-            String decodedData = new String(Base64.getDecoder().decode(signingData));
-            // To avoid the replay attacks check if the date provided in the header is within 24hrs and matches with
-            // the date used while encoding the data.
-            // Data format for ref: String.format("data=%s_timestamp=%s", dataset.toString(), date)
-            String timestampFieldFormat = TIMESTAMP + EQUAL;
-            String date =
-                    decodedData.substring(decodedData.indexOf(timestampFieldFormat) + timestampFieldFormat.length());
+            // Verify the signature to check if the data is tampered
+            if (verifier.verifySignature(signatureBytes)) {
+                String decodedData = new String(Base64.getDecoder().decode(signingData));
+                // To avoid the replay attacks check if the date provided in the header is within 24hrs and matches with
+                // the date used while encoding the data.
+                // Data format for ref: String.format("data=%s_timestamp=%s", dataset.toString(), date)
+                String timestampFieldFormat = TIMESTAMP + EQUAL;
+                String date = decodedData.substring(
+                        decodedData.indexOf(timestampFieldFormat) + timestampFieldFormat.length());
 
-            return Instant.parse(date).isAfter(Instant.now().minus(24, ChronoUnit.HOURS));
+                return Instant.parse(date).isAfter(Instant.now().minus(24, ChronoUnit.HOURS));
+            }
         }
         return false;
+    }
+
+    private static Ed25519Signer setupVerifier(byte[] signingDataBytes, AsymmetricKeyParameter keyParameters) {
+        // Set up Ed25519 verifier
+        Ed25519Signer verifier = new Ed25519Signer();
+        verifier.init(false, keyParameters);
+        verifier.update(signingDataBytes, 0, signingDataBytes.length);
+        return verifier;
     }
 }
