38a983c5f2c9e0058caed08ebcb92251dcbfac9c
Jacques Ikot <jacquesikot@gmail.com>
2024-04-12 08:41:10 +0100

    fix feature flag

--- app/client/cypress/e2e/Regression/ClientSide/ExplorerTests/Widgets_Sidebar.ts
+++ app/client/cypress/e2e/Regression/ClientSide/ExplorerTests/Widgets_Sidebar.ts
@@ -118,6 +118,7 @@ describe(
     });
 
     it("2. All widgets should be present within their tags and these tags should be collapsible", () => {
+      featureFlagIntercept({ release_drag_drop_building_blocks_enabled: true });
       agHelper.GetElement(entityExplorer._widgetTagsList).each(($widgetTag) => {
         // check that tags are collapsible
         cy.wrap($widgetTag).find(locators._adsV2CollapsibleHeader).click({
@@ -208,6 +209,7 @@ describe(
     });
 
     it("4. Widget search should work", () => {
+      featureFlagIntercept({ release_drag_drop_building_blocks_enabled: true });
       agHelper.TypeText(entityExplorer._widgetSearchInput, "text");
       agHelper.AssertElementLength(entityExplorer._widgetCards, 3);
 
