5764147a7c98b380e4e75b74a3c0d3bd88a4ae55
Rahul Barwal <rahul.barwal@appsmith.com>
2024-04-16 14:52:33 +0530

    Refactor WidgetAdditionSagas.ts to improve code organization and remove unused imports

--- app/client/src/sagas/BuildingBlocksSagas.ts
+++ app/client/src/sagas/BuildingBlocksSagas.ts
@@ -1,8 +1,12 @@
+import type { ImportBuildingBlockToApplicationResponse } from "@appsmith/api/ApplicationApi";
+import ApplicationApi from "@appsmith/api/ApplicationApi";
 import type { ReduxAction } from "@appsmith/constants/ReduxActionConstants";
 import {
   ReduxActionErrorTypes,
   ReduxActionTypes,
 } from "@appsmith/constants/ReduxActionConstants";
+import { getCanvasWidgets } from "@appsmith/selectors/entitiesSelector";
+import { getCurrentWorkspaceId } from "@appsmith/selectors/selectedWorkspaceSelectors";
 import { isAirgapped } from "@appsmith/utils/airgapHelpers";
 import { flattenDSL } from "@shared/dsl";
 import type { WidgetProps } from "@shared/dsl/src/migrate/types";
@@ -15,44 +19,56 @@ import {
 import { pasteWidget } from "actions/widgetActions";
 import { selectWidgetInitAction } from "actions/widgetSelectionActions";
 import type { ApiResponse } from "api/ApiResponses";
-import ApplicationApi from "@appsmith/api/ApplicationApi";
-import { getCanvasWidgets } from "@appsmith/selectors/entitiesSelector";
-import { getCurrentWorkspaceId } from "@appsmith/selectors/selectedWorkspaceSelectors";
 import { STARTER_BUILDING_BLOCKS } from "constants/TemplatesConstants";
 import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
-import {
-  type WidgetLayoutPositionInfo,
-  getWidgetLayoutMetaInfo,
-} from "layoutSystems/anvil/utils/layouts/widgetPositionUtils";
+import type { WidgetLayoutPositionInfo } from "layoutSystems/anvil/utils/layouts/widgetPositionUtils";
 import type { CopiedWidgetData } from "layoutSystems/anvil/utils/paste/types";
 import { getWidgetHierarchy } from "layoutSystems/anvil/utils/paste/utils";
-import { LayoutSystemTypes } from "layoutSystems/types";
-import type { CanvasWidgetsReduxState } from "reducers/entityReducers/canvasWidgetsReducer";
 import { all, call, delay, put, select, takeEvery } from "redux-saga/effects";
 import {
   getCurrentApplicationId,
   getCurrentPageId,
 } from "selectors/editorSelectors";
-import { getLayoutSystemType } from "selectors/layoutSystemSelectors";
-import { saveCopiedWidgets } from "utils/storage";
+import { getCopiedWidgets, saveCopiedWidgets } from "utils/storage";
 import { validateResponse } from "./ErrorSagas";
 import { postPageAdditionSaga } from "./TemplatesSagas";
 import { SelectionRequestType } from "./WidgetSelectUtils";
-import { getWidgets } from "./selectors";
-import type { PluginType } from "entities/Action";
 
 const isAirgappedInstance = isAirgapped();
 
-interface ImportBuildingBlockToApplicationResponse {
-  widgetDsl: string;
-  onPageLoadActions: {
-    confirmBeforeExecute: boolean;
-    id: string;
-    jsonPathKeys: string[];
-    name: string;
-    pluginType: PluginType;
-    timeoutInMillisecond: number;
-  }[];
+export function* saveBuildingBlockWidgetsToStore(
+  response: ApiResponse<ImportBuildingBlockToApplicationResponse>,
+) {
+  const buildingBlockDsl = JSON.parse(response.data.widgetDsl);
+  const buildingBlockWidgets = buildingBlockDsl.children;
+  const flattenedBlockWidgets = buildingBlockWidgets.map(
+    (widget: WidgetProps) => flattenDSL(widget),
+  );
+
+  const widgetsToPasteInCanvas: CopiedWidgetData[] = yield all(
+    flattenedBlockWidgets.map((widget: FlattenedWidgetProps, index: number) => {
+      const widgetPositionInfo: WidgetLayoutPositionInfo | null = null;
+      return {
+        hierarchy: getWidgetHierarchy(
+          buildingBlockWidgets[index].type,
+          buildingBlockWidgets[index].widgetId,
+        ),
+        list: Object.values(widget)
+          .map((obj) => ({ ...obj }))
+          .reverse(),
+        parentId: MAIN_CONTAINER_WIDGET_ID,
+        widgetId: buildingBlockWidgets[index].widgetId,
+        widgetPositionInfo,
+      };
+    }),
+  );
+
+  yield saveCopiedWidgets(
+    JSON.stringify({
+      widgets: widgetsToPasteInCanvas,
+      flexLayers: [],
+    }),
+  );
 }
 
 function* apiCallForForkBuildingBlockToApplication(request: {
@@ -71,54 +87,11 @@ function* apiCallForForkBuildingBlockToApplication(request: {
         workspaceId: request.workspaceId,
       });
     const isValid: boolean = yield validateResponse(response);
-    const layoutSystemType: LayoutSystemTypes =
-      yield select(getLayoutSystemType);
 
-    const allWidgets: CanvasWidgetsReduxState = yield select(getWidgets);
     yield select(getCanvasWidgets);
 
     if (isValid) {
-      const buildingBlockDsl = JSON.parse(response.data.widgetDsl);
-      const buildingBlockWidgets = buildingBlockDsl.children;
-      const flattenedBlockWidgets = buildingBlockWidgets.map(
-        (widget: WidgetProps) => flattenDSL(widget),
-      );
-
-      const widgetsToPasteInCanvas: CopiedWidgetData[] = yield all(
-        flattenedBlockWidgets.map(
-          (widget: FlattenedWidgetProps, index: number) => {
-            let widgetPositionInfo: WidgetLayoutPositionInfo | null = null;
-            if (
-              widget.parentId &&
-              layoutSystemType === LayoutSystemTypes.ANVIL
-            ) {
-              widgetPositionInfo = getWidgetLayoutMetaInfo(
-                allWidgets[widget?.parentId]?.layout[0] ?? null,
-                widget.widgetId,
-              );
-            }
-            return {
-              hierarchy: getWidgetHierarchy(
-                buildingBlockWidgets[index].type,
-                buildingBlockWidgets[index].widgetId,
-              ),
-              list: Object.values(widget)
-                .map((obj) => ({ ...obj }))
-                .reverse(),
-              parentId: MAIN_CONTAINER_WIDGET_ID,
-              widgetId: buildingBlockWidgets[index].widgetId,
-              widgetPositionInfo,
-            };
-          },
-        ),
-      );
-
-      yield saveCopiedWidgets(
-        JSON.stringify({
-          widgets: widgetsToPasteInCanvas,
-          flexLayers: [],
-        }),
-      );
+      yield saveBuildingBlockWidgetsToStore(response);
 
       yield put(pasteWidget(false, { x: 0, y: 0 }));
       yield call(postPageAdditionSaga, request.applicationId);
@@ -158,6 +131,7 @@ function* forkStarterBuildingBlockToApplicationSaga(
     templateName: string;
   }>,
 ) {
+  const existingCopiedWidgets: unknown = yield call(getCopiedWidgets);
   try {
     const activePageId: string = yield select(getCurrentPageId);
     const applicationId: string = yield select(getCurrentApplicationId);
@@ -174,6 +148,9 @@ function* forkStarterBuildingBlockToApplicationSaga(
     yield put({
       type: ReduxActionErrorTypes.IMPORT_STARTER_BUILDING_BLOCK_TO_APPLICATION_ERROR,
     });
+  }
+  if (existingCopiedWidgets) {
+    yield call(saveCopiedWidgets, JSON.stringify(existingCopiedWidgets));
   }
 }
 export default function* watchActionSagas() {
