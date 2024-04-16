cf79fa0d1486f4aeb4e3f92fc64bcc2c9b27449f
Abhijeet <abhi.nagarnaik@gmail.com>
2024-04-17 00:00:38 +0530

    feat: Add projections for generic method

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/BaseAppsmithRepositoryCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/BaseAppsmithRepositoryCEImpl.java
@@ -30,6 +30,7 @@ import jakarta.persistence.criteria.CriteriaUpdate;
 import jakarta.persistence.criteria.Expression;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.Selection;
 import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.NonNull;
@@ -338,6 +339,11 @@ public abstract class BaseAppsmithRepositoryCEImpl<T extends BaseDomain> impleme
                             Specification.allOf(specifications).toPredicate(root, cq, cb),
                             root.get(FieldName.DELETED_AT).isNull());
 
+                    if (!CollectionUtils.isEmpty(params.getFields())) {
+                        List<Selection<?>> selectionList = new ArrayList<>();
+                        params.getFields().forEach(f -> selectionList.add(root.get(f)));
+                        cq.multiselect(selectionList);
+                    }
                     if (!permissionGroups.isEmpty()) {
                         Map<String, String> fnVars = new HashMap<>();
                         fnVars.put("p", params.getPermission().getValue());
