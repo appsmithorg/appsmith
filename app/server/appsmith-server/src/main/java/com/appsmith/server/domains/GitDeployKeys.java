5e44cffe2a66b8bb932162f4292ac0957c28d89f
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-04-17 11:12:54 +0530

    Add missing `@Document` on GitDeployKeys

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/GitDeployKeys.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/GitDeployKeys.java
@@ -1,9 +1,14 @@
 package com.appsmith.server.domains;
 
+import com.appsmith.external.models.BaseDomain;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-public class GitDeployKeys {
+@EqualsAndHashCode(callSuper = true)
+@Document
+public class GitDeployKeys extends BaseDomain {
     String email;
 
     GitAuth gitAuth;
