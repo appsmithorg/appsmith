21e8d3a0060b9b4721b49149f307354b073332d7
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-04-18 18:51:17 +0530

    imports

--- app/server/appsmith-server/src/test/java/com/appsmith/server/controllers/PageControllerTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/controllers/PageControllerTest.java
@@ -20,7 +20,13 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
