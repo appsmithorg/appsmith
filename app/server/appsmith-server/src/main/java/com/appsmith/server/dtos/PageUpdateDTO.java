a8ebe14683a6fc58689e2528ea3c28a4d6eebfb4
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-04-19 12:06:26 +0530

    fix: Add `isHidden` to PageUpdateDTO

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/PageUpdateDTO.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/PageUpdateDTO.java
@@ -6,13 +6,15 @@ import jakarta.validation.constraints.Size;
 public record PageUpdateDTO(
         @Pattern(regexp = "[^/\\\\:]+", message = "/, \\, : not allowed in page names") @Size(max = 30) String name,
         @Pattern(regexp = "[-a-z]+") @Size(max = 20) String icon,
-        @Pattern(regexp = "[-\\w]*") String customSlug) {
+        @Pattern(regexp = "[-\\w]*") String customSlug,
+        Boolean isHidden) {
 
     public PageDTO toPageDTO() {
         final PageDTO page = new PageDTO();
         page.setName(name);
         page.setIcon(icon);
         page.setCustomSlug(customSlug);
+        page.setIsHidden(isHidden);
         return page;
     }
 }
