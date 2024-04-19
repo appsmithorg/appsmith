fc4ce87e90f5c18f144277a1d29a563f4451e4b2
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2024-04-19 16:15:11 +0530

    corrected time

--- app/client/cypress/e2e/Regression/ClientSide/Templates/ForkTemplateToGitConnectedApp.js
+++ app/client/cypress/e2e/Regression/ClientSide/Templates/ForkTemplateToGitConnectedApp.js
@@ -44,7 +44,7 @@ describe(
       cy.get(template.templateViewForkButton).first().click();
       cy.waitUntil(() => cy.xpath("//span[text()='Setting up the template']"), {
         errorMsg: "Setting Templates did not finish even after 75 seconds",
-        timeout: 950000,
+        timeout: 75000,
         interval: 5000,
       }).then(($ele) => {
         cy.wrap($ele).should("have.length", 0);
