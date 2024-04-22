c2722b9cd5f15c18d82b5f7bf8ca91557db1ce76
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-04-22 09:52:10 +0530

    Update AppsmithErrorTest.java

--- app/server/appsmith-server/src/test/java/com/appsmith/server/helpers/AppsmithErrorTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/helpers/AppsmithErrorTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
