60d45ea6ff2205244d39e9e0aef0e88cb1997d55
Jacques Ikot <jacquesikot@gmail.com>
2024-04-22 15:58:37 +0100

    feat: add analytics for drag and drop building blocks (#32699)
    ## Description
    > [!TIP]  
    Add events to track the dragging of building blocks, dropping of blocks,
    and the time taken from drag till drop is complete.
    
    **Drag Event**
    ```
    AnalyticsUtil.logEvent("DRAG_BUILDING_BLOCK_INITIATED", {
            applicationId,
            workspaceId,
            source: "explorer",
            eventData: {
              buildingBlockName: props.details.displayName,
            },
          });
    ```
    
    **Drop Event**
    ```
    AnalyticsUtil.logEvent("DROP_BUILDING_BLOCK_INITIATED", {
            applicationId,
            workspaceId,
            source: "explorer",
            eventData: {
              buildingBlockName: props.details.displayName,
            },
          });
    
    AnalyticsUtil.logEvent("DROP_BUILDING_BLOCK_COMPLETED", {
            applicationId,
            workspaceId,
            source: "explorer",
            eventData: {
              buildingBlockName: dragDetails.newWidget.displayName,
              timeTakenToCompletion: timeTakenTo CompleteValueInSeconds,
              timeTakenToDropWidgets: timeTakenValueInSeconds
            },
          });
    ```
    
    Fixes #32492  
    
    ## Automation
    
    /ok-to-test tags="@tag.Widget"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/8785306375>
    > Commit: 8316506b039256ad6d171a3a81ddaec56cecdfc2
    > Cypress dashboard url: <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=8785306375&attempt=1"
    target="_blank">Click here!</a>
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced analytics tracking for building block drag-and-drop
    operations.
    - Enhanced functionality for adding and managing building blocks within
    the application.
    - **Refactor**
    - Updated action type constants for better consistency in handling
    building block operations.
    - **Bug Fixes**
    - Improved logic for setting the start time of building block drag
    operations to ensure accurate tracking.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Anagh Hegde <anagh@appsmith.com>
    Co-authored-by: Rahul Barwal <rahul.barwal@appsmith.com>

--- app/client/src/selectors/buildingBlocksSelectors.ts
+++ app/client/src/selectors/buildingBlocksSelectors.ts
@@ -2,3 +2,6 @@ import type { AppState } from "@appsmith/reducers";
 
 export const isDraggingBuildingBlockToCanvas = (state: AppState) =>
   state.ui.buildingBlocks.isDraggingBuildingBlocksToCanvas;
+
+export const getBuildingBlockDragStartTimestamp = (state: AppState) =>
+  state.ui.buildingBlocks.buildingBlockDragStartTimestamp;
