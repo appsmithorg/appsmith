586cd0d3ddc31d680b37331a6129822cfc3c6fd0
Aman Agarwal <aman@appsmith.com>
2024-04-25 00:53:39 +0530

    fix: change closeModal using string to modal.name

--- app/client/cypress/e2e/Regression/ClientSide/ActionExecution/ActionSelector_JsToNonJSMode_2_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/ActionExecution/ActionSelector_JsToNonJSMode_2_spec.ts
@@ -253,7 +253,7 @@ describe("JS to non-JS mode in Action Selector", { tags: ["@tag.JS"] }, () => {
       0,
     );
 
-    propPane.EnterJSContext("onClick", "{{closeModal('Modal1')}}", true, false);
+    propPane.EnterJSContext("onClick", "{{closeModal(Modal1.name)}}", true, false);
     propPane.ToggleJSMode("onClick", false);
 
     agHelper.GetNAssertElementText(
