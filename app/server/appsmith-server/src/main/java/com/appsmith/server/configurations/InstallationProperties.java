d5bbc51df1c04a09bd00cab5bb83d6b0cb2da60d
Trisha Anand <trisha@appsmith.com>
2024-04-25 15:56:51 +0530

    Adding baseline DTO to read deployment properties from disk

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/InstallationProperties.java
@@ -0,0 +1,8 @@
+package com.appsmith.server.configurations;
+
+import lombok.Data;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Data
+public class InstallationProperties extends InstallationPropertiesCE {}
