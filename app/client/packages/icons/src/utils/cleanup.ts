3f95e2075e6e81928cd76b9456b7f505bb5e6ca0
Valera Melnikov <valera@appsmith.com>
2024-04-25 11:45:49 +0300

    chore: add autogeneration stories script

--- app/client/packages/icons/src/utils/cleanup.ts
+++ app/client/packages/icons/src/utils/cleanup.ts
@@ -5,6 +5,8 @@ async function cleanup() {
   await fs.emptyDirSync("./src/icons/Thumbnails");
   await fs.emptyDirSync("./src/components/Icons");
   await fs.emptyDirSync("./src/components/Thumbnails");
+  await fs.emptyDirSync("./src/stories");
+  await fs.writeFile(`./src/index.ts`, "");
 }
 
 cleanup();
