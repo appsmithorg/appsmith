6541b940930e61592404b8f1b99582e9d617833f
Valera Melnikov <valera@appsmith.com>
2024-04-25 11:45:49 +0300

    chore: add autogeneration stories script

--- app/client/packages/icons/src/utils/generateIndex.ts
+++ app/client/packages/icons/src/utils/generateIndex.ts
@@ -1,46 +1,39 @@
-import fg from "fast-glob";
-import type { PathLike } from "fs";
 import fs from "fs-extra";
-import path from "path";
 
-const createIndexItem = (name: string, dir: string) => {
+const createImportListString = (name: string, dir: string) => {
   return `export { ${name} } from "./components/${dir}/${name}";
 `;
 };
 
 async function generateIndex() {
-  const entries = await fg("./src/components/**/*.tsx");
-
-  entries.map(async (filepath: PathLike) => {
-    await fs.readFile(filepath, "utf-8", async (err) => {
-      if (err) {
-        // eslint-disable-next-line no-console
-        return console.error(err);
-      }
-
-      const dir = path
-        .dirname(filepath as string)
-        .replace("./src/components/", "");
-      const name = path.basename(filepath as string).replace(".tsx", "");
-
-      // Clearing the data in the file
-      await fs.writeFile(`./src/index.ts`, "");
-      await fs.appendFile(
-        `./src/index.ts`,
-        createIndexItem(name, dir),
-        "utf8",
-        function (err) {
-          // eslint-disable-next-line no-console
-          if (err) return console.error(err);
-        },
-      );
-    });
-  });
+  await appendExports("Thumbnails");
+  await appendExports("Icons");
 
   // eslint-disable-next-line no-console
   console.error(
     "\x1b[32mIndex.ts file generation completed successfully!\x1b[0m",
   );
+}
+
+async function appendExports(dir: string) {
+  await fs.readdir(`./src/components/${dir}/`, async (err, files) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      return console.error(err);
+    }
+
+    let importList = ``;
+
+    files.forEach((file) => {
+      const name = file.replace(".tsx", "");
+      importList += createImportListString(name, dir);
+    });
+
+    await fs.appendFile(`./src/index.ts`, importList, "utf8", function (err) {
+      // eslint-disable-next-line no-console
+      if (err) return console.error(err);
+    });
+  });
 }
 
 generateIndex();
