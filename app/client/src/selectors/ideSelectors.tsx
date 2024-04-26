dfead818cd8467219b0d9d05b3d589e04957f944
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-04-26 17:06:57 +0530

    feat: Removed tabs revamp feature flag (#32956)
    ## Description
    
    This PR will remove tabs revamp feature flag and make it available for
    every users.
    
    
    Fixes #32955
    
    ## Automation
    
    /ok-to-test tags="@tag.Sanity, @tag.IDE"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/8847166703>
    > Commit: 7c27a28e1f73a8470543e453c7000d3d773116e9
    > Cypress dashboard url: <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=8847166703&attempt=1"
    target="_blank">Click here!</a>
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    
    
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **Refactor**
    - Removed the feature flag for the IDE tabs revamp, simplifying the
    rendering logic and component behavior across various IDE components.
    - Streamlined the handling of tabs in the IDE, eliminating conditional
    feature flag checks and associated conditional rendering.
    - Adjusted test suites to align with the removal of the feature flag,
    ensuring consistency in component behavior testing.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/selectors/ideSelectors.tsx
+++ app/client/src/selectors/ideSelectors.tsx
@@ -47,9 +47,5 @@ export const getQueryTabs = (state: AppState) =>
 
 export const getIDETabs = (state: AppState) => state.ui.ide.tabs;
 
-export const getIsTabsRevampEnabled = createSelector(
-  selectFeatureFlags,
-  (flags) => flags.release_ide_tabs_revamp_enabled,
-);
 export const getShowCreateNewModal = (state: AppState) =>
   state.ui.ide.showCreateModal;
