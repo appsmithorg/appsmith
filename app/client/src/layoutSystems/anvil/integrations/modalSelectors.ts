b68ad19c09f768b99d28b72626ceefaa1b79056d
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-04-30 12:16:45 +0530

    fix: use a separate state to get currently open detached widgets.

--- app/client/src/layoutSystems/anvil/integrations/modalSelectors.ts
+++ app/client/src/layoutSystems/anvil/integrations/modalSelectors.ts
@@ -1,15 +1,5 @@
 import type { AppState } from "@appsmith/reducers";
-import { getAllDetachedWidgetIds, getWidgetsMeta } from "sagas/selectors";
 
 export const getCurrentlyOpenAnvilDetachedWidgets = (state: AppState) => {
-  const allExistingDetachedWidgets = getAllDetachedWidgetIds(state);
-  if (allExistingDetachedWidgets.length === 0) {
-    return [];
-  }
-  const metaWidgets = getWidgetsMeta(state);
-  const currentlyOpenWidgets = allExistingDetachedWidgets.filter((modalId) => {
-    const modal = metaWidgets[modalId];
-    return modal && modal.isVisible;
-  });
-  return currentlyOpenWidgets;
+  return state.ui.anvilDetachedWidgets.currentlyOpenDetachedWidgets;
 };
