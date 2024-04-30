ea289b838f045f2ff994c9e6ba8521705defa11c
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2024-04-30 17:35:27 +0530

    added new event

--- app/client/src/sagas/InitSagas.ts
+++ app/client/src/sagas/InitSagas.ts
@@ -290,10 +290,11 @@ export function* startAppEngine(action: ReduxAction<AppEnginePayload>) {
       action.payload.mode,
     );
     engine.startPerformanceTracking();
+    yield call(engine.setupEngine, action.payload);
     const allResponses: InitConsolidatedApi = yield call(getInitResponses, {
       ...action.payload,
     });
-    yield call(engine.setupEngine, action.payload);
+    yield put({ type: ReduxActionTypes.LINT_SETUP });
     const { applicationId, toLoadPageId } = yield call(
       engine.loadAppData,
       action.payload,
