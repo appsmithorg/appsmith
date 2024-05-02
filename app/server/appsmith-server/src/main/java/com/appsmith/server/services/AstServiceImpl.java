809710245dccc32c6e0c51e50ed386883198011b
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-05-02 13:00:23 +0530

    chore: Add an internal request ID for logging (#33086)
    /ok-to-test tags="@tag.All"
    
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/8918415111>
    > Commit: f902355ece5445d27fd8a65dcf4607afb4c5d656
    > Cypress dashboard url: <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=8918415111&attempt=2"
    target="_blank">Click here!</a>
    
    <!-- end of auto-generated comment: Cypress test results  -->

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/AstServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/AstServiceImpl.java
@@ -2,6 +2,7 @@ package com.appsmith.server.services;
 
 import com.appsmith.server.configurations.CommonConfig;
 import com.appsmith.server.configurations.InstanceConfig;
+import com.appsmith.server.helpers.RTSCaller;
 import com.appsmith.server.services.ce.AstServiceCEImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AstServiceImpl extends AstServiceCEImpl implements AstService {
 
-    public AstServiceImpl(CommonConfig commonConfig, InstanceConfig instanceConfig) {
-        super(commonConfig, instanceConfig);
+    public AstServiceImpl(CommonConfig commonConfig, InstanceConfig instanceConfig, RTSCaller rtsCaller) {
+        super(commonConfig, instanceConfig, rtsCaller);
     }
 }
