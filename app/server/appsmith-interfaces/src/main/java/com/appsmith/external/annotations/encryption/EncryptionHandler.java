f37ef41d41b9925de233f10ca300f89bc3ffe044
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-05-02 07:42:01 +0530

    chore: Fix warnings in ConsolidatedAPIServiceImpl

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/annotations/encryption/EncryptionHandler.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/annotations/encryption/EncryptionHandler.java
@@ -9,6 +9,7 @@ import reactor.util.annotation.NonNull;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.WildcardType;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -171,6 +172,10 @@ public class EncryptionHandler {
                             Type[] typeArguments;
                             ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
                             typeArguments = parameterizedType.getActualTypeArguments();
+                            if (typeArguments[1] instanceof WildcardType) {
+                                return;
+                            }
+
                             Class<?> subFieldType = (Class<?>) typeArguments[1];
 
                             if (this.encryptedFieldsMap.containsKey(subFieldType)) {
