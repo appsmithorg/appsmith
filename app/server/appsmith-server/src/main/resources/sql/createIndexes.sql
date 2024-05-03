2e1bba84f39e0cf5eb63e9dc101e366ef847e246
Nirmal Sarswat <nirmal@appsmith.com>
2024-05-03 17:01:34 +0530

    adding indexes and refactoring

---
+++ app/server/appsmith-server/src/main/resources/sql/createIndexes.sql
@@ -0,0 +1,10 @@
+create unique index if not exists u_name on config(name);
+create unique index if not exists u_email on password_reset_token(email);
+create unique index if not exists u_package_name on plugin(package_name);
+create unique index if not exists u_email on "user"(email);
+create unique index if not exists u_name on sequence(name);
+create unique index if not exists u_email on password_reset_token(email);
+create unique index if not exists plugin_name_package_name_version_index on plugin(plugin_name, package_name, version);
+create unique index if not exists u_user_id on user_data(user_id);
+create unique index if not exists u_workspace_datasource_deleted on datasource(workspace_id, name, deleted_at);
+create unique index if not exists u_workspace_app_deleted_git_application_metadata on application(workspace_id, name, deleted_at, (git_application_metadata ->> 'remoteUrl'), (git_application_metadata ->>'branchName'));
