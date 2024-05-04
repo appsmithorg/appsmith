3eb6d5bbb9e7f0139360d375f02acc6432370b23
Ankita Kinger <ankita@appsmith.com>
2024-05-04 22:03:29 +0530

    reverting a change

--- app/client/src/ee/pages/AdminSettings/config/index.ts
+++ app/client/src/ee/pages/AdminSettings/config/index.ts
@@ -1,3 +1,3 @@
 export * from "ce/pages/AdminSettings/config";
-import { default as CE_ConfigFactory } from "pages/AdminSettings/config/ConfigFactory";
-export default CE_ConfigFactory;
+import { ConfigFactory } from "pages/AdminSettings/config/ConfigFactory";
+export default ConfigFactory;
