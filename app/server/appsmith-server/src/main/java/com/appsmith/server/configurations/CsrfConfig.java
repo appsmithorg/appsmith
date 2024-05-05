56a3516a0581e68fd334f86874e974f67e97e58f
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-05-05 14:21:34 +0530

    chore: Fix login form right after logout

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/CsrfConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/CsrfConfig.java
@@ -1,8 +1,6 @@
 package com.appsmith.server.configurations;
 
-import com.appsmith.server.authentication.handlers.AccessDeniedHandler;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
@@ -11,7 +9,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfServerLogoutHandler;
 import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.security.web.server.csrf.DefaultCsrfToken;
+import org.springframework.security.web.server.csrf.ServerCsrfTokenRepository;
 import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.stereotype.Component;
@@ -20,13 +21,16 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
+import java.util.UUID;
+
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 /**
- * This component has two purposes:
+ * This component has three purposes:
  * <ol>
- * <li>The CSRFSpec customizer, or in English, responsible for configuring CSRF for our API. Handled by the {@code customize} method.
- * <li>A WebFilter, that ensures the CSRF token Mono actually gets subscribed to. Handled by the {@code filter} method.
+ * <li>The CSRFSpec customizer, or in English, responsible for configuring CSRF for our API. Handled by the {@link #customize} method.
+ * <li>A request matcher, responsible for deciding CSRF token should be checked. Handled by the {@link #matches} method.
+ * <li>A WebFilter, that ensures the CSRF token Mono actually gets subscribed to. Handled by the {@link #filter} method.
  * </ol>
  * References:
  * <ul>
@@ -36,13 +40,11 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
  * OWASP on CSRF</a>
  */
 @Component
-@RequiredArgsConstructor
-public class CsrfConfig implements Customizer<ServerHttpSecurity.CsrfSpec>, WebFilter {
+public class CsrfConfig implements Customizer<ServerHttpSecurity.CsrfSpec>, ServerWebExchangeMatcher, WebFilter {
 
-    private final AccessDeniedHandler accessDeniedHandler;
-
-    // This custom header is likely not needed anymore, now that we have token-style CSRF protection.
+    @SuppressWarnings("UastIncorrectHttpHeaderInspection")
     private static final String X_REQUESTED_BY_NAME = "X-Requested-By";
+
     private static final String X_REQUESTED_BY_VALUE = "Appsmith";
 
     void applyTo(ServerHttpSecurity http) {
@@ -50,16 +52,18 @@ public class CsrfConfig implements Customizer<ServerHttpSecurity.CsrfSpec>, WebF
     }
 
     @Override
-    public void customize(ServerHttpSecurity.CsrfSpec spec) {
-        spec.requireCsrfProtectionMatcher(this::match)
-                .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+    public void customize(@NonNull ServerHttpSecurity.CsrfSpec spec) {
+        // TODO: Add exception handling, on error, redirect to the login page or whatever the referrer was, with error
+        //  information.
+        spec.requireCsrfProtectionMatcher(this)
+                .csrfTokenRepository(new Repository())
                 // TODO: This shouldn't be necessary. This is weaker than the default and recommended option,
                 //  `XorServerCsrfTokenRequestAttributeHandler`. Find out why the default request handler isn't working.
-                .csrfTokenRequestHandler(new ServerCsrfTokenRequestAttributeHandler())
-                .accessDeniedHandler(accessDeniedHandler);
+                .csrfTokenRequestHandler(new ServerCsrfTokenRequestAttributeHandler());
     }
 
-    private Mono<ServerWebExchangeMatcher.MatchResult> match(ServerWebExchange exchange) {
+    @Override
+    public Mono<ServerWebExchangeMatcher.MatchResult> matches(@NonNull ServerWebExchange exchange) {
         final ServerHttpRequest request = exchange.getRequest();
         final HttpHeaders headers = request.getHeaders();
 
@@ -90,5 +94,45 @@ public class CsrfConfig implements Customizer<ServerHttpSecurity.CsrfSpec>, WebF
         final Mono<CsrfToken> csrfTokenMono =
                 ObjectUtils.defaultIfNull(exchange.getAttribute(CsrfToken.class.getName()), Mono.empty());
         return csrfTokenMono.then(chain.filter(exchange));
+    }
+
+    /**
+     * This little Repository class might just as well be the default {@link CookieServerCsrfTokenRepository} itself.
+     * Two of three methods just delegate to an instance of that class. We delegate instead of extending because it's a
+     * final class.
+     * <p>
+     * Problem: On logging out, the {@link CsrfServerLogoutHandler} calls the repository's {@link #saveToken} method,
+     * with a {@code null} token. This clears the {@code XSRF-TOKEN} cookie on the browser. Because of this, the client
+     * SPA is unable to load set a {@code _csrf} hidden input in the login form that shows up just after logout. The
+     * user has to refresh the page to get a new token in the cookie, and for the login form to work again.
+     * <p>
+     * To solve for this, we override the behaviour of {@link #saveToken} when a {@code null} token is passed, and
+     * instead of clearing, we generate a new token to be saved in the cookie. With this, the login form that shows up
+     * just after a logout is able to capture the CSRF token, and so the form works fine.
+     */
+    public static class Repository implements ServerCsrfTokenRepository {
+        private final CookieServerCsrfTokenRepository delegate = CookieServerCsrfTokenRepository.withHttpOnlyFalse();
+
+        @Override
+        public Mono<CsrfToken> generateToken(ServerWebExchange exchange) {
+            return delegate.generateToken(exchange);
+        }
+
+        @Override
+        public Mono<Void> saveToken(ServerWebExchange exchange, CsrfToken token) {
+            if (token == null) {
+                // Called by CsrfLogoutHandler, which tries to clear the token. We want to regenerate, not clear.
+                token = new DefaultCsrfToken(
+                        // Values taken from CookieServerCsrfTokenRepository
+                        "X-XSRF-TOKEN", "_csrf", UUID.randomUUID().toString());
+            }
+
+            return delegate.saveToken(exchange, token);
+        }
+
+        @Override
+        public Mono<CsrfToken> loadToken(ServerWebExchange exchange) {
+            return delegate.loadToken(exchange);
+        }
     }
 }
