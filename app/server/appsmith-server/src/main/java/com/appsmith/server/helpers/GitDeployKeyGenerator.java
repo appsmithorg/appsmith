86e1f895a7419710a3fb699e5301a8bcc0074722
Nidhi <nidhi@appsmith.com>
2024-05-06 18:25:32 +0530

    fix: Start using SHA2 instead of SHA1 algorithms for signing during git operations (#33166)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/GitDeployKeyGenerator.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/GitDeployKeyGenerator.java
@@ -1,22 +1,32 @@
 package com.appsmith.server.helpers;
 
-import com.appsmith.git.helpers.StringOutputStream;
 import com.appsmith.server.constants.Assets;
 import com.appsmith.server.domains.GitAuth;
 import com.appsmith.server.dtos.GitDeployKeyDTO;
 import com.appsmith.server.exceptions.AppsmithError;
 import com.appsmith.server.exceptions.AppsmithException;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.KeyPair;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.bouncycastle.crypto.params.AsymmetricKeyParameter;
+import org.bouncycastle.crypto.util.OpenSSHPublicKeyUtil;
+import org.bouncycastle.crypto.util.PublicKeyFactory;
 
+import java.io.IOException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
-import static org.reflections.Reflections.log;
+import static com.appsmith.external.git.constants.SSHConstants.ECDSA_KEY_FACTORY_IDENTIFIER;
+import static com.appsmith.external.git.constants.SSHConstants.ECDSA_TYPE_PREFIX;
+import static com.appsmith.external.git.constants.SSHConstants.RSA_KEY_FACTORY_IDENTIFIER;
+import static com.appsmith.external.git.constants.SSHConstants.RSA_TYPE_PREFIX;
 
+@Slf4j
 public class GitDeployKeyGenerator {
     public enum supportedProtocols {
         ECDSA(256, ""),
@@ -41,38 +51,45 @@ public class GitDeployKeyGenerator {
     }
 
     public static GitAuth generateSSHKey(String keyType) {
-        JSch jsch = new JSch();
-        KeyPair kpair;
-        int key;
+        String alg;
         int keySize;
-        if (!StringUtils.isEmpty(keyType) && keyType.equals(supportedProtocols.RSA.name())) {
-            key = KeyPair.RSA;
-            keySize = supportedProtocols.RSA.key_size;
-        } else {
-            key = KeyPair.ECDSA;
-            keySize = supportedProtocols.ECDSA.key_size;
-        }
+        KeyPair keyPair;
+        String publicKey;
 
         try {
-            kpair = KeyPair.genKeyPair(jsch, key, keySize);
-        } catch (JSchException e) {
-            log.error("failed to generate ECDSA key pair", e);
-            throw new AppsmithException(AppsmithError.SSH_KEY_GENERATION_ERROR);
+            if (!StringUtils.isEmpty(keyType) && keyType.equals(supportedProtocols.RSA.name())) {
+                alg = RSA_KEY_FACTORY_IDENTIFIER;
+                keySize = supportedProtocols.RSA.key_size;
+                keyPair = getKeyPair(alg, keySize);
+                publicKey = writeJavaPublicKeyToSSH2(keyPair.getPublic(), RSA_TYPE_PREFIX);
+            } else {
+                alg = ECDSA_KEY_FACTORY_IDENTIFIER;
+                keySize = supportedProtocols.ECDSA.key_size;
+                keyPair = getKeyPair(alg, keySize);
+                publicKey = writeJavaPublicKeyToSSH2(keyPair.getPublic(), ECDSA_TYPE_PREFIX);
+            }
+        } catch (NoSuchAlgorithmException | IOException e) {
+            log.debug("Error while creating key pair", e);
+            throw new AppsmithException(AppsmithError.SSH_KEY_GENERATION_ERROR, e);
         }
 
-        StringOutputStream privateKeyOutput = new StringOutputStream();
-        StringOutputStream publicKeyOutput = new StringOutputStream();
-
-        kpair.writePrivateKey(privateKeyOutput);
-        kpair.writePublicKey(publicKeyOutput, "appsmith");
-
         GitAuth gitAuth = new GitAuth();
-        gitAuth.setPublicKey(publicKeyOutput.toString());
-        gitAuth.setPrivateKey(privateKeyOutput.toString());
+        gitAuth.setPublicKey(publicKey);
+        byte[] encodedPrivateKey = keyPair.getPrivate().getEncoded();
+        gitAuth.setPrivateKey(Base64.getEncoder().encodeToString(encodedPrivateKey));
         gitAuth.setGeneratedAt(Instant.now());
         gitAuth.setDocUrl(Assets.GIT_DEPLOY_KEY_DOC_URL);
 
         return gitAuth;
+    }
+
+    private static KeyPair getKeyPair(String alg, int keySize) throws NoSuchAlgorithmException {
+        KeyPair keyPair;
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(alg);
+        keyPairGenerator.initialize(keySize);
+        keyPair = keyPairGenerator.generateKeyPair();
+
+        return keyPair;
     }
 
     public static List<GitDeployKeyDTO> getSupportedProtocols() {
@@ -80,5 +97,11 @@ public class GitDeployKeyGenerator {
         protocolList.add(supportedProtocols.ECDSA.getProtocolDetails());
         protocolList.add(supportedProtocols.RSA.getProtocolDetails());
         return protocolList;
+    }
+
+    private static String writeJavaPublicKeyToSSH2(final PublicKey publicKey, String prefix) throws IOException {
+        AsymmetricKeyParameter key = PublicKeyFactory.createKey(publicKey.getEncoded());
+        final byte[] sshKey = OpenSSHPublicKeyUtil.encodePublicKey(key);
+        return prefix + Base64.getEncoder().encodeToString(sshKey) + " appsmith";
     }
 }
