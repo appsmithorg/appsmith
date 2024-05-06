1b29d6ff4b11065713253160157c96be8b24e48f
Diljit VJ <diljit@appsmith.com>
2024-05-06 15:12:42 +0530

    Change messages

--- app/client/scripts/circular_deps_check.js
+++ app/client/scripts/circular_deps_check.js
@@ -1,11 +1,11 @@
 const { parseDependencyTree, parseCircular, prettyCircular } = require("dpdm");
 
-const CIRCULAR_DEPS_IN_RELEASE = 2965;
+const CIRCULAR_DEPS_IN_RELEASE = 2964;
 
 parseDependencyTree("./src", {}).then((tree) => {
   const circulars = parseCircular(tree);
   if (circulars.length > CIRCULAR_DEPS_IN_RELEASE) {
-    console.log("More deps than release!");
+    console.log("Failed!");
   }
   console.log(prettyCircular(circulars));
 });
