54311e35bfd74a55584e401e7fdf3b00e32a4a14
Diljit VJ <diljit@appsmith.com>
2024-05-06 14:31:51 +0530

    Format the message in the comment

--- app/client/scripts/circular_deps_check.js
+++ app/client/scripts/circular_deps_check.js
@@ -7,4 +7,6 @@ parseDependencyTree("./src", {}).then((tree) => {
   if (circulars.length > CIRCULAR_DEPS_IN_RELEASE) {
     console.log("More deps than release!");
   }
+
+  console.log(circulars);
 });
