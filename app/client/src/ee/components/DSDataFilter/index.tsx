de97783b79b9d16a2979f236215cbcf7c18fee00
Ankita Kinger <ankita@appsmith.com>
2024-05-06 17:36:12 +0530

    fix: Workspace name editor input going away on hovering other menu items of workspace (#33179)
    ## Description
    
    - Fixing workspace name editor input going away on hovering other menu
    items of workspace
    - Improving code splitting of some existing files
    
    
    Fixes [#32359](https://github.com/appsmithorg/appsmith/issues/32359)
    
    ## Automation
    
    /ok-to-test tags="@tag.All"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/8967913548>
    > Commit: 054e745df86e0115e49be6aaa4173961259c9921
    > Cypress dashboard url: <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=8967913548&attempt=1"
    target="_blank">Click here!</a>
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No

--- app/client/src/ee/components/DSDataFilter/index.tsx
+++ app/client/src/ee/components/DSDataFilter/index.tsx
@@ -1,3 +1,3 @@
-import DSDataFilter from "ce/components/DSDataFilter";
-
-export default DSDataFilter;
+export * from "ce/components/DSDataFilter";
+import { default as CE_DSDataFilter } from "ce/components/DSDataFilter";
+export default CE_DSDataFilter;
