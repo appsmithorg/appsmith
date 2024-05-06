c9a02e4bfc3ef1746d6d7aaf01c107c331aa53c8
Diljit VJ <diljit@appsmith.com>
2024-05-06 14:19:35 +0530

    Add the number of cylic deps in release branch

--- app/client/scripts/circular_deps_check.js
+++ app/client/scripts/circular_deps_check.js
@@ -1,10 +1,9 @@
 const { parseDependencyTree, parseCircular } = require("dpdm");
 
-const CIRCULAR_DEPS_IN_RELEASE = 0;
+const CIRCULAR_DEPS_IN_RELEASE = 2971;
 
 parseDependencyTree("./src", {}).then((tree) => {
   const circulars = parseCircular(tree);
-  console.log(circulars.length);
   if (circulars.length > CIRCULAR_DEPS_IN_RELEASE) {
     console.log("More deps than release!");
   }
