a49c9efd4edaf8d4ca77f256c0854621e73f0158
Aman Agarwal <aman@appsmith.com>
2024-05-08 12:11:03 +0530

    fix: allowing bottom tabs to be selectable as text

--- app/client/src/components/editorComponents/ApiResponseView.tsx
+++ app/client/src/components/editorComponents/ApiResponseView.tsx
@@ -546,7 +546,10 @@ function ApiResponseView(props: Props) {
   if (!open) return null;
 
   return (
-    <ResponseContainer className="t--api-bottom-pane-container" ref={panelRef}>
+    <ResponseContainer
+      className="t--api-bottom-pane-container select-text"
+      ref={panelRef}
+    >
       <Resizer
         initialHeight={responseTabHeight}
         onResizeComplete={(height: number) => {
