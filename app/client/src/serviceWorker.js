299b853d9a724b9f0dd9a1907e60bc1aea37cfd7
Diljit VJ <diljit@appsmith.com>
2024-05-13 10:30:38 +0530

    Revert other changes

--- app/client/src/serviceWorker.js
+++ app/client/src/serviceWorker.js
@@ -9,7 +9,7 @@ import {
 
 setCacheNameDetails({
   prefix: "appsmith",
-  // suffix: "",
+  suffix: undefined,
   precache: "precache-v1",
   runtime: "runtime",
   googleAnalytics: "appsmith-ga",
@@ -22,8 +22,6 @@ const regexMap = {
   ),
   shims: new RegExp(/shims\/.*.js/),
   profile: new RegExp(/v1\/(users\/profile|workspaces)/),
-  appEditUrl: new RegExp(/\/app\/[\w\d-]+\/[\w\d]+-[a-f0-9]+\/edit/),
-  appViewUrl: new RegExp(/\/app\/[\w\d-]+\/[\w\d]+-[a-f0-9]+(\/)?/),
 };
 
 /* eslint-disable no-restricted-globals */
@@ -35,58 +33,6 @@ precacheAndRoute(toPrecache);
 self.__WB_DISABLE_DEV_DEBUG_LOGS = false;
 skipWaiting();
 clientsClaim();
-
-const CACHE_NAME = "api-cache";
-const API_CACHE_DURATION = 10000; // Cache duration in milliseconds (10 seconds)
-const pendingApiRequests = new Map();
-
-// Function to handle API requests with caching, deduplication, and expiration
-const handleApiRequest = async (apiUrl) => {
-  const cache = await caches.open(CACHE_NAME);
-  let cachedResponse = await cache.match(apiUrl);
-  let now = Date.now();
-
-  // Check if there is a valid cached response
-  if (
-    cachedResponse &&
-    new Date(cachedResponse.headers.get("sw-cache-expires")) > now
-  ) {
-    return cachedResponse; // Serve from cache
-  }
-
-  // Check for ongoing request
-  if (pendingApiRequests.has(apiUrl)) {
-    return pendingApiRequests.get(apiUrl); // Wait for the existing request to complete
-  }
-
-  const fetchPromise = fetch(apiUrl)
-    .then(async (response) => {
-      if (response.ok) {
-        const clonedResponse = response.clone();
-        // Store in cache with expiration header
-        const headers = new Headers(clonedResponse.headers);
-        headers.set(
-          "sw-cache-expires",
-          new Date(now + API_CACHE_DURATION).toString(),
-        );
-        await cache.put(
-          apiUrl,
-          new Response(clonedResponse.body, {
-            status: clonedResponse.status,
-            statusText: clonedResponse.statusText,
-            headers: headers,
-          }),
-        );
-      }
-      return response;
-    })
-    .finally(() => {
-      pendingApiRequests.delete(apiUrl);
-    });
-
-  pendingApiRequests.set(apiUrl, fetchPromise);
-  return fetchPromise;
-};
 
 // This route's caching seems too aggressive.
 // TODO(abhinav): Figure out if this is really necessary.
@@ -106,32 +52,7 @@ registerRoute(({ url }) => {
 }, new StaleWhileRevalidate());
 
 registerRoute(
-  new Route(
-    ({ request, sameOrigin }) => {
-      return sameOrigin && request.destination === "document";
-    },
-    async ({ event, request }) => {
-      const isAppEditUrl = regexMap.appEditUrl.test(request.url);
-      const isAppViewUrl = regexMap.appViewUrl.test(request.url);
-
-      if (isAppEditUrl) {
-        const pageId = request.url.split("-").pop().split("/")[0];
-        const apiUrl = `/api/v1/consolidated-api/edit?defaultPageId=${pageId}`;
-        handleApiRequest(apiUrl);
-      } else if (isAppViewUrl) {
-        const pageId = request.url.split("-").pop().split("/")[0];
-        const apiUrl = `/api/v1/consolidated-api/view?defaultPageId=${pageId}`;
-        handleApiRequest(apiUrl);
-      }
-      const networkHandler = new NetworkOnly();
-      return networkHandler.handle({ event, request });
-    },
-  ),
-);
-
-// Route for fetching the API directly
-registerRoute(
-  new RegExp("/api/v1/consolidated-api/"),
-  async ({ url }) => handleApiRequest(url.href),
-  "GET",
+  new Route(({ request, sameOrigin }) => {
+    return sameOrigin && request.destination === "document";
+  }, new NetworkOnly()),
 );
