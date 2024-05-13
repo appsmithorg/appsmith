ff376d64c147742fe35fbfae523a56ef5825c446
Diljit VJ <diljit@appsmith.com>
2024-05-13 23:07:10 +0530

    Revert unnecessary changes

--- app/client/src/ce/AppRouter.tsx
+++ app/client/src/ce/AppRouter.tsx
@@ -156,8 +156,6 @@ export default function AppRouter() {
     dispatch(initCurrentPage());
   }, []);
 
-  useEffect(() => {}, [safeCrash]);
-
   useBrandingTheme();
 
   const isLoading = isConsolidatedPageLoading;
