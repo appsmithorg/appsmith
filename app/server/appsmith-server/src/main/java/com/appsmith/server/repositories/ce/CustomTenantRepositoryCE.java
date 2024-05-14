cc9456c1394d4cb886cab04dd89b5ec7bfbf63cf
nilansh <nilansh@appsmith.com>
2024-05-14 12:42:42 +0530

    moved cache invalidation to repository layer

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomTenantRepositoryCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomTenantRepositoryCE.java
@@ -2,5 +2,10 @@ package com.appsmith.server.repositories.ce;
 
 import com.appsmith.server.domains.Tenant;
 import com.appsmith.server.repositories.AppsmithRepository;
+import reactor.core.publisher.Mono;
 
-public interface CustomTenantRepositoryCE extends AppsmithRepository<Tenant> {}
+public interface CustomTenantRepositoryCE extends AppsmithRepository<Tenant> {
+    Mono<Tenant> save(Tenant tenant);
+
+    Mono<Tenant> update(String tenantId, Tenant tenant);
+}
