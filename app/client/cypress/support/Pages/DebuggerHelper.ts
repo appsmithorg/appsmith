cbc370bfc3097ebcfe8268878a01cd690b913aa5
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-05-15 14:12:47 +0700

    refactor: removed unused code

--- app/client/cypress/support/Pages/DebuggerHelper.ts
+++ app/client/cypress/support/Pages/DebuggerHelper.ts
@@ -110,21 +110,7 @@ export class DebuggerHelper {
         this.agHelper.AssertElementVisibility(
           this.locators._bottomPaneContainer[pageType],
         );
-        // this.agHelper.AssertHeight(
-        //   this.locators._bottomPaneContainer[pageType],
-        //   this.bottomPaneHeight,
-        // );
         break;
-      // case PageType.Query:
-      // case PageType.DataSources:
-      //   this.agHelper.AssertElementVisibility(
-      //     this.locators._bottomPaneContainer[pageType],
-      //   );
-      //   // this.agHelper.AssertHeight(
-      //   //   this.locators._bottomPaneContainer[pageType],
-      //   //   this.bottomPaneHeight - 1, // -1 to offset error
-      //   // );
-      //   break;
     }
   }
 
