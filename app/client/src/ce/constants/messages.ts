8c597f8845514dcfa7eb0ffcefc2eaa78f40ae81
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-05-15 14:11:58 +0700

    update: replace tab with string

--- app/client/src/ce/constants/messages.ts
+++ app/client/src/ce/constants/messages.ts
@@ -540,6 +540,7 @@ export const LOGS_FILTER_OPTION_SYSTEM = () => "System logs";
 export const NO_LOGS = () => "No logs to show";
 export const NO_ERRORS = () => "No signs of trouble here!";
 export const DEBUGGER_ERRORS = () => "Errors";
+export const DEBUGGER_RESPONSE = () => "Response";
 export const DEBUGGER_LOGS = () => "Logs";
 export const INSPECT_ENTITY = () => "Inspect entity";
 export const INSPECT_ENTITY_BLANK_STATE = () => "Select an entity to inspect";
