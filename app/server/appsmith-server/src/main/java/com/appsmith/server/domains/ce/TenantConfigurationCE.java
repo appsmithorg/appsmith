744e4ef7304f819f22738c626c15786b7d1bc197
Nilansh Bansal <nilansh@appsmith.com>
2024-05-16 11:49:34 +0530

    chore: Revert "feat: Moved Tenant Information to Redis for quick access" (#33512)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/TenantConfigurationCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/TenantConfigurationCE.java
@@ -10,13 +10,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.apache.commons.lang3.ObjectUtils;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Data
-public class TenantConfigurationCE implements Serializable {
+public class TenantConfigurationCE {
 
     private String googleMapsKey;
 
