8d36f30c9e5570dc5537f600b62b831dd3aa3432
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-05-17 14:40:07 +0530

    chore: adding code comments

--- app/client/src/layoutSystems/anvil/editor/canvas/providers/AnvilWidgetElevationProvider.tsx
+++ app/client/src/layoutSystems/anvil/editor/canvas/providers/AnvilWidgetElevationProvider.tsx
@@ -21,7 +21,19 @@ const AnvilWidgetElevationContext = createContext<
 
 export const useAnvilWidgetElevation = () =>
   useContext(AnvilWidgetElevationContext);
-
+/**
+ * AnvilWidgetElevationProvider is a indexes all sections and zones and records their evaluated value of elevation(Visual Separation).
+ *
+ * Why not just use the evaluated values directly?
+ * Because we need to keep track of the elevation of each widget in the editor to apply the correct elevation styles.
+ * elevation being a bindable property, we need to keep track of the evaluated value of elevation of each sections and zones in the editor.
+ *
+ * When adding compensators to the dragged widgets(useAnvilDnDCompensators), we need to know the elevation of the zone widget as well as its corresponding siblings
+ * to decide if the zone has to treated as a elevated zone or not.
+ *
+ * In-order to skip iterating the data tree every time we need to know the elevation of a widget, we are storing the elevation of each widget in this context.
+ * This way we do not have dependency on the data tree to know the elevation of a widget.
+ */
 export const AnvilWidgetElevationProvider = ({
   children,
 }: {
