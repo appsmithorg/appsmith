1ba2d2e5a342aa3e5ce86480ee484f7a7c2315f2
Abhijeet <abhi.nagarnaik@gmail.com>
2024-05-17 12:07:47 +0530

    chore: Add stricter checks for DB URL and don't provide fallback for DB URL

--- deploy/docker/fs/opt/appsmith/entrypoint.sh
+++ deploy/docker/fs/opt/appsmith/entrypoint.sh
@@ -178,13 +178,12 @@ configure_database_connection_url() {
     export APPSMITH_DB_URL="${APPSMITH_MONGODB_URI}"
   fi
 
-  if [[ "${APPSMITH_DB_URL}" == *"postgresql"* ]]; then
+  if [[ "${APPSMITH_DB_URL}" == "jdbc:postgresql:"* ]]; then
+      isPostgresUrl=1
+  elif [[ "${APPSMITH_DB_URL}" == "postgresql:"* ]]; then
     isPostgresUrl=1
-    # Check if APPSMITH_DB_URL is a PostgreSQL URL and doesn't start with "jdbc:", prepend "jdbc:"
-    if [[ "${APPSMITH_DB_URL}" != "jdbc:"* ]]; then
-      export APPSMITH_DB_URL="jdbc:${APPSMITH_DB_URL}"
-    fi
-  elif [[ "${APPSMITH_DB_URL}" == *"mongodb"* ]]; then
+    export APPSMITH_DB_URL="jdbc:${APPSMITH_DB_URL}"
+  elif [[ "${APPSMITH_DB_URL}" == "mongodb"* ]]; then
     isMongoUrl=1
   fi
 }
@@ -507,7 +506,7 @@ if [[ -z "${DYNO}" ]]; then
     echo "Initializing MongoDB"
     init_mongodb
     init_replica_set
-  elif [ $isPostgresUrl -eq 1 ]; then
+  elif [[ $isPostgresUrl -eq 1 ]]; then
     echo "Initializing Postgres"
     # init_postgres
   fi
