05e5bcc5f384353aade66b29983461c61f79c4ca
Nirmal Sarswat <nirmal@appsmith.com>
2024-05-21 17:13:49 +0530

    chore: super user creation bean

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/UpdateSuperUserHelper.java
@@ -0,0 +1,7 @@
+package com.appsmith.server.helpers;
+
+import com.appsmith.server.helpers.ce.UpdateSuperUserHelperCE;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class UpdateSuperUserHelper extends UpdateSuperUserHelperCE {}
