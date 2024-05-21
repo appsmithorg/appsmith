277ecc6b5cf08f2824ee8f4af6c78a5273bf4925
Nandan <nandan@thinkify.io>
2024-05-21 22:13:36 +0530

    added right paste command which is in use

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -1459,23 +1459,21 @@ Cypress.Commands.add(
     cy.get(tooltipSelector).should("be.visible").and("contain", expectedText);
   },
 );
+
 Cypress.Commands.add(
   "paste",
-  {
-    prevSubject: true,
-    element: true,
-  },
-  ($element, text) => {
-    const subString = text.slice(0, -1);
-    const lastChar = text.slice(-1);
-
-    cy.get(commonlocators.entityExplorersearch)
-      .clear({ force: true })
-      .click({ force: true })
-      .then(() => {
-        $element.text(subString);
-        $element.val(subString);
-        cy.get($element).type(lastChar);
-      });
+  { prevSubject: true },
+  (selector, pastePayload) => {
+    cy.wrap(selector).then(($destination) => {
+      const pasteEvent = Object.assign(
+        new Event("paste", { bubbles: true, cancelable: true }),
+        {
+          clipboardData: {
+            getData: () => pastePayload,
+          },
+        },
+      );
+      $destination[0].dispatchEvent(pasteEvent);
+    });
   },
 );
