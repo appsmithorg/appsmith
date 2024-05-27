a940dc179c3fba4781ed04afc0a3e361389c4fa0
sondermanish <sonder.manish@gmail.com>
2024-05-27 13:43:55 +0530

    removed mono.defer

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ce/autocommit/AutoCommitEligibilityHelperImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ce/autocommit/AutoCommitEligibilityHelperImpl.java
@@ -54,10 +54,11 @@ public class AutoCommitEligibilityHelperImpl extends AutoCommitEligibilityHelper
                 .defaultIfEmpty(FALSE)
                 .cache();
 
-        return Mono.defer(() -> gitRedisUtils.addFileLock(defaultApplicationId))
-                .then(Mono.defer(() -> isServerMigrationRequiredMonoCached))
-                .then(Mono.defer(() -> gitRedisUtils.releaseFileLock(defaultApplicationId)))
-                .then(Mono.defer(() -> isServerMigrationRequiredMonoCached))
+        return gitRedisUtils
+                .addFileLock(defaultApplicationId)
+                .then(isServerMigrationRequiredMonoCached)
+                .then(gitRedisUtils.releaseFileLock(defaultApplicationId))
+                .then(isServerMigrationRequiredMonoCached)
                 .onErrorResume(error -> {
                     log.debug(
                             "error while retrieving the metadata for defaultApplicationId : {}, branchName : {} error : {}",
