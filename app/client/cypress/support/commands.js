b62916c27d01a21bd333747212b5f7552bb5ce6d
Nandan <nandan@thinkify.io>
2024-05-29 21:21:18 +0530

    updated commands file

--- app/client/cypress/support/commands.js
+++ app/client/cypress/support/commands.js
@@ -120,7 +120,6 @@ Cypress.Commands.add("testSelfSignedCertificateSettingsInREST", (isOAuth2) => {
   cy.get(datasource.useCertInAuth).should("not.exist");
   cy.get(datasource.certificateDetails).should("not.exist");
   // cy.TargetDropdownAndSelectOption(datasource.useSelfSignedCert, "Yes");
-  cy.togglebar(datasource.useSelfSignedCert);
   cy.get(datasource.useSelfSignedCert).should("be.checked");
   if (isOAuth2) {
     cy.get(datasource.useCertInAuth).should("exist");
@@ -441,10 +440,6 @@ Cypress.Commands.add("DeleteWorkspaceByApi", () => {
       cy.log(response.status);
     });
   }
-});
-
-Cypress.Commands.add("togglebar", (value) => {
-  cy.get(value).check({ force: true }).should("be.checked");
 });
 
 Cypress.Commands.add("NavigateToJSEditor", () => {
