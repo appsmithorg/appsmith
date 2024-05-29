7814315bab793ff7c2b1c92cbf1d5020fd574988
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2024-05-29 14:12:16 +0530

    Add unit test case

--- app/server/appsmith-interfaces/src/test/java/com/appsmith/external/helpers/restApiUtils/helpers/RequestCaptureFilterTest.java
+++ app/server/appsmith-interfaces/src/test/java/com/appsmith/external/helpers/restApiUtils/helpers/RequestCaptureFilterTest.java
@@ -1,93 +1,46 @@
 package com.appsmith.external.helpers.restApiUtils.helpers;
 
-import com.appsmith.external.helpers.restApiUtils.connections.ApiKeyAuthentication;
-import com.appsmith.external.models.ActionExecutionRequest;
-import com.appsmith.external.models.ApiKeyAuth;
-import com.appsmith.external.models.DatasourceConfiguration;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ExchangeFunction;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.time.Duration;
+import java.lang.reflect.Method;
+import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RequestCaptureFilterTest {
 
-    private final RequestCaptureFilter requestCaptureFilter = new RequestCaptureFilter(new ObjectMapper());
-
-    @BeforeEach
-    public void setUp() throws IOException, URISyntaxException {
-        final ApiKeyAuthentication connection = ApiKeyAuthentication.create(
-                        new ApiKeyAuth(ApiKeyAuth.Type.QUERY_PARAMS, "token", null, "12345"))
-                .block(Duration.ofMillis(100));
-        connection.filter(Mockito.mock(ClientRequest.class), Mockito.mock(ExchangeFunction.class));
+    @Test
+    public void testMaskQueryParamInURL_NoQueryParams() throws Exception {
+        final URI uri = URI.create("http://example.com/api");
+        final String maskedUrl = invokeMaskQueryParamInURL(uri, "token");
+        assertEquals("http://example.com/api", maskedUrl);
     }
 
     @Test
-    public void testMaskQueryParamInURL_NoQueryParams() {
-
-        final DatasourceConfiguration dsConfig = new DatasourceConfiguration();
-        dsConfig.setUrl("http://example.com/api");
-        dsConfig.setAuthentication(new ApiKeyAuth(ApiKeyAuth.Type.QUERY_PARAMS, "token", null, "12345"));
-
-        final ActionExecutionRequest actionExecutionRequest = new ActionExecutionRequest();
-        actionExecutionRequest.setHttpMethod(HttpMethod.GET);
-
-        final ActionExecutionRequest result =
-                requestCaptureFilter.populateRequestFields(actionExecutionRequest, false, dsConfig);
-
-        assertEquals("http://example.com/api", result.getUrl());
+    public void testMaskQueryParamInURL_MatchingQueryParam() throws Exception {
+        final URI uri = URI.create("http://example.com/api?token=12345&user=admin");
+        final String maskedUrl = invokeMaskQueryParamInURL(uri, "token");
+        assertEquals("http://example.com/api?token=****&user=admin", maskedUrl);
     }
 
     @Test
-    public void testMaskQueryParamInURL_MatchingQueryParam() {
-        final DatasourceConfiguration dsConfig = new DatasourceConfiguration();
-        dsConfig.setUrl("http://example.com/api?token=12345&user=admin");
-        dsConfig.setAuthentication(new ApiKeyAuth(ApiKeyAuth.Type.QUERY_PARAMS, "token", null, "12345"));
-
-        final ActionExecutionRequest actionExecutionRequest = new ActionExecutionRequest();
-        actionExecutionRequest.setHttpMethod(HttpMethod.GET);
-
-        final ActionExecutionRequest result =
-                requestCaptureFilter.populateRequestFields(actionExecutionRequest, false, dsConfig);
-
-        assertEquals("http://example.com/api?token=****&user=admin", result.getUrl());
+    public void testMaskQueryParamInURL_NonMatchingQueryParam() throws Exception {
+        final URI uri = URI.create("http://example.com/api?session=abcd&user=admin");
+        final String maskedUrl = invokeMaskQueryParamInURL(uri, "token");
+        assertEquals("http://example.com/api?session=abcd&user=admin", maskedUrl);
     }
 
     @Test
-    public void testMaskQueryParamInURL_NonMatchingQueryParam() {
-        final DatasourceConfiguration dsConfig = new DatasourceConfiguration();
-        dsConfig.setUrl("http://example.com/api?session=abcd&user=admin");
-        dsConfig.setAuthentication(new ApiKeyAuth(ApiKeyAuth.Type.QUERY_PARAMS, "token", null, "12345"));
-
-        final ActionExecutionRequest actionExecutionRequest = new ActionExecutionRequest();
-        actionExecutionRequest.setHttpMethod(HttpMethod.GET);
-
-        final ActionExecutionRequest result =
-                requestCaptureFilter.populateRequestFields(actionExecutionRequest, false, dsConfig);
-
-        assertEquals("http://example.com/api?session=abcd&user=admin", result.getUrl());
+    public void testMaskQueryParamInURL_MultipleMatchingQueryParams() throws Exception {
+        final URI uri = URI.create("http://example.com/api?token=12345&token=67890&user=admin");
+        final String maskedUrl = invokeMaskQueryParamInURL(uri, "token");
+        assertEquals("http://example.com/api?token=****&token=****&user=admin", maskedUrl);
     }
 
-    @Test
-    public void testMaskQueryParamInURL_MultipleMatchingQueryParams() {
-        final DatasourceConfiguration dsConfig = new DatasourceConfiguration();
-        dsConfig.setUrl("http://example.com/api?token=12345&token=67890&user=admin");
-        dsConfig.setAuthentication(new ApiKeyAuth(ApiKeyAuth.Type.QUERY_PARAMS, "token", null, "12345"));
-
-        final ActionExecutionRequest actionExecutionRequest = new ActionExecutionRequest();
-        actionExecutionRequest.setHttpMethod(HttpMethod.GET);
-
-        final ActionExecutionRequest result =
-                requestCaptureFilter.populateRequestFields(actionExecutionRequest, false, dsConfig);
-
-        assertEquals("http://example.com/api?token=****&token=****&user=admin", result.getUrl());
+    private String invokeMaskQueryParamInURL(final URI uri, final String queryParamKeyToMask) throws Exception {
+        final Method method =
+                RequestCaptureFilter.class.getDeclaredMethod("maskQueryParamInURL", URI.class, String.class);
+        method.setAccessible(true);
+        return (String) method.invoke(null, uri, queryParamKeyToMask);
     }
 }
