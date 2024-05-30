8eed4b589ffc057aeb6878e9e3417ddc2afd1c7e
Anagh Hegde <anagh@appsmith.com>
2024-05-30 16:55:21 +0530

    fix: ignore DSL parsing errors in the forking flow (#33831)

--- app/server/appsmith-server/src/main/java/com/appsmith/server/fork/internal/ApplicationForkingServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/fork/internal/ApplicationForkingServiceCEImpl.java
@@ -348,6 +348,14 @@ public class ApplicationForkingServiceCEImpl implements ApplicationForkingServic
                 })
                 .flatMapIterable(tuple2 -> clonedPages)
                 .flatMap(clonedPage -> updateLayoutService.updatePageLayoutsByPageId(clonedPage.getId()))
+                .onErrorResume(throwable -> {
+                    if (throwable instanceof AppsmithException e
+                            && AppsmithError.INVALID_DYNAMIC_BINDING_REFERENCE.equals(e.getError())) {
+                        log.error("Error while cloning page {} ", throwable.getMessage());
+                        return Mono.just("");
+                    }
+                    return Mono.error(throwable);
+                })
                 // Now publish all the example applications which have been cloned to ensure that there is a
                 // view mode for the newly created user.
                 .then(Mono.just(newApplicationIds))
