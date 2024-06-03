06384344db582e56d6b873d7bbf41d1e6bb6b634
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-06-03 17:12:44 +0530

    fix types

--- app/client/src/components/propertyControls/CodeEditorControl.tsx
+++ app/client/src/components/propertyControls/CodeEditorControl.tsx
@@ -11,6 +11,7 @@ import {
 import LazyCodeEditor from "components/editorComponents/LazyCodeEditor";
 import { bindingHintHelper } from "components/editorComponents/CodeEditor/hintHelpers";
 import { slashCommandHintHelper } from "components/editorComponents/CodeEditor/commandsHelper";
+import type { EditorProps } from "components/editorComponents/CodeEditor";
 
 class CodeEditorControl extends BaseControl<ControlProps> {
   render() {
@@ -34,7 +35,7 @@ class CodeEditorControl extends BaseControl<ControlProps> {
         additionalDynamicData={this.props.additionalAutoComplete}
         hinting={[bindingHintHelper, slashCommandHintHelper]}
         input={{ value: propertyValue, onChange: this.onChange }}
-        maxHeight={controlConfig?.maxHeight as string | number | undefined}
+        maxHeight={controlConfig?.maxHeight as EditorProps["maxHeight"]}
         mode={EditorModes.TEXT_WITH_BINDING}
         positionCursorInsideBinding
         size={EditorSize.EXTENDED}
