a212ff922659c1fb67c8d663fb6eeb41c37842fb
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-06-03 17:20:21 +0530

    chore: Except for `id`, use `text` instead of `varchar` (#33929)
    [Slack
    conversation](https://theappsmith.slack.com/archives/C06Q3A97USE/p1717391965015429).

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/ce/V1__createTablesAndFunctions.sql
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/ce/V1__createTablesAndFunctions.sql
@@ -3,19 +3,19 @@ create table action_collection
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     default_resources jsonb,
-    application_id varchar(255),
-    context_type varchar(255),
+    application_id text,
+    context_type text,
     published_collection jsonb,
     unpublished_collection jsonb,
-    workspace_id varchar(255)
+    workspace_id text
 );
 
 create table application
@@ -23,48 +23,48 @@ create table application
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    app_is_example boolean not null,
+    app_is_example boolean not null default false,
     application_version integer,
     client_schema_version integer,
-    cloned_from_application_id varchar(255),
+    cloned_from_application_id text,
     collapse_invisible_widgets boolean,
-    color varchar(255),
-    edit_mode_theme_id varchar(255),
+    color text,
+    edit_mode_theme_id text,
     embed_setting jsonb,
     evaluation_version integer,
     export_with_configuration boolean,
     fork_with_configuration boolean,
-    forked_from_template_title varchar(255),
+    forked_from_template_title text,
     forking_enabled boolean,
     git_application_metadata jsonb,
-    icon varchar(255),
+    icon text,
     is_auto_update boolean,
     is_community_template boolean,
     is_manual_update boolean,
     is_public boolean,
     last_deployed_at timestamp(6) with time zone,
     last_edited_at timestamp(6) with time zone,
-    name varchar(255) not null,
+    name text not null,
     pages jsonb,
     published_app_layout jsonb,
     published_application_detail jsonb,
     published_customjslibs jsonb,
-    published_mode_theme_id varchar(255),
+    published_mode_theme_id text,
     published_pages jsonb,
     server_schema_version integer,
-    slug varchar(255),
+    slug text,
     unpublished_app_layout jsonb,
     unpublished_application_detail jsonb,
     unpublished_customjslibs jsonb,
     view_mode boolean,
-    workspace_id varchar(255)
+    workspace_id text
 );
 
 create table application_snapshot
@@ -72,14 +72,14 @@ create table application_snapshot
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    application_id varchar(255),
+    application_id text,
     chunk_order integer not null,
     data bytea
 );
@@ -89,14 +89,14 @@ create table asset
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    content_type varchar(255),
+    content_type text,
     data bytea
 );
 
@@ -105,18 +105,18 @@ create table collection
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     actions jsonb,
-    application_id varchar(255),
-    name varchar(255),
+    application_id text,
+    name text,
     shared boolean,
-    workspace_id varchar(255)
+    workspace_id text
 );
 
 create table config
@@ -124,15 +124,15 @@ create table config
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     config jsonb,
-    name varchar(255)
+    name text
         constraint uk_kjjh66cda2b9nc24it8fhbfwx
             unique
 );
@@ -142,10 +142,10 @@ create table customjslib
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
@@ -154,11 +154,11 @@ create table customjslib
     -- This was `String` in MongoDB, but since we need to store `NUL` (\u0000) character in it (for libraries like
     -- xlsx), which Postgres doesn't allow in text/varchar, we have to use a byte array (`bytea`) here.
     defs bytea,
-    docs_url varchar(255),
-    name varchar(255),
-    uid_string varchar(255),
+    docs_url text,
+    name text,
+    uid_string text,
     url text,
-    version varchar(255)
+    version text
 );
 
 create table datasource
@@ -166,10 +166,10 @@ create table datasource
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
@@ -180,10 +180,10 @@ create table datasource
     is_configured boolean,
     is_mock boolean,
     is_template boolean,
-    name varchar(255),
-    plugin_id varchar(255),
-    template_name varchar(255),
-    workspace_id varchar(255)
+    name text,
+    plugin_id text,
+    template_name text,
+    workspace_id text
 );
 
 create table datasource_storage
@@ -191,16 +191,16 @@ create table datasource_storage
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     datasource_configuration jsonb,
-    datasource_id varchar(255),
-    environment_id varchar(255),
+    datasource_id text,
+    environment_id text,
     invalids jsonb,
     is_configured boolean
 );
@@ -210,15 +210,15 @@ create table datasource_storage_structure
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    datasource_id varchar(255),
-    environment_id varchar(255),
+    datasource_id text,
+    environment_id text,
     structure jsonb
 );
 
@@ -227,16 +227,16 @@ create table email_verification_token
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    email varchar(255),
+    email text,
     token_generated_at timestamp(6) with time zone,
-    token_hash varchar(255)
+    token_hash text
 );
 
 create table git_deploy_keys
@@ -244,14 +244,14 @@ create table git_deploy_keys
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    email varchar(255),
+    email text,
     git_auth jsonb
 );
 
@@ -260,21 +260,21 @@ create table new_action
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     default_resources jsonb,
-    application_id varchar(255),
+    application_id text,
     documentation jsonb,
-    plugin_id varchar(255),
-    plugin_type varchar(255),
+    plugin_id text,
+    plugin_type text,
     published_action jsonb,
     unpublished_action jsonb,
-    workspace_id varchar(255)
+    workspace_id text
 );
 
 create table new_page
@@ -282,15 +282,15 @@ create table new_page
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     default_resources jsonb,
-    application_id varchar(255),
+    application_id text,
     published_page jsonb,
     unpublished_page jsonb
 );
@@ -300,17 +300,17 @@ create table password_reset_token
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    email varchar(255),
+    email text,
     first_request_time timestamp(6) with time zone,
-    request_count integer not null,
-    token_hash varchar(255)
+    request_count integer not null DEFAULT 0,
+    token_hash text
 );
 
 create table permission_group
@@ -318,22 +318,22 @@ create table permission_group
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     assigned_to_group_ids jsonb,
     assigned_to_user_ids jsonb,
-    default_domain_id varchar(255),
-    default_domain_type varchar(255),
-    default_workspace_id varchar(255),
-    description varchar(255),
-    name varchar(255) not null,
+    default_domain_id text,
+    default_domain_type text,
+    default_workspace_id text,
+    description text,
+    name text not null,
     permissions jsonb,
-    tenant_id varchar(255)
+    tenant_id text
 );
 
 create table plugin
@@ -341,42 +341,42 @@ create table plugin
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    action_component varchar(255),
+    action_component text,
     action_params jsonb,
     action_ui_config jsonb,
     allow_user_datasources boolean,
-    datasource_component varchar(255),
+    datasource_component text,
     datasource_params jsonb,
     datasource_ui_config jsonb,
     default_install boolean,
-    documentation_link varchar(255),
-    generatecrudpage_component varchar(255),
-    icon_location varchar(255),
+    documentation_link text,
+    generatecrudpage_component text,
+    icon_location text,
     is_dependent_oncs boolean,
-    is_remote_plugin boolean not null,
-    is_supported_for_air_gap boolean not null,
-    jar_location varchar(255),
-    max_appsmith_version_supported varchar(255),
-    min_appsmith_version_supported varchar(255),
-    name varchar(255),
-    package_name varchar(255),
-    plugin_name varchar(255),
-    response_type varchar(255),
-    type varchar(255),
-    ui_component varchar(255),
-    version varchar(255)
+    is_remote_plugin boolean not null default false,
+    is_supported_for_air_gap boolean not null default true,
+    jar_location text,
+    max_appsmith_version_supported text,
+    min_appsmith_version_supported text,
+    name text,
+    package_name text,
+    plugin_name text,
+    response_type text,
+    type text,
+    ui_component text,
+    version text
 );
 
 create table sequence
 (
-    name varchar(255) not null
+    name text not null
         primary key,
     next_number bigint
 );
@@ -386,16 +386,16 @@ create table tenant
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    display_name varchar(255),
-    pricing_plan varchar(255),
-    slug varchar(255) unique,
+    display_name text,
+    pricing_plan text,
+    slug text unique,
     tenant_configuration jsonb
 );
 
@@ -404,21 +404,21 @@ create table theme
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    application_id varchar(255),
+    application_id text,
     config jsonb,
-    display_name varchar(255),
+    display_name text,
     is_system_theme boolean not null,
-    name varchar(255),
+    name text,
     properties jsonb,
     stylesheet jsonb,
-    workspace_id varchar(255)
+    workspace_id text
 );
 
 create table usage_pulse
@@ -426,18 +426,18 @@ create table usage_pulse
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    email varchar(255),
-    instance_id varchar(255),
+    email text,
+    instance_id text,
     is_anonymous_user boolean,
-    tenant_id varchar(255),
-    "user" varchar(255),
+    tenant_id text,
+    "user" text,
     view_mode boolean
 );
 
@@ -446,21 +446,21 @@ create table "user"
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
-    current_workspace_id varchar(255),
+    current_workspace_id text,
     email text,
     email_verification_required boolean,
     email_verified boolean,
-    examples_workspace_id varchar(255),
+    examples_workspace_id text,
     group_ids jsonb,
     hashed_email text,
-    invite_token varchar(255),
+    invite_token text,
     is_anonymous boolean,
     is_enabled boolean,
     is_system_generated boolean,
@@ -468,9 +468,9 @@ create table "user"
     password text,
     password_reset_initiated boolean,
     permissions jsonb,
-    source varchar(255),
-    state varchar(255),
-    tenant_id varchar(255),
+    source text,
+    state text,
+    tenant_id text,
     workspace_ids jsonb
 );
 
@@ -479,25 +479,25 @@ create table user_data
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     git_profiles jsonb,
-    is_intercom_consent_given boolean not null,
-    proficiency varchar(255),
-    profile_photo_asset_id varchar(255),
+    is_intercom_consent_given boolean not null default false,
+    proficiency text,
+    profile_photo_asset_id text,
     recently_used_app_ids jsonb,
     recently_used_entity_ids jsonb,
     recently_used_workspace_ids jsonb,
-    release_notes_viewed_version varchar(255),
-    role varchar(255),
-    use_case varchar(255),
+    release_notes_viewed_version text,
+    role text,
+    use_case text,
     user_claims jsonb,
-    user_id varchar(255)
+    user_id text
 );
 
 create table workspace
@@ -505,24 +505,24 @@ create table workspace
     id varchar(255) not null
         primary key,
     created_at timestamp(6) with time zone,
-    created_by varchar(255),
+    created_by text,
     deleted_at timestamp(6) with time zone,
-    git_sync_id varchar(255),
-    modified_by varchar(255),
+    git_sync_id text,
+    modified_by text,
     policies jsonb,
     policy_map jsonb,
     updated_at timestamp(6) with time zone,
     default_permission_groups jsonb,
-    domain varchar(255),
-    email varchar(255),
+    domain text,
+    email text,
     has_environments boolean,
     is_auto_generated_workspace boolean,
-    logo_asset_id varchar(255),
-    name varchar(255),
+    logo_asset_id text,
+    name text,
     plugins jsonb,
-    slug varchar(255),
-    tenant_id varchar(255),
-    website varchar(255)
+    slug text,
+    tenant_id text,
+    website text
 );
 
 create function jsonb_minus(l jsonb, r text) returns jsonb
