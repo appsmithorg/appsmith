ef18170e8951b177379388693ac3036fd7dd7e96
Arpit Mohan <arpit@appsmith.com>
2024-06-04 12:33:29 +0530

    Adding a nodeJS script to setup Cypress on local machines. This replaces the shell script because it's more extendable and can be modified more easily by engineers within the team

--- app/client/cypress/scripts/cypress-local-setup.js
+++ app/client/cypress/scripts/cypress-local-setup.js
@@ -1,13 +1,7 @@
-import { execSync } from "child_process";
-import { createInterface } from "readline";
-import { readFileSync, existsSync, writeFileSync } from "fs";
-import promptSync from "prompt-sync";
-const prompt = promptSync();
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const { execSync } = require("child_process");
+const { readFileSync, existsSync, writeFileSync } = require("fs");
+const path = require("path");
+const prompt = require("prompt-sync")();
 
 // Function to check if a Docker container is running
 function isContainerRunning(containerName) {
@@ -21,105 +15,7 @@ function isContainerRunning(containerName) {
   }
 }
 
-async function setupCypress() {
-  // Get the baseUrl from cypress.config.ts file
-  let baseUrl = "https://dev.appsmith.com";
-  try {
-    const cypressConfig = readFileSync("../../cypress.config.ts", "utf8");
-    const baseUrlMatch = cypressConfig.match(/baseUrl\s*:\s*"([^"]+)"/);
-    if (baseUrlMatch) {
-      baseUrl = baseUrlMatch[1];
-      console.log(
-        "INFO",
-        `Base url is ${baseUrl}. Please verify if it is correct. If not, please update it in cypress.config.ts file.`,
-      );
-    } else {
-      console.error(
-        "ERROR",
-        "Base url not found in cypress.config.ts. Please configure `baseUrl` property in cypress.config.ts file.",
-      );
-      process.exit(1);
-    }
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      console.error("ERROR", "cypress.config.ts file not found");
-    } else {
-      console.error("ERROR", "Error reading cypress.config.ts file:", err);
-    }
-    process.exit(1);
-  }
-
-  // Check if appsmith is running. If it's not running, check if we want the user to continue without it.
-  try {
-    const isDevAppsmithAccessible = await fetch(baseUrl)
-      .then((response) => response.ok)
-      .catch((error) => {
-        console.error(
-          "ERROR",
-          `Error checking availability of dev.appsmith.com: ${error.message}`,
-        );
-        return false;
-      });
-
-    if (!isDevAppsmithAccessible) {
-      let user_input = prompt(
-        `https://dev.appsmith.com is not accessible. Do you wish to continue without setting it up? (yes/no): `,
-      );
-      user_input = user_input.trim().toLowerCase();
-      switch (user_input) {
-        case "yes":
-        case "y":
-          console.log("INFO", "Continuing without setting up dev.appsmith.com");
-          break;
-        case "no":
-        case "n":
-          rl.close();
-          process.exit(1);
-        default:
-          console.log("ERROR", "Invalid input. Please enter yes or no.");
-          rl.close();
-          process.exit(1);
-      }
-    }
-  } catch (error) {
-    console.error("ERROR", `Exception caught: ${error.message}`);
-  }
-
-  // Install Cypress using yarn install on the app/client repository
-  console.log("INFO", "Installing Cypress..");
-  try {
-    execSync("yarn install", { cwd: "../../" });
-  } catch (error) {
-    console.error("ERROR", `Error installing Cypress: ${error.message}`);
-  }
-
-  // Check if cypress.env.json file exists. If not, create it.
-  const filePath = "../../cypress.env.json";
-  if (!existsSync(filePath)) {
-    const testEnvData = {
-      env: {
-        USERNAME: "testUser@test.com",
-        PASSWORD: "testPass",
-        TESTUSERNAME1: "viewerappsmith@test.com",
-        TESTPASSWORD1: "viewerPass",
-        TESTUSERNAME2: "developerappsmith@test.com",
-        TESTPASSWORD2: "developerPass",
-      },
-    };
-    writeFileSync(filePath, JSON.stringify(testEnvData, null, 2));
-    console.log("INFO", "${GIT_REPO}/app/client/cypress.env.json file created");
-  } else {
-    console.log(
-      "INFO",
-      "${GIT_REPO}/app/client/cypress.env.json file already exists",
-    );
-  }
-
-  console.log(
-    "INFO",
-    "Please add APPSMITH_GIT_ROOT=./container-volumes/git-storage into server-side .env for running Git cases locally along with the server.",
-  );
-
+function ensureTEDIsRunning() {
   // Check if TED is running. If not, then ask user if they wish to pull and run the TED container
   const isTedRunning = isContainerRunning("ted");
 
@@ -159,14 +55,119 @@ async function setupCypress() {
           console.log("ERROR", "Invalid input. Please enter yes or no.");
           process.exit(1);
       }
-
-      rl.close();
     } catch (error) {
       console.error("ERROR", `Error: ${error.message}`);
-      rl.close();
       process.exit(1);
     }
   }
+}
+
+async function checkIfAppsmithIsRunning(baseUrl) {
+  // Check if appsmith is running. If it's not running, check if we want the user to continue without it.
+  try {
+    const isDevAppsmithAccessible = await fetch(baseUrl)
+      .then((response) => response.ok)
+      .catch((error) => {
+        console.error(
+          "ERROR",
+          `Error checking availability of dev.appsmith.com: ${error.message}`,
+        );
+        return false;
+      });
+
+    if (!isDevAppsmithAccessible) {
+      let user_input = prompt(
+        `https://dev.appsmith.com is not accessible. Do you wish to continue without setting it up? (yes/no): `,
+      );
+      user_input = user_input.trim().toLowerCase();
+      switch (user_input) {
+        case "yes":
+        case "y":
+          console.log("INFO", "Continuing without setting up dev.appsmith.com");
+          break;
+        case "no":
+        case "n":
+          process.exit(1);
+        default:
+          console.log("ERROR", "Invalid input. Please enter yes or no.");
+          process.exit(1);
+      }
+    }
+  } catch (error) {
+    console.error("ERROR", `Exception caught: ${error.message}`);
+  }
+}
+
+function getBaseUrl(repoRoot) {
+  try {
+    const cypressConfig = readFileSync(`${repoRoot}/cypress.config.ts`, "utf8");
+    const baseUrlMatch = cypressConfig.match(/baseUrl\s*:\s*"([^"]+)"/);
+    if (baseUrlMatch) {
+      baseUrl = baseUrlMatch[1];
+      console.log(
+        "INFO",
+        `Base url is ${baseUrl}. Please verify if it is correct. If not, please update it in cypress.config.ts file.`,
+      );
+      return baseUrl;
+    } else {
+      console.error(
+        "ERROR",
+        "Base url not found in cypress.config.ts. Please configure `baseUrl` property in cypress.config.ts file.",
+      );
+      process.exit(1);
+    }
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      console.error("ERROR", "cypress.config.ts file not found");
+    } else {
+      console.error("ERROR", "Error reading cypress.config.ts file:", err);
+    }
+    process.exit(1);
+  }
+}
+
+function ensureCypressEnvFileExists(repoRoot) {
+  // Check if cypress.env.json file exists. If not, create it.
+  const filePath = `${repoRoot}/cypress.env.json`;
+  if (!existsSync(filePath)) {
+    const testEnvData = {
+      USERNAME: "testUser@test.com",
+      PASSWORD: "testPass",
+      TESTUSERNAME1: "viewerappsmith@test.com",
+      TESTPASSWORD1: "viewerPass",
+      TESTUSERNAME2: "developerappsmith@test.com",
+      TESTPASSWORD2: "developerPass",
+    };
+    writeFileSync(filePath, JSON.stringify(testEnvData, null, 2));
+    console.log("INFO", `${repoRoot}/cypress.env.json file created`);
+  } else {
+    console.log("INFO", `${repoRoot}/cypress.env.json file already exists`);
+  }
+}
+
+async function setupCypress() {
+  // Get the baseUrl from cypress.config.ts file
+  let repoRoot = path.join(__dirname, "..", "..");
+  let baseUrl = getBaseUrl(repoRoot);
+
+  await checkIfAppsmithIsRunning(baseUrl);
+
+  // Install Cypress using yarn install on the app/client repository
+  console.log("INFO", "Installing Cypress..");
+  try {
+    execSync("yarn install", { cwd: `${repoRoot}` });
+  } catch (error) {
+    console.error("ERROR", `Error installing Cypress: ${error.message}`);
+  }
+
+  ensureCypressEnvFileExists(repoRoot);
+
+  console.log(
+    "INFO",
+    "Please add APPSMITH_GIT_ROOT=./container-volumes/git-storage into server-side .env for running Git cases locally along with the server.",
+  );
+
+  ensureTEDIsRunning();
 
   console.log(
     "INFO",
@@ -174,7 +175,7 @@ async function setupCypress() {
   );
   console.log(
     "INFO",
-    "In order to run single spec, please use the command: npx cypress run --spec <specpath>",
+    `In order to run single spec, please use the command: cd ${repoRoot} && npx cypress run --spec <specpath> --browser chrome`,
   );
   console.log(
     "INFO",
