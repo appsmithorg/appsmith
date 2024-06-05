a7dd0921e9467e535ca0d4abbb57fd4872a144e1
Pawan Kumar <pawan@appsmith.com>
2024-06-05 10:16:35 +0530

    chore: Create a new datasource for WDS table (#33928)
    Fixes #33388
    
    /ok-to-test tags="@tag.IDE, @tag.Widget, @tag.Table"<!-- This is an
    auto-generated comment: Cypress test results -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/9349570140>
    > Commit: 06384344db582e56d6b873d7bbf41d1e6bb6b634
    > Cypress dashboard url: <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=9349570140&attempt=1"
    target="_blank">Click here!</a>
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    ---------
    
    Co-authored-by: Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>

--- app/client/src/widgets/wds/WDSTableWidget/config/defaultsConfig.ts
+++ app/client/src/widgets/wds/WDSTableWidget/config/defaultsConfig.ts
@@ -1,6 +1,13 @@
 import { InlineEditingSaveOptions } from "../constants";
-import type { WidgetDefaultProps } from "WidgetProvider/constants";
+import {
+  BlueprintOperationTypes,
+  type WidgetDefaultProps,
+} from "WidgetProvider/constants";
 import { ResponsiveBehavior } from "layoutSystems/common/utils/constants";
+import { DEFAULT_DATA } from "../constants/data";
+import type { WidgetProps } from "widgets/BaseWidget";
+import type { DynamicPath } from "utils/DynamicBindingUtils";
+import get from "lodash/get";
 
 export const defaultsConfig = {
   responsiveBehavior: ResponsiveBehavior.Fill,
@@ -19,7 +26,7 @@ export const defaultsConfig = {
   dynamicPropertyPathList: [],
   dynamicBindingPathList: [],
   primaryColumns: {},
-  tableData: "",
+  tableData: DEFAULT_DATA,
   columnWidthMap: {},
   columnOrder: [],
   enableClientSideSearch: true,
@@ -35,4 +42,30 @@ export const defaultsConfig = {
   buttonLabel: "Action",
   buttonColor: "accent",
   buttonVariant: "filled",
+  blueprint: {
+    operations: [
+      {
+        type: BlueprintOperationTypes.MODIFY_PROPS,
+        fn: (widget: WidgetProps & { children?: WidgetProps[] }) => {
+          const dynamicPropertyPathList: DynamicPath[] = [
+            ...get(widget, "dynamicPropertyPathList", []),
+          ];
+
+          dynamicPropertyPathList.push({
+            key: "tableData",
+          });
+
+          const updatePropertyMap = [
+            {
+              widgetId: widget.widgetId,
+              propertyName: "dynamicPropertyPathList",
+              propertyValue: dynamicPropertyPathList,
+            },
+          ];
+
+          return updatePropertyMap;
+        },
+      },
+    ],
+  },
 } as unknown as WidgetDefaultProps;
