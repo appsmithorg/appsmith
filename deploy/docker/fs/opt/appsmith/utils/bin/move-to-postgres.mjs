ae23ad21883ac53bb3d8169084df2d60a06e275b
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-06-06 08:02:33 +0530

    chore: Don't write JSONL file if there's no data

--- deploy/docker/fs/opt/appsmith/utils/bin/move-to-postgres.mjs
+++ deploy/docker/fs/opt/appsmith/utils/bin/move-to-postgres.mjs
@@ -1,19 +1,19 @@
-import { spawn } from "child_process";
+import {spawn} from "child_process";
 import {MongoClient} from "mongodb";
-import * as fs from "node:fs"
+import * as fs from "node:fs";
 
 // Don't use `localhost` here, it'll try to connect on IPv6, irrespective of whether you have it enabled or not.
 let mongoDbUrl;
 
 let mongoDumpFile = null;
-const EXPORT_ROOT = "mongo-data"
+const EXPORT_ROOT = "mongo-data";
 
 for (let i = 2; i < process.argv.length; ++i) {
-  const arg = process.argv[i]
+  const arg = process.argv[i];
   if (arg.startsWith("--mongodb-url=") && !mongoDbUrl) {
     mongoDbUrl = extractValueFromArg(arg);
   } else if (arg.startsWith("--mongodb-dump=") && !mongoDumpFile) {
-    mongoDumpFile = extractValueFromArg(arg)
+    mongoDumpFile = extractValueFromArg(arg);
   } else {
     console.error("Unknown/unexpected argument: " + arg);
     process.exit(1);
@@ -33,36 +33,43 @@ if (mongoDumpFile) {
     stdio: "inherit",
   });
 
-  mongoDbUrl = "mongodb://localhost/tmp"
+  mongoDbUrl = "mongodb://localhost/tmp";
 
   // mongorestore 'mongodb://localhost/' --archive=mongodb-data.gz --gzip --nsFrom='appsmith.*' --nsTo='appsmith.*'
   spawn("mongorestore", [mongoDbUrl, "--archive=" + mongoDumpFile, "--gzip", "--noIndexRestore"]);
 }
 
 const mongoClient = await new MongoClient(mongoDbUrl);
-mongoClient.on("error", console.error)
+mongoClient.on("error", console.error);
 const mongoDb = mongoClient.db();
 
 fs.mkdirSync(EXPORT_ROOT, {recursive: true});
 
 for await (const {name: collectionName} of mongoDb.listCollections({}, {nameOnly: true})) {
   console.log("Collection:", collectionName);
-  const outFile = fs.openSync(EXPORT_ROOT + "/" + collectionName + ".jsonl", "w")
+  let outFile = null;
   for await (const doc of mongoDb.collection(collectionName).find()) {
     // TODO(Shri): Should we cleanup nested `ObjectId`s, like all over `doc`?
     doc.id = doc._id.toString();
     delete doc._id;
 
-    fs.writeSync(outFile, JSON.stringify(doc) + "\n")
+    if (outFile == null) {
+      // Don't create the file unless there's data to write.
+      outFile = fs.openSync(EXPORT_ROOT + "/" + collectionName + ".jsonl", "w");
+    }
+
+    fs.writeSync(outFile, JSON.stringify(doc) + "\n");
   }
 
-  fs.closeSync(outFile)
+  if (outFile != null) {
+    fs.closeSync(outFile);
+  }
 }
 
 await mongoClient.close();
 mongoServer?.kill();
 
-console.log("done")
+console.log("done");
 
 // TODO(Shri): We shouldn't need this.
 process.exit(0);
