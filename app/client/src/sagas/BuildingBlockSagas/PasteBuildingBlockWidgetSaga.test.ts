6a0a35691c7a2b38f76cbf3150ec7751079a45d9
Abhijeet <abhi.nagarnaik@gmail.com>
2024-06-06 22:35:23 +0530

    fix: Few more tests

--- app/client/src/sagas/BuildingBlockSagas/PasteBuildingBlockWidgetSaga.test.ts
+++ app/client/src/sagas/BuildingBlockSagas/PasteBuildingBlockWidgetSaga.test.ts
@@ -130,7 +130,7 @@ describe("pasteBuildingBlockWidgetsSaga", () => {
 
     // Step 7: mock the entire copied widget handling logic
     for (let i = 0; i < newWidgetList.length; i++) {
-      result = generator.next({ pageId: "pageId" });
+      result = generator.next({ pageId: "01234567890abcdef00000000" });
     }
 
     result = generator.next();
