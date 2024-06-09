9f01ca402f23632a28394f44d9c8af7e1ddfa5e7
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-06-09 11:45:25 +0530

    chore: Make core encrypt/decrypt methods statically available

---
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/helpers/EncryptionHelper.java
@@ -0,0 +1,32 @@
+package com.appsmith.external.helpers;
+
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+
+import java.util.HexFormat;
+
+public final class EncryptionHelper {
+
+    private static final String salt = System.getProperty("encrypt.salt");
+
+    private static final String password = System.getProperty("encrypt.password");
+
+    private static final HexFormat hexFormat = HexFormat.of();
+
+    private static final TextEncryptor textEncryptor = makeTextEncryptor();
+
+    private EncryptionHelper() {}
+
+    private static TextEncryptor makeTextEncryptor() {
+        final String saltInHex = hexFormat.formatHex(salt.getBytes());
+        return Encryptors.delux(password, saltInHex);
+    }
+
+    public static String encryptString(String plaintext) {
+        return textEncryptor.encrypt(plaintext);
+    }
+
+    public static String decryptString(String encryptedText) {
+        return textEncryptor.decrypt(encryptedText);
+    }
+}
