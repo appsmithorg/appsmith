99fa93d61e89a8df01bc22cd1abfd8527b4e73ac
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-06-10 16:37:50 +0530

    chore: Adding specs/tests for space distribution and copy paste sagas (#34063)
    [![workerB](https://img.shields.io/endpoint?url=https%3A%2F%2Fworkerb.linearb.io%2Fv2%2Fbadge%2Fprivate%2FU2FsdGVkX1992iLVivcpoDwVtCrlTUPBIEmtU4nlPs%2Fcollaboration.svg%3FcacheSeconds%3D60)](https://workerb.linearb.io/v2/badge/collaboration-page?magicLinkId=mFGYFxI)
    ## Description
    - Adding additional specs for space distribution and section deletion.
    - Adding unit tests for anvil pasting.
    
    
    Fixes #33739
    _or_  
    Fixes `Issue URL`
    > [!WARNING]  
    > _If no issue exists, please create an issue first, and check with the
    maintainers if the issue is valid._
    
    ## Automation
    
    /ok-to-test tags="@tag.Anvil"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/9443154667>
    > Commit: 4a770670aaf2f00d175066b597680345d840cd60
    > Cypress dashboard url: <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=9443154667&attempt=1"
    target="_blank">Click here!</a>
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    
    
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Added new test cases for verifying section removal and visual aspects
    of background-less zones in the Anvil layout system.
    - Introduced methods to handle mouse events for space distribution
    within sections.
    - Added mock data generation functionality for widgets, sections, zones,
    and layouts.
    
    - **Tests**
    - Implemented tests for paste operations in the Anvil layout system,
    including various mock functions and scenarios.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ app/client/src/layoutSystems/anvil/utils/paste/destinationUtils/destinationUtils.test.ts
@@ -0,0 +1,45 @@
+import { FlexLayerAlignment } from "layoutSystems/common/utils/constants";
+import { registerLayoutComponents } from "../../layouts/layoutUtils";
+import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
+import { getDestinedParent } from ".";
+import { generateMockDataWithSectionAndZone } from "./mockData.helper";
+
+describe("paste destination utils tests", () => {
+  beforeAll(() => {
+    registerLayoutComponents();
+  });
+  it("should correctly identify the parent hierarchy for a copied widget when no widget is selected", () => {
+    const { allWidgets, copiedWidgets } = generateMockDataWithSectionAndZone();
+    const selectedWidget: any = allWidgets[MAIN_CONTAINER_WIDGET_ID];
+    const generator = getDestinedParent(
+      allWidgets,
+      copiedWidgets,
+      selectedWidget,
+    );
+    const result = generator.next().value;
+    expect(result.parentOrder).toEqual([MAIN_CONTAINER_WIDGET_ID]);
+    // correctly identifies the parent hierarchy for a single copied widget
+    expect(result.alignment).toEqual(FlexLayerAlignment.Start);
+    // target adding to the main canvas layout
+    expect(result.layoutOrder).toEqual(
+      allWidgets[MAIN_CONTAINER_WIDGET_ID].layout,
+    );
+    // target adding to the main canvas layout in position 1
+    expect(result.rowIndex).toEqual([1]);
+  });
+  it("should correctly identify the parent hierarchy for a copied widget when a widget is selected", () => {
+    const { allWidgets, copiedWidgets, mockWidgetId } =
+      generateMockDataWithSectionAndZone();
+    const selectedWidget = allWidgets[mockWidgetId];
+    const generator = getDestinedParent(
+      allWidgets,
+      copiedWidgets,
+      selectedWidget,
+    );
+    const result = generator.next().value;
+    // correctly identifies the parent hierarchy for copied widgets
+    expect(result.parentOrder).toEqual(["widget-mock", "zone-mock"]);
+    expect(result.alignment).toEqual(FlexLayerAlignment.Start);
+    expect(result.rowIndex).toEqual([1, 1, 1]);
+  });
+});
