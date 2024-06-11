6863aa8ce3c173cf7b908586453aab42e0a047a8
Alex Golovanov <alex@appsmith.com>
2024-06-11 15:54:50 +0300

    feat: add redux for canvas analytics

--- app/client/src/actions/analyticsActions.ts
+++ app/client/src/actions/analyticsActions.ts
@@ -7,3 +7,22 @@ export const segmentInitSuccess = () => ({
 export const segmentInitUncertain = () => ({
   type: ReduxActionTypes.SEGMENT_INIT_UNCERTAIN,
 });
+
+export const recordAnalyticsForSideBySideWidgetHover = (
+  widgetType: string,
+) => ({
+  type: ReduxActionTypes.RECORD_ANALYTICS_FOR_SIDE_BY_SIDE_WIDGET_HOVER,
+  payload: widgetType,
+});
+
+export const sendAnalyticsForSideBySideHover = () => ({
+  type: ReduxActionTypes.SEND_ANALYTICS_FOR_SIDE_BY_SIDE_HOVER,
+});
+
+export const recordAnalyticsForSideBySideNavigation = () => ({
+  type: ReduxActionTypes.RECORD_ANALYTICS_FOR_SIDE_BY_SIDE_NAVIGATION,
+});
+
+export const resetAnalyticsForSideBySideHover = () => ({
+  type: ReduxActionTypes.RESET_ANALYTICS_FOR_SIDE_BY_SIDE_HOVER,
+});
--- app/client/src/selectors/analyticsSelectors.tsx
+++ app/client/src/selectors/analyticsSelectors.tsx
@@ -2,3 +2,6 @@ import type { AppState } from "@appsmith/reducers";
 
 export const getSegmentState = (state: AppState) =>
   state.ui.analytics.telemetry.segmentState;
+
+export const getIdeCanvasSideBySideHoverState = (state: AppState) =>
+  state.ui.analytics.ideCanvasSideBySideHover;
