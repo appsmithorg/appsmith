37635701f1073d445d732a1498c0a8b556d09ca3
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-06-11 16:47:30 +0530

    fix validation in phoneinput

--- app/client/src/widgets/wds/WDSPhoneInputWidget/widget/helpers.ts
+++ app/client/src/widgets/wds/WDSPhoneInputWidget/widget/helpers.ts
@@ -6,7 +6,7 @@ import type { ISDCodeProps } from "constants/ISDCodes_v2";
 import { ISDCodeOptions } from "constants/ISDCodes_v2";
 
 export function validateInput(props: any) {
-  const value = props.text ?? "";
+  const value = props.parsedText ?? "";
   const isInvalid = "isValid" in props && !props.isValid && !!props.isDirty;
 
   const conditionalProps: any = {};
