791b045a94bd53bf4b20c6ab2abf9e58ba12b7d9
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-06-12 13:27:41 +0800

    update: added comments

--- app/client/src/ce/constants/WorkflowConstants.ts
+++ app/client/src/ce/constants/WorkflowConstants.ts
@@ -16,6 +16,8 @@ export interface Workflow {
   slug: string; // Slug of the workflow (Not in use currently).
   mainJsObjectId: string; // ID of the main JS object.
   tokenGenerated: boolean;
+  // Evaluation version of the application. Used to ensure the escape characters are properly evaluated.
+  // PR for reference: https://github.com/appsmithorg/appsmith/pull/8796
   evaluationVersion?: EvaluationVersion;
   token?: string;
 }
