94d02811cd314a84393e9d351aa036683e1e2101
Alex Golovanov <alex@appsmith.com>
2024-06-13 22:18:49 +0300

    perf: move analytics wrapper to a component and attach to editor canvas

---
+++ app/client/src/layoutSystems/common/AnalyticsWrapper/AnalyticsWrapper.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import useIsInSideBySideEditor from "utils/hooks/useIsInSideBySideEditor";
+import { LAYOUT_WRAPPER_ID } from "./constants";
+import { sendAnalyticsForSideBySideHover } from "actions/analyticsActions";
+import { getLayoutSystemType } from "selectors/layoutSystemSelectors";
+import { LayoutSystemTypes } from "layoutSystems/types";
+
+export const AnalyticsWrapper: React.FC = ({ children }) => {
+  const dispatch = useDispatch();
+  const isInSideBySideEditor = useIsInSideBySideEditor();
+  const layoutSystemType = useSelector(getLayoutSystemType);
+
+  const handleMouseLeave: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    if (
+      isInSideBySideEditor &&
+      ((e.relatedTarget instanceof Element &&
+        e.relatedTarget.contains(document.getElementById(LAYOUT_WRAPPER_ID))) ||
+        layoutSystemType === LayoutSystemTypes.ANVIL)
+    ) {
+      dispatch(sendAnalyticsForSideBySideHover());
+    }
+  };
+  return (
+    <div
+      className="contents"
+      id={LAYOUT_WRAPPER_ID}
+      onMouseLeave={handleMouseLeave}
+    >
+      {children}
+    </div>
+  );
+};
