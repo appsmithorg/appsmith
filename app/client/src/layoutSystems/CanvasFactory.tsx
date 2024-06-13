4e92f5cede0d33a29555a9ba17264c2f46b500d6
Alex Golovanov <alex@appsmith.com>
2024-06-13 20:30:13 +0300

    chore: reverted canvas factory changes

--- app/client/src/layoutSystems/CanvasFactory.tsx
+++ app/client/src/layoutSystems/CanvasFactory.tsx
@@ -1,17 +1,10 @@
 import React, { memo, useMemo } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
+import { useSelector } from "react-redux";
 import { getRenderMode } from "selectors/editorSelectors";
 import { getLayoutSystemType } from "selectors/layoutSystemSelectors";
 import type { WidgetProps } from "widgets/BaseWidget";
 import withWidgetProps from "widgets/withWidgetProps";
 import { getLayoutSystem } from "./withLayoutSystemWidgetHOC";
-
-import { sendAnalyticsForSideBySideHover } from "actions/analyticsActions";
-
-import { LAYOUT_WRAPPER_ID } from "./constants";
-import styles from "./styles.module.css";
-import useIsInSideBySideEditor from "utils/hooks/useIsInSideBySideEditor";
 
 // ToDo(#27615): destructure withWidgetProps to withCanvasProps by picking only necessary props of a canvas.
 
@@ -22,8 +15,6 @@ import useIsInSideBySideEditor from "utils/hooks/useIsInSideBySideEditor";
  */
 
 const LayoutSystemBasedCanvas = memo((props: WidgetProps) => {
-  const dispatch = useDispatch();
-
   const renderMode = useSelector(getRenderMode);
   const layoutSystemType = useSelector(getLayoutSystemType);
   const { canvasSystem } = useMemo(
@@ -36,27 +27,7 @@ const LayoutSystemBasedCanvas = memo((props: WidgetProps) => {
     ],
   );
   const { Canvas, propertyEnhancer } = canvasSystem;
-
-  const isInSideBySideEditor = useIsInSideBySideEditor();
-  const handleMouseLeave: React.MouseEventHandler<HTMLDivElement> = (e) => {
-    if (
-      isInSideBySideEditor &&
-      e.relatedTarget instanceof Element &&
-      e.relatedTarget.contains(document.getElementById(LAYOUT_WRAPPER_ID))
-    ) {
-      dispatch(sendAnalyticsForSideBySideHover());
-    }
-  };
-
-  return (
-    <div
-      className={styles.root}
-      id={LAYOUT_WRAPPER_ID}
-      onMouseLeave={handleMouseLeave}
-    >
-      <Canvas {...propertyEnhancer(props)} />
-    </div>
-  );
+  return <Canvas {...propertyEnhancer(props)} />;
 });
 
 const HydratedLayoutSystemBasedCanvas = withWidgetProps(
