d8db02d61a62df6e9c5ba413df9fb75e61734aba
sondermanish <sonder.manish@gmail.com>
2024-06-17 14:41:02 +0530

    added changes

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/ApplicationDetailCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/ApplicationDetailCE.java
@@ -1,6 +1,7 @@
 package com.appsmith.server.domains.ce;
 
 import com.appsmith.external.views.Git;
+import com.appsmith.external.views.Views;
 import com.appsmith.server.domains.Application;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.EqualsAndHashCode;
@@ -13,13 +14,13 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 public class ApplicationDetailCE {
-    @JsonView(Git.class)
+    @JsonView({Views.Public.class, Git.class})
     Application.AppPositioning appPositioning;
 
-    @JsonView(Git.class)
+    @JsonView({Views.Public.class, Git.class})
     Application.NavigationSetting navigationSetting;
 
-    @JsonView(Git.class)
+    @JsonView({Views.Public.class, Git.class})
     Application.ThemeSetting themeSetting;
 
     public ApplicationDetailCE() {
