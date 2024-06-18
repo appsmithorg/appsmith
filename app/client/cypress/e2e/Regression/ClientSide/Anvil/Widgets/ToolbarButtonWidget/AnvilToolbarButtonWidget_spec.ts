0649a4a93cc7ca59d1c6cf86b762e2dc028422af
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-06-18 16:08:45 +0530

    add toolbar button widget snapshots

---
+++ app/client/cypress/e2e/Regression/ClientSide/Anvil/Widgets/ToolbarButtonWidget/AnvilToolbarButtonWidget_spec.ts
@@ -0,0 +1,26 @@
+import { agHelper, anvilSnapshot, deployMode, locators } from "../../../../../../support/Objects/ObjectsCore";
+import { ANVIL_EDITOR_TEST, modifierKey } from "../../../../../../support/Constants";
+
+describe(
+  `${ANVIL_EDITOR_TEST}: Anvil tests for Toolbar Button Widget`,
+  { tags: ["@tag.Anvil"] },
+  () => {
+    const widgetName = "ToolbarButtonWidget";
+
+    before(() => {
+      agHelper.AddDsl(`anvil${widgetName}`);
+    });
+
+    it("1. Canvas Mode", () => {
+      anvilSnapshot.verifyCanvasMode(widgetName);
+    });
+
+    it("2. Preview Mode", () => {
+      anvilSnapshot.verifyPreviewMode(widgetName);
+    });
+
+    it("3. Deploy Mode", () => {
+      anvilSnapshot.verifyDeployMode(widgetName);
+    });
+  },
+);
