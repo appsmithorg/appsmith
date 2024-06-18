826bbebf00c0bd2f030c28b1598f2d56a1f2ae6a
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-06-18 16:48:30 +0530

    fix prettier errors

--- app/client/cypress/e2e/Regression/ClientSide/Anvil/Widgets/InlineButtonWidget/AnvilInlineButtonWidget_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Anvil/Widgets/InlineButtonWidget/AnvilInlineButtonWidget_spec.ts
@@ -1,5 +1,8 @@
 import { ANVIL_EDITOR_TEST } from "../../../../../../support/Constants";
-import { agHelper, anvilSnapshot } from "../../../../../../support/Objects/ObjectsCore";
+import {
+  agHelper,
+  anvilSnapshot,
+} from "../../../../../../support/Objects/ObjectsCore";
 
 describe(
   `${ANVIL_EDITOR_TEST}: Anvil tests for Inline Button Widget`,
