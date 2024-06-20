34132cf8db5a92e10e723a1626caa7d86258b514
Abhijeet <abhi.nagarnaik@gmail.com>
2024-06-20 23:44:45 +0530

    chore: Cleanup permission utils to add user in permission

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/PermissionUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/PermissionUtils.java
@@ -1,15 +1,10 @@
 package com.appsmith.server.helpers;
 
-import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.dtos.Permission;
-import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
-
-import static com.appsmith.server.helpers.ReactiveContextUtils.getCurrentUser;
 
 public class PermissionUtils {
 
@@ -17,35 +12,5 @@ public class PermissionUtils {
         Set<Permission> permissions = new HashSet<>();
         Arrays.stream(permissionArgs).peek(permissions::addAll).toList();
         return permissions;
-    }
-
-    public static Mono<Optional<AclPermission>> updateAclWithUserContext(Optional<AclPermission> permission) {
-        if (permission.isEmpty()) {
-            return Mono.just(permission);
-        }
-        // Make sure the user context is not available in the permission object to avoid any static data leaks from the
-        // earlier call.
-        permission.get().setUser(null);
-        return getCurrentUser()
-                .map(user -> {
-                    permission.get().setUser(user);
-                    return permission;
-                })
-                .switchIfEmpty(Mono.just(permission));
-    }
-
-    public static Mono<AclPermission> updateAclWithUserContext(AclPermission permission) {
-        if (permission == null) {
-            return Mono.empty();
-        }
-        // Make sure the user context is not available in the permission object to avoid any static data leaks from the
-        // earlier call.
-        permission.setUser(null);
-        return getCurrentUser()
-                .map(user -> {
-                    permission.setUser(user);
-                    return permission;
-                })
-                .switchIfEmpty(Mono.just(permission));
     }
 }
