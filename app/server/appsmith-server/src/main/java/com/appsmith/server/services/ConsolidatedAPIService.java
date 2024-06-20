7e9cbe0a28807dec5ef3df168698508dbe9e0d0c
Anagh Hegde <anagh@appsmith.com>
2024-06-20 11:54:26 +0530

    fix bad imports

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ConsolidatedAPIService.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ConsolidatedAPIService.java
@@ -1,3 +1,5 @@
 package com.appsmith.server.services;
 
+import com.appsmith.server.services.ce_compatible.ConsolidatedAPIServiceCECompatible;
+
 public interface ConsolidatedAPIService extends ConsolidatedAPIServiceCECompatible {}
