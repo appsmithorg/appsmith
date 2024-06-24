b5d3a460a2e909a8f3ad388b0fcfdc98254da93d
Ashok Kumar M <35134347+marks0351@users.noreply.github.com>
2024-06-24 11:47:26 +0530

    feat: Anvil dnd sagas unit tests (#34407)
    [![workerB](https://img.shields.io/endpoint?url=https%3A%2F%2Fworkerb.linearb.io%2Fv2%2Fbadge%2Fprivate%2FU2FsdGVkX1IH0H4UjkYrlztX0lkyWZ1n2uZYh8Epg%2Fcollaboration.svg%3FcacheSeconds%3D60)](https://workerb.linearb.io/v2/badge/collaboration-page?magicLinkId=Pmk6xa9)
    ## Description
    > [!TIP]  
    > _Add a TL;DR when the description is longer than 500 words or
    extremely technical (helps the content, marketing, and DevRel team)._
    >
    > _Please also include relevant motivation and context. List any
    dependencies that are required for this change. Add links to Notion,
    Figma or any other documents that might be relevant to the PR._
    
    
    Fixes #33981
    _or_  
    Fixes `Issue URL`
    > [!WARNING]  
    > _If no issue exists, please create an issue first, and check with the
    maintainers if the issue is valid._
    
    ## Automation
    
    /ok-to-test tags="@tag.Anvil"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/9639877981>
    > Commit: 311f59c5d6cff265ea985c13f9891193866304d6
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=9639877981&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Anvil`
    
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Tests**
    - Introduced test cases for adding and moving widgets within a canvas
    layout.
    
    - **New Features**
    - Added functionality to generate mock data for a layout with two
    sections, each containing a zone widget.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ app/client/src/layoutSystems/anvil/integrations/sagas/anvilDraggingSagas/mockData.helper.ts
@@ -0,0 +1,113 @@
+import { generateReactKey } from "@shared/dsl/src/migrate/utils";
+import { MAIN_CONTAINER_WIDGET_ID } from "constants/WidgetConstants";
+import { LayoutComponentTypes } from "layoutSystems/anvil/utils/anvilTypes";
+import { FlexLayerAlignment } from "layoutSystems/common/utils/constants";
+
+export const generateMockDataWithTwoSections = () => {
+  const mainCanvasLayoutId = generateReactKey();
+  const section1Id = generateReactKey();
+  const section2Id = generateReactKey();
+  const zone1Id = generateReactKey();
+  const zone2Id = generateReactKey();
+  const section1Layout = {
+    layoutType: LayoutComponentTypes.SECTION,
+    layout: [
+      {
+        widgetId: zone1Id,
+        alignment: FlexLayerAlignment.Start,
+        widgetType: "ZONE_WIDGET",
+      },
+    ],
+  };
+  const section2Layout = {
+    layoutType: LayoutComponentTypes.SECTION,
+    layout: [
+      {
+        widgetId: zone2Id,
+        alignment: FlexLayerAlignment.Start,
+        widgetType: "ZONE_WIDGET",
+      },
+    ],
+  };
+  const allWidgets: any = {
+    [MAIN_CONTAINER_WIDGET_ID]: {
+      widgetName: "Main Container",
+      widgetId: MAIN_CONTAINER_WIDGET_ID,
+      children: [section1Id, section2Id],
+      layout: [
+        {
+          layoutId: mainCanvasLayoutId,
+          layoutType: LayoutComponentTypes.ALIGNED_LAYOUT_COLUMN,
+          childTemplate: {
+            insertChild: true,
+            layoutId: "",
+            layoutType: LayoutComponentTypes.WIDGET_ROW,
+            layout: [],
+          },
+          layout: [
+            {
+              layoutType: LayoutComponentTypes.WIDGET_ROW,
+              layout: [
+                {
+                  widgetId: section1Id,
+                  alignment: FlexLayerAlignment.Start,
+                  widgetType: "SECTION_WIDGET",
+                },
+              ],
+            },
+            {
+              layoutType: LayoutComponentTypes.WIDGET_ROW,
+              layout: [
+                {
+                  widgetId: section2Id,
+                  alignment: FlexLayerAlignment.Start,
+                  widgetType: "SECTION_WIDGET",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    [section1Id]: {
+      widgetName: "Section 1",
+      type: "SECTION_WIDGET",
+      widgetId: section1Id,
+      children: [zone1Id],
+      layout: [section1Layout],
+      zoneCount: 1,
+      parentId: MAIN_CONTAINER_WIDGET_ID,
+    },
+    [section2Id]: {
+      widgetName: "Section 2",
+      type: "SECTION_WIDGET",
+      widgetId: section2Id,
+      children: [zone2Id],
+      layout: [section2Layout],
+      zoneCount: 1,
+      parentId: MAIN_CONTAINER_WIDGET_ID,
+    },
+    [zone1Id]: {
+      widgetName: "Zone 1",
+      type: "ZONE_WIDGET",
+      widgetId: zone1Id,
+      children: [],
+      parentId: section1Id,
+    },
+    [zone2Id]: {
+      widgetName: "Zone 2",
+      type: "ZONE_WIDGET",
+      widgetId: zone2Id,
+      children: [],
+      parentId: section2Id,
+    },
+  };
+  return {
+    allWidgets,
+    mainCanvasLayoutId,
+    section1Id,
+    section2Id,
+    zone1Id,
+    zone2Id,
+  };
+};
