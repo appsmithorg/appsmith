179602e52dcc2cdcaeecc5f67af5bc4ac12ee431
Valera Melnikov <valera@appsmith.com>
2024-06-24 13:03:11 +0300

    fix: don't call modal onClose method in edit mode

--- app/client/src/widgets/wds/WDSModalWidget/widget/index.tsx
+++ app/client/src/widgets/wds/WDSModalWidget/widget/index.tsx
@@ -85,7 +85,7 @@ class WDSModalWidget extends BaseWidget<ModalWidgetProps, WidgetState> {
   }
 
   onModalClose = () => {
-    if (this.props.onClose) {
+    if (!this.props.disableWidgetInteraction && this.props.onClose) {
       super.executeAction({
         triggerPropertyName: "onClose",
         dynamicString: this.props.onClose,
