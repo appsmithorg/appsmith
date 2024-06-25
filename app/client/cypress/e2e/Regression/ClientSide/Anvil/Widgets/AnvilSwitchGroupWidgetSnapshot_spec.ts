626808700acf70ded1524fdc9f60cd374bbd41c6
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-06-25 15:25:10 +0530

    add required files for toggle tests

---
+++ app/client/cypress/e2e/Regression/ClientSide/Anvil/Widgets/AnvilSwitchGroupWidgetSnapshot_spec.ts
@@ -0,0 +1,27 @@
+anvilRadioGroupWidgetimport { ANVIL_EDITOR_TEST } from "../../../../../support/Constants";
+import {
+  agHelper,
+  anvilSnapshot,
+} from "../../../../../support/Objects/ObjectsCore";
+
+describe(
+  `${ANVIL_EDITOR_TEST}: Anvil tests for Switch Group Widget`,
+  { tags: ["@tag.Anvil"] },
+  () => {
+    before(() => {
+      agHelper.AddDsl("anvilSwitchGroupWidget");
+    });
+
+    it("1. Canvas Mode", () => {
+      anvilSnapshot.verifyCanvasMode("SwitchGroupWidget");
+    });
+
+    it("2. Preview Mode", () => {
+      anvilSnapshot.verifyPreviewMode("SwitchoGroupWidget");
+    });
+
+    it("3. Deploy Mode", () => {
+      anvilSnapshot.verifyDeployMode("SwitchGroupWidget");
+    });
+  },
+);
