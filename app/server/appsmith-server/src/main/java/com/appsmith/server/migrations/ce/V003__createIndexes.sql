302ac1d2af785b2c19a6cab2409633ef8fc962c2
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-06-29 14:46:17 +0530

    Conditional constraints for plugin table

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/ce/V003__createIndexes.sql
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/ce/V003__createIndexes.sql
@@ -5,7 +5,12 @@ create unique index if not exists config_name_key on config(name) ;
 create unique index if not exists prt_email_key on password_reset_token(email);
 create unique index if not exists user_email_key on "user"(email);
 create unique index if not exists sequence_name_key on sequence(name);
-create unique index if not exists plugin_name_package_name_version_key on plugin(plugin_name, package_name, version) NULLS NOT DISTINCT;
+
+-- plugin table constraints
+create unique index if not exists plugin_plugin_name_key on plugin(plugin_name) WHERE version IS NULL;
+create unique index if not exists plugin_package_name_key on plugin(package_name) WHERE version IS NULL;
+create unique index if not exists plugin_name_package_name_version_key on plugin(package_name, version) WHERE version IS NULL;
+
 create unique index if not exists user_id_key on user_data(user_id);
 create unique index if not exists datasource_workspace_name_deleted_key on datasource(workspace_id, name, deleted_at) NULLS NOT DISTINCT;
 create unique index if not exists application_workspace_name_deleted_git_application_metadata_key on application(workspace_id, name, deleted_at, (git_application_metadata ->> 'remoteUrl'), (git_application_metadata ->>'branchName')) NULLS NOT DISTINCT;
