86c083e48c8975a794653a5a74e2be3f68ff45e4
PrasadMadine <varaprasad.madine@zemosolabs.com>
2024-07-09 13:47:54 +0530

    fixed the bug alert message is showing twice in select widget and added cypress testing for the same

---
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/Select/Select4_spec.ts
@@ -0,0 +1,37 @@
+import {
+  agHelper, 
+  draggableWidgets,
+  deployMode,
+  entityExplorer,
+  locators,
+  propPane,
+} from "../../../../../support/Objects/ObjectsCore";
+  
+describe(
+  "Select widget tests",
+  { tags: ["@tag.Widget", "@tag.Select"] },
+  function () {
+    before(() => {
+      entityExplorer.DragDropWidgetNVerify(draggableWidgets.SELECT);
+    });
+  
+  
+    it("Validate OnDropdownClose events are rendering show alert only once", () => {
+      propPane.EnterJSContext(
+        "onDropdownClose",
+        "{{showAlert('Dropdown closed!','success')}}",
+        true,
+      );
+      propPane.ToggleJSMode("onDropdownClose", false);
+      deployMode.DeployApp(locators._widgetInDeployed(draggableWidgets.SELECT));
+      agHelper.GetNClick(locators._widgetInDeployed(draggableWidgets.SELECT));
+      agHelper.AssertElementVisibility(
+        locators._selectOptionValue("Red"),
+        true,
+      );
+      agHelper.GetNClick(locators._selectOptionValue("Red"));
+      agHelper.ValidateToastMessage("Dropdown closed!");
+    });
+  },
+);
+  
