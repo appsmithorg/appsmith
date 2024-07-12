22264605aeb795156bd464694b94ac1333e7e2a4
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-07-12 08:12:05 +0530

    Use GitHub Artifacts for server jars

--- scripts/prepare_server_artifacts.sh
+++ scripts/prepare_server_artifacts.sh
@@ -19,8 +19,20 @@ rm -rf "$target"/{pg,mongo}
 cp -r "app/server/dist" "$target/pg"
 mv "$target/pg"/server-*.jar "$target/pg/server.jar"
 
-# Grab MongoDB server artifacts from that Docker image.
-docker run --name xx -d --rm --entrypoint sleep "appsmith/appsmith-$EDITION:$MONGO_TAG" infinity
-docker cp xx:/opt/appsmith/backend "$target/mongo"
-docker cp xx:/opt/appsmith/info.json "$target/mongo/source-info.json"
-docker rm --force xx
+# Grab other server artifacts from GitHub Workflow Artifacts
+runs_data="$(
+  gh api 'repos/{owner}/{repo}/actions/workflows/test-build-docker-image.yml/runs?status=success&per_page=1&exclude_pull_requests=true&branch=release'
+)"
+artifact_data="$(
+  gh api "$(echo "$runs_data" | jq -r '.workflow_runs[0].artifacts_url')?name=server-build" \
+    | jq '.artifacts[0]'
+)"
+curl --location \
+  --output server-build.zip \
+  --oauth2-bearer "$(gh auth token)" \
+  "$(echo "$artifact_data" | jq -r .archive_download_url)"
+unzip server-build.zip
+rm server-build.zip
+mv server-*.jar server.jar
+mv server.jar plugins "$target/mongo"
+echo "$artifact_data" | jq > "$target/mongo/source-artifact.json"
