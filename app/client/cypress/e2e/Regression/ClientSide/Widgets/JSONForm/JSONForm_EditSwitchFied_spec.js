f3ab0fa12ba24d8e5b68773ff04f70d4ddf766f4
Harshithazemoso <harshithabusetti@zemosolabs.com>
2024-07-12 17:55:39 +0530

    fix:changes in cypress testing for switchfield spec in  jsonformwidget

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/JSONForm/JSONForm_EditSwitchFied_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/JSONForm/JSONForm_EditSwitchFied_spec.js
@@ -1,62 +1,64 @@
 import EditorNavigation, {
-    EntityType,
-  } from "../../../../../support/Pages/EditorNavigation";
-  
-  const commonlocators = require("../../../../../locators/commonlocators.json");
-  const jsonFormDslSwitchWidgetSourceData = require("../../../../../fixtures/jsonFormDslSwitchWidgetSourceData.json");
-  import { ObjectsRegistry } from "../../../../../support/Objects/Registry";
-  const locators = ObjectsRegistry.CommonLocators;
-  const {
-    deployMode,
-    propPane,
-  } = require("../../../../../support/Objects/ObjectsCore");
-  
-  const fieldPrefix = ".t--jsonformfield";
-  
-  describe(
-    "JSON Form Widget Custom Field",
-    { tags: ["@tag.Widget", "@tag.JSONForm"] },
-    () => {
-      it("uses the custom field when the accessor matches", () => {
-        const formDsl = JSON.parse(
-          JSON.stringify(jsonFormDslSwitchWidgetSourceData),
-        );
-  
-        cy.addDsl(formDsl);
-  
-        cy.openPropertyPane("jsonformwidget");
-  
-        // Add new custom field
-        cy.get(".t--add-column-btn")
-          .contains("Add new field")
-          .click({ force: true });
-  
-        cy.openFieldConfiguration("customField1");
-  
-        cy.testJsontext("propertyname", "education");
-        cy.testJsontext("text", "Education");
-        cy.selectDropdownValue(commonlocators.jsonFormFieldType, "Switch");
-  
-        // check for the position alignment
-        cy.get(commonlocators.optionposition).last().click({ force: true });
-        cy.get('.t--jsonformfield-customField1.switch')
-        .should('have.attr', 'direction', 'row-reverse');
-        cy.get(commonlocators.optionpositionL).last().click({ force: true });
-        cy.get('.t--jsonformfield-customField1.switch')
-        .should('have.attr', 'direction', 'row');
-        cy.closePropertyPane();
-  
-        const sourceData = {
-          name: "John",
-          email: "john@example.com",
-        };
-  
-        EditorNavigation.SelectEntityByName("JSONForm1", EntityType.Widget);
-        propPane.NavigateBackToPropertyPane();
-        propPane.EnterJSContext("Source data", JSON.stringify(sourceData), true);
-  
-        cy.get(`${fieldPrefix}-name label`).contains("Name");
-        cy.get(`${fieldPrefix}-name input`).should("have.value", "John");
+  EntityType,
+} from "../../../../../support/Pages/EditorNavigation";
+
+const commonlocators = require("../../../../../locators/commonlocators.json");
+const jsonFormDslSwitchWidgetSourceData = require("../../../../../fixtures/jsonFormDslSwitchWidgetSourceData.json");
+const { propPane } = require("../../../../../support/Objects/ObjectsCore");
+
+describe(
+  "JSON Form Widget Custom Field",
+  { tags: ["@tag.Widget", "@tag.JSONForm"] },
+  () => {
+    it("uses the custom field when the accessor matches", () => {
+      const formDsl = JSON.parse(
+        JSON.stringify(jsonFormDslSwitchWidgetSourceData),
+      );
+
+      cy.addDsl(formDsl);
+
+      cy.openPropertyPane("jsonformwidget");
+
+      // Add new custom field
+      cy.get(commonlocators.jsonFormAddNewCustomFieldBtn).click({
+        force: true,
       });
-    },
-  );
+
+      cy.openFieldConfiguration("customField1");
+
+      cy.testJsontext("propertyname", "education");
+      cy.testJsontext("text", "Education");
+      cy.selectDropdownValue(commonlocators.jsonFormFieldType, "Switch");
+
+      // check for the position alignment
+      cy.get(commonlocators.optionposition).last().click({ force: true });
+      cy.get(commonlocators.jsonformFieldCustomFieldSwitch).should(
+        "have.attr",
+        "direction",
+        "row-reverse",
+      );
+      cy.get(commonlocators.optionpositionL).last().click({ force: true });
+      cy.get(commonlocators.jsonformFieldCustomFieldSwitch).should(
+        "have.attr",
+        "direction",
+        "row",
+      );
+      cy.closePropertyPane();
+
+      const sourceData = {
+        name: "John",
+        email: "john@example.com",
+      };
+
+      EditorNavigation.SelectEntityByName("JSONForm1", EntityType.Widget);
+      propPane.NavigateBackToPropertyPane();
+      propPane.EnterJSContext("Source data", JSON.stringify(sourceData), true);
+
+      cy.get(`${commonlocators.jsonFieldPrefix} label`).contains("Name");
+      cy.get(`${commonlocators.jsonFieldPrefix} input`).should(
+        "have.value",
+        "John",
+      );
+    });
+  },
+);
