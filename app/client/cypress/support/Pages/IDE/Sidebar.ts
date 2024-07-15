9577297de122be1ae7810f9387ae8c79c95c42a4
NandanAnantharamu <67676905+NandanAnantharamu@users.noreply.github.com>
2024-07-15 13:43:34 +0530

    review comments incorporated

--- app/client/cypress/support/Pages/IDE/Sidebar.ts
+++ app/client/cypress/support/Pages/IDE/Sidebar.ts
@@ -22,7 +22,7 @@ export class Sidebar {
     );
   }
 
-  assertVisible(timeout = 4000) {
+  assertVisible(timeout: number = 4000) {
     cy.get(this.locators.sidebar, { timeout }).should("be.visible");
   }
 }
