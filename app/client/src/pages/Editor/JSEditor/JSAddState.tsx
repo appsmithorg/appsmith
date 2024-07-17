a006f80b2fb836db7a143a7688c64e9d08b80bd0
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-07-17 17:32:32 +0530

    feat: created constant for editor tab height

--- app/client/src/pages/Editor/JSEditor/JSAddState.tsx
+++ app/client/src/pages/Editor/JSEditor/JSAddState.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Flex } from "design-system";
 import AddJS from "pages/Editor/IDE/EditorPane/JS/Add";
+import { EDITOR_TABS_HEIGHT } from "../IDE/EditorPane/constants";
 
 const JSAddState = () => {
   return (
-    <Flex height="calc(100% - 32px)" justifyContent="center">
+    <Flex height={`calc(100% - ${EDITOR_TABS_HEIGHT})`} justifyContent="center">
       <AddJS
         containerProps={{
           px: "spaces-4",
