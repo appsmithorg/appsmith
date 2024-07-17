5faa22ae3f627d3266acd3b74823947d8644ea3a
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-07-17 17:34:25 +0530

    fix: fuzy search function fixes

--- app/client/src/pages/Editor/IDE/EditorPane/utils.ts
+++ app/client/src/pages/Editor/IDE/EditorPane/utils.ts
@@ -1,5 +1,4 @@
 import Fuse from "fuse.js";
-import get from "lodash/get";
 
 export const createAddClassName = (name: string) => {
   return "t--datasoucre-create-option-" + name.toLowerCase().replace(/ /g, "_");
@@ -8,19 +7,18 @@ export const createAddClassName = (name: string) => {
 const FUSE_OPTIONS = {
   shouldSort: true,
   threshold: 0.1,
+  keys: ["title"],
 };
 
 export const fuzzySearchInObjectItems = <T extends any[]>(
   searchStr: string,
   files: T,
-  keysToSearch = ["title"],
-  itemsKey = "items",
 ): T => {
   if (searchStr && searchStr !== "") {
     const newFiles = files
-      .map((group: any) => {
-        const items = get(group, itemsKey);
-        const fuse = new Fuse(items, { ...FUSE_OPTIONS, keys: keysToSearch });
+      .map((group) => {
+        const items = group["items"];
+        const fuse = new Fuse(items, FUSE_OPTIONS);
         const resultItems = fuse.search(searchStr);
         return { ...group, items: resultItems };
       })
