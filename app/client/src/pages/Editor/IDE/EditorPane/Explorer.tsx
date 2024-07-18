1ebac6032c623e3792e0a900a7962c3c485d69d4
Hetu Nandu <hetunandu@gmail.com>
2024-07-18 11:55:48 +0530

    Revert "separate end points for queries"
    This reverts commit 50852decb9e92ea6e06624c789e52184ef3ae37b.

--- app/client/src/pages/Editor/IDE/EditorPane/Explorer.tsx
+++ app/client/src/pages/Editor/IDE/EditorPane/Explorer.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import { Flex } from "design-system";
 import { Switch, useRouteMatch } from "react-router";
 import { SentryRoute } from "@appsmith/AppRouter";
-import {
-  jsSegmentRoutes,
-  querySegmentRoutes,
-} from "@appsmith/pages/Editor/IDE/EditorPane/constants";
+import { jsSegmentRoutes } from "@appsmith/pages/Editor/IDE/EditorPane/constants";
 import { JSExplorer } from "./JS";
-import { QueryExplorer } from "./Query";
 
 const EditorPaneExplorer = () => {
   const { path } = useRouteMatch();
@@ -22,10 +18,6 @@ const EditorPaneExplorer = () => {
         <SentryRoute
           component={JSExplorer}
           path={jsSegmentRoutes.map((route) => `${path}${route}`)}
-        />
-        <SentryRoute
-          component={QueryExplorer}
-          path={querySegmentRoutes.map((route) => `${path}${route}`)}
         />
       </Switch>
     </Flex>
