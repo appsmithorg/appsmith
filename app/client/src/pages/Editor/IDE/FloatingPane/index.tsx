9f0c3dff72344a92847815d65ecfc12b67d9f9be
Hetu Nandu <hetunandu@gmail.com>
2024-07-18 18:15:13 +0530

    Close on click outside

--- app/client/src/pages/Editor/IDE/FloatingPane/index.tsx
+++ app/client/src/pages/Editor/IDE/FloatingPane/index.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import Wrapper from "./Wrapper";
 import Content from "./Content";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { isFloatingPaneVisible } from "./selectors";
+import { useOnClickOutside } from "utils/hooks/useOnClickOutside";
+import { updateFloatingPane } from "./actions";
 
 const FloatingPane = () => {
   const isVisible = useSelector(isFloatingPaneVisible);
+  const ref = useRef(null);
+  const dispatch = useDispatch();
+  useOnClickOutside([ref], () => {
+    dispatch(updateFloatingPane({ selectedWidgetId: "0" }));
+  });
   if (!isVisible) return null;
   return (
-    <Wrapper>
-      <Content />
-    </Wrapper>
+    <div ref={ref}>
+      <Wrapper>
+        <Content />
+      </Wrapper>
+    </div>
   );
 };
 
