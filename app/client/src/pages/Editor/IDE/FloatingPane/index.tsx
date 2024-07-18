afa324209ffe54bd0c3c9a1edc8b56c6a3944efd
Hetu Nandu <hetunandu@gmail.com>
2024-07-18 12:02:18 +0530

    Add visibility state

--- app/client/src/pages/Editor/IDE/FloatingPane/index.tsx
+++ app/client/src/pages/Editor/IDE/FloatingPane/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Wrapper from "./Wrapper";
 import Content from "./Content";
+import { useSelector } from "react-redux";
+import { isFloatingPaneVisible } from "./selectors";
 
 const FloatingPane = () => {
-  // Add state to hide this
+  const isVisible = useSelector(isFloatingPaneVisible);
+  if (!isVisible) return null;
   return (
     <Wrapper>
       <Content />
