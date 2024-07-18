d7204df22f9d5305ea364d7a018f8df2b0d1939d
subrata <subrata@appsmith.com>
2024-07-18 19:25:38 +0600

    Introduce Lite API to populate the sidebar

--- app/server/appsmith-server/src/main/java/com/appsmith/server/bbhack/BBServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/bbhack/BBServiceImpl.java
@@ -2,6 +2,7 @@ package com.appsmith.server.bbhack;
 
 import com.appsmith.external.models.ActionDTO;
 import com.appsmith.server.domains.BuildingBlockHack;
+import com.appsmith.server.dtos.ApplicationJson;
 import com.appsmith.server.dtos.BBMainDTO;
 import com.appsmith.server.dtos.BBResponseDTO;
 import com.appsmith.server.newactions.base.NewActionService;
@@ -36,6 +37,14 @@ public class BBServiceImpl implements BBService {
         return bbMainDTO;
     }
 
+    private BBMainDTO prepareDTOFromDomainLite(BuildingBlockHack buildingBlockHack) {
+        BBMainDTO bbMainDTO = new BBMainDTO();
+        bbMainDTO.setId(buildingBlockHack.getId());
+        bbMainDTO.setName(buildingBlockHack.getName());
+        bbMainDTO.setIcon(buildingBlockHack.getIcon());
+        return bbMainDTO;
+    }
+
     @Override
     public Mono<BBResponseDTO> createCustomBuildingBlock(BBMainDTO bbMainDTO) {
         return repository.save(constructBBDomainFromDTO(bbMainDTO)).flatMap(savedBB -> {
@@ -64,6 +73,25 @@ public class BBServiceImpl implements BBService {
                 })
                 .collectList()
                 .map(bbResponseDTOS -> bbResponseDTOS);
+    }
+
+    @Override
+    public Mono<List<BBResponseDTO>> fetchAllBuildingBlocksLite() {
+        return repository
+                .findAll()
+                .flatMap(bb -> {
+                    BBResponseDTO bbResponseDTO = new BBResponseDTO();
+                    bbResponseDTO.setBb(prepareDTOFromDomainLite(bb));
+
+                    return Mono.just(bbResponseDTO);
+                })
+                .collectList()
+                .map(bbResponseDTOS -> bbResponseDTOS);
+    }
+
+    @Override
+    public Mono<ApplicationJson> prepareApplicationJsonFromBB(BuildingBlockHack buildingBlockHack) {
+        return null;
     }
 
     private Mono<List<ActionDTO>> getActionListMono(BuildingBlockHack buildingBlockHack) {
