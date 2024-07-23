2ea1919154488d0714a012f2d578df3d710ac936
Jagadeesh-90 <jagadeesh.palla@zemosolabs.com>
2024-07-23 20:32:13 +0530

    fix:url in cypress test

--- app/client/cypress/e2e/Regression/ClientSide/FormLogin/TermsAndConditions.ts
+++ app/client/cypress/e2e/Regression/ClientSide/FormLogin/TermsAndConditions.ts
@@ -1,7 +1,7 @@
 describe("Terms-And-Conditions", () => {
     it("1. Should check the color of titles is #202939", () => {
-        cy.visit("terms-and-conditions.html");
-        cy.wait(1000);
+        const termsAndConditionsUrl = "terms-and-conditions.html";
+        cy.visit(termsAndConditionsUrl);
         cy.get("h1, h2, h3, h4, h5, h6").each(($el) => {
             cy.wrap($el).should("have.css", "color", "rgb(32, 41, 57)");
         });
