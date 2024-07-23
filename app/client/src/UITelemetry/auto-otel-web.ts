727a6e343d8b7f86a19be64cc39c47c3ae9751ad
Diljit VJ <diljit@appsmith.com>
2024-07-23 14:37:09 +0530

    chore: Register OTLP meter provider

--- app/client/src/UITelemetry/auto-otel-web.ts
+++ app/client/src/UITelemetry/auto-otel-web.ts
@@ -1,27 +1,36 @@
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
-import { registerInstrumentations } from "@opentelemetry/instrumentation";
 import { ZoneContextManager } from "@opentelemetry/context-zone";
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { Resource } from "@opentelemetry/resources";
-import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import {
+  SEMRESATTRS_SERVICE_NAME,
+  SEMRESATTRS_SERVICE_VERSION,
+  SEMRESATTRS_SERVICE_INSTANCE_ID,
+} from "@opentelemetry/semantic-conventions";
 import { getAppsmithConfigs } from "@appsmith/configs";
 import { W3CTraceContextPropagator } from "@opentelemetry/core";
-import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
+import {
+  MeterProvider,
+  PeriodicExportingMetricReader,
+} from "@opentelemetry/sdk-metrics";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import type { Context, TextMapSetter } from "@opentelemetry/api";
+import { metrics } from "@opentelemetry/api";
 
 const { newRelic } = getAppsmithConfigs();
 const { applicationId, otlpEndpoint, otlpLicenseKey, otlpServiceName } =
   newRelic;
 
-const provider = new WebTracerProvider({
+const tracerProvider = new WebTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: otlpServiceName,
-    [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: applicationId,
-    [SemanticResourceAttributes.SERVICE_VERSION]: "1.0.0",
+    [SEMRESATTRS_SERVICE_NAME]: otlpServiceName,
+    [SEMRESATTRS_SERVICE_INSTANCE_ID]: applicationId,
+    [SEMRESATTRS_SERVICE_VERSION]: "1.0.0",
   }),
 });
 
-const newRelicExporter = new OTLPTraceExporter({
+const nrTracesExporter = new OTLPTraceExporter({
   url: `${otlpEndpoint}/v1/traces`,
   headers: {
     "api-key": otlpLicenseKey,
@@ -29,7 +38,7 @@ const newRelicExporter = new OTLPTraceExporter({
 });
 
 const processor = new BatchSpanProcessor(
-  newRelicExporter,
+  nrTracesExporter,
   //Optional BatchSpanProcessor Configurations
   {
     // The maximum queue size. After the size is reached spans are dropped.
@@ -48,7 +57,11 @@ const CUSTOM_OTLP_TRACE_HEADER = "traceparent-otlp";
 //We are overriding the default header "traceparent" used for trace context because the browser
 // agent shares the same header's distributed tracing
 class CustomW3CTraceContextPropagator extends W3CTraceContextPropagator {
-  inject(context, carrier, setter) {
+  inject(
+    context: Context,
+    carrier: Record<string, unknown>,
+    setter: TextMapSetter,
+  ) {
     // Call the original inject method to get the default traceparent header
     super.inject(context, carrier, setter);
 
@@ -60,40 +73,36 @@ class CustomW3CTraceContextPropagator extends W3CTraceContextPropagator {
   }
 }
 
-provider.addSpanProcessor(processor);
-provider.register({
+tracerProvider.addSpanProcessor(processor);
+tracerProvider.register({
   contextManager: new ZoneContextManager(),
   propagator: new CustomW3CTraceContextPropagator(),
 });
 
-const addCustomAttributesToSpan = (span) => {
-  const sessionId = window.localStorage.getItem("OTLP_SESSION_ID");
+// registerInstrumentations({
+//   instrumentations: [],
+// });
 
-  span.setAttribute("otlpsessionId", sessionId);
-};
+const nrMetricsExporter = new OTLPMetricExporter({
+  url: `${otlpEndpoint}/v1/metrics`,
+  headers: {
+    "api-key": otlpLicenseKey,
+  },
+});
 
-registerInstrumentations({
-  instrumentations: [
-    getWebAutoInstrumentations({
-      "@opentelemetry/instrumentation-xml-http-request": {
-        enabled: true,
-        ignoreUrls: [/bam.nr-data.net/],
-        addCustomAttributesToSpan,
-      },
-      "@opentelemetry/instrumentation-user-interaction": {
-        enabled: false,
-      },
-      "@opentelemetry/instrumentation-document-load": {
-        enabled: true,
-        applyCustomAttributesOnSpan: {
-          documentFetch: addCustomAttributesToSpan,
-          documentLoad: addCustomAttributesToSpan,
-          resourceFetch: addCustomAttributesToSpan,
-        },
-      },
-      "@opentelemetry/instrumentation-fetch": {
-        enabled: false,
-      },
+const meterProvider = new MeterProvider({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: otlpServiceName,
+    [SEMRESATTRS_SERVICE_INSTANCE_ID]: applicationId,
+    [SEMRESATTRS_SERVICE_VERSION]: "1.0.0",
+  }),
+  readers: [
+    new PeriodicExportingMetricReader({
+      exporter: nrMetricsExporter,
+      exportIntervalMillis: 10000, // Adjust the export interval as needed
     }),
   ],
 });
+
+// Register the MeterProvider globally
+metrics.setGlobalMeterProvider(meterProvider);
