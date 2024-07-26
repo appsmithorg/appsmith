aad18cfbf8a7e9d9d777351b93f0c55d46e5a5b1
Valera Melnikov <valera@appsmith.com>
2024-07-26 11:49:34 +0300

    chore: remove unused ads-old components

--- app/client/packages/design-system/ads-old/src/MenuDivider/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const MenuDivider = styled.div`
-  margin: var(--ads-spaces-1) var(--ads-spaces-6);
-  border-top: 1px solid var(--ads-menu-divider-border-color);
-`;
-
-export default MenuDivider;
