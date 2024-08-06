c0cf077954aff6e2848f812569476521702c0b17
Diljit VJ <diljit@appsmith.com>
2024-08-06 23:20:48 +0530

    Revert "chore: Add cache control header for static files"
    This reverts commit f5c0cf5969e800e381cec32d39119201bc19d6c0.

--- deploy/docker/fs/opt/appsmith/caddy-reconfigure.mjs
+++ deploy/docker/fs/opt/appsmith/caddy-reconfigure.mjs
@@ -1,50 +1,44 @@
-import * as fs from "fs";
-import { dirname } from "path";
-import { spawnSync } from "child_process";
-import { X509Certificate } from "crypto";
+import * as fs from "fs"
+import {dirname} from "path"
+import {spawnSync} from "child_process"
+import {X509Certificate} from "crypto"
 
 // The custom domain is expected to only have the domain. So if it has a protocol, we ignore the whole value.
 // This was the effective behaviour before Caddy.
-const CUSTOM_DOMAIN = (process.env.APPSMITH_CUSTOM_DOMAIN || "").replace(
-  /^https?:\/\/.+$/,
-  ""
-);
-const CaddyfilePath = process.env.TMP + "/Caddyfile";
-const AppsmithCaddy = process.env._APPSMITH_CADDY;
+const CUSTOM_DOMAIN = (process.env.APPSMITH_CUSTOM_DOMAIN || "").replace(/^https?:\/\/.+$/, "")
+const CaddyfilePath = process.env.TMP + "/Caddyfile"
+const AppsmithCaddy = process.env._APPSMITH_CADDY
 
 // Rate limit environment.
-const isRateLimitingEnabled = process.env.APPSMITH_RATE_LIMIT !== "disabled";
-const RATE_LIMIT = parseInt(process.env.APPSMITH_RATE_LIMIT || 100, 10);
+const isRateLimitingEnabled = process.env.APPSMITH_RATE_LIMIT !== "disabled"
+const RATE_LIMIT = parseInt(process.env.APPSMITH_RATE_LIMIT || 100, 10)
 
-let certLocation = null;
+let certLocation = null
 if (CUSTOM_DOMAIN !== "") {
   try {
-    fs.accessSync("/appsmith-stacks/ssl/fullchain.pem", fs.constants.R_OK);
-    certLocation = "/appsmith-stacks/ssl";
+    fs.accessSync("/appsmith-stacks/ssl/fullchain.pem", fs.constants.R_OK)
+    certLocation = "/appsmith-stacks/ssl"
   } catch {
     // no custom certs, see if old certbot certs are there.
-    const letsEncryptCertLocation =
-      "/appsmith-stacks/letsencrypt/live/" + CUSTOM_DOMAIN;
-    const fullChainPath = letsEncryptCertLocation + `/fullchain.pem`;
+    const letsEncryptCertLocation = "/appsmith-stacks/letsencrypt/live/" + CUSTOM_DOMAIN
+    const fullChainPath = letsEncryptCertLocation + `/fullchain.pem`
     try {
-      fs.accessSync(fullChainPath, fs.constants.R_OK);
-      console.log(
-        "Old Let's Encrypt cert file exists, now checking if it's expired."
-      );
+      fs.accessSync(fullChainPath, fs.constants.R_OK)
+      console.log("Old Let's Encrypt cert file exists, now checking if it's expired.")
       if (!isCertExpired(fullChainPath)) {
-        certLocation = letsEncryptCertLocation;
+        certLocation = letsEncryptCertLocation
       }
     } catch {
       // no certs there either, ignore.
     }
   }
+
 }
 
-const frameAncestorsPolicy = (
-  process.env.APPSMITH_ALLOWED_FRAME_ANCESTORS || "'self'"
-).replace(/;.*$/, "");
+const frameAncestorsPolicy = (process.env.APPSMITH_ALLOWED_FRAME_ANCESTORS || "'self'")
+  .replace(/;.*$/, "")
 
-const parts = [];
+const parts = []
 
 parts.push(`
 {
@@ -97,10 +91,6 @@ parts.push(`
     X-Appsmith-Request-Id {http.request.uuid}
   }
 
-  header /static/* {
-    Cache-Control "public, max-age=31536000, immutable"
-  }
-
   request_body {
     max_size ${process.env.APPSMITH_CODEC_SIZE || 150}MB
   }
@@ -142,17 +132,13 @@ parts.push(`
     import reverse_proxy 9001
   }
 
-  ${
-    isRateLimitingEnabled
-      ? `rate_limit {
+  ${isRateLimitingEnabled ? `rate_limit {
     zone dynamic_zone {
       key {http.request.remote_ip}
       events ${RATE_LIMIT}
       window 1s
     }
-  }`
-      : ""
-  }
+  }`: ""}
 
   handle_errors {
     respond "{err.status_code} {err.status_text}" {err.status_code}
@@ -164,7 +150,7 @@ parts.push(`
 :${process.env.PORT || 80} {
   import all-config
 }
-`);
+`)
 
 if (CUSTOM_DOMAIN !== "") {
   if (certLocation) {
@@ -174,14 +160,16 @@ if (CUSTOM_DOMAIN !== "") {
       import all-config
       tls ${certLocation}/fullchain.pem ${certLocation}/privkey.pem
     }
-    `);
+    `)
+
   } else {
     // No custom certificate, bind to the custom domain explicitly, so Caddy can auto-provision the cert.
     parts.push(`
     https://${CUSTOM_DOMAIN} {
       import all-config
     }
-    `);
+    `)
+
   }
 
   // We have to own the http-to-https redirect, since we need to remove the `Server` header from the response.
@@ -191,45 +179,44 @@ if (CUSTOM_DOMAIN !== "") {
     header -Server
     header Connection close
   }
-  `);
+  `)
 }
 
 if (!process.argv.includes("--no-finalize-index-html")) {
-  finalizeIndexHtml();
+  finalizeIndexHtml()
 }
 
-fs.mkdirSync(dirname(CaddyfilePath), { recursive: true });
-fs.writeFileSync(CaddyfilePath, parts.join("\n"));
-spawnSync(AppsmithCaddy, ["fmt", "--overwrite", CaddyfilePath]);
-spawnSync(AppsmithCaddy, ["reload", "--config", CaddyfilePath]);
+fs.mkdirSync(dirname(CaddyfilePath), { recursive: true })
+fs.writeFileSync(CaddyfilePath, parts.join("\n"))
+spawnSync(AppsmithCaddy, ["fmt", "--overwrite", CaddyfilePath])
+spawnSync(AppsmithCaddy, ["reload", "--config", CaddyfilePath])
 
 function finalizeIndexHtml() {
   let info = null;
   try {
-    info = JSON.parse(fs.readFileSync("/opt/appsmith/info.json", "utf8"));
-  } catch (e) {
+    info = JSON.parse(fs.readFileSync("/opt/appsmith/info.json", "utf8"))
+  } catch(e) {
     // info will be empty, that's okay.
-    console.error("Error reading info.json", e.message);
+    console.error("Error reading info.json", e.message)
   }
 
   const extraEnv = {
     APPSMITH_VERSION_ID: info?.version ?? "",
     APPSMITH_VERSION_SHA: info?.commitSha ?? "",
     APPSMITH_VERSION_RELEASE_DATE: info?.imageBuiltAt ?? "",
-  };
+  }
 
-  const content = fs
-    .readFileSync("/opt/appsmith/editor/index.html", "utf8")
-    .replaceAll(
-      /\{\{env\s+"(APPSMITH_[A-Z0-9_]+)"}}/g,
-      (_, name) => process.env[name] || extraEnv[name] || ""
-    );
+  const content = fs.readFileSync("/opt/appsmith/editor/index.html", "utf8").replaceAll(
+    /\{\{env\s+"(APPSMITH_[A-Z0-9_]+)"}}/g,
+    (_, name) => (process.env[name] || extraEnv[name] || "")
+  )
 
-  fs.writeFileSync(process.env.WWW_PATH + "/index.html", content);
+  fs.writeFileSync(process.env.WWW_PATH + "/index.html", content)
 }
 
 function isCertExpired(path) {
-  const cert = new X509Certificate(fs.readFileSync(path, "utf-8"));
-  console.log(path, cert);
-  return new Date(cert.validTo) < new Date();
+  const cert = new X509Certificate(fs.readFileSync(path, "utf-8"))
+  console.log(path, cert)
+  return new Date(cert.validTo) < new Date()
 }
+
