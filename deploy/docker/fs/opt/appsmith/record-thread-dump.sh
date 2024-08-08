1e3d8d77c2e8e8ba0d3bafc87e38ae1067bc35c7
Nidhi Nair <nidhi@appsmith.com>
2024-08-08 16:39:12 +0530

    chore: Added multiple scripts to trigger profiles

---
+++ deploy/docker/fs/opt/appsmith/record-thread-dump.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o noglob
+
+location=/appsmith-stacks/heap_dumps/ad-hoc/$(date "+%Y_%m_%d_%H_%S")/thread-profile;
+mkdir -p $location; 
+jcmd $(pgrep -f -- "-jar\sserver.jar") Thread.print > $location/trace-${HOSTNAME}.log
+jcmd $(pgrep -f -- "-jar\sserver.jar") Thread.cpu > $location/cpu-${HOSTNAME}.log
