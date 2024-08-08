a9bc4e485d968f8c4e6ee09e23c6d130bd822bcb
Aman Agarwal <aman@appsmith.com>
2024-08-09 00:07:16 +0530

    fix: hidden page was not being checked for route change

--- app/client/src/selectors/editorSelectors.tsx
+++ app/client/src/selectors/editorSelectors.tsx
@@ -255,6 +255,25 @@ export const getViewModePageList = createSelector(
   },
 );
 
+export const getViewModePageListWithHiddenPages = createSelector(
+  getPageList,
+  getCurrentPageId,
+  (pageList: PageListReduxState["pages"], currentPageId?: string) => {
+    if (currentPageId) {
+      const currentPage = pageList.find(
+        (page) => page.pageId === currentPageId,
+      );
+      if (!!currentPage?.isHidden) {
+        return [currentPage];
+      }
+
+      return pageList;
+    }
+
+    return [];
+  },
+);
+
 const defaultLayout: AppLayoutConfig = {
   type: "FLUID",
 };
