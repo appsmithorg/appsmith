7957300892106058c86713862cb65264c3a2d025
Vadim Vaitenko <vadim@appsmith.com>
2024-08-09 13:46:50 +0200

    Unit tests

--- app/client/packages/design-system/theming/src/color/tests/DarkModeTheme.test.ts
+++ app/client/packages/design-system/theming/src/color/tests/DarkModeTheme.test.ts
@@ -567,19 +567,19 @@ describe("fgNeutral color", () => {
   it("should return correct color when chroma < 0.04", () => {
     const { fgNeutral } = new DarkModeTheme("oklch(0.45 0.03 60)").getColors();
 
-    expect(fgNeutral).toEqual("rgb(88.912% 88.912% 88.912%)");
+    expect(fgNeutral).toEqual("rgb(60.245% 60.245% 60.245%)");
   });
 
   it("should return correct color when chroma > 0.04 and hue is between 120 and 300", () => {
     const { fgNeutral } = new DarkModeTheme("oklch(0.45 0.1 150)").getColors();
 
-    expect(fgNeutral).toEqual("rgb(86.896% 89.904% 87.34%)");
+    expect(fgNeutral).toEqual("rgb(58.362% 61.165% 58.781%)");
   });
 
   it("should return correct color when chroma > 0.04 and hue is not between 120 and 300", () => {
     const { fgNeutral } = new DarkModeTheme("oklch(0.45 0.1 110)").getColors();
 
-    expect(fgNeutral).toEqual("rgb(89.043% 89.298% 85.788%)");
+    expect(fgNeutral).toEqual("rgb(60.369% 60.601% 57.331%)");
   });
 });
 
@@ -589,7 +589,7 @@ describe("fgNeutralSubtle color", () => {
       "oklch(0.45 0.03 60)",
     ).getColors();
 
-    expect(fgNeutralSubtle).toEqual("rgb(69.98% 69.98% 69.98%)");
+    expect(fgNeutralSubtle).toEqual("rgb(42.772% 42.772% 42.772%)");
   });
 });
 
