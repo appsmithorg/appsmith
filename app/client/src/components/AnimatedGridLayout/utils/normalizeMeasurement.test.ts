120433561c026726d29a1f993aa3de533ead6b51
Alex Golovanov <alex@appsmith.com>
2024-08-13 15:46:27 +0300

    chore: tweaked regex units regex

--- app/client/src/components/AnimatedGridLayout/utils/normalizeMeasurement.test.ts
+++ app/client/src/components/AnimatedGridLayout/utils/normalizeMeasurement.test.ts
@@ -8,6 +8,9 @@ describe("normalizeMeasurement", () => {
     [" 1 ", 1],
     [" ", 0],
     [".", 0],
+    ["1.1fr", 1.1],
+    ["1.1px", 1.1],
+    ["1.1%", 1.1],
   ])("%s -> %s", (input, output) => {
     expect(normalizeMeasurement(input)).toBe(output);
   });
