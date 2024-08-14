d0644b36458a468e1c7ef459f626b89a28e772f3
Aman Agarwal <aman@appsmith.com>
2024-08-14 14:52:52 +0530

    fix: updated the require

--- app/client/packages/eslint-plugin/index.js
+++ app/client/packages/eslint-plugin/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable-next-line @typescript-eslint/no-var-requires */
-const { rule } = require("./object-keys-replacement/rule");
+const { rule: objectKeysRule } = require("./object-keys-replacement/rule");
 
 module.exports = {
   rules: {
-    "object-keys": rule,
+    "object-keys": objectKeysRule,
   },
 };
