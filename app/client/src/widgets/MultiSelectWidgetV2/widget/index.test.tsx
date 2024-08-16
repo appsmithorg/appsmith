16d0315523f7a756337c287f0906a4d1870152f7
Sai Charan <saicharan.chetpelly@zemosolabs.com>
2024-08-16 17:23:39 +0530

    feat: add test for MultiSelectWidgetV2 MultiSelectTreeWidget

---
+++ app/client/src/widgets/MultiSelectWidgetV2/widget/index.test.tsx
@@ -0,0 +1,115 @@
+import { render, screen } from "@testing-library/react";
+import { dark, theme } from "constants/DefaultTheme";
+import React from "react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { ThemeProvider } from "styled-components";
+import type { MultiSelectWidgetProps } from "./";
+import MultiSelectWidget from "./";
+
+import "@testing-library/jest-dom";
+
+import { RenderModes } from "constants/WidgetConstants";
+
+describe("<MultiSelectWidget />", () => {
+  const initialState = {
+    ui: {
+      appSettingsPane: {
+        isOpen: false,
+      },
+      widgetDragResize: {
+        lastSelectedWidget: "Widget1",
+        selectedWidgets: ["Widget1"],
+      },
+      users: {
+        featureFlag: {
+          data: {},
+        },
+      },
+      propertyPane: {
+        isVisible: true,
+        widgetId: "Widget1",
+      },
+      debugger: {
+        errors: {},
+      },
+      editor: {
+        isPreviewMode: false,
+      },
+      widgetReflow: {
+        enableReflow: true,
+      },
+      autoHeightUI: {
+        isAutoHeightWithLimitsChanging: false,
+      },
+      mainCanvas: {
+        width: 1159,
+      },
+      canvasSelection: {
+        isDraggingForSelection: false,
+      },
+    },
+    entities: { canvasWidgets: {}, app: { mode: "canvas" } },
+  };
+
+  const renderMultiSelectWidget = (props: MultiSelectWidgetProps) => {
+    const store = configureStore()(initialState);
+    return render(
+      <Provider store={store}>
+        <ThemeProvider
+          theme={{ ...theme, colors: { ...theme.colors, ...dark } }}
+        >
+          <MultiSelectWidget {...props} />
+        </ThemeProvider>
+      </Provider>,
+    );
+  };
+
+  test("should show error when Required is true and default selected value is empty", async () => {
+    const mockDataWithOptions: MultiSelectWidgetProps = {
+      widgetId: "Widget1",
+      type: "MULTI_SELECT_WIDGET_V2",
+      widgetName: "MultiSelect1",
+      renderMode: RenderModes.CANVAS,
+      parentColumnSpace: 2,
+      parentRowSpace: 3,
+      leftColumn: 2,
+      rightColumn: 4,
+      topRow: 1,
+      bottomRow: 2,
+      isLoading: false,
+      version: 1,
+      options: [
+        {
+          label: "female",
+          value: "F",
+        },
+        {
+          label: "male",
+          value: "M",
+        },
+      ],
+      onOptionChange: "mock-option-change",
+      isRequired: true,
+      isFilterable: false,
+      serverSideFiltering: false,
+      onFilterUpdate: "mock-update",
+      updateWidgetMetaProperty: jest.fn(),
+      labelText: "select label",
+      filterText: "",
+      selectedOptionLabel: "",
+      defaultOptionValue: [],
+      selectedOptionValue: "",
+      isWidgetSelected: true,
+      selectedOption: {
+        label: "none",
+        value: "NONE",
+      },
+      onFilterChange: "",
+      selectedOptions: [],
+    };
+
+    renderMultiSelectWidget(mockDataWithOptions);
+    expect(screen.getByText("This field is required")).toBeInTheDocument();
+  });
+});
