d73a9cec94fdd16077fb61270fb0ab0d3cf56019
Anagh Hegde <anagh@appsmith.com>
2024-08-19 15:29:21 +0530

    add test for update of existing app

--- app/server/appsmith-server/src/test/java/com/appsmith/server/transactions/ImportApplicationTransactionServiceTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/transactions/ImportApplicationTransactionServiceTest.java
@@ -1,10 +1,19 @@
 package com.appsmith.server.transactions;
 
+import com.appsmith.external.models.ActionDTO;
+import com.appsmith.external.models.Datasource;
+import com.appsmith.external.models.DatasourceStorage;
+import com.appsmith.external.models.PluginType;
 import com.appsmith.server.actioncollections.base.ActionCollectionService;
+import com.appsmith.server.domains.ActionCollection;
 import com.appsmith.server.domains.Application;
+import com.appsmith.server.domains.ApplicationPage;
 import com.appsmith.server.domains.NewAction;
+import com.appsmith.server.domains.NewPage;
 import com.appsmith.server.domains.Workspace;
+import com.appsmith.server.dtos.ActionCollectionDTO;
 import com.appsmith.server.dtos.ApplicationJson;
+import com.appsmith.server.dtos.PageDTO;
 import com.appsmith.server.exceptions.AppsmithError;
 import com.appsmith.server.exceptions.AppsmithException;
 import com.appsmith.server.extensions.AfterAllCleanUpExtension;
@@ -12,12 +21,14 @@ import com.appsmith.server.helpers.MockPluginExecutor;
 import com.appsmith.server.helpers.PluginExecutorHelper;
 import com.appsmith.server.imports.importable.ImportableService;
 import com.appsmith.server.imports.internal.ImportService;
+import com.appsmith.server.migrations.ApplicationVersion;
 import com.appsmith.server.migrations.JsonSchemaMigration;
 import com.appsmith.server.newactions.base.NewActionService;
 import com.appsmith.server.repositories.cakes.ActionCollectionRepositoryCake;
 import com.appsmith.server.repositories.cakes.ApplicationRepositoryCake;
 import com.appsmith.server.repositories.cakes.NewActionRepositoryCake;
 import com.appsmith.server.repositories.cakes.NewPageRepositoryCake;
+import com.appsmith.server.services.ApplicationPageService;
 import com.appsmith.server.services.WorkspaceService;
 import com.google.gson.Gson;
 import jakarta.transaction.TransactionalException;
@@ -40,6 +51,10 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 
@@ -60,7 +75,7 @@ public class ImportApplicationTransactionServiceTest {
     @Autowired
     ApplicationRepositoryCake applicationRepository;
 
-    @MockBean
+    @Autowired
     NewActionService newActionService;
 
     @Autowired
@@ -69,7 +84,7 @@ public class ImportApplicationTransactionServiceTest {
     @Autowired
     NewPageRepositoryCake newPageRepository;
 
-    @MockBean
+    @Autowired
     ActionCollectionService actionCollectionService;
 
     @Autowired
@@ -80,6 +95,9 @@ public class ImportApplicationTransactionServiceTest {
 
     @MockBean
     ImportableService<NewAction> newActionImportableService;
+
+    @Autowired
+    ApplicationPageService applicationPageService;
 
     @Autowired
     private Gson gson;
@@ -131,6 +149,121 @@ public class ImportApplicationTransactionServiceTest {
                 })
                 .map(jsonSchemaMigration::migrateArtifactToLatestSchema)
                 .map(artifactExchangeJson -> (ApplicationJson) artifactExchangeJson);
+    }
+
+    private ApplicationJson createApplicationJSON(List<String> pageNames) {
+        ApplicationJson applicationJson = new ApplicationJson();
+
+        // set the application data
+        Application application = new Application();
+        application.setName("Template Application");
+        application.setSlug("template-application");
+        application.setForkingEnabled(true);
+        application.setIsPublic(true);
+        application.setApplicationVersion(ApplicationVersion.LATEST_VERSION);
+        applicationJson.setExportedApplication(application);
+
+        DatasourceStorage sampleDatasource = new DatasourceStorage();
+        sampleDatasource.setName("SampleDS");
+        sampleDatasource.setPluginId("restapi-plugin");
+
+        applicationJson.setDatasourceList(List.of(sampleDatasource));
+
+        // add pages and actions
+        List<NewPage> newPageList = new ArrayList<>(pageNames.size());
+        List<NewAction> actionList = new ArrayList<>();
+        List<ActionCollection> actionCollectionList = new ArrayList<>();
+
+        for (String pageName : pageNames) {
+            NewPage newPage = new NewPage();
+            newPage.setUnpublishedPage(new PageDTO());
+            newPage.getUnpublishedPage().setName(pageName);
+            newPage.getUnpublishedPage().setLayouts(List.of());
+            newPageList.add(newPage);
+
+            NewAction action = new NewAction();
+            action.setId(pageName + "_SampleQuery");
+            action.setPluginType(PluginType.API);
+            action.setPluginId("restapi-plugin");
+            action.setUnpublishedAction(new ActionDTO());
+            action.getUnpublishedAction().setName("SampleQuery");
+            action.getUnpublishedAction().setPageId(pageName);
+            action.getUnpublishedAction().setDatasource(new Datasource());
+            action.getUnpublishedAction().getDatasource().setId("SampleDS");
+            action.getUnpublishedAction().getDatasource().setPluginId("restapi-plugin");
+            actionList.add(action);
+
+            ActionCollection actionCollection = new ActionCollection();
+            actionCollection.setId(pageName + "_SampleJS");
+            actionCollection.setUnpublishedCollection(new ActionCollectionDTO());
+            actionCollection.getUnpublishedCollection().setName("SampleJS");
+            actionCollection.getUnpublishedCollection().setPageId(pageName);
+            actionCollection.getUnpublishedCollection().setPluginId("js-plugin");
+            actionCollection.getUnpublishedCollection().setPluginType(PluginType.JS);
+            actionCollection.getUnpublishedCollection().setBody("export default {\\n\\t\\n}");
+            actionCollectionList.add(actionCollection);
+        }
+
+        applicationJson.setPageList(newPageList);
+        applicationJson.setActionList(actionList);
+        applicationJson.setActionCollectionList(actionCollectionList);
+        return applicationJson;
+    }
+
+    @Test
+    @WithUserDetails(value = "api_user")
+    public void importIntoExistingApplication_exceptionDuringImport_dataRestoredBack() {
+        // Create a git connected application
+        String uniqueString = UUID.randomUUID().toString();
+
+        Workspace newWorkspace = new Workspace();
+        newWorkspace.setName("Template");
+        Workspace createdWorkspace = workspaceService.create(newWorkspace).block();
+
+        Application destApplication = new Application();
+        destApplication.setName("App_" + uniqueString);
+        destApplication.setSlug("my-slug");
+        destApplication.setIsPublic(false);
+        destApplication.setForkingEnabled(false);
+        Mono<Application> createAppAndPageMono = applicationPageService
+                .createApplication(destApplication, createdWorkspace.getId())
+                .flatMap(application -> {
+                    PageDTO pageDTO = new PageDTO();
+                    pageDTO.setName("Home");
+                    pageDTO.setApplicationId(application.getId());
+                    return applicationPageService.createPage(pageDTO).thenReturn(application);
+                });
+
+        Application application = createAppAndPageMono.block();
+
+        // let's create an ApplicationJSON which we'll merge with application created by createAppAndPageMono
+        ApplicationJson applicationJson = createApplicationJSON(List.of("Home", "About"));
+
+        Mockito.when(newActionImportableService.importEntities(any(), any(), any(), any(), any()))
+                .thenReturn(Mono.error(new AppsmithException(AppsmithError.GENERIC_BAD_REQUEST)));
+
+        Mono<Application> resultMono = importService
+                .importNewArtifactInWorkspaceFromJson(createdWorkspace.getId(), applicationJson)
+                .map(importableArtifact -> (Application) importableArtifact);
+
+        // Verify for the failure state
+        StepVerifier.create(resultMono)
+                .expectErrorMatches(error -> error instanceof AppsmithException
+                        && error.getMessage()
+                                .contains(AppsmithError.GENERIC_JSON_IMPORT_ERROR.getMessage(
+                                        createdWorkspace.getId(), "")))
+                .verify();
+
+        // Verify the db state after the import failed
+        Application dbApp = applicationRepository.findById(application.getId()).block();
+
+        assertThat(dbApp).isNotNull();
+        assertThat(dbApp.getPages().size()).isEqualTo(2);
+        for (ApplicationPage page : dbApp.getPages()) {
+            NewPage newPage = newPageRepository.findById(page.getId()).block();
+            assertThat(newPage).isNotNull();
+            assertThat(newPage.getUnpublishedPage().getName()).isIn("Home", "Page1");
+        }
     }
 
     @Test
