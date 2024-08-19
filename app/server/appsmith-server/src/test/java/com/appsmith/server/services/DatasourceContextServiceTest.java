a0aee936eabf6f8b948916c79c688032e4514c67
Anagh Hegde <anagh@appsmith.com>
2024-08-19 19:49:33 +0530

    remove the duplicate test

--- app/server/appsmith-server/src/test/java/com/appsmith/server/services/DatasourceContextServiceTest.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/services/DatasourceContextServiceTest.java
@@ -49,7 +49,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.DirtiesContext;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -148,77 +147,6 @@ public class DatasourceContextServiceTest {
                 .collectList()
                 .block();
         Workspace deletedWorkspace = workspaceService.archiveById(workspaceId).block();
-    }
-
-    @Test
-    @WithUserDetails(value = "api_user")
-    public void testDatasourceCache_afterDatasourceDeleted_doesNotReturnOldConnection() {
-        // Never require the datasource connection to be stale
-        Plugin emptyPlugin = new Plugin();
-        doReturn(false).doReturn(false).when(datasourceContextService).getIsStale(any(), any());
-
-        MockPluginExecutor mockPluginExecutor = new MockPluginExecutor();
-        MockPluginExecutor spyMockPluginExecutor = spy(mockPluginExecutor);
-        /* Return two different connection objects if `datasourceCreate` method is called twice */
-        doReturn(Mono.just("connection_1"))
-                .doReturn(Mono.just("connection_2"))
-                .when(spyMockPluginExecutor)
-                .datasourceCreate(any());
-        Mockito.when(pluginExecutorHelper.getPluginExecutor(any())).thenReturn(Mono.just(spyMockPluginExecutor));
-
-        DatasourceStorage datasourceStorage = new DatasourceStorage();
-        datasourceStorage.setEnvironmentId(defaultEnvironmentId);
-        datasourceStorage.setDatasourceId("id1");
-        datasourceStorage.setDatasourceConfiguration(new DatasourceConfiguration());
-        datasourceStorage.setWorkspaceId(workspaceId);
-
-        DatasourceContextIdentifier datasourceContextIdentifier =
-                new DatasourceContextIdentifier(datasourceStorage.getDatasourceId(), null);
-
-        Object monitor = new Object();
-        // Create one instance of datasource connection
-        Mono<DatasourceContext<?>> dsContextMono1 = datasourceContextService.getCachedDatasourceContextMono(
-                datasourceStorage, emptyPlugin, spyMockPluginExecutor, monitor, datasourceContextIdentifier);
-
-        Datasource datasource = new Datasource();
-        datasource.setId("id1");
-        datasource.setWorkspaceId("workspaceId1");
-        datasource.setPluginId("mockPluginId");
-        HashMap<String, DatasourceStorageDTO> storages = new HashMap<>();
-        storages.put(
-                defaultEnvironmentId,
-                datasourceStorageService.createDatasourceStorageDTOFromDatasourceStorage(datasourceStorage));
-        datasource.setDatasourceStorages(storages);
-
-        doReturn(Mono.just(datasource))
-                .when(datasourceRepository)
-                .findById("id1", datasourcePermission.getDeletePermission());
-        doReturn(Mono.just(datasource))
-                .when(datasourceRepository)
-                .findById("id1", datasourcePermission.getExecutePermission());
-        doReturn(Mono.just(new Plugin())).when(pluginService).findById("mockPlugin");
-        doReturn(Mono.just(0L)).when(newActionRepository).countByDatasourceId("id1");
-        doReturn(Mono.just(datasource)).when(datasourceRepository).archiveById("id1");
-        doReturn(Flux.just(datasourceStorage)).when(datasourceStorageService).findStrictlyByDatasourceId("id1");
-        doReturn(Mono.just(datasourceStorage)).when(datasourceStorageService).archive(datasourceStorage);
-
-        // Now delete the datasource and check if the cache retains the same instance of connection
-        Mono<DatasourceContext<?>> dsContextMono2 = datasourceService
-                .archiveById("id1")
-                .flatMap(deleted -> datasourceContextService.getCachedDatasourceContextMono(
-                        datasourceStorage, emptyPlugin, spyMockPluginExecutor, monitor, datasourceContextIdentifier));
-
-        StepVerifier.create(dsContextMono1)
-                .assertNext(dsContext1 -> {
-                    assertEquals("connection_1", dsContext1.getConnection());
-                })
-                .verifyComplete();
-
-        StepVerifier.create(dsContextMono2)
-                .assertNext(dsContext1 -> {
-                    assertEquals("connection_2", dsContext1.getConnection());
-                })
-                .verifyComplete();
     }
 
     @Test
