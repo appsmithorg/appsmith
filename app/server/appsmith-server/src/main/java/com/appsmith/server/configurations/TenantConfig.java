110f514f759057ddae14c8b10a534f710283f5b1
Abhijeet <abhi.nagarnaik@gmail.com>
2024-08-20 17:42:41 +0530

    fix: Testcases for consolidatedAPI

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/TenantConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/TenantConfig.java
@@ -2,7 +2,7 @@ package com.appsmith.server.configurations;
 
 import com.appsmith.server.domains.Tenant;
 import com.appsmith.server.helpers.CollectionUtils;
-import com.appsmith.server.services.TenantService;
+import com.appsmith.server.repositories.TenantRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -17,16 +17,16 @@ import static com.appsmith.external.models.BaseDomain.policySetToMap;
 @Slf4j
 public class TenantConfig implements ApplicationListener<ApplicationStartedEvent> {
 
-    private final TenantService tenantService;
+    private final TenantRepository tenantRepository;
 
     // Method to cleanup the cache and update the default tenant policies if the policyMap is empty. This will make sure
     // cache will be updated if we update the tenant via startup DB migrations.
     private Mono<Tenant> cleanupAndUpdateRefreshDefaultTenantPolicies() {
         log.debug("Cleaning up and updating default tenant policies on server startup");
-        return tenantService.getDefaultTenant().flatMap(tenant -> {
+        return tenantRepository.findBySlug("default").flatMap(tenant -> {
             if (CollectionUtils.isNullOrEmpty(tenant.getPolicyMap())) {
                 tenant.setPolicyMap(policySetToMap(tenant.getPolicies()));
-                return tenantService.save(tenant);
+                return this.tenantRepository.save(tenant);
             }
             return Mono.just(tenant);
         });
