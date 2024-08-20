c23bd2ad01d0a8ce214762ddea7ed71e6d4a758e
Apeksha Bhosale <7846888+ApekshaBhosale@users.noreply.github.com>
2024-08-20 20:03:50 +0530

    PR comments fixed

--- deploy/docker/fs/opt/appsmith/record-thread-dump.sh
+++ deploy/docker/fs/opt/appsmith/record-thread-dump.sh
@@ -10,11 +10,8 @@ arg=${1:-}
 # Set the location based on the argument
 if [ "$arg" == "preStop" ]; then
   location=/appsmith-stacks/heap_dumps/preStop/${HOSTNAME}/$(date "+%Y_%m_%d_%H_%S");
-elif [ "$arg" == "adHoc" ]; then
-  location=/appsmith-stacks/heap_dumps/ad-hoc/${HOSTNAME}/thread-profile/$(date "+%Y_%m_%d_%H_%S");
 else
-  echo "Invalid argument. Use 'preStop' or 'adHoc'."
-  exit 1
+  location=/appsmith-stacks/heap_dumps/ad-hoc/${HOSTNAME}/thread-profile/$(date "+%Y_%m_%d_%H_%S");
 fi
 
 mkdir -p $location; 
