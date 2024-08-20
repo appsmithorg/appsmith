183605fb34ddb815222724c8bf17afb538668f99
Abhijeet <abhi.nagarnaik@gmail.com>
2024-08-20 16:44:17 +0530

    chore: Add cleanup and update stage for tenant at the server restart

---
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/TenantConfig.java
@@ -0,0 +1,35 @@
+package com.appsmith.server.configurations;
+
+import com.appsmith.server.helpers.CollectionUtils;
+import com.appsmith.server.repositories.CacheableRepositoryHelper;
+import com.appsmith.server.services.TenantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.appsmith.external.models.BaseDomain.policySetToMap;
+
+@Configuration
+@RequiredArgsConstructor
+public class TenantConfig {
+
+    private final TenantService tenantService;
+    private final CacheableRepositoryHelper cacheableRepositoryHelper;
+
+    // Bean to cleanup the cache and update the default tenant policies on every server restart. This will make sure
+    // cache will be updated if we update the tenant via migrations.
+    @Bean
+    public void cleanupAndUpdateRefreshDefaultTenantPolicies() {
+        tenantService
+                .getDefaultTenantId()
+                .flatMap(cacheableRepositoryHelper::evictCachedTenant)
+                .then(tenantService.getDefaultTenant())
+                .flatMap(tenant -> {
+                    if (CollectionUtils.isNullOrEmpty(tenant.getPolicyMap())) {
+                        tenant.setPolicyMap(policySetToMap(tenant.getPolicies()));
+                    }
+                    return tenantService.save(tenant);
+                })
+                .subscribe();
+    }
+}
