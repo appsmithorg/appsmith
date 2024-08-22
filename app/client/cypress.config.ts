1bf3f38e912695551c679afe44f31cddfd57b94a
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-08-22 11:09:29 +0530

    update anvil distribution spec

--- app/client/cypress.config.ts
+++ app/client/cypress.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     openMode: 0,
   },
   e2e: {
-    baseUrl: "https://dev.appsmith.com/",
+    baseUrl: "https://release.app.appsmith.com/",
     env: {
       USERNAME: "xxxx",
       PASSWORD: "xxx",
