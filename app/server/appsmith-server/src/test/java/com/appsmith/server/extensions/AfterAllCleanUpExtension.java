695161f5df23349bd5758de0c033ebfe5758c787
Abhijeet <abhi.nagarnaik@gmail.com>
2024-08-26 17:42:09 +0530

    chore: Remove cached tenant during cleanup stage

--- app/server/appsmith-server/src/test/java/com/appsmith/server/extensions/AfterAllCleanUpExtension.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/extensions/AfterAllCleanUpExtension.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static com.appsmith.server.testhelpers.cleanup.DBCleanup.deleteAllRoutines;
 import static com.appsmith.server.testhelpers.cleanup.DBCleanup.deleteAllTables;
+import static com.appsmith.server.testhelpers.cleanup.DBCleanup.deleteKeysWithPattern;
 
 /**
  * This SpringExtension is used to clean up the database after all tests have run. It drops all tables and routines.
@@ -30,9 +32,12 @@ public class AfterAllCleanUpExtension implements AfterAllCallback {
         Class<?> testClass = context.getRequiredTestClass();
         ApplicationContext applicationContext = SpringExtension.getApplicationContext(context);
         JdbcTemplate jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+        ReactiveRedisTemplate<String, Object> reactiveRedisTemplate =
+                applicationContext.getBean("reactiveRedisTemplate", ReactiveRedisTemplate.class);
 
         log.debug("Cleaning up after all tests for {}", testClass.getName());
         deleteAllTables(jdbcTemplate);
         deleteAllRoutines(jdbcTemplate);
+        deleteKeysWithPattern("tenant:.*", reactiveRedisTemplate).block();
     }
 }
--- app/server/appsmith-server/src/test/java/com/appsmith/server/testhelpers/cleanup/DBCleanup.java
+++ app/server/appsmith-server/src/test/java/com/appsmith/server/testhelpers/cleanup/DBCleanup.java
@@ -1,9 +1,14 @@
 package com.appsmith.server.testhelpers.cleanup;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.jdbc.core.JdbcTemplate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+@Slf4j
 public class DBCleanup {
     public static void deleteAllTables(JdbcTemplate jdbcTemplate) {
         List<String> tableNames =
@@ -21,5 +26,23 @@ public class DBCleanup {
         for (String routineName : routineNames) {
             jdbcTemplate.execute("DROP FUNCTION IF EXISTS public." + routineName + " CASCADE");
         }
+    }
+
+    public static Mono<Long> deleteKeysWithPattern(
+            String pattern, ReactiveRedisTemplate<String, Object> reactiveRedisTemplate) {
+        // SCAN for keys that match the pattern
+        return reactiveRedisTemplate
+                .scan()
+                .filter(key -> key.matches(pattern)) // Filter keys based on the pattern
+                .collectList()
+                .flatMap(keys -> {
+                    // Use Redis to delete all matched keys
+                    if (!keys.isEmpty()) {
+                        log.info("Deleting keys: {}", keys);
+                        return reactiveRedisTemplate.delete(Flux.fromIterable(keys));
+                    } else {
+                        return Mono.just(0L); // No keys to delete
+                    }
+                });
     }
 }
