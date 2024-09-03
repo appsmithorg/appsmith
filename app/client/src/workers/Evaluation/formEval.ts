1a4d1505f0fdfe9081c1a87fcf896a51d2b80f5d
Aman Agarwal <aman@appsmith.com>
2024-09-03 16:33:13 +0530

    feat: support client side db editor form

--- app/client/src/workers/Evaluation/formEval.ts
+++ app/client/src/workers/Evaluation/formEval.ts
@@ -39,7 +39,7 @@ let evalConfigPaths: string[] = [];
 
 // This regex matches the config property string up to countless places.
 export const MATCH_ACTION_CONFIG_PROPERTY =
-  /\b(actionConfiguration\.\w+.(?:(\w+.)){1,})\b/g;
+  /\b(actionConfiguration\.\w+(\[\d+\]\.){0,1}\w+)(?:(\w+.)){1,}\b/g;
 export function matchExact(r: RegExp, str: string) {
   const match = str.match(r);
   return match || [];
