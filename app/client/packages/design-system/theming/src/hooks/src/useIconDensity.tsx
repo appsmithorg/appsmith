89cc3164c682816f479f6596f5047ba2d2585f67
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-09-04 13:03:32 +0530

    revert to switch case

--- app/client/packages/design-system/theming/src/hooks/src/useIconDensity.tsx
+++ app/client/packages/design-system/theming/src/hooks/src/useIconDensity.tsx
@@ -3,14 +3,15 @@ import type { IconDensity } from "../../token";
 
 export const useIconDensity = (density: IconDensity, userDensity = 1) => {
   const strokeWidth = useMemo(() => {
-    if (userDensity < 1) {
-      return density.tight;
-    } else if (userDensity === 1) {
-      return density.regular;
-    } else if (userDensity > 1) {
-      return density.loose;
-    } else {
-      return density.regular;
+    switch (true) {
+      case userDensity < 1:
+        return density.tight;
+      case userDensity === 1:
+        return density.regular;
+      case userDensity > 1:
+        return density.loose;
+      default:
+        return density.regular;
     }
   }, [userDensity, density]);
 
--- app/client/packages/design-system/theming/src/hooks/src/useIconSizing.tsx
+++ app/client/packages/design-system/theming/src/hooks/src/useIconSizing.tsx
@@ -3,14 +3,15 @@ import type { IconSizing } from "../../token";
 
 export const useIconSizing = (sizing: IconSizing, userSizing = 1) => {
   const iconSize = useMemo(() => {
-    if (userSizing < 1) {
-      return sizing.small;
-    } else if (userSizing === 1) {
-      return sizing.regular;
-    } else if (userSizing > 1) {
-      return sizing.big;
-    } else {
-      return sizing.regular;
+    switch (true) {
+      case userSizing < 1:
+        return sizing.small;
+      case userSizing === 1:
+        return sizing.regular;
+      case userSizing > 1:
+        return sizing.big;
+      default:
+        return sizing.regular;
     }
   }, [userSizing, sizing]);
 
