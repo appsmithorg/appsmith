70494d9580f84a3e990f1ec0a3f0f0f85cce2826
Nidhi Nair <nidhi@appsmith.com>
2024-09-04 12:15:40 +0530

    chore: Enable netty metrics

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/ReactorNettyConfiguration.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/ReactorNettyConfiguration.java
@@ -1,15 +1,22 @@
 package com.appsmith.server.configurations;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
+@RequiredArgsConstructor
 @Component
 public class ReactorNettyConfiguration implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+    private final CommonConfig commonConfig;
+
     @Override
     public void customize(NettyReactiveWebServerFactory factory) {
-        factory.addServerCustomizers(httpServer -> httpServer.metrics(true, Function.identity()));
+        if (commonConfig.isTracingDetail()) {
+            factory.addServerCustomizers(httpServer -> httpServer.metrics(true, Function.identity()));
+        }
     }
 }
