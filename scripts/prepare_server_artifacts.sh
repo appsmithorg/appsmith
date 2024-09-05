47c0f83ce87fcd2c3701c08aa897b3d8e4db8b57
Abhijeet <41686026+abhvsn@users.noreply.github.com>
2024-09-05 17:02:12 +0530

    Update prepare_server_artifacts.sh

--- scripts/prepare_server_artifacts.sh
+++ scripts/prepare_server_artifacts.sh
@@ -4,7 +4,7 @@ cd "$(git rev-parse --show-toplevel)"
 
 if [[ -z "${EDITION-}" ]]; then
   export EDITION=ce
-  if [[ "$(git remote get-url origin)" == *appsmithorg/appsmith-ee.git ]]; then
+  if [[ "$(git remote get-url origin)" == *appsmithorg/appsmith-ee* ]]; then
     export EDITION=ee
   fi
 fi
@@ -20,7 +20,9 @@ cp -r "app/server/dist" "$target/pg"
 mv "$target/pg"/server-*.jar "$target/pg/server.jar"
 
 # Grab MongoDB server artifacts from that Docker image.
-docker run --name xx -d --rm --entrypoint sleep "appsmith/appsmith-$EDITION:$MONGO_TAG" infinity
+image="appsmith/appsmith-$EDITION:$MONGO_TAG"
+docker run --name xx --detach --entrypoint sleep "$image" infinity
 docker cp xx:/opt/appsmith/backend "$target/mongo"
 docker cp xx:/opt/appsmith/info.json "$target/mongo/source-info.json"
 docker rm --force xx
+docker image rm "$image"
