7a53aff29ed88a8367927dc581f29d0035cba0e0
Ilia <ilia@appsmith.com>
2024-09-06 09:49:52 +0200

    feat: add ComboBox wds component (#36052)
    ## Description
    - Added ComboBox wds component. It is partly unstyled. It will be fixed
    in upcoming PRs
    - Added ComboBox widget
    
    Fixes #36015 
    
    > [!WARNING]  
    > _If no issue exists, please create an issue first, and check with the
    maintainers if the issue is valid._
    
    ## Automation
    
    /ok-to-test tags="@tag.All"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/10718261812>
    > Commit: b13c79c63b809bfdb281dc74a7eaac028ece56a5
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=10718261812&attempt=2"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.All`
    > Spec:
    > <hr>Thu, 05 Sep 2024 11:36:28 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [x] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a customizable and accessible ComboBox component for item
    selection.
    - Added support for loading states, validation, and contextual help
    within the ComboBox.
    - Integrated new configuration options for widget behavior and
    appearance.
    - Enhanced metadata for the ComboBox, including search tags for improved
    discoverability.
    	- Added new icon and thumbnail components for ComboBox representation.
    
    - **Bug Fixes**
    - Enhanced validation logic to ensure proper input formatting for
    ComboBox options.
    
    - **Documentation**
    - Added Storybook stories to demonstrate various ComboBox states and
    functionalities.
    
    - **Style**
    - Implemented a comprehensive CSS module for consistent styling of the
    ComboBox component.
    
    - **Chores**
    - Updated widget mapping to include the new ComboBox widget in the
    application.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Vadim Vaitenko <vadim@appsmith.com>

---
+++ app/client/packages/design-system/widgets/src/components/ComboBox/stories/ComboBox.stories.tsx
@@ -0,0 +1,86 @@
+import { Button, ComboBox, Flex, SIZES } from "@appsmith/wds";
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { items, itemsWithIcons } from "./items";
+
+/**
+ * A select displays a collapsible list of options and allows a user to select one of them.
+ */
+const meta: Meta<typeof ComboBox> = {
+  component: ComboBox,
+  title: "WDS/Widgets/ComboBox",
+};
+
+export default meta;
+type Story = StoryObj<typeof ComboBox>;
+
+export const Main: Story = {
+  args: {
+    items: items,
+  },
+  render: (args) => (
+    <Flex width="sizing-60">
+      <ComboBox {...args} />
+    </Flex>
+  ),
+};
+
+/**
+ * The component supports two sizes `small` and `medium`. Default size is `medium`.
+ */
+export const Sizes: Story = {
+  render: () => (
+    <Flex direction="column" gap="spacing-4" width="sizing-60">
+      {Object.keys(SIZES)
+        .filter((size) => !["large"].includes(size))
+        .map((size) => (
+          <ComboBox items={items} key={size} placeholder={size} size={size} />
+        ))}
+    </Flex>
+  ),
+};
+
+export const Loading: Story = {
+  args: {
+    placeholder: "Loading",
+    isLoading: true,
+    items: items,
+  },
+};
+
+export const Validation: Story = {
+  render: () => (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert("Form submitted");
+      }}
+    >
+      <Flex direction="column" gap="spacing-2" width="sizing-60">
+        <ComboBox
+          description="description"
+          isRequired
+          items={items}
+          label="Validation"
+        />
+        <Button type="submit">Submit</Button>
+      </Flex>
+    </form>
+  ),
+};
+
+export const ContextualHelp: Story = {
+  args: {
+    label: "Label",
+    placeholder: "Contextual Help Text",
+    contextualHelp: "This is a contextual help text",
+    items: items,
+  },
+};
+
+export const WithIcons: Story = {
+  args: {
+    label: "With icons",
+    items: itemsWithIcons,
+  },
+};
---
+++ app/client/src/widgets/wds/WDSComboBoxWidget/config/settersConfig.ts
@@ -0,0 +1,16 @@
+export const settersConfig = {
+  __setters: {
+    setVisibility: {
+      path: "isVisible",
+      type: "boolean",
+    },
+    setDisabled: {
+      path: "isDisabled",
+      type: "boolean",
+    },
+    setData: {
+      path: "options",
+      type: "array",
+    },
+  },
+};
