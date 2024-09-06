3842691380da9ce4b14a721af9a5426a069db24c
Rahul Barwal <rahul.barwal@appsmith.com>
2024-09-06 16:56:29 +0530

    Updates spec file

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/DateColumn_ISOFormat_AddNewRow_EditRow_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/DateColumn_ISOFormat_AddNewRow_EditRow_spec.ts
@@ -4,34 +4,26 @@ import {
   propPane,
   agHelper,
   table,
+  locators,
 } from "../../../../../support/Objects/ObjectsCore";
 
 const tableData = `[
     {
       "step": "#1",
       "task": "Drop a table",
-      "completed": true,
       "Date": "2021-05-25 12:00"
     },
     {
       "step": "#2",
       "task": "Create a query fetch_users with the Mock DB",
-      "completed": true,
       "Date": "2021-05-27 14:30"
     },
     {
       "step": "#3",
       "task": "Bind the query using => fetch_users.data",
-      "completed": false,
       "Date": "2021-05-29 16:45"
     }
   ]`;
-const getCurrentDateISO = (): string => {
-  const now = new Date();
-  now.setHours(0, 0, 0, 0);
-  const offset = now.getTimezoneOffset() * 60000;
-  return new Date(now.getTime() - offset).toISOString().slice(0, 19) + "+05:30";
-};
 
 describe(
   "Table Widget V2 - Add New Row and Edit Date Column",
@@ -42,41 +34,46 @@ describe(
       propPane.EnterJSContext("Table data", tableData);
     });
 
-    const togglePropertyAndGoBack = (
-      propertyName: string,
-      toggle: "On" | "Off" = "On",
-    ) => {
-      propPane.TogglePropertyState(propertyName, toggle);
-      propPane.NavigateBackToPropertyPane();
-    };
-
     it("1. Verify Date column is visible and editable", () => {
       propPane.TogglePropertyState("Allow adding a row", "On");
-      agHelper.AssertElementExist(table._addNewRow);
-      // enabling the ediatable option for the three columns
-      table.EditColumn("step", "v2");
-
-      togglePropertyAndGoBack("Editable", "On");
+      // enabling the editable option for the three columns
       table.EditColumn("Date", "v2");
+      propPane.TogglePropertyState("Editable", "On");
+      propPane.SelectPropertiesDropDown("Date format", "YYYY-MM-DD");
 
-      togglePropertyAndGoBack("Editable", "On");
       table.AddNewRow();
+      agHelper
+        .GetText(`${table._tableRow1Child3} ${locators._inputField}`)
+        .then(($textData) => expect($textData).to.eq(""));
+      const datepickerLocator = `${table._tableRow1Child3} ${locators._inputField}`;
+      agHelper.GetNClick(datepickerLocator, 0, true);
+      agHelper
+        .GetElement(`.bp3-datepicker-day-wrapper`)
+        .contains("11")
+        .click({ force: true });
 
-      // entering the values in the table
-      table.EditTableCell(0, 0, "22");
-
-      // Click on the date input to open the date picker
-      table.EditTableCell(0, 3, "");
-
-      agHelper.GetNClick(`.DayPicker-Day--today > .bp3-datepicker-day-wrapper`);
+      // add a text widget to check the values
       cy.dragAndDropToCanvas("textwidget", { x: 300, y: 600 });
       propPane.UpdatePropertyFieldValue("Text", `{{Table1.newRow.Date}}`);
+      agHelper.GetText(locators._textWidget, "text").then(($text: any) => {
+        expect($text).to.eq(getYYYYMMDDForThisMonth("11"));
+      });
 
-      const localISOTime = getCurrentDateISO();
-      cy.get(".t--widget-textwidget .bp3-ui-text").should(
-        "contain",
-        localISOTime.split("T")[0] + "T00:00:00+05:30",
+      // check the same date in europe timezone(Issue: https://github.com/appsmithorg/appsmith/issues/25081)
+      agHelper.GetNClick(locators._textWidget);
+      propPane.UpdatePropertyFieldValue(
+        "Text",
+        `{{moment(Table1.newRow.date).tz("Europe/Amsterdam").format("YYYY-MM-DD")}}`,
       );
+
+      agHelper.GetText(locators._textWidget, "text").then(($text: any) => {
+        expect($text).to.eq(getYYYYMMDDForThisMonth("11"));
+      });
     });
   },
 );
+
+const getYYYYMMDDForThisMonth = (date: string): string => {
+  const now = new Date();
+  return `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, "0")}-${date}`;
+};
