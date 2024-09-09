b0516d09e4268def58306571166fe6e6318103b0
Ankita Kinger <ankita@appsmith.com>
2024-09-09 16:15:06 +0530

    Revert "fix: fixed back button to redirect to applications page (#35900)"
    This reverts commit 420dc9b4c442a70c7616622086a6617ca87a6f76.

--- app/client/src/pages/UserProfile/index.test.tsx
+++ app/client/src/pages/UserProfile/index.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import { lightTheme } from "selectors/themeSelectors";
@@ -87,15 +87,6 @@ jest.mock("actions/gitSyncActions", () => ({
   fetchGlobalGitConfigInit: jest.fn(),
 }));
 
-const mockHistoryPush = jest.fn();
-
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useHistory: () => ({
-    push: mockHistoryPush,
-  }),
-}));
-
 const mockStore = configureStore([]);
 describe("Git config ", () => {
   // TODO: Fix this the next time the file is edited
@@ -135,22 +126,6 @@ describe("Git config ", () => {
       </Provider>,
     );
     expect(getAllByText("Sign in to your account")).toBeInTheDocument;
-  });
-
-  it("should call history push when user goes back to applications", () => {
-    store = mockStore(defaultStoreState);
-    render(
-      <Provider store={store}>
-        <ThemeProvider theme={lightTheme}>
-          <Router>
-            <UserProfile />
-          </Router>
-        </ThemeProvider>
-      </Provider>,
-    );
-    const backButton = screen.getByText("Back");
-    fireEvent.click(backButton);
-    expect(mockHistoryPush).toHaveBeenCalledWith("/applications");
   });
 
   afterAll(() => {
