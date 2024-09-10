fcf8a4379eb7fa6ff88db518fc9135c7beb308b4
Jacques Ikot <jacquesikot@gmail.com>
2024-09-10 05:23:35 +0100

    move test data to fixtures

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/TableV2Filter1_1_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/TableV2Filter1_1_Spec.ts
@@ -1,3 +1,4 @@
+import { demoTableDataForSelect } from "../../../../../fixtures/Table/DemoTableData";
 import { featureFlagIntercept } from "../../../../../support/Objects/FeatureFlags";
 import {
   entityExplorer,
@@ -12,53 +13,6 @@ import {
 import EditorNavigation, {
   EntityType,
 } from "../../../../../support/Pages/EditorNavigation";
-
-const demoTableData = `
-{{
-  [
-    {
-      role: 10,
-      id: 1,
-      name: "Alice Johnson",
-      email: "alice.johnson@example.com",
-      age: 28,
-      gender: 2
-    },
-    {
-      role: 20,
-      id: 2,
-      name: "Bob Smith",
-      email: "bob.smith@example.com",
-      age: 34,
-      gender: 1
-    },
-    {
-      role: 30,
-      id: 3,
-      name: "Charlie Brown",
-      email: "charlie.brown@example.com",
-      age: 25,
-      gender: 3
-    },
-    {
-      role: 20,
-      id: 4,
-      name: "Diana Prince",
-      email: "diana.prince@example.com",
-      age: 30,
-      gender: 2
-    },
-    {
-      role: 10,
-      id: 5,
-      name: "Evan Williams",
-      email: "evan.williams@example.com",
-      age: 27,
-      gender: 1
-    }
-  ]
-}}
-  `;
 
 describe(
   "Verify various Table_Filter combinations",
@@ -194,7 +148,7 @@ describe(
       featureFlagIntercept({ release_table_cell_label_value_enabled: true });
       deployMode.NavigateBacktoEditor();
       EditorNavigation.SelectEntityByName("Table1", EntityType.Widget);
-      propPane.EnterJSContext("Table data", demoTableData);
+      propPane.EnterJSContext("Table data", demoTableDataForSelect);
 
       // Edit role column to select type
       table.ChangeColumnType("role", "Select", "v2");
---
+++ app/client/cypress/fixtures/Table/DemoTableData.ts
@@ -0,0 +1,46 @@
+export const demoTableDataForSelect = `
+{{
+  [
+    {
+      role: 10,
+      id: 1,
+      name: "Alice Johnson",
+      email: "alice.johnson@example.com",
+      age: 28,
+      gender: 2
+    },
+    {
+      role: 20,
+      id: 2,
+      name: "Bob Smith",
+      email: "bob.smith@example.com",
+      age: 34,
+      gender: 1
+    },
+    {
+      role: 30,
+      id: 3,
+      name: "Charlie Brown",
+      email: "charlie.brown@example.com",
+      age: 25,
+      gender: 3
+    },
+    {
+      role: 20,
+      id: 4,
+      name: "Diana Prince",
+      email: "diana.prince@example.com",
+      age: 30,
+      gender: 2
+    },
+    {
+      role: 10,
+      id: 5,
+      name: "Evan Williams",
+      email: "evan.williams@example.com",
+      age: 27,
+      gender: 1
+    }
+  ]
+}}
+  `;
