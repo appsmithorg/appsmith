d9b56309e7b81e116f751eba773e289a267471f4
Hetu Nandu <hetunandu@gmail.com>
2024-09-11 17:16:51 +0530

    Convert to Module Callout

--- app/client/src/ce/pages/Editor/AppPluginActionEditor/AppPluginActionEditor.tsx
+++ app/client/src/ce/pages/Editor/AppPluginActionEditor/AppPluginActionEditor.tsx
@@ -5,11 +5,13 @@ import {
   PluginActionForm,
   PluginActionResponsePane,
 } from "PluginActionEditor";
+import ConvertToModuleCallout from "./components/ConvertToModuleCallout";
 
 const AppPluginActionEditor = () => {
   return (
     <PluginActionEditor>
       <PluginActionToolbar />
+      <ConvertToModuleCallout />
       <PluginActionForm />
       <PluginActionResponsePane />
     </PluginActionEditor>
---
+++ app/client/src/ce/pages/Editor/AppPluginActionEditor/components/ConvertToModuleCallout.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Flex, Icon } from "@appsmith/ads";
+import { useSelector } from "react-redux";
+import { getIsActionConverting } from "ee/selectors/entitiesSelector";
+import { usePluginActionContext } from "PluginActionEditor";
+import {
+  ENTITY_ICON_SIZE,
+  EntityIcon,
+} from "pages/Editor/Explorer/ExplorerIcons";
+import ConvertEntityNotification from "ee/pages/common/ConvertEntityNotification";
+import { resolveIcon } from "pages/Editor/utils";
+
+const PluginIcon = () => {
+  const { action, plugin } = usePluginActionContext();
+  return (
+    resolveIcon({
+      iconLocation: plugin.iconLocation || "",
+      pluginType: plugin.type,
+      moduleType: action.actionConfiguration?.body?.moduleType,
+    }) || (
+      <EntityIcon
+        height={`${ENTITY_ICON_SIZE}px`}
+        width={`${ENTITY_ICON_SIZE}px`}
+      >
+        <Icon name="module" />
+      </EntityIcon>
+    )
+  );
+};
+
+const ConvertToModuleCallout = () => {
+  const { action } = usePluginActionContext();
+  const isConverting = useSelector((state) =>
+    getIsActionConverting(state, action.id),
+  );
+  if (!isConverting) return null;
+  return (
+    <Flex p="spaces-7">
+      {/* eslint-disable-next-line react-perf/jsx-no-jsx-as-prop */}
+      <ConvertEntityNotification icon={<PluginIcon />} name={action.name} />
+    </Flex>
+  );
+};
+
+export default ConvertToModuleCallout;
