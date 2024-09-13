8d0eab243cd26b5bccd5b341f1966be98e19d472
Hetu Nandu <hetunandu@gmail.com>
2024-09-13 11:35:57 +0530

    fix lint issue

--- app/client/src/ce/pages/Editor/AppPluginActionEditor/components/ConvertToModule/ConvertToModuleCallout.tsx
+++ app/client/src/ce/pages/Editor/AppPluginActionEditor/components/ConvertToModule/ConvertToModuleCallout.tsx
@@ -10,34 +10,29 @@ import {
 import ConvertEntityNotification from "ee/pages/common/ConvertEntityNotification";
 import { resolveIcon } from "pages/Editor/utils";
 
-const PluginIcon = () => {
-  const { action, plugin } = usePluginActionContext();
-  return (
-    resolveIcon({
-      iconLocation: plugin.iconLocation || "",
-      pluginType: plugin.type,
-      moduleType: action.actionConfiguration?.body?.moduleType,
-    }) || (
-      <EntityIcon
-        height={`${ENTITY_ICON_SIZE}px`}
-        width={`${ENTITY_ICON_SIZE}px`}
-      >
-        <Icon name="module" />
-      </EntityIcon>
-    )
-  );
-};
-
 const ConvertToModuleCallout = () => {
-  const { action } = usePluginActionContext();
+  const { action, plugin } = usePluginActionContext();
   const isConverting = useSelector((state) =>
     getIsActionConverting(state, action.id),
   );
-  if (!isConverting) return null;
+
+  const PluginIcon = resolveIcon({
+    iconLocation: plugin.iconLocation || "",
+    pluginType: plugin.type,
+    moduleType: action.actionConfiguration?.body?.moduleType,
+  }) || (
+    <EntityIcon
+      height={`${ENTITY_ICON_SIZE}px`}
+      width={`${ENTITY_ICON_SIZE}px`}
+    >
+      <Icon name="module" />
+    </EntityIcon>
+  );
+
+  if (isConverting) return null;
   return (
     <Flex p="spaces-7" w="100%">
-      {/* eslint-disable-next-line react-perf/jsx-no-jsx-as-prop */}
-      <ConvertEntityNotification icon={<PluginIcon />} name={action.name} />
+      <ConvertEntityNotification icon={PluginIcon} name={action.name} />
     </Flex>
   );
 };
