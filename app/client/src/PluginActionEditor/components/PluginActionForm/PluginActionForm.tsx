fbd581dba8ccb50eb57f871b08e8ced9b7ae75b6
Hetu Nandu <hetunandu@gmail.com>
2024-09-18 15:43:15 +0530

    Usage in new module

--- app/client/src/PluginActionEditor/components/PluginActionForm/PluginActionForm.tsx
+++ app/client/src/PluginActionEditor/components/PluginActionForm/PluginActionForm.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import APIEditorForm from "./components/APIEditorForm";
+import { Flex } from "@appsmith/ads";
 
 const PluginActionForm = () => {
-  return <div />;
+  return (
+    <Flex p="spaces-2" w="100%">
+      <APIEditorForm />
+    </Flex>
+  );
 };
 
 export default PluginActionForm;
