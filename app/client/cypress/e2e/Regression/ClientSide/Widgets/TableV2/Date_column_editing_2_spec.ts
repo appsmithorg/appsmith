6b6b7a89016902699709c7524551d6926f0089f0
Jacques Ikot <jacquesikot@gmail.com>
2024-09-19 04:30:30 +0100

    add base test

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/Date_column_editing_2_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/Date_column_editing_2_spec.ts
@@ -92,5 +92,67 @@ describe(
           expect($textData).to.include("YYYY-MM-DDTHH:mm:ss.SSSZ"),
         );
     });
+
+    it("4. should allow input format of Unix Timestamp(ms) and not throw Invalid Value error when inline editing", () => {
+      EditorNavigation.SelectEntityByName("Table1", EntityType.Widget);
+      // propPane.NavigateBackToPropertyPane();
+      propPane.UpdatePropertyFieldValue(
+        "Table data",
+        `
+        {{
+          [
+              {
+                unix: 1726713837918,
+                role: 1,
+                id: 1,
+                name: "Alice Johnson",
+                email: "alice.johnson@example.com",
+                age: 28,
+                gender: 2,
+              },
+              {
+                unix: 1726713837918,
+                role: 2,
+                id: 2,
+                name: "Bob Smith",
+                email: "bob.smith@example.com",
+                age: 34,
+                gender: 1
+              },
+              {
+                unix: 1726713837918,
+                role: 3,
+                id: 3,
+                name: "Charlie Brown",
+                email: "charlie.brown@example.com",
+                age: 25,
+                gender: 3
+              },
+              {
+                unix: 1726713837918,
+                role: 2,
+                id: 4,
+                name: "Diana Prince",
+                email: "diana.prince@example.com",
+                age: 30,
+                gender: 2
+              },
+              {
+                unix: 1726713837918,
+                role: 1,
+                id: 5,
+                name: "Evan Williams",
+                email: "evan.williams@example.com",
+                age: 27,
+                gender: 1
+              }
+            ]
+          }}
+        `,
+      );
+      table.EditColumn("unix", "v2");
+      // table.ChangeColumnType("unix", "Date", "v2");
+      // propPane.UpdatePropertyFieldValue("Date format", "Milliseconds");
+    });
   },
 );
