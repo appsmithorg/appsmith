c76e6c031c699ad1c623140fa584b8aeb1f11c1c
Valera Melnikov <valera@appsmith.com>
2024-09-19 16:01:03 +0300

    chore:delete unused files (#36368)
    ## Description
    I used [knip](https://knip.dev/) to identify unused files. There will be
    a few more related PRs, after that I will add CI check for this.
    
    Fixes #35778
    
    Related EE PR https://github.com/appsmithorg/appsmith-ee/pull/5165
    
    ## Automation
    
    /ok-to-test tags="@tag.Sanity"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/10940962461>
    > Commit: 31749570e9a3f9e4cce250eedba4ec18aace23f9
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=10940962461&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Sanity`
    > Spec:
    > <hr>Thu, 19 Sep 2024 12:55:41 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [x] No

--- app/client/src/selectors/tourSelectors.tsx
+++
@@ -1,7 +0,0 @@
-import type { AppState } from "ee/reducers";
-
-export const getActiveTourIndex = (state: AppState) =>
-  state.ui.tour?.activeTourIndex;
-
-export const getActiveTourType = (state: AppState) =>
-  state.ui.tour?.activeTourType;
