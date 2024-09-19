726c80e91317c02ed8d1761d825b3efa412308c8
Rishabh-Rathod <rishabh.rathod@appsmith.com>
2024-09-20 01:55:46 +0530

    fix: remove redundant eval trigger

--- app/client/src/pages/AppViewer/index.tsx
+++ app/client/src/pages/AppViewer/index.tsx
@@ -28,10 +28,7 @@ import { useSelector } from "react-redux";
 import BrandingBadge from "./BrandingBadge";
 import { setAppViewHeaderHeight } from "actions/appViewActions";
 import { CANVAS_SELECTOR } from "constants/WidgetConstants";
-import {
-  setupPublishedPage,
-  fetchPublishedPageResourcesAction,
-} from "actions/pageActions";
+import { fetchPublishedPageResourcesAction } from "actions/pageActions";
 import usePrevious from "utils/hooks/usePrevious";
 import { getIsBranchUpdated } from "../utils";
 import { APP_MODE } from "entities/App";
@@ -165,8 +162,6 @@ function AppViewer(props: Props) {
         )?.pageId;
 
         if (pageId) {
-          dispatch(setupPublishedPage(pageId, true));
-
           // Used for fetching page resources
           dispatch(fetchPublishedPageResourcesAction(basePageId));
         }
