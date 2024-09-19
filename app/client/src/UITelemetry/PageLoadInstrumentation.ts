c2521fae8101ab757cc9e9fcbe3300de423325f2
Diljit <diljit@appsmith.com>
2024-09-19 11:06:14 +0530

    chore: Page load instrumentation: add network and device info to page load spans (#36395)
    ## Description
    - Remove page visibility change listener in Page load instrumentation.
    The page visibility data is not working as expected.
    - Add `otlpSessionId` and `appMode` attributes to all PageView and
    PageViewTimings data pushed by the new relic browser agent. This will
    help us infer session based information and join reports with Page load
    Spans
    - Add network and device specific attributes to the `PAGE_LOAD` span
    
    
    Fixes #`Issue Number`  
    _or_  
    Fixes `Issue URL`
    > [!WARNING]  
    > _If no issue exists, please create an issue first, and check with the
    maintainers if the issue is valid._
    
    ## Automation
    
    /ok-to-test tags="@tag.Sanity"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/10934682384>
    > Commit: de172593ce8d7782fb526d3294e8a63155e3c849
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=10934682384&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Sanity`
    > Spec:
    > <hr>Thu, 19 Sep 2024 05:22:19 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced telemetry data collection during page load events, capturing
    device memory and network attributes.
    - Introduced functionality to gather operating system details (name and
    version) for improved telemetry insights.
    - Improved integration with New Relic for detailed monitoring through
    custom attributes.
    
    - **Bug Fixes**
    - Removed outdated logic for tracking page visibility, focusing on more
    relevant metrics.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/UITelemetry/PageLoadInstrumentation.ts
+++ app/client/src/UITelemetry/PageLoadInstrumentation.ts
@@ -9,6 +9,16 @@ import type {
 } from "web-vitals";
 import isString from "lodash/isString";
 
+type TNavigator = Navigator & {
+  deviceMemory: number;
+  connection: {
+    effectiveType: string;
+    downlink: number;
+    rtt: number;
+    saveData: boolean;
+  };
+};
+
 export class PageLoadInstrumentation extends InstrumentationBase {
   // PerformanceObserver to observe resource timings
   resourceTimingObserver: PerformanceObserver | null = null;
@@ -16,10 +26,6 @@ export class PageLoadInstrumentation extends InstrumentationBase {
   rootSpan: Span;
   // List of resource URLs to ignore
   ignoreResourceUrls: string[] = [];
-  // Timestamp when the page was last hidden
-  pageLastHiddenAt: number = 0;
-  // Duration the page was hidden for
-  pageHiddenFor: number = 0;
   // Flag to check if navigation entry was pushed
   wasNavigationEntryPushed: boolean = false;
   // Set to keep track of resource entries
@@ -44,7 +50,11 @@ export class PageLoadInstrumentation extends InstrumentationBase {
   }
 
   enable(): void {
-    this.addVisibilityChangeListener();
+    // Register connection change listener
+    this.addConnectionAttributes();
+
+    // Add device attributes to the root span
+    this.addDeviceAttributes();
 
     // Listen for LCP and FCP events
     // reportAllChanges: true will report all LCP and FCP events
@@ -61,17 +71,26 @@ export class PageLoadInstrumentation extends InstrumentationBase {
     }
   }
 
-  private addVisibilityChangeListener() {
-    // Listen for page visibility changes to track time spent on hidden page
-    document.addEventListener("visibilitychange", () => {
-      if (document.visibilityState === "hidden") {
-        this.pageLastHiddenAt = performance.now();
-      } else {
-        const endTime = performance.now();
-
-        this.pageHiddenFor = endTime - this.pageLastHiddenAt;
-      }
+  private addDeviceAttributes() {
+    this.rootSpan.setAttributes({
+      deviceMemory: (navigator as TNavigator).deviceMemory,
+      hardwareConcurrency: navigator.hardwareConcurrency,
     });
+  }
+
+  private addConnectionAttributes() {
+    if ((navigator as TNavigator).connection) {
+      const { downlink, effectiveType, rtt, saveData } = (
+        navigator as TNavigator
+      ).connection;
+
+      this.rootSpan.setAttributes({
+        effectiveConnectionType: effectiveType,
+        connectionDownlink: downlink,
+        connectionRtt: rtt,
+        connectionSaveData: saveData,
+      });
+    }
   }
 
   // Handler for LCP report
@@ -156,7 +175,6 @@ export class PageLoadInstrumentation extends InstrumentationBase {
         element: this.getElementName(element),
         entryType,
         loadTime,
-        pageHiddenFor: this.pageHiddenFor,
       },
       0,
     );
