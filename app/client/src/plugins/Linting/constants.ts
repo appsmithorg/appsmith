0e47d57de2f82b567fc925346906e0ac05d35acc
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-09-20 01:53:37 +0530

    update: new opts based on linterversion

--- app/client/src/plugins/Linting/constants.ts
+++ app/client/src/plugins/Linting/constants.ts
@@ -2,32 +2,75 @@ import { ECMA_VERSION } from "@shared/ast";
 import type { LintOptions } from "jshint";
 import isEntityFunction from "./utils/isEntityFunction";
 
-export const lintOptions = (globalData: Record<string, boolean>) =>
-  ({
-    indent: 2,
-    esversion: ECMA_VERSION,
-    eqeqeq: false, // Not necessary to use ===
-    curly: false, // Blocks can be added without {}, eg if (x) return true
-    freeze: true, // Overriding inbuilt classes like Array is not allowed
-    undef: true, // Undefined variables should be reported as error
-    forin: false, // Doesn't require filtering for..in loops with obj.hasOwnProperty()
-    noempty: false, // Empty blocks are allowed
-    strict: false, // We won't force strict mode
-    unused: "strict", // Unused variables are not allowed
-    asi: true, // Tolerate Automatic Semicolon Insertion (no semicolons)
-    boss: true, // Tolerate assignments where comparisons would be expected
-    evil: false, // Use of eval not allowed
-    funcscope: true, // Tolerate variable definition inside control statements
-    sub: true, // Don't force dot notation
-    expr: true, // suppresses warnings about the use of expressions where normally you would expect to see assignments or function calls
-    // environments
-    browser: false,
-    worker: true,
-    mocha: false,
-    // global values
-    globals: globalData,
-    loopfunc: true,
-  }) as LintOptions;
+export const lintOptions = (
+  globalData: Record<string, boolean>,
+  linterVersion = 1,
+) => {
+  if (linterVersion === 1) {
+    return {
+      indent: 2,
+      esversion: ECMA_VERSION,
+      eqeqeq: false, // Not necessary to use ===
+      curly: false, // Blocks can be added without {}, eg if (x) return true
+      freeze: true, // Overriding inbuilt classes like Array is not allowed
+      undef: true, // Undefined variables should be reported as error
+      forin: false, // Doesn't require filtering for..in loops with obj.hasOwnProperty()
+      noempty: false, // Empty blocks are allowed
+      strict: false, // We won't force strict mode
+      unused: "strict", // Unused variables are not allowed
+      asi: true, // Tolerate Automatic Semicolon Insertion (no semicolons)
+      boss: true, // Tolerate assignments where comparisons would be expected
+      evil: false, // Use of eval not allowed
+      funcscope: true, // Tolerate variable definition inside control statements
+      sub: true, // Don't force dot notation
+      expr: true, // suppresses warnings about the use of expressions where normally you would expect to see assignments or function calls
+      // environments
+      browser: false,
+      worker: true,
+      mocha: false,
+      // global values
+      globals: globalData,
+      loopfunc: true,
+    } as LintOptions;
+  } else {
+    const eslintGlobals: Record<string, "writable" | "readonly"> = {};
+
+    for (const key in globalData) {
+      //eslintGlobals[key] = globalData[key] ? "writable" : "readonly";
+      eslintGlobals[key] = "readonly";
+    }
+
+    return {
+      languageOptions: {
+        ecmaVersion: ECMA_VERSION,
+        globals: eslintGlobals,
+        sourceType: "script",
+      },
+      rules: {
+        indent: ["off", "tab"],
+        //indent: "off",
+        eqeqeq: "off",
+        curly: "off",
+        "no-extend-native": "error",
+        "no-undef": "error",
+        "guard-for-in": "off",
+        "no-empty": "off",
+        strict: "off",
+        "no-unused-vars": [
+          "error",
+          { vars: "all", args: "all", ignoreRestSiblings: false },
+        ],
+        //semi: ["error", "never"],
+        "no-cond-assign": "off",
+        "no-eval": "error",
+        "block-scoped-var": "off",
+        "dot-notation": "off",
+        "no-unused-expressions": "off",
+        "no-loop-func": "off",
+      },
+    };
+  }
+};
 export const JS_OBJECT_START_STATEMENT = "export default";
 export const INVALID_JSOBJECT_START_STATEMENT = `JSObject must start with '${JS_OBJECT_START_STATEMENT}'`;
 export const INVALID_JSOBJECT_START_STATEMENT_ERROR_CODE =
