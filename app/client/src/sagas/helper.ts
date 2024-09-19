39478d7c747a855f0ca3d019c13e04e193ec4a40
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-09-19 13:13:40 +0530

    remove apiutils test file

--- app/client/src/sagas/helper.ts
+++ app/client/src/sagas/helper.ts
@@ -237,7 +237,9 @@ export function* getFromServerWhenNoPrefetchedResult(
           },
           status,
         },
-      });
+        // TODO: Fix this the next time the file is edited
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any);
 
       return resp;
     }
