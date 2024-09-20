c237564da90df1f859d8769b68c716976d0c7098
Ankita Kinger <ankita@appsmith.com>
2024-09-20 12:51:17 +0530

    updating imports

--- app/client/src/IDE/hooks/index.ts
+++ app/client/src/IDE/hooks/index.ts
@@ -1,2 +1,3 @@
 export { useIsInSideBySideEditor } from "./useIsInSideBySideEditor";
 export { useIsEditorInitialised } from "ee/IDE/hooks/useIsEditorInitialised";
+export { useActionSettingsConfig } from "ee/IDE/hooks/useActionSettingsConfig";
--- app/client/src/PluginActionEditor/PluginActionEditor.tsx
+++ app/client/src/PluginActionEditor/PluginActionEditor.tsx
@@ -14,8 +14,7 @@ import EntityNotFoundPane from "pages/Editor/EntityNotFoundPane";
 import Spinner from "components/editorComponents/Spinner";
 import CenteredWrapper from "components/designSystems/appsmith/CenteredWrapper";
 import { Text } from "@appsmith/ads";
-import { useIsEditorInitialised } from "IDE/hooks";
-import { useActionSettingsConfig } from "ee/IDE/hooks/useActionSettingsConfig";
+import { useIsEditorInitialised, useActionSettingsConfig } from "IDE/hooks";
 
 interface ChildrenProps {
   children: React.ReactNode | React.ReactNode[];
