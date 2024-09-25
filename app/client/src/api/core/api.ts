88dc351652e65feabccc9536f97e6ffa0134df00
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-09-25 13:07:37 +0530

    add comment for why we pass 2nd second parameter in api functions

--- app/client/src/api/core/api.ts
+++ app/client/src/api/core/api.ts
@@ -5,6 +5,8 @@ import { apiFactory } from "./factory";
 const apiInstance = apiFactory();
 
 export async function get<T>(...args: Parameters<typeof apiInstance.get>) {
+  // Note: we are passing AxiosResponseData as the second type argument to set the default type of the response data.
+  // The reason is we modify the response data in the response interceptor and we want to make sure that the response data's type matches what we do in the interceptor.
   return apiInstance.get<T, AxiosResponseData<T>>(...args);
 }
 
