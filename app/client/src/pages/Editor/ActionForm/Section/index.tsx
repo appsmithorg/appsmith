e8f330c541385f8ba17b6a3e9a28dc449626f0c3
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-09-25 10:43:08 +0530

    feat: added full size support for section and zone

--- app/client/src/pages/Editor/ActionForm/Section/index.tsx
+++ app/client/src/pages/Editor/ActionForm/Section/index.tsx
@@ -4,14 +4,20 @@ import styles from "./styles.module.css";
 interface SectionProps {
   children: React.ReactNode;
   isStandalone?: boolean;
+  isFullSize?: boolean;
 }
 
 const Section: React.FC<SectionProps> = ({
   children,
+  isFullSize = false,
   isStandalone = false,
 }) => {
   return (
-    <div className={styles.section} data-standalone={isStandalone.toString()}>
+    <div
+      className={styles.section}
+      data-full-size={isFullSize.toString()}
+      data-standalone={isStandalone.toString()}
+    >
       {children}
     </div>
   );
--- app/client/src/pages/Editor/ActionForm/Zone/index.tsx
+++ app/client/src/pages/Editor/ActionForm/Zone/index.tsx
@@ -4,11 +4,20 @@ import styles from "./styles.module.css";
 interface ZoneProps {
   children: React.ReactNode;
   layout?: "single_column" | "double_column";
+  isFullSize?: boolean;
 }
 
-const Zone: React.FC<ZoneProps> = ({ children, layout = "single_column" }) => {
+const Zone: React.FC<ZoneProps> = ({
+  children,
+  isFullSize = false,
+  layout = "single_column",
+}) => {
   return (
-    <div className={styles.zone} data-layout={layout}>
+    <div
+      className={styles.zone}
+      data-full-size={isFullSize.toString()}
+      data-layout={layout}
+    >
       {children}
     </div>
   );
