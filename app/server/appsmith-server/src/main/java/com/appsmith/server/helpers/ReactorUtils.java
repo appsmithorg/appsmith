51d92eb2d5378a0ebf19e3f579a00636781315db
Anagh Hegde <anagh@appsmith.com>
2024-09-25 15:06:21 +0530

    Create a new bounded scheduler for all the db ops

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ReactorUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ReactorUtils.java
@@ -2,6 +2,7 @@ package com.appsmith.server.helpers;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.Optional;
@@ -10,17 +11,27 @@ import java.util.function.Supplier;
 public class ReactorUtils {
     private ReactorUtils() {}
 
+    private static final String ELASTIC_THREAD_POOL_NAME = "appsmith-db-elastic-pool";
+
+    /*
+     * This scheduler is used to run for all the db ops
+     * The number of threads is set to 300, and the queue size is set to the default value
+     * The reason for the higher number 300 is
+     * to support the cache evictions flow used in PermissionGroupCEImpl class
+     * TODO : We need to revisit this number and see if we can reduce it by modifying the cache eviction flow to accept List of users
+     */
+    public static final Scheduler elasticScheduler =
+            Schedulers.newBoundedElastic(300, Schedulers.DEFAULT_BOUNDED_ELASTIC_QUEUESIZE, ELASTIC_THREAD_POOL_NAME);
+
     public static <T> Mono<T> asMono(Supplier<Optional<T>> supplier) {
-        return Mono.defer(() -> Mono.justOrEmpty(supplier.get())).subscribeOn(Schedulers.boundedElastic());
+        return Mono.defer(() -> Mono.justOrEmpty(supplier.get())).subscribeOn(elasticScheduler);
     }
 
     public static <T> Mono<T> asMonoDirect(Supplier<T> supplier) {
-        return Mono.defer(() -> Mono.justOrEmpty(supplier.get())).subscribeOn(Schedulers.boundedElastic());
+        return Mono.defer(() -> Mono.justOrEmpty(supplier.get())).subscribeOn(elasticScheduler);
     }
 
     public static <T> Flux<T> asFlux(Supplier<? extends Iterable<T>> supplier) {
-        return Mono.fromCallable(supplier::get)
-                .flatMapMany(Flux::fromIterable)
-                .subscribeOn(Schedulers.boundedElastic());
+        return Mono.fromCallable(supplier::get).flatMapMany(Flux::fromIterable).subscribeOn(elasticScheduler);
     }
 }
