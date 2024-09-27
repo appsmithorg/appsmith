5995e4292a3d8d3533c4d79a9cb1f82b06d20da6
Ankita Kinger <ankita@appsmith.com>
2024-09-27 19:48:05 +0530

    chore: Handling the updation of action name in the plugin action toolbar (#36560)
    ## Description
    
    Handling the updation of action name in the plugin action toolbar in the
    new modularised flow.
    
    Fixes [#36498](https://github.com/appsmithorg/appsmith/issues/36498)
    
    ## Automation
    
    /ok-to-test tags="@tag.All"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/11071023786>
    > Commit: 73647e50cfeb6919b30c674f8f3a3a219f6f98c8
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=11071023786&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.All`
    > Spec:
    > <hr>Fri, 27 Sep 2024 14:15:24 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new component for editing plugin action names, enhancing
    user experience in managing plugin actions.
    - Added optional icon size property to the editable text component for
    improved customization.
    - Enhanced the `CommonEditorForm` and `QueryEditorHeader` components to
    display plugin-specific information and saving status.
    
    - **Bug Fixes**
    - Streamlined action dispatching logic, improving reliability in saving
    actions.
    
    - **Documentation**
    - Updated interfaces and prop types for better clarity and type safety
    in the codebase.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/PluginActionEditor/index.ts
+++ app/client/src/PluginActionEditor/index.ts
@@ -6,3 +6,8 @@ export {
 export { default as PluginActionToolbar } from "./components/PluginActionToolbar";
 export { default as PluginActionForm } from "./components/PluginActionForm";
 export { default as PluginActionResponse } from "./components/PluginActionResponse";
+export type {
+  SaveActionNameParams,
+  PluginActionNameEditorProps,
+} from "./components/PluginActionNameEditor";
+export { default as PluginActionNameEditor } from "./components/PluginActionNameEditor";
