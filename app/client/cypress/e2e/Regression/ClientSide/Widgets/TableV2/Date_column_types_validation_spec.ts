2abc737be8f41b371b05206fe82c3da0798e3a67
Jacques Ikot <jacquesikot@gmail.com>
2024-09-27 12:50:26 +0100

    fix test case and move fixtures

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/Date_column_types_validation_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/Date_column_types_validation_spec.ts
@@ -1,3 +1,4 @@
+import { tableDateColumnTypes } from "../../../../../fixtures/tableDateColumnTypes";
 import {
   agHelper,
   entityExplorer,
@@ -8,7 +9,6 @@ import {
 import EditorNavigation, {
   EntityType,
 } from "../../../../../support/Pages/EditorNavigation";
-import { tableV2DateTestData } from "./fixtures";
 import { getFormattedTomorrowDates } from "./helpers";
 
 describe(
@@ -19,10 +19,11 @@ describe(
       entityExplorer.DragNDropWidget("tablewidgetv2", 350, 500);
       EditorNavigation.SelectEntityByName("Table1", EntityType.Widget);
       propPane.ToggleJSMode("Table data", true);
-      propPane.UpdatePropertyFieldValue("Table data", tableV2DateTestData);
+      propPane.UpdatePropertyFieldValue("Table data", tableDateColumnTypes);
+      table.EditColumn("unixs", "v2");
     });
 
-    afterEach(() => {
+    beforeEach(() => {
       propPane.NavigateBackToPropertyPane(false);
     });
 
@@ -57,111 +58,93 @@ describe(
     };
 
     it("1. should allow inline editing of Unix Timestamp in seconds (unix/s)", () => {
-      table.ChangeColumnType("unix/s", "Date");
+      table.ChangeColumnType("unixs", "Date");
       setEditableDateFormats("Epoch");
       clickAndValidateDateCell(0, 0);
     });
 
     it("2. should allow inline editing of Unix Timestamp in milliseconds (unix/ms)", () => {
-      table.ChangeColumnType("unix/ms", "Date");
+      table.ChangeColumnType("unixms", "Date");
       setEditableDateFormats("Milliseconds");
       clickAndValidateDateCell(0, 1);
     });
 
     it("3. should allow inline editing of date in YYYY-MM-DD format", () => {
-      table.ChangeColumnType("yyyy-mm-dd", "Date");
+      table.EditColumn("yyyymmdd", "v2");
       setEditableDateFormats("YYYY-MM-DD");
       clickAndValidateDateCell(0, 2);
     });
 
     it("4. should allow inline editing of date in YYYY-MM-DD HH:mm format", () => {
-      table.ChangeColumnType("yyyy-mm-dd hh:mm", "Date");
+      table.EditColumn("yyyymmddhhmm", "v2");
       setEditableDateFormats("YYYY-MM-DD HH:mm");
       clickAndValidateDateCell(0, 3);
     });
 
     it("5. should allow inline editing of date in ISO 8601 format (YYYY-MM-DDTHH:mm:ss)", () => {
-      table.ChangeColumnType("yyyy-mm-ddTHH:mm:ss", "Date");
+      table.EditColumn("iso8601", "v2");
       setEditableDateFormats("YYYY-MM-DDTHH:mm:ss");
       clickAndValidateDateCell(0, 4);
     });
 
-    it("6. should allow inline editing of date in YYYY-MM-DD HH:mm:ss format", () => {
-      table.ChangeColumnType("yyyy-mm-dd hh:mm:ss", "Date");
-      setEditableDateFormats("YYYY-MM-DD HH:mm:ss");
+    it("6. should allow inline editing of date in YYYY-MM-DD HH:mm format", () => {
+      table.EditColumn("yyyymmddTHHmmss", "v2");
+      setEditableDateFormats("YYYY-MM-DD HH:mm");
       clickAndValidateDateCell(0, 5);
     });
 
     it("7. should allow inline editing of date in 'do MMM yyyy' format", () => {
-      table.ChangeColumnType("do MMM yyyy", "Date");
-      setEditableDateFormats("Do MMM YYYY");
+      table.ChangeColumnType("yyyymmddhhmmss", "Date");
+      setEditableDateFormats("YYYY-MM-DDTHH:mm:ss");
       clickAndValidateDateCell(0, 6);
     });
 
     it("8. should allow inline editing of date in DD/MM/YYYY format", () => {
-      table.ChangeColumnType("dd/mm/yyyy", "Date");
-      setEditableDateFormats("DD/MM/YYYY");
+      table.ChangeColumnType("doMMMyyyy", "Date");
+      setEditableDateFormats("Do MMM YYYY");
       clickAndValidateDateCell(0, 7);
     });
 
     it("9. should allow inline editing of date in DD/MM/YYYY HH:mm format", () => {
-      table.ChangeColumnType("dd/mm/yyyy hh:mm", "Date");
-      setEditableDateFormats("DD/MM/YYYY HH:mm");
+      table.EditColumn("ddmmyyyy", "v2");
+      setEditableDateFormats("DD/MM/YYYY");
       clickAndValidateDateCell(0, 8);
     });
 
     it("10. should allow inline editing of date in LLL (Month Day, Year Time) format", () => {
-      table.ChangeColumnType("lll", "Date");
-      setEditableDateFormats("LLL");
+      table.EditColumn("ddmmyyyyhhmm", "v2");
+      setEditableDateFormats("DD/MM/YYYY HH:mm");
       clickAndValidateDateCell(0, 9);
     });
 
     it("11. should allow inline editing of date in LL (Month Day, Year) format", () => {
-      table.ChangeColumnType("ll", "Date");
-      setEditableDateFormats("LL");
+      table.EditColumn("lll", "v2");
+      setEditableDateFormats("LLL");
       clickAndValidateDateCell(0, 10);
     });
 
-    it("12. should allow inline editing of date in 'D MMMM, YYYY' format", () => {
-      table.ChangeColumnType("d mmmm, yyyy", "Date");
-      setEditableDateFormats("D MMMM, YYYY");
+    it.only("12. should allow inline editing of date in 'D MMMM, YYYY' format", () => {
+      table.EditColumn("ll", "v2");
+      setEditableDateFormats("LL");
       clickAndValidateDateCell(0, 11);
     });
 
-    it("13. should allow inline editing of date in 'h:mm A D MMMM, YYYY' format", () => {
-      table.ChangeColumnType("h:mm A d mmmm, yyyy", "Date");
-      setEditableDateFormats("h:mm A D MMMM, YYYY");
+    it.only("13. should allow inline editing of date in 'h:mm A D MMMM, YYYY' format", () => {
+      table.EditColumn("dmmmmyyyy", "v2");
+      setEditableDateFormats("D MMMM, YYYY");
       clickAndValidateDateCell(0, 12);
     });
 
-    it("14. should allow inline editing of date in MM-DD-YYYY format", () => {
-      table.ChangeColumnType("mm-dd-yyyy", "Date");
-      setEditableDateFormats("MM-DD-YYYY");
+    it.only("14. should allow inline editing of date in MM-DD-YYYY format", () => {
+      table.EditColumn("hmmAdmmmmyyyy", "v2");
+      setEditableDateFormats("H:mm A D MMMM, YYYY");
       clickAndValidateDateCell(0, 13);
     });
 
-    it("15. should allow inline editing of date in DD-MM-YYYY format", () => {
-      table.ChangeColumnType("dd-mm-yyyy", "Date");
-      setEditableDateFormats("DD-MM-YYYY");
+    it.only("15. should allow inline editing of date in DD-MM-YYYY format", () => {
+      table.EditColumn("mm1dd1yyyy", "v2");
+      setEditableDateFormats("MM-DD-YYYY");
       clickAndValidateDateCell(0, 14);
-    });
-
-    it("16. should allow inline editing of date in MM/DD/YYYY format", () => {
-      table.ChangeColumnType("mm/dd/yyyy", "Date");
-      setEditableDateFormats("MM/DD/YYYY");
-      clickAndValidateDateCell(0, 15);
-    });
-
-    it("17. should allow inline editing of date in DD/MM/YY format", () => {
-      table.ChangeColumnType("dd/mm/yy", "Date");
-      setEditableDateFormats("DD/MM/YY");
-      clickAndValidateDateCell(0, 16);
-    });
-
-    it("18. should allow inline editing of date in MM/DD/YY format", () => {
-      table.ChangeColumnType("mm/dd/yy", "Date");
-      setEditableDateFormats("MM/DD/YY");
-      clickAndValidateDateCell(0, 17);
     });
   },
 );
