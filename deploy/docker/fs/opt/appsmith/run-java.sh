14a0f35ae70e45f842608ba34096665471244860
Anagh Hegde <anagh@appsmith.com>
2024-09-30 21:01:37 +0530

    refactor

--- deploy/docker/fs/opt/appsmith/run-java.sh
+++ deploy/docker/fs/opt/appsmith/run-java.sh
@@ -36,7 +36,7 @@ match-proxy-url() {
 if [[ "$mode" == "pg" ]]; then
   extract_postgres_db_params "$APPSMITH_DB_URL"
   waitForPostgresAvailability
-  sh /opt/appsmith/pg-default-schema.sh &
+  init_pg_db
 fi
 
 if match-proxy-url "${HTTP_PROXY-}"; then
