733f523833bab561f0152374452927c7a36b780e
Pawan Kumar <pawankumar@Pawans-MacBook-Pro-2.local>
2024-09-30 16:35:39 +0530

    update validate helper

--- app/client/src/modules/ui-builder/ui/wds/WDSDatePickerWidget/widget/helpers.ts
+++ app/client/src/modules/ui-builder/ui/wds/WDSDatePickerWidget/widget/helpers.ts
@@ -1,7 +1,7 @@
 import type { WDSDatePickerWidgetProps } from "./types";
 
 export function validateInput(props: WDSDatePickerWidgetProps) {
-  if (!props.isValid) {
+  if (props.isValid === false) {
     return {
       validationStatus: "invalid",
       errorMessage: "Please select a valid date",
