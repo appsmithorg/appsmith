733ba5458ba4d9991a7fca02df648504cc7fe82c
Ilia Znamenskii <ilia@appsmith.com>
2024-10-02 10:18:55 +0200

    add assistantName property

--- app/client/src/modules/ui-builder/ui/wds/WDSAIChatWidget/widget/config/propertyPaneContent.ts
+++ app/client/src/modules/ui-builder/ui/wds/WDSAIChatWidget/widget/config/propertyPaneContent.ts
@@ -5,6 +5,25 @@ export const propertyPaneContent = [
     sectionName: "General",
     children: [
       {
+        helpText: "Gives the open AI Assistant a name to be displayed in chat",
+        propertyName: "assistantName",
+        label: "Assistant Name",
+        controlType: "INPUT_TEXT",
+        isJSConvertible: false,
+        isBindProperty: false,
+        isTriggerProperty: false,
+        defaultValue: "",
+      },
+      {
+        propertyName: "senderName",
+        label: "Sender Name",
+        controlType: "INPUT_TEXT",
+        isJSConvertible: false,
+        isBindProperty: false,
+        isTriggerProperty: false,
+        defaultValue: "",
+      },
+      {
         helpText: "Controls the visibility of the widget",
         propertyName: "isVisible",
         label: "Visible",
