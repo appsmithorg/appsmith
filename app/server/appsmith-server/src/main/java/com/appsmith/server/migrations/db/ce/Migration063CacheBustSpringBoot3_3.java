ab05f09622ab90e9880fce9c0371443b2ce47f81
Nidhi Nair <nidhi@appsmith.com>
2024-10-04 13:24:15 +0530

    fix: Modified session clear migration to work with client APIs

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration063CacheBustSpringBoot3_3.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration063CacheBustSpringBoot3_3.java
@@ -3,32 +3,31 @@ package com.appsmith.server.migrations.db.ce;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ScanOptions;
 import reactor.core.publisher.Mono;
 
-@RequiredArgsConstructor
 @Slf4j
 @ChangeUnit(order = "063", id = "reset_session_oauth2_spring_3_3")
 public class Migration063CacheBustSpringBoot3_3 {
-
-    private final ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
 
     @RollbackExecution
     public void rollbackExecution() {}
 
     @Execution
-    public void execute() {
+    public void execute(
+            @Qualifier("reactiveRedisTemplate") final ReactiveRedisTemplate<String, Object> reactiveRedisTemplate) {
         final String authorizedClientsKey =
                 "sessionAttr:org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository.AUTHORIZED_CLIENTS";
 
-        deleteKeysAcrossCluster("spring:session:sessions:*", authorizedClientsKey)
+        deleteKeysAcrossCluster(reactiveRedisTemplate, "spring:session:sessions:*", authorizedClientsKey)
                 .block();
     }
 
-    public Mono<Void> deleteKeysAcrossCluster(String pattern, String hashField) {
+    public Mono<Void> deleteKeysAcrossCluster(
+            ReactiveRedisTemplate<String, Object> reactiveRedisTemplate, String pattern, String hashField) {
         // Flux to scan all keys matching the pattern
         return reactiveRedisTemplate
                 .scan(ScanOptions.scanOptions().match(pattern).build())
