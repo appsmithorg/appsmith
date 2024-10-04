42bc1fcaebaae11f97286b74633c91f751e47da7
Nidhi Nair <nidhi@appsmith.com>
2024-10-04 13:25:45 +0530

    fix: Modified session clear migration to work with client APIs

--- app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration063CacheBustSpringBoot3_3.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/migrations/db/ce/Migration063CacheBustSpringBoot3_3.java
@@ -3,22 +3,20 @@ package com.appsmith.server.migrations.db.ce;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 
-@RequiredArgsConstructor
 @Slf4j
 @ChangeUnit(order = "063", id = "reset_session_oauth2_spring_3_3")
 public class Migration063CacheBustSpringBoot3_3 {
-
-    private final ReactiveRedisTemplate<String, Object> reactiveRedisTemplate;
 
     @RollbackExecution
     public void rollbackExecution() {}
 
     @Execution
-    public void execute() {
+    public void execute(
+            @Qualifier("reactiveRedisTemplate") final ReactiveRedisTemplate<String, Object> reactiveRedisTemplate) {
         reactiveRedisTemplate
                 .getConnectionFactory()
                 .getReactiveConnection()
