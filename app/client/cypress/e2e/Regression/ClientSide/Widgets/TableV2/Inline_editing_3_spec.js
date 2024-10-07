78c8bf310c7c35c7a8107ebb644c2841464f84d8
Rahul Barwal <rahul.barwal@appsmith.com>
2024-10-07 11:40:04 +0530

    Fix inline editing test case to verify updated cell value

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/Inline_editing_3_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/Inline_editing_3_spec.js
@@ -149,13 +149,15 @@ describe(
       cy.enterTableCellValue(0, 0, "newValue");
       cy.saveTableCellValue(0, 0);
       agHelper.Sleep();
+
+      const exected = JSON.parse(
+        `[  {    "index": 0,    "updatedFields": {      "step": "newValue"    },    "allFields": {      "step": "newValue",      "task": "Drop a table",      "status": "✅"    }  }]`,
+      );
       agHelper
         .GetText(".t--widget-textwidget .bp3-ui-text", "text")
-        .then((text) => {
+        .should((text) => {
           text = JSON.parse(text);
-          const exected = JSON.parse(
-            `[  {    "index": 0,    "updatedFields": {      "step": "newValue"    },    "allFields": {      "step": "newValue",      "task": "Drop a table",      "status": "✅"    }  }]`,
-          );
+
           expect(text).to.deep.equal(exected);
         });
       cy.openPropertyPane("textwidget");
