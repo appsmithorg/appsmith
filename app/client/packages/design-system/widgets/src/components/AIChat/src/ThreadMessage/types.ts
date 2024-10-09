4adab522c0e126a494fac9951f4fed9d7a84c47c
Ilia Znamenskii <ilia@appsmith.com>
2024-10-09 09:42:31 +0200

    remove ai chat widget

--- app/client/packages/design-system/widgets/src/components/AIChat/src/ThreadMessage/types.ts
+++
@@ -1,7 +0,0 @@
-import type { HTMLProps } from "react";
-
-export interface ThreadMessageProps extends HTMLProps<HTMLLIElement> {
-  content: string;
-  isAssistant: boolean;
-  username: string;
-}
