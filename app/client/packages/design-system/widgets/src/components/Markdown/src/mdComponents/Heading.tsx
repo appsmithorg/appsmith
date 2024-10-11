211357f693c132a0035d0d8125922bf84789f39d
Pawan Kumar <pawan@appsmith.com>
2024-10-11 15:57:41 +0530

    bug fixes reported by vadim

--- app/client/packages/design-system/widgets/src/components/Markdown/src/mdComponents/Heading.tsx
+++ app/client/packages/design-system/widgets/src/components/Markdown/src/mdComponents/Heading.tsx
@@ -10,12 +10,13 @@ type HeadingProps = React.ClassAttributes<HTMLDivElement> &
 const createHeading = (
   size: TextProps["size"],
   level: 1 | 2 | 3 | 4 | 5 | 6,
+  fontWeight: TextProps["fontWeight"] = 700,
 ) => {
   const HeadingComponent = ({ children, ref }: HeadingProps) => (
     <Text
       color="neutral"
       data-component={`h${level}`}
-      fontWeight={700}
+      fontWeight={fontWeight}
       ref={ref as Ref<HTMLDivElement>}
       size={size}
     >
@@ -32,5 +33,5 @@ export const h1 = createHeading("heading", 1);
 export const h2 = createHeading("title", 2);
 export const h3 = createHeading("subtitle", 3);
 export const h4 = createHeading("body", 4);
-export const h5 = createHeading("caption", 5);
-export const h6 = createHeading("footnote", 6);
+export const h5 = createHeading("body", 5, 500);
+export const h6 = createHeading("body", 6, 300);
--- app/client/test/__mocks__/reactMarkdown.tsx
+++ app/client/test/__mocks__/reactMarkdown.tsx
@@ -3,6 +3,3 @@ import React from "react";
 jest.mock("react-markdown", () => (props: { children: unknown }) => {
   return <>{props.children}</>;
 });
-
-jest.mock("remark-gfm", () => () => {
-})
