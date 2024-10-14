3038891bcbf66e1e3f2824f9192292ac66c8756c
Rudraprasad Das <rudra@appsmith.com>
2024-10-14 16:51:16 +0800

    fix: shifting app creation to a new block

--- app/client/cypress/e2e/Regression/ClientSide/Git/GitPersistBranch_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Git/GitPersistBranch_spec.ts
@@ -15,27 +15,30 @@ describe(
     tags: ["@tag.Git", "@tag.GitPersistBranch"],
   },
   function () {
-    it("Check if branch persist after changing branch and exiting the app", function () {
-      featureFlagIntercept({ release_git_persist_branch_enabled: true }, true);
+    before(() => {
       agHelper.GenerateUUID();
       cy.get("@guid").then((uid) => {
         wsName = "GitPB-" + uid;
-        appName = "GitBranchProtect-2" + uid;
+        appName = "GitPB1-" + uid;
         homePage.CreateNewWorkspace(wsName, true);
         homePage.CreateAppInWorkspace(wsName, appName);
         gitSync.CreateNConnectToGit("test-git-perssit-branch", true, true);
-
         cy.get("@gitRepoName").then((resRepoName) => {
           repoName = resRepoName.toString();
-          gitSync.CreateGitBranch("b1", false);
-          cy.get("@gitbranchName").then((resBranchName) => {
-            const branchName = resBranchName.toString();
-            homePage.NavigateToHome();
-            homePage.EditAppFromAppHover(appName);
-            gitSync.AssertBranchName(branchName);
-            gitSync.AssertBranchNameInUrl(branchName);
-          });
+          homePage.NavigateToHome();
         });
+      });
+    });
+    it("Check if branch persist after changing branch and exiting the app", function () {
+      featureFlagIntercept({ release_git_persist_branch_enabled: true }, true);
+      homePage.EditAppFromAppHover(appName);
+      gitSync.CreateGitBranch("b1", false);
+      cy.get("@gitbranchName").then((resBranchName) => {
+        const branchName = resBranchName.toString();
+        homePage.NavigateToHome();
+        homePage.EditAppFromAppHover(appName);
+        gitSync.AssertBranchName(branchName);
+        gitSync.AssertBranchNameInUrl(branchName);
       });
     });
   },
