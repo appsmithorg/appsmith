098b318bf5eafb82417acfee742d8dfe437e352a
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-10-18 18:30:33 +0530

    Sync changes from EE excluding enterprise directory

--- app/client/src/PluginActionEditor/hooks/index.ts
+++ app/client/src/PluginActionEditor/hooks/index.ts
@@ -1,6 +1,6 @@
 export { useActionSettingsConfig } from "ee/PluginActionEditor/hooks/useActionSettingsConfig";
 export { useHandleDeleteClick } from "ee/PluginActionEditor/hooks/useHandleDeleteClick";
-export { useHandleDuplicateClick } from "ee/PluginActionEditor/hooks/useHandleDuplicateClick";
+export { useHandleDuplicateClick } from "./useHandleDuplicateClick";
 export { useHandleRunClick } from "ee/PluginActionEditor/hooks/useHandleRunClick";
 export { useBlockExecution } from "ee/PluginActionEditor/hooks/useBlockExecution";
 export { useAnalyticsOnRunClick } from "ee/PluginActionEditor/hooks/useAnalyticsOnRunClick";
