2cf89440319bcb8cee1afaffeb798785eacaf340
Abhijeet <abhi.nagarnaik@gmail.com>
2024-10-22 09:26:22 +0530

    chore: Keep NoArgsConstructor for RecentlyUsedEntityDTO

--- app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/RecentlyUsedEntityDTO.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/dtos/RecentlyUsedEntityDTO.java
@@ -2,11 +2,13 @@ package com.appsmith.server.dtos;
 
 import com.appsmith.server.dtos.ce.RecentlyUsedEntityCE_DTO;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldNameConstants;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @FieldNameConstants
 public class RecentlyUsedEntityDTO extends RecentlyUsedEntityCE_DTO {
     public static class Fields extends RecentlyUsedEntityCE_DTO.Fields {}
