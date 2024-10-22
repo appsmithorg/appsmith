67046f869320fd0a300046019481d585ee1c975d
Ashit Rath <ashit@appsmith.com>
2024-10-22 10:48:03 +0530

    chore: Split library side pane for adding package control section (#36926)
    ## Description
    Purpose of this PR is to split the Libraries section to extend it in EE
    and introduced a tab layout that has both js libraries and packages
    Changes:
    1. Increased width of Libraries pane to 384px
    2. Added a route `/package` which opens the same as library 
    3. Extracted the JSLibraries section to be reused as a tab panel body in
    EE
    4. Created `SidePaneWrapper` common component to be reused in EE
    5. Split the title of the Libraries section under a hook to return empty
    string if package feature flag is enabled.
    6. Minor linting refactors.
    
    In CE there is only one visual change i.e the increase in width of the
    Libraries side pane from 250px to 384px
    
    
    [Figma](https://www.figma.com/design/Z0QsSdGOydURn6WIMQ3rHM/Appsmith-Reusability?node-id=6816-319511&node-type=frame&t=x0DzN9HIxjefe3J1-0)
    
    PR for https://github.com/appsmithorg/appsmith-ee/pull/5362
    ## Automation
    
    /ok-to-test tags="@tag.All"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/11434291701>
    > Commit: ec76cc569ea90ba55e451f5ab18fa85244ac076f
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=11434291701&attempt=3"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.All`
    > Spec:
    > <hr>Mon, 21 Oct 2024 08:54:31 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Introduced new functions for constructing URLs for application
    libraries and packages.
    - Added new route constants and matching functions for improved routing
    of application packages and libraries.
    - Implemented a custom hook for dynamic header titles in the libraries
    section.
    - Added a new `LibrarySidePane` component to enhance the editor
    interface.
    - Introduced a new `SidePaneWrapper` component for better layout
    management.
    
    - **Bug Fixes**
    - Enhanced entity identification logic to recognize both libraries and
    packages.
    
    - **Chores**
      - Updated routing logic for improved maintainability.
      - Added constants for configurable UI component widths.
    
    These updates aim to enhance user experience and improve the overall
    functionality of the application.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ app/client/src/IDE/Components/SidePaneWrapper.tsx
@@ -0,0 +1,29 @@
+import { Flex } from "@appsmith/ads";
+import React from "react";
+import type { ReactNode } from "react";
+import styled from "styled-components";
+
+interface SidePaneContainerProps {
+  children?: ReactNode;
+  padded?: boolean;
+}
+
+const StyledContainer = styled(Flex)<Pick<SidePaneContainerProps, "padded">>`
+  padding: ${({ padded }) => padded && "var(--ads-v2-spaces-2)"};
+`;
+
+function SidePaneWrapper({ children, padded = false }: SidePaneContainerProps) {
+  return (
+    <StyledContainer
+      borderRight="1px solid var(--ads-v2-color-border)"
+      flexDirection="column"
+      height="100%"
+      padded={padded}
+      width={"100%"}
+    >
+      {children}
+    </StyledContainer>
+  );
+}
+
+export default SidePaneWrapper;
