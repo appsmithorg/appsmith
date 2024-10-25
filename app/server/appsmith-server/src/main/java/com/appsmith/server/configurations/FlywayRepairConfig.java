99e500a387b2c4af66a6d522d54852149610ac99
Anagh Hegde <anagh@appsmith.com>
2024-10-25 17:26:54 +0530

    run flyway migration at startup

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/FlywayRepairConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/FlywayRepairConfig.java
@@ -2,6 +2,7 @@ package com.appsmith.server.configurations;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,17 +19,24 @@ public class FlywayRepairConfig {
 
     @Bean
     public Flyway flyway() {
-        Flyway flyway = Flyway.configure()
+        return Flyway.configure()
                 .dataSource(
                         dataSourceProperties.getUrl(),
                         dataSourceProperties.getUsername(),
                         dataSourceProperties.getPassword())
-                .locations("classpath:com/appsmith/server/migrations")
+                .locations("classpath:db/migration") // Adjust path if needed
                 .baselineOnMigrate(true)
                 .baselineVersion("0")
                 .load();
-        flyway.repair(); // Optionally repair the schema history if needed
-        flyway.migrate();
-        return flyway;
+    }
+
+    // ApplicationRunner bean to run migrations at startup
+    // TODO - Remove this once the pg and release branch is merged
+    @Bean
+    public ApplicationRunner flywayMigrationRunner(Flyway flyway) {
+        return args -> {
+            flyway.repair(); // Fixes any inconsistencies in the schema history
+            flyway.migrate();
+        };
     }
 }
