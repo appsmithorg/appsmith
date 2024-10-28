64f1342a404b761c31d0182884a8e8b16e25db07
Jacques Ikot <jacquesikot@gmail.com>
2024-10-28 18:42:06 +0100

    feat: remove release_table_cell_label_value_enabled feature flag (#37110)
    ## Description
    Removes the `release_table_cell_label_value_enabled` feature flag from
    the codebase.
    
    This feature flag helped us launch the tables ability to show labels
    instead of values for select column types. It has been in production for
    over 2 months with no bug or unexpected behaviours reported, warranting
    a removal of the feature flag from the codebase.
    
    
    Fixes #37109 
    
    ## Automation
    
    /ok-to-test tags="@tag.Table, @tag.Sanity, @tag.Select"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/11550654098>
    > Commit: d225470d767c441c1b9e07d79e59e5f052339198
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=11550654098&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Table, @tag.Sanity, @tag.Select`
    > Spec:
    > <hr>Mon, 28 Oct 2024 09:57:21 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced the `SelectCell` component for improved label value
    determination.
      
    - **Bug Fixes**
    - Removed unnecessary complexity related to feature flag checks,
    resulting in a more reliable display of cell labels.
      
    - **Tests**
    - Updated test cases for table filtering and sorting functionalities,
    removing feature flag dependencies while maintaining core logic.
    - Enhanced test coverage for select column functionality, including
    dynamic bindings and server-side filtering checks.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/ce/entities/FeatureFlag.ts
+++ app/client/src/ce/entities/FeatureFlag.ts
@@ -32,8 +32,6 @@ export const FEATURE_FLAG = {
     "rollout_remove_feature_walkthrough_enabled",
   release_drag_drop_building_blocks_enabled:
     "release_drag_drop_building_blocks_enabled",
-  release_table_cell_label_value_enabled:
-    "release_table_cell_label_value_enabled",
   rollout_side_by_side_enabled: "rollout_side_by_side_enabled",
   release_layout_conversion_enabled: "release_layout_conversion_enabled",
   release_anvil_toggle_enabled: "release_anvil_toggle_enabled",
@@ -63,7 +61,6 @@ export const DEFAULT_FEATURE_FLAG_VALUE: FeatureFlags = {
   license_gac_enabled: false,
   release_anvil_enabled: false,
   release_drag_drop_building_blocks_enabled: false,
-  release_table_cell_label_value_enabled: false,
   license_git_branch_protection_enabled: false,
   license_git_continuous_delivery_enabled: false,
   license_widget_rtl_support_enabled: false,
