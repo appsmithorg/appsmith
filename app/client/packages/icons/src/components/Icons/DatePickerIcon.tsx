5da5a1e5077db5fddba6a6f783802f8be75dd6f0
Valera Melnikov <valera@appsmith.com>
2024-10-28 14:55:42 +0300

    chore: chore: add SVGR to appsmith-icon package, add processing for custom icons, update date-fns (#37115)
    ## Description
    - chore: add SVGR to appsmith-icon package
    - add processing for custom icons
    - update date-fns
    
    Changes are necessary for [this EE
    PR](https://github.com/appsmithorg/appsmith-ee/pull/5448).
    
    ## Automation
    
    /ok-to-test tags="@tag.Sanity"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/11551127644>
    > Commit: 44fd968044ad5a5ca173c565b3b0c1d6f030cd1e
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=11551127644&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Sanity`
    > Spec:
    > <hr>Mon, 28 Oct 2024 10:11:55 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [x] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Introduced `AIAvatarCustomIcon` component for enhanced icon
    customization.
    - Added `CustomIcons` documentation in Storybook for better visibility
    of custom icons.
    
    - **Improvements**
    - Updated multiple icon components to accept SVG properties, enhancing
    flexibility and reusability.
    - Enhanced type safety for `IconCmp` and `ThumbnailCmp` properties
    across various interfaces.
    - Expanded functionality in the `generateStories` function to include
    stories for custom icons.
    
    - **Chores**
    - Improved import statements and export structures for better code
    organization.
    - Extended cleanup functionality to ensure directories are properly
    managed.
    - Modified the `generateComponents` function to utilize a more robust
    SVG-to-React transformation process.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/packages/icons/src/components/Icons/DatePickerIcon.tsx
+++ app/client/packages/icons/src/components/Icons/DatePickerIcon.tsx
@@ -1,2 +1,4 @@
-import React from "react";
-export const DatePickerIcon = () => <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"><path stroke="#000" stroke-linecap="round" stroke-linejoin="round" d="M12.5 1.5h-10a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-10a1 1 0 0 0-1-1M9.5 5.5v8M5.5 5.5v8M1.5 5.5h12M1.5 9.5h12M4.5 3.5v-3m6 3v-3"/></svg>;
+import * as React from "react";
+import type { SVGProps } from "react";
+const DatePickerIcon = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="none" {...props}><path stroke="#000" strokeLinecap="round" strokeLinejoin="round" d="M12.5 1.5h-10a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-10a1 1 0 0 0-1-1M9.5 5.5v8M5.5 5.5v8M1.5 5.5h12M1.5 9.5h12M4.5 3.5v-3m6 3v-3" /></svg>;
+export { DatePickerIcon };
