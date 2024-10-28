44fd968044ad5a5ca173c565b3b0c1d6f030cd1e
Valera Melnikov <valera@appsmith.com>
2024-10-28 11:43:57 +0300

    chore: add SVGR to appsmith-icon package, add processing for custom icons, update date-fns

--- app/client/packages/icons/src/components/Thumbnails/RadioGroupThumbnail.tsx
+++ app/client/packages/icons/src/components/Thumbnails/RadioGroupThumbnail.tsx
@@ -1,2 +1,4 @@
-import React from "react";
-export const RadioGroupThumbnail = () => <svg xmlns="http://www.w3.org/2000/svg" width="72" height="76" fill="none"><circle cx="22" cy="29" r="6.5" fill="#FFBFA1" stroke="#CC3D00"/><circle cx="22" cy="29" r="3.5" fill="#fff" stroke="#CC3D00"/><rect width="20" height="8" x="34" y="25" fill="#CDD5DF" rx="1"/><rect width="11" height="11" x="16.5" y="41.5" fill="#fff" stroke="#CDD5DF" rx="5.5"/><rect width="22" height="8" x="34" y="43" fill="#CDD5DF" rx="1"/></svg>;
+import * as React from "react";
+import type { SVGProps } from "react";
+const RadioGroupThumbnail = (props: SVGProps<SVGSVGElement>) => <svg xmlns="http://www.w3.org/2000/svg" width={72} height={76} fill="none" {...props}><circle cx={22} cy={29} r={6.5} fill="#FFBFA1" stroke="#CC3D00" /><circle cx={22} cy={29} r={3.5} fill="#fff" stroke="#CC3D00" /><rect width={20} height={8} x={34} y={25} fill="#CDD5DF" rx={1} /><rect width={11} height={11} x={16.5} y={41.5} fill="#fff" stroke="#CDD5DF" rx={5.5} /><rect width={22} height={8} x={34} y={43} fill="#CDD5DF" rx={1} /></svg>;
+export { RadioGroupThumbnail };
