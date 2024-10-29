664e702512fd859d5df3c8d1b0b5d98d81497a95
Anagh Hegde <anagh@appsmith.com>
2024-10-29 15:24:25 +0530

    make elastic threadpool configurable

--- app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ReactorUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/helpers/ReactorUtils.java
@@ -13,6 +13,10 @@ public class ReactorUtils {
 
     private static final String ELASTIC_THREAD_POOL_NAME = "appsmith-db-elastic-pool";
 
+    private static final int maxThreadCount = System.getenv("APPSMITH_DB_ELASTIC_THREAD_MAX_VALUE") != null
+            ? Integer.parseInt(System.getenv("APPSMITH_DB_ELASTIC_THREAD_MAX_VALUE"))
+            : 300;
+
     /*
      * This scheduler is used to run for all the db ops
      * The number of threads is set to 300, and the queue size is set to the default value
@@ -20,8 +24,8 @@ public class ReactorUtils {
      * to support the cache evictions flow used in PermissionGroupCEImpl class
      * TODO : We need to revisit this number and see if we can reduce it by modifying the cache eviction flow to accept List of users
      */
-    public static final Scheduler elasticScheduler =
-            Schedulers.newBoundedElastic(300, Schedulers.DEFAULT_BOUNDED_ELASTIC_QUEUESIZE, ELASTIC_THREAD_POOL_NAME);
+    public static final Scheduler elasticScheduler = Schedulers.newBoundedElastic(
+            maxThreadCount, Schedulers.DEFAULT_BOUNDED_ELASTIC_QUEUESIZE, ELASTIC_THREAD_POOL_NAME);
 
     public static <T> Mono<T> asMono(Supplier<Optional<T>> supplier) {
         return Mono.defer(() -> Mono.justOrEmpty(supplier.get())).subscribeOn(elasticScheduler);
