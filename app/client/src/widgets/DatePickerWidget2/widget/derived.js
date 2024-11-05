d47896f50180232d6c434e5a4b06637ea4499afe
Rahul Barwal <rahul.barwal@appsmith.com>
2024-11-05 15:18:12 +0530

    chore: Refactor date validation in DatePickerWidget2 for improved readability and early returns on optional selections

--- app/client/src/widgets/DatePickerWidget2/widget/derived.js
+++ app/client/src/widgets/DatePickerWidget2/widget/derived.js
@@ -1,53 +1,62 @@
 /* eslint-disable @typescript-eslint/no-unused-vars*/
 export default {
   isValidDate: (props, moment, _) => {
-    const minDate = new Date(props.minDate);
-    const maxDate = new Date(props.maxDate);
-    const selectedDate =
-      props.selectedDate !== ""
-        ? moment(new Date(props.selectedDate))
-        : props.selectedDate;
-    let dateValid = true;
-
-    if (!!props.minDate && !!props.maxDate) {
-      if (!selectedDate) {
-        dateValid = !props.isRequired;
-      } else {
-        let granularityToCheck = undefined;
-        let inclusivityMarkers = undefined;
-
-        switch (props.timePrecision) {
-          case "None":
-            granularityToCheck = "day";
-            inclusivityMarkers = "[]";
-            break;
-          case "second":
-          case "minute":
-          case "millisecond":
-            granularityToCheck = props.timePrecision;
-            inclusivityMarkers = "[]";
-            break;
-        }
-        dateValid = selectedDate.isBetween(
-          minDate,
-          maxDate,
-          granularityToCheck,
-          inclusivityMarkers,
-        );
-      }
-    } else if (!!props.minDate) {
-      dateValid = !!selectedDate
-        ? selectedDate.isAfter(minDate)
-        : !props.isRequired;
-    } else if (!!props.maxDate) {
-      dateValid = !!selectedDate
-        ? selectedDate.isBefore(maxDate)
-        : !props.isRequired;
-    } else {
-      dateValid = props.isRequired ? !!selectedDate : true;
+    // Early return if no date is selected and it's not required
+    if (!props.selectedDate && !props.isRequired) {
+      return true;
     }
 
-    return dateValid;
+    // Parse dates
+    const minDate = props.minDate ? new Date(props.minDate) : null;
+    const maxDate = props.maxDate ? new Date(props.maxDate) : null;
+    const selectedDate = props.selectedDate
+      ? moment(new Date(props.selectedDate))
+      : null;
+
+    // Handle no selected date case
+    if (!selectedDate) {
+      return !props.isRequired;
+    }
+
+    // Get comparison settings based on time precision
+    const { granularity, inclusivity } = getDateComparisonSettings(
+      props.timePrecision,
+    );
+
+    // Check date range constraints
+    if (minDate && maxDate) {
+      return selectedDate.isBetween(minDate, maxDate, granularity, inclusivity);
+    }
+
+    if (minDate) {
+      return selectedDate.isAfter(minDate, granularity, inclusivity);
+    }
+
+    if (maxDate) {
+      return selectedDate.isBefore(maxDate, granularity, inclusivity);
+    }
+
+    return true;
   },
   //
 };
+
+function getDateComparisonSettings(timePrecision) {
+  const settings = {
+    granularity: undefined,
+    inclusivity: "[]", // Include both bounds by default
+  };
+
+  switch (timePrecision) {
+    case "None":
+      settings.granularity = "day";
+      break;
+    case "second":
+    case "minute":
+    case "millisecond":
+      settings.granularity = timePrecision;
+      break;
+  }
+
+  return settings;
+}
