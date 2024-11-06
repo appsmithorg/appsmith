bc5ca39e13acbe93c5eaa7d45573b6a84cb25cc5
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-11-06 20:28:10 +0530

    feat: poc for center align and toolbar shadow

--- app/client/src/IDE/Structure/Toolbar.tsx
+++ app/client/src/IDE/Structure/Toolbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Flex } from "@appsmith/ads";
 
 interface ToolbarProps {
@@ -6,14 +6,43 @@ interface ToolbarProps {
 }
 
 const Toolbar = (props: ToolbarProps) => {
+  useEffect(function detectScrollbar() {
+    const ele = document.getElementById("uqi-editor-form-content");
+    const toolbar = document.getElementById("action-toolbar");
+
+    const handleScroll = function () {
+      if (ele && ele.scrollTop > 0) {
+        toolbar?.style.setProperty(
+          "box-shadow",
+          "0 4px 6px rgba(0, 0, 0, 0.1)",
+        );
+      } else {
+        toolbar?.style.setProperty("box-shadow", "none");
+      }
+    };
+
+    if (ele) {
+      ele.addEventListener("scroll", handleScroll);
+    }
+
+    return function cleanup() {
+      ele?.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <Flex
       alignItems="center"
-      borderBottom="1px solid var(--ads-v2-color-border-muted);"
       flexDirection="row"
       height="32px"
+      id="action-toolbar"
       justifyContent="space-between"
       padding="spaces-2"
+      style={{
+        transition: "box-shadow 0.3s ease",
+        position: "sticky",
+        top: 0,
+      }}
     >
       {props.children}
     </Flex>
