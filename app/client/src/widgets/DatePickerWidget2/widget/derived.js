53975a0ec123a2bed58b7a2fa8c928e9d19968c3
Rahul Barwal <rahul.barwal@appsmith.com>
2024-11-06 16:25:31 +0530

    chore: Enhance date validation in DatePickerWidget2 by refining isValidDate logic and adjusting timePrecision comparisons

--- app/client/src/widgets/DatePickerWidget2/widget/derived.js
+++ app/client/src/widgets/DatePickerWidget2/widget/derived.js
@@ -1,49 +1,55 @@
+/* eslint-disable @typescript-eslint/no-unused-vars*/
 export default {
-  isValidDate: (props, moment) => {
-    // Early return if no date is selected and it's not required
+  isValidDate: (props, moment, _) => {
     if (!props.selectedDate && !props.isRequired) {
       return true;
     }
 
-    // Parse dates
     const minDate = props.minDate ? new Date(props.minDate) : null;
     const maxDate = props.maxDate ? new Date(props.maxDate) : null;
     const selectedDate = props.selectedDate
       ? moment(new Date(props.selectedDate))
-      : null;
+      : props.selectedDate;
 
-    // Handle no selected date case
     if (!selectedDate) {
       return !props.isRequired;
     }
 
-    // Set comparison settings based on time precision
-    let granularity, inclusivity;
+    let granularity,
+      inclusivity = "[]";
 
     switch (props.timePrecision) {
       case "None":
         granularity = "day";
-        inclusivity = "[]";
         break;
       case "second":
       case "minute":
       case "millisecond":
         granularity = props.timePrecision;
-        inclusivity = "[]";
         break;
+      default:
+        granularity = undefined;
+        inclusivity = undefined;
     }
 
-    // Check date range constraints
     if (minDate && maxDate) {
       return selectedDate.isBetween(minDate, maxDate, granularity, inclusivity);
     }
 
     if (minDate) {
-      return selectedDate.isAfter(minDate);
+      if (props.timePrecision === "None") {
+        return selectedDate.isSameOrAfter(minDate, granularity);
+      }
+
+      return selectedDate.isSame(minDate, granularity);
     }
 
     if (maxDate) {
-      return selectedDate.isBefore(maxDate);
+      if (props.timePrecision === "None") {
+        return selectedDate.isSameOrBefore(maxDate, granularity);
+      }
+
+      return selectedDate.isBefore(maxDate, granularity);
     }
 
     return true;
