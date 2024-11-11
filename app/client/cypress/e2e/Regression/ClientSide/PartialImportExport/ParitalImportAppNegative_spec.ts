d6def97d40727aa514aa47cd083c76bc38154078
“NandanAnantharamu” <nandan@thinkify.io>
2024-11-11 23:06:43 +0530

    test: added test for incompatible file

---
+++ app/client/cypress/e2e/Regression/ClientSide/PartialImportExport/ParitalImportAppNegative_spec.ts
@@ -0,0 +1,20 @@
+import {
+  homePage,
+  partialImportExport,
+  agHelper
+} from "../../../../support/Objects/ObjectsCore";
+
+describe(
+  "Partial import App",
+  { tags: ["@tag.ImportExport", "@tag.Git"] },
+  () => {
+    beforeEach(() => {
+      partialImportExport.OpenImportModal();
+    });
+
+    it("1. Verify Importing App into Page shows error message for incompatible json file ", () => {
+      homePage.ImportApp("PartialImportAppNegative.json", "", true);
+      agHelper.ValidateToastMessage("Unable to import artifact in workspace The file is not compatible with the current partial import operation. Please check the file and try again.. {1}")
+    });
+  },
+);
