6217fc9b107329bd7b8469f8d17f5f4f0c8135df
“NandanAnantharamu” <nandan@thinkify.io>
2024-11-13 08:06:01 +0530

    test: updated the spec to check if the API is visible or not

--- app/client/cypress/e2e/Sanity/Datasources/RestApiDatasource_spec.js
+++ app/client/cypress/e2e/Sanity/Datasources/RestApiDatasource_spec.js
@@ -13,13 +13,24 @@ describe(
   function () {
     it("1. Create a rest datasource + Bug 14566", function () {
       apiPage.CreateAndFillApi(testdata.baseUrl + testdata.methods);
-      cy.get(".t--store-as-datasource").click();
+      agHelper.WaitUntilEleAppear(apiPage._saveAsDS);
+      cy.get(apiPage._saveAsDS).click({ force: true });
+      //verifying there is no error toast, Bug 14566
       agHelper.AssertElementAbsence(
         locators._specificToast("Duplicate key error"),
-      ); //verifying there is no error toast, Bug 14566
+      );
       cy.testSelfSignedCertificateSettingsInREST(false);
+      cy.get("body").then((body) => {
+        if (
+          body.find('[value="http://host.docker.internal:5001"]').length < 1
+        ) {
+          cy.get('[placeholder="https://example.com"]').type(
+            "http://host.docker.internal:5001",
+          );
+        }
+      });
       cy.saveDatasource();
-      cy.contains(".datasource-highlight", "http://host.docker.internal:5001"); //failing here since Save as Datasource is broken
+      cy.contains(".datasource-highlight", "http://host.docker.internal:5001");
       cy.SaveAndRunAPI();
     });
   },
