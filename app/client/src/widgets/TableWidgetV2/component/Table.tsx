0dabe58fb29a31c757309e1e77d238add5f05d8b
Rahul Barwal <rahul.barwal@appsmith.com>
2024-11-15 18:50:52 +0530

    Added virtual check for HTML  and server side paginated tables.

--- app/client/src/widgets/TableWidgetV2/component/Table.tsx
+++ app/client/src/widgets/TableWidgetV2/component/Table.tsx
@@ -30,7 +30,11 @@ import {
 } from "./Constants";
 import { Colors } from "constants/Colors";
 import type { EventType } from "constants/AppsmithActionConstants/ActionConstants";
-import { type EditableCell, type TableVariant } from "../constants";
+import {
+  ColumnTypes,
+  type EditableCell,
+  type TableVariant,
+} from "../constants";
 import SimpleBar from "simplebar-react";
 import "simplebar-react/dist/simplebar.min.css";
 import { createGlobalStyle } from "styled-components";
@@ -326,7 +330,9 @@ export function Table(props: TableProps) {
   const shouldUseVirtual =
     props.serverSidePaginationEnabled &&
     !props.columns.some(
-      (column) => !!column.columnProperties.allowCellWrapping,
+      (column) =>
+        !!column.columnProperties.allowCellWrapping ||
+        column.metaProperties?.type === ColumnTypes.HTML,
     );
 
   useEffect(() => {
