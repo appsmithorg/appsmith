31f1c573d85e51ce66f78de39a782929411207cd
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-11-20 11:13:51 +0530

    feat: Flex,Box added forwarded ref

--- app/client/packages/design-system/ads/src/Box/Box.tsx
+++ app/client/packages/design-system/ads/src/Box/Box.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import clsx from "classnames";
 
 import type { BoxProps } from "./Box.types";
@@ -6,24 +6,30 @@ import { BoxClassName, PropsToBeCssPrefixPrepended } from "./Box.constants";
 import { StyledBox } from "./Box.styles";
 import { CSS_VARIABLE_PREFIX } from "../__config__/constants";
 
-function Box({ children, className, ...rest }: BoxProps) {
-  const transformedRest = Object.entries(rest).reduce((acc, [key, value]) => {
-    const newValue = PropsToBeCssPrefixPrepended.includes(key)
-      ? `var(${CSS_VARIABLE_PREFIX}${value})`
-      : value;
+const Box = forwardRef<HTMLDivElement, BoxProps>(
+  ({ children, className, ...rest }, ref) => {
+    const transformedRest = Object.entries(rest).reduce((acc, [key, value]) => {
+      const newValue = PropsToBeCssPrefixPrepended.includes(key)
+        ? `var(${CSS_VARIABLE_PREFIX}${value})`
+        : value;
 
-    return {
-      ...acc,
-      [key]: newValue,
-    };
-  }, {} as BoxProps);
+      return {
+        ...acc,
+        [key]: newValue,
+      };
+    }, {} as BoxProps);
 
-  return (
-    <StyledBox className={clsx(BoxClassName, className)} {...transformedRest}>
-      {children}
-    </StyledBox>
-  );
-}
+    return (
+      <StyledBox
+        className={clsx(BoxClassName, className)}
+        ref={ref}
+        {...transformedRest}
+      >
+        {children}
+      </StyledBox>
+    );
+  },
+);
 
 Box.displayName = "Box";
 
--- app/client/packages/design-system/ads/src/Flex/Flex.tsx
+++ app/client/packages/design-system/ads/src/Flex/Flex.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import clsx from "classnames";
 
 import type { FlexProps } from "./Flex.types";
 import { FlexClassName } from "./Flex.constants";
 import { Box } from "../Box";
 
-function Flex({ children, className, ...rest }: FlexProps) {
-  return (
-    <Box className={clsx(FlexClassName, className)} display={"flex"} {...rest}>
-      {children}
-    </Box>
-  );
-}
+const Flex = forwardRef<HTMLDivElement, FlexProps>(
+  ({ children, className, ...rest }, ref) => {
+    return (
+      <Box
+        className={clsx(FlexClassName, className)}
+        display={"flex"}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </Box>
+    );
+  },
+);
 
 Flex.displayName = "Flex";
 
