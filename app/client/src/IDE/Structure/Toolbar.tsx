08990eca5ddf2df9699686b1483421be2e5b273a
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-11-20 11:14:32 +0530

    feat: Added shadow on scroll for toolbar

--- app/client/src/IDE/Structure/Toolbar.tsx
+++ app/client/src/IDE/Structure/Toolbar.tsx
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Flex } from "@appsmith/ads";
+import { debounce } from "lodash";
+import { cn } from "@appsmith/utils";
 
 interface ToolbarProps {
   children?: React.ReactNode[] | React.ReactNode;
 }
 
 const Toolbar = (props: ToolbarProps) => {
+  const [shadow, setShadow] = useState(false);
+  const toolbarRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(function handleScrollEffect() {
+    const handleScroll = debounce((e: Event) => {
+      const scrolledElement = e.target as HTMLElement;
+
+      if (!scrolledElement) return;
+
+      const isScrolled = scrolledElement.scrollTop > 0;
+
+      setShadow(isScrolled);
+    }, 20);
+
+    if (toolbarRef.current) {
+      const adjacentElement = toolbarRef.current
+        .nextElementSibling as HTMLElement;
+
+      if (adjacentElement) {
+        adjacentElement.addEventListener("scroll", handleScroll, {
+          capture: true,
+        });
+      }
+
+      return () => {
+        if (adjacentElement) {
+          adjacentElement.removeEventListener("scroll", handleScroll, {
+            capture: true,
+          });
+        }
+      };
+    }
+  }, []);
+
   return (
     <Flex
       alignItems="center"
-      borderBottom="1px solid var(--ads-v2-color-border-muted);"
+      className={cn("sticky top-0 transition-shadow duration-300 ease-in-out", {
+        "shadow-md": shadow,
+      })}
       flexDirection="row"
       height="32px"
       justifyContent="space-between"
       padding="spaces-2"
+      ref={toolbarRef}
+      zIndex="10"
     >
       {props.children}
     </Flex>
