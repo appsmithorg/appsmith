d6d394738f4fb0d357478959c81a826e14ef5b5e
albinAppsmith <87797149+albinAppsmith@users.noreply.github.com>
2024-11-20 11:10:45 +0530

    Added CN utility

---
+++ app/client/packages/utils/src/cn/cn.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export { cn };
--- app/client/packages/utils/src/index.ts
+++ app/client/packages/utils/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./object";
 export * from "./compose";
+export * from "./cn";
