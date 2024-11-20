70461041c611d47421151ecc2589984157fb777e
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-11-20 11:35:29 +0530

    remove console log

--- app/client/cypress/support/Objects/FeatureFlags.ts
+++ app/client/cypress/support/Objects/FeatureFlags.ts
@@ -42,7 +42,6 @@ export const getConsolidatedDataApi = (
       ) {
         const originalResponse = res?.body;
         const updatedResponse = produce(originalResponse, (draft: any) => {
-          console.log("request headers", req.headers);
           draft.data.responseMeta.version = req.headers["x-appsmith-version"];
           draft.data.featureFlags.data = {
             ...flags,
