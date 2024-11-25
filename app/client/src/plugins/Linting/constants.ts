ee88b1b901883bd855fea1562059ee88f302b26e
Ayush Pahwa <ayushpahwa96@gmail.com>
2024-11-25 14:31:10 +0530

    update: import rule def from custom rules file

--- app/client/src/plugins/Linting/constants.ts
+++ app/client/src/plugins/Linting/constants.ts
@@ -2,6 +2,7 @@ import { ECMA_VERSION } from "@shared/ast";
 import type { LintOptions } from "jshint";
 import isEntityFunction from "./utils/isEntityFunction";
 import type { Linter } from "eslint-linter-browserify";
+import { noFloatingPromisesLintRule } from "./customRules/no-floating-promises";
 
 export enum LINTER_TYPE {
   "JSHINT" = "JSHint",
@@ -57,7 +58,15 @@ export const lintOptions = (
         globals: eslintGlobals,
         sourceType: "script",
       },
+      plugins: {
+        customRules: {
+          rules: {
+            "no-floating-promises": noFloatingPromisesLintRule,
+          },
+        },
+      },
       rules: {
+        "customRules/no-floating-promises": "error",
         eqeqeq: "off",
         curly: "off",
         "no-extend-native": "error",
