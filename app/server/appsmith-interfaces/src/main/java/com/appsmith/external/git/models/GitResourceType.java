271a15d75b69071b4deab447dffea99abb9a32d8
Nidhi Nair <nidhi@appsmith.com>
2024-11-26 18:42:04 +0530

    chore: Added git resource map types

---
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/git/models/GitResourceType.java
@@ -0,0 +1,26 @@
+package com.appsmith.external.git.models;
+
+import java.util.Locale;
+
+public enum GitResourceType {
+    ROOT_CONFIG,
+    DATASOURCE_CONFIG,
+    JSLIB_CONFIG,
+    PAGE_CONFIG,
+    JSOBJECT_CONFIG,
+    JSOBJECT_DATA,
+    QUERY_CONFIG,
+    QUERY_DATA,
+    WIDGET_CONFIG,
+
+// ----------------------------------
+
+// ----------------------------------
+
+;
+
+    @Override
+    public String toString() {
+        return this.name().toLowerCase(Locale.ROOT);
+    }
+}
