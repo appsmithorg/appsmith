260e039f9858b270d155471fe6f9baca4474e997
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-11-26 16:30:53 +0530

    Fix type error

--- app/client/src/api/interceptors/response/failureHandlers/handleBadRequestError.ts
+++ app/client/src/api/interceptors/response/failureHandlers/handleBadRequestError.ts
@@ -4,17 +4,17 @@ import { handleVersionMismatch } from "sagas/WebsocketSagas/versionUpdatePrompt"
 import { getAppsmithConfigs } from "ee/configs";
 import { SERVER_ERROR_CODES } from "ee/constants/ApiConstants";
 
-export const handleBadRequestError = async (
-  error: AxiosError<ApiResponse<{ serverVersion: string }>>,
-) => {
+export const handleBadRequestError = async (error: AxiosError<ApiResponse>) => {
   const errorCode = error?.response?.data?.responseMeta.error?.code;
 
   if (
     error?.response?.status === 400 &&
     SERVER_ERROR_CODES.VERSION_MISMATCH.includes("" + errorCode)
   ) {
-    const message = error?.response?.data?.responseMeta.error?.message;
-    const serverVersion = error?.response?.data?.data?.serverVersion;
+    const responseData = error?.response?.data;
+    const message = responseData?.responseMeta.error?.message;
+    const serverVersion = (responseData?.data as { serverVersion: string })
+      .serverVersion;
 
     handleVersionMismatch(getAppsmithConfigs().appVersion.id, serverVersion);
 
