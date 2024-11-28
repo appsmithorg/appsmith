da8b8d6ff278ed358b71e0042c0ac21237df16b7
rishabhrathod01 <rishabhrathod2012@gmail.com>
2024-11-28 16:49:10 +0530

    remove unused span

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/constants/spans/ce/OnLoadSpanCE.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/constants/spans/ce/OnLoadSpanCE.java
@@ -8,18 +8,12 @@ public class OnLoadSpanCE {
             APPSMITH_SPAN_PREFIX + "getAllExecutablesByCreatorIdFlux";
     public static final String EXECUTABLE_NAME_TO_EXECUTABLE_MAP =
             APPSMITH_SPAN_PREFIX + "executableNameToExecutableMap";
-    public static final String EXECUTABLE_IN_CREATOR_CONTEXT = APPSMITH_SPAN_PREFIX + "executablesInCreatorContext";
     public static final String ADD_DIRECTLY_REFERENCED_EXECUTABLES_TO_GRAPH =
             APPSMITH_SPAN_PREFIX + "addDirectlyReferencedExecutablesToGraph";
-    public static final String GET_POSSIBLE_ENTITY_REFERENCES = APPSMITH_SPAN_PREFIX + "getPossibleEntityReferences";
     public static final String UPDATE_EXECUTABLE_SELF_REFERENCING_PATHS =
             APPSMITH_SPAN_PREFIX + "updateExecutableSelfReferencingPaths";
-    public static final String GET_POSSIBLE_ENTITY_PARENTS_MAP = APPSMITH_SPAN_PREFIX + "getPossibleEntityParentsMap";
     public static final String ADD_EXPLICIT_USER_SET_ON_LOAD_EXECUTABLES_TO_GRAPH =
             APPSMITH_SPAN_PREFIX + "addExplicitUserSetOnLoadExecutablesToGraph";
     public static final String GET_UNPUBLISHED_ON_LOAD_EXECUTABLES_EXPLICIT_SET_BY_USER_IN_CREATOR_CONTEXT =
             APPSMITH_SPAN_PREFIX + "getUnpublishedOnLoadExecutablesExplicitSetByUserInCreatorContext";
-
-    public static final String GET_POSSIBLE_REFERENCES_FROM_DYNAMIC_BINDING =
-            APPSMITH_SPAN_PREFIX + "getPossibleReferencesFromDynamicBinding";
 }
