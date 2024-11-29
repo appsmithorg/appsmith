6773f51d6a4f08b19fd1fc8f2d5cd5868317aaa9
Shrikant Sharat Kandula <shrikant@appsmith.com>
2024-11-29 18:46:11 +0530

    chore: Move all remaining actions into links (#37829)
    The chain+link framework is now used for all the steps in the backup
    command.
    
    
    ## Automation
    
    /test sanity
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/12080116829>
    > Commit: fcc016fd1fa0bbe9b4cae2478bf4f7750ee5459d
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=12080116829&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Sanity`
    > Spec:
    > <hr>Fri, 29 Nov 2024 07:12:22 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [x] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Introduced new classes for managing backup processes, including
    `BackupFolderLink`, `DiskSpaceLink`, `EnvFileLink`, `GitStorageLink`,
    `MongoDumpLink`, and `ManifestLink`.
    - Added functionality for checking available disk space and managing
    temporary backup folders.
    	- Enhanced encryption handling with a new password prompt mechanism.
    
    - **Bug Fixes**
    - Streamlined backup process by removing redundant functions and
    ensuring proper error handling.
    
    - **Documentation**
    - Updated comments and documentation for new classes and methods to
    improve clarity.
    
    - **Refactor**
    - Restructured backup functionality for improved modularity and
    maintainability.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ app/client/packages/rts/src/ctl/backup/links/EnvFileLink.ts
@@ -0,0 +1,65 @@
+import type { Link } from ".";
+import type { BackupState } from "../BackupState";
+import fsPromises from "fs/promises";
+
+const SECRETS_WARNING = `
+***************************** IMPORTANT!!! *****************************
+*** Please ensure you have saved the APPSMITH_ENCRYPTION_SALT and    ***
+*** APPSMITH_ENCRYPTION_PASSWORD variables from the docker.env file. ***
+*** These values are not included in the backup export.              ***
+************************************************************************
+`;
+
+/**
+ * Exports the docker environment file to the backup folder. If encryption is not enabled, sensitive information is
+ * not written to the backup folder.
+ */
+export class EnvFileLink implements Link {
+  constructor(private readonly state: BackupState) {}
+
+  async doBackup() {
+    console.log("Exporting docker environment file");
+    const content = await fsPromises.readFile(
+      "/appsmith-stacks/configuration/docker.env",
+      { encoding: "utf8" },
+    );
+    let cleanedContent = removeSensitiveEnvData(content);
+
+    if (this.state.isEncryptionEnabled) {
+      cleanedContent +=
+        "\nAPPSMITH_ENCRYPTION_SALT=" +
+        process.env.APPSMITH_ENCRYPTION_SALT +
+        "\nAPPSMITH_ENCRYPTION_PASSWORD=" +
+        process.env.APPSMITH_ENCRYPTION_PASSWORD;
+    }
+
+    await fsPromises.writeFile(
+      this.state.backupRootPath + "/docker.env",
+      cleanedContent,
+    );
+    console.log("Exporting docker environment file done.");
+  }
+
+  async postBackup() {
+    if (!this.state.isEncryptionEnabled) {
+      console.log(SECRETS_WARNING);
+    }
+  }
+}
+
+export function removeSensitiveEnvData(content: string): string {
+  // Remove encryption and Mongodb data from docker.env
+  const output_lines = [];
+
+  content.split(/\r?\n/).forEach((line) => {
+    if (
+      !line.startsWith("APPSMITH_ENCRYPTION") &&
+      !line.startsWith("APPSMITH_MONGODB") &&
+      !line.startsWith("APPSMITH_DB_URL=")
+    ) {
+      output_lines.push(line);
+    }
+  });
+
+  return output_lines.join("\n");
+}
