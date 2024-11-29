63eec766358f7207321dd8e7b6f17c25f79718b0
Pawan Kumar <pawan@appsmith.com>
2024-11-29 16:20:58 +0530

    chore: add wds datepicker widget (#37711)
    ![CleanShot 2024-11-26 at 15 56
    15](https://github.com/user-attachments/assets/d812f475-11e1-4750-9018-bdd39d5a5de3)
    
    /ok-to-test tags="@tag.Anvil"
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Introduced the WDS Date Picker Widget, enhancing date selection
    capabilities within the UI.
    - Added configuration options for widget size, visibility, and
    autocomplete behavior.
    - Implemented comprehensive validation for date input, ensuring accurate
    user selections.
    - Expanded widget collection to include the new WDS Date Picker Widget.
    - Introduced new constants for date format options, facilitating diverse
    formatting choices.
    - Added support for a new "Date" input type, enhancing input widget
    configurability.
    
    - **Documentation**
    - Updated property pane configurations to include detailed settings for
    date format, validation, and event handling.
    
    - **Bug Fixes**
    - Improved handling of derived properties to ensure proper context
    during widget interactions.
    
    These updates collectively improve user experience and flexibility in
    date selection within the application.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/12083004714>
    > Commit: b17348e03db911501970d2c8a59c4fea30a175e1
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=12083004714&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Anvil`
    > Spec:
    > <hr>Fri, 29 Nov 2024 10:44:39 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    ---------
    
    Co-authored-by: Vadim Vaitenko <vadim@appsmith.com>

---
+++ app/client/src/modules/ui-builder/ui/wds/WDSDatePickerWidget/config/metaConfig.ts
@@ -0,0 +1,21 @@
+import { WIDGET_TAGS } from "constants/WidgetConstants";
+
+export const metaConfig = {
+  name: "DatePicker",
+  tags: [WIDGET_TAGS.INPUTS],
+  needsMeta: true,
+  searchTags: [
+    "datepicker",
+    "appointment",
+    "calendar",
+    "date",
+    "day",
+    "hour",
+    "meeting",
+    "moment",
+    "schedule",
+    "time",
+    "week",
+    "year",
+  ],
+};
