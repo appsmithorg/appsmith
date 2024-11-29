adab9c502a99bfc85a98ed8fccf51bc2389f2621
Ankita Kinger <ankita@appsmith.com>
2024-11-29 16:15:49 +0530

    adding error message in API response tab

--- app/client/src/PluginActionEditor/components/PluginActionResponse/components/ApiResponse.tsx
+++ app/client/src/PluginActionEditor/components/PluginActionResponse/components/ApiResponse.tsx
@@ -46,14 +46,14 @@ export const ResponseTabErrorContainer = styled.div`
   height: fit-content;
   background: var(--ads-v2-color-bg-error);
   border-bottom: 1px solid var(--ads-v2-color-border);
+  font-size: 12px;
+  line-height: 16px;
 `;
 
 export const ResponseTabErrorContent = styled.div`
   display: flex;
   align-items: flex-start;
   gap: 4px;
-  font-size: 12px;
-  line-height: 16px;
 `;
 
 export const ResponseTabErrorDefaultMessage = styled.div`
@@ -93,7 +93,7 @@ export function ApiResponse(props: {
     );
   }
 
-  const { messages, pluginErrorDetails, request } = props.actionResponse;
+  const { body, messages, pluginErrorDetails, request } = props.actionResponse;
 
   const runHasFailed = hasFailed(props.actionResponse);
   const requestWithTimestamp = getUpdateTimestamp(request);
@@ -121,7 +121,7 @@ export function ApiResponse(props: {
           <ResponseTabErrorContent>
             <ResponseTabErrorDefaultMessage>
               Your API failed to execute
-              {pluginErrorDetails && ":"}
+              {(pluginErrorDetails || body) && ":"}
             </ResponseTabErrorDefaultMessage>
             {pluginErrorDetails && (
               <>
@@ -142,6 +142,9 @@ export function ApiResponse(props: {
               source={actionSource}
             />
           </ResponseTabErrorContent>
+          {body && (
+            <div className="t--debugger-log-downstream-message">{body}</div>
+          )}
           {requestWithTimestamp && (
             <JsonWrapper className="t--debugger-log-state" onClick={noop}>
               <ReactJson src={requestWithTimestamp} {...apiReactJsonProps} />
