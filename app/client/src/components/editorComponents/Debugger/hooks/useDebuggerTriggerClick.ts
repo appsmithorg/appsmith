a5af6028c14e67a8c9bdb6ce2bf85ddbec71ad5f
Hetu Nandu <hetunandu@gmail.com>
2024-12-03 14:58:47 +0530

    chore: Revert default tab selection on debug CTA click

--- app/client/src/components/editorComponents/Debugger/hooks/useDebuggerTriggerClick.ts
+++ app/client/src/components/editorComponents/Debugger/hooks/useDebuggerTriggerClick.ts
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router";
+import { DEBUGGER_TAB_KEYS } from "../constants";
 import { setCanvasDebuggerState } from "actions/debuggerActions";
 import AnalyticsUtil from "ee/utils/AnalyticsUtil";
 import type { FocusEntityInfo } from "navigation/FocusEntity";
@@ -16,7 +17,6 @@ import { EditorViewMode } from "ee/entities/IDE/constants";
 import type { ReduxAction } from "ee/constants/ReduxActionConstants";
 import type { CanvasDebuggerState } from "reducers/uiReducers/debuggerReducer";
 import type { AppState } from "ee/reducers";
-import { DEBUGGER_TAB_KEYS } from "../constants";
 
 interface Config {
   set: (
@@ -63,19 +63,20 @@ const useDebuggerTriggerClick = () => {
   const state = useSelector(config.get);
 
   return () => {
-    // If debugger is already open then we will close debugger.
-    if (state.open) {
+    // If debugger is already open and selected tab is error tab then we will close debugger.
+    if (state.open && state.selectedTab === DEBUGGER_TAB_KEYS.ERROR_TAB) {
       dispatch(config.set({ open: false }));
     } else {
-      // If debugger is not open then we will open debugger.
+      // If debugger is not open then we will open debugger and show error tab.
       if (!state.open) {
-        dispatch(config.set({ open: true }));
-
-        // If debugger does not have any selected tab then we will set the selected tab to logs tab.
-        if (!state.selectedTab) {
-          dispatch(config.set({ selectedTab: DEBUGGER_TAB_KEYS.LOGS_TAB }));
-        }
+        dispatch(
+          config.set({ open: true, selectedTab: DEBUGGER_TAB_KEYS.ERROR_TAB }),
+        );
       }
+
+      // Select error tab if debugger is open and selected tab is not error tab.
+      // And also when we are opening debugger.
+      dispatch(config.set({ selectedTab: DEBUGGER_TAB_KEYS.ERROR_TAB }));
     }
 
     if (!state.open) {
