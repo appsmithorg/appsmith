f65f265ce857fed75f9a30791e06867ff4531262
Sagar Khalasi <sagar@appsmith.com>
2024-12-09 13:51:50 +0530

    Basic setup

--- app/client/cypress/e2e/Regression/ClientSide/ActionExecution/ActionSelector_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/ActionExecution/ActionSelector_spec.ts
@@ -20,49 +20,67 @@ describe(
   { tags: ["@tag.JS"] },
   () => {
     before(() => {
-      homePage.NavigateToHome();
-      homePage.ImportApp("setIntervalApp.json");
+      entityExplorer.DragDropWidgetNVerify(draggableWidgets.BUTTON);
     });
 
-    it("1. Verify that callbacks can be configured with a success event", () => {
-      
+    it.only("1. Verify that actions can be configured ", () => {
+      propPane.EnterJSContext(
+        "onClick",
+        `{{showAlert("Action Selector Test Message", '')}}`,
+        true,
+      );
+      propPane.ToggleJSMode("onClick", false);
+      agHelper.ClickButton("Submit");
+      agHelper.ValidateToastMessage("Action Selector Test Message", 0, 1);
+
+      deployMode.DeployApp();
+      agHelper.AssertElementVisibility(appSettings.locators._header);
+      agHelper.ClickButton("Submit");
+      agHelper.ValidateToastMessage("Action Selector Test Message", 0, 1);
+      deployMode.NavigateBacktoEditor();
     });
 
-    it("2. Verify that callbacks can be configured with a failure event", () => {
-      
+    it.only("2. Verify that callbacks can be configured with a success event", () => {
+      EditorNavigation.SelectEntityByName("Button1", EntityType.Widget);
+      propPane.EnterJSContext("onClick", `{{showAlert("Action Success Message", '')}}`);
+      propPane.ToggleJSMode("onClick", false);
+
+      agHelper.GetNClick(propPane._actionCardByTitle("Show alert"));
+
+      // add a success callback
+      agHelper.GetNClick(propPane._actionAddCallback("success"));
+      agHelper.GetNClick(locators._dropDownValue("Store value"));
     });
 
-    it("3. Verify that callbacks can be chained", () => {
-      
+    it.only("3. Verify that callbacks can be configured with a failure event", () => {
+      // add an error callback
+      agHelper.GetNClick(propPane._actionAddCallback("failure"));
+      agHelper.GetNClick(locators._dropDownValue("Navigate to"));
+
+      propPane.ValidateJSFieldValue(
+        "onClick",
+        `{{showAlert('Hello!', '').then(() => {  storeValue("", "");}).catch(() => {  navigateTo("", {}, 'SAME_WINDOW');});}}`,
+      );
+
+      cy.pause();
+     
     });
 
-    it("4. Verify that callbacks can be deleted", () => {
-      
+    it("4. Verify that callbacks can be chained", () => {
     });
 
-    it("5. Verify that the Callbacks section reflects the number of active callbacks accurately", () => {
-      
-    });
+    it("5. Verify that callbacks can be deleted", () => {});
 
-    it("6. Verify that configured actions on existing apps are intact", () => {
-      
-    });
+    it("6. Verify that the Callbacks section reflects the number of active callbacks accurately", () => {});
 
-    it("7. Verify that configured actions stay intact on import of an app", () => {
-      
-    });
+    it("7. Verify that configured actions on existing apps are intact", () => {});
 
-    it("8. Verify that configured actions stay intact on partial import of a page", () => {
-      
-    });
+    it("8. Verify that configured actions stay intact on import of an app", () => {});
 
-    it("9. Verify that configured actions stay intact on forking an app", () => {
-      
-    });
-    
-    it("10. Verify that configured actions stay intact on navigating between pages", () => {
-      
-    });
-    
+    it("9. Verify that configured actions stay intact on partial import of a page", () => {});
+
+    it("10. Verify that configured actions stay intact on forking an app", () => {});
+
+    it("11. Verify that configured actions stay intact on navigating between pages", () => {});
   },
 );
