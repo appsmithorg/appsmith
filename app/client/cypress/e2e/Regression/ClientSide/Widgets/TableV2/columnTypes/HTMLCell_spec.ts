aecd362ee1bf3278cbb8153e34a22ab5082b0078
Rahul Barwal <rahul.barwal@appsmith.com>
2024-12-09 18:41:54 +0530

    - Introduced tags for better categorization of test cases.

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/columnTypes/HTMLCell_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/columnTypes/HTMLCell_spec.ts
@@ -5,77 +5,78 @@ import {
   propPane,
   table,
 } from "../../../../../../support/Objects/ObjectsCore";
-import EditorNavigation, {
-  EntityType,
-} from "../../../../../../support/Pages/EditorNavigation";
 
-describe("Table Filter for HTML Cell", function () {
-  before(() => {
-    featureFlagIntercept({
-      release_table_html_column_type_enabled: true,
+describe(
+  "Table Filter for HTML Cell",
+  { tags: ["@tag.Widget", "@tag.Table"] },
+  function () {
+    before(() => {
+      featureFlagIntercept({
+        release_table_html_column_type_enabled: true,
+      });
+      entityExplorer.DragDropWidgetNVerify("tablewidgetv2", 650, 250);
+      propPane.EnterJSContext("Table data", JSON.stringify(htmlTableData));
     });
-    entityExplorer.DragDropWidgetNVerify("tablewidgetv2", 650, 250);
-    propPane.EnterJSContext("Table data", JSON.stringify(htmlTableData));
-  });
 
-  it("1. Ensures HTML column type is available", function () {
-    table.ReadTableRowColumnData(1, 3, "v2").then(($cellData) => {
-      expect($cellData).to.include("Active");
-    });
-  });
-
-  it("2. Verify HTML columns are searchable", function () {
-    table.ReadTableRowColumnData(1, 3, "v2").then(($cellData) => {
-      expect($cellData).to.include("Active");
-      table.SearchTable($cellData);
-      table.ReadTableRowColumnData(0, 3, "v2").then((afterSearch) => {
-        expect(afterSearch).to.eq($cellData);
+    it("1. Ensures HTML column type is available", function () {
+      table.ReadTableRowColumnData(1, 3, "v2").then(($cellData) => {
+        expect($cellData).to.include("Active");
       });
     });
-    table.RemoveSearchTextNVerify("1", "v2");
-  });
 
-  it("3. Verify Table Filter for HTML columns", function () {
-    propPane.ExpandIfCollapsedSection("search\\&filters");
-    agHelper.AssertExistingToggleState("Allow filtering", "false");
-    propPane.TogglePropertyState("Allow filtering", "On");
+    it("2. Verify HTML columns are searchable", function () {
+      table.ReadTableRowColumnData(1, 3, "v2").then(($cellData) => {
+        expect($cellData).to.include("Active");
+        table.SearchTable($cellData);
+        table.ReadTableRowColumnData(0, 3, "v2").then((afterSearch) => {
+          expect(afterSearch).to.eq($cellData);
+        });
+      });
+      table.RemoveSearchTextNVerify("1", "v2");
+    });
 
-    table.OpenNFilterTable("status", "contains", "Active");
-    table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
-      expect($cellData).to.include("Active");
-    });
-    table.RemoveFilterNVerify("1", true, true, 0, "v2");
+    it("3. Verify Table Filter for HTML columns", function () {
+      propPane.ExpandIfCollapsedSection("search\\&filters");
+      agHelper.AssertExistingToggleState("Allow filtering", "false");
+      propPane.TogglePropertyState("Allow filtering", "On");
 
-    table.OpenNFilterTable("status", "contains", "Suspended");
-    table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
-      expect($cellData).to.include("Suspended");
-    });
-    table.RemoveFilterNVerify("1", true, true, 0, "v2");
+      table.OpenNFilterTable("status", "contains", "Active");
+      table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
+        expect($cellData).to.include("Active");
+      });
+      table.RemoveFilterNVerify("1", true, true, 0, "v2");
 
-    table.OpenNFilterTable("status", "empty", "");
-    table.ReadTableRowColumnData(0, 0, "v2").then(($cellData) => {
-      expect($cellData).to.include("1");
-    });
-    table.RemoveFilterNVerify("1", true, true, 0, "v2");
+      table.OpenNFilterTable("status", "contains", "Suspended");
+      table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
+        expect($cellData).to.include("Suspended");
+      });
+      table.RemoveFilterNVerify("1", true, true, 0, "v2");
 
-    table.OpenNFilterTable("status", "not empty", "");
-    table.ReadTableRowColumnData(0, 0, "v2").then(($cellData) => {
-      expect($cellData).to.include("2");
-    });
-    table.RemoveFilterNVerify("1", true, true, 0, "v2");
-  });
+      table.OpenNFilterTable("status", "empty", "");
+      table.ReadTableRowColumnData(0, 0, "v2").then(($cellData) => {
+        expect($cellData).to.include("1");
+      });
+      table.RemoveFilterNVerify("1", true, true, 0, "v2");
 
-  it("4. Verify Table sorting for HTML columns", function () {
-    table.SortColumn("status", "asc");
-    table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
-      expect($cellData).to.include("Active");
+      table.OpenNFilterTable("status", "not empty", "");
+      table.ReadTableRowColumnData(0, 0, "v2").then(($cellData) => {
+        expect($cellData).to.include("2");
+      });
+      table.RemoveFilterNVerify("1", true, true, 0, "v2");
     });
-    table.SortColumn("status", "desc");
-    table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
-      expect($cellData).to.include("Suspended");
+
+    it("4. Verify Table sorting for HTML columns", function () {
+      table.SortColumn("status", "asc");
+      table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
+        expect($cellData).to.include("Active");
+      });
+      table.SortColumn("status", "desc");
+      table.ReadTableRowColumnData(0, 3, "v2").then(($cellData) => {
+        expect($cellData).to.include("Suspended");
+      });
     });
-  });
-});
+  },
+);
 
 const htmlTableData = [
   {
