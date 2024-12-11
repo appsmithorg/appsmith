d01be546252150d7c13b879ad594d25e4e4003d1
Ankita Kinger <ankita@appsmith.com>
2024-12-11 15:07:39 +0530

    moving segmented header to ADS extracting out the functionalities via props

---
+++ app/client/packages/design-system/ads/src/Templates/EntityExplorer/NavigationHeader/NavigationHeader.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import {
+  SegmentedControl,
+  type SegmentedControlOption,
+} from "../../../SegmentedControl";
+import { Flex } from "../../../Flex";
+
+interface Props {
+  selectedSegment: string;
+  onSegmentChange: (value: string) => void;
+  options: SegmentedControlOption[];
+  children?: React.ReactNode | React.ReactNode[];
+}
+
+const Container = styled(Flex)`
+  #editor-pane-segment-control {
+    max-width: 247px;
+  }
+`;
+
+export const NavigationHeader = (props: Props) => {
+  const { children, onSegmentChange, options, selectedSegment } = props;
+
+  return (
+    <Container
+      alignItems="center"
+      backgroundColor="var(--ads-v2-colors-control-track-default-bg)"
+      className="ide-editor-left-pane__header"
+      gap="spaces-2"
+      justifyContent="space-between"
+      padding="spaces-2"
+    >
+      <SegmentedControl
+        id="editor-pane-segment-control"
+        onChange={onSegmentChange}
+        options={options}
+        value={selectedSegment}
+      />
+      {children}
+    </Container>
+  );
+};
---
+++ app/client/packages/design-system/ads/src/Templates/EntityExplorer/NavigationHeader/index.tsx
@@ -0,0 +1,1 @@
+export { NavigationHeader } from "./NavigationHeader";
