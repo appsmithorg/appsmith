ac0e8edc7db3e41ec58e44fd8074a03f092851bf
Anagh Hegde <anagh@appsmith.com>
2024-12-13 02:13:16 +0530

    refactor the crud method to custom repo class

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomDatasourceRepositoryCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomDatasourceRepositoryCE.java
@@ -6,9 +6,19 @@ import com.appsmith.server.repositories.AppsmithRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 public interface CustomDatasourceRepositoryCE extends AppsmithRepository<Datasource> {
 
     Flux<Datasource> findAllByWorkspaceId(String workspaceId, AclPermission permission);
 
     Mono<Datasource> findByNameAndWorkspaceId(String name, String workspaceId, AclPermission aclPermission);
+
+    Flux<Datasource> findByWorkspaceId(String workspaceId, AclPermission aclPermission);
+
+    Flux<Datasource> findAllByWorkspaceIdWithoutPermissions(String workspaceId);
+
+    Flux<Datasource> findByIdIn(List<String> ids);
+
+    Mono<Long> countByDeletedAtNull();
 }
--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomDatasourceRepositoryCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/CustomDatasourceRepositoryCEImpl.java
@@ -3,10 +3,13 @@ package com.appsmith.server.repositories.ce;
 import com.appsmith.external.models.Datasource;
 import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.helpers.ce.bridge.Bridge;
+import com.appsmith.server.helpers.ce.bridge.BridgeQuery;
 import com.appsmith.server.repositories.BaseAppsmithRepositoryImpl;
 import org.springframework.data.domain.Sort;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 public class CustomDatasourceRepositoryCEImpl extends BaseAppsmithRepositoryImpl<Datasource>
         implements CustomDatasourceRepositoryCE {
@@ -27,5 +30,29 @@ public class CustomDatasourceRepositoryCEImpl extends BaseAppsmithRepositoryImpl
                 .criteria(Bridge.equal(Datasource.Fields.name, name).equal(Datasource.Fields.workspaceId, workspaceId))
                 .permission(aclPermission)
                 .one();
+    }
+
+    @Override
+    public Flux<Datasource> findByWorkspaceId(String workspaceId, AclPermission aclPermission) {
+        final BridgeQuery<Datasource> q = Bridge.equal(Datasource.Fields.workspaceId, workspaceId);
+        return queryBuilder().criteria(q).permission(aclPermission).all();
+    }
+
+    @Override
+    public Flux<Datasource> findAllByWorkspaceIdWithoutPermissions(String workspaceId) {
+        final BridgeQuery<Datasource> q = Bridge.equal(Datasource.Fields.workspaceId, workspaceId);
+        return queryBuilder().criteria(q).all();
+    }
+
+    @Override
+    public Flux<Datasource> findByIdIn(List<String> ids) {
+        final BridgeQuery<Datasource> q = Bridge.in(Datasource.Fields.id, ids);
+        return queryBuilder().criteria(q).all();
+    }
+
+    @Override
+    public Mono<Long> countByDeletedAtNull() {
+        final BridgeQuery<Datasource> q = Bridge.isNull(Datasource.Fields.deletedAt);
+        return queryBuilder().criteria(q).count();
     }
 }
