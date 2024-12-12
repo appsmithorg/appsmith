0808b279fa65182f4b7d3c373883f4771238df70
Rudraprasad Das <rudra@appsmith.com>
2024-12-12 23:18:09 +0800

    chore: git mod - adding init saga and connecting components to ctx (#38088)
    ## Description
    - Adds more selectors
    - Adds more sagas
    - Introduces init steps for git
    - Improvements upon CtxAwareGitQuickActions
    
    Fixes #37800 
    Fixes #36814 
    
    
    ## Automation
    
    /ok-to-test tags="@tag.Git"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/12295168481>
    > Commit: ea1ab497cad677aac36f044462e623e526d421d1
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=12295168481&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Git`
    > Spec:
    > <hr>Thu, 12 Dec 2024 12:06:09 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [ ] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Summary by CodeRabbit
    
    - **New Features**
      - Introduced a context provider for managing Git-related state.
      - Added a `GitQuickActions` component for various Git actions.
    - Implemented custom hooks for managing Git branches, metadata,
    operations, and settings.
    - Added Redux Saga functions for fetching Git metadata and protected
    branches.
      - Enhanced autocommit functionality with polling for progress.
    - Introduced actions for managing the autocommit process and fetching
    protected branches.
    - Added new actions to initialize Git for editor context and manage
    loading states.
    
    - **Bug Fixes**
      - Improved error handling in various action creators and sagas.
    
    - **Chores**
    - Updated action creators and types for better type safety and clarity.
    - Refined test cases for components to ensure accurate button selection.
    - Modified request functions to utilize Axios promises directly for
    better consistency.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/src/git/requests/fetchAutocommitProgressRequest.types.ts
+++ app/client/src/git/requests/fetchAutocommitProgressRequest.types.ts
@@ -1,7 +1,11 @@
+import type { ApiResponse } from "api/types";
 import type { AutocommitStatus } from "../constants/enums";
 
-export interface FetchAutocommitProgressResponse {
+export interface FetchAutocommitProgressResponseData {
   autoCommitResponse: AutocommitStatus;
   progress: number;
   branchName: string;
 }
+
+export type FetchAutocommitProgressResponse =
+  ApiResponse<FetchAutocommitProgressResponseData>;
