0e6555f50e74f30a0aead984d0597cd2161772b9
Abhijeet <abhi.nagarnaik@gmail.com>
2024-12-12 16:58:07 +0530

    chore: Refactor

--- deploy/docker/tests/test-pg-auth.sh
+++ deploy/docker/tests/test-pg-auth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -o errexit
-# set -x
 
 source ./composes.sh
+source ./pg-test-utils.sh
 
 
 # Function to update the APPSMITH_DB_URL in docker.env
@@ -11,41 +11,6 @@ source ./composes.sh
 update_db_url() {
   docker exec "${container_name}" bash -c "sed -i 's|^APPSMITH_DB_URL=mongodb|# &|' /appsmith-stacks/configuration/docker.env"
   docker exec "${container_name}" bash -c "sed -i 's|^APPSMITH_POSTGRES_DB_URL=|APPSMITH_DB_URL=|' /appsmith-stacks/configuration/docker.env"
-}
-
-# Function to check if the Appsmith instance is up
-is_appsmith_instance_ready() {
-  local max_retries=200
-  local retry_count=0
-  local response_code
-
-  while [ $retry_count -lt $max_retries ]; do
-    response_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/health)
-    if [[ $response_code -eq 200 ]]; then
-      echo "Appsmith instance is ready."
-      return 0
-    fi
-    echo "Waiting for Appsmith instance to be ready... (Attempt: $((retry_count + 1)))"
-    retry_count=$((retry_count + 1))
-    sleep 2
-  done
-  return 1
-}
-
-# Function to wait until the postgres is ready
-wait_for_postgres() {
-  local max_retries=200
-  local retry_count=0
-
-  while [ $retry_count -lt $max_retries ]; do
-    if docker exec "${container_name}" pg_isready; then
-      echo "Postgres is ready."
-      return 0
-    fi
-    echo "Waiting for Postgres to be ready... (Attempt: $((retry_count + 1)))"
-    retry_count=$((retry_count + 1))
-    sleep 2
-  done
 }
 
 # Function to read the password from the PostgreSQL URL in docker.env.sh
@@ -93,37 +58,6 @@ EOF
   elif [[ $appsmith_user_remote_access -ne 0 ]]; then
     echo "appsmith user does not have read access to databases with local tcp socket: ❌"
   fi
-  return 1
-}
-
-# Function to check if the Appsmith user has read access to databases
-check_user_datasource_access_with_host_port_wo_auth() {
-  docker exec "${container_name}" bash -c "psql -h 127.0.0.1 -p 5432 -U postgres -c '\l'"
-  return $?
-}
-
-# Function to check if the Appsmith user has read access to databases
-check_user_datasource_access_with_local_port_wo_auth() {
-  docker exec "${container_name}" bash -c "psql -p 5432 -U postgres -c '\l'"
-  return $?
-}
-
-# Function to check if the user exists in the database
-check_user_exists() {
-  local user
-  user=$1
-  local max_retries=200
-  local retry_count=0
-  while [ $retry_count -lt $max_retries ]; do
-    if docker exec "${container_name}" bash -c "psql -p 5432 -U postgres -c \"\du\" | grep -q -w \"$user\""; then
-      echo "$user user exists."
-      return 0
-    fi
-    echo "Waiting for $user user to be created... (Attempt: $((retry_count + 1)))"
-    retry_count=$((retry_count + 1))
-    sleep 1
-  done
-  echo "$user user does not exist."
   return 1
 }
 
@@ -203,7 +137,6 @@ test_postgres_auth_enabled_upgrade_from_150tolocal() {
     
     # Check if the Appsmith instance is up
     if is_appsmith_instance_ready; then
-      # Check for appsmith user in postgres db
       check_user_exists appsmith
       # Check if the Appsmith user has read access to databases
       if check_user_datasource_access_with_auth; then
@@ -213,9 +146,9 @@ test_postgres_auth_enabled_upgrade_from_150tolocal() {
           exit 1
       fi
     else
-      echo "Appsmith instance failed to start."
-      echo "Test ${FUNCNAME[0]} Failed ❌"
-      exit 1
+        echo "Appsmith instance failed to start."
+        echo "Test ${FUNCNAME[0]} Failed ❌"
+        exit 1
     fi
 }
 
@@ -278,7 +211,6 @@ test_postgres_auth_enabled_restart_localtolocal() {
 
     # Check if the Appsmith instance is up
     if is_appsmith_instance_ready; then
-      # Check for appsmith user in postgres db
       check_user_exists appsmith
       # Check if the Appsmith user has read access to databases
       if check_user_datasource_access_with_auth; then
