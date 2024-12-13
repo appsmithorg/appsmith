ff19aa22e3532a7f008bf80facd1ca8a7d9232e0
Anagh Hegde <anagh@appsmith.com>
2024-12-13 14:44:49 +0530

    remove the rference for curd method

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/DatasourceStorageStructureRepositoryCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/ce/DatasourceStorageStructureRepositoryCE.java
@@ -3,10 +3,6 @@ package com.appsmith.server.repositories.ce;
 import com.appsmith.external.models.DatasourceStorageStructure;
 import com.appsmith.server.repositories.BaseRepository;
 import com.appsmith.server.repositories.CustomDatasourceStorageStructureRepository;
-import reactor.core.publisher.Mono;
 
 public interface DatasourceStorageStructureRepositoryCE
-        extends BaseRepository<DatasourceStorageStructure, String>, CustomDatasourceStorageStructureRepository {
-
-    Mono<DatasourceStorageStructure> findByDatasourceIdAndEnvironmentId(String datasourceId, String environmentId);
-}
+        extends BaseRepository<DatasourceStorageStructure, String>, CustomDatasourceStorageStructureRepository {}
