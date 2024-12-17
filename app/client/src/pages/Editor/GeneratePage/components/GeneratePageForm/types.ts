c0cd8f38c1d538bebc74d536a23f7b89a74a7c78
Jacques Ikot <jacquesikot@gmail.com>
2024-12-17 08:58:06 +0100

    feat: remove generatePage folder

--- app/client/src/pages/Editor/GeneratePage/components/GeneratePageForm/types.ts
+++
@@ -1,8 +0,0 @@
-export interface GeneratePagePayload {
-  tableName: string;
-  columns?: string[];
-  searchColumn?: string;
-  // TODO: Fix this the next time the file is edited
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  pluginSpecificParams?: Record<any, any>;
-}
