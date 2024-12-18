065d489dd0c7694839c10264af8a27b22e6a1bf3
Hetu Nandu <hetunandu@gmail.com>
2024-12-18 17:12:07 +0530

    chore: Add bold selected segment

--- app/client/packages/design-system/ads/src/Templates/EntityExplorer/EditorSegments/EditorSegments.styles.ts
+++ app/client/packages/design-system/ads/src/Templates/EntityExplorer/EditorSegments/EditorSegments.styles.ts
@@ -4,5 +4,13 @@ import { Flex } from "../../../Flex";
 export const Container = styled(Flex)`
   .editor-pane-segment-control {
     max-width: 247px;
+
+    .ads-v2-segmented-control__segments-container-segment {
+      &[data-selected="true"] {
+        span {
+          font-weight: var(--ads-v2-font-weight-bold);
+        }
+      }
+    }
   }
 `;
