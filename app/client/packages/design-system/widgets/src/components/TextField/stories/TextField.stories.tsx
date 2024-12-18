c556bda0ecbae89388821185ab86d340c553bc1e
Valera Melnikov <valera@appsmith.com>
2024-12-18 18:58:43 +0300

    chore: add custom consistent-storybook-title rule

--- app/client/packages/design-system/widgets/src/components/TextField/stories/TextField.stories.tsx
+++ app/client/packages/design-system/widgets/src/components/TextField/stories/TextField.stories.tsx
@@ -4,7 +4,7 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Flex, Icon, TextField, Button } from "@appsmith/wds";
 
 const meta: Meta<typeof TextField> = {
-  title: "WDS/Widgets/TextField",
+  title: "WDS/Widgets/Text Field",
   component: TextField,
   tags: ["autodocs"],
   args: {
