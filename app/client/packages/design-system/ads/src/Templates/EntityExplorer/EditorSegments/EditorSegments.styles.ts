a94e072ab76b5a1146f25dd822576c6b01e57c1e
Hetu Nandu <hetunandu@gmail.com>
2024-12-19 14:06:27 +0530

    chore: Apply bold styles on the segment itself

--- app/client/packages/design-system/ads/src/Templates/EntityExplorer/EditorSegments/EditorSegments.styles.ts
+++ app/client/packages/design-system/ads/src/Templates/EntityExplorer/EditorSegments/EditorSegments.styles.ts
@@ -4,13 +4,5 @@ import { Flex } from "../../../Flex";
 export const Container = styled(Flex)`
   .editor-pane-segment-control {
     max-width: 247px;
-
-    .ads-v2-segmented-control__segments-container-segment {
-      &[data-selected="true"] {
-        span {
-          font-weight: var(--ads-v2-font-weight-bold);
-        }
-      }
-    }
   }
 `;
