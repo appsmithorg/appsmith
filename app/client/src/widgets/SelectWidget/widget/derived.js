86e3c8966151f4cdc281a6b1fd49bc8cfa11efcc
Rahul Barwal <rahul.barwal@appsmith.com>
2024-12-19 13:58:41 +0530

    Enhance SelectWidget label and value handling logic
    - Updated the label extraction logic to support both static and dynamic keys from sourceData.
    - Improved value mapping to handle cases where optionValue is an array, ensuring consistency in label and value retrieval.
    - Added detailed documentation comments to clarify the expected structure of sourceData and the behavior of optionLabel and optionValue props.
    
    This change enhances the flexibility and usability of the SelectWidget component.

--- app/client/src/widgets/SelectWidget/widget/derived.js
+++ app/client/src/widgets/SelectWidget/widget/derived.js
@@ -5,16 +5,57 @@ export default {
       values = [],
       sourceData = props.sourceData || [];
 
+    /**
+     * SourceData:
+     *  [{
+     *    "name": "Blue",
+     *    "code": "name"
+     *  },{
+     *    "name": "Green",
+     *    "code": "name"
+     *  },{
+     *    "name": "Red",
+     *    "code": "name"
+     *  }]
+     * The `Label key` in UI can take following values:
+     * 1. Normal string, without any quotes. e.g `name`
+     *    This can be assumed as a key in each item of sourceData. We search it in each item of sourceData.
+     * 2. Except this everything comes in `{{}}`. It can have 2 types of values:
+     *    a. Expressions that evaluate to a normal string. e.g `{{(() => `name`)()}}`
+     *        In this case evaluated value will be ['name', 'name', 'name'].
+     *        i. This can be assumed as a key in each item of sourceData. Handled by `allLabelsEqual` check.
+     *    b. Dynamic property accessed via `item` object. e.g `{{item.name}}`
+     *        In this case evaluated value will be actual values form sourceData ['Red', 'Green', 'Blue'].
+     *        Hence we can assume that this array is the labels array.
+     * */
     if (typeof props.optionLabel === "string") {
       labels = sourceData.map((d) => d[props.optionLabel]);
     } else if (_.isArray(props.optionLabel)) {
-      labels = sourceData.map((d, i) => d[props.optionLabel[i]]);
+      const allLabelsEqual = props.optionLabel.every(
+        (label, _, arr) => label === arr[0],
+      );
+      const doesKeyExistInSourceData = props.optionLabel[0] in sourceData[0];
+
+      if (allLabelsEqual && doesKeyExistInSourceData) {
+        labels = sourceData.map((d, i) => d[props.optionLabel[0]]);
+      } else {
+        labels = props.optionLabel;
+      }
     }
 
     if (typeof props.optionValue === "string") {
       values = sourceData.map((d) => d[props.optionValue]);
     } else if (_.isArray(props.optionValue)) {
-      values = sourceData.map((d, i) => d[props.optionValue[i]]);
+      const allValuesEqual = props.optionValue.every(
+        (value, _, arr) => value === arr[0],
+      );
+      const doesKeyExistInSourceData = props.optionValue[0] in sourceData[0];
+
+      if (allValuesEqual && doesKeyExistInSourceData) {
+        values = sourceData.map((d, i) => d[props.optionValue[0]]);
+      } else {
+        values = props.optionValue;
+      }
     }
 
     return sourceData.map((d, i) => ({
