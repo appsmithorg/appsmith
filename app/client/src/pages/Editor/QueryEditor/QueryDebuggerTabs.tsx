e8b73e8ea265ee10ae03ba15ce50e8a4087ea4a3
Ankita Kinger <ankita@appsmith.com>
2024-12-20 09:52:47 +0530

    fixing client build

--- app/client/src/pages/Editor/QueryEditor/QueryDebuggerTabs.tsx
+++ app/client/src/pages/Editor/QueryEditor/QueryDebuggerTabs.tsx
@@ -12,7 +12,7 @@ import {
 } from "ee/constants/messages";
 import DebuggerLogs from "components/editorComponents/Debugger/DebuggerLogs";
 import ErrorLogs from "components/editorComponents/Debugger/Errors";
-import { Datasource } from "PluginActionEditor/components/PluginActionResponse/components/DatasourceTab";
+import { DatasourceTab } from "PluginActionEditor/components/PluginActionResponse/components/DatasourceTab";
 import type { ActionResponse } from "api/ActionAPI";
 import type { SourceEntity } from "entities/AppsmithConsole";
 import type { Action } from "entities/Action";
@@ -219,7 +219,7 @@ function QueryDebuggerTabs({
       key: DEBUGGER_TAB_KEYS.DATASOURCE_TAB,
       title: "Datasource",
       panelComponent: (
-        <Datasource
+        <DatasourceTab
           currentActionId={currentActionConfig.id}
           datasourceId={currentActionConfig.datasource.id || ""}
           datasourceName={datasource?.name || ""}
