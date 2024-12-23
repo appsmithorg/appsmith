7eca0c73449c3c4fb3f8ffa9ba9a1d46a2213cda
Nilesh Sarupriya <20905988+nsarupr@users.noreply.github.com>
2024-12-23 12:03:07 +0530

    add CE file for extension

--- app/server/appsmith-plugins/appsmithAiPlugin/src/main/java/com/external/plugins/AppsmithAiPlugin.java
+++ app/server/appsmith-plugins/appsmithAiPlugin/src/main/java/com/external/plugins/AppsmithAiPlugin.java
@@ -26,6 +26,8 @@ import com.external.plugins.services.AiFeatureService;
 import com.external.plugins.services.AiFeatureServiceFactory;
 import com.external.plugins.services.AiServerService;
 import com.external.plugins.services.AiServerServiceImpl;
+import com.external.plugins.services.TriggerService;
+import com.external.plugins.services.TriggerServiceImpl;
 import com.external.plugins.utils.RequestUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -34,16 +36,10 @@ import org.pf4j.PluginWrapper;
 import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static com.appsmith.external.constants.CommonFieldName.VALUE;
-import static com.external.plugins.constants.AppsmithAiConstants.DISABLED;
-import static com.external.plugins.constants.AppsmithAiConstants.LABEL;
-import static com.external.plugins.constants.AppsmithAiConstants.LIST_FILES;
-import static com.external.plugins.constants.AppsmithAiConstants.UPLOAD_FILES;
 import static com.external.plugins.constants.AppsmithAiConstants.USECASE;
 import static com.external.plugins.utils.FileUtils.getFileIds;
 import static com.external.plugins.utils.FileUtils.hasFiles;
@@ -57,6 +53,8 @@ public class AppsmithAiPlugin extends BasePlugin {
 
     public static class AppsmithAiPluginExecutor extends BaseRestApiPluginExecutor {
         private static final AiServerService aiServerService = new AiServerServiceImpl();
+
+        private static final TriggerService triggerService = new TriggerServiceImpl();
         private static final Gson gson = new GsonBuilder().create();
 
         public AppsmithAiPluginExecutor(SharedConfig config) {
@@ -80,57 +78,7 @@ public class AppsmithAiPlugin extends BasePlugin {
         public Mono<TriggerResultDTO> trigger(
                 APIConnection connection, DatasourceConfiguration datasourceConfiguration, TriggerRequestDTO request) {
             log.debug(Thread.currentThread().getName() + ": trigger() called for AppsmithAI plugin.");
-            String requestType = request.getRequestType();
-            return switch (requestType) {
-                case UPLOAD_FILES -> uploadFiles(request);
-                case LIST_FILES -> listFiles(datasourceConfiguration, request);
-                default -> super.trigger(connection, datasourceConfiguration, request);
-            };
-        }
-
-        private Mono<TriggerResultDTO> uploadFiles(TriggerRequestDTO request) {
-            SourceDetails sourceDetails = SourceDetails.createSourceDetails(request);
-            return aiServerService
-                    .uploadFiles(request.getFiles(), sourceDetails)
-                    .flatMap(response -> {
-                        TriggerResultDTO triggerResultDTO = new TriggerResultDTO();
-                        triggerResultDTO.setTrigger(response);
-                        return Mono.just(triggerResultDTO);
-                    })
-                    .onErrorResume(error -> handleError("An error has occurred while trying to upload files", error));
-        }
-
-        private Mono<TriggerResultDTO> listFiles(
-                DatasourceConfiguration datasourceConfiguration, TriggerRequestDTO request) {
-            SourceDetails sourceDetails = SourceDetails.createSourceDetails(request);
-            List<String> fileIds = getFileIds(datasourceConfiguration);
-            if (fileIds.isEmpty()) {
-                TriggerResultDTO triggerResultDTO = new TriggerResultDTO();
-                triggerResultDTO.setTrigger(List.of(Map.of(
-                        DISABLED, true, LABEL, "No files available in the datasource", VALUE, "NO_FILES_AVAILABLE")));
-                return Mono.just(triggerResultDTO);
-            }
-            return aiServerService
-                    .getFilesStatus(fileIds, sourceDetails)
-                    .flatMap(fileStatusDTO -> {
-                        List<Map<String, Object>> response = new ArrayList<>();
-                        fileStatusDTO.getFiles().forEach(file -> {
-                            Map<String, Object> dropdownOption = new HashMap<>();
-                            if (!file.isProcessed()) {
-                                dropdownOption.put(LABEL, "(Processing...) " + file.getName());
-                            } else {
-                                dropdownOption.put(LABEL, file.getName());
-                            }
-                            dropdownOption.put(VALUE, file.getId());
-                            dropdownOption.put(DISABLED, !file.isProcessed());
-                            response.add(dropdownOption);
-                        });
-                        TriggerResultDTO triggerResultDTO = new TriggerResultDTO();
-                        triggerResultDTO.setTrigger(response);
-                        return Mono.just(triggerResultDTO);
-                    })
-                    .onErrorResume(
-                            error -> handleError("An error has occurred while trying to list uploaded files", error));
+            return triggerService.executeTrigger(connection, datasourceConfiguration, request);
         }
 
         private Mono<TriggerResultDTO> handleError(String message, Throwable error) {
