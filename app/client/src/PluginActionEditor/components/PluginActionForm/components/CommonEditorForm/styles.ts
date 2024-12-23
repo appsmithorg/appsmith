fdf874aafbb38d9de1000c8e307f1a7c52662d91
Alex Golovanov <alex@appsmith.com>
2024-12-23 14:31:18 +0300

    fix: co-locate request tabs within editor form & refactor overflow

--- app/client/src/PluginActionEditor/components/PluginActionForm/components/CommonEditorForm/styles.ts
+++ app/client/src/PluginActionEditor/components/PluginActionForm/components/CommonEditorForm/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Tabs as AdsTabs, TabPanel as AdsTabPanel } from "@appsmith/ads";
 
 export const RequestMethodSelectContainer = styled.div`
   width: 100px;
@@ -9,4 +10,27 @@ export const RequestMethodSelectContainer = styled.div`
 
 export const DatasourcePathFieldContainer = styled.div`
   width: 100%;
+`;
+
+export const FormHeader = styled.div`
+  position: sticky;
+  grid-area: header;
+  top: calc(-1 * var(--ads-v2-spaces-4));
+  padding-top: var(--ads-v2-spaces-4);
+  margin-top: calc(-1 * var(--ads-v2-spaces-4));
+  align-self: start;
+  z-index: 100;
+  background-color: var(--ads-color-background);
+`;
+
+export const Tabs = styled(AdsTabs)`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: auto 1fr;
+  grid-template-areas: "header" "body";
+  width: 100%;
+`;
+
+export const TabPanel = styled(AdsTabPanel)`
+  height: calc(100% - 50px);
 `;
