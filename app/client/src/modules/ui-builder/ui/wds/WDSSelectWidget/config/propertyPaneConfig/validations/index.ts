ecf9934859b6fc7789099fbda996754ce60cec7c
Pawan Kumar <pawan@appsmith.com>
2024-12-24 17:28:57 +0530

    chore: fix wds select widget bugs + refactor (#38304)
    Fixes #38197 
    
    /ok-to-test tags="@tag.Anvil"
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced styling for select input components, improving visual
    consistency.
    - Introduced new validation functions for widget properties, ensuring
    better data integrity.
    - Added properties for improved configuration of select widgets,
    including dynamic data binding.
    - New utility functions for handling options in select widgets,
    enhancing functionality.
    - Introduced a constant for sample data to facilitate testing and
    development.
    
    - **Bug Fixes**
    - Resolved issues with widget rendering and responsiveness to property
    changes.
    
    - **Refactor**
    - Streamlined widget implementation by leveraging inherited
    functionalities and simplifying methods.
    - Updated methods to improve handling of derived properties and options.
    - Removed obsolete configuration files and validation functions to clean
    up the codebase.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/12480977859>
    > Commit: d669e05ed5b7ce07a54259b7e301eb65341c1b02
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=12480977859&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Anvil`
    > Spec:
    > <hr>Tue, 24 Dec 2024 11:43:59 UTC
    <!-- end of auto-generated comment: Cypress test results  -->

--- app/client/src/modules/ui-builder/ui/wds/WDSSelectWidget/config/propertyPaneConfig/validations/index.ts
+++ app/client/src/modules/ui-builder/ui/wds/WDSSelectWidget/config/propertyPaneConfig/validations/index.ts
@@ -1,2 +1,1 @@
-export { defaultOptionValidation } from "./defaultOptionValidation";
-export { optionsCustomValidation } from "./optionsCustomValidation";
+export { defaultOptionValueValidation } from "./defaultOptionValueValidation";
