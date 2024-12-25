1f7c8ff7f0eeeadc8b53746ef039086f490d548e
sondermanish <sonder.manish@gmail.com>
2024-12-25 21:51:40 +0530

    added autocommit and protected branches methods

--- app/server/appsmith-server/src/main/java/com/appsmith/server/git/central/CentralGitServiceImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/git/central/CentralGitServiceImpl.java
@@ -3,6 +3,7 @@ package com.appsmith.server.git.central;
 import com.appsmith.server.datasources.base.DatasourceService;
 import com.appsmith.server.exports.internal.ExportService;
 import com.appsmith.server.git.GitRedisUtils;
+import com.appsmith.server.git.autocommit.helpers.GitAutoCommitHelper;
 import com.appsmith.server.git.resolver.GitArtifactHelperResolver;
 import com.appsmith.server.git.resolver.GitHandlingServiceResolver;
 import com.appsmith.server.git.utils.GitAnalyticsUtils;
@@ -17,42 +18,13 @@ import com.appsmith.server.solutions.DatasourcePermission;
 import io.micrometer.observation.ObservationRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.reactive.TransactionalOperator;
 
 @Slf4j
 @Service
 public class CentralGitServiceImpl extends CentralGitServiceCECompatibleImpl implements CentralGitService {
 
-    public CentralGitServiceImpl(
-            GitProfileUtils gitProfileUtils,
-            GitAnalyticsUtils gitAnalyticsUtils,
-            UserDataService userDataService,
-            SessionUserService sessionUserService,
-            GitArtifactHelperResolver gitArtifactHelperResolver,
-            GitHandlingServiceResolver gitHandlingServiceResolver,
-            GitPrivateRepoHelper gitPrivateRepoHelper,
-            DatasourceService datasourceService,
-            DatasourcePermission datasourcePermission,
-            WorkspaceService workspaceService,
-            PluginService pluginService,
-            ImportService importService,
-            ExportService exportService,
-            GitRedisUtils gitRedisUtils,
-            ObservationRegistry observationRegistry) {
-        super(
-                gitProfileUtils,
-                gitAnalyticsUtils,
-                userDataService,
-                sessionUserService,
-                gitArtifactHelperResolver,
-                gitHandlingServiceResolver,
-                gitPrivateRepoHelper,
-                datasourceService,
-                datasourcePermission,
-                workspaceService,
-                pluginService,
-                importService,
-                exportService,
-                gitRedisUtils,
-                observationRegistry);
+    public CentralGitServiceImpl(GitRedisUtils gitRedisUtils, GitProfileUtils gitProfileUtils, GitAnalyticsUtils gitAnalyticsUtils, UserDataService userDataService, SessionUserService sessionUserService, GitArtifactHelperResolver gitArtifactHelperResolver, GitHandlingServiceResolver gitHandlingServiceResolver, GitPrivateRepoHelper gitPrivateRepoHelper, DatasourceService datasourceService, DatasourcePermission datasourcePermission, WorkspaceService workspaceService, PluginService pluginService, ImportService importService, ExportService exportService, GitAutoCommitHelper gitAutoCommitHelper, TransactionalOperator transactionalOperator, ObservationRegistry observationRegistry) {
+        super(gitRedisUtils, gitProfileUtils, gitAnalyticsUtils, userDataService, sessionUserService, gitArtifactHelperResolver, gitHandlingServiceResolver, gitPrivateRepoHelper, datasourceService, datasourcePermission, workspaceService, pluginService, importService, exportService, gitAutoCommitHelper, transactionalOperator, observationRegistry);
     }
 }
