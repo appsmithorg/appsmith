7e053395ee581021db07aca61543a879d3a0abbd
Sagar Khalasi <sagar@appsmith.com>
2025-01-03 10:23:43 +0530

    Branding image related test case

--- app/client/cypress/e2e/Regression/ClientSide/AdminSettings/Branding_settings_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/AdminSettings/Branding_settings_Spec.ts
@@ -1,17 +1,108 @@
 import {
   agHelper,
   adminSettings,
+  locators,
 } from "../../../../support/Objects/ObjectsCore";
-import { CURRENT_REPO, REPO } from "../../../../fixtures/REPO";
-import { featureFlagIntercept } from "../../../../support/Objects/FeatureFlags";
+import AdminsSettings from "../../../../locators/AdminsSettings";
 
 describe(
   "Admin Branding Page - Branding page validations",
   { tags: ["@tag.Settings"] },
   () => {
     it("1. Verify branding data update for community user", () => {
-      featureFlagIntercept({ license_branding_enabled: true });
       adminSettings.NavigateToAdminSettings();
+      agHelper.AssertElementVisibility(AdminsSettings.LeftPaneBrandingLink);
+      agHelper.GetNClick(AdminsSettings.LeftPaneBrandingLink);
+
+      // branding logo PNG
+      cy.get(AdminsSettings.AdmingSettingsLogoInput).selectFile(
+        "cypress/fixtures/appsmithlogo.png",
+        { force: true },
+      );
+      agHelper.AssertElementAbsence(locators._toastMsg);
+      agHelper.AssertElementVisibility(
+        AdminsSettings.AdmingSettingsLogoInputImage,
+      );
+      agHelper.WaitForCondition(() => {
+        cy.get(AdminsSettings.BrandingLogo)
+          .invoke("attr", "src")
+          .then((src) => {
+            cy.get(AdminsSettings.AdmingSettingsLogoInputImage)
+              .invoke("attr", "src")
+              .should("equal", src);
+          });
+      });
+
+      // branding logo jpg
+      cy.get(AdminsSettings.AdmingSettingsLogoInput).selectFile(
+        "cypress/fixtures/AAAFlowerVase.jpeg",
+        { force: true },
+      );
+      agHelper.AssertElementAbsence(locators._toastMsg);
+      agHelper.AssertElementVisibility(
+        AdminsSettings.AdmingSettingsLogoInputImage,
+      );
+      agHelper.WaitForCondition(() => {
+        cy.get(AdminsSettings.BrandingLogo)
+          .invoke("attr", "src")
+          .then((src) => {
+            cy.get(AdminsSettings.AdmingSettingsLogoInputImage)
+              .invoke("attr", "src")
+              .should("equal", src);
+          });
+      });
+
+      // branding logo svg
+      // comment due to bug: https://github.com/appsmithorg/appsmith/issues/34329
+      // cy.get(AdminsSettings.AdmingSettingsLogoInput).selectFile(
+      //   "cypress/fixtures/appsmith-community-logo.svg",
+      //   { force: true },
+      // );
+      // agHelper.AssertElementAbsence(locators._toastMsg);
+
+      // agHelper.AssertElementVisibility(
+      //   AdminsSettings.AdmingSettingsLogoInputImage,
+      // );
+      // agHelper.WaitForCondition(() => {
+      //   cy.get(AdminsSettings.BrandingLogo)
+      //     .invoke("attr", "src")
+      //     .then((src) => {
+      //       cy.get(AdminsSettings.AdmingSettingsLogoInputImage)
+      //         .invoke("attr", "src")
+      //         .should("equal", src);
+      //     });
+      // });
+
+      // branding favicon png
+      cy.get(AdminsSettings.AdmingSettingsFaviconInput).selectFile(
+        "cypress/fixtures/branding_sample.png",
+        { force: true },
+      );
+      agHelper.AssertElementAbsence(locators._toastMsg);
+      agHelper.AssertElementVisibility(
+        AdminsSettings.AdmingSettingsFaviconInputImage,
+      );
+      cy.get(AdminsSettings.BrandingFavicon)
+        .invoke("attr", "src")
+        .then((src) => {
+          cy.get(AdminsSettings.AdmingSettingsFaviconInputImage)
+            .invoke("attr", "src")
+            .should("equal", src);
+        });
+
+      // branding favicon jpg
+      cy.get(AdminsSettings.AdmingSettingsFaviconInput).selectFile(
+        "cypress/fixtures/branding_samplejpg.jpg",
+        { force: true },
+      );
+      agHelper.AssertElementAbsence(locators._toastMsg);
+
+      // branding favicon ico
+      cy.get(AdminsSettings.AdmingSettingsFaviconInput).selectFile(
+        "cypress/fixtures/branding_sampleICO.ico",
+        { force: true },
+      );
+      agHelper.AssertElementAbsence(locators._toastMsg);
     });
   },
 );
