3760bbd998b639570df773731ad50f4a78257067
“NandanAnantharamu” <nandan@thinkify.io>
2025-01-04 15:54:03 +0530

    updated assetion and locators

--- app/client/cypress/e2e/Regression/ClientSide/PartialImportExport/VerifyImport_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/PartialImportExport/VerifyImport_spec.ts
@@ -3,6 +3,7 @@ import {
   homePage,
   gitSync,
   appSettings,
+  locators,
 } from "../../../../support/Objects/ObjectsCore";
 import EditorNavigation, {
   AppSidebar,
@@ -19,6 +20,29 @@ describe(
     });
 
     it("1. Verify Import Option", () => {
+      let Datasource = [
+        "AWSLambda",
+        "Airtable",
+        "GSheets_RWDSelected",
+        "GSheets_RWDAll",
+        "Hubspot",
+        "gsheet",
+        "Twilio",
+        "Dynamo",
+        "ElasticSearch",
+        "Firestore",
+        "Movies",
+        "Mongo",
+        "Oracle",
+        "Redshift",
+        "PostGreSQL",
+        "SMTP",
+        "Snowflake",
+        "S3",
+        "Oauth2.0",
+        "Pixabay",
+        "OpenAI",
+      ];
       AppSidebar.navigate(AppSidebarButton.Settings);
       agHelper.GetNClick(appSettings.locators._importHeader);
       agHelper.AssertElementEnabledDisabled(appSettings.locators._importBtn);
@@ -35,6 +59,11 @@ describe(
       agHelper.GetNClick(appSettings.locators._importBtn);
       homePage.ImportApp("TryToCoverMore.json", "", true);
       agHelper.GetNClick(ReconnectLocators.SkipToAppBtn);
+
+      AppSidebar.navigate(AppSidebarButton.Data);
+      Datasource.forEach((ds) => {
+        agHelper.GetNAssertContains(locators._listItemTitle, ds);
+      });
     });
   },
 );
