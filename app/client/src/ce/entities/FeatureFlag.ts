fe2ab7b4f6b5669bf1cca8f9982fd078b4adc4fc
Hetu Nandu <hetunandu@gmail.com>
2025-01-06 17:25:41 +0530

    chore: Update flag name

--- app/client/src/ce/entities/FeatureFlag.ts
+++ app/client/src/ce/entities/FeatureFlag.ts
@@ -55,7 +55,7 @@ export const FEATURE_FLAG = {
     "config_mask_session_recordings_enabled",
   config_user_session_recordings_enabled:
     "config_user_session_recordings_enabled",
-  release_new_entity_item_enabled: "release_new_entity_item_enabled",
+  release_ads_entity_item_enabled: "release_ads_entity_item_enabled",
 } as const;
 
 export type FeatureFlag = keyof typeof FEATURE_FLAG;
@@ -102,7 +102,7 @@ export const DEFAULT_FEATURE_FLAG_VALUE: FeatureFlags = {
   kill_session_recordings_enabled: false,
   config_user_session_recordings_enabled: true,
   config_mask_session_recordings_enabled: true,
-  release_new_entity_item_enabled: false,
+  release_ads_entity_item_enabled: false,
 };
 
 export const AB_TESTING_EVENT_KEYS = {
