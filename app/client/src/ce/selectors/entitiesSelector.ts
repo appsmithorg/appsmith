f8784fcf6b1f6a2641d67a87dddc569963372e64
Hetu Nandu <hetu@appsmith.com>
2025-01-13 13:54:30 +0530

    feat: ADS Entity List Tree (#38493)
    Co-authored-by: Ankita Kinger <ankita@appsmith.com>

--- app/client/src/ce/selectors/entitiesSelector.ts
+++ app/client/src/ce/selectors/entitiesSelector.ts
@@ -1072,9 +1072,6 @@ export const getExistingActionNames = createSelector(
   },
 );
 
-export const getEditingEntityName = (state: AppState) =>
-  state.ui.explorer.entity.editingEntityName;
-
 export const getExistingJSCollectionNames = createSelector(
   getJSCollections,
   (jsActions) =>
