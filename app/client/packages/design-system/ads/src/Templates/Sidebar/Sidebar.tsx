fe484aa08f51865e1ed2638de0717536b8ac2b7b
Hetu Nandu <hetunandu@gmail.com>
2025-01-14 15:32:12 +0530

    chore: Reorg folder structure

--- app/client/packages/design-system/ads/src/Templates/Sidebar/Sidebar.tsx
+++ app/client/packages/design-system/ads/src/Templates/Sidebar/Sidebar.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 import SidebarButton from "./SidebarButton";
 import type { SidebarButtonProps } from "./SidebarButton/SidebarButton";
-import { Flex } from "@appsmith/ads";
-
-const Container = styled(Flex)`
-  width: 50px;
-  border-right: 1px solid var(--ads-v2-color-border);
-  height: 100%;
-  flex-direction: column;
-  justify-content: space-between;
-  background-color: var(--ads-v2-color-bg);
-  position: relative;
-`;
+import * as Styled from "./styles";
 
 // Sidebar handles the correct handling of sidebar button. It will check if
 // the button should be selected and only handle calling the onClick
@@ -34,7 +23,7 @@ export function IDESidebar(props: IDESidebarProps) {
   const { bottomButtons, editorState, onClick, topButtons } = props;
 
   return (
-    <Container className="t--sidebar" id={props.id}>
+    <Styled.Container className="t--sidebar" id={props.id}>
       <div>
         {topButtons.map((button) => (
           <SidebarButton
@@ -65,6 +54,6 @@ export function IDESidebar(props: IDESidebarProps) {
           />
         ))}
       </div>
-    </Container>
+    </Styled.Container>
   );
 }
--- app/client/packages/design-system/ads/src/Templates/Sidebar/SidebarButton/SidebarButton.tsx
+++ app/client/packages/design-system/ads/src/Templates/Sidebar/SidebarButton/SidebarButton.tsx
@@ -1,21 +1,11 @@
 import React, { useCallback } from "react";
-import { Flex } from "../../../Flex";
 import { Icon } from "../../../Icon";
 import { Text } from "../../../Text";
 import { Tooltip } from "../../../Tooltip";
-import styled from "styled-components";
 
-import { Condition } from "../enums";
-
-const ConditionConfig: Record<Condition, { icon: string; color: string }> = {
-  [Condition.Warn]: {
-    icon: "warning",
-    color: "#ffe283",
-  },
-  // TODO add this information for further conditions
-  // Error: { color: "", icon: "" },
-  // Success: { color: "", icon: "" },
-};
+import type { Condition } from "../enums";
+import { ConditionConfig } from "./constants";
+import * as Styled from "./styles";
 
 export interface SidebarButtonProps {
   title?: string;
@@ -28,51 +18,6 @@ export interface SidebarButtonProps {
   condition?: Condition;
 }
 
-const Container = styled(Flex)`
-  justify-content: center;
-  flex-direction: column;
-  width: 50px;
-  text-align: center;
-  align-items: center;
-  padding: 8px 0;
-`;
-
-const IconContainer = styled.div`
-  padding: 2px;
-  border-radius: 3px;
-  width: 32px;
-  height: 32px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  position: relative;
-
-  &[data-selected="false"] {
-    background-color: var(--ads-v2-color-bg);
-
-    &:hover {
-      background-color: var(--ads-v2-color-bg-subtle, #f1f5f9);
-    }
-  }
-
-  &[data-selected="true"] {
-    background-color: var(--ads-v2-color-bg-muted);
-  }
-`;
-
-const ConditionIcon = styled(Icon)`
-  position: absolute;
-  bottom: 3px;
-  right: -1px;
-
-  &.t--sidebar-${Condition.Warn}-condition-icon {
-    color: ${ConditionConfig[Condition.Warn].color};
-  }
-
-  // TODO add more condition colors here
-`;
-
 function SidebarButton(props: SidebarButtonProps) {
   const { condition, icon, onClick, selected, title, tooltip, urlSuffix } =
     props;
@@ -83,13 +28,13 @@ function SidebarButton(props: SidebarButtonProps) {
   }, [selected, onClick, urlSuffix]);
 
   return (
-    <Container>
+    <Styled.Container>
       <Tooltip
         content={tooltip}
         isDisabled={!!title && !tooltip}
         placement={"right"}
       >
-        <IconContainer
+        <Styled.IconContainer
           className={`t--sidebar-${title || tooltip}`}
           data-selected={selected}
           data-testid={"t--sidebar-" + props.testId}
@@ -98,17 +43,17 @@ function SidebarButton(props: SidebarButtonProps) {
         >
           <Icon name={icon} size="lg" />
           {condition && (
-            <ConditionIcon
+            <Styled.ConditionIcon
               className={`t--sidebar-${condition}-condition-icon`}
               data-testid={`t--sidebar-${condition}-condition-icon`}
               name={ConditionConfig[condition].icon}
               size="md"
             />
           )}
-        </IconContainer>
+        </Styled.IconContainer>
       </Tooltip>
       {title ? <Text kind="body-s">{title}</Text> : null}
-    </Container>
+    </Styled.Container>
   );
 }
 
