56f968fb074fd153458aab52def91e1d757fa807
Sagar Khalasi <sagar@appsmith.com>
2025-01-16 20:24:47 +0530

    fix: Flaky test case fix (#38703)
    ## Description
    Code improvement
    
    
    Fixes #`Issue Number`  
    _or_  
    Fixes `Issue URL`
    > [!WARNING]  
    > _If no issue exists, please create an issue first, and check with the
    maintainers if the issue is valid._
    
    ## Automation
    
    /ok-to-test tags="@tag.Settings"
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/12808540560>
    > Commit: a5bfdaf3c397f05bd73d66a0fe57a84b92a36944
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=12808540560&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Settings`
    > Spec:
    > <hr>Thu, 16 Jan 2025 12:33:05 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [x] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **Tests**
    - Updated test cases for admin settings to improve interaction
    reliability
    	- Modified save button interaction and element visibility checks
    - Updated test expectations for instance name changes in Enterprise
    Edition
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/client/cypress/e2e/Regression/ClientSide/AdminSettings/General_settings_Spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/AdminSettings/General_settings_Spec.ts
@@ -4,6 +4,7 @@ import {
 } from "../../../../support/Objects/ObjectsCore";
 import { CURRENT_REPO, REPO } from "../../../../fixtures/REPO";
 import { featureFlagIntercept } from "../../../../support/Objects/FeatureFlags";
+import AdminsSettings from "../../../../locators/AdminsSettings";
 
 describe(
   "Admin Settings Page - General page validations",
@@ -12,13 +13,15 @@ describe(
     it("1. TC# 2439 Verify 'Page title' changes upon changing Instance name", () => {
       featureFlagIntercept({ license_branding_enabled: true });
       adminSettings.NavigateToAdminSettings();
+      agHelper.WaitUntilEleAppear(adminSettings._instanceName);
       agHelper.ClearNType(adminSettings._instanceName, "Testing Instance name");
-      agHelper.ClickButton("Save");
+      agHelper.ClickOutside();
+      agHelper.GetNClick(AdminsSettings.saveButton, 0, true);
       agHelper.ValidateToastMessage("Successfully saved");
       if (CURRENT_REPO === REPO.CE) cy.title().should("eq", "Appsmith");
       //verifying that Instance name is not changed in CE
       else if (CURRENT_REPO === REPO.EE)
-        cy.title().should("eq", "Testing Instance name"); //verifying that Instance name is not changed in EE
+        cy.title().should("eq", "Testing Instance name"); //verifying that Instance name is changed in EE
     });
     it("2. TC# 2439 Verify 'Page title' does not change upon changing Instance name in free plan", () => {
       featureFlagIntercept({ license_branding_enabled: false });
