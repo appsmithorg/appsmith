5685b006eaed9805484b5003e6d49cb372d72945
Aman Agarwal <aman@appsmith.com>
2025-01-17 15:52:45 +0530

    fix: moved css to integration directory, export type error fix

--- app/client/src/ee/pages/Editor/DataSourceEditor/ExternalSaasConnection.tsx
+++ app/client/src/ee/pages/Editor/DataSourceEditor/ExternalSaasConnection.tsx
@@ -1,1 +1,3 @@
 export * from "ce/pages/Editor/DataSourceEditor/ExternalSaasConnection";
+import { default as CE_ExternalSaasConnection } from "ce/pages/Editor/DataSourceEditor/ExternalSaasConnection";
+export default CE_ExternalSaasConnection;
