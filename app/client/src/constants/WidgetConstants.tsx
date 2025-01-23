7ef090b71e51ef1b02e7655f8ae3a70fdf2db9f7
Haj Rezvan <rezvanianragahi2@gmail.com>
2025-01-23 10:40:45 +0330

    🔧: established naming convention for widget tags

--- app/client/src/constants/WidgetConstants.tsx
+++ app/client/src/constants/WidgetConstants.tsx
@@ -241,7 +241,7 @@ export const WIDGET_TAGS = {
   CONTENT: "Content",
   EXTERNAL: "External",
   BUILDING_BLOCKS: "Building Blocks",
-  MYDATEPICKER : "MyDatePicker",
+  PERSIAN_DATE_PICKERS : "MyDatePicker",
 } as const;
 
 export type WidgetTags = (typeof WIDGET_TAGS)[keyof typeof WIDGET_TAGS];
