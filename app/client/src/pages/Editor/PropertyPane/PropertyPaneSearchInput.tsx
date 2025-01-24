2f6c1d68a978369f6fe33b6a7a31a193059121c5
Albin <albin@appsmith.com>
2025-01-24 09:41:52 +0530

    fix: reverted propertypanesearch input changes

--- app/client/src/pages/Editor/PropertyPane/PropertyPaneSearchInput.tsx
+++ app/client/src/pages/Editor/PropertyPane/PropertyPaneSearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import styled from "styled-components";
 import { SearchInput } from "@appsmith/ads";
 import { useSelector } from "react-redux";
@@ -6,6 +6,7 @@ import {
   getShouldFocusPanelPropertySearch,
   getShouldFocusPropertySearch,
 } from "selectors/propertyPaneSelectors";
+import { isCurrentFocusOnInput } from "utils/editorContextUtils";
 import { PROPERTY_SEARCH_INPUT_PLACEHOLDER } from "ee/constants/messages";
 
 const Container = styled.div`
@@ -28,24 +29,26 @@ interface PropertyPaneSearchInputProps {
 
 export function PropertyPaneSearchInput(props: PropertyPaneSearchInputProps) {
   const inputRef = useRef<HTMLInputElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const shouldFocusSearch = useSelector(getShouldFocusPropertySearch);
   const shouldFocusPanelSearch = useSelector(getShouldFocusPanelPropertySearch);
   const isPanel = !!props.isPanel;
 
-  // eslint-disable-next-line @appsmith/named-use-effect
   useEffect(() => {
     // Checks if the property pane opened not because of focusing an input inside a widget
     if (
-      shouldFocusSearch ||
+      shouldFocusSearch &&
       // while the panel transition happens, focus will be happening twice. Once on the main pane and then on the panel
       // The following check will make sure that the focus is only done once and prevents the UI jittering
-      (!shouldFocusSearch && isPanel === shouldFocusPanelSearch)
+      isPanel === shouldFocusPanelSearch
     ) {
       setTimeout(
         () => {
           //checking for active element
           //inside timeout to have updated active element
-          inputRef.current?.focus();
+          if (!isCurrentFocusOnInput()) {
+            wrapperRef.current?.focus();
+          }
         },
         // Layered panels like Column Panel's transition takes 300ms.
         // To avoid UI jittering, we are delaying the focus by 300ms.
@@ -54,15 +57,44 @@ export function PropertyPaneSearchInput(props: PropertyPaneSearchInputProps) {
     }
   }, [shouldFocusSearch, shouldFocusPanelSearch, isPanel]);
 
+  const handleInputKeydown = useCallback((e: KeyboardEvent) => {
+    switch (e.key) {
+      case "Escape":
+        wrapperRef.current?.focus();
+        break;
+    }
+  }, []);
+
+  const handleWrapperKeydown = useCallback((e: React.KeyboardEvent) => {
+    switch (e.key) {
+      case "Enter":
+        inputRef.current?.focus();
+        break;
+    }
+  }, []);
+
+  useEffect(() => {
+    inputRef.current?.addEventListener("keydown", handleInputKeydown);
+
+    return () => {
+      inputRef.current?.removeEventListener("keydown", handleInputKeydown);
+    };
+  }, []);
+
   return (
     <Container tabIndex={-1}>
-      <SearchInputWrapper>
+      <SearchInputWrapper
+        className="t--property-pane-search-input-wrapper"
+        onKeyDown={handleWrapperKeydown}
+        ref={wrapperRef}
+        tabIndex={0}
+      >
         <SearchInput
           className="propertyPaneSearch t--property-pane-search-input-wrapper"
           onChange={props.onTextChange}
           placeholder={PROPERTY_SEARCH_INPUT_PLACEHOLDER}
           ref={inputRef}
-          tabIndex={0}
+          tabIndex={-1}
         />
       </SearchInputWrapper>
     </Container>
