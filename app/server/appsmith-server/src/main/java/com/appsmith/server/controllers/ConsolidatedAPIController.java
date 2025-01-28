f91e4f79fbb2f6306ee840aa7eb1b3a6be7c67a7
Diljit VJ <diljit@appsmith.com>
2025-01-28 16:41:38 +0530

    add missing import after sync with release

--- app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ConsolidatedAPIController.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/controllers/ConsolidatedAPIController.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
