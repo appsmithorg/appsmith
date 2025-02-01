46e8606cf28197003177434b312860fd3461fcf3
Diljit VJ <diljit@appsmith.com>
2025-02-01 14:34:39 +0530

    chore: add logs in computeConsolidatedAPIResponseEtag fn

--- app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/ConsolidatedAPIServiceCEImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/services/ce/ConsolidatedAPIServiceCEImpl.java
@@ -648,6 +648,7 @@ public class ConsolidatedAPIServiceCEImpl implements ConsolidatedAPIServiceCE {
     @NotNull public String computeConsolidatedAPIResponseEtag(
             ConsolidatedAPIResponseDTO consolidatedAPIResponseDTO, String defaultPageId, String applicationId) {
         if (isBlank(defaultPageId) && isBlank(applicationId)) {
+            log.error("Skipping etag computation: Both defaultPageId and applicationId are blank");
             return "";
         }
 
@@ -664,6 +665,7 @@ public class ConsolidatedAPIServiceCEImpl implements ConsolidatedAPIServiceCE {
                     : null;
 
             if (lastDeployedAt == null) {
+                log.error("Skipping etag computation: lastDeployedAt is null");
                 return "";
             }
 
