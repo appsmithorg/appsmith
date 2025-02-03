d42691406d5cfeef5d5e56b6f814532b589bdcff
Vemparala Surya Vamsi <vamsi@appsmith.com>
2025-02-03 22:39:51 +0530

    first update bypass immer

--- app/client/src/utils/ReducerUtils.ts
+++ app/client/src/utils/ReducerUtils.ts
@@ -32,6 +32,16 @@ export const createImmerReducer = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return function reducer(state = initialState, action: ReduxAction<any>) {
     if (handlers.hasOwnProperty(action.type)) {
+      if (action?.payload?.updates) {
+        const updates = action?.payload?.updates;
+
+        for (const update of updates) {
+          if (update.kind === "newTree") {
+            return update.rhs;
+          }
+        }
+      }
+
       const fn = handlers[action.type];
 
       return create(state, (draft) => fn(draft, action));
