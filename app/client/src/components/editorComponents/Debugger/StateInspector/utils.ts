ce416df279989e95389a095606a18e7fbeda0cf3
Hetu Nandu <hetu@appsmith.com>
2025-02-04 12:53:58 +0530

    fix: Selected item in State Inspector (#38955)

--- app/client/src/components/editorComponents/Debugger/StateInspector/utils.ts
+++ app/client/src/components/editorComponents/Debugger/StateInspector/utils.ts
@@ -1,21 +1,15 @@
 import type { ListItemWithoutOnClick } from "./types";
 import type { ListItemProps } from "@appsmith/ads";
-import type { GenericEntityItem } from "ee/entities/IDE/constants";
 
 export const enhanceItemForListItem = (
   item: ListItemWithoutOnClick,
-  selectedItem: GenericEntityItem | undefined,
-  setSelectedItem: (item: GenericEntityItem) => void,
+  selectedItemId: string | undefined,
+  setSelectedItem: (itemId: string) => void,
 ): ListItemProps => {
   return {
     ...item,
-    isSelected: selectedItem ? selectedItem.key === item.id : false,
-    onClick: () =>
-      setSelectedItem({
-        key: item.id,
-        title: item.title,
-        icon: item.startIcon,
-      }),
+    isSelected: selectedItemId ? selectedItemId === item.id : false,
+    onClick: () => setSelectedItem(item.id as string),
     size: "md",
   };
 };
