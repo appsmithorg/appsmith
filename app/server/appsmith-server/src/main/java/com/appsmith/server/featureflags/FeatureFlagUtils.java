a9728e882b97c348be2115b5173ba90d0b52828b
Abhijeet <abhi.nagarnaik@gmail.com>
2025-02-07 14:30:12 +0530

    chore: Fix tests

--- app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/FeatureFlagUtils.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/featureflags/FeatureFlagUtils.java
@@ -3,7 +3,6 @@ package com.appsmith.server.featureflags;
 import com.appsmith.external.enums.FeatureFlagEnum;
 import com.appsmith.server.constants.FeatureMigrationType;
 import com.appsmith.server.domains.TenantConfiguration;
-import com.appsmith.server.helpers.CollectionUtils;
 import com.appsmith.server.helpers.EnumUtils;
 
 import java.util.HashMap;
@@ -13,12 +12,14 @@ public class FeatureFlagUtils {
 
     public static Map<FeatureFlagEnum, FeatureMigrationType> getValidFeaturesWithPendingMigration(
             TenantConfiguration tenantConfiguration) {
-        Map<FeatureFlagEnum, FeatureMigrationType> featureMigrationTypeMap = new HashMap<>();
 
-        if (tenantConfiguration == null
-                || CollectionUtils.isNullOrEmpty(tenantConfiguration.getFeaturesWithPendingMigration())) {
-            return featureMigrationTypeMap;
+        // Only check for null value for featuresWithPendingMigration as empty map is a valid state and migration
+        // status is updated to completed based on number of elements in the map
+        if (tenantConfiguration == null || tenantConfiguration.getFeaturesWithPendingMigration() == null) {
+            return null;
         }
+
+        Map<FeatureFlagEnum, FeatureMigrationType> featureMigrationTypeMap = new HashMap<>();
         tenantConfiguration.getFeaturesWithPendingMigration().forEach((featureFlag, featureMigrationType) -> {
             FeatureFlagEnum featureFlagEnum = EnumUtils.getEnumFromString(FeatureFlagEnum.class, featureFlag);
             if (featureFlagEnum == null) {
