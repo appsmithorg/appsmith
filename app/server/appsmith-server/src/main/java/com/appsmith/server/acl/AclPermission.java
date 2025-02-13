c2783f09229a62ebae4fca843eef8faad720a86f
Trisha Anand <trisha@appsmith.com>
2025-02-13 17:05:52 +0530

    Adding older enums back with @Deprecated enum so that the objects are still readable in the code

--- app/server/appsmith-server/src/main/java/com/appsmith/server/acl/AclPermission.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/acl/AclPermission.java
@@ -121,7 +121,9 @@ public enum AclPermission {
     READ_PERMISSION_GROUPS("read:permissionGroups", PermissionGroup.class),
 
     // Manage organization permissions
-    MANAGE_ORGANIZATION("manage:tenants", Organization.class),
+    MANAGE_ORGANIZATION("manage:organization", Organization.class),
+    @Deprecated(forRemoval = true, since = "v1.62")
+    MANAGE_TENANT("manage:tenant", Organization.class),
 
     CONNECT_TO_GIT("connectToGit:applications", Application.class),
     MANAGE_PROTECTED_BRANCHES("manageProtectedBranches:applications", Application.class),
