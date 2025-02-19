aecc93c115bfa17e39b7c67598afc14ef0e3089c
Rahul Barwal <rahul.barwal@appsmith.com>
2025-02-19 11:28:17 +0530

    refactor(TableWidget): Simplify TableColumnHeader with TableContext
    - Removes explicit prop passing in TableColumnHeader
    - Uses TableContext to access shared table state
    - Calculates header width using useMemo
    - Simplifies header rendering and interaction logic

--- app/client/src/widgets/TableWidgetV2/component/StaticTable/index.tsx
+++ app/client/src/widgets/TableWidgetV2/component/StaticTable/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import SimpleBar from "simplebar-react";
 import "simplebar-react/dist/simplebar.min.css";
-import {
-  MULTISELECT_CHECKBOX_WIDTH,
-  TABLE_SCROLLBAR_WIDTH,
-} from "../Constants";
+import { TABLE_SCROLLBAR_WIDTH } from "../Constants";
 import TableColumnHeader from "../header/TableColumnHeader";
 import { TableBody } from "../TableBody";
 import { useAppsmithTable } from "../TableContext";
@@ -14,68 +11,28 @@ const StaticTable = (_: StaticTableProps, ref: React.Ref<SimpleBar>) => {
   const {
     accentColor,
     borderRadius,
-    canFreezeColumn,
     columns,
-    disableDrag,
-    editMode,
-    enableDrag,
     getTableBodyProps,
-    handleAllRowSelectClick,
-    handleColumnFreeze,
-    handleReorderColumn,
-    headerGroups,
     height,
     isAddRowInProgress,
     isLoading,
-    isResizingColumn,
-    isSortable,
     multiRowSelection,
     nextPageClick,
     pageSize,
     prepareRow,
     primaryColumnId,
-    rowSelectionState,
     scrollContainerStyles,
     selectedRowIndex,
     selectedRowIndices,
     selectTableRow,
-    sortTableColumn,
     subPage,
     tableSizes,
-    totalColumnsWidth,
-    widgetId,
     width,
   } = useAppsmithTable();
 
   return (
     <SimpleBar ref={ref} style={scrollContainerStyles}>
-      <TableColumnHeader
-        accentColor={accentColor}
-        borderRadius={borderRadius}
-        canFreezeColumn={canFreezeColumn}
-        columns={columns}
-        disableDrag={disableDrag}
-        editMode={editMode}
-        enableDrag={enableDrag}
-        handleAllRowSelectClick={handleAllRowSelectClick}
-        handleColumnFreeze={handleColumnFreeze}
-        handleReorderColumn={handleReorderColumn}
-        headerGroups={headerGroups}
-        headerWidth={
-          multiRowSelection && totalColumnsWidth
-            ? MULTISELECT_CHECKBOX_WIDTH + totalColumnsWidth
-            : totalColumnsWidth
-        }
-        isResizingColumn={isResizingColumn}
-        isSortable={isSortable}
-        multiRowSelection={multiRowSelection}
-        prepareRow={prepareRow}
-        rowSelectionState={rowSelectionState}
-        sortTableColumn={sortTableColumn}
-        subPage={subPage}
-        widgetId={widgetId}
-        width={width}
-      />
+      <TableColumnHeader />
       <TableBody
         accentColor={accentColor}
         borderRadius={borderRadius}
