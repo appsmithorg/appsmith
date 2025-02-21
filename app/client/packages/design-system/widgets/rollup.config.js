b94ce67c5d0ee9828b93d715af23fdc030b16826
Pawan Kumar <pawan@appsmith.com>
2025-02-21 11:09:49 +0530

    update rollup config

--- app/client/packages/design-system/widgets/rollup.config.js
+++ app/client/packages/design-system/widgets/rollup.config.js
@@ -1,68 +1,54 @@
-import { fileURLToPath } from "url";
-import { dirname } from "path";
 import path from "path";
-import { defineConfig } from "rollup";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+import postcssNesting from "postcss-nesting";
+import postcssImport from "postcss-import";
+import postcssAtRulesVariables from "postcss-at-rules-variables";
+import postcssEach from "postcss-each";
+import postcssModulesValues from "postcss-modules-values";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+import json from "@rollup/plugin-json";
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import terser from "@rollup/plugin-terser";
 import babel from "@rollup/plugin-babel";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 
-const BUILD_DIR = path.resolve(__dirname, "build"); // build
-const UMD_DIR = path.resolve(BUILD_DIR, "umd"); // build/umd
+const BUILD_DIR = path.resolve(__dirname, "build");
 
 const EXTERNALS = ["react", "react-dom"];
 
-export default defineConfig({
+export default {
   input: path.resolve(__dirname, "src/index.ts"),
   output: {
-    file: path.resolve(UMD_DIR, "design-system.js"),
-    format: "umd",
-    name: "JsArtisanTest",
+    file: path.resolve(BUILD_DIR, "bundle.js"),
+    format: "esm",
     sourcemap: true,
+    inlineDynamicImports: true,
     globals: {
       react: "React",
       "react-dom": "ReactDOM",
     },
-    inlineDynamicImports: true,
-    extend: true,
-    exports: "named",
   },
-  external: EXTERNALS,
   plugins: [
+    json(),
     replace({
       preventAssignment: true,
       values: {
         "process.env.NODE_ENV": JSON.stringify("production"),
         "process.env.REACT_APP_ENV": JSON.stringify("production"),
       },
-    }),
-    nodeResolve({
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-      browser: true,
-      preferBuiltins: false,
-      dedupe: EXTERNALS,
-    }),
+    }), 
     commonjs({
       include: /node_modules/,
       transformMixedEsModules: true,
       requireReturnsDefault: "preferred",
       esmExternals: true,
-      ignoreDynamicRequires: true,
-      ignoreTryCatch: true,
-    }),
-    typescript({
-      tsconfig: "./tsconfig.json",
-      declaration: false,
-      module: "esnext",
-      outDir: UMD_DIR,
-      noForceEmit: true,
     }),
     babel({
       exclude: "node_modules/**",
@@ -77,26 +63,29 @@ export default defineConfig({
       configFile: false
     }),
     postcss({
-      inject: true,
-      minimize: true,
-      extract: false,
       modules: true,
+      minimize: true,
+      sourceMap: true,
+      plugins: [
+        postcssNesting(),
+        postcssImport(),
+        postcssAtRulesVariables(),
+        postcssEach(),
+        postcssModulesValues(),
+      ],
     }),
-    terser({
-      format: {
-        comments: false,
-      },
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        drop_console: true,
-      },
+    typescript({
+      tsconfig: "./tsconfig.json",
+      declaration: true,
+      declarationDir: path.resolve(BUILD_DIR),
+      rootDir: "src",
+      outDir: path.resolve(BUILD_DIR),
+    }),
+    resolve({
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      browser: true,
+      preferBuiltins: false,
+      dedupe: EXTERNALS,
     }),
   ],
-  preserveEntrySignatures: false,
-  treeshake: {
-    propertyReadSideEffects: false,
-    moduleSideEffects: false,
-  },
-});
+};
