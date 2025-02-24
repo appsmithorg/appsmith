d5e3efe5dc53f1e8d5a922b4542c8de7fdffd77d
Laveena Enid <laveena@appsmith.com>
2025-02-24 17:32:22 +0530

    Added tests for hubspot integration
    Signed-off-by: Laveena Enid <laveena@appsmith.com>

--- app/client/cypress/support/Pages/DataSources.ts
+++ app/client/cypress/support/Pages/DataSources.ts
@@ -317,6 +317,7 @@ export class DataSources {
   private _entityTriggerElement = ".t--template-menu-trigger";
   _dsSchemaTableResponse = ".t--table-response";
   _imgSnowflakeLogo = "//img[contains(@src, 'snowflake.svg')]";
+  _imgHubspotLogo = "//img[contains(@src, 'hubspot.png')]";
   _dsConfigProperties = (index: number) =>
     "input[name*='datasourceConfiguration.properties[" + index + "]']";
   _dsConfigAuthType = `[data-testid*='datasourceConfiguration.authentication.authenticationType']`;
@@ -608,7 +609,25 @@ export class DataSources {
         : password,
     );
   }
-
+  public FillHubspotDSForm(
+    environment = this.dataManager.defaultEnviorment,
+    password = "",
+  ) {
+    this.ValidateNSelectDropdown(
+      "Authentication type",
+      "Please select an option",
+      "Bearer token",
+    );
+    this.agHelper.TypeText(
+      this.locator._inputFieldByName("Bearer token") +
+        "//" +
+        this.locator._inputField,
+      !password
+        ? this.dataManager.dsValues[environment].hubspotBearerToken
+        : password,
+    );
+    this.agHelper.Sleep();
+  }
   public FillMongoDSForm(
     environment = this.dataManager.defaultEnviorment,
     shouldAddTrailingSpaces = false,
