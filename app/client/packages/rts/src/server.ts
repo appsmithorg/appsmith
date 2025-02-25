9be377ae11ee63b338ef9aad7eb6196bf0ab3d66
Shrikant Sharat Kandula <shrikant@appsmith.com>
2025-02-25 18:24:03 +0530

    lint

--- app/client/packages/rts/src/server.ts
+++ app/client/packages/rts/src/server.ts
@@ -2,7 +2,6 @@ import server from "./ee/server";
 import log from "loglevel";
 import { VERSION as buildVersion } from "./version"; // release version of the api
 
-
 const APPSMITH_RTS_PORT = process.env.APPSMITH_RTS_PORT || 8091;
 
 server.listen(APPSMITH_RTS_PORT, () => {
