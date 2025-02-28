fe88eeaf109487e73e9cb4b90fbaa6d5f6396ade
Pawan Kumar <pawan@appsmith.com>
2025-02-28 15:52:57 +0530

    add ce files

--- app/client/packages/design-system/widgets/src/components/TextArea/src/types.ts
+++ app/client/packages/design-system/widgets/src/components/TextArea/src/types.ts
@@ -11,5 +11,5 @@ export interface TextAreaProps extends AriaTextFieldProps, FieldProps {
   fieldClassName?: string;
   inputClassName?: string;
   size?: Exclude<keyof typeof SIZES, "xSmall">;
-  maxRows?: number;
+  "data-testid"?: string;
 }
