3acd355c307392d72ee875a4f9d1b8e93cc52255
Alex Golovanov <alex@appsmith.com>
2025-03-14 17:37:22 +0300

    fix: correct showNavbar behavior & render optimization

--- app/client/src/pages/AppViewer/Navigation/index.tsx
+++ app/client/src/pages/AppViewer/Navigation/index.tsx
@@ -1,108 +1,111 @@
-import React, { useEffect, useRef, useState } from "react";
+// React and core libraries
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import type { ApplicationPayload } from "entities/Application";
+
+// Third-party libraries
 import { useDispatch, useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+
+// Type imports
 import type { AppState } from "ee/reducers";
-import {
-  getCurrentBasePageId,
-  getViewModePageList,
-} from "selectors/editorSelectors";
-import { getCurrentWorkspaceId } from "ee/selectors/selectedWorkspaceSelectors";
-import { getCurrentUser } from "selectors/usersSelectors";
-import type { User } from "constants/userConstants";
-import type { Theme } from "constants/DefaultTheme";
-import { getThemeDetails, ThemeMode } from "selectors/themeSelectors";
-import HtmlTitle from "../AppViewerHtmlTitle";
+import type { ApplicationPayload } from "entities/Application";
+
+// Application-specific imports
+import { setAppViewHeaderHeight } from "actions/appViewActions";
 import { NAVIGATION_SETTINGS } from "constants/AppConstants";
+import { builderURL } from "ee/RouteBuilder";
+import { getCurrentApplication } from "ee/selectors/applicationSelectors";
+import { getCurrentWorkspaceId } from "ee/selectors/selectedWorkspaceSelectors";
+import AnalyticsUtil from "ee/utils/AnalyticsUtil";
 import PageMenu from "pages/AppViewer/PageMenu";
 import { useHref } from "pages/Editor/utils";
-import { builderURL } from "ee/RouteBuilder";
-import TopHeader from "./components/TopHeader";
-import Sidebar from "./Sidebar";
-import { getCurrentApplication } from "ee/selectors/applicationSelectors";
+import {
+  getCurrentBasePageId,
+  getCurrentPageId,
+  getViewModePageList,
+} from "selectors/editorSelectors";
+import { getThemeDetails, ThemeMode } from "selectors/themeSelectors";
+import { getCurrentUser } from "selectors/usersSelectors";
 import { useIsMobileDevice } from "utils/hooks/useDeviceDetect";
-import { setAppViewHeaderHeight } from "actions/appViewActions";
-import AnalyticsUtil from "ee/utils/AnalyticsUtil";
+
+// Relative imports
+import HtmlTitle from "../AppViewerHtmlTitle";
+import Sidebar from "./Sidebar";
+import TopHeader from "./components/TopHeader";
 
 export function Navigation() {
+  const dispatch = useDispatch();
   const { search } = useLocation();
-  const queryParams = new URLSearchParams(search);
-  const isEmbed = queryParams.get("embed") === "true";
-  const showNavBar = queryParams.get("navbar") === "true";
-  const hideHeader = isEmbed && !showNavBar;
   const [isMenuOpen, setMenuOpen] = useState(false);
   const headerRef = useRef<HTMLDivElement>(null);
+  const isMobile = useIsMobileDevice();
   const basePageId = useSelector(getCurrentBasePageId);
+  const pageId = useSelector(getCurrentPageId);
   const editorURL = useHref(builderURL, { basePageId });
-  const currentWorkspaceId: string = useSelector(getCurrentWorkspaceId);
-  const currentUser: User | undefined = useSelector(getCurrentUser);
-  const lightTheme: Theme = useSelector((state: AppState) =>
+
+  const currentWorkspaceId = useSelector(getCurrentWorkspaceId);
+  const currentUser = useSelector(getCurrentUser);
+  const lightTheme = useSelector((state: AppState) =>
     getThemeDetails(state, ThemeMode.LIGHT),
   );
   const currentApplicationDetails: ApplicationPayload | undefined = useSelector(
     getCurrentApplication,
   );
   const pages = useSelector(getViewModePageList);
-  const isMobile = useIsMobileDevice();
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    const header = document.querySelector(".js-appviewer-header");
+  const queryParams = new URLSearchParams(search);
+  const isEmbed = queryParams.get("embed") === "true";
+  const forceShowNavBar = queryParams.get("navbar") === "true";
+  const hideHeader = isEmbed && !forceShowNavBar;
 
-    dispatch(setAppViewHeaderHeight(header?.clientHeight || 0));
-
-    return () => {
-      dispatch(setAppViewHeaderHeight(0));
-    };
-  }, [
-    currentApplicationDetails?.applicationDetail?.navigationSetting?.navStyle,
+  const navStyle =
+    currentApplicationDetails?.applicationDetail?.navigationSetting?.navStyle;
+  const orientation =
     currentApplicationDetails?.applicationDetail?.navigationSetting
-      ?.orientation,
-  ]);
-  useEffect(() => {
-    if (showNavBar && currentApplicationDetails) {
-      AnalyticsUtil.logEvent("APP_VIEWED_WITH_NAVBAR", {
-        id: currentApplicationDetails.id,
-      });
-    }
-  }, [showNavBar, currentApplicationDetails]);
+      ?.orientation;
+  const applicationId = currentApplicationDetails?.id;
 
-  const renderNavigation = () => {
-    if (
-      currentApplicationDetails?.applicationDetail?.navigationSetting
-        ?.orientation === NAVIGATION_SETTINGS.ORIENTATION.SIDE
-    ) {
-      return (
-        <>
-          {/*
-           * We need to add top header since we want the current mobile
-           * navigation experience until we create the new sidebar for mobile.
-           */}
-          {isMobile ? (
-            <TopHeader
-              currentApplicationDetails={currentApplicationDetails}
-              currentUser={currentUser}
-              currentWorkspaceId={currentWorkspaceId}
-              isMenuOpen={isMenuOpen}
-              pages={pages}
-              setMenuOpen={setMenuOpen}
-              showUserSettings={!isEmbed}
-            />
-          ) : (
-            <Sidebar
-              currentApplicationDetails={currentApplicationDetails}
-              currentUser={currentUser}
-              currentWorkspaceId={currentWorkspaceId}
-              pages={pages}
-              showUserSettings={!isEmbed}
-            />
-          )}
-        </>
-      );
-    }
+  const currentPage = pages.find((page) => page.pageId === pageId);
+  const showNavbar = !(
+    currentApplicationDetails?.applicationDetail?.navigationSetting
+      ?.showNavbar === false || currentPage?.isHidden
+  );
 
-    return (
+  // TODO: refactor this to not directly reference a DOM element by class defined elsewhere
+  useEffect(
+    function adjustHeaderHeightEffect() {
+      const header = document.querySelector(".js-appviewer-header");
+
+      dispatch(setAppViewHeaderHeight(header?.clientHeight || 0));
+
+      return () => {
+        dispatch(setAppViewHeaderHeight(0));
+      };
+    },
+    [navStyle, orientation, dispatch],
+  );
+
+  useEffect(
+    function trackNavbarAnalyticsEffect() {
+      if (forceShowNavBar && applicationId) {
+        AnalyticsUtil.logEvent("APP_VIEWED_WITH_NAVBAR", {
+          id: applicationId,
+        });
+      }
+    },
+    [forceShowNavBar, applicationId],
+  );
+
+  const navigation = useMemo(() => {
+    return orientation === NAVIGATION_SETTINGS.ORIENTATION.SIDE && !isMobile ? (
+      <Sidebar
+        currentApplicationDetails={currentApplicationDetails}
+        currentUser={currentUser}
+        currentWorkspaceId={currentWorkspaceId}
+        pages={pages}
+        showUserSettings={!isEmbed}
+      />
+    ) : (
       <TopHeader
         currentApplicationDetails={currentApplicationDetails}
         currentUser={currentUser}
@@ -113,7 +116,16 @@ export function Navigation() {
         showUserSettings={!isEmbed}
       />
     );
-  };
+  }, [
+    currentApplicationDetails,
+    currentUser,
+    currentWorkspaceId,
+    isEmbed,
+    isMenuOpen,
+    isMobile,
+    orientation,
+    pages,
+  ]);
 
   if (hideHeader) return <HtmlTitle />;
 
@@ -124,8 +136,7 @@ export function Navigation() {
         and we are creating the default values only when any nav settings via the
         settings pane has changed, we need to hide the navbar ONLY when the showNavbar
         setting is explicitly set to false by the user via the settings pane. */}
-        {currentApplicationDetails?.applicationDetail?.navigationSetting
-          ?.showNavbar !== false && renderNavigation()}
+        {showNavbar && navigation}
 
         {/* Mobile Sidebar */}
         <PageMenu
