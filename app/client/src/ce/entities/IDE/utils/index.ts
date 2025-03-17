c4e0b6cdb00aa001fa54aba2d4b6ce5151a2d7e4
Alex Golovanov <alex@appsmith.com>
2025-03-17 13:04:24 +0300

    refactor: add missing exports

--- app/client/src/ce/entities/IDE/utils/index.ts
+++ app/client/src/ce/entities/IDE/utils/index.ts
@@ -1,4 +1,4 @@
 export { getBaseUrlsForIDEType } from "./getBaseUrlsForIDEType";
 export { getIDETypeByUrl } from "./getIDETypeByUrl";
 export { saveEntityName } from "./saveEntityName";
-export { getEditableTabPermissions } from "./getEditableTabPermissions";
+export * from "./getEditableTabPermissions";
