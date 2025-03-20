e9cb8420ee985fa16960bb3dad030c15f40e9f21
Abhijeet <41686026+abhvsn@users.noreply.github.com>
2025-03-20 15:14:22 +0530

    chore: Code-split CS config class (#39813)
    ## Description
    PR to code split cloud services configs.
    
    /test Sanity
    
    ### :mag: Cypress test results
    <!-- This is an auto-generated comment: Cypress test results  -->
    > [!TIP]
    > 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
    > Workflow run:
    <https://github.com/appsmithorg/appsmith/actions/runs/13956077465>
    > Commit: 458e1607df835771ddb0cb8869cdff2692e25abc
    > <a
    href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=13956077465&attempt=1"
    target="_blank">Cypress dashboard</a>.
    > Tags: `@tag.Sanity`
    > Spec:
    > <hr>Wed, 19 Mar 2025 21:06:02 UTC
    <!-- end of auto-generated comment: Cypress test results  -->
    
    
    ## Communication
    Should the DevRel and Marketing teams inform users about this change?
    - [ ] Yes
    - [x] No
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced an enhanced cloud configuration setup that provides default
    values for key settings.
    - **Refactor**
    - Streamlined the existing cloud configuration by centralizing its
    management and removing redundant customizations.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/CloudServicesConfig.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/configurations/CloudServicesConfig.java
@@ -1,30 +1,9 @@
 package com.appsmith.server.configurations;
 
+import com.appsmith.server.configurations.ce.CloudServicesConfigCE;
 import lombok.Getter;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
-public class CloudServicesConfig {
-    private String baseUrl;
-
-    @Value("${appsmith.cloud_services.template_upload_auth_header}")
-    private String templateUploadAuthHeader;
-
-    @Autowired
-    public void setBaseUrl(@Value("${appsmith.cloud_services.base_url:}") String value) {
-        baseUrl = StringUtils.isEmpty(value) ? "https://cs.appsmith.com" : value;
-    }
-
-    @Value("${appsmith.cloud_services.signature_base_url}")
-    String baseUrlWithSignatureVerification;
-
-    public String getBaseUrlWithSignatureVerification() {
-        return StringUtils.isEmpty(this.baseUrlWithSignatureVerification)
-                ? this.getBaseUrl()
-                : this.baseUrlWithSignatureVerification;
-    }
-}
+public class CloudServicesConfig extends CloudServicesConfigCE {}
