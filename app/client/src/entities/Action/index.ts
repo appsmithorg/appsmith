appsmithorg/appsmith

commit 97b374c1ef8e54e2924277dbe40b414a68692d01

chore: Introduce action isDirty map (#39872)
## Description

The server now adds a isDirty map in the action object to signify if any
aspect of the action is not saved (is dirty)

This is needed for the EE change
https://github.com/appsmithorg/appsmith-ee/pull/6713


https://www.notion.so/appsmith/Implement-save-button-for-AI-Query-1b9fe271b0e2808285dcc797ad281141?pvs=4

## Automation

/ok-to-test tags="@tag.Datasource"

### :mag: Cypress test results
<!-- This is an auto-generated comment: Cypress test results  -->
> [!TIP]
> 🟢 🟢 🟢 All cypress tests have passed! 🎉 🎉 🎉
> Workflow run:
<https://github.com/appsmithorg/appsmith/actions/runs/14029254723>
> Commit: 4321d95d427a1dfd07aedecd3731390ed0a4688e
> <a
href="https://internal.appsmith.com/app/cypress-dashboard/rundetails-65890b3c81d7400d08fa9ee5?branch=master&workflowId=14029254723&attempt=1"
target="_blank">Cypress dashboard</a>.
> Tags: `@tag.Datasource`
> Spec:
> <hr>Mon, 24 Mar 2025 07:51:29 UTC
<!-- end of auto-generated comment: Cypress test results  -->


## Communication
Should the DevRel and Marketing teams inform users about this change?
- [ ] Yes
- [ ] No


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->
## Summary by CodeRabbit

## Summary by CodeRabbit

- **New Features**
- Added a capability that improves how the app retrieves a specific
action from a collection based on unique identifiers, ensuring smoother
interaction.
- Enhanced state management in actions by introducing a new property to
track the update status of schema generation, supporting more robust
workflow handling.
<!-- end of auto-generated comment: release notes by coderabbit.ai -->

app/client/src/entities/Action/index.ts
@@ -166,6 +166,9 @@ export interface BaseAction {
   visualization?: {
     result: VisualizationElements;
   };
+  isDirtyMap: {
+    SCHEMA_GENERATION: boolean;
+  };
 }
 
 interface BaseApiAction extends BaseAction {

app/client/src/PluginActionEditor/transformers/RestActionTransformers.test.ts
@@ -40,6 +40,9 @@ const BASE_ACTION: ApiAction = {
     },
     timeoutInMillisecond: 5000,
   },
+  isDirtyMap: {
+    SCHEMA_GENERATION: false,
+  },
   jsonPathKeys: [],
   messages: [],
 };

app/client/src/ce/selectors/entitiesSelector.ts
@@ -834,6 +834,20 @@ export const getJsCollectionByBaseId = (
   return jsaction && jsaction.config;
 };
 
+export const getJSCollectionAction = (
+  state: AppState,
+  collectionId: string,
+  actionId: string,
+) => {
+  const jsCollection = getJSCollection(state, collectionId);
+
+  if (jsCollection) {
+    return jsCollection.actions.find((action) => action.id === actionId);
+  }
+
+  return null;
+};
+
 /**
  *
  * getJSCollectionFromAllEntities is used to get the js collection from all jsAction entities (including module instance entities) )

app/client/src/components/editorComponents/utils.test.ts
@@ -47,6 +47,9 @@ const TEST_JS_FUNCTION = {
   userPermissions: ["read:actions", "execute:actions", "manage:actions"],
   validName: "JSObject1.myFun234y",
   cacheResponse: "",
+  isDirtyMap: {
+    SCHEMA_GENERATION: false,
+  },
 };
 
 describe("GetJSResponseViewState", () => {

app/client/src/entities/Action/actionProperties.test.ts
@@ -23,6 +23,9 @@ const DEFAULT_ACTION: Action = {
   pluginId: "",
   messages: [],
   pluginType: PluginType.DB,
+  isDirtyMap: {
+    SCHEMA_GENERATION: false,
+  },
 };
 
 describe("getReactivePathsOfAction", () => {

app/client/src/pages/Editor/JSEditor/utils.test.ts
@@ -61,6 +61,9 @@ const BASE_JS_ACTION = (useLiterals = false) => {
       timeoutInMillisecond: 1000,
       jsArguments: [],
     },
+    isDirtyMap: {
+      SCHEMA_GENERATION: false,
+    },
   };
 };
 

app/client/src/sagas/BuildingBlockSagas/tests/fixtures.ts
@@ -152,5 +152,8 @@ export const newlyCreatedActions: Action[] = [
       "execute:actions",
       "manage:actions",
     ],
+    isDirtyMap: {
+      SCHEMA_GENERATION: false,
+    },
   },
 ];

app/client/test/factories/Actions/API.ts
@@ -75,4 +75,7 @@ export const APIFactory = Factory.Sync.makeFactory<ApiAction>({
     "manage:actions",
   ],
   confirmBeforeExecute: false,
+  isDirtyMap: {
+    SCHEMA_GENERATION: false,
+  },
 });

commit 11832084fdd70f8f23ac2b824a3ba683c5024378

fix: Make isDirty map optional (#39890)
## Description

fixes issue introduced in
https://github.com/appsmithorg/appsmith/commit/97b374c1ef8e54e2924277dbe40b414a68692d01

## Automation

/ok-to-test tags="@tag.Datasource"

### :mag: Cypress test results
<!-- This is an auto-generated comment: Cypress test results  -->
> [!IMPORTANT]
> 🟣 🟣 🟣 Your tests are running.
> Tests running at:
<https://github.com/appsmithorg/appsmith/actions/runs/14052145641>
> Commit: 799b54d6b73ad803e5323de64886411fe2427740
> Workflow: `PR Automation test suite`
> Tags: `@tag.Datasource`
> Spec: ``
> <hr>Tue, 25 Mar 2025 05:11:15 UTC
<!-- end of auto-generated comment: Cypress test results  -->


## Communication
Should the DevRel and Marketing teams inform users about this change?
- [ ] Yes
- [ ] No


<!-- This is an auto-generated comment: release notes by coderabbit.ai
-->

## Summary by CodeRabbit

- **Refactor**
- Enhanced action configuration by making certain settings optional,
which increases flexibility in how actions are handled and instantiated.

<!-- end of auto-generated comment: release notes by coderabbit.ai -->

app/client/src/entities/Action/index.ts
@@ -166,7 +166,7 @@ export interface BaseAction {
   visualization?: {
     result: VisualizationElements;
   };
-  isDirtyMap: {
+  isDirtyMap?: {
     SCHEMA_GENERATION: boolean;
   };
 }