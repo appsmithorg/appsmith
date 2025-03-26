845406a08a93ef55176bde1e353aea9f0fcb5563
jacquesikot <jacquesikot@gmail.com>
2025-03-26 15:31:45 +0100

    fix: disabled prop type mismatch between PropertyPaneControlConfig and LazyCodeEditor

--- app/client/src/components/propertyControls/CodeEditorControl.tsx
+++ app/client/src/components/propertyControls/CodeEditorControl.tsx
@@ -1,50 +1,56 @@
-import type { ChangeEvent } from "react";
-import React from "react";
-import type { ControlProps } from "./BaseControl";
-import BaseControl from "./BaseControl";
-import type { EventOrValueHandler } from "redux-form";
+import { slashCommandHintHelper } from "components/editorComponents/CodeEditor/commandsHelper";
 import {
   EditorModes,
   EditorSize,
+  EditorTheme,
   TabBehaviour,
 } from "components/editorComponents/CodeEditor/EditorConfig";
-import LazyCodeEditor from "components/editorComponents/LazyCodeEditor";
 import { bindingHintHelper } from "components/editorComponents/CodeEditor/hintHelpers";
-import { slashCommandHintHelper } from "components/editorComponents/CodeEditor/commandsHelper";
-import type { EditorProps } from "components/editorComponents/CodeEditor";
+import LazyCodeEditor from "components/editorComponents/LazyCodeEditor";
+import type { ChangeEvent } from "react";
+import React from "react";
+import type { EventOrValueHandler } from "redux-form";
+import type { ControlProps } from "./BaseControl";
+import BaseControl from "./BaseControl";
 
 class CodeEditorControl extends BaseControl<ControlProps> {
   render() {
     const {
       controlConfig,
       dataTreePath,
+      disabled,
       evaluatedValue,
       expected,
       propertyValue,
       useValidationMessage,
     } = this.props;
 
-    const props: Partial<ControlProps> = {};
-
-    if (dataTreePath) props.dataTreePath = dataTreePath;
-
-    if (evaluatedValue) props.evaluatedValue = evaluatedValue;
-
-    if (expected) props.expected = expected;
-
+    // PropertyPaneControlConfig's disabled is a function (props: any, propertyPath: string) => boolean
+    // while LazyCodeEditor expects a boolean. Convert function to boolean result.
+    const isReadOnly = typeof disabled === "function"
+      ? disabled(this.props.widgetProperties, this.props.propertyName)
+      : !!disabled;
+    
+    const maxHeight = controlConfig?.maxHeight ? 
+      String(controlConfig.maxHeight) : undefined;
+    
     return (
       <LazyCodeEditor
         additionalDynamicData={this.props.additionalAutoComplete}
+        dataTreePath={dataTreePath}
+        evaluatedValue={evaluatedValue}
+        expected={expected}
         hinting={[bindingHintHelper, slashCommandHintHelper]}
         input={{ value: propertyValue, onChange: this.onChange }}
-        maxHeight={controlConfig?.maxHeight as EditorProps["maxHeight"]}
+        isReadOnly={isReadOnly}
+        marking={[]}
+        maxHeight={maxHeight}
         mode={EditorModes.TEXT_WITH_BINDING}
         positionCursorInsideBinding
         size={EditorSize.EXTENDED}
         tabBehaviour={TabBehaviour.INDENT}
-        theme={this.props.theme}
+        theme={EditorTheme.LIGHT} 
         useValidationMessage={useValidationMessage}
-        {...props}
         AIAssisted
       />
     );
