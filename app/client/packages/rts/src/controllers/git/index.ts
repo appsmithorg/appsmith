541cf54302a8065b2aba1503ea94d2068da4b01e
Diljit VJ <diljit@appsmith.com>
2025-03-28 17:02:52 +0530

    add rts logs

--- app/client/packages/rts/src/controllers/git/index.ts
+++ app/client/packages/rts/src/controllers/git/index.ts
@@ -8,13 +8,18 @@ import type { GitResetRequestDTO } from "../../types/git.dto";
 
 export default class GitController extends BaseController {
   async reset(req: Request, res: Response) {
+    const request: GitResetRequestDTO = req.body;
+
     try {
-      const request: GitResetRequestDTO = req.body;
+      log.info(`Resetting git repository for ${request.repoPath}`);
 
       const result = await reset(request);
 
+      log.info(`Git repository reset for ${request.repoPath}`);
+
       return super.sendResponse(res, result);
     } catch (err) {
+      log.info(`Error resetting git repository for ${request.repoPath}`);
       log.error(err);
 
       return super.sendError(
