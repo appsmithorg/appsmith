d303ab5491e5ea5a9708c29cf366373a0bfb3aab
Pawan Kumar <pawan@appsmith.com>
2025-04-03 11:32:33 +0530

    remove title in sidebar

--- app/client/packages/design-system/widgets/src/components/Sidebar/src/SidebarContent.tsx
+++ app/client/packages/design-system/widgets/src/components/Sidebar/src/SidebarContent.tsx
@@ -2,10 +2,7 @@ import clsx from "clsx";
 import React, { type ReactNode, type Ref } from "react";
 
 import { Flex } from "../../Flex";
-import { Text } from "../../Text";
-import { Button } from "../../Button";
 import styles from "./styles.module.css";
-import { useSidebar } from "./use-sidebar";
 
 interface SidebarContentProps {
   title?: string;
@@ -18,8 +15,7 @@ const _SidebarContent = (
   props: SidebarContentProps,
   ref: Ref<HTMLDivElement>,
 ) => {
-  const { children, className, extraTitleButton, title, ...rest } = props;
-  const { isMobile, setState, state } = useSidebar();
+  const { children, className, ...rest } = props;
 
   return (
     <div
@@ -29,41 +25,6 @@ const _SidebarContent = (
       {...rest}
     >
       <Flex direction="column" height="100%" isInner>
-        <Flex
-          alignItems="center"
-          className={styles.sidebarHeader}
-          isInner
-          justifyContent="space-between"
-        >
-          {Boolean(title) && (
-            <Text
-              className={styles.sidebarTitle}
-              fontWeight={500}
-              lineClamp={1}
-            >
-              {title}
-            </Text>
-          )}
-
-          <Flex marginLeft="auto">
-            {extraTitleButton}
-            {!isMobile && (
-              <Button
-                className={styles.sidebarHeaderExpandButton}
-                color="neutral"
-                icon={
-                  state === "full-width"
-                    ? "arrows-diagonal-minimize"
-                    : "arrows-diagonal-2"
-                }
-                onPress={() =>
-                  setState(state === "full-width" ? "expanded" : "full-width")
-                }
-                variant="ghost"
-              />
-            )}
-          </Flex>
-        </Flex>
         <div className={styles.sidebarContentInner}>{children}</div>
       </Flex>
     </div>
