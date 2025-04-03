1ab2639d48d7b0ec86daf3b70eb9772638f16025
jacquesikot <jacquesikot@gmail.com>
2025-04-03 06:10:12 +0100

    implenent channges

---
+++ app/client/src/widgets/TableWidgetV2/component/VirtualTable/InifiniteScrollBody/useInfiniteScroll.tsx
@@ -0,0 +1,23 @@
+import type { Row } from "react-table";
+import { useEffect } from "react";
+
+export interface UseInfiniteScrollProps {
+  loadMore: () => void;
+  rows: Row<Record<string, unknown>>[];
+  pageSize: number;
+}
+
+export const useInfiniteScroll = ({
+  loadMore,
+  rows,
+  pageSize,
+}: UseInfiniteScrollProps) => {
+  useEffect(() => {
+    // If cachedRows is just a single page, call loadMore to fetch the next page
+    if (rows.length > 0 && rows.length <= pageSize) {
+      loadMore();
+    }
+  }, [rows.length, pageSize, loadMore]);
+
+  return;
+};
