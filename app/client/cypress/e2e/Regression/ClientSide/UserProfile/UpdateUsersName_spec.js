75a745611a87c9be445819e71883a6e1d19394ac
Ankita Kinger <ankita@appsmith.com>
2025-04-05 14:05:05 +0530

    fixing userprofile  test

--- app/client/cypress/e2e/Regression/ClientSide/UserProfile/UpdateUsersName_spec.js
+++ app/client/cypress/e2e/Regression/ClientSide/UserProfile/UpdateUsersName_spec.js
@@ -22,7 +22,7 @@ describe("Update a user's name", { tags: ["@tag.Settings"] }, function () {
       cy.reload();
       cy.get(homePage.profileMenu).click();
       cy.get(".t--edit-profile").click({ force: true });
-      cy.get(".t--user-name").contains(username);
+      cy.get("[data-testid=t--display-name]").contains(username);
     });
   });
 
