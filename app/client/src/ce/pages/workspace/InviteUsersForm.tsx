242933aa9ef216f20cafe0032f9235d810212e78
Ankita Kinger <ankita@appsmith.com>
2025-04-07 21:42:11 +0530

    updating styles for invite modal to fix UI issue

--- app/client/src/ce/pages/workspace/InviteUsersForm.tsx
+++ app/client/src/ce/pages/workspace/InviteUsersForm.tsx
@@ -83,7 +83,7 @@ export const ErrorBox = styled.div<{ message?: boolean }>`
 
 export const StyledInviteFieldGroup = styled.div`
   display: flex;
-  align-items: baseline;
+  align-items: center;
   justify-content: space-between;
   gap: 0.8rem;
 `;
