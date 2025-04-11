92393fb4c1b38d315b71dbf62d3331a9dbb004f3
sondermanish <manish@appsmith.com>
2025-04-11 19:28:33 +0530

    fixed discard flow

--- app/server/appsmith-git/src/main/java/com/appsmith/git/files/FileUtilsCEImpl.java
+++ app/server/appsmith-git/src/main/java/com/appsmith/git/files/FileUtilsCEImpl.java
@@ -885,7 +885,12 @@ public class FileUtilsCEImpl implements FileInterface {
     protected Tuple2<GitResourceIdentity, Object> getGitResourceIdentity(Path baseRepoPath, String filePath) {
         Path path = baseRepoPath.resolve(filePath);
         GitResourceIdentity identity;
-        Object contents = fileOperations.readFile(path);
+        Object contents = null;
+
+        if (filePath.endsWith(JSON_EXTENSION)) {
+            contents = fileOperations.readFile(path);
+        }
+
         if (!filePath.contains("/")) {
             identity = new GitResourceIdentity(GitResourceType.ROOT_CONFIG, filePath, filePath);
         } else if (filePath.matches(DATASOURCE_DIRECTORY + "/.*")) {
