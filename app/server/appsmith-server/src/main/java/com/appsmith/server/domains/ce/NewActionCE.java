4a75687a8107bc9e88f4503d074912333fd11392
“sneha122” <sneha@appsmith.com>
2025-05-12 23:46:33 +0530

    server build issue fixed

--- app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/NewActionCE.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/domains/ce/NewActionCE.java
@@ -73,10 +73,8 @@ public class NewActionCE extends RefAwareDomain {
                 dotted(unpublishedAction, ActionDTO.Fields.userSetOnLoad);
         public static final String unpublishedAction_runBehaviour =
                 dotted(unpublishedAction, ActionDTO.Fields.runBehaviour);
-        public static final String unpublishedAction_executeOnLoad = dotted(
-                unpublishedAction,
-                ActionDTO.Fields
-                        .executeOnLoad); // This is kept in code to provide backwards compatibility for older actions
+        public static final String unpublishedAction_executeOnLoad =
+                dotted(unpublishedAction, ActionDTO.Fields.executeOnLoad);
         public static final String unpublishedAction_fullyQualifiedName =
                 dotted(unpublishedAction, ActionDTO.Fields.fullyQualifiedName);
         public static final String unpublishedAction_actionConfiguration_httpMethod =
