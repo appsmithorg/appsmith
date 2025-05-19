4f88a93734ff4a6f71b5fed22dd8364a14486093
jacquesikot <jacquesikot@gmail.com>
2025-05-19 10:45:17 +0100

    fix: Correct condition for displaying left side content in UserAuth container

--- app/client/src/pages/UserAuth/Container.tsx
+++ app/client/src/pages/UserAuth/Container.tsx
@@ -52,7 +52,7 @@ function Container(props: ContainerProps) {
   const brandingEnabled = isBrandingEnabled(featureFlags);
 
   const shouldShowLeftSideContent =
-    cloudHosting && !isMobileDevice && !multiOrgEnabled && brandingEnabled;
+    cloudHosting && !isMobileDevice && multiOrgEnabled && !brandingEnabled;
   const isAiAgentInstanceEnabled = useSelector(getIsAiAgentInstanceEnabled);
 
   return (
