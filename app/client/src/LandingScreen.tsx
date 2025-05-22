862a45195e44aca772b8533e67e8b18561022206
jacquesikot <jacquesikot@gmail.com>
2025-05-22 16:58:28 +0100

    feat: enhance landing screen user redirection logic

--- app/client/src/LandingScreen.tsx
+++ app/client/src/LandingScreen.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import type { DefaultRootState } from "react-redux";
 import { getCurrentUser, getUserAuthError } from "selectors/usersSelectors";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import type { User } from "constants/userConstants";
 import { ANONYMOUS_USERNAME } from "constants/userConstants";
 import { Redirect } from "react-router";
 import { APPLICATIONS_URL, AUTH_LOGIN_URL, BASE_URL } from "constants/routes";
 import PageLoadingBar from "pages/common/PageLoadingBar";
 import ServerUnavailable from "pages/common/ErrorPages/ServerUnavailable";
+import { getOrganizationConfig } from "ce/selectors/organizationSelectors";
+import { isWithinAnOrganization } from "ce/selectors/organizationSelectors";
 
 interface Props {
   user?: User;
@@ -15,10 +17,25 @@ interface Props {
 }
 
 function LandingScreen(props: Props) {
-  if (props.user && window.location.pathname === BASE_URL) {
-    if (props.user.email === ANONYMOUS_USERNAME) {
+  const organizationConfig = useSelector(getOrganizationConfig);
+  const isWithinOrg = useSelector(isWithinAnOrganization);
+
+  const LOGIN_SLUG = "login";
+  const hasLoginSlug = organizationConfig.slug === LOGIN_SLUG;
+
+  if (window.location.pathname === BASE_URL) {
+    // User exists but is anonymous or doesn't have org access
+    if (props.user && props.user.email === ANONYMOUS_USERNAME) {
       return <Redirect to={AUTH_LOGIN_URL} />;
-    } else {
+    }
+
+    // User doesn't exist or has login slug but not in an org
+    if (!props.user || (hasLoginSlug && !isWithinOrg)) {
+      return <Redirect to={AUTH_LOGIN_URL} />;
+    }
+
+    // Authenticated user with proper access
+    if (props.user) {
       return <Redirect to={APPLICATIONS_URL} />;
     }
   }
