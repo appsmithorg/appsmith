101e298a0cbfc0b7ffcd01a867f15f291174b581
Ankita Kinger <ankita@appsmith.com>
2025-06-19 20:05:42 +0530

    fixing a cyclic dependency

--- app/client/src/ce/entities/DataTree/types.ts
+++ app/client/src/ce/entities/DataTree/types.ts
@@ -6,19 +6,10 @@ import type { ActionDescription } from "ee/workers/Evaluation/fns";
 import type { Variable } from "entities/JSCollection";
 import type { DependencyMap, DynamicPath } from "utils/DynamicBindingUtils";
 import type { Page } from "entities/Page";
-import type { MetaWidgetsReduxState } from "reducers/entityReducers/metaWidgetsReducer";
 import type { WidgetConfigProps } from "WidgetProvider/types";
-import type { ActionDataState } from "ee/reducers/entityReducers/actionsReducer";
 import type { WidgetProps } from "widgets/BaseWidget";
-import type { CanvasWidgetsReduxState } from "ee/reducers/entityReducers/canvasWidgetsReducer";
-import type { MetaState } from "reducers/entityReducers/metaReducer";
 import type { AppDataState } from "reducers/entityReducers/appReducer";
-import type { JSCollectionDataState } from "ee/reducers/entityReducers/jsActionsReducer";
 import type { AppTheme } from "entities/AppTheming";
-import type { LoadingEntitiesState } from "reducers/evaluationReducers/loadingEntitiesReducer";
-import type { LayoutSystemTypes } from "layoutSystems/types";
-import type { Module } from "ee/constants/ModuleConstants";
-import type { ModuleInstance } from "ee/constants/ModuleInstanceConstants";
 import type { ActionRunBehaviourType } from "PluginActionEditor/types/PluginActionTypes";
 
 // TODO: Fix this the next time the file is edited
@@ -199,28 +190,6 @@ export interface AppsmithEntity extends Omit<AppDataState, "store"> {
   workspaceName: string;
   appName: string;
   currentEnvironmentName: string;
-}
-
-export interface DataTreeSeed {
-  actions: ActionDataState;
-  // TODO: Fix this the next time the file is edited
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  editorConfigs: Record<string, any[]>;
-  pluginDependencyConfig: Record<string, DependencyMap>;
-  widgets: CanvasWidgetsReduxState;
-  widgetsMeta: MetaState;
-  appData: AppDataState;
-  jsActions: JSCollectionDataState;
-  theme: AppTheme["properties"];
-  metaWidgets: MetaWidgetsReduxState;
-  isMobile: boolean;
-  moduleInputs: Module["inputsForm"];
-  moduleInstances: Record<string, ModuleInstance> | null;
-  // TODO: Fix this the next time the file is edited
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  moduleInstanceEntities: any;
-  layoutSystemType: LayoutSystemTypes;
-  loadingEntities: LoadingEntitiesState;
 }
 
 export type DataTreeEntityConfig =
