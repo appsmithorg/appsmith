ca483cf57fe3faba6b74b6122d546250e2c6f4d2
“sneha122” <sneha@appsmith.com>
2025-06-24 18:19:42 +0530

    chore: backend page unload changes

--- app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/RunBehaviourEnum.java
+++ app/server/appsmith-interfaces/src/main/java/com/appsmith/external/models/RunBehaviourEnum.java
@@ -6,5 +6,6 @@ package com.appsmith.external.models;
 public enum RunBehaviourEnum {
     MANUAL, // Action will only run when manually triggered
     ON_PAGE_LOAD, // Action will run when the page loads
-    AUTOMATIC // Action will run automatically (When any of its dependencies change) if the feature flag is enabled
+    AUTOMATIC, // Action will run automatically (When any of its dependencies change) if the feature flag is enabled
+    ON_PAGE_UNLOAD, // Action will run when the page unloads
 }
