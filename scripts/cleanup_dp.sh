da3f5d0b0e4724138109b9fd051913d53ad7bca1
Goutham Pratapa <goutham@appsmith.com>
2025-06-26 11:00:20 +0530

    chore: update cleanup dp to delete merged PRs

--- scripts/cleanup_dp.sh
+++ scripts/cleanup_dp.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 # Configure the AWS & kubectl environment
 
-mkdir ~/.aws; touch ~/.aws/config
-
-echo "[default]
-aws_access_key_id = $AWS_ACCESS_KEY_ID
-aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
-
-echo "[default]
-region = $region
+# Configure AWS CLI and kubectl environment
+mkdir -p ~/.aws
+cat > ~/.aws/config <<EOF
+[default]
+region = ap-south-1
 output = json
-[profile eksci]
-role_arn= $AWS_ROLE_ARN
-output = json
-region = $region
-source_profile = default" > ~/.aws/config
+EOF
 
-sts_output=$(aws sts assume-role --role-arn env.AWS_ROLE_ARN --role-session-name ekscisession)
-export AWS_ACCESS_KEY_ID=$(echo $sts_output | jq -r '.Credentials''.AccessKeyId');\
-export AWS_SECRET_ACCESS_KEY=$(echo $sts_output | jq -r '.Credentials''.SecretAccessKey');\
-export AWS_SESSION_TOKEN=$(echo $sts_output | jq -r '.Credentials''.SessionToken');
+aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 
-aws eks update-kubeconfig --region ap-south-1 --name uat-cluster --profile eksci
+export region="ap-south-1"
+export cluster_name="uatx-cluster"
+
+# Update kubeconfig
+aws eks update-kubeconfig --region "$region" --name "$cluster_name"
 
 echo "Set the default namespace"
 kubectl config set-context --current --namespace=default
@@ -46,7 +41,5 @@ for i in $deployed_charts
       kubectl delete ns $i || true
       kubectl patch pv $i-appsmith -p '{"metadata":{"finalizers":null}}' || true
       kubectl delete pv $i-appsmith --grace-period=0 --force || true
-#      ACCESS_POINT_ID=$(aws efs describe-access-points --file-system-id "$DP_EFS_ID" | jq -r '.AccessPoints[] | select(.Name=="'"ce$pr"'") | .AccessPointId')
-#      aws efs delete-access-point --access-point-id $ACCESS_POINT_ID
     fi
   done
