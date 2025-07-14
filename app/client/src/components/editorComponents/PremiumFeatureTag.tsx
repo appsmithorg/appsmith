da8a4ece7dcc8e06367e0a3859e4ea631561e4c0
jacquesikot <jacquesikot@gmail.com>
2025-07-14 10:15:03 +0100

    feat: enhance PremiumFeatureTag with accessibility support

--- app/client/src/components/editorComponents/PremiumFeatureTag.tsx
+++ app/client/src/components/editorComponents/PremiumFeatureTag.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Icon, Tag } from "@appsmith/ads";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
 
 function PremiumFeatureTag() {
   return (
     <Tag isClosable={false} size="md">
       <Icon name="star-line" size="sm" />
+      <VisuallyHidden>Premium feature</VisuallyHidden>
     </Tag>
   );
 }
