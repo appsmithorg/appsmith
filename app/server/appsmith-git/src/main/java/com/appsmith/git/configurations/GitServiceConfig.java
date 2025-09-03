2177b1a53d55b53a1ae02de51d5b816d62c7f944
sondermanish <manish@appsmith.com>
2025-09-03 14:59:12 +0530

    modified config

--- app/server/appsmith-git/src/main/java/com/appsmith/git/configurations/GitServiceConfig.java
+++ app/server/appsmith-git/src/main/java/com/appsmith/git/configurations/GitServiceConfig.java
@@ -3,18 +3,23 @@ package com.appsmith.git.configurations;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Data
 @Configuration
 public class GitServiceConfig {
 
-    @Value("${appsmith.git.root:}")
+    @Value("${appsmith.git.root}")
     private String gitRootPath;
 
     @Value("gitInitializeRepo/GitConnect-Initialize-Repo-Template")
     private String readmeTemplatePath;
 
     public Boolean isGitInMemory() {
-        return gitRootPath.startsWith("/dev/shm/") || gitRootPath.startsWith("/tmp");
+        if (!StringUtils.hasText(gitRootPath)) {
+            return Boolean.FALSE;
+        }
+
+        return gitRootPath.startsWith("/dev/shm") || gitRootPath.startsWith("/tmp/shm");
     }
 }
