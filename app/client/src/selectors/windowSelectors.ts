c360db0e2d95578610a99cd6af84a1bdeec46a02
Tom Jose <tomjose92@gmail.com>
2025-10-29 20:44:42 +0530

    Added feature flag for enabling window dimensions as part of state. Only when its enabled will the appsmith state object have a ui object. Currently setting it as true.

--- app/client/src/selectors/windowSelectors.ts
+++ app/client/src/selectors/windowSelectors.ts
@@ -1,5 +1,12 @@
 import type { DefaultRootState } from "react-redux";
+import { selectFeatureFlagCheck } from "ee/selectors/featureFlagsSelectors";
+import { FEATURE_FLAG } from "ee/entities/FeatureFlag";
 
 export const getWindowDimensions = (state: DefaultRootState) => {
-  return state.ui.windowDimensions;
+  const isWindowDimensionsEnabled = selectFeatureFlagCheck(
+    state,
+    FEATURE_FLAG.release_window_dimensions_enabled,
+  );
+
+  return isWindowDimensionsEnabled ? state.ui.windowDimensions : undefined;
 };
