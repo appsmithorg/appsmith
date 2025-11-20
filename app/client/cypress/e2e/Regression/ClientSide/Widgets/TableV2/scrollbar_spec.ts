cc5a94d0664824cc370bdbd7665b23186e494989
Rahul Barwal <rahul.barwal@appsmith.com>
2025-11-20 12:16:48 +0530

    Enhance scrollbar height calculation in table widget tests to account for hidden horizontal scrollbar

--- app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/scrollbar_spec.ts
+++ app/client/cypress/e2e/Regression/ClientSide/Widgets/TableV2/scrollbar_spec.ts
@@ -18,14 +18,22 @@ describe(
 
     it("1. Table body should not have the scrollbar", function () {
       let tableHeight = 0;
+      const TABLE_SCROLLBAR_HEIGHT = 10; // From Constants.ts
 
       cy.get(".t--draggable-tablewidgetv2 .table").then(($table) => {
         tableHeight = $table[0].clientHeight;
       });
 
       cy.get(".t--draggable-tablewidgetv2 .table .simplebar-content").then(
-        ($scrollBox) =>
-          expect($scrollBox[0].clientHeight).to.be.equal(tableHeight),
+        ($scrollBox) => {
+          // The scroll container height calculation always accounts for the horizontal scrollbar
+          // by subtracting TABLE_SCROLLBAR_HEIGHT, even when the scrollbar is not visible.
+          // Therefore, we need to account for this in our comparison.
+          const expectedScrollBoxHeight = tableHeight - TABLE_SCROLLBAR_HEIGHT;
+          expect($scrollBox[0].clientHeight).to.be.equal(
+            expectedScrollBoxHeight,
+          );
+        },
       );
     });
   },
