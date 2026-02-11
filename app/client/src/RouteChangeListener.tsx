f70f56393465e394dc03d3e868c16d0368e60793
Luis Ibarra <luissebastianibarrav@gmail.com>
2026-02-11 12:32:42 -0500

    refactor(RouteChangeListener): remove Betterbugs metadata update on route change

--- app/client/src/RouteChangeListener.tsx
+++ app/client/src/RouteChangeListener.tsx
@@ -3,7 +3,6 @@ import { useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useLocation } from "react-router";
 import type { AppsmithLocationState } from "utils/history";
-import BetterbugsUtil from "utils/Analytics/betterbugs";
 
 export default function RouteChangeListener() {
   const location = useLocation<AppsmithLocationState>();
@@ -14,7 +13,6 @@ export default function RouteChangeListener() {
     const prevLocation = prevLocationRef;
 
     dispatch(routeChanged(location, prevLocation.current));
-    BetterbugsUtil.updateMetadata();
     prevLocation.current = location;
   }, [location.pathname, location.hash]);
 
