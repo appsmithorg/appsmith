1f6aead5bf41f3ab4a3ecb97919caa6c6a4a76b9
sondermanish <manish@appsmith.com>
2026-03-04 14:28:14 +0530

    added api timeout

--- app/client/src/api/services/ConsolidatedPageLoadApi/api.ts
+++ app/client/src/api/services/ConsolidatedPageLoadApi/api.ts
@@ -1,4 +1,5 @@
 import { api } from "api/core";
+import { CONSOLIDATED_API_TIMEOUT_MS } from "ee/constants/ApiConstants";
 import type { InitConsolidatedApi } from "sagas/InitSagas";
 import type { ConsolidatedApiParams } from "./types";
 import { ConsolidatedApiUtils } from "./url";
@@ -8,7 +9,9 @@ export const getConsolidatedPageLoadDataView = async (
 ) => {
   const viewUrl = ConsolidatedApiUtils.getViewUrl(params);
 
-  return api.get<InitConsolidatedApi>(viewUrl);
+  return api.get<InitConsolidatedApi>(viewUrl, {
+    timeout: CONSOLIDATED_API_TIMEOUT_MS,
+  });
 };
 
 export const getConsolidatedPageLoadDataEdit = async (
@@ -16,5 +19,7 @@ export const getConsolidatedPageLoadDataEdit = async (
 ) => {
   const editUrl = ConsolidatedApiUtils.getEditUrl(params);
 
-  return api.get<InitConsolidatedApi>(editUrl);
+  return api.get<InitConsolidatedApi>(editUrl, {
+    timeout: CONSOLIDATED_API_TIMEOUT_MS,
+  });
 };
