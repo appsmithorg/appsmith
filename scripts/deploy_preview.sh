702b47f927e5649f58a36346756d5f5ec236c31f
Wyatt Walter <wyattwalter@gmail.com>
2026-04-06 15:36:54 -0500

    fix: setup aws oidc to test

--- scripts/deploy_preview.sh
+++ scripts/deploy_preview.sh
@@ -13,9 +13,6 @@ region = ap-south-1
 output = json
 EOF
 
-aws configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-
 export region="ap-south-1"
 export cluster_name="uatx-cluster"
 
