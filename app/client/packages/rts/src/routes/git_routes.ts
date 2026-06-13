23011a87b031605fbba075785664f100cf3a2d96
orbisai0security <mediratta01.pally@gmail.com>
2026-06-13 10:36:42 +0000

    fix: V-002 security vulnerability
    Automated security fix generated by OrbisAI Security

--- app/client/packages/rts/src/routes/git_routes.ts
+++ app/client/packages/rts/src/routes/git_routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { body } from "express-validator";
 import GitController from "@controllers/git";
 import { Validator } from "@middlewares/Validator";
 
@@ -6,6 +7,11 @@ const router = express.Router();
 const gitController = new GitController();
 const validator = new Validator();
 
-router.post("/reset", validator.validateRequest, gitController.reset);
+router.post(
+  "/reset",
+  body("repoPath").isString().notEmpty().withMessage("repoPath is required and must be a string"),
+  validator.validateRequest,
+  gitController.reset,
+);
 
 export default router;
